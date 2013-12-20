533c6139c242db08de58ca1d00e7dc03c58b46c3
Joost de Valk <joost@yoast.com>
2013-12-20 09:46:47 +0100

    Make `$bad_sitemap` public. Change hook for sitemap redirect Revert change to XSL stylesheet URL as that gives issues on WP Multisite installs

--- inc/class-sitemaps.php
+++ inc/class-sitemaps.php
@@ -25,7 +25,7 @@ class WPSEO_Sitemaps {
 	/**
 	 *     Flag to indicate if this is an invalid or empty sitemap.
 	 */
-	private $bad_sitemap = false;
+	public $bad_sitemap = false;
 
 	/**
 	 * The maximum number of entries per sitemap page
@@ -50,13 +50,12 @@ class WPSEO_Sitemaps {
 			define( "ENT_XML1", 16 );
 
 		add_action( 'init', array( $this, 'init' ), 1 );
-		add_action( 'wp_loaded', array( $this, 'redirect' ), 1 );
+		add_action( 'template_redirect', array( $this, 'redirect' ) );
 		add_filter( 'redirect_canonical', array( $this, 'canonical' ) );
 		add_action( 'wpseo_hit_sitemap_index', array( $this, 'hit_sitemap_index' ) );
 
 		// default stylesheet
-		$abs_main_sitemap_url = str_replace( get_option( 'home' ), $_SERVER['SERVER_NAME'], home_url( 'main-sitemap.xsl' ) );
-		$this->stylesheet = '<?xml-stylesheet type="text/xsl" href="' . $abs_main_sitemap_url . '"?>';
+		$this->stylesheet = '<?xml-stylesheet type="text/xsl" href="' . home_url( 'main-sitemap.xsl' ) . '"?>';
 
 		$this->options = get_wpseo_options();
 	}
