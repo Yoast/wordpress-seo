ab5863a82b61c90377de7008efddeb172227915e
Danny van Kooten <dannyvankooten@gmail.com>
2013-12-28 12:15:21 +0100

    Fixes hash error after being redirected from FB API + some code improvements to wp-seo-admin.js

--- js/wp-seo-admin.js
+++ js/wp-seo-admin.js
@@ -1,20 +1,40 @@
-jQuery(document).ready(function () {
-	jQuery("#enablexmlsitemap").change(function() {
-		if (jQuery("#enablexmlsitemap").is(':checked')) {
-			jQuery("#sitemapinfo").css("display","block");
-		} else {
-			jQuery("#sitemapinfo").css("display","none");
-		}
-	}).change();
-	jQuery("#cleanpermalinks").change(function() {
-		if (jQuery("#cleanpermalinks").is(':checked')) {
-			jQuery("#cleanpermalinksdiv").css("display","block");
-		} else {
-			jQuery("#cleanpermalinksdiv").css("display","none");
-		}
-	}).change();		
-});
+(function($) {
 
+	$(document).ready(function() {
+
+		// events
+		$("#enablexmlsitemap").change(function() {
+			$("#sitemapinfo").toggle($(this).is(':checked'));
+		}).change();
+
+		$("#cleanpermalinks").change(function() {
+			$("#cleanpermalinksdiv").toggle($(this).is(':checked'));
+		}).change();
+
+		$('#wpseo-tabs a').click(function() {
+			$('#wpseo-tabs a').removeClass('nav-tab-active');
+			$('.wpseotab').removeClass('active');
+		
+			var id = $(this).attr('id').replace('-tab','');
+			$('#' + id).addClass('active');
+			$(this).addClass('nav-tab-active');
+		});
+
+		// init
+		var active_tab = window.location.hash.replace('#top#','');
+
+		if ( active_tab == '' || active_tab == '#_=_') {
+			active_tab = $('.wpseotab').attr('id');
+		}
+
+		$('#' + active_tab).addClass('active');
+		$('#' + active_tab + '-tab').addClass('nav-tab-active');
+		
+	});
+
+})(jQuery);
+
+// global functions
 function setWPOption( option, newval, hide, nonce ) {
 	jQuery.post(ajaxurl, { 
 			action: 'wpseo_set_option', 
@@ -39,20 +59,3 @@ function wpseo_killBlockingFiles( nonce ) {
 			jQuery('#block_files').html(data);
 	});
 }
-
-jQuery(document).ready(function(){	
-	var active_tab = window.location.hash.replace('#top#','');
-	if ( active_tab == '' )
-		active_tab = jQuery('.wpseotab').attr('id');
-	jQuery('#'+active_tab).addClass('active');
-	jQuery('#'+active_tab+'-tab').addClass('nav-tab-active');
-	
-	jQuery('#wpseo-tabs a').click(function() {
-		jQuery('#wpseo-tabs a').removeClass('nav-tab-active');
-		jQuery('.wpseotab').removeClass('active');
-	
-		var id = jQuery(this).attr('id').replace('-tab','');
-		jQuery('#'+id).addClass('active');
-		jQuery(this).addClass('nav-tab-active');
-	});
-});
