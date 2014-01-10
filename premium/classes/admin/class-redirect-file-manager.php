6bf214adc7237ac83e31965e6897a0cb4388310d
Barry Kooij <git@barrykooij.nl>
2014-01-10 14:02:27 +0100

    Fixed a bug in the create_upload_dir function

--- premium/classes/admin/class-redirect-file-manager.php
+++ premium/classes/admin/class-redirect-file-manager.php
@@ -30,13 +30,13 @@ class WPSEO_Redirect_File_Manager {
 		wp_mkdir_p( self::get_dir() );
 
 		// Create an empty .htaccess file
-		if ( ! file_exists( self::get_file_dir() . '/.htaccess' ) ) {
-			@file_put_contents( self::get_file_dir() . '/.htaccess', '' );
+		if ( ! file_exists( self::get_dir() . '/.htaccess' ) ) {
+			@file_put_contents( self::get_dir() . '/.htaccess', '' );
 		}
 
 		// Create an empty index.php file
-		if ( ! file_exists( self::get_file_dir() . '/index.php' ) ) {
-			@file_put_contents( self::get_file_dir() . '/index.php', '' );
+		if ( ! file_exists( self::get_dir() . '/index.php' ) ) {
+			@file_put_contents( self::get_dir() . '/index.php', '' );
 		}
 
 		// Create an empty redirect file
