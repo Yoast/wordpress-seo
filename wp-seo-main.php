dace3dfe36b9681617e09c53e5192d330f25b2fd
Barry Kooij <git@barrykooij.nl>
2014-01-10 13:49:29 +0100

    Created a new plugin file for the WP-SEO plugin
    Fixes #20

--- wp-seo-main.php
+++ wp-seo-main.php
@@ -1,31 +1,4 @@
 <?php
-/*
-Plugin Name: WordPress SEO
-Version: 1.4.22
-Plugin URI: http://yoast.com/wordpress/seo/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseoplugin
-Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
-Author: Joost de Valk
-Author URI: http://yoast.com/
-Text Domain: wordpress-seo
-Domain Path: /languages/
-License: GPL v3
-
-WordPress SEO Plugin
-Copyright (C) 2008-2013, Joost de Valk - joost@yoast.com
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 /**
  * @package Main
  */
@@ -71,9 +44,9 @@ function wpseo_init() {
 	if ( isset( $options['enablexmlsitemap'] ) && $options['enablexmlsitemap'] )
 		require_once( WPSEO_PATH . 'inc/class-sitemaps.php' );
 
-    if( file_exists( WPSEO_PATH . 'premium/class-premium.php' ) ) {
-        require_once( WPSEO_PATH . 'premium/class-premium.php' );
-    }
+	if( file_exists( WPSEO_PATH . 'premium/class-premium.php' ) ) {
+		require_once( WPSEO_PATH . 'premium/class-premium.php' );
+	}
 }
 
 /**
