da2d4991adaa4f9a9439737458a897147efb7169
jrfnl <github_nospam@adviesenzo.nl>
2014-01-14 09:34:03 +0100

    comment clarification

--- inc/wpseo-non-ajax-functions.php
+++ inc/wpseo-non-ajax-functions.php
@@ -504,7 +504,7 @@ function allow_custom_field_edits( $allcaps, $cap, $args ) {
 	// but this is safer to check).
 	if ( in_array( $args[0], array( 'edit_post_meta', 'add_post_meta' ) ) ) {
 		// Only allow editing rights for users who have the rights to edit this post and make sure
-		// the meta value starts with _yoast_wpseo.
+		// the meta value starts with _yoast_wpseo (WPSEO_Meta::$meta_prefix).
 		if ( ( isset( $args[2] ) && current_user_can( 'edit_post', $args[2] ) ) && ( ( isset( $args[3] ) && $args[3] !== '' ) && strpos( $args[3], WPSEO_Meta::$meta_prefix ) === 0 ) ) {
 			$allcaps[$args[0]] = true;
 		}
