094b46556fa700b89f46903b175e3749c2d3aa66
Barry Kooij <git@barrykooij.nl>
2014-01-22 21:59:21 +0100

    Fixed a site_url bug

--- premium/classes/admin/class-crawl-issue-table.php
+++ premium/classes/admin/class-crawl-issue-table.php
@@ -106,7 +106,7 @@ class WPSEO_Crawl_Issue_Table extends WP_List_Table {
 		$service = new WPSEO_GWT_Service( $this->gwt );
 
 		// Get crawl issues
-		$crawl_issues = $service->get_crawl_issues( site_url() . '/' );
+		$crawl_issues = $service->get_crawl_issues( site_url( '/' ) );
 
 		// Handle the search
 		if ( null != $this->search_string ) {
