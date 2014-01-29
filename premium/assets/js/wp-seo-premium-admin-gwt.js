29ececdc091c73ad1d9a7f6bdb57609bba1a701c
Barry Kooij <git@barrykooij.nl>
2014-01-29 18:02:51 +0100

    JS GWT confirm box fix

--- premium/assets/js/wp-seo-premium-admin-gwt.js
+++ premium/assets/js/wp-seo-premium-admin-gwt.js
@@ -8,25 +8,28 @@ function wpseo_gwt_open_authorize_code_window(url) {
 
 function create_redirect(old_url) {
 	var new_url = prompt(wpseo_premium_strings.enter_new_url);
-	if (null != new_url && '' != new_url) {
-		jQuery.post(
-				ajaxurl,
-				{
-					action    : 'wpseo_create_redirect',
-					ajax_nonce: jQuery('.wpseo_redirects_ajax_nonce').val(),
-					old_url   : old_url,
-					new_url   : new_url
-				},
-				function (response) {
-					var update_div = jQuery('<div>').addClass('updated').html('<p><bold>' + wpseo_premium_strings.redirect_saved + '</bold></p>');
-					jQuery('.wrap').prepend(update_div);
 
-					jQuery(update_div).delay(500).fadeOut('slow', function () {
-						jQuery(this).remove();
-					});
-				}
-		);
-	} else {
-		alert(wpseo_premium_strings.error_new_url);
+	if (null != new_url) {
+		if ('' != new_url) {
+			jQuery.post(
+					ajaxurl,
+					{
+						action    : 'wpseo_create_redirect',
+						ajax_nonce: jQuery('.wpseo_redirects_ajax_nonce').val(),
+						old_url   : old_url,
+						new_url   : new_url
+					},
+					function (response) {
+						var update_div = jQuery('<div>').addClass('updated').html('<p><bold>' + wpseo_premium_strings.redirect_saved + '</bold></p>');
+						jQuery('.wrap').prepend(update_div);
+
+						jQuery(update_div).delay(500).fadeOut('slow', function () {
+							jQuery(this).remove();
+						});
+					}
+			);
+		} else {
+			alert(wpseo_premium_strings.error_new_url);
+		}
 	}
 }
