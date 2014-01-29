b9df4323ec15d64e64f4a86445c3d0a0365bf59b
Barry Kooij <git@barrykooij.nl>
2014-01-29 17:34:14 +0100

    Added security check to plugin file

--- wp-seo.php
+++ wp-seo.php
@@ -27,6 +27,12 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+if ( ! function_exists( 'add_filter' ) ) {
+	header( 'Status: 403 Forbidden' );
+	header( 'HTTP/1.1 403 Forbidden' );
+	exit();
+}
+
 /**
  * @internal Nobody should be able to overrule the real version number as this can cause serious issues
  * with the options, so no if ( ! defined() )
