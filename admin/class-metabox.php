af246540cdaea5b066171221ecee30759ac22806
Julien Meyer <julien.meyer2@gmail.com>
2014-02-03 13:26:46 +0100

    Add support for html entities
    Decode html entities in post content to support french special characters (some themes store html entity in post table). See Issue : https://github.com/Yoast/wordpress-seo/issues/613

--- admin/class-metabox.php
+++ admin/class-metabox.php
@@ -79,6 +79,10 @@ class WPSEO_Metabox {
 	 * @return string
 	 */
 	public function strtolower_utf8( $string ) {
+		
+		// Prevent comparison between utf8 characters and html entities (é vs &eacute;)
+        	$string = html_entity_decode($string);
+		
 		$convert_to   = array(
 			"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u",
 			"v", "w", "x", "y", "z", "à", "á", "â", "ã", "ä", "å", "æ", "ç", "è", "é", "ê", "ë", "ì", "í", "î", "ï",
