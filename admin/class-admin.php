8f698f6d3190637f25c11477add64449cea403ba
Joost de Valk <joost@yoast.com>
2014-02-23 14:47:21 +0100

    Remove unneeded `dirname` calls.

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -352,7 +352,7 @@ class WPSEO_Admin {
 	 * Register the settings page for the Network settings.
 	 */
 	function register_network_settings_page() {
-		add_menu_page( __( 'Yoast WordPress SEO:', 'wordpress-seo' ) . ' ' . __( 'MultiSite Settings', 'wordpress-seo' ), __( 'SEO', 'wordpress-seo' ), 'delete_users', 'wpseo_dashboard', array( $this, 'network_config_page' ), plugins_url( 'images/yoast-icon.png', dirname( WPSEO_FILE ) ) );
+		add_menu_page( __( 'Yoast WordPress SEO:', 'wordpress-seo' ) . ' ' . __( 'MultiSite Settings', 'wordpress-seo' ), __( 'SEO', 'wordpress-seo' ), 'delete_users', 'wpseo_dashboard', array( $this, 'network_config_page' ), plugins_url( 'images/yoast-icon.png', WPSEO_FILE ) );
 	}
 
 	/**
@@ -493,7 +493,7 @@ class WPSEO_Admin {
 	 * Enqueues the (tiny) global JS needed for the plugin.
 	 */
 	function config_page_scripts() {
-		wp_enqueue_script( 'wpseo-admin-global-script', plugins_url( 'js/wp-seo-admin-global.js', dirname( WPSEO_FILE ) ), array( 'jquery' ), WPSEO_VERSION, true );
+		wp_enqueue_script( 'wpseo-admin-global-script', plugins_url( 'js/wp-seo-admin-global.js', WPSEO_FILE ), array( 'jquery' ), WPSEO_VERSION, true );
 	}
 
 	/**
