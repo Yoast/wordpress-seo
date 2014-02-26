e42728abc2dcd742684ca09e3bf789e6ab126137
Barry Kooij <git@barrykooij.nl>
2014-02-26 20:16:37 +0100

    Upped WPSEO core version
    This won’t be needed once WPSEO 1.5 is released

--- wp-seo-premium.php
+++ wp-seo-premium.php
@@ -27,7 +27,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-define( 'WPSEO_VERSION', '1.4.24' );
+define( 'WPSEO_VERSION', '1.4.25' );
 
 if ( ! defined( 'WPSEO_FILE' ) ) {
 	define( 'WPSEO_FILE', __FILE__ );
