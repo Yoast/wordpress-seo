e772d6e684191ebf9edf0b944cb7ca8bfe11d7e3
Barry Kooij <git@barrykooij.nl>
2014-02-26 15:42:48 +0100

    Version update to 1.4.25

--- wp-seo.php
+++ wp-seo.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WordPress SEO
-Version: 1.4.24
+Version: 1.4.25
 Plugin URI: http://yoast.com/wordpress/seo/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseoplugin
 Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
 Author: Joost de Valk
@@ -64,7 +64,7 @@ if ( version_compare( PHP_VERSION, '5.2', '<' ) ) {
 }
 
 // WordPress SEO version
-define( 'WPSEO_VERSION', '1.4.24' );
+define( 'WPSEO_VERSION', '1.4.25' );
 
 function wpseo_init() {
 	require_once( WPSEO_PATH . 'inc/wpseo-functions.php' );
