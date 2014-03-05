9b941ef9bea53fe63c7cd60a237f3d9feee39a5f
Barry Kooij <git@barrykooij.nl>
2014-03-05 17:04:00 +0100

    Removed allot of methods from WPSEO_GWT, eventually this class will be removed.

--- premium/classes/admin/class-gwt.php
+++ premium/classes/admin/class-gwt.php
@@ -5,6 +5,11 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 	die;
 }
 
+/**
+ * Class WPSEO_GWT
+ *
+ * @todo delete this class
+ */
 class WPSEO_GWT {
 
 	const OPTION_REFRESH_TOKEN = 'wpseo-premium-gwt-refresh_token';
@@ -36,7 +41,7 @@ class WPSEO_GWT {
 	 * The constructor
 	 */
 	public function __construct() {
-		add_action( 'admin_init', array( $this, 'catch_authorization_code_post' ) );
+
 	}
 
 	/**
@@ -57,103 +62,5 @@ class WPSEO_GWT {
 		return get_option( self::OPTION_REFRESH_TOKEN, null );
 	}
 
-	/**
-	 * Get the access token, this is the correct function to retreive the WPSEO GWT access token
-	 *
-	 * @return null
-	 */
-	public function get_access_token() {
-		if ( null === $this->access_token ) {
-
-			// Check if we have a refresh token
-			$refresh_token = $this->get_refresh_token();
-			if ( null !== $refresh_token ) {
-				// We do! Let's get a new access token
-				// Do the actual access token refresh thingy...
-
-				// Create a new client
-				$client = new WPSEO_GWT_Google_Client();
-
-				// Refresh token
-				$response = $client->refreshToken( $refresh_token );
-
-				// Check response
-				if ( '' != $response ) {
-					$response = json_decode( $response );
-
-					// Check if there is an access_token
-					if ( isset( $response->access_token ) ) {
-						// Set access_token
-						$this->access_token = $response->access_token;
-					}
-
-				}
-
-			}
-
-		}
-
-		return $this->access_token;
-	}
-
-	/**
-	 * Do authentication with Google servers and store refresh_token
-	 *
-	 * @param $authorization_code
-	 *
-	 * @return bool
-	 */
-	private function authenticate( $authorization_code ) {
-
-		// Create a client object
-		$client = new WPSEO_GWT_Google_Client();
-
-		// Authenticate client
-		try {
-			$client->authenticate( $authorization_code );
-
-			// Get access response
-			$response = $client->getAccessToken();
-
-			// Check if there is a response body
-			if ( '' != $response ) {
-				$response = json_decode( $response );
-
-				if ( is_object( $response ) ) {
-
-					// Save the refresh token
-					$this->save_refresh_token( $response->refresh_token );
-
-					return true;
-
-				}
-
-			}
-		} catch ( Google_AuthException $exception ) {
-		}
-
-
-		return false;
-
-	}
-
-	/**
-	 * Catch the authorization_code POST and authenticate the user
-	 */
-	public function catch_authorization_code_post() {
-		if ( isset ( $_POST['gwt']['authorization_code'] ) && trim( $_POST['gwt']['authorization_code'] ) != '' ) {
-
-			$redirect_url_appendix = '';
-
-			// Authenticate user
-			if ( ! $this->authenticate( $_POST['gwt']['authorization_code'] ) ) {
-				$redirect_url_appendix = '&error=1';
-			}
-
-			// Redirect user to prevent a post resubmission which causes an oauth error
-			wp_redirect( admin_url( 'admin.php' ) . '?page=' . $_GET['page'] . $redirect_url_appendix );
-			exit;
-		}
-	}
 
 }
