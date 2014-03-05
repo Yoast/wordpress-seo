df4a41217da12d9946693677caadbcc32bf11984
Barry Kooij <git@barrykooij.nl>
2014-03-05 17:03:22 +0100

    Get a new access token by refresh token in the construct of WPSEO_GWT_Google_Client

--- premium/classes/admin/class-gwt-google-client.php
+++ premium/classes/admin/class-gwt-google-client.php
@@ -11,15 +11,41 @@ if ( ! class_exists( 'Google_Client' ) ) {
 
 class WPSEO_GWT_Google_Client extends Google_Client {
 
+	const OPTION_REFRESH_TOKEN = 'wpseo-premium-gwt-refresh_token';
+
 	public function __construct() {
 
 		parent::__construct();
 
+		// Set our settings
 		$this->setApplicationName( "WordPress SEO Premium" ); // Not sure if used
 		$this->setClientId( '887668307827-4jhsr06rntrt3g3ss2r72dblf3ca7msv.apps.googleusercontent.com' );
 		$this->setClientSecret( 'pPW5gLoTNtNHyiDH6YRn-CIB' );
 		$this->setRedirectUri( 'urn:ietf:wg:oauth:2.0:oob' );
 		$this->setScopes( array( 'https://www.google.com/webmasters/tools/feeds/' ) );
+
+		// Let's get an access token if we've got a refresh token
+		$refresh_token = get_option( self::OPTION_REFRESH_TOKEN, '' );
+		if ( '' != $refresh_token ) {
+
+			// Refresh the token
+			$response = $this->refreshToken( $refresh_token );
+
+			// Check response
+			if ( '' != $response ) {
+				$response = json_decode( $response );
+
+				// Check if there is an access_token
+				if ( isset( $response->access_token ) ) {
+
+					// Set access_token
+					$this->setAccessToken( $response->access_token );
+
+				}
+
+			}
+
+		}
 
 	}
 
