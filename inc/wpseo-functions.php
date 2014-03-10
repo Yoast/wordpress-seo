c3981908daf0bb885b8c3b2116bb734507c376dc
Barry Kooij <git@barrykooij.nl>
2014-03-10 15:08:09 +0100

    Removed duplicate methods

--- inc/wpseo-functions.php
+++ inc/wpseo-functions.php
@@ -1095,29 +1095,6 @@ function wpseo_get_term_meta( $term, $taxonomy, $meta ) {
 }
 
 /**
- * Check if the web server is running on Apache
- * @return bool
- */
-function wpseo_is_apache() {
-	if ( isset( $_SERVER['SERVER_SOFTWARE'] ) && stristr( $_SERVER['SERVER_SOFTWARE'], 'apache' ) !== false ) {
-		return true;
-	}
-	return false;
-}
-
-/**
- * Check if the web service is running on Nginx
- *
- * @return bool
- */
-function wpseo_is_nginx() {
-	if ( isset( $_SERVER['SERVER_SOFTWARE'] ) && stristr( $_SERVER['SERVER_SOFTWARE'], 'nginx' ) !== false ) {
-		return true;
-	}
-	return false;
-}
-
-/**
  * WordPress SEO breadcrumb shortcode
  * [wpseo_breadcrumb]
  *
