ee78536838bf7cafd750fdb0a9fc4515b8246069
Barry Kooij <git@barrykooij.nl>
2014-03-11 19:33:35 +0100

    Version bump to 1.0.5

--- premium/changelog.md
+++ premium/changelog.md
@@ -1,3 +1,11 @@
+= 1.0.5 =
+
+* Updated WordPress SEO core to 1.5.2
+
+= 1.0.4 =
+
+* Updated WordPress SEO core to 1.5.0
+
 = 1.0.3 =
 
 * Fixed an incorrect constant define.
--- wp-seo-premium.php
+++ wp-seo-premium.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: WordPress SEO Premium
-Version: 1.0.4
+Version: 1.0.5
 Plugin URI: http://yoast.com/wordpress/seo-premium/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseoplugin
 Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
 Author: Joost de Valk
