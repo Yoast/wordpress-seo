75299eb83af9c2ae67c5d5de48a7fefb0aa8428c
Joost de Valk <joost@yoast.com>
2014-03-11 13:28:58 +0100

    Don't show "ads" for extensions that are already installed.

--- admin/pages/licenses.php
+++ admin/pages/licenses.php
@@ -24,31 +24,53 @@ global $wpseo_admin_pages;
 
 	<div class="tabwrapper">
 		<div id="extensions" class="wpseotab">
+			<?php
+				if ( ! class_exists( 'WPSEO_Premium' ) ) {
+			?>
 			<div class="extension seo-premium">
 				<a target="_blank" href="https://yoast.com/wordpress/plugins/seo-premium/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners"><h3>WordPress SEO Premium</h3></a>
 				<p>The premium version of WordPress SEO with more features & support.</p>
 				<p><a target="_blank" href="https://yoast.com/wordpress/plugins/seo-premium/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners" class="button-primary">Get this extension</a></p>
 			</div>
+			<?php
+				}
+				if ( ! class_exists( 'wpseo_Video_Sitemap' ) ) {
+			?>
 			<div class="extension video-seo">
 				<a target="_blank" href="https://yoast.com/wordpress/plugins/video-seo/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners"><h3>Video SEO</h3></a>
 				<p>Optimize your videos to show them off in search results and get more clicks!</p>
 				<p><a target="_blank" href="https://yoast.com/wordpress/plugins/video-seo/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners" class="button-primary">Get this extension</a></p>
 			</div>
+			<?php
+				}
+				if ( ! defined( 'WPSEO_LOCAL_VERSION' ) ) {
+			?>
 			<div class="extension local-seo">
 				<a target="_blank" href="https://yoast.com/wordpress/plugins/local-seo/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners"><h3>Local SEO</h3></a>
 				<p>Rank better locally and in Google Maps, without breaking a sweat!</p>
 				<p><a target="_blank" href="https://yoast.com/wordpress/plugins/local-seo/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners" class="button-primary">Get this extension</a></p>
 			</div>
+			<?php
+				}
+				if ( ! class_exists( 'wpseo_Video_Manual' ) ) {
+			?>
 			<div class="extension video-manuals">
 				<a target="_blank" href="https://yoast.com/wordpress/plugins/video-manual-wordpress-seo/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners"><h3>WordPress SEO Training Videos</h3></a>
 				<p>Spend less time training your clients on how to use the WordPress SEO plugin!</p>
 				<p><a target="_blank" href="https://yoast.com/wordpress/plugins/video-manual-wordpress-seo/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners" class="button-primary">Get this extension</a></p>
 			</div>
+			<?php
+				}
+				if ( class_exists( 'Woocommerce' ) && ! class_exists( 'Yoast_WooCommerce_SEO' ) ) {
+			?>
 			<div class="extension woocommerce-seo">
 				<a target="_blank" href="https://yoast.com/wordpress/plugins/yoast-woocommerce-seo/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners"><h3>Yoast WooCommerce SEO</h3></a>
 				<p>Seamlessly integrate WooCommerce with WordPress SEO and get extra features!</p>
 				<p><a target="_blank" href="https://yoast.com/wordpress/plugins/yoast-woocommerce-seo/#utm_source=wordpress-seo-config&utm_medium=banner&utm_campaign=extension-page-banners" class="button-primary">Get this extension</a></p>
 			</div>
+			<?php
+				}
+			?>
 		</div>
 		<div id="licenses" class="wpseotab">
 
