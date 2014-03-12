2d7267b989aa4bf7bfe388737956d4f49b220f40
Danny van Kooten <dannyvankooten@gmail.com>
2014-03-12 12:20:11 +0100

    first tests for wpseo_metabox class

--- tests/test-class-wpseo-metabox.php
+++ tests/test-class-wpseo-metabox.php
@@ -26,13 +26,103 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 		$this->post = get_post( $post_id );
 	}
 
-
-
 	/**
 	* Placeholder test to prevent PHPUnit throwing warnings.
 	*/
 	public function test_class_is_tested() {
 		$this->assertTrue( true );
 	}
-	
+
+	/**
+	* @covers WPSEO_Metabox::get_post_date()
+	*/
+	public function test_get_post_date() {
+		$expected_date = date( 'j M Y', strtotime( $this->post->post_date ) );
+		$this->assertEquals( $expected_date, $this->metabox->get_post_date( $this->post ) );
+	}
+		
+	/**
+	* @covers WPSEO_Metabox::enqueue()
+	*/
+	public function test_enqueue_not_firing_on_options_page() {
+		global $pagenow;
+		$pagenow = 'options.php';
+
+		// call enqueue function
+		$this->metabox->enqueue();
+
+		$enqueued = wp_script_is( 'wp-seo-metabox', 'enqueued' );
+		$this->assertFalse( $enqueued );		
+	}
+
+	/**
+	* @covers WPSEO_Metabox::enqueue()
+	*/
+	public function test_enqueue_firing_on_new_post_page() {
+		global $pagenow;
+		$pagenow = 'post-new.php';
+
+		// call enqueue function
+		$this->metabox->enqueue();
+
+		$enqueued = wp_script_is( 'wp-seo-metabox', 'enqueued' );
+		$this->assertTrue( $enqueued );		
+	}
+
+	public function test_column_heading_is_hooked() {
+
+		$this->metabox->setup_page_analysis();
+		$hooked = !! has_filter( 'manage_post_posts_columns', array( $this->metabox, 'column_heading') );
+
+		$this->assertTrue( $hooked );
+	}
+
+	/**
+	* @covers WPSEO_Metabox::column_heading()
+	*/
+	public function test_column_heading_has_score( ) {
+		$columns = $this->metabox->column_heading( array() );
+		$this->assertArrayHasKey( 'wpseo-score', $columns );
+	}
+
+	/**
+	* @covers WPSEO_Metabox::column_heading()
+	*/
+	public function test_column_heading_has_focuskw( ) {
+		$columns = $this->metabox->column_heading( array() );
+		$this->assertArrayHasKey( 'wpseo-focuskw', $columns );
+	}
+
+	/**
+	* @covers WPSEO_Metabox::column_heading()
+	*/
+	public function test_column_heading_has_metadesc( ) {
+		$columns = $this->metabox->column_heading( array() );
+		$this->assertArrayHasKey( 'wpseo-metadesc', $columns );
+	}
+
+	/**
+	* @covers WPSEO_Metabox::strtolower_utf8()
+	*/
+	public function test_strtolower_utf8() {
+		$input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜÝАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЪЬЭЮЯĄĆĘŁŃÓŚŹŻ';
+		$expected_output = 'abcdefghijklmnopqrstuvwxyzàáâãäåæçèéêëìíîïðñòóôõöøùúûüýабвгдеёжзийклмнопрстуфхцчшщъъьэюяąćęłńóśźż';
+		$this->assertEquals( $expected_output, $this->metabox->strtolower_utf8( $input ) );
+	}
+
+	public function test_add_metabox() {
+		global $wp_meta_boxes;
+
+		$this->metabox->add_meta_box();
+
+		$post_types = get_post_types( array( 'public' => true ) );
+
+		// test if all post types have the wpseo_meta metabox
+		foreach( $post_types as $post_type) {
+			$this->assertArrayHasKey( 'wpseo_meta', $wp_meta_boxes[$post_type]['normal']['high'] );
+		}
+	}
+
+
+
 }
