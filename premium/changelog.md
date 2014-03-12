e50d58348afc6320128006758b2816a322826c2e
Barry Kooij <git@barrykooij.nl>
2014-03-12 15:02:48 +0100

    Bumped version to 1.0.6

--- premium/changelog.md
+++ premium/changelog.md
@@ -1,3 +1,7 @@
+= 1.0.6 =
+
+* Updated WordPress SEO core to 1.5.2.1
+
 = 1.0.5 =
 
 * Updated WordPress SEO core to 1.5.2
--- premium/class-premium.php
+++ premium/class-premium.php
@@ -20,8 +20,8 @@ class WPSEO_Premium {
 
 	const OPTION_CURRENT_VERSION = 'wpseo_current_version';
 
-	const PLUGIN_VERSION_NAME = '1.0.5';
-	const PLUGIN_VERSION_CODE = '6';
+	const PLUGIN_VERSION_NAME = '1.0.6';
+	const PLUGIN_VERSION_CODE = '7';
 	const PLUGIN_AUTHOR       = 'Yoast';
 	const EDD_STORE_URL       = 'https://yoast.com';
 	const EDD_PLUGIN_NAME     = 'WordPress SEO Premium';
