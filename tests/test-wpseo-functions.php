3e1b36db41380ef16050cc3873ff64ff835c71d2
Danny van Kooten <dannyvankooten@gmail.com>
2014-03-13 16:59:16 +0100

    added a few wpseo-functions tests

--- tests/test-wpseo-functions.php
+++ tests/test-wpseo-functions.php
@@ -15,6 +15,28 @@ class WPSEO_Functions_Test extends WPSEO_UnitTestCase {
 		update_option( 'wpseo_xml', $options );
 	}
 
+	/**
+	* @covers wpseo_is_apache()
+	*/
+	public function test_wpseo_is_apache() {
+		$_SERVER['SERVER_SOFTWARE'] = 'Apache/2.2.22';
+		$this->assertTrue( wpseo_is_apache() );
+
+		$_SERVER['SERVER_SOFTWARE'] = 'nginx/1.5.11';
+		$this->assertFalse( wpseo_is_apache() );
+	}
+
+	/**
+	* @covers test_wpseo_is_nginx()
+	*/
+	public function test_wpseo_is_nginx() {
+		$_SERVER['SERVER_SOFTWARE'] = 'nginx/1.5.11';
+		$this->assertTrue( wpseo_is_nginx() );
+
+		$_SERVER['SERVER_SOFTWARE'] = 'Apache/2.2.22';
+		$this->assertFalse( wpseo_is_nginx() );
+	}
+
 	/*
 	* dummy test to prevent warning
 	*/
