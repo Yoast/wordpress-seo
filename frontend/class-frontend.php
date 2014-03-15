b00ff1da7eaed36b94d3ed688d08f17fac3517ca
Joost de Valk <joost@yoast.com>
2014-03-15 20:04:54 +0100

    `wp_head` is actually too late as the `<title>` may have already been echoed... A very late hook on `template_redirect` is a better solution. Related to #817.

--- frontend/class-frontend.php
+++ frontend/class-frontend.php
@@ -102,7 +102,7 @@ if ( ! class_exists( 'WPSEO_Frontend' ) ) {
 			add_filter( 'the_excerpt_rss', array( $this, 'embed_rssfooter_excerpt' ) );
 
 			if ( $this->options['forcerewritetitle'] === true ) {
-				add_action( 'wp_head', array( $this, 'force_rewrite_output_buffer' ), -1 );
+				add_action( 'template_redirect', array( $this, 'force_rewrite_output_buffer' ), 99999 );
 				add_action( 'wp_footer', array( $this, 'flush_cache' ), -1 );
 			}
 
