0721c75a20c5f534c8ce014adb45d5f4110aacd9
jrfnl <github_nospam@adviesenzo.nl>
2014-03-24 22:12:24 +0100

    Fix for disabled ctype extension *sigh*

--- inc/wpseo-functions.php
+++ inc/wpseo-functions.php
@@ -1033,6 +1033,26 @@ function wpseo_shortcode_yoast_breadcrumb() {
 add_shortcode( 'wpseo_breadcrumb', 'wpseo_shortcode_yoast_breadcrumb' );
 
 
+/**
+ * Emulate PHP native ctype_digit() function for when the ctype extension would be disabled *sigh*
+ * Only emulates the behaviour for when the input is a string, does not handle integer input as ascii value
+ *
+ * @param	string	$string
+ *
+ * @return 	bool
+ */
+if ( ! extension_loaded( 'ctype' ) || ! function_exists( 'ctype_digit' ) ) {
+	function ctype_digit( $string ) {
+		$return = false;
+		if ( ( is_string( $string ) && $string !== '' ) && preg_match( '`^\d+$`', $string ) === 1 ){
+			$return = true;
+		}
+		return $return;
+	}
+}
+
+
+
 /********************** DEPRECATED FUNCTIONS **********************/
 
 
