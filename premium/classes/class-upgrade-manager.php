ab0f5f8183dabd312c55349fa57d34c20eefc5ac
Barry Kooij <git@barrykooij.nl>
2014-05-09 17:00:46 +0200

    Upgrade manager formats redirects.

--- premium/classes/class-upgrade-manager.php
+++ premium/classes/class-upgrade-manager.php
@@ -1,6 +1,8 @@
 <?php
 
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
 
 class WPSEO_Upgrade_Manager {
 
@@ -13,7 +15,7 @@ class WPSEO_Upgrade_Manager {
 		$current_version = get_site_option( WPSEO_Premium::OPTION_CURRENT_VERSION, 1 );
 
 		// Check if update is required
-		if( WPSEO_Premium::PLUGIN_VERSION_CODE > $current_version ) {
+		if ( WPSEO_Premium::PLUGIN_VERSION_CODE > $current_version ) {
 
 			// Do update
 			$this->do_update( $current_version );
@@ -49,6 +51,45 @@ class WPSEO_Upgrade_Manager {
 			// Remove old license options
 			delete_option( 'wpseo_license_key' );
 			delete_option( 'wpseo_license_status' );
+
+		}
+
+		// Upgrade to version 1.2.0
+		if ( $current_version < 13 ) {
+
+			/**
+			 * Upgrade redirects
+			 */
+
+			// URL Redirects
+			$url_redirect_manager = new WPSEO_URL_Redirect_Manager();
+			$url_redirects        = $url_redirect_manager->get_redirects();
+
+			// Loop through the redirects
+			foreach ( $url_redirects as $old_url => $redirect ) {
+				// Check if the redirect is not an array yet
+				if ( ! is_array( $redirect ) ) {
+					$url_redirects[ $old_url ] = array( 'url' => $redirect, 'type' => '301' );
+				}
+			}
+
+			// Save the URL redirects
+			$url_redirect_manager->save_redirects( $url_redirects );
+
+			// Regex Redirects
+			$regex_redirect_manager = new WPSEO_REGEX_Redirect_Manager();
+			$regex_redirects        = $regex_redirect_manager->get_redirects();
+
+			// Loop through the redirects
+			foreach ( $regex_redirects as $old_url => $redirect ) {
+				// Check if the redirect is not an array yet
+				if ( ! is_array( $redirect ) ) {
+					$regex_redirects[ $old_url ] = array( 'url' => $redirect, 'type' => '301' );
+				}
+			}
+
+			// Save the URL redirects
+			$regex_redirect_manager->save_redirects( $regex_redirects );
 
 		}
 
