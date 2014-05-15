bd9c1baaedfa8f6b9b8556afb56d5099c4e75a34
Danny van Kooten <dannyvankooten@gmail.com>
2014-05-15 14:12:46 +0200

    Added first basic wpseo_replace_vars test.

--- tests/test-wpseo-functions.php
+++ tests/test-wpseo-functions.php
@@ -14,11 +14,46 @@ class WPSEO_Functions_Test extends WPSEO_UnitTestCase {
 	}
 
 	public function test_wpseo_remove_capabilities() {
-		// TODO
+
 	}
 
+	/**
+	 * @covers wpseo_replace_vars
+	 */
 	public function test_wpseo_replace_vars() {
-		// TODO
+
+		// create author
+		$user_id = $this->factory->user->create(
+			array(
+				'user_login' => 'User_Login',
+				'display_name' => 'User_Nicename'
+			)
+		);
+
+		// create post
+		$post_id = $this->factory->post->create(
+			array(
+				'post_title' => 'Post_Title',
+				'post_content' => 'Post_Content',
+				'post_excerpt' => 'Post_Excerpt',
+				'post_author' => $user_id,
+				'post_date' => date( 'Y-m-d H:i:s', strtotime( '2000-01-01 2:30:00' ) )
+			)
+		);
+
+		// get post
+		$post = get_post( $post_id );
+
+		$input = '%%title%% %%excerpt%% %%date%% %%name%%';
+		$expected = "Post_Title Post_Excerpt ". mysql2date( get_option( 'date_format' ), $post->post_date , true ) ." User_Nicename";
+		$output = wpseo_replace_vars( $input, (array) $post );
+		$this->assertEquals( $expected, $output );
+
+		/*
+			TODO
+			- Test all Basic Variables
+			- Test all Advanced Variables
+		 */
 	}
 
 	public function test_wpseo_get_terms() {
