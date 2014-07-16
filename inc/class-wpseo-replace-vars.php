a5395dfeec82915438cb520d77af14ffb4f924a2
Barry Kooij <git@barrykooij.nl>
2014-07-16 13:26:02 +0200

    Use the correct separator in retrieve_sep()

--- inc/class-wpseo-replace-vars.php
+++ inc/class-wpseo-replace-vars.php
@@ -406,6 +406,17 @@ if ( ! class_exists( 'WPSEO_Replace_Vars' ) ) {
 				$replacement = $GLOBALS['sep'];
 			}
 
+			// Get the titles option and the separator options
+			$titles_options    = get_option( 'wpseo_titles' );
+			$seperator_options = WPSEO_Option_Titles::get_instance()->get_separator_options();
+
+			// This should always be set, but just to be sure
+			if ( isset( $seperator_options[ $titles_options['separator'] ] ) ) {
+				
+				// Set the new replacement
+				$replacement = $seperator_options[ $titles_options['separator'] ];
+			}
+
 			/**
 			 * Filter: 'wpseo_replacements_filter_sep' - Allow customization of the separator character(s)
 			 *
