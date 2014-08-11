08b7c245d7099290a872b8ee740ec8e3fe4a3e24
andizer <andy@Andys-MacBook-Pro.local>
2014-08-11 11:56:17 +0200

    more code style...

--- admin/class-bulk-editor-list-table.php
+++ admin/class-bulk-editor-list-table.php
@@ -3,14 +3,14 @@
  * @package Admin
  */
 
-if ( !defined( 'WPSEO_VERSION' ) ) {
+if ( ! defined( 'WPSEO_VERSION' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
 }
 
 
-if ( !class_exists( 'WPSEO_Bulk_List_Table' ) ) {
+if ( ! class_exists( 'WPSEO_Bulk_List_Table' ) ) {
 	/**
 	 *
 	 */
@@ -546,7 +546,7 @@ if ( !class_exists( 'WPSEO_Bulk_List_Table' ) ) {
 								break;
 
 							case 'col_existing_yoast_seo_title':
-								$cell_value = ( ( !empty( $meta_data[WPSEO_Meta::$meta_prefix . 'title'] ) ) ? $meta_data[WPSEO_Meta::$meta_prefix . 'title'] : '' );
+								$cell_value = ( ( ! empty( $meta_data[WPSEO_Meta::$meta_prefix . 'title'] ) ) ? $meta_data[WPSEO_Meta::$meta_prefix . 'title'] : '' );
 								echo sprintf( '<td %2$s id="wpseo-existing-title-%3$s">%1$s</td>', $cell_value, $attributes, $rec->ID );
 								break;
 
