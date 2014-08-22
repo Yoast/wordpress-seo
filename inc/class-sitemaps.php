0ca9227430b81b7207ad848d2602ae265e52fc70
andizer <andy@Andys-MacBook-Pro.local>
2014-08-22 23:56:56 +0200

    Removing array_key_exists

--- inc/class-sitemaps.php
+++ inc/class-sitemaps.php
@@ -1377,11 +1377,9 @@ if ( ! class_exists( 'WPSEO_Sitemaps' ) ) {
 					$is_exclude_on = get_the_author_meta( 'wpseo_excludeauthorsitemap', $user->ID );
 					if ( $is_exclude_on === 'on' ) {
 						$exclude_user = true;
-					} elseif ( array_key_exists( 'disable_author_noposts', $options ) ) {
-						if ( $options['disable_author_noposts'] == 1 ) {
-							$count_posts  = count_user_posts( $user->ID );
-							$exclude_user = $count_posts == 0;
-						}
+					} elseif ( $options['disable_author_noposts'] === true ) {
+						$count_posts  = count_user_posts( $user->ID );
+						$exclude_user = $count_posts == 0;
 					} else {
 						$user_role    = $user->roles[0];
 						$target_key   = "user_role-{$user_role}-not_in_sitemap";
