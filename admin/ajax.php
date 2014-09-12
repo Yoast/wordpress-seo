32da1612de6691a11ba43e06506c75967602d19c
andizer <andy@Andys-MacBook-Pro.local>
2014-09-12 12:12:13 +0200

    one more code style thing

--- admin/ajax.php
+++ admin/ajax.php
@@ -207,7 +207,7 @@ function wpseo_upsert_meta( $post_id, $new_meta_value, $orig_meta_value, $meta_k
 
 	if ( $sanitized_new_meta_value === $orig_meta_value && $sanitized_new_meta_value !== $new_meta_value ) {
 		$upsert_results['status']  = 'failure';
-		$upsert_results['results'] =  __( 'You have used HTML in your value which is not allowed.', 'wordpress-seo' );
+		$upsert_results['results'] = __( 'You have used HTML in your value which is not allowed.', 'wordpress-seo' );
 
 		return $upsert_results;
 	}
