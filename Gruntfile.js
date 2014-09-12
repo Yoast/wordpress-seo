391f8634e76e6a2750daf2408078bc8850f7189a
Joost de Valk <joost@yoast.com>
2014-09-12 15:37:33 +0200

    Grunt all the things.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,206 +1,285 @@
 /* global require */
-module.exports = function(grunt) {
-    'use strict';
+module.exports = function (grunt) {
+	'use strict';
 
-    require('load-grunt-tasks')(grunt, {
-        pattern: ['grunt-*', 'assemble-less']
-    });
+	require('load-grunt-tasks')(grunt, {
+		pattern: ['grunt-*', 'assemble-less']
+	});
 
-    require('time-grunt')(grunt);
+	require('time-grunt')(grunt);
 
-    // Project configuration.
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
+	// Project configuration.
+	grunt.initConfig({
+		pkg       : grunt.file.readJSON('package.json'),
 
-        // Watch source files
-        watch: {
-            gruntfile: {
-                files: ['Gruntfile.js'],
-                tasks: ['jshint:grunt', 'jsvalidate', 'jscs']
-            },
-            php: {
-                files: ['**/*.php', '*/*.php', '!node_modules/**'],
-                tasks: ['phplint']
-            },
-            js: {
-                files: [
-                    'js/*.js'
-                ],
-                tasks: ['build:js', 'jshint', 'jsvalidate', 'jscs']
-            }
-        },
+		// Watch source files
+		watch     : {
+			gruntfile: {
+				files: ['Gruntfile.js'],
+				tasks: ['jshint:grunt', 'jsvalidate', 'jscs']
+			},
+			php      : {
+				files: ['**/*.php', '*/*.php', '!node_modules/**'],
+				tasks: ['phplint', 'phpcs']
+			},
+			js       : {
+				files: [
+					'js/*.js'
+				],
+				tasks: ['jshint', 'jsvalidate', 'jscs', 'build:js']
+			},
+			css      : {
+				tasks: ['build:css']
+			}
+		},
 
-        // JavaScript
+		// JavaScript
 
-        // Lint JS code practices
-        jshint: {
-            grunt: {
-                options: {
-                    jshintrc: '.gruntjshintrc'
-                },
-                src: ['Gruntfile.js']
-            }
-        },
+		// Lint JS code practices
+		jshint    : {
+			grunt: {
+				options: {
+					jshintrc: '.gruntjshintrc'
+				},
+				src    : ['Gruntfile.js']
+			}
+		},
 
-        // Lint JS for code standards
-        jscs: {
-            options: {
-                config: '.jscsrc'
-            },
-            all: {
-                files: {
-                    src: [
-                        'Gruntfile.js',
-                        '.gruntjshintrc',
-                        '.jshintrc',
-                        'package.json'
-                    ]
-                }
-            }
-        },
+		// Lint JS for code standards
+		jscs      : {
+			options: {
+				config: '.jscsrc'
+			},
+			all    : {
+				files: {
+					src: [
+						'Gruntfile.js',
+						'.gruntjshintrc',
+						'.jshintrc',
+						'package.json'
+					]
+				}
+			}
+		},
 
-        // Lint JSON files for syntax errors
-        jsonlint: {
-            all: {
-                src: [
-                    '.gruntjshintrc',
-                    '.jshintrc',
-                    'package.json'
-                ]
-            }
-        },
+		// Lint JSON files for syntax errors
+		jsonlint  : {
+			all: {
+				src: [
+					'.gruntjshintrc',
+					'.jshintrc',
+					'package.json'
+				]
+			}
+		},
 
-        // Lint .js files for syntax errors
-        jsvalidate: {
-            all: {
-                options: {
-                    verbose: true
-                },
-                files: {
-                    src: [
-                        'Gruntfile.js'
-                    ]
-                }
-            }
-        },
+		// Lint .js files for syntax errors
+		jsvalidate: {
+			all: {
+				options: {
+					verbose: true
+				},
+				files  : {
+					src: [
+						'Gruntfile.js'
+					]
+				}
+			}
+		},
 
-        // PHP
+		uglify : {
+			'wordpres-seo': {
+				options: {
+					preserveComments: 'some',
+					report          : 'gzip'
+				},
+				files  : [
+					{
+						expand: true,
+						cwd   : 'js',
+						src   : ['*.js', '!*.min.js'],
+						dest  : 'js',
+						ext   : '.min.js',
+						extDot: 'first',
+						isFile: true
+					}
+				]
+			}
+		},
 
-        // Lint .php files for syntax errors
-        phplint: {
-            all: ['**/*.php', '!node_modules/**']
-        },
+		// CSS
+		autoprefixer: {
+			options: {
+				browsers: [
+					'last 1 versions',
+					'Explorer >= 8'
+				]
+			},
+			all: {
+				src: [
+					'css/*.css', '!css/*.min.css'
+				],
+				options: {
+					// diff: 'tmp/autoprefixer.patch'
+				}
+			}
+		},
 
-        // Lint .php files for code standards
-        phpcs: {
-            all: {
-                //adjust these to the folder you do and don't want to be treated
-                dir: ['**/*.php', '!admin/license-manager/**', '!node_modules/**']
-            },
-            options: {
-                standard: 'codesniffer.xml',
-                reportFile: 'phpcs.txt',
-                ignoreExitCode: true
-            }
-        },
+		csscomb: {
+			css: {
+				expand: true,
+				src   : ['css/*.css', '!css/*.min.css']
+			}
+		},
 
-        // Optimize images to save bytes
-        imagemin: {
-            images: {
-                files: [{
-                    expand: true,
-                    // this would require the addition of a assets folder from which the images are
-                    // processed and put inside the images folder
-                    cwd: 'assets/images/',
-                    src: ['*.*'],
-                    dest: 'images'
-                }]
-            }
-        },
+		cssbeautifier : {
+			files : ['css/*.css', '!css/*.min.css'],
+			options : {
+				indent: '\t',
+				openbrace: 'end-of-line',
+				autosemicolon: true
+			}
+		},
 
-        // I18n
-        addtextdomain: {
-            options: {
-                textdomain: 'wordpress-seo'
-            },
-            php: {
-                files: {
-                    src: [
-                        '*php', '**/*.php', '!admin/license-manager/**', '!node_modules/**'
-                    ]
-                }
-            }
-        },
+		cssmin: {
+			minify: {
+				expand: true,
+				cwd   : 'css/',
+				src   : ['*.css', '!*.min.css'],
+				dest  : 'css/',
+				ext   : '.min.css'
+			}
+		},
 
-        checktextdomain: {
-            options: {
-                text_domain: 'wordpress-seo',
-                keywords: [
-                    '__:1,2d',
-                    '_e:1,2d',
-                    '_x:1,2c,3d',
-                    '_ex:1,2c,3d',
-                    '_n:1,2,4d',
-                    '_nx:1,2,4c,5d',
-                    '_n_noop:1,2,3d',
-                    '_nx_noop:1,2,3c,4d',
-                    'esc_attr__:1,2d',
-                    'esc_html__:1,2d',
-                    'esc_attr_e:1,2d',
-                    'esc_html_e:1,2d',
-                    'esc_attr_x:1,2c,3d',
-                    'esc_html_x:1,2c,3d'
-                ]
-            },
-            files: {
-                expand: true,
-                src: [
-                    '**/*.php', '!node_modules/**'
-                ]
-            }
-        },
+		// PHP
 
-        makepot: {
-            plugin: {
-                options: {
-                    domainPath: '/languages',
-                    potFilename: 'wordpress-seo.pot',
-                    processPot: function(pot) {
-                        pot.headers['report-msgid-bugs-to'] = 'http://wordpress.org/support/plugin/wordpress-seo\n';
-                        pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
-                        pot.headers['last-translator'] = 'Remkus de Vries <translations@yoast.com>\n';
-                        pot.headers['language-team'] = 'Yoast Translate <translations@yoast.com>\n';
-                        pot.headers['x-generator'] = 'grunt-wp-i18n 0.4.4';
-                        pot.headers['x-poedit-basepath'] = '.';
-                        pot.headers['x-poedit-language'] = 'English';
-                        pot.headers['x-poedit-country'] = 'UNITED STATES';
-                        pot.headers['x-poedit-sourcecharset'] = 'utf-8';
-                        pot.headers['x-poedit-keywordslist'] = '__;_e;_x:1,2c;_ex:1,2c;_n:1,2; _nx:1,2,4c;_n_noop:1,2;_nx_noop:1,2,3c;esc_attr__; esc_html__;esc_attr_e; esc_html_e;esc_attr_x:1,2c; esc_html_x:1,2c;';
-                        pot.headers['x-poedit-bookmarks'] = '';
-                        pot.headers['x-poedit-searchpath-0'] = '.';
-                        pot.headers['x-textdomain-support'] = 'yes';
-                        return pot;
-                    },
-                    type: 'wp-plugin'
-                }
-            }
-        }
-    });
+		// Lint .php files for syntax errors
+		phplint      : {
+			all: ['**/*.php', '!node_modules/**']
+		},
 
-    grunt.registerTask('check', [
-        'jshint',
-        'jsonlint',
-        'jsvalidate',
-        'checktextdomain'
-    ]);
+// Lint .php files for code standards
+		phpcs        : {
+			all    : {
+				//adjust these to the folder you do and don't want to be treated
+				dir: ['**/*.php', '!admin/license-manager/**', '!node_modules/**']
+			},
+			options: {
+				standard      : 'codesniffer.xml',
+				reportFile    : 'phpcs.txt',
+				ignoreExitCode: true
+			}
+		},
 
-    grunt.registerTask('build:i18n', [
-        'addtextdomain',
-        'makepot'
-    ]);
+// Optimize images to save bytes
+		imagemin     : {
+			images: {
+				files: [{
+					expand: true,
+					// this would require the addition of a assets folder from which the images are
+					// processed and put inside the images folder
+					cwd   : 'assets/images/',
+					src   : ['*.*'],
+					dest  : 'images'
+				}]
+			}
+		},
 
-    grunt.registerTask('default', [
-        'build'
-    ]);
+// I18n
+		addtextdomain: {
+			options: {
+				textdomain: 'wordpress-seo'
+			},
+			php    : {
+				files: {
+					src: [
+						'*php', '**/*.php', '!admin/license-manager/**', '!node_modules/**'
+					]
+				}
+			}
+		},
+
+		checktextdomain: {
+			options: {
+				text_domain: 'wordpress-seo',
+				keywords   : [
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d',
+					'esc_attr__:1,2d',
+					'esc_html__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_html_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'esc_html_x:1,2c,3d'
+				]
+			},
+			files  : {
+				expand: true,
+				src   : [
+					'**/*.php', '!node_modules/**'
+				]
+			}
+		},
+
+		makepot: {
+			plugin: {
+				options: {
+					domainPath : '/languages',
+					potFilename: 'wordpress-seo.pot',
+					processPot : function (pot) {
+						pot.headers['report-msgid-bugs-to'] = 'http://github.com/yoast/wordpress-seo\n';
+						pot.headers['plural-forms'] = 'nplurals=2; plural=n != 1;';
+						pot.headers['last-translator'] = 'Remkus de Vries <translations@yoast.com>\n';
+						pot.headers['language-team'] = 'Yoast Translate <translations@yoast.com>\n';
+						pot.headers['x-generator'] = 'grunt-wp-i18n 0.4.4';
+						pot.headers['x-poedit-basepath'] = '.';
+						pot.headers['x-poedit-language'] = 'English';
+						pot.headers['x-poedit-country'] = 'UNITED STATES';
+						pot.headers['x-poedit-sourcecharset'] = 'utf-8';
+						pot.headers['x-poedit-keywordslist'] = '__;_e;_x:1,2c;_ex:1,2c;_n:1,2; _nx:1,2,4c;_n_noop:1,2;_nx_noop:1,2,3c;esc_attr__; esc_html__;esc_attr_e; esc_html_e;esc_attr_x:1,2c; esc_html_x:1,2c;';
+						pot.headers['x-poedit-bookmarks'] = '';
+						pot.headers['x-poedit-searchpath-0'] = '.';
+						pot.headers['x-textdomain-support'] = 'yes';
+						return pot;
+					},
+					type       : 'wp-plugin'
+				}
+			}
+		}
+	})
+	;
+
+	grunt.registerTask('check', [
+		'jshint',
+		'jsonlint',
+		'jsvalidate',
+		'checktextdomain'
+	]);
+
+	grunt.registerTask('build:css', [
+		'autoprefixer',
+		'csscomb',
+		'cssbeautifier',
+		'cssmin'
+	]);
+
+	grunt.registerTask('build:js', [
+		'uglify'
+	]);
+
+	grunt.registerTask('build:i18n', [
+		'addtextdomain',
+		'makepot'
+	]);
+
+	grunt.registerTask('default', [
+		'build'
+	]);
 
 };
