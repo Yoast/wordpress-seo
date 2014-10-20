06a58551ac6c3a2bab34423803fd7cc42de4f1bb
michigehrig <exciting91@users.noreply.github.com>
2014-10-20 17:40:58 +0200

    Revert "Adding check that was removed earlier.[Finishes #80630282]"

--- inc/class-sitemaps.php
+++ inc/class-sitemaps.php
@@ -754,13 +754,9 @@ if ( ! class_exists( 'WPSEO_Sitemaps' ) ) {
 						$url = array();
 
 						if ( isset( $p->post_modified_gmt ) && $p->post_modified_gmt != '0000-00-00 00:00:00' && $p->post_modified_gmt > $p->post_date_gmt ) {
-							$url['mod'] = $p->post_modified_gmt;
+							$url['mod'] = $p->post_date_gmt;
 						} else {
-							if ( '0000-00-00 00:00:00' != $p->post_date_gmt ) {
-								$url['mod'] = $p->post_date_gmt;
-							} else {
-								$url['mod'] = $p->post_date;
-							}
+							$url['mod'] = $p->post_date;
 						}
 
 						$url['loc'] = get_permalink( $p );
