ad6d57144dc483f189f73ce7b1e83b4f9e09ecc2
Andy Meerwaldt <andy@yoast.com>
2014-10-30 13:53:49 +0100

    Adding filters for hiding notifications.

--- premium/classes/class-post-watcher.php
+++ premium/classes/class-post-watcher.php
@@ -1,6 +1,15 @@
 <?php
 
-class WPSEO_Post_Watcher {
+class WPSEO_Post_Watcher extends WPSEO_Watcher {
+
+	/**
+	 * Type of watcher.
+	 *
+	 * This will be used for the filters.
+	 *
+	 * @var string
+	 */
+	protected $watch_type = 'post';
 
 	/**
 	 * Add an extra field to post edit screen so we know the old url in the 'post_updated' hook
@@ -8,25 +17,19 @@ class WPSEO_Post_Watcher {
 	 * @param $post
 	 */
 	public function old_url_field( $post ) {
-
 		// $post must be set
 		if ( null != $post ) {
+			$url = $this->get_target_url( $post->ID );
 
-			// Use the correct URL path
-			$url = parse_url( get_permalink( $post->ID ) );
-			$url = $url['path'];
-
-			// Output the hidden field
-			echo '<input type="hidden" name="wpseo_old_url" value="' . esc_attr( $url ) . '"/>';
-
+			echo $this->parse_url_field( $url );
 		}
-
 	}
+
 
 	/**
 	 * Detect if the slug changed, hooked into 'post_updated'
 	 *
-	 * @param $post_id
+	 * @param integer $post_id
 	 * @param $post
 	 * @param $post_before
 	 */
@@ -37,34 +40,19 @@ class WPSEO_Post_Watcher {
 		}
 
 		// Get the new URL
-		$new_url = parse_url( get_permalink( $post_id ) );
-		$new_url = $new_url['path'];
+		$new_url = $this->get_target_url( $post_id );
 
 		// Get the old URL
 		$old_url = esc_url( $_POST['wpseo_old_url'] );
 
-		// Get the site URL
-		$site = parse_url( get_site_url() );
-
 		// Check if we should create a redirect
-		if ( in_array( $post->post_status, array(
-				'publish',
-				'static'
-			) ) && $old_url != $new_url && $old_url != '/' && ( !isset( $site['path'] ) || ( isset( $site['path'] ) && $old_url != $site['path'] . '/' ) )
-		) {
-
-			// The URL redirect manager
-			$redirect_manager = new WPSEO_URL_Redirect_Manager();
-
-			// Create the redirect
-			$redirect_manager->create_redirect( $old_url, $new_url, 301 );
-
+		if ( in_array( $post->post_status, array( 'publish', 'static' ) ) && $this->should_create_redirect( $old_url, $new_url ) ) {
 			// Format the message
-			$message = sprintf( __( "WordPress SEO Premium created a <a href='%s'>redirect</a> from the old post URL to the new post URL. <a href='%s'>Click here to undo this</a>.", 'wordpress-seo' ), admin_url( 'admin.php?page=wpseo_redirects&s=' . urlencode( $old_url ) ), 'javascript:wpseo_undo_redirect("' . urlencode( $old_url ) . '", "' . wp_create_nonce( 'wpseo-redirects-ajax-security' ) . '");' );
+			$message = sprintf( __( "WordPress SEO Premium created a <a href='%s'>redirect</a> from the old post URL to the new post URL. <a href='%s'>Click here to undo this</a>.", 'wordpress-seo' ), $this->admin_redirect_url( $old_url ), $this->javascript_undo_redirect( $old_url ) );
 
-			// Add the message to the notifications center
-			Yoast_Notification_Center::get()->add_notification( new Yoast_Notification( $message ) );
+			$this->create_redirect( $old_url, $new_url );
 
+			$this->create_notification( $message, 'slug_change' );
 		}
 
 	}
@@ -76,26 +64,12 @@ class WPSEO_Post_Watcher {
 	 */
 	public function detect_post_trash( $post_id ) {
 
-		// Get the post
-		$post = get_post( $post_id );
+		if( $url = $this->check_if_redirect_needed( $post_id )) {
 
-		// No revisions please
-		if ( $post->post_status != 'inherit' ) {
-			// Get the right URL
-			$url = parse_url( get_permalink( $post_id ) );
-			$url = $url['path'];
+			// Format the message
+			$message = sprintf( __( "WordPress SEO Premium detected that you moved a post to the trash. <a href='%s'>Click here to create a redirect from the old post URL</a>.", 'wordpress-seo' ), $this->javascript_create_redirect( $url ) );
 
-			// If $url is not a single /, there may be the option to create a redirect
-			if ( $url !== '/' ) {
-				// Message should only be shown if there isn't already a redirect
-				if ( ! $this->check_if_redirect_exists( $url ) ) {
-					// Format the message
-					$message = sprintf( __( "WordPress SEO Premium detected that you moved a post to the trash. <a href='%s'>Click here to create a redirect from the old post URL</a>.", 'wordpress-seo' ), 'javascript:wpseo_create_redirect("' . urlencode( $url ) . '", "' . wp_create_nonce( 'wpseo-redirects-ajax-security' ) . '");' );
-
-					// Add the message to the notifications center
-					Yoast_Notification_Center::get()->add_notification( new Yoast_Notification( $message ) );
-				}
-			}
+			$this->create_notification( $message, 'trash' );
 		}
 
 	}
@@ -107,26 +81,12 @@ class WPSEO_Post_Watcher {
 	 */
 	public function detect_post_untrash( $post_id ) {
 
-		// Get the post
-		$post = get_post( $post_id );
+		if( $url = $this->check_if_redirect_needed( $post_id )) {
 
-		// No revisions please
-		if ( $post->post_status != 'inherit' ) {
-			// Get the right URL
-			$url = parse_url( get_permalink( $post_id ) );
-			$url = $url['path'];
+			// Format the message
+			$message = sprintf( __( "WordPress SEO Premium detected that you restored a post from the trash. <a href='%s'>Click here to remove the redirect</a>.", 'wordpress-seo' ), $this->javascript_undo_redirect( $url ) );
 
-			// If $url is not a single /, there may be the option to create a redirect
-			if ( $url !== '/' ) {
-				// Message should only be shown if there's already a redirect
-				if ( $this->check_if_redirect_exists( $url ) ) {
-					// Format the message
-					$message = sprintf( __( "WordPress SEO Premium detected that you restored a post from the trash. <a href='%s'>Click here to remove the redirect</a>.", 'wordpress-seo' ), 'javascript:wpseo_undo_redirect("' . urlencode( $url ) . '", "' . wp_create_nonce( 'wpseo-redirects-ajax-security' ) . '");' );
-
-					// Add the message to the notifications center
-					Yoast_Notification_Center::get()->add_notification( new Yoast_Notification( $message ) );
-				}
-			}
+			$this->create_notification( $message, 'untrash' );
 		}
 
 	}
@@ -138,26 +98,13 @@ class WPSEO_Post_Watcher {
 	 */
 	public function detect_post_delete( $post_id ) {
 
-		// Get the post
-		$post = get_post( $post_id );
+		// Is a redirect needed
+		if( $url = $this->check_if_redirect_needed( $post_id )) {
 
-		// No revisions please
-		if ( $post->post_status != 'inherit' ) {
-			// Get the right URL
-			$url = parse_url( get_permalink( $post_id ) );
-			$url = $url['path'];
+			// Format the message
+			$message = sprintf( __( "WordPress SEO Premium detected that you deleted a post. <a href='%s'>Click here to create a redirect from the old post URL</a>.", 'wordpress-seo' ), $this->javascript_create_redirect( $url ) );
 
-			// If $url is not a single /, there may be the option to create a redirect
-			if ( $url !== '/' ) {
-				// Message should only be shown if there isn't already a redirect
-				if ( !$this->check_if_redirect_exists( $url ) ) {
-					// Format the message
-					$message = sprintf( __( "WordPress SEO Premium detected that you deleted a post. <a href='%s'>Click here to create a redirect from the old post URL</a>.", 'wordpress-seo' ), 'javascript:wpseo_create_redirect("' . urlencode( $url ) . '", "' . wp_create_nonce( 'wpseo-redirects-ajax-security' ) . '");' );
-
-					// Add the message to the notifications center
-					Yoast_Notification_Center::get()->add_notification( new Yoast_Notification( $message ) );
-				}
-			}
+			$this->create_notification( $message, 'delete' );
 		}
 
 	}
@@ -174,6 +121,52 @@ class WPSEO_Post_Watcher {
 		$redirects        = $redirect_manager->get_redirects();
 
 		return array_key_exists( $url, $redirects );
+	}
+
+
+	/**
+	 * This method checks if a redirect is needed.
+	 *
+	 * This method will check if url as redirect already exists
+	 *
+	 * @param integer $post_id
+	 *
+	 * @return mixed
+	 */
+	protected function check_if_redirect_needed( $post_id ) {
+
+		// Get the post
+		$post = get_post( $post_id );
+
+		// No revisions please
+		if ( $post->post_status != 'inherit' ) {
+			// Get the right URL
+			$url = $this->get_target_url( $post_id );
+
+			// If $url is not a single /, there may be the option to create a redirect
+			if ( $url !== '/' ) {
+				// Message should only be shown if there isn't already a redirect
+				if ( ! $this->check_if_redirect_exists( $url ) ) {
+					return $url;
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Get the URL to the post and returns it's path
+	 *
+	 * @param integer $post_id
+	 *
+	 * @return string
+	 */
+	protected function get_target_url( $post_id ) {
+		// Use the correct URL path
+		$url = parse_url( get_permalink( $post_id ) );
+		$url = $url['path'];
+
+		return $url;
 	}
 
 }
---
+++ premium/classes/class-watcher.php
@@ -0,0 +1,106 @@
+<?php
+
+abstract class WPSEO_Watcher {
+
+	/**
+	 * Parses the hidden field with the old url to show in the form
+	 *
+	 * @param string $url
+	 *
+	 * @return string
+	 */
+	protected function parse_url_field( $url ) {
+
+		// Output the hidden field
+		return '<input type="hidden" name="wpseo_old_url" value="' . esc_attr( $url ) . '"/>';
+
+	}
+
+	/**
+	 * This method checks if it's desirable to create a redirect
+	 *
+	 * @param string $old_url
+	 * @param string $new_url
+	 *
+	 * @return bool
+	 */
+	protected function should_create_redirect( $old_url, $new_url ) {
+
+		// Get the site URL
+		$site = parse_url( get_site_url() );
+
+		if ( $old_url != $new_url && $old_url != '/' && ( ! isset( $site['path'] ) || ( isset( $site['path'] ) && $old_url != $site['path'] . '/' ) ) ) {
+			return true;
+		}
+	}
+
+	/**
+	 * Creates notification with given $message
+	 *
+	 * This method will also apply filter for $notification_type to determine if notification has to be shown
+	 *
+	 * @param string $message
+	 * @param string $notification_type
+	 */
+	protected function create_notification( $message, $notification_type ) {
+		$show_notification = true;
+		$show_notification = apply_filters('wpseo_redirect_notification_' . $this->watch_type . '_' . $notification_type, $show_notification);
+
+		if( $show_notification ) {
+			// Add the message to the notifications center
+			Yoast_Notification_Center::get()->add_notification( new Yoast_Notification( $message ) );
+		}
+
+	}
+
+	/**
+	 * Opens the redirect manager and create the redirect
+	 *
+	 * @param string $old_url
+	 * @param string $new_url
+	 * @param int $header_code
+	 */
+	protected function create_redirect( $old_url, $new_url, $header_code = 301 ) {
+		// The URL redirect manager
+		$redirect_manager = new WPSEO_URL_Redirect_Manager();
+
+		// Create the redirect
+		$redirect_manager->create_redirect( $old_url, $new_url, $header_code );
+	}
+
+	/**
+	 * Returns the string to the javascript method from where a new redirect can be added
+	 *
+	 * @param string $url
+	 *
+	 * @return string
+	 */
+	protected function javascript_create_redirect( $url ) {
+		return 'javascript:wpseo_create_redirect("' . urlencode( $url ) . '", "' . wp_create_nonce( 'wpseo-redirects-ajax-security' ) . '");';
+	}
+
+	/**
+	 * Returns the string to the javascript method from where the added redirect can be undone
+	 *
+	 * @param string $old_url
+	 *
+	 * @return string
+	 */
+	protected function javascript_undo_redirect( $old_url ) {
+		return 'javascript:wpseo_undo_redirect("' . urlencode( $old_url ) . '", "' . wp_create_nonce( 'wpseo-redirects-ajax-security' ) . '");';
+	}
+
+	/**
+	 * Return the url to the admin page where the just added redirect can be found
+	 *
+	 * @param string $old_url
+	 *
+	 * @return string
+	 */
+	protected function admin_redirect_url( $old_url ) {
+		return admin_url( 'admin.php?page=wpseo_redirects&s=' . urlencode( $old_url ) );
+	}
+
+}
+
+?>
