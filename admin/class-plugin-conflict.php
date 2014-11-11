bccdccc48f12d3c16e9fcc9ef5b33f555e5969a4
Andy Meerwaldt <andy@yoast.com>
2014-11-11 16:15:51 +0100

    The full list of XML sitemaps

--- admin/class-plugin-conflict.php
+++ admin/class-plugin-conflict.php
@@ -23,7 +23,7 @@ if ( ! class_exists( 'WPSEO_Plugin_Conflict' ) ) {
 		 */
 		protected $plugins = array(
 			// The plugin which are writing OG metadata
-			'open_graph' => array(
+			'open_graph'   => array(
 				'2-click-socialmedia-buttons/2-click-socialmedia-buttons.php',
 
 				// 2 Click Social Media Buttons
@@ -71,14 +71,27 @@ if ( ! class_exists( 'WPSEO_Plugin_Conflict' ) ) {
 			),
 
 			'xml_sitemaps' => array(
-				'xml-sitemap-feed/xml-sitemap.php'						 // XML Sitemap & Google News feeds
-
+				'google-sitemap-plugin/google-sitemap-plugin.php',									// Google Sitemap (BestWebSoft)
+				'xml-sitemaps/xml-sitemaps.php',													// XML Sitemaps (Denis de Bernardy and Mike Koepke)
+				'bwp-google-xml-sitemaps/bwp-simple-gxs.php',										// Better WordPress Google XML Sitemaps (Khang Minh)
+				'google-sitemap-generator/sitemap.php',												// Google XML Sitemaps (Arne Brachhold)
+				'xml-sitemap-feed/xml-sitemap.php',													// XML Sitemap & Google News feeds (RavanH)
+				'google-monthly-xml-sitemap/monthly-xml-sitemap.php',								// Google Monthly XML Sitemap (Andrea Pernici)
+				'simple-google-sitemap-xml/simple-google-sitemap-xml.php',							// Simple Google Sitemap XML (iTx Technologies)
+				'another-simple-xml-sitemap/another-simple-xml-sitemap.php',						// Another Simple XML Sitemap
+				'xml-maps/google-sitemap.php',														// Xml Sitemap (Jason Martens)
+				'google-xml-sitemap-generator-by-anton-dachauer/adachauer-google-xml-sitemap.php',	// Google XML Sitemap Generator by Anton Dachauer (Anton Dachauer)
+				'wp-xml-sitemap/wp-xml-sitemap.php',												// WP XML Sitemap (Team Vivacity)
+				'sitemap-generator-for-webmasters/sitemap.php',										// Sitemap Generator for Webmasters (iwebslogtech)
+				'xml-sitemap-xml-sitemapcouk/xmls.php',												// XML Sitemap - XML-Sitemap.co.uk (Simon Hancox)
+				'sewn-in-xml-sitemap/sewn-xml-sitemap.php',											// Sewn In XML Sitemap (jcow)
+				'rps-sitemap-generator/rps-sitemap-generator.php',									// RPS Sitemap Generator (redpixelstudios)
 			)
 		);
 
 		/**
 		 * Overrides instance to set with this class as class
-		 * 
+		 *
 		 * @return Yoast_Plugin_Conflict
 		 */
 		public static function get_instance( $classname = __CLASS__ ) {
