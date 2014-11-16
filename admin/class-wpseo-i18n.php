b3201ad3756591c4d63abee0e05cd4ffb0a965aa
Joost de Valk <joost@yoast.com>
2014-11-16 22:17:04 +0100

    Add campaign tracking

--- admin/class-wpseo-i18n.php
+++ admin/class-wpseo-i18n.php
@@ -10,7 +10,7 @@ class WPSEO_i18n extends yoast_i18n {
 		$this->project_slug            = 'wordpress-seo';
 		$this->plugin_name             = 'WordPress SEO by Yoast';
 		$this->hook                    = 'wpseo_admin_footer';
-		$this->translate_project_url   = 'http://translate.yoast.com/';
+		$this->translate_project_url   = 'http://translate.yoast.com/projects#utm_source=plugin&utm_medium=promo-box&utm_campaign=i18n-promo';
 		$this->translate_project_name  = 'Yoast Translate';
 		$this->translate_project_logo  = 'https://cdn.yoast.com/wp-content/uploads/i18n-images/Yoast_Translate.svg';
 	}
