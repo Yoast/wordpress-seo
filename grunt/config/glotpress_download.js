9f9f6345053676deb6ca6815f3ea081bbdaa4e40
Joost de Valk <joost@yoast.com>
2014-11-21 21:43:06 +0100

    Fix implementation

--- grunt/config/glotpress_download.js
+++ grunt/config/glotpress_download.js
@@ -1,17 +1,17 @@
 // https://github.com/markoheijnen/grunt-glotpress
 module.exports = {
-	download: {
+	plugin: {
 		options: {
-			url: '<%= pkg.plugin.glotpress %>',
-			domainPath: '<%= paths.languages %>',
+			url        : '<%= pkg.plugin.glotpress %>',
+			domainPath : '<%= paths.languages %>',
 			file_format: "%domainPath%%textdomain%-%wp_locale%.%format%",
-			slug: '<%= pkg.plugin.textdomain %>',
-			textdomain: '<%= pkg.plugin.textdomain %>',
-			formats: ['mo'],
-			filter: {
-				translation_sets: false,
+			slug       : '<%= pkg.plugin.textdomain %>',
+			textdomain : '<%= pkg.plugin.textdomain %>',
+			formats    : ['mo'],
+			filter     : {
+				translation_sets  : false,
 				minimum_percentage: 50,
-				waiting_strings: false
+				waiting_strings   : false
 			}
 		}
 	}
