3ba414372027a8a8353ed0e8de9a80a1c06c28fa
Joost de Valk <joost@yoast.com>
2014-12-14 16:54:21 +0100

    Use Frontend class methods instead of twitter class as they're no longer there

--- tests/test-class-twitter.php
+++ tests/test-class-twitter.php
@@ -115,7 +115,7 @@ class WPSEO_Twitter_Test extends WPSEO_UnitTestCase {
 		$post_id = $this->factory->post->create();
 		$this->go_to( get_permalink( $post_id ) );
 
-		$expected = $this->metatag( 'title', self::$class_instance->title( '' ) );
+		$expected = $this->metatag( 'title', $GLOBALS['wpseo_front']->title( '' ) );
 		self::$class_instance->title();
 		$this->expectOutput( $expected );
 	}
@@ -137,7 +137,7 @@ class WPSEO_Twitter_Test extends WPSEO_UnitTestCase {
 
 		// test wpseo meta
 		WPSEO_Meta::set_value( 'metadesc', 'Meta description', $post_id );
-		$expected = $this->metatag( 'description', self::$class_instance->metadesc( false ) );
+		$expected = $this->metatag( 'description', $GLOBALS['wpseo_front']->metadesc( false ) );
 		self::$class_instance->description();
 		$this->expectOutput( $expected );
 	}
@@ -150,7 +150,7 @@ class WPSEO_Twitter_Test extends WPSEO_UnitTestCase {
 		$post_id = $this->factory->post->create();
 		$this->go_to( get_permalink( $post_id ) );
 
-		$expected = $this->metatag( 'url', esc_url( self::$class_instance->canonical( false ) ) );
+		$expected = $this->metatag( 'url', esc_url( $GLOBALS['wpseo_front']->canonical( false ) ) );
 		self::$class_instance->url();
 		$this->expectOutput( $expected );
 	}
