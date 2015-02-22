d362222ce194f6a3916f6245d9bbfc4010cb4d1d
Joost de Valk <joost@yoast.com>
2015-02-22 14:05:30 +0100

    Let's stick with defaults (10 per page) and see if that makes it work.

--- tests/test-class-wpseo-frontend.php
+++ tests/test-class-wpseo-frontend.php
@@ -525,11 +525,9 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase {
 	 * @covers WPSEO_Frontend::adjacent_rel_links
 	 */
 	public function test_adjacent_rel_links() {
-		update_site_option( 'posts_per_page', 5 );
-
 		// create a category, add 26 posts to it, go to page 2 of its archives
 		$category_id = wp_create_category( 'boom' );
-		$this->factory->post->create_many( 12, array( 'post_category' => array( $category_id ) ) );
+		$this->factory->post->create_many( 22, array( 'post_category' => array( $category_id ) ) );
 
 		$category_link = get_category_link( $category_id );
 
