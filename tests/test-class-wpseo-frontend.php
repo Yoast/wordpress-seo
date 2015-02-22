c80dbc5b44af22e2d64a4def5e5a3cd402c3728f
Joost de Valk <joost@yoast.com>
2015-02-23 00:50:14 +0100

    Override the go_to function in core as its broken when path isn't set.

--- tests/test-class-wpseo-frontend.php
+++ tests/test-class-wpseo-frontend.php
@@ -958,4 +958,44 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase {
 		$this->expectOutput( $expected, self::$class_instance->internal_search_json_ld() );
 	}
 
+	/**
+	 * Override the go_to function in core as its broken when path isn't set.
+	 *
+	 * @param $url
+	 */
+	function go_to( $url ) {
+		// note: the WP and WP_Query classes like to silently fetch parameters
+		// from all over the place (globals, GET, etc), which makes it tricky
+		// to run them more than once without very carefully clearing everything
+		$_GET = $_POST = array();
+		foreach (array('query_string', 'id', 'postdata', 'authordata', 'day', 'currentmonth', 'page', 'pages', 'multipage', 'more', 'numpages', 'pagenow') as $v) {
+			if ( isset( $GLOBALS[$v] ) ) unset( $GLOBALS[$v] );
+		}
+		$parts = parse_url($url);
+		if (isset($parts['scheme'])) {
+			$req = isset( $parts['path'] ) ? $parts['path'] : '';
+			if (isset($parts['query'])) {
+				$req .= '?' . $parts['query'];
+				// parse the url query vars into $_GET
+				parse_str($parts['query'], $_GET);
+			}
+		} else {
+			$req = $url;
+		}
+		if ( ! isset( $parts['query'] ) ) {
+			$parts['query'] = '';
+		}
+
+		$_SERVER['REQUEST_URI'] = $req;
+		unset($_SERVER['PATH_INFO']);
+
+		$this->flush_cache();
+		unset($GLOBALS['wp_query'], $GLOBALS['wp_the_query']);
+		$GLOBALS['wp_the_query'] = new WP_Query();
+		$GLOBALS['wp_query'] = $GLOBALS['wp_the_query'];
+		$GLOBALS['wp'] = new WP();
+		_cleanup_query_vars();
+
+		$GLOBALS['wp']->main($parts['query']);
+	}
 }
