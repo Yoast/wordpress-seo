e25bf906750d8c873cd0e33ba84919a86fa73ecf
Caroline Geven <caroline@yoast.com>
2015-03-03 16:02:14 +0100

    Fixed code and inserted counter so we don't overload servers.

--- premium/assets/js/wp-seo-premium-quickedit-notification.js
+++ premium/assets/js/wp-seo-premium-quickedit-notification.js
@@ -6,28 +6,10 @@
     //If current page is edit.php, proceed.
     if (current_page == 'edit.php' ) {
 
-        /**
-         * The current known slug of the post that is being edited through quick edit
-         */
-        var current_slug;
-
-        jQuery( '.editinline' ).click( function(e) {
-            //Get post_id of post that is being edited
-            var post_id = jQuery(this).closest('tr').attr('id').replace('post-', '');
-
-            //Get current slug of post that is being edited
-            current_slug = jQuery( '#inline_' + post_id ).find('.post_name').html();
-        });
-
         jQuery( '.button-primary' ).click(function() {
-
-            //Get new slug of post that is being edited
-
-            //current_slug = jQuery( '#inline_' + post_id ).find('.post_name').html();
-
             var post_id = jQuery(this).closest('tr').attr('id').replace('edit-', '');
 
-            current_slug = jQuery( '#inline_' + post_id ).find('.post_name').html();
+            var current_slug = jQuery( '#inline_' + post_id ).find('.post_name').html();
 
             var new_slug = jQuery("input[name=post_name]").val();
 
@@ -35,9 +17,12 @@
                 show_notification();
             }
         });
-    }}));
+    }
+}));
 
-function show_notification(){
+var notification_counter = 0;
+
+function show_notification() {
     jQuery.post(
         ajaxurl,
         { action: 'yoast_get_notifications'},
@@ -46,7 +31,8 @@ function show_notification(){
             if ( response != '' ) {
 
                 jQuery(response).insertAfter('h2');
-            } else {
+            } else if (notification_counter < 20 && response == '' ) {
+                notification_counter++;
                 setTimeout('show_notification()', 100);
             }
         });
