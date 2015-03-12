36d3f56a86fa0761d6487bfa5f9294cf86d6d08b
Caroline Geven <caroline@yoast.com>
2015-03-12 15:55:08 +0100

    Remove the error message when user removes featured image

--- js/wp-seo-featured-image.js
+++ js/wp-seo-featured-image.js
@@ -5,6 +5,10 @@ jQuery(document).ready(function () {
     });
 });
 
+/**
+ * Check if image is smaller than 200x200 pixels. If this is the case, show a warning
+ * @param featuredImage
+ */
 function yst_checkFeaturedImage( featuredImage ) {
     var attachment = featuredImage.state().get('selection').first().toJSON();
 
@@ -16,8 +20,40 @@ function yst_checkFeaturedImage( featuredImage ) {
         }
     }
 
-    if ( attachment.width > 199 && attachment.height > 199 && document.getElementById( 'yst_opengraph_image_warning' ) ) {
-        jQuery( '#yst_opengraph_image_warning').remove();
-        document.getElementById('postimagediv').style.border = 'none';
+    yst_overrideElemFunction();
+}
+
+/**
+ * Counter to make sure we do not end up in an endless loop if there' no remove-post-thumbnail id
+ * @type {number}
+ */
+var thumbIdCounter = 0;
+
+/**
+ * If there's a remove-post-thumbnail id, add an onclick. When this id is clicked, call yst_removeOpengraphWarning
+ * If not, check again after 100ms. Do not do this for more than 10 times so we do not end up in an endless loop
+ */
+function yst_overrideElemFunction() {
+    if( document.getElementById( 'remove-post-thumbnail' ) != null ){
+        thumbIdCounter = 0;
+        removeThumb = document.getElementById( 'remove-post-thumbnail' ).onclick;
+        document.getElementById( 'remove-post-thumbnail' ).onclick = yst_removeOpengraphWarning;
+    }else{
+        thumbIdCounter++;
+        if(thumbIdCounter < 10){
+            setTimeout( 'yst_overrideElemFunction()', 100 );
+        }
     }
 }
+
+/**
+ * Remove error message
+ */
+function yst_removeOpengraphWarning() {
+    jQuery( '#yst_opengraph_image_warning' ).remove();
+    document.getElementById( 'postimagediv' ).style.border = 'none';
+
+    //Make sure the original function does its work
+    removeThumb();
+}
+
