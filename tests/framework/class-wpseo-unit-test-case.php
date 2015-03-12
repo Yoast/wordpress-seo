c1acf768b7b0e90ed7ed357a86c0ea8f13eaff95
Andrey Savchenko <contact@rarst.net>
2015-03-12 15:48:02 +0200

    Normalized line endings in testing buffered output

--- tests/framework/class-wpseo-unit-test-case.php
+++ tests/framework/class-wpseo-unit-test-case.php
@@ -37,6 +37,8 @@ class WPSEO_UnitTestCase extends WP_UnitTestCase {
 	protected function expectOutput( $expected ) {
 		$output = ob_get_contents();
 		ob_clean();
+		$output   = preg_replace( '|\R|', "\r\n", $output );
+		$expected = preg_replace( '|\R|', "\r\n", $expected );
 		$this->assertEquals( $expected, $output );
 	}
 }
