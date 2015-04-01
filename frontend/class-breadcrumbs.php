6b3f9f7140bb024c1b76f53a5a43cdab0db33709
Omar Reiss <omar.reiss@gmail.com>
2015-04-01 11:06:05 +0200

    Revert "Revert "Fixes #1926""

--- frontend/class-breadcrumbs.php
+++ frontend/class-breadcrumbs.php
@@ -765,25 +765,25 @@ class WPSEO_Breadcrumbs {
 				$inner_elm = 'strong';
 			}
 
-			$class = '';
-			if ( $i === ( $this->crumb_count - 1 ) ) {
-				$class = ' class="breadcrumb_last"';
-			}
-
-
-			$link_output = '<' . $this->element . ' typeof="v:Breadcrumb">';
-
 			if ( ( isset( $link['url'] ) && ( is_string( $link['url'] ) && $link['url'] !== '' ) ) &&
 			     ( $i < ( $this->crumb_count - 1 ) || $GLOBALS['paged'] )
 			) {
-				$link_output .= '<a href="' . esc_url( $link['url'] ) . '"' . $class . ' rel="v:url" property="v:title">' . $link['text'] . '</a>';
+				if ( $i === 0 ) {
+					$link_output .= '<' . $this->element . ' typeof="v:Breadcrumb">';
+				}
+				else {
+					$link_output .= '<' . $this->element . ' rel="v:child" typeof="v:Breadcrumb">';
+				}
+				$link_output .= '<a href="' . esc_url( $link['url'] ) . '" rel="v:url" property="v:title">' . $link['text'] . '</a>';
 			}
 			else {
-				$link_output .= '<' . $inner_elm . $class . ' property="v:title">' . $link['text'] . '</' . $inner_elm . '>';
+				$link_output .= '<' . $inner_elm . ' class="breadcrumb_last">' . $link['text'] . '</' . $inner_elm . '>';
+				// This is the last element, now close all previous elements.
+				while ( $i > 0 ) {
+					$link_output .= '</' . $this->element . '>';
+					$i--;
+				}
 			}
-
-			$link_output .= '</' . $this->element . '>';
-
 		}
 
 		/**
@@ -816,10 +816,7 @@ class WPSEO_Breadcrumbs {
 	 */
 	private function wrap_breadcrumb() {
 		if ( is_string( $this->output ) && $this->output !== '' ) {
-			$output = '
-		<' . $this->wrapper . $this->get_output_id() . $this->get_output_class() . ' prefix="v: http://rdf.data-vocabulary.org/#">
-			' . $this->output . '
-		</' . $this->wrapper . '>';
+			$output = '<' . $this->wrapper . $this->get_output_id() . $this->get_output_class() . ' xmlns:v="http://rdf.data-vocabulary.org/#">' . $this->output . '</' . $this->wrapper . '>';
 
 			/**
 			 * Filter: 'wpseo_breadcrumb_output' - Allow changing the HTML output of the WP SEO breadcrumbs class
