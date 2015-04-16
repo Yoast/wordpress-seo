e0bd0a0bc8a2de2db4a7b03ca13476515a45606a
Danny <danny@yoast.com>
2015-04-16 16:15:25 +0200

    added grunt tasks for js validation/building

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/* global require, process */
+module.exports = function(grunt) {
+    'use strict';
+
+    require('time-grunt')(grunt);
+
+    // Define project configuration
+    var project = {
+        paths: {
+            get config() {
+                return this.grunt + 'config/';
+            },
+            grunt: 'grunt/',
+            js: 'js/'
+
+        },
+        files: {
+            js: [
+                'js/*.js',
+                '!js/*.min.js'
+            ],
+
+            get config() {
+                return project.paths.config + '*.js';
+            },
+            grunt: 'Gruntfile.js'
+        },
+        pkg: grunt.file.readJSON( 'package.json' )
+    };
+
+    // Load Grunt configurations and tasks
+    require( 'load-grunt-config' )(grunt, {
+        configPath: require( 'path' ).join( process.cwd(), project.paths.config ),
+        data: project
+    });
+};
