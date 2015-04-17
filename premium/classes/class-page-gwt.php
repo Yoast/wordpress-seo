8333b14c22f277b61331cb9c89c450cc0a089f8f
Andy Meerwaldt <andy@yoast.com>
2015-04-17 12:01:35 +0200

    Putting HTML in its own view

--- premium/classes/class-page-gwt.php
+++ premium/classes/class-page-gwt.php
@@ -21,112 +21,18 @@ class WPSEO_Page_GWT {
 	 */
 	public function display() {
 
-		$config = array(
-			'application_name' => 'WordPress SEO by Yoast Premium',
-			'client_id'        => '972827778625-rvd2mfvj3fnc97es9p57vqaap2lucm3h.apps.googleusercontent.com',
-			'client_secret'    => 'i32Z2SFYPdxNRALHf25uwMFW',
-		);
-
 		Yoast_Api_Libs::load_api_libraries( array( 'google' ) );
 
 		// Create a new WPSEO GWT Google Client
-		$gwt_client = new WPSEO_GWT_Client( $config );
+		$gwt_client = new WPSEO_GWT_Client(
+			array(
+				'application_name' => 'WordPress SEO by Yoast Premium',
+				'client_id'        => '972827778625-rvd2mfvj3fnc97es9p57vqaap2lucm3h.apps.googleusercontent.com',
+				'client_secret'    => 'i32Z2SFYPdxNRALHf25uwMFW',
+			)
+		);
 
-		// Admin header
-		Yoast_Form::get_instance()->admin_header( false, 'wpseo_redirects', false, 'yoast_wpseo_redirects_options' );
-		?>
-		<h2 class="nav-tab-wrapper" id="wpseo-tabs">
-			<form action="" method="post">
-				<input type="submit" name="reload-crawl-issues" id="reload-crawl-issue" class="button-primary"
-					   style="float: right;" value="<?php _e( 'Reload crawl issues', 'wordpress-seo-premium' ); ?>">
-			</form>
-			<?php $this->platform_tabs(); ?>
-		</h2>
-
-<!--		<div class="tabwrapper>">-->
-			<?php
-
-			switch ( $this->current_tab ) {
-				case 'settings' :
-					?>
-					<h2>Google Webmaster Tools Settings</h2>
-
-					<form action="<?php echo admin_url( 'options.php' ); ?>" method="post">
-						<?php
-						settings_fields( 'yoast_wpseo_gwt_options' );
-						Yoast_Form::get_instance()->set_option( 'wpseo-premium-gwt' );
-
-						// Get the sites
-						$service = new WPSEO_GWT_Service( $gwt_client );
-						$sites   = $service->get_sites();
-
-						echo Yoast_Form::get_instance()->select( 'profile', __( 'Profile', 'wordpress-seo-premium' ), $sites );
-
-						?>
-
-						<p class="submit">
-							<input type="submit" name="submit" id="submit" class="button button-primary"
-								   value="<?php _e( 'Save Changes', 'wordpress-seo-premium' ); ?>">
-						</p>
-					</form>
-					<?php
-					break;
-
-				default :
-					// Check if there is an access token
-					if ( null != $gwt_client->getAccessToken() ) {
-						// echo "<h2>Google Webmaster Tools Errors</h2>\n";
-
-						$category = '';
-						if ( $filter_category = filter_input( INPUT_GET, 'category' ) ) {
-							$category = "&category={$filter_category}";
-						}
-
-						// Open <form>
-						echo "<form id='wpseo-crawl-issues-table-form' action='" . admin_url( 'admin.php' ) . '?page=' . esc_attr( $_GET['page'] ) . $category . "' method='post'>\n";
-
-						// AJAX nonce
-						echo "<input type='hidden' class='wpseo_redirects_ajax_nonce' value='" . wp_create_nonce( 'wpseo-redirects-ajax-security' ) . "' />\n";
-
-						// The list table
-						$list_table = new WPSEO_Crawl_Issue_Table( $gwt_client );
-						$list_table->prepare_items();
-						$list_table->search_box( __( 'Search', 'wordpress-seo-premium' ), 'wpseo-crawl-issues-search' );
-						$list_table->display();
-
-						// Close <form>
-						echo "</form>\n";
-
-					}
-					else {
-
-						// Get the oauth URL
-						$oath_url = $gwt_client->createAuthUrl();
-
-						// Print auth screen
-						echo '<p>' . __( 'To allow WordPress SEO Premium to fetch your Google Webmaster Tools information, please enter your Google Authorization Code.', 'wordpress-seo-premium' ) . "</p>\n";
-						echo "<a href='javascript:wpseo_gwt_open_authorize_code_window(\"{$oath_url}\");'>" . __( 'Click here to get a Google Authorization Code', 'wordpress-seo-premium' ) . "</a>\n";
-
-						echo '<p>' . __( 'Please enter the Authorization Code in the field below and press the Authenticate button.', 'wordpress-seo-premium' ) . "</p>\n";
-						echo "<form action='' method='post'>\n";
-						echo "<input type='text' name='gwt[authorization_code]' value='' />";
-						echo "<input type='hidden' name='gwt[gwt_nonce]' value='" . wp_create_nonce( 'wpseo-gwt_nonce' ) . "' />";
-						echo "<input type='submit' name='gwt[Submit]' value='" . __( 'Authenticate', 'wordpress-seo-premium' ) . "' class='button-primary' />";
-						echo "</form>\n";
-					}
-
-					?>
-					<!--			</div>-->
-					<?php
-					break;
-			}
-			?>
-
-		<br class="clear">
-		<?php
-
-		// Admin footer
-		Yoast_Form::get_instance()->admin_footer( false );
+		require_once WPSEO_PREMIUM_PATH . 'views/gwt-display.php';
 	}
 
 	/**
---
+++ premium/views/gwt-display.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * @package	WPSEO
+ * @subpackage	Premium
+ */
+
+	// Admin header
+	Yoast_Form::get_instance()->admin_header( false, 'wpseo_redirects', false, 'yoast_wpseo_redirects_options' );
+?>
+	<h2 class="nav-tab-wrapper" id="wpseo-tabs">
+		<form action="" method="post">
+			<input type="submit" name="reload-crawl-issues" id="reload-crawl-issue" class="button-primary"
+				   style="float: right;" value="<?php _e( 'Reload crawl issues', 'wordpress-seo-premium' ); ?>">
+		</form>
+		<?php $this->platform_tabs(); ?>
+	</h2>
+
+<?php
+
+switch ( $this->current_tab ) {
+	case 'settings' :
+		?>
+		<h2>Google Webmaster Tools Settings</h2>
+
+		<form action='<?php echo admin_url( 'options.php' ); ?>' method='post'>
+			<?php
+			settings_fields( 'yoast_wpseo_gwt_options' );
+			Yoast_Form::get_instance()->set_option( 'wpseo-premium-gwt' );
+
+			// Get the sites
+			$service = new WPSEO_GWT_Service( $gwt_client );
+
+			echo Yoast_Form::get_instance()->select( 'profile', __( 'Profile', 'wordpress-seo-premium' ), $service->get_sites() );
+
+			?>
+			<p class="submit">
+				<input type="submit" name="submit" id="submit" class="button button-primary" value="<?php _e( 'Save Changes', 'wordpress-seo-premium' ); ?>">
+			</p>
+		</form>
+		<?php
+		break;
+
+	default :
+		// Check if there is an access token
+		if ( null != $gwt_client->getAccessToken() ) {
+			$category = '';
+			if ( $filter_category = filter_input( INPUT_GET, 'category' ) ) {
+				$category = "&category={$filter_category}";
+			}
+
+			// Open <form>
+			echo "<form id='wpseo-crawl-issues-table-form' action='" . admin_url( 'admin.php' ) . '?page=' . esc_attr( filter_input( INPUT_GET, 'page' ) ) . $category . "' method='post'>\n";
+
+			// AJAX nonce
+			echo "<input type='hidden' class='wpseo_redirects_ajax_nonce' value='" . wp_create_nonce( 'wpseo-redirects-ajax-security' ) . "' />\n";
+
+			// The list table
+			$list_table = new WPSEO_Crawl_Issue_Table( $gwt_client );
+			$list_table->prepare_items();
+			$list_table->search_box( __( 'Search', 'wordpress-seo-premium' ), 'wpseo-crawl-issues-search' );
+			$list_table->display();
+
+			// Close <form>
+			echo "</form>\n";
+
+		} else {
+
+			// Get the oauth URL
+			$oath_url = $gwt_client->createAuthUrl();
+
+			// Print auth screen
+			echo '<p>' . __( 'To allow WordPress SEO Premium to fetch your Google Webmaster Tools information, please enter your Google Authorization Code.', 'wordpress-seo-premium' ) . "</p>\n";
+			echo "<a href='javascript:wpseo_gwt_open_authorize_code_window(\"{$oath_url}\");'>" . __( 'Click here to get a Google Authorization Code', 'wordpress-seo-premium' ) . "</a>\n";
+
+			echo '<p>' . __( 'Please enter the Authorization Code in the field below and press the Authenticate button.', 'wordpress-seo-premium' ) . "</p>\n";
+			echo "<form action='' method='post'>\n";
+			echo "<input type='text' name='gwt[authorization_code]' value='' />";
+			echo "<input type='hidden' name='gwt[gwt_nonce]' value='" . wp_create_nonce( 'wpseo-gwt_nonce' ) . "' />";
+			echo "<input type='submit' name='gwt[Submit]' value='" . __( 'Authenticate', 'wordpress-seo-premium' ) . "' class='button-primary' />";
+			echo "</form>\n";
+		}
+
+		break;
+}
+?>
+	<br class="clear" />
+<?php
+
+// Admin footer
+Yoast_Form::get_instance()->admin_footer( false );
