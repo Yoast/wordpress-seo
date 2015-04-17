e6b2546586d7a47777be64a66b207fb909edcea5
Andy Meerwaldt <andy@yoast.com>
2015-04-17 11:33:41 +0200

    Making some methods protected

--- premium/classes/class-crawl-issue-table.php
+++ premium/classes/class-crawl-issue-table.php
@@ -29,6 +29,13 @@ class WPSEO_Crawl_Issue_Table extends WP_List_Table {
 	protected $_column_headers;
 
 	/**
+	 * The category that is displayed
+	 *
+	 * @var mixed|string
+	 */
+	private $current_view;
+
+	/**
 	 * WPSEO_Redirect_Table constructor
 	 *
 	 * @param WPSEO_GWT_Client $gwt
@@ -39,6 +46,9 @@ class WPSEO_Crawl_Issue_Table extends WP_List_Table {
 		// Set GWT client
 		$this->gwt = $gwt;
 
+		// Set the current view
+		$this->current_view = ( $status = filter_input( INPUT_GET, 'category' )) ? $status : 'all';
+
 		// Set page views
 		add_filter( 'views_' . $this->screen->id, array( $this, 'add_page_views' ) );
 
@@ -46,70 +56,6 @@ class WPSEO_Crawl_Issue_Table extends WP_List_Table {
 		if ( ( $search_string = filter_input( INPUT_GET, 's' ) ) != '' ) {
 			$this->search_string = $search_string;
 		}
-	}
-
-	/**
-	 * Get the current view
-	 *
-	 * @return string
-	 */
-	private function get_current_view() {
-		return ( $status = filter_input( INPUT_GET, 'category' )) ? $status : 'all' ;
-	}
-
-	/**
-	 * Add page views
-	 *
-	 * @param array $views
-	 *
-	 * @return array
-	 */
-	public function add_page_views( $views ) {
-
-		// Get current
-		$current = $this->get_current_view();
-
-		$views_arr = array(
-			'all'                  => __( 'All', 'wordpress-seo-premium' ),
-			'auth_permissions'     => __( 'Authentication permissions', 'wordpress-seo' ),
-			'many_to_one_redirect' => __( 'Many to one redirect', 'wordpress-seo' ),
-			'not_followed'         => __( 'Not followed', 'wordpress-seo' ),
-			'not_found'            => __( 'Not found', 'wordpress-seo' ),
-			'other'                => __( 'Other', 'wordpress-seo' ),
-			'roboted'              => __( 'Roboted', 'wordpress-seo' ),
-			'server_error'         => __( 'Server Error', 'wordpress-seo' ),
-			'soft_404'             => __( 'Soft 404', 'wordpress-seo' ),
-		);
-
-		$new_views = array();
-
-		foreach ( $views_arr as $key => $val ) {
-			$new_views[ $key ] = "<a href='" . esc_attr( add_query_arg( array( 'category' => $key, 'paged' => 1 ) ) ) . "'" . ( ( $current == $key ) ? " class='current'" : '' ) . ">{$val}</a>";
-		}
-
-		return $new_views;
-	}
-
-	/**
-	 * Search through the items
-	 *
-	 * @return array
-	 */
-	private function do_search( ) {
-		$results = array();
-
-		if ( is_array( $this->items ) ) {
-			foreach ( $this->items as $item ) {
-				foreach ( $item as $value ) {
-					if ( stristr( $value, $this->search_string ) !== false ) {
-						$results[] = $item;
-						continue;
-					}
-				}
-			}
-		}
-
-		return $results;
 	}
 
 	/**
@@ -139,48 +85,33 @@ class WPSEO_Crawl_Issue_Table extends WP_List_Table {
 	}
 
 	/**
-	 * URL column
+	 * Add page views
 	 *
-	 * @param array $item
+	 * @param array $views
 	 *
-	 * @return string
+	 * @return array
 	 */
-	public function column_url( $item ) {
-		$actions = array(
-			'create_redirect' => '<a href="javascript:wpseo_create_redirect(\'' . urlencode( $item['url'] ) . '\', \'' . $this->get_current_view() . '\');">' . __( 'Create redirect', 'wordpress-seo-premium' ) . '</a>',
-			'view'            => '<a href="' . $item['url'] . '" target="_blank">' . __( 'View', 'wordpress-seo-premium' ) . '</a>',
+	public function add_page_views( $views ) {
+
+		$views_arr = array(
+			'all'                  => __( 'All', 'wordpress-seo-premium' ),
+			'auth_permissions'     => __( 'Authentication permissions', 'wordpress-seo' ),
+			'many_to_one_redirect' => __( 'Many to one redirect', 'wordpress-seo' ),
+			'not_followed'         => __( 'Not followed', 'wordpress-seo' ),
+			'not_found'            => __( 'Not found', 'wordpress-seo' ),
+			'other'                => __( 'Other', 'wordpress-seo' ),
+			'roboted'              => __( 'Roboted', 'wordpress-seo' ),
+			'server_error'         => __( 'Server Error', 'wordpress-seo' ),
+			'soft_404'             => __( 'Soft 404', 'wordpress-seo' ),
 		);
 
-		return sprintf(
-			'<span class="value">%1$s</span> %2$s',
-			$item['url'],
-			$this->row_actions( $actions )
-		);
-	}
+		$new_views = array();
 
-	/**
-	 * Linked from column
-	 *
-	 * @param array $item
-	 *
-	 * @return string
-	 */
-	public function column_linked_from( $item ) {
-		return sprintf(
-			'<a href="%1$s" target="_blank">%1$s</a>', $item['linked_from']
-		);
-	}
+		foreach ( $views_arr as $key => $val ) {
+			$new_views[ $key ] = "<a href='" . esc_attr( add_query_arg( array( 'category' => $key, 'paged' => 1 ) ) ) . "'" . ( ( $this->current_view == $key ) ? " class='current'" : '' ) . ">{$val}</a>";
+		}
 
-	/**
-	 * Default method to display a column
-	 *
-	 * @param array  $item
-	 * @param string $column_name
-	 *
-	 * @return mixed
-	 */
-	public function column_default( $item, $column_name ) {
-		return $item[ $column_name ];
+		return $new_views;
 	}
 
 	/**
@@ -207,6 +138,7 @@ class WPSEO_Crawl_Issue_Table extends WP_List_Table {
 		return $columns;
 	}
 
+
 	/**
 	 * Return the columns that are sortable
 	 *
@@ -222,6 +154,51 @@ class WPSEO_Crawl_Issue_Table extends WP_List_Table {
 		);
 
 		return $sortable_columns;
+	}
+
+	/**
+	 * Default method to display a column
+	 *
+	 * @param array  $item
+	 * @param string $column_name
+	 *
+	 * @return mixed
+	 */
+	protected function column_default( $item, $column_name ) {
+		return $item[ $column_name ];
+	}
+
+	/**
+	 * URL column
+	 *
+	 * @param array $item
+	 *
+	 * @return string
+	 */
+	protected function column_url( $item ) {
+		$actions = array(
+			'create_redirect' => '<a href="javascript:wpseo_create_redirect(\'' . urlencode( $item['url'] ) . '\', \'' . $this->current_view . '\');">' . __( 'Create redirect', 'wordpress-seo-premium' ) . '</a>',
+			'view'            => '<a href="' . $item['url'] . '" target="_blank">' . __( 'View', 'wordpress-seo-premium' ) . '</a>',
+		);
+
+		return sprintf(
+			'<span class="value">%1$s</span> %2$s',
+			$item['url'],
+			$this->row_actions( $actions )
+		);
+	}
+
+	/**
+	 * Linked from column
+	 *
+	 * @param array $item
+	 *
+	 * @return string
+	 */
+	protected function column_linked_from( $item ) {
+		return sprintf(
+			'<a href="%1$s" target="_blank">%1$s</a>', $item['linked_from']
+		);
 	}
 
 	/**
@@ -261,6 +238,30 @@ class WPSEO_Crawl_Issue_Table extends WP_List_Table {
 
 		// Set the order
 		$ci_args['order']   = ( $order = filter_input( INPUT_GET, 'order' ) ) ? esc_sql( $order ) : 'asc';
+	}
+
+	/**
+	 * Search through the items
+	 *
+	 * @return array
+	 *
+	 * @todo: Probably this had to be done in the query
+	 */
+	private function do_search( ) {
+		$results = array();
+
+		if ( is_array( $this->items ) ) {
+			foreach ( $this->items as $item ) {
+				foreach ( $item as $value ) {
+					if ( stristr( $value, $this->search_string ) !== false ) {
+						$results[] = $item;
+						continue;
+					}
+				}
+			}
+		}
+
+		return $results;
 	}
 
 	/**
@@ -383,7 +384,9 @@ class WPSEO_Crawl_Issue_Table_Data {
 		$subquery = 'SELECT platform.post_id FROM wp_postmeta platform';
 
 		if ( $category = filter_input( INPUT_GET, 'category' ) ) {
-			$subquery .= ' INNER JOIN wp_postmeta category ON category.post_id = platform.post_id && category.meta_key = "wpseo_ci_issue_type" AND category.meta_value = "' . WPSEO_GWT_Mapper::category( $category ) . '"';
+			if ( $category !== 'all' ) {
+				$subquery .= ' INNER JOIN wp_postmeta category ON category.post_id = platform.post_id && category.meta_key = "wpseo_ci_issue_type" AND category.meta_value = "' . WPSEO_GWT_Mapper::category( $category ) . '"';
+			}
 		}
 
 		$subquery .= ' WHERE platform.meta_key = "wpseo_ci_crawl_type" && platform.meta_value = "' . WPSEO_GWT_Mapper::platform( $platform ) . '"';
