942f532ab3ef0894a885d258048eb6bdb7317bd9
Caroline Geven <caroline@yoast.com>
2015-04-22 14:49:06 +0200

    Code style class-post-watcher

--- premium/classes/class-post-watcher.php
+++ premium/classes/class-post-watcher.php
@@ -1,5 +1,11 @@
 <?php
+/**
+ * @package WPSEO\Premium
+ */
 
+/**
+ * Class WPSEO_Post_Watcher
+ */
 class WPSEO_Post_Watcher extends WPSEO_Watcher {
 
 	/**
@@ -28,7 +34,7 @@ class WPSEO_Post_Watcher extends WPSEO_Watcher {
 	/**
 	 * Add an extra field to post edit screen so we know the old url in the 'post_updated' hook
 	 *
-	 * @param $post
+	 * @param object $post
 	 */
 	public function old_url_field( $post ) {
 		// $post must be set
@@ -44,13 +50,13 @@ class WPSEO_Post_Watcher extends WPSEO_Watcher {
 	 * Detect if the slug changed, hooked into 'post_updated'
 	 *
 	 * @param integer $post_id
-	 * @param $post
-	 * @param $post_before
+	 * @param object  $post
+	 * @param object  $post_before
 	 */
 	public function detect_slug_change( $post_id, $post, $post_before ) {
 		$old_url = $this->get_old_url( $post, $post_before );
 
-		if ( !$old_url ) {
+		if ( ! $old_url ) {
 			return;
 		}
 
@@ -66,11 +72,11 @@ class WPSEO_Post_Watcher extends WPSEO_Watcher {
 	/**
 	 * Offer to create a redirect from the post that is about to get trashed
 	 *
-	 * @param $post_id
+	 * @param int $post_id
 	 */
 	public function detect_post_trash( $post_id ) {
 
-		if( $url = $this->check_if_redirect_needed( $post_id )) {
+		if( $url = $this->check_if_redirect_needed( $post_id ) ) {
 
 			// Format the message
 			$message = sprintf( __( "WordPress SEO Premium detected that you moved a post to the trash. <a href='%s'>Click here to create a redirect from the old post URL</a>.", 'wordpress-seo-premium' ), $this->javascript_create_redirect( $url ) );
@@ -83,11 +89,11 @@ class WPSEO_Post_Watcher extends WPSEO_Watcher {
 	/**
 	 * Offer to create a redirect from the post that is about to get  restored from the trash
 	 *
-	 * @param $post_id
+	 * @param int $post_id
 	 */
 	public function detect_post_untrash( $post_id ) {
 
-		if( $url = $this->check_if_redirect_needed( $post_id )) {
+		if( $url = $this->check_if_redirect_needed( $post_id ) ) {
 
 			// Format the message
 			$message = sprintf( __( "WordPress SEO Premium detected that you restored a post from the trash. <a href='%s'>Click here to remove the redirect</a>.", 'wordpress-seo-premium' ), $this->javascript_undo_redirect( $url ) );
@@ -100,12 +106,12 @@ class WPSEO_Post_Watcher extends WPSEO_Watcher {
 	/**
 	 * Offer to create a redirect from the post that is about to get deleted
 	 *
-	 * @param $post_id
+	 * @param int $post_id
 	 */
 	public function detect_post_delete( $post_id ) {
 
 		// Is a redirect needed
-		if( $url = $this->check_if_redirect_needed( $post_id )) {
+		if( $url = $this->check_if_redirect_needed( $post_id ) ) {
 
 			// Format the message
 			$message = sprintf( __( "WordPress SEO Premium detected that you deleted a post. <a href='%s'>Click here to create a redirect from the old post URL</a>.", 'wordpress-seo-premium' ), $this->javascript_create_redirect( $url ) );
@@ -178,13 +184,13 @@ class WPSEO_Post_Watcher extends WPSEO_Watcher {
 	/**
 	 * Get the old url
 	 *
-	 * @param $post
-	 * @param $post_before
+	 * @param object $post
+	 * @param object $post_before
 	 *
 	 * @return bool|string
 	 */
 	protected function get_old_url( $post, $post_before ) {
-		if ( !isset( $_POST['wpseo_old_url'] ) ) {
+		if ( ! isset( $_POST['wpseo_old_url'] ) ) {
 			// Check if request is inline action and new slug is not old slug, if so set wpseo_old_url
 			if ( ! empty( $_POST['action'] ) && $_POST['action'] === 'inline-save' && $post->post_name !== $post_before->post_name ) {
 				return '/' . $post_before->post_name . '/';
@@ -198,15 +204,15 @@ class WPSEO_Post_Watcher extends WPSEO_Watcher {
 	/**
 	 * Display notification
 	 *
-	 * @param $old_url
-	 * @param $new_url
+	 * @param string $old_url
+	 * @param string $new_url
 	 */
 	protected function set_notification( $old_url, $new_url ) {
 		$message = sprintf( __( "WordPress SEO Premium created a <a href='%s'>redirect</a> from the old post URL to the new post URL. <a href='%s'>Click here to undo this</a>.", 'wordpress-seo-premium' ), $this->admin_redirect_url( $old_url ), $this->javascript_undo_redirect( $old_url ) );
 
 		$this->create_redirect( $old_url, $new_url );
 
-		//Only set notification when the slug change was not saved through quick edit
+		// Only set notification when the slug change was not saved through quick edit
 		$this->create_notification( $message, 'slug_change' );
 	}
 
