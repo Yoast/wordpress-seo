8460f52cd957a5d41019b69adc883c61f5efc1c5
Andy Meerwaldt <andy@yoast.com>
2015-05-20 12:04:04 +0200

    Handling the fetching and saving of the issues

--- premium/classes/class-crawl-issue-count.php
+++ premium/classes/class-crawl-issue-count.php
@@ -14,26 +14,59 @@ class WPSEO_Crawl_Issue_Count {
 	CONST OPTION_CI_COUNTS     = 'wpseo_crawl_issues_counts';
 
 	/**
+	 * All the categories
+	 * @var array|void
+	 */
+	private $issues = array();
+
+	/**
+	 * @var string
+	 */
+	private $platform;
+
+	/**
+	 * @var string
+	 */
+	private $category;
+
+	/**
 	 *
 	 * @param WPSEO_GWT_Service $service
+	 * @param string            $platform
+	 * @param string            $category
 	 */
-	public function __construct( WPSEO_GWT_Service $service ) {
+	public function __construct( WPSEO_GWT_Service $service, $platform, $category ) {
 
 		add_action( 'wpseo_gwt_reset_data', array( $this, 'clear_counts' ) );
 
-		if ( $this->get_last_fetch() <= strtotime( '-12 hours' ) ) {
-			$this->service = $service;
+		$this->platform = $platform;
+		$this->category = $category;
 
-			// Remove the timestamp
+		if ( filter_input( INPUT_POST, 'reload-crawl-issues' ) ) {
 			$this->remove_last_fetch();
-
-			// Fetching the counts by setting an options
-			$this->fetch_counts();
-
-			// Saving the current timestamp
-			$this->save_last_fetch();
 		}
 
+		$this->fetch_counts( $service );
+
+		// Listing the issues
+		$this->list_issues();
+	}
+
+	/**
+	 * Getting the array with all the issues for current platform - category
+	 *
+	 * @return array|void
+	 */
+	public function get_issues() {
+		return $this->issues;
+	}
+
+	/**
+	 * Returns the total amount of the issues
+	 * @return int
+	 */
+	public function get_total_issues() {
+		return count( $this->issues );
 	}
 
 	/**
@@ -48,10 +81,41 @@ class WPSEO_Crawl_Issue_Count {
 	}
 
 	/**
-	 * Fetching the counts from the service and store them in an option
+	 * Fetching the counts from the GWT API
+	 *
+	 * @param WPSEO_GWT_Service $service
 	 */
-	private function fetch_counts() {
-		update_option( self::OPTION_CI_COUNTS, $this->service->get_crawl_issue_counts() );
+	private function fetch_counts( WPSEO_GWT_Service $service ) {
+		if ( $this->should_fetch( $this->get_last_fetch() ) ) {
+			$this->service = $service;
+
+			// Remove the timestamp
+			$this->remove_last_fetch();
+
+			// Fetching the counts by setting an option
+			$this->set_counts( $this->service->get_crawl_issue_counts() );
+
+			// Saving the current timestamp
+			$this->save_last_fetch();
+		}
+	}
+
+	/**
+	 * Getting the counts from the options
+	 *
+	 * @return array
+	 */
+	private function get_counts() {
+		return get_option( self::OPTION_CI_COUNTS, array() );
+	}
+
+	/**
+	 * Fetching the counts from the service and store them in an option
+	 *
+	 * @param array $counts
+	 */
+	private function set_counts( array $counts ) {
+		update_option( self::OPTION_CI_COUNTS, $counts );
 	}
 
 	/**
@@ -76,6 +140,55 @@ class WPSEO_Crawl_Issue_Count {
 	 */
 	private function remove_last_fetch() {
 		delete_option( self::OPTION_CI_LAST_FETCH );
+	}
+
+	/**
+	 * Listing the issues from the database
+	 *
+	 */
+	private function list_issues() {
+		$counts = $this->get_counts();
+
+		if ( array_key_exists( $this->platform, $counts ) ) {
+			$counts[ $this->platform ] = $this->list_category_issues( $counts[ $this->platform ] );
+
+			// Write the new counts value;
+			$this->set_counts( $counts );
+		}
+	}
+
+	/**
+	 * Listing the issues for current category.
+
+	 * @param array $counts
+	 *
+	 * @return mixed
+	 */
+	private function list_category_issues( $counts ) {
+		// Fetching the issues
+		$issue_crawler = new WPSEO_Crawl_Category_Issues( $this->platform, $this->category );
+
+		// When the issues have to be fetched
+		if ( array_key_exists( $this->category, $counts ) && $counts[ $this->category ]['count'] > 0 && $this->should_fetch( $counts[ $this->category ]['last_fetch'] )  ) {
+			$this->issues = $issue_crawler->fetch_issues();
+
+			// Set last fetch
+			$counts[ $this->category ]['last_fetch'] = time();
+		}
+
+		$this->issues = $issue_crawler->get_issues();
+
+		return $counts;
+	}
+	/**
+	 * Check if given date is later then 12 hours ago.
+	 *
+	 * @param int $last_fetch
+	 *
+	 * @return bool
+	 */
+	private function should_fetch( $last_fetch ) {
+		return ( $last_fetch <= strtotime( '-12 hours' ) );
 	}
 
 }
