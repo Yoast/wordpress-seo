8244b4876201ff920d1ef99e1930ff8aa20a4934
Andrey Savchenko <contact@rarst.net>
2015-06-01 12:59:39 +0300

    Extracted Frontend instance retrieval for DRY and code readability

--- frontend/class-opengraph.php
+++ frontend/class-opengraph.php
@@ -205,7 +205,8 @@ class WPSEO_OpenGraph {
 	 */
 	public function og_title( $echo = true ) {
 
-		$is_posts_page = WPSEO_Frontend::get_instance()->is_posts_page();
+		$frontend      = WPSEO_Frontend::get_instance();
+		$is_posts_page = $frontend->is_posts_page();
 
 		if ( is_singular() || $is_posts_page ) {
 
@@ -214,7 +215,7 @@ class WPSEO_OpenGraph {
 			$title   = WPSEO_Meta::get_value( 'opengraph-title', $post_id );
 
 			if ( $title === '' ) {
-				$title = WPSEO_Frontend::get_instance()->title( '' );
+				$title = $frontend->title( '' );
 			}
 			else {
 				// Replace WP SEO Variables
@@ -222,10 +223,10 @@ class WPSEO_OpenGraph {
 			}
 		}
 		else if ( is_front_page() ) {
-			$title = ( isset( $this->options['og_frontpage_title'] ) && $this->options['og_frontpage_title'] !== '' ) ? $this->options['og_frontpage_title'] : WPSEO_Frontend::get_instance()->title( '' );
+			$title = ( isset( $this->options['og_frontpage_title'] ) && $this->options['og_frontpage_title'] !== '' ) ? $this->options['og_frontpage_title'] : $frontend->title( '' );
 		}
 		else {
-			$title = WPSEO_Frontend::get_instance()->title( '' );
+			$title = $frontend->title( '' );
 		}
 
 		/**
@@ -501,18 +502,19 @@ class WPSEO_OpenGraph {
 	 * @return string $ogdesc
 	 */
 	public function description( $echo = true ) {
-		$ogdesc = '';
+		$ogdesc   = '';
+		$frontend = WPSEO_Frontend::get_instance();
 
 		if ( is_front_page() ) {
 			if ( isset( $this->options['og_frontpage_desc'] ) && $this->options['og_frontpage_desc'] !== '' ) {
 				$ogdesc = wpseo_replace_vars( $this->options['og_frontpage_desc'], null );
 			}
 			else {
-				$ogdesc = WPSEO_Frontend::get_instance()->metadesc( false );
+				$ogdesc = $frontend->metadesc( false );
 			}
 		}
 
-		$is_posts_page = WPSEO_Frontend::get_instance()->is_posts_page();
+		$is_posts_page = $frontend->is_posts_page();
 
 		if ( is_singular() || $is_posts_page ) {
 			$post_id = ( $is_posts_page ) ? get_option( 'page_for_posts' ) : get_the_ID();
@@ -524,7 +526,7 @@ class WPSEO_OpenGraph {
 
 			// Use metadesc if $ogdesc is empty
 			if ( $ogdesc === '' ) {
-				$ogdesc = WPSEO_Frontend::get_instance()->metadesc( false );
+				$ogdesc = $frontend->metadesc( false );
 			}
 
 			// og:description is still blank so grab it from get_the_excerpt()
@@ -535,7 +537,7 @@ class WPSEO_OpenGraph {
 
 		if ( is_category() || is_tag() || is_tax() ) {
 
-			$ogdesc = WPSEO_Frontend::get_instance()->metadesc( false );
+			$ogdesc = $frontend->metadesc( false );
 
 			if ( '' == $ogdesc ) {
 				$ogdesc = trim( strip_tags( term_description() ) );
