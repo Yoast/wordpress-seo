d1dfafb281c72f50290377d94720a233a8bf53d7
Andy Meerwaldt <andy@yoast.com>
2015-06-25 11:27:03 +0200

    Remove the definitions

--- inc/class-wpseo-meta.php
+++ inc/class-wpseo-meta.php
@@ -177,36 +177,6 @@ class WPSEO_Meta {
 				'default_value' => '',
 				'description'   => '', // Translation added later.
 			),
-			'sitemap-include'      => array(
-				'type'          => 'select',
-				'title'         => '', // Translation added later.
-				'default_value' => '-',
-				'description'   => '', // Translation added later.
-				'options'       => array(
-					'-'      => '', // Translation added later.
-					'always' => '', // Translation added later.
-					'never'  => '', // Translation added later.
-				),
-			),
-			'sitemap-prio'         => array(
-				'type'          => 'select',
-				'title'         => '', // Translation added later.
-				'default_value' => '-',
-				'description'   => '', // Translation added later.
-				'options'       => array(
-					'-'   => '', // Translation added later.
-					'1'   => '', // Translation added later.
-					'0.9' => '0.9',
-					'0.8' => '0.8 - ', // Translation added later.
-					'0.7' => '0.7',
-					'0.6' => '0.6 - ', // Translation added later.
-					'0.5' => '0.5 - ', // Translation added later.
-					'0.4' => '0.4',
-					'0.3' => '0.3',
-					'0.2' => '0.2',
-					'0.1' => '0.1 - ', // Translation added later.
-				),
-			),
 			'canonical'            => array(
 				'type'          => 'text',
 				'title'         => '', // Translation added later.
