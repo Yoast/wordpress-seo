077e735ef5ca13e1ad58bb2cab1518f50f9db7d5
mnwalker <mark@awcode.com>
2015-07-02 15:24:58 +0700

    Add Filters to customise sitemaps

--- inc/class-sitemaps.php
+++ inc/class-sitemaps.php
@@ -928,9 +928,10 @@ class WPSEO_Sitemaps {
 			return;
 		}
 
-		$this->sitemap = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" ';
-		$this->sitemap .= 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" ';
-		$this->sitemap .= 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+		$sitemap_urlset = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" ';
+  		$sitemap_urlset .= 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" ';
+  		$sitemap_urlset.= 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+  		$this->sitemap = apply_filters("sm_urlset_header", $sitemap_urlset);
 		$this->sitemap .= $output;
 
 		// Filter to allow adding extra URLs, only do this on the first XML sitemap, not on all.
@@ -1045,9 +1046,10 @@ class WPSEO_Sitemaps {
 			return;
 		}
 
-		$this->sitemap = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ';
-		$this->sitemap .= 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" ';
-		$this->sitemap .= 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+		$sitemap_urlset = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ';
+  		$sitemap_urlset .= 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" ';
+  		$sitemap_urlset.= 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+  		$this->sitemap = apply_filters("sm_urlset_header", $sitemap_urlset);
 		if ( is_string( $output ) && trim( $output ) !== '' ) {
 			$this->sitemap .= $output;
 		}
@@ -1144,9 +1146,10 @@ class WPSEO_Sitemaps {
 			return;
 		}
 
-		$this->sitemap = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" ';
-		$this->sitemap .= 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" ';
-		$this->sitemap .= 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+		$sitemap_urlset = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" ';
+  		$sitemap_urlset .= 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" ';
+  		$sitemap_urlset.= 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+  		$this->sitemap = apply_filters("sm_urlset_header", $sitemap_urlset);
 		$this->sitemap .= $output;
 
 		// Filter to allow adding extra URLs, only do this on the first XML sitemap, not on all.
@@ -1259,6 +1262,7 @@ class WPSEO_Sitemaps {
 			}
 			unset( $img );
 		}
+		$output = apply_filters("sm_render_xml", $output, $url['loc']);
 		$output .= "\t</url>\n";
 
 		return $output;
