83e2cccc8fee5396c9f8a3708e895ccbc6c0e3a3
Andy Meerwaldt <andy@yoast.com>
2015-07-06 14:24:28 +0200

    Using comma's instead of dots.

--- admin/pages/xml-sitemaps.php
+++ admin/pages/xml-sitemaps.php
@@ -105,7 +105,7 @@ $yform->checkbox( 'enablexmlsitemap', __( 'Check this box to enable XML sitemap 
 
 		<div id="exclude-post" class="wpseotab">
 			<?php
-			echo '<p>' . sprintf( __( 'You can exclude posts from the sitemap by entering a comma separated string with the Post ID\'s. The format will become something like: %1$s.', 'wordpress-seo' ), '<code>1,2,99,100</code>' ) . '</p>';
+			echo '<p>' , sprintf( __( 'You can exclude posts from the sitemap by entering a comma separated string with the Post ID\'s. The format will become something like: %1$s.', 'wordpress-seo' ), '<code>1,2,99,100</code>' ) , '</p>';
 			$yform->textinput( 'excluded-posts', __( 'Post to exclude', 'wordpress-seo' ) );
 			?>
 		</div>
