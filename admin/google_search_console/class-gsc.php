8ed59497cfcbae45a443c4630cfb5c2d31b1ff1f
Andy Meerwaldt <andy@yoast.com>
2015-07-07 11:17:00 +0200

    Improved translations, thanks @omarreiss

--- admin/google_search_console/class-gsc.php
+++ admin/google_search_console/class-gsc.php
@@ -113,7 +113,7 @@ class WPSEO_GSC {
 
 		// Is there a reset post than we will remove the posts and data.
 		if ( filter_input( INPUT_POST, 'gsc_reset' ) ) {
-			$this->add_notification( __( 'The Google Webmaster Tools data is removed successful. You have to authenticate again if you want to fetch this data.', 'wordpress-seo' ), 'update' );
+			$this->add_notification( __( 'The Google Webmaster Tools data has been removed. You will have to re-authenticate if you want to retrieve the data again.', 'wordpress-seo' ), 'update' );
 			WPSEO_GSC_Settings::clear_data( $this->service );
 		}
 
@@ -123,7 +123,7 @@ class WPSEO_GSC {
 			WPSEO_GSC_Settings::reload_issues();
 
 			// Adding the notification.
-			$this->add_notification( __( 'The issues are reloaded successfully! Please refresh the page to display the new issues.', 'wordpress-seo' ), 'updated' );
+			$this->add_notification( __( 'The issues have been successfully reloaded! Please refresh the page to display the new issues.', 'wordpress-seo' ), 'updated' );
 		}
 
 		// Catch bulk action request.
