4c4d4e3a3bdffb3928d4160646192dddde88238b
Omar Reiss <omar.reiss@gmail.com>
2015-07-07 12:18:00 +0200

    Update xml-sitemaps.php

--- admin/pages/xml-sitemaps.php
+++ admin/pages/xml-sitemaps.php
@@ -107,7 +107,7 @@ $yform->checkbox( 'enablexmlsitemap', __( 'Check this box to enable XML sitemap 
 			<?php
 			/* Translators: %1$s: expands to '<code>1,2,99,100</code>' */
 			echo '<p>' , sprintf( __( 'You can exclude posts from the sitemap by entering a comma separated string with the Post ID\'s. The format will become something like: %1$s.', 'wordpress-seo' ), '<code>1,2,99,100</code>' ) , '</p>';
-			$yform->textinput( 'excluded-posts', __( 'Post to exclude', 'wordpress-seo' ) );
+			$yform->textinput( 'excluded-posts', __( 'Posts to exclude', 'wordpress-seo' ) );
 			?>
 		</div>
 
