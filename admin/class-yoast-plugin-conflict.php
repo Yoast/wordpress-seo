661e25c2bcecde0affd56309d6705769730a7ed1
Jon Gilkison <jon.gilkison@gmail.com>
2015-07-23 18:01:38 +0700

    Fixed get_plugin_name() to use WP_PLUGIN_DIR constant

--- admin/class-yoast-plugin-conflict.php
+++ admin/class-yoast-plugin-conflict.php
@@ -108,7 +108,7 @@ class Yoast_Plugin_Conflict {
 	 * @return bool
 	 */
 	private function get_plugin_name( $plugin ) {
-		$plugin_details = get_plugin_data( ABSPATH . '/wp-content/plugins/' . $plugin );
+		$plugin_details = get_plugin_data( WP_PLUGIN_DIR .'/'. $plugin );
 
 		if ( $plugin_details['Name'] != '' ) {
 			return $plugin_details['Name'];
