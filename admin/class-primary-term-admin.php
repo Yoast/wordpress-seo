4c3c742c94cbe4c0e6eb28e9fc27b4d28784a3bc
Anton Timmermans <anton@yoast.com>
2015-07-27 14:23:38 +0200

    Abstract primary term logic to new class
    This makes it more manageable and clear.

--- admin/class-primary-term-admin.php
+++ admin/class-primary-term-admin.php
@@ -50,10 +50,12 @@ class WPSEO_Primary_Term_Admin {
 
 			$terms = get_terms( $taxonomy->name );
 
+			$primary_term = new WPSEO_Primary_Term( $taxonomy->name, get_the_ID() );
+
 			return array(
 				'title'   => $taxonomy->labels->singular_name,
 				'name'    => $taxonomy->name,
-				'primary' => $this->get_primary_term_id( $taxonomy->name, get_the_ID() ),
+				'primary' => $primary_term->get_primary_term(),
 				'terms'   => array_map( function ( $term ) {
 					return array(
 						'id'   => $term->term_id,
@@ -83,42 +85,6 @@ class WPSEO_Primary_Term_Admin {
 	}
 
 	/**
-	 * Returns the primary taxonomy ID for a certain post and a taxonomy
-	 *
-	 * @param string $taxonomy
-	 * @param int    $post_id
-	 *
-	 * @return false|int
-	 */
-	public function get_primary_term_id( $taxonomy, $post_id ) {
-		$primary_category = get_post_meta( $post_id, WPSEO_Meta::$meta_prefix . 'primary_' . $taxonomy, true );
-
-		// By default the first term (sorted by ID) is the primary term.
-		if ( ! $primary_category ) {
-			$terms = get_the_terms( $post_id, $taxonomy );
-
-			if ( ! empty( $terms ) ) {
-				usort( $terms, '_usort_terms_by_ID' );
-				$primary_category = array_shift( $terms );
-				$primary_category = $primary_category->term_id;
-			}
-		}
-
-		return ( (int) $primary_category ) ? : false;
-	}
-
-	/**
-	 * Set's the primary category for a certain post
-	 *
-	 * @param string $taxonomy The taxonomy to set the primary term.
-	 * @param int    $post_id The post ID for which to set the primary category.
-	 * @param int    $primary_term_id The primary category ID to set for the post.
-	 */
-	public function set_primary_term_id( $taxonomy, $post_id, $primary_term_id ) {
-		update_post_meta( $post_id, WPSEO_Meta::$meta_prefix . 'primary_' . $taxonomy, $primary_term_id );
-	}
-
-	/**
 	 * Filters post_link_category to change the category to the chosen category by the user
 	 *
 	 * @param stdClass $category The category that is now used for the post link.
@@ -126,7 +92,8 @@ class WPSEO_Primary_Term_Admin {
 	 * @return array|null|object|WP_Error The category we want to use for the post link.
 	 */
 	public function post_link_category( $category ) {
-		$primary_category = $this->get_primary_term_id( 'category', get_the_ID() );
+		$primary_category = new WPSEO_Primary_Term( 'category', get_the_ID() );
+		$primary_category = $primary_category->get_primary_term();
 
 		if ( false !== $primary_category && $primary_category !== $category->cat_ID ) {
 			$category = get_category( $primary_category );
@@ -152,7 +119,8 @@ class WPSEO_Primary_Term_Admin {
 			return;
 		}
 
-		$this->set_primary_term_id( $taxonomy->name, $post_ID, $primary_term );
+		$primary_term_object = new WPSEO_Primary_Term( $taxonomy->name, $post_ID );
+		$primary_term_object->set_primary_term( $primary_term );
 	}
 
 	/**
---
+++ inc/class-wpseo-primary-term.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * @package WPSEO
+ */
+
+/**
+ * Represents a post's primary term
+ */
+class WPSEO_Primary_Term {
+
+	/**
+	 * @var string
+	 */
+	protected $taxonomy_name;
+
+	/**
+	 * @var int
+	 */
+	protected $post_ID;
+
+	/**
+	 * The taxonomy this term is part of
+	 *
+	 * @param string $taxonomy_name
+	 * @param int    $post_ID
+	 */
+	public function __construct( $taxonomy_name, $post_ID ) {
+		$this->taxonomy_name = $taxonomy_name;
+		$this->post_ID = $post_ID;
+	}
+
+	/**
+	 * Returns the primary term ID
+	 *
+	 * @return int
+	 */
+	public function get_primary_term() {
+		$primary_term = get_post_meta( $this->post_ID, WPSEO_Meta::$meta_prefix . 'primary_' . $this->taxonomy_name, true );
+
+		// By default the first term (sorted by ID) is the primary term.
+		if ( ! $primary_term ) {
+			$terms = get_the_terms( $this->post_ID, $this->taxonomy_name );
+
+			if ( ! empty( $terms ) ) {
+				usort( $terms, '_usort_terms_by_ID' );
+				$primary_term = array_shift( $terms );
+				$primary_term = $primary_term->term_id;
+			}
+		}
+
+		return ( (int) $primary_term ) ? : false;
+	}
+
+	/**
+	 * Set's the new primary term ID
+	 *
+	 * @param int $new_primary_term
+	 */
+	public function set_primary_term( $new_primary_term ) {
+		update_post_meta( $this->post_ID, WPSEO_Meta::$meta_prefix . 'primary_' . $this->taxonomy_name, $new_primary_term );
+	}
+}
