c3bb1ddef6539f7fef05bf6be41884d2c827457e
Danny <danny@yoast.com>
2015-07-27 10:23:12 +0200

    Updated grunt file, removed concat.js gruntconfig.

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,6 @@ module.exports = function(grunt) {
                 'spec/*.js',
                 '!spec/*.min.js'
             ],
-
             get config() {
                 return project.paths.config + '*.js';
             },
--- grunt/config/concat.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	dist: {
-		src: ['js/dist/*.min.js', 'js/dist/config/*.min.js'],
-		dest: 'js/dist/yoast-seo-content-analysis.min.js'
-	}
-};
