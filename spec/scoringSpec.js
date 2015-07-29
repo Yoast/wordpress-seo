bef4a6b5f895ba4b4d490bd6ba1961c61ae40e08
omarreiss <omar.reiss@gmail.com>
2015-07-29 11:21:05 +0200

    fix scoring spec

--- spec/scoringSpec.js
+++ spec/scoringSpec.js
@@ -27,7 +27,7 @@ describe("a test for the scoring function of all functions in the analyzer", fun
        expect(analyzeScore[1].text).toBe("The keyword density is 1.3%, which is great, the focus keyword was found 2 times.");
        expect(analyzeScore[2].name).toBe("fleschReading");
        expect(analyzeScore[2].score).toBe(5);
-       expect(analyzeScore[2].text).toBe("The copy scores 49.0 in the <a href='https://en.wikipedia.org/wiki/Flesch-Kincaid_readability_test#Flesch_Reading_Ease'>Flesch Reading Ease</a> test, which is considered difficult to read. Try to make shorter sentences, using less difficult words to improve readability.");
+       expect(analyzeScore[2].text).toBe("The copy scores 49.0 in the <a href='https://en.wikipedia.org/wiki/Flesch-Kincaid_readability_test#Flesch_Reading_Ease' target='new'>Flesch Reading Ease</a> test, which is considered difficult to read. Try to make shorter sentences, using less difficult words to improve readability.");
        expect(analyzeScore[3].name).toBe("firstParagraph");
        expect(analyzeScore[3].score).toBe(3);
        expect(analyzeScore[3].text).toBe("The focus keyword doesn\'t appear in the first paragraph of the copy, make sure the topic is clear immediately.");
@@ -39,7 +39,7 @@ describe("a test for the scoring function of all functions in the analyzer", fun
        expect(analyzeScore[5].text).toBe("A meta description has been specified, but it does not contain the focus keyword.");
        expect(analyzeScore[6].name).toBe("stopwordKeywordCount");
        expect(analyzeScore[6].score).toBe(5);
-       expect(analyzeScore[6].text).toBe("The focus keyword for this page contains one or more <a href='https://en.wikipedia.org/wiki/Stop_words'>stop words</a>, consider removing them. Found 'about'.");
+       expect(analyzeScore[6].text).toBe("The focus keyword for this page contains one or more <a href='https://en.wikipedia.org/wiki/Stop_words' target='new'>stop words</a>, consider removing them. Found 'about'.");
        expect(analyzeScore[7].name).toBe("subHeadings");
        expect(analyzeScore[7].score).toBe(7);
        expect(analyzeScore[8].name).toBe("pageTitleLength");
@@ -57,7 +57,7 @@ describe("a test for the scoring function of all functions in the analyzer", fun
        expect(analyzeScore[13].score).toBe(3);
        expect(analyzeScore[13].text).toBe("No images appear in this page, consider adding some as appropriate.");
        expect(analyzeScore[14].name).toBe("linkCount");
-       expect(analyzeScore[14].score).toBe(9);
-       expect(analyzeScore[14].text).toBe("This page has 3 outbound link(s).");
+       expect(analyzeScore[14].score).toBe(2);
+       expect(analyzeScore[14].text).toBe("You're linking to another page with the focus keyword you want this page to rank for, consider changing that if you truly want this page to rank.");
    });
 });
