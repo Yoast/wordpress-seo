03977f612b780b48341ca1891100d895cbffa98f
Danny <danny@yoast.com>
2015-07-30 14:30:48 +0200

    Updated bower.json to include the analyzer.min for the recalculate scores.

--- grunt/config/uglify.js
+++ grunt/config/uglify.js
@@ -6,7 +6,8 @@ module.exports = {
 			report: 'gzip'
 		},
 		files: {
-			'<%= paths.js %>/dist/yoast-seo-content-analysis.min.js': ['<%= paths.js %>/*.js', '<%= paths.js %>/config/*.js']
+			'<%= paths.js %>/dist/yoast-seo-content-analysis.min.js': ['<%= paths.js %>/*.js', '<%= paths.js %>/config/*.js'],
+			'<%= paths.js %>/dist/yoast-seo-content-analyzer.min.js': ['<%= paths.js %>/analyzer.js', '<%= paths.js %>/config/scoring.js']
 		}
 	}
 };
