efaf8151ad8a020e42740db9ef57cb27ffb7a2a8
Andy Meerwaldt <andy@yoast.com>
2015-08-20 16:26:13 +0200

    Writing tests for the file handlers

---
+++ tests/premium/test-class-redirect-file-htaccess.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @package WPSEO\Tests/Premium
+ */
+
+/**
+ * Test class for testing WPSEO_Redirect_File_Htaccess
+ *
+ * Class WPSEO_Redirect_File_Htaccess_Test
+ */
+class WPSEO_Redirect_File_Htaccess_Test extends WPSEO_UnitTestCase {
+
+	/**
+	 * @var WPSEO_Redirect_File_Apache
+	 */
+	protected $class_instance;
+
+	/**
+	 * Setting up the class instance and fill it with some fake redirects
+	 */
+	public function setUp() {
+		$this->class_instance = $this->getMock( 'WPSEO_Redirect_File_Htaccess', array( 'write_htaccess_content' ) );
+
+		// Letting write htaccess_content always return true.
+		$this->class_instance
+			->method( 'write_htaccess_content' )
+			->will( $this->returnValue( true ) );
+	}
+
+
+	/**
+	 * Testing if the saving method will be called and returns true
+	 *
+	 * @covers WPSEO_Redirect_File_Htaccess::generate_content
+	 */
+	public function test_save() {
+		$this->assertTrue( $this->class_instance->save( array( 'test' => array( 'url' => 'more-test', 'type' => 301 ) ), array() ) );
+	}
+
+	/**
+	 * Testing if the saving method will be called and returns true
+	 *
+	 * @covers WPSEO_Redirect_File_Htaccess::generate_content
+	 */
+	public function test_generate_content() {
+		$class_instance = $this->getMock( 'WPSEO_Redirect_File_Htaccess', array( 'generate_content' ) );
+		$class_instance
+			->expects( $this->once() )
+			->method( 'generate_content' )
+			->will( $this->returnValue( true ) );
+
+		$class_instance->save( array( 'test' => array( 'url' => 'more-test', 'type' => 301 ) ), array() );
+	}
+
+}
