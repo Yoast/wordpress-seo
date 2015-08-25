b92f1a0ceeacc0e36676d0d54efe148d392d2c5c
Danny <danny@yoast.com>
2015-08-25 14:01:34 +0200

    added concat option to grunt to make an easier to debug file

---
+++ grunt/config/concat.js
@@ -0,0 +1,9 @@
+module.exports = {
+	options: {
+		separator: ';'
+	},
+	dist: {
+		src: ["<%= paths.js %>/*.js","node_modules/jed/jed.js","<%= paths.js %>/config/*.js"],
+		dest: 'dist/yoast-seo-content-analysis.min.js'
+	}
+};
