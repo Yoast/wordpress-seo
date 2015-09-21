5080ef3f092edf6f8e853443787adbea6204cada
omarreiss <omar.reiss@gmail.com>
2015-09-21 13:50:48 +0200

    Add documentation to concat.js

--- grunt/config/concat.js
+++ grunt/config/concat.js
@@ -3,6 +3,15 @@ module.exports = {
 		separator: ";",
 		process: function( src, filepath ) {
 			"use strict";
+			/**
+			 * For the sake of convenience, we are shipping Jed as a hard dependency of YoastSEO.js.
+			 * To keep the global scope clean, we are putting Jed on the YoastSEO object.
+			 *
+			 * How to namespace Jed:
+			 *
+			 * Jed assigns itself to the scope on which the closure in which Jed is defined is bound.
+			 * In order to namespace Jed within YoastSEO, all we have to do is wrap it in a closure that is called with YoastSEO as its scope.
+			 */
 			if ( filepath === "node_modules/jed/jed.js" ) {
 				return "YoastSEO = ( 'undefined' === typeof YoastSEO ) ? {} : YoastSEO;(function() {" + src + "}.call(YoastSEO))";
 			}
