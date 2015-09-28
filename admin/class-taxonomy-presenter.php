0ef31b6a70d6c02ffae71d7a8b7706d39f9484d0
Andy Meerwaldt <andy@yoast.com>
2015-09-28 14:47:59 +0200

    Separate the responsibilities of the method.

--- admin/class-taxonomy-presenter.php
+++ admin/class-taxonomy-presenter.php
@@ -11,7 +11,7 @@ class WPSEO_Taxonomy_Presenter {
 	/**
 	 * The taxonomy meta data for the current term
 	 *
-	 * @var bool|mixed
+	 * @var array
 	 */
 	private $tax_meta;
 
@@ -30,7 +30,7 @@ class WPSEO_Taxonomy_Presenter {
 	public function display_fields( array $fields ) {
 		foreach ( $fields as $field_name => $field_options ) {
 			if ( empty( $field_options['hide'] ) ) {
-				$this->form_row( 'wpseo_' . $field_name, $field_options['label'], $field_options['description'], $field_options['type'], $field_options['options'] );
+				$this->form_row( 'wpseo_' . $field_name, $field_options );
 			}
 		}
 	}
@@ -38,52 +38,58 @@ class WPSEO_Taxonomy_Presenter {
 	/**
 	 * Create a row in the form table.
 	 *
-	 * @param string $field_name      Variable the row controls.
-	 * @param string $label    Label for the variable.
-	 * @param string $description     Description of the use of the variable.
-	 * @param string $type     Type of form row to create.
-	 * @param array  $options  Options to use when form row is a select box.
+	 * @param string $field_name    Variable the row controls.
+	 * @param array  $field_options Array with the field configuration.
 	 */
-	private function form_row( $field_name, $label, $description, $type = 'text', $options = array() ) {
-		$value = '';
-		if ( isset( $this->tax_meta[ $field_name ] ) && $this->tax_meta[ $field_name ] !== '' ) {
-			$value = $this->tax_meta[ $field_name ];
-		}
+	private function form_row( $field_name, array $field_options ) {
+		$esc_field_name = esc_attr( $field_name );
 
-		$esc_var = esc_attr( $field_name );
-		$field   = '';
+		$label = $this->get_label( $field_options['label'], $esc_field_name );
+		$field = $this->get_field( $field_options['type'], $esc_field_name, $this->get_field_value( $field_name ), (array) $field_options['options'] );
+		$help  = $this->get_help( $field, $field_options['description'], $esc_field_name );
 
-		$class = '';
-		if ( ! empty( $options['class'] ) ) {
-			$class = ' class="' . esc_attr( $options['class'] ) . '"';
-		}
+		echo $this->parse_row( $label, $help, $field );
+	}
 
-		switch ( $type ) {
+	/**
+	 * @param string $field_type  The fieldtype, e.g: text, checkbox, etc.
+	 * @param string $field_name  The name of the field.
+	 * @param string $field_value The value of the field.
+	 * @param array  $options     Array with additional options.
+	 *
+	 * @return string
+	 */
+	private function get_field($field_type, $field_name, $field_value, array $options) {
+
+		$class = $this->get_class( $options );
+		$field = '';
+
+		switch ( $field_type ) {
 			case 'text' :
-				$field .= '<input name="' . $esc_var . '" id="' . $esc_var . '" ' . $class . ' type="text" value="' . esc_attr( $value ) . '" size="40"/>';
+				$field .= '<input name="' . $field_name . '" id="' . $field_name . '" ' . $class . ' type="text" value="' . esc_attr( $field_value ) . '" size="40"/>';
 				break;
 			case 'checkbox' :
-				$field .= '<input name="' . $esc_var . '" id="' . $esc_var . '" type="checkbox" ' . checked( $value ) . '/>';
+				$field .= '<input name="' . $field_name . '" id="' . $field_name . '" type="checkbox" ' . checked( $field_value ) . '/>';
 				break;
 			case 'textarea' :
 				$rows = 3;
 				if ( ! empty( $options['rows'] ) ) {
 					$rows = $options['rows'];
 				}
-				$field .= '<textarea class="large-text" rows="' . esc_attr( $rows ) . '" id="' . $esc_var . '" name="' . $esc_var . '">' . esc_textarea( $value ) . '</textarea>';
+				$field .= '<textarea class="large-text" rows="' . esc_attr( $rows ) . '" id="' . $field_name . '" name="' . $field_name . '">' . esc_textarea( $field_value ) . '</textarea>';
 				break;
 			case 'upload' :
-				$field .= '<input id="' . $esc_var . '" type="text" size="36" name="' . $esc_var . '" value="' . esc_attr( $value ) . '" />';
-				$field .= '<input id="' . $esc_var . '_button" class="wpseo_image_upload_button button" type="button" value="Upload Image" />';
+				$field .= '<input id="' . $field_name . '" type="text" size="36" name="' . $field_name . '" value="' . esc_attr( $field_value ) . '" />';
+				$field .= '<input id="' . $field_name . '_button" class="wpseo_image_upload_button button" type="button" value="Upload Image" />';
 				break;
 			case 'select' :
 				if ( is_array( $options ) && $options !== array() ) {
-					$field .= '<select name="' . $esc_var . '" id="' . $esc_var . '">';
+					$field .= '<select name="' . $field_name . '" id="' . $field_name . '">';
 
 					$select_options = ( array_key_exists( 'options', $options ) ) ? $options['options'] : $options;
 
 					foreach ( $select_options as $option => $option_label ) {
-						$selected = selected( $option, $value, false );
+						$selected = selected( $option, $field_value, false );
 						$field .= '<option ' . $selected . ' value="' . esc_attr( $option ) . '">' . esc_html( $option_label ) . '</option>';
 					}
 					unset( $option, $option_label, $selected );
@@ -92,30 +98,85 @@ class WPSEO_Taxonomy_Presenter {
 				}
 				break;
 			case 'hidden' :
-				$field .= '<input name="' . $esc_var . '" id="hidden_' . $esc_var . '" type="hidden" value="' . esc_attr( $value ) . '" />';
+				$field .= '<input name="' . $field_name . '" id="hidden_' . $field_name . '" type="hidden" value="' . esc_attr( $field_value ) . '" />';
 				break;
-		}
-
-		$help = '';
-		if ( $field !== '' && ( is_string( $description ) && $description !== '' ) ) {
-			$help = $this->parse_help( $esc_var, $description );
 		}
 
 		if ( $field !== '' && ( ! empty( $options['description'] ) && is_string( $options['description'] ) ) ) {
 			$field .= '<p class="description">' . $options['description'] . '</p>';
 		}
 
-		echo '<tr>
-			<th scope="row">' . ( ( '' !== $label ) ? '<label for="' . $esc_var . '">' . esc_html( $label ) . '</label>' : '' ) . '' . $help . '</th>
-			<td>' . $field . '</td>
-		</tr>';
+		return $field;
+	}
+
+	/**
+	 * Getting the value for given field_name
+	 *
+	 * @param string $field_name
+	 *
+	 * @return string
+	 */
+	private function get_field_value( $field_name ) {
+		if ( isset( $this->tax_meta[ $field_name ] ) && $this->tax_meta[ $field_name ] !== '' ) {
+			return $this->tax_meta[ $field_name ];
+		}
+
+		return '';
+	}
+
+	/**
+	 * Getting the class attributes if $options contains a class key
+	 *
+	 * @param array $options
+	 *
+	 * @return string
+	 */
+	private function get_class( array $options ) {
+		if ( ! empty( $options['class'] ) ) {
+			return ' class="' . esc_attr( $options['class'] ) . '"';
+		}
+
+		return '';
+	}
+
+	/**
+	 * Getting the label HTML
+	 *
+	 * @param string $label
+	 * @param string $field_name
+	 *
+	 * @return string
+	 */
+	private function get_label( $label, $field_name ) {
+		if ( $label !== '' ) {
+			return '<label for="' . $field_name . '">' . esc_html( $label ) . '</label>';
+		}
+
+		return '';
+	}
+
+	/**
+	 * Returns the help text
+	 *
+	 * @param string $field_html The generated HTML for the field.
+	 * @param string $help_text  The help text that will be displayed.
+	 * @param string $field_name The field name.
+	 *
+	 * @return string
+	 */
+	private function get_help( $field_html, $help_text, $field_name ) {
+		if ( $field_html !== '' && ( is_string( $help_text ) && $help_text !== '' ) ) {
+			return $this->parse_help( $field_name, $help_text );
+		}
+
+		return '';
 	}
 
 	/**
 	 * Parsing question mark with the help-text
 	 *
-	 * @param string $field_name
-	 * @param string $help_text
+	 * @param string $field_name The name of the field where the helptext is generated for.
+	 * @param string $help_text  The help text itself.
 	 *
 	 * @return string
 	 */
@@ -132,6 +193,19 @@ class WPSEO_Taxonomy_Presenter {
 			esc_attr( $field_name . 'help' ),
 			esc_attr( $help_text )
 		);
+	}
+
+	/**
+	 * Returns the HTML for the row which contains label, help and the field.
+	 *
+	 * @param string $label The html for the label if there was a label set.
+	 * @param string $help  The html for the help, when it's there.
+	 * @param string $field The html for the field.
+	 *
+	 * @return string
+	 */
+	private function parse_row( $label, $help, $field ) {
+		return '<tr><th scope="row">' . $label . $help . '</th><td>' . $field . '</td></tr>';
 	}
 
 }
