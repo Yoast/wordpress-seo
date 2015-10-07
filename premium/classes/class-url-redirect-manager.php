71b92aadb6763f9a9b737c2dbc61a9449c7b81ba
Andy Meerwaldt <andy@yoast.com>
2015-10-07 15:22:34 +0200

    Overriding some methods.

--- premium/classes/class-url-redirect-manager.php
+++ premium/classes/class-url-redirect-manager.php
@@ -29,16 +29,33 @@ class WPSEO_URL_Redirect_Manager extends WPSEO_Redirect_Manager {
 	}
 
 	/**
+	 * Create a new redirect
+	 *
+	 * @param string $old_value
+	 * @param string $new_value
+	 * @param int    $type
+	 *
+	 * @return bool|array
+	 */
+	public function create_redirect( $old_value, $new_value, $type ) {
+		$old_value = WPSEO_Utils::format_url( $old_value );
+
+		return parent::create_redirect( $old_value, $new_value, $type );
+	}
+
+	/**
 	 * Save the redirect
 	 *
-	 * @param array $old_redirect_arr
-	 * @param array $new_redirect
+	 * @param string $old_redirect_key
+	 * @param array  $new_redirect
+	 *
+	 * @return array|bool
 	 */
-	public function save_redirect( array $old_redirect_arr, array $new_redirect ) {
+	public function update_redirect( $old_redirect_key, array $new_redirect ) {
 		// Format the URL if it's an URL.
 		$new_redirect['key'] = WPSEO_Utils::format_url( $new_redirect['key'] );
 
-		parent::save_redirect( $old_redirect_arr, $new_redirect );
+		return parent::update_redirect( $old_redirect_key, $new_redirect );
 	}
 
 	/**
@@ -52,4 +69,18 @@ class WPSEO_URL_Redirect_Manager extends WPSEO_Redirect_Manager {
 			'regex' => new WPSEO_REGEX_Redirect_Manager(),
 		);
 	}
+
+	/*
+	protected function validate( $old_url, $new_url ) {
+		// If the $new_url already exists, we've to check for an infinite redirect loop.
+		$validation = new WPSEO_Redirect_Validate( $this->get_redirects() );
+
+		if ( $validation->trace_loop() ) {
+
+		}
+
+
+	}
+	*/
+
 }
