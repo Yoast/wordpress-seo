58c471cd73134046ebd1ee87d40ea45ce8b73840
Andy Meerwaldt <andy@yoast.com>
2015-10-12 11:44:52 +0200

    Combine the two tests.

--- tests/functions/test-wpseo-xml-sitemaps-base-url.php
+++ tests/functions/test-wpseo-xml-sitemaps-base-url.php
@@ -18,6 +18,7 @@ class WPSEO_XML_Sitemaps_Base_URL extends WPSEO_UnitTestCase {
 		update_option( 'home', 'http://example.org' );
 
 		$this->assertEquals( 'http://example.org/sitemap.xml', wpseo_xml_sitemaps_base_url( 'sitemap.xml' ) );
+		$this->assertNotEquals( 'https://example.org/sitemap.xml', wpseo_xml_sitemaps_base_url( 'sitemap.xml' ) );
 	}
 
 	/**
@@ -27,24 +28,6 @@ class WPSEO_XML_Sitemaps_Base_URL extends WPSEO_UnitTestCase {
 		update_option( 'home', 'https://example.org' );
 
 		$this->assertEquals( 'https://example.org/sitemap.xml', wpseo_xml_sitemaps_base_url( 'sitemap.xml' ) );
-	}
-
-	/**
-	 * Call the http sitemap with the settings on https
-	 */
-	public function test_http_with_https() {
-		update_option( 'home', 'https://example.org' );
-
 		$this->assertNotEquals( 'http://example.org/sitemap.xml', wpseo_xml_sitemaps_base_url( 'sitemap.xml' ) );
 	}
-
-	/**
-	 * Call the https sitemap with the settings on http
-	 */
-	public function test_https_with_http() {
-		update_option( 'home', 'http://example.org' );
-
-		$this->assertNotEquals( 'https://example.org/sitemap.xml', wpseo_xml_sitemaps_base_url( 'sitemap.xml' ) );
-	}
-
 }
