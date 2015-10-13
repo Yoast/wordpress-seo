1268bb430de1541513fc6f8ae104b67636a658d5
Andy Meerwaldt <andy@yoast.com>
2015-10-13 10:48:41 +0200

    Notify the admin and show a notice for each admin user.

--- admin/class-onpage-status.php
+++ admin/class-onpage-status.php
@@ -9,30 +9,41 @@
 class WPSEO_OnPage_Status {
 
 	/**
-	 * Fetching the new status from the API for given $home_url
-	 *
-	 * @param string $home_url Target url for the indexable status lookup.
-	 *
-	 * @return bool
+	 * @var string Target url for the indexable status lookup.
 	 */
-	public function fetch_new_status( $home_url ) {
-		$response         = $this->do_request( $home_url );
-		$new_index_status = ( $response['is_indexable'] === 1 );
+	private $target_url;
 
-		return $new_index_status;
+	/**
+	 * @var bool The fetched status of the target url is indexable or not.
+	 */
+	private $fetched_index_status;
+
+	/**
+	 * Construct the status object
+	 *
+	 * @param string $target_url The URL that will be fetched.
+	 */
+	public function __construct( $target_url = '' ) {
+		$this->target_url = $target_url;
+	}
+
+	/**
+	 * Fetching the new status from the API for the set target_url
+	 */
+	public function fetch_new_status() {
+		$response           = $this->do_request( $this->target_url );
+		$this->fetched_index_status = ( $response['is_indexable'] === 1 );
 	}
 
 	/**
 	 * Compare new index status and store the value when the current status isn't different from the new status
 	 *
-	 * @param bool $new_index_status The fetched status that will be compared with the current status.
-	 *
 	 * @return bool
 	 */
-	public function compare_index_status( $new_index_status) {
+	public function compare_index_status() {
 		// When the status isn't different from the current status, just save the new status.
-		if ( $this->get_index_status() !== $new_index_status ) {
-			$this->set_index_status( $new_index_status );
+		if ( $this->get_current_index_status() !== $this->fetched_index_status ) {
+			$this->set_index_status();
 
 			return true;
 		}
@@ -46,7 +57,16 @@ class WPSEO_OnPage_Status {
 	 * @return bool
 	 */
 	public function is_indexable() {
-		return $this->get_index_status();
+		return ! empty( $this->get_current_index_status() );
+	}
+
+	/**
+	 * Returns the value of the fetched_index_status property
+	 *
+	 * @return bool
+	 */
+	public function get_fetched_index_status() {
+		return $this->fetched_index_status;
 	}
 
 	/**
@@ -54,13 +74,13 @@ class WPSEO_OnPage_Status {
 	 *
 	 * @param string $home_url The URL that will be send to the API.
 	 *
-	 * @return array|mixed|object
+	 * @return array
 	 */
 	private function do_request( $home_url ) {
 		$response  = wp_remote_get( WPSEO_ONPAGE . $this->get_end_url( $home_url ) );
 		$json_body = json_decode( wp_remote_retrieve_body( $response ), true );
 
-		// OnPage recognized a redirect, fetch the data of that URL.
+		// OnPage.org recognized a redirect, fetch the data of that URL by calling this method with the value from OnPage.org.
 		if ( ! empty( $json_body['passes_juice_to'] ) ) {
 			return $this->do_request( $json_body['passes_juice_to'] );
 		}
@@ -71,20 +91,17 @@ class WPSEO_OnPage_Status {
 	/**
 	 * Getting the current saved index status.
 	 *
-	 * @return bool
+	 * @return int|null
 	 */
-	private function get_index_status() {
-		$option_value = get_site_option( 'wpseo_onpage_index_status', 0 );
-		return ! empty( $option_value );
+	private function get_current_index_status() {
+		return get_site_option( 'wpseo_onpage_index_status', null );
 	}
 
 	/**
-	 * Setting a new index status
-	 *
-	 * @param bool $new_index_status The status that will be saved.
+	 * Setting the new index status
 	 */
-	private function set_index_status( $new_index_status ) {
-		update_site_option( 'wpseo_onpage_index_status', $new_index_status );
+	private function set_index_status() {
+		update_site_option( 'wpseo_onpage_index_status', (int) $this->fetched_index_status );
 	}
 
 	/**
--- admin/class-onpage.php
+++ admin/class-onpage.php
@@ -9,6 +9,11 @@
 class WPSEO_OnPage {
 
 	/**
+	 * The name of the user meta key for storing the dismissed status.
+	 */
+	const USERMETAVALUE = 'wpseo_dismiss_onpage';
+
+	/**
 	 * @var WPSEO_OnPage_Status
 	 */
 	private $onpage_status;
@@ -19,7 +24,7 @@ class WPSEO_OnPage {
 	public function __construct() {
 		$this->set_hooks();
 
-		$this->onpage_status = new WPSEO_OnPage_Status();
+		$this->onpage_status = new WPSEO_OnPage_Status( home_url() );
 	}
 
 	/**
@@ -39,16 +44,43 @@ class WPSEO_OnPage {
 	 * Fetching the data from onpage.
 	 */
 	public function fetch_from_onpage() {
-		$index_status = $this->onpage_status->fetch_new_status( home_url() );
-		$this->onpage_status->compare_index_status( $index_status );
+		$this->onpage_status->fetch_new_status();
+
+		// When the value is changed, the method will return true.
+		if ( $this->onpage_status->compare_index_status() ) {
+			$this->remove_hide_notice_user_meta();
+			$this->notify_admin_by_email();
+		}
 	}
 
 	/**
 	 * Setting the cronjob
 	 */
 	public function set_cron() {
+
+		wp_unschedule_event( 'wpseo_onpage_fetch' );
+
 		if ( ! wp_next_scheduled( 'wpseo_onpage_fetch' ) ) {
 			wp_schedule_event( time(), 'weekly', 'wpseo_onpage_fetch' );
+		}
+	}
+
+	/**
+	 * Show a notice when the website is not indexable
+	 */
+	public function show_notice() {
+		$show_notice = WPSEO_Utils::grant_access() && $this->user_has_not_dismissed() && ! $this->onpage_status->is_indexable();
+		if ( $show_notice ) {
+			Yoast_Notification_Center::get()->add_notification(
+				new Yoast_Notification(
+					__( 'Your site is currently not indexable.', 'wordpress-seo' ),
+					array(
+						'type'  => 'error yoast-dismissible',
+						'id'    => 'wpseo-dismiss-onpageorg',
+						'nonce' => wp_create_nonce( 'wpseo-dismiss-onpageorg' ),
+					)
+				)
+			);
 		}
 	}
 
@@ -62,8 +94,67 @@ class WPSEO_OnPage {
 		// Add weekly schedule to the cron job schedules.
 		add_filter( 'cron_schedules', array( $this, 'add_weekly_schedule' ) );
 
+		// Adding admin notice if necessary.
+		add_filter( 'admin_init', array( $this, 'show_notice' ) );
+
 		// Setting the action for the OnPage fetch.
 		add_action( 'wpseo_onpage_fetch', array( $this, 'fetch_from_onpage' ) );
+	}
+
+	/**
+	 * Send an email to the site admin
+	 *
+	 */
+	private function notify_admin_by_email() {
+		$index_status_value = $this->get_status_value();
+
+		wp_mail(
+			get_option( 'admin_email' ),
+			__( 'OnPage.org index status', 'wordpress-seo' ),
+			sprintf(
+				__(
+					'The indexability from your website %1$s, went from %2$s to %3$s'
+				),
+				home_url(),
+				$index_status_value['old_status'],
+				$index_status_value['new_status']
+			)
+		);
+	}
+
+	/**
+	 * Returns the array with the values for the new and the old index status
+	 *
+	 * @return array
+	 */
+	private function get_status_value() {
+		$index_status  = $this->onpage_status->get_fetched_index_status();
+		$not_indexable = __( 'not indexable', 'wordpress-seo' );
+		$indexable     = __( 'indexable', 'wordpress-seo' );
+		if ( $index_status ) {
+			return [ 'old_status' => $not_indexable, 'new_status' => $indexable ];
+		}
+
+		return [ 'old_status' => $indexable, 'new_status' => $not_indexable ];
+	}
+
+	/**
+	 * Removes the hide notice state in the user meta table
+	 */
+	private function remove_hide_notice_user_meta() {
+		global $wpdb;
+
+		// Remove the user meta data
+		$wpdb->query( "DELETE FROM " . $wpdb->usermeta . " WHERE meta_key = '" . WPSEO_OnPage::USERMETAVALUE . "'" );
+	}
+
+	/**
+	 * Get the state from the user to check if the current user has dismissed
+	 *
+	 * @return mixed
+	 */
+	private function user_has_not_dismissed() {
+		return '1' === ( get_user_meta( get_current_user_id(), WPSEO_OnPage::USERMETAVALUE, true ) );
 	}
 
 }
