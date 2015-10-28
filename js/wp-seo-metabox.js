8b0f31c63824a74ff957b969060e1278c24294a8
omarreiss <omar.reiss@gmail.com>
2015-10-28 10:01:34 +0100

    codestyle

--- js/wp-seo-metabox.js
+++ js/wp-seo-metabox.js
@@ -42,7 +42,7 @@
 				active_page = active_page.replace( '#wpseo-meta-section-', '' );
 			}
 			jQuery( '#wpseo-meta-section-' + active_page ).addClass( 'active' );
-			jQuery( '.wpseo-metabox-sidebar li').filter( function( i, el ) {
+			jQuery( '.wpseo-metabox-sidebar li').filter( function() {
 				return jQuery( this ).find('.wpseo-meta-section-link').attr( 'href' ) === '#wpseo-meta-section-' + active_page;
 			} ).addClass('active');
 
@@ -90,9 +90,9 @@
 	};
 
 	jQuery( document ).ready( function() {
-		jQuery( '.wpseo-meta-section').each( function( _, el) {
-			jQuery(el).find('.wpseo-metabox-tabs li:first').addClass("active");
-			jQuery(el).find('.wpseotab:first').addClass("active");
+		jQuery( '.wpseo-meta-section').each( function( _, el ) {
+			jQuery( el ).find( '.wpseo-metabox-tabs li:first' ).addClass( 'active' );
+			jQuery( el ).find( '.wpseotab:first' ).addClass( 'active' );
 		});
 		window.wpseo_init_tabs();
 	});
