0c5df2bc78e782d1b02d40d26fbf1d72a8bf1737
omarreiss <omar.reiss@gmail.com>
2015-10-30 16:54:25 +0100

    let term focuskw field name be consistent with the post field name

--- admin/taxonomy/class-taxonomy-content-fields.php
+++ admin/taxonomy/class-taxonomy-content-fields.php
@@ -20,7 +20,7 @@ class WPSEO_Taxonomy_Content_Fields extends WPSEO_Taxonomy_Fields {
 				sprintf( __( 'This is a rendering of what this post might look like in Google\'s search results.<br/><br/>Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/snippet-preview/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
 				'div'
 			),
-			'keyword' => $this->get_field_config(
+			'focuskw' => $this->get_field_config(
 				__( 'Focus Keyword', 'wordpress-seo' ),
 				sprintf( __( 'Pick the main keyword or keyphrase that this post/page is about.<br/><br/>Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/focus-keyword/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=focus-keyword">', '</a>' )
 			),
