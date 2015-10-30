4c65a66a35292a7159c5853df6c5f9d7aa0868a1
Andy Meerwaldt <andy@yoast.com>
2015-10-30 11:25:55 +0100

    Removing the identify methods and adding the suggest logic.

--- premium/classes/class-helpscout-beacon.php
+++ premium/classes/class-helpscout-beacon.php
@@ -7,23 +7,62 @@
  * This class adds the helpscout beacon by hooking on admin_footer.
  */
 class WPSEO_HelpScout_Beacon {
+
 	const YST_SEO_SUPPORT_IDENTIFY = 'yst_seo_support_identify';
 
 	/**
-	 * Setting the hook to load the beacon
+	 * @var string The current opened page without the prefix.
 	 */
-	public function __construct() {
-		add_action( 'admin_footer', array( $this, 'load_beacon' ) );
+	private $current_page = '';
+
+	/**
+	 * Setting the hook to load the beacon
+	 *
+	 * @param string $current_page The current opened page without the prefix.
+	 */
+	public function __construct( $current_page ) {
+		$this->current_page = $current_page;
+		add_action( 'admin_enqueue_scripts', array( $this, 'load_assets' ) );
 	}
 
 	/**
-	 * Loading the beacon view.
+	 * Loading the js file and the translations for the HelpScout beacon.
 	 */
-	public function load_beacon() {
-		$translation = WPSEO_Utils::json_encode( $this->get_translations() );
-		$identify    = WPSEO_Utils::json_encode( $this->identify_data() );
+	public function load_assets() {
+		wp_enqueue_script( 'yoast-seo-helpscout-beacon', plugins_url( 'premium/assets/js/yoast-seo-helpscout-beacon' . WPSEO_CSSJS_SUFFIX . '.js', WPSEO_FILE ), array(), WPSEO_VERSION, true );
+		wp_localize_script( 'yoast-seo-helpscout-beacon', 'wpseoHelpscoutBeaconL10n', $this->localize_beacon() );
+	}
 
-		require WPSEO_PATH . 'premium/views/helpscout_beacon.php';
+	/**
+	 * Loads the beacon translations
+	 *
+	 * @return array
+	 */
+	private function localize_beacon() {
+		return array(
+			'config' => array(
+				'instructions' => $this->get_instructions(),
+				'icon'         => 'question',
+				'color'        => '#A4286A',
+				'poweredBy'    => false,
+				'translation'  => $this->get_translations(),
+			),
+			'identify' => $this->get_identify(),
+			'suggest'  => $this->get_suggest(),
+		);
+	}
+
+	/**
+	 * Parsing the text for the beacon instructions, this is what the user sees when he is using the beacon
+	 *
+	 * @return string
+	 */
+	private function get_instructions() {
+		$return  = __( "Please explain what you're trying to find or do. If something isn't working, please explain what you expect to happen. If you can make a screenshot, please attach it.", 'yoastcom' );
+		$return .= ' ';
+		$return .= __( "Note: submitting this form also sends us debug info about your server.", 'yoastcom' );
+
+		return $return;
 	}
 
 	/**
@@ -55,15 +94,17 @@ class WPSEO_HelpScout_Beacon {
 		);
 	}
 
+
 	/**
 	 * Retrieve data to populate the beacon email form
 	 *
 	 * @return array
 	 */
-	private function identify_data() {
+	private function get_identify() {
 		$identify_data = get_transient( self::YST_SEO_SUPPORT_IDENTIFY );
 		if ( ! $identify_data ) {
-			$identify_data = $this->build_identify_data();
+			$identifier = new WPSEO_HelpScout_Beacon_Identifier();
+			$identify_data = $identifier->get_data();
 			if ( ! defined( 'WP_DEBUG' ) || ! WP_DEBUG ) {
 				set_transient( self::YST_SEO_SUPPORT_IDENTIFY, $identify_data, DAY_IN_SECONDS );
 			}
@@ -73,153 +114,14 @@ class WPSEO_HelpScout_Beacon {
 	}
 
 	/**
-	 * Build data to populate the beacon email form
+	 * Getting the suggestions for the current page
 	 *
 	 * @return array
 	 */
-	private function build_identify_data() {
-		// Do not make these strings translateable! They are for our support agents, the user won't see them!
-		$identify_data = array(
-			'name'                                                     => $this->identify_user(),
-			'email'                                                    => $this->identify_user( 'email' ),
-			'WordPress Version'                                        => $this->identify_wordpress_version(),
-			'Yoast SEO'                                                => $this->identify_yoast_seo_data(),
-			'Server'                                                   => $this->identify_server(),
-			'<a href="' . admin_url( 'themes.php' ) . '">Theme</a>'    => $this->identify_theme(),
-			'<a href="' . admin_url( 'plugins.php' ) . '">Plugins</a>' => $this->identify_plugins(),
-		);
+	private function get_suggest() {
+		$suggest = new WPSEO_HelpScout_Beacon_Suggest();
 
-		return $identify_data;
+		return $suggest->get_suggest( $this->current_page );
 	}
 
-	/**
-	 * Returns basic info about the server software
-	 *
-	 * @return string
-	 */
-	private function identify_server() {
-		$out = '<table>';
-		$out .= '<tr><td>IP</td><td>' . filter_input( INPUT_SERVER, 'SERVER_ADDR' ) . '</td></tr>';
-		$out .= '<tr><td>Hostname</td><td>' . gethostbyaddr( filter_input( INPUT_SERVER, 'SERVER_ADDR' ) ) . '</td></tr>';
-		$out .= '<tr><td>OS</td><td>' . php_uname( 's r' ) . '</td></tr>';
-		$out .= '<tr><td>PHP</td><td>' . PHP_VERSION . '</td></tr>';
-		$out .= '<tr><td>CURL</td><td>' . $this->identify_curl() . '</td></tr>';
-		$out .= '</table>';
-
-		return $out;
-	}
-
-	/**
-	 * Returns info about the Yoast SEO plugin version and license
-	 *
-	 * @return string
-	 */
-	private function identify_yoast_seo_data() {
-		$license_manager = new Yoast_Plugin_License_Manager( new WPSEO_Product_Premium() );
-
-		$out = '<table>';
-		$out .= '<tr><td>Version</td><td>' . WPSEO_VERSION . '</td></tr>';
-		$out .= '<tr><td>License</td><td>' . '<a href=" ' . admin_url( 'admin.php?page=wpseo_licenses#top#licenses' ) . ' ">' . $license_manager->get_license_key() . '</a>' . '</td></tr>';
-		$out .= '<tr><td>Status</td><td>' . $license_manager->get_license_status() . '</td></tr>';
-		$out .= '</table>';
-
-		return $out;
-	}
-
-	/**
-	 * Returns info about the current user
-	 *
-	 * @param string $what What to retrieve, defaults to name
-	 *
-	 * @return string
-	 */
-	private function identify_user( $what = 'name' ) {
-		global $current_user;
-		get_currentuserinfo();
-
-		switch ( $what ) {
-			case 'email':
-				$out = $current_user->user_email;
-				break;
-			case 'name':
-			default:
-				$out = $current_user->user_firstname . ' ' . $current_user->user_lastname;
-				break;
-		}
-
-		return $out;
-	}
-
-	/**
-	 * Returns the WordPress version + a suffix if current WP is multi site
-	 *
-	 * @return string
-	 */
-	private function identify_wordpress_version() {
-		global $wp_version;
-		$msg = $wp_version;
-		if ( is_multisite() ) {
-			$msg .= ' MULTI-SITE';
-		}
-
-		return $msg;
-	}
-
-	/**
-	 * Returns the curl version, if curl is found
-	 * @return string
-	 */
-	private function identify_curl() {
-		if ( function_exists( 'curl_version' ) ) {
-			$curl = curl_version();
-			$msg  = $curl['version'];
-			if ( ! $curl['features'] & CURL_VERSION_SSL ) {
-				$msg .= " - NO SSL SUPPORT";
-			}
-		} else {
-			$msg = 'No CURL installed';
-		}
-
-		return $msg;
-	}
-
-	/**
-	 * Returns a formatted HTML string for the current theme
-	 *
-	 * @return string
-	 */
-	private function identify_theme() {
-		$theme = wp_get_theme();
-
-		$msg = '<a href="' . $theme->display( 'ThemeURI' ) . '">' . $theme->display( 'Name' ) . '</a> v' . $theme->display( 'Version' ) . ' by ' . $theme->display( 'Author' );
-
-		if ( is_child_theme() ) {
-			$msg .= '<br />' . 'Child theme of: ' . $theme->display( 'Template' );
-		}
-
-		return $msg;
-	}
-
-	/**
-	 * Returns a formatted HTML list of all active plugins
-	 *
-	 * @return string
-	 */
-	private function identify_plugins() {
-		$updates_avail = get_site_transient( 'update_plugins' );
-
-		$msg = '';
-		foreach ( wp_get_active_and_valid_plugins() as $plugin ) {
-			$plugin_data = get_plugin_data( $plugin );
-
-			$plugin_file = str_replace( trailingslashit( WP_PLUGIN_DIR ), '', $plugin );
-
-			if ( isset( $updates_avail->response[ $plugin_file ] ) ) {
-				$msg .= '<i class="icon-close1"></i> ';
-			}
-			$msg .= '<a href="' . $plugin_data['PluginURI'] . '">' . $plugin_data['Name'] . '</a> v' . $plugin_data['Version'] . '<br/>';
-		}
-
-		return $msg;
-	}
 }
