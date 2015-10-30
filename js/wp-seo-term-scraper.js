fc4cd63e2e5c02d0ffe7ffa7452e505f6f6c9d98
omarreiss <omar.reiss@gmail.com>
2015-10-30 17:42:54 +0100

    whitespace

--- js/wp-seo-term-scraper.js
+++ js/wp-seo-term-scraper.js
@@ -125,8 +125,8 @@ YoastSEO = ( 'undefined' === typeof YoastSEO ) ? {} : YoastSEO;
 			this.bindSnippetEvents( document.getElementById( elems [ i ] ), snippetPreview );
 		}
 		var title = document.getElementById( 'snippet_title' );
-		title.addEventListener( 'focus', snippetPreview.setSiteName.bind ( snippetPreview ) );
-		title.addEventListener( 'blur', snippetPreview.unsetSiteName.bind ( snippetPreview ) );
+		title.addEventListener( 'focus', snippetPreview.setSiteName.bind( snippetPreview ) );
+		title.addEventListener( 'blur', snippetPreview.unsetSiteName.bind( snippetPreview ) );
 	};
 
 	/**
