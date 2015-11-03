e9be58d202a684dbe9db9edca3a0aa48a0adb129
Andy Meerwaldt <andy@yoast.com>
2015-11-03 11:12:57 +0100

    Fixing the codestyle.

--- admin/ajax/class-recalculate-scores-ajax.php
+++ admin/ajax/class-recalculate-scores-ajax.php
@@ -56,10 +56,7 @@ class WPSEO_Recalculate_Scores_Ajax {
 		foreach ( $scores as $score ) {
 			WPSEO_Meta::set_value( 'linkdex', $score['score'], $score['post_id'] );
 		}
-
-		exit;
-
-//		WPSEO_Meta::set_value( 'linkdex', filter_input( INPUT_POST, 'score' ), filter_input( INPUT_POST, 'post_id' ) );
+		
 	}
 
 	/**
