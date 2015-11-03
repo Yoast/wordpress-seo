f885cbfe94b1273a0c94312777669dfe7d4110cc
Anton Timmermans <me@atimmer.com>
2015-11-03 21:43:42 +0100

    Fix codestyle

--- admin/taxonomy/class-taxonomy-content-fields.php
+++ admin/taxonomy/class-taxonomy-content-fields.php
@@ -26,7 +26,7 @@ class WPSEO_Taxonomy_Content_Fields extends WPSEO_Taxonomy_Fields {
 			),
 			'analysis' => $this->get_field_config(
 				__( 'Analysis', 'wordpress-seo' ),
-					sprintf( __( 'This is the content analysis, a collection of content checks that analyze the content of your page. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/real-time-content-analysis/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
+				sprintf( __( 'This is the content analysis, a collection of content checks that analyze the content of your page. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/real-time-content-analysis/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
 				'div'
 			),
 			'title' => $this->get_field_config(
