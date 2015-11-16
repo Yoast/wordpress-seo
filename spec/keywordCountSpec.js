2c7fd34e30dc4744981add5667459aabc11d44f6
Danny <danny@yoast.com>
2015-11-16 12:20:31 +0100

    Fix the keyword count to match only the text and not inside tags.

--- spec/keywordCountSpec.js
+++ spec/keywordCountSpec.js
@@ -19,3 +19,17 @@ describe("A keyword count test with no keyword in the test, only the alt", funct
 	});
 });
 
+keywordCountArgs2 = {
+	text: '<img src="plaatje" animation="a bunch of filler texts" alt="keyword" width="100" /> TestText',
+	keyword: "width"
+};
+
+describe("A keyword count test with no keyword in the test, but keyword is in the tag", function(){
+	it("should return no keyword found in the text.", function(){
+		var textAnalyzer = Factory.buildAnalyzer(keywordCountArgs2);
+		var result = textAnalyzer.keywordCount();
+		expect(result).toBe(0);
+	});
+});
+
+
