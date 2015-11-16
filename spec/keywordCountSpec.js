34513779bb285146dd08d26ae70b308af30ab494
Danny <danny@yoast.com>
2015-11-16 12:08:55 +0100

    Adds test for keyword count

--- spec/keywordCountSpec.js
+++ spec/keywordCountSpec.js
@@ -1,1 +1,21 @@
+require("../js/config/config.js");
+require("../js/config/scoring.js");
+require("../js/analyzer.js");
+require("../js/preprocessor.js");
+require("../js/analyzescorer.js");
+require("../js/stringhelper.js");
+
+
+keywordCountArgs = {
+	text: '<img src="plaatje" animation="a bunch of filler texts" alt="keyword" width="100" /> TestText',
+	keyword: "keyword"
+};
+
+describe("A keyword count test with no keyword in the test, only the alt", function(){
+	it("should return no keyword found in the text.", function(){
+		var textAnalyzer = Factory.buildAnalyzer(keywordCountArgs);
+		var result = textAnalyzer.keywordCount();
+		expect(result).toBe(0);
+	});
+});
 
