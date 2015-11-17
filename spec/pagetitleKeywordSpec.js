Yoast/wordpress-seo

commit 65a1b0cd11da25bdb8dfff3c359282d0cfef6be4

Fix matching with -

js/analyzer.js
@@ -563,7 +563,7 @@ YoastSEO.Analyzer.prototype.pageTitleKeyword = function() {
 	if ( typeof this.config.pageTitle !== "undefined" ) {
 		result[ 0 ].result.matches = this.stringHelper.countMatches(
 			this.config.pageTitle.toLocaleLowerCase(),
-			this.keywordRegex
+			this.keywordRegexInverse
 		);
 		result[ 0 ].result.position = this.config.pageTitle.indexOf( this.config.keyword );
 	}

js/snippetPreview.js
@@ -213,8 +213,8 @@ YoastSEO.SnippetPreview.prototype.getPeriodMatches = function() {
  */
 YoastSEO.SnippetPreview.prototype.formatKeyword = function( textString ) {
 
-	// removes characters from the keyword that could break the regex, or give unwanted results, includes the -
-	var keyword = this.refObj.rawData.keyword.replace( /[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "" );
+	// removes characters from the keyword that could break the regex, or give unwanted results
+	var keyword = this.refObj.rawData.keyword.replace( /[\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, " " );
 
 	// Match keyword case-insensitively
 	var keywordRegex = new RegExp( "\\b" + keyword + "\\b", "ig" );

commit 2e45ae8ee7c2a65297dc4c77a5f7c12a180f5f6a

Fix matching spaces in keywords
Matching spaces was broken after 65a1b0c, fix this. Also adds unit tests
.

dist/yoast-seo.js
@@ -573,7 +573,7 @@ YoastSEO.Analyzer.prototype.pageTitleKeyword = function() {
 	if ( typeof this.config.pageTitle !== "undefined" ) {
 		result[ 0 ].result.matches = this.stringHelper.countMatches(
 			this.config.pageTitle.toLocaleLowerCase(),
-			this.keywordRegexInverse
+			this.config.keyword
 		);
 		result[ 0 ].result.position = this.config.pageTitle.indexOf( this.config.keyword );
 	}

js/analyzer.js
@@ -573,7 +573,7 @@ YoastSEO.Analyzer.prototype.pageTitleKeyword = function() {
 	if ( typeof this.config.pageTitle !== "undefined" ) {
 		result[ 0 ].result.matches = this.stringHelper.countMatches(
 			this.config.pageTitle.toLocaleLowerCase(),
-			this.keywordRegexInverse
+			this.config.keyword
 		);
 		result[ 0 ].result.position = this.config.pageTitle.indexOf( this.config.keyword );
 	}

spec/pagetitleKeywordSpec.js
@@ -47,3 +47,65 @@ describe("a test to check if the keyword occurs in the pagetitle", function(){
         expect(result[0].result.matches).toBe(0);
     });
 });
+
+titleKeywordArg4 = {
+    pageTitle: "focus keyword",
+    keyword: "focus keyword",
+    queue: ["pageTitleKeyword"]
+};
+titleKeywordArg5 = {
+    pageTitle: "focus-keyword",
+    keyword: "focus-keyword",
+    queue: ["pageTitleKeyword"]
+};
+titleKeywordArg6 = {
+    pageTitle: "focus-keyword",
+    keyword: "focus keyword",
+    queue: ["pageTitleKeyword"]
+};
+titleKeywordArg7 = {
+    pageTitle: "focus keyword",
+    keyword: "focus-keyword",
+    queue: ["pageTitleKeyword"]
+};
+
+describe("a test with keywords", function() {
+    var pageTitleAnalyzer;
+    var result;
+
+    it("returns correct results with spaces in the keyword", function() {
+        pageTitleAnalyzer = Factory.buildAnalyzer(titleKeywordArg4);
+
+        result = pageTitleAnalyzer.pageTitleKeyword();
+
+        expect(result[0 ].result.matches ).toBe(1);
+        expect(result[0 ].result.position ).toBe(0);
+    });
+
+    it("returns correct results with a dash in the keyword", function() {
+        pageTitleAnalyzer = Factory.buildAnalyzer(titleKeywordArg5);
+
+        result = pageTitleAnalyzer.pageTitleKeyword();
+
+        expect(result[0 ].result.matches ).toBe(1);
+        expect(result[0 ].result.position ).toBe(0);
+    });
+
+    it("returns correct results with a dash in the keyword", function() {
+        pageTitleAnalyzer = Factory.buildAnalyzer(titleKeywordArg6);
+
+        result = pageTitleAnalyzer.pageTitleKeyword();
+
+        expect(result[0 ].result.matches ).toBe(0);
+        expect(result[0 ].result.position ).toBe(-1);
+    });
+
+    it("returns correct results with a dash in the keyword", function() {
+        pageTitleAnalyzer = Factory.buildAnalyzer(titleKeywordArg7);
+
+        result = pageTitleAnalyzer.pageTitleKeyword();
+
+        expect(result[0 ].result.matches ).toBe(0);
+        expect(result[0 ].result.position ).toBe(-1);
+    });
+});