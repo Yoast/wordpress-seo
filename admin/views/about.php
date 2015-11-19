43e305d233e13d372aa86c189f5d69839abb44ab
omarreiss <omar.reiss@gmail.com>
2015-11-19 09:32:54 +0100

    Ignore code style error on the about page.

--- admin/views/about.php
+++ admin/views/about.php
@@ -57,7 +57,9 @@ function wpseo_display_contributors( $contributors ) {
 			time. Which is also why it's now in plain sight all the time.</p>
 
 		<div class="headline-feature feature-video">
+			<?php // @codingStandardsIgnoreStart ?>
 			<script charset="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js" async></script>
+			<?php // @codingStandardsIgnoreEnd ?>
 			<div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;">
 				<div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;">
 					<div class="wistia_embed wistia_async_vdcuq7tfh1 videoFoam=true" style="height:100%;width:100%">
