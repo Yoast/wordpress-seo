ad10938f2ad9553e72efc63cc22518b152dc93f6
Danny <danny@yoast.com>
2015-11-24 15:50:37 +0100

    Add module for keyphrase sizecheck.

---
+++ js/analyses/keyphraseSize.js
@@ -0,0 +1,17 @@
+var sanitizeString = require("../stringProcessing/sanitizeString.js");
+
+/**
+ * Checks the number of words in the keyphrase
+ *
+ * @param {String} text The keyphrase to count words in.
+ * @returns {int} The wordcount of the given keyphrase.
+ */
+module.exports = function( text ) {
+	text = sanitizeString( text );
+
+	if ( text === "" ) {
+		return 0;
+	}
+
+	return text.split( /\s/g ).length;
+};
---
+++ js/stringProcessing/sanitizeString.js
@@ -0,0 +1,16 @@
+var stripTags = require("../stringProcessing/stripTags.js");
+var stripSpaces = require("../stringProcessing/stripSpaces.js");
+
+/**
+ * Strip HTMLtags characters from string that break regex
+ *
+ * @param {String} text The text to strip the characters from.
+ * @returns {String} The text without characters.
+ */
+module.exports = function( text ){
+	text = text.replace( /[\[\]\/\{\}\(\)\*\+\?\\\^\$\|]/g, "" );
+	text = stripTags( text );
+	text = stripSpaces( text );
+
+	return text;
+};
