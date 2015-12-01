4aebfd2b26c864c3479bf08e3b5308645279f899
Andy Meerwaldt <andy@yoast.com>
2015-12-01 11:01:31 +0100

    Fix coverage after renaming.

--- tests/premium/test-class-redirect-exporter-nginx.php
+++ tests/premium/test-class-redirect-exporter-nginx.php
@@ -11,7 +11,7 @@ class WPSEO_Redirect_Exporter_Nginx_Test extends WPSEO_UnitTestCase {
 	/**
 	 * Testing if the export method will be called and returns true
 	 *
-	 * @covers WPSEO_Redirect_Export_File::export
+	 * @covers WPSEO_Redirect_Exporter_Nginx::export
 	 */
 	public function test_export() {
 		$class_instance = $this->getMock( 'WPSEO_Redirect_Exporter_Nginx', array( 'save' ) );
@@ -34,7 +34,7 @@ class WPSEO_Redirect_Exporter_Nginx_Test extends WPSEO_UnitTestCase {
 	 *
 	 * Testing if the export method will be called and returns true
 	 *
-	 * @covers WPSEO_Redirect_Export_File::export
+	 * @covers WPSEO_Redirect_Exporter_Nginx::export
 	 */
 	public function test_export_empty() {
 		$class_instance = new WPSEO_Redirect_Exporter_Nginx();
@@ -45,7 +45,7 @@ class WPSEO_Redirect_Exporter_Nginx_Test extends WPSEO_UnitTestCase {
 	/**
 	 * Test if formatting will be done correctly
 	 *
-	 * @covers WPSEO_Redirect_Exporter_File::format
+	 * @covers WPSEO_Redirect_Exporter_Nginx::format
 	 */
 	public function test_format() {
 		$class_instance = new WPSEO_Redirect_Exporter_Nginx();
