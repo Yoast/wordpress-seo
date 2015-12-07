92bec138aa0d6a0548b392152d11dfd439607a5d
Andy Meerwaldt <andy@yoast.com>
2015-12-07 13:35:53 +0100

    Deal with 410 request, because a target isn't needed in that case.

--- premium/assets/js/wpseo-premium-admin-overview.js
+++ premium/assets/js/wpseo-premium-admin-overview.js
@@ -37,36 +37,40 @@ function wpseo_undo_redirect( origin, target, type, nonce, id ) {
  * Creates a redirect
  *
  * @param {string} origin
+ * @param {string} type
  * @param {string} nonce
  * @param {string} id
  */
-function wpseo_create_redirect( origin, nonce, id ) {
-	var target = window.prompt( wpseo_premium_strings.enter_new_url.replace( '%s', origin ) );
+function wpseo_create_redirect( origin, type, nonce, id ) {
+	var target = '';
+	if( parseInt( type, 10 ) !== 410 ) {
+		target = window.prompt(wpseo_premium_strings.enter_new_url.replace('%s', origin));
 
-	if ( null !== target ) {
-		if ( '' !== target ) {
-			jQuery.post(
-				ajaxurl,
-				{
-					action: 'wpseo_add_redirect_plain',
-					ajax_nonce: nonce,
-					redirect : {
-						origin : origin,
-						target : target,
-						type   : 301
-					},
-					id: id
-				},
-				function( response ) {
-					var resp = JSON.parse( response );
-					jQuery( '#' + resp.id + ' p' ).html( wpseo_premium_strings.redirect_saved.replace( '%1$s', '<code>' + resp.old_url + '</code>' ).replace( '%2$s', '<code>' + resp.new_url + '</code>' ) );
-				}
-			);
-		}
-		else {
+		if ( target === '' ) {
 			window.alert( wpseo_premium_strings.error_new_url );
+			return;
 		}
 	}
+
+	jQuery.post(
+		ajaxurl,
+		{
+			action: 'wpseo_add_redirect_plain',
+			ajax_nonce: nonce,
+			redirect: {
+				origin: origin,
+				target: target,
+				type:   type
+			},
+			id: id
+		},
+		function( response ) {
+			var success_message = wpseo_premium_strings.redirect_saved.replace( '%1$s', '<code>' + response.origin + '</code>' ).replace( '%2$s', '<code>' + response.target + '</code>');
+
+			jQuery( '#' + id ).html( '<p>' + success_message + '</p>'  );
+		},
+		'json'
+	);
 }
 
 (function( $ ) {
