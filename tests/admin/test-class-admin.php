3c2d9318b2ec318c8c9d7cae8a92970df00a296c
Caroline Geven <caroline@yoast.com>
2015-12-07 11:00:45 +0100

    Test for wpseo_admin

---
+++ tests/admin/test-class-admin.php
@@ -0,0 +1,22 @@
+<?php
+
+class WPSEO_Admin_Test extends WPSEO_UnitTestCase {
+
+	/**
+	 * Test that admin_features returns the correct array.
+	 *
+	 * @covers WPSEO_Admin::get_admin_features
+	 */
+	public function test_get_admin_features() {
+		$class_instance = new WPSEO_Admin();
+
+		$admin_features = array(
+			'gsc'              => new WPSEO_GSC(),
+			'primary_category' => new WPSEO_Primary_Term_Admin(),
+			'dashboard_widget' => new Yoast_Dashboard_Widget(),
+		);
+
+		$this->assertEquals( $admin_features, $class_instance->get_admin_features() );
+
+	}
+}
