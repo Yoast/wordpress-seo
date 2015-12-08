b00cb7fcf2070b8fb72e0dff82739cad374f4a2c
Danny <danny@yoast.com>
2015-12-08 11:09:08 +0100

    Adds metadescription length function

---
+++ js/analyses/metaDescriptionLength.js
@@ -0,0 +1,9 @@
+/**
+ * Returns the length of the metaDescription.
+ *
+ * @param {String} text Text to check.
+ * @returns {Int} The length of the text.
+ */
+module.exports = function( text ){
+	return text.length;
+};
---
+++ spec/analyses/metaDescriptionLengthSpec.js
@@ -0,0 +1,8 @@
+var metaDescriptionLength = require("../../js/analyses/metaDescriptionLength.js");
+
+describe("test for the length of meta", function(){
+	it("returns integer with length", function(){
+		expect( metaDescriptionLength( "this is a meta" ) ).toBe( 14 );
+		expect( metaDescriptionLength( "" ) ).toBe( 0 );
+	})
+});
