b8e69dc79444cfccd8c51c337dcec707d8adfb70
Caroline Geven <caroline@yoast.com>
2015-12-09 12:27:13 +0100

    Tests for wpseo_primary_term class

--- tests/inc/test-class-wpseo-primary-term.php
+++ tests/inc/test-class-wpseo-primary-term.php
@@ -1,7 +1,39 @@
 <?php
 
 class WPSEO_Primary_Term_Test extends WPSEO_UnitTestCase {
-    
+
+    /**
+     * Return the correct primary term when primary term already exists.
+     *
+     * @covers WPSEO_Primary_Term::get_primary_term
+     */
+    public function test_get_primary_term_WHERE_primary_term_EXISTS() {
+        $taxonomy_name = 'yoast';
+        $post_id = 3;
+        $primary_term_id = 54;
+
+        $class_instance = new WPSEO_Primary_Term( $taxonomy_name, $post_id );
+
+        $class_instance->set_primary_term( $primary_term_id );
+
+        $this->assertEquals( $primary_term_id, $class_instance->get_primary_term() );
+    }
+
+    /**
+     * When there's no term for the post, return false.
+     *
+     * @covers WPSEO_Primary_Term::get_primary_term
+     */
+    public function test_get_primary_term_WHERE_primary_term_DOES_NOT_EXIST_AND_terms_ARE_EMPTY() {
+        $taxonomy_name = 'yoast';
+        $post_id = 3;
+
+        $class_instance = new WPSEO_Primary_Term( $taxonomy_name, $post_id );
+
+        $this->assertFalse( $class_instance->get_primary_term() );
+    }
+
+
     /**
      * Test that set_primary_term succesfully updates the primary_term
      *
