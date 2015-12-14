f6e50dfb9f24feddf159316f016d60d59357a636
Andy Meerwaldt <andy@yoast.com>
2015-12-14 14:51:30 +0100

    Add class for showing the table column in taxonomy overview

---
+++ admin/taxonomy/class-taxonomy-columns.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * @package WPSEO\Admin
+ */
+
+/**
+ * This class adds columns to the taxonomy table.
+ */
+class WPSEO_Taxonomy_Columns {
+
+	/**
+	 * WPSEO_Taxonomy_Columns constructor.
+	 */
+	public function __construct() {
+
+		$this->taxonomy = FILTER_INPUT( INPUT_GET, 'taxonomy' );
+
+		add_filter( 'manage_edit-' . $this->taxonomy . '_columns', array( $this, 'add_columns' ) );
+		add_filter( 'manage_' . $this->taxonomy . '_custom_column', array( $this, 'parse_column' ), 10, 3 );
+	}
+
+	/**
+	 * Adds the SEO score to the table.
+	 *
+	 * @param array $columns Current set columns.
+	 *
+	 * @return array
+	 */
+	public function add_columns( array $columns ) {
+
+		$new_columns = array();
+
+		foreach( $columns as $column_name => $column_value ) {
+			$new_columns[ $column_name ] = $column_value;
+
+			if ( $column_name === 'description' ) {
+				$new_columns[ 'wpseo_score' ] = __( 'SEO', 'wordpress-seo' );
+			}
+
+		}
+
+		return $new_columns;
+	}
+
+	/**
+	 * Parses the column.
+	 *
+	 * @param string  $content		The current content of the column.
+	 * @param string  $column_name  The name of the column.
+	 * @param integer $term_id      ID of requested taxonomy.
+	 *
+	 * @return string
+	 */
+	public function parse_column( $content, $column_name, $term_id ) {
+
+		switch ( $column_name ) {
+			case 'wpseo_score':
+				$term = get_term( $term_id, $this->taxonomy );
+
+				$no_index = WPSEO_Taxonomy_Meta::get_term_meta( $term_id, $this->taxonomy, 'noindex' );
+
+				if ( ! $this->is_indexable( $term, $no_index ) ) {
+					$rank  = new WPSEO_Rank( WPSEO_Rank::NO_INDEX );
+					$title = __( 'Term is set to noindex.', 'wordpress-seo' );
+				}
+				elseif ( WPSEO_Taxonomy_Meta::get_term_meta( $term_id, $this->taxonomy, 'focuskw' ) === '' ) {
+					$rank  = new WPSEO_Rank( WPSEO_Rank::NO_FOCUS );
+					$title = __( 'Focus keyword not set.', 'wordpress-seo' );
+				}
+				else {
+					$score = (int) WPSEO_Taxonomy_Meta::get_term_meta( $term_id, $this->taxonomy, 'linkdex' );
+					$rank  = WPSEO_Rank::from_numeric_score( $score );
+					$title = $rank->get_label();
+				}
+
+				return '<div title="' . esc_attr( $title ) . '" class="wpseo-score-icon ' . esc_attr( $rank->get_css_class() ) . '"></div>';
+
+				break;
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Check if the taxonomy is indexable.
+	 *
+	 * @param mixed       $term
+	 * @param bool|string $no_index
+	 *
+	 * @return bool
+	 */
+	private function is_indexable( $term, $no_index ) {
+		static $options;
+
+		// Saving the options once, because it's static
+		if ( $options === null) {
+			$options = WPSEO_Options::get_all();
+		}
+
+		// When the no_index value is not empty and not default, check if its value is index.
+		if ( ! empty( $no_index ) && $no_index !== 'default' ) {
+			return ( $no_index === 'index' );
+		}
+
+		// Check if the default for taxonomy is empty (this will be index)
+		$no_index_key = 'noindex-tax-' . $term->taxonomy;
+		if ( is_object( $term ) && ( isset( $options[ $no_index_key ] ) ) ) {
+			return ( empty( $options[ $no_index_key ] ) );
+		}
+
+		return true;
+	}
+
+}
