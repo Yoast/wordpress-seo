e933703d587514eea46b050a0bff68a52a91151d
Andy Meerwaldt <andy@yoast.com>
2015-12-18 13:45:27 +0100

    Add localization for recalculating and calculate the terms

--- admin/class-recalculate-scores.php
+++ admin/class-recalculate-scores.php
@@ -11,20 +11,82 @@
 class WPSEO_Recalculate_Scores {
 
 	/**
-	 * @var array The fields which should be always queried, can be extended by array_merge
+	 * @var array The totals for the terms and posts
 	 */
-	private $query_fields   = array(
-		'post_type'      => 'any',
-		'meta_key'       => '_yoast_wpseo_focuskw',
-		'posts_per_page' => -1,
-	);
+	private $recalculate_totals = array();
 
 	/**
-	 * Constructing the object by setting the AJAX hooks
+	 * Constructing the object by modalbox, the localization and the totals.
 	 */
 	public function __construct() {
+		add_action( 'admin_init', array( $this, 'init' ), 20 );
+	}
+
+	/**
+	 * Initialize the recalculate class by setting the needed values.
+	 */
+	public function init() {
+		$this->recalculate_totals = $this->get_totals();
+
+		add_action( 'admin_enqueue_scripts', array( $this, 'recalculate_assets' ) );
+
 		// Loading the modal box.
 		$this->modal_box();
+	}
+
+	/**
+	 * Run the localize script.
+	 */
+	public function recalculate_assets() {
+		wp_enqueue_script( 'wpseo-recalculate-script', plugins_url( 'js/wp-seo-recalculate-' . '306' . WPSEO_CSSJS_SUFFIX . '.js', WPSEO_FILE ), array(
+			'jquery',
+			'jquery-ui-core',
+			'jquery-ui-progressbar',
+		), WPSEO_VERSION, true );
+		wp_localize_script( 'wpseo-recalculate-script', 'wpseoRecalculateL10n', $this->recalculate_totals );
+	}
+
+	/**
+	 * Gets the totals for the posts and terms.
+	 *
+	 * @return array
+	 */
+	private function get_totals() {
+		return array(
+			'posts'      => $this->calculate_posts(),
+			'terms'      => $this->calculate_terms(),
+		);
+	}
+
+	/**
+	 * Getting the total number of posts
+	 *
+	 * @return int
+	 */
+	private function calculate_posts() {
+		$count_posts_query = new WP_Query(
+			array(
+				'post_type'      => 'any',
+				'meta_key'       => '_yoast_wpseo_focuskw',
+				'posts_per_page' => -1,
+			)
+		);
+
+		return $count_posts_query->found_posts;
+	}
+
+	/**
+	 * Get the total number of terms
+	 *
+	 * @return int
+	 */
+	private function calculate_terms() {
+		$total = 0;
+		foreach ( get_taxonomies(array(), 'objects') as $taxonomy ) {
+			$total += wp_count_terms( $taxonomy->name );
+		}
+
+		return $total;
 	}
 
 	/**
@@ -38,7 +100,7 @@ class WPSEO_Recalculate_Scores {
 			/* translators: 1: expands to a <span> containing the number of posts recalculated. 2: expands to a <strong> containing the total number of posts. */
 			__( '%1$s of %2$s done.', 'wordpress-seo' ),
 			'<span id="wpseo_count">0</span>',
-			'<strong id="wpseo_count_total">' . $this->calculate_posts() . '</strong>'
+			'<strong id="wpseo_count_total">' . ( $this->recalculate_totals['posts'] + $this->recalculate_totals['terms'] ) . '</strong>'
 		);
 
 		?>
@@ -49,17 +111,6 @@ class WPSEO_Recalculate_Scores {
 			<p><?php echo $progress; ?></p>
 		</div>
 		<?php
-	}
-
-	/**
-	 * Getting the total number of posts
-	 *
-	 * @return int
-	 */
-	private function calculate_posts() {
-		$count_posts_query = new WP_Query( $this->query_fields );
-
-		return $count_posts_query->found_posts;
 	}
 
 }
