eeb5d6fab7f33743a6b7629b2d9ab45baf057118
terw-dan <danny@yoast.com>
2015-12-23 14:19:57 +0100

    Updates comment for subheading

--- js/analyses/matchKeywordInSubheadings.js
+++ js/analyses/matchKeywordInSubheadings.js
@@ -9,6 +9,9 @@ var subheadingMatch = require( "../stringProcessing/subheadingsMatch.js" );
  *
  * @param {string} text The text to check for subheadings.
  * @returns {object} the result object.
+ * count: the number of matches
+ * matches:the number of ocurrences of the keyword for each match
+ * 
  */
 module.exports = function( text, keyword ) {
 	var matches;
