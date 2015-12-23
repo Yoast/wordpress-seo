4bc61728a2c165a94b8f3eec145406ece910fe4f
Caroline Geven <caroline@yoast.com>
2015-12-23 12:27:49 +0100

    Added return to phpdocs in class-wpseo-primary-term

--- inc/class-wpseo-primary-term.php
+++ inc/class-wpseo-primary-term.php
@@ -32,7 +32,7 @@ class WPSEO_Primary_Term {
 	/**
 	 * Returns the primary term ID
 	 *
-	 * @return int
+	 * @return int|bool
 	 */
 	public function get_primary_term() {
 		$primary_term = get_post_meta( $this->post_ID, WPSEO_Meta::$meta_prefix . 'primary_' . $this->taxonomy_name, true );
