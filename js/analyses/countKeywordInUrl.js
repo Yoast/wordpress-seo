9c1e1736899944110eeaeb01791b7c1d988e4064
terw-dan <danny@yoast.com>
2015-12-23 10:03:02 +0100

    Renames text variable to url

--- js/analyses/countKeywordInUrl.js
+++ js/analyses/countKeywordInUrl.js
@@ -4,11 +4,11 @@ var wordMatch = require( "../stringProcessing/matchTextWithWord.js" );
 /**
  * Matches the keyword in the URL. Replaces whitespaces with dashes and uses dash as wordboundary.
  *
- * @param {string} text The text to check for keyword
+ * @param {url} url The URL to check for keyword
  * @param {string} keyword The keyword to match
  * @returns {int} Number of times the keyword is found.
  */
-module.exports = function( text, keyword ) {
+module.exports = function( url, keyword ) {
 	if ( typeof keyword !== "undefined" && typeof text !== "undefined" ) {
 		keyword = keyword.replace( /\s/ig, "-" );
 		return wordMatch( text, keyword );
