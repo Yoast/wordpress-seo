7147c2f3bdf56dfaa6afa68b9c5ee05aecb84192
Jimmy Comack <jimmy@yoast.com>
2016-01-04 19:48:46 +0100

    Fixed typo in docblock.

--- js/analyses/checkUrlForStopwords.js
+++ js/analyses/checkUrlForStopwords.js
@@ -3,7 +3,7 @@
 var stopwords = require( "../analyses/checkStringForStopwords.js" );
 
 /**
- * Matches stopwords in the URL. replaces - and _ with whitespace.
+ * Matches stopwords in the URL. Replaces - and _ with whitespace.
  *
  * @param {string} url The URL to check for stopwords.
  * @returns {array} stopwords found in URL
