d4b178e3504da8eef23dc3239473e58e74e3a7f0
isvaljek <ivan.svaljek@gmail.com>
2016-01-05 07:58:23 +0100

    checks for dom element existance (now even more prettier js)

--- js/wp-seo-term-scraper-305.js
+++ js/wp-seo-term-scraper-305.js
@@ -319,7 +319,7 @@
 		var textNode = jQuery( '.term-description-wrap' ).find( 'td' ).find( 'textarea' ).val();
 
 		var newEditor = document.getElementById( 'wp-description-wrap' );
-		if(typeof(newEditor) != 'undefined') {
+		if(typeof(newEditor) !== 'undefined') {
 			newEditor.style.display = 'none';	
 		} 
 		var text = jQuery( '.term-description-wrap' ).find( 'td' ).find( 'p' );
@@ -327,7 +327,7 @@
 		jQuery( '.term-description-wrap' ).find( 'td' ).html( '' );
 		//append the editor and the helptext
 		jQuery( '.term-description-wrap' ).find( 'td' ).append( newEditor ).append( text );
-		if(typeof(newEditor) != 'undefined') {
+		if(typeof(newEditor) !== 'undefined') {
 			newEditor.style.display = 'block';	
 		} 
 		document.getElementById('description').value = textNode;
