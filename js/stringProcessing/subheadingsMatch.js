1b90f68ce20c8b9f4357d1f29014b578b1fa4cec
Jimmy Comack <jimmy@yoast.com>
2016-01-05 14:13:36 +0100

    Found a bug when using the replaceString module. Added a todo so this can be looked at later.

--- js/stringProcessing/subheadingsMatch.js
+++ js/stringProcessing/subheadingsMatch.js
@@ -16,6 +16,7 @@ module.exports = function( matches, keyword ) {
 	} else {
 		foundInHeader = 0;
 		for ( var i = 0; i < matches.length; i++ ) {
+			// TODO: This replaceString call seemingly doesn't work, as no replacement value is being sent to the .replace method in replaceString
 			var formattedHeaders = replaceString(
 				matches[ i ], removalWords
 			);
