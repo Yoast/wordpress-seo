36a36ef3a94bb86253a6ba1658a5bef3845ddf6a
Jimmy Comack <jimmy@yoast.com>
2016-01-05 11:17:24 +0100

    Fixed a typo in the inline documentation.

--- js/stringProcessing/checkNofollow.js
+++ js/stringProcessing/checkNofollow.js
@@ -9,7 +9,7 @@
 module.exports = function( text ) {
 	var linkFollow = "Dofollow";
 
-	//matches all nofollow links, case insensitive and global
+	// Matches all nofollow links, case insensitive and global
 	if ( text.match( /rel=([\'\"])nofollow\1/ig ) !== null ) {
 		linkFollow = "Nofollow";
 	}
