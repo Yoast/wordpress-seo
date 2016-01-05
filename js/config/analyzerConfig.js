c1ce8c364f47ddbb0ce5f24111fdfa596352a166
Jimmy Comack <jimmy@yoast.com>
2016-01-05 10:59:00 +0100

    Improved the decription of the return object

--- js/config/analyzerConfig.js
+++ js/config/analyzerConfig.js
@@ -1,8 +1,8 @@
 /**
- * Returns a configobject with maxSlugLength, maxURLLength and MaxMeta to be used
+ * Returns a configobject with maxSlugLength, maxUrlLength and MaxMeta to be used
  * for analysis
  *
- * @returns {object} the config object
+ * @returns {object} the config object containing the maxSlugLength, maxUrlLength and the MaxMeta values
  */
 module.exports = function(){
 	return {
