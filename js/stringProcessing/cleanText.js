d9e98e9c929a54c82e1eb72420fa0b1b4fc6efae
Jimmy Comack <jimmy@yoast.com>
2016-01-05 11:23:27 +0100

    Fixed the text in the return statement.
    Also added an early return, thus reducing a level of indentation.

--- js/stringProcessing/cleanText.js
+++ js/stringProcessing/cleanText.js
@@ -8,46 +8,45 @@ var unifyWhitespace = require( "../stringProcessing/unifyWhitespace.js" );
  * Removes words, duplicate spaces and sentence terminators, and words consisting of only digits
  * from the text. This is used for the flesh reading ease test.
  *
- * @param {String} text The text to clean
+ * @param {String} text The cleaned text
  * @returns {String} The text
  */
 module.exports = function( text ) {
-	if ( text !== "" ) {
-		text = replaceDiacritics( text );
-		text = text.toLocaleLowerCase();
-
-		text = unifyWhitespace( text );
-
-		// replace comma', hyphens etc with spaces
-		text = text.replace( /[\-\;\:\,\(\)\"\'\|\“\”]/g, " " );
-
-		// remove apostrophe
-		text = text.replace( /[\’]/g, "" );
-
-		// unify all terminators
-		text = text.replace( /[.?!]/g, "." );
-
-		// Remove double spaces
-		text = stripSpaces( text );
-
-		// add period in case it is missing
-		text += ".";
-
-		// replace newlines with spaces
-		text = text.replace( /[ ]*(\n|\r\n|\r)[ ]*/g, " " );
-
-		// remove duplicate terminators
-		text = text.replace( /([\.])[\. ]+/g, "$1" );
-
-		// pad sentence terminators
-		text = text.replace( /[ ]*([\.])+/g, "$1 " );
-
-		// Remove double spaces
-		text = stripSpaces( text );
-
-		if ( text === "." ) {
-			text = "";
-		}
+	if ( text === "." ) {
+		return "";
 	}
+
+	text = replaceDiacritics( text );
+	text = text.toLocaleLowerCase();
+
+	text = unifyWhitespace( text );
+
+	// replace comma', hyphens etc with spaces
+	text = text.replace( /[\-\;\:\,\(\)\"\'\|\“\”]/g, " " );
+
+	// remove apostrophe
+	text = text.replace( /[\’]/g, "" );
+
+	// unify all terminators
+	text = text.replace( /[.?!]/g, "." );
+
+	// Remove double spaces
+	text = stripSpaces( text );
+
+	// add period in case it is missing
+	text += ".";
+
+	// replace newlines with spaces
+	text = text.replace( /[ ]*(\n|\r\n|\r)[ ]*/g, " " );
+
+	// remove duplicate terminators
+	text = text.replace( /([\.])[\. ]+/g, "$1" );
+
+	// pad sentence terminators
+	text = text.replace( /[ ]*([\.])+/g, "$1 " );
+
+	// Remove double spaces
+	text = stripSpaces( text );
+
 	return text;
 };
