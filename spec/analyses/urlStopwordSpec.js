e9b5e5aad3a930a4e2cf7c05285beb12b0c229a3
Jimmy Comack <jimmy@yoast.com>
2016-01-05 12:36:23 +0100

    Updated the spec to test more situations and used toEqual to explicitly set the expected result.

--- spec/analyses/urlStopwordSpec.js
+++ spec/analyses/urlStopwordSpec.js
@@ -2,9 +2,10 @@ var urlStopwords = require( "../../js/analyses/checkUrlForStopwords.js" );
 
 describe( "Checks the URL for stopwords", function(){
 	it("returns any stopwords found", function(){
-		expect( urlStopwords( "before-and-after" ) ).toContain( "before" );
-		expect( urlStopwords( "before-and-after" ) ).toContain( "after" );
-		expect( urlStopwords( "before-after" ) ).toContain( "before" );
-		expect( urlStopwords( "before-after" ) ).toContain( "after" );
+		expect( urlStopwords( "before-and-after" ) ).toEqual( [ "after", "and", "before" ] );
+		expect( urlStopwords( "before-after" ) ).toEqual( [ "after", "before" ] );
+		expect( urlStopwords( "stopword-after" ) ).toEqual( [ "after" ] );
+		expect( urlStopwords( "stopword" ) ).toEqual( [  ] );
+		expect( urlStopwords( "" ) ).toEqual( [  ] );
 	});
 });
