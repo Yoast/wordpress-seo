49a1c1b798595cd0879e949984ddeae21bda01b9
Jimmy Comack <jimmy@yoast.com>
2016-02-01 15:34:59 +0100

    Fixed code style issues

--- js/wp-seo-admin-media-302.js
+++ js/wp-seo-admin-media-302.js
@@ -7,8 +7,8 @@
 'use strict';
 // Taken and adapted from http://www.webmaster-source.com/2013/02/06/using-the-wordpress-3-5-media-uploader-in-your-plugin-or-theme/
 jQuery( document ).ready(
-	function ( $ ) {
-		$( '.wpseo_image_upload_button' ).each( function ( index, element ) {
+	function( $ ) {
+		$( '.wpseo_image_upload_button' ).each(function( index, element ) {
 			var wpseo_target_button_id = $( element ).attr( 'id' );
 			var wpseo_target_id = wpseo_target_button_id.replace( /_button$/, '' );
 
@@ -17,14 +17,14 @@ jQuery( document ).ready(
 
 			var wpseo_custom_uploader = frames.file_frame = wp.media( {
 				title: wpseoMediaL10n.choose_image,
-				button: {text: wpseoMediaL10n.choose_image},
+				button: { text: wpseoMediaL10n.choose_image },
 				multiple: false,
 				library: {
 					type: 'image'
 				}
 			} );
 
-			wpseo_custom_uploader.on( 'select', function () {
+			wpseo_custom_uploader.on( 'select', function() {
 					var attachment = wpseo_custom_uploader.state().get( 'selection' ).first().toJSON();
 					document.getElementById( wpseo_target_id ).style.border = 'none';
 
@@ -34,11 +34,10 @@ jQuery( document ).ready(
 				}
 			);
 
-			$( element ).click( function ( e ) {
+			$( element ).click( function( e ) {
 				e.preventDefault();
 				wpseo_custom_uploader.open();
 			} );
-
 
 			/**
 			 * Check if the uploaded file is an image. If this is not the case, show a warning
@@ -57,11 +56,7 @@ jQuery( document ).ready(
 						.css( 'color', '#dd3d36' );
 					document.getElementById( wpseo_target_id ).style.border = '2px solid #dd3d36';
 				}
-
-
 			}
-
-
 		} );
 	}
 );
