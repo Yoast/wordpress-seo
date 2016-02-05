80bdff8d0f7a374d4b44d5ca5945be9dad5aad12
terw-dan <danny@yoast.com>
2016-02-05 10:42:06 +0100

    Adds tests for arabic

--- spec/stringProcessing/wordCountSpec.js
+++ spec/stringProcessing/wordCountSpec.js
@@ -4,5 +4,7 @@ describe("counts words in a string", function(){
 	it("returs the number of words", function(){
 		expect(wordCountFunction("this is a string")).toBe(4);
 		expect(wordCountFunction("this is a string, a very nice string.")).toBe(8);
+		expect(wordCountFunction(" انتشار کتابی درباره خفن‌ترین آزمایش های تاریخ علم، توجه رسانه‌ها را به خود جلب کردیل‌ها در اسید و دیگر آزمایش‌های خفن عل این عنوان کتابی است که چند")).toBe(28);
+		expect(wordCountFunction("<p class='class'>word</p>")).toBe(1);
 	});
 });
