4068af79a7491eb8974f418d4f75fe0b767846aa
Anton Timmermans <anton@yoast.com>
2016-02-11 09:44:25 +0100

    Prefix script handle to prevent conflicts
    It looks like we have conflicts with other plugins about jquery-qtip.
    This change prevents this issue completely by prefixing the handle.

--- admin/metabox/class-metabox.php
+++ admin/metabox/class-metabox.php
@@ -731,11 +731,11 @@ class WPSEO_Metabox extends WPSEO_Meta {
 			wp_enqueue_style( 'yoast-seo', plugins_url( 'css/dist/yoast-seo/yoast-seo-' . '307' . '.min.css', WPSEO_FILE ), array(), WPSEO_VERSION );
 			wp_enqueue_style( 'jquery-qtip', plugins_url( 'css/jquery.qtip' . WPSEO_CSSJS_SUFFIX . '.css', WPSEO_FILE ), array(), '2.2.1' );
 
-			wp_enqueue_script( 'jquery-qtip', plugins_url( 'js/jquery.qtip.min.js', WPSEO_FILE ), array( 'jquery' ), '2.2.1', true );
+			wp_enqueue_script( 'wp-seo-jquery-qtip', plugins_url( 'js/jquery.qtip.min.js', WPSEO_FILE ), array( 'jquery' ), '2.2.1', true );
 			wp_enqueue_script( 'wp-seo-metabox', plugins_url( 'js/wp-seo-metabox-' . '302' . WPSEO_CSSJS_SUFFIX . '.js', WPSEO_FILE ), array(
 				'jquery',
 				'jquery-ui-core',
-				'jquery-qtip',
+				'wp-seo-jquery-qtip',
 			), WPSEO_VERSION );
 
 			wp_enqueue_script( 'wpseo-admin-media', plugins_url( 'js/wp-seo-admin-media-' . '302' . WPSEO_CSSJS_SUFFIX . '.js', WPSEO_FILE ), array(
