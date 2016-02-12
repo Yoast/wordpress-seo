1908f666d3d995550ef34bdba8559b85b42d4bda
Anton Timmermans <anton@yoast.com>
2016-02-12 12:59:01 +0100

    Revert "Replace Yoast SEO in string with sprintf"
    Don't include this in 3.1, do this in 3.2
    
    This reverts commit c72ac58911df3e534d6be15700e350a2413a7676.

--- admin/class-bulk-title-editor-list-table.php
+++ admin/class-bulk-title-editor-list-table.php
@@ -42,10 +42,8 @@ class WPSEO_Bulk_Title_Editor_List_Table extends WPSEO_Bulk_List_Table {
 	public function get_columns() {
 
 		$columns = array(
-			/* translators: %1$s expands to Yoast SEO */
-			'col_existing_yoast_seo_title' => sprintf( __( 'Existing %1$s Title', 'wordpress-seo' ), 'Yoast SEO' ),
-			/* translators: %1$s expands to Yoast SEO */
-			'col_new_yoast_seo_title'      => sprintf( __( 'New %1$s Title', 'wordpress-seo' ), 'Yoast SEO' ),
+			'col_existing_yoast_seo_title' => __( 'Existing Yoast SEO Title', 'wordpress-seo' ),
+			'col_new_yoast_seo_title'      => __( 'New Yoast SEO Title', 'wordpress-seo' ),
 		);
 
 		return $this->merge_columns( $columns );
