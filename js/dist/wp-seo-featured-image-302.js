e95b78f07fc3d683ff523d7b4274df3042d5c84b
Andy Meerwaldt <andy@yoast.com>
2016-02-16 11:08:01 +0100

    Put the javascript files in the dist directory.

--- js/dist/wp-seo-featured-image-302.js
+++ js/dist/wp-seo-featured-image-302.js
@@ -1,3 +1,4 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /* global wp */
 /* global removeThumb:true */
 /* global wpseoFeaturedImageL10n */
@@ -159,3 +160,5 @@ function yst_removeOpengraphWarning() {
 	//Make sure the original function does its work
 	removeThumb();
 }
+
+},{}]},{},[1]);
