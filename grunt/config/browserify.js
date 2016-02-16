d3a5d03a914f0c4e70881bee5da51174fd71fb89
Andy Meerwaldt <andy@yoast.com>
2016-02-16 11:16:41 +0100

    Change file mapping.

--- grunt/config/browserify.js
+++ grunt/config/browserify.js
@@ -1,22 +1,21 @@
 module.exports = {
 	build: {
 		files: {
-			"js/wp-seo-admin-306.js": ["js/src/wp-seo-admin.js"],
-			"js/wp-seo-admin-global-302.js": ["js/src/wp-seo-admin-global.js"],
-			"js/wp-seo-admin-gsc-302.js": ["js/src/wp-seo-admin-gsc.js"],
-			"js/wp-seo-admin-media-302.js": ["js/src/wp-seo-admin-media.js"],
-			"js/wp-seo-bulk-editor.js": ["js/src/wp-seo-bulk-editor.js"],
-			"js/wp-seo-dismissible.js": ["js/src/wp-seo-dismissible.js"],
-			"js/wp-seo-export-302.js": ["js/src/wp-seo-export.js"],
-			"js/wp-seo-featured-image-302.js": ["js/src/wp-seo-featured-image.js"],
-			"js/wp-seo-metabox-302.js": ["js/src/wp-seo-metabox.js"],
-			"js/wp-seo-metabox-category.js": ["js/src/wp-seo-metabox-category.js"],
-
-			"js/wp-seo-post-scraper-305.js": ["js/src/wp-seo-post-scraper.js"],
-			"js/wp-seo-recalculate-306.js": ["js/src/wp-seo-recalculate.js"],
-			"js/wp-seo-replacevar-plugin-302.js": ["js/src/wp-seo-replacevar-plugin.js"],
-			"js/wp-seo-shortcode-plugin-305.js": ["js/src/wp-seo-shortcode-plugin.js"],
-			"js/wp-seo-term-scraper-305.js": ["js/src/wp-seo-term-scraper.js"]
+			"js/dist/wp-seo-admin-306.js": ["js/src/wp-seo-admin.js"],
+			"js/dist/wp-seo-admin-global-302.js": ["js/src/wp-seo-admin-global.js"],
+			"js/dist/wp-seo-admin-gsc-302.js": ["js/src/wp-seo-admin-gsc.js"],
+			"js/dist/wp-seo-admin-media-302.js": ["js/src/wp-seo-admin-media.js"],
+			"js/dist/wp-seo-bulk-editor.js": ["js/src/wp-seo-bulk-editor.js"],
+			"js/dist/wp-seo-dismissible.js": ["js/src/wp-seo-dismissible.js"],
+			"js/dist/wp-seo-export-302.js": ["js/src/wp-seo-export.js"],
+			"js/dist/wp-seo-featured-image-302.js": ["js/src/wp-seo-featured-image.js"],
+			"js/dist/wp-seo-metabox-302.js": ["js/src/wp-seo-metabox.js"],
+			"js/dist/wp-seo-metabox-category.js": ["js/src/wp-seo-metabox-category.js"],
+			"js/dist/wp-seo-post-scraper-305.js": ["js/src/wp-seo-post-scraper.js"],
+			"js/dist/wp-seo-recalculate-306.js": ["js/src/wp-seo-recalculate.js"],
+			"js/dist/wp-seo-replacevar-plugin-302.js": ["js/src/wp-seo-replacevar-plugin.js"],
+			"js/dist/wp-seo-shortcode-plugin-305.js": ["js/src/wp-seo-shortcode-plugin.js"],
+			"js/dist/wp-seo-term-scraper-305.js": ["js/src/wp-seo-term-scraper.js"]
 		}
 	}
 };
