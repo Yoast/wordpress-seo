7869c42642affe990b5e40a7bd36cb0fc14c9144
terw-dan <danny@yoast.com>
2016-02-19 11:55:34 +0100

    Fixes spec for subheadings

--- spec/analyses/subheadingSpec.js
+++ spec/analyses/subheadingSpec.js
@@ -10,14 +10,15 @@ describe("a test for matching keywords in subheadings", function(){
 		expect( result.count ).toBe(0);
 
 		result = subheadingFunction( "<h2>this is a heading with a dashed key-word</h2>", "key-word" );
-		expect( result.count ).toBe(1);
+		expect( result.matches ).toBe(1);
 
 		result = subheadingFunction( "<h2>this is a heading with a underscored key_word</h2>", "key_word" );
-		expect( result.count ).toBe(1);
+		expect( result.matches ).toBe(1);
+
+		result = subheadingFunction( "<h2>this is a heading with a underscored key-word</h2>", "key word" );
+		expect( result.matches ).toBe( 0 );
 
 		result = subheadingFunction( "<h2>this is a heading with a underscored key_word</h2>", "key word" );
-		expect( result.count ).toBe( 0 );
-
-
+		expect( result.matches ).toBe( 0 );
 	});
 });
