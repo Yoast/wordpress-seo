85475db748724458a58319ff8c0eecbd8f1dc1da
Anton Timmermans <anton@yoast.com>
2016-02-19 13:18:53 +0100

    Add missing dots at end of inline comments

--- tests/test-class-wpseo-metabox.php
+++ tests/test-class-wpseo-metabox.php
@@ -19,7 +19,7 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 	 */
 	public function test_get_post_date() {
 
-		// Create and go to post
+		// Create and go to post.
 		$post_id = $this->factory->post->create();
 		$this->go_to( get_permalink( $post_id ) );
 
@@ -36,7 +36,7 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 		global $pagenow;
 		$pagenow = 'options.php';
 
-		// Call enqueue function
+		// Call enqueue function.
 		self::$class_instance->enqueue();
 
 		$enqueued = wp_script_is( 'wp-seo-metabox', 'enqueued' );
@@ -50,13 +50,13 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 		global $pagenow;
 		$pagenow = 'post-new.php';
 
-		// Prefix used in WPSEO-admin-asset-manager
+		// Prefix used in WPSEO-admin-asset-manager.
 
 		$asset_manager = new WPSEO_Admin_Asset_Manager();
 		$asset_manager->register_assets();
 
 
-		// Call enqueue function
+		// Call enqueue function.
 		self::$class_instance->enqueue();
 
 		$enqueued = wp_script_is( WPSEO_Admin_Asset_Manager::PREFIX . 'metabox', 'enqueued' );
@@ -71,7 +71,7 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 		$post_types = get_post_types( array( 'public' => true ) );
 		unset( $post_types['attachment'] );
 
-		// Test if all post types have the wpseo_meta metabox
+		// Test if all post types have the wpseo_meta metabox.
 		foreach ( $post_types as $post_type ) {
 			$this->assertArrayHasKey( 'wpseo_meta', $wp_meta_boxes[ $post_type ]['normal']['high'] );
 		}
@@ -79,16 +79,16 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 
 	public function test_save_postdata() {
 
-		// Create and go to post
+		// Create and go to post.
 		$post_id = $this->factory->post->create();
 		$this->go_to( get_permalink( $post_id ) );
 
 		$post = get_post( $post_id );
 
-		// Setup
+		// Setup.
 		$GLOBALS['wpseo_admin'] = new WPSEO_Admin;
 
-		// vars
+		// vars.
 		$meta_fields = apply_filters( 'wpseo_save_metaboxes', array() );
 		$meta_fields = array_merge(
 			$meta_fields,
@@ -96,10 +96,10 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 			self::$class_instance->get_meta_field_defs( 'advanced' )
 		);
 
-		// Set $_POST data to be saved
+		// Set $_POST data to be saved.
 		foreach ( $meta_fields as $key => $field ) {
 
-			// Set text fields
+			// Set text fields.
 			if ( $field['type'] === 'text' ) {
 				$_POST[ WPSEO_Metabox::$form_prefix . $key ] = 'text';
 			}
@@ -108,10 +108,10 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 			}
 		}
 
-		// Call method that saves the $_POST data
+		// Call method that saves the $_POST data.
 		self::$class_instance->save_postdata( $post->ID );
 
-		// Check if output matches
+		// Check if output matches.
 		$custom = get_post_custom( $post->ID );
 		foreach ( $meta_fields as $key => $field ) {
 
@@ -121,7 +121,7 @@ class WPSEO_Metabox_Test extends WPSEO_UnitTestCase {
 
 			$value = $custom[ WPSEO_Metabox::$meta_prefix . $key ][0];
 
-			// Set text fields
+			// Set text fields.
 			if ( $field['type'] === 'text' ) {
 				$this->assertNotEmpty( $value );
 			}
