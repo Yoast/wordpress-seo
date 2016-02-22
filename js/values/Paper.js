7c9b1b7bcf6c3b2b652434b590dd7974c94e9cc2
terw-dan <danny@yoast.com>
2016-02-22 12:29:21 +0100

    Adds missing JSdoc

--- js/values/Paper.js
+++ js/values/Paper.js
@@ -1,6 +1,6 @@
 /**
  * Construct the Paper object and set the keyword property
- * @param {string} keyword
+ * @param {string} keyword The keyword to use for analyses.
  * @param {object} metas The object containing all metavalues.
  * @constructor
  */
@@ -12,7 +12,7 @@ var Paper = function( keyword, metas ) {
 
 /**
  * Check whether a keyword is available
- * @returns {boolean}
+ * @returns {boolean} Returns true if keyword isn't empty
  */
 Paper.prototype.hasKeyword = function() {
 	return this._keyword !== "";
@@ -20,7 +20,7 @@ Paper.prototype.hasKeyword = function() {
 
 /**
  * Return the associated keyword or an empty string if no keyword is available
- * @returns {string}
+ * @returns {string} Returns Keyword
  */
 Paper.prototype.getKeyword = function() {
 	return this._keyword;
@@ -28,7 +28,7 @@ Paper.prototype.getKeyword = function() {
 
 /**
  * Check whether a metaDescription is available
- * @returns {boolean}
+ * @returns {boolean} Returns true if metaDescription isn't empty
  */
 Paper.prototype.hasMetaDescription = function() {
 	return this._metaDescription !== "";
@@ -36,7 +36,7 @@ Paper.prototype.hasMetaDescription = function() {
 
 /**
  * Return the metaDescription or an empty string if no metaDescription is available
- * @returns {string}
+ * @returns {string} Returns the metaDescription
  */
 Paper.prototype.getMetaDescription = function() {
 	return this._metaDescription;
