a47605ec5cd9f691219f667fd0626eeb7c0d5af2
Andy Meerwaldt <andy@yoast.com>
2016-02-22 13:30:10 +0100

    Correct the @package line.

--- admin/formatter/class-metabox-formatter.php
+++ admin/formatter/class-metabox-formatter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package WPSEO\Admin|Formatter
+ * @package WPSEO\Admin\Formatter
  */
 
 /**
--- admin/formatter/class-post-metabox-formatter.php
+++ admin/formatter/class-post-metabox-formatter.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package WPSEO\Admin|Scraper
+ * @package WPSEO\Admin\Formatter
  */
 
 /**
