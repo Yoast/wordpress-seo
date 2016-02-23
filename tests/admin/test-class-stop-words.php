376306d866cb75c17839ae3c272dead13d047c91
Anton Timmermans <anton@yoast.com>
2016-02-23 10:47:19 +0100

    Fix stop words filtering
    Stop words filtering was broken because it assumes the post name is
    empty before it will filter stop words. Because of the slug
    synchronisation we do on changing the slug anywhere the post name is
    always set. This meant that the stop words would never be filtered.
    
    Fix this by filtering the stop words on the "get sample permalink" ajax
    request. This also makes it much more clear towards the users what is
    happening.

---
+++ tests/admin/test-class-stop-words.php
@@ -0,0 +1,46 @@
+<?php
+
+class WPSEO_Admin_Stop_WordsTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @var WPSEO_Admin_Stop_Words
+	 */
+	protected $subject;
+
+	public function setUp() {
+		$this->subject = new WPSEO_Admin_Stop_Words();
+	}
+
+	/**
+	 * @cover WPSEO_Admin_Stop_Words:remove_in
+	 */
+	public function test_remove_stop_words() {
+		$original = 'and-without-about-stop-blaat-words';
+		$expected = 'without-stop-words';
+		$subject = $this->getMock( WPSEO_Admin_Stop_Words::class, array( 'list_stop_words' ) );
+
+		$subject->method( 'list_stop_words' )
+			->willReturn( array( 'and', 'about', 'blaat' ) );
+
+		/* @type WPSEO_Admin_Stop_Words $subject */
+		$this->assertEquals( $expected, $subject->remove_in( $original ) );
+	}
+
+	/**
+	 * @cover WPSEO_Admin_Stop_Words:remove_in
+	 */
+	public function test_remove_stop_words_EMPTY() {
+		$original = '';
+		$expected = '';
+
+		$this->assertEquals( $expected, $this->subject->remove_in( $original ) );
+	}
+
+	/**
+	 * Check if list_stop_words works and throws no errors
+	 */
+	public function test_stop_words() {
+		$stopwords = $this->subject->list_stop_words();
+
+		$this->assertTrue( is_array( $stopwords ) );
+	}
+}
