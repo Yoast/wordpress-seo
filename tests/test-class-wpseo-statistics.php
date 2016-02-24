a6026e30d2bbe3c53652634f911022576a75d736
Jip Moors <jip@yoast.com>
2016-02-24 11:47:27 +0100

    Optimisation in unit tests
    Statistics:
    Instead of testing for the full range, just testing the boundaries. This means that only 4 items need to be created instead of 100.
    
    Frontend:
    Instead of creating 22 items to test for 3 pages (with 10 items per page) just set items per page to 1 and test with 3 items.
    Resetting post per page to the default in tearDown.

--- tests/test-class-wpseo-statistics.php
+++ tests/test-class-wpseo-statistics.php
@@ -32,25 +32,94 @@ class WPSEO_Statistics_Test extends WPSEO_UnitTestCase {
 		$this->assertEquals( 0, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::NO_INDEX ) ) );
 	}
 
+	/**
+	 * Tests if the statistics functions can correctly count the amount of posts in the database
+	 *
+	 * @covers WPSEO_Statistics::get_post_count
+	 */
+	public function test_filled_statistics_no_focus() {
+		$posts = $this->factory->post->create_many( 2, array(
+			'post_status' => 'publish'
+		) );
+
+		add_post_meta( $posts[0], '_yoast_wpseo_linkdex', 0 );
+		add_post_meta( $posts[1], '_yoast_wpseo_linkdex', 1 );
+
+		$this->assertEquals( 1, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::NO_FOCUS ) ) );
+	}
 
 	/**
 	 * Tests if the statistics functions can correctly count the amount of posts in the database
 	 *
 	 * @covers WPSEO_Statistics::get_post_count
 	 */
-	public function test_filled_statistics() {
-		$posts = $this->factory->post->create_many( 100, array(
+	public function test_filled_statistics_bad() {
+		$posts = $this->factory->post->create_many( 4, array(
 			'post_status' => 'publish'
 		) );
 
-		$i = 0;
-		foreach ( $posts as $post_id ) {
-			add_post_meta( $post_id, '_yoast_wpseo_linkdex', $i++ );
-		}
+		add_post_meta( $posts[0], '_yoast_wpseo_linkdex', 0 ); // not bad
+		add_post_meta( $posts[1], '_yoast_wpseo_linkdex', 1 ); // bad
+		add_post_meta( $posts[2], '_yoast_wpseo_linkdex', 40 ); // bad
+		add_post_meta( $posts[3], '_yoast_wpseo_linkdex', 41 ); // not bad
 
-		$this->assertEquals( 40, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::BAD ) ) );
-		$this->assertEquals( 30, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::OK ) ) );
-		$this->assertEquals( 29, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::GOOD ) ) );
+		$this->assertEquals( 2, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::BAD ) ) );
+	}
+
+	/**
+	 * Tests if the statistics functions can correctly count the amount of posts in the database
+	 *
+	 * @covers WPSEO_Statistics::get_post_count
+	 */
+	public function test_filled_statistics_ok() {
+		$posts = $this->factory->post->create_many( 4, array(
+			'post_status' => 'publish'
+		) );
+
+		add_post_meta( $posts[0], '_yoast_wpseo_linkdex', 40 ); // not ok
+		add_post_meta( $posts[1], '_yoast_wpseo_linkdex', 41 ); // ok
+		add_post_meta( $posts[2], '_yoast_wpseo_linkdex', 70 ); // ok
+		add_post_meta( $posts[3], '_yoast_wpseo_linkdex', 71 ); // not ok
+
+		$this->assertEquals( 2, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::OK ) ) );
+	}
+
+	/**
+	 * Tests if the statistics functions can correctly count the amount of posts in the database
+	 *
+	 * @covers WPSEO_Statistics::get_post_count
+	 */
+	public function test_filled_statistics_good() {
+		$posts = $this->factory->post->create_many( 4, array(
+			'post_status' => 'publish'
+		) );
+
+		add_post_meta( $posts[0], '_yoast_wpseo_linkdex', 70 ); // not good
+		add_post_meta( $posts[1], '_yoast_wpseo_linkdex', 71 ); // good
+		add_post_meta( $posts[2], '_yoast_wpseo_linkdex', 100 ); // good
+		add_post_meta( $posts[3], '_yoast_wpseo_linkdex', 101 ); // not good
+
+		$this->assertEquals( 2, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::GOOD ) ) );
+	}
+
+	/**
+	 * Tests if the functions only count published posts
+	 *
+	 * @covers WPSEO_Statistics::get_post_count
+	 */
+	public function test_all_statistics_published_posts() {
+		$posts = $this->factory->post->create_many( 4, array(
+			'post_status' => 'publish'
+		) );
+
+		add_post_meta( $posts[0], '_yoast_wpseo_linkdex', 0 ); // no-focus
+		add_post_meta( $posts[0], '_yoast_wpseo_linkdex', 1 ); // bad
+		add_post_meta( $posts[1], '_yoast_wpseo_linkdex', 41 ); // ok
+		add_post_meta( $posts[2], '_yoast_wpseo_linkdex', 71 ); // good
+
+		$this->assertEquals( 1, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::BAD ) ) );
+		$this->assertEquals( 1, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::OK ) ) );
+		$this->assertEquals( 1, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::GOOD ) ) );
 		$this->assertEquals( 1, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::NO_FOCUS ) ) );
 	}
 
@@ -60,14 +129,14 @@ class WPSEO_Statistics_Test extends WPSEO_UnitTestCase {
 	 * @covers WPSEO_Statistics::get_post_count
 	 */
 	public function test_only_published_posts() {
-		$posts = $this->factory->post->create_many( 100, array(
+		$posts = $this->factory->post->create_many( 4, array(
 			'post_status' => 'draft'
 		) );
 
-		$i = 0;
-		foreach ( $posts as $post_id ) {
-			add_post_meta( $post_id, '_yoast_wpseo_linkdex', $i++ );
-		}
+		add_post_meta( $posts[0], '_yoast_wpseo_linkdex', 0 ); // no-focus
+		add_post_meta( $posts[0], '_yoast_wpseo_linkdex', 1 ); // bad
+		add_post_meta( $posts[1], '_yoast_wpseo_linkdex', 41 ); // ok
+		add_post_meta( $posts[2], '_yoast_wpseo_linkdex', 71 ); // good
 
 		$this->assertEquals( 0, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::BAD ) ) );
 		$this->assertEquals( 0, $this->instance->get_post_count( new WPSEO_Rank( WPSEO_Rank::OK ) ) );
