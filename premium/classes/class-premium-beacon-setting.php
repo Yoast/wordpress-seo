cdf42b986d5891a47febc1bd015719e35cfdc4af
Anton Timmermans <anton@yoast.com>
2016-02-25 17:39:02 +0100

    Add missing PHPDoc

--- premium/classes/class-premium-beacon-setting.php
+++ premium/classes/class-premium-beacon-setting.php
@@ -8,7 +8,11 @@
  */
 class WPSEO_Premium_Beacon_Setting implements Yoast_HelpScout_Beacon_Setting {
 	/**
-	 * {@inheritdoc}
+	 * Returns a list of helpscout hashes to show the user for a certain page.
+	 *
+	 * @param string $page The current admin page we are on.
+	 *
+	 * @return array A list of suggestions for the beacon
 	 */
 	public function get_suggestions( $page ) {
 		switch ( $page ) {
