723fc30b4a41b3fae321db88fde7418f40e5b3e5
Anton Timmermans <anton@yoast.com>
2016-02-29 21:25:51 +0100

    Prevent duplicate redirect creation notifications
    When changing the slug using quick-edit we show a notification. This
    used to be added after every h2 on the page. Change this to insert after
    the first element inside the .wrap. This will break with multiple wraps,
    but I hope people won't add more elements called .wrap inside WordPress.

--- premium/assets/js/wp-seo-premium-quickedit-notification.js
+++ premium/assets/js/wp-seo-premium-quickedit-notification.js
@@ -20,7 +20,8 @@ function wpseo_show_notification() {
 		{ action: 'yoast_get_notifications' },
 		function(response) {
 			if (response !== '') {
-				jQuery(response).insertAfter('h2');
+				var insertAfterElem = jQuery( '.wrap' ).children().eq( 0 );
+				jQuery(response ).insertAfter( insertAfterElem );
 				wpseoMakeDismissible();
 				wpseo_notification_counter = 0;
 			} else if (wpseo_notification_counter < 20 && response === '') {
