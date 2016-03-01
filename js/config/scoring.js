8f715f9b4e439a76472af92e12b8cd675504bae3
terw-dan <danny@yoast.com>
2016-03-01 10:21:20 +0100

    Add check for missing scores

--- js/config/scoring.js
+++ js/config/scoring.js
@@ -6,54 +6,6 @@
 var AnalyzerScoring = function( i18n ) {
     this.analyzerScoring = [
         {
-            scoreName: "wordCount",
-            scoreArray: [
-                {
-                    min: 300,
-                    score: 9,
-
-                    /* translators: %1$d expands to the number of words in the text, %2$s to the recommended minimum of words */
-                    text: i18n.dgettext( "js-text-analysis", "The text contains %1$d words, this is more than the %2$d word recommended minimum.")
-                },
-                {
-                    min: 250,
-                    max: 299,
-                    score: 7,
-
-                    /* translators: %1$d expands to the number of words in the text, %2$s to the recommended minimum of words */
-                    text: i18n.dgettext( "js-text-analysis", "The text contains %1$d words, this is slightly below the %2$d word recommended minimum. Add a bit more copy.")
-                },
-                {
-                    min: 200,
-                    max: 249,
-                    score: 5,
-
-                    /* translators: %1$d expands to the number of words in the text, %2$d to the recommended minimum of words */
-                    text: i18n.dgettext( "js-text-analysis", "The text contains %1$d words, this is below the %2$d word recommended minimum. Add more useful content on this topic for readers.")
-                },
-                {
-                    min: 100,
-                    max: 199,
-                    score: -10,
-
-                    /* translators: %1$d expands to the number of words in the text, %2$d to the recommended minimum of words */
-                    text: i18n.dgettext( "js-text-analysis", "The text contains %1$d words, this is below the %2$d word recommended minimum. Add more useful content on this topic for readers.")
-                },
-                {
-                    min: 0,
-                    max: 99,
-                    score: -20,
-
-                    /* translators: %1$d expands to the number of words in the text */
-                    text: i18n.dgettext( "js-text-analysis", "The text contains %1$d words. This is far too low and should be increased.")
-                }
-            ],
-            replaceArray: [
-                { name: "wordCount", position: "%1$d", source: "matcher" },
-                { name: "recommendedWordcount", position: "%2$d", value: 300 }
-
-            ]
-        },{
 			scoreName: "keyphraseSizeCheck",
 			scoreArray: [
 				{
