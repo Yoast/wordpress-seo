96188bdeb92ee893c7a17fff185beb8c0f0d5aa7
Jimmy Comack <jimmy@yoast.com>
2016-03-03 21:04:55 +0100

    Fixed some tab issues and added missing docblock

--- js/helpers/replaceAll.js
+++ js/helpers/replaceAll.js
@@ -1,11 +1,17 @@
 var escapeRegExp = require( "lodash/string/escapeRegExp" );
 
+/**
+ * Replace placeholders in the passed string.
+ * @param {string} string The string that needs placeholders to be replaced.
+ * @param {object} mapObject The replacement values to be used.
+ * @returns {string} The string with the replaced placeholders.
+ */
 var replaceAll = function( string, mapObject ) {
-    var replacementString = escapeRegExp( Object.keys( mapObject ).join( "|" ) ).replace( "\\|", "|" );
+	var replacementString = escapeRegExp( Object.keys( mapObject ).join( "|" ) ).replace( "\\|", "|" );
 
-    return string.replace( new RegExp( replacementString, "gi" ), function( matched ) {
-        return mapObject[ matched.toLowerCase() ];
-    } );
+	return string.replace( new RegExp( replacementString, "gi" ), function( matched ) {
+		return mapObject[ matched.toLowerCase() ];
+	} );
 };
 
 module.exports = replaceAll;
