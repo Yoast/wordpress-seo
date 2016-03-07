daf4464ea1bf29e11dcce15a034ebf4e7ff0bfc2
Andy Meerwaldt <andy@yoast.com>
2016-03-07 10:57:59 +0100

    Change tests for the select object

--- tests/admin/test-class-yoast-form-select.php
+++ tests/admin/test-class-yoast-form-select.php
@@ -1,118 +1,97 @@
 <?php
 
-/**
- * Class Yoast_Form_Select_Test
- *
- * @group test
- */
 class Yoast_Form_Select_Test extends PHPUnit_Framework_TestCase {
 
 	/**
 	 * Testing with valid options.
 	 *
 	 * @covers Yoast_Form_Select::__construct
-	 * @covers Yoast_Form_Select::get_html
-	 * @covers Yoast_Form_Select::parse_options
-	 * @covers Yoast_Form_Select::sanitize_options
-	 * @covers Yoast_Form_Select::sanitize_option
-	 * @covers Yoast_Form_Select::parse_option
+	 * @covers Yoast_Form_Select::get_select_values
+	 * @covers Yoast_Form_Select::filter_invalid_options
+	 * @covers Yoast_Form_Select::is_valid_option
 	 */
 	public function test_with_valid_options() {
-		$select = new Yoast_Form_Select( 'test-field', 'test-field', array( 'foo' => 'bar', 'baz' => 'foo' ), false );
-		$html   = $select->get_html();
+		$select = new Yoast_Form_Select( 'test-id', 'test-field', 'test', array( 'foo' => 'bar', 'baz' => 'foo' ), false );
+		$values = $select->get_select_values();
 
-		$this->assertContains('<select class="select" name="test-field" id="test-field">', $html);
-		$this->assertContains('<option value="foo">bar</option>', $html);
-		$this->assertContains('<option value="baz">foo</option>', $html);
+		$this->assertEquals( 'test-id', $values['select_id'] );
+		$this->assertEquals( 'test-field', $values['select_name'] );
+		$this->assertEquals( 'test', $values['select_class'] );
+		$this->assertEquals(  array( 'foo' => 'bar', 'baz' => 'foo' ) , $values['select_options'] );
+		$this->assertEquals(  false, $values['selected_option'] );
 	}
+
 	/**
 	 * Testing with valid options and one option being active.
 	 *
 	 * @covers Yoast_Form_Select::__construct
-	 * @covers Yoast_Form_Select::get_html
-	 * @covers Yoast_Form_Select::parse_options
-	 * @covers Yoast_Form_Select::sanitize_options
-	 * @covers Yoast_Form_Select::sanitize_option
-	 * @covers Yoast_Form_Select::parse_option
+	 * @covers Yoast_Form_Select::get_select_values
+	 * @covers Yoast_Form_Select::filter_invalid_options
+	 * @covers Yoast_Form_Select::is_valid_option
 	 */
 	public function test_with_active_option() {
-		$select = new Yoast_Form_Select( 'test-field', 'test-field', array( 'foo' => 'bar', 'baz' => 'foo' ), 'baz' );
-		$html   = $select->get_html();
+		$select = new Yoast_Form_Select( 'test-id', 'test-field', 'test', array( 'foo' => 'bar', 'baz' => 'foo' ), 'baz' );
+		$values = $select->get_select_values();
 
-		$this->assertContains('<select class="select" name="test-field" id="test-field">', $html);
-		$this->assertContains('<option value="foo">bar</option>', $html);
-		$this->assertContains('<option value="baz" selected=\'selected\'>foo</option>', $html);
+		$this->assertEquals(  'baz', $values['selected_option'] );
 	}
 
 	/**
-	 * Testing what will happen when one options is totally blank
+	 * Testing what will happen when one option is totally blank
 	 *
 	 * @covers Yoast_Form_Select::__construct
-	 * @covers Yoast_Form_Select::get_html
-	 * @covers Yoast_Form_Select::parse_options
-	 * @covers Yoast_Form_Select::sanitize_options
-	 * @covers Yoast_Form_Select::sanitize_option
-	 * @covers Yoast_Form_Select::parse_option
+	 * @covers Yoast_Form_Select::get_select_values
+	 * @covers Yoast_Form_Select::filter_invalid_options
+	 * @covers Yoast_Form_Select::is_valid_option
 	 */
 	public function test_with_empty_option() {
-		$select = new Yoast_Form_Select( 'test-field', 'test-field', array( '' => '', 'foo' => 'bar' ), false );
-		$html   = $select->get_html();
+		$select = new Yoast_Form_Select( 'test-id', 'test-field', 'test', array( '' => '', 'foo' => 'bar' ), false );
+		$values = $select->get_select_values();
 
-		$this->assertContains('<select class="select" name="test-field" id="test-field">', $html);
-		$this->assertContains('<option value="" selected=\'selected\'></option>', $html);
-		$this->assertContains('<option value="foo">bar</option>', $html);
+		$this->assertEquals( array( '' => '', 'foo' => 'bar' ), $values['select_options'] );
 	}
 
 	/**
 	 * Test if invalid options will be sanitized
 	 *
 	 * @covers Yoast_Form_Select::__construct
-	 * @covers Yoast_Form_Select::get_html
-	 * @covers Yoast_Form_Select::parse_options
-	 * @covers Yoast_Form_Select::sanitize_options
-	 * @covers Yoast_Form_Select::sanitize_option
-	 * @covers Yoast_Form_Select::parse_option
+	 * @covers Yoast_Form_Select::get_select_values
+	 * @covers Yoast_Form_Select::filter_invalid_options
+	 * @covers Yoast_Form_Select::is_valid_option
 	 */
 	public function test_with_invalid_option() {
-		$select = new Yoast_Form_Select( 'test-field', 'test-field', array( 'no-label' => '', 'foo' => 'bar' ), false );
-		$html   = $select->get_html();
+		$select = new Yoast_Form_Select( 'test-id', 'test-field', 'test', array( 'no-label' => '', 'foo' => 'bar' ), false );
+		$values = $select->get_select_values();
 
-		$this->assertContains('<select class="select" name="test-field" id="test-field">', $html);
-		$this->assertNotContains('<option value="no-label"></option>', $html);
-		$this->assertContains('<option value="foo">bar</option>', $html);
+		$this->assertEquals(  array( 'foo' => 'bar' ), $values['select_options'] );
 	}
 
 	/**
 	 * Test if invalid options will be sanitized
 	 *
 	 * @covers Yoast_Form_Select::__construct
-	 * @covers Yoast_Form_Select::get_html
-	 * @covers Yoast_Form_Select::parse_options
-	 * @covers Yoast_Form_Select::sanitize_options
-	 * @covers Yoast_Form_Select::sanitize_option
+	 * @covers Yoast_Form_Select::get_select_values
+	 * @covers Yoast_Form_Select::filter_invalid_options
+	 * @covers Yoast_Form_Select::is_valid_option
 	 */
 	public function test_with_only_one_invalid_option() {
-		$select = new Yoast_Form_Select( 'test-field', 'test-field', array( 'no-label' => '' ), false );
-		$html   = $select->get_html();
+		$select = new Yoast_Form_Select( 'test-id', 'test-field', 'test', array( 'no-label' => '' ), false );
+		$values = $select->get_select_values();
 
-		$this->assertContains('<select class="select" name="test-field" id="test-field">', $html);
-		$this->assertNotContains('<option value="no-label"></option>', $html);
+		$this->assertEquals(  array( ), $values['select_options'] );
 	}
 
 	/**
 	 * Test what happens when no options are given.
 	 *
 	 * @covers Yoast_Form_Select::__construct
-	 * @covers Yoast_Form_Select::get_html
-	 * @covers Yoast_Form_Select::parse_options
+	 * @covers Yoast_Form_Select::get_select_values
 	 */
 	public function test_without_options() {
-		$select = new Yoast_Form_Select( 'test-field', 'test-field', array(), false );
-		$html   = $select->get_html();
+		$select = new Yoast_Form_Select( 'test-field', 'test-field', 'test', array(), false );
+		$values = $select->get_select_values();
 
-		$this->assertContains('<select class="select" name="test-field" id="test-field">', $html);
-		$this->assertNotContains('<option', $html);
-		$this->assertNotContains('</option>', $html);
+		$this->assertEquals(  array( ), $values['select_options'] );
 	}
 
 
