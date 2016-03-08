45f5f4a05d0f7bed44261e9c4f654a19f85fc04e
Jimmy Comack <jimmy@yoast.com>
2016-03-08 17:15:38 +0100

    Made a quick method to repeat a string

--- spec/assessments/countWordAssessmentSpec.js
+++ spec/assessments/countWordAssessmentSpec.js
@@ -17,6 +17,22 @@ var constructi18n = function() {
 
 var i18n = constructi18n();
 
+/**
+ * This method repeats a string and returns a new string based on the string and the amount of repetitions.
+ * @param string
+ * @param times
+ * @returns {string}
+ */
+var repeatString = function(string, times) {
+	var resultString = "";
+
+	for (var i = 0; i < times; i++) {
+		resultString += string;
+	}
+
+	return resultString;
+};
+
 describe( "A word count assessment", function(){
 	it( "assesses a single word", function(){
 		var mockPaper = new Paper( "sample" );
@@ -35,28 +51,28 @@ describe( "A word count assessment", function(){
 	} );
 
 	it( "assesses a medium word count", function(){
-		var mockPaper = new Paper( "Sample ".repeat(150) );
+		var mockPaper = new Paper( repeatString("Sample ", 150) );
 		var assessment = wordCountAssessment( mockPaper, i18n );
 
 		expect( assessment.getScore() ).toEqual( -10 );
 	} );
 
 	it( "assesses a slightly higher than medium word count", function(){
-		var mockPaper = new Paper( "Sample ".repeat(225) );
+		var mockPaper = new Paper( repeatString("Sample ", 225) );
 		var assessment = wordCountAssessment( mockPaper, i18n );
 
 		expect( assessment.getScore() ).toEqual( 5 );
 	} );
 
-	it( "assesses a almost at the recommended amount, word count", function(){
-		var mockPaper = new Paper( "Sample ".repeat(275) );
+	it( "assesses an almost at the recommended amount, word count", function(){
+		var mockPaper = new Paper( repeatString("Sample ", 275) );
 		var assessment = wordCountAssessment( mockPaper, i18n );
 
 		expect( assessment.getScore() ).toEqual( 7 );
 	} );
 
 	it( "assesses high word count", function(){
-		var mockPaper = new Paper( "Sample ".repeat(325) );
+		var mockPaper = new Paper( repeatString("Sample ", 325) );
 		var assessment = wordCountAssessment( mockPaper, i18n );
 
 		expect( assessment.getScore() ).toEqual( 9 );
