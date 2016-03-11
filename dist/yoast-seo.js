80208a9eb4595bdee8502c9e21d73fba098eb7c5
Jimmy Comack <jimmy@yoast.com>
2016-03-11 09:18:45 +0100

    Fixed some more code style issues

--- dist/yoast-seo.js
+++ dist/yoast-seo.js
@@ -1714,7 +1714,7 @@ var calculateWordCountResult = function( wordCount, i18n ) {
 		return {
 			score: 7,
 			/* translators: %1$d expands to the number of words in the text, %2$s to the recommended minimum of words */
-			text : i18n.dngettext(
+			text: i18n.dngettext(
 				"js-text-analysis",
 				"The text contains %1$d word, this is slightly below the %2$d word recommended minimum. Add a bit more copy.",
 				"The text contains %1$d words, this is slightly below the %2$d word recommended minimum. Add a bit more copy.",
@@ -1727,7 +1727,7 @@ var calculateWordCountResult = function( wordCount, i18n ) {
 		return {
 			score: 5,
 			/* translators: %1$d expands to the number of words in the text, %2$d to the recommended minimum of words */
-			text : i18n.dngettext(
+			text: i18n.dngettext(
 				"js-text-analysis",
 				"The text contains %1$d word, this is below the %2$d word recommended minimum. Add more useful content on this topic for readers.",
 				"The text contains %1$d words, this is below the %2$d word recommended minimum. Add more useful content on this topic for readers.",
@@ -1740,7 +1740,7 @@ var calculateWordCountResult = function( wordCount, i18n ) {
 		return {
 			score: -10,
 			/* translators: %1$d expands to the number of words in the text, %2$d to the recommended minimum of words */
-			text : i18n.dngettext(
+			text: i18n.dngettext(
 				"js-text-analysis",
 				"The text contains %1$d word, this is below the %2$d word recommended minimum. Add more useful content on this topic for readers.",
 				"The text contains %1$d words, this is below the %2$d word recommended minimum. Add more useful content on this topic for readers.",
@@ -1753,7 +1753,7 @@ var calculateWordCountResult = function( wordCount, i18n ) {
 		return {
 			score: -20,
 			/* translators: %1$d expands to the number of words in the text */
-			text : i18n.dngettext(
+			text: i18n.dngettext(
 				"js-text-analysis",
 				"The text contains %1$d word, this is far too low and should be increased.",
 				"The text contains %1$d words, this is far too low and should be increased.",
