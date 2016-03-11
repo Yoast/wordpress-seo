29a3fbb92a468e5b0d70ecf0353ed94c37f4f85d
Andy Meerwaldt <andy@yoast.com>
2016-03-11 09:54:04 +0100

    Setting up the grunt tasks for the sass and css files

---
+++ grunt/config/sass.js
@@ -0,0 +1,12 @@
+
+//https://github.com/sindresorhus/grunt-sass
+module.exports = {
+	options: {
+		outputStyle: "compressed"
+	},
+	build: {
+		files: {
+			"dist/yoast-social-preview.min.css": "css/social_preview.scss"
+		}
+	}
+};
