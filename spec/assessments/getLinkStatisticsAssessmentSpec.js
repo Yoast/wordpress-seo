44058a291bde678f704a3881967655cac3bff790
terw-dan <danny@yoast.com>
2016-03-17 13:49:16 +0100

    Includes specs for links

--- spec/assessments/getLinkStatisticsAssessmentSpec.js
+++ spec/assessments/getLinkStatisticsAssessmentSpec.js
@@ -11,13 +11,41 @@ describe( "An assessor running the linkStatistics", function(){
 			url: "http://example.com"
 		};
 		var mockPaper = new Paper( "a test with a <a href='http://yoast.com' alt=''>keyword link </a>"  );
-		var assessment = linkStatisticAssessment( mockPaper, i18n );
+
+		var mockResult = {externalDofollow: 1,
+			externalNofollow: 0,
+			externalTotal: 1,
+			internalDofollow: 0,
+			internalNofollow: 0,
+			internalTotal: 0,
+			otherDofollow: 0,
+			otherNofollow: 0,
+			otherTotal:0,
+			total: 1,
+			totalKeyword: 0,
+			totalNaKeyword: 1};
+
+		var assessment = linkStatisticAssessment( mockPaper, Factory.buildMockResearcher( mockResult ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 2 );
 		expect( assessment.getText() ).toEqual ( 'Outbound links appear in this page' );
 
 		mockPaper = new Paper( "a test with a <a href='http://yoast.com' alt='' rel='nofollow'> link </a>", attributes );
-		assessment = linkStatisticAssessment( mockPaper, i18n );
+
+		var mockResult = {externalDofollow: 0,
+			externalNofollow: 1,
+			externalTotal: 1,
+			internalDofollow: 0,
+			internalNofollow: 0,
+			internalTotal: 0,
+			otherDofollow: 0,
+			otherNofollow: 0,
+			otherTotal:0,
+			total: 1,
+			totalKeyword: 0,
+			totalNaKeyword: 0};
+
+		assessment = linkStatisticAssessment( mockPaper, Factory.buildMockResearcher( mockResult ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 7 );
 		expect( assessment.getText() ).toEqual ( 'This page has 1 outbound link(s), all nofollowed.' );
