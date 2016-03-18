8ae9a832dbc0566034cf68c471a936595fbf8120
terw-dan <danny@yoast.com>
2016-03-18 15:00:15 +0100

    updates spec

--- spec/analyses/keywordDensitySpec.js
+++ spec/analyses/keywordDensitySpec.js
@@ -1,16 +1,27 @@
-var keywordDensity = require("../../js/analyses/getKeywordDensity.js");
+var keywordDensity = require("../../js/researches/getKeywordDensity.js");
+var Paper = require( "../../js/values/Paper.js" );
 
 describe("Test for counting the keywordDensity in a text", function(){
 	it("returns keywordDensity", function(){
-		expect( keywordDensity("a string of text with the keyword in it, density should be 7.7%", "keyword" ) ).toBe( "7.7" );
-		expect( keywordDensity("a string of text without the keyword in it, density should be 0%", "empty" ) ).toBe( "0.0" );
-		expect( keywordDensity("Waltz keepin auf mitz auf keepin äöüß weiner blitz deutsch spitzen. ", "äöüß" ) ).toBe("9.1");
-		expect( keywordDensity("Lorem ipsum dolor sit amet, key word consectetur key-word adipiscing elit", "key-word" ) ).toBe("9.1");
-		expect( keywordDensity( "a string of text with the kapaklı in it, density should be 7.7%", "kapaklı" ) ).toBe( "7.7" );
-		expect( keywordDensity( "a string of text with the key-word in it, density should be 7.7%", "key-word" ) ).toBe( "7.7" );
-		expect( keywordDensity( "a string of text with the key_word in it, density should be 7.7%", "key_word" ) ).toBe( "7.7" );
-		expect( keywordDensity( "a string of text with the key_word in it, density should be 7.7%", "key word" ) ).toBe( "0.0" );
-		expect( keywordDensity( "a string of text with the key-word in it, density should be 7.7%", "key word" ) ).toBe( "0.0" );
-		expect( keywordDensity( "a string of text with the key&word in it, density should be 7.7%", "key&word" ) ).toBe( "7.7" );
+		var mockPaper = new Paper( "a string of text with the keyword in it, density should be 7.7%", {keyword: "keyword"} );
+		expect( keywordDensity(mockPaper ) ).toBe( "7.7" );
+		mockPaper = new Paper( "a string of text without the keyword in it, density should be 0%", {keyword: "empty"} );
+		expect( keywordDensity( mockPaper ) ).toBe( "0.0" );
+		mockPaper = new Paper( "Waltz keepin auf mitz auf keepin äöüß weiner blitz deutsch spitzen. ", {keyword: "äöüß"} );
+		expect( keywordDensity( mockPaper ) ).toBe("9.1");
+		mockPaper = new Paper( "Lorem ipsum dolor sit amet, key word consectetur key-word adipiscing elit ", {keyword: "key-word"} );
+		expect( keywordDensity( mockPaper  ) ).toBe("9.1");
+		mockPaper = new Paper( "a string of text with the kapaklı in it, density should be 7.7%", {keyword: "kapaklı"} );
+		expect( keywordDensity( mockPaper ) ).toBe( "7.7" );
+		mockPaper = new Paper( "a string of text with the key-word in it, density should be 7.7%", {keyword: "key-word"} );
+		expect( keywordDensity( mockPaper ) ).toBe( "7.7" );
+		mockPaper = new Paper( "a string of text with the key_word in it, density should be 7.7%", {keyword: "key_word"} );
+		expect( keywordDensity( mockPaper ) ).toBe( "7.7" );
+		mockPaper = new Paper( "a string of text with the key_word in it, density should be 0.0%", {keyword: "key word"} );
+		expect( keywordDensity( mockPaper ) ).toBe( "0.0" );
+		mockPaper = new Paper( "a string of text with the key-word in it, density should be 0.0%", {keyword: "key word"} );
+		expect( keywordDensity( mockPaper ) ).toBe( "0.0" );
+		mockPaper = new Paper( "a string of text with the key&word in it, density should be 7.7%", {keyword: "key&word"} );
+		expect( keywordDensity( mockPaper ) ).toBe( "7.7" );
 	});
 });
