0d043259c8d9f35bbb8878bf51fab030a9c31fcf
terw-dan <danny@yoast.com>
2016-03-18 10:27:24 +0100

    fix codestyle

--- js/assessments/pagetitleKeyword.js
+++ js/assessments/pagetitleKeyword.js
@@ -1,22 +1,30 @@
 var AssessmentResult = require( "../values/AssessmentResult.js" );
-var inRange = require( "lodash/number/inRange" );
 
+/**
+ * Executes the pagetitle keyword assessment and returns an assessment result.
+ * @param {Paper} paper The Paper object to assess.
+ * @param {Researcher} researcher The Researcher object containing all available researches.
+ * @param {object} i18n The locale object.
+ * @returns {AssessmentResult} The result of the assessment with text and score
+ */
 var pageTitleKeywordAssessment = function( paper, researcher, i18n ) {
 	var findPageTitleKeywordResult = researcher.getResearch( "findKeywordInPageTitle" );
 	var score, text;
 
-	if ( findPageTitleKeywordResult. matches === 0 ) {
+	if ( findPageTitleKeywordResult.matches === 0 ) {
 		score = 2;
 		text = i18n.sprintf( i18n.dgettext( "js-text-analysis", "The focus keyword '%1$s' does not appear in the page title." ), paper.getKeyword() );
 	}
 
-	if( findPageTitleKeywordResult.matches > 0 && findPageTitleKeywordResult.position === 1 ) {
+	if ( findPageTitleKeywordResult.matches > 0 && findPageTitleKeywordResult.position === 1 ) {
 		score = 9;
-		text = i18n.dgettext( "js-text-analysis", "The page title contains the focus keyword, at the beginning which is considered to improve rankings.");
+		text = i18n.dgettext( "js-text-analysis", "The page title contains the focus keyword, at the beginning which is considered " +
+			"to improve rankings." );
 	}
-	if( findPageTitleKeywordResult.matches > 0 && findPageTitleKeywordResult.position > 1 ) {
+	if ( findPageTitleKeywordResult.matches > 0 && findPageTitleKeywordResult.position > 1 ) {
 		score = 6;
-		text = i18n.dgettext( "js-text-analysis", "The page title contains the focus keyword, but it does not appear at the beginning; try and move it to the beginning.");
+		text = i18n.dgettext( "js-text-analysis", "The page title contains the focus keyword, but it does not appear at the beginning;" +
+			" try and move it to the beginning." );
 	}
 	var assessmentResult = new AssessmentResult();
 
--- spec/analyses/pageTitleKeywordSpec.js
+++ spec/analyses/pageTitleKeywordSpec.js
@@ -5,30 +5,30 @@ var result;
 describe("Match keywords in string", function(){
 	it("returns number of matches and position", function(){
 
-		var mockPaper = new Paper( "", { keyword: "keyword", title: "keyword in a string"})
+		var mockPaper = new Paper( "", { keyword: "keyword", title: "keyword in a string" } );
 
 		result = pageTitleKeyword( mockPaper );
 		expect( result.matches ).toBe( 1 );
 		expect( result.position ).toBe( 0 );
 
-		var mockPaper = new Paper( "", { keyword: "keyword", title: "a string with a keyword and another keyword"})
+		var mockPaper = new Paper( "", { keyword: "keyword", title: "a string with a keyword and another keyword" } );
 		result = pageTitleKeyword( mockPaper );
 		expect( result.matches ).toBe( 2 );
 		expect( result.position ).toBe( 16 );
 
-		var mockPaper = new Paper( "", { keyword: "", title: "a string with words"})
+		var mockPaper = new Paper( "", { keyword: "", title: "a string with words" } );
 		result = pageTitleKeyword( mockPaper );
 		expect( result.matches).toBe( 0 );
 
-		var mockPaper = new Paper( "", { keyword: "нечто", title: "ст, чтобы проверить нечто Test текст, чтобы "})
+		var mockPaper = new Paper( "", { keyword: "нечто", title: "ст, чтобы проверить нечто Test текст, чтобы " } );
 		result = pageTitleKeyword( mockPaper );
 		expect( result.matches ).toBe( 1 );
 
-		var mockPaper = new Paper( "", { keyword: "äbc", title: "äbc"})
+		var mockPaper = new Paper( "", { keyword: "äbc", title: "äbc" } );
 		result = pageTitleKeyword( mockPaper );
 		expect( result.matches ).toBe( 1 );
 
-		var mockPaper = new Paper( "", { keyword: "focus keyword", title: "focus-keyword"})
+		var mockPaper = new Paper( "", { keyword: "focus keyword", title: "focus-keyword" } );
 		result = pageTitleKeyword( mockPaper );
 		expect( result.matches ).toBe( 0 );
 	});
