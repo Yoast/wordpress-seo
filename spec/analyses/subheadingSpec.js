aca4bf7de932e6d1cc1031c2f7975cf0af645de2
terw-dan <danny@yoast.com>
2016-03-21 13:19:25 +0100

    Updates spec

--- spec/analyses/subheadingSpec.js
+++ spec/analyses/subheadingSpec.js
@@ -1,27 +1,36 @@
 var subheadingFunction = require("../../js/researches/matchKeywordInSubheadings.js");
+var Paper = require( "../../js/values/Paper.js" );
+
 
 describe("a test for matching keywords in subheadings", function(){
 	it("returns the number of matches in the subheadings", function(){
-		var result = subheadingFunction( "<h2>Lorem ipsum</h2> keyword sit amet, consectetur adipiscing elit", "keyword");
+		var mockPaper = new Paper( "<h2>Lorem ipsum</h2> keyword sit amet, consectetur adipiscing elit", {keyword: "keyword" } );
+		var result = subheadingFunction( mockPaper );
 		expect( result.count ).toBe(1);
 		expect( result.matches).toBe(0);
 
-		result = subheadingFunction( "Pellentesque sit amet justo ex. Suspendisse feugiat pulvinar leo eu consectetur", "keyword" );
+		mockPaper = new Paper( "Pellentesque sit amet justo ex. Suspendisse feugiat pulvinar leo eu consectetur" );
+		result = subheadingFunction( mockPaper );
 		expect( result.count ).toBe(0);
 
-		result = subheadingFunction( "<h2>this is a heading with a diacritic keyword kapaklı </h2>", "kapaklı" );
+		mockPaper = new Paper( "<h2>this is a heading with a diacritic keyword kapaklı </h2>", {keyword: "kapaklı" } );
+		result = subheadingFunction( mockPaper );
 		expect( result.matches ).toBe( 1 );
 
-		result = subheadingFunction( "<h2>this is a heading with a dashed key-word</h2>", "key-word" );
+		mockPaper = new Paper( "<h2>this is a heading with a dashed key-word</h2>", {keyword: "key-word" } );
+		result = subheadingFunction( mockPaper );
 		expect( result.matches ).toBe(1);
 
-		result = subheadingFunction( "<h2>this is a heading with a underscored key_word</h2>", "key_word" );
+		mockPaper = new Paper( "<h2>this is a heading with a underscored key_word</h2>", {keyword: "key_word" } );
+		result = subheadingFunction( mockPaper );
 		expect( result.matches ).toBe(1);
 
-		result = subheadingFunction( "<h2>this is a heading with a underscored key-word</h2>", "key word" );
+		mockPaper = new Paper("<h2>this is a heading with a underscored key-word</h2>", {keyword: "key word" } );
+		result = subheadingFunction( mockPaper );
 		expect( result.matches ).toBe( 0 );
 
-		result = subheadingFunction( "<h2>this is a heading with a underscored key_word</h2>", "key word" );
+		mockPaper = new Paper( "<h2>this is a heading with a underscored key_word</h2>", {keyword: "key word" } );
+		result = subheadingFunction( mockPaper );
 		expect( result.matches ).toBe( 0 );
 	});
 });
