48f5fe9fb38eec7f06a9f72075e1c6bec7b54461
Andy Meerwaldt <andy@yoast.com>
2016-03-21 08:59:32 +0100

    Add helpers

---
+++ js/helpers/addClass.js
@@ -0,0 +1,17 @@
+/**
+ * Adds a class to an element
+ *
+ * @param {HTMLElement} element The element to add the class to.
+ * @param {string} className The class to add.
+ *
+ * @return {void}
+ */
+module.exports = function( element, className ) {
+	var classes = element.className.split( " " );
+
+	if ( -1 === classes.indexOf( className ) ) {
+		classes.push( className );
+	}
+
+	element.className = classes.join( " " );
+};
