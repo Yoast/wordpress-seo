cbed17810bc605c0a6c8e05721e9a26cf121b748
Jimmy Comack <jimmy@yoast.com>
2016-03-21 11:30:46 +0100

    Added missing doc

--- js/researches/stopWordsInUrl.js
+++ js/researches/stopWordsInUrl.js
@@ -4,7 +4,7 @@ var stopWordsInText = require( "./stopWordsInText.js" );
 
 /**
  * Matches stopwords in the URL. Replaces - and _ with whitespace.
- * @param {Paper} paper
+ * @param {Paper} paper The Paper object to get the url from.
  * @returns {Array} stopwords found in URL
  */
 module.exports = function( paper ) {
