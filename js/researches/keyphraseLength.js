40783e7aa786c79c1fe6b102cdd829f5855ffb6a
Anton Timmermans <services@atimmer.com>
2016-03-21 21:41:30 +0100

    Add missing JSDoc

--- js/researches/keyphraseLength.js
+++ js/researches/keyphraseLength.js
@@ -5,11 +5,12 @@ var sanitizeString = require( "../stringProcessing/sanitizeString" );
  * Determines the length in words of a the keyphrase, the keyword is a keyphrase if it is more than one word.
  *
  * @param {Paper} paper The paper to research
+ * @returns {number} The length of the keyphrase
  */
 function keyphraseLengthResearch( paper ) {
-    var keyphrase = sanitizeString( paper.getKeyword() );
+	var keyphrase = sanitizeString( paper.getKeyword() );
 
-    return countWords( keyphrase );
+	return countWords( keyphrase );
 }
 
 module.exports = keyphraseLengthResearch;
