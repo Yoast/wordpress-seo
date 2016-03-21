58a979387dd3900b0e15886bd0a9ec35c5f33afc
terw-dan <danny@yoast.com>
2016-03-21 15:46:09 +0100

    Fixes jsdoc

--- js/assessments/matchKeywordInSubheading.js
+++ js/assessments/matchKeywordInSubheading.js
@@ -3,28 +3,28 @@ var AssessmentResult = require( "../values/AssessmentResult.js" );
 /**
  * Returns a score and text based on the keyword matches object.
  *
- * @param {object} subheadings The object with all subheadings matches.
- * @param {object} i18n The object used for translations
- * @returns {object} resultObject with score and text
+ * @param {object} subHeadings The object with all subHeadings matches.
+ * @param {object} i18n The object used for translations.
+ * @returns {object} resultObject with score and text.
  */
-var calculateKeywordMatchesResult = function( subheadings, i18n ) {
-	if ( subheadings.count === 0 ) {
+var calculateKeywordMatchesResult = function( subHeadings, i18n ) {
+	if ( subHeadings.count === 0 ) {
 		return {
 			score: 7,
 			text: i18n.dgettext( "js-text-analysis", "No subheading tags (like an H2) appear in the copy." )
 		};
 	}
-	if ( subheadings.matches === 0 ) {
+	if ( subHeadings.matches === 0 ) {
 		return {
 			score: 3,
 			text: i18n.dgettext( "js-text-analysis", "You have not used your focus keyword in any subheading (such as an H2) in your copy." )
 		};
 	}
-	if ( subheadings.matches >= 1 ) {
+	if ( subHeadings.matches >= 1 ) {
 		return {
 			score: 9,
 			text: i18n.sprintf( i18n.dgettext( "js-text-analysis", "The focus keyword appears in %2$d (out of %1$d) subheadings in the copy. " +
-				"While not a major ranking factor, this is beneficial." ), subheadings.count, subheadings.matches )
+				"While not a major ranking factor, this is beneficial." ), subHeadings.count, subHeadings.matches )
 		};
 	}
 };
@@ -34,18 +34,18 @@ var calculateKeywordMatchesResult = function( subheadings, i18n ) {
  *
  * @param {object} paper The paper to use for the assessment.
  * @param {object} researcher The researcher used for calling research.
- * @param {object} i18n The object used for translations
+ * @param {object} i18n The object used for translations.
  * @returns {object} the Assessmentresult
  */
-var matchKeywordinSubheadingAssessment = function( paper,  researcher, i18n ) {
-	var subheadings = researcher.getResearch( "matchKeywordInSubheadings" );
-	var subheadingsResult = calculateKeywordMatchesResult( subheadings, i18n );
+var matchKeywordinSubHeadingAssessment = function( paper, researcher, i18n ) {
+	var subHeadings = researcher.getResearch( "matchKeywordInSubheadings" );
+	var subHeadingsResult = calculateKeywordMatchesResult( subHeadings, i18n );
 	var assessmentResult = new AssessmentResult();
 
-	assessmentResult.setScore( subheadingsResult.score );
-	assessmentResult.setText( subheadingsResult.text );
+	assessmentResult.setScore( subHeadingsResult.score );
+	assessmentResult.setText( subHeadingsResult.text );
 
 	return assessmentResult;
 };
 
-module.exports = matchKeywordinSubheadingAssessment;
+module.exports = matchKeywordinSubHeadingAssessment;
