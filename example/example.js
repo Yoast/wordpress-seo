ca70631b5b456e96b05a519d5f2bcc55c5346f3d
Andy Meerwaldt <andy@yoast.com>
2016-03-21 09:00:44 +0100

    Add twitter preview.

--- example/example.js
+++ example/example.js
@@ -1,9 +1,18 @@
-var FacebookPreview = require( "../js/facebook-preview.js" );
+var FacebookPreview = require( "../js/facebookPreview.js" );
+var TwitterPreview  = require( "../js/twitterPreview.js" );
 
 var facebookPreview = new FacebookPreview(
 	{
-		targetElement: document.getElementById(  'output-container' )
+		targetElement: document.getElementById(  'facebook-container' )
 	}
 );
 
 facebookPreview.init();
+
+var twitterPreview = new TwitterPreview(
+	{
+		targetElement: document.getElementById(  'twitter-container' )
+	}
+);
+
+twitterPreview.init();
