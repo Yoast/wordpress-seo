4359744994506ae6037cf7b440215921195d6a8d
terw-dan <danny@yoast.com>
2016-03-25 15:19:33 +0100

    updates specs

--- spec/assessments/stopWordInKeywordAssessmentSpec.js
+++ spec/assessments/stopWordInKeywordAssessmentSpec.js
@@ -20,7 +20,7 @@ describe( "A stop word in keyword assessment", function() {
 		});
 
 		var assessment = stopWordsInKeywordAssessment( mockPaper, Factory.buildMockResearcher( [ "about" ] ), i18n );
-		expect( assessment.getScore() ).toEqual( "na" );
+		expect( assessment.getScore() ).toEqual( 0 );
 		expect( assessment.getText() ).toEqual ( "Your focus keyword contains a stop word. This may or may not be wise depending on the circumstances. Read <a href='https://yoast.com/handling-stopwords/' target='new'>this article</a> for more info.");
 	} );
 
@@ -30,7 +30,7 @@ describe( "A stop word in keyword assessment", function() {
 		});
 
 		var assessment = stopWordsInKeywordAssessment( mockPaper, Factory.buildMockResearcher( [ "about", "before" ] ), i18n );
-		expect( assessment.getScore() ).toEqual( "na" );
+		expect( assessment.getScore() ).toEqual( 0 );
 		expect( assessment.getText() ).toEqual ( "Your focus keyword contains 2 stop words. This may or may not be wise depending on the circumstances. Read <a href='https://yoast.com/handling-stopwords/' target='new'>this article</a> for more info.");
 	} );
 } );
