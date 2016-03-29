a1c1b9cd2397827e89c82f6c870ff6428aea23e1
Anton Timmermans <anton@yoast.com>
2016-03-29 18:18:56 +0200

    Remove unused error prototype

--- js/errors/invalidRange.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-module.exports = function InvalidRangeError( message ) {
-	Error.captureStackTrace( this, this.constructor );
-	this.name = this.constructor.name;
-	this.message = message || "Invalid range";
-};
-
-require( "util" ).inherits( module.exports, Error );
