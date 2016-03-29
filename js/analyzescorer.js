e58d2d4f16a0174e4416e7abc90acd03bfcd43e9
Jimmy Comack <jimmy@yoast.com>
2016-03-29 12:24:28 +0200

    Fixed faulty reference in the analyze scorer

--- js/analyzescorer.js
+++ js/analyzescorer.js
@@ -83,7 +83,7 @@ AnalyzeScorer.prototype.genericScore = function( obj ) {
 	var scoreObj = this.scoreLookup( obj.test );
 
 	if ( isUndefined( scoreObj ) ) {
-		return assessments[ obj.test ]( this.paper, YoastSEO.app.researcher,  this.i18n );
+		return assessments[ obj.test ].callback( this.paper, YoastSEO.app.researcher,  this.i18n );
 	}
 
 	return this.calculateScore( obj, scoreObj, scoreObj.scoreName );
