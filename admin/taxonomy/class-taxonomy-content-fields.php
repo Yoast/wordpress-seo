cf0f27a531277c7fc08f497deefeeb38de2e121c
Andrea Fercia <a.fercia@gmail.com>
2016-03-29 14:42:24 +0200

    Make the Taxonomy metabox help links open in a new window.
    Also, some minor CSS tweakings.

--- admin/taxonomy/class-taxonomy-content-fields.php
+++ admin/taxonomy/class-taxonomy-content-fields.php
@@ -17,7 +17,7 @@ class WPSEO_Taxonomy_Content_Fields extends WPSEO_Taxonomy_Fields {
 		$fields = array(
 			'snippet' => $this->get_field_config(
 				__( 'Snippet Editor', 'wordpress-seo' ),
-				sprintf( __( 'This is a rendering of what this post might look like in Google\'s search results. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/snippet-preview/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
+				sprintf( __( 'This is a rendering of what this post might look like in Google\'s search results. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a target="_blank" href="https://yoast.com/snippet-preview/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
 				'div',
 				array(
 					'help-button' => __( 'Snippet Editor Help', 'wordpress-seo' )
@@ -25,7 +25,7 @@ class WPSEO_Taxonomy_Content_Fields extends WPSEO_Taxonomy_Fields {
 			),
 			'focuskw' => $this->get_field_config(
 				__( 'Focus Keyword', 'wordpress-seo' ),
-				sprintf( __( 'Pick the main keyword or keyphrase that this post/page is about. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/focus-keyword/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=focus-keyword">', '</a>' ),
+				sprintf( __( 'Pick the main keyword or keyphrase that this post/page is about. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a target="_blank" href="https://yoast.com/focus-keyword/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=focus-keyword">', '</a>' ),
 				'text',
 				array(
 					'help-button' => __( 'Focus Keyword Help', 'wordpress-seo' )
@@ -33,7 +33,7 @@ class WPSEO_Taxonomy_Content_Fields extends WPSEO_Taxonomy_Fields {
 			),
 			'analysis' => $this->get_field_config(
 				__( 'Content Analysis', 'wordpress-seo' ),
-				sprintf( __( 'This is the content analysis, a collection of content checks that analyze the content of your page. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a href="https://yoast.com/real-time-content-analysis/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
+				sprintf( __( 'This is the content analysis, a collection of content checks that analyze the content of your page. Read %sthis post%s for more info.', 'wordpress-seo' ), '<a target="_blank" href="https://yoast.com/real-time-content-analysis/#utm_source=wordpress-seo-metabox&amp;utm_medium=inline-help&amp;utm_campaign=snippet-preview">', '</a>' ),
 				'div',
 				array(
 					'help-button' => __( 'Content Analysis Help', 'wordpress-seo' )
