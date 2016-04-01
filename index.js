688fa277d8542502a7fdcbe38aedd7f07a1dfcb6
Anton Timmermans <anton@yoast.com>
2016-04-02 01:08:20 +0200

    Add bundled plugin export

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+var plugins = {
+	usedKeywords: require( "./js/bundledPlugins/previouslyUsedKeywords" )
+};
+
 module.exports = {
 	Assessor: require( "./js/assessor" ),
 	App: require( "./js/app.js" ),
 	Pluggable: require( "./js/app" ),
 	Researcher: require( "./js/researcher" ),
-	SnippetPreview: require( "./js/snippetPreview.js" )
+	SnippetPreview: require( "./js/snippetPreview.js" ),
+	bundledPlugins: plugins
 };
