c9c25b254ea747987ecad8299e1d95542efd6350
Andy Meerwaldt <andy@yoast.com>
2016-04-04 14:17:41 +0200

    Remove image urls

--- example/example.js
+++ example/example.js
@@ -3,10 +3,7 @@ var TwitterPreview  = require( "../js/twitterPreview.js" );
 
 var facebookPreview = new FacebookPreview(
 	{
-		targetElement: document.getElementById(  'facebook-container' ),
-		data : {
-			imageUrl : 'http://www.filmtotaal.nl/images/newscontent/j76kpfrclkou_200.jpg'
-		}
+		targetElement: document.getElementById(  'facebook-container' )
 	}
 );
 
@@ -14,10 +11,7 @@ facebookPreview.init();
 
 var twitterPreview = new TwitterPreview(
 	{
-		targetElement: document.getElementById(  'twitter-container' ),
-		data : {
-			imageUrl : 'http://www.filmtotaal.nl/images/newscontent/j76kpfrclkou_200.jpg'
-		}
+		targetElement: document.getElementById(  'twitter-container' )
 	}
 );
 
