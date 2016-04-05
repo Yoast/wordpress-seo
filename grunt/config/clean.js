30e517373f3d252899af19fee6098bf9c98829b8
Anton Timmermans <anton@yoast.com>
2016-04-05 15:41:13 +0200

    Fix GitHub documentation link

--- grunt/config/clean.js
+++ grunt/config/clean.js
@@ -1,4 +1,4 @@
-// https://github.com/stephenharris/grunt-checktextdomain
+// https://github.com/gruntjs/grunt-contrib-clean
 module.exports = {
 	'po-files': [ 'languages/*.po', 'languages/<%= pkg.plugin.textdomain %>-temp.pot', 'languages/yoast-seo.json' ]
 };
