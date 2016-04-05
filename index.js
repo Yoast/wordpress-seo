67f657de83211daa731f9321c279099b4086e038
Anton Timmermans <anton@yoast.com>
2016-04-05 11:30:20 +0200

    Expose score to rating as an API

--- index.js
+++ index.js
@@ -2,11 +2,17 @@ var plugins = {
 	usedKeywords: require( "./js/bundledPlugins/previouslyUsedKeywords" )
 };
 
+var helpers = {
+	scoreToRating: require( "./js/interpreters/scoreToRating" ),
+};
+
 module.exports = {
 	Assessor: require( "./js/assessor" ),
 	App: require( "./js/app.js" ),
 	Pluggable: require( "./js/app" ),
 	Researcher: require( "./js/researcher" ),
 	SnippetPreview: require( "./js/snippetPreview.js" ),
-	bundledPlugins: plugins
+
+	bundledPlugins: plugins,
+	helpers: helpers
 };
