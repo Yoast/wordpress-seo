5ae23abc2151afc44a4a208042c76380178dcba1
Jimmy Comack <jimmy@yoast.com>
2016-04-07 13:34:18 +0200

    Fixed and moved some specs

--- spec/assessorSpec.js
+++ spec/assessorSpec.js
@@ -14,39 +14,11 @@ describe( "create an assessor", function() {
 	it( "creates an assessor", function() {
 		var mockPaper = new Paper( "text" );
 		expect( new Assessor( i18n ) ).toBeDefined();
-		expect( Object.keys( new Assessor( i18n ).getAvailableAssessments() ) ).toContain("textLength");
-		expect( Object.keys( new Assessor( i18n ).getAvailableAssessments() ) ).toContain("fleschReadingEase");
+		expect( Object.keys( new Assessor( i18n ).getAvailableAssessments() ) ).toEqual( [] );
 	});
 });
 
 var assessor = new Assessor( i18n );
-
-describe ( "running assessments in the assessor", function() {
-	it( "runs assessments without any specific requirements", function() {
-		assessor.assess( new Paper( "" ) );
-		expect( assessor.getValidResults().length ).toBe( 5 );
-	});
-
-	it( "additionally runs assessments that only require a text", function() {
-		assessor.assess( new Paper( "text" ) );
-		expect( assessor.getValidResults().length ).toBe( 8 );
-	});
-
-	it( "additionally runs assessments that only require a keyword", function() {
-		assessor.assess( new Paper( "text", { keyword: "keyword" } ) );
-		expect( assessor.getValidResults().length ).toBe( 9 );
-	});
-
-	it( "additionally runs assessments that require text and a keyword", function() {
-		assessor.assess( new Paper( "text", { keyword: "keyword" } ) );
-		expect( assessor.getValidResults().length ).toBe( 9 );
-	});
-
-	it( "additionally runs assessments that require an url", function() {
-		assessor.assess( new Paper( "text", { url: "sample url" } ) );
-		expect( assessor.getValidResults().length ).toBe( 8 );
-	});
-});
 
 var result5 = new AssessmentResult();
 result5.setScore( 5 );
