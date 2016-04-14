24b53e6f17e1a90f65fbed498c8f8ebb8577a568
Andrey Savchenko <contact@rarst.net>
2016-04-14 12:49:42 +0300

    Removed raw SQL in favor of user query for last user modified time.

--- inc/sitemaps/class-author-sitemap-provider.php
+++ inc/sitemaps/class-author-sitemap-provider.php
@@ -35,8 +35,6 @@ class WPSEO_Author_Sitemap_Provider implements WPSEO_Sitemap_Provider {
 	 */
 	public function get_index_links( $max_entries ) {
 
-		global $wpdb;
-
 		$options = WPSEO_Options::get_all();
 
 		if ( $options['disable-author'] || $options['disable_author_sitemap'] ) {
@@ -57,52 +55,45 @@ class WPSEO_Author_Sitemap_Provider implements WPSEO_Sitemap_Provider {
 			$max_pages = (int) ceil( $count / $max_entries );
 		};
 
-		// Must use custom raw query because WP User Query does not support ordering by usermeta.
-		// Retrieve the newest updated profile timestamp overall.
-		// TODO order by usermeta supported since WP 3.7, update implementation? R.
-		$date_query = "
-			SELECT mt1.meta_value
-			FROM $wpdb->users
-				INNER JOIN $wpdb->usermeta ON ($wpdb->users.ID = $wpdb->usermeta.user_id)
-				INNER JOIN $wpdb->usermeta AS mt1 ON ($wpdb->users.ID = mt1.user_id)
-			WHERE 1=1
-				AND (
-					($wpdb->usermeta.meta_key = %s AND CAST($wpdb->usermeta.meta_value AS CHAR) != '0')
-					AND mt1.meta_key = '_yoast_wpseo_profile_updated'
-				)
-			ORDER BY mt1.meta_value
-		";
-
 		$index = array();
 
-		for ( $i = 0; $i < $max_pages; $i++ ) {
-
-			$count = ( $max_pages > 1 ) ? ( $i + 1 ) : '';
-
-			if ( empty( $count ) || $count === $max_pages ) {
-				$sql = $wpdb->prepare(
-					$date_query . ' DESC LIMIT 1',
-					$wpdb->get_blog_prefix() . 'user_level'
-				);
-				// Retrieve the newest updated profile timestamp by an offset.
-			}
-			else {
-				$sql = $wpdb->prepare(
-					$date_query . ' DESC LIMIT 1 OFFSET %d',
-					$wpdb->get_blog_prefix() . 'user_level',
-					( ( $max_entries * ( $i + 1 ) ) - 1 )
-				);
-			}
-
-			$date = $wpdb->get_var( $sql );
+		for ( $page = 1; $page <= $max_pages; $page++ ) {
 
 			$index[] = array(
-				'loc'     => WPSEO_Sitemaps_Router::get_base_url( 'author-sitemap' . $count . '.xml' ),
-				'lastmod' => '@' . $date, // @ for explicit timestamp format
+				'loc'     => WPSEO_Sitemaps_Router::get_base_url( 'author-sitemap' . $page . '.xml' ),
+				'lastmod' => '@' . $this->get_last_modified( $max_entries, $page ), // @ for explicit timestamp format
 			);
 		}
 
 		return $index;
+	}
+
+	/**
+	 * Retrieve profile update time for most recently updated user on a sitemap page.
+	 *
+	 * @param int $max_entries Entries per page of sitemap.
+	 * @param int $page        Current page.
+	 *
+	 * @return int|false Timestamp of last profile update or false on failure.
+	 */
+	protected function get_last_modified( $max_entries, $page ) {
+
+		$users = get_users( array(
+			'who'      => 'authors',
+			'number'   => 1, // We sort time descending, so first on the page is most recent.
+			'offset'   => ( $page - 1 ) * $max_entries,
+			'meta_key' => '_yoast_wpseo_profile_updated',
+			'orderby'  => 'meta_value_num',
+			'order'    => 'DESC',
+		) );
+
+		if ( empty( $users ) ) {
+			return false;
+		}
+
+		$user = array_pop( $users );
+
+		return $user->_yoast_wpseo_profile_updated;
 	}
 
 	/**
@@ -130,7 +121,6 @@ class WPSEO_Author_Sitemap_Provider implements WPSEO_Sitemap_Provider {
 			$offset = ( ( $current_page - 1 ) * $max_entries );
 		}
 
-		// Query for users with this meta.
 		$users = get_users( array(
 			'who'      => 'authors',
 			'offset'   => $offset,
