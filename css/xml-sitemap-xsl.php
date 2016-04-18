15d8d1e3299eb45ffdc113948d7a3ddeb03f6b10
Andrey Savchenko <contact@rarst.net>
2016-04-18 16:03:04 +0300

    Added time zone to displayed sitemap data.

--- css/xml-sitemap-xsl.php
+++ css/xml-sitemap-xsl.php
@@ -113,7 +113,7 @@ $xsl = '<?xml version="1.0" encoding="UTF-8"?>
 										<a href="{$sitemapURL}"><xsl:value-of select="sitemap:loc"/></a>
 									</td>
 									<td>
-										<xsl:value-of select="concat(substring(sitemap:lastmod,0,11),concat(\' \', substring(sitemap:lastmod,12,5)))"/>
+										<xsl:value-of select="concat(substring(sitemap:lastmod,0,11),concat(\' \', substring(sitemap:lastmod,12,5)),concat(\' \', substring(sitemap:lastmod,20,6)))"/>
 									</td>
 								</tr>
 							</xsl:for-each>
@@ -128,11 +128,11 @@ $xsl = '<?xml version="1.0" encoding="UTF-8"?>
 						<table id="sitemap" cellpadding="3">
 							<thead>
 								<tr>
-									<th width="75%">URL</th>
+									<th width="70%">URL</th>
 									<th title="Index Priority" width="5%">Prio</th>
 									<th width="5%">Images</th>
 									<th title="Change Frequency" width="5%">Ch. Freq.</th>
-									<th title="Last Modification Time" width="10%">Last Mod.</th>
+									<th title="Last Modification Time" width="15%">Last Mod.</th>
 								</tr>
 							</thead>
 							<tbody>
@@ -158,7 +158,7 @@ $xsl = '<?xml version="1.0" encoding="UTF-8"?>
 											<xsl:value-of select="concat(translate(substring(sitemap:changefreq, 1, 1),concat($lower, $upper),concat($upper, $lower)),substring(sitemap:changefreq, 2))"/>
 										</td>
 										<td>
-											<xsl:value-of select="concat(substring(sitemap:lastmod,0,11),concat(\' \', substring(sitemap:lastmod,12,5)))"/>
+											<xsl:value-of select="concat(substring(sitemap:lastmod,0,11),concat(\' \', substring(sitemap:lastmod,12,5)),concat(\' \', substring(sitemap:lastmod,20,6)))"/>
 										</td>
 									</tr>
 								</xsl:for-each>
