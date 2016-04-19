ac5635a254d579eb45d1aa6d9b692b7f71b555f1
terw-dan <danny@yoast.com>
2016-04-19 09:46:12 +0200

    Update text in test

--- spec/assessments/getSubheadingLengthSpec.js
+++ spec/assessments/getSubheadingLengthSpec.js
@@ -19,11 +19,11 @@ describe( "An assessment for finding the lenght of the subheadings.", function()
 	it( "returns headings > 30 chars, 1 too long heading. ", function() {
 		assessment = subHeadingLengthAssessment.getResult( paper, Factory.buildMockResearcher( [5, 5, 35 ] ), i18n );
 		expect( assessment.getScore() ).toBe(  4.777777777777778 );
-		expect( assessment.getText() ).toBe( "You have 1 subheading which contain more than the recommended maximum of 30 characters." );
+		expect( assessment.getText() ).toBe( "You have 1 subheading containing more than the recommended maximum of 30 characters." );
 	} );
 	it( "returns headings > 30 chars. 2 too long headings. ", function() {
 		assessment = subHeadingLengthAssessment.getResult( paper, Factory.buildMockResearcher( [5, 34, 35 ] ), i18n );
 		expect( assessment.getScore() ).toBe( 4.777777777777778 );
-		expect( assessment.getText() ).toBe( "You have 2 subheadings which contain more than the recommended maximum of 30 characters." );
+		expect( assessment.getText() ).toBe( "You have 2 subheadings containing more than the recommended maximum of 30 characters." );
 	} );
 });
