6a7da65cf31fe34972e0d902c1de44ff4acd3d04
Anton Timmermans <email@atimmer.com>
2016-04-19 14:18:02 +0200

    Put Facebook in a placeholder

--- example/yoast-social-previews.js
+++ example/yoast-social-previews.js
@@ -599,9 +599,15 @@ FacebookPreview.prototype.imageTooSmall = function() {
 	this.removeImageClasses();
 
 	if ( this.data.imageUrl === '' ) {
-		message = this.i18n.dgettext( "yoast-social-previews", "We are unable to detect an image in your post that is large enough to be displayed on Facebook. We advise you to select a Facebook image that fits the recommended image size." )
+		message = this.i18n.sprintf(
+			this.i18n.dgettext( "yoast-social-previews", "We are unable to detect an image in your post that is large enough to be displayed on Facebook. We advise you to select a %1$s image that fits the recommended image size." ),
+			"Facebook"
+		)
 	} else {
-		message = this.i18n.dgettext( "yoast-social-previews", "The image you selected is too small for Facebook" );
+		message = this.i18n.sprintf(
+			this.i18n.dgettext( "yoast-social-previews", "The image you selected is too small for %1$s" ),
+			"Facebook"
+		);
 	}
 
 	imagePlaceholder(
