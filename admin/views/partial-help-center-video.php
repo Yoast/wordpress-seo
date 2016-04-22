3ed84bb4cfe401424d4a71c3ab5229caaf722f63
Jip Moors <jhp.moors@gmail.com>
2016-04-22 11:34:51 +0200

    Only hide Premium but show Training CTA (cherry picked from commit 1a9083b)

--- admin/views/partial-help-center-video.php
+++ admin/views/partial-help-center-video.php
@@ -17,12 +17,12 @@ if ( ! empty( $tab_video_url ) ) :
 	<div class="wpseo-tab-video__panel wpseo-tab-video__panel--video">
 		<div class="wpseo-tab-video__data" data-url="<?php echo $tab_video_url ?>"></div>
 	</div>
-	<?php
+	<div class="wpseo-tab-video__panel wpseo-tab-video__panel--text">
+		<?php
 
-	// Don't show for Premium.
-	if ( ! defined( 'WPSEO_PREMIUM_PLUGIN_FILE' ) ) :
-		?>
-		<div class="wpseo-tab-video__panel wpseo-tab-video__panel--text">
+		// Don't show for Premium.
+		if ( ! defined( 'WPSEO_PREMIUM_PLUGIN_FILE' ) ) :
+			?>
 			<div class="wpseo-tab-video__panel__textarea">
 				<h3><?php _e( 'Need more help?', 'wordpress-seo' ); ?></h3>
 				<?php /* translators: %s expands to Yoast SEO Premium */ ?>
@@ -32,18 +32,20 @@ if ( ! empty( $tab_video_url ) ) :
 				      target="_blank"><?php printf( __( 'Get %s &raquo;', 'wordpress-seo' ), 'Yoast SEO Premium' ); ?></a>
 				</p>
 			</div>
-			<div class="wpseo-tab-video__panel__textarea">
-				<?php /* translators: %s expands to Yoast SEO */ ?>
-				<h3><?php printf( __( 'Want to be a %s Expert?', 'wordpress-seo' ), 'Yoast SEO' ); ?></h3>
-				<?php /* translators: %$1s expands to Yoast SEO */ ?>
-				<p><?php printf( __( 'Follow our %1$s for WordPress training and become a certified %1$s Expert!', 'wordpress-seo' ), 'Yoast SEO' ); ?></p>
-				<?php /* translators: %s expands to Yoast SEO for WordPress */ ?>
-				<p><a href="https://yoa.st/wordpress-training-vt"
-				      target="_blank"><?php printf( __( 'Enroll in the %s training &raquo;', 'wordpress-seo' ), 'Yoast SEO for WordPress' ); ?></a>
-				</p>
-			</div>
+			<?php
+		endif;
+		?>
+		<div class="wpseo-tab-video__panel__textarea">
+			<?php /* translators: %s expands to Yoast SEO */ ?>
+			<h3><?php printf( __( 'Want to be a %s Expert?', 'wordpress-seo' ), 'Yoast SEO' ); ?></h3>
+			<?php /* translators: %$1s expands to Yoast SEO */ ?>
+			<p><?php printf( __( 'Follow our %1$s for WordPress training and become a certified %1$s Expert!', 'wordpress-seo' ), 'Yoast SEO' ); ?></p>
+			<?php /* translators: %s expands to Yoast SEO for WordPress */ ?>
+			<p><a href="https://yoa.st/wordpress-training-vt"
+			      target="_blank"><?php printf( __( 'Enroll in the %s training &raquo;', 'wordpress-seo' ), 'Yoast SEO for WordPress' ); ?></a>
+			</p>
 		</div>
-		<?php
-	endif;
+	</div>
+	<?php
 
 endif;
