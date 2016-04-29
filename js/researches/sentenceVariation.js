e40cc7939ce94f382b9a66b68881db268132c68c
Andy Meerwaldt <andy@yoast.com>
2016-04-29 10:26:06 +0200

    Add documentation.

--- js/researches/sentenceVariation.js
+++ js/researches/sentenceVariation.js
@@ -2,6 +2,12 @@ var getSentences = require( "../stringProcessing/getSentences.js" );
 var sentencesLength = require( "../stringProcessing/sentencesLength.js" );
 var sum = require( "lodash/sum" );
 
+/**
+ * Calculates the standard deviation of a text
+ *
+ * @param {Paper} paper the Paper object to use in this count.
+ * @returns {number} The calculated standard deviation
+ */
 module.exports = function( paper ){
 	var text = paper.getText();
 	var sentences = getSentences( text );
@@ -10,9 +16,9 @@ module.exports = function( paper ){
 	var totalWords = sum( wordCountPerSentence );
 	var average = totalWords / totalSentences;
 	var variation;
-	sentences.map( function( sentence ){
+	sentences.map( function( sentence ) {
 		variation = wordCountPerSentence - average;
-		 return Math.pow( variation , 2 );
+		return Math.pow( variation , 2 );
 	} );
 
 	var totalOfSquares = sum( sentences );
