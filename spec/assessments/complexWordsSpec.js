c8e71a577582feffd30c9d312ef30647328cba13
terw-dan <danny@yoast.com>
2016-04-29 13:18:44 +0200

    Updates spec after fixen floating point bug

--- spec/assessments/complexWordsSpec.js
+++ spec/assessments/complexWordsSpec.js
@@ -14,8 +14,7 @@ describe( "an assessment returning complex words", function() {
 	it( "runs a test with exactly 10% too many syllables", function(){
 		var mockPaper = new Paper( "" );
 		var result = wordComplexityAssessment.getResult( mockPaper, Factory.buildMockResearcher( [ 1, 2, 3, 1, 3, 3, 1, 2, 1, 10 ] ), i18n );
-		//floating point bug
-		expect( result.getScore() ).toBe( 7.0200000000000005 );
+		expect( result.getScore() ).toBe( 7.02 );
 		expect( result.getText() ).toBe( "10% of the words contain over 4 syllables, which is within the recommended range." );
 	} );
 
