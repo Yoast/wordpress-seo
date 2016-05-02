a181046118af46c7626f4a620119ea519f735fca
terw-dan <danny@yoast.com>
2016-05-02 15:44:46 +0200

    Adds passive voice assessment

--- js/researches/passivevoice.js
+++ js/researches/passivevoice.js
@@ -1,1 +1,61 @@
+var getSentences = require( "../stringProcessing/getSentences.js" );
+var arrayToRegex = require( "../stringProcessing/createRegexFromArray.js" );
 
+var verbEndingEd = require( "../language/en_US/verb-ending-ed.js" )();
+var verbEndingIng = require( "../language/en_US/verb-ending-ing.js" )();
+var auxiliaries = require( "../language/en_us/auxiliaries.js" )();
+
+var filter = require( "lodash/filter");
+var isUndefined = require( "lodash/isUndefined" );
+var isEmpty = require( "lodash/isEmpty" );
+
+module.exports = function( paper) {
+	var text = paper.getText();
+	var sentences = getSentences( text );
+
+	//var verb_edRegex = arrayToRegex( verbEndingEd );
+
+	var auxiliaryRegex = arrayToRegex( auxiliaries );
+
+	// Matches all words ending in ED or ING.
+	var firstSortRegex = new RegExp( "([a-z]+)ed[ \n\r\t\.,'\(\)\"\+\-;!?:\/»«‹›<>]", "ig" );
+
+	var possibleMatches = [];
+	var matches;
+	var passiveCount = 0;
+
+	// First check if there is an auxiliary word in the sentence
+	sentences.map( function( sentence ) {
+		if( sentence.match( auxiliaryRegex ) !== null ) {
+			possibleMatches.push( sentence );
+		}
+	} );
+
+	//split op aux, dan match op -ed, match -ed in nonverb-ed list
+	// stoppen op andere aux of stopword
+	//
+
+	possibleMatches.map( function( sentence ) {
+
+		// todo needs proper regex.
+		var parts = sentence.split( auxiliaryRegex );
+		parts.shift();
+
+		parts = parts.filter( function( part ) {
+			return ! isUndefined( part ) && part !== " ";
+		} );
+
+		parts = parts.filter( function( part ) {
+			return part.match( auxiliaryRegex) === null
+		} );
+
+		if( parts.length > 0 ) {
+			parts.map( function( subSentence ){
+				console.log( subSentence );
+				var matches = subSentence.match( firstSortRegex );
+				console.log( matches );
+			} );
+		}
+	} );
+	return passiveCount;
+};
--- spec/researches/passiveVoiceSpec.js
+++ spec/researches/passiveVoiceSpec.js
@@ -1,1 +1,37 @@
+var passiveVoice = require( "../../js/researches/passiveVoice.js" );
+var Paper = require( "../../js/values/Paper.js" );
 
+var paper;
+
+describe( "detecting passive voice in sentences", function() {
+	it( "returns passive voice in  1 sentence", function() {
+		paper = new Paper( "he was abducted by aliens" );
+		expect( passiveVoice( paper) ).toBe( 1 );
+	} );
+	it( "returns no passive", function(){
+		paper = new Paper( "Andy drives to work" );
+		expect( passiveVoice( paper) ).toBe( 0 );
+	} );
+	it( "returns passive", function(){
+		paper = new Paper( "The car was repaired by Andy" );
+		expect( passiveVoice( paper) ).toBe( 1 );
+	} );
+	it( "returns no passive", function(){
+		paper = new Paper( "My mother would always make the fries" );
+		expect( passiveVoice( paper) ).toBe( 0 );
+	} )
+	it( "returns no passive", function(){
+		paper = new Paper( "The bills used to be paid by Jerry" );
+		expect( passiveVoice( paper) ).toBe( 1 );
+	} )
+	it( "returns no passive", function(){
+		paper = new Paper( "Andy is going to make a beautiful dinner tonight" );
+		expect( passiveVoice( paper) ).toBe( 1 );
+	} )
+
+	it( "returns no passive", function(){
+		paper = new Paper( "Andy is going to make a beautiful dinner tonight, he is" );
+		expect( passiveVoice( paper) ).toBe( 1 );
+	} )
+
+} );
