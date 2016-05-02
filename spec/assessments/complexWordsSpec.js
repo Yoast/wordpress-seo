3e7bd81912a5000c5b2de6c866bb0eba3740c6ff
terw-dan <danny@yoast.com>
2016-05-02 09:27:12 +0200

    Adds . to text

--- spec/assessments/complexWordsSpec.js
+++ spec/assessments/complexWordsSpec.js
@@ -8,21 +8,21 @@ describe( "an assessment returning complex words", function() {
 		var mockPaper = new Paper( "" );
 		var result = wordComplexityAssessment.getResult( mockPaper, Factory.buildMockResearcher( [ 1, 2, 3, 4, 3, 3, 4, 2, 1, 10 ] ), i18n );
 		expect( result.getScore() ).toBe( 3 );
-		expect( result.getText() ).toBe( "30% of the words contain over 4 syllables, which is more than the recommended maximum of 10%" );
+		expect( result.getText() ).toBe( "30% of the words contain over 4 syllables, which is more than the recommended maximum of 10%." );
 	} );
 
 	it( "runs a test with exactly 10% too many syllables", function(){
 		var mockPaper = new Paper( "" );
 		var result = wordComplexityAssessment.getResult( mockPaper, Factory.buildMockResearcher( [ 1, 2, 3, 1, 3, 3, 1, 2, 1, 10 ] ), i18n );
 		expect( result.getScore() ).toBe( 7.02 );
-		expect( result.getText() ).toBe( "10% of the words contain over 4 syllables, which is less than or equal to the recommended maximum of 10%" );
+		expect( result.getText() ).toBe( "10% of the words contain over 4 syllables, which is less than or equal to the recommended maximum of 10%." );
 	} );
 
 	it( "runs a test with 0% too many syllables", function(){
 		var mockPaper = new Paper( "" );
 		var result = wordComplexityAssessment.getResult( mockPaper, Factory.buildMockResearcher( [ 1, 2, 3, 1, 3, 3, 1, 2, 1, 1 ] ), i18n );
 		expect( result.getScore() ).toBe( 9 );
-		expect( result.getText() ).toBe( "0% of the words contain over 4 syllables, which is less than or equal to the recommended maximum of 10%" );
+		expect( result.getText() ).toBe( "0% of the words contain over 4 syllables, which is less than or equal to the recommended maximum of 10%." );
 	} )
 
 } );
