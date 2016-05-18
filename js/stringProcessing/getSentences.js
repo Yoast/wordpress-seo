035693dd3027e1ae8a66c4e281c39d08608376c6
terw-dan <danny@yoast.com>
2016-05-18 13:42:42 +0200

    Improves sentence detection

--- js/stringProcessing/getSentences.js
+++ js/stringProcessing/getSentences.js
@@ -1,6 +1,103 @@
-var cleanText = require( "../stringProcessing/cleanText.js" );
+var stripSpaces = require( "../stringProcessing/stripSpaces.js" );
+
 var filter = require( "lodash/filter" );
 var isEmpty = require( "lodash/isEmpty" );
+var isUndefined = require( "lodash/isUndefined" );
+var difference = require( "lodash/difference" );
+var forEach = require( "lodash/forEach" );
+var isNaN = require( "lodash/isNan" );
+
+/**
+ * Checks if the period is followed with a whitespace. If not, it is no ending of a sentence.
+ *
+ * @param {string} text The text to split in sentences
+ * @param {number} index The current index to look for.
+ * @returns {boolean} True if it doesn't match a whitespace.
+ */
+var inValidateOnWhiteSpace = function( text, index ) {
+	return text.substring( index + 1 ).match( /\s/ ) === null;
+};
+
+/**
+ * Checks if the character in the next sentence is a capital letter or a number. If not, this period is not the end
+ * of a sentence.
+ *
+ * @param {string} text The text to split in sentences
+ * @param {Array} indices The array with indices of periods in the text.
+ * @param {number} i The current iterator.
+ * @returns {boolean} False if it doesn't match a capital.
+ */
+var inValidateOnCapital = function( text, indices, i ) {
+	var firstChar = text.substring( indices[ i ] + 2, indices[ i  ] + 3 );
+	if( firstChar === firstChar.toLocaleLowerCase() && isNaN( parseInt( firstChar ) ) ) {
+		return true;
+	}
+};
+
+/**
+ * Filters the indices that aren't valid endings to sentences.
+ * @param {string} text The text to split in sentences.
+ * @param {Array} indices The array with indices of periods in the text.
+ * @returns {Array} The filtered indices.
+ */
+var filterIndices = function( text, indices ) {
+	var filteredIndices = [];
+	for ( var i = 0; i < indices.length; i++ ) {
+		if( !isUndefined( indices[ i + 1 ] ) ) {
+			if ( inValidateOnWhiteSpace( text, indices[ i ] ) ) {
+				filteredIndices.push( indices[ i ] );
+			}
+			if ( inValidateOnCapital( text, indices, i ) ) {
+				filteredIndices.push( indices[ i ] );
+			}
+		}
+	}
+
+	// Returns the difference between the indices, and the filtered indices, being the correct indices.
+	return difference( indices, filteredIndices );
+};
+
+/**
+ * Split sentences based on index.
+ *
+ * @param {Array} indices The array with indices of periods in the text.
+ * @param {string} text The text to split in sentences.
+ * @returns {Array} Array with sentences.
+ */
+var splitOnIndex = function( indices, text ) {
+	var curIndex = 0, sentences = [];
+
+	forEach( indices, function( index ) {
+		sentences.push( text.substring( curIndex, index ) );
+
+		// Adds 1 to skip the period.
+		curIndex = index + 1;
+	} );
+	return sentences;
+};
+
+/**
+ * Unifies terminators and strip spaces
+ *
+ * @param {String} text The string to clean.
+ * @returns {String} The cleaned text.
+ */
+var cleanText = function( text ) {
+	if( text === "" ) {
+		return text;
+	}
+
+	// Unify all terminators.
+	text = text.replace( /[.?!]/g, "." );
+
+	// Add period in case it is missing.
+	text += ".";
+
+	// Remove double spaces
+	text = stripSpaces( text );
+
+	return text;
+};
 
 /**
  * Returns sentences in a string.
@@ -8,9 +105,18 @@ var isEmpty = require( "lodash/isEmpty" );
  * @returns {Array} Sentences found in the text.
  */
 module.exports = function( text ) {
-	var cleanedText = cleanText( text );
-	var sentences = cleanedText.split( "." );
+	text = cleanText( text );
+
+	var indices = [];
+	var periodIndex = text.indexOf( "." );
+	while( periodIndex > -1 ) {
+		indices.push( periodIndex );
+		periodIndex = text.indexOf( ".", periodIndex + 1 );
+	}
+	indices = filterIndices( text, indices );
+	var sentences = splitOnIndex( indices, text );
+
 	return filter( sentences, function( sentence ) {
-		return(  !isEmpty( sentence ) );
+		return( !isEmpty( sentence ) );
 	} );
 };
