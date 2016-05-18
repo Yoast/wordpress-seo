6e56554628020f9339cd0988b2ec45bbadcd0a0f
Jimmy Comack <jimmy@yoast.com>
2016-05-18 16:52:40 +0200

    Implemented a marker for sentence length

--- js/assessments/sentenceLengthInTextAssessment.js
+++ js/assessments/sentenceLengthInTextAssessment.js
@@ -3,19 +3,46 @@ var countTooLongSentences = require( "./../assessmentHelpers/checkForTooLongSent
 var calculateTooLongSentences = require( "./../assessmentHelpers/sentenceLengthPercentageScore.js" );
 var fixFloatingPoint = require( "../helpers/fixFloatingPoint.js" );
 
+var Mark = require( "../values/Mark.js" );
+var marker = require( "../renderers/marker.js" );
+
+var map = require( "lodash/map" );
+
+var recommendedValue = 20;
+var maximumPercentage = 25;
+
 /**
- * Calculates sentence length score
- * @param {array} sentences The array containing sentences.
- * @param {object} i18n The object used for translations.
- * @returns {object} Object containing score and text.
+ * Gets the sentences that are qualified as being too long.
+ *
+ * @param {Object} sentences The sentences to filter through.
+ * @param {Number} recommendedValue The recommended number of words.
+ * @returns {Array} Array with all the sentences considered to be too long.
+ */
+var tooLongSentences = function( sentences, recommendedValue ) {
+	return countTooLongSentences( sentences, recommendedValue );
+};
+
+/**
+ * Get the total amount of sentences that are qualified as being too long.
+ *
+ * @param {Object} sentences The sentences to filter through.
+ * @param {Number} recommendedValue The recommended number of words.
+ * @returns {Number} The amount of sentences that are considered too long.
+ */
+var tooLongSentencesTotal = function( sentences, recommendedValue ) {
+	return tooLongSentences( sentences, recommendedValue ).length;
+};
+
+/**
+ * Calculates sentence length score.
+ *
+ * @param {Object} sentences The object containing sentences and its length.
+ * @param {Object} i18n The object used for translations.
+ * @returns {Object} Object containing score and text.
  */
 var calculateSentenceLengthResult = function( sentences, i18n ) {
-	var recommendedValue = 20;
-	var maximumPercentage = 25;
-
-	var tooLong = countTooLongSentences( sentences, recommendedValue );
-	var percentage = fixFloatingPoint( ( tooLong / sentences.length ) * 100 );
-
+	var tooLongTotal = tooLongSentencesTotal( sentences, recommendedValue );
+	var percentage = fixFloatingPoint( ( tooLongTotal / sentences.length ) * 100 );
 	var score = calculateTooLongSentences( percentage );
 
 	if ( score >= 7 ) {
@@ -28,6 +55,7 @@ var calculateSentenceLengthResult = function( sentences, i18n ) {
 				"which is less than the recommended maximum of %2$s." ), percentage + "%", maximumPercentage + "%", recommendedValue )
 		};
 	}
+
 	return {
 		score: score,
 
@@ -38,6 +66,7 @@ var calculateSentenceLengthResult = function( sentences, i18n ) {
 			percentage + "%", maximumPercentage + "%", recommendedValue )
 	};
 };
+
 /**
  * Scores the percentage of sentences including more than the recommended number of words.
  *
@@ -47,7 +76,7 @@ var calculateSentenceLengthResult = function( sentences, i18n ) {
  * @returns {object} The Assessmentresult
  */
 var sentenceLengthInTextAssessment = function( paper, researcher, i18n ) {
-	var sentenceCount= researcher.getResearch( "countSentencesFromText" );
+	var sentenceCount = researcher.getResearch( "countSentencesFromText" );
 	var sentenceResult = calculateSentenceLengthResult( sentenceCount, i18n );
 	var assessmentResult = new AssessmentResult();
 
@@ -57,10 +86,31 @@ var sentenceLengthInTextAssessment = function( paper, researcher, i18n ) {
 	return assessmentResult;
 };
 
+/**
+ * Mark the sentences.
+ *
+ * @param {Paper} paper The paper to use for the marking.
+ * @param {Researcher} researcher The researcher to use.
+ * @returns {Array} Array with all the marked sentences.
+ */
+var sentenceLengthMarker = function( paper, researcher ) {
+	var sentenceCount = researcher.getResearch( "countSentencesFromText" );
+	var sentences = tooLongSentences( sentenceCount, recommendedValue );
+
+	return map( sentences, function( sentence ) {
+		var marked = marker( sentence );
+		return new Mark( {
+			original: sentence,
+			marked: marked
+		} );
+	} );
+};
+
 module.exports = {
 	identifier: "textSentenceLength",
 	getResult: sentenceLengthInTextAssessment,
 	isApplicable: function( paper ) {
 		return paper.hasText();
-	}
+	},
+	getMarks: sentenceLengthMarker
 };
