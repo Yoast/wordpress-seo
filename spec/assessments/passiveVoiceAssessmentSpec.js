375d029f7934731508c36346e5ee1bbae15acb7b
terw-dan <danny@yoast.com>
2016-05-19 10:37:48 +0200

    Updates specs

--- spec/assessments/passiveVoiceAssessmentSpec.js
+++ spec/assessments/passiveVoiceAssessmentSpec.js
@@ -6,36 +6,37 @@ var i18n = Factory.buildJed();
 var paper = new Paper();
 describe( "An assessment for scoring passive voice.", function() {
 	it( "scores 1 passive sentence - 5%", function() {
-		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 20, passives: 1 } ), i18n );
+		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 20, passives: [ 1 ]} ), i18n );
 		expect( assessment.getScore() ).toBe( 9 );
 		expect( assessment.getText() ).toBe( "5% of the sentences is written in the passive voice, which is within the recommended range." );
 	} );
 
 	it( "scores 2 passive sentences - 10%", function() {
-		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 20, passives: 2 } ), i18n );
+		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 20, passives: [ 1, 2 ] } ), i18n );
 		expect( assessment.getScore() ).toBe( 7.02 );
 		expect( assessment.getText() ).toBe( "10% of the sentences is written in the passive voice, which is within the recommended range." );
 	} );
 
 	it( "scores 10 passive sentence - 50%", function() {
-		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 20, passives: 10 } ), i18n );
+		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 20, passives: [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] } ), i18n );
 		expect( assessment.getScore() ).toBe( 3 );
 		expect( assessment.getText() ).toBe( "50% of the sentences is written in the passive voice, which is more than the recommended maximum of 10%. " +
 			"Try to use their active counterparts." );
 	} );
 
 	it( "scores 5 passive sentences - 25%", function() {
-		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 20, passives: 5 } ), i18n );
+		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 20, passives: [ 1, 2, 3, 4, 5 ] } ), i18n );
 		expect( assessment.getScore() ).toBe( 3 );
 		expect( assessment.getText() ).toBe( "25% of the sentences is written in the passive voice, which is more than the recommended maximum of 10%. " +
 			"Try to use their active counterparts." );
 	} );
 
 	it( "scores 5 passive sentences - 33%", function() {
-		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 30, passives: 4 } ), i18n );
+		var assessment = passiveVoiceAssessment.getResult( paper, Factory.buildMockResearcher( {total: 30, passives: [ 1, 2, 3, 4 ] } ), i18n );
 		expect( assessment.getScore() ).toBe( 5.02 );
 		expect( assessment.getText() ).toBe( "13.33% of the sentences is written in the passive voice, which is more than the recommended maximum of 10%. " +
 			"Try to use their active counterparts." );
 	} );
-});
 
+	
+});
