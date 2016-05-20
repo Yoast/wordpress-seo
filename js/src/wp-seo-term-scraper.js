b096814c9e48b344424351f41d9ad2d9ac841f99
Andrea Fercia <a.fercia@gmail.com>
2016-05-20 15:18:41 +0200

    Hide and show the editor with CSS instead of JS.

--- js/src/wp-seo-term-scraper.js
+++ js/src/wp-seo-term-scraper.js
@@ -216,28 +216,28 @@ var getDescriptionPlaceholder = require( './analysis/getDescriptionPlaceholder' 
 	};
 
 	/**
-	 * add new descriptionfield to content, creates new element via wp_editor and appends this to the term-description-wrap
-	 * this way we can use the wp tinyMCE editor on the descriptionfield.
+	 * Get the editor created via wp_editor() and append it to the term-description-wrap
+	 * table cell. This way we can use the wp tinyMCE editor on the description field.
 	 */
 	var insertTinyMCE = function() {
 		// Get the table cell that contains the description textarea.
 		var descriptionTd = jQuery( '.term-description-wrap' ).find( 'td' );
-		// Get the textNode from the original textField.
+		// Get the textNode from the original textarea.
 		var textNode = descriptionTd.find( 'textarea' ).val();
-
+		// Get the editor container.
 		var newEditor = document.getElementById( 'wp-description-wrap' );
-
-		if ( null === newEditor ) {
-			return;
-		}
-
-		newEditor.style.display = 'none';
+		// Get the description help text below the textarea.
 		var text = descriptionTd.find( 'p' );
 		// Empty the TD with the old description textarea.
 		descriptionTd.html( '' );
-		// Append the editor and the helptext.
+		/*
+		 * The editor is printed out via PHP as child of the form and initially
+		 * hidden with a child `>` CSS selector. We now move the editor and the
+		 * help text in a new position so the previous CSS rule won't apply any
+		 * longer and the editor will be visible.
+		 */
 		descriptionTd.append( newEditor ).append( text );
-		newEditor.style.display = 'block';
+		// Populate the editor textarea with the original content,
 		document.getElementById('description').value = textNode;
 	};
 
