37398405c121038c6ee0bc1fafb2397ac4726515
terw-dan <danny@yoast.com>
2016-05-23 11:28:15 +0200

    Updates paragraphs too short

--- js/assessments/paragraphTooShortAssessment.js
+++ js/assessments/paragraphTooShortAssessment.js
@@ -3,16 +3,25 @@ var fixFloatingPoint = require( "../helpers/fixFloatingPoint.js" );
 var isParagraphTooShort = require( "../helpers/isValueTooShort" );
 var filter = require( "lodash/filter" );
 
+var recommendedValue = 40;
+
 /**
  * Returns an array containing only the paragraphs shorter than the recommended length.
  * @param {array} paragraphsLength The array containing the lengths of individual paragraphs.
  * @param {number} recommendedValue The recommended minimum length of a paragraph.
  * @returns {number} The number of too short paragraphs.
  */
+var getTooShortParagraphs = function( paragraphsLength  ) {
+	return filter( paragraphsLength, function( paragraph ) {
+		return isParagraphTooShort( recommendedValue, paragraph.wordCount );
+	} );
+};
+
+	/*
 var getTooShortParagraphs = function( paragraphsLength, recommendedValue ) {
 	var tooShortParagraphs = filter( paragraphsLength, isParagraphTooShort.bind( null, recommendedValue ) );
 	return tooShortParagraphs.length;
-};
+};*/
 
 /**
  * Returns the scores and text for the ParagraphTooShortAssessment
@@ -30,7 +39,7 @@ var calculateParagraphLengthResult = function( paragraphsLength, tooShortParagra
 	// 6 is the number of scorepoints between 3, minscore and 9, maxscore. For scoring we use 40 steps, each step is 0.15
 	// Up to 13 is for scoring a 3, higher numbers give higher scores.
 	// floatingPointFix because of js rounding errors
-	var score = 3 + Math.max( Math.min( ( 0.15 ) * ( paragraphsLength[ 0 ] - 13 ), 6 ), 0 );
+	var score = 3 + Math.max( Math.min( ( 0.15 ) * ( paragraphsLength[ 0 ].wordCount - 13 ), 6 ), 0 );
 	score = fixFloatingPoint( score );
 	if ( score >= 7 ) {
 		return {
@@ -45,8 +54,8 @@ var calculateParagraphLengthResult = function( paragraphsLength, tooShortParagra
 		text: i18n.sprintf( i18n.dngettext( "js-text-analysis", "%1$d of the paragraphs contains less than the recommended minimum " +
 				"of %2$d words. Try to expand this paragraph, or connect it to the previous or next paragraph.",
 				"%1$d of the paragraphs contain less than the recommended minimum of %2$d words.  Try to expand these paragraphs, " +
-				"or connect each of them to the previous or next paragraph.", tooShortParagraphs ),
-			tooShortParagraphs, recommendedValue )
+				"or connect each of them to the previous or next paragraph.", tooShortParagraphs.length ),
+			tooShortParagraphs.length, recommendedValue )
 	};
 };
 
@@ -61,10 +70,9 @@ var paragraphLengthAssessment = function( paper, researcher, i18n ) {
 	var paragraphsLength = researcher.getResearch( "getParagraphLength" );
 	paragraphsLength = paragraphsLength.sort(
 		function( a, b ) {
-			return a - b;
+			return a.wordCount - b.wordCount;
 		}
 	);
-	var recommendedValue = 40;
 	var tooShortParagraphs = getTooShortParagraphs( paragraphsLength, recommendedValue );
 	var paragraphLengthResult = calculateParagraphLengthResult( paragraphsLength, tooShortParagraphs, recommendedValue, i18n );
 	var assessmentResult = new AssessmentResult();
