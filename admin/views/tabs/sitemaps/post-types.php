65ed2b0e928622ec1576b645c864e9ffa327a037
Junaid Bhura <info@junaidbhura.com>
2016-05-23 17:47:20 +0530

    Align document arguments

--- admin/views/tabs/sitemaps/post-types.php
+++ admin/views/tabs/sitemaps/post-types.php
@@ -26,7 +26,7 @@ if ( is_array( $post_types ) && $post_types !== array() ) {
 		 * Allow adding custom checkboxes to the admin sitemap page - Post Types tab
 		 *
 		 * @api  Yoast_Form  $yform  The Yoast_Form object
-		 * @api  Object  $pt  The post type
+		 * @api  Object      $pt     The post type
 		 */
 		do_action( 'wpseo_admin_page_sitemap_post_types', $yform, $pt );
 	}
