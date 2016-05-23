22d30803d4e7cd680faa347886b6851e97269431
Junaid Bhura <info@junaidbhura.com>
2016-05-23 09:36:49 +0530

    New hook for admin page meta taxonomies

--- admin/views/tabs/metas/taxonomies.php
+++ admin/views/tabs/metas/taxonomies.php
@@ -42,6 +42,13 @@ if ( is_array( $taxonomies ) && $taxonomies !== array() ) {
 					/* translators: %1$s expands to Yoast SEO */
 				), sprintf( __( '%1$s Meta Box', 'wordpress-seo' ), 'Yoast SEO' ) );
 		}
+		/**
+		 * Allow adding a custom checkboxes to the admin meta page - Taxonomies tab
+		 *
+		 * @api  WPSEO_Admin_Pages  $yform  The WPSEO_Admin_Pages object
+		 * @api  Object  $tax  The taxonomy
+		 */
+		do_action( 'wpseo_admin_page_meta_taxonomies', $yform, $tax );
 		echo '<br/><br/>';
 		echo '</div>';
 	}
