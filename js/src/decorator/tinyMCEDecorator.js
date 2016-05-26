4ffae1bbc75b343b66d556667180179031a2bf30
Anton Timmermans <email@atimmer.com>
2016-05-26 13:16:42 +0200

    Don't save yoastmark elements to the database
    We achieve this by marking the elements as mce-bogus, this is a special
    tinyMCE attribute that makes tinyMCE see the elements as bogus which
    isn't part of the actual content of the post.
    
    This also means that when the user switches to the text tab the marks
    are removed.

--- js/src/decorator/tinyMCEDecorator.js
+++ js/src/decorator/tinyMCEDecorator.js
@@ -12,14 +12,26 @@ var removeMarks = require( 'yoastseo/js/markers/removeMarks' );
  * @param {Array.<Mark>} marks The marks to show in the editor.
  */
 function markTinyMCE( editor, paper, marks ) {
+	var dom = editor.dom;
 	var html = editor.getContent();
 	html = removeMarks( html );
 
+	// Generate marked HTML.
 	_forEach( marks, function( mark ) {
 		html = mark.applyWithReplace( html );
 	});
 
+	// Replace the contents in the editor with the marked HTML.
 	editor.setContent( html );
+
+	var markElements = dom.select( "yoastmark" );
+	/*
+	 * The `mce-bogus` data is an internal tinyMCE indicator that the elements themselves shouldn't be saved.
+	 * Add data-mce-bogus after the elements have been inserted because setContent strips elements with data-mce-bogus.
+	 */
+	_forEach( markElements, function( markElement ) {
+		markElement.setAttribute( 'data-mce-bogus', '1' );
+	} );
 }
 
 /**
