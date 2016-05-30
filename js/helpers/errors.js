51f8dca7cec1a6468716441cf344bb7995727b5c
Anton Timmermans <email@atimmer.com>
2016-05-30 12:14:22 +0200

    Catch exception caught by assessments

---
+++ js/helpers/errors.js
@@ -0,0 +1,23 @@
+var isUndefined = require( "lodash/isUndefined" );
+
+/**
+ * Shows and error trace of the error message in the console if the console is available.
+ *
+ * @param {string} [errorMessage=""] The error message.
+ */
+function showTrace( errorMessage ) {
+	if ( isUndefined( errorMessage ) ) {
+		errorMessage = "";
+	}
+
+	if (
+		!isUndefined( console ) &&
+		!isUndefined( console.trace )
+	) {
+		console.trace( errorMessage );
+	}
+}
+
+module.exports = {
+	showTrace: showTrace
+};
