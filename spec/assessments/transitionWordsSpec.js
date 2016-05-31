972f524051e9e0cec2c2c0da524ae5e21581d0c8
IreneStr <irene@yoast.com>
2016-05-31 15:40:45 +0200

    Remove upper limit transition words

--- spec/assessments/transitionWordsSpec.js
+++ spec/assessments/transitionWordsSpec.js
@@ -7,7 +7,7 @@ describe( "An assessment for transition word percentage", function(){
 	it( "returns the score for 25.0% sentences with transition words", function(){
 		var mockPaper = new Paper();
 		var assessment = transitionWordsAssessment.getResult( mockPaper, Factory.buildMockResearcher( { totalSentences: 4,
-			transitionWordSentences: 1} ), i18n );
+			transitionWordSentences: 1 } ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 9 );
 		expect( assessment.getText() ).toEqual ( "25.0% of the sentences contain a transition word or phrase, which is great." );
@@ -16,17 +16,16 @@ describe( "An assessment for transition word percentage", function(){
 	it( "returns the score for 33.3% sentences with transition words", function(){
 		mockPaper = new Paper();
 		assessment = transitionWordsAssessment.getResult( mockPaper, Factory.buildMockResearcher( { totalSentences: 3,
-			transitionWordSentences: 1} ), i18n );
+			transitionWordSentences: 1 } ), i18n );
 
-		expect( assessment.getScore() ).toEqual( 5.020000000000003 );
-		expect( assessment.getText() ).toEqual ( "33.3% of the sentences contain a transition word or phrase, " +
-			"which is more than the recommended maximum of 30%." );
+		expect( assessment.getScore() ).toEqual( 9 );
+		expect( assessment.getText() ).toEqual ( "33.3% of the sentences contain a transition word or phrase, which is great." );
 		expect( assessment.hasMarks() ).toBe( true );
 	} );
 	it( "returns the score for 16.7% sentences with transition words", function(){
 		mockPaper = new Paper();
 		assessment = transitionWordsAssessment.getResult( mockPaper, Factory.buildMockResearcher( { totalSentences: 6,
-			transitionWordSentences: 1} ), i18n );
+			transitionWordSentences: 1 } ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 5.019999999999998 );
 		expect( assessment.getText() ).toEqual ( "16.7% of the sentences contain a transition word or phrase, " +
@@ -36,18 +35,17 @@ describe( "An assessment for transition word percentage", function(){
 	it( "returns the score for 27.8% sentences with transition words", function(){
 		mockPaper = new Paper();
 		assessment = transitionWordsAssessment.getResult( mockPaper, Factory.buildMockResearcher( { totalSentences: 18,
-			transitionWordSentences: 5} ), i18n );
-		expect( assessment.getScore() ).toEqual( 8.353333333333332 );
+			transitionWordSentences: 5 } ), i18n );
+		expect( assessment.getScore() ).toEqual( 9 );
 		expect( assessment.getText() ).toEqual ( "27.8% of the sentences contain a transition word or phrase, which is great." );
 		expect( assessment.hasMarks() ).toBe( true );
 	} );
 	it( "returns the score for 90% sentences with transition words", function(){
 		mockPaper = new Paper();
 		assessment = transitionWordsAssessment.getResult( mockPaper, Factory.buildMockResearcher( { totalSentences: 10,
-				transitionWordSentences: 9} ), i18n );
-		expect( assessment.getScore() ).toEqual( 3 );
-		expect( assessment.getText() ).toEqual ( "90.0% of the sentences contain a transition word or phrase, " +
-			"which is more than the recommended maximum of 30%." );
+				transitionWordSentences: 9 } ), i18n );
+		expect( assessment.getScore() ).toEqual( 9 );
+		expect( assessment.getText() ).toEqual ( "90.0% of the sentences contain a transition word or phrase, which is great." );
 		expect( assessment.hasMarks() ).toBe( true );
 	} );
 	it( "is not applicable for empty papers", function(){
