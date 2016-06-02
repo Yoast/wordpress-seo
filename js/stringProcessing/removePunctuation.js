1f0900830dfa6e0f5036faf801af53458f20eeae
Anton Timmermans <email@atimmer.com>
2016-06-02 13:49:04 +0200

    Don't create regexes on each function call

--- js/stringProcessing/removePunctuation.js
+++ js/stringProcessing/removePunctuation.js
@@ -1,3 +1,11 @@
+// These are sentence terminators, that never should be in the middle of a word.
+var sentenceTerminators = /[.?!:;,]/g;
+
+// Replace all other punctuation chars at the beginning or at the end of a word.
+var punctuationRegexString = "[\-()_\\[\\]’“”\"'\/]";
+var punctuationRegexStart = new RegExp( "^" + punctuationRegexString );
+var punctuationRegexEnd = new RegExp( punctuationRegexString + "$" );
+
 /**
  * Replaces punctuation chars from the given text string.
  *
@@ -6,14 +14,7 @@
  * @returns {String} The sanitized text.
  */
 module.exports = function( text ) {
-	// These are sentence terminators, that never should be in the middle of a word.
-	text = text.replace( /[.?!:;,]/g, "" );
-
-	// Replace all other punctuation chars at the beginning or at the end of a word.
-	var punctuationRegexString = "[\-()_\\[\\]’“”\"'\/]";
-	var punctuationRegexStart = new RegExp( "^" + punctuationRegexString );
-	var punctuationRegexEnd = new RegExp( punctuationRegexString + "$" );
-
+	text = text.replace( sentenceTerminators, "" );
 	text = text.replace( punctuationRegexStart, "" );
 	text = text.replace( punctuationRegexEnd, "" );
 
