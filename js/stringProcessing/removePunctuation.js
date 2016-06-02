2b996a5cf57f54dff6f8b173a307cb9015d43b1c
Andy Meerwaldt <andy@yoast.com>
2016-06-02 11:27:26 +0200

    Add string processing for removing punctuation.

---
+++ js/stringProcessing/removePunctuation.js
@@ -0,0 +1,21 @@
+/**
+ * Replaces punctuation chars from the given text string.
+ *
+ * @param {String} text The text to remove the punctuation chars for.
+ *
+ * @returns {String} The sanitized text.
+ */
+module.exports = function( text ) {
+	// These are sentence terminators, that never should be in the middle of a word.
+	text = text.replace( /[.?!:;,]/g, '' );
+
+	// Replace all other punctuation chars at the beginning or at the end of a word.
+	var punctuationRegexString = "[\-()_\\[\\]’“”\"'\/]";
+	var punctuationRegexStart = new RegExp( "^" + punctuationRegexString );
+	var punctuationRegexEnd = new RegExp( punctuationRegexString + "$" );
+
+	text = text.replace( punctuationRegexStart, '' );
+	text = text.replace( punctuationRegexEnd, '' );
+
+	return text;
+};
