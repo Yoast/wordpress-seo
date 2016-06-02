5559ed7d9c5ec8a2fb6afed4d38ab8a41d327b10
Andy Meerwaldt <andy@yoast.com>
2016-06-02 14:53:17 +0200

    Add tests for sentence terminators.

--- spec/stringProcessing/removePunctuationSpec.js
+++ spec/stringProcessing/removePunctuationSpec.js
@@ -10,10 +10,6 @@ describe("a test removing punctuation from a string", function(){
 		expect( removePunctuation("-test") ).toBe( "test" );
 	});
 
-	it("returns string without full stops", function(){
-		expect( removePunctuation(".t.e.s.t") ).toBe( "test" );
-	});
-
 	it("returns string with punctuation in middle of word untouched", function(){
 		expect( removePunctuation("te-st") ).toBe( "te-st" );
 	});
