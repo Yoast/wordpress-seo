91e36d9fdb4c626200439b102da7a059fb18b301
Andy Meerwaldt <andy@yoast.com>
2016-06-06 11:13:38 +0200

    Apply codestyle.

--- spec/stringProcessing/getWordsSpec.js
+++ spec/stringProcessing/getWordsSpec.js
@@ -1,21 +1,21 @@
 var getWords = require( "../../js/stringProcessing/getWords" );
 
-describe("a test getting words from a sentence", function(){
+describe( "a test getting words from a sentence", function() {
 
-	it("returns an empty array", function(){
+	it( "returns an empty array", function() {
 		expect( getWords( "" ).length ).toBe( 0 );
 	});
 
-	it("returns an array without html", function(){
-		var words = getWords("<strong>strong</strong> and <em>emphasized</em>");
+	it( "returns an array without html", function() {
+		var words = getWords( "<strong>strong</strong> and <em>emphasized</em>" );
 
 		expect( words[0] ).toBe( "strong" );
 		expect( words[1] ).toBe( "and" );
 		expect( words[2] ).toBe( "emphasized" );
 	});
 
-	it("returns an array without the space comma", function(){
-		var words = getWords("strong , emphasized");
+	it( "returns an array without the space comma", function() {
+		var words = getWords( "strong , emphasized" );
 
 		expect( words[0] ).toBe( "strong" );
 		expect( words[1] ).toBe( "emphasized" );
--- spec/stringProcessing/removeSentenceTerminitorsSpec.js
+++ spec/stringProcessing/removeSentenceTerminitorsSpec.js
@@ -1,17 +1,17 @@
 var removeSentenceTerminators = require( "../../js/stringProcessing/removeSentenceTerminators" );
 
-describe("a test removing sentence terminators from a string", function(){
+describe( "a test removing sentence terminators from a string", function() {
 
-	it("returns string without full stops", function(){
-		expect( removeSentenceTerminators(".t.e.s.t") ).toBe( "test" );
+	it( "returns string without full stops", function() {
+		expect( removeSentenceTerminators( ".t.e.s.t" ) ).toBe( "test" );
 	});
 
-	it("returns string with none terminator in middle of word untouched", function(){
-		expect( removeSentenceTerminators("te-st") ).toBe( "te-st" );
+	it( "returns string with none terminator in middle of word untouched", function() {
+		expect( removeSentenceTerminators( "te-st" ) ).toBe( "te-st" );
 	});
 
-	it("returns string width question marks in the words", function(){
-		expect( removeSentenceTerminators("t?e?st") ).toBe( "test" );
+	it( "returns string width question marks in the words", function() {
+		expect( removeSentenceTerminators( "t?e?st" ) ).toBe( "test" );
 	});
 
 });
