7a42119f9c85115cafd2d3b15027a3cfc2ccba61
Joost de Valk <joost@yoast.com>
2016-06-08 15:07:24 +0200

    remove important mark

--- inc/wpseo-non-ajax-functions.php
+++ inc/wpseo-non-ajax-functions.php
@@ -79,7 +79,7 @@ function wpseo_admin_bar_menu() {
 
 	// Yoast Icon.
 	$icon_svg = WPSEO_Utils::get_icon_svg();
-	$title = '<div id="yoast-ab-icon" class="ab-item yoast-logo svg" style="background-image: url(\''.$icon_svg.'\') !important;"></div>';
+	$title = '<div id="yoast-ab-icon" class="ab-item yoast-logo svg" style="background-image: url(\''.$icon_svg.'\');"></div>';
 
 	$wp_admin_bar->add_menu( array(
 		'id'    => 'wpseo-menu',
