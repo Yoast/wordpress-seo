30de1f05fa7f7eb28b4695c4b604af290d378ee4
Joost de Valk <joost@yoast.com>
2016-06-13 10:33:20 +0200

    code style fix

--- js/contentAssessor.js
+++ js/contentAssessor.js
@@ -1,11 +1,11 @@
-var Assessor = require("./assessor.js");
+var Assessor = require( "./assessor.js" );
 
-var fleschReadingEase = require("./assessments/fleschReadingEaseAssessment.js");
-var paragraphTooLong = require("./assessments/paragraphTooLongAssessment.js");
-var sentenceLengthInText = require("./assessments/sentenceLengthInTextAssessment.js");
-var subheadingDistributionTooLong = require("./assessments/subheadingDistributionTooLongAssessment.js");
-var transitionWords = require("./assessments/transitionWordsAssessment.js");
-var passiveVoice = require("./assessments/passiveVoiceAssessment.js");
+var fleschReadingEase = require( "./assessments/fleschReadingEaseAssessment.js" );
+var paragraphTooLong = require( "./assessments/paragraphTooLongAssessment.js" );
+var sentenceLengthInText = require( "./assessments/sentenceLengthInTextAssessment.js" );
+var subheadingDistributionTooLong = require( "./assessments/subheadingDistributionTooLongAssessment.js" );
+var transitionWords = require( "./assessments/transitionWordsAssessment.js" );
+var passiveVoice = require( "./assessments/passiveVoiceAssessment.js" );
 // var subHeadingLength = require( "./assessments/getSubheadingLengthAssessment.js" );
 // var getSubheadingPresence = require( "./assessments/subheadingPresenceAssessment.js" );
 // var sentenceVariation = require( "./assessments/sentenceVariationAssessment.js" );
@@ -14,12 +14,12 @@ var passiveVoice = require("./assessments/passiveVoiceAssessment.js");
 // var subheadingDistributionTooShort = require( "./assessments/subheadingDistributionTooShortAssessment.js" );
 // var paragraphTooShort = require( "./assessments/paragraphTooShortAssessment.js" );
 // var sentenceLengthInDescription = require( "./assessments/sentenceLengthInDescriptionAssessment.js" );
-var textPresence = require("./assessments/textPresenceAssessment.js");
+var textPresence = require( "./assessments/textPresenceAssessment.js" );
 
-var scoreToRating = require("./interpreters/scoreToRating");
+var scoreToRating = require( "./interpreters/scoreToRating" );
 
-var map = require("lodash/map");
-var sum = require("lodash/sum");
+var map = require( "lodash/map" );
+var sum = require( "lodash/sum" );
 
 /**
  * Creates the Assessor
@@ -30,27 +30,27 @@ var sum = require("lodash/sum");
  *
  * @constructor
  */
-var ContentAssessor = function (i18n, options) {
-    Assessor.call(this, i18n, options);
+var ContentAssessor = function ( i18n, options ) {
+	Assessor.call( this, i18n, options );
 
-    this._assessments = [
-        fleschReadingEase,
-        subheadingDistributionTooLong,
-        paragraphTooLong,
-        sentenceLengthInText,
-        transitionWords,
-        passiveVoice,
-        textPresence
-        // sentenceVariation,
-        // sentenceBeginnings,
-        // wordComplexity,
-        // subheadingDistributionTooShort,
-        // paragraphTooShort
-        // sentenceLengthInDescription,
-    ];
+	this._assessments = [
+		fleschReadingEase,
+		subheadingDistributionTooLong,
+		paragraphTooLong,
+		sentenceLengthInText,
+		transitionWords,
+		passiveVoice,
+		textPresence
+		// sentenceVariation,
+		// sentenceBeginnings,
+		// wordComplexity,
+		// subheadingDistributionTooShort,
+		// paragraphTooShort
+		// sentenceLengthInDescription,
+	];
 };
 
-require("util").inherits(ContentAssessor, Assessor);
+require( "util" ).inherits( ContentAssessor, Assessor );
 
 /**
  * Calculates the negative points based on the assessment results.
@@ -58,31 +58,31 @@ require("util").inherits(ContentAssessor, Assessor);
  * @returns {number} The total negative points for the results.
  */
 ContentAssessor.prototype.calculatePenaltyPoints = function () {
-    var results = this.getValidResults();
+	var results = this.getValidResults();
 
-    var negativePoints = map(results, function (result) {
-        var weight, rating = scoreToRating(result.getScore());
+	var negativePoints = map( results, function ( result ) {
+		var weight, rating = scoreToRating( result.getScore() );
 
-        // Convert the ratings to negative 'points'.
-        switch (rating) {
-            case "bad":
-                weight = 3;
-                break;
+		// Convert the ratings to negative 'points'.
+		switch ( rating ) {
+			case "bad":
+				weight = 3;
+				break;
 
-            case "ok":
-                weight = 2;
-                break;
+			case "ok":
+				weight = 2;
+				break;
 
-            default:
-            case "good":
-                weight = 0;
-                break;
-        }
+			default:
+			case "good":
+				weight = 0;
+				break;
+		}
 
-        return weight;
-    });
+		return weight;
+	} );
 
-    return sum(negativePoints);
+	return sum( negativePoints );
 };
 
 /**
@@ -93,32 +93,31 @@ ContentAssessor.prototype.calculatePenaltyPoints = function () {
  *
  * @private
  */
-ContentAssessor.prototype._ratePenaltyPoints = function (totalPenaltyPoints) {
-    if (this._lastPaper.getLocale().indexOf("en_") > -1) {
-        // Determine the total score based on the total negative points.
-        if (totalPenaltyPoints > 6) {
-            // A red indicator.
-            return 30;
-        }
+ContentAssessor.prototype._ratePenaltyPoints = function ( totalPenaltyPoints ) {
+	if ( this._lastPaper.getLocale().indexOf( "en_" ) > -1 ) {
+		// Determine the total score based on the total negative points.
+		if ( totalPenaltyPoints > 6 ) {
+			// A red indicator.
+			return 30;
+		}
 
-        if (totalPenaltyPoints > 4) {
-            // An orange indicator.
-            return 60;
-        }
-    }
-    else {
-        if (totalPenaltyPoints > 3) {
-            // A red indicator.
-            return 30;
-        }
+		if ( totalPenaltyPoints > 4 ) {
+			// An orange indicator.
+			return 60;
+		}
+	} else {
+		if ( totalPenaltyPoints > 3 ) {
+			// A red indicator.
+			return 30;
+		}
 
-        if (totalPenaltyPoints > 2) {
-            // An orange indicator.
-            return 60;
-        }
-    }
-    // A green indicator.
-    return 90;
+		if ( totalPenaltyPoints > 2 ) {
+			// An orange indicator.
+			return 60;
+		}
+	}
+	// A green indicator.
+	return 90;
 };
 
 /**
@@ -127,16 +126,16 @@ ContentAssessor.prototype._ratePenaltyPoints = function (totalPenaltyPoints) {
  * @returns {number} The overall score.
  */
 ContentAssessor.prototype.calculateOverallScore = function () {
-    var results = this.getValidResults();
+	var results = this.getValidResults();
 
-    // If you have no content, you have a red indicator.
-    if (results.length === 0) {
-        return 30;
-    }
+	// If you have no content, you have a red indicator.
+	if ( results.length === 0 ) {
+		return 30;
+	}
 
-    var totalPenaltyPoints = this.calculatePenaltyPoints();
+	var totalPenaltyPoints = this.calculatePenaltyPoints();
 
-    return this._ratePenaltyPoints(totalPenaltyPoints);
+	return this._ratePenaltyPoints( totalPenaltyPoints );
 };
 
 module.exports = ContentAssessor;
