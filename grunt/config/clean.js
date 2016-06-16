1a06be83eafaa76edd0f9cc89deed6b761bbb549
Anton Timmermans <email@atimmer.com>
2016-06-16 11:10:52 +0200

    Remove CSS and JavaScript sources in release step

--- grunt/config/clean.js
+++ grunt/config/clean.js
@@ -1,4 +1,14 @@
 // https://github.com/gruntjs/grunt-contrib-clean
 module.exports = {
-	'po-files': [ 'languages/*.po', 'languages/<%= pkg.plugin.textdomain %>-temp.pot', 'languages/yoast-seo.json' ]
+	'po-files': [ 'languages/*.po', 'languages/<%= pkg.plugin.textdomain %>-temp.pot', 'languages/yoast-seo.json' ],
+	'release-css': [
+		'css/src/**',
+		'css/*.css',
+		'!css/*.min.css'
+	],
+	'release-js': [
+		'js/src/**',
+		'js/dist/*.js',
+		'!js/dist/*.min.js'
+	]
 };
