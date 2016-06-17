237c26d39f82bcf6f557407cf609f92303278e13
Andrea Fercia <a.fercia@gmail.com>
2016-06-17 10:18:25 +0200

    Use a fieldset legend and get rid of the table.

--- admin/views/tabs/metas/general.php
+++ admin/views/tabs/metas/general.php
@@ -13,17 +13,8 @@ if ( ! current_theme_supports( 'title-tag' ) ) {
 	$yform->light_switch( 'forcerewritetitle', __( 'Force rewrite titles', 'wordpress-seo' ) );
 	echo '<p class="description">', sprintf( __( '%1$s has auto-detected whether it needs to force rewrite the titles for your pages, if you think it\'s wrong and you know what you\'re doing, you can change the setting here.', 'wordpress-seo' ), 'Yoast SEO' ) . '</p>';
 }
-?>
-<table class="form-table">
-	<tr>
-		<th>
-			<?php _e( 'Title Separator', 'wordpress-seo' ); ?>
-		</th>
-		<td>
-			<?php
-			$yform->radio( 'separator', WPSEO_Option_Titles::get_instance()->get_separator_options(), '' );
-			echo '<p class="description">', __( 'Choose the symbol to use as your title separator. This will display, for instance, between your post title and site name.', 'wordpress-seo' ), ' ', __( 'Symbols are shown in the size they\'ll appear in the search results.', 'wordpress-seo' ), '</p>';
-			?>
-		</td>
-	</tr>
-</table>
+
+$legend      = __( 'Title Separator', 'wordpress-seo' );
+$legend_attr = array( 'class' => 'radiogroup yoast-title-separator-legend' );
+$yform->radio( 'separator', WPSEO_Option_Titles::get_instance()->get_separator_options(), $legend, $legend_attr );
+echo '<p class="description">', __( 'Choose the symbol to use as your title separator. This will display, for instance, between your post title and site name.', 'wordpress-seo' ), ' ', __( 'Symbols are shown in the size they\'ll appear in the search results.', 'wordpress-seo' ), '</p>';
