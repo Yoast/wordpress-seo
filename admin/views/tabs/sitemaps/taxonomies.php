346d5ca60b3e23fbee3374fe381d24a3c1da4dcb
Andrea Fercia <a.fercia@gmail.com>
2016-06-20 12:38:45 +0200

    XML sitemap settings better headings.

--- admin/views/tabs/sitemaps/taxonomies.php
+++ admin/views/tabs/sitemaps/taxonomies.php
@@ -9,6 +9,8 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 	exit();
 }
 
+echo '<h2>' . __( 'Taxonomies sitemap settings', 'wordpress-seo' ) . '</h2>';
+
 /**
  * Filter the taxonomies to present in interface for exclusion.
  *
