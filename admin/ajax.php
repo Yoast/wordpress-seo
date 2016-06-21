ee0bce0bb2add99b1e5a88fc9c4835b7e4221610
Anton Timmermans <email@atimmer.com>
2016-06-21 11:32:35 +0200

    Fix WPSEO version number in deprecation warning

--- admin/ajax.php
+++ admin/ajax.php
@@ -442,7 +442,7 @@ new Yoast_Dismissable_Notice_Ajax( 'recalculate', Yoast_Dismissable_Notice_Ajax:
  * @deprecated 3.3.2
  */
 function wpseo_get_export() {
-	_deprecated_function( __METHOD__, 'WPSEO 2.0', 'This method is deprecated.' );
+	_deprecated_function( __METHOD__, 'WPSEO 3.3.2', 'This method is deprecated.' );
 
 	wpseo_ajax_json_echo_die( '' );
 }
