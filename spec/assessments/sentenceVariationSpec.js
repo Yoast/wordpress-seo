30a10266a86ccdc8b9a8b8fe75b85adb02f77c93
terw-dan <danny@yoast.com>
2016-06-23 09:12:36 +0200

    Adds spec

--- spec/assessments/sentenceVariationSpec.js
+++ spec/assessments/sentenceVariationSpec.js
@@ -45,7 +45,19 @@ describe( "An assessment for sentence variation", function() {
 	} );
 
 	it( "returns no score when there is only 1 sentence", function() {
-		mockPaper = new Paper( "" );
+		mockPaper = new Paper( "This is one sentence." );
+		var assessment = sentenceVariationAssessment.getResult( mockPaper, Factory.buildMockResearcher( 20 ), i18n );
+		expect( assessment.hasScore() ).toBe( false );
+	} );
+
+	it( "returns no score when there is only HTMLtags ", function() {
+		mockPaper = new Paper( "<iframe src='framesource'>" );
+		var assessment = sentenceVariationAssessment.getResult( mockPaper, Factory.buildMockResearcher( 20 ), i18n );
+		expect( assessment.hasScore() ).toBe( false );
+	} );
+
+	it( "returns no score when there is only HTMLtags and 1 sentence", function() {
+		mockPaper = new Paper( "<iframe src='framesource'> One sentence." );
 		var assessment = sentenceVariationAssessment.getResult( mockPaper, Factory.buildMockResearcher( 20 ), i18n );
 		expect( assessment.hasScore() ).toBe( false );
 	} );
