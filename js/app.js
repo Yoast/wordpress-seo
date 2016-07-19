e8a90fbc992fbb8b52a71e0e21f2b589c2201bff
terw-dan <danny@yoast.com>
2016-07-19 12:16:38 +0200

    Measure title before creating paper

--- js/app.js
+++ js/app.js
@@ -525,6 +525,8 @@ App.prototype.runAnalyzer = function() {
 	}
 
 	this.analyzerData = this.modifyData( this.rawData );
+	
+	this.snippetPreview.measureTitle();
 
 	// Create a paper object for the Researcher
 	this.paper = new Paper( this.analyzerData.text, {
