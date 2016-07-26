cb71112c996a857e7518b8dfb01ab2097411a931
Andrey Savchenko <contact@rarst.net>
2016-07-26 14:48:56 +0300

    Prevented keyword usage meta query for invalid keyword.

--- inc/class-wpseo-meta.php
+++ inc/class-wpseo-meta.php
@@ -1043,6 +1043,11 @@ class WPSEO_Meta {
 	 * @return array
 	 */
 	public static function keyword_usage( $keyword, $post_id ) {
+
+		if ( empty( $keyword ) ) {
+			return array();
+		}
+
 		$get_posts = new WP_Query(
 			array(
 				'meta_key'       => '_yoast_wpseo_focuskw',
