c04b6853fa7a1f1b928df34688f5affced5a7975
omarreiss <omar.reiss@gmail.com>
2016-07-26 12:28:17 +0200

    Fix codestyle

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
-import React from 'react';
+import React from "react";
 
 class App extends React.Component {
 
 	render() {
 		return (
 			<div></div>
-		)
+		);
 	}
 }
 
-export default App
+export default App;
