ffea36ec56f274db6b91b943fd81e5bace099605
Andy Meerwaldt <andy@yoast.com>
2016-07-26 16:14:48 +0200

    Sets the steps properly.

--- js/wizard.js
+++ js/wizard.js
@@ -7,6 +7,10 @@ function parseSteps( steps ) {
 	var previous      = null;
 	var stepsReversed = [];
 	for( let step in steps ) {
+		if ( ! steps.hasOwnProperty( step ) ) {
+			continue;
+		}
+
 		steps[ step ].previous = previous;
 
 		previous = step;
@@ -18,10 +22,11 @@ function parseSteps( steps ) {
 	stepsReversed.pop();
 
 	for( let step in steps ) {
-		if ( steps.hasOwnProperty( step ) ) {
-			steps[step].next = stepsReversed.pop();
+		if ( ! steps.hasOwnProperty( step ) ) {
+			continue;
 		}
 
+		steps[step].next = stepsReversed.pop();
 	}
 
 	return steps;
@@ -33,86 +38,23 @@ class Wizard extends React.Component {
 
 	constructor() {
 		super()
-
-		// let keys  = Array.from( Steps.keys() )
-		// let steps = Array.from( Steps.values() )
-		// let currentStep = 0
-		//
-		// this.state = {
-		// 	currentStep,
-		// 	steps,
-		// 	keys,
-		// 	previousStep: this.getPreviousStep( keys, currentStep ),
-		// 	nextStep: this.getNextStep( keys, currentStep )
-		// };
-		//
-		// this.gotoNext = this.gotoNext.bind(this)
 	}
 
 	componentWillMount() {
-		this.props.steps = parseSteps( this.props.steps );
+		this.setState( {
+			steps: parseSteps( this.props.steps )
+		});
+
 	}
 
-
-	getNextStep( keys, currentStep ) {
-
-		let nextStep = null;
-
-		if( keys[ currentStep + 1 ] ) {
-			nextStep = currentStep + 1;
-		}
-
-		return nextStep;
-	}
-
-	getPreviousStep( keys, currentStep ) {
-		let previousStep = null;
-
-
-		if( keys[ currentStep - 1 ] ) {
-			previousStep = currentStep - 1;
-		}
-
-		return previousStep;
-	}
-
-	gotoNext() {
-		var nextStep = this.state.currentStep + 1;
-
-		this.setState(
-			{
-				currentStep: nextStep,
-				previousStep: this.getPreviousStep( this.state.keys, nextStep ),
-				nextStep: this.getNextStep( this.state.keys, nextStep )
-			}
-		)
-	}
-
-	previousStep() {
-		let previous = this.state.previousStep;
-		if( previous !== null ) {
-			previous = this.state.steps[ previous ];
-			return <Step ref="previous" id={previous.id} title={previous.title} fields={previous.fields}  />
-		}
-
-		return '';
-	}
-
-	nextStep() {
-		let next = this.state.nextStep;
-		if( next !== null ) {
-			next = this.state.steps[ next ];
-			return <Step update={this.gotoNext} ref="next" id={next.id} title={next.title} fields={next.fields}  />
-		}
-
-		return '';
-	}
 
 	render() {
+
+		console.log(this.state.steps);
+
 		return (
 			<div>
-				{this.previousStep()}
-				{this.nextStep()}
+
 			</div>
 
 		)
