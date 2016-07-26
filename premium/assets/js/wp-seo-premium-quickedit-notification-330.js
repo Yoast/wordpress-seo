78cc3019a7644c277dfd5ff805304300b1447f9a
Jimmy Comack <jimmy@yoast.com>
2016-07-26 14:24:19 +0200

    Cleaned up the code and now also using the enter key will properly trigger the notification

--- premium/assets/js/wp-seo-premium-quickedit-notification-330.js
+++ premium/assets/js/wp-seo-premium-quickedit-notification-330.js
@@ -17,42 +17,115 @@ function wpseo_show_notification() {
 	jQuery.post(
 		ajaxurl,
 		{ action: 'yoast_get_notifications' },
-		function(response) {
-			if (response !== '') {
+		function( response ) {
+			if ( response !== '' ) {
 				var insertAfterElement = jQuery( '.wrap' ).children().eq( 0 );
 				jQuery(response ).insertAfter( insertAfterElement );
 				wpseo_notification_counter = 0;
-			} else if (wpseo_notification_counter < 20 && response === '') {
+			}
+
+			if ( wpseo_notification_counter < 20 && response === '' ) {
 				wpseo_notification_counter++;
-				setTimeout(wpseo_show_notification, 100);
+				setTimeout( wpseo_show_notification, 100 );
 			}
 		}
 	);
 }
 
+/**
+ * Gets the current page based on the current URL.
+ *
+ * @returns {string} The current page.
+ */
 function wpseo_get_current_page() {
-	// We want to show a redirect message when the slug is changed using quick edit. Therefore we need to get the current page.
 	return jQuery(location).attr('pathname').split('/').pop();
 }
 
-function wpseo_get_current_slug( post_id ) {
+/**
+ * Gets the current slug of a post based on the current page and post or term being edited.
+ *
+ * @returns {string} The slug of the current post or term.
+ */
+function wpseo_get_current_slug() {
+	var currentPost = wpseo_get_item_id();
 	var currentPage = wpseo_get_current_page();
 
 	if ( currentPage === 'edit.php' ) {
-		return jQuery('#inline_' + post_id ).find('.post_name').html();
+		return jQuery( '#inline_' + currentPost ).find('.post_name').html();
 	}
 
 	if ( currentPage === 'edit-tags.php' ) {
-		return jQuery('#inline_' + post_id).find('.slug').html();
+		return jQuery( '#inline_' + currentPost ).find('.slug').html();
 	}
 
 	return '';
 }
 
-function wpseo_compare_slug( currentSlug ) {
-	var wpseo_new_slug = jQuery('input[name=post_name]').val();
+/**
+ * Checks whether or not the slug has changed.
+ *
+ * @returns {boolean} Whether or not the slug has changed.
+ */
+function wpseo_slug_changed() {
+	var editor = wpseo_get_active_editor();
+	var currentSlug = wpseo_get_current_slug();
+	var wpseo_new_slug =  editor.find( 'input[name=post_name]' ).val();
 
-	if ( currentSlug !== wpseo_new_slug) {
+	return currentSlug !== wpseo_new_slug;
+}
+
+/**
+ * Gets the currently active editor used in quick edit.
+ *
+ * @returns {Object} The editor that is currently active.
+ */
+function wpseo_get_active_editor() {
+	return jQuery( 'tr.inline-edit-post.inline-editor' );
+}
+
+/**
+ * Gets the current post or term id.
+ * Returns an empty string if no editor is currently active.
+ *
+ * @returns {string} The ID of the current post or term.
+ */
+function wpseo_get_item_id() {
+	var editor = wpseo_get_active_editor();
+
+	if ( editor === '' ) {
+		return '';
+	}
+
+	return editor.attr( 'id' ).replace( 'edit-', '' );
+}
+
+/**
+ * Handles the key-based events in the quick edit editor.
+ *
+ * @param ev {Event} The event currently being executed.
+ */
+function wpseo_handle_key_events( ev ) {
+	if ( ev.which !== 13 ) {
+		return;
+	}
+
+	if ( wpseo_slug_changed() ) {
+		wpseo_show_notification();
+	}
+}
+
+
+/**
+ * Handles the button-based events in the quick edit editor.
+ *
+ * @param ev {Event} The event currently being executed.
+ */
+function wpseo_handle_button_events( ev ) {
+	if ( jQuery( ev.target ).attr('id') === 'save-order' ) {
+		return;
+	}
+
+	if ( wpseo_slug_changed() ) {
 		wpseo_show_notification();
 	}
 }
@@ -60,32 +133,16 @@ function wpseo_compare_slug( currentSlug ) {
 (jQuery(function() {
 	var wpseo_current_page = wpseo_get_current_page();
 
-	// If current page is edit.php, proceed.
-	if (wpseo_current_page === 'edit.php' || wpseo_current_page === 'edit-tags.php') {
-		if ( jQuery(this).attr('id') === 'save-order' ) {
-			return;
-		}
-
-		var tr = jQuery( this ).closest('tr');
-
-		console.log( tr );
-
-		if ( tr.length === 0 ) {
-			return;
-		}
-
-		var wpseo_post_id = tr.attr('id').replace('edit-', '');
-		var wpseo_current_slug = wpseo_get_current_slug( wpseo_post_id );
-
-		$( 'td', '#inline-edit').on( 'keydown', function(e){
-			if ( e.which === 13 && ! $( e.target ).hasClass( 'cancel' ) ) {
-				wpseo_compare_slug( wpseo_current_slug );
-			}
-		});
-
-		// When user clicks on save button after doing a quick edit, get the post id, current slug and new slug.
-		jQuery('.button-primary').click(function() {
-			wpseo_compare_slug( wpseo_current_slug );
-		});
+	// If current page isn't edit.php, stop execution.
+	if ( wpseo_current_page !== 'edit.php' && wpseo_current_page !== 'edit-tags.php' ) {
+		return;
 	}
+
+	jQuery( 'td', '#inline-edit').on( 'keydown', function( ev ){
+		wpseo_handle_key_events( ev );
+	});
+
+	jQuery( '.button-primary' ).click(function( ev ) {
+		wpseo_handle_button_events( ev );
+	});
 }));
