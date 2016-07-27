aaece1358c7fd5be6d7d7906f646ebbde7900fd3
Andrea Fercia <a.fercia@gmail.com>
2016-07-27 15:02:27 +0200

    Add missing text domain to the notifications count bubbles.

--- inc/wpseo-non-ajax-functions.php
+++ inc/wpseo-non-ajax-functions.php
@@ -76,7 +76,7 @@ function wpseo_admin_bar_menu() {
 			if ( $notification_count > 0 ) {
 				// Always show Alerts page when clicking on the main link.
 				/* translators: %s: number of notifications */
-				$counter_screen_reader_text = sprintf( _n( '%s notification', '%s notifications', $notification_count ), number_format_i18n( $notification_count ) );
+				$counter_screen_reader_text = sprintf( _n( '%s notification', '%s notifications', $notification_count, 'wordpress-seo' ), number_format_i18n( $notification_count ) );
 				$counter = sprintf( ' <div class="wp-core-ui wp-ui-notification yoast-issue-counter"><span aria-hidden="true">%d</span><span class="screen-reader-text">%s</span></div>', $notification_count, $counter_screen_reader_text );
 			}
 
