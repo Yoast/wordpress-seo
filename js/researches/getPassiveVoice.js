19b01c76d65f4543c866aba60fb00c88dd505311
terw-dan <danny@yoast.com>
2016-07-27 10:27:39 +0200

    Removes a redundant loop

--- js/researches/getPassiveVoice.js
+++ js/researches/getPassiveVoice.js
@@ -25,18 +25,24 @@ var ingExclusionArray = [ "king", "cling", "ring", "being" ];
 var irregularExclusionArray = [ "get", "gets", "getting", "got", "gotten" ];
 
 /**
- * Returns the indices of a string in a sentence. If it is found multiple times, will return multiple indices.
+ * Returns the indices of a string in a sentence. If it is found multiple times, it will return multiple indices.
  *
  * @param {string} part The part to find in the sentence.
  * @param {string} sentence The sentence to check for parts.
  * @returns {Array} All indices found.
  */
 function getIndicesOf( part, sentence ) {
-	var startIndex = 0, searchStrLen = part.length;
+	var startIndex = 0;
+	var searchStringLength = part.length;
 	var index, indices = [];
 	while ( ( index = sentence.indexOf( part, startIndex ) ) > -1 ) {
-		indices.push( index );
-		startIndex = index + searchStrLen;
+		indices.push(
+			{
+				index: index,
+				match: part
+			}
+		);
+		startIndex = index + searchStringLength;
 	}
 	return indices;
 }
@@ -54,14 +60,10 @@ var matchArray = function( sentence, matches ) {
 
 	forEach( matches, function( part ) {
 		part = stripSpaces( part );
-		var indices = getIndicesOf( part, sentence );
-
 		if ( !matchWordInSentence( part, sentence ) ) {
 			return;
 		}
-		forEach( indices, function( index ) {
-			matchedParts.push( { index: index, match: part } );
-		} );
+		matchedParts = matchedParts.concat( getIndicesOf( part, sentence ) );
 	} );
 
 	return matchedParts;
--- spec/researches/passiveVoiceSpec.js
+++ spec/researches/passiveVoiceSpec.js
@@ -357,7 +357,7 @@ describe( "detecting passive voice in sentences", function() {
 
 	it( "returns no passive sentence when the subsentence has no auxiliary, when the auxiliary is used multiple times", function () {
 		// Passive: no passive, auxiliary: was
-		paper = new Paper("He thought she was the one who knew about the six buried in his back yard, but he was wrong.");
+		paper = new Paper( "He thought she was the one who knew about the six buried in his back yard, but he was wrong." );
 		expect( passiveVoice( paper ).passives.length ).toBe( 0 );
 	});
 
