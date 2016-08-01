cf52f4c835ca8ae4186e29d15af4778c0ed3f3bb
Andy Meerwaldt <andy@yoast.com>
2016-08-01 13:17:47 +0200

    Process CR

--- js/components/html.js
+++ js/components/html.js
@@ -1,39 +1,28 @@
 import React from 'react';
-import Field from './field';
 
 /**
  *  Represents a piece of HTML. This can be useful on the opening and closing screen of the wizard, to add some
  *  introduction text, a success message or CTA towards the end.
+ *
+ * @param {Object} props The properties to use.
+ * @returns {JSX}
+ * @constructor
  */
-class HTML extends Field {
+const HTML = ( props ) => {
+	return (
+		<div dangerouslySetInnerHTML={{__html: props.properties.html}}></div>
+	);
+};
 
-	/**
-	 * Gets the html as an object.
-	 *
-	 * @returns {{__html: (string|*|string|null|string|string)}}
-	 */
-	getHTML() {
-		return { __html: this.props.html} ;
-	}
-
-	/**
-	 * Returns the rendered html
-	 *
-	 * @returns {XML}
-	 */
-	render() {
-		return (
-			<div dangerouslySetInnerHTML={this.getHTML()}></div>
-		)
-	}
-}
 
 HTML.propTypes = {
-	html: React.PropTypes.string
+	properties: React.PropTypes.object.isRequired
 };
 
 HTML.defaultProps = {
-	html: ''
+	properties: {
+		html: ''
+	}
 };
 
 export default HTML;
--- js/components/input.js
+++ js/components/input.js
@@ -1,29 +1,30 @@
 import React from 'react';
-import Field from './field';
 
 /**
  * Represents a text input interface, like a regular input field or a text area. Initially it should render a
  * normal text input. We might add other representations later on.
+ *
+ * @param {Object} props The properties to use.
+ * @returns {JSX}
+ * @constructor
  */
-class Input extends Field {
+const Input = ( props ) => {
 
-	render() {
-		return (
-			<div>
-				<h2>{this.props.properties.label}</h2>
-				<label htmlFor={this.props.name}>{this.props.label}</label>
-				<input type={this.props.type} id={this.props.name} name={this.props.name} defaultValue={this.props.data} placeholder={this.props.placeholder} />
-			</div>
-		)
-	}
+	return (
+		<div>
+			<h2>{props.properties.label}</h2>
+			<label htmlFor={props.name}>{props.label}</label>
+			<input type={props.type} id={props.name} name={props.name} defaultValue={props.data} placeholder={props.placeholder} />
+		</div>
+	)
 
 }
 
 Input.propTypes = {
-	label: React.PropTypes.string,
+	label: React.PropTypes.string.isRequired,
 	placeholder: React.PropTypes.string,
-	type: React.PropTypes.string,
-	name: React.PropTypes.string,
+	type: React.PropTypes.string.isRequired,
+	name: React.PropTypes.string.isRequired,
 	value: React.PropTypes.string
 };
 
