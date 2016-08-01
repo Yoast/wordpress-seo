5528257223e87c09d156f3757ce0e0ef63d13652
terw-dan <danny@yoast.com>
2016-08-01 10:08:59 +0200

    Fixes docs

--- js/stringProcessing/removePunctuation.js
+++ js/stringProcessing/removePunctuation.js
@@ -1,4 +1,4 @@
-// Replace all other punctuation chars at the beginning or at the end of a word.
+// Replace all other punctuation characters at the beginning or at the end of a word.
 var punctuationRegexString = "[\\–\\-\\(\\)_\\[\\]’“”\"'.?!:;,¿¡«»]+";
 var punctuationRegexStart = new RegExp( "^" + punctuationRegexString );
 var punctuationRegexEnd = new RegExp( punctuationRegexString + "$" );
@@ -6,7 +6,7 @@ var punctuationRegexEnd = new RegExp( punctuationRegexString + "$" );
 /**
  * Replaces punctuation characters from the given word string.
  *
- * @param {String} word The word to remove the punctuation chars for.
+ * @param {String} word The word to remove the punctuation characters for.
  *
  * @returns {String} The sanitized word.
  */
