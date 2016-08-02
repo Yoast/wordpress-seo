9ad3d1bbd6dab8e7094d9ed86284511a20d2a408
Andy Meerwaldt <andy@yoast.com>
2016-08-02 13:02:08 +0200

    Add documentation.

--- js/helpers/postJSONRequest.js
+++ js/helpers/postJSONRequest.js
@@ -4,23 +4,41 @@ import 'whatwg-fetch';
 let postJSONRequest = null;
 
 if( typeof jQuery === typeof undefined  ) {
-	postJSONRequest = ( endpoint, postdata, onsuccess, onfail ) => {
+
+	/**
+	 * Wrapper method when fetch should be used.
+	 *
+	 * @param {string}   url     The endpoint to send the data to.
+	 * @param {Object}   data    The JSON object to send to the server.
+	 * @param {function} success Callback for When the request is successful.
+	 * @param {function} error   Callback for When the request is failed.
+	 */
+	postJSONRequest = ( url, data, success, error ) => {
 		fetch(
-			endpoint,
+			url,
 			{
 				method: 'POST',
 				headers: {
 					'Accept': 'application/json',
 					'Content-Type': 'application/json'
 				},
-				body: postdata
+				body: data
 			}
 		)
-		.then( onsuccess )
-		.catch( onfail )
+		.then( success )
+		.catch( error )
 	};
 }
 else {
+
+	/**
+	 * Wrapper method for doing request when jQuery is active.
+	 *
+	 * @param {string}   url     The endpoint to send the data to.
+	 * @param {Object}   data    The JSON object to send to the server.
+	 * @param {function} success Callback for When the request is successful.
+	 * @param {function} error   Callback for When the request is failed.
+	 */
 	postJSONRequest = ( url, data, success, error ) => {
 		let contentType = "application/json";
 		let method = "POST";
