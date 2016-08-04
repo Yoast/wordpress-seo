2b7f79c8aaded95df5206f304947f3c29beb1fe8
Jimmy Comack <jimmy@yoast.com>
2016-08-04 15:06:28 +0200

    Added new elements and added missing functionality

---
+++ forms/Textarea.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+/**
+ * Represents the textarea HTML element.
+ *
+ * @param {Object} props The properties to use.
+ * @returns {JSX}
+ * @constructor
+ */
+const Textarea = ( props ) => {
+	return (
+		<textarea id={props.name} name={props.name} onChange={props.onChange} {...props.optionalAttributes}>{props.value}</textarea>
+	);
+};
+
+/**
+ * Adds validation for the properties.
+ *
+ * @type {{type: string, name: string, placeholder: string, value: string, optionalAttributes: object}}
+ */
+Textarea.propTypes = {
+	name: React.PropTypes.string.isRequired,
+	optionalAttributes: React.PropTypes.object,
+};
+
+/**
+ * Defines the default values for the properties.
+ *
+ * @type {{type: string, name: string}}
+ */
+Textarea.defaultProps = {
+	name: "",
+	value: "",
+	className: "",
+	placeholder: "",
+};
+
+export default Textarea;
