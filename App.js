72c05519aefba8819e849733a1850b60de2dd2b8
Jimmy Comack <jimmy@yoast.com>
2016-08-09 15:02:10 +0200

    Removed debug code

--- App.js
+++ App.js
@@ -7,28 +7,10 @@ class App extends React.Component {
 	constructor() {
 		super();
 
-		this.state = {
-			value: 'hoi',
-			readOnly: '',
-		}
+		this.state = {};
 	}
 
 	render() {
-		let optionalAttributes = {
-			className: 'je moeder',
-			placeholder: 'ok doei',
-			id: 'hoi diede',
-			value: this.state.value,
-		};
-
-		let hoi = {
-			placeholder: "hoi"
-		};
-
-		let op2 = Object.assign({}, optionalAttributes);
-
-		op2.readOnly = true;
-
 		return (
 			<div>
 				<SearchResultPreview />
