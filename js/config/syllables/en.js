8db62025636a3da5cb4f9206772a0aaaac89d190
Anton Timmermans <email@atimmer.com>
2016-08-09 17:00:21 +0200

    Move syllable config to separate files

---
+++ js/config/syllables/en.js
@@ -0,0 +1,26 @@
+module.exports = {
+	syllableExclusion: {
+		subtractSyllables: {
+			syllables: [ "cial", "tia", "cius", "cious", "giu", "ion", "iou", "sia$", "[^aeiuoyt]{2,}ed$", "[aeiouy][^aeiuoyts]{1,}e$", ".ely$", "[cg]h?e[sd]", "rved$", "rved", "[aeiouy][dt]es?$", "[aeiouy][^aeiouydt]e[sd]?$", "^[dr]e[aeiou][^aeiou]+$", "[aeiouy]rse$" ],
+			multiplier: -1
+		},
+		addSyllables: {
+			syllables: [ "ia", "riet", "dien", "iu", "io", "ii", "[aeiouym][bdp]le$", "[aeiou]{3}", "^mc", "ism$", "([^aeiouy])\1l$", "[^l]lien", "^coa[dglx].", "[^gq]ua[^auieo]", "dnt$", "uity$", "ie(r|st)", "[aeiouy]ing", "[aeiouw]y[aeiou]", "[^ao]ire[ds]", "[^ao]ire$"],
+			multiplier: +1
+		}
+
+	},
+	exclusionWords: [
+		{ word: "business", syllables: 2 },
+		{ word: "coheiress", syllables: 3 },
+		{ word: "colonel", syllables: 2 },
+		{ word: "heiress", syllables: 2 },
+		// The abbreviation i.e. should be counted as 2 syllables.
+		{ word: "i.e", syllables: 2},
+		{ word: "shoreline", syllables: 2 },
+		{ word: "simile", syllables: 3 },
+		{ word: "unheired", syllables: 2 },
+		{ word: "wednesday", syllables: 2 }
+	],
+	vowels: 'aeiouy'
+};
