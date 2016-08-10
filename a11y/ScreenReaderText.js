93145fd50212b73a5fe879dd308cce9577e163ba
omarreiss <omar.reiss@gmail.com>
2016-08-10 17:17:34 +0200

    Add ScreenReaderText and ScreenReaderShortcut components

---
+++ a11y/ScreenReaderText.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Styles from "./Styles";
+
+/**
+ * Renders a div with text that is only shown to screen readers.
+ * Can be used to provide some extra context for users with screen readers.
+ *
+ * @param {Object} props The view properties.
+ * @returns {JSX} ScreenReaderText The div containing the screen reader text.
+ * @constructor
+ */
+const ScreenReaderText = ( props ) => {
+	return (
+		<div className="screen-reader-text" style={ Styles.ScreenReaderText.default }>
+			{ props.children }
+		</div>
+	);
+};
+
+ScreenReaderText.propTypes = {
+	children: React.PropTypes.string.isRequired,
+};
+
+export default ScreenReaderText;
