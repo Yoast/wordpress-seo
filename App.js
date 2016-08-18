d5bd1610e0779932a7a199ff87218e0b24966f9f
Rens Weerman <rens@yoast.com>
2016-08-18 14:38:13 +0200

    App.js for testing purposes

--- App.js
+++ App.js
@@ -1,14 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import Input from  './forms/Input';
+import Wizard from "./composites/onboarding-wizard/wizard";
+import Config from "./composites/onboarding-wizard/config/config";
 
 class App extends React.Component {
+
 	render() {
 		return (
-			<div>Demo text</div>
-		)
+			<Wizard {...Config} />
+		);
 	}
-};
+}
 
 ReactDOM.render( <App />, document.getElementById( 'container' ) );
