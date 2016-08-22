56478e422c1522382075cb84ed42ad283356caa2
Rens Weerman <rens@yoast.com>
2016-08-22 09:23:41 +0200

    Install material UI framework

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import ReactDOM from "react-dom";
 import Wizard from "./composites/OnboardingWizard/OnboardingWizard";
 import Config from "./composites/OnboardingWizard/config/production-config";
 
+// Required to make Material UI work with touch screens.
+var injectTapEventPlugin = require("react-tap-event-plugin");
+injectTapEventPlugin();
+
 class App extends React.Component {
 
 	render() {
