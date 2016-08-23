4777ec26c29a4c529befb6f627229e223e296660
Anton Timmermans <email@atimmer.com>
2016-08-23 11:40:26 +0200

    Use browserify to compile all JavaScript

---
+++ premium/assets/js/wp-seo-premium-quickedit-notification-330.js
@@ -0,0 +1,162 @@
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+/* global ajaxurl */
+/* global wpseo_undo_redirect */
+/* jshint -W097 */
+'use strict';
+
+/**
+ * Use notification counter so we can count how many times the function wpseo_show_notification is called.
+ *
+ * @type {number}
+ */
+
+var wpseo_notification_counter = 0;
+
+/**
+ * Show notification to user when there's a redirect created. When the response is empty, up the notification counter with 1, wait 100 ms and call function again.
+ * Stop when the notification counter is bigger than 20.
+ */
+function wpseo_show_notification() {
+	jQuery.post(ajaxurl, { action: 'yoast_get_notifications' }, function (response) {
+		if (response !== '') {
+			var insertAfterElement = jQuery('.wrap').children().eq(0);
+			jQuery(response).insertAfter(insertAfterElement);
+			wpseo_notification_counter = 0;
+		}
+
+		if (wpseo_notification_counter < 20 && response === '') {
+			wpseo_notification_counter++;
+			setTimeout(wpseo_show_notification, 500);
+		}
+	});
+}
+
+/**
+ * Gets the current page based on the current URL.
+ *
+ * @returns {string} The current page.
+ */
+function wpseo_get_current_page() {
+	return jQuery(location).attr('pathname').split('/').pop();
+}
+
+/**
+ * Gets the current slug of a post based on the current page and post or term being edited.
+ *
+ * @returns {string} The slug of the current post or term.
+ */
+function wpseo_get_current_slug() {
+	var currentPost = wpseo_get_item_id();
+	var currentPage = wpseo_get_current_page();
+
+	if (currentPage === 'edit.php') {
+		return jQuery('#inline_' + currentPost).find('.post_name').html();
+	}
+
+	if (currentPage === 'edit-tags.php') {
+		return jQuery('#inline_' + currentPost).find('.slug').html();
+	}
+
+	return '';
+}
+
+/**
+ * Checks whether or not the slug has changed.
+ *
+ * @returns {boolean} Whether or not the slug has changed.
+ */
+function wpseo_slug_changed() {
+	var editor = wpseo_get_active_editor();
+	var currentSlug = wpseo_get_current_slug();
+	var wpseo_new_slug = editor.find('input[name=post_name]').val();
+
+	return currentSlug !== wpseo_new_slug;
+}
+
+/**
+ * Gets the currently active editor used in quick edit.
+ *
+ * @returns {Object} The editor that is currently active.
+ */
+function wpseo_get_active_editor() {
+	return jQuery('tr.inline-editor');
+}
+
+/**
+ * Gets the current post or term id.
+ * Returns an empty string if no editor is currently active.
+ *
+ * @returns {string} The ID of the current post or term.
+ */
+function wpseo_get_item_id() {
+	var editor = wpseo_get_active_editor();
+
+	if (editor === '') {
+		return '';
+	}
+
+	return editor.attr('id').replace('edit-', '');
+}
+
+/**
+ * Handles the key-based events in the quick edit editor.
+ *
+ * @param ev {Event} The event currently being executed.
+ */
+function wpseo_handle_key_events(ev) {
+	// 13 refers to the enter key.
+	if (ev.which === 13 && wpseo_slug_changed()) {
+		wpseo_show_notification();
+	}
+}
+
+/**
+ * Handles the button-based events in the quick edit editor.
+ *
+ * @param ev {Event} The event currently being executed.
+ */
+function wpseo_handle_button_events(ev) {
+	if (jQuery(ev.target).attr('id') !== 'save-order' && wpseo_slug_changed()) {
+		wpseo_show_notification();
+	}
+}
+
+/**
+ * Undoes a redirect.
+ *
+ * @param {string} origin The redirect's origin.
+ * @param {string} target The redirect's target.
+ * @param {string} type The type of redirect.
+ * @param {string} nonce The nonce being used to validate the current AJAX request.
+ * @param {object} source The DOMElement containing the alerts.
+ */
+function wpseo_undo_redirect(origin, target, type, nonce, source) {
+	jQuery.post(ajaxurl, {
+		action: 'wpseo_delete_redirect_plain',
+		ajax_nonce: nonce,
+		redirect: {
+			origin: origin,
+			target: target,
+			type: type
+		}
+	}, function () {
+		jQuery(source).closest('.yoast-alert').fadeOut('slow');
+	});
+}
+
+jQuery(function () {
+	var wpseo_current_page = wpseo_get_current_page();
+
+	// If current page is edit*.php, continue execution.
+	if (wpseo_current_page === 'edit.php' || wpseo_current_page === 'edit-tags.php') {
+		jQuery('#inline-edit input').on('keydown', function (ev) {
+			wpseo_handle_key_events(ev);
+		});
+
+		jQuery('.button-primary').click(function (ev) {
+			wpseo_handle_button_events(ev);
+		});
+	}
+});
+
+},{}]},{},[1]);
--- premium/grunt/config/browserify.js
+++ premium/grunt/config/browserify.js
@@ -8,7 +8,11 @@ module.exports = {
 		files: {
 			'assets/js/yoast-premium-social-preview-340.js': [ 'assets/js/src/yoast-premium-social-preview.js' ],
 			'assets/js/wp-seo-premium-multi-keyword-341.js': [ 'assets/js/src/multi-keyword.js' ],
-			'assets/js/wpseo-premium-contact-support.js': [ 'assets/js/src/contact-support.js' ]
+			'assets/js/wpseo-premium-contact-support.js': [ 'assets/js/src/contact-support.js' ],
+			'assets/js/wp-seo-premium-admin-redirects-340.js': [ 'assets/js/src/admin-redirects.js' ],
+			'assets/js/wp-seo-premium-custom-fields-plugin.js': [ 'assets/js/src/custom-fields-plugin.js' ],
+			'assets/js/wp-seo-premium-quickedit-notification-330.js': [ 'assets/js/src/quickedit-notification.js' ],
+			'assets/js/wpseo-premium-yoast-overlay.js': [ 'assets/js/src/yoast-overlay.js' ]
 		}
 	}
 };
