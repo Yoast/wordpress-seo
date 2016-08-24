10f0ef5165684b4df05c7c989ad097256e401d13
Jimmy Comack <jimcom89@gmail.com>
2016-08-24 14:53:10 +0200

    Added a first test for the composite components.

--- composites/SearchResultForm/tests/SearchResultFormTest.js
+++ composites/SearchResultForm/tests/SearchResultFormTest.js
@@ -1,4 +1,5 @@
 jest.unmock("../SearchResultForm");
+jest.unmock("i18n-calypso");
 
 import React from "react";
 import TestUtils from "react-addons-test-utils";
@@ -8,9 +9,11 @@ describe( "A SearchResultForm component", () => {
 	var renderer = TestUtils.createRenderer();
 
 	it( "generates a SearchResultForm based on the props", () => {
-		renderer.render( <SearchResultForm /> );
+		renderer.render( <SearchResultForm name="customTextArea" label="Custom Textarea" onChange={() => {}} /> );
 
 		let result = renderer.getRenderOutput();
+
+		expect(result.props.label).toBe( "Custom Textarea" );
 	} );
 
 } );
