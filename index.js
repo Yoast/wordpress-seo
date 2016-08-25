6c750e401df072d226cd80cc8b046f766c89b5c4
Jimmy Comack <jimcom89@gmail.com>
2016-08-25 15:01:36 +0200

    Added missing exports

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 import Textfield from "./forms/composites/Textfield";
-export {Textfield};
+import Button from "./forms/Button";
+import Input from "./forms/Input";
+import Label from "./forms/Label";
+import Progressbar from "./forms/Progressbar";
+import Section from "./forms/Section";
+import Textarea from "./forms/Textarea";
+
+export {
+	Textfield,
+	Button,
+	Input,
+	Label,
+	Progressbar,
+	Section,
+	Textarea,
+};
