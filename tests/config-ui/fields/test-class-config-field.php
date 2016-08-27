b172050cace4464b621c0bd742b67d43d5c22e9c
Jip Moors <jhp.moors@gmail.com>
2016-08-27 18:14:39 +0200

    Add tests for Config Field abstract classes

---
+++ tests/config-ui/fields/test-class-config-field.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @package WPSEO\UnitTests
+ */
+
+/**
+ * Class WPSEO_Config_Field_
+ */
+class WPSEO_Config_Field_ extends WPSEO_Config_Field {
+
+	/**
+	 * @param $data
+	 */
+	public function set_data( $data ) {
+		$this->data = $data;
+	}
+}
+
+/**
+ * Class WPSEO_Config_Field_Test
+ */
+class WPSEO_Config_Field_Test extends WPSEO_UnitTestCase {
+
+	/**
+	 * @covers WPSEO_Config_Field::__construct()
+	 */
+	public function test_constructor() {
+
+		$identifier = 'i';
+		$component  = 'c';
+
+		$field = new WPSEO_Config_Field_( $identifier, $component );
+
+		$this->assertEquals( $identifier, $field->get_identifier() );
+		$this->assertEquals( $component, $field->get_component() );
+	}
+
+	/**
+	 * @covers WPSEO_Config_Field::set_property()
+	 * @covers WPSEO_Config_Field::get_properties()
+	 */
+	public function test_properties() {
+		$property       = 'p';
+		$property_value = 'pv';
+
+		$field = new WPSEO_Config_Field_( 'a', 'b' );
+		$field->set_property( $property, $property_value );
+
+		$properties = $field->get_properties();
+
+		$this->assertEquals(
+			array( $property => $property_value ),
+			$properties
+		);
+	}
+
+	/**
+	 * @covers WPSEO_Config_Field::get_data()
+	 */
+	public function test_get_data() {
+		$data = 'test';
+
+		$field = new WPSEO_Config_Field_( 'a', 'b' );
+		$field->set_data( $data );
+
+		$this->assertEquals( $data, $field->get_data() );
+	}
+}
