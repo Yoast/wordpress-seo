cf043afb0827cba24bffb4f84dac3f023d73acc2
Rens Weerman <rens@yoast.com>
2016-09-01 11:31:45 +0200

    Processed feedback

--- composites/OnboardingWizard/Step.js
+++ composites/OnboardingWizard/Step.js
@@ -38,7 +38,6 @@ class Step extends React.Component {
 	 * @returns {void}
 	 */
 	componentWillReceiveProps( props ) {
-		this.key
 		this.setFieldValues( props.fields, props.currentStep );
 	}
 
@@ -106,7 +105,7 @@ class Step extends React.Component {
 			let currentField = fields[ name ];
 
 			if ( typeof Components[ currentField.componentName ] === "undefined"
-			     | !Components[ currentField.componentName ] ) {
+			     || !Components[ currentField.componentName ] ) {
 				console.error(`Trying to load non-existing component: ${currentField.componentName}`);
 				return null;
 			}
