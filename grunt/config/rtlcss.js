d02d19b785140a8dcd0716106440c60fafde38b2
Andrea Fercia <a.fercia@gmail.com>
2016-09-01 22:33:16 +0200

    Add rtlcss plugin to swap dashicons.

--- grunt/config/rtlcss.js
+++ grunt/config/rtlcss.js
@@ -1,7 +1,38 @@
 // https://github.com/MohammadYounes/grunt-rtlcss
 module.exports = {
 	options: {
-	    clean: true
+	    clean: true,
+		plugins: [
+			{
+				name: 'swap-dashicons-left-right-arrows',
+				priority: 10,
+				directives: {
+					control: {},
+					value: []
+				},
+				processors: [
+					{
+						expr: /content/im,
+						action: function( prop, value ) {
+							if ( value === '"\\f141"' ) { // dashicons-arrow-left
+								value = '"\\f139"';
+							} else if ( value === '"\\f340"' ) { // dashicons-arrow-left-alt
+								value = '"\\f344"';
+							} else if ( value === '"\\f341"' ) { // dashicons-arrow-left-alt2
+								value = '"\\f345"';
+							} else if ( value === '"\\f139"' ) { // dashicons-arrow-right
+								value = '"\\f141"';
+							} else if ( value === '"\\f344"' ) { // dashicons-arrow-right-alt
+								value = '"\\f340"';
+							} else if ( value === '"\\f345"' ) { // dashicons-arrow-right-alt2
+								value = '"\\f341"';
+							}
+							return { prop: prop, value: value };
+						}
+					}
+				]
+			}
+		]
 	},
 	plugin: {
 		expand: true,
