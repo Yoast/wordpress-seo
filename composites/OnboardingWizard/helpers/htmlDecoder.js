7fc891c5a89f56ed62f9409a44ae6a4e049b36b1
Rens Weerman <rens@yoast.com>
2016-09-02 15:38:19 +0200

    Processed feedback

--- composites/OnboardingWizard/helpers/htmlDecoder.js
+++ composites/OnboardingWizard/helpers/htmlDecoder.js
@@ -1,3 +1,9 @@
+/**
+ * @summary Converts HTML entities to the actual symbols.
+ *
+ * @param htmlText The HTML text.
+ * @returns {string} The string with converted HTML entities.
+ */
 let decodeHTML = ( htmlText ) => {
 	var txt = document.createElement( "textarea" );
 	txt.innerHTML = htmlText;
