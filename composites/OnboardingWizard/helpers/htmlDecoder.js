27c306064db13590feee55eea5d5509dda402140
Rens Weerman <rens@yoast.com>
2016-09-02 15:39:46 +0200

    Add source for decoding html in JS

--- composites/OnboardingWizard/helpers/htmlDecoder.js
+++ composites/OnboardingWizard/helpers/htmlDecoder.js
@@ -1,6 +1,8 @@
 /**
  * @summary Converts HTML entities to the actual symbols.
  *
+ * Source: http://stackoverflow.com/questions/3700326/decode-amp-back-to-in-javascript
+ *
  * @param htmlText The HTML text.
  * @returns {string} The string with converted HTML entities.
  */
