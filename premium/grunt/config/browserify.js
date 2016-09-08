4cc40b181f60ac09ecd0ee8d8f49a9c4cdcade17
Anton Timmermans <email@atimmer.com>
2016-09-08 14:00:51 +0200

    Bump asset version numbers

--- premium/grunt/config/browserify.js
+++ premium/grunt/config/browserify.js
@@ -11,9 +11,9 @@ module.exports = {
 			"assets/js/dist/wpseo-premium-contact-support-350.js": [ "assets/js/src/contact-support.js" ],
 			"assets/js/dist/wp-seo-premium-admin-redirects-350.js": [ "assets/js/src/admin-redirects.js" ],
 			"assets/js/dist/wp-seo-premium-custom-fields-plugin-350.js": [ "assets/js/src/custom-fields-plugin.js" ],
-			"assets/js/dist/wp-seo-premium-quickedit-notification-350.js": [ "assets/js/src/quickedit-notification.js" ],
+			"assets/js/dist/wp-seo-premium-quickedit-notification-352.js": [ "assets/js/src/quickedit-notification.js" ],
 			"assets/js/dist/wpseo-premium-yoast-overlay-350.js": [ "assets/js/src/yoast-overlay.js" ],
-			"assets/js/dist/wp-seo-premium-redirect-notifications-350.js": [ "assets/js/src/redirect-notifications.js" ],
+			"assets/js/dist/wp-seo-premium-redirect-notifications-352.js": [ "assets/js/src/redirect-notifications.js" ],
 		},
 	},
 };
