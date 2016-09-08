5559bf117883d8dcb272eed9f76591a3e72ede54
Andy Meerwaldt <andy@yoast.com>
2016-09-08 14:17:32 +0200

    Fix codestyle issue.

--- admin/config-ui/components/class-component-connect-google-search-console.php
+++ admin/config-ui/components/class-component-connect-google-search-console.php
@@ -130,7 +130,7 @@ class WPSEO_Config_Component_Connect_Google_Search_Console implements WPSEO_Conf
 	protected function get_profilelist() {
 		$profiles = array();
 		$sites    = $this->gsc_service->get_sites();
-		foreach( $sites as $siteKey => $siteValue ) {
+		foreach ( $sites as $siteKey => $siteValue ) {
 			$profiles[ untrailingslashit( $siteKey )  ] = untrailingslashit( $siteValue );
 		}
 
