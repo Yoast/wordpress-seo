76e035f99e9932e15802cb96951a6cb902d66e18
Anton Timmermans <email@atimmer.com>
2016-09-16 14:47:21 +0200

    Change copy of several explanations on wizard

--- admin/config-ui/fields/class-field-upsell-configuration-service.php
+++ admin/config-ui/fields/class-field-upsell-configuration-service.php
@@ -16,18 +16,15 @@ class WPSEO_Config_Field_Upsell_Configuration_Service extends WPSEO_Config_Field
 
 		$intro_text = __( 'Welcome to the Yoast SEO installation wizard. In a few simple steps we\'ll help you configure your SEO settings to match your website\'s needs!', 'wordpress-seo' );
 
-		$upsell_text = __( 'As a service to our Premium customers, we offer the Yoast SEO configuration service. Setting up Yoast SEO Premium can be a daunting task if you’re not into the ins and outs of SEO. Although we try to make it as simple as possible, we do realize that it might not be “for you”. For this reason we offer the Yoast SEO configuration service. We’ll set up Yoast SEO Premium for you and make sure it has the correct settings for your site!', 'wordpress-seo' );
-
 		/* Translators: %1$s opens the link, %2$s closes the link. */
-		$call_to_action = sprintf(
-			__( 'Order %1$sYoast SEO Premium with our configuration service%2$s here!', 'wordpress-seo' ),
+		$upsell_text = sprintf(
+			__( 'While we strive to make setting up Yoast SEO as easy as possible, we understand it can be daunting. If you’d rather have us set up Yoast SEO for you (and get a copy of Yoast SEO Premium in the process), order our %1$sYoast SEO configuration service%2$s here!', 'wordpress-seo' ),
 			'<a target="_blank" href="https://yoa.st/configuration-package">',
 			'</a>'
 		);
 
 		$html = '<p>' . $intro_text . '</p>';
 		$html .= '<p><em>' . $upsell_text . '</em></p>';
-		$html .= '<p><em>' . $call_to_action . '</em></p>';
 
 
 		$this->set_property( 'html', $html );
