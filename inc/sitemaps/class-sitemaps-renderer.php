a45bae65451030b03dcdb497a20bbb83c5dcb20f
stodorovic <sasa.todorovic@gmx.com>
2016-11-26 09:38:45 +0100

    Add XML schema for image sitemap

--- inc/sitemaps/class-sitemaps-renderer.php
+++ inc/sitemaps/class-sitemaps-renderer.php
@@ -79,7 +79,8 @@ class WPSEO_Sitemaps_Renderer {
 	public function get_sitemap( $links, $type, $current_page ) {
 
 		$urlset = '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" '
-			. 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd" '
+			. 'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd '
+			.                     'http://www.google.com/schemas/sitemap-image/1.1 http://www.google.com/schemas/sitemap-image/1.1/sitemap-image.xsd" '
 			. 'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
 
 		/**
