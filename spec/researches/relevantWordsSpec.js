0d9c2fd16d52b43d6c85b51dd10e7f723542c6b9
IreneStr <irene@yoast.com>
2016-11-28 16:09:33 +0100

    Fixes unittests

--- spec/researches/relevantWordsSpec.js
+++ spec/researches/relevantWordsSpec.js
@@ -1,18 +1,19 @@
 var relevantWordsResearch = require( "../../js/researches/relevantWords" );
 var Paper = require( "../../js/values/Paper" );
 var WordCombination = require( "../../js/values/WordCombination" );
+var functionWords = require( "../../js/researches/english/functionWords.js" )().all;
 
 describe( "relevantWords research", function() {
 
 	it( "calls through to the string processing function", function() {
 		var input = "Here are a ton of syllables. Syllables are very important. I think the syllable combinations are even more important. Syllable combinations for the win!";
-		var input = new Paper( input );
+		input = new Paper( input );
 		var expected = [
-			new WordCombination( [ "syllable", "combinations" ], 2 ),
-			new WordCombination( [ "syllables" ], 2 ),
-			new WordCombination( [ "important" ], 2 ),
-			new WordCombination( [ "syllable" ], 2 ),
-			new WordCombination( [ "combinations" ], 2 ),
+			new WordCombination( [ "syllable", "combinations" ], 2, functionWords ),
+			new WordCombination( [ "syllables" ], 2, functionWords ),
+			new WordCombination( [ "important" ], 2, functionWords ),
+			new WordCombination( [ "syllable" ], 2, functionWords ),
+			new WordCombination( [ "combinations" ], 2, functionWords ),
 		];
 
 		// Make sure our words aren't filtered by density.
