cca002a8f7d8693961710f2f849bf67d9cd1d063
terw-dan <danny@yoast.com>
2016-11-30 11:48:17 +0100

    Change user locale

--- admin/formatter/class-metabox-formatter.php
+++ admin/formatter/class-metabox-formatter.php
@@ -87,7 +87,7 @@ class WPSEO_Metabox_Formatter {
 	 * @return array
 	 */
 	private function get_translations() {
-		$file = plugin_dir_path( WPSEO_FILE ) . 'languages/wordpress-seo-' . get_locale() . '.json';
+		$file = plugin_dir_path( WPSEO_FILE ) . 'languages/wordpress-seo-' . get_user_locale() . '.json';
 		if ( file_exists( $file ) && $file = file_get_contents( $file ) ) {
 			return json_decode( $file, true );
 		}
