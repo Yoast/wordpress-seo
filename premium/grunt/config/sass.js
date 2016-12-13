9392535cf79d810d1d3eeceb320d35766c58018c
Anton Timmermans <email@atimmer.com>
2016-12-13 13:30:56 +0100

    Bump CSS asset version number

--- premium/grunt/config/sass.js
+++ premium/grunt/config/sass.js
@@ -4,7 +4,7 @@
 module.exports = {
 	build: {
 		files: {
-			"<%= paths.css %>premium-metabox-380.css": [ "assets/css/src/metabox.scss" ],
+			"<%= paths.css %>premium-metabox-400.css": [ "assets/css/src/metabox.scss" ],
 			"<%= paths.css %>premium-redirects-340.css": [ "assets/css/src/redirects.scss" ],
 			"<%= paths.css %>premium-social-preview-340.css": [ "assets/css/src/social-preview.scss" ],
 		},
