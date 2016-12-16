af2925a896b4fc55b5773170fb97a6e4faa55516
Andrea Fercia <a.fercia@gmail.com>
2016-12-16 11:13:08 +0100

    Better wording.

--- premium/classes/class-premium-prominent-words-recalculation.php
+++ premium/classes/class-premium-prominent-words-recalculation.php
@@ -45,7 +45,7 @@ class WPSEO_Premium_Prominent_Words_Recalculation implements WPSEO_WordPress_Int
 				<?php
 				printf(
 					/* translators: %1$s expands to a link start tag to the Yoast.com website, %2$s is the link closing tag. */
-					__( 'The Site wide analysis can help you get the most out of %1$sthe internal linking tool%2$s and calculate the most prominent words for your complete site.', 'wordpress-seo-premium' ),
+					__( 'The Site wide analysis can help you get the most out of %1$sthe internal linking tool%2$s and calculate the most prominent words for your site.', 'wordpress-seo-premium' ),
 					'<a href="https://yoa.st/oj" target="_blank">',
 					'</a>'
 				);
