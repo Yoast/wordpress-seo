9a1a7256dbe34286923cd8a452420eb118b0a513
IreneStr <irene@yoast.com>
2016-12-21 12:15:02 +0100

    Move let

--- spec/errors/missingArgumentSpec.js
+++ spec/errors/missingArgumentSpec.js
@@ -1,8 +1,8 @@
 import MissingArgumentError from "../../js/errors/missingArgument.js";
 
 describe( "missing argument error", function() {
-	let error = new MissingArgumentError( "Error" );
 	it( "has the correct name and message", function() {
+		let error = new MissingArgumentError( "Error" );
 		expect ( error.message ).toBe( "Error" );
 		expect ( error.name ).toBe( "MissingArgumentError" );
 	} );
