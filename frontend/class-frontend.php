5d30b47e6462bcbe701b4dd8e52b0573f3199a2f
Lars Jensen <lars.jensen@exenova.dk>
2017-01-07 14:30:42 +0100

    I am not able to reduce complexity of function unless I do more extensive changes to the function. This may be overkill for this simple additional option.

--- frontend/class-frontend.php
+++ frontend/class-frontend.php
@@ -1228,19 +1228,19 @@ class WPSEO_Frontend {
 		 */
 		$keywords = apply_filters( 'wpseo_metakeywords', trim( $keywords ) ); // More appropriately named.
 
-		
-		if ( is_string( $keywords ) && $keywords !== '' ) {
-			if ( $echo !== false ) {
+		if ( $echo !== false ) {
+			if ( is_string( $keywords ) && $keywords !== '' ) {
 				echo '<meta name="keywords" content="', esc_attr( strip_tags( stripslashes( $keywords ) ) ), '"/>', "\n";
 			}
-			elseif ( $echo === false ){
-				return esc_attr( strip_tags( stripslashes( $keywords ) ) ); 
-			} 
 		}
-
-		if ( $echo === false ){
-			return ''; 
-		}  
+		else {
+			if ( is_string( $keywords ) && $keywords !== '' ) {
+				return esc_attr( strip_tags( stripslashes( $keywords ) ) );
+			}
+			else {
+				return '';
+			}
+		}
 	}
 
 	/**
