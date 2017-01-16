cb50070c6bd78142b42de4fb04bb5a5bc16bd290
Anton Timmermans <email@atimmer.com>
2017-01-16 11:06:37 +0100

    Fix error where this wasn't correctly set
    This was caused by letting babel run over the code. Probably before
    babel `this` would be implicitly set somewhere else to the correct
    value.

--- js/snippetPreview.js
+++ js/snippetPreview.js
@@ -926,14 +926,16 @@ SnippetPreview.prototype.updateProgressBars = function() {
 	titleRating = rateTitleLength( this.data.titleWidth );
 	metaDescriptionRating = rateMetaDescLength( metaDescription.length );
 
-	updateProgressBar(
+	updateProgressBar.call(
+		this,
 		this.element.progress.title,
 		this.data.titleWidth,
 		titleMaxLength,
 		titleRating
 	);
 
-	updateProgressBar(
+	updateProgressBar.call(
+		this,
 		this.element.progress.metaDesc,
 		metaDescription.length,
 		metadescriptionMaxLength,
