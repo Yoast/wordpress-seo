369604861ee5c2201ac8c734c81721d5349c0e4e
Anton Timmermans <email@atimmer.com>
2017-01-17 13:05:11 +0100

    Bump JS asset version numbers

--- premium/grunt/config/browserify.js
+++ premium/grunt/config/browserify.js
@@ -9,15 +9,15 @@ module.exports = {
 			},
 		},
 		files: {
-			"assets/js/dist/yoast-premium-site-wide-analysis-402.js": [ "assets/js/src/site-wide-analysis.js" ],
-			"assets/js/dist/yoast-premium-social-preview-390.js": [ "assets/js/src/yoast-premium-social-preview.js" ],
+			"assets/js/dist/yoast-premium-site-wide-analysis-410.js": [ "assets/js/src/site-wide-analysis.js" ],
+			"assets/js/dist/yoast-premium-social-preview-410.js": [ "assets/js/src/yoast-premium-social-preview.js" ],
 			"assets/js/dist/wpseo-premium-contact-support-370.js": [ "assets/js/src/contact-support.js" ],
 			"assets/js/dist/wp-seo-premium-admin-redirects-370.js": [ "assets/js/src/admin-redirects.js" ],
 			"assets/js/dist/wp-seo-premium-custom-fields-plugin-350.js": [ "assets/js/src/custom-fields-plugin.js" ],
 			"assets/js/dist/wp-seo-premium-quickedit-notification-352.js": [ "assets/js/src/quickedit-notification.js" ],
 			"assets/js/dist/wpseo-premium-yoast-overlay-350.js": [ "assets/js/src/yoast-overlay.js" ],
 			"assets/js/dist/wp-seo-premium-redirect-notifications-352.js": [ "assets/js/src/redirect-notifications.js" ],
-			"assets/js/dist/wp-seo-premium-metabox-402.js": [ "assets/js/src/metabox.js" ],
+			"assets/js/dist/wp-seo-premium-metabox-410.js": [ "assets/js/src/metabox.js" ],
 		},
 	},
 	"release-es6": {
