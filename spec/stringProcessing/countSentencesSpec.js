3a458101cfddda259cca6ab6edc0c1d2d3911ab1
terw-dan <danny@yoast.com>
2017-01-17 16:54:12 +0100

    Adds spec

--- spec/stringProcessing/countSentencesSpec.js
+++ spec/stringProcessing/countSentencesSpec.js
@@ -3,6 +3,8 @@ var sentenceCount = require("../../js/stringProcessing/countSentences.js");
 describe("Counting of sentences", function(){
 	it("returns the number of sentences in a string", function(){
 		expect( sentenceCount( "First sentence. Second sentence. 3rd sentence." ) ).toBe( 3 );
+		expect( sentenceCount( "" ) ).toBe( 0 );
+
 
 		// These don't work yet because of the repeated sentence terminators, but they aren't important right now.
 		// expect( sentenceCount( "First sentence.. Second sentence. 3rd sentence." ) ).toBe( 3 );
