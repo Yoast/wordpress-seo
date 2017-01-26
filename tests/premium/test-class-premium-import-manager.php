3406e0f10e94d5b5beb552ae74b7ac3ab0d5aaf6
Andy Meerwaldt <andy@yoast.com>
2017-01-26 10:23:16 +0100

    Add tests for redirects with quotes in the origin.

--- tests/premium/test-class-premium-import-manager.php
+++ tests/premium/test-class-premium-import-manager.php
@@ -14,7 +14,18 @@ class WPSEO_Premium_Import_Manager_Test extends WPSEO_UnitTestCase {
 	 *
 	 * @dataProvider plain_redirect_provider
 	 */
-	public function test_match_redirect_regex( $regex, $htaccess, $expected ) {
+	public function test_match_plain_redirects_regex_without_quotes( $regex, $htaccess, $expected ) {
+		$instance = new WPSEO_Premium_Import_Manager_Double();
+		$match = $instance->match_with_given_regex( $regex , $htaccess );
+
+		$this->assertEquals( $expected, $match[0][3] );
+
+	}
+	/**
+	 *
+	 * @dataProvider plain_redirect_provider_with_quotes
+	 */
+	public function test_match_plain_redirects_regex_with_quotes( $regex, $htaccess, $expected ) {
 		$instance = new WPSEO_Premium_Import_Manager_Double();
 		$match = $instance->match_with_given_regex( $regex , $htaccess );
 
@@ -27,41 +38,72 @@ class WPSEO_Premium_Import_Manager_Test extends WPSEO_UnitTestCase {
 		$regex_to_match = '`^Redirect ([0-9]{3}) ([^"\s]+) ([a-z0-9-_+/.:%&?=#\][]+)`im';
 
 		return array(
-			// Mentioned in https://github.com/Yoast/wordpress-seo-premium/issues/934
+			// Mentioned in https://github.com/Yoast/wordpress-seo-premium/issues/934.
 			array(
 				$regex_to_match,
 				'Redirect 301 /denver-balayage-sal/ https://dothebangthingsalon.com/denver-balayage-salon/',
-				'https://dothebangthingsalon.com/denver-balayage-salon/'
+				'https://dothebangthingsalon.com/denver-balayage-salon/',
 			),
-			// Mentioned in https://github.com/Yoast/wordpress-seo-premium/issues/934
+			// Mentioned in https://github.com/Yoast/wordpress-seo-premium/issues/934.
 			array(
 				$regex_to_match,
 				'Redirect 301 /video/endoscopic-repair-of-tracheal-bronchial-sinus-tract/nqf3kv0qyp /video/endoscopic-repair-of-tracheal-bronchial-sinus-tract/',
-				'/video/endoscopic-repair-of-tracheal-bronchial-sinus-tract/'
+				'/video/endoscopic-repair-of-tracheal-bronchial-sinus-tract/',
 			),
 
 			// Try to match the other ones.
 			array(
 				$regex_to_match,
 				'Redirect 301 redirect-this https://to-an-url-starting-with/123-numeric-value',
-				'https://to-an-url-starting-with/123-numeric-value'
+				'https://to-an-url-starting-with/123-numeric-value',
 			),
 			array(
 				$regex_to_match,
 				'Redirect 301 redirect-this /with.html?attibute=one&another_attribute=two',
-				'/with.html?attibute=one&another_attribute=two'
+				'/with.html?attibute=one&another_attribute=two',
 			),
 			array(
 				$regex_to_match,
 				'Redirect 301 redirect-this /with.html?attibute[]=123',
-				'/with.html?attibute[]=123'
+				'/with.html?attibute[]=123',
 			),
-
 		);
-
-
-
 	}
 
 
+	public function plain_redirect_provider_with_quotes(  ) {
+		$regex_to_match = '`^Redirect ([0-9]{3}) "([^"]+)" ([a-z0-9-_+/.:%&?=#\][]+)`im';
+
+		return array(
+			// Mentioned in https://github.com/Yoast/wordpress-seo-premium/issues/934
+			array(
+				$regex_to_match,
+				'Redirect 301 "/denver-balayage-sal/" https://dothebangthingsalon.com/denver-balayage-salon/',
+				'https://dothebangthingsalon.com/denver-balayage-salon/',
+			),
+			// Mentioned in https://github.com/Yoast/wordpress-seo-premium/issues/934
+			array(
+				$regex_to_match,
+				'Redirect 301 "/video/endoscopic-repair-of-tracheal-bronchial-sinus-tract/nqf3kv0qyp" /video/endoscopic-repair-of-tracheal-bronchial-sinus-tract/',
+				'/video/endoscopic-repair-of-tracheal-bronchial-sinus-tract/',
+			),
+
+			// Try to match the other ones.
+			array(
+				$regex_to_match,
+				'Redirect 301 "redirect-this" https://to-an-url-starting-with/123-numeric-value',
+				'https://to-an-url-starting-with/123-numeric-value',
+			),
+			array(
+				$regex_to_match,
+				'Redirect 301 "redirect-this" /with.html?attibute=one&another_attribute=two',
+				'/with.html?attibute=one&another_attribute=two',
+			),
+			array(
+				$regex_to_match,
+				'Redirect 301 "redirect-this" /with.html?attibute[]=123',
+				'/with.html?attibute[]=123',
+			),
+		);
+	}
 }
