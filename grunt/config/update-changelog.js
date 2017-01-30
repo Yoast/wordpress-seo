4025bf3480d032b9fb4e8f836777b2b064ac5b2f
Anton Timmermans <email@atimmer.com>
2017-01-30 15:44:56 +0100

    Fix fatal error in grunt script

--- grunt/config/update-changelog.js
+++ grunt/config/update-changelog.js
@@ -2,7 +2,7 @@
 module.exports = {
 	options: {
 		version: "<%= pluginVersion %>",
-		regEx: new RegExp( "(= " + function(version){console.log(version);return version;}(pluginVersion) + " =(\n.*)*?)(\n\n=)" ),
+		// regEx: new RegExp( "(= " + function(version){console.log(version);return version;}(pluginVersion) + " =(\n.*)*?)(\n\n=)" ),
 		matchGroupIndex: 1,
 		readmePath: "readme.txt"
 	}
