8632059676f0374cab8a6b3c6fadd9dba3ad6c19
Andrea Fercia <a.fercia@gmail.com>
2017-02-02 10:25:22 +0100

    Make eslint happy.

--- grunt/config/update-changelog.js
+++ grunt/config/update-changelog.js
@@ -2,8 +2,8 @@
 module.exports = {
 	options: {
 		version: "<%= pluginVersion %>",
-		// regEx: new RegExp( "(= " + function(version){console.log(version);return version;}(pluginVersion) + " =(\n.*)*?)(\n\n=)" ),
+		// RegEx: new RegExp( "(= " + function(version){console.log(version);return version;}(pluginVersion) + " =(\n.*)*?)(\n\n=)" ),
 		matchGroupIndex: 1,
-		readmePath: "readme.txt"
-	}
+		readmePath: "readme.txt",
+	},
 };
