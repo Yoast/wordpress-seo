29394f1ca911af89c61b087ab6f1dfeb8bb95331
Andy Meerwaldt <andy@yoast.com>
2017-02-14 14:52:29 +0100

    Enqueue assets to the search console page.

---
+++ premium/classes/class-premium-gsc.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package WPSEO\Premium\Classes
+ */
+
+/**
+ * Registers the premium WordPress implementation of Google Search Console
+ */
+class WPSEO_Premium_GSC implements WPSEO_WordPress_Integration {
+
+	/**
+	 * Registers all hooks to WordPress
+	 */
+	public function register_hooks() {
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue' ) );
+	}
+
+	/**
+	 * Enqueues site wide analysis script
+	 */
+	public function enqueue() {
+		$page = filter_input( INPUT_GET, 'page' );
+
+		wp_register_script( WPSEO_Admin_Asset_Manager::PREFIX . 'premium-gsc', plugin_dir_url( WPSEO_PREMIUM_FILE ) . 'assets/js/dist/yoast-premium-gsc-440' . WPSEO_CSSJS_SUFFIX . '.js', array(), WPSEO_VERSION, true );
+
+		if ( $page !== 'wpseo_search_console' ) {
+			return;
+		}
+
+		$data = array(
+			'restApi' => array(
+				'root' => esc_url_raw( rest_url() ),
+				'nonce' => wp_create_nonce( 'wp_rest' ),
+			),
+		);
+
+		wp_enqueue_script( WPSEO_Admin_Asset_Manager::PREFIX . 'premium-gsc' );
+		wp_localize_script( WPSEO_Admin_Asset_Manager::PREFIX . 'premium-gsc', 'yoastPremiumGSC', array( 'data' => $data ) );
+	}
+}
