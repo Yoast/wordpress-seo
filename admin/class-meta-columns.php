85b4e83a7ddcac28d340a91be66504f069c666fa
Thijs Hulshof <thijsh@yoast.com>
2017-02-24 10:13:51 +0100

    Changes references from wpseo_titles to wpseo

--- admin/class-meta-columns.php
+++ admin/class-meta-columns.php
@@ -36,9 +36,9 @@ class WPSEO_Meta_Columns {
 	public function setup_hooks() {
 		$this->set_post_type_hooks();
 
-		$options = WPSEO_Options::get_option( 'wpseo_titles' );
+		$options = WPSEO_Options::get_option( 'wpseo' );
 
-		if ( ! empty( $options['keyword-analysis-active'] ) ) {
+		if ( ! empty( $options['keyword_analysis_active'] ) ) {
 			add_action( 'restrict_manage_posts', array( $this, 'posts_filter_dropdown' ) );
 		}
 
