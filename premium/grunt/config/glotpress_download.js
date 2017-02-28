b5c6dbd8c7bbf3ef6bd88a7d68a63a104dc72e9d
Anton Timmermans <email@atimmer.com>
2017-02-28 12:15:13 +0100

    Download free plugin translations on release step
    Fixes #1082

--- premium/grunt/config/glotpress_download.js
+++ premium/grunt/config/glotpress_download.js
@@ -1,18 +1,39 @@
 // https://github.com/markoheijnen/grunt-glotpress
-module.exports = {
-	plugin: {
-		options: {
-			url: '<%= pkg.plugin.glotpress %>',
-			domainPath: '<%= paths.languages %>',
-			file_format: '%domainPath%/%textdomain%-%wp_locale%.%format%',
-			slug: '<%= pkg.plugin.slug %>',
-			textdomain: '<%= pkg.plugin.textdomain %>',
-			formats: [ 'mo', 'po' ],
-			filter: {
-				translation_sets: false,
-				minimum_percentage: 50,
-				waiting_strings: false
-			}
-		}
-	}
+module.exports = ( grunt ) => {
+	let freePackage = grunt.file.readJSON( "../package.json" );
+	const fileFormat = "%domainPath%/%textdomain%-%wp_locale%.%format%";
+
+	return {
+		plugin: {
+			options: {
+				url: "<%= pkg.plugin.glotpress %>",
+				domainPath: "<%= paths.languages %>",
+				file_format: fileFormat,
+				slug: "<%= pkg.plugin.slug %>",
+				textdomain: "<%= pkg.plugin.textdomain %>",
+				formats: [ "mo", "po" ],
+				filter: {
+					translation_sets: false,
+					minimum_percentage: 50,
+					waiting_strings: false
+				},
+			},
+		},
+		freePhpTranslations: {
+			options: {
+				url: freePackage.plugin.glotpress,
+				domainPath: "../languages/",
+				file_format: fileFormat,
+				slug: "wp-plugins/" + freePackage.plugin.textdomain + "/dev/",
+				textdomain: freePackage.plugin.textdomain,
+				formats: [ "mo" ],
+				filter: {
+					translation_sets: false,
+					// Use the same percentage as WordPress
+					minimum_percentage: 95,
+					waiting_strings: false,
+				},
+			},
+		},
+	};
 };
