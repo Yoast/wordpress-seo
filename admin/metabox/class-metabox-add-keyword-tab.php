61fee3a37d062e04476806649d3125ecbb25dad0
Andy Meerwaldt <andy@yoast.com>
2017-03-13 14:45:23 +0100

    Change button value

--- admin/metabox/class-metabox-add-keyword-tab.php
+++ admin/metabox/class-metabox-add-keyword-tab.php
@@ -21,7 +21,7 @@ class Metabox_Add_Keyword_Tab implements WPSEO_Metabox_Tab {
 		ob_start();
 		?>
         <li class="wpseo-tab-add-keyword">
-            <button type="button" class="wpseo-add-keyword button">+ <?php _e( 'Add more keywords', 'wordpress-seo' ); ?></button>
+            <button type="button" class="wpseo-add-keyword button">+ <?php _e( 'Add keyword', 'wordpress-seo' ); ?></button>
         </li>
 
 		<?php
