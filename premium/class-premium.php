5984e0c69619713ef45c6adfebcfbf7830b07230
Anton Timmermans <email@atimmer.com>
2017-03-21 11:45:52 +0100

    Bump version numbers

--- premium/class-premium.php
+++ premium/class-premium.php
@@ -23,7 +23,7 @@ class WPSEO_Premium {
 
 	const OPTION_CURRENT_VERSION = 'wpseo_current_version';
 
-	const PLUGIN_VERSION_NAME = '4.4';
+	const PLUGIN_VERSION_NAME = '4.5';
 	const PLUGIN_VERSION_CODE = '16';
 	const PLUGIN_AUTHOR = 'Yoast';
 	const EDD_STORE_URL = 'http://my.yoast.com';
