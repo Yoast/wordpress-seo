e607bcada70d6b2dc50747188b30d3585edf31a0
Jip Moors <jhp.moors@gmail.com>
2017-03-28 11:02:51 +0200

    Fix unit tests

--- tests/config-ui/test-class-configuration-structure.php
+++ tests/config-ui/test-class-configuration-structure.php
@@ -52,6 +52,8 @@ class WPSEO_Configuration_Structure_Test extends PHPUnit_Framework_TestCase {
 			'multipleAuthors',
 			'connectGoogleSearchConsole',
 			'titleTemplate',
+			'newsletterSignup',
+			'suggestions',
 			'success',
 		);
 
