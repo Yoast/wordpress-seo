7031479134e77dea91efdab6b8c1108791e57e5e
Andy Meerwaldt <andy@yoast.com>
2017-03-29 14:58:21 +0200

    Mix the use of post types.

--- tests/premium/test-class-premium-prominent-words-unindexed-post-query.php
+++ tests/premium/test-class-premium-prominent-words-unindexed-post-query.php
@@ -5,8 +5,6 @@
 
 /**
  * Test class for the unindexed posts query.
- *
- * @group test
  */
 class WPSEO_Premium_Prominent_Words_Unindexed_Post_Query_Test extends WPSEO_UnitTestCase {
 
@@ -38,10 +36,21 @@ class WPSEO_Premium_Prominent_Words_Unindexed_Post_Query_Test extends WPSEO_Unit
 		$this->assertFalse( $this->class_instance->exceeds_limit( 10 ) );
 	}
 
+
 	/**
-	 * Tests the exceeds_limit with 20 posts results in an exceeded limit.
+	 * Tests the exceeds_limit with 10 posts and pages results in a non exceeded limit
 	 */
-	public function test_exceeds_limit_with_20_posts() {
+	public function test_exceeds_limit_with_10_posts_and_pages() {
+		$this->factory()->post->create_many( 4, array( 'post_type' => 'post' ) );
+		$this->factory()->post->create_many( 4, array( 'post_type' => 'page' ) );
+
+		$this->assertFalse( $this->class_instance->exceeds_limit( 10 ) );
+	}
+
+	/**
+	 * Tests the exceeds_limit with 20 posts and pages results in an exceeded limit.
+	 */
+	public function test_exceeds_limit_with_20_posts_and_pages() {
 		$this->factory()->post->create_many( 20 );
 
 		$this->assertTrue( $this->class_instance->exceeds_limit( 10 ) );
@@ -65,5 +74,20 @@ class WPSEO_Premium_Prominent_Words_Unindexed_Post_Query_Test extends WPSEO_Unit
 		}
 
 		$this->assertEquals( 10, $this->class_instance->get_total( 'post' ) );
+	}
+
+	/**
+	 * Tests get_total with a couple of posts present.
+	 */
+	public function test_get_total_with_posts_and_pages() {
+		$posts = $this->factory()->post->create_many( 10 );
+		$this->factory()->post->create_many( 8, array( 'post_type' => 'page' ) );
+
+		foreach ( $posts as $post_id ) {
+			delete_post_meta( $post_id, WPSEO_Premium_Prominent_Words_Versioning::POST_META_NAME );
+		}
+
+		$this->assertEquals( 10, $this->class_instance->get_total( 'post' ) );
+		$this->assertEquals( 8, $this->class_instance->get_total( 'page' ) );
 	}
 }
