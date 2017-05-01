a81a5d21706f30c4ec2df6bbe86545813da82cdb
Andrea Fercia <a.fercia@gmail.com>
2017-05-01 13:03:03 +0200

    Fix tests.

--- spec/fields/textFieldSpec.js
+++ spec/fields/textFieldSpec.js
@@ -7,14 +7,14 @@ describe( 'a text field', function() {
 			value: "value"
 		});
 
-		expect( textField.render() ).toBe( '<label><input type="text" value="value" /></label>' );
+		expect( textField.render() ).toBe( '<label><span class="snippet-editor__caret-hook"></span><input type="text" value="value" /></label>' );
 	});
 
 	it( 'should accept a changed value', function() {
 		var textField = new TextField();
 		textField.setValue( "value" );
 
-		expect( textField.render() ).toBe( '<label><input type="text" value="value" /></label>' );
+		expect( textField.render() ).toBe( '<label><span class="snippet-editor__caret-hook"></span><input type="text" value="value" /></label>' );
 	});
 
 	it( 'should accept a placeholder', function() {
@@ -22,7 +22,7 @@ describe( 'a text field', function() {
 			placeholder: "placeholder"
 		});
 
-		expect( textField.render() ).toBe( '<label><input type="text" placeholder="placeholder" /></label>' );
+		expect( textField.render() ).toBe( '<label><span class="snippet-editor__caret-hook"></span><input type="text" placeholder="placeholder" /></label>' );
 	});
 
 	it( 'should accept a name', function() {
@@ -30,7 +30,7 @@ describe( 'a text field', function() {
 			name: "name"
 		});
 
-		expect( textField.render() ).toBe( '<label><input type="text" name="name" /></label>' );
+		expect( textField.render() ).toBe( '<label><span class="snippet-editor__caret-hook"></span><input type="text" name="name" /></label>' );
 	});
 
 	it( 'should accept an id', function() {
@@ -38,7 +38,7 @@ describe( 'a text field', function() {
 			id: "id"
 		});
 
-		expect( textField.render() ).toBe( '<label for="id"><input type="text" id="id" /></label>' );
+		expect( textField.render() ).toBe( '<label for="id"></label><span class="snippet-editor__caret-hook" id="id__caret-hook"></span><input type="text" id="id" />' );
 	});
 
 	it( 'should accept a class', function() {
@@ -46,14 +46,14 @@ describe( 'a text field', function() {
 			className: "class"
 		});
 
-		expect( textField.render() ).toBe( '<label><input type="text" class="class" /></label>' );
+		expect( textField.render() ).toBe( '<label><span class="snippet-editor__caret-hook"></span><input type="text" class="class" /></label>' );
 	});
 
 	it( 'should accept a changed class', function() {
 		var textField = new TextField();
 		textField.setClassName( "class" );
 
-		expect( textField.render() ).toBe( '<label><input type="text" class="class" /></label>' );
+		expect( textField.render() ).toBe( '<label><span class="snippet-editor__caret-hook"></span><input type="text" class="class" /></label>' );
 	});
 
 	it( 'should accept a title', function() {
@@ -61,7 +61,7 @@ describe( 'a text field', function() {
 			title: "title"
 		});
 
-		expect( textField.render() ).toBe( '<label>title<input type="text" /></label>' );
+		expect( textField.render() ).toBe( '<label>title<span class="snippet-editor__caret-hook"></span><input type="text" /></label>' );
 	});
 
 	it( 'should accept multiple values', function() {
@@ -71,7 +71,7 @@ describe( 'a text field', function() {
 			id: "id"
 		});
 
-		expect( textField.render() ).toBe( '<label for="id">title<input type="text" value="value" id="id" /></label>' );
+		expect( textField.render() ).toBe( '<label for="id">title</label><span class="snippet-editor__caret-hook" id="id__caret-hook"></span><input type="text" value="value" id="id" />' );
 	});
 
 	it( 'should accept a label class', function() {
@@ -79,6 +79,6 @@ describe( 'a text field', function() {
 			labelClassName: "label-class"
 		});
 
-		expect( textField.render() ).toBe( '<label class="label-class"><input type="text" /></label>' );
+		expect( textField.render() ).toBe( '<label class="label-class"><span class="snippet-editor__caret-hook"></span><input type="text" /></label>' );
 	});
 });
