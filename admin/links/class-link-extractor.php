e2838cf598a63b1d9fa510f9fd307b12ca49539f
Andy Meerwaldt <andy@yoast.com>
2017-06-12 11:32:57 +0200

    Added a class for extracting links from a text.

---
+++ admin/links/class-link-extractor.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @package WPSEO\Admin\Links
+ */
+
+/**
+ * Represents the link extractor.
+ */
+class WPSEO_Link_Extractor {
+
+	/** @var string */
+	protected $content;
+
+	/**
+	 * Sets the content.
+	 *
+	 * @param string $content The content to extract the links from.
+	 */
+	public function __construct( $content ) {
+		$this->content = $content;
+	}
+
+	/**
+	 * Extracts the hrefs from the content and returns them as an array.
+	 *
+	 * @return array All the extracted links
+	 */
+	public function extract() {
+		$regexp = "<a\s[^>]*href=(\"??)([^\" >]*?)\\1[^>]*>.*<\/a>";
+		$links  = array();
+		if ( preg_match_all("/$regexp/siU", $this->content, $matches, PREG_SET_ORDER ) ) {
+
+			foreach ( $matches as $match ) {
+				$links[] = $match[2];
+			}
+		}
+
+		return $links;
+	}
+}
