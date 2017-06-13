87821f791d84e350358070b7464e770d349ea90c
Andy Meerwaldt <andy@yoast.com>
2017-06-13 14:43:12 +0200

    Checks if a post is processable

--- admin/links/class-link-watcher.php
+++ admin/links/class-link-watcher.php
@@ -22,6 +22,11 @@ class WPSEO_Link_Watcher {
 	 * @param WP_Post $post    The post object.
 	 */
 	public function save_post( $post_id, WP_Post $post ) {
+
+		if ( ! $this->is_processable( $post_id ) ) {
+			return;
+		}
+
 		$content = $post->post_content;
 		$content = apply_filters( 'the_content', $content );
 		$content = str_replace( ']]>', ']]&gt;', $content );
@@ -32,5 +37,33 @@ class WPSEO_Link_Watcher {
 
 		$processor = new WPSEO_Link_Content_Processor();
 		$processor->process( $post_id, $content );
+	}
+
+	/**
+	 * Checks if the post is accessible.
+	 *
+	 * @param int $post_id The post id.
+	 *
+	 * @return bool True when the post is accessible.
+	 */
+	protected function is_processable( $post_id ) {
+		// When the post is a revision.
+		if ( wp_is_post_revision( $post_id ) ) {
+			return false;
+		}
+
+		// When the post status is not publish.
+		if ( get_post_status( $post_id ) !== 'publish' ) {
+			return false;
+		}
+
+		// When the post type is not public.
+		$post_type = get_post_type( $post_id );
+		$post_type_object = get_post_type_object( $post_type );
+		if ( $post_type_object->public === false ) {
+			return false;
+		}
+
+		return true;
 	}
 }
