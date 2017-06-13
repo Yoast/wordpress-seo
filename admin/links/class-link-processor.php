4f84b9dfca0d57753bf591cd7b4469ab7e72846d
Andy Meerwaldt <andy@yoast.com>
2017-06-13 10:53:02 +0200

    Rename dependency in object

--- admin/links/class-link-processor.php
+++ admin/links/class-link-processor.php
@@ -11,37 +11,36 @@ class WPSEO_Link_Processor {
 	/** @var WPSEO_Link_Type_Classifier */
 	protected $classifier;
 
-	/** @var WPSEO_Link_Populator */
-	protected $populator;
+	/** @var WPSEO_Link_Internal_Lookup */
+	protected $internal_lookup;
 
 	/**
 	 * Sets the dependencies for this object.
 	 *
-	 * @param WPSEO_Link_Type_Classifier $classifier The classifier to use.
-	 * @param WPSEO_Link_Populator       $populator  The populator to use.
+	 * @param WPSEO_Link_Type_Classifier $classifier      The classifier to use.
+	 * @param WPSEO_Link_Internal_Lookup $internal_lookup The internal lookup to use.
 	 */
-	public function __construct( WPSEO_Link_Type_Classifier $classifier, WPSEO_Link_Populator $populator ) {
+	public function __construct( WPSEO_Link_Type_Classifier $classifier, WPSEO_Link_Internal_Lookup $internal_lookup ) {
 		$this->classifier = $classifier;
-		$this->populator  = $populator;
+		$this->internal_lookup = $internal_lookup;
 	}
 
 	/**
 	 * Formats an array of links to WPSEO_Link object.
 	 *
 	 * @param array $extracted_links The links for format.
-	 * @param int   $post_id         The post id.
 	 *
 	 * @return WPSEO_Link[] The formatted links.
 	 */
-	public function process( array $extracted_links, $post_id ) {
-		$links = array();
-		foreach ( $extracted_links as $extracted_link ) {
-			$link_type = $this->classifier->classify( $extracted_link );
-			$target_post_id = $this->populator->populate( $extracted_link, $link_type );
+	public function process( array $extracted_links ) {
+		return array_map( array( $this, 'process_link' ), $extracted_links );
+	}
 
-			$links[] = new WPSEO_Link( $extracted_link, $post_id, $target_post_id, $link_type );
-		}
+	public function process_link( $link ) {
+		$link_type = $this->classifier->classify( $link );
+		$target_post_id = $this->internal_lookup->lookup( $link, $link_type );
 
-		return $links;
+		return new WPSEO_Link( $link, $target_post_id, $link_type );
+
 	}
 }
