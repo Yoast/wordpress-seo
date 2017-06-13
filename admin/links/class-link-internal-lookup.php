2f9efdce5331d138e2bddf3bb43fbb5c00cdc0c8
Andy Meerwaldt <andy@yoast.com>
2017-06-13 10:54:36 +0200

    Get rid of populator

--- admin/links/class-link-internal-lookup.php
+++ admin/links/class-link-internal-lookup.php
@@ -4,12 +4,12 @@
  */
 
 /**
- * Represents the link populator. This class tries get the postid for a given internal link.
+ * Represents the internal link lookup. This class tries get the postid for a given internal link.
  */
 class WPSEO_Link_Internal_Lookup {
 
 	/**
-	 * Populates the given link for the given type. If type is outbound return 0 as post id.
+	 * Gets a post id for the given link for the given type. If type is outbound it returns 0 as post id.
 	 *
 	 * @param string $link The link to populate.
 	 * @param string $type The type for the link: outbound or internal.
