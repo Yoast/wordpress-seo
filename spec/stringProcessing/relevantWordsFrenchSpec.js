2a2ccb87c689019212809786cc71ba32c9e879be
IreneStr <irene@yoast.com>
2017-06-16 10:18:50 +0200

    Really add test for French relevant words

--- spec/stringProcessing/relevantWordsFrenchSpec.js
+++ spec/stringProcessing/relevantWordsFrenchSpec.js
@@ -1,36 +1,30 @@
 let WordCombination = require( "../../js/values/WordCombination" );
 let relevantWords = require( "../../js/stringProcessing/relevantWords" );
 let getRelevantWords = relevantWords.getRelevantWords;
-let spanishFunctionWords = require( "../../js/researches/spanish/functionWords.js" )().all;
+let frenchFunctionWords = require( "../../js/researches/french/functionWords.js" )().all;
 
-describe( "gets Spanish word combinations", function() {
+describe( "gets French word combinations", function() {
 	it( "returns word combinations", function() {
-		let input = "No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una " +
-			"tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar porque " +
-			"hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar " +
-			"porque hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a " +
-			"trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos " +
-			"ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta de nieve. " +
-			"No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta de " +
-			"nieve. No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta " +
-			"de nieve. No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta " +
-			"de nieve. No pudimos ir a trabajar porque hubo una tormenta de nieve. No pudimos ir a trabajar porque hubo una tormenta " +
-			"de nieve.";
+		let input = "Mercredi, un texte issu de la rencontre, la veille, entre le président français Emmanuel Macron" +
+			"et la première ministre britannique Theresa May présentait des axes de travail pour lutter contre le terrorisme" +
+			" sur Internet, évoquant, sans les nommer, les grands réseaux sociaux. Vingt-quatre heures plus tard seulement," +
+			" la publication sur Facebook d’un long texte présentant une liste des dispositions – certaines nouvelles, la plupart" +
+			" déjà connues – apparaît comme une réponse aux propositions formulées par les deux dirigeants, même" +
+			" si le réseau social explique que sa communication était prévue depuis longtemps. Dans ce document," +
+			" Facebook annonce notamment utiliser des technologies d’intelligence artificielle (IA) pour repérer" +
+			" les contenus faisant l’apologie du terrorisme – une possibilité que Mark Zuckerberg, le fondateur de" +
+			" l’entreprise, avait déjà évoquée. Elle est désormais effective, explique au Monde, par visioconférence," +
+			" Brian Fishman, chargé de la lutte contre le terrorisme à Facebook :";
 		let expected = [
-			new WordCombination( [ 'trabajar', 'porque', 'hubo', 'una', 'tormenta' ], 19, spanishFunctionWords ),
-			new WordCombination( [ 'hubo', 'una', 'tormenta', 'de', 'nieve' ], 19, spanishFunctionWords ),
-			new WordCombination( [ 'tormenta', 'de', 'nieve' ], 19, spanishFunctionWords ),
-			new WordCombination( [ 'no', 'pudimos', 'ir', 'a', 'trabajar' ], 19, spanishFunctionWords ),
-			new WordCombination( [ 'pudimos', 'ir', 'a', 'trabajar' ], 19, spanishFunctionWords ),
-			new WordCombination( [ 'hubo', 'una', 'tormenta' ], 19, spanishFunctionWords ),
-			new WordCombination( [ "trabajar" ], 19, spanishFunctionWords ),
-			new WordCombination( [ "tormenta" ], 19, spanishFunctionWords ),
+			new WordCombination( [ 'terrorisme' ], 3, frenchFunctionWords ),
+			new WordCombination( [ 'facebook' ], 3, frenchFunctionWords ),
+			new WordCombination( [ 'texte' ], 2, frenchFunctionWords ),
 		];
 
 		// Make sure our words aren't filtered by density.
 		spyOn( WordCombination.prototype, "getDensity" ).and.returnValue( 0.01 );
 
-		let words = getRelevantWords( input, "es_ES" );
+		let words = getRelevantWords( input, "fr_FR" );
 
 		words.forEach( function( word ) {
 			delete( word._relevantWords );
