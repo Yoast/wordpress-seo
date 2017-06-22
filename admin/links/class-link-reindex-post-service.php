f944bd5cb110b55da881565df185b80f7cdd0508
Andy Meerwaldt <andy@yoast.com>
2017-06-22 10:44:37 +0200

    Add server for the reindex

---
+++ admin/links/class-link-reindex-post-service.php
@@ -0,0 +1,27 @@
+<?php
+
+class WPSEO_Link_Reindex_Post_Service {
+
+	/**
+	 * @param WP_REST_Request $request
+	 *
+	 * @return WP_REST_Response
+	 */
+	public function reindex( WP_REST_Request $request ) {
+		global $wpdb;
+
+		$storage = new WPSEO_Link_Storage( $wpdb->get_blog_prefix() );
+		$content_processor = new WPSEO_Link_Content_Processor( $storage );
+
+		$posts = WPSEO_Link_Reindex_Post_Query::get_post_by_post_type( $request->get_param( 'postType' ) );
+		foreach( $posts as $post ) {
+			// Apply the filters to have the same content as shown on the frontend.
+			$content = apply_filters( 'the_content', $post->post_content );
+			$content = str_replace( ']]>', ']]&gt;', $content );
+
+			$content_processor->process( $post->ID, $content );
+		}
+
+		return new WP_REST_Response( count( $posts ) );
+	}
+}
