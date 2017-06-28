2fca58ed777ce2c2938b120512518fd1a0d9a8d9
Andy Meerwaldt <andy@yoast.com>
2017-06-28 13:48:31 +0200

    Implement the installable interface

--- admin/links/class-link-storage.php
+++ admin/links/class-link-storage.php
@@ -6,9 +6,15 @@
 /**
  * Represents the storage of an seo link.
  */
-class WPSEO_Link_Storage {
+class WPSEO_Link_Storage implements WPSEO_Installable {
 
 	const TABLE_NAME = 'yoast_seo_links';
+
+	/** @var WPSEO_Database_Proxy */
+	protected $database_proxy;
+
+	/** @var null|string */
+	protected $table_prefix;
 
 	/**
 	 * Sets the table prefix.
@@ -21,6 +27,7 @@ class WPSEO_Link_Storage {
 		}
 
 		$this->table_prefix = $table_prefix;
+		$this->database_proxy = new WPSEO_Database_Proxy( $this->get_table_name(), true );
 	}
 
 	/**
@@ -33,38 +40,24 @@ class WPSEO_Link_Storage {
 	}
 
 	/**
-	 * Creates the links table if it doesn't exist.
+	 * Creates the database table.
 	 *
 	 * @return boolean True if the table was created, false if something went wrong.
 	 */
-	public function create_table() {
-		global $wpdb;
-
-		$charset_collate = $wpdb->get_charset_collate();
-
-		$create_table = sprintf( '
-			CREATE TABLE IF NOT EXISTS %1$s (
-                id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-                url varchar(255) NOT NULL,
-                post_id bigint(20) unsigned NOT NULL,
-                target_post_id bigint(20) unsigned NOT NULL,
-                type VARCHAR(8) NOT NULL,
-                PRIMARY KEY (id),
-                KEY link_direction (post_id, type)
-			) %2$s',
-			$this->get_table_name(),
-			$charset_collate
+	public function install() {
+		return $this->database_proxy->create_table(
+			array(
+				'id bigint(20) unsigned NOT NULL AUTO_INCREMENT',
+                'url varchar(255) NOT NULL',
+                'post_id bigint(20) unsigned NOT NULL',
+                'target_post_id bigint(20) unsigned NOT NULL',
+                'type VARCHAR(8) NOT NULL',
+			),
+			array(
+				'PRIMARY KEY (id)',
+                'KEY link_direction (post_id, type)',
+			)
 		);
-
-		$suppressed = $wpdb->suppress_errors();
-		$is_created = (bool) $wpdb->query( $create_table );
-		$wpdb->suppress_errors( $suppressed );
-
-		if ( $is_created === false ) {
-			WPSEO_Link_Table_Accessible::set_inaccessible();
-		}
-
-		return $is_created;
 	}
 
 	/**
@@ -77,8 +70,7 @@ class WPSEO_Link_Storage {
 	public function get_links( $post_id ) {
 		global $wpdb;
 
-		$suppressed = $wpdb->suppress_errors();
-		$results = $wpdb->get_results(
+		$results = $this->database_proxy->get_results(
 			$wpdb->prepare( '
 				SELECT url, post_id, target_post_id, type
 				FROM ' . $this->get_table_name() . '
@@ -86,9 +78,8 @@ class WPSEO_Link_Storage {
 				$post_id
 			)
 		);
-		$wpdb->suppress_errors( $suppressed );
 
-		if ( $wpdb->last_error !== '' ) {
+		if ( $this->database_proxy->has_error() ) {
 			WPSEO_Link_Table_Accessible::set_inaccessible();
 		}
 
@@ -108,24 +99,17 @@ class WPSEO_Link_Storage {
 	}
 
 	/**
-	 * Removes all record for given post_id.
+	 * Removes all records for given post_id.
 	 *
 	 * @param int $post_id The post_id to remove the records for.
 	 *
 	 * @return int|false The number of rows updated, or false on error.
 	 */
 	public function cleanup( $post_id ) {
-		global $wpdb;
-
-		$suppressed = $wpdb->suppress_errors();
-
-		$is_deleted = $wpdb->delete(
-			$this->get_table_name(),
+		$is_deleted = $this->database_proxy->delete(
 			array( 'post_id' => $post_id ),
 			array( '%d' )
 		);
-
-		$wpdb->suppress_errors( $suppressed );
 
 		if ( $is_deleted === false ) {
 			WPSEO_Link_Table_Accessible::set_inaccessible();
@@ -144,12 +128,7 @@ class WPSEO_Link_Storage {
 	 * @return void
 	 */
 	protected function save_link( WPSEO_Link $link, $link_key, $post_id ) {
-		global $wpdb;
-
-		$suppressed = $wpdb->suppress_errors();
-
-		$inserted = $wpdb->insert(
-			$this->get_table_name(),
+		$inserted = $this->database_proxy->insert(
 			array(
 				'url' => $link->get_url(),
 				'post_id' => $post_id,
@@ -158,8 +137,6 @@ class WPSEO_Link_Storage {
 			),
 			array( '%s', '%d', '%d', '%s' )
 		);
-
-		$wpdb->suppress_errors( $suppressed );
 
 		if ( $inserted === false ) {
 			WPSEO_Link_Table_Accessible::set_inaccessible();
