5b4070f47d6d8d223ccabe47e8bb05b05175abb8
Andy Meerwaldt <andy@yoast.com>
2017-06-28 11:37:12 +0200

    Remove updating post meta

--- admin/links/class-link-content-processor.php
+++ admin/links/class-link-content-processor.php
@@ -46,7 +46,5 @@ class WPSEO_Link_Content_Processor {
 		$this->storage->cleanup( $post_id );
 		$this->storage->save_links( $post_id, $links );
 
-		// Update post meta to flag that processing has been done.
-		update_post_meta( $post_id, WPSEO_Link_Factory::get_index_meta_key(), '1' );
 	}
 }
