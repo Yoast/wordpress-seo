b6688eeaaa0232735a882c1611e11b20b3ca3177
Anton Timmermans <email@atimmer.com>
2017-07-07 10:14:10 +0200

    Fix tests after changing in 5.0 release branch
    Fixes #7408

--- tests/test-class-admin-asset.php
+++ tests/test-class-admin-asset.php
@@ -51,7 +51,7 @@ class WPSEO_Admin_Asset_Test extends WPSEO_UnitTestCase {
 		$this->assertEquals( '.suffix', $asset->get_suffix() );
 
 		$this->assertEquals( home_url() . '/wp-content/plugins/wordpress-seo/js/dist/src.suffix.js', $asset->get_url( WPSEO_Admin_Asset::TYPE_JS, WPSEO_FILE ) );
-		$this->assertEquals( home_url() . '/wp-content/plugins/wordpress-seo/css/src.suffix.css', $asset->get_url( WPSEO_Admin_Asset::TYPE_CSS, WPSEO_FILE ) );
+		$this->assertEquals( home_url() . '/wp-content/plugins/wordpress-seo/css/dist/src.suffix.css', $asset->get_url( WPSEO_Admin_Asset::TYPE_CSS, WPSEO_FILE ) );
 		$this->assertEquals( '', $asset->get_url( '', WPSEO_FILE ) );
 
 	}
