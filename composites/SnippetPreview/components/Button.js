4a46d39ffa3fcd9baf4dfa7f7be583b2b660c7aa
Alexander Botteram <alexander@yoast.com>
2017-07-27 11:05:07 +0200

    Split Button component into basic button and specifically styled button for SnippetPreview

--- composites/SnippetPreview/components/Button.js
+++ composites/SnippetPreview/components/Button.js
@@ -1,34 +1,48 @@
 import styled from "styled-components";
+import colors from "../../../style-guide/colors.json";
 
 /**
- * @summary:
- * Returns a basic styled button
+ * @summary Returns a basic styled button.
  *
- * @param {object} props Component props
+ * @param {object} props Component props.
  *
- * @returns {ReactElement} Styled button
+ * @returns {ReactElement} Styled button.
  */
-const Button = styled.button`
+export const Button = styled.button`
 	display: inline-block;
 	padding: 8px 10px;
-    border: 1px solid #dbdbdb;
-    border-radius: 4px;
-    color: #555;
-    background: #f7f7f7;
-    font-size: 0.8rem;
-    line-height: 15px;
-    cursor: pointer;
-    box-sizing: border-box;
-    font-family: inherit;
-    font-weight: inherit;
-    
-    :focus {
+	border: 1px solid ${colors.$color_button_border};
+	border-radius: 4px;
+	background: ${colors.$color_button};
+	color: ${colors.$color_button_text};
+	cursor: pointer;
+	box-sizing: border-box;
+	font-size: inherit;
+	font-family: inherit;
+	font-weight: inherit;
+`;
+
+/**
+ * @summary Returns a button styled for the SnippetPreview.
+ *
+ * @param {object} props Component props.
+ *
+ * @returns {ReactElement} Styled button.
+ */
+export const SnippetPreviewButton = styled( Button )`
+	line-height: 15px;
+	font-size: 0.8rem;
+
+	:focus {
 		border-color: #5b9dd9;
 		outline: none;
 		color: #23282d;
 		background-color: #fafafa;
 		box-shadow: 0 0 3px rgba(0, 115, 170, 0.8);
-    }
-`;
+	}
+`
 
-export default Button;
+Button.defaultProps = {
+	type: "button",
+};
+
