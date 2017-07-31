cfd484f2d89a1036bfbfc7062e23a34b36432be6
Andy Meerwaldt <andy@yoast.com>
2017-07-31 11:03:03 +0200

    Use site details instead of user details.

--- admin/tracking/class-tracking-default-data.php
+++ admin/tracking/class-tracking-default-data.php
@@ -15,33 +15,14 @@ class WPSEO_Tracking_Default_Data implements WPSEO_Collection {
 	 */
 	public function get() {
 		return array(
-			'name'         => $this->get_user_info( 'name' ),
-			'email'        => $this->get_user_info( 'email' ),
+			'name'         => get_option( 'blogname' ),
+			'email'        => get_option( 'admin_email' ),
 			'@timestamp'   => date( 'U' ),
 			'wpVersion'    => $this->get_wordpress_version(),
 			'homeURL'      => home_url(),
 			'isMultisite'  => is_multisite(),
 			'siteLanguage' => get_bloginfo( 'language' ),
 		);
-	}
-
-	/**
-	 * Returns info about the current user.
-	 *
-	 * @param string $what What to retrieve, defaults to name.
-	 *
-	 * @return string
-	 */
-	protected function get_user_info( $what = 'name' ) {
-		$current_user = wp_get_current_user();
-		switch ( $what ) {
-			case 'email':
-				return $current_user->user_email;
-			case 'name':
-				return trim( $current_user->user_firstname . ' ' . $current_user->user_lastname );
-		}
-
-		return '';
 	}
 
 	/**
