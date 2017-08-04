4d6ae0be6b931769b7807b6d3de3d02c51f051dd
Anton Timmermans <email@atimmer.com>
2017-08-04 15:46:57 +0200

    Revert "Remove image optimization from release build"
    This reverts commit 61e52fd3bd18fe99222186c62c8518bc315e5d04.

--- grunt/config/imagemin.js
+++ grunt/config/imagemin.js
@@ -6,7 +6,7 @@ module.exports = {
 			// this would require the addition of a assets folder from which the images are
 			// processed and put inside the images folder
 			cwd: "<%= paths.images %>",
-			src: [ "*.*" ],
+			src: [ "*.*", "!*.gif" ],
 			dest: "<%= paths.images %>",
 			isFile: true,
 		} ],
