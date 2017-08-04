61e52fd3bd18fe99222186c62c8518bc315e5d04
Anton Timmermans <email@atimmer.com>
2017-08-04 15:11:21 +0200

    Remove image optimization from release build
    Because it breaks on Travis. This should be done on commit anyway.
    
    Also, optimize the images already present 🙃

--- grunt/config/imagemin.js
+++ grunt/config/imagemin.js
@@ -6,7 +6,7 @@ module.exports = {
 			// this would require the addition of a assets folder from which the images are
 			// processed and put inside the images folder
 			cwd: "<%= paths.images %>",
-			src: [ "*.*", "!*.gif" ],
+			src: [ "*.*" ],
 			dest: "<%= paths.images %>",
 			isFile: true,
 		} ],
