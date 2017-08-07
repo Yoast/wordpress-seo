2623fa036367f63d3d81333d8dc3ae497c4a0f3f
IreneStr <irene@yoast.com>
2017-08-07 14:41:59 +0200

    Mock generateRandomKey

--- composites/SnippetPreview/tests/InputFieldTest.js
+++ composites/SnippetPreview/tests/InputFieldTest.js
@@ -2,6 +2,8 @@ import React from "react";
 import renderer from "react-test-renderer";
 import InputField from "../components/InputField";
 
+jest.mock( "draft-js/lib/generateRandomKey", () => () => "test" );
+
 test( "the input field matches the snapshot", () => {
 	const component = renderer.create(
 		<InputField placeholder="placeholder"/>
