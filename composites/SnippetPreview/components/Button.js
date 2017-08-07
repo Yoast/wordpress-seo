43fbb2208fe4320e4cc9af76728ccf521e3ca110
Alexander Botteram <alexander@yoast.com>
2017-08-07 16:10:57 +0200

    Applied CR feedback

--- composites/SnippetPreview/components/Button.js
+++ composites/SnippetPreview/components/Button.js
@@ -2,28 +2,51 @@ import styled from "styled-components";
 import colors from "../../../style-guide/colors.json";
 
 /**
+ * Returns a component with applied focus styles
+ *
+ * @param {ReactElement} component The original component
+ *
+ * @returns {ReactElement} Component with applied focus styles
+ */
+export function addFocusStyle( component ) {
+	return styled( component )`
+		&::-moz-focus-inner {
+			border-width: 0;
+		}
+
+		&:focus {
+			outline: none;
+			border-color: ${ colors.$color_blue };
+			background-color: ${ colors.$color_white };
+			box-shadow: 0 0 3px ${ colors.$palette_blue_dark };
+		}
+	`;
+}
+
+/**
  * Returns a basic styled button.
  *
  * @param {object} props Component props.
  *
  * @returns {ReactElement} Styled button.
  */
-export const Button = styled.button`
-	display: inline-block;
-	padding: 8px 10px;
-	border: 1px solid ${ colors.$color_button_border };
-	border-radius: 4px;
-	background: ${ colors.$color_button };
-	color: ${ colors.$color_button_text };
-	cursor: pointer;
-	box-sizing: border-box;
-	font-size: inherit;
-	font-family: inherit;
-	font-weight: inherit;
-	outline: none;
-`;
+export const BaseButton = addFocusStyle(
+	styled.button`
+		display: inline-block;
+		padding: 8px 10px;
+		border: 1px solid ${ colors.$color_button_border };
+		border-radius: 4px;
+		background: ${ colors.$color_button };
+		color: ${ colors.$color_button_text };
+		cursor: pointer;
+		box-sizing: border-box;
+		font-size: inherit;
+		font-family: inherit;
+		font-weight: inherit;
+	`
+);
 
-Button.defaultProps = {
+BaseButton.defaultProps = {
 	type: "button",
 };
 
@@ -34,29 +57,7 @@ Button.defaultProps = {
  *
  * @returns {ReactElement} Styled button.
  */
-export const SnippetPreviewButton = styled( Button )`
+export const SnippetPreviewButton = styled( BaseButton )`
 	line-height: 15px;
 	font-size: 0.8rem;
 `;
-
-/**
- * Returns a component with applied focus styles
- *
- * @param {ReactElement} component The original component
- *
- * @returns {ReactElement} Component with applied focus styles
- */
-export function focus( component ) {
-	return styled( component )`
-		&::-moz-focus-inner {
-			border-width: 0;
-			padding: 0;
-		}
-
-		&:focus {
-			border-color: ${ colors.$color_blue };
-			background-color: ${ colors.$color_white };
-			box-shadow: 0 0 3px ${ colors.$palette_blue_dark };
-		}
-	`;
-}
