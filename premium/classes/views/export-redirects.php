ea078751cf7d982942ef56bad47c1a3c9ae06ac6
Jip Moors <jhp.moors@gmail.com>
2017-08-11 08:57:49 +0200

    Convert spaces to tabs

--- premium/classes/views/export-redirects.php
+++ premium/classes/views/export-redirects.php
@@ -10,6 +10,6 @@
 	<p><?php _e( 'Please note that the first row in this file is a header. This row should be ignored when parsing or importing the data from the export.', 'wordpress-seo-premium' ) ?></p>
 	<form action="" method="post" accept-charset="<?php esc_attr_e( get_bloginfo( 'charset' ), 'wordpress-seo-premium' ); ?>">
 		<?php wp_nonce_field( 'wpseo-export', '_wpnonce', true ); ?>
-        <input type="submit" class="button button-primary" name="export" value="<?php _e( 'Export redirects', 'wordpress-seo-premium' ); ?>"/>
-    </form>
+		<input type="submit" class="button button-primary" name="export" value="<?php _e( 'Export redirects', 'wordpress-seo-premium' ); ?>"/>
+	</form>
 </div>
