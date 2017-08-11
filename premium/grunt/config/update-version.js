60105665a155e78e4ec3e59cc1b056cf41573c74
Jip Moors <jhp.moors@gmail.com>
2017-08-11 11:34:53 +0200

    Copy update version task and config to premium folder

---
+++ premium/grunt/config/update-version.js
@@ -0,0 +1,38 @@
+// Custom task
+module.exports = {
+	options: {
+		version: "<%= pluginVersion %>",
+	},
+	readme: {
+		options: {
+			regEx: /(Stable tag: )(\d\d?(\.\d\d?){0,3})([^\.\d]?.*?\n)/,
+			preVersionMatch: "$1",
+			postVersionMatch: "$4",
+		},
+		src: "../readme.txt",
+	},
+	pluginFile: {
+		options: {
+			regEx: /(\* Version: )(\d\d?(\.\d\d?){0,3})([^\.\d]?.*?\n)/,
+			preVersionMatch: "$1",
+			postVersionMatch: "$4",
+		},
+		src: "../wp-seo-premium.php",
+	},
+	initializer: {
+		options: {
+			regEx: /(define\( \'WPSEO_VERSION\'\, \')(\d\d?(\.\d\d?){0,3})(\'.*?\n)/,
+			preVersionMatch: "$1",
+			postVersionMatch: "$4",
+		},
+		src: "../wp-seo-main.php",
+	},
+	premiumClass: {
+		options: {
+			regEx: /(const PLUGIN_VERSION_NAME = )(\')(\d\d?(\.\d\d?){0,3})(\'.*?\n)/,
+			preVersionMatch: "$1$2",
+			postVersionMatch: "$5",
+		},
+		src: "class-premium.php",
+	},
+};
