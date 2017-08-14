543b9883da962c1edcdb001df48445b46bd24732
IreneStr <irene@yoast.com>
2017-08-14 15:46:46 +0200

    composites/Plugin/ContentAnalysis/reducers

--- composites/Plugin/ContentAnalysis/reducers/contentAnalysis/keywordResultsReducer.js
+++ composites/Plugin/ContentAnalysis/reducers/contentAnalysis/keywordResultsReducer.js
@@ -1,14 +1,9 @@
 import { UPDATE_SEO_RESULT } from "../../actions/contentAnalysis";
-import _union from "lodash/union";
 
 /**
  * Initial state
  */
-const initialState = {
-	seo: {
-		byKeyword: {},
-	},
-};
+const initialState = {};
 
 /**
  * Reducers
@@ -21,11 +16,16 @@ const initialState = {
  * @param {Object} action The current action received.
  * @returns {Object} The updated seo object.
  */
-export function SEOReducer( state = initialState.seo, action ) {
+export function seoReducer( state = initialState.seo, action ) {
 	switch ( action.type ) {
 		case UPDATE_SEO_RESULT:
+			if( ! state[ action.keyword ] ) {
+				return Object.assign( {}, state, {
+					[ action.keyword ]: [ action.result ],
+				} );
+			}
 			return Object.assign( {}, state, {
-				byKeyword: { keyword: action.keyword, results: { results: [ action.result ] } },
+				[ action.keyword ]: [ ...state[ action.keyword ], action.result ],
 			} );
 		default:
 			return state;
