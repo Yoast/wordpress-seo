8c6c26a7dc705dabd2cdd2ac6c63b91b775f0e81
IreneStr <irene@yoast.com>
2017-08-14 12:02:28 +0200

    Add actions and action creators

---
+++ composites/Plugin/ContentAnalysis/actions/ContentAnalysis.js
@@ -0,0 +1,41 @@
+/*
+ * Action types
+ */
+
+export const UPDATE_SEO_RESULT = "UPDATE_SEO_RESULT";
+export const UPDATE_READABILITY_RESULT = "UPDATE_READABILITY_RESULT";
+
+/*
+ * Action creators
+ */
+
+
+/**
+ * An action creator for updating a single SEO result.
+ *
+ * @param {string} keyword The focus keyword.
+ * @param {Object} result The SEO result.
+ *
+ * @returns {Object} An update SEO result action.
+ */
+export function updateSeoResult( keyword, result ) {
+	return {
+		type: UPDATE_SEO_RESULT,
+		keyword: keyword,
+		result: result,
+	};
+}
+
+/**
+ * An action creator for updating a single readability result.
+ *
+ * @param {Object} result The readability result.
+ *
+ * @returns {Object} An update readability result action.
+ */
+export function updateReadabilityResult( result ) {
+	return {
+		type: UPDATE_READABILITY_RESULT,
+		result: result,
+	};
+}
---
+++ composites/Plugin/ContentAnalysis/actions/tests/ContentAnalysisTest.js
@@ -0,0 +1,22 @@
+import * as actions from "../ContentAnalysis";
+
+
+test( "update SEO Result action creator", () => {
+	const expected = {
+		type: actions.UPDATE_SEO_RESULT,
+		keyword: "keyword",
+		result: { id: "result", score: 9, description: "This is a great score!", markingIsActive: true },
+	};
+	const actual = actions.updateSeoResult( "keyword", { id: "result", score: 9, description: "This is a great score!", markingIsActive: true } );
+	expect( actual ).toEqual( expected );
+} );
+
+test( "update readability Result action creator", () => {
+	const expected = {
+		type: actions.UPDATE_READABILITY_RESULT,
+		result: { id: "result", score: 3, description: "This is a bad score!", markingIsActive: false },
+
+	};
+	const actual = actions.updateReadabilityResult( { id: "result", score: 3, description: "This is a bad score!", markingIsActive: false } );
+	expect( actual ).toEqual( expected );
+} );
