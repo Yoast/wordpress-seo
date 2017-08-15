819c0038593911729c57add01258351201d3e7e2
Herre Groen <herre@yoast.com>
2017-08-15 14:09:39 +0200

    Set defaults in form and add the option to do so.

--- admin/views/tabs/tool/export-keywords.php
+++ admin/views/tabs/tool/export-keywords.php
@@ -13,12 +13,15 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 <h2><?php __( 'Export keywords to a CSV file', 'wordpress-seo' ); ?></h2>
 <form action="" method="post" accept-charset="<?php echo esc_attr( get_bloginfo( 'charset' ) ); ?>">
 	<?php
-    wp_nonce_field( 'wpseo-export', '_wpnonce', true );
-    $yform->checkbox( 'export-post-title', __( 'Export post title', 'wordpress-seo' ) );
+	wp_nonce_field( 'wpseo-export', '_wpnonce', true );
+	$yform->set_options_value( 'export-post-title', true );
+	$yform->checkbox( 'export-post-title', __( 'Export post title', 'wordpress-seo' ) );
+	$yform->set_options_value( 'export-post-url', true );
 	$yform->checkbox( 'export-post-url', __( 'Export post URL', 'wordpress-seo' ) );
-    $yform->checkbox( 'export-seo-score', __( 'Export SEO score', 'wordpress-seo' ) );
-    $yform->checkbox( 'export-keywords', __( 'Export keywords', 'wordpress-seo' ) );
-    $yform->checkbox( 'export-keywords-score', __( 'Export keyword scores', 'wordpress-seo' ) );
+	$yform->checkbox( 'export-seo-score', __( 'Export SEO score', 'wordpress-seo' ) );
+	$yform->set_options_value( 'export-keywords', true );
+	$yform->checkbox( 'export-keywords', __( 'Export keywords', 'wordpress-seo' ) );
+	$yform->checkbox( 'export-keywords-score', __( 'Export keyword scores', 'wordpress-seo' ) );
 	?>
 	<input type="submit" class="button button-primary" name="export-posts" value="<?php echo __( 'Export keywords', 'wordpress-seo' ); ?>"/>
 </form>
