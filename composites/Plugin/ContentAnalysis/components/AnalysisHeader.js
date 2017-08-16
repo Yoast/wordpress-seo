73dfc930ec94a1de591e4220e848dd42b5f4292a
boblinthorst <bob.linthorst@gmail.com>
2017-08-16 08:54:39 +0200

    Moved responsibility of creating header to a seperate compoment

--- composites/Plugin/ContentAnalysis/components/AnalysisHeader.js
+++ composites/Plugin/ContentAnalysis/components/AnalysisHeader.js
@@ -3,60 +3,12 @@ import React from "react";
 import styled from "styled-components";
 import colors from "../../../../style-guide/colors.json";
 import { Icon } from "../../Shared/components/Icon";
-import { edit } from "../../../../style-guide/svg";
-import defaults from "../../../../config/defaults.json";
+import { caretUp, caretDown } from "../../../../style-guide/svg";
+import { ButtonHeader } from "./ButtonHeader";
 
 const AnalysisHeaderContainer = styled.div`
 	margin-top: 20px;
 	background-color: ${ colors.$color_white };
-`;
-
-const AnalysisHeader = styled.button`
-	display: flex;
-	width: 100%;
-	justify-content: space-between;
-	align-items: center;
-	background-color: ${ colors.$color_white };
-	padding: 0;
-	border: none;
-	text-align: left;
-	font-weight: 300;
-	cursor: pointer;
-	// When clicking, the button text disappears in Safari 10 because of color: activebuttontext.
-	color: ${ colors.$color_blue };
-	@media screen and ( max-width: ${ defaults.css.breakpoint.tablet }px ) {
-		padding: 16px 24px;
-	}
-	@media screen and ( max-width: ${ defaults.css.breakpoint.mobile }px ) {
-		padding: 16px;
-	}
-	svg {
-		flex: 0 0 40px;
-		// Compensate the SVGIcon width-viewbox size.
-		margin-right: -10px;
-		// Add some spacing between icon and text.
-		margin-left: 10px;
-		// Compensate the height difference with a line of text (32px).
-		margin-top: -4px;
-		margin-bottom: -4px;
-		// Looks like Safari 10 doesn't like align-items: center for SVGs and needs some help.
-		align-self: flex-start;
-		
-		@media screen and ( max-width: ${ defaults.css.breakpoint.tablet }px ) {
-		margin-top: 4px;
-		margin-bottom: 4px;
-		margin-right: -2px;
-		}
-	}
-`;
-
-const AnalysisTitle = styled.span`
-	flex: 1 1 auto;
-	font-size: 1.5em;
-	// Chrome needs 8 decimals to make this 32px without roundings.
-	line-height: 1.33333333;
-	// Looks like Safari 10 doesn't like align-items: center for SVGs and needs some help.
-	align-self: center;
 `;
 
 export default class ListToggle extends React.Component {
@@ -73,11 +25,6 @@ export default class ListToggle extends React.Component {
 		this.state = {
 			isOpen: this.props.isOpen,
 		};
-
-		let children = this.props.children;
-		// todo: Error when children doesn't have a child. prevent this error from being shown??
-		children = children.props.children;
-		console.log( children.length );
 
 		this.toggleOpen = this.toggleOpen.bind( this );
 	}
@@ -97,19 +44,21 @@ export default class ListToggle extends React.Component {
 	 * @returns {void}
 	 */
 	toggleOpen() {
+		console.log( "toggleOpen called" );
+
 		this.setState( {
 			isOpen: ! this.state.isOpen,
 		} );
 	}
 
-	/** todo: icon color, color_grey_dark.
+	/**
 	 * Gets the correct arrow based on whether the list is collapsed or not.
 	 *
 	 * @returns {ReactElement} The upArrow when the header is collapsed, otherwise the downArrow.
 	 */
 	getArrow() {
-		let upArrow = <Icon icon={ edit } iconColor= { colors.$color_grey_dark }  iconSize="20px" />;
-		let downArrow = <Icon icon={ edit } iconColor= { colors.$color_grey_dark } iconSize="20px" />;
+		let upArrow = <Icon icon={ caretUp } iconColor= { colors.$color_grey_dark }  iconSize="20px" />;
+		let downArrow = <Icon icon={ caretDown } iconColor= { colors.$color_grey_dark } iconSize="20px" />;
 
 		return this.isOpen() ? upArrow : downArrow;
 	}
@@ -128,12 +77,9 @@ export default class ListToggle extends React.Component {
 
 		return (
 			<AnalysisHeaderContainer>
-				<AnalysisHeader onClick={ this.toggleOpen } aria-expanded={ this.isOpen() } role="heading" aria-level="3" >
+				<ButtonHeader title={ this.props.title } headerClick={ this.toggleOpen } aria-expanded={ this.isOpen() } isOpen={ this.isOpen() } >
 					{ this.getArrow() }
-					<AnalysisTitle>
-						{ this.props.title }
-					</AnalysisTitle>
-				</AnalysisHeader>
+				</ButtonHeader>
 				{ children }
 			</AnalysisHeaderContainer>
 		);
