bbf42d73bfc712a92ef961959c469a43a41dbe44
Jip Moors <jhp.moors@gmail.com>
2017-08-17 08:44:35 +0200

    Create grunt artifact command

--- premium/grunt/config/copy.js
+++ premium/grunt/config/copy.js
@@ -5,5 +5,27 @@ module.exports = {
 			'assets/dist/social_preview/twitter-placeholder.svg': [ 'node_modules/yoast-social-previews/dist/twitter-placeholder.svg' ],
 			'assets/dist/social_preview/yoast-social-preview-390.min.css': [ 'node_modules/yoast-social-previews/dist/yoast-social-preview.min.css' ]
 		}
-	}
+	},
+	artifact: {
+		files: [
+			{
+				expand: true,
+				cwd: ".",
+				src: [
+					"../wp-seo-premium.php",
+					"class-premium.php",
+
+					"classes/**",
+					"languages/**",
+					"assets/dist/**",
+					"css/dist/**/*.min.css",
+					"js/dist/**/*.min.js",
+
+					"!languages/wordpress-seo-premium.pot",
+					"!changelog.md",
+				],
+				dest: "artifact",
+			},
+		],
+	},
 };
