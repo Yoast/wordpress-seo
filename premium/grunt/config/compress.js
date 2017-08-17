90fdfb874a41734e447aeae9bb21d8275550a202
Jip Moors <jhp.moors@gmail.com>
2017-08-17 10:04:25 +0200

    Fix compress command for relative directory path

--- premium/grunt/config/compress.js
+++ premium/grunt/config/compress.js
@@ -5,8 +5,9 @@ module.exports = {
 		},
 		files: [
 			{
-				cwd: "artifact/",
-				serc: [ "**" ],
+				expand: true,
+				cwd: "../artifact",
+				src: [ "**" ],
 				dest: "wordpress-seo-premium",
 			},
 		],
