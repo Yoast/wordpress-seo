a8ac88970df982fb290329b76c476cbd1da1bd72
IreneStr <irene@yoast.com>
2017-08-23 08:24:41 +0200

    Undo changes in contentanalysis.js

--- composites/Plugin/ContentAnalysis/components/ContentAnalysis.js
+++ composites/Plugin/ContentAnalysis/components/ContentAnalysis.js
@@ -1,12 +1,6 @@
 import React from "react";
 import styled from "styled-components";
 
-import { IconRadioButton } from "./../../Shared/components/Button.js";
-import eye from "./../../../../style-guide/svg/eye.svg";
-import eyeSlash from "./../../../../style-guide/svg/eye-slash.svg";
-import colors from "./../../../../style-guide/colors.json";
-
-
 export const ContentAnalysisContainer = styled.div`
 	min-height: 700px;
 	width: 100%;
@@ -19,13 +13,5 @@ export const ContentAnalysisContainer = styled.div`
  * @returns {ReactElement} The ContentAnalysis component.
  */
 export default function ContentAnalysis() {
-	return ( <ContentAnalysisContainer>
-		<br />
-		<IconRadioButton name="group1" id="RadioButton" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ false }/>
-		<IconRadioButton name="group1" id="RadioButton2" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ false }/>
-		<IconRadioButton name="group1" id="RadioButton3" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ false }/>
-		<IconRadioButton name="group1" id="RadioButton4" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ false }/>
-		<IconRadioButton name="group1" id="RadioButton5" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ true }/>
-		<IconRadioButton name="group1" id="RadioButton6" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ false }/>
-	</ContentAnalysisContainer> );
+	return <ContentAnalysisContainer/>;
 }
