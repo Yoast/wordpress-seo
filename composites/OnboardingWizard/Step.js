345741f81316d3c3194b3aef07a31770aa4477f0
boblinthorst <bob.linthorst@gmail.com>
2017-08-29 15:41:39 +0200

    applied a max-width to the width of the onboarding wizard, and made the seperator tiles slightly smaller to have them fit on one line

--- composites/OnboardingWizard/Step.js
+++ composites/OnboardingWizard/Step.js
@@ -258,7 +258,9 @@ class Step extends React.Component {
 			<div className={`${this.props.classPrefix}--step--container`} ref="stepContainer"
 				tabIndex="-1" aria-labelledby="step-title">
 				<h1 id="step-title">{this.props.title}</h1>
-				{ this.getFieldComponents( this.props.fields ) }
+					<div className={`${this.props.classPrefix}-content-container`}>
+						{ this.getFieldComponents( this.props.fields ) }
+					</div>
 			</div>
 		);
 	}
