2889798e3dd24c05352b71e958e2f2af11392a62
Manuel <manuel@yoast.com>
2017-08-30 16:41:51 +0200

    Add test

--- spec/researches/german/getSentencePartsSpec.js
+++ spec/researches/german/getSentencePartsSpec.js
@@ -7,4 +7,8 @@ describe( "splits German sentences into parts", function() {
 		expect( getSentenceParts( sentence )[ 1 ].getSentencePartText() ).toBe( "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron." );
 		expect( getSentenceParts( sentence ).length ).toBe( 2 );
 	} );
+	it ( "splits German sentences that begin with a stopword into sentence parts", function() {
+		var sentence =  "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron.";
+		expect( getSentenceParts( sentence )[ 0 ].getSentencePartText() ).toBe( "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron." );
+	} );
 } );
