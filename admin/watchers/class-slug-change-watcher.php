71237d348260915fcfe811876390506af8965028
Herre Groen <herre@yoast.com>
2017-08-30 10:20:35 +0200

    Add working notification.

--- admin/watchers/class-slug-change-watcher.php
+++ admin/watchers/class-slug-change-watcher.php
@@ -1,5 +1,9 @@
 <?php
+/** @package WPSEO\Admin\Watchers */
 
+/**
+ * Class WPSEO_Slug_Change_Watcher
+ */
 class WPSEO_Slug_Change_Watcher implements WPSEO_WordPress_Integration {
 
 	/**
@@ -36,36 +40,48 @@ class WPSEO_Slug_Change_Watcher implements WPSEO_WordPress_Integration {
 			return;
 		}
 
-		// If the post URL wasn't public before, or isn't public now, don't even check if we have to redirect.
-		if ( ! $this->check_public_post_status( get_post_status( $post_before->ID ) ) || ! $this->check_public_post_status( get_post_status( $post->ID ) ) ) {
+		// If the post URL wasn't visible before, or isn't visible now, don't even check if we have to redirect.
+		if ( ! $this->check_visible_post_status( get_post_status( $post_before->ID ) ) || ! $this->check_visible_post_status( get_post_status( $post->ID ) ) ) {
 			return;
 		}
 
-		add_action( 'admin_notices', array( $this, 'show_notice' ) );
-	}
-
-
-	public function show_notice() {
-		$class = 'notice notice-error';
-		$message = __( 'Irks! An error has occurred.', 'sample-text-domain' );
-
-		printf( '<div class="%1$s"><p>%2$s</p></div>', esc_attr( $class ), esc_html( $message ) );
+		$this->add_notification();
 	}
 
 	/**
-	 * Checks whether the given post status is public or not
+	 * Checks whether the given post status is visible or not
 	 *
 	 * @param string $post_status The post status to check.
 	 *
 	 * @return bool
 	 */
-	protected function check_public_post_status( $post_status ) {
-		$public_post_statuses = array(
+	protected function check_visible_post_status( $post_status ) {
+		$visible_post_statuses = array(
 			'publish',
 			'static',
 			'private',
 		);
 
-		return in_array( $post_status, $public_post_statuses, true );
+		return in_array( $post_status, $visible_post_statuses, true );
+	}
+
+	/**
+	 * Adds a notification to be shown on the next page request since posts are updated in an ajax request.
+	 */
+	protected function add_notification() {
+		$notification = new Yoast_Notification(
+			sprintf(
+			/* translators: %1$s expands to the anchor opening tag, %2$s to the anchor closing tag. */
+				__(
+					'You just changed the URL of a post. To ensure your visitors do not see a 404, you should create a redirect. %1$sLearn how to create redirects here.%2$s',
+					'wordpress-seo'
+				),
+				'<a href="https://yoast.com/which-redirect/">',
+				'</a>'
+			), array( 'type' => 'notice-info' )
+		);
+
+		$notification_center = Yoast_Notification_Center::get();
+		$notification_center->add_notification( $notification );
 	}
 }
