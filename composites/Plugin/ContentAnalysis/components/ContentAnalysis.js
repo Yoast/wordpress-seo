9eb0fe52bc079339a59c64e5f287bc3d4dad6876
Andrea Fercia <a.fercia@gmail.com>
2017-08-31 13:47:25 +0200

    Revert ContentAnalysis.

--- composites/Plugin/ContentAnalysis/components/ContentAnalysis.js
+++ composites/Plugin/ContentAnalysis/components/ContentAnalysis.js
@@ -1,17 +1,10 @@
 import React from "react";
 import styled from "styled-components";
-import { BaseButton, Button, InlineFlexButton, IconButton } from "../../Shared/components/Button";
-import { YoastButton, YoastButtonLink } from "../../Shared/components/YoastButton";
-import { edit } from "../../../../style-guide/svg";
 
 export const ContentAnalysisContainer = styled.div`
 	min-height: 700px;
-	padding: 40px;
+	width: 100%;
 	background-color: white;
-
-	button {
-		margin-right: 10px;
-	}
 `;
 
 /**
@@ -20,13 +13,5 @@ export const ContentAnalysisContainer = styled.div`
  * @returns {ReactElement} The ContentAnalysis component.
  */
 export default function ContentAnalysis() {
-	return <ContentAnalysisContainer>
-		<BaseButton>Base Button</BaseButton>
-		<Button>Button</Button>
-		<IconButton icon={ edit } iconColor="#c00" aria-label="IconButton with icon only" />
-		<IconButton icon={ edit } iconColor="#c00">Icon Button</IconButton>
-		<YoastButton>Yoast Button</YoastButton>
-		<YoastButton backgroundColor="lightblue" textColor="#333">Yoast Button</YoastButton>
-		<YoastButton backgroundColor="lightblue" textColor="#333" withTextShadow={ false }>Yoast Button</YoastButton>
-	</ContentAnalysisContainer>;
+	return <ContentAnalysisContainer/>;
 }
