0c9bac015403bfbdd172602cd58cc8c04546f317
Alexander Botteram <alexander@yoast.com>
2017-09-06 15:48:51 +0200

    Initial setup Wordpress feed component

---
+++ composites/Plugin/DashboardWidget/WordpressFeed.js
@@ -0,0 +1,116 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+import colors from "../../../style-guide/colors.json";
+
+/**
+ * @typedef  {Object}     Feed
+ * @property {string}     title       The title of the website.
+ * @property {string}     description A description of the website.
+ * @property {string}     link        A link to the website.
+ * @property {FeedItem[]} items     The items in the feed.
+ */
+
+/**
+ * @typedef  {Object} FeedItem
+ * @property {string} title       The title of the item.
+ * @property {string} content     The content of the item, will be HTML encoded.
+ * @property {string} description A summary of the content, will be HTML encoded.
+ * @property {string} link        A link to the item.
+ * @property {string} creator     The creator of the item.
+ * @property {string} date        The publication date of the item.
+ */
+
+const WordpressFeedContainer = styled.div`
+	padding: 8px;
+	box-sizing: border-box;
+	
+	p {
+		margin: 0;
+	}
+`;
+
+const WordpressFeedHeader = styled.h3`
+	padding: 0 8px;
+`;
+
+const WordpressFeedList = styled.ul`
+	margin: 8px 0;
+	list-style: none;
+	padding: 0;
+`;
+
+const WordpressFeedLink = styled.a`
+`;
+
+const WordpressFeedListItemContainer = styled.li`
+	padding: 0 8px 8px;
+	
+	:last-child {
+		border-bottom: 1px solid ${ colors.$palette_grey };
+	}
+`;
+
+const WordpressFeedFooter = styled.div`
+	padding: 0 8px;
+`;
+
+const WordpressFeedListItem = ( props ) => {
+	return (
+		<WordpressFeedListItemContainer>
+			<WordpressFeedLink
+				href={ props.link }
+				target="_blank">
+				{ props.title }
+			</WordpressFeedLink>
+			<p>{ props.description }</p>
+		</WordpressFeedListItemContainer>
+	);
+};
+
+WordpressFeedListItem.propTypes = {
+	title: PropTypes.string.isRequired,
+	link: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+};
+
+/**
+ * Displays a parsed wordpress feed.
+ *
+ * @param {Object} props    The component props.
+ * @param {Feed} props.feed The feed object.
+ *
+ * @returns {ReactElement} The WordpressFeed component.
+ */
+const WordpressFeed = ( props ) => {
+	return (
+		<WordpressFeedContainer>
+			<WordpressFeedHeader>
+				{ props.title }
+			</WordpressFeedHeader>
+			<WordpressFeedList>
+				{ props.feed.items.map( item => (
+					<WordpressFeedListItem
+						title={ item.title }
+						link={ item.link }
+						description={ item.description } />
+				) ) }
+			</WordpressFeedList>
+			<WordpressFeedFooter>
+				<WordpressFeedLink
+					href={ props.feed.link }
+					target="_blank">
+					{ props.footerText }
+				</WordpressFeedLink>
+			</WordpressFeedFooter>
+		</WordpressFeedContainer>
+	);
+};
+
+WordpressFeed.propTypes = {
+	feed: PropTypes.object.isRequired,
+	title: PropTypes.object.isRequired,
+	footerText: PropTypes.string.isRequired,
+};
+
+export default WordpressFeed;
