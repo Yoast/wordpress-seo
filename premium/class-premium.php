2fe6aaa7003f9b7a83d17812aa4390f4b7573087
Jip Moors <jhp.moors@gmail.com>
2017-09-06 10:16:02 +0200

    Use v3 of i18n instead of v3

--- premium/class-premium.php
+++ premium/class-premium.php
@@ -250,7 +250,7 @@ class WPSEO_Premium {
 	 * @link https://github.com/Yoast/i18n-module
 	 */
 	private function register_i18n_promo_class() {
-		new Yoast_I18n_v2(
+		new Yoast_I18n_v3(
 			array(
 				'textdomain'     => 'wordpress-seo-premium',
 				'project_slug'   => 'wordpress-seo-premium',
