5d1de8ac0fe29fff2a80b2c10740440ba87244b7
Jip Moors <jhp.moors@gmail.com>
2017-09-06 17:26:23 +0200

    Remove function moved to deprecated file

--- inc/wpseo-functions.php
+++ inc/wpseo-functions.php
@@ -80,29 +80,6 @@ if ( ! function_exists( 'yoast_get_primary_term' ) ) {
 }
 
 /**
- * Add the bulk edit capability to the proper default roles.
- */
-function wpseo_add_capabilities() {
-	// @todo Add deprecation warning.
-	WPSEO_Capability_Manager_Factory::get()->add();
-	return;
-
-	$roles = array(
-		'administrator',
-		'editor',
-	);
-
-	$roles = apply_filters( 'wpseo_bulk_edit_roles', $roles );
-
-	foreach ( $roles as $role ) {
-		$r = get_role( $role );
-		if ( $r ) {
-			$r->add_cap( 'wpseo_bulk_edit' );
-		}
-	}
-}
-
-/**
  * Replace `%%variable_placeholders%%` with their real value based on the current requested page/post/cpt
  *
  * @param string $string the string to replace the variables in.
