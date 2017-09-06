b0af20a6f05d3ca48db6309ce57d7bafe7e830b8
Jip Moors <jhp.moors@gmail.com>
2017-09-06 17:25:15 +0200

    Add comment context

--- admin/roles/class-register-roles.php
+++ admin/roles/class-register-roles.php
@@ -14,9 +14,11 @@ class WPSEO_Register_Roles implements WPSEO_WordPress_Integration {
 	public function register() {
 		$role_manager = WPSEO_Role_Manager_Factory::get();
 
+		// Add all `wpseo_*` capabilities to the manager.
 		$capability_manager = WPSEO_Capability_Manager_Factory::get();
 		$role_manager->register( 'wpseo_manager', 'SEO Manager', $capability_manager->get_capabilities() );
 
+		// Only add specific editor capabilities.
 		$role_manager->register( 'wpseo_editor', 'SEO Editor', array(
 			'wpseo_bulk_edit',
 			'wpseo_manage_redirects',
