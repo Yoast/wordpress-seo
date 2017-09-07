5c2efd2b009dd19122943e4c3ff9c90b0e6ac1b3
Jip Moors <jhp.moors@gmail.com>
2017-09-07 10:08:29 +0200

    Combine editor capabilties for new roles

--- admin/roles/class-register-roles.php
+++ admin/roles/class-register-roles.php
@@ -19,13 +19,71 @@ class WPSEO_Register_Roles implements WPSEO_WordPress_Integration {
 
 		// Add all `wpseo_*` capabilities to the manager.
 		$capability_manager = WPSEO_Capability_Manager_Factory::get();
-		$role_manager->register( 'wpseo_manager', 'SEO Manager', $capability_manager->get_capabilities() );
+		$role_manager->register(
+			'wpseo_manager',
+			'SEO Manager',
+			$this->combine_capabilities( 'editor', $capability_manager->get_capabilities(), true ),
+			$this->combine_capabilities( 'editor', array(), false )
+		);
 
 		// Only add specific editor capabilities.
-		$role_manager->register( 'wpseo_editor', 'SEO Editor', array(
-			'wpseo_bulk_edit',
-			'wpseo_manage_redirects',
-			'wpseo_edit_advanced_metadata'
-		) );
+		$role_manager->register(
+			'wpseo_editor',
+			'SEO Editor',
+			$this->combine_capabilities(
+				'editor',
+				array(
+					'wpseo_bulk_edit',
+					'wpseo_edit_advanced_metadata'
+				), true
+			),
+			$this->combine_capabilities( 'editor', array(), false )
+		);
+	}
+
+	/**
+	 * Combines existing role capabilities with provided capabilities.
+	 *
+	 * @param string $role         Role to fetch capabilities of.
+	 * @param array  $capabilities Capabilities to merge.
+	 * @param bool   $enabled      Fetch granted or restricted capabilities.
+	 *
+	 * @return array
+	 */
+	protected function combine_capabilities( $role, array $capabilities, $enabled ) {
+		$wp_role = get_role( $role );
+		if ( ! $wp_role ) {
+			return $capabilities;
+		}
+
+		$role_capabilities = $wp_role->capabilities;
+		$filter_callback   = $enabled ? 'is_enabled' : 'is_disabled';
+
+		$filtered_capabilities = array_filter( $role_capabilities, array( $this, $filter_callback ) );
+		$filtered_capabilities = array_keys( $filtered_capabilities );
+
+		return array_merge( $filtered_capabilities, $capabilities );
+	}
+
+	/**
+	 * Return true if true.
+	 *
+	 * @param bool $test Value to test against.
+	 *
+	 * @return bool If test is true.
+	 */
+	protected function is_enabled( $test ) {
+		return $test === true;
+	}
+
+	/**
+	 * Returns true if false.
+	 *
+	 * @param bool $test Value to test against.
+	 *
+	 * @return bool If test is false.
+	 */
+	protected function is_disabled( $test ) {
+		return $test === false;
 	}
 }
