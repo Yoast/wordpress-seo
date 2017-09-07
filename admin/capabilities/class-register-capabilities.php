a7f47ec04d1c11fc8805042dee266fb8326a1c87
Jip Moors <jhp.moors@gmail.com>
2017-09-07 17:07:55 +0200

    Register capabilities on the registered roles

--- admin/capabilities/class-register-capabilities.php
+++ admin/capabilities/class-register-capabilities.php
@@ -18,6 +18,18 @@ class WPSEO_Register_Capabilities implements WPSEO_WordPress_Integration {
 		$manager = WPSEO_Capability_Manager_Factory::get();
 
 		$manager->register( 'wpseo_bulk_edit', array( 'editor' ) );
-		$manager->register( 'wpseo_manage_options', array( 'administrator' ) );
+
+		/*
+		 * Respect MultiSite 'access' setting if set to 'super admins only'.
+		 * This means that local admins do not get the `wpseo_manage_options` capability.
+		 */
+		$ms_options = WPSEO_Options::get_option( 'ms' );
+		if ( $ms_options['access'] !== 'superadmins' ) {
+			$manager->register( 'wpseo_manage_options', array( 'administrator' ) );
+		}
+
+		// Add capabilities to WPSEO Roles.
+		$manager->register( 'wpseo_bulk_edit', array( 'wpseo_editor' ) );
+		$manager->register( 'wpseo_manage_options', array( 'wpseo_manager' ) );
 	}
 }
