8f02b28c317f951cffcb82d34e403fdd856473e6
Herre Groen <herre@yoast.com>
2017-09-11 14:35:23 +0200

    Fix PHPCS errors and add shortlinks.

--- admin/config-ui/fields/class-field-google-search-console-intro.php
+++ admin/config-ui/fields/class-field-google-search-console-intro.php
@@ -16,20 +16,20 @@ class WPSEO_Config_Field_Google_Search_Console_Intro extends WPSEO_Config_Field 
 
 		/* translators: %1$s is the plugin name */
 		$html = sprintf(
-			__( '%1$s integrates with Google Search Console. Search Console is a '.
-				'must-have tool for site owners. You can use it to get a lot of data about ' .
-				'the health of your site. This data can be accessed from within %1$s ' .
-				'if you grant ia access. Don\'t have a Search Console account or is your ' .
-				'site not activated yet? Find out %2$show to connect Search Console to your site.%3$s',
+			__( '%1$s integrates with Google Search Console. Search Console is a
+ must-have tool for site owners. You can use it to get a lot of data about
+ the health of your site. This data can be accessed from within %1$s
+ if you grant ia access. Don\'t have a Search Console account or is your
+ site not activated yet? Find out %2$show to connect Search Console to your site.%3$s',
 				'wordpress-seo' ),
 			'Yoast SEO',
-			'<a href="https://kb.yoast.com/kb/how-to-connect-your-website-to-google-webmaster-tools/">',
+			'<a href="https://yoa.st/1ex">',
 			'</a>' );
 
-		$disclaimer = __( 'Ps: we don\'t store your data in any way and don\'t have full access to your account. ' .
-						  'Your privacy is safe with us.' );
+		$disclaimer = __( 'Ps: we don\'t store your data in any way and don\'t have full access to your account. 
+Your privacy is safe with us.', 'wordpress-seo' );
 
-		$html = '<p>' . $html . '</p>' . '<small>' . esc_html( $disclaimer ) . '</small>';
+		$html = '<p>' . $html . '</p><small>' . esc_html( $disclaimer ) . '</small>';
 
 		$this->set_property( 'html', $html );
 	}
--- admin/config-ui/fields/class-field-social-profiles-intro.php
+++ admin/config-ui/fields/class-field-social-profiles-intro.php
@@ -16,11 +16,11 @@ class WPSEO_Config_Field_Social_Profiles_Intro extends WPSEO_Config_Field {
 
 		$intro_text = sprintf(
 			/* translators: %1$s is the plugin name, %2$s is a link opening tag, %3$s is a link closing tag. */
-			__( '%1$s can tell search engines about your social media profiles. ' .
-				'These will be used in Google\'s Knowledge Graph. There are additional ' .
-				'sharing options in the advanced settings. More %2$sinfo%3$s.', 'wordpress-seo' ),
+			__( '%1$s can tell search engines about your social media profiles.
+ These will be used in Google\'s Knowledge Graph. There are additional
+ sharing options in the advanced settings. More %2$sinfo%3$s.', 'wordpress-seo' ),
 			'Yoast SEO',
-			'<a href="https://kb.yoast.com/kb/social-profile-knowledge-graph/">',
+			'<a href="https://yoa.st/1ey">',
 			'</a>'
 		);
 
