850c23c1bd6a1e01c7c295cde702087a82c76701
Herre Groen <herre@yoast.com>
2017-09-11 14:35:23 +0200

    Fix PHPCS errors and add shortlinks.

--- admin/config-ui/fields/class-field-post-type-visibility.php
+++ admin/config-ui/fields/class-field-post-type-visibility.php
@@ -14,12 +14,12 @@ class WPSEO_Config_Field_Post_Type_Visibility extends WPSEO_Config_Field {
 	public function __construct() {
 		parent::__construct( 'postTypeVisibility', 'HTML' );
 
-		$copy = __( 'Please specify which post types you want search engines to index. ' .
-					'If you do not know the differences between these, it\'s best to choose the ' .
-					'default settings. For instance, setting \'Media\' to visible would index your ' .
-					'images which could lead to visitors finding your images without ever ' .
-					'coming to your site. If you have a custom post type on your site, you need ' .
-					'to activate these by hand.', 'wordpress-seo' );
+		$copy = __( 'Please specify which post types you want search engines to index.
+ If you do not know the differences between these, it\'s best to choose the
+ default settings. For instance, setting \'Media\' to visible would index your
+ images which could lead to visitors finding your images without ever
+ coming to your site. If you have a custom post type on your site, you need
+ to activate these by hand.', 'wordpress-seo' );
 
 		$html = '<p>' . esc_html( $copy ) . '</p><br/>';
 
