6c61b48b8ac7583ca89109dd924df1cb8e52e573
Jip Moors <jhp.moors@gmail.com>
2017-09-12 08:04:42 +0200

    Add all capabilities to the manager as well

--- admin/capabilities/class-register-capabilities.php
+++ admin/capabilities/class-register-capabilities.php
@@ -17,7 +17,8 @@ class WPSEO_Register_Capabilities implements WPSEO_WordPress_Integration {
 	public function register() {
 		$manager = WPSEO_Capability_Manager_Factory::get();
 
-		$manager->register( 'wpseo_bulk_edit', array( 'editor', 'wpseo_editor' ) );
+		$manager->register( 'wpseo_bulk_edit', array( 'editor', 'wpseo_editor', 'wpseo_manager' ) );
+		$manager->register( 'wpseo_edit_advanced_metadata', array( 'wpseo_editor', 'wpseo_manager' ) );
 
 		$manager->register( 'wpseo_manage_options', array( 'wpseo_manager' ) );
 
