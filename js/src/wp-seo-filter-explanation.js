47061e03c956154fe45f015d0589ff85e00c28a5
Herre Groen <herre@yoast.com>
2017-09-21 14:29:17 +0200

    Fix ESLint errors.

--- js/src/wp-seo-filter-explanation.js
+++ js/src/wp-seo-filter-explanation.js
@@ -1,7 +1,7 @@
-( function ( $) {
+/* global yoastFilterExplanation */
+
+( function( $ ) {
 	$( "#posts-filter .tablenav.top" ).after(
 		`<div class="notice notice-info inline wpseo-filter-explanation"><p class="dashicons-before dashicons-lightbulb">${ yoastFilterExplanation.text }</p></div>`
 	);
-
-	debugger;
 }( jQuery ) );
