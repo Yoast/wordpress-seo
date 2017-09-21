5c9ec76c7f2725d8a1f483a7351251c97cf31f0a
Jip Moors <jhp.moors@gmail.com>
2017-09-21 09:01:39 +0200

    Change action copy
    Licenses are not used anymore, subscriptions and sites are the things that are controlled now.

--- admin/views/licenses.php
+++ admin/views/licenses.php
@@ -159,7 +159,7 @@ if ( class_exists( 'Woocommerce' ) ) {
 					<a target="_blank" href="https://my.yoast.com" class="yoast-link--license"><?php _e( 'Manage your license on My Yoast', 'wordpress-seo' ); ?></a>
 				<?php else : ?>
 					<div class="yoast-button yoast-button--noarrow yoast-button--extension yoast-button--extension-not-activated"><?php _e( 'Not activated', 'wordpress-seo' ); ?></div>
-					<a target="_blank" href="https://my.yoast.com" class="yoast-link--license"><?php _e( 'Activate your license on My Yoast', 'wordpress-seo' ); ?></a>
+					<a target="_blank" href="https://yoa.st/13i" class="yoast-link--license"><?php _e( 'Activate your site on My Yoast', 'wordpress-seo' ); ?></a>
 				<?php endif; ?>
 				</a>
 
@@ -217,7 +217,7 @@ if ( class_exists( 'Woocommerce' ) ) {
 								<a target="_blank" href="https://yoa.st/13k" class="yoast-link--license"><?php _e( 'Manage your license on My Yoast', 'wordpress-seo' ); ?></a>
 							<?php else : ?>
 								<div class="yoast-button yoast-button--noarrow  yoast-button--extension yoast-button--extension-not-activated"><?php _e( 'Not activated', 'wordpress-seo' ); ?></div>
-								<a target="_blank" href="https://yoa.st/13i" class="yoast-link--license"><?php _e( 'Activate your license on My Yoast', 'wordpress-seo' ); ?></a>
+								<a target="_blank" href="https://yoa.st/13i" class="yoast-link--license"><?php _e( 'Activate your site on My Yoast', 'wordpress-seo' ); ?></a>
 							<?php endif; ?>
 						<?php else : ?>
 							<a target="_blank" class="yoast-button yoast-button--noarrow yoast-button-go-to  yoast-button--extension yoast-button--extension-buy" href="<?php echo esc_url( $extension->get_buy_url() ); ?>">
