e10941be8ac519c08beb418af7a8af57ede80216
Alexander Botteram <alexander@yoast.com>
2017-09-26 14:09:57 +0200

    Implemented a11y notice tests

---
+++ composites/Plugin/Shared/tests/A11yNoticeTest.js
@@ -0,0 +1,26 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import { A11yNotice } from "../components/A11yNotice";
+
+test( "the A11yNotice matches the snapshot", () => {
+	const component = renderer.create(
+		<A11yNotice>A11yNoticeValue</A11yNotice>
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
+
+
+test( "the A11yNotice can be changed to a different element", () => {
+	const H2A11yNotice = A11yNotice.withComponent( "h2" );
+
+	const component = renderer.create(
+		<H2A11yNotice>H2A11yNoticeValue</H2A11yNotice>
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+	expect( tree.type ).toEqual( "h2" );
+} );
