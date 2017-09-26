f8ecf1957c7d13eebb9abf6bda9725c08c0b0c96
Alexander Botteram <alexander@yoast.com>
2017-09-26 14:11:23 +0200

    Updated A11yNotice component

--- composites/Plugin/Shared/components/A11yNotice.js
+++ composites/Plugin/Shared/components/A11yNotice.js
@@ -1,46 +1,19 @@
 import styled from "styled-components";
-import PropTypes from "prop-types";
-
-import colors from "../../../../style-guide/colors.json";
-import { rgba } from "../../../../style-guide/helpers";
-import { addButtonStyles, addFontSizeStyles } from "./Button";
 
 /**
- * Returns a basic link styled like a button.
+ * A span tag invisible to the user, but not to screen readers.
  *
- * @param {object} props Component props.
- *
- * @returns {ReactElement} Styled button.
+ * If you need a different element then a span, you can use styled-component's .withComponent function.
  */
-export const BaseLinkButton = addButtonStyles(
-	styled.a`
-		text-decoration: none;
-		color: ${ props => props.textColor };
-		border-color: ${ props => props.borderColor };
-		background: ${ props => props.backgroundColor };
-		box-shadow: 0 1px 0 ${ props => rgba( props.boxShadowColor, 1 ) };
-	`
-);
-
-BaseLinkButton.propTypes = {
-	backgroundColor: PropTypes.string,
-	textColor: PropTypes.string,
-	borderColor: PropTypes.string,
-	boxShadowColor: PropTypes.string,
-};
-
-BaseLinkButton.defaultProps = {
-	backgroundColor: colors.$color_button,
-	textColor: colors.$color_button_text,
-	borderColor: colors.$color_button_border,
-	boxShadowColor: colors.$color_button_border,
-};
-
-/**
- * Returns a link styled like a button with set font size.
- *
- * @param {object} props Component props.
- *
- * @returns {ReactElement} Styled link.
- */
-export const LinkButton = addFontSizeStyles( BaseLinkButton );
+export const A11yNotice = styled.span`
+	border: 0;
+	clip: rect(1px, 1px, 1px, 1px);
+	clip-path: inset(50%);
+	height: 1px;
+	margin: -1px;
+	overflow: hidden;
+	padding: 0;
+	position: absolute !important;
+	width: 1px;
+	word-wrap: normal !important;
+`;
