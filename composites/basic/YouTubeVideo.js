4b4f11c3b9e6b9b176145a979f0f44feba120d98
Alexander Botteram <alexander@yoast.com>
2017-09-26 11:45:53 +0200

    Added newline bewteen relative and library imports in YoutubeVideo component

--- composites/basic/YouTubeVideo.js
+++ composites/basic/YouTubeVideo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+
 import IFrame from "../../composites/basic/IFrame";
 
 /* Responsive videos. */
