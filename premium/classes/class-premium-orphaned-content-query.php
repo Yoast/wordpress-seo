ebed4f7a430699c6f11090dc2a0fbfcc55f6c7ed
Andy Meerwaldt <andy@yoast.com>
2017-09-27 12:19:03 +0200

    Added class for getting total orphaned items

---
+++ premium/classes/class-premium-orphaned-content-query.php
@@ -0,0 +1,37 @@
+<?php
+
+class WPSEO_Premium_Orphaned_Content_Query {
+
+	/**
+	 * Returns the table name where the counts are stored.
+	 *
+	 * @return array
+	 */
+	public static function get_post_type_counts() {
+		global $wpdb;
+
+		$storage = new WPSEO_Meta_Storage();
+
+		$query = "
+			SELECT COUNT( ID ) as total_orphaned, post_type
+			  FROM " . $wpdb->posts . "
+			 WHERE 
+			    ID IN( 
+			        SELECT object_id 
+			          FROM " . $storage->get_table_name() . "
+			        WHERE  internal_link_count = '0' AND ( incoming_link_count = '0' OR incoming_link_count IS NULL ) 
+			    )
+			 GROUP BY post_type
+		";
+
+		$results = $wpdb->get_results( $query );
+
+		$post_type_counts = array();
+		foreach ( $results as $result ) {
+			$post_type_counts[ $result->post_type ] = $result->total_orphaned;
+		}
+
+		return $post_type_counts;
+	}
+
+}
