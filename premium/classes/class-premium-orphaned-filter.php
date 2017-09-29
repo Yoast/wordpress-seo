6e67d1a6a743a83f3c6052b4fb6952493bd6c488
Jip Moors <jhp.moors@gmail.com>
2017-09-29 14:58:34 +0200

    Use WPSEO_Premium_Orphaned_Post_Query::get_orphaned_object_ids()
    Cleanup unneeded code
    Fix doc return value
    Cache translated label

--- premium/classes/class-premium-orphaned-filter.php
+++ premium/classes/class-premium-orphaned-filter.php
@@ -9,14 +9,9 @@
 class WPSEO_Premium_Orphaned_Filter extends WPSEO_Abstract_Post_Filter {
 
 	/**
-	 * @var string|null
-	 */
-	protected $link_table_name = null;
-
-	/**
 	 * Returns the query value this filter uses.
 	 *
-	 * @return {string} The query value this filter uses.
+	 * @return string The query value this filter uses.
 	 */
 	public function get_query_val() {
 		return 'orphaned';
@@ -33,7 +28,8 @@ class WPSEO_Premium_Orphaned_Filter extends WPSEO_Abstract_Post_Filter {
 		if ( $this->is_filter_active() ) {
 			global $wpdb;
 
-			$where .= ' AND ' . $wpdb->posts . '.ID IN ( ' . $this->get_subquery() . ' ) ';
+			$post_ids = WPSEO_Premium_Orphaned_Post_Query::get_orphaned_object_ids();
+			$where .= ' AND ' . $wpdb->posts . '.ID IN ( ' . implode( ',', array_map( 'intval', $post_ids ) ) . ' ) ';
 		}
 
 		return $where;
@@ -45,7 +41,13 @@ class WPSEO_Premium_Orphaned_Filter extends WPSEO_Abstract_Post_Filter {
 	 * @return string The label for this filter.
 	 */
 	protected function get_label() {
-		return __( 'Orphaned content', 'wordpress-seo-premium' );
+		static $label;
+
+		if ( $label === null ) {
+			$label = __( 'Orphaned content', 'wordpress-seo-premium' );
+		}
+
+		return $label;
 	}
 
 	/**
@@ -56,42 +58,20 @@ class WPSEO_Premium_Orphaned_Filter extends WPSEO_Abstract_Post_Filter {
 	protected function get_post_total() {
 		global $wpdb;
 
-		return (int) $wpdb->get_var(
-			$wpdb->prepare('
-				SELECT COUNT(id)
-				FROM ' . $wpdb->posts . '
-				WHERE  ' . $wpdb->posts . '.ID IN (' . $this->get_subquery() . ')
-				AND ' . $wpdb->posts . '.post_status IN ("publish", "future", "pending", "private")
-				AND ' . $wpdb->posts . '.post_type = %s;',
-				$this->get_current_post_type()
-			)
-		);
-	}
-
-	/**
-	 * Returns the name of the Yoast link table.
-	 *
-	 * @return string The nameof the Yoast link table.
-	 */
-	protected function get_link_table_name() {
-		if ( $this->link_table_name === null ) {
-			$link_storage = new WPSEO_Meta_Storage();
-			$this->link_table_name = $link_storage->get_table_name();
+		$post_ids = WPSEO_Premium_Orphaned_Post_Query::get_orphaned_object_ids();
+		if ( empty( $post_ids ) ) {
+			return 0;
 		}
 
-		return $this->link_table_name;
-	}
+		$query = $wpdb->prepare('
+				SELECT COUNT(ID)
+				  FROM ' . $wpdb->posts . '
+				 WHERE ID IN (' . implode( ',', array_fill( 0, count( $post_ids ), '%d' ) ) . ')
+				   AND post_status = "publish"
+				   AND post_type = %s',
+			array_merge( $post_ids, array( $this->get_current_post_type() ) )
+		);
 
-	/**
-	 * Returns a query that gets all IDs of objects that have no incoming or outgoing links.
-	 *
-	 * @return string The query.
-	 */
-	protected function get_subquery() {
-		return 'SELECT object_id
-				FROM ' . $this->get_link_table_name() . '
-				WHERE ' . $this->get_link_table_name() . '.internal_link_count = 0
-				AND ( ' . $this->get_link_table_name() . '.incoming_link_count IS NULL
-				OR ' . $this->get_link_table_name() . '.incoming_link_count = 0 )';
+		return (int) $wpdb->get_var( $query );
 	}
 }
