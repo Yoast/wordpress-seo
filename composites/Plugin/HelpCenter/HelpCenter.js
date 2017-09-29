fe2d1114666ba7f2cbcde93100272173408d6f93
IreneStr <irene@yoast.com>
2017-09-29 13:40:42 +0200

    Fix helpcenter heigth and padding, and make tab items a prop

--- composites/Plugin/HelpCenter/HelpCenter.js
+++ composites/Plugin/HelpCenter/HelpCenter.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import PropTypes from "prop-types";
 
 import YoastTabs from "../Shared/components/YoastTabs";
-import AlgoliaSearcher from "../../AlgoliaSearch/AlgoliaSearcher";
 
 export const HelpCenterContainer = styled.div`
 	box-sizing: border-box;
-	min-height: 700px;
+	padding: 24px 40px;
+	min-height: 432px;
 	width: 100%;
 	background-color: white;
 `;
@@ -16,31 +17,15 @@ export const HelpCenterContainer = styled.div`
  *
  * @returns {ReactElement} The ContentAnalysis component.
  */
-export default function HelpCenter() {
+export default function HelpCenter( props ) {
 	return (
 		<HelpCenterContainer>
 			<YoastTabs
-				items={ [
-					{
-						label: "Video tutorial",
-						id: "video_tutorial",
-						content: <h1>Video tutorial</h1>,
-					},
-					{
-						label: "Knowledge base",
-						id: "knowledge_base",
-						content: <AlgoliaSearcher />,
-					},
-					{
-						label: "Support",
-						id: "support",
-						content: <h1>Support</h1>,
-					},
-				] } />
+				items={ props.items } />
 		</HelpCenterContainer>
 	);
 }
 
 HelpCenter.propTypes = {
-
-}
+	items: PropTypes.object.isRequired,
+};
