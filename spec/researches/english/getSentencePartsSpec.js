28714b740eb3c7c5b0ec290cdfb60e11cb4d86c6
Manuel <manuel@yoast.com>
2017-09-29 14:18:59 +0200

    Fix spec description

--- spec/researches/english/getSentencePartsSpec.js
+++ spec/researches/english/getSentencePartsSpec.js
@@ -17,7 +17,7 @@ describe( "splits English sentences into parts", function() {
 		expect( getSentenceParts( sentence )[ 0 ].getSentencePartText() ).toBe( "is praise due." );
 		expect( getSentenceParts( sentence ).length ).toBe( 1 );
 	} );
-	it ( "correctly splits when matching punctuation after words ending in ing", function() {
+	it ( "correctly splits English sentences when matching punctuation after words ending in ing", function() {
 		var sentence = "(is having)";
 		expect( getSentenceParts( sentence )[ 0 ].getSentencePartText() ).toBe( "having)" );
 		expect( getSentenceParts( sentence ).length ).toBe( 1 );
