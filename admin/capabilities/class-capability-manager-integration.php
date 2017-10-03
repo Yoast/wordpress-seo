73702660330d90b1e270de52cfa2bab335223da3
JoryHogeveen <jory.johan.hogeveen@gmail.com>
2017-10-03 14:22:13 +0200

    Add User Role Editor groups integration

--- admin/capabilities/class-capability-manager-integration.php
+++ admin/capabilities/class-capability-manager-integration.php
@@ -15,6 +15,9 @@ class WPSEO_Capability_Manager_Integration {
 	public function register_hooks() {
 		add_filter( 'members_get_capabilities', array( $this, 'get_capabilities' ) );
 		add_action( 'members_register_cap_groups', array( $this, 'action_members_register_cap_group' ) );
+
+		add_filter( 'ure_capabilities_groups_tree', array( $this, 'filter_ure_capabilities_groups_tree' ) );
+		add_filter( 'ure_custom_capability_groups', array( $this, 'filter_ure_custom_capability_groups' ), 10, 2 );
 	}
 
 	/**
@@ -52,5 +55,38 @@ class WPSEO_Capability_Manager_Integration {
 				'diff_added' => true,
 			)
 		);
+	}
+
+	/**
+	 * Add Yoast SEO capability group in the User Role Editor plugin.
+	 *
+	 * @see     URE_Capabilities_Groups_Manager::get_groups_tree()
+	 * @param   array  $groups  Current groups
+	 * @return  array
+	 */
+	public function filter_ure_capabilities_groups_tree( $groups ) {
+		$groups = (array) $groups;
+		$groups['wordpress-seo'] = array(
+			'caption' => esc_html__( 'Yoast SEO', 'wordpress-seo' ),
+			'parent'  => 'custom',
+			'level'   => 3,
+		);
+		return $groups;
+	}
+
+	/**
+	 * Add capabilities to the Yoast SEO group in the User Role Editor plugin.
+	 *
+	 * @see     URE_Capabilities_Groups_Manager::get_cap_groups()
+	 * @param   array   $groups  Current capability groups
+	 * @param   string  $cap_id  Capability identifier
+	 * @return  array
+	 */
+	public function filter_ure_custom_capability_groups( $groups, $cap_id ) {
+		if ( in_array( $cap_id, $this->get_capabilities(), true ) ) {
+			$groups = (array) $groups;
+			$groups[] = 'wordpress-seo';
+		}
+		return $groups;
 	}
 }
