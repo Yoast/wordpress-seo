2bd92d18ce7889508dffe37bc2709dc935c7c285
IreneStr <irene@yoast.com>
2017-10-03 08:53:26 +0200

    Remove committed content analysis

--- composites/Plugin/ContentAnalysis/components/ContentAnalysis.js
+++ composites/Plugin/ContentAnalysis/components/ContentAnalysis.js
@@ -1,12 +1,9 @@
 import React from "react";
 import styled from "styled-components";
 
-import AlgoliaSearcher from "../../../AlgoliaSearch/AlgoliaSearcher";
-
 export const ContentAnalysisContainer = styled.div`
 	min-height: 700px;
 	width: 100%;
-	padding-top: 40px;
 	background-color: white;
 `;
 
@@ -16,7 +13,5 @@ export const ContentAnalysisContainer = styled.div`
  * @returns {ReactElement} The ContentAnalysis component.
  */
 export default function ContentAnalysis() {
-	return <ContentAnalysisContainer>
-		<AlgoliaSearcher maxWidth="1280px" enableLiveSearch={ false } />
-	</ContentAnalysisContainer>;
+	return <ContentAnalysisContainer />
 }
