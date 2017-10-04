bfcba4c7d975257490e10ba94ee5c077e9fc8178
Sören Wrede <soerenwrede@gmail.com>
2017-10-04 13:35:22 +0200

    I18N: Make title translatable
    Make title in WPSEO_Config_Field_Success_Message translatable.

--- admin/config-ui/fields/class-field-success-message.php
+++ admin/config-ui/fields/class-field-success-message.php
@@ -19,8 +19,10 @@ class WPSEO_Config_Field_Success_Message extends WPSEO_Config_Field {
 			__( '%1$s will now take care of all the needed technical optimization of your site. To really improve your site\'s performance in the search results, it\'s important to start creating content that ranks well for keywords you care about. Check out this video in which we explain how to use the %1$s metabox when you edit posts or pages.', 'wordpress-seo' ),
 			'Yoast SEO'
 		);
-
-		$this->set_property( 'title', 'You\'ve done it!' );
+		
+		$title = __( 'You\'ve done it!', 'wordpress-seo' );
+		
+		$this->set_property( 'title', $title );
 		$this->set_property( 'message', $success_message );
 		$this->set_property( 'video', array(
 				'url'   => WPSEO_Shortlinker::get( 'https://yoa.st/metabox-screencast' ),
