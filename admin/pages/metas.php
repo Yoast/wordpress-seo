f8ad70d86e8f6c34ed3642e5feb5649cd2a72e87
Jimmy Comack <jimmy@yoast.com>
2017-10-04 14:51:35 +0200

    Updated explanation text

--- admin/pages/metas.php
+++ admin/pages/metas.php
@@ -53,7 +53,7 @@ function yoast_add_meta_options_help_center_tabs( $tabs ) {
 function add_template_variables_helpcenter() {
 	$explanation = sprintf(
 		/* translators: %1$s expands to Yoast SEO. */
-		__( 'The title &amp; metas settings for %1$s are made up of variables that are replaced by specific values from the page when the page is displayed. The tabs on the left explain the available variables.', 'wordpress-seo' ),
+		__( 'The title &amp; metas settings for %1$s are made up of variables that are replaced by specific values from the page when the page is displayed. The table below contains a list of the available variables.', 'wordpress-seo' ),
 		'Yoast SEO'
 	);
 
