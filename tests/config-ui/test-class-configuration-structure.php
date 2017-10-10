723b0a32819552e98c0aaefdbcf8982db1c28d6e
jrfnl <jrfnl@users.noreply.github.com>
2017-10-10 18:43:04 +0200

    Tests: align mock method with original being mocked.
    The mock method was missing one parameter which the original method has (and parameter comments).

--- tests/config-ui/test-class-configuration-structure.php
+++ tests/config-ui/test-class-configuration-structure.php
@@ -11,13 +11,14 @@ class WPSEO_Configuration_Structure_Mock extends WPSEO_Configuration_Structure {
 	/**
 	 * Make add_step public
 	 *
-	 * @param string $identifier
-	 * @param string $title
-	 * @param array  $fields
-	 * @param bool   $navigation
+	 * @param string $identifier Identifier for this step.
+	 * @param string $title      Title to display for this step.
+	 * @param array  $fields     Fields to use on the step.
+	 * @param bool   $navigation Show navigation buttons.
+	 * @param bool   $full_width Wheter the step content is full width or not.
 	 */
-	public function add_step_mock( $identifier, $title, $fields, $navigation = true ) {
-		$this->add_step( $identifier, $title, $fields, $navigation );
+	public function add_step_mock( $identifier, $title, $fields, $navigation = true, $full_width = false ) {
+		$this->add_step( $identifier, $title, $fields, $navigation, $full_width );
 	}
 }
 
