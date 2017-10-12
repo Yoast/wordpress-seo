c60d3e226a97ed71e77b47b1c506617971579653
Andy Meerwaldt <andy@yoast.com>
2017-10-12 10:59:18 +0200

    Add container for the pre-dashboard elements

--- admin/views/tabs/dashboard/dashboard.php
+++ admin/views/tabs/dashboard/dashboard.php
@@ -13,6 +13,9 @@ $alerts_data = Yoast_Alerts::get_template_variables();
 		/* translators: %1$s expands to Yoast SEO */
 		printf( __( '%1$s Dashboard', 'wordpress-seo' ), 'Yoast SEO' );
 		?></h2>
+
+	<div id="yoast-pre-dashboard-pre-container"></div>
+
 	<div class="yoast-container yoast-container__alert">
 		<?php include WPSEO_PATH . 'admin/views/partial-alerts-errors.php'; ?>
 	</div>
