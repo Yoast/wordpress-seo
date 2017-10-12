00b962d93a450f4c1d9c239a9199e4eda9aed3da
IreneStr <irene@yoast.com>
2017-10-12 21:48:04 +0200

    Implement ChangingIconButton

--- composites/Plugin/Shared/components/ChangingIconButton.js
+++ composites/Plugin/Shared/components/ChangingIconButton.js
@@ -6,13 +6,7 @@ import colors from "../../../../style-guide/colors.json";
 import { Icon } from "./Icon";
 import { rgba } from "../../../../style-guide/helpers";
 
-export const RadioButtonBase = styled.input.attrs( {
-	type: "radio",
-} )`
-	display: none;
-`;
-
-export const RadioButtonLabel = styled.label`
+export const ChangingIconButtonBase = styled.button`
 	box-sizing: border-box;
 	touch-action: manipulation;
 	-webkit-appearance: none;
@@ -20,47 +14,46 @@ export const RadioButtonLabel = styled.label`
 	display: inline-block;
 	text-align: center;
 	border: 1px solid ${ colors.$color_button_border };
-	background: ${ props => props.checked ? props.checkedBackground : props.uncheckedBackground };
+	background-color: ${ props => props.checked ? props.checkedBackground : props.uncheckedBackground };
 	box-shadow: ${ props => props.checked
 		? `inset 0 2px 0 ${ rgba( props.checkedBoxShadowColor, 0.7 ) }`
 		: `0 1px 0 ${ rgba( props.uncheckedBoxShadowColor, 0.7 ) }` };
 	border-radius: 3px;
 	cursor: pointer;
 	outline: none;
-	padding-top: ${ props => props.checked ? "2px" : "2px" };
+	padding-top: 2px;
 	height: ${ props => props.checked ? "25px" : "24px" };
+		
+		&:hover {
+			// Invert colors and icon on hover.
+			background-color: ${ props => props.checked ? props.uncheckedBackground : props.checkedBackground };
+			box-shadow: ${ props => props.checked
+				? `0 1px 0 ${ rgba( props.uncheckedBoxShadowColor, 0.7 ) }`
+				: `inset 0 2px 0 ${ rgba( props.checkedBoxShadowColor, 0.7 ) }` };
+		}
 `;
 
-const IconRadioButton = ( props ) => {
+const ChangingIconButton = ( props ) => {
 	return (
-		<span>
-			<RadioButtonBase
-				type="radio"
-				id={ props.id }
-				name={ props.name }
-			/>
-			<RadioButtonLabel
-				onClick={ props.onClick }
-				className="label"
-				checked={ props.checked }
-				htmlFor={ props.id }
-				uncheckedBoxShadowColor={ props.uncheckedBoxShadowColor }
-				checkedBoxShadowColor={ props.checkedBoxShadowColor }
-				checkedBackground={ props.checkedBackground }
-				uncheckedBackground={ props.uncheckedBackground }
-				aria-label={ props.id }
-				aria-checked={ props.checked }
-			>
-				{ props.checked
-					? <Icon icon={ props.checkedIcon } color={ props.checkedIconColor }/>
-					: <Icon icon={ props.uncheckedIcon } color={ props.uncheckedIconColor }/>
-				}
-			</RadioButtonLabel>
-		</span>
+		<ChangingIconButtonBase
+			onClick={ props.onClick }
+			checked={ props.checked }
+			uncheckedBoxShadowColor={ props.uncheckedBoxShadowColor }
+			checkedBoxShadowColor={ props.checkedBoxShadowColor }
+			checkedBackground={ props.checkedBackground }
+			uncheckedBackground={ props.uncheckedBackground }
+			aria-label={ props.id }
+			aria-checked={ props.checked }
+		>
+			{ props.checked
+				? <Icon icon={ props.checkedIcon } color={ props.checkedIconColor }/>
+				: <Icon icon={ props.uncheckedIcon } color={ props.uncheckedIconColor }/>
+			}
+		</ChangingIconButtonBase>
 	);
 };
 
-IconRadioButton.propTypes = {
+ChangingIconButton.propTypes = {
 	id: PropTypes.string.isRequired,
 	onClick: PropTypes.func.isRequired,
 	boxShadowColor: PropTypes.string,
@@ -72,11 +65,10 @@ IconRadioButton.propTypes = {
 	uncheckedIconColor: PropTypes.string,
 	checkedIcon: PropTypes.func.isRequired,
 	uncheckedIcon: PropTypes.func.isRequired,
-	name: PropTypes.string.isRequired,
 	checked: PropTypes.bool.isRequired,
 };
 
-IconRadioButton.defaultProps = {
+ChangingIconButton.defaultProps = {
 	uncheckedBoxShadowColor: colors.$color_button_border,
 	checkedBoxShadowColor: colors.$color_purple,
 	checkedBackground: colors.$color_pink_dark,
@@ -85,4 +77,4 @@ IconRadioButton.defaultProps = {
 	uncheckedIconColor: colors.$color_button_text,
 };
 
-export default IconRadioButton;
+export default ChangingIconButton;
--- composites/Plugin/Shared/tests/RadioButtonTest.js
+++ composites/Plugin/Shared/tests/RadioButtonTest.js
@@ -1,22 +1,22 @@
 import React from "react";
 import renderer from "react-test-renderer";
 
-import IconRadioButton from "../components/RadioButton";
+import IconRadioButton from "../components/ChangingIconButton";
 import eye from "../../../../style-guide/svg/eye.svg";
 import eyeSlash from "../../../../style-guide/svg/eye-slash.svg";
 
-test( "the unchecked IconRadioButton matches the snapshot", () => {
+test( "the unchecked ChangingIconButton matches the snapshot", () => {
 	const component = renderer.create(
-		<IconRadioButton name="group1" id="RadioButton" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ false }/>
+		<ChangingIconButton name="group1" id="RadioButton" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ false }/>
 	);
 
 	let tree = component.toJSON();
 	expect( tree ).toMatchSnapshot();
 } );
 
-test( "the checked IconRadioButton matches the snapshot", () => {
+test( "the checked ChangingIconButton matches the snapshot", () => {
 	const component = renderer.create(
-		<IconRadioButton name="group1" id="RadioButton2" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ true }/>
+		<ChangingIconButton name="group1" id="RadioButton2" checkedIcon={ eyeSlash } uncheckedIcon={ eye } checked={ true }/>
 	);
 
 	let tree = component.toJSON();
