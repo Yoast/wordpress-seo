e9db14290de19b6674b4d686b83dc728b099af19
Jip Moors <jhp.moors@gmail.com>
2017-10-13 11:37:59 +0200

    Revert unneeded code for patch release

--- premium/classes/class-premium-register-capabilities.php
+++ premium/classes/class-premium-register-capabilities.php
@@ -14,7 +14,6 @@ class WPSEO_Premium_Register_Capabilities implements WPSEO_WordPress_Integration
 	 */
 	public function register_hooks() {
 		add_action( 'wpseo_register_capabilities', array( $this, 'register' ) );
-		add_filter( 'user_has_cap', array( $this, 'filter_user_caps' ) );
 	}
 
 	/**
@@ -26,24 +25,5 @@ class WPSEO_Premium_Register_Capabilities implements WPSEO_WordPress_Integration
 		$manager = WPSEO_Capability_Manager_Factory::get();
 
 		$manager->register( 'wpseo_manage_redirects', array( 'editor', 'wpseo_editor', 'wpseo_manager' ) );
-	}
-
-	/**
-	 * Adds the `wpseo_manage_redirects` to the user if the user has `wpseo_manage_options`.
-	 *
-	 * @param array $capabilities Configured capabilities.
-	 *
-	 * @return array Filtered capabilities.
-	 */
-	public function filter_user_caps( $capabilities ) {
-		// Add wpseo_mananage_redirects to wpso_manage_options if not already set.
-		if ( ! array_key_exists( 'wpseo_manage_redirects', $capabilities )
-			 && isset( $capabilities['wpseo_manage_options'] )
-			 && $capabilities['wpseo_manage_options'] === true
-		) {
-			$capabilities['wpseo_manage_redirects'] = true;
-		}
-
-		return $capabilities;
 	}
 }
