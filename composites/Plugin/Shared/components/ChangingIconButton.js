f1c3c2c51c060157fedf7d440c6187da187da7de
IreneStr <irene@yoast.com>
2017-10-13 11:14:05 +0200

    Change icon color on hover

--- composites/Plugin/Shared/components/ChangingIconButton.js
+++ composites/Plugin/Shared/components/ChangingIconButton.js
@@ -14,22 +14,26 @@ export const ChangingIconButtonBase = styled.button`
 	display: inline-block;
 	text-align: center;
 	border: 1px solid ${ colors.$color_button_border };
-	background-color: ${ props => props.checked ? props.checkedBackground : props.uncheckedBackground };
-	box-shadow: ${ props => props.checked
-		? `inset 0 2px 0 ${ rgba( props.checkedBoxShadowColor, 0.7 ) }`
-		: `0 1px 0 ${ rgba( props.uncheckedBoxShadowColor, 0.7 ) }` };
+	background-color: ${ props => props.pressed ? props.pressedBackground : props.unpressedBackground };
+	box-shadow: ${ props => props.pressed
+		? `inset 0 2px 0 ${ rgba( props.pressedBoxShadowColor, 0.7 ) }`
+		: `0 1px 0 ${ rgba( props.unpressedBoxShadowColor, 0.7 ) }` };
 	border-radius: 3px;
 	cursor: pointer;
 	outline: none;
 	padding-top: 2px;
-	height: ${ props => props.checked ? "25px" : "24px" };
+	height: ${ props => props.pressed ? "24px" : "25px" };
 		
 		&:hover {
-			// Invert colors and icon on hover.
-			background-color: ${ props => props.checked ? props.uncheckedBackground : props.checkedBackground };
-			box-shadow: ${ props => props.checked
-				? `0 1px 0 ${ rgba( props.uncheckedBoxShadowColor, 0.7 ) }`
-				: `inset 0 2px 0 ${ rgba( props.checkedBoxShadowColor, 0.7 ) }` };
+			// Invert background color on hover.
+			background-color: ${ props => props.pressed ? props.unpressedBackground : props.pressedBackground };
+			box-shadow: ${ props => props.pressed
+				? `0 1px 0 ${ rgba( props.unpressedBoxShadowColor, 0.7 ) }`
+				: `inset 0 2px 0 ${ rgba( props.pressedBoxShadowColor, 0.7 ) }` };
+		}
+		// Invert icon color on hover.
+		&:hover svg { 
+			fill: ${ props => props.pressed ? props.unpressedIconColor : props.pressedIconColor } 
 		}
 `;
 
@@ -37,17 +41,19 @@ const ChangingIconButton = ( props ) => {
 	return (
 		<ChangingIconButtonBase
 			onClick={ props.onClick }
-			checked={ props.checked }
-			uncheckedBoxShadowColor={ props.uncheckedBoxShadowColor }
-			checkedBoxShadowColor={ props.checkedBoxShadowColor }
-			checkedBackground={ props.checkedBackground }
-			uncheckedBackground={ props.uncheckedBackground }
+			pressed={ props.pressed }
+			unpressedBoxShadowColor={ props.unpressedBoxShadowColor }
+			pressedBoxShadowColor={ props.pressedBoxShadowColor }
+			pressedBackground={ props.pressedBackground }
+			unpressedBackground={ props.unpressedBackground }
 			aria-label={ props.id }
-			aria-checked={ props.checked }
+			aria-pressed={ props.pressed }
+			unpressedIconColor={ props.unpressedIconColor }
+			pressedIconColor={ props.pressedIconColor }
 		>
-			{ props.checked
-				? <Icon icon={ props.checkedIcon } color={ props.checkedIconColor }/>
-				: <Icon icon={ props.uncheckedIcon } color={ props.uncheckedIconColor }/>
+			{ props.pressed
+				? <Icon icon={ props.pressedIcon } color={ props.pressedIconColor } size="18px"/>
+				: <Icon icon={ props.unpressedIcon } color={ props.unpressedIconColor } size="18px"/>
 			}
 		</ChangingIconButtonBase>
 	);
@@ -57,24 +63,24 @@ ChangingIconButton.propTypes = {
 	id: PropTypes.string.isRequired,
 	onClick: PropTypes.func.isRequired,
 	boxShadowColor: PropTypes.string,
-	uncheckedBoxShadowColor: PropTypes.string,
-	checkedBoxShadowColor: PropTypes.string,
-	checkedBackground: PropTypes.string,
-	uncheckedBackground: PropTypes.string,
-	checkedIconColor: PropTypes.string,
-	uncheckedIconColor: PropTypes.string,
-	checkedIcon: PropTypes.func.isRequired,
-	uncheckedIcon: PropTypes.func.isRequired,
-	checked: PropTypes.bool.isRequired,
+	unpressedBoxShadowColor: PropTypes.string,
+	pressedBoxShadowColor: PropTypes.string,
+	pressedBackground: PropTypes.string,
+	unpressedBackground: PropTypes.string,
+	pressedIconColor: PropTypes.string,
+	unpressedIconColor: PropTypes.string,
+	pressedIcon: PropTypes.func.isRequired,
+	unpressedIcon: PropTypes.func.isRequired,
+	pressed: PropTypes.bool.isRequired,
 };
 
 ChangingIconButton.defaultProps = {
-	uncheckedBoxShadowColor: colors.$color_button_border,
-	checkedBoxShadowColor: colors.$color_purple,
-	checkedBackground: colors.$color_pink_dark,
-	uncheckedBackground: colors.$color_button,
-	checkedIconColor: colors.$color_white,
-	uncheckedIconColor: colors.$color_button_text,
+	unpressedBoxShadowColor: colors.$color_button_border,
+	pressedBoxShadowColor: colors.$color_purple,
+	pressedBackground: colors.$color_pink_dark,
+	unpressedBackground: colors.$color_button,
+	pressedIconColor: colors.$color_white,
+	unpressedIconColor: colors.$color_button_text,
 };
 
 export default ChangingIconButton;
