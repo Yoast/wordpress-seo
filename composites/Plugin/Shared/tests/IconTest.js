4345351f319ec0fe01f8812f2e87591757c97b23
IreneStr <irene@yoast.com>
2017-10-18 14:09:37 +0200

    Update tests

--- composites/Plugin/Shared/tests/IconTest.js
+++ composites/Plugin/Shared/tests/IconTest.js
@@ -3,10 +3,20 @@ import renderer from "react-test-renderer";
 
 import edit from "../../../../style-guide/svg/edit.svg";
 import { Icon } from "../components/Icon";
+import { IconWithAriaLabel } from "../components/Icon";
 
 test( "the Icon matches the snapshot", () => {
 	const component = renderer.create(
 		<Icon icon={ edit } color="black" size="16px" />
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
+
+test( "the IconWithAriaLabel matches the snapshot", () => {
+	const component = renderer.create(
+		<IconWithAriaLabel icon={ edit } color="black" size="16px" ariaLabel="This is an aria label" />
 	);
 
 	let tree = component.toJSON();
