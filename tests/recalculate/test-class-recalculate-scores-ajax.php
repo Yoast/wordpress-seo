c8094f7c61919dc8670a8929d325eaa262ac5246
jrfnl <jrfnl@users.noreply.github.com>
2017-10-18 12:09:53 +0200

    Documentation/Tests: fix up parameter comments for mock methods in test classes

--- tests/recalculate/test-class-recalculate-scores-ajax.php
+++ tests/recalculate/test-class-recalculate-scores-ajax.php
@@ -48,7 +48,7 @@ class WPSEO_Recalculate_Scores_Ajax_Test extends WPSEO_UnitTestCase {
 	/**
 	 * Override die handler for test_get_total()
 	 *
-	 * @param $response
+	 * @param mixed $response Fake response.
 	 *
 	 * @return array Callable handler for wp_die
 	 */
@@ -88,7 +88,7 @@ class WPSEO_Recalculate_Scores_Ajax_Test extends WPSEO_UnitTestCase {
 	/**
 	 * Override die handler for test_get_total_with_posts()
 	 *
-	 * @param $response
+	 * @param mixed $response Fake response.
 	 *
 	 * @return array Callable handler for wp_die
 	 */
