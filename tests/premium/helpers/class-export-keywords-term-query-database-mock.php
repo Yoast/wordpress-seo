8c312dddc7bf53bf011929fad42c652d5d6d2078
jrfnl <jrfnl@users.noreply.github.com>
2017-10-20 11:59:29 +0200

    CS/Docs: add minimal documentation to mock method

--- tests/premium/helpers/class-export-keywords-term-query-database-mock.php
+++ tests/premium/helpers/class-export-keywords-term-query-database-mock.php
@@ -27,12 +27,20 @@ class WPSEO_Export_Keywords_Term_Query_Database_Mock {
 		$this->type  = $type;
 	}
 
+	/**
+	 * Prepare an SQL query.
+	 *
+	 * @param string $query SQL query.
+	 * @param mixed  $args  Variable number of replacement arguments.
+	 *
+	 * @return string
+	 */
 	public function prepare( $query, $args ) {
 		global $wpdb;
 
 		$args = func_get_args();
 		array_shift( $args );
-		if ( isset( $args[0] ) && is_array($args[0]) ) {
+		if ( isset( $args[0] ) && is_array( $args[0] ) ) {
 			$args = $args[0];
 		}
 		return $wpdb->prepare( $query, $args );
