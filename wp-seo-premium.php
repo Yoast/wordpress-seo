fb8401165024fb4467d64f5078f81bd400a120f3
Andy Meerwaldt <andy@yoast.com>
2017-10-25 13:39:27 +0200

    Prefix the redirect handler

--- wp-seo-premium.php
+++ wp-seo-premium.php
@@ -48,8 +48,8 @@ if ( ! is_admin() ) {
 	require_once $wpseo_premium_dir . 'classes/redirect/class-redirect-util.php';
 	require_once $wpseo_premium_dir . 'classes/redirect/class-redirect-handler.php';
 
-	$redirect_handler = new WPSEO_Redirect_Handler();
-	$redirect_handler->load();
+	$wpseo_redirect_handler = new WPSEO_Redirect_Handler();
+	$wpseo_redirect_handler->load();
 }
 
 /**
