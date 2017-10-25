7b8709a4d6ebcb7757eaeeed31a237616a387cd4
jrfnl <jrfnl@users.noreply.github.com>
2017-10-25 19:40:07 +0200

    CS/cleanup: minor function refactoring

--- admin/google_search_console/class-gsc-bulk-action.php
+++ admin/google_search_console/class-gsc-bulk-action.php
@@ -35,14 +35,16 @@ class WPSEO_GSC_Bulk_Action {
 	 * @return string|bool
 	 */
 	private function determine_bulk_action() {
-		// If posted action is the selected one above the table, return that value.
-		if ( ( $action = filter_input( INPUT_POST, 'action' ) ) && $action !== '-1' ) {
-			return $action;
-		}
+		$action_inputs = array(
+			'action', // Bulk action select above the table.
+			'action2', // Bulk action select below the table.
+		);
 
-		// If posted action is the selected one below the table, return that value.
-		if ( ( $action = filter_input( INPUT_POST, 'action2' ) ) && $action !== '-1' ) {
-			return $action;
+		foreach ( $action_inputs as $action_name ) {
+			$action = filter_input( INPUT_POST, $action_name );
+			if ( ! empty( $action ) && $action !== '-1' ) {
+				return $action;
+			}
 		}
 
 		return false;
