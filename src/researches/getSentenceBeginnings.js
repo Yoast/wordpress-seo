89b3a34d2d264f5840c7112d6275720a1514f74f
Anton Timmermans <email@atimmer.com>
2017-11-08 14:29:20 +0100

    Fix Travis build

--- src/researches/getSentenceBeginnings.js
+++ src/researches/getSentenceBeginnings.js
@@ -92,6 +92,7 @@ function getSentenceBeginning( sentence, firstWordExceptions ) {
  * Gets the first word of each sentence from the text, and returns an object containing the first word of each sentence and the corresponding counts.
  *
  * @param {Paper} paper The Paper object to get the text from.
+ * @param {Researcher} researcher The researcher this research is a part of.
  * @returns {Object} The object containing the first word of each sentence and the corresponding counts.
  */
 module.exports = function( paper, researcher ) {
