0688b05193f4a8cf783f2a561bbf5f4a489ce661
Alexander Botteram <alexander@yoast.com>
2017-11-10 14:10:15 +0100

    Revert "Removed test"
    This reverts commit 146e0e56c2f201169679e1b0f6fe2c9d23176985.

---
+++ js/tests/edit.test.js
@@ -0,0 +1,8 @@
+import initialize from "../src/edit.js"
+
+describe( 'initialize', () => {
+	it( 'initializes all functionality on the edit screen', () => {
+		const actual = initialize();
+		expect( actual.store ).toBeDefined();
+	} );
+} );
