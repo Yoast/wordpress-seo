146e0e56c2f201169679e1b0f6fe2c9d23176985
Alexander Botteram <alexander@yoast.com>
2017-11-10 14:03:16 +0100

    Removed test

--- js/tests/edit.test.js
+++
@@ -1,8 +0,0 @@
-import initialize from "../src/edit.js"
-
-describe( 'initialize', () => {
-	it( 'initializes all functionality on the edit screen', () => {
-		const actual = initialize();
-		expect( actual.store ).toBeDefined();
-	} );
-} );
