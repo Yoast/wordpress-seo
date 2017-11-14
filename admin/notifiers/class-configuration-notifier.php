06f8e169b03afb6f7a015a9909258f032b2a7fc1
Andy Meerwaldt <andy@yoast.com>
2017-11-14 10:56:05 +0100

    Move object to notifiers directory

--- admin/notifiers/class-configuration-notifier.php
+++ admin/notifiers/class-configuration-notifier.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * @package WPSEO\Admin
+ * @package WPSEO\Admin\Notifiers
  */
 
 /**
@@ -15,7 +15,13 @@ class WPSEO_Configuration_Notifier {
 	 */
 	public function notify() {
 		$notification  = '<div class="yoast-container yoast-container__configuration-wizard">';
-		$notification .= '<a href="/wp-admin/admin.php?page=wpseo_dashboard&amp;yoast_promo_hide_premium_upsell_admin_block=1" style="" class="alignright"><span class="screen-reader-text">Dismiss this item.</span><span class="dashicons dashicons-no-alt"></span></a>';
+		$notification .= sprintf(
+			'<img src="%1$s" height="%2$s" width="%3$d"  />',
+			esc_url( plugin_dir_url( WPSEO_FILE ) . 'images/new-to-configuration-notice.svg' ),
+			60,
+			60
+		);
+		$notification .= '<div class="yoast-container__configuration-wizard--content">';
 		$notification .= '<h3>' . esc_html__( 'First-time SEO configuration', 'wordpress-seo' ) . '</h3>';
 		$notification .= '<p>';
 		$notification .= sprintf(
@@ -25,6 +31,12 @@ class WPSEO_Configuration_Notifier {
 			'</a>'
 		);
 		$notification .= '</p>';
+		$notification .= '</div>';
+		$notification .= sprintf(
+			'<a href="%1$s" style="" class="yoast-container__configuration-wizard--dismiss alignright"><span class="screen-reader-text">%2$s</span><span class="dashicons dashicons-no-alt"></span></a>',
+			esc_url(  admin_url( 'admin.php?page=wpseo_dashboard&amp;dismiss_get_started=1' ) ),
+			esc_html__( 'Dismiss this item.', 'wordpress-seo' )
+		);
 		$notification .= '</div>';
 
 		return $notification;
