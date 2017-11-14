31fbcc7382c68e0b336deb0690ab40a69e9a63f9
Andy Meerwaldt <andy@yoast.com>
2017-11-14 11:23:05 +0100

    Stop doing checks here, just tell don't ask

--- admin/views/tabs/dashboard/dashboard.php
+++ admin/views/tabs/dashboard/dashboard.php
@@ -6,9 +6,9 @@
 /** @noinspection PhpUnusedLocalVariableInspection */
 $alerts_data = Yoast_Alerts::get_template_variables();
 
-$options      = WPSEO_Options::get_options( array( 'wpseo' ) );
-$is_dismissed = WPSEO_Configuration_Notification::is_dismissed( get_current_user_id() );
-$notifier     = new WPSEO_Configuration_Notifier();
+$options  = WPSEO_Options::get_options( array( 'wpseo' ) );
+$notifier = new WPSEO_Configuration_Notifier( $options );
+$notifier->listen();
 ?>
 <div class="yoast-alerts">
 
@@ -17,9 +17,7 @@ $notifier     = new WPSEO_Configuration_Notifier();
 		printf( __( '%1$s Dashboard', 'wordpress-seo' ), 'Yoast SEO' );
 		?></h2>
 
-	<?php if ( ! $is_dismissed && ! empty( $options['show_onboarding_notice'] ) ) : ?>
-		<?php echo $notifier->notify(); ?>
-	<?php endif ?>
+	<?php echo $notifier->notify(); ?>
 
 	<div class="yoast-container yoast-container__alert">
 		<?php include WPSEO_PATH . 'admin/views/partial-alerts-errors.php'; ?>
