437e6d00a302b2603a6ef1d353284ea24d0061cc
Andy Meerwaldt <andy@yoast.com>
2017-11-14 11:08:34 +0100

    Move listener behaviour to notifier object.

--- admin/notifiers/class-configuration-notifier.php
+++ admin/notifiers/class-configuration-notifier.php
@@ -6,7 +6,7 @@
 /**
  * Represents the logic for showing the notification.
  */
-class WPSEO_Configuration_Notifier {
+class WPSEO_Configuration_Notifier implements WPSEO_Listener {
 
 	/**
 	 * Returns the content of the notification.
@@ -40,5 +40,27 @@ class WPSEO_Configuration_Notifier {
 		$notification .= '</div>';
 
 		return $notification;
+	}
+
+	/**
+	 * Listens to an argument in the request URL, when triggered just set the notification to dismissed.
+	 *
+	 * @return void
+	 */
+	public function listen() {
+		if ( ! $this->is_triggered()  ) {
+			return;
+		}
+
+		WPSEO_Configuration_Notification::set_dismissed( get_current_user_id() );
+	}
+
+	/**
+	 * Checks if the request url contains the required argument.
+	 *
+	 * @return bool True when action has been triggered.
+	 */
+	protected function is_triggered() {
+		return filter_input( INPUT_GET, 'dismiss_get_started' ) !== '1';
 	}
 }
