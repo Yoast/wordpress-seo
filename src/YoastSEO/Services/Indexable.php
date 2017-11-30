35a46cab1e83238729400740e1dfe62c38dd21d9
Jip Moors <jhp.moors@gmail.com>
2017-11-30 10:31:56 +0100

    Save term meta data on save

--- src/YoastSEO/Services/Indexable.php
+++ src/YoastSEO/Services/Indexable.php
@@ -12,6 +12,66 @@ class Indexable implements Integration {
 	 */
 	public function register_hooks() {
 		add_action( 'wp_insert_post', array( $this, 'save_post_meta' ), PHP_INT_MAX, 2 );
+		add_action( 'edit_term', array( $this, 'save_term_meta' ), PHP_INT_MAX, 3 );
+	}
+
+	/**
+	 * Update the taxonomy meta data on save.
+	 *
+	 * @param int    $term_id          ID of the term to save data for.
+	 * @param int    $taxonomy_term_id The taxonomy_term_id for the term.
+	 * @param string $taxonomy         The taxonomy the term belongs to.
+	 */
+	public function save_term_meta( $term_id, $taxonomy_term_id, $taxonomy ) {
+		/** @var \Yoast\YoastSEO\Models\Indexable $model */
+		$model = Yoast_Model::factory( 'Indexable' )
+							->where( 'object_id', $term_id )
+							->where( 'object_type', 'term' )
+							->where( 'object_sub_type', $taxonomy )
+							->find_one();
+
+		if ( ! $model ) {
+			$model                  = Yoast_Model::factory( 'Indexable' )->create();
+			$model->object_id       = $term_id;
+			$model->object_type     = 'term';
+			$model->object_sub_type = $taxonomy;
+		}
+
+		$term_meta = \WPSEO_Taxonomy_Meta::get_term_meta( $term_id, $taxonomy );
+
+		$model->permalink = get_term_link( $term_id, $taxonomy );
+		$model->canonical = $term_meta['wpseo_canonical'];
+
+		$model->title         = $term_meta['wpseo_title'];
+		$model->description   = $term_meta['wpseo_desc'];
+		$model->content_score = $term_meta['wpseo_content_score'];
+
+		$model->breadcrumb_title = $term_meta['wpseo_bctitle'];
+
+		$model->og_title       = $term_meta['wpseo_opengraph-title'];
+		$model->og_description = $term_meta['wpseo_opengraph-description'];
+		$model->og_image_url   = $term_meta['wpseo_opengraph-image'];
+
+		$model->twitter_title       = $term_meta['wpseo_twitter-title'];
+		$model->twitter_description = $term_meta['wpseo_twitter-description'];
+		$model->twitter_image_url   = $term_meta['wpseo_twitter-image'];
+
+		$model->robots_noindex = $term_meta['wpseo_noindex'];
+
+		// Not implemented yet.
+		$model->cornerstone     = 0;
+		$model->robots_nofollow = 0;
+
+		// $model->internal_link_count = null;
+		// $model->incoming_link_count = null;
+
+		// @todo: wpseo_sitemap_include
+
+		$model->modified_date_gmt = gmdate( 'Y-m-d H:i:s' );
+
+		$model->save();
+
+		exit;
 	}
 
 	/**
@@ -37,8 +97,7 @@ class Indexable implements Integration {
 		// Implement filling of meta values.
 		$post_meta = \get_post_meta( $id );
 
-		$model->post_date_gmt          = $post->post_date_gmt;
-		$model->post_modified_date_gmt = $post->post_modified_gmt;
+		$model->modified_date_gmt = gmdate( 'Y-m-d H:i:s' );
 
 		$model->permalink = get_permalink( $id );
 
@@ -81,6 +140,16 @@ class Indexable implements Integration {
 		$model->save();
 	}
 
+	/**
+	 * Helper function
+	 *
+	 * @todo convert to something prettier.
+	 *
+	 * @param $model
+	 * @param $post_meta
+	 * @param $target
+	 * @param $source
+	 */
 	protected function set_meta_value( $model, $post_meta, $target, $source ) {
 		if ( ! isset( $post_meta[ $source ] ) ) {
 			return;
