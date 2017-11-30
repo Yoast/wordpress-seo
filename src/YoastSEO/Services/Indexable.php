62304527a6037ae5a13307c37744398180236579
Jip Moors <jhp.moors@gmail.com>
2017-11-30 12:04:12 +0100

    Added todos at several places

--- src/YoastSEO/Services/Indexable.php
+++ src/YoastSEO/Services/Indexable.php
@@ -11,10 +11,45 @@ class Indexable implements Integration {
 	 * Registers all hooks to WordPress.
 	 */
 	public function register_hooks() {
-		add_action( 'wp_insert_post', array( $this, 'save_post_meta' ), PHP_INT_MAX, 2 );
+		// @todo split this up in separate watchers.
+		add_action( 'wp_insert_post', array( $this, 'save_post_meta' ), PHP_INT_MAX, 1 );
 
+		// @todo split this up in separate watchers.
 		add_action( 'created_term', array( $this, 'save_term_meta' ), PHP_INT_MAX, 3 );
 		add_action( 'edited_term', array( $this, 'save_term_meta' ), PHP_INT_MAX, 3 );
+
+		// @todo split this up in separate watchers.
+		add_action( 'profile_update', array( $this, 'save_user_meta' ), PHP_INT_MAX, 2 );
+
+		// @todo introduce `delete_post`, `delete_term` and `delete_user` watchers
+	}
+
+	/**
+	 * @param int $user_id User ID.
+	 */
+	public function save_user_meta( $user_id ) {
+		/** @var \Yoast\YoastSEO\Models\Indexable $model */
+		$model = Yoast_Model::factory( 'Indexable' )
+							->where( 'object_id', $user_id )
+							->where( 'object_type', 'user' )
+							->find_one();
+
+		if ( ! $model ) {
+			$model              = Yoast_Model::factory( 'Indexable' )->create();
+			$model->object_id   = $user_id;
+			$model->object_type = 'user';
+		}
+
+		$model->modified_date_gmt = gmdate( 'Y-m-d H:i:s' );
+
+
+		$model->title           = get_the_author_meta( 'wpseo_title', $user_id );
+		$model->description     = get_the_author_meta( 'wpseo_metadesc', $user_id );
+		$model->sitemap_exclude = get_the_author_meta( 'wpseo_excludeauthorsitemap', $user_id ) === 'on';
+
+		$model->permalink = get_author_posts_url( $user_id );
+
+		$model->save();
 	}
 
 	/**
@@ -39,6 +74,8 @@ class Indexable implements Integration {
 			$model->object_sub_type = $taxonomy;
 		}
 
+		$model->modified_date_gmt = gmdate( 'Y-m-d H:i:s' );
+
 		$term_meta = \WPSEO_Taxonomy_Meta::get_term_meta( $term_id, $taxonomy );
 
 		$model->permalink = get_term_link( $term_id, $taxonomy );
@@ -60,46 +97,55 @@ class Indexable implements Integration {
 
 		$model->robots_noindex = $term_meta['wpseo_noindex'];
 
+		switch ( $term_meta['wpseo_sitemap_include'] === 'always' ) {
+			case 'always':
+				$model->sitemap_exclude = 0;
+				break;
+			case 'never':
+				$model->sitemap_exclude = 1;
+				break;
+			default:
+				$model->sitemap_exclude = null;
+				break;
+		}
+
 		// Not implemented yet.
 		$model->cornerstone     = 0;
 		$model->robots_nofollow = 0;
-
 		// $model->internal_link_count = null;
 		// $model->incoming_link_count = null;
-
-		// @todo: wpseo_sitemap_include
-
-		$model->modified_date_gmt = gmdate( 'Y-m-d H:i:s' );
 
 		$model->save();
 	}
 
 	/**
-	 * @param int $id Post ID.
+	 * @param int $post_id Post ID.
 	 */
-	public function save_post_meta( $id, $post ) {
-		$post_type = get_post_type( $id );
+	public function save_post_meta( $post_id ) {
+		// @todo Don't run for auto-draft or revision.
+
+		$post_type = get_post_type( $post_id );
 
 		/** @var \Yoast\YoastSEO\Models\Indexable $model */
 		$model = Yoast_Model::factory( 'Indexable' )
-							->where( 'object_id', $id )
+							->where( 'object_id', $post_id )
 							->where( 'object_type', 'post' )
 							->where( 'object_sub_type', $post_type )
 							->find_one();
 
 		if ( ! $model ) {
 			$model                  = Yoast_Model::factory( 'Indexable' )->create();
-			$model->object_id       = $id;
+			$model->object_id       = $post_id;
 			$model->object_type     = 'post';
-			$model->object_sub_type = get_post_type( $id );
+			$model->object_sub_type = get_post_type( $post_id );
 		}
 
 		// Implement filling of meta values.
-		$post_meta = \get_post_meta( $id );
+		$post_meta = \get_post_meta( $post_id );
 
 		$model->modified_date_gmt = gmdate( 'Y-m-d H:i:s' );
 
-		$model->permalink = get_permalink( $id );
+		$model->permalink = get_permalink( $post_id );
 
 		$this->set_meta_value( $model, $post_meta, 'canonical', '_yoast_wpseo_canonical' );
 
@@ -123,9 +169,11 @@ class Indexable implements Integration {
 		$this->set_meta_value( $model, $post_meta, 'twitter_image_url', '_yoast_wpseo_twitter-image' );
 		$this->set_meta_value( $model, $post_meta, 'twitter_description', '_yoast_wpseo_twitter-description' );
 
+		$model->sitemap_exclude = null;
+
 		try {
 			$seo_meta = Yoast_Model::factory( 'SEO_Meta' )
-								   ->where( 'object_id', $id )
+								   ->where( 'object_id', $post_id )
 								   ->find_one();
 
 			if ( $seo_meta ) {
@@ -136,7 +184,6 @@ class Indexable implements Integration {
 
 		}
 
-		// Optimize?
 		$model->save();
 	}
 
--- src/YoastSEO/Services/Migration.php
+++ src/YoastSEO/Services/Migration.php
@@ -2,6 +2,21 @@
 
 namespace Yoast\YoastSEO\Services;
 
-interface Migration {
-	public function migrate();
+use Symfony\Component\Console\Output\NullOutput;
+use Yoast\Fake_Input;
+use Yoast\Migrate_Command;
+
+class Migration {
+	public function migrate() {
+
+		try {
+			$input = new Fake_Input();
+			$input->setArgument( 'command', 'migrate' );
+
+			$migrate = new Migrate_Command();
+			$migrate->run( $input, new NullOutput() );
+		} catch ( \Exception $exception ) {
+//			 @todo: Handle exception.
+		}
+	}
 }
