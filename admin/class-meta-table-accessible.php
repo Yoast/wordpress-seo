3d465c09360a0c1102aa43740ed954959c5612ac
Jip Moors <jhp.moors@gmail.com>
2017-12-04 11:35:29 +0100

    Always use transient to check if the tables are accessible
    Introduce extra functions to better check against transient.
    The transient was only used to check if the table was inaccessble at some point.
    This commit introduces the transient value when the table is accessble, without an expiry time.
    
    This will prevent any table checks having to be made after a successful one.

--- admin/class-meta-table-accessible.php
+++ admin/class-meta-table-accessible.php
@@ -14,7 +14,14 @@ class WPSEO_Meta_Table_Accessible {
 	 * @return bool True when table is accessible.
 	 */
 	public static function is_accessible() {
-		return get_transient( self::transient_name() ) === false;
+		$value = get_transient( self::transient_name() );
+
+		// If the value is not set, check the table.
+		if ( null === $value ) {
+			return self::check_table();
+		}
+
+		return $value === 0;
 	}
 
 	/**
@@ -25,19 +32,39 @@ class WPSEO_Meta_Table_Accessible {
 	}
 
 	/**
+	 * Sets the transient value to 0, to indicate the table is accessbile.
+	 */
+	protected static function set_accessible() {
+		set_transient( self::transient_name(), 0 );
+	}
+
+	/**
 	 * Checks if the table exists if not, set the transient to indicate the inaccessible table.
+	 *
+	 * @deprecated 6.0
 	 *
 	 * @return bool True if table is accessible.
 	 */
 	public static function check_table_is_accessible() {
+		_deprecated_function( __FUNCTION__, '6.0', __CLASS__ . '::is_accessible' );
+		return self::is_accessible();
+	}
+
+	/**
+	 * Checks if the table exists if not, set the transient to indicate the inaccessible table.
+	 *
+	 * @return bool
+	 */
+	protected static function check_table() {
 		global $wpdb;
 
-		$storage = new WPSEO_Meta_Storage();
+		$storage = new WPSEO_Link_Storage();
 		if ( $wpdb->get_var( 'SHOW TABLES LIKE "' . $storage->get_table_name() . '"' ) !== $storage->get_table_name() ) {
 			self::set_inaccessible();
 			return false;
 		}
 
+		self::set_accessible();
 		return true;
 	}
 
@@ -56,6 +83,6 @@ class WPSEO_Meta_Table_Accessible {
 	 * @return string The name of the transient to use.
 	 */
 	protected static function transient_name() {
-		return 'wpseo_meta_table_accessible';
+		return 'wpseo_meta_table_inaccessible';
 	}
 }
