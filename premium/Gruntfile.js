28d4beadfd3ff6c94f5c20226771988a33ed4f73
Jip Moors <jhp.moors@gmail.com>
2017-12-05 12:42:29 +0100

    Bump version 5.9.1

--- premium/Gruntfile.js
+++ premium/Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
 
 	require('time-grunt')(grunt);
 
-	let pluginVersion = "5.9";
+	let pluginVersion = "5.9.1";
 
 	// Define project configuration
 	var project = {
