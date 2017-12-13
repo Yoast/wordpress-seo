51111428b950c3af13b7eba86fe9629aa53ca272
Jimmy Comack <jimmy@yoast.com>
2017-12-13 13:58:25 +0100

    Fixed tests

--- spec/bundledPlugins/previouslyUsedKeywordsSpec.js
+++ spec/bundledPlugins/previouslyUsedKeywordsSpec.js
@@ -28,7 +28,7 @@ describe( "checks for keyword doubles", function(){
 			"be sure to make very clear which URL on your site is the most important for this keyword." );
 
 		expect( plugin.scoreAssessment( {id: 1, count: 2}, paper, i18n ).score ).toBe( 1 );
-		expect( plugin.scoreAssessment({id: 1, count: 2 }, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://search?keyword' target='_blank'>2 times before</a>, it's probably a good idea to read <a href='https://yoast.com/cornerstone-content-rank/' target='_blank'>this post on cornerstone content</a> and improve your keyword strategy." );
+		expect( plugin.scoreAssessment({id: 1, count: 2 }, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://search?keyword' target='_blank'>2 times before</a>. It’s probably a good idea to read <a href='https://yoa.st/20x' target='_blank' rel='noopener noreferrer'>this post</a> about why you should not use your focus keyword more than once." );
 
 		expect( plugin.scoreAssessment( {id: 0, count: 0}, paper, i18n ).score ).toBe( 9 );
 		expect( plugin.scoreAssessment( {id: 0, count: 0}, paper, i18n ).text ).toBe( "You've never used this focus keyword before, very good." );
@@ -37,7 +37,7 @@ describe( "checks for keyword doubles", function(){
 	it("escapes the keyword's special characters in the url", function(){
 		var paper = new Paper( "text", { keyword: "keyword/bla"} );
 		var plugin = new PreviouslyUsedKeywords( app, args, i18n );
-		expect( plugin.scoreAssessment({id: 1, count: 2 }, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://search?keyword%2Fbla' target='_blank'>2 times before</a>, it's probably a good idea to read <a href='https://yoast.com/cornerstone-content-rank/' target='_blank'>this post on cornerstone content</a> and improve your keyword strategy." );
+		expect( plugin.scoreAssessment({id: 1, count: 2 }, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://search?keyword%2Fbla' target='_blank'>2 times before</a>. It’s probably a good idea to read <a href='https://yoa.st/20x' target='_blank' rel='noopener noreferrer'>this post</a> about why you should not use your focus keyword more than once." );
 	});
 });
 
