81ec630b42d87207ab985868f24170c4f69219f6
IreneStr <irene@yoast.com>
2017-12-18 16:07:34 +0100

    Update tests

--- spec/bundledPlugins/previouslyUsedKeywordsSpec.js
+++ spec/bundledPlugins/previouslyUsedKeywordsSpec.js
@@ -24,11 +24,10 @@ describe( "checks for keyword doubles", function(){
 
 		var plugin = new PreviouslyUsedKeywords( app, args, i18n );
 		expect( plugin.scoreAssessment( {id: 1, count: 1}, paper, i18n ).score ).toBe( 6 );
-		expect( plugin.scoreAssessment( {id: 1, count: 1}, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://post?1' target='_blank'>once before</a>, " +
-			"be sure to make very clear which URL on your site is the most important for this keyword." );
+		expect( plugin.scoreAssessment( {id: 1, count: 1}, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://post?1' target='_blank'>once before</a>. It’s probably a good idea to read about <a href='https://yoa.st/20x' target='_blank' rel='noopener noreferrer'>why you should not use your focus keyword more than once</a>." );
 
 		expect( plugin.scoreAssessment( {id: 1, count: 2}, paper, i18n ).score ).toBe( 1 );
-		expect( plugin.scoreAssessment({id: 1, count: 2 }, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://search?keyword' target='_blank'>2 times before</a>. It’s probably a good idea to read <a href='https://yoa.st/20x' target='_blank' rel='noopener noreferrer'>this post</a> about why you should not use your focus keyword more than once." );
+		expect( plugin.scoreAssessment({id: 1, count: 2 }, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://search?keyword' target='_blank'>2 times before</a>. It’s probably a good idea to read about <a href='https://yoa.st/20x' target='_blank' rel='noopener noreferrer'>why you should not use your focus keyword more than once</a>." );
 
 		expect( plugin.scoreAssessment( {id: 0, count: 0}, paper, i18n ).score ).toBe( 9 );
 		expect( plugin.scoreAssessment( {id: 0, count: 0}, paper, i18n ).text ).toBe( "You've never used this focus keyword before, very good." );
@@ -37,7 +36,7 @@ describe( "checks for keyword doubles", function(){
 	it("escapes the keyword's special characters in the url", function(){
 		var paper = new Paper( "text", { keyword: "keyword/bla"} );
 		var plugin = new PreviouslyUsedKeywords( app, args, i18n );
-		expect( plugin.scoreAssessment({id: 1, count: 2 }, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://search?keyword%2Fbla' target='_blank'>2 times before</a>. It’s probably a good idea to read <a href='https://yoa.st/20x' target='_blank' rel='noopener noreferrer'>this post</a> about why you should not use your focus keyword more than once." );
+		expect( plugin.scoreAssessment({id: 1, count: 2 }, paper, i18n ).text ).toBe( "You've used this focus keyword <a href='http://search?keyword%2Fbla' target='_blank'>2 times before</a>. It’s probably a good idea to read about <a href='https://yoa.st/20x' target='_blank' rel='noopener noreferrer'>why you should not use your focus keyword more than once</a>." );
 	});
 });
 
