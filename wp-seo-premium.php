45d94b2c7c82e9d910a4c1fb60873e0d722a9b70
jrfnl <jrfnl@users.noreply.github.com>
2017-12-25 08:10:13 +0100

    Documentation: Add short description to plugin file headers [2]
    Includes minor docblock layout changes.
    
    1. For the main plugin file: join the three docblocks into one and align the headers.
    2. `@var` tags for views are in a separate docblock everywhere else. Fixes for consistency.
    3. If provided, a file "long description" should follow the short description. Tags should start after these.

--- wp-seo-premium.php
+++ wp-seo-premium.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * @package WPSEO\Main
- */
-
-/**
+ * Yoast SEO Plugin.
+ *
+ * WPSEO Premium plugin file.
+ *
+ * @package   WPSEO\Main
+ * @copyright Copyright (C) 2008-2016, Yoast BV - support@yoast.com
+ * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3 or higher
+ *
+ * @wordpress-plugin
  * Plugin Name: Yoast SEO Premium
- * Version: 6.1.2
- * Plugin URI: https://yoast.com/wordpress/plugins/seo/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseoplugin
+ * Version:     6.1.2
+ * Plugin URI:  https://yoast.com/wordpress/plugins/seo/#utm_source=wpadmin&utm_medium=plugin&utm_campaign=wpseoplugin
  * Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
- * Author: Team Yoast
- * Author URI: https://yoast.com/
+ * Author:      Team Yoast
+ * Author URI:  https://yoast.com/
  * Text Domain: wordpress-seo
  * Domain Path: /languages/
- * License: GPL v3
- */
-
-/**
- * Yoast SEO Plugin
- * Copyright (C) 2008-2016, Yoast BV - support@yoast.com
+ * License:     GPL v3
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
