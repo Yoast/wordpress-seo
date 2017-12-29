ee214d3ad2d269349f304cbb0cd9d9aec7664d18
Jip Moors <jhp.moors@gmail.com>
2017-12-29 13:22:54 +0100

    Expose method to get table name

--- src/Yoast_Model.php
+++ src/Yoast_Model.php
@@ -16,15 +16,25 @@ class Yoast_Model extends Model {
 	 * @return \ORMWrapper
 	 */
 	public static function of_type( $class_name, $connection_name = null ) {
-		global $wpdb;
-
 		// Prepend namespace to the class name.
 		$class = self::$auto_prefix_models . $class_name;
 
 		// Set the class variable to the custom value based on the WPDB prefix.
-		$class::$_table = $wpdb->prefix . 'yoast_' . strtolower( $class_name );
+		$class::$_table = self::get_table_name( $class_name );
 
-		// Proceed normally.
 		return parent::factory( $class_name, $connection_name );
+	}
+
+	/**
+	 * Exposes method to get the table name to use.
+	 *
+	 * @param string $table_name Simple table name.
+	 *
+	 * @return string Prepared full table name.
+	 */
+	public static function get_table_name( $table_name ) {
+		global $wpdb;
+
+		return $wpdb->prefix . 'yoast_' . strtolower( $table_name );
 	}
 }
