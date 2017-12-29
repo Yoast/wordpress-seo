74a2d165339fa061c43d1e4c74990d9154ab7cf7
Jip Moors <jhp.moors@gmail.com>
2017-12-29 15:51:25 +0100

    Formatting and documentation

--- src/Config/Migrations.php
+++ src/Config/Migrations.php
@@ -20,45 +20,59 @@ class Migrations {
 	}
 
 	/**
-	 *
+	 * Initializes the migrations.
 	 */
 	public function initialize() {
 		// @todo evaluate prefixing approach for global constants.
 		define( 'RUCKUSING_BASE', WPSEO_PATH . '/vendor/ruckusing/ruckusing-migrations' );
 		define( 'RUCKUSING_TS_SCHEMA_TBL_NAME', Yoast_Model::get_table_name( 'migrations' ) );
 
-		$main = new Ruckusing_FrameworkRunner( $this->get_configuration(), array( 'db:migrate', 'env=development' ), new Migration_Null_Logger() );
+		$main = new Ruckusing_FrameworkRunner( $this->get_configuration(), array(
+			'db:migrate',
+			'env=production'
+		), new Migration_Null_Logger() );
 		$main->execute();
 
+		/*
+		 * As the Ruckusing_FrameworkRunner is setting its own error and exception handlers,
+		 * we need to restore the defaults.
+		 */
 		restore_error_handler();
 		restore_exception_handler();
 	}
 
 	/**
-	 * @return array
+	 * Retrieves the migration configuration.
+	 *
+	 * @return array List of configuration elements.
 	 */
 	protected function get_configuration() {
 		return array(
 			'db'             => array(
-				'development' => array(
-					'type'     => 'mysql',
-					'host'     => DB_HOST,
-					'port'     => 3306,
-					'database' => DB_NAME,
-					'user'     => DB_USER,
-					'password' => DB_PASSWORD,
-					'charset'  => $this->get_charset(),
-					'directory' => '',
+				'production' => array(
+					'type'      => 'mysql',
+					'host'      => DB_HOST,
+					'port'      => 3306,
+					'database'  => DB_NAME,
+					'user'      => DB_USER,
+					'password'  => DB_PASSWORD,
+					'charset'   => $this->get_charset(),
+					'directory' => '', // This needs to be set, to use the migrations folder as base folder.
 				),
 			),
 			'migrations_dir' => array( 'default' => WPSEO_PATH . 'migrations' ),
-			'db_dir'         => WPSEO_PATH . 'db',
-			'log_dir'        => WPSEO_PATH . 'logs',
+			// This needs to be set but is not used.
+			'db_dir'         => true,
+			// This needs to be set but is not used.
+			'log_dir'        => true,
+			// This needs to be set but is not used.
 		);
 	}
 
 	/**
-	 * @return mixed
+	 * Retrieves the database charset.
+	 *
+	 * @return string Charset configured for the database.
 	 */
 	protected function get_charset() {
 		return $this->wpdb->charset;
