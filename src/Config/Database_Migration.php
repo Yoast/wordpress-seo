1edfa7a44b26bf3d96e43a6b42ca9b8c61ddfcca
Jip Moors <jhp.moors@gmail.com>
2018-01-09 16:09:38 +0100

    Extract methods

--- src/Config/Database_Migration.php
+++ src/Config/Database_Migration.php
@@ -28,31 +28,27 @@ class Database_Migration {
 	 * @return bool
 	 */
 	public function initialize() {
-		if ( '1' === get_transient('yoast_migration_problem' ) ) {
+		if ( '1' === get_transient( 'yoast_migration_problem' ) ) {
 			return false;
 		}
 
-		// @todo evaluate prefixing approach for global constants.
-		// @todo handle already defined constant, throw exception/error message?
+		// If the defines could not be set, we do not want to run.
+		if ( ! $this->set_defines( Yoast_Model::get_table_name( 'migrations' ) ) ) {
+			$this->set_failed_state( 'Defines could not be set.' );
 
-		$table_name = Yoast_Model::get_table_name( 'migrations' );
-
-		if ( is_dir( WPSEO_PATH . YOAST_VENDOR_PREFIX_DIRECTORY . '/ruckusing' ) ) {
-			define( YOAST_VENDOR_DEFINE_PREFIX . 'RUCKUSING_BASE', WPSEO_PATH . YOAST_VENDOR_PREFIX_DIRECTORY . '/ruckusing' );
-			define( YOAST_VENDOR_DEFINE_PREFIX . 'RUCKUSING_TS_SCHEMA_TBL_NAME', $table_name );
-		} else {
-			define( 'RUCKUSING_BASE', WPSEO_PATH . 'vendor/ruckusing/ruckusing-migrations' );
-			define( 'RUCKUSING_TS_SCHEMA_TBL_NAME', $table_name );
+			return false;
 		}
 
-		$ruckusing_classes = new ClassAliases\Ruckusing();
-		$prefix            = new Prefix_Dependencies( YOAST_VENDOR_NS_PREFIX );
-		$prefix->prefix( $ruckusing_classes->get_classes() );
+		$this->prefix_dependencies();
 
-		$main = new Ruckusing_FrameworkRunner( $this->get_configuration(), array(
-			'db:migrate',
-			'env=production'
-		), new Migration_Null_Logger() );
+		$main = new Ruckusing_FrameworkRunner(
+			$this->get_configuration(),
+			array(
+				'db:migrate',
+				'env=production'
+			),
+			new Migration_Null_Logger()
+		);
 
 		/*
 		 * As the Ruckusing_FrameworkRunner is setting its own error and exception handlers,
@@ -63,11 +59,11 @@ class Database_Migration {
 
 		try {
 			$main->execute();
-		} catch( \Exception $exception ) {
+		} catch ( \Exception $exception ) {
 			// Something went wrong...
 			// Disable functionality?
-			// Set transient.. try again later.
-			set_transient( 'yoast_migration_problem', 1, DAY_IN_SECONDS );
+			$this->set_failed_state( $exception->getMessage() );
+
 			return false;
 		}
 
@@ -109,5 +105,58 @@ class Database_Migration {
 	 */
 	protected function get_charset() {
 		return $this->wpdb->charset;
+	}
+
+	/**
+	 * Prefixes the dependencies.
+	 *
+	 * @return void
+	 */
+	protected function prefix_dependencies() {
+		$ruckusing_classes = new ClassAliases\Ruckusing();
+		$prefix            = new Prefix_Dependencies( YOAST_VENDOR_NS_PREFIX );
+		$prefix->prefix( $ruckusing_classes->get_classes() );
+	}
+
+	/**
+	 * @param $table_name
+	 *
+	 * @return bool
+	 */
+	protected function set_defines( $table_name ) {
+		if ( $this->prefixed_available() ) {
+			define( YOAST_VENDOR_DEFINE_PREFIX . 'RUCKUSING_BASE', WPSEO_PATH . YOAST_VENDOR_PREFIX_DIRECTORY . '/ruckusing' );
+			define( YOAST_VENDOR_DEFINE_PREFIX . 'RUCKUSING_TS_SCHEMA_TBL_NAME', $table_name );
+
+			return true;
+		}
+
+		if ( ! defined( 'RUCKUSING_BASE' ) ) {
+			define( 'RUCKUSING_BASE', WPSEO_PATH . 'vendor/ruckusing/ruckusing-migrations' );
+			define( 'RUCKUSING_TS_SCHEMA_TBL_NAME', $table_name );
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @return bool
+	 */
+	protected function prefixed_available() {
+		return is_dir( WPSEO_PATH . YOAST_VENDOR_PREFIX_DIRECTORY . '/ruckusing' );
+	}
+
+	/**
+	 * Handles state persistence for a failed migration environment.
+	 *
+	 * @param string $message Message explaining the reason for the failed state.
+	 *
+	 * @return void
+	 */
+	protected function set_failed_state( $message ) {
+		// @todo do something with the message.
+		set_transient( 'yoast_migration_problem', 1, DAY_IN_SECONDS );
 	}
 }
