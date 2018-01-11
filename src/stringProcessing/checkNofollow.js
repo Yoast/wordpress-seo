675d4c7e498a315f7c8178b9dd82f6902a773b55
Jip Moors <jhp.moors@gmail.com>
2018-01-11 11:00:42 +0100

    Apply CR

--- src/stringProcessing/checkNofollow.js
+++ src/stringProcessing/checkNofollow.js
@@ -2,7 +2,6 @@
 
 // We use an external library, which can be found here: https://github.com/fb55/htmlparser2.
 let htmlparser = require( "htmlparser2" );
-let isString = require( "lodash/isString" );
 
 /**
  * Checks if a links has a nofollow attribute value. If it has, returns Nofollow, otherwise Dofollow.
@@ -18,15 +17,15 @@ module.exports = function( anchorHTML ) {
 		 * Detects if there is a `nofollow` argument value in the `rel` argument of a link.
 		 *
 		 * @param {string} tagName The tag name.
-		 * @param {object} nodeValue The attribute with the keys and values of the tag.
+		 * @param {object} attributes  The attribute with the keys and values of the tag.
 		 * @returns {void}
 		 */
-		onopentag: function( tagName, nodeValue ) {
-			if ( tagName !== "a" || ! isString( nodeValue.rel ) ) {
+		onopentag: function( tagName, attributes ) {
+			if ( tagName !== "a" || ! attributes.rel ) {
 				return;
 			}
 
-			if ( nodeValue.rel.toLowerCase().split( /\s/ ).includes( "nofollow" ) ) {
+			if ( attributes.rel.toLowerCase().split( /\s/ ).includes( "nofollow" ) ) {
 				linkFollow = "Nofollow";
 			}
 		},
