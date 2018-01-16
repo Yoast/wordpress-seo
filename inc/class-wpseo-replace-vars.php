2efc7c9910e7b095dfed41a1890de0a2389428a6
John-B <john@birchall.info>
2018-01-16 17:33:09 +0000

    issue 8661
    change auto-generated description max length to 320, also trim it to word boundary

--- inc/class-wpseo-replace-vars.php
+++ inc/class-wpseo-replace-vars.php
@@ -334,7 +334,9 @@ class WPSEO_Replace_Vars {
 				$replacement = wp_strip_all_tags( $this->args->post_excerpt );
 			}
 			elseif ( $this->args->post_content !== '' ) {
-				$replacement = wp_html_excerpt( strip_shortcodes( $this->args->post_content ), 155 );
+                $replacement = wp_html_excerpt( strip_shortcodes( $this->args->post_content ), 320 );
+                // trim the auto-generated string to a word boundary
+                $replacement = substr($replacement, 0, strrpos($replacement, ' '));
 			}
 		}
 
