74573b4d75c752630cc1d4066e6da882847778a9
Jip <jhp.moors@gmail.com>
2018-01-22 16:53:50 +0100

    Moved line down

--- tests/inc/test-class-post-type.php
+++ tests/inc/test-class-post-type.php
@@ -193,12 +193,13 @@ class WPSEO_Post_Type_Test extends WPSEO_UnitTestCase {
 	 * @covers WPSEO_Post_Type::is_rest_enabled()
 	 */
 	public function test_rest_enabled_post_types() {
-		register_post_type( 'custom-post-type', array( 'public' => true ) );
 		$this->assertTrue( WPSEO_Post_Type::is_rest_enabled( 'post' ) );
 		$this->assertFalse( WPSEO_Post_Type::is_rest_enabled( 'invalid_post_type' ) );
 
 		register_post_type( 'custom-post-type-api', array( 'public' => true, 'show_in_rest' => true ) );
 		$this->assertTrue( WPSEO_Post_Type::is_rest_enabled( 'custom-post-type-api' ) );
+
+		register_post_type( 'custom-post-type', array( 'public' => true ) );
 		$this->assertFalse( WPSEO_Post_Type::is_rest_enabled( 'custom-post-type' ) );
 	}
 }
