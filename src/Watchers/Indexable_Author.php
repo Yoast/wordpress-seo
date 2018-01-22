b167a88c4b6e7007f18b1fcb742871de4b841b40
Jip Moors <jhp.moors@gmail.com>
2018-01-22 11:14:36 +0100

    Add get_indexable to author watcher

--- src/Watchers/Indexable_Author.php
+++ src/Watchers/Indexable_Author.php
@@ -26,17 +26,10 @@ class Indexable_Author implements Integration {
 	 * @return void
 	 */
 	public function delete_meta( $user_id ) {
-		/** @var Indexable $model */
-		$model = Yoast_Model::of_type( 'Indexable' )
-							->where( 'object_id', $user_id )
-							->where( 'object_type', 'user' )
-							->find_one();
-
-		if ( ! $model ) {
-			return;
+		$indexable = $this->get_indexable( $user_id, false );
+		if ( $indexable ) {
+			$indexable->delete();
 		}
-
-		$model->delete();
 	}
 
 	/**
@@ -47,26 +40,38 @@ class Indexable_Author implements Integration {
 	 * @return void
 	 */
 	public function save_meta( $user_id ) {
-		/** @var Indexable $model */
-		$model = Yoast_Model::of_type( 'Indexable' )
-							->where( 'object_id', $user_id )
-							->where( 'object_type', 'user' )
-							->find_one();
-
-		if ( ! $model ) {
-			$model              = Yoast_Model::of_type( 'Indexable' )->create();
-			$model->object_id   = $user_id;
-			$model->object_type = 'user';
-		}
+		$model = $this->get_indexable( $user_id );
 
 		$model->updated_at = gmdate( 'Y-m-d H:i:s' );
+		$model->permalink = get_author_posts_url( $user_id );
 
 		$model->title              = get_the_author_meta( 'wpseo_title', $user_id );
 		$model->description        = get_the_author_meta( 'wpseo_metadesc', $user_id );
 		$model->include_in_sitemap = get_the_author_meta( 'wpseo_excludeauthorsitemap', $user_id ) === 'on';
 
-		$model->permalink = get_author_posts_url( $user_id );
-
 		$model->save();
+	}
+
+	/**
+	 * Retrieves the indexable for a user.
+	 *
+	 * @param int  $user_id     The use to retrieve the indexable for.
+	 * @param bool $auto_create Optional. Create the indexable when it does not exist yet.
+	 *
+	 * @return bool|Indexable
+	 */
+	protected function get_indexable( $user_id, $auto_create = true ) {
+		$indexable = Yoast_Model::of_type( 'Indexable' )
+								->where( 'object_id', $user_id )
+								->where( 'object_type', 'user' )
+								->find_one();
+
+		if ( $auto_create && ! $indexable ) {
+			$indexable              = Yoast_Model::of_type( 'Indexable' )->create();
+			$indexable->object_id   = $user_id;
+			$indexable->object_type = 'user';
+		}
+
+		return $indexable;
 	}
 }
