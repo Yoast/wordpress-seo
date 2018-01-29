9008f6556cd1f72822791792fff5364294ce87e1
Manuel <manuel@yoast.com>
2018-01-29 11:11:09 +0100

    Simplify code

--- src/researches/getReadingTime.js
+++ src/researches/getReadingTime.js
@@ -5,8 +5,6 @@ module.exports = function( paper ) {
 	let numberOfWords = wordCountInText( paper );
 	let numberOfImages = imageCount( paper );
 
-	let readingTime = Math.ceil( ( numberOfWords / 200 ) + ( numberOfImages * 0.2 ) );
-
-	return readingTime;
+	return Math.ceil( ( numberOfWords / 200 ) + ( numberOfImages * 0.2 ) );
 };
 
