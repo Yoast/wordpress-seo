0efc508bdb8a35824fe92d2d682d0e7056268304
Anton Timmermans <email@atimmer.com>
2018-01-31 14:54:38 +0100

    Improve documentation of setTitleWidth

--- src/snippetPreview.js
+++ src/snippetPreview.js
@@ -1232,9 +1232,11 @@ SnippetPreview.prototype.getTitleWidth = function() {
 
 /**
  * Allows to manually set the title width.
+ *
  * This may be useful in setups where the title field will not always be rendered.
  *
  * @param {Number} titleWidth The width of the title in pixels.
+ *
  * @returns {void}
  */
 SnippetPreview.prototype.setTitleWidth = function( titleWidth ) {
