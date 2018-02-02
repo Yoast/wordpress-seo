a770f32ed8265395dce091af35950ce52a867025
Joost de Valk <joost@yoast.com>
2018-02-02 11:00:56 +0100

    Introduce get and set function for WPSEO_Options (#8816)
    * Add get and set function
    * Remove get_option_value method as it shouldn't be used
    * Added test to make sure Option keys are unique
    * Remove irrelevant tests

--- tests/inc/options/test-class-wpseo-options.php
+++ tests/inc/options/test-class-wpseo-options.php
@@ -100,39 +100,76 @@ class WPSEO_Options_Test extends WPSEO_UnitTestCase {
 	/**
 	 * Tests if the get_option_value function returns a valid result.
 	 *
-	 * @covers WPSEO_Options::get_option_value()
+	 * @covers WPSEO_Options::get()
 	 */
-	public function test_get_option_value_returns_valid_result() {
-		// Make sure these two have their default values
+	public function test_get_returns_valid_result() {
 		$option                            = WPSEO_Options::get_option( 'wpseo' );
 		$option['keyword_analysis_active'] = true;
 		$option['show_onboarding_notice']  = false;
 		update_option( 'wpseo', $option );
 
-		$result = WPSEO_Options::get_option_value( 'wpseo', 'keyword_analysis_active' );
+		$result = WPSEO_Options::get( 'keyword_analysis_active' );
 		$this->assertTrue( $result );
 
-		$result = WPSEO_Options::get_option_value( 'wpseo', 'show_onboarding_notice' );
+		$result = WPSEO_Options::get( 'show_onboarding_notice' );
 		$this->assertFalse( $result );
 	}
 
 	/**
 	 * Tests if the get_option_value function returns a valid result.
 	 *
-	 * @covers WPSEO_Options::get_option_value()
+	 * @covers WPSEO_Options::get()
 	 */
-	public function test_get_option_value_returns_null_result() {
-		$result = WPSEO_Options::get_option_value( 'wpseo', 'non_existent_value' );
+	public function test_get_returns_null_result() {
+		$result = WPSEO_Options::get( 'non_existent_value' );
 		$this->assertNull( $result );
 	}
 
 	/**
 	 * Tests if the get_option_value function returns a valid result.
 	 *
-	 * @covers WPSEO_Options::get_option_value()
+	 * @covers WPSEO_Options::get()
 	 */
-	public function test_get_option_value_returns_default_result() {
-		$result = WPSEO_Options::get_option_value( 'wpseo', 'non_existent_value', array() );
+	public function test_get_returns_default_result() {
+		$result = WPSEO_Options::get( 'non_existent_value', array() );
 		$this->assertEquals( array(), $result );
+	}
+
+	/**
+	 * Tests if the get_option_value function returns a valid result.
+	 *
+	 * @covers WPSEO_Options::get()
+	 */
+	public function test_set_works() {
+		$option_before                            = WPSEO_Options::get_option( 'wpseo' );
+		$option_before['keyword_analysis_active'] = true;
+		$option_before['show_onboarding_notice']  = false;
+		update_option( 'wpseo', $option_before );
+
+		// Turn them around and see if they still return the correct value
+		WPSEO_Options::set( 'keyword_analysis_active', false );
+		WPSEO_Options::set( 'show_onboarding_notice', true );
+
+		$result = WPSEO_Options::get( 'keyword_analysis_active' );
+		$this->assertFalse( $result );
+
+		$result = WPSEO_Options::get( 'show_onboarding_notice' );
+		$this->assertTrue( $result );
+	}
+
+	/**
+	 * Tests if unique keys are used in all options.
+	 */
+	public function test_make_sure_keys_are_unique_over_options() {
+		$keys = array();
+
+		foreach ( array_keys( WPSEO_Options::$options ) as $option_name ) {
+			$option_keys = array_keys( WPSEO_Options::get_option( $option_name ) );
+			$intersected = array_intersect( $option_keys, $keys );
+
+			$this->assertEquals( array(), $intersected, 'Option keys must be unique (' . $option_name . ').' );
+
+			$keys = array_merge( $keys, $option_keys );
+		}
 	}
 }
