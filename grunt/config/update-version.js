41ce31ca3bc13b39cd7097accf885cc0441bae25
Jip Moors <jhp.moors@gmail.com>
2018-02-09 11:49:36 +0100

    Make sure the RC1 part is handled properly in updating version

--- grunt/config/update-version.js
+++ grunt/config/update-version.js
@@ -5,25 +5,25 @@ module.exports = {
 	},
 	readme: {
 		options: {
-			regEx: /(Stable tag: )(\d\d?(\.\d\d?){0,3})([^\.\d]?.*?\n)/,
+			regEx: /(Stable tag: )(\d+(\.\d+){0,3})([^\.\d]?.*?)(\n)/,
 			preVersionMatch: "$1",
-			postVersionMatch: "$4",
+			postVersionMatch: "$5",
 		},
 		src: "readme.txt",
 	},
 	pluginFile: {
 		options: {
-			regEx: /(\* Version: )(\d\d?(\.\d\d?){0,3})([^\.\d]?.*?\n)/,
+			regEx: /(\* Version: )(\d+(\.\d+){0,3})([^\.\d]?.*?)(\n)/,
 			preVersionMatch: "$1",
-			postVersionMatch: "$4",
+			postVersionMatch: "$5",
 		},
 		src: "wp-seo.php",
 	},
 	initializer: {
 		options: {
-			regEx: /(define\( \'WPSEO_VERSION\'\, \')(\d\d?(\.\d\d?){0,3})(\'.*?\n)/,
+			regEx: /(define\( \'WPSEO_VERSION\'\, \')(\d+(\.\d+){0,3})([^\.^\'\d]?.*?)(\' \);\n)/,
 			preVersionMatch: "$1",
-			postVersionMatch: "$4",
+			postVersionMatch: "$5",
 		},
 		src: "wp-seo-main.php",
 	},
