900a7a0f109d3f6545805462c84cf9cef285d7d1
Joost de Valk <joost@yoast.com>
2018-02-09 12:15:00 +0100

    Merged trunk, slight CSS change

--- admin/views/tabs/metas/general.php
+++ admin/views/tabs/metas/general.php
@@ -16,5 +16,5 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 // To improve readability, this tab has been divided into 5 separate blocks, included below.
 require dirname( __FILE__ ) . '/general/force-rewrite-title.php';
 require dirname( __FILE__ ) . '/general/title-separator.php';
-require dirname( __FILE__ ) . '/general/knowledge-graph.php';
 require dirname( __FILE__ ) . '/general/homepage.php';
+require dirname( __FILE__ ) . '/general/knowledge-graph.php';
