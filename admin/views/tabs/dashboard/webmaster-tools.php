f1ba0cad4cafbefb66f9977ebf41966c3cc344ce
Andrea Fercia <a.fercia@gmail.com>
2018-02-13 17:19:24 +0100

    Help for Dashboard webmaster tools tab.

--- admin/views/tabs/dashboard/webmaster-tools.php
+++ admin/views/tabs/dashboard/webmaster-tools.php
@@ -13,11 +13,15 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 	exit();
 }
 
-echo '<h2>' . esc_html__( 'Webmaster Tools verification', 'wordpress-seo' ) . '</h2>';
-printf(
-	'<p>%s</p>',
-	esc_html__( 'You can use the boxes below to verify with the different Webmaster Tools, if your site is already verified, you can just forget about these. Enter the verify meta values for:', 'wordpress-seo' )
+$webmaster_tools_help = new WPSEO_Admin_Help_Panel(
+	'search-appearance-webmaster-tools',
+	__( 'Learn more about the Webmaster Tools verification', 'wordpress-seo' ),
+	__( 'You can use the boxes below to verify with the different Webmaster Tools, if your site is already verified, you can just forget about these.', 'wordpress-seo' ),
+	'has-wrapper'
 );
+
+echo '<h2 class="help-button-inline">' . esc_html__( 'Webmaster Tools verification', 'wordpress-seo' ) . $webmaster_tools_help->get_button_html() . '</h2>';
+echo $webmaster_tools_help->get_panel_html();
 
 $yform->textinput( 'msverify', '<a target="_blank" href="' . esc_url( 'http://www.bing.com/webmaster/?rfp=1#/Dashboard/?url=' . urlencode( str_replace( 'http://', '', get_bloginfo( 'url' ) ) ) ) . '">' . __( 'Bing Webmaster Tools', 'wordpress-seo' ) . '</a>' );
 $yform->textinput( 'googleverify', '<a target="_blank" href="' . esc_url( 'https://www.google.com/webmasters/verification/verification?hl=en&siteUrl=' . urlencode( get_bloginfo( 'url' ) ) . '/' ) . '">Google Search Console</a>' );
