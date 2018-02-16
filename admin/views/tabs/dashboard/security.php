5113fd46b9f9183c83e0b42ac6eb89b16149e752
Andrea Fercia <a.fercia@gmail.com>
2018-02-16 16:47:32 +0100

    Add example Help to toggle_switch() in the security tab.

--- admin/views/tabs/dashboard/security.php
+++ admin/views/tabs/dashboard/security.php
@@ -15,20 +15,26 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 
 echo '<h2>' . esc_html__( 'Security setting', 'wordpress-seo' ) . '</h2>';
 
+$disableadvanced_help = new WPSEO_Admin_Help_Panel(
+	'disableadvanced',
+	esc_html__( 'Help on the advanced meta box setting', 'wordpress-seo' ),
+	sprintf(
+		/* translators: %1$s expands to Yoast SEO */
+		esc_html__( 'The advanced section of the %1$s meta box allows a user to noindex posts or change the canonical. These are things you might not want if you don\'t trust your authors, so by default, only administrators can do this. Enabling the advanced box allows all users to change these settings.', 'wordpress-seo' ),
+		'Yoast SEO'
+	)
+);
+
 $yform->toggle_switch(
 	'disableadvanced_meta',
 	array(
 		'off' => __( 'Enabled', 'wordpress-seo' ),
 		'on'  => __( 'Disabled', 'wordpress-seo' ),
 	),
-	/* translators: %1$s expands to Yoast SEO */
-	sprintf( __( 'Advanced part of the %1$s meta box', 'wordpress-seo' ), 'Yoast SEO' )
+	sprintf(
+		/* translators: %1$s expands to Yoast SEO */
+		__( 'Advanced part of the %1$s meta box', 'wordpress-seo' ),
+		'Yoast SEO'
+	),
+	$disableadvanced_help->get_button_html() . $disableadvanced_help->get_panel_html()
 );
-
-echo '<p>';
-printf(
-	/* translators: %1$s expands to Yoast SEO */
-	esc_html__( 'The advanced section of the %1$s meta box allows a user to noindex posts or change the canonical. These are things you might not want if you don\'t trust your authors, so by default, only administrators can do this. Enabling the advanced box allows all users to change these settings.', 'wordpress-seo' ),
-	'Yoast SEO'
-);
-echo '</p>';
