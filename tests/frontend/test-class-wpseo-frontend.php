cb384f80b5a860d50a62c2d5ad2ff5d229140bb0
Jip Moors <jhp.moors@gmail.com>
2018-02-21 14:06:03 +0100

    Added tests to verify behaviour
    Update `@covers`

--- tests/frontend/test-class-wpseo-frontend.php
+++ tests/frontend/test-class-wpseo-frontend.php
@@ -20,6 +20,14 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 	}
 
 	/**
+	 * Reset permalink structure.
+	 */
+	public function setUp() {
+		parent::setUp();
+		$this->set_permalink_structure( '' );
+	}
+
+	/**
 	 * Reset after running a test.
 	 */
 	public function tearDown() {
@@ -213,6 +221,9 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 
 	/**
 	 * @covers WPSEO_Frontend::adjacent_rel_links
+	 * @covers WPSEO_Frontend::rel_links_archive
+	 * @covers WPSEO_Frontend::adjacent_rel_link
+	 * @covers WPSEO_Frontend::get_pagination_base
 	 * @covers WPSEO_Frontend::canonical
 	 */
 	public function test_adjacent_rel_links_canonical_post_type() {
@@ -237,6 +248,9 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 
 	/**
 	 * @covers WPSEO_Frontend::adjacent_rel_links
+	 * @covers WPSEO_Frontend::rel_links_archive
+	 * @covers WPSEO_Frontend::adjacent_rel_link
+	 * @covers WPSEO_Frontend::get_pagination_base
 	 * @covers WPSEO_Frontend::canonical
 	 */
 	public function test_adjacent_rel_links_canonical_author() {
@@ -254,6 +268,9 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 
 	/**
 	 * @covers WPSEO_Frontend::adjacent_rel_links
+	 * @covers WPSEO_Frontend::rel_links_archive
+	 * @covers WPSEO_Frontend::adjacent_rel_link
+	 * @covers WPSEO_Frontend::get_pagination_base
 	 * @covers WPSEO_Frontend::canonical
 	 */
 	public function test_adjacent_rel_links_canonical_date_archive() {
@@ -267,6 +284,9 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 
 	/**
 	 * @covers WPSEO_Frontend::adjacent_rel_links
+	 * @covers WPSEO_Frontend::rel_links_archive
+	 * @covers WPSEO_Frontend::adjacent_rel_link
+	 * @covers WPSEO_Frontend::get_pagination_base
 	 * @covers WPSEO_Frontend::canonical
 	 */
 	public function test_adjacent_rel_links_canonical_category() {
@@ -282,6 +302,90 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 		$category_link = get_category_link( $category_id );
 
 		$this->run_test_on_consecutive_pages( $category_link );
+	}
+
+	/**
+	 * Makes sure posts which are paginated have the correct next/prev links.
+	 *
+	 * @covers WPSEO_Frontend::adjacent_rel_links
+	 * @covers WPSEO_Frontend::rel_links_single
+	 * @covers WPSEO_Frontend::adjacent_rel_link
+	 * @covers WPSEO_Frontend::get_pagination_base
+	 * @covers WPSEO_Frontend::canonical
+	 */
+	public function test_adjacent_rel_links_canonical_split_up_post_pretty_permalinks() {
+		$this->set_permalink_structure( '/%postname%/' );
+
+		$post_id = $this->factory->post->create(
+			array(
+				'post_type'    => 'post',
+				'post_content' => '
+Page 1/5
+<!--nextpage-->
+Page 2/3
+<!--nextpage-->
+Page 3/3
+'
+			)
+		);
+
+		$url = get_permalink( $post_id );
+
+		$this->run_test_on_consecutive_post_parts( $url );
+	}
+
+	/**
+	 * Makes sure posts which are paginated have the correct next/prev links.
+	 *
+	 * @covers WPSEO_Frontend::adjacent_rel_links
+	 * @covers WPSEO_Frontend::rel_links_archive
+	 * @covers WPSEO_Frontend::adjacent_rel_link
+	 * @covers WPSEO_Frontend::get_pagination_base
+	 * @covers WPSEO_Frontend::canonical
+	 */
+	public function test_adjacent_rel_links_canonical_split_up_post() {
+		$post_id = $this->factory->post->create(
+			array(
+				'post_type'    => 'post',
+				'post_content' => '
+Page 1/5
+<!--nextpage-->
+Page 2/3
+<!--nextpage-->
+Page 3/3
+'
+			)
+		);
+
+		$url = get_permalink( $post_id );
+
+		$this->run_test_on_consecutive_post_parts( $url );
+	}
+
+	/**
+	 * Makes sure no rel/next will be output when the post is not split up.
+	 *
+	 * @covers WPSEO_Frontend::adjacent_rel_links
+	 * @covers WPSEO_Frontend::rel_links_single
+	 * @covers WPSEO_Frontend::adjacent_rel_link
+	 * @covers WPSEO_Frontend::get_pagination_base
+	 * @covers WPSEO_Frontend::canonical
+	 */
+	public function test_adjacent_rel_links_non_split_post() {
+		$post_id = $this->factory->post->create(
+			array(
+				'post_type'    => 'post',
+				'post_content' => 'No nextpage HTML comment present.'
+			)
+		);
+
+		$url = get_permalink( $post_id );
+
+		// Test page 1 of the post type archives, should have just a rel=next and a canonical.
+		$this->go_to( $url );
+
+		self::$class_instance->adjacent_rel_links();
+		$this->expectOutput( '' );
 	}
 
 	/**
@@ -613,32 +717,97 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 		$this->go_to( $initial_url );
 
 		$page_2_link = get_pagenum_link( 2, false );
-		$expected    = '<link rel="next" href="' . esc_url( $page_2_link ) . '" />' . "\n";
+		$page_3_link = get_pagenum_link( 3, false );
+
+		$expected = '<link rel="next" href="' . esc_url( $page_2_link ) . '" />' . "\n";
 
 		self::$class_instance->adjacent_rel_links();
 		$this->assertEquals( $initial_url, self::$class_instance->canonical( false ) );
 		$this->expectOutput( $expected );
 
+		self::$class_instance->reset();
 
 		// Test page 2 of the post type archives, should have a rel=next and rel=prev and a canonical.
-		self::$class_instance->reset();
 		$this->go_to( $page_2_link );
 
-		$page_3_link = get_pagenum_link( 3, false );
-		$expected    = '<link rel="prev" href="' . esc_url( $initial_url ) . '" />' . "\n" . '<link rel="next" href="' . esc_url( $page_3_link ) . '" />' . "\n";
+		$expected = '<link rel="prev" href="' . esc_url( $initial_url ) . '" />' . "\n" . '<link rel="next" href="' . esc_url( $page_3_link ) . '" />' . "\n";
 
 		self::$class_instance->adjacent_rel_links();
-		$this->assertEquals( $page_2_link, self::$class_instance->canonical( false ) );
-		$this->expectOutput( $expected );
+		$this->assertEquals( $page_2_link, self::$class_instance->canonical( false ), 'Testing the link for the next page' );
+		$this->expectOutput( $expected, 'Expecting previous and next URLs to be set for the 2nd page.' );
+
+		self::$class_instance->reset();
 
 		// Test page 3 of the author archives, should have just a rel=prev and a canonical.
-		self::$class_instance->reset();
 		$this->go_to( $page_3_link );
 
 		$expected = '<link rel="prev" href="' . esc_url( $page_2_link ) . '" />' . "\n";
 		self::$class_instance->adjacent_rel_links();
-		$this->assertEquals( $page_3_link, self::$class_instance->canonical( false ) );
-		$this->expectOutput( $expected );
+		$this->assertEquals( $page_3_link, self::$class_instance->canonical( false ), 'Testing the link for the previous page' );
+		$this->expectOutput( $expected, 'Expecting previous URL to be set for the 3nd page.' );
+
+		self::$class_instance->reset();
+	}
+
+	/**
+	 * @param string $initial_url URL to start off from.
+	 *
+	 * @return void
+	 */
+	private function run_test_on_consecutive_post_parts( $initial_url ) {
+		/** @var WP_Rewrite $wp_rewrite */
+		global $wp_rewrite;
+
+		// Test page 1 of the post type archives, should have just a rel=next and a canonical.
+		$this->go_to( $initial_url );
+
+		/*
+		 * As WordPress core is internally completely broken on this functionality, adding logic here
+		 * is the only way to test for the different situations.
+		 */
+		if ( ! $wp_rewrite->using_permalinks() ) {
+			$page_2_link = get_pagenum_link( 2, false );
+			$page_3_link = get_pagenum_link( 3, false );
+
+			// Prepare for is_single usage.
+			$page_2_link = str_replace( 'paged=', 'page=', $page_2_link );
+			$page_3_link = str_replace( 'paged=', 'page=', $page_3_link );
+		} else {
+			$page_2_link = user_trailingslashit( rtrim( $initial_url, '/' ) . '/2' );
+			$page_3_link = user_trailingslashit( rtrim( $initial_url, '/' ) . '/3' );
+		}
+
+		self::$class_instance->adjacent_rel_links();
+		$this->expectOutput(
+			'<link rel="next" href="' . esc_url( $page_2_link ) . '" />' . "\n",
+			'Expect next link to be present in the output.'
+		);
+
+		$this->assertEquals( $initial_url, self::$class_instance->canonical( false ) );
+
+		self::$class_instance->reset();
+
+		// Test page 2 of the post type archives, should have a rel=next and rel=prev and a canonical.
+		$this->go_to( $page_2_link );
+
+		self::$class_instance->adjacent_rel_links();
+		$this->expectOutput(
+			'<link rel="prev" href="' . esc_url( $initial_url ) . '" />' . "\n" .
+			'<link rel="next" href="' . esc_url( $page_3_link ) . '" />' . "\n",
+			'Expecting previous and next URLs to be set for the 2nd page.'
+		);
+
+		$this->assertEquals( $page_2_link, self::$class_instance->canonical( false ), 'Testing the link for the next page' );
+
+		self::$class_instance->reset();
+
+		// Test page 3 of the author archives, should have just a rel=prev and a canonical.
+		$this->go_to( $page_3_link );
+
+		$expected = '<link rel="prev" href="' . esc_url( $page_2_link ) . '" />' . "\n";
+		self::$class_instance->adjacent_rel_links();
+		$this->assertEquals( $page_3_link, self::$class_instance->canonical( false ), 'Testing the link for the previous page' );
+		$this->expectOutput( $expected, 'Expecting previous URL to be set for the 3nd page.' );
 	}
 
 	/**
@@ -650,7 +819,10 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 	 */
 	private function run_webmaster_tools_authentication_option_test( $option_name, $test_value, $expected ) {
 		WPSEO_Options::set( $option_name, $test_value );
-		$this->expectOutput( $expected, self::$class_instance->webmaster_tools_authentication() );
+
+		self::$class_instance->webmaster_tools_authentication();
+		$this->expectOutput( $expected );
+
 		WPSEO_Options::set( $option_name, '' );
 	}
 
@@ -658,7 +830,8 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 	 * @covers WPSEO_Frontend::get_queried_post_type()
 	 */
 	public function test_get_queried_post_type() {
-		$wp_query = $this->getMockBuilder( 'WP_Query' )
+		$wp_query = $this
+			->getMockBuilder( 'WP_Query' )
 			->setMethods( array( 'get' ) )
 			->getMock();
 
@@ -677,7 +850,8 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 	 * @covers WPSEO_Frontend::get_queried_post_type()
 	 */
 	public function test_get_queried_post_type_array() {
-		$wp_query = $this->getMockBuilder( 'WP_Query' )
+		$wp_query = $this
+			->getMockBuilder( 'WP_Query' )
 			->setMethods( array( 'get' ) )
 			->getMock();
 
