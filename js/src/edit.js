ae042c00501c25b6c2ea4f718e1a19ff3a57f3ef
IreneStr <irene@yoast.com>
2018-02-27 13:19:29 +0100

    Use content analysis as sidebar component

--- js/src/edit.js
+++ js/src/edit.js
@@ -155,16 +155,29 @@ export function initialize( args ) {
 
 	renderReactApps( store, args );
 
-	const { registerEditorMenuItem, registerSidebar } = wp.editPost;
+	const { registerMoreMenuItem, registerSidebar } = wp.editPost;
+	const { Panel, PanelBody } = wp.components;
 
-	const Component = () => <h1>Test</h1>;
+
+	const Component = () => {
+		return (
+			<Panel>
+				<PanelBody>
+					<h2>SEO analysis</h2>
+						{ wrapInTopLevelComponents( SeoAnalysis, store ) }
+					<h2>Readability analysis</h2>
+						{ wrapInTopLevelComponents( ContentAnalysis, store ) }
+				</PanelBody>
+			</Panel>
+		);
+	};
 
 	registerSidebar( "yoast/analysis", {
 		render: Component,
-		title: "Yoast SEO"
+		title: "Yoast SEO",
 	} );
 
-	registerEditorMenuItem( "yoast/analysis-menu", {
+	registerMoreMenuItem( "yoast/analysis-menu", {
 		title: "Yoast SEO",
 		icon: <Icon />,
 		target: "yoast/analysis",
