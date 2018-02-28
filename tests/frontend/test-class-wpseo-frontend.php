f14e292b1fc4309cfa2e2e30f8f8131d8b0e53ca
Jip Moors <jhp.moors@gmail.com>
2018-02-28 10:37:26 +0100

    Improve documentation

--- tests/frontend/test-class-wpseo-frontend.php
+++ tests/frontend/test-class-wpseo-frontend.php
@@ -257,11 +257,15 @@ class WPSEO_Frontend_Test extends WPSEO_UnitTestCase_Frontend {
 	}
 
 	/**
+	 * Tests adjecent rel links on author overview.
+	 *
 	 * @covers WPSEO_Frontend::adjacent_rel_links
 	 * @covers WPSEO_Frontend::rel_links_archive
 	 * @covers WPSEO_Frontend::adjacent_rel_link
 	 * @covers WPSEO_Frontend::get_pagination_base
 	 * @covers WPSEO_Frontend::canonical
+	 *
+	 * @return void
 	 */
 	public function test_adjacent_rel_links_canonical_author() {
 		update_option( 'posts_per_page', 1 );
