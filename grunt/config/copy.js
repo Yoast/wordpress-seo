53be5ee01704f036c474c8a13ab276d14b01f7d3
dutchmartin <martijn.groeneveldt@gmail.com>
2018-03-07 16:45:08 +0100

    Created composer-artifact grunt command

--- grunt/config/copy.js
+++ grunt/config/copy.js
@@ -1,4 +1,13 @@
 // See https://github.com/gruntjs/grunt-contrib-copy
+
+// Take the artifact src file path and remove the vendor folder
+let composerFiles = function() {
+	//let files = this.__esModule.exports.artifact.files;
+	//console.log(files);
+	//let index = files.indexOf( "vendor/**" );
+	//files.splice( index, 1 )
+	return this.__esModule.exports.artifact.files;
+};
 module.exports = {
 	dependencies: {
 		files: [
@@ -53,4 +62,34 @@ module.exports = {
 			},
 		],
 	},
+	composer_artifact: {
+		files: [
+			{
+				expand: true,
+				cwd: ".",
+				src: [
+					"admin/**",
+					"css/**/*.min.css",
+					"css/main-sitemap.xsl",
+					"deprecated/**",
+					"frontend/**",
+					"images/**",
+					"inc/**",
+					"js/dist/**/*.min.js",
+					"js/dist/select2/i18n/*.js",
+					"languages/**",
+					"index.php",
+					"license.txt",
+					"readme.txt",
+					"wp-seo*.php",
+					"wpml-config.xml",
+					"!languages/wordpress-seo.pot",
+					"composer.lock",
+					"composer.json",
+					"!**/README.md",
+				],
+				dest: "artifact",
+			},
+		],
+	}
 };
