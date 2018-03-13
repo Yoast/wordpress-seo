6152618ad1f674bd126c8557c140893fa9090305
Jip Moors <jhp.moors@gmail.com>
2018-03-13 10:54:09 +0100

    Add tests to for notification behaviour

--- admin/onpage/class-onpage.php
+++ admin/onpage/class-onpage.php
@@ -25,17 +25,19 @@ class WPSEO_OnPage {
 
 	/**
 	 * Constructing the object
+	 *
+	 * @param WPSEO_OnPage_Option|null $option Option to use.
 	 */
-	public function __construct() {
+	public function __construct( WPSEO_OnPage_Option $option = null ) {
 		// We never want to fetch on AJAX request because doing a remote request is really slow.
 		if ( defined( 'DOING_AJAX' ) && DOING_AJAX === true ) {
 			return;
 		}
 
-		$this->onpage_option = new WPSEO_OnPage_Option();
+		$this->onpage_option = ( $option === null ) ? new WPSEO_OnPage_Option() : $option;
 
 		// Adding admin notice if necessary.
-		add_filter( 'admin_init', array( $this, 'show_notice' ) );
+		add_action( 'admin_init', array( $this, 'show_notice' ) );
 
 		if ( $this->onpage_option->is_enabled() ) {
 			$this->set_hooks();
@@ -124,7 +126,7 @@ class WPSEO_OnPage {
 	 */
 	private function get_indexability_notification() {
 		$notice = sprintf(
-			/* translators: 1: opens a link to a related knowledge base article. 2: closes the link */
+		/* translators: 1: opens a link to a related knowledge base article. 2: closes the link */
 			__( '%1$sYour homepage cannot be indexed by search engines%2$s. This is very bad for SEO and should be fixed.', 'wordpress-seo' ),
 			'<a href="' . WPSEO_Shortlinker::get( 'https://yoa.st/onpageindexerror' ) . '" target="_blank">',
 			'</a>'
