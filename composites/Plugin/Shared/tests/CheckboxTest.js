5a70b9768f6f6572ae97fc5e159a2ecbd79f1ec5
Maarten Leenders <maarten@yoast.com>
2018-03-14 16:08:42 +0100

    added tests and snapshots

---
+++ composites/Plugin/Shared/tests/CheckboxTest.js
@@ -0,0 +1,47 @@
+import React from "react";
+import renderer from "react-test-renderer";
+
+import Checkbox from "../components/Checkbox";
+
+test( "the Checkbox matches the snapshot", () => {
+	const component = renderer.create(
+		<Checkbox id="test-id" onChange={ value => console.log( value ) } label="test label" />
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
+
+test( "the Checkbox matches the snapshot when an array is provided as a label", () => {
+	const component = renderer.create(
+		<Checkbox id="test-id" onChange={ value => console.log( value ) } label={ [ "test label ", "using arrays" ] } />
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
+
+test( "Checkbox executes callback", () => {
+	let event = {
+		target: {
+			checked: false,
+		},
+	};
+	const component = renderer.create(
+		<Checkbox
+			id="testCallback"
+			onChange={ () => {
+				console.log( "changed" );
+			} }
+			label="testCallbackLabel"
+		/>
+	);
+
+	let tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+
+	tree[ 0 ].props.onChange( event );
+
+	tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
+} );
