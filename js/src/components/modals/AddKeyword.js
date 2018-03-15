c4ed1a57c319ab1bdd1cbc4f292c5e07debcbd77
Andrea Fercia <a.fercia@gmail.com>
2018-03-15 17:59:45 +0100

    Style the AddKeyword modal content.

--- js/src/components/modals/AddKeyword.js
+++ js/src/components/modals/AddKeyword.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 import Icon from "yoast-components/composites/Plugin/Shared/components/Icon";
 import YoastSeoIcon from "yoast-components/composites/basic/YoastSeoIcon";
@@ -8,10 +9,40 @@ import { makeOutboundLink } from "yoast-components/utils/makeOutboundLink";
 const YouCanLink = makeOutboundLink();
 const BuyButtonLink = makeOutboundLink();
 
+const StyledContainer = styled.div`
+	min-width: 600px;
+
+	@media screen and ( max-width: 680px ) {
+		min-width: 0;
+		width: 86vw;
+	}
+`;
+
+const StyledIcon = styled( Icon )`
+	float: right;
+	margin: 0 0 16px 16px;
+`;
+
+const StyledList = styled.ul`
+	list-style: none;
+	margin: 0 0 16px;
+
+	li {
+		margin: 5px 0 0 0;
+		padding-left: 16px;
+	}
+
+	span[aria-hidden="true"]:before {
+		margin: 0 8px 0 -16px;
+		font-weight: bold;
+		content: "+";
+	}
+`;
+
 const AddKeyword = ( props ) => {
 	return (
-		<div>
-			<Icon icon={ YoastSeoIcon } width="150px" height="150px" className="alignright" />
+		<StyledContainer>
+			<StyledIcon icon={ YoastSeoIcon } width="150px" height="150px" />
 			<h2>{ props.translations.title }</h2>
 			<p>
 				<YouCanLink href={ props.translations.link }>
@@ -19,17 +50,17 @@ const AddKeyword = ( props ) => {
 				</YouCanLink>
 			</p>
 			<p>{ props.translations.other }</p>
-			<ul>
+			<StyledList role="list">
 				{ props.translations.benefits.map( ( benefit, index ) =>
-					<li key={ index }><strong>{ benefit }</strong></li>
+					<li key={ index }><strong><span aria-hidden="true"></span>{ benefit }</strong></li>
 				) }
-			</ul>
+			</StyledList>
 			<BuyButtonLink href={ props.translations.buylink } className="button button-primary">
 				{ props.translations.buy }
 			</BuyButtonLink>
 			<br/>
 			<small>{ props.translations.small }</small>
-		</div>
+		</StyledContainer>
 	);
 };
 
