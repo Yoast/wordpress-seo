7b6f5f42bdd00d93bb7290ee37900e29df52673e
Igor <35524806+igorschoester@users.noreply.github.com>
2018-03-21 12:17:00 +0100

    Revert "Revert "Use StyledSection for analyses""

--- admin/formatter/class-metabox-formatter.php
+++ admin/formatter/class-metabox-formatter.php
@@ -129,6 +129,7 @@ class WPSEO_Metabox_Formatter {
 				),
 			),
 			'markdownEnabled'       => $this->is_markdown_enabled(),
+			'analysisHeadingTitle'  => __( 'Analysis', 'wordpress-seo' ),
 		);
 	}
 
