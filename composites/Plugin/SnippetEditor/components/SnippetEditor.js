cdc7e0f6ae2edcfdc540b68a3b54d77ff47515d4
Anton Timmermans <email@atimmer.com>
2018-03-22 14:49:31 +0100

    Add DraftJS serialization functions
    This gives us the ability to persist the content in the DraftJS
    editor.

--- composites/Plugin/SnippetEditor/components/SnippetEditor.js
+++ composites/Plugin/SnippetEditor/components/SnippetEditor.js
@@ -1,9 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import { injectIntl, intlShape, defineMessages, FormattedMessage } from "react-intl";
-import { EditorState, convertToRaw } from 'draft-js';
-import Editor from 'draft-js-plugins-editor';
-import createMentionPlugin, { defaultSuggestionsFilter } from 'draft-js-mention-plugin';
+import { EditorState, convertToRaw, convertFromRaw } from "draft-js";
+import Editor from "draft-js-plugins-editor";
+import createMentionPlugin, { defaultSuggestionsFilter } from "draft-js-mention-plugin";
+import { serializeEditor, unserializeEditor } from "../serialization";
 
 const messages = defineMessages( {
 	seoTitle: {
@@ -24,8 +25,12 @@ class SnippetEditor extends React.Component {
 	constructor( props ) {
 		super( props );
 
+		let rawData = unserializeEditor( "%%title%% %%post_type%% test123 fa" );
+
+		let contentState = convertFromRaw( rawData );
+
 		this.state = {
-			titleEditorState: EditorState.createEmpty(),
+			titleEditorState: EditorState.createWithContent( contentState ),
 			descriptionEditorState: EditorState.createEmpty(),
 			replaceVars: props.replaceVars,
 		};
@@ -43,6 +48,11 @@ class SnippetEditor extends React.Component {
 
 	onTitleChange( editorState ) {
 		console.log( "BLOCKS =", convertToRaw( editorState.getCurrentContent() ) );
+		console.log( JSON.stringify( convertToRaw( editorState.getCurrentContent() ) ) );
+
+		const rawData = convertToRaw( editorState.getCurrentContent() );
+
+		console.log( "SERIALIZED", serializeEditor( rawData ) );
 
 		this.setState( {
 			titleEditorState: editorState,
@@ -56,10 +66,10 @@ class SnippetEditor extends React.Component {
 	}
 
 	onSearchChange( { value } ) {
-		this.setState({
-			replaceVars: defaultSuggestionsFilter(value, this.props.replaceVars),
-		});
-	};
+		this.setState( {
+			replaceVars: defaultSuggestionsFilter( value, this.props.replaceVars ),
+		} );
+	}
 
 	onAddMention() {
 		console.log( "onAddMention", arguments );
@@ -92,7 +102,9 @@ class SnippetEditor extends React.Component {
 						editorState={ titleEditorState }
 						onChange={ this.onTitleChange }
 						plugins={ [ this.mentionsPlugin ] }
-						ref={(element) => { this.editor = element; }}
+						ref={( element ) => {
+							this.editor = element;
+						}}
 					/>
 					<MentionSuggestions
 						onSearchChange={ this.onSearchChange }
