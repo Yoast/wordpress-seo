22be03ed4f2a8fc03b1c52e08967b171e515ed78
Jip Moors <jhp.moors@gmail.com>
2018-03-25 17:29:01 +0200

    Add `grunt eslint:tests` command
    Configures seperate command to test the specs folder for coding standards.
    
    Results: ✖ 4971 problems (4674 errors, 297 warnings)

--- grunt/config/eslint.js
+++ grunt/config/eslint.js
@@ -1,9 +1,15 @@
 // See https://github.com/sindresorhus/grunt-eslint
 module.exports = {
 	target: {
-		src: [ "<%= files.js %>", "!src/templates.js" ],
+		src: [ "<%= files.js %>", "<%= files.jsDontLint %>" ],
 		options: {
-			maxWarnings: 140,
+			maxWarnings: 14,
+		},
+	},
+	tests: {
+		src: [ "<%= files.jsTests %>" ],
+		options: {
+			configFile: ".eslintrc-tests",
 		},
 	},
 };
