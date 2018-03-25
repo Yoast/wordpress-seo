f7a316573449617fc512f23fdb634ea17223d5aa
Jip Moors <jhp.moors@gmail.com>
2018-03-25 18:09:59 +0200

    Allow camelcase for arguments

--- spec/helpers/factory.js
+++ spec/helpers/factory.js
@@ -5,7 +5,7 @@ let FactoryProto = function() {};
 FactoryProto.prototype.buildJed = function() {
 	return new Jed( {
 		domain: "js-text-analysis",
-		locale_data: {
+		locale_data: { // eslint-disable-line camelcase
 			"js-text-analysis": {
 				"": {},
 			},
