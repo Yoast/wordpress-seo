c1c214d389a03ef464fd864d83085f7e88426fbb
Anton Timmermans <email@atimmer.com>
2018-03-26 14:45:12 +0200

    Use the WordPress version of a11y speak

--- composites/AlgoliaSearch/AlgoliaSearcher.js
+++ composites/AlgoliaSearch/AlgoliaSearcher.js
@@ -1,7 +1,7 @@
 import React from "react";
 import initAlgoliaSearch from "algoliasearch";
 import isUndefined from "lodash/isUndefined";
-import a11ySpeak from "a11y-speak";
+import { speak as a11ySpeak } from "@wordpress/a11y";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 import { injectIntl, intlShape, defineMessages } from "react-intl";
