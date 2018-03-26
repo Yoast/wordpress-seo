219d47ede3ff57537ddde641a8f72dcf34c351b4
jrfnl <jrfnl@users.noreply.github.com>
2018-03-26 16:36:36 +0200

    CS: clean up after latest merges - documentation fixes
    Punctuation, spelling, accuracy and breaking up of very long lines.

--- tests/frontend/test-class-wpseo-frontend-robots.php
+++ tests/frontend/test-class-wpseo-frontend-robots.php
@@ -221,7 +221,7 @@ final class WPSEO_Frontend_Robots_Test extends WPSEO_UnitTestCase_Frontend {
 		// Test that when this is _not_ set, we also do NOT have a noindex.
 		$user_id = $this->factory->user->create();
 		$this->go_to( get_author_posts_url( $user_id ) );
-		$expected = ''; // index,follow is automatically set to empty string.
+		$expected = ''; // The default "index,follow" is automatically set to empty string.
 		$this->assertEquals( $expected, self::$class_instance->robots() );
 	}
 
