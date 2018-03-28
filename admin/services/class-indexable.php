c6882b4eb18f1b22392d5614291aa41314926826
Andy Meerwaldt <andy@yoast.com>
2018-03-28 14:31:17 +0200

    Add indexable service

---
+++ admin/services/class-indexable.php
@@ -0,0 +1,41 @@
+<?php
+
+
+class WPSEO_Indexable_Service {
+
+	/**
+	 *
+	 */
+	public function get_indexable( WP_REST_Request $request ) {
+
+		$type = $request->get_param( 'object_type' );
+		$provider = $this->get_provider( $type );
+
+		if( $provider === null || $provider->exists( $object_id ) ) {
+			return new WP_REST_Response( 'Unknown type '. $type, 404 );
+		}
+
+		$object_id = $request->get_param( 'object_id' );
+		return new WP_REST_Response( $provider->get( $object_id ) );
+	}
+
+	/**
+	 * @param $type
+	 *
+	 * @return null|WPSEO_Indexable_Service_Provider
+	 */
+	protected function get_provider( $type ) {
+		if ( $type === 'post' ) {
+			return new WPSEO_Indexable_Service_Post_Provider();
+		}
+
+		if ( $type === 'term' ) {
+			return new WPSEO_Indexable_Service_Term_Provider();
+
+		}
+
+		return null;
+	}
+
+
+}
