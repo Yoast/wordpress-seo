63071046b4cae3b0f70febeac5803c90d010a89d
Andrea Fercia <a.fercia@gmail.com>
2018-03-30 11:19:20 +0200

    Fix tests.

--- composites/Plugin/Shared/tests/YoastModalTest.js
+++ composites/Plugin/Shared/tests/YoastModalTest.js
@@ -4,7 +4,7 @@ import { mount } from "enzyme";
 import YoastModal from "../components/YoastModal";
 
 describe( "YoastModal", () => {
-	test( "the YoastModal renders children when open", () => {
+	test( "the YoastModal renders its children when open", () => {
 		const hook = document.createElement( "div" );
 
 		const wrapper = mount(
@@ -18,10 +18,32 @@ describe( "YoastModal", () => {
 			</YoastModal>
 		);
 
-		expect( wrapper.find( YoastModal ).find( ".yoast-modal__inside" ).text() ).toBe( "Hi!" );
+		// Get the real generated HTML excluding the wrapper components.
+		const modal = wrapper.find( "div.yoast-modal__overlay" );
+		expect( modal.find( ".yoast-modal__inside" ).text() ).toBe( "Hi!" );
 	} );
 
-	test( "the YoastModal with its own elements renders children when open", () => {
+	test( "the YoastModal has a role dialog and aria label", () => {
+		const hook = document.createElement( "div" );
+
+		const wrapper = mount(
+			<YoastModal
+				isOpen={ true }
+				appElement={ hook }
+				onClose={ () => {} }
+				modalAriaLabel="Some label"
+			>
+				Hi!
+			</YoastModal>
+		);
+
+		// Get the real generated HTML excluding the wrapper components.
+		const modalContent = wrapper.find( "div.yoast-modal__overlay .yoast-modal__content" );
+		expect( modalContent.getDOMNode().getAttribute( "role" ) ).toBe( "dialog" );
+		expect( modalContent.getDOMNode().getAttribute( "aria-label" ) ).toBe( "Some label" );
+	} );
+
+	test( "the YoastModal renders its default elements", () => {
 		const hook = document.createElement( "div" );
 
 		const wrapper = mount(
@@ -39,9 +61,16 @@ describe( "YoastModal", () => {
 			</YoastModal>
 		);
 
-		const modal = wrapper.find( ".my-modal" );
-		expect( modal.find( ".yoast-modal__inside" ).text() ).toBe( "Hello!" );
-		expect( modal.find( "h1" ).text() ).toBe( "title" );
+		// Get the real generated HTML excluding the wrapper components.
+		const modal = wrapper.find( "div.yoast-modal__overlay" );
+
+		/*
+		 * Note: `styled-components` wraps the DOM nodes and also passes class names e.g.
+		 *    <styled.h1 className="yoast-modal__title">
+		 *        <h1 className="yoast-modal__title sc-bdVaJa iXppQv">
+		 * so we need to get the real DOM element.
+		 */
+		expect( modal.find( "h1.yoast-modal__title" ).text() ).toBe( "title" );
 		expect( modal.find( "button.yoast-modal__button-close-icon" ).getDOMNode().getAttribute( "aria-label" ) ).toBe( "Close X" );
 		expect( modal.find( "button.yoast-modal__button-close" ).text() ).toBe( "Close" );
 	} );
