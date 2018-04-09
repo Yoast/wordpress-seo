da0d6042df476fec5b7dbed3e66e8e6887e57155
IreneStr <irene@yoast.com>
2018-04-09 10:46:10 +0200

    Remove unused import

--- composites/Plugin/ContentAnalysis/reducers/contentAnalysisReducer.js
+++ composites/Plugin/ContentAnalysis/reducers/contentAnalysisReducer.js
@@ -1,6 +1,5 @@
 import { readabilityResultsReducer } from "../reducers/contentAnalysis/readabilityResultsReducer";
 import { keywordResultsReducer } from "../reducers/contentAnalysis/keywordResultsReducer";
-import { overallScoreReducer } from "../reducers/contentAnalysis/overallScoreReducer";
 import { combineReducers } from "redux";
 
 export default combineReducers( {
