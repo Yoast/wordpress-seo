f19b865da978f364cb1d7148ccd7b3dc297c5f33
dutchmartin <martijn.groeneveldt@gmail.com>
2018-04-10 10:13:56 +0200

    Update release script

--- scripts/deploy_to_dist.sh
+++ scripts/deploy_to_dist.sh
@@ -3,18 +3,17 @@
 USER="Yoast-dist"
 REPO="wordpress-seo"
 REPO_URL="git@github.com:$USER/$REPO.git"
-# Get the latest tag
-#lastTag=$(git describe --abbrev=0 --tags)
-lastTag=$(TRAVIS_TAG)
+# Get the latest tag.
+lastTag=$1
 mainDir=$(pwd)
-# Create a new git repos
+# Create a new git repos.
 cd ./artifact
 git init
 git remote add origin ${REPO_URL}
-#commit the files
+# Commit the files.
 git add -A
 git commit -m "commit version tag ${lastTag} "
-#tag the commit
+# Tag the commit.
 git tag ${lastTag} $(git rev-parse HEAD)
-#push
+# Force push to master.
 git push -u origin master --tags -f -v
