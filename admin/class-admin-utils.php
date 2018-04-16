dec8f954637bfc24139d2a698592ca84790d6d87
Jimmy <jcomack@users.noreply.github.com>
2018-04-16 09:48:33 +0200

    Added missing . in description.

--- admin/class-admin-utils.php
+++ admin/class-admin-utils.php
@@ -71,7 +71,7 @@ class WPSEO_Admin_Utils {
 	/**
 	 * Determines whether or not the user has an invalid version of PHP installed.
 	 *
-	 * @return bool Whether or not PHP 5.2 or lower is installed
+	 * @return bool Whether or not PHP 5.2 or lower is installed.
 	 */
 	public static function is_supported_php_version_installed() {
 		$checker = new Whip_RequirementsChecker( array( 'php' => PHP_VERSION ) );
