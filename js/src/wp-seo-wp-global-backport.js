463d3d1aa5533263f440a89e19b63fcf9848c59d
Anton Timmermans <email@atimmer.com>
2018-04-18 11:29:03 +0200

    Address code review feedback

--- js/src/wp-seo-wp-global-backport.js
+++ js/src/wp-seo-wp-global-backport.js
@@ -2,26 +2,21 @@ import * as importedData from "@wordpress/data";
 import * as importedElement from "@wordpress/element";
 import get from "lodash/get";
 
-let element = get( window, "wp.element", null );
-let data = get( window, "wp.data", null );
-
-// If Gutenberg is present we can just use their wp.element and wp.data.
-if ( element === null ) {
-	element = importedElement;
-}
-
-if ( data === null ) {
-	data = importedData;
-}
+/*
+ * If Gutenberg is present we can just use their wp.element and wp.data. Otherwise
+ * we use the imported objects.
+ */
+let element = get( window, "wp.element", importedElement );
+let data = get( window, "wp.data", importedData );
 
 // Create our own global.
 const yoast = window.yoast || {};
 
-// Back port all wp globals on our own private `_wp` for isolation.
+// Backport all wp globals on our own private `_wp` for isolation.
 yoast._wp = {
 	element,
 	data,
 };
 
-// Put is all actually on the global.
+// Put it all actually on the global.
 window.yoast = yoast;
