dcbb961f4c1129fa2315427373df7c327ec722c5
dutchmartin <martijn.groeneveldt@gmail.com>
2018-04-18 15:31:38 +0200

    Added the final config for deployment

--- scripts/deploy_to_dist.sh
+++ scripts/deploy_to_dist.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
+######################
+## Deployment script #
+######################
+## arg 1: git tag   ##
+## arg 2: repo-name ##
+######################
 # Repo to deploy to:
 USER="Yoast-dist"
-REPO="wordpress-seo"
+REPO=$2
 REPO_URL="git@github.com:$USER/$REPO.git"
 # Get the latest tag.
 lastTag=$1
 mainDir=$(pwd)
-# Create a new git repos.
+# Clone the dist repo.
+git clone ${REPO_URL} dist-repo --no-checkout
+# Copy the git folder with the entire history.
+cp -r ./dist-repo/.git ./artifact
+# Navigate to the to be committed folder.
 cd ./artifact
-git init
-git remote add origin ${REPO_URL}
 # Commit the files.
 git add -A
 git commit -m "commit version tag ${lastTag} "
 # Tag the commit.
 git tag ${lastTag} $(git rev-parse HEAD)
-# Force push to master.
+# Push to master.
 git push -u origin master --tags -f -v
