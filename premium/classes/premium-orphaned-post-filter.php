e83762b63ee0acad3656fe5209428b4bd5c5fde2
Joost de Valk <joost@yoast.com>
2018-04-23 13:43:09 +0200

    Don't show private or unpublished posts

--- premium/classes/premium-orphaned-post-filter.php
+++ premium/classes/premium-orphaned-post-filter.php
@@ -93,6 +93,7 @@ class WPSEO_Premium_Orphaned_Post_Filter extends WPSEO_Abstract_Post_Filter {
 	public function filter_posts( $where ) {
 		if ( $this->is_filter_active() ) {
 			$where .= $this->get_where_filter();
+			$where .= $this->filter_published_posts();
 		}
 
 		return $where;
@@ -120,6 +121,16 @@ class WPSEO_Premium_Orphaned_Post_Filter extends WPSEO_Abstract_Post_Filter {
 			' AND ' . $wpdb->posts . '.ID IN ( ' . implode( ',', array_fill( 0, count( $post_ids ), '%d' ) ) . ' ) ',
 			$post_ids
 		);
+	}
+
+	/**
+	 * Adds a published posts filter so we don't show unpublished posts in the orphaned pages results.
+	 *
+	 * @return string A published posts filter.
+	 */
+	protected function filter_published_posts() {
+		global $wpdb;
+		return " AND $wpdb->posts.post_status = 'publish' AND $wpdb->posts.post_password = ''";
 	}
 
 	/**
