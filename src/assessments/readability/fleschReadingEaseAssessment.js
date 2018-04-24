fe7e326af5eaf7a2daf25248e4b0ce79ce0c3ff9
Natalia <natalia@yoast.com>
2018-04-24 10:52:41 +0200

    Remove availability of Flesch assessment

--- src/assessments/readability/fleschReadingEaseAssessment.js
+++ src/assessments/readability/fleschReadingEaseAssessment.js
@@ -3,7 +3,7 @@ var inRange = require( "lodash/inRange" );
 
 var getLanguageAvailability = require( "../../helpers/getLanguageAvailability.js" );
 
-var availableLanguages = [ "en", "nl", "de", "it", "ru" ];
+var availableLanguages = [ "en", "nl", "de", "it" ];
 
 /**
  * Calculates the assessment result based on the fleschReadingScore
