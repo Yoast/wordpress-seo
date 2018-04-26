e83c9e5c8d44650010a288074d9be94b5946d338
Alain Schlesser <alain.schlesser@gmail.com>
2018-04-26 10:41:39 +0200

    Remove unneeded else

--- cli/class-cli-redirect-delete-command.php
+++ cli/class-cli-redirect-delete-command.php
@@ -23,9 +23,9 @@ final class WPSEO_CLI_Redirect_Delete_Command extends WPSEO_CLI_Redirect_Base_Co
 
 		if ( ! $this->has_redirect( $origin ) ) {
 			WP_CLI::error( "Redirect does not exist for '{$origin}'." );
-		} else {
-			$success = $this->delete_redirect( $origin );
 		}
+
+		$success = $this->delete_redirect( $origin );
 
 		if ( ! $success ) {
 			WP_CLI::error( "Could not delete redirect: '{$origin}'." );
