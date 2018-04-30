9c6c8e34243a270b36476d4a7c6615d41248455e
Igor <igor@yoast.com>
2018-04-30 10:46:06 +0200

    Add tests.

---
+++ composites/Plugin/Shared/tests/HelpTextTest.js
@@ -0,0 +1,28 @@
+/* describe it */
+
+/* External dependencies */
+import React from "react";
+import renderer from "react-test-renderer";
+
+/* Internal dependencies */
+import HelpText from "../components/HelpText";
+
+describe( "HelpText", () => {
+	it( "matches the snapshot by default", () => {
+		const component = renderer.create(
+			<HelpText text="Some help text." />
+		);
+
+		let tree = component.toJSON();
+		expect( tree ).toMatchSnapshot();
+	} );
+
+	it( "matches the snapshot when an array is provided as text", () => {
+		const component = renderer.create(
+			<HelpText text={ [ "Text ", "<a href=\"https://www.example.com\">with a link</a>", " in the middle." ] } />
+		);
+
+		let tree = component.toJSON();
+		expect( tree ).toMatchSnapshot();
+	} );
+} );
