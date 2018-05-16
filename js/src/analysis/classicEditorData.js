edf2d8b23c1b748db6b640bb50dc024c74943f81
Maarten Leenders <maarten@yoast.com>
2018-05-16 14:42:40 +0200

    Restructure replaceVars object in initialData creation

--- js/src/analysis/classicEditorData.js
+++ js/src/analysis/classicEditorData.js
@@ -1,5 +1,7 @@
 /* External dependencies */
 import removeMarks from "yoastseo/js/markers/removeMarks";
+import forEach from "lodash/forEach";
+import omit from "lodash/omit";
 
 /* Internal dependencies */
 import { updateReplacementVariable } from "../redux/actions/snippetEditor";
@@ -135,6 +137,54 @@ class ClassicEditorData {
 	}
 
 	/**
+	 * Map the custom_fields field in the replacevars to a format suited for redux.
+	 *
+	 * @param {Object} replaceVars       The original replacevars.
+	 *
+	 * @returns {Object}                 The restructured replacevars object without custom_fields.
+	 */
+	mapCustomFields( replaceVars ) {
+		if( ! replaceVars.custom_fields ) {
+			return replaceVars;
+		}
+
+		let customFieldReplaceVars = {};
+		forEach( replaceVars.custom_fields, ( value, key ) => {
+			customFieldReplaceVars[ `cf_${ key }` ] = value;
+		} );
+
+		return omit( {
+			...replaceVars,
+			...customFieldReplaceVars,
+		}, "custom_fields" );
+	}
+
+	/**
+	 * Map the custom_taxonomies field in the replacevars to a format suited for redux.
+	 *
+	 * @param {Object} replaceVars       The original replacevars.
+	 *
+	 * @returns {Object}                 The restructured replacevars object without custom_taxonomies.
+	 */
+	mapCustomTaxonomies( replaceVars ) {
+		console.log( replaceVars );
+		if( ! replaceVars.custom_taxonomies ) {
+			return replaceVars;
+		}
+
+		let customTaxonomyReplaceVars = {};
+		forEach( replaceVars.custom_taxonomies, ( value, key ) => {
+			customTaxonomyReplaceVars[ `ct_${ key }` ] = value.name;
+			customTaxonomyReplaceVars[ `ct_desc_${ key }` ] = value.description;
+		} );
+
+		return omit( {
+			...replaceVars,
+			...customTaxonomyReplaceVars,
+		}, "custom_taxonomies" );
+	}
+
+	/**
 	 * Gets the initial data from the replacevars and document.
 	 *
 	 * @param {Object} replaceVars The replaceVars object.
@@ -142,6 +192,8 @@ class ClassicEditorData {
 	 * @returns {Object} The data.
 	 */
 	getInitialData( replaceVars ) {
+		replaceVars = this.mapCustomFields( replaceVars );
+		replaceVars = this.mapCustomTaxonomies( replaceVars );
 		return {
 			...replaceVars,
 			title: this.getTitle(),
