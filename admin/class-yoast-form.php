76ca80b7bb99377ba3091a8b300f3349665dae32
Felix Arntz <felix-arntz@leaves-and-love.net>
2018-05-16 10:49:19 +0200

    Use check_requirements() method to determine whether the network settings API should be used in Yoast_Form.

--- admin/class-yoast-form.php
+++ admin/class-yoast-form.php
@@ -77,7 +77,7 @@ class Yoast_Form {
 		if ( $form === true ) {
 			$enctype = ( $contains_files ) ? ' enctype="multipart/form-data"' : '';
 
-			if ( is_network_admin() ) {
+			if ( Yoast_Network_Settings_API::check_requirements() ) {
 				$action_url       = network_admin_url( 'settings.php' );
 				$hidden_fields_cb = array( Yoast_Network_Settings_API::get(), 'settings_fields' );
 			} else {
