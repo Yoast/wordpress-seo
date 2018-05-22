9e5c6cddf1f9c6abe4c621c628a5a761eb7b5c4a
Alexander Botteram <a.botteram@gmail.com>
2018-05-22 16:25:05 +0200

    Restored removed prop

--- composites/Plugin/SnippetEditor/components/ReplacementVariableEditor.js
+++ composites/Plugin/SnippetEditor/components/ReplacementVariableEditor.js
@@ -256,6 +256,7 @@ class ReplacementVariableEditor extends React.Component {
 					onBlur={ onBlur }
 					plugins={ [ this.mentionsPlugin ] }
 					ref={ this.setEditorRef }
+					stripPastedStyles={ true }
 					ariaLabelledBy={ ariaLabelledBy }
 				/>
 				<MentionSuggestions
