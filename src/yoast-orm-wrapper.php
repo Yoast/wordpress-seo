fd98fda4e6b5abba35fd1cf1b333c87f2515befb
Andy Meerwaldt <andy@yoast.com>
2018-05-28 10:31:52 +0200

    Improves the docs

--- src/yoast-orm-wrapper.php
+++ src/yoast-orm-wrapper.php
@@ -18,9 +18,9 @@ use YoastSEO_Vendor\ORM;
  * directly. It is used internally by the Model base
  * class.
  *
- *
  * The methods documented below are magic methods that conform to PSR-1.
  * This documentation exposes these methods to doc generators and IDEs.
+ *
  * @see http://www.php-fig.org/psr/psr-1/
  *
  * @method void setClassName($class_name)
@@ -30,18 +30,18 @@ use YoastSEO_Vendor\ORM;
  */
 class ORMWrapper extends ORM {
 	/**
-	 * The wrapped find_one and find_many classes will
-	 * return an instance or instances of this class.
+	 * The wrapped find_one and find_many classes will return an instance or
+	 * instances of this class.
 	 *
 	 * @var string $_class_name
 	 */
 	protected $_class_name;
 
 	/**
-	 * Set the name of the class which the wrapped
-	 * methods should return instances of.
+	 * Set the name of the class which the wrapped methods should return
+	 * instances of.
 	 *
-	 * @param  string $class_name
+	 * @param string $class_name The classname to set.
 	 *
 	 * @return void
 	 */
@@ -50,15 +50,14 @@ class ORMWrapper extends ORM {
 	}
 
 	/**
-	 * Add a custom filter to the method chain specified on the
-	 * model class. This allows custom queries to be added
-	 * to models. The filter should take an instance of the
-	 * ORM wrapper as its first argument and return an instance
-	 * of the ORM wrapper. Any arguments passed to this method
-	 * after the name of the filter will be passed to the called
-	 * filter function as arguments after the ORM class.
+	 * Add a custom filter to the method chain specified on the model class.
+	 * This allows custom queries to be added to models. The filter should take
+	 * an instance of the ORM wrapper as its first argument and return an
+	 * instance of the ORM wrapper. Any arguments passed to this method after
+	 * the name of the filter will be passed to the called filter function as
+	 * arguments after the ORM class.
 	 *
-	 * @return ORMWrapper
+	 * @return ORMWrapper Instance of the ORM wrapper.
 	 */
 	public function filter() {
 		$args            = \func_get_args();
@@ -67,34 +66,35 @@ class ORMWrapper extends ORM {
 		if ( \method_exists( $this->_class_name, $filter_function ) ) {
 			return \call_user_func_array( array( $this->_class_name, $filter_function ), $args );
 		}
+
+		return null;
 	}
 
 	/**
-	 * Factory method, return an instance of this
-	 * class bound to the supplied table name.
+	 * Factory method, return an instance of this class bound to the supplied
+	 * table name.
 	 *
-	 * A repeat of content in parent::for_table, so that
-	 * created class is ORMWrapper, not ORM
+	 * A repeat of content in parent::for_table, so that created class is
+	 * ORMWrapper, not ORM
 	 *
-	 * @param  string $table_name
-	 * @param  string $connection_name
+	 * @param  string $table_name      The table to create instance for.
+	 * @param  string $connection_name The connection name.
 	 *
-	 * @return ORMWrapper
+	 * @return ORMWrapper Instance of the ORM wrapper.
 	 */
 	public static function for_table( $table_name, $connection_name = parent::DEFAULT_CONNECTION ) {
 		static::_setup_db( $connection_name );
 
-		return new self( $table_name, array(), $connection_name );
+		return new static( $table_name, array(), $connection_name );
 	}
 
 	/**
-	 * Method to create an instance of the model class
-	 * associated with this wrapper and populate
-	 * it with the supplied Idiorm instance.
+	 * Method to create an instance of the model class associated with this
+	 * wrapper and populate it with the supplied Idiorm instance.
 	 *
-	 * @param  ORMWrapper|ORM $orm
+	 * @param ORMWrapper|ORM $orm The ORM used by model.
 	 *
-	 * @return bool|Yoast_Model
+	 * @return bool|Yoast_Model Instance of the model class.
 	 */
 	protected function _create_model_instance( $orm ) {
 		if ( $orm === \false ) {
@@ -109,24 +109,22 @@ class ORMWrapper extends ORM {
 	}
 
 	/**
-	 * Wrap Idiorm's find_one method to return
-	 * an instance of the class associated with
-	 * this wrapper instead of the raw ORM class.
+	 * Wrap Idiorm's find_one method to return an instance of the class
+	 * associated with this wrapper instead of the raw ORM class.
 	 *
-	 * @param  null|integer $id
+	 * @param null|integer $id The ID to lookup.
 	 *
-	 * @return Yoast_Model
+	 * @return Yoast_Model Instance of the model.
 	 */
 	public function find_one( $id = null ) {
 		return $this->_create_model_instance( parent::find_one( $id ) );
 	}
 
 	/**
-	 * Wrap Idiorm's find_many method to return
-	 * an array of instances of the class associated
-	 * with this wrapper instead of the raw ORM class.
+	 * Wrap Idiorm's find_many method to return an array of instances of the
+	 * class associated with this wrapper instead of the raw ORM class.
 	 *
-	 * @return array
+	 * @return array The found results.
 	 */
 	public function find_many() {
 		$results = parent::find_many();
@@ -138,13 +136,12 @@ class ORMWrapper extends ORM {
 	}
 
 	/**
-	 * Wrap Idiorm's create method to return an
-	 * empty instance of the class associated with
-	 * this wrapper instead of the raw ORM class.
+	 * Wrap Idiorm's create method to return an empty instance of the class
+	 * associated with this wrapper instead of the raw ORM class.
 	 *
 	 * @param null|mixed $data The data to pass.
 	 *
-	 * @return ORMWrapper|bool
+	 * @return ORMWrapper|bool Instance of the ORM.
 	 */
 	public function create( $data = null ) {
 		return $this->_create_model_instance( parent::create( $data ) );
