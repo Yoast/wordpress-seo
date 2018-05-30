154ca1cf63171b836c49dc495742353d0c250837
Igor <igor@yoast.com>
2018-05-30 09:54:48 +0200

    Add bottom edge offset

--- composites/Plugin/SnippetEditor/positionSuggestions.js
+++ composites/Plugin/SnippetEditor/positionSuggestions.js
@@ -1,7 +1,11 @@
 import mentionPluginPositionSuggestions from "draft-js-mention-plugin/lib/utils/positionSuggestions";
 
+// Extra offset for when the popover has to move above the cursor.
 const MENTION_PLUGIN_CURSOR_HEIGHT_OFFSET = 31;
+// Extra offset when checking if the popover falls outside of the right of the screen.
 const MENTION_PLUGIN_RIGHT_EDGE_OFFSET = 5;
+// Extra offset when checking if the popover falls outside of the bottom of the screen.
+const MENTION_PLUGIN_BOTTOM_EDGE_OFFSET = 25;
 
 /**
  * Determines the position for the Draft.js mention plugin suggestions.
@@ -21,7 +25,7 @@ const MENTION_PLUGIN_RIGHT_EDGE_OFFSET = 5;
  */
 export const positionSuggestions = ( { decoratorRect, popover, state, props } ) => {
 	const rightEdge = ( decoratorRect.x || decoratorRect.left ) + popover.offsetWidth + MENTION_PLUGIN_RIGHT_EDGE_OFFSET;
-	const bottomEdge = ( decoratorRect.y || decoratorRect.top ) + popover.offsetHeight;
+	const bottomEdge = ( decoratorRect.y || decoratorRect.top ) + popover.offsetHeight + MENTION_PLUGIN_BOTTOM_EDGE_OFFSET;
 	const { left, top, ...restProps } = mentionPluginPositionSuggestions( { decoratorRect, popover, state, props } );
 
 	let adjustedLeft = null;
