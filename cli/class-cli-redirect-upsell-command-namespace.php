a6293c3ec14fb095f50f3521121cfad9f3cc5bb0
Alain Schlesser <alain.schlesser@gmail.com>
2018-06-05 14:51:25 +0200

    Rename "upsell" command namespace

--- cli/class-cli-redirect-upsell-command-namespace.php
+++ cli/class-cli-redirect-upsell-command-namespace.php
@@ -10,6 +10,6 @@ use WP_CLI\Dispatcher\CommandNamespace;
 /**
  * The redirect functionality is only available in Yoast SEO Premium.
  */
-final class WPSEO_CLI_Redirect_Command_Namespace extends CommandNamespace {
+final class WPSEO_CLI_Redirect_Upsell_Command_Namespace extends CommandNamespace {
 	// Intentionally left empty.
 }
