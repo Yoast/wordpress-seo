daa5f1d35e5c17fb80713b4256dc8aa61206bd76
Igor <igor@yoast.com>
2018-06-07 21:28:28 +0200

    Remove margin-top that got back in

--- composites/Plugin/SnippetEditor/components/SnippetEditorFields.js
+++ composites/Plugin/SnippetEditor/components/SnippetEditorFields.js
@@ -61,7 +61,6 @@ const InputContainer = styled.div.attrs( {
 	font-family: Arial, Roboto-Regular, HelveticaNeue, sans-serif;
 	font-size: 14px;
 	cursor: text;
-	margin-top: 5px;
 
 	&::before {
 		display: block;
