745b51b9f7bf01b8cef4b13f99c855cfae96a230
IreneStr <irene@yoast.com>
2018-06-12 14:03:49 +0200

    Prettify styling of entities and snippet editor

---
+++ composites/Plugin/SnippetEditor/components/Mention.js
@@ -0,0 +1,31 @@
+import React from "react";
+import styled from "styled-components";
+import colors from "../../../../style-guide/colors";
+import PropTypes from "prop-types";
+
+const StyledMention = styled.span`
+	color: ${ colors.$color_white };
+	background-color: ${ colors.$color_pink_dark };
+	padding: 0px 8px;
+	margin: 0 2px;
+	border-radius: 17px;
+	
+	&:hover {
+		color: ${ colors.$color_white };
+		background-color: ${ colors.$color_pink_dark };
+	}
+`;
+
+export const Mention = ( { children, className } ) => {
+	return <StyledMention
+		className={className}
+		spellCheck={false}
+	>
+		{children}
+	</StyledMention>;
+};
+
+Mention.propTypes = {
+	children: PropTypes.node,
+	className: PropTypes.string,
+};
--- composites/Plugin/SnippetEditor/components/ReplacementVariableEditorStandalone.js
+++ composites/Plugin/SnippetEditor/components/ReplacementVariableEditorStandalone.js
@@ -14,6 +14,7 @@ import styled from "styled-components";
 // Internal dependencies.
 import { replacementVariablesShape } from "../constants";
 import { positionSuggestions } from "../positionSuggestions";
+import { Mention } from "./Mention";
 import {
 	serializeEditor,
 	unserializeEditor,
@@ -134,6 +135,7 @@ class ReplacementVariableEditorStandalone extends React.Component {
 			mentionTrigger: "%",
 			entityMutability: "IMMUTABLE",
 			positionSuggestions,
+			mentionComponent: Mention,
 		} );
 
 		this.singleLinePlugin = createSingleLinePlugin( {
