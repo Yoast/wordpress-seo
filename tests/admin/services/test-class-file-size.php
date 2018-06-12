943a3832e9b18dc1443ed024cd2069248322d6aa
Andy Meerwaldt <andy@yoast.com>
2018-06-12 11:57:40 +0200

    Add unittests

---
+++ tests/admin/services/test-class-file-size.php
@@ -0,0 +1,97 @@
+<?php
+/**
+ * WPSEO plugin test file.
+ *
+ * @package WPSEO\Tests\Admin
+ */
+
+/**
+ * Unit Test Class.
+ */
+class WPSEO_File_Size_Service_Test extends WPSEO_UnitTestCase {
+
+	/**
+	 * Tests the retrieval of the file size for external hosted file.
+	 *
+	 * @covers WPSEO_File_Size_Service::get
+	 */
+	public function test_get_for_externally_hosted_file() {
+		$request = new WP_REST_Request();
+		$request->set_param( 'url', 'external.file' );
+
+		$instance = $this
+			->getMockBuilder( 'WPSEO_File_Size_Service' )
+			->setMethods( array( 'is_externally_hosted' ) )
+			->getMock();
+
+		$instance
+			->expects( $this->once() )
+			->method( 'is_externally_hosted' )
+			->willReturn( true );
+
+		$response = $instance->get( $request );
+
+		$this->assertEquals( 'failure', $response->data['type'] );
+		$this->assertEquals( 'Cannot get the size of external.file because it is hosted externally.', $response->data['response'] );
+	}
+
+	/**
+	 * Tests the retrieval of the file size when unknown error occurs.
+	 *
+	 * @covers WPSEO_File_Size_Service::get
+	 */
+	public function test_get_with_unknown_failure() {
+		$request = new WP_REST_Request();
+		$request->set_param( 'url', 'unknown.file' );
+
+		$instance = $this
+			->getMockBuilder( 'WPSEO_File_Size_Service' )
+			->setMethods( array( 'is_externally_hosted', 'get_file_size' ) )
+			->getMock();
+
+		$instance
+			->expects( $this->once() )
+			->method( 'is_externally_hosted' )
+			->willReturn( false );
+
+		$instance
+			->expects( $this->once() )
+			->method( 'get_file_size' )
+			->willReturn( false );
+
+		$response = $instance->get( $request );
+
+		$this->assertEquals( 'failure', $response->data['type'] );
+		$this->assertEquals( 'Cannot get the size of unknown.file because of unknown reasons.', $response->data['response'] );
+	}
+
+	/**
+	 * Tests the retrieval of the file size is successful
+	 *
+	 * @covers WPSEO_File_Size_Service::get
+	 */
+	public function test_get_on_success() {
+		$request = new WP_REST_Request();
+		$request->set_param( 'url', 'unknown.file' );
+
+		$instance = $this
+			->getMockBuilder( 'WPSEO_File_Size_Service' )
+			->setMethods( array( 'is_externally_hosted', 'get_file_size' ) )
+			->getMock();
+
+		$instance
+			->expects( $this->once() )
+			->method( 'is_externally_hosted' )
+			->willReturn( false );
+
+		$instance
+			->expects( $this->once() )
+			->method( 'get_file_size' )
+			->willReturn( 2048 );
+
+		$response = $instance->get( $request );
+
+		$this->assertEquals( 'success', $response->data['type'] );
+		$this->assertEquals( 2048, $response->data['response'] );
+	}
+}
