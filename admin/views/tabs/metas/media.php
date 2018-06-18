5db8e8d03b1fc22bf6c248481521fc2141577960
Dieter <gargamelk@hotmail.com>
2018-06-18 15:24:47 +0200

    Changed color of warning

--- admin/views/tabs/metas/media.php
+++ admin/views/tabs/metas/media.php
@@ -39,7 +39,7 @@ $yform->toggle_switch(
 
 if ( $yform->options['disable-attachment'] === 'off' ) {
 	?>
-	<div class="notice error is-dismissible" >
+	<div class="notice notice-warning is-dismissible" >
 		<p><?php _e( 'Your website is configured to show \'attachment\' pages for the images and media you\'ve uploaded. In most cases, this can be harmful to your SEO. We generally recommend hiding these pages by enabling the "Redirect attachment URLs to the attachment itself" option in our \'Media\' settings tab.', 'wordpress-seo' ); ?></p>
 	</div>
 	<?php
