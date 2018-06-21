eec9faea0ef73d784e03bc39df5e8970f1118339
boblinthorst <bob.linthorst@gmail.com>
2018-06-21 10:42:25 +0200

    Moved responsiblity of creating the associative array from the static function to the register_help_text function

--- inc/class-wpseo-replacement-variable.php
+++ inc/class-wpseo-replacement-variable.php
@@ -9,9 +9,14 @@
 /**
  * Class WPSEO_Replace_Var_Data
  *
- * This class is used by class-wpseo-replace-vars to store the data of a single row of the snippet variables table.
+ * This class stores the data of a single snippet variable.
  */
-class WPSEO_Replace_Var_Data {
+class WPSEO_Replacement_Variable {
+
+	/**
+	 * @var string The variable to use.
+	 */
+	protected $variable;
 
 	/**
 	 * @var string The label of the replacement variable.
@@ -26,14 +31,34 @@ class WPSEO_Replace_Var_Data {
 	/**
 	 * WPSEO_Replace_Var_Data constructor.
 	 *
+	 * @param string $variable      The variable that is replaced.
 	 * @param string $label         The label of the replacement variable.
 	 * @param string $description   The description of the replacement variable.
 	 *
-	 * @return \WPSEO_Replace_Var_Data
+	 * @return \WPSEO_Replacement_Variable
 	 */
-	public function __construct( $label, $description ) {
+	public function __construct( $variable, $label, $description ) {
+		$this->variable = $variable;
 		$this->label = $label;
 		$this->description = $description;
+	}
+
+	/**
+	 * Returns the variable to use.
+	 *
+	 * @return string
+	 */
+	public function get_variable() {
+		return $this->variable;
+	}
+
+	/**
+	 * Sets the replacement variable to use.
+	 *
+	 * @param string $variable The variable that is replaced.
+	 */
+	public function set_variable( $variable ) {
+		$this->variable = $variable;
 	}
 
 	/**
