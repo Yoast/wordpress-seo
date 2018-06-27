6db58449e327172cfc921e13ab3d8594f7e47191
Maarten Leenders <maarten@yoast.com>
2018-06-27 11:39:01 +0200

    Rename single field editors and pass unique id to them

--- js/src/components/SettingsReplacementVariableField.js
+++ js/src/components/SettingsReplacementVariableField.js
@@ -19,7 +19,7 @@ const SnippetEditorWidthContainer = styled.div`
 	max-width: 640px;
 `;
 
-class SettingsTitleReplacementVariableEditor extends React.Component {
+class SettingsReplacementVariableField extends React.Component {
 	constructor( props ) {
 		super( props );
 
@@ -41,12 +41,14 @@ class SettingsTitleReplacementVariableEditor extends React.Component {
 			replacementVariables,
 			recommendedReplacementVariables,
 			field,
+			fieldId,
 		} = this.props;
 
 		return (
 			<SnippetEditorWidthContainer>
 				<ReplaceVarEditor
 					label={ label }
+					fieldId={ fieldId + "-snippet-editor" }
 					isActive={ this.state.isActive }
 					isHowvered={ this.state.isHovered }
 					content={ field.value }
@@ -63,10 +65,11 @@ class SettingsTitleReplacementVariableEditor extends React.Component {
 	}
 }
 
-SettingsTitleReplacementVariableEditor.propTypes = {
+SettingsReplacementVariableField.propTypes = {
 	replacementVariables: replacementVariablesShape,
 	recommendedReplacementVariables: recommendedReplacementVariablesShape,
 	label: PropTypes.string.isRequired,
+	fieldId: PropTypes.string.isRequired,
 	field: linkFieldsShape,
 };
 
@@ -77,4 +80,4 @@ export default linkHiddenFields( props => {
 			fieldId: props.fieldId,
 		},
 	];
-} )( SettingsTitleReplacementVariableEditor );
+} )( SettingsReplacementVariableField );
