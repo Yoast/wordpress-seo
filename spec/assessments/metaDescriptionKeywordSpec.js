62f2a10fa76cb480d834015518b6a5e0da5b0a00
Manuel <manuel@yoast.com>
2018-06-28 11:55:23 +0200

    Add links for first assessments

--- spec/assessments/metaDescriptionKeywordSpec.js
+++ spec/assessments/metaDescriptionKeywordSpec.js
@@ -12,7 +12,7 @@ describe( "the metadescription keyword assessment", function() {
 		var result = metaDescriptionKeyword.getResult( paper, researcher, i18n );
 
 		expect( result.getScore() ).toBe( 3 );
-		expect( result.getText() ).toBe( "A meta description has been specified, but it does not contain the focus keyword." );
+		expect( result.getText() ).toBe( "A meta description has been specified, but it <a href='https://yoa.st/2pf' target='_blank'>does not contain the focus keyword</a>." );
 	} );
 
 	it( "should score a meta with no matching keyword as bad", function() {
@@ -22,7 +22,7 @@ describe( "the metadescription keyword assessment", function() {
 		var result = metaDescriptionKeyword.getResult( paper, researcher, i18n );
 
 		expect( result.getScore() ).toBe( 9 );
-		expect( result.getText() ).toBe( "The meta description contains the focus keyword." );
+		expect( result.getText() ).toBe( "The meta description <a href='https://yoa.st/2pf' target='_blank'>contains the focus keyword</a>." );
 	} );
 
 	it( "should not score since there is no meta", function() {
