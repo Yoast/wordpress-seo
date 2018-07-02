5b925ae6fccab0102ba6bb45e0726a8d97793ded
Alexander Botteram <a.botteram@gmail.com>
2018-07-02 13:30:21 +0200

    Removed redundant curly braces

--- composites/Plugin/SnippetEditor/components/SnippetEditorFields.js
+++ composites/Plugin/SnippetEditor/components/SnippetEditorFields.js
@@ -211,7 +211,7 @@ class SnippetEditorFields extends React.Component {
 							onBlur={ () => onBlur() }
 							innerRef={ ref => this.setRef( "slug", ref ) }
 							aria-labelledby={ this.uniqueId + "-slug" }
-							id={ "snippet-editor-field-slug" }
+							id="snippet-editor-field-slug"
 						/>
 					</InputContainerWithCaretStyles>
 				</FormSection>
@@ -230,7 +230,7 @@ class SnippetEditorFields extends React.Component {
 						recommendedReplacementVariables={ recommendedReplacementVariables }
 						content={ description }
 						onChange={ content => onChange( "description", content ) }
-						fieldId={ "snippet-editor-field-description" }
+						fieldId="snippet-editor-field-description"
 					/>
 					<ProgressBar
 						max={ descriptionLengthProgress.max }
