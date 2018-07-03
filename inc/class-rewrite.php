da7c9868fb99fa5d2b2c7463f53eeaac8585dc6c
Andy Meerwaldt <andy@yoast.com>
2018-07-03 15:49:59 +0200

    Fix codestyle.

--- inc/class-rewrite.php
+++ inc/class-rewrite.php
@@ -132,30 +132,27 @@ class WPSEO_Rewrite {
 		$categories = get_categories( array( 'hide_empty' => false ) );
 		if ( is_array( $categories ) && $categories !== array() ) {
 			foreach ( $categories as $category ) {
-				$category_nicename          = $category->slug;
+				$category_nicename = $category->slug;
 				if ( $category->parent == $category->cat_ID ) {
 					// Recursive recursion.
 					$category->parent = 0;
 				}
 				elseif ( $taxonomy->rewrite['hierarchical'] != 0 && $category->parent !== 0 ) {
-					$parents = get_category_parents( $category->parent, false, '/', true );
+						$parents = get_category_parents( $category->parent, false, '/', true );
 					if ( ! is_wp_error( $parents ) ) {
-						$category_nicename          = $parents . $category_nicename;
+						$category_nicename = $parents . $category_nicename;
 					}
 					unset( $parents );
 				}
 
 				$category_rewrite = $this->add_category_rewrites( $category_rewrite, $category_nicename, $blog_prefix, $wp_rewrite->pagination_base );
-				
-				//Add rules for the uppercase encoded URIs.
+
+				// Adds rules for the uppercase encoded URIs.
 				$category_nicename_filtered = $this->convert_encoded_to_upper( $category_nicename );
 
 				if ( $category_nicename_filtered !== $category_nicename ) {
-
 					$category_rewrite = $this->add_category_rewrites( $category_rewrite, $category_nicename_filtered, $blog_prefix, $wp_rewrite->pagination_base );
-
 				}
-
 			}
 			unset( $categories, $category, $category_nicename, $category_nicename_filtered );
 		}
@@ -170,70 +167,57 @@ class WPSEO_Rewrite {
 	}
 
 	/**
-	 * Add required category rewrites rules.
-	 * 
-	 * @param array  $rewrites the current set of rules.
-	 * @param string $category_name category nicename.
-	 * @param string $blog_prefix Multisite blog prefix.
+	 * Adds required category rewrites rules.
+	 *
+	 * @param array  $rewrites        The current set of rules.
+	 * @param string $category_name   Category nicename.
+	 * @param string $blog_prefix     Multisite blog prefix.
 	 * @param string $pagination_base WP_Query pagination base.
-	 * 
+	 *
 	 * @return array The added set of rules.
 	 */
 	protected function add_category_rewrites( $rewrites, $category_name, $blog_prefix, $pagination_base ) {
-		
 		$rewrite_name = $blog_prefix . '(' . $category_name . ')';
-					
-		$rewrites[ $rewrite_name . '/(?:feed/)?(feed|rdf|rss|rss2|atom)/?$' ] = 'index.php?category_name=$matches[1]&feed=$matches[2]';
+
+		$rewrites[ $rewrite_name . '/(?:feed/)?(feed|rdf|rss|rss2|atom)/?$' ]    = 'index.php?category_name=$matches[1]&feed=$matches[2]';
 		$rewrites[ $rewrite_name . '/' . $pagination_base . '/?([0-9]{1,})/?$' ] = 'index.php?category_name=$matches[1]&paged=$matches[2]';
-		$rewrites[ $rewrite_name . '/?$' ] = 'index.php?category_name=$matches[1]';
-	
+		$rewrites[ $rewrite_name . '/?$' ]                                       = 'index.php?category_name=$matches[1]';
+
 		return $rewrites;
-
 	}
 
 	/**
-	 * Convert encoded URI string to uppercase.
-	 * 
-	 * @param string $encoded The encoded string.
-	 * 
-	 * @return bool
-	 */
-	public function encode_to_upper( &$encoded ) {
-
-		if ( strpos( $encoded, '%' ) === false  ) {
-	
-			return false;
-	
-		}
-
-		$encoded = strtoupper( $encoded );
-
-		return true;
-	
-	}
-
-	/**
-	 * Walk through category nicename and convert encoded parts
+	 * Walks through category nicename and convert encoded parts
 	 * into uppercase using $this->encode_to_upper().
-	 * 
+	 *
 	 * @param string $name The encoded category URI string.
-	 * 
-	 * @return string
+	 *
+	 * @return string The convered URI string.
 	 */
-	public function convert_encoded_to_upper( $name ) {
-
+	protected function convert_encoded_to_upper( $name ) {
 		// Checks if name has any encoding in it.
 		if ( strpos( $name, '%' ) === false  ) {
-
 			return $name;
-
 		}
-		
-		$name = explode( '/', $name );
-	
-		array_walk( $name, array( $this, 'encode_to_upper' ) );
-	
-		return implode( '/', $name );
+
+		$names = explode( '/', $name );
+		$names = array_map( array( $this, 'encode_to_upper' ), $names );
+
+		return implode( '/', $names );
 	}
 
+	/**
+	 * Converts the encoded URI string to uppercase.
+	 *
+	 * @param string $encoded The encoded string.
+	 *
+	 * @return string The uppercased string.
+	 */
+	public function encode_to_upper( $encoded ) {
+		if ( strpos( $encoded, '%' ) === false  ) {
+			return $encoded;
+		}
+
+		return strtoupper( $encoded );
+	}
 } /* End of class */
