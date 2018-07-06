c541bcad9bc3ddc0d62ecfc53af106181c8c9294
Andy Meerwaldt <andy@yoast.com>
2018-07-06 12:48:49 +0200

    Move the alert to the view itself.

--- admin/views/tabs/metas/paper-content/media-content.php
+++ admin/views/tabs/metas/paper-content/media-content.php
@@ -7,6 +7,8 @@
  * @var Yoast_Form $yform
  */
 
+$wpseo_post_type = get_post_type_object( 'attachment' );
+
 $media_help = new WPSEO_Admin_Help_Panel(
 	'search-appearance-media',
 	__( 'Learn more about the Media and attachment URLs setting', 'wordpress-seo' ),
@@ -32,6 +34,23 @@ $yform->toggle_switch(
 	<br/>
 
 	<?php
+	$noindex_option_name = 'noindex-' . $wpseo_post_type->name;
+
+	if ( WPSEO_Options::get( 'is-media-purge-relevant' ) && WPSEO_Options::get( $noindex_option_name ) === false ) {
+		$description = sprintf(
+			/* translators: %1$s expands to the link to the article, %2$s closes the link to the article */
+			esc_html( __( 'By enabling this option, attachment URLs become visible to both your visitors and Google.
+				To add value to your website, they should contain useful information, or they might have a
+				negative impact on your ranking. Please carefully consider this and %1$sread this post%2$s if
+				you want more information about the impact of showing media in search results.', 'wordpress-seo'
+			) ),
+			'<a href="' . esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/2r8' ) ) . '" rel="noopener nofollow" target="_blank">',
+			'</a>'
+		);
+
+		echo '<div style="clear:both; background-color: #ffeb3b; color: #000000; padding: 16px; max-width: 450px; margin-bottom: 32px;">' . $description . '</div>';
+	}
+
 	$view_utils = new Yoast_View_Utils();
 	$view_utils->show_post_type_settings( 'attachment', false );
 	?>
