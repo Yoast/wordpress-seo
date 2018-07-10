2dd11538c782903887cc92a3a920506f94dfe9cf
Andy Meerwaldt <andy@yoast.com>
2018-07-10 09:06:48 +0200

    Fix indentation.

--- admin/views/tabs/metas/paper-content/date-archives-settings.php
+++ admin/views/tabs/metas/paper-content/date-archives-settings.php
@@ -15,23 +15,23 @@
 	), __( 'Date archives', 'wordpress-seo' ) );
 ?>
 <div id='date-archives-titles-metas-content' class='archives-titles-metas-content'>
-<?php
+	<?php
 		$date_archives_help = new WPSEO_Admin_Help_Panel(
-		'noindex-archive-wpseo',
-		esc_html__( 'Help on the date archives search results setting', 'wordpress-seo' ),
-		sprintf(
-		/* translators: 1: expands to <code>noindex</code>; 2: link open tag; 3: link close tag. */
-		esc_html__( 'Not showing the date archives in the search results technically means those will have a %1$s robots meta and will be excluded from XML sitemaps. %2$sMore info on the search results settings%3$s.', 'wordpress-seo' ),
-		'<code>noindex</code>',
-		'<a href="' . esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/show-x' ) ) . '" target="_blank" rel="noopener noreferrer">',
-			'</a>'
-		)
+			'noindex-archive-wpseo',
+			esc_html__( 'Help on the date archives search results setting', 'wordpress-seo' ),
+			sprintf(
+			/* translators: 1: expands to <code>noindex</code>; 2: link open tag; 3: link close tag. */
+			esc_html__( 'Not showing the date archives in the search results technically means those will have a %1$s robots meta and will be excluded from XML sitemaps. %2$sMore info on the search results settings%3$s.', 'wordpress-seo' ),
+			'<code>noindex</code>',
+			'<a href="' . esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/show-x' ) ) . '" target="_blank" rel="noopener noreferrer">',
+				'</a>'
+			)
 		);
 
 		$yform->index_switch(
-		'noindex-archive-wpseo',
-		__( 'date archives', 'wordpress-seo' ),
-		$date_archives_help->get_button_html() . $date_archives_help->get_panel_html()
+			'noindex-archive-wpseo',
+			__( 'date archives', 'wordpress-seo' ),
+			$date_archives_help->get_button_html() . $date_archives_help->get_panel_html()
 		);
 
 		$recommended_replace_vars     = new WPSEO_Admin_Recommended_Replace_Vars();
