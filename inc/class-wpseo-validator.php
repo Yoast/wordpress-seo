937a0f2388bdce1f3052a27e0e20383712889a76
Jimmy Comack <jimmy@yoast.com>
2018-07-11 15:42:04 +0200

    Replacing filter_var for boolean values with a simple is_boolean until further notice

--- inc/class-wpseo-validator.php
+++ inc/class-wpseo-validator.php
@@ -18,7 +18,7 @@ class WPSEO_Validator {
 	 * @return bool Whether or not the passed variable is a valid boolean.
 	 */
 	public static function is_boolean( $variable ) {
-		return filter_var( $variable, FILTER_VALIDATE_BOOLEAN, array( 'flags' => FILTER_NULL_ON_FAILURE ) ) !== null;
+		return is_bool( $variable ) !== null;
 	}
 
 	/**
