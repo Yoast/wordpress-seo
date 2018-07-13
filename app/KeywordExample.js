2d1c2edc5f770e32cdcaa7af6f6fb8805fbed3fc
boblinthorst <bob.linthorst@gmail.com>
2018-07-13 12:09:52 +0200

    fixed keywordInput app

--- app/KeywordExample.js
+++ app/KeywordExample.js
@@ -1,6 +1,7 @@
 // External dependencies.
 import React, { Component } from "react";
 import styled from "styled-components";
+import debounce from "lodash/debounce";
 
 // Internal dependencies.
 import KeywordInput from "../composites/Plugin/Shared/components/KeywordInput";
@@ -9,9 +10,10 @@ import SynonymsSection from "../composites/Plugin/Synonyms/components/SynonymsSe
 const Container = styled.div`
 	background-color: white;
 	margin: 5em auto 0;
-	padding: 0 0 10px;
+	padding: 10px;
 	display: flex;
 	flex-direction: column;
+	width: 512px;
 `;
 
 export default class KeywordExample extends Component {
@@ -20,17 +22,38 @@ export default class KeywordExample extends Component {
 	 *
 	 * @returns {ReactElement} The rendered keyword example.
 	 */
+
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			keyword: "myKeywordo",
+		};
+
+		this.updateKeyword = this.updateKeyword.bind( this );
+	}
+
+	updateKeyword( event ) {
+		this.setState( {
+			keyword: event,
+		} );
+	}
+
+
 	render() {
 		return (
 			<Container>
 				<KeywordInput
 					id="focus-keyword"
 					label={ "Focus keyword"}
+				    onChange={ this.updateKeyword }
+					keyword={ this.state.keyword }
 				/>
 
 				<SynonymsSection
 					label={ "Keyword synonyms" }
 					onChange={ synonyms => console.log( 'SynonymsField change event', synonyms ) }
+				    synonyms=""
 				/>
 			</Container>
 		);
