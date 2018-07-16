36e578f4a41bccbd56472a9ce995b3b2e42539f0
Jip Moors <jhp.moors@gmail.com>
2018-07-16 10:43:17 +0200

    Bump version 7.8.1

--- premium/premium.php
+++ premium/premium.php
@@ -27,7 +27,7 @@ class WPSEO_Premium {
 	const OPTION_CURRENT_VERSION = 'wpseo_current_version';
 
 	/** @var string */
-	const PLUGIN_VERSION_NAME = '7.8';
+	const PLUGIN_VERSION_NAME = '7.8.1';
 
 	/** @var string */
 	const PLUGIN_VERSION_CODE = '16';
--- wp-seo-premium.php
+++ wp-seo-premium.php
@@ -10,7 +10,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast SEO Premium
- * Version:     7.8
+ * Version:     7.8.1
  * Plugin URI:  https://yoa.st/2jc
  * Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
  * Author:      Team Yoast
