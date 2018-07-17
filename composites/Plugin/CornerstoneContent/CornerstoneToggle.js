21dcb546c61a9517b236b4002f2e68f46e41c2ed
Dieter <gargamelk@hotmail.com>
2018-07-17 20:41:46 +0200

    Added Gutenberg toggle and Cornerstone toggle

--- composites/Plugin/CornerstoneContent/CornerstoneToggle.js
+++ composites/Plugin/CornerstoneContent/CornerstoneToggle.js
@@ -1,12 +1,44 @@
 import React from "react";
+import PropTypes from "prop-types";
+import Styled from "styled-components";
 import GutenbergToggle from "../Shared/components/GutenbergToggle";
 
-export default function CornerstoneToggle() {
-	return <GutenbergToggle
-		key="toggle"
-		checked={ true }
-		onChange={ () => {} }
-		id={ "CornerstoneToggleId" }
-	/>;
+// Add label.
+const Cornerstone = Styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    
+    label { 
+        margin-right: 10px;
+        flex-shrink: 0;
+        max-width: 75%;
+    }
+`;
+
+class CornerstoneToggle extends React.Component {
+	render() {
+		return (
+			<Cornerstone>
+				<label htmlFor={"CornerstoneToggleId"}>Cornerstone</label>
+				<GutenbergToggle
+					key="toggle"
+					checked={this.props.checked}
+					onChange={this.props.onChange}
+					id={"CornerstoneToggleId"}
+				/>
+			</Cornerstone>
+		)
+	}
 }
 
+CornerstoneToggle.propTypes = {
+	checked: PropTypes.bool,
+	onChange: PropTypes.func.isRequired,
+};
+
+CornerstoneToggle.defaultProps = {
+	checked: false,
+};
+
+export default CornerstoneToggle;
