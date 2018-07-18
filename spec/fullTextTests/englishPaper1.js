b4a073da51ce8008606734f7ce52ae813724e5fe
Natalia <natalia@yoast.com>
2018-07-18 19:01:00 +0200

    Add more assessments

--- spec/fullTextTests/englishPaper1.js
+++ spec/fullTextTests/englishPaper1.js
@@ -6,16 +6,32 @@ const Paper = require( "../../js/values/Paper.js" );
 
 const name = "englishPaper1";
 
-const paper = new Paper( "This is a very interesting paper", { locale: "en_EN" } );
+const paper = new Paper( "This is a very interesting paper one two three.", { keyword: "one two three", locale: "en_EN" } );
 
 const expectedResults = {
 	introductionKeyword: {
 		score: 9,
-		resultText: "You are awesome in introductionKeyword assessment!",
+		resultText: "The focus keyword appears in the <a href='https://yoa.st/2pc' target='_blank'>first paragraph</a> of the copy.",
 	},
+	keyphraseLength: {
+		score: 9,
+		resultText: "Awesome keyphrase lenght",
+	},
+	keywordDensity: {
+		score: 9,
+		resultText: "Great keywordDensity!",
+	},
+	keywordStopWords: {
+		score: 9,
+		resultText: "AAAA",
+	},
+
+
+
+
 	fleschReadingEase: {
 		score: 9,
-		resultText: "You are also awesome in Flesch reading ease!",
+		resultText: "The copy scores 66.1 in the <a href='https://yoa.st/flesch-reading' target='_blank'>Flesch Reading Ease</a> test, which is considered ok to read. ",
 	}
 };
 
--- spec/fullTextTests/runFullTextTests.js
+++ spec/fullTextTests/runFullTextTests.js
@@ -1,11 +1,10 @@
 // SEO assessments
 //import * as introductionKeyword from "../../js/assessments/seo/introductionKeywordAssessment";
 const introductionKeyword = require( "../../js/assessments/seo/introductionKeywordAssessment" );
-/*
-import KeyphraseLengthAssessment from "../../js/assessments/seo/KeyphraseLengthAssessment";
-import KeywordDensityAssessment from "../../js/assessments/seo/KeywordDensityAssessment";
-import * as keywordStopWords from "../../js/assessments/seo/keywordStopWordsAssessment";
-import * as metaDescriptionKeyword from "../../js/assessments/seo/metaDescriptionKeywordAssessment";
+const KeyphraseLengthAssessment = require( "../../js/assessments/seo/KeyphraseLengthAssessment" );
+const KeywordDensityAssessment = require( "../../js/assessments/seo/KeywordDensityAssessment" );
+const keywordStopWords = require( "../../js/assessments/seo/keywordStopWordsAssessment" );
+import * as metaDescriptionKeywordAssessment from "../../js/assessments/seo/metaDescriptionKeywordAssessment";
 import * as MetaDescriptionLength from "../../js/assessments/seo/metaDescriptionLengthAssessment";
 import * as SubheadingsKeyword from "../../js/assessments/seo/subheadingsKeywordAssessment";
 import * as textCompetingLinks from "../../js/assessments/seo/textCompetingLinksAssessment";
@@ -19,9 +18,9 @@ import UrlKeywordAssessment from "../../js/assessments/seo/UrlKeywordAssessment"
 import * as UrlLength from "../../js/assessments/seo/urlLengthAssessment";
 import * as urlStopWords from "../../js/assessments/seo/urlStopWordsAssessment";
 import LargestKeywordDistance from "../../js/assessments/seo/LargestKeywordDistanceAssessment";
- */
+
 // Content assessments
-const FleschReadingEase = require( "../../js/assessments/readability/fleschReadingEaseAssessment" );
+const FleschReadingAssessment = require( "../../js/assessments/readability/fleschReadingEaseAssessment" );
 /*
 const paragraphTooLong = require( "../../js/assessments/readability/paragraphTooLongAssessment" );
 const SentenceLengthInText = require( "../../sjsrc/assessments/readability/sentenceLengthInTextAssessment" );
@@ -34,6 +33,42 @@ const textPresence = require( "../../js/assessments/readability/textPresenceAsse
 
 // Researches
 const findKeywordInFirstParagraph = require( "../../js/researches/findKeywordInFirstParagraph.js" );
+const keyphraseLength = require( "../../js/researches/keyphraseLength" );
+const keywordCount = require( "../../js/researches/keywordCount" );
+const getKeywordDensity = require( "../../js/researches/getKeywordDensity.js" );
+const stopWordsInKeyword = require( "../../js/researches/stopWordsInKeyword" );
+
+
+const calculateFleschReading = require( "../../js/researches/calculateFleschReading.js" );
+
+
+const wordCountInText = require( "../../js/researches/wordCountInText.js" );
+const getLinkStatistics = require( "../../js/researches/getLinkStatistics.js" );
+const linkCount = require( "../../js/researches/countLinks.js" );
+const getLinks = require( "../../js/researches/getLinks.js" );
+const urlLength = require( "../../js/researches/urlIsTooLong.js" );
+const findKeywordInPageTitle = require( "../../js/researches/findKeywordInPageTitle.js" );
+const matchKeywordInSubheadings = require( "../../js/researches/matchKeywordInSubheadings.js" );
+const stopWordsInUrl = require( "../../js/researches/stopWordsInUrl" );
+const metaDescriptionLength = require( "../../js/researches/metaDescriptionLength.js" );
+const imageCount = require( "../../js/researches/imageCountInText.js" );
+const altTagCount = require( "../../js/researches/imageAltTags.js" );
+const metaDescriptionKeyword = require( "../../js/researches/metaDescriptionKeyword.js" );
+const keywordCountInUrl = require( "../../js/researches/keywordCountInUrl" );
+const pageTitleWidth = require( "../../js/researches/pageTitleWidth.js" );
+const wordComplexity = require( "../../js/researches/getWordComplexity.js" );
+const getParagraphLength = require( "../../js/researches/getParagraphLength.js" );
+const countSentencesFromText = require( "../../js/researches/countSentencesFromText.js" );
+const countSentencesFromDescription = require( "../../js/researches/countSentencesFromDescription.js" );
+const getSubheadingTextLengths = require( "../../js/researches/getSubheadingTextLengths.js" );
+const findTransitionWords = require( "../../js/researches/findTransitionWords.js" );
+const passiveVoice = require( "../../js/researches/getPassiveVoice.js" );
+const getSentenceBeginnings = require( "../../js/researches/getSentenceBeginnings.js" );
+const relevantWords = require( "../../js/researches/relevantWords" );
+const readingTime = require( "../../js/researches/readingTime" );
+const getTopicDensity = require( "../../js/researches/getTopicDensity" );
+const topicCount = require( "../../js/researches/topicCount" );
+const largestKeywordDistance = require( "../../js/researches/largestKeywordDistance" );
 
 const contentConfiguration = require( "../../src/config/content/combinedConfig" );
 const researcher = require( "../../src/researcher" );
@@ -50,13 +85,14 @@ const testPapers = [
 
 testPapers.forEach( function( testPaper ) {
 	describe( "Full-text test for paper " + testPaper.name, function() {
-		const locale = testPaper.paper.getLocale();
+		const paper = testPaper.paper;
+		const locale = paper.getLocale();
+		const expectedResults = testPaper.expectedResults;
 		let result = {};
 
 		// Create a list of all assessments to be run
 		/*const assessments = [
-			introductionKeyword,
-			new KeyphraseLengthAssessment(),
+
 			new KeywordDensityAssessment(),
 			keywordStopWords,
 			metaDescriptionKeyword,
@@ -83,15 +119,62 @@ testPapers.forEach( function( testPaper ) {
 		];*/
 
 		it( "returns a score and the associated feedback text for the introductionKeyword assessment", function () {
-			result.introductionKeyword = introductionKeyword.getResult( testPaper.paper, factory.buildMockResearcher( "findKeywordInFirstParagraph" ), i18n );
-			expect( result.introductionKeyword.getScore() ).toBe( testPaper.expectedResults.introductionKeyword.score );
-			expect( result.introductionKeyword.getText() ).toBe( testPaper.expectedResults.introductionKeyword.resultText );
+			result.introductionKeyword = introductionKeyword.getResult(
+				paper,
+				factory.buildMockResearcher( findKeywordInFirstParagraph( paper) ),
+				i18n
+			);
+			expect( result.introductionKeyword.getScore() ).toBe( expectedResults.introductionKeyword.score );
+			expect( result.introductionKeyword.getText() ).toBe( expectedResults.introductionKeyword.resultText );
 		} );
 
-		it( "returns a score and the associated feedback text for the introductionKeyword assessment", function () {
-			result.fleschReadingEase = new FleschReadingEase( contentConfiguration( locale ).fleschReading ).getResult( testPaper.paper, factory.buildMockResearcher( "calculateFleschReading" ), i18n );
-			expect( result.fleschReadingEase.getScore() ).toBe( testPaper.expectedResults.fleschReadingEase.score );
-			expect( result.fleschReadingEase.getText() ).toBe( testPaper.expectedResults.fleschReadingEase.resultText );
-		} )
+		it( "returns a score and the associated feedback text for the keyphraseLength assessment", function () {
+			result.keyphraseLength = new KeyphraseLengthAssessment().getResult(
+				paper,
+				factory.buildMockResearcher( keyphraseLength( paper ) ),
+				i18n
+			);
+			expect( result.keyphraseLength.getScore() ).toBe( expectedResults.keyphraseLength.score );
+			expect( result.keyphraseLength.getText() ).toBe( expectedResults.keyphraseLength.resultText );
+		} );
+
+		it( "returns a score and the associated feedback text for the keywordDensity assessment", function () {
+			result.keywordDensity = new KeywordDensityAssessment().getResult(
+				paper,
+				factory.buildMockResearcher( {
+					keywordCount: { count: keywordCount( paper ) },
+					keywordDensity: getKeywordDensity( paper, factory.buildMockResearcher( {
+						keywordCount: { count: keywordCount( paper ) },
+						} ) )
+				} ),
+				i18n
+			);
+			expect( result.keywordDensity.getScore() ).toBe( expectedResults.keywordDensity.score );
+			expect( result.keywordDensity.getText() ).toBe( expectedResults.keywordDensity.resultText );
+		} );
+
+		it( "returns a score and the associated feedback text for the keywordStopWords assessment", function () {
+			result.keywordStopWords = keywordStopWords.getResult(
+				paper,
+				factory.buildMockResearcher( stopWordsInKeyword( paper ) ),
+				i18n
+			);
+			expect( result.keywordStopWords.getScore() ).toBe( expectedResults.keywordStopWords.score );
+			expect( result.keywordStopWords.getText() ).toBe( expectedResults.keywordStopWords.resultText );
+		} );
+
+
+
+
+		/*it( "returns a score and the associated feedback text for the fleschReadingEase assessment", function () {
+			result.fleschReadingEase = new FleschReadingAssessment( contentConfiguration( locale ).fleschReading ).getResult(
+				paper,
+				factory.buildMockResearcher( calculateFleschReading( paper ) ),
+				i18n
+			);
+			//new FleschReadingAssessment( contentConfiguration( paper.getLocale() ).fleschReading ).getResult( paper, factory.buildMockResearcher( 100.0 ), i18n );
+			expect( result.fleschReadingEase.getScore() ).toBe( expectedResults.fleschReadingEase.score );
+			expect( result.fleschReadingEase.getText() ).toBe( expectedResults.fleschReadingEase.resultText );
+		} )*/
 	} )
 } );
