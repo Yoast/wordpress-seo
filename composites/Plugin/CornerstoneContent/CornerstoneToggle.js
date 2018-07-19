5b583cfe43035c32026fdec399d188e9cb7132c4
boblinthorst <bob.linthorst@gmail.com>
2018-07-19 12:18:44 +0200

    Added state manangement for now

--- composites/Plugin/CornerstoneContent/CornerstoneToggle.js
+++ composites/Plugin/CornerstoneContent/CornerstoneToggle.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Styled from "styled-components";
+import noop from "lodash/noop";
+
 import GutenbergToggle from "../Shared/components/GutenbergToggle";
 
-// Add label.
 const Cornerstone = Styled.div`
     display: flex;
     justify-content: space-between;
@@ -17,28 +18,64 @@ const Cornerstone = Styled.div`
 `;
 
 class CornerstoneToggle extends React.Component {
+
+	/**
+	 * The constructor.
+	 *
+	 * @param {object} props The component props.
+	 *
+	 * @returns {void}
+	 */
+	constructor( props ) {
+		super( props );
+
+		this.state = {
+			cornerstoneToggleState: this.props.checked,
+		};
+
+		this.handleChange = this.handleChange.bind( this );
+	}
+
+	/**
+	 * Handles changes on the cornerstoneToggle.
+	 *
+	 * @returns {void}
+	 */
+	handleChange() {
+		this.setState( { cornerstoneToggleState: ! this.state.cornerstoneToggleState } );
+		this.props.onChange();
+	}
+
+	/**
+	 * Renders the CornerstoneToggle component.
+	 *
+	 * @returns {ReactElement} the CornerstoneToggle component.
+	 */
 	render() {
 		return (
 			<Cornerstone>
-				<label htmlFor={"CornerstoneToggleId"}>Cornerstone</label>
+				<label htmlFor={this.props.key}>Cornerstone</label>
 				<GutenbergToggle
-					key="toggle"
-					checked={this.props.checked}
-					onChange={this.props.onChange}
-					id={"CornerstoneToggleId"}
+					key={this.props.key}
+					checked={this.state.cornerstoneToggleState}
+					onChange={this.handleChange}
+					id={this.props.toggleId}
 				/>
 			</Cornerstone>
-		)
+		);
 	}
 }
 
 CornerstoneToggle.propTypes = {
+	key: PropTypes.string,
 	checked: PropTypes.bool,
 	onChange: PropTypes.func.isRequired,
+	toggleId: PropTypes.string,
 };
 
 CornerstoneToggle.defaultProps = {
 	checked: false,
+	onChange: noop,
 };
 
 export default CornerstoneToggle;
