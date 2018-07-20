a04b06f3e85751d399f239a85af9464232773d5f
Manuel <manuel@yoast.com>
2018-07-20 11:56:43 +0200

    Change calls in taxonomy assessor and add taxonomy assessor spec

--- js/src/assessors/taxonomyAssessor.js
+++ js/src/assessors/taxonomyAssessor.js
@@ -1,17 +1,17 @@
-var Assessor = require( "yoastseo/js/assessor.js" );
+const Assessor = require( "yoastseo/js/assessor.js" );
 
-var introductionKeyword = require( "yoastseo/js/assessments/seo/introductionKeywordAssessment.js" );
-var keyphraseLength = require( "yoastseo/js/assessments/seo/keyphraseLengthAssessment.js" );
-var keywordDensity = require( "yoastseo/js/assessments/seo/keywordDensityAssessment.js" );
-var keywordStopWords = require( "yoastseo/js/assessments/seo/keywordStopWordsAssessment.js" );
-var metaDescriptionKeyword = require( "yoastseo/js/assessments/seo/metaDescriptionKeywordAssessment.js" );
-var MetaDescriptionLength = require( "yoastseo/js/assessments/seo/metaDescriptionLengthAssessment.js" );
-var titleKeyword = require( "yoastseo/js/assessments/seo/titleKeywordAssessment.js" );
-var TitleWidth = require( "yoastseo/js/assessments/seo/pageTitleWidthAssessment.js" );
-var UrlKeyword = require( "yoastseo/js/assessments/seo/urlKeywordAssessment.js" );
-var UrlLength = require( "yoastseo/js/assessments/seo/urlLengthAssessment.js" );
-var urlStopWords = require( "yoastseo/js/assessments/seo/urlStopWordsAssessment.js" );
-var taxonomyTextLength = require( "yoastseo/js/assessments/seo/taxonomyTextLengthAssessment" );
+import IntroductionKeywordAssessment from "yoastseo/js/assessments/seo/introductionKeywordAssessment.js";
+import KeyphraseLengthAssessment from "yoastseo/js/assessments/seo/keyphraseLengthAssessment.js";
+import KeywordDensityAssessment from "yoastseo/js/assessments/seo/keywordDensityAssessment.js";
+const keywordStopWords = require( "yoastseo/js/assessments/seo/keywordStopWordsAssessment.js" );
+import MetaDescriptionKeywordAssessment from "yoastseo/js/assessments/seo/metaDescriptionKeywordAssessment.js";
+const MetaDescriptionLength = require( "yoastseo/js/assessments/seo/metaDescriptionLengthAssessment.js" );
+import TitleKeywordAssessment from "yoastseo/js/assessments/seo/titleKeywordAssessment.js";
+const TitleWidth = require( "yoastseo/js/assessments/seo/pageTitleWidthAssessment.js" );
+import UrlKeywordAssessment from "yoastseo/js/assessments/seo/urlKeywordAssessment.js";
+const UrlLength = require( "yoastseo/js/assessments/seo/urlLengthAssessment.js" );
+const urlStopWords = require( "yoastseo/js/assessments/seo/urlStopWordsAssessment.js" );
+const taxonomyTextLength = require( "yoastseo/js/assessments/seo/taxonomyTextLengthAssessment" );
 
 /**
  * Creates the Assessor
@@ -23,16 +23,16 @@ var TaxonomyAssessor = function( i18n ) {
 	Assessor.call( this, i18n );
 
 	this._assessments = [
-		introductionKeyword,
-		keyphraseLength,
-		keywordDensity,
+		new IntroductionKeywordAssessment(),
+		new KeyphraseLengthAssessment(),
+		new KeywordDensityAssessment(),
 		keywordStopWords,
-		metaDescriptionKeyword,
+		new MetaDescriptionKeywordAssessment(),
 		new MetaDescriptionLength(),
 		taxonomyTextLength,
-		titleKeyword,
+		new TitleKeywordAssessment(),
 		new TitleWidth(),
-		new UrlKeyword(),
+		new UrlKeywordAssessment(),
 		new UrlLength(),
 		urlStopWords,
 	];
