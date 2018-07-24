6a841e183c6d4d6473b2a402b876d76cbaa84e16
Igor <igor@yoast.com>
2018-07-24 08:37:54 +0200

    Fix filenames from yoastseo.js refactor

--- js/src/assessors/taxonomyAssessor.js
+++ js/src/assessors/taxonomyAssessor.js
@@ -1,14 +1,14 @@
 const Assessor = require( "yoastseo/js/assessor.js" );
 
-import IntroductionKeywordAssessment from "yoastseo/js/assessments/seo/introductionKeywordAssessment.js";
-import KeyphraseLengthAssessment from "yoastseo/js/assessments/seo/keyphraseLengthAssessment.js";
-import KeywordDensityAssessment from "yoastseo/js/assessments/seo/keywordDensityAssessment.js";
+import IntroductionKeywordAssessment from "yoastseo/js/assessments/seo/IntroductionKeywordAssessment";
+import KeyphraseLengthAssessment from "yoastseo/js/assessments/seo/KeyphraseLengthAssessment";
+import KeywordDensityAssessment from "yoastseo/js/assessments/seo/KeywordDensityAssessment";
 const keywordStopWords = require( "yoastseo/js/assessments/seo/keywordStopWordsAssessment.js" );
-import MetaDescriptionKeywordAssessment from "yoastseo/js/assessments/seo/metaDescriptionKeywordAssessment.js";
+import MetaDescriptionKeywordAssessment from "yoastseo/js/assessments/seo/MetaDescriptionKeywordAssessment";
 const MetaDescriptionLength = require( "yoastseo/js/assessments/seo/metaDescriptionLengthAssessment.js" );
-import TitleKeywordAssessment from "yoastseo/js/assessments/seo/titleKeywordAssessment.js";
+import TitleKeywordAssessment from "yoastseo/js/assessments/seo/TitleKeywordAssessment";
 const TitleWidth = require( "yoastseo/js/assessments/seo/pageTitleWidthAssessment.js" );
-import UrlKeywordAssessment from "yoastseo/js/assessments/seo/urlKeywordAssessment.js";
+import UrlKeywordAssessment from "yoastseo/js/assessments/seo/UrlKeywordAssessment";
 const UrlLength = require( "yoastseo/js/assessments/seo/urlLengthAssessment.js" );
 const urlStopWords = require( "yoastseo/js/assessments/seo/urlStopWordsAssessment.js" );
 const taxonomyTextLength = require( "yoastseo/js/assessments/seo/taxonomyTextLengthAssessment" );
@@ -19,7 +19,7 @@ const taxonomyTextLength = require( "yoastseo/js/assessments/seo/taxonomyTextLen
  * @param {object} i18n The i18n object used for translations.
  * @constructor
  */
-var TaxonomyAssessor = function( i18n ) {
+const TaxonomyAssessor = function( i18n ) {
 	Assessor.call( this, i18n );
 
 	this._assessments = [
