b0ad305ebff225cd43e92295b5c49d7672bc748e
Manuel <manuel@yoast.com>
2018-07-25 10:59:40 +0200

    Bump version and build assets

--- js/dist/commons-791-RC1.js
+++ js/dist/commons-791-RC1.js
@@ -91,7 +91,7 @@
 /******/ 		if (__webpack_require__.nc) {
 /******/ 			script.setAttribute("nonce", __webpack_require__.nc);
 /******/ 		}
-/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-79x-RC1.min.js";
+/******/ 		script.src = __webpack_require__.p + "" + chunkId + "-791-RC1.min.js";
 /******/ 		var timeout = setTimeout(onScriptComplete, 120000);
 /******/ 		script.onerror = script.onload = onScriptComplete;
 /******/ 		function onScriptComplete() {
