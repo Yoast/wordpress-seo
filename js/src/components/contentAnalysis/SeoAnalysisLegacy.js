4ab932f480b65cff16f12f43efe4bfbc496d705a
Maarten Leenders <maarten@yoast.com>
2018-07-26 11:18:19 +0200

    Keep old AnalysisSection functional in the metabox by copying to legacy component

---
+++ js/src/components/contentAnalysis/SeoAnalysisLegacy.js
@@ -0,0 +1,51 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import Results from "./Results";
+
+/**
+ * Redux container for the seo analysis.
+ */
+class SeoAnalysisLegacy extends React.Component {
+	render() {
+		return (
+			<Results
+				showLanguageNotice={ false }
+				results={ this.props.results }
+				marksButtonClassName="yoast-tooltip yoast-tooltip-s"
+				marksButtonStatus={ this.props.marksButtonStatus }
+			/>
+		);
+	}
+}
+
+SeoAnalysisLegacy.propTypes = {
+	results: PropTypes.array,
+	marksButtonStatus: PropTypes.string,
+	hideMarksButtons: PropTypes.bool,
+};
+
+/**
+ * Maps redux state to SeoAnalysisLegacy props.
+ *
+ * @param {Object} state The redux state.
+ * @param {Object} ownProps The component's props.
+ *
+ * @returns {Object} Props that should be passed to SeoAnalysisLegacy.
+ */
+function mapStateToProps( state, ownProps ) {
+	const marksButtonStatus = ownProps.hideMarksButtons ? "disabled" : state.marksButtonStatus;
+
+	let results = null;
+	if( state.analysis.seo[ state.activeKeyword ] ) {
+		results = state.analysis.seo[ state.activeKeyword ].results;
+	}
+
+	return {
+		results,
+		marksButtonStatus: marksButtonStatus,
+	};
+}
+
+export default connect( mapStateToProps )( SeoAnalysisLegacy );
