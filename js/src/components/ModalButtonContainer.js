124dbd3cae9f9fe8008be3303a70db41974dbafd
Maarten Leenders <maarten@yoast.com>
2018-07-30 10:53:35 +0200

    Remove unnecessary mounthook logic

--- js/src/components/ModalButtonContainer.js
+++ js/src/components/ModalButtonContainer.js
@@ -7,7 +7,6 @@ const ModalButtonContainer = ( props ) => {
 	return (
 		<IntlProvider messages={ props.intl }>
 			<ModalIntl
-				mountHook={ props.mountHook }
 				appElement={ props.appElement }
 				openButtonIcon={ props.openButtonIcon }
 				labels={ props.intl }
