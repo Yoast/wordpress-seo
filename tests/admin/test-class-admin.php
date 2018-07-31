2874c7f90bfe8414c85b592c593ab053fb8557e0
Herre Groen <herre@yoast.com>
2018-07-31 10:29:19 +0200

    Remove unnecessary spaces.

--- tests/admin/test-class-admin.php
+++ tests/admin/test-class-admin.php
@@ -22,9 +22,9 @@ class WPSEO_Admin_Test extends WPSEO_UnitTestCase {
 		$class_instance = new WPSEO_Admin();
 
 		$admin_features = array(
-			'google_search_console'  => new WPSEO_GSC(),
-			'primary_category'       => new WPSEO_Primary_Term_Admin(),
-			'dashboard_widget'       => new Yoast_Dashboard_Widget(),
+			'google_search_console' => new WPSEO_GSC(),
+			'primary_category'      => new WPSEO_Primary_Term_Admin(),
+			'dashboard_widget'      => new Yoast_Dashboard_Widget(),
 		);
 
 		$this->assertEquals( $admin_features, $class_instance->get_admin_features() );
