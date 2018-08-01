364b2a841003dee2accf1b4eaf8c7defaa79e1c6
Jip Moors <jhp.moors@gmail.com>
2018-08-01 13:43:38 +0200

    Add return declaration

--- js/src/components/SidebarCollapsible.js
+++ js/src/components/SidebarCollapsible.js
@@ -5,6 +5,8 @@ import { Collapsible } from "yoast-components";
  * Sidebar Collapsible component with default padding and separator
  *
  * @param {object} props The properties for the component.
+ *
+ * @return {ReactElement} The Collapsible component.
  */
 const SidebarCollapsible = ( props ) => {
 	return <Collapsible hasPadding={true} hasSeparator={true} {...props} />;
