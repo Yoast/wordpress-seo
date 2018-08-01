a3fd443d8f199cd3fe3d37939720f5d6c06f624c
Jip Moors <jhp.moors@gmail.com>
2018-08-01 15:37:12 +0200

    Fix coding standards

--- js/src/components/SidebarCollapsible.js
+++ js/src/components/SidebarCollapsible.js
@@ -4,12 +4,12 @@ import { Collapsible } from "yoast-components";
 /**
  * Sidebar Collapsible component with default padding and separator
  *
- * @param {object} props The properties for the component.
+ * @param {Object} props The properties for the component.
  *
  * @returns {ReactElement} The Collapsible component.
  */
 const SidebarCollapsible = ( props ) => {
-	return <Collapsible hasPadding={true} hasSeparator={true} {...props} />;
+	return <Collapsible hasPadding={ true } hasSeparator={ true } { ...props } />;
 };
 
 export default SidebarCollapsible;
