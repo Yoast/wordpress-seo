85bc538164d19c332ee5eac2fb8cf028cb0ee224
Anton Timmermans <email@atimmer.com>
2018-08-01 11:47:57 +0200

    Deconstruct for awesomeness

--- js/src/helpers/classicEditor.js
+++ js/src/helpers/classicEditor.js
@@ -49,8 +49,8 @@ class RegisteredComponentsContainer extends Component {
 	 * @returns {React.Element[]} The rendered components in an array.
 	 */
 	render() {
-		return this.state.registeredComponents.map( ( registered ) => {
-			return <registered.Component key={ registered.key } />;
+		return this.state.registeredComponents.map( ( { Components, key } ) => {
+			return <Component key={ key } />;
 		} );
 	}
 }
