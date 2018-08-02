cbf577749a38868ae55b8949ba5c247498891a27
Alexander Botteram <a.botteram@gmail.com>
2018-08-02 10:47:37 +0200

    Fixed tests

--- tests/admin/test-class-admin.php
+++ tests/admin/test-class-admin.php
@@ -22,9 +22,10 @@ class WPSEO_Admin_Test extends WPSEO_UnitTestCase {
 		$class_instance = new WPSEO_Admin();
 
 		$admin_features = array(
-			'google_search_console' => new WPSEO_GSC(),
-			'primary_category'      => new WPSEO_Primary_Term_Admin(),
-			'dashboard_widget'      => new Yoast_Dashboard_Widget(),
+			'google_search_console'  => new WPSEO_GSC(),
+			'primary_category'       => new WPSEO_Primary_Term_Admin(),
+			'dashboard_widget'       => new Yoast_Dashboard_Widget(),
+			'structured-data-blocks' => new WPSEO_Structured_Data_Blocks(),
 		);
 
 		$this->assertEquals( $admin_features, $class_instance->get_admin_features() );
