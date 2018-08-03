67741805b99866e01dd28d9a68aad50124ef4366
Jip <jhp.moors@gmail.com>
2018-08-03 16:16:03 +0200

    Use different shortlink

--- admin/metabox/class-metabox.php
+++ admin/metabox/class-metabox.php
@@ -418,7 +418,7 @@ class WPSEO_Metabox extends WPSEO_Meta {
 	private function get_buy_premium_link() {
 		return sprintf( '<div class="%1$s"><a target="_blank" rel="noopener noreferrer" href="%2$s" class="wpseo-meta-section-link"><span class="dashicons dashicons-star-filled wpseo-buy-premium"></span>%3$s</a></div>',
 			'wpseo-metabox-buy-premium',
-			esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/add-keywords-popup' ) ),
+			esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/pe-premium-page' ) ),
 			__( 'Go Premium', 'wordpress-seo' )
 		);
 	}
