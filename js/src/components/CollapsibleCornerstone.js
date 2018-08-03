dde928d8e781bf5bf32785c81f72dc68a00a79d3
Andrea Fercia <a.fercia@gmail.com>
2018-08-03 17:52:43 +0200

    Improve cornerstone collapsible l10n and a11y.

--- js/src/components/CollapsibleCornerstone.js
+++ js/src/components/CollapsibleCornerstone.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { __, sprintf } from "@wordpress/i18n";
+import { utils } from "yoast-components";
 
 import Collapsible from "./SidebarCollapsible";
 import CornerstoneToggle from "yoast-components/composites/Plugin/CornerstoneContent/components/CornerstoneToggle";
@@ -12,13 +13,17 @@ import CornerstoneToggle from "yoast-components/composites/Plugin/CornerstoneCon
  * @constructor
  */
 export default function CollapsibleCornerstone( { isCornerstone, onChange, postTypeName } ) {
+	const { makeOutboundLink } = utils;
+	const LearnMoreLink = makeOutboundLink();
+
 	return (
-		<Collapsible title="Cornerstone content">
-			<p> { sprintf(
-				__( "Mark the most important %1$s as 'cornerstone content' to improve your site structure. ", "wordpress-seo" ),
+		<Collapsible title={ __( "Cornerstone content", "wordpress-seo" ) }>
+			<p>{ sprintf(
+				__( "Mark the most important %1$s as 'cornerstone content' to improve your site structure.", "wordpress-seo" ),
 				postTypeName.toLowerCase()
-			) }
-				<a href='https://yoa.st/1i9' target="_blank"> { __( "Learn more about cornerstone content.", "wordpress-seo" ) } </a>
+			) } <LearnMoreLink href={ "https://yoa.st/1i9" } rel={ null }>
+					{ __( "Learn more about cornerstone content.", "wordpress-seo" ) }
+				</LearnMoreLink>
 			</p>
 			<CornerstoneToggle
 				isEnabled={ isCornerstone }
