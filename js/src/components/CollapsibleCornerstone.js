8b6b779220abe888669d8f89a686af7d7f8718e8
Dieter <gargamelk@hotmail.com>
2018-08-06 10:43:50 +0200

    Changed copy, removed extra code to have access to post type

--- js/src/components/CollapsibleCornerstone.js
+++ js/src/components/CollapsibleCornerstone.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { __, sprintf } from "@wordpress/i18n";
+import { __ } from "@wordpress/i18n";
 
 import Collapsible from "./SidebarCollapsible";
 import CornerstoneToggle from "yoast-components/composites/Plugin/CornerstoneContent/components/CornerstoneToggle";
@@ -11,13 +11,10 @@ import CornerstoneToggle from "yoast-components/composites/Plugin/CornerstoneCon
  * @returns {ReactElement} The collapsible cornerstone toggle component.
  * @constructor
  */
-export default function CollapsibleCornerstone( { isCornerstone, onChange, postTypeName } ) {
+export default function CollapsibleCornerstone( { isCornerstone, onChange } ) {
 	return (
 		<Collapsible title="Cornerstone content">
-			<p> { sprintf(
-				__( "Mark the most important %1$s as 'cornerstone content' to improve your site structure. ", "wordpress-seo" ),
-				postTypeName.toLowerCase()
-			) }
+			<p> { __( "Cornerstone content should be the most important and extensive articles on your site. ", "wordpress-seo" ) }
 				<a href='https://yoa.st/1i9' target="_blank"> { __( "Learn more about cornerstone content.", "wordpress-seo" ) } </a>
 			</p>
 			<CornerstoneToggle
@@ -31,5 +28,4 @@ export default function CollapsibleCornerstone( { isCornerstone, onChange, postT
 CollapsibleCornerstone.propTypes = {
 	isCornerstone: PropTypes.bool,
 	onChange: PropTypes.func,
-	postTypeName: PropTypes.string,
 };
