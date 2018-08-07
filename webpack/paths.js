5277b0b3389157323bc7678c147e1d2f8fceeb04
dutchmartin <martijng@yoast.com>
2018-08-07 16:41:00 +0200

    Make yoast components available.

--- webpack/paths.js
+++ webpack/paths.js
@@ -23,6 +23,7 @@ const entry = {
 	"wp-seo-replacevar-plugin": "./wp-seo-replacevar-plugin.js",
 	"wp-seo-structured-data-blocks": "./wp-seo-structured-data-blocks.js",
 	analysis: "./analysis.js",
+	components: "./components.js",
 
 	"wp-seo-admin-global": "./wp-seo-admin-global.js",
 	"wp-seo-admin-gsc": "./wp-seo-admin-gsc.js",
