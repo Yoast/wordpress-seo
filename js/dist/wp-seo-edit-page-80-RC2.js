6292c3a13c3d4a92d04391934698b97902dd337d
Dieter <gargamelk@hotmail.com>
2018-08-07 14:41:22 +0200

    Version bump for RC2

---
+++ js/dist/wp-seo-edit-page-80-RC2.js
@@ -0,0 +1,26 @@
+yoastWebpackJsonp([24],{
+
+/***/ 2345:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+(function ($) {
+	// Set the yoast-tooltips on the list table links columns that have links.
+	$(".yoast-column-header-has-tooltip").each(function () {
+		var parentLink = $(this).closest("th").find("a");
+
+		parentLink.addClass("yoast-tooltip yoast-tooltip-n yoast-tooltip-multiline").attr("aria-label", $(this).data("label"));
+	});
+
+	// Clean up the columns titles HTML for the Screen Options checkboxes labels.
+	$(".yoast-column-header-has-tooltip, .yoast-tooltip", "#screen-meta").each(function () {
+		var text = $(this).text();
+		$(this).replaceWith(text);
+	});
+})(jQuery);
+
+/***/ })
+
+},[2345]);
