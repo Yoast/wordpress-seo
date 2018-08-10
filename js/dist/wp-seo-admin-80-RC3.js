eb584ec143e829f98d11879b3fe27fdeac7eae46
Jimmy Comack <jimmy@yoast.com>
2018-08-10 13:07:55 +0200

    Version bump for RC3

---
+++ js/dist/wp-seo-admin-80-RC3.js
@@ -0,0 +1,423 @@
+yoastWebpackJsonp([12],{
+
+/***/ 100:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+// The babel polyfill sets the _babelPolyfill to true. So only load it ourselves if the variable is undefined or false.
+if (typeof window._babelPolyfill === "undefined" || !window._babelPolyfill) {
+	// eslint-disable-next-line global-require
+	__webpack_require__(248);
+}
+
+/***/ }),
+
+/***/ 1053:
+/***/ (function(module, exports) {
+
+var containerPolite, containerAssertive, previousMessage = "";
+
+/**
+ * Build the live regions markup.
+ *
+ * @param {String} ariaLive Optional. Value for the "aria-live" attribute, default "polite".
+ *
+ * @returns {Object} $container The ARIA live region jQuery object.
+ */
+var addContainer = function( ariaLive ) {
+	ariaLive = ariaLive || "polite";
+
+	var container = document.createElement( "div" );
+	container.id = "a11y-speak-" + ariaLive;
+	container.className = "a11y-speak-region";
+
+	var screenReaderTextStyle = "clip: rect(1px, 1px, 1px, 1px); position: absolute; height: 1px; width: 1px; overflow: hidden; word-wrap: normal;";
+	container.setAttribute( "style", screenReaderTextStyle );
+
+	container.setAttribute( "aria-live", ariaLive );
+	container.setAttribute( "aria-relevant", "additions text" );
+	container.setAttribute( "aria-atomic", "true" );
+
+	document.querySelector( "body" ).appendChild( container );
+	return container;
+};
+
+/**
+ * Specify a function to execute when the DOM is fully loaded.
+ *
+ * @param {Function} callback A function to execute after the DOM is ready.
+ *
+ * @returns {void}
+ */
+var domReady = function( callback ) {
+	if ( document.readyState === "complete" || ( document.readyState !== "loading" && !document.documentElement.doScroll ) ) {
+		return callback();
+	}
+
+	document.addEventListener( "DOMContentLoaded", callback );
+};
+
+/**
+ * Create the live regions when the DOM is fully loaded.
+ */
+domReady( function() {
+	containerPolite = document.getElementById( "a11y-speak-polite" );
+	containerAssertive = document.getElementById( "a11y-speak-assertive" );
+
+	if ( containerPolite === null ) {
+		containerPolite = addContainer( "polite" );
+	}
+	if ( containerAssertive === null ) {
+		containerAssertive = addContainer( "assertive" );
+	}
+} );
+
+/**
+ * Clear the live regions.
+ */
+var clear = function() {
+	var regions = document.querySelectorAll( ".a11y-speak-region" );
+	for ( var i = 0; i < regions.length; i++ ) {
+		regions[ i ].textContent = "";
+	}
+};
+
+/**
+ * Update the ARIA live notification area text node.
+ *
+ * @param {String} message  The message to be announced by Assistive Technologies.
+ * @param {String} ariaLive Optional. The politeness level for aria-live. Possible values:
+ *                          polite or assertive. Default polite.
+ */
+var A11ySpeak = function( message, ariaLive ) {
+	// Clear previous messages to allow repeated strings being read out.
+	clear();
+
+	/*
+	 * Strip HTML tags (if any) from the message string. Ideally, messages should
+	 * be simple strings, carefully crafted for specific use with A11ySpeak.
+	 * When re-using already existing strings this will ensure simple HTML to be
+	 * stripped out and replaced with a space. Browsers will collapse multiple
+	 * spaces natively.
+	 */
+	message = message.replace( /<[^<>]+>/g, " " );
+
+	if ( previousMessage === message ) {
+		message = message + "\u00A0";
+	}
+
+	previousMessage = message;
+
+	if ( containerAssertive && "assertive" === ariaLive ) {
+		containerAssertive.textContent = message;
+	} else if ( containerPolite ) {
+		containerPolite.textContent = message;
+	}
+};
+
+module.exports = A11ySpeak;
+
+
+/***/ }),
+
+/***/ 1928:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(100);
+
+var _a11ySpeak = __webpack_require__(1053);
+
+var _a11ySpeak2 = _interopRequireDefault(_a11ySpeak);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/* global wpseoAdminGlobalL10n, ajaxurl, wpseoSelect2Locale */
+
+(function () {
+	"use strict";
+
+	/**
+  * Detects the wrong use of variables in title and description templates
+  *
+  * @param {element} e The element to verify.
+  *
+  * @returns {void}
+  */
+
+	function wpseoDetectWrongVariables(e) {
+		var warn = false;
+		var errorId = "";
+		var wrongVariables = [];
+		var authorVariables = ["userid", "name", "user_description"];
+		var dateVariables = ["date"];
+		var postVariables = ["title", "parent_title", "excerpt", "excerpt_only", "caption", "focuskw", "pt_single", "pt_plural", "modified", "id"];
+		var specialVariables = ["term404", "searchphrase"];
+		var taxonomyVariables = ["term_title", "term_description"];
+		var taxonomyPostVariables = ["category", "category_description", "tag", "tag_description"];
+		if (e.hasClass("posttype-template")) {
+			wrongVariables = wrongVariables.concat(specialVariables, taxonomyVariables);
+		} else if (e.hasClass("homepage-template")) {
+			wrongVariables = wrongVariables.concat(authorVariables, dateVariables, postVariables, specialVariables, taxonomyVariables, taxonomyPostVariables);
+		} else if (e.hasClass("taxonomy-template")) {
+			wrongVariables = wrongVariables.concat(authorVariables, dateVariables, postVariables, specialVariables);
+		} else if (e.hasClass("author-template")) {
+			wrongVariables = wrongVariables.concat(postVariables, dateVariables, specialVariables, taxonomyVariables, taxonomyPostVariables);
+		} else if (e.hasClass("date-template")) {
+			wrongVariables = wrongVariables.concat(authorVariables, postVariables, specialVariables, taxonomyVariables, taxonomyPostVariables);
+		} else if (e.hasClass("search-template")) {
+			wrongVariables = wrongVariables.concat(authorVariables, dateVariables, postVariables, taxonomyVariables, taxonomyPostVariables, ["term404"]);
+		} else if (e.hasClass("error404-template")) {
+			wrongVariables = wrongVariables.concat(authorVariables, dateVariables, postVariables, taxonomyVariables, taxonomyPostVariables, ["searchphrase"]);
+		}
+		jQuery.each(wrongVariables, function (index, variable) {
+			errorId = e.attr("id") + "-" + variable + "-warning";
+			if (e.val().search("%%" + variable + "%%") !== -1) {
+				e.addClass("wpseo-variable-warning-element");
+				var msg = wpseoAdminGlobalL10n.variable_warning.replace("%s", "%%" + variable + "%%");
+				if (jQuery("#" + errorId).length) {
+					jQuery("#" + errorId).html(msg);
+				} else {
+					e.after(' <div id="' + errorId + '" class="wpseo-variable-warning">' + msg + "</div>");
+				}
+
+				(0, _a11ySpeak2.default)(wpseoAdminGlobalL10n.variable_warning.replace("%s", variable), "assertive");
+
+				warn = true;
+			} else {
+				if (jQuery("#" + errorId).length) {
+					jQuery("#" + errorId).remove();
+				}
+			}
+		});
+		if (warn === false) {
+			e.removeClass("wpseo-variable-warning-element");
+		}
+	}
+
+	/**
+  * Sets a specific WP option
+  *
+  * @param {string} option The option to update.
+  * @param {string} newval The new value for the option.
+  * @param {string} hide   The ID of the element to hide on success.
+  * @param {string} nonce  The nonce for the action.
+  *
+  * @returns {void}
+  */
+	function setWPOption(option, newval, hide, nonce) {
+		jQuery.post(ajaxurl, {
+			action: "wpseo_set_option",
+			option: option,
+			newval: newval,
+			_wpnonce: nonce
+		}, function (data) {
+			if (data) {
+				jQuery("#" + hide).hide();
+			}
+		});
+	}
+
+	/**
+  * Copies the meta description for the homepage.
+  *
+  * @returns {void}
+  */
+	function wpseoCopyHomeMeta() {
+		jQuery("#copy-home-meta-description").on("click", function () {
+			jQuery("#og_frontpage_desc").val(jQuery("#meta_description").val());
+		});
+	}
+
+	/**
+  * Makes sure we store the action hash so we can return to the right hash
+  *
+  * @returns {void}
+  */
+	function wpseoSetTabHash() {
+		var conf = jQuery("#wpseo-conf");
+		if (conf.length) {
+			var currentUrl = conf.attr("action").split("#")[0];
+			conf.attr("action", currentUrl + window.location.hash);
+		}
+	}
+
+	/**
+  * When the hash changes, get the base url from the action and then add the current hash
+  */
+	jQuery(window).on("hashchange", wpseoSetTabHash);
+
+	/**
+  * Adds select2 for selected fields.
+  *
+  * @returns {void}
+  */
+	function initSelect2() {
+		var select2Width = "400px";
+
+		// Select2 for General settings: your info: company or person. Width is the same as the width for the other fields on this page.
+		jQuery("#company_or_person").select2({
+			width: select2Width,
+			language: wpseoSelect2Locale
+		});
+
+		// Select2 for Twitter card meta data in Settings
+		jQuery("#twitter_card_type").select2({
+			width: select2Width,
+			language: wpseoSelect2Locale
+		});
+
+		// Select2 for taxonomy breadcrumbs in Advanced
+		jQuery("#breadcrumbs select").select2({
+			width: select2Width,
+			language: wpseoSelect2Locale
+		});
+
+		// Select2 for profile in Search Console
+		jQuery("#profile").select2({
+			width: select2Width,
+			language: wpseoSelect2Locale
+		});
+	}
+
+	/**
+  * Set the initial active tab in the settings pages.
+  *
+  * @returns {void}
+  */
+	function setInitialActiveTab() {
+		var activeTabId = window.location.hash.replace("#top#", "");
+		/* In some cases, the second # gets replace by %23, which makes the tab
+   * switching not work unless we do this. */
+		if (activeTabId.search("#top") !== -1) {
+			activeTabId = window.location.hash.replace("#top%23", "");
+		}
+		/*
+   * WordPress uses fragment identifiers for its own in-page links, e.g.
+   * `#wpbody-content` and other plugins may do that as well. Also, facebook
+   * adds a `#_=_` see PR 506. In these cases and when it's empty, default
+   * to the first tab.
+   */
+		if ("" === activeTabId || "#" === activeTabId.charAt(0)) {
+			/*
+    * Reminder: jQuery attr() gets the attribute value for only the first
+    * element in the matched set so this will always be the first tab id.
+    */
+			activeTabId = jQuery(".wpseotab").attr("id");
+		}
+
+		jQuery("#" + activeTabId).addClass("active");
+		jQuery("#" + activeTabId + "-tab").addClass("nav-tab-active").click();
+	}
+
+	window.wpseoDetectWrongVariables = wpseoDetectWrongVariables;
+	window.setWPOption = setWPOption;
+	window.wpseoCopyHomeMeta = wpseoCopyHomeMeta;
+	// eslint-disable-next-line
+	window.wpseoSetTabHash = wpseoSetTabHash;
+
+	jQuery(document).ready(function () {
+		/**
+   * When the hash changes, get the base url from the action and then add the current hash.
+   */
+		wpseoSetTabHash();
+
+		// Toggle the Author archives section.
+		jQuery("#disable-author input[type='radio']").change(function () {
+			// The value on is disabled, off is enabled.
+			if (jQuery(this).is(":checked")) {
+				jQuery("#author-archives-titles-metas-content").toggle(jQuery(this).val() === "off");
+			}
+		}).change();
+
+		// Toggle the Date archives section.
+		jQuery("#disable-date input[type='radio']").change(function () {
+			// The value on is disabled, off is enabled.
+			if (jQuery(this).is(":checked")) {
+				jQuery("#date-archives-titles-metas-content").toggle(jQuery(this).val() === "off");
+			}
+		}).change();
+
+		// Toggle the Media section.
+		jQuery("#disable-attachment input[type='radio']").change(function () {
+			// The value on is disabled, off is enabled.
+			if (jQuery(this).is(":checked")) {
+				jQuery("#media_settings").toggle(jQuery(this).val() === "off");
+			}
+		}).change();
+
+		// Toggle the Format-based archives section.
+		jQuery("#disable-post_format").change(function () {
+			jQuery("#post_format-titles-metas").toggle(jQuery(this).is(":not(:checked)"));
+		}).change();
+
+		// Toggle the Breadcrumbs section.
+		jQuery("#breadcrumbs-enable").change(function () {
+			jQuery("#breadcrumbsinfo").toggle(jQuery(this).is(":checked"));
+		}).change();
+
+		// Handle the settings pages tabs.
+		jQuery("#wpseo-tabs").find("a").click(function () {
+			jQuery("#wpseo-tabs").find("a").removeClass("nav-tab-active");
+			jQuery(".wpseotab").removeClass("active");
+
+			var id = jQuery(this).attr("id").replace("-tab", "");
+			var activeTab = jQuery("#" + id);
+			activeTab.addClass("active");
+			jQuery(this).addClass("nav-tab-active");
+			if (activeTab.hasClass("nosave")) {
+				jQuery("#submit").hide();
+			} else {
+				jQuery("#submit").show();
+			}
+		});
+
+		// Handle the Company or Person select.
+		jQuery("#company_or_person").change(function () {
+			var companyOrPerson = jQuery(this).val();
+			if ("company" === companyOrPerson) {
+				jQuery("#knowledge-graph-company").show();
+				jQuery("#knowledge-graph-person").hide();
+			} else if ("person" === companyOrPerson) {
+				jQuery("#knowledge-graph-company").hide();
+				jQuery("#knowledge-graph-person").show();
+			} else {
+				jQuery("#knowledge-graph-company").hide();
+				jQuery("#knowledge-graph-person").hide();
+			}
+		}).change();
+
+		// Check correct variables usage in title and description templates.
+		jQuery(".template").on("input", function () {
+			wpseoDetectWrongVariables(jQuery(this));
+		});
+
+		// Prevent form submission when pressing Enter on the switch-toggles.
+		jQuery(".switch-yoast-seo input").on("keydown", function (event) {
+			if ("keydown" === event.type && 13 === event.which) {
+				event.preventDefault();
+			}
+		});
+
+		// Allow collapsing of the content types sections.
+		jQuery("body").on("click", "button.toggleable-container-trigger", function (event) {
+			var target = jQuery(event.currentTarget);
+			var toggleableContainer = target.parent().siblings(".toggleable-container");
+
+			toggleableContainer.toggleClass("toggleable-container-hidden");
+			target.attr("aria-expanded", !toggleableContainer.hasClass("toggleable-container-hidden")).find("span").toggleClass("dashicons-arrow-up-alt2 dashicons-arrow-down-alt2");
+		});
+
+		wpseoCopyHomeMeta();
+		setInitialActiveTab();
+		initSelect2();
+	});
+})();
+
+/***/ })
+
+},[1928]);
