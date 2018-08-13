21d8b2f40ce297daf7d4799d297df7ea03f22a70
dutchmartin <martijng@yoast.com>
2018-08-13 16:00:53 +0200

    Rebuild branch of pr #10611.

--- js/src/components/contentAnalysis/SeoAnalysis.js
+++ js/src/components/contentAnalysis/SeoAnalysis.js
@@ -15,6 +15,7 @@ import ModalButtonContainer from "../ModalButtonContainer";
 import getIndicatorForScore from "../../analysis/getIndicatorForScore";
 import { getIconForScore } from "./mapResults";
 import { utils } from "yoast-components";
+import isNil from "lodash/isNil";
 
 const AnalysisHeader = styled.span`
 	font-size: 1em;
@@ -107,6 +108,10 @@ class SeoAnalysis extends React.Component {
 		if( this.props.keyword === "" ) {
 			score.className = "na";
 			score.screenReaderReadabilityText = __( "Enter a focus keyword to calculate the SEO score", "wordpress-seo" );
+		}
+
+		if ( isNil( this.props.overallScore ) ) {
+			score.className = "loading";
 		}
 
 		return (
