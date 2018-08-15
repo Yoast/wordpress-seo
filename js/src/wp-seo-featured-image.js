edb005d8e3ab1860150f1ba3f88e2d5cf27e4780
Igor <igor@yoast.com>
2018-08-15 11:08:27 +0200

    Undo featured image app change

--- js/src/wp-seo-featured-image.js
+++ js/src/wp-seo-featured-image.js
@@ -13,8 +13,8 @@ import "./helpers/babel-polyfill";
 	var $postImageDiv;
 	var $postImageDivHeading;
 
-	var FeaturedImagePlugin = function() {
-		this._app = YoastSEO.app;
+	var FeaturedImagePlugin = function( app ) {
+		this._app = app;
 
 		this.featuredImage = null;
 		this.pluginName = "addFeaturedImagePlugin";
