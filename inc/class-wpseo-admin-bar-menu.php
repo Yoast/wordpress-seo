57fe9ad78fb82e24234cdd77ee52dfa0b3476248
Andy Meerwaldt <andy@yoast.com>
2018-08-17 11:00:09 +0200

    Check if properties exists instead of forcing a specific object

--- inc/class-wpseo-admin-bar-menu.php
+++ inc/class-wpseo-admin-bar-menu.php
@@ -479,7 +479,12 @@ class WPSEO_Admin_Bar_Menu implements WPSEO_WordPress_Integration {
 	 *
 	 * @return string Score markup, or empty string if none available.
 	 */
-	protected function get_post_score( WP_Post $post ) {
+	protected function get_post_score( $post ) {
+		if ( ! is_object( $post ) || ! property_exists( $post, 'ID' ) ) {
+			return '';
+		}
+
+
 		if ( apply_filters( 'wpseo_use_page_analysis', true ) !== true ) {
 			return '';
 		}
@@ -528,7 +533,11 @@ class WPSEO_Admin_Bar_Menu implements WPSEO_WordPress_Integration {
 	 *
 	 * @return string Score markup, or empty string if none available.
 	 */
-	protected function get_term_score( WP_Term $term ) {
+	protected function get_term_score( $term ) {
+		if ( ! is_object( $term ) || ! property_exists( $term, 'term_id' ) || property_exists( $term, 'taxonomy' ) ) {
+			return '';
+		}
+
 		$analysis_seo         = new WPSEO_Metabox_Analysis_SEO();
 		$analysis_readability = new WPSEO_Metabox_Analysis_Readability();
 
