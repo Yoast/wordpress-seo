9b8518814501d2ccfd719d05e49822d77cc24560
Herre Groen <herre@yoast.com>
2018-08-17 15:53:14 +0200

    Transfer previously used keywords assessment to webworker.

---
+++ js/src/wp-seo-used-keywords-assessment.js
@@ -0,0 +1,34 @@
+/* global analysisWorker */
+import { bundledPlugins } from "yoastseo";
+const UsedKeywordsPlugin = bundledPlugins.usedKeywords;
+
+class UsedKeywordsAssessment {
+	constructor() {
+		this._initialized = false;
+	}
+
+	register() {
+		analysisWorker.registerMessageHandler( "updateKeywordUsage", this.updateKeywordUsage.bind( this ), "used-keywords-assessment" );
+		analysisWorker.registerMessageHandler( "initialize", this.initialize.bind( this ), "used-keywords-assessment" );
+	}
+
+	initialize( options ) {
+		this._plugin = new UsedKeywordsPlugin( analysisWorker, options );
+		this._plugin.registerPlugin();
+		this._initialized = true;
+	}
+
+	updateKeywordUsage( keywordUsage ) {
+		if ( ! this._initialized ) {
+			throw new Error( "UsedKeywordsAssessment must be initialized before keywords can be updated." );
+		}
+
+		// Clear worker paper cache to force new analysis.
+		analysisWorker.clearCache();
+		this._plugin.updateKeywordUsage( keywordUsage );
+	}
+}
+
+const keywordsAssessment = new UsedKeywordsAssessment();
+
+keywordsAssessment.register();
