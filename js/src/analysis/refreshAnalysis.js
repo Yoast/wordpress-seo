9720ffa3aa728d3f8e1c4f8e9c87c65c7f2f6ccf
Igor <igor@yoast.com>
2018-08-21 15:07:18 +0200

    Adapt to renaming

--- js/src/analysis/refreshAnalysis.js
+++ js/src/analysis/refreshAnalysis.js
@@ -8,21 +8,21 @@ import {
 /**
  * Refreshes the analysis.
  *
- * @param {AnalysisWorkerWrapper} analysisWorker      The analysis worker to
- *                                                    request the analysis from.
- * @param {Function}              collectAnalysisData Function that collects the
- *                                                    analysis data.
- * @param {Function}              applyMarks          Function that applies the
- *                                                    marks in the content.
- * @param {Object}                store               The store.
+ * @param {AnalysisWorkerWrapper} worker      The analysis worker to request the
+ *                                            analysis from.
+ * @param {Function}              collectData Function that collects the
+ *                                            analysis data.
+ * @param {Function}              applyMarks  Function that applies the marks in
+ *                                            the content.
+ * @param {Object}                store       The store.
  *
  * @returns {void}
  */
-export default function refreshAnalysis( analysisWorker, collectAnalysisData, applyMarks, store ) {
-	const data = collectAnalysisData();
+export default function refreshAnalysis( worker, collectData, applyMarks, store ) {
+	const data = collectData();
 	const paper = Paper.parse( data );
 
-	analysisWorker.analyze( data )
+	worker.analyze( data )
 		.then( ( { result: { seo, readability } } ) => {
 			if ( seo ) {
 				// Only update the main results, which are located under the empty string key.
