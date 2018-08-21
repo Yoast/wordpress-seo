f25439f79b363baea14ff53bd16dc7851ed7ce5e
Anton Timmermans <email@atimmer.com>
2018-08-21 13:16:24 +0200

    Fix whitespace

--- admin/metabox/class-metabox.php
+++ admin/metabox/class-metabox.php
@@ -791,9 +791,9 @@ class WPSEO_Metabox extends WPSEO_Meta {
 		$analysis_worker_location          = new WPSEO_Admin_Asset_Analysis_Worker_Location( $asset_manager->flatten_version( WPSEO_VERSION ) );
 		$used_keywords_assessment_location = new WPSEO_Admin_Asset_Analysis_Worker_Location( $asset_manager->flatten_version( WPSEO_VERSION ), 'used-keywords-assessment' );
 		wp_localize_script( WPSEO_Admin_Asset_Manager::PREFIX . 'post-scraper', 'wpseoAnalysisWorkerL10n', array(
-		    'url'                     => $analysis_worker_location->get_url(),
-            'keywords_assessment_url' => $used_keywords_assessment_location->get_url(),
-        ) );
+			'url'                     => $analysis_worker_location->get_url(),
+			'keywords_assessment_url' => $used_keywords_assessment_location->get_url(),
+		) );
 
 		/**
 		 * Remove the emoji script as it is incompatible with both React and any
