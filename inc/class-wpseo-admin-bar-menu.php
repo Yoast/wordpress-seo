92c66970421b4918ec2b9f45b8de8262aeeb91c9
Caroline Geven <caroline@1234s-MacBook-Pro.local>
2018-08-24 08:49:01 +0200

    Change moz link to correct one

--- inc/class-wpseo-admin-bar-menu.php
+++ inc/class-wpseo-admin-bar-menu.php
@@ -268,7 +268,7 @@ class WPSEO_Admin_Bar_Menu implements WPSEO_WordPress_Integration {
 			'parent' => self::ANALYSIS_SUBMENU_IDENTIFIER,
 			'id'     => 'wpseo-inlinks-ose',
 			'title'  => __( 'Check Inlinks (OSE)', 'wordpress-seo' ),
-			'href'   => '//moz.com/researchtools/ose/links?site=' . urlencode( $url ),
+			'href'   => 'https://moz.com/link-explorer',
 			'meta'   => array( 'target' => '_blank' ),
 		) );
 		$wp_admin_bar->add_menu( array(
