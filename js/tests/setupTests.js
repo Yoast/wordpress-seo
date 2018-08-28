dea19038ac54a92b1000e8437d49395da1eae9aa
Alexander Botteram <a.botteram@gmail.com>
2018-08-28 13:51:36 +0200

    Added global mock for @wordpress/i18n

--- js/tests/setupTests.js
+++ js/tests/setupTests.js
@@ -3,3 +3,10 @@ import Enzyme from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
 
 Enzyme.configure( { adapter: new EnzymeAdapter() } );
+
+jest.mock( "@wordpress/i18n", () => {
+	return {
+		__: translation => translation,
+	};
+} );
+
