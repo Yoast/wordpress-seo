3fbf8c29b4758d1acf9ff82a10b79e2bce226348
Anton Timmermans <email@atimmer.com>
2018-08-29 14:15:12 +0200

    Build dist from src folder

--- grunt/config/clean.js
+++ grunt/config/clean.js
@@ -1,4 +1,7 @@
 // See https://github.com/gruntjs/grunt-contrib-clean
 module.exports = {
 	build: [ "js/dist/*.min.js", "!<%= paths.js %>/dist/yoast-seo-content-analysis.min.js" ],
+	publish: [
+		"dist",
+	],
 };
