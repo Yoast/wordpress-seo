7a9ec8ad0f43eedd926e715e5e6eefc3890e890b
Alexander Botteram <a.botteram@gmail.com>
2018-08-29 11:04:16 +0200

    Added locale data for i18n in jest

--- js/tests/setupTests.js
+++ js/tests/setupTests.js
@@ -1,12 +1,15 @@
 import "raf/polyfill";
 import Enzyme from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
+import { setLocaleData } from "@wordpress/i18n";
 
 Enzyme.configure( { adapter: new EnzymeAdapter() } );
 
-jest.mock( "@wordpress/i18n", () => {
-	return {
-		__: translation => translation,
-	};
-} );
-
+/* eslint-disable */
+setLocaleData( {
+	"": {
+		domain: "wordpress-seo",
+		lang: "en",
+		plural_forms: "nplurals=2; plural=(n != 1);",
+	},
+}, "wordpress-seo" );
