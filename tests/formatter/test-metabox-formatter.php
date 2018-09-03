ad5771b2c8b08411e696778844dd13eefd12877c
Jip Moors <jhp.moors@gmail.com>
2018-09-03 16:20:31 +0200

    Move the fake language file to the tests/assets directory
    As the language folder has been removed from the repo, this no longer works in Travis.

--- tests/formatter/test-metabox-formatter.php
+++ tests/formatter/test-metabox-formatter.php
@@ -44,7 +44,7 @@ class WPSEO_Metabox_Formatter_Test extends WPSEO_UnitTestCase {
 	 * @covers WPSEO_Metabox_Formatter::get_translations
 	 */
 	public function test_with_fake_language_file() {
-		$file_name = plugin_dir_path( WPSEO_FILE ) . 'languages/wordpress-seo-' . WPSEO_Utils::get_user_locale() . '.json';
+		$file_name = plugin_dir_path( WPSEO_FILE ) . 'tests/assets/wordpress-seo-' . WPSEO_Utils::get_user_locale() . '.json';
 		file_put_contents(
 			$file_name,
 			json_encode( array( 'key' => 'value' ) )
