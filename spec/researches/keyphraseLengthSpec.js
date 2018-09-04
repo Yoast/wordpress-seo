561e317cfc7b282a4de41e228e52f148042af60a
Natalia <natalia@yoast.com>
2018-09-04 17:35:55 +0200

    Add specs

--- spec/researches/keyphraseLengthSpec.js
+++ spec/researches/keyphraseLengthSpec.js
@@ -17,6 +17,30 @@ describe( "the keyphrase length research", function() {
 
 describe( "the keyphrase length research", function() {
 	it( "should count the words in the input", function() {
+		const paper = new Paper( "", { keyword: "word word the word" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchDataProvider( "morphology", morphologyData );
+
+		const result = keyphraseLength( paper, researcher );
+
+		expect( result ).toBe( 3 );
+	} );
+} );
+
+describe( "the keyphrase length research", function() {
+	it( "should count the words in the input", function() {
+		const paper = new Paper( "", { keyword: "mot mot le mot", locale: "fr_FR" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchDataProvider( "morphology", morphologyData );
+
+		const result = keyphraseLength( paper, researcher );
+
+		expect( result ).toBe( 3 );
+	} );
+} );
+
+describe( "the keyphrase length research", function() {
+	it( "should count the words in the input", function() {
 		const paper = new Paper( "", { keyword: "" } );
 		const researcher = new Researcher( paper );
 		researcher.addResearchDataProvider( "morphology", morphologyData );
