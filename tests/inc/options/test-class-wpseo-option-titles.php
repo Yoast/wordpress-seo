4612c7bb42490feefebd4de8050561105dd1acc2
Sergey Biryukov <sergeybiryukov.ru@gmail.com>
2018-09-05 14:14:18 +0200

    Add a unit test for WPSEO_Option_Titles::enrich_defaults() cache invalidation

---
+++ tests/inc/options/test-class-wpseo-option-titles.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * WPSEO plugin test file.
+ *
+ * @package WPSEO\Tests\Inc\Options
+ */
+
+/**
+ * Unit Test Class.
+ */
+class WPSEO_Option_Titles_Test extends WPSEO_UnitTestCase {
+
+	/**
+	 * Tests if the enrich_defaults() cache is properly invalidated
+     * when a new post type or taxonomy is registered.
+	 *
+	 * @covers WPSEO_Option_Titles::enrich_defaults()
+	 */
+	public function test_enrich_defaults_cache_invalidation() {
+		$wpseo_option_titles = WPSEO_Option_Titles::get_instance();
+
+		register_post_type( 'custom-post-type', array( 'public' => true ) );
+		$this->assertArrayHasKey( 'title-custom-post-type', $wpseo_option_titles->get_defaults() );
+
+		register_taxonomy( 'custom-taxonomy', 'post' );
+		$this->assertArrayHasKey( 'title-tax-custom-taxonomy', $wpseo_option_titles->get_defaults() );
+
+		register_taxonomy_for_object_type( 'custom-taxonomy', 'custom-post-type' );
+		$this->assertArrayHasKey( 'post_types-custom-post-type-maintax', $wpseo_option_titles->get_defaults() );
+
+		unregister_taxonomy_for_object_type( 'custom-taxonomy', 'custom-post-type' );
+		unregister_taxonomy( 'custom-taxonomy' );
+		unregister_post_type( 'custom-post-type' );
+	}
+
+}
