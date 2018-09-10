2e04f65e54784175fd82b80c51584b6293a73806
Igor <igor@yoast.com>
2018-09-10 14:14:04 +0200

    Add assertion for name specification

--- tests/admin/test-class-admin-asset-analysis-worker-location.php
+++ tests/admin/test-class-admin-asset-analysis-worker-location.php
@@ -16,12 +16,16 @@ final class Test_WPSEO_Admin_Asset_Analysis_Worker_Location extends PHPUnit_Fram
 	 * @covers WPSEO_Admin_Asset_Analysis_Worker_Location::get_url()
 	 */
 	public function test_get_url() {
+		// Default name.
 		$expected_js = home_url() . '/wp-content/plugins/wordpress-seo/js/dist/wp-seo-analysis-worker-test.js';
+		$location    = new WPSEO_Admin_Asset_Analysis_Worker_Location( 'test' );
+		$actual_js   = $location->get_url( $location->get_asset(), WPSEO_Admin_Asset::TYPE_JS );
+		$this->assertEquals( $expected_js, $actual_js );
 
-		$location = new WPSEO_Admin_Asset_Analysis_Worker_Location( 'test' );
-
-		$actual_js = $location->get_url( $location->get_asset(), WPSEO_Admin_Asset::TYPE_JS );
-
+		// Specified name.
+		$expected_js = home_url() . '/wp-content/plugins/wordpress-seo/js/dist/wp-seo-something-else-version.js';
+		$location    = new WPSEO_Admin_Asset_Analysis_Worker_Location( 'version', 'something-else' );
+		$actual_js   = $location->get_url( $location->get_asset(), WPSEO_Admin_Asset::TYPE_JS );
 		$this->assertEquals( $expected_js, $actual_js );
 	}
 }
