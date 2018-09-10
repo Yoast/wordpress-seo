1f9b9dfce8347cc14ec21b9e9049d72d30593923
Igor <igor@yoast.com>
2018-09-10 14:18:13 +0200

    Split assertions to different functions

--- tests/admin/test-class-admin-asset-analysis-worker-location.php
+++ tests/admin/test-class-admin-asset-analysis-worker-location.php
@@ -21,8 +21,14 @@ final class Test_WPSEO_Admin_Asset_Analysis_Worker_Location extends PHPUnit_Fram
 		$location    = new WPSEO_Admin_Asset_Analysis_Worker_Location( 'test' );
 		$actual_js   = $location->get_url( $location->get_asset(), WPSEO_Admin_Asset::TYPE_JS );
 		$this->assertEquals( $expected_js, $actual_js );
+	}
 
-		// Specified name.
+	/**
+	 * Tests the get_url function when we pass a name.
+	 *
+	 * @covers WPSEO_Admin_Asset_Analysis_Worker_Location::get_url()
+	 */
+	public function test_get_url_with_name() {
 		$expected_js = home_url() . '/wp-content/plugins/wordpress-seo/js/dist/wp-seo-something-else-version.js';
 		$location    = new WPSEO_Admin_Asset_Analysis_Worker_Location( 'version', 'something-else' );
 		$actual_js   = $location->get_url( $location->get_asset(), WPSEO_Admin_Asset::TYPE_JS );
