677db3b888bc697ff066c070c6d481c4ac5f07af
Hans-Christiaan <hans-christiaan@yoast.com>
2018-09-11 16:42:53 +0200

    Refactored meta description to take morphology of keywords into account.

--- src/researches/metaDescriptionKeyword.js
+++ src/researches/metaDescriptionKeyword.js
@@ -1,19 +1,38 @@
-import matchTextWithWord from "../stringProcessing/matchTextWithWord.js";
-
-import { escapeRegExp } from "lodash-es";
+import matchTextWithArray from "../stringProcessing/matchTextWithArray";
+import getSentences from "../stringProcessing/getSentences";
 
 /**
  * Matches the keyword in the description if a description and keyword are available.
  * default is -1 if no description and/or keyword is specified
  *
  * @param {Paper} paper The paper object containing the description.
- * @returns {number} The number of matches with the keyword
+ * @param {Researcher} researcher
+ * @returns { { fullDescription: Number[], perSentence: Number[][] } } The number of matches per keyword, for the entire description and for each individual sentence.
  */
-export default function( paper ) {
+export default function( paper, researcher ) {
 	if ( paper.getDescription() === "" ) {
-		return -1;
+		return {
+			fullDescription: null,
+			perSentence: null
+		};
 	}
-	var keyword = escapeRegExp( paper.getKeyword() );
-	return matchTextWithWord( paper.getDescription(), keyword, paper.getLocale() ).count;
+	const description = paper.getDescription();
+	const locale = paper.getLocale();
+
+	const keyPhraseForms = researcher.getResearch( "morphology" ).keyphraseForms;
+
+	const sentences = getSentences( description );
+
+	let matchResults = { };
+
+	matchResults.fullDescription = keyPhraseForms.map(
+		keywordForms => matchTextWithArray( description, keywordForms, locale ).count
+	);
+
+	matchResults.perSentence = sentences.map( sentence =>
+		keyPhraseForms.map( keywordForms => matchTextWithArray( sentence, keywordForms, locale ).count )
+	);
+
+	return matchResults;
 }
 
