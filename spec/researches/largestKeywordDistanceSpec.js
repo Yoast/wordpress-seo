81a419dbca1b4cbb8d669ef2f7da6857bff1878d
Manuel <manuel@yoast.com>
2018-09-14 14:26:08 +0200

    Add tests for different locales and function word stripping

--- spec/researches/largestKeywordDistanceSpec.js
+++ spec/researches/largestKeywordDistanceSpec.js
@@ -58,6 +58,29 @@ const topicLong = [
 	[ "something", "somethings" ],
 ];
 
+const sentencesIT = [
+	"Che cosa straordinaria!",
+	"Straordinaria è una parola strana.",
+	"Ho trovato una chiave e una parola straordinaria.",
+	"E ancora una chiave e qualcosa.",
+	"È qualcosa che non ha niente da fare con questo che cerchiamo.",
+	"Ah, una chiave!",
+	"Ancora niente!",
+	"Una parola e ancora un'altra e poi un'altra ancora, che schifo!",
+];
+
+const topicShortIT = [
+	[ "parola" ],
+	[ "chiave" ],
+];
+
+const topicLongIT = [
+	[ "parola" ],
+	[ "chiave" ],
+	[ "straordinaria" ],
+	[ "qualcosa" ],
+];
+
 
 describe( "Test for computing the sentence score", function() {
 	it( "for a short topic", function() {
@@ -66,6 +89,14 @@ describe( "Test for computing the sentence score", function() {
 
 	it( "for a long topic", function() {
 		expect( computeScoresPerSentenceLongTopic( topicLong, sentences, "en_EN" ) ).toEqual( [ 6, 6, 9, 6, 6, 6, 3, 6 ] );
+	} );
+
+	it( "for a short topic for a language that doesn't support morphology", function() {
+		expect( computeScoresPerSentenceShortTopic( topicShortIT, sentencesIT, "it_IT" ) ).toEqual( [ 3, 6, 9, 6, 3, 6, 3, 6 ] );
+	} );
+
+	it( "for a long topic for a language that doesn't support morphology", function() {
+		expect( computeScoresPerSentenceLongTopic( topicLongIT, sentencesIT, "it_IT" ) ).toEqual( [ 6, 6, 9, 6, 6, 6, 3, 6 ] );
 	} );
 } );
 
@@ -80,7 +111,7 @@ describe( "Test for computing the step function", function() {
 } );
 
 describe( "Test for a step-function research", function() {
-	it( "returns an average score over all sentences and all topic forms and markers for sentences that don't contain the topic at all", function() {
+	it( "returns an average score over all sentences and all topic forms; returns markers for sentences that don't contain the topic at all", function() {
 		const paper = new Paper(
 			sentences.join( " " ),
 			{
@@ -96,6 +127,114 @@ describe( "Test for a step-function research", function() {
 		expect( largestKeywordDistanceResearcher( paper, researcher ) ).toEqual( {
 			averageScore: 7.8,
 			sentencesToHighlight: [ "Again nothing!" ],
+		} );
+	} );
+
+	it( "returns the same score when function words are added", function() {
+		const paper = new Paper(
+			sentences.join( " " ),
+			{
+				locale: "en_EN",
+				keyword: "the key word",
+				/*
+				 * No function word has been added to something; something is already a function word. In topics
+				 * that solely consist of function words, these words are not filtered. Therefore adding another
+				 * function word to "something" would change the result because it would get analyzed like a content word.
+				 */
+				synonyms: "such remarkable, something, very word",
+			}
+		);
+
+		const researcher = new Researcher( paper );
+		researcher.addResearchDataProvider( "morphology", morphologyData );
+
+		expect( largestKeywordDistanceResearcher( paper, researcher ) ).toEqual( {
+			averageScore: 7.8,
+			sentencesToHighlight: [ "Again nothing!" ],
+		} );
+	} );
+
+	it( "returns an average score (for a language without morphology support) over all sentences and all topic forms; returns markers for sentences that don't contain the topic at all", function() {
+		const paper = new Paper(
+			sentencesIT.join( " " ),
+			{
+				locale: "it_IT",
+				keyword: "parola chiave",
+				synonyms: "straordinaria, qualcosa, parola",
+			}
+		);
+
+		const researcher = new Researcher( paper );
+		researcher.addResearchDataProvider( "morphology", morphologyData );
+
+		expect( largestKeywordDistanceResearcher( paper, researcher ) ).toEqual( {
+			averageScore: 7.8,
+			sentencesToHighlight: [ "Ancora niente!" ],
+		} );
+	} );
+
+	it( "returns the same score when function words are added (for a language without morphological support, but with function words)", function() {
+		const paper = new Paper(
+			sentencesIT.join( " " ),
+			{
+				locale: "it_IT",
+				keyword: "la parola chiave",
+				synonyms: "tanta straordinaria, qualcosa, molto parola",
+			}
+		);
+
+		const researcher = new Researcher( paper );
+		researcher.addResearchDataProvider( "morphology", morphologyData );
+
+		expect( largestKeywordDistanceResearcher( paper, researcher ) ).toEqual( {
+			averageScore: 7.8,
+			sentencesToHighlight: [ "Ancora niente!" ],
+		} );
+	} );
+
+	it( "when the topic words don't contain function words, returns the same score for a language without morphological support when that language is set to a locale without function words ", function() {
+		const paper = new Paper(
+			sentencesIT.join( " " ),
+			{
+				// Fictitious locale that doesn't have function word support.
+				locale: "xx_XX",
+				keyword: "parola chiave",
+				/*
+				 * Since there are only content words the score doesn't change.
+				 * (Qualcosa is technically a function word but is analyzed as a content word here since it's the
+				 * only word in the synonym.)
+				 */
+				synonyms: "straordinaria, qualcosa, parola",
+			}
+		);
+
+		const researcher = new Researcher( paper );
+		researcher.addResearchDataProvider( "morphology", morphologyData );
+
+		expect( largestKeywordDistanceResearcher( paper, researcher ) ).toEqual( {
+			averageScore: 7.8,
+			sentencesToHighlight: [ "Ancora niente!" ],
+		} );
+	} );
+
+	it( "when the topic words contain function words, returns a different score for a language without morphological support when that language is set to a locale without function words", function() {
+		const paper = new Paper(
+			sentencesIT.join( " " ),
+			{
+				// Fictitious locale that doens't have function word support.
+				locale: "xx_XX",
+				keyword: "la parola chiave",
+				// The added function words are now analyzed as content words, so the score changes.
+				synonyms: "tanta straordinaria, qualcosa, molto parola",
+			}
+		);
+
+		const researcher = new Researcher( paper );
+		researcher.addResearchDataProvider( "morphology", morphologyData );
+
+		expect( largestKeywordDistanceResearcher( paper, researcher ) ).toEqual( {
+			averageScore: 6.7,
+			sentencesToHighlight: [ "Ancora niente!" ],
 		} );
 	} );
 } );
