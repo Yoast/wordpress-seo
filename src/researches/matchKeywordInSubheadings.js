dc8b88a7c23e86c5733251ce3b4578e784d7bc9f
Natalia <natalia@yoast.com>
2018-09-24 16:23:57 +0200

    Small revisions

--- src/researches/matchKeywordInSubheadings.js
+++ src/researches/matchKeywordInSubheadings.js
@@ -3,43 +3,21 @@
 import stripSomeTags from "../stringProcessing/stripNonTextTags.js";
 
 import { getSubheadingContents } from "../stringProcessing/getSubheadings.js";
-import matchTextWithArray from "../stringProcessing/matchTextWithArray";
-
-/**
- * Checks if the given subheading reflects the given topic.
- * "Reflects" is defined as a subheading having at least 50% of the content words in the key phrase
- * (morphological forms included).
- *
- * @param {String} subheading the subheading to check.
- * @param {Array<String[]>} keyphraseForms the key words and their forms to check.
- * @param {String} locale the current locale
- * @returns {Boolean} if the fraction of found keywords in the subheading is bigger than 0.5.
- */
-const subheadingReflectsTopic = function( subheading, keyphraseForms, locale ) {
-	const keywordFormsFound = keyphraseForms.filter(
-		keywordForms => matchTextWithArray( subheading, keywordForms, locale ).count > 0
-	);
-
-	// Over half the keywords should be in the subheading.
-	return ( keywordFormsFound.length / keyphraseForms.length ) > 0.5;
-};
+import { findTopicFormsInString } from "./findKeywordFormsInString.js";
 
 /**
  * Computes the amount of subheadings reflecting the topic.
  *
- * @param {String[]} subheadings the subheadings to check.
  * @param {Object} topicForms the main key phrase and its synonyms to check.
- * @param {String} locale the current locale.
- * @returns {Number} the amount of subheadings reflecting the topic.
+ * @param {String[]} subheadings the subheadings to check.
+ * @param {boolean} useSynonyms Whether to match synonyms or only main keyphrase.
+ * @param {string} locale the current locale.
+ * @returns {number} the amount of subheadings reflecting the topic.
  */
-const numberOfSubheadingsReflectingTopic = function( subheadings, topicForms, locale ) {
+const numberOfSubheadingsReflectingTopic = function( topicForms, subheadings, useSynonyms, locale ) {
 	return subheadings.filter(
 		subheading => {
-			const reflectsMainTopic = subheadingReflectsTopic( subheading, topicForms.keyphraseForms, locale );
-			const reflectsSynonym = topicForms.synonymsForms.some(
-				synonymForm => subheadingReflectsTopic( subheading, synonymForm, locale )
-			);
-			return reflectsMainTopic || reflectsSynonym;
+			return findTopicFormsInString( topicForms, subheading, useSynonyms, locale ).percentWordMatches > 50;
 		}
 	).length;
 
@@ -51,21 +29,22 @@ const numberOfSubheadingsReflectingTopic = function( subheadings, topicForms, lo
  *
  * Also checks for synonyms.
  *
- * @param {object} paper The paper object containing the text and keyword.
+ * @param {Object} paper The paper object containing the text and keyword.
  * @param {Researcher} researcher The researcher object.
- * @returns {object} the result object.
+ * @returns {Object} the result object.
  */
 export default function( paper, researcher ) {
-	let text = paper.getText();
-	let topicForms = researcher.getResearch( "morphology" );
+	const text = stripSomeTags( paper.getText() );
+	const topicForms = researcher.getResearch( "morphology" );
 	const locale = paper.getLocale();
-	const result = { count: 0 };
-	text = stripSomeTags( text );
+	const result = { count: 0, matches: 0 };
 	const subheadings = getSubheadingContents( text );
+
+	const useSynonyms = true;
 
 	if ( 0 !== subheadings.length ) {
 		result.count = subheadings.length;
-		result.matches = numberOfSubheadingsReflectingTopic( subheadings, topicForms, locale );
+		result.matches = numberOfSubheadingsReflectingTopic( topicForms, subheadings, useSynonyms, locale );
 	}
 
 	return result;
