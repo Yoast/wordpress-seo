3986cdd26c1f3f92c7c6f5b702323788c6c0fea8
Herre Groen <herre@yoast.com>
2018-10-01 16:35:18 +0200

    Fix indexable endpoint singular route.

--- admin/endpoints/class-endpoint-indexable.php
+++ admin/endpoints/class-endpoint-indexable.php
@@ -11,7 +11,7 @@
 class WPSEO_Endpoint_Indexable implements WPSEO_Endpoint, WPSEO_Endpoint_Storable {
 
 	const REST_NAMESPACE 	= 'yoast/v1';
-	const ENDPOINT_SINGULAR = 'indexables/(?P<object_id>\d+)';
+	const ENDPOINT_SINGULAR = 'indexables/(?P<object_type>\w+)/(?P<object_id>\d+)';
 
 	const CAPABILITY_RETRIEVE = 'manage_options';
 	const CAPABILITY_STORE 	  = 'manage_options';
