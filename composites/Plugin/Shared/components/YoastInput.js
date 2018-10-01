ebf90fbb50e5dc6065026fad3cdc97b0e2dd08ff
Andrea Fercia <a.fercia@gmail.com>
2018-10-01 11:08:55 +0200

    Standardize input field padding to 8px as per design feedback.

--- composites/Plugin/Shared/components/YoastInput.js
+++ composites/Plugin/Shared/components/YoastInput.js
@@ -19,9 +19,9 @@ export const YoastInputLabel = styled.label`
 `;
 
 export const YoastInputField = styled.input`
-	padding: 0.75em;
-	font-size: 1em;
 	&&& {
+		padding: 8px;
+		font-size: 1em;
 		box-shadow: inset 0 1px 2px ${ rgba( colors.$color_black, 0.07 ) };
 		border: 1px solid ${ colors.$color_input_border };
 		border-radius: 0;
