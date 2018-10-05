27bdd8ec41d607a3e6b942e5e4ef6aaf624b1968
Hans-Christiaan <hans-christiaan@yoast.com>
2018-10-05 12:00:30 +0200

    Removed trailing whitespace from test.

--- spec/relatedKeywordTaxonomyAssessorSpec.js
+++ spec/relatedKeywordTaxonomyAssessorSpec.js
@@ -56,7 +56,6 @@ describe( "running assessments in the assessor", function() {
 		assessor.assess( new Paper( "This is a keyword. Lorem ipsum dolor sit amet, vim illum aeque constituam at. Id latine tritani alterum pro. Ei quod stet affert sed. Usu putent fabellas suavitate id. Quo ut stet recusabo torquatos. Eum ridens possim expetenda te. Ex per putant comprehensam. At vel utinam cotidieque, at erat brute eum, velit percipit ius et. Has vidit accusata deterruisset ea, quod facete te vis. Vix ei duis dolor, id eum sonet fabulas. Id vix imperdiet efficiantur. Percipit probatus pertinax te sit. Putant intellegebat eu sit. Vix reque tation prompta id, ea quo labore viderer definiebas. Oratio vocibus offendit an mei, est esse pericula liberavisse.", { keyword: "keyword", description: "description" } ) );
 		const AssessmentResults = assessor.getValidResults();
 		const assessments = getResults( AssessmentResults );
-				
 		expect( assessments ).toEqual( [
 			"introductionKeyword",
 			"keyphraseLength",
