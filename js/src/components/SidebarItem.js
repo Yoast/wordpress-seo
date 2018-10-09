e29c84efbb34897fc31708cd47338c0ebf54c3ff
IreneStr <irene@yoast.com>
2018-10-09 12:50:12 +0200

    Add location context to SidebarItem

--- js/src/components/SidebarItem.js
+++ js/src/components/SidebarItem.js
@@ -1,11 +1,13 @@
 /* External dependencies */
 import React from "react";
 
+import addLocationContext from "./higherorder/addLocationContext"
+
 /* Internal dependencies */
 import withYoastSidebarPriority from "./higherorder/withYoastSidebarPriority";
 
-const SidebarItem = withYoastSidebarPriority( ( { children } ) => {
+const SidebarItem = withYoastSidebarPriority( addLocationContext( ( { children } ) => {
 	return <div>{ children }</div>;
-} );
+}, "sidebar" ) );
 
 export default SidebarItem;
