73f925fecb86a8839352e875b0b93aefbf40fb01
Igor <igor@yoast.com>
2018-10-10 11:13:37 +0200

    Overwrite ESLint config to move new errors back to warnings and increase the warning limit

--- grunt/config/eslint.js
+++ grunt/config/eslint.js
@@ -6,7 +6,7 @@ module.exports = function( grunt ) {
 		target: {
 			src: [ "<%= files.js %>", "<%= files.jsDontLint %>" ],
 			options: {
-				maxWarnings: 45,
+				maxWarnings: 87,
 				fix: fix,
 			},
 		},
