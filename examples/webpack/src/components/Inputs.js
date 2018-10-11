7f00fac4ed8274a6f254f4751ae816600a8c29ae
Igor <igor@yoast.com>
2018-10-11 09:23:00 +0200

    Add actual titleWidth to paper

--- examples/webpack/src/components/Inputs.js
+++ examples/webpack/src/components/Inputs.js
@@ -5,14 +5,17 @@ import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 
 import { setPaperAttribute } from "../redux/actions/paper";
+import measureTextWidth from "../utils/measureTextWidth";
 
-function renderPaperAttribute( props, id, placeholder, label = null, Component = Input, defaultValue = "" ) {
-	const { setPaperAttribute: onChange, paper } = props;
+function renderPaperAttribute( props, id, placeholder, label = null, onChange = null, Component = Input, defaultValue = "" ) {
+	if ( onChange === null ) {
+		onChange = props.setPaperAttribute;
+	}
 
 	return (
 		<Component
 			id={ id }
-			value={ paper[ id ] || defaultValue }
+			value={ props.paper[ id ] || defaultValue }
 			label={ label || id }
 			placeholder={ placeholder }
 			onChange={ onChange }
@@ -22,10 +25,13 @@ function renderPaperAttribute( props, id, placeholder, label = null, Component =
 
 function Inputs( props ) {
 	return <section>
-		{ renderPaperAttribute( props, "text", "Write a text", null, TextArea ) }
+		{ renderPaperAttribute( props, "text", "Write a text", null, null, TextArea ) }
 		{ renderPaperAttribute( props, "keyword", "Choose a focus keyword", "Focus keyword" ) }
 		{ renderPaperAttribute( props, "synonyms", "Choose keyword synonyms" ) }
-		{ renderPaperAttribute( props, "title", "Write the SEO title" ) }
+		{ renderPaperAttribute( props, "title", "Write the SEO title", null, ( id, value ) => {
+			props.setPaperAttribute( id, value );
+			props.setPaperAttribute( "titleWidth", measureTextWidth( value ) );
+		} ) }
 		{ renderPaperAttribute( props, "description", "Write a meta description" ) }
 		{ renderPaperAttribute( props, "locale", "en_US" ) }
 	</section>;
