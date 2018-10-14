57baaae9a538852b3b39e976cd3abd391960bb54
pedro-mendonca <ped.gaspar@gmail.com>
2018-10-14 18:32:02 +0100

    Remove new lines and tabs by separating each sentence in a separate string

--- admin/links/class-link-compatibility-notifier.php
+++ admin/links/class-link-compatibility-notifier.php
@@ -34,16 +34,16 @@ class WPSEO_Link_Compatibility_Notifier {
 	protected function get_notification() {
 		return new Yoast_Notification(
 			sprintf(
-				/* translators: %1$s: Yoast SEO. %2$s: Version number of Yoast SEO. %3$s: PHP version %4$s: The current PHP versione. %5$s link to knowledge base article about solving PHP issue. %6$s: is anchor closing. */
-				__(
-					'The <strong>Text link counter</strong> feature (introduced in %1$s %2$s) is currently disabled. For this feature to work %1$s requires at least PHP version %3$s. We have detected PHP version %4$s on this website.
-					Please read the following %5$sknowledge base article%6$s to find out how to resolve this problem.',
-					'wordpress-seo'
-				),
+				/* translators: %1$s: Yoast SEO. %2$s: Version number of Yoast SEO. %3$s: PHP version %4$s: The current PHP version. */
+				__( 'The <strong>Text link counter</strong> feature (introduced in %1$s %2$s) is currently disabled. For this feature to work %1$s requires at least PHP version %3$s. We have detected PHP version %4$s on this website.', 'wordpress-seo' ),
 				'Yoast SEO',
 				'5.0',
 				'5.3',
-				phpversion(),
+				phpversion()
+			) . '<br>' .
+			sprintf(
+				/* translators: %1$s: link to knowledge base article about solving PHP issue. %2$s: is anchor closing. */
+				esc_html__( 'Please read the following %1$sknowledge base article%2$s to find out how to resolve this problem.', 'wordpress-seo' ),
 				'<a href="' . WPSEO_Shortlinker::get( 'https://yoa.st/16f' ) . '" target="_blank">',
 				'</a>'
 			),
