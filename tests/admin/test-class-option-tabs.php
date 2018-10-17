a0ce5e98607ff4741c10d8d22aca81482eff427e
Andy Meerwaldt <andy@yoast.com>
2018-10-17 14:17:15 +0200

    Add documentation for the tests

--- tests/admin/test-class-option-tabs.php
+++ tests/admin/test-class-option-tabs.php
@@ -1,19 +1,34 @@
 <?php
 
+/**
+ * WPSEO plugin test file.
+ *
+ * @package WPSEO\Tests\Admin
+ */
+
+/**
+ * Unit Test Class.
+ */
 class WPSEO_Option_Tabs_Test extends WPSEO_UnitTestCase {
 
+	/**
+	 * Tests the retrieval of the base.
+	 *
+	 * @covers WPSEO_Option_Tabs::get_base()
+	 */
 	public function test_get_base() {
 		$option_tabs = new WPSEO_Option_Tabs( 'base' );
 
 		$this->assertEquals( 'base', $option_tabs->get_base() );
 	}
 
+	/**
+	 * Tests the addition of a new tab.
+	 *
+	 * @covers WPSEO_Option_Tabs::add_tab()
+	 */
 	public function test_add_tab() {
-
 		$option_tabs = new WPSEO_Option_Tabs( 'base' );
-
-		$before = $option_tabs->get_tabs();
-
 		$option_tabs->add_tab( new WPSEO_Option_Tab( 'name', 'label' ) );
 
 		$after = $option_tabs->get_tabs();
@@ -21,6 +36,11 @@ class WPSEO_Option_Tabs_Test extends WPSEO_UnitTestCase {
 		$this->assertTrue( ! empty( $after ) );
 	}
 
+	/**
+	 * Tests the retrieval of the tabs.
+	 *
+	 * @covers WPSEO_Option_Tabs::get_tabs()
+	 */
 	public function test_get_tabs() {
 		$option_tab  = new WPSEO_Option_Tab( 'name', 'label' );
 		$option_tabs = new WPSEO_Option_Tabs( 'base' );
@@ -29,6 +49,11 @@ class WPSEO_Option_Tabs_Test extends WPSEO_UnitTestCase {
 		$this->assertEquals( array( $option_tab ), $option_tabs->get_tabs() );
 	}
 
+	/**
+	 * Tests if the given tab is active.
+	 *
+	 * @covers WPSEO_Option_Tabs::get_active_tab()
+	 */
 	public function test_is_active_tab() {
 		$option_tab  = new WPSEO_Option_Tab( 'name', 'label' );
 		$option_tabs = new WPSEO_Option_Tabs( 'base', 'name' );
@@ -37,6 +62,11 @@ class WPSEO_Option_Tabs_Test extends WPSEO_UnitTestCase {
 		$this->assertTrue( $option_tabs->is_active_tab( $option_tab ) );
 	}
 
+	/**
+	 * Tests of the given tab is not active.
+	 *
+	 * @covers WPSEO_Option_Tabs::is_active_tab()
+	 */
 	public function test_is_active_tab_not_active() {
 		$option_tab  = new WPSEO_Option_Tab( 'name', 'label' );
 		$option_tabs = new WPSEO_Option_Tabs( 'base', 'inactive_name' );
@@ -45,12 +75,22 @@ class WPSEO_Option_Tabs_Test extends WPSEO_UnitTestCase {
 		$this->assertFalse( $option_tabs->is_active_tab( $option_tab ) );
 	}
 
+	/**
+	 * Tests retrieval of an active tab without having any tabs.
+	 *
+	 * @covers WPSEO_Option_Tabs::is_active_tab()
+	 */
 	public function test_get_active_tab_without_any_active_tab_being_set() {
 		$option_tabs = new WPSEO_Option_Tabs( 'base' );
 
 		$this->assertNull( $option_tabs->get_active_tab() );
 	}
 
+	/**
+	 * Tests the retrieval of the active tab.
+	 *
+	 * @covers WPSEO_Option_Tabs::get_active_tab()
+	 */
 	public function test_get_active_tab() {
 		$option_tab  = new WPSEO_Option_Tab( 'name', 'label' );
 		$option_tabs = new WPSEO_Option_Tabs( 'base', 'name' );
@@ -59,13 +99,34 @@ class WPSEO_Option_Tabs_Test extends WPSEO_UnitTestCase {
 		$this->assertEquals( $option_tab, $option_tabs->get_active_tab() );
 	}
 
+	/**
+	 * Tests the retrieval of the active tab without having any tabs.
+	 *
+	 * @covers WPSEO_Option_Tabs::get_active_tab()
+	 */
 	public function test_get_active_tab_WITH_nonexisting_tab_set_as_active() {
 		$option_tabs = new WPSEO_Option_Tabs( 'base', 'nonexisting' );
 
 		$this->assertNull( $option_tabs->get_active_tab() );
 	}
 
-	public function test_display(  ) {
+	/**
+	 * Tests the retrieval of the active tab without having a active tab being set
+	 *
+	 * @covers WPSEO_Option_Tabs::get_active_tab()
+	 */
+	public function test_get_active_tab_WITH_no_tab_set_as_active() {
+		$option_tabs = new WPSEO_Option_Tabs( 'base' );
+
+		$this->assertNull( $option_tabs->get_active_tab() );
+	}
+
+	/**
+	 * Tests the displaying of the tabs.
+	 *
+	 * @covers WPSEO_Option_Tabs::display
+	 */
+	public function test_display() {
 		$option_tab  = new WPSEO_Option_Tab( 'name', 'label' );
 		$option_tabs = new WPSEO_Option_Tabs( 'base', 'name' );
 		$option_tabs->add_tab( $option_tab );
@@ -73,11 +134,5 @@ class WPSEO_Option_Tabs_Test extends WPSEO_UnitTestCase {
 		$option_tabs->display( new Yoast_Form() );
 
 		$this->expectOutputContains( '<h2 class="nav-tab-wrapper" id="wpseo-tabs">' );
-
-
 	}
-
-
-
-
 }
--- tests/config-ui/components/test-class-component-mailchimp-signup.php
+++ tests/config-ui/components/test-class-component-mailchimp-signup.php
@@ -1,19 +1,45 @@
 <?php
+/**
+ * WPSEO plugin test file.
+ *
+ * @package WPSEO\Tests\ConfigUI\Components
+ */
 
+/**
+ * Class WPSEO_Config_Component_Mailchimp_Signup_Test
+ */
 class WPSEO_Config_Component_Mailchimp_Signup_Test extends WPSEO_UnitTestCase {
 
+
+	/**
+	 * Tests the get_identifier
+	 *
+	 * @covers WPSEO_Config_Component_Mailchimp_Signup::get_identifier()
+	 */
 	public function test_get_identifier() {
 		$mailchimp_signup = new WPSEO_Config_Component_Mailchimp_Signup();
 
 		$this->assertEquals( 'MailchimpSignup', $mailchimp_signup->get_identifier() );
 	}
 
+
+	/**
+	 * Tests the get_field
+	 *
+	 * @covers WPSEO_Config_Component_Mailchimp_Signup::get_field()
+	 */
 	public function test_get_field() {
 		$mailchimp_signup = new WPSEO_Config_Component_Mailchimp_Signup();
 
 		$this->assertInstanceOf( 'WPSEO_Config_Field', $mailchimp_signup->get_field() );
 	}
 
+
+	/**
+	 * Tests getting the data.
+	 *
+	 * @covers WPSEO_Config_Component_Mailchimp_Signup::get_data()
+	 */
 	public function test_get_data() {
 		$mailchimp_signup = new WPSEO_Config_Component_Mailchimp_Signup();
 
@@ -22,8 +48,12 @@ class WPSEO_Config_Component_Mailchimp_Signup_Test extends WPSEO_UnitTestCase {
 			$mailchimp_signup->get_data() );
 	}
 
+	/**
+	 * Tests setting the data.
+	 *
+	 * @covers WPSEO_Config_Component_Mailchimp_Signup::set_data()
+	 */
 	public function test_set_data() {
-
 		// Sets the current user id to make sure hasSignup will be saved.
 		wp_set_current_user( 1 );
 
@@ -34,6 +64,4 @@ class WPSEO_Config_Component_Mailchimp_Signup_Test extends WPSEO_UnitTestCase {
 			$mailchimp_signup->set_data( array( 'hasSignup' => true ) )
 		);
 	}
-
-
 }
