295d5f404a990b08348fe1db4a435fe88566ead1
nataliashitova <36954173+nataliashitova@users.noreply.github.com>
2018-10-19 10:02:12 +0200

    Changed wording of comment.
    Co-Authored-By: hansjovis <hc.braun.93@gmail.com>

--- src/assessments/seo/MetaDescriptionKeywordAssessment.js
+++ src/assessments/seo/MetaDescriptionKeywordAssessment.js
@@ -67,7 +67,7 @@ class MetaDescriptionKeywordAssessment extends Assessment {
 	 */
 	calculateResult( i18n ) {
 
-		// GOOD result when the meta description contains every keyhrase 1 or 2 times (including synonyms).
+		// GOOD result when the meta description contains keyhrase or a synonym 1 or 2 times.
 		if ( this._keyphraseCounts === 1 || this._keyphraseCounts === 2 ) {
 			return {
 				score: this._config.scores.good,
