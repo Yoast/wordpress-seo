78b3fbae1e5b2c7cc645f27910ccbbd62c2982fb
Hans-Christiaan <hans-christiaan@yoast.com>
2018-10-19 10:40:32 +0200

    Made meta descr keyword specs work witth actual morpholoy data.

--- spec/researches/metaDescriptionKeywordSpec.js
+++ spec/researches/metaDescriptionKeywordSpec.js
@@ -1,70 +1,79 @@
 import metaDescriptionKeyword from "../../src/researches/metaDescriptionKeyword.js";
 import Paper from "../../src/values/Paper.js";
-import Factory from "../specHelpers/factory.js";
 
-const mockResearcherWord = Factory.buildMockResearcher( { keyphraseForms: [ [ "word", "words" ] ] } );
-const mockResearcherWordUmlaut = Factory.buildMockResearcher( { keyphraseForms: [ [ "keywörd", "keywörds" ] ] } );
-
-// The morphological research escapes the forms that it adds to the list of forms, so we should mimic this behavior here.
-const mockResearcherWordDollarSign = Factory.buildMockResearcher( { keyphraseForms: [ [ "\\$keyword", "\\$keywords" ] ] } );
-
-const mockResearcherKeyWordPhrase = Factory.buildMockResearcher( {
-	keyphraseForms: [ [ "key", "keys" ], [ "word", "words" ] ],
-	synonymsForms: [ [ [ "key", "keys" ], [ "phrase", "phrases" ] ] ],
-} );
+import Researcher from "../../src/researcher";
+import morphologyData from "../../premium-configuration/data/morphologyData.json";
 
 describe( "the metadescription keyword match research", function() {
 	it( "returns the number ( 1 ) of keywords found", function() {
-		var paper = new Paper( "", { keyword: "word", description: "a description with a word" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherWord );
+		const paper = new Paper( "", { keyword: "word", description: "a description with a word" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toEqual( 1 );
 	} );
 
 	it( "returns the number ( 2 ) of keywords found", function() {
-		var paper = new Paper( "", { keyword: "word", description: "a description with a word and a word" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherWord );
+		const paper = new Paper( "", { keyword: "word", description: "a description with a word and a word" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toEqual( 2 );
 	} );
 
 	it( "returns the number ( 0 ) of keywords found", function() {
-		var paper = new Paper( "", { keyword: "word", description: "a description with a bla" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherWord );
+		const paper = new Paper( "", { keyword: "word", description: "a description with a bla" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toEqual( 0 );
 	} );
 
 	it( "returns -1 because there is no meta", function() {
-		var paper = new Paper( "", { keyword: "word", description: "" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherWord );
+		const paper = new Paper( "", { keyword: "word", description: "" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toBe( -1 );
 	} );
 
 	it( "returns the number ( 1 ) of keywords found", function() {
-		var paper = new Paper( "", { keyword: "keywörd", description: "a description with a keyword", locale: "en_US" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherWordUmlaut );
+		const paper = new Paper( "", { keyword: "keywörd", description: "a description with a keyword", locale: "en_US" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toEqual( 1 );
 	} );
 
 	it( "returns the number ( 1 ) of keywords found when the keyword begins with $", function() {
-		var paper = new Paper( "", { keyword: "$keyword", description: "a description with a $keyword" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherWordDollarSign );
+		const paper = new Paper( "", { keyword: "$keyword", description: "a description with a $keyword" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toEqual( 1 );
 	} );
 
 	it( "returns the number ( 1 ) of keywords found when the keyword", function() {
-		var paper = new Paper( "", { keyword: "key word", description: "a description with a key word and a key" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherKeyWordPhrase );
+		const paper = new Paper( "", { keyword: "key word", description: "a description with a key word and a key" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toEqual( 1 );
 	} );
 
 	it( "returns the number ( 1 ) of keywords and synonyms found", function() {
-		var paper = new Paper( "", { keyword: "key word", synonyms: "key phrase", description: "a description with a key word and a phrase" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherKeyWordPhrase );
+		const paper = new Paper( "", { keyword: "key word", synonyms: "key phrase", description: "a description with a key word and a phrase" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toEqual( 1 );
 	} );
 
 	it( "returns the number ( 1 ) of keywords and synonyms found", function() {
-		var paper = new Paper( "", { keyword: "key word", synonyms: "key phrase", description: "a description with a key phrase" } );
-		var result = metaDescriptionKeyword( paper, mockResearcherKeyWordPhrase );
+		const paper = new Paper( "", { keyword: "key word", synonyms: "key phrase", description: "a description with a key phrase" } );
+		const researcher = new Researcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+		const result = metaDescriptionKeyword( paper, researcher );
 		expect( result ).toEqual( 1 );
 	} );
 } );
