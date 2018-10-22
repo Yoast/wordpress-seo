f64e0e8f5e447efe1b23c4ccf548c14fe69f8974
Manuel <manuel@yoast.com>
2018-10-22 13:19:11 +0200

    Fix errors

--- src/helpers/includesAny.js
+++ src/helpers/includesAny.js
@@ -9,7 +9,7 @@ import { includes } from "lodash-es";
  * @returns {boolean} Whether a value was found in the collection.
  */
 export default function includesAny( collection, values ) {
-	for( let i = 0; i < values.length; i++ ) {
+	for ( let i = 0; i < values.length; i++ ) {
 		if ( includes( collection, values[ i ] ) ) {
 			return true;
 		}
