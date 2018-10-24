37fa9098e02d13f0f60b2934ef3c15753d0c228e
Igor <igor@yoast.com>
2018-10-24 14:44:20 +0200

    Refactor Italian participle to not use regex for indirect precedence

--- src/researches/italian/passiveVoice/ItalianParticiple.js
+++ src/researches/italian/passiveVoice/ItalianParticiple.js
@@ -1,7 +1,7 @@
 import Participle from "../../../values/Participle.js";
 import checkException from "../../passiveVoice/periphrastic/checkException.js";
 import directPrecedenceException from "../../../stringProcessing/directPrecedenceExceptionWithoutRegex";
-import precedenceException from "../../../stringProcessing/precedenceException";
+import precedenceException from "../../../stringProcessing/precedenceExceptionWithoutRegex";
 
 
 /**
@@ -29,11 +29,10 @@ require( "util" ).inherits( ItalianParticiple, Participle );
 ItalianParticiple.prototype.isPassive = function() {
 	let sentencePart = this.getSentencePart();
 	const participle = this.getParticiple();
-	let participleIndex = sentencePart.indexOf( participle );
 	let language = this.getLanguage();
 
 	return ! this.directPrecedenceException( sentencePart, participle, language ) &&
-		! this.precedenceException( sentencePart, participleIndex, language );
+		! this.precedenceException( sentencePart, participle, language );
 };
 
 ItalianParticiple.prototype.directPrecedenceException = directPrecedenceException;
