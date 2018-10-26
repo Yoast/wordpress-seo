c705ee2c5f5fe893e73f473bd8cdb42ae31194e9
Andrea Fercia <a.fercia@gmail.com>
2018-10-26 11:28:25 +0200

    Add missing escaping.

--- admin/class-premium-popup.php
+++ admin/class-premium-popup.php
@@ -77,8 +77,8 @@ class WPSEO_Premium_Popup {
 		$assets_uri = trailingslashit( plugin_dir_url( WPSEO_FILE ) );
 
 		/* translators: %s expands to Yoast SEO Premium */
-		$cta_text        = sprintf( __( 'Get %s', 'wordpress-seo' ), 'Yoast SEO Premium' );
-		$new_tab_message = '<span class="screen-reader-text">' . __( '(Opens in a new browser tab)', 'wordpress-seo' ) . '</span>';
+		$cta_text        = esc_html( sprintf( __( 'Get %s', 'wordpress-seo' ), 'Yoast SEO Premium' ) );
+		$new_tab_message = '<span class="screen-reader-text">' . esc_html__( '(Opens in a new browser tab)', 'wordpress-seo' ) . '</span>';
 		$caret_icon      = '<span aria-hidden="true" class="yoast-button-upsell__caret"></span>';
 		$classes         = '';
 		if ( $popup ) {
