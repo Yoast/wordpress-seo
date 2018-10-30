06913c3ea4a8641f544f565bad85012d9fb92afd
stodorovic <sasa.todorovic@gmx.com>
2018-10-30 09:27:43 +0100

    Addes tests when woocommerce is not activated

--- tests/frontend/test-class-woocommerce-shop-page.php
+++ tests/frontend/test-class-woocommerce-shop-page.php
@@ -11,6 +11,28 @@
 class WPSEO_WooCommerce_Shop_Page_Test extends WPSEO_UnitTestCase {
 
 	/**
+	 * Tests the situation when woocommerce isn't activated.
+	 *
+	 * @covers WPSEO_WooCommerce_Shop_Page::get_shop_page_id()
+	 */
+	public function test_get_shop_page_id() {
+		$woocommerce_shop_page = new WPSEO_WooCommerce_Shop_Page();
+
+		$this->assertEquals( -1, $woocommerce_shop_page->get_shop_page_id() );
+	}
+
+	/**
+	 * Tests the situation when woocommerce isn't activated.
+	 *
+	 * @covers WPSEO_WooCommerce_Shop_Page::is_shop_page()
+	 */
+	public function test_is_shop_page() {
+		$woocommerce_shop_page = new WPSEO_WooCommerce_Shop_Page();
+
+		$this->assertEquals( false, $woocommerce_shop_page->is_shop_page() );
+	}
+
+	/**
 	 * Tests the situation where the currently opened page isn't a shop page.
 	 *
 	 * @covers WPSEO_WooCommerce_Shop_Page::get_page_id()
