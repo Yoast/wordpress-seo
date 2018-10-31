9e6e39c06170114a01df480bbb6f4f114a821b70
Igor <igor@yoast.com>
2018-10-31 09:44:47 +0100

    Add YOAST_RECALIBRATION environment variable that defaults to 'disabled'

--- examples/webpack/config/env.js
+++ examples/webpack/config/env.js
@@ -77,6 +77,8 @@ function getClientEnvironment(publicUrl) {
         // This should only be used as an escape hatch. Normally you would put
         // images into the `src` and `import` them in code to get their paths.
         PUBLIC_URL: publicUrl,
+        // Custom variables.
+        YOAST_RECALIBRATION: process.env.YOAST_RECALIBRATION || "disabled",
       }
     );
   // Stringify all values so we can feed into Webpack DefinePlugin
