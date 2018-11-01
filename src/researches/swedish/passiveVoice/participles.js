a9a2b9a532c69a7e865e01ea37daf1bb0973be20
Agnieszka Szuba <agnieszka@yoast.com>
2018-11-01 15:55:08 +0100

    Add specs

---
+++ src/researches/swedish/passiveVoice/participles.js
@@ -0,0 +1,18 @@
+/**
+ * Returns a list of all participles used for the Swedish passive voice assessment.
+ * @returns {Array} The list with participles.
+ */
+
+const passiveVerbs = [
+
+];
+
+/**
+ * Returns lists with passive verb-forms to be used by the passive voice assessment.
+ * @returns {Object} The object with passive verb-form lists.
+ */
+export default function() {
+	return {
+		all: passiveVerbs,
+	};
+}
