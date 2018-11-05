f04fcc54a4f523d5017672347fa224ea20aacd51
Herre Groen <herre@yoast.com>
2018-11-05 13:57:53 +0100

    Extract wp dependencies to their own files and register them under their normal name.

--- admin/class-admin-asset-manager.php
+++ admin/class-admin-asset-manager.php
@@ -16,11 +16,6 @@ class WPSEO_Admin_Asset_Manager {
 	protected $asset_location;
 
 	/**
-	 * @var array The backport dependencies.
-	 */
-	protected static $backport_dependencies;
-
-	/**
 	 *  Prefix for naming the assets.
 	 */
 	const PREFIX = 'yoast-seo-';
@@ -173,7 +168,7 @@ class WPSEO_Admin_Asset_Manager {
 	}
 
 	/**
-	 * Reregisters the globals backport asset with the correct dependencies.
+	 * Registers the WordPress dependencies that exist in 5.0 in case they are not present.
 	 *
 	 * This function can be removed when WordPress 5.1 has been released, because from 5.0 wp-elements will be
 	 * registered earlier, which means we don't have to reregister things.
@@ -181,77 +176,51 @@ class WPSEO_Admin_Asset_Manager {
 	 * @return void
 	 */
 	public function register_wp_assets() {
-		// The dependencies that are registered on 'admin_init'.
-		$previous_deps = self::$backport_dependencies;
-
-		// The dependencies that are present on 'admin_init'.
-		$current_deps = $this->get_backport_dependencies();
-
-		/*
-		 * This is false when Gutenberg is active, because in that case Gutenberg's scripts are not registered yet
-		 * on 'admin_init', but they are on 'admin_enqueue_scripts'.
-		 */
-		if ( $current_deps === $previous_deps ) {
-			return;
-		}
-
-		wp_deregister_script( self::PREFIX . 'wp-globals-backport' );
-
 		$flat_version = $this->flatten_version( WPSEO_VERSION );
-		$args = array(
-			'name' => 'wp-globals-backport',
-			'src'  => 'wp-seo-wp-globals-backport-' . $flat_version,
-			'deps' => $current_deps,
+
+		wp_register_script( 'lodash-base', plugins_url( 'js/vendor/lodash.min.js', WPSEO_FILE ), array(), false, true );
+		wp_register_script( 'lodash', plugins_url( 'js/vendor/lodash-noconflict.js', WPSEO_FILE ), array( 'lodash-base' ), false, true );
+		wp_register_script( 'wp-polyfill-ecmascript', plugins_url( 'js/dist/babel-polyfill-' . $flat_version . '.js', WPSEO_FILE ), array(), false, true );
+
+		wp_register_script(
+			'wp-element',
+			plugins_url( 'js/dist/wp-element-' . $flat_version . '.js', WPSEO_FILE ),
+			array( 'lodash', 'wp-polyfill-ecmascript' ),
+			false,
+			true
 		);
 
-		$script = new WPSEO_Admin_Asset( $args );
-		$this->register_script( $script );
-	}
+		wp_register_script(
+			'wp-api-fetch',
+			plugins_url( 'js/dist/wp-apiFetch-' . $flat_version . '.js', WPSEO_FILE ),
+			array( 'wp-i18n', 'wp-polyfill-ecmascript' ),
+			false,
+			true
+		);
 
-	/**
-	 * Gets the correct dependencies for the global backport.
-	 *
-	 * @return array The dependencies for the global backport.
-	 */
-	protected function get_backport_dependencies() {
-		$backport_wp_dependencies = array( self::PREFIX . 'react-dependencies' );
+		wp_register_script(
+			'wp-components',
+			plugins_url( 'js/dist/wp-components-' . $flat_version . '.js', WPSEO_FILE ),
+			array( 'lodash', 'wp-api-fetch', 'wp-i18n', 'wp-polyfill-ecmascript' ),
+			false,
+			true
+		);
 
-		// If Gutenberg is present we can borrow their globals for our own.
-		if ( $this->should_load_gutenberg_assets() ) {
-			$backport_wp_dependencies[] = 'wp-element';
-			$backport_wp_dependencies[] = 'wp-data';
-			$backport_wp_dependencies[] = 'wp-components';
+		wp_register_script(
+			'wp-data',
+			plugins_url( 'js/dist/wp-data-' . $flat_version . '.js', WPSEO_FILE ),
+			array( 'lodash', 'wp-element', 'wp-polyfill-ecmascript' ),
+			false,
+			true
+		);
 
-			/*
-			 * The version of TinyMCE that Gutenberg uses is incompatible with
-			 * the one core uses. So we need to make sure that the core version
-			 * is used in the classic editor.
-			 *
-			 * $_GET is used here because as far as I am aware you cannot use
-			 * filter_input to check for the existence of a query variable.
-			 */
-			if ( wp_script_is( 'tinymce-latest', 'registered' ) && isset( $_GET['classic-editor'] ) ) {
-				wp_deregister_script( 'tinymce-latest' );
-				wp_register_script( 'tinymce-latest', includes_url( 'js/tinymce/' ) . 'wp-tinymce.php', array( 'jquery' ), false, true );
-			}
-		}
-		else {
-			if ( wp_script_is( 'lodash', 'registered' ) ) {
-				$backport_wp_dependencies[] = 'lodash';
-			}
-			else {
-				if ( ! wp_script_is( self::PREFIX . 'lodash', 'registered' ) ) {
-					wp_register_script( self::PREFIX . 'lodash-base', plugins_url( 'js/vendor/lodash.min.js', WPSEO_FILE ), array(), false, true );
-					wp_register_script( self::PREFIX . 'lodash', plugins_url( 'js/vendor/lodash-noconflict.js', WPSEO_FILE ), array( self::PREFIX . 'lodash-base' ), false, true );
-				}
-				$backport_wp_dependencies[] = self::PREFIX . 'lodash';
-			}
-		}
-
-		// Save the $backport_dependencies to use in register_wp_assets.
-		self::$backport_dependencies = $backport_wp_dependencies;
-
-		return $backport_wp_dependencies;
+		wp_register_script(
+			'wp-i18n',
+			plugins_url( 'js/dist/wp-i18n-' . $flat_version . '.js', WPSEO_FILE ),
+			array( 'wp-polyfill-ecmascript' ),
+			false,
+			true
+		);
 	}
 
 	/**
@@ -275,20 +244,12 @@ class WPSEO_Admin_Asset_Manager {
 
 		$flat_version = $this->flatten_version( WPSEO_VERSION );
 
-		$backport_wp_dependencies = $this->get_backport_dependencies();
-
-		// If Gutenberg's babel polyfill is not present, use our own.
-		$babel_polyfill = 'wp-polyfill-ecmascript';
-		if ( ! wp_script_is( 'wp-polyfill-ecmascript', 'registered' ) ) {
-			$babel_polyfill = self::PREFIX . 'babel-polyfill';
-		}
-
 		return array(
 			array(
 				'name' => 'react-dependencies',
 				// Load webpack-commons for bundle support.
 				'src'  => 'commons-' . $flat_version,
-				'deps' => array( $babel_polyfill ),
+				'deps' => array( 'wp-polyfill-ecmascript' ),
 			),
 			array(
 				'name' => 'search-appearance',
@@ -299,16 +260,12 @@ class WPSEO_Admin_Asset_Manager {
 				),
 			),
 			array(
-				'name' => 'wp-globals-backport',
-				'src'  => 'wp-seo-wp-globals-backport-' . $flat_version,
-				'deps' => $backport_wp_dependencies,
-			),
-			array(
 				'name' => 'yoast-modal',
 				'src'  => 'wp-seo-modal-' . $flat_version,
 				'deps' => array(
 					'jquery',
-					self::PREFIX . 'wp-globals-backport',
+					'wp-element',
+					'wp-i18n',
 					self::PREFIX . 'components',
 				),
 			),
@@ -317,7 +274,8 @@ class WPSEO_Admin_Asset_Manager {
 				'src'  => 'wp-seo-help-center-' . $flat_version,
 				'deps' => array(
 					'jquery',
-					self::PREFIX . 'wp-globals-backport',
+					'wp-element',
+					'wp-i18n',
 					self::PREFIX . 'components',
 				),
 			),
@@ -370,7 +328,11 @@ class WPSEO_Admin_Asset_Manager {
 					'jquery',
 					self::PREFIX . 'select2',
 					self::PREFIX . 'select2-translations',
-					self::PREFIX . 'wp-globals-backport',
+					self::PREFIX . 'react-dependencies',
+					'wp-element',
+					'wp-i18n',
+					'wp-data',
+					'wp-components',
 				),
 				'in_footer' => false,
 			),
@@ -399,7 +361,10 @@ class WPSEO_Admin_Asset_Manager {
 					'wp-util',
 					'wp-api',
 					'wp-sanitize',
-					self::PREFIX . 'wp-globals-backport',
+					'wp-element',
+					'wp-i18n',
+					'wp-data',
+					'wp-api-fetch',
 					self::PREFIX . 'analysis',
 					self::PREFIX . 'react-dependencies',
 					self::PREFIX . 'components',
@@ -410,8 +375,11 @@ class WPSEO_Admin_Asset_Manager {
 				'src'  => 'wp-seo-term-scraper-' . $flat_version,
 				'deps' => array(
 					'wp-sanitize',
+					'wp-element',
+					'wp-i18n',
+					'wp-data',
+					'wp-api-fetch',
 					self::PREFIX . 'replacevar-plugin',
-					self::PREFIX . 'wp-globals-backport',
 					self::PREFIX . 'analysis',
 					self::PREFIX . 'components',
 				),
@@ -450,8 +418,11 @@ class WPSEO_Admin_Asset_Manager {
 				'deps' => array(
 					'jquery',
 					'wp-util',
+					'wp-element',
+					'wp-i18n',
+					'wp-components',
+					'wp-data',
 					self::PREFIX . 'react-dependencies',
-					self::PREFIX . 'wp-globals-backport',
 				),
 			),
 			array(
@@ -478,7 +449,8 @@ class WPSEO_Admin_Asset_Manager {
 				'src'  => 'configuration-wizard-' . $flat_version,
 				'deps' => array(
 					'jquery',
-					self::PREFIX . 'wp-globals-backport',
+					'wp-element',
+					'wp-i18n',
 					self::PREFIX . 'components',
 				),
 			),
@@ -524,7 +496,8 @@ class WPSEO_Admin_Asset_Manager {
 				'deps' => array(
 					self::PREFIX . 'api',
 					'jquery',
-					self::PREFIX . 'wp-globals-backport',
+					'wp-element',
+					'wp-i18n',
 					self::PREFIX . 'components',
 				),
 			),
@@ -540,22 +513,26 @@ class WPSEO_Admin_Asset_Manager {
 				'name' => 'analysis',
 				'src'  => 'analysis-' . $flat_version,
 				'deps' => array(
+					'lodash',
 					self::PREFIX . 'react-dependencies',
 				),
 			),
 			array(
 				'name' => 'components',
 				'src'  => 'components-' . $flat_version,
-				'deps' => array( self::PREFIX . 'analysis' ),
+				'deps' => array(
+					self::PREFIX . 'analysis',
+					self::PREFIX . 'styled-components',
+				),
 			),
 			array(
 				'name' => 'structured-data-blocks',
 				'src'  => 'wp-seo-structured-data-blocks-' . $flat_version,
-				'deps' => array( 'wp-blocks', 'wp-i18n', 'wp-element' ),
+				'deps' => array( 'wp-blocks', 'wp-i18n', 'wp-element', self::PREFIX . 'styled-components' ),
 			),
 			array(
-				'name' => 'babel-polyfill',
-				'src'  => 'babel-polyfill-' . $flat_version,
+				'name' => 'styled-components',
+				'src'  => 'styled-components-' . $flat_version,
 			),
 		);
 	}
