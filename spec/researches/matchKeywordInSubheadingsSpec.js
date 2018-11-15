a6c5e3e7e5cb0b3c7da3d3425108ffb39a344524
Igor <igor@yoast.com>
2018-11-15 09:07:31 +0100

    Add comments to spec

--- spec/researches/matchKeywordInSubheadingsSpec.js
+++ spec/researches/matchKeywordInSubheadingsSpec.js
@@ -141,6 +141,7 @@ describe( "Matching keyphrase in subheadings with recalibration enabled", () => 
 	} );
 
 	it( "matching is stricter with languages that do not support function words", () => {
+		// There is no function word support for Afrikaans.
 		const paper = new Paper( "<h2>So ’n groot hond</h2>", {
 			keyword: "So ’n groot huis",
 			locale: "af",
@@ -150,9 +151,12 @@ describe( "Matching keyphrase in subheadings with recalibration enabled", () => 
 			synonymsForms: [],
 		} );
 
+		// All the words should match and since hond !== huis the expected result is 0.
 		expect( matchKeywordInSubheadings( paper, researcher ).matches ).toBe( 0 );
 
+		// There is function word support for English.
 		paper._attributes.locale = "en_US";
+		// More than 50% should match. With 1 of the 4 words mismatching the expected result is 1.
 		expect( matchKeywordInSubheadings( paper, researcher ).matches ).toBe( 1 );
 	} );
 } );
