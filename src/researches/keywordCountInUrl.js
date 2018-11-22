107b9e2bfd24f372df8293d85ef8e1080485f7d1
Hans-Christiaan Braun <hc.braun.93@gmail.com>
2018-11-22 10:22:50 +0100

    Updated JSDoc of keywordCountInUrl.

--- src/researches/keywordCountInUrl.js
+++ src/researches/keywordCountInUrl.js
@@ -3,7 +3,7 @@
 import { findTopicFormsInString } from "./findKeywordFormsInString.js";
 
 /**
- * Matches the keyword in the URL. Replaces dashes with whitespaces and uses whitespace as wordboundary.
+ * Matches the keyword in the URL. Replaces dashes and underscores with whitespaces and uses whitespace as wordboundary.
  *
  * @param {Paper} paper the Paper object to use in this count.
  * @param {Researcher} researcher The Researcher object containing all available researches.
