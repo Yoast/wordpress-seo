5c94d60549b64df3f21a9ed705b18fd2041898a8
IreneStr <irene@yoast.com>
2018-11-22 13:54:14 +0100

    Clean up translatable strings and add escaping

--- admin/links/class-link-table-accessible-notifier.php
+++ admin/links/class-link-table-accessible-notifier.php
@@ -34,16 +34,18 @@ class WPSEO_Link_Table_Accessible_Notifier {
 	protected function get_notification() {
 		return new Yoast_Notification(
 			sprintf(
-				/* translators: %1$s: Yoast SEO. %2$s: Version number of Yoast SEO. */
-				__( 'The <strong>Text link counter</strong> feature (introduced in %1$s %2$s) is currently disabled.', 'wordpress-seo' ),
+				/* translators: %1$s: Yoast SEO. %2$s: Version number of Yoast SEO. %3$s: strong opening tag, %4$s: strong closing tag  */
+				esc_html__( 'The %3$sText link counter%4$s feature (introduced in %1$s %2$s) is currently disabled.', 'wordpress-seo' ),
 				'Yoast SEO',
-				'5.0'
+				'5.0',
+				'<strong>',
+				'</strong>'
 			) . ' ' .
 			sprintf(
 				/* translators: %1$s: Yoast SEO. */
-				__( 'For this feature to work %1$s needs to create a table in your database. We were unable to create this table automatically.', 'wordpress-seo' ),
+				esc_html__( 'For this feature to work, %1$s needs to create a table in your database. We were unable to create this table automatically.', 'wordpress-seo' ),
 				'Yoast SEO'
-			) . '<br>' .
+			) . '<br><br>' .
 			sprintf(
 				/* translators: %1$s: link to knowledge base article about solving table issue. %2$s: is anchor closing. */
 				esc_html__( 'Please read the following %1$sknowledge base article%2$s to find out how to resolve this problem.', 'wordpress-seo' ),
