25586359d614fdb60442ab9740ce19cce217bbb5
Natalia <natalia@yoast.com>
2018-11-26 15:29:18 +0100

    Fix import FrenchPaper3 and other minor fixes

--- spec/fullTextTests/testTexts/index.js
+++ spec/fullTextTests/testTexts/index.js
@@ -1,19 +1,17 @@
 // English papers
-
 import englishPaper1 from "./en/englishPaper1";
 import englishPaper2 from "./en/englishPaper2";
 import englishPaper3 from "./en/englishPaper3";
 import englishPaper4 from "./en/englishPaper4";
 
 // Swedish papers
-
 import swedishPaper1 from "./sv/swedishPaper1";
 import swedishPaper2 from "./sv/swedishPaper2";
 
 // French papers
-
 import frenchPaper1 from "./fr/frenchPaper1";
 import frenchPaper2 from "./fr/frenchPaper2";
+import frenchPaper3 from "./fr/frenchPaper3";
 
 export default [
 	englishPaper1,
@@ -24,5 +22,6 @@ export default [
 	swedishPaper2,
 	frenchPaper1,
 	frenchPaper2,
+	frenchPaper3,
 ];
 
