c1bd02698a7ffbae56a3f2dfd31144b46665f76a
Agnieszka Szuba <agnieszka@yoast.com>
2018-11-27 11:40:31 +0100

    Add another German text

--- spec/fullTextTests/testTexts/de/germanPaper2.js
+++ spec/fullTextTests/testTexts/de/germanPaper2.js
@@ -28,7 +28,7 @@ const expectedResults = {
 	keywordDensity: {
 		isApplicable: true,
 		score: 9,
-		resultText: "<a href='https://yoa.st/33v' target='_blank'>Keyphrase density</a>: 1%. This is great!",
+		resultText: "<a href='https://yoa.st/33v' target='_blank'>Keyphrase density</a>: 1.1%. This is great!",
 	},
 	metaDescriptionKeyword: {
 		isApplicable: true,
@@ -58,7 +58,7 @@ const expectedResults = {
 	textLength: {
 		isApplicable: true,
 		score: 9,
-		resultText: "<a href='https://yoa.st/34n' target='_blank'>Text length</a>: The text contains 735 words. Good job!",
+		resultText: "<a href='https://yoa.st/34n' target='_blank'>Text length</a>: The text contains 799 words. Good job!",
 	},
 	externalLinks: {
 		isApplicable: true,
@@ -103,7 +103,7 @@ const expectedResults = {
 	fleschReadingEase: {
 		isApplicable: true,
 		score: 3,
-		resultText: "<a href='https://yoa.st/34r' target='_blank'>Flesch Reading Ease</a>: The copy scores 46 in the test, which is considered difficult to read. <a href='https://yoa.st/34s' target='_blank'>Try to make shorter sentences to improve readability</a>.",
+		resultText: "<a href='https://yoa.st/34r' target='_blank'>Flesch Reading Ease</a>: The copy scores 47.5 in the test, which is considered difficult to read. <a href='https://yoa.st/34s' target='_blank'>Try to make shorter sentences, using less difficult words to improve readability</a>.",
 	},
 	subheadingsTooLong: {
 		isApplicable: true,
@@ -112,7 +112,7 @@ const expectedResults = {
 	},
 	textParagraphTooLong: {
 		isApplicable: true,
-		score: 6,
+		score: 3,
 		resultText: "<a href='https://yoa.st/35d' target='_blank'>Paragraph length</a>: 1 of the paragraphs contains more than the recommended maximum of 150 words. <a href='https://yoa.st/35e' target='_blank'>Shorten your paragraphs</a>!",
 	},
 	textSentenceLength: {
@@ -122,13 +122,13 @@ const expectedResults = {
 	},
 	textTransitionWords: {
 		isApplicable: true,
-		score: 9,
-		resultText: "<a href='https://yoa.st/34z' target='_blank'>Transition words</a>: Well done!",
+		score: 6,
+		resultText: "<a href='https://yoa.st/34z' target='_blank'>Transition words</a>: Only 28% of the sentences contain transition words, which is not enough. <a href='https://yoa.st/35a' target='_blank'>Use more of them</a>.",
 	},
 	passiveVoice: {
 		isApplicable: true,
-		score: 3,
-		resultText: "<a href='https://yoa.st/34t' target='_blank'>Passive voice</a>: 15.8 % of the sentences contain passive voice, which is more than the recommended maximum of 10%. <a href='https://yoa.st/34u' target='_blank'>Try to use their active counterparts</a>",
+		score: 6,
+		resultText: "<a href='https://yoa.st/34t' target='_blank'>Passive voice</a>: 11% of the sentences contain passive voice, which is more than the recommended maximum of 10%. <a href='https://yoa.st/34u' target='_blank'>Try to use their active counterparts</a>.",
 	},
 	textPresence: {
 		isApplicable: true,
--- spec/fullTextTests/testTexts/index.js
+++ spec/fullTextTests/testTexts/index.js
@@ -10,6 +10,8 @@ import swedishPaper2 from "./sv/swedishPaper2";
 
 // German papers
 import germanPaper1 from "./de/germanPaper1";
+import germanPaper2 from "./de/germanPaper2";
+import germanPaper3 from "./de/germanPaper3";
 
 // French papers
 import frenchPaper1 from "./fr/frenchPaper1";
@@ -24,6 +26,8 @@ export default [
 	swedishPaper1,
 	swedishPaper2,
 	germanPaper1,
+	germanPaper2,
+	germanPaper3,
 	frenchPaper1,
 	frenchPaper2,
 	frenchPaper3,
