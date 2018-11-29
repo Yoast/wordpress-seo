d21393fbb9af644e29416f28894b86d93252a7b8
Hans-Christiaan <hans-christiaan@yoast.com>
2018-11-29 12:08:39 +0100

    Split code createWorker code into two functions, reducing complexity of original function.

--- src/worker/createWorker.js
+++ src/worker/createWorker.js
@@ -1,4 +1,25 @@
 /**
+ * Creates an URL to a blob. This blob imports a script for use in a web worker (using `importScripts`).
+ *
+ * @param {string} url The URL to the script that has to be loaded.
+ * @returns {string} the URL to the blob.
+ */
+function createBlobURL( url ) {
+	const URL = window.URL || window.webkitURL;
+	const BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
+
+	let blob;
+	try {
+		blob = new Blob( [ "importScripts('" + url + "');" ], { type: "application/javascript" } );
+	} catch ( e1 ) {
+		const blobBuilder = new BlobBuilder();
+		blobBuilder.append( "importScripts('" + url + "');" );
+		blob = blobBuilder.getBlob( "application/javascript" );
+	}
+	return URL.createObjectURL( blob );
+}
+
+/**
  * Creates a WebWorker using the given url.
  *
  * @param {string} url The url of the worker.
@@ -6,23 +27,12 @@
  * @returns {Worker} The worker.
  */
 function createWorker( url ) {
-	const URL = window.URL || window.webkitURL;
-	const BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder || window.MozBlobBuilder;
-
 	let worker = null;
 	try {
 		worker = new Worker( url );
 	} catch ( e ) {
 		try {
-			let blob;
-			try {
-				blob = new Blob( [ "importScripts('" + url + "');" ], { type: "application/javascript" } );
-			} catch ( e1 ) {
-				const blobBuilder = new BlobBuilder();
-				blobBuilder.append( "importScripts('" + url + "');" );
-				blob = blobBuilder.getBlob( "application/javascript" );
-			}
-			const blobUrl = URL.createObjectURL( blob );
+			const blobUrl = createBlobURL( url );
 			worker = new Worker( blobUrl );
 		} catch ( e2 ) {
 			throw e2;
