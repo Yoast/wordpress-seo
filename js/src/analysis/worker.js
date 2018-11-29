6a9420427c357df1e68b72ce437c82a983f69e2d
Natalia <natalia@yoast.com>
2018-11-29 10:32:21 +0100

    Add warnings with links

--- js/src/analysis/worker.js
+++ js/src/analysis/worker.js
@@ -4,6 +4,7 @@ import isUndefined from "lodash/isUndefined";
 import merge from "lodash/merge";
 import { AnalysisWorkerWrapper, createWorker } from "yoastseo";
 import { __, sprintf } from "@wordpress/i18n";
+import interpolateComponents from "interpolate-components";
 
 // Internal dependencies.
 import getContentLocale from "./getContentLocale";
@@ -31,8 +32,13 @@ export function createAnalysisWorker( dispatch ) {
 		let message = [];
 		if ( isRecalibrationBetaActive ) {
 			message = [
-				/* translators: 1: link opening tag, 2: link closing tag */
-				__( "Sorry! Something went wrong while loading the recalibrated analysis beta! Please deactivate the recalibration beta in your features.", "wordpress-seo" ),
+				interpolateComponents( {
+					/* Translators: {{link}} resolves to the link to SEO/Features, {{/link}} resolves to the link closing tag. */
+					mixedString: __( "Sorry! Something went wrong while loading the recalibrated analysis beta! {{link}}Please deactivate the recalibration beta in your features.{{/link}}", "wordpress-seo" ),
+					components: {
+						link: <a href='/wp-admin/admin.php?page=wpseo_dashboard#top#features' target='_blank' />,
+					},
+				} ),
 			];
 		} else {
 			message.push( "Sorry! Something went wrong while loading the analysis! If the problem persists please inform us about this error -copy-error-link-."  );
