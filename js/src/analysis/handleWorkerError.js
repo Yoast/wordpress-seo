3b54d60d0537d987e6d300bb2a25f9511212177a
Natalia <natalia@yoast.com>
2018-11-29 17:30:24 +0100

    Fix React dependency and add a test for handleWorkerError

--- js/src/analysis/handleWorkerError.js
+++ js/src/analysis/handleWorkerError.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { __ } from "@wordpress/i18n";
 import interpolateComponents from "interpolate-components";
 import { setWarningMessage } from "../redux/actions/warning";
@@ -8,17 +9,15 @@ export default function handleWorkerError( error ) {
 
 	let message = [];
 	if ( isRecalibrationBetaActive ) {
-		message = [
-			interpolateComponents( {
+		message = interpolateComponents( {
 				/* Translators: {{link}} resolves to the link to SEO/Features, {{/link}} resolves to the link closing tag. */
 				mixedString: __( "Sorry! Something went wrong while loading the recalibrated analysis beta! {{link}}Please deactivate the recalibration beta in your features.{{/link}}", "wordpress-seo" ),
 				components: {
 					link: <a href='/wp-admin/admin.php?page=wpseo_dashboard#top#features' target='_blank' />,
 				},
-			} ),
-		];
+			} );
 	} else {
-		message.push( "Sorry! Something went wrong while loading the analysis! If the problem persists please inform us about this error -copy-error-link-."  );
+		message.push( "Sorry! Something went wrong while loading the analysis! If the problem persists please inform us about this error."  );
 	}
 	YoastSEO.store.dispatch( setWarningMessage( message ) );
 }
---
+++ js/tests/analysis/handleWorkerError.test.js
@@ -0,0 +1,53 @@
+import React from "react";
+import handleWorkerError from "../../src/analysis/handleWorkerError";
+
+describe( "handleWorkerError for the Recalibrated version", () => {
+	beforeEach( () => {
+		window.wpseoPostScraperL10n = {
+			recalibrationBetaActive: "1",
+		};
+	} );
+
+	test( "dispatches a recalibration-specific warning", () => {
+		window.YoastSEO = {
+			store: {
+				dispatch: jest.fn(),
+			},
+		};
+
+		handleWorkerError( "error message" );
+
+		expect( YoastSEO.store.dispatch ).toHaveBeenCalledTimes( 1 );
+		expect( YoastSEO.store.dispatch ).toHaveBeenCalledWith( {
+			type: "SET_WARNING_MESSAGE",
+			message: [
+				"Sorry! Something went wrong while loading the recalibrated analysis beta! ",
+				<a href="/wp-admin/admin.php?page=wpseo_dashboard#top#features" target="_blank">Please deactivate the recalibration beta in your features.</a>
+			]
+		} );
+	} );
+} );
+
+describe( "handleWorkerError for the non-Recalibrated version", () => {
+	beforeEach( () => {
+		window.wpseoPostScraperL10n = {
+			recalibrationBetaActive: "",
+		};
+	} );
+
+	test( "dispatches a non-recalibration specific warning", () => {
+		window.YoastSEO = {
+			store: {
+				dispatch: jest.fn(),
+			},
+		};
+
+		handleWorkerError( "error message" );
+
+		expect( YoastSEO.store.dispatch ).toHaveBeenCalledTimes( 1 );
+		expect( YoastSEO.store.dispatch ).toHaveBeenCalledWith( {
+			type: "SET_WARNING_MESSAGE",
+			message: ["Sorry! Something went wrong while loading the analysis! If the problem persists please inform us about this error."],
+		} );
+	} );
+} );
