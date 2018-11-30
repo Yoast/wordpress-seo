c15b20f144444d3519874527771a93b23f80e0be
Dieter <gargamelk@hotmail.com>
2018-11-30 13:53:24 +0100

    Moved cards to yoast-components

--- js/src/courses-overview.js
+++ js/src/courses-overview.js
@@ -1,7 +1,6 @@
 import { render } from "@wordpress/element";
 import styled from "styled-components";
-import { FullHeightCard } from "./components/courses/Card";
-import CardDetails from "./components/courses/CardDetails";
+import { CardDetails, FullHeightCard } from "yoast-components";
 
 const element = document.getElementById( "yoast-courses-overview" );
 
@@ -23,7 +22,7 @@ const CourseListItem = styled.li`
 const courses = [
 	{
 		title: "SEO for Beginners",
-		image: "../wp-content/plugins/wordpress-seo/js/src/components/courses/sample_course_card_header.png",
+		image: "../wp-content/plugins/wordpress-seo/images/sample_course_card_header.png",
 		description: "In this free course, you'll get quick wins to make your site rank higher in Google, Bing, and Yahoo.",
 		courseUrl: "https://yoast.com/academy/keyword-research-training/",
 		shopUrl: "https://yoast.com/cart/?add-to-cart=1311259",
@@ -33,7 +32,7 @@ const courses = [
 	},
 	{
 		title: "Keyword Research",
-		image: "../wp-content/plugins/wordpress-seo/js/src/components/courses/sample_course_card_header.png",
+		image: "../wp-content/plugins/wordpress-seo/images/sample_course_card_header.png",
 		description: "Do you know the essential first step of good SEO? It’s keyword research. In this training, you’ll learn how to research and select the keywords that will guide searchers to your pages.",
 		courseUrl: "https://yoast.com/academy/keyword-research-training/",
 		shopUrl: "https://yoast.com/cart/?add-to-cart=1311259",
@@ -43,7 +42,7 @@ const courses = [
 	},
 	{
 		title: "Structured Data",
-		image: "../wp-content/plugins/wordpress-seo/js/src/components/courses/sample_course_card_header.png",
+		image: "../wp-content/plugins/wordpress-seo/images/sample_course_card_header.png",
 		description: "Get your site ready for rich search results that bring in loads of customers. Make your site stand out from the crowd by adding structured data!",
 		courseUrl: "https://yoast.com/academy/keyword-research-training/",
 		shopUrl: "https://yoast.com/cart/?add-to-cart=1311259",
