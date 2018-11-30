21d324c07ad23b69a48326d96f45c838e46c29bc
Natalia <natalia@yoast.com>
2018-11-30 09:37:24 +0100

    Revert changes in the worker

--- js/src/analysis/worker.js
+++ js/src/analysis/worker.js
@@ -3,8 +3,6 @@ import get from "lodash/get";
 import isUndefined from "lodash/isUndefined";
 import merge from "lodash/merge";
 import { AnalysisWorkerWrapper, createWorker } from "yoastseo";
-import { __, sprintf } from "@wordpress/i18n";
-import interpolateComponents from "interpolate-components";
 
 // Internal dependencies.
 import getContentLocale from "./getContentLocale";
@@ -12,39 +10,15 @@ import getDefaultQueryParams from "./getDefaultQueryParams";
 import getTranslations from "./getTranslations";
 import isContentAnalysisActive from "./isContentAnalysisActive";
 import isKeywordAnalysisActive from "./isKeywordAnalysisActive";
-import { setWarningMessage } from "../redux/actions/warning";
-import getL10nObject from "./getL10nObject";
 
 /**
  * Instantiates an analysis worker (wrapper).
  *
- * @param {Function} dispatch Function that dispatches an action.
- *
  * @returns {AnalysisWorkerWrapper} The analysis worker.
  */
-export function createAnalysisWorker( dispatch ) {
-	try {
-		const url = get( window, [ "wpseoAnalysisWorkerL10n", "url" ], "wp-seo-analysis-worker.js" );
-		return new AnalysisWorkerWrapper( createWorker( url ) );
-	} catch( error ) {
-		const isRecalibrationBetaActive = getL10nObject().recalibrationBetaActive;
-
-		let message = [];
-		if ( isRecalibrationBetaActive ) {
-			message = [
-				interpolateComponents( {
-					/* Translators: {{link}} resolves to the link to SEO/Features, {{/link}} resolves to the link closing tag. */
-					mixedString: __( "Sorry! Something went wrong while loading the recalibrated analysis beta! {{link}}Please deactivate the recalibration beta in your features.{{/link}}", "wordpress-seo" ),
-					components: {
-						link: <a href='/wp-admin/admin.php?page=wpseo_dashboard#top#features' target='_blank' />,
-					},
-				} ),
-			];
-		} else {
-			message.push( "Sorry! Something went wrong while loading the analysis! If the problem persists please inform us about this error -copy-error-link-."  );
-		}
-		dispatch( setWarningMessage( message ) );
-	}
+export function createAnalysisWorker() {
+	const url = get( window, [ "wpseoAnalysisWorkerL10n", "url" ], "wp-seo-analysis-worker.js" );
+	return new AnalysisWorkerWrapper( createWorker( url ) );
 }
 
 /**
