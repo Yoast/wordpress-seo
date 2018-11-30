e6aa97834dc6bc0a6c9c93f772a248268e4191e8
Natalia <natalia@yoast.com>
2018-11-30 12:17:29 +0100

    Add new feedback strings

--- js/src/analysis/handleWorkerError.js
+++ js/src/analysis/handleWorkerError.js
@@ -16,20 +16,29 @@ export default function handleWorkerError() {
 	let message = [];
 	if ( isRecalibrationBetaActive ) {
 		message = interpolateComponents( {
-			/* Translators: {{link}} resolves to the link to SEO/Features, {{/link}} resolves to the link closing tag. */
+			/* Translators:
+			 * {{link}} resolves to the link to SEO/Features,
+			 * {{/link}} resolves to the link closing tag,
+			 * {{bold}} resolves to a bold opening tag (<b>),
+			 * {{/bold}} resolves to a bold closing tag.
+			 */
 			mixedString: __(
-				"Sorry! Something went wrong while loading the recalibrated analysis beta! " +
-				"{{link}}Please deactivate the recalibration beta in your features.{{/link}}",
+				"We're sorry! Unfortunately, the recalibrated analysis beta doesn't work as intended with your current " +
+				"setup {{bold}}yet{{/bold}}. " +
+				"Please {{link}}deactivate the recalibration beta in your features{{/link}} and please try again later. " +
+				"We value your input!",
 				"wordpress-seo"
 			),
 			components: {
+				bold: <b />,
 				link: <a href="/wp-admin/admin.php?page=wpseo_dashboard#top#features" target="_blank" />,
 			},
 		} );
 	} else {
 		message.push(
 			__(
-				"Sorry! Something went wrong while loading the analysis! If the problem persists please inform us about this error.",
+				"Sorry! Something went wrong while loading the analysis! If the problem persists please inform us about " +
+				"this error. Thanks!",
 				"wordpress-seo"
 			)
 		);
