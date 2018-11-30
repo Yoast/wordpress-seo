fd3678c6cea82fa5c409a7b4d11470d68f03d534
Natalia <natalia@yoast.com>
2018-11-30 13:59:49 +0100

    Add tests to the redux reducer and action

---
+++ js/tests/redux/reducers/warning.test.js
@@ -0,0 +1,22 @@
+/* global describe, it, expect */
+
+import { setWarningMessage } from "../../../src/redux/actions/warning";
+import warning from "../../../src/redux/reducers/warning";
+
+describe( "cornerstone reducers", () => {
+	describe( "cornerstoneContentReducer on receiving the TOGGLE_CORNERSTONE_CONTENT action", () => {
+		it( "should set isCornerstoneContent's current value to true if it was false", () => {
+			const state = {
+				key: "value",
+			};
+			const action = setWarningMessage( [ "test message" ] );
+			const expected = {
+				key: "value",
+				message: [ "test message" ]
+			};
+			const actual = warning( state, action );
+
+			expect( actual ).toEqual( expected );
+		} );
+	} );
+} );
