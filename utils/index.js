dc0c7d2de4153232a721fa4ba6c354d19fbd3605
Alexander Botteram <a.botteram@gmail.com>
2018-12-03 15:15:36 +0100

    Expose buildStructuredUrl in main index.js

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,9 @@
 import { makeOutboundLink } from "./makeOutboundLink";
 import getFeed from "./getFeed";
+import buildStructuredUrl from "./buildStructuredUrl";
 
 export default {
 	getFeed,
 	makeOutboundLink,
+	buildStructuredUrl,
 };
