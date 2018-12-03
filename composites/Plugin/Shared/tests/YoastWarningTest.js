00aeda9b439073efc7d6aad2245fbc6f65b1dda8
Igor <igor@yoast.com>
2018-12-03 15:13:53 +0100

    Add RTL test

--- composites/Plugin/Shared/tests/YoastWarningTest.js
+++ composites/Plugin/Shared/tests/YoastWarningTest.js
@@ -1,6 +1,7 @@
 import React from "react";
 import renderer from "react-test-renderer";
 import YoastWarning from "../components/YoastWarning";
+import { ThemeProvider } from "styled-components";
 
 test( "YoastWarning matches the snapshot", () => {
 	const component = renderer.create(
@@ -25,4 +26,21 @@ test( "YoastWarning does not render without a message", () => {
 	const tree = component.toJSON();
 	expect( tree ).toMatchSnapshot();
 	expect( tree ).toBeNull();
+} );
+
+test( "YoastWarning with a right to left language matches the snapshot", () => {
+	const component = renderer.create(
+		<ThemeProvider theme={ { isRtl: true } }>
+			<YoastWarning
+				message={ [
+					"This is a warning message",
+					<a key={ 1 } href="https://yoast.com" target="_blank">Go to Yoast.com</a>,
+					<p key={ 2 }>React will render this</p>,
+				] }
+			/>
+		</ThemeProvider>
+	);
+
+	const tree = component.toJSON();
+	expect( tree ).toMatchSnapshot();
 } );
