6dc168b1e93d5333731e50a56e20d9937943b36a
nataliashitova <36954173+nataliashitova@users.noreply.github.com>
2018-12-04 12:01:16 +0100

    Update StructuredIrrelevantSpec.js

--- spec/tree/values/nodes/StructuredIrrelevantSpec.js
+++ spec/tree/values/nodes/StructuredIrrelevantSpec.js
@@ -1,7 +1,7 @@
 import StructuredIrrelevant from "../../../../src/tree/values/nodes/StructuredIrrelevant";
 
 describe( "StructuredIrrelevant", () => {
-	it( "can make an StructuredIrrelevant node", () => {
+	it( "can make a StructuredIrrelevant node", () => {
 		const nodeContents = "<script> console.log('hey!'); </script>";
 		const structuredIrrelevantNode = new StructuredIrrelevant( nodeContents );
 		expect( structuredIrrelevantNode.content ).toEqual( nodeContents );
