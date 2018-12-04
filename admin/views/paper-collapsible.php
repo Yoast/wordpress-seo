397390e7d9f3e30c47e2c61db5a04f2b09a6ece7
Alexander Botteram <a.botteram@gmail.com>
2018-12-04 15:21:11 +0100

    Optionally render id attribute in paper-collapsible.php

--- admin/views/paper-collapsible.php
+++ admin/views/paper-collapsible.php
@@ -19,7 +19,7 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 	exit();
 }
 ?>
-<div class="paper tab-block" id="<?php if ( $paper_id ) { echo esc_attr( 'wpseo-' . $paper_id ); } ?>">
+<div class="paper tab-block" <?php if ( $paper_id ) { echo 'id="' . esc_attr( 'wpseo-' . $paper_id ) . '""'; } ?>>
 
 	<?php
 	if ( ! empty( $title ) ) {
