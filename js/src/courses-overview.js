3a24aa407aa8510c8a5fc3c5310e17b4b3551367
Dieter <gargamelk@hotmail.com>
2018-12-04 14:56:56 +0100

    Replaced hard coded courses with reading out the Courses RSS feed

--- js/src/courses-overview.js
+++ js/src/courses-overview.js
@@ -1,8 +1,9 @@
 import { render } from "@wordpress/element";
 import styled from "styled-components";
-import { CardDetails, FullHeightCard } from "yoast-components";
-
-const element = document.getElementById( "yoast-courses-overview" );
+import { CardDetails, FullHeightCard, utils } from "yoast-components";
+import React from "react";
+import ReactDOM from "react-dom";
+const { getFeed } = utils;
 
 const OuterContainer = styled.ul`
 	display: grid;
@@ -19,76 +20,84 @@ const CourseListItem = styled.li`
 	width: 100%;
 `;
 
-const courses = [
-	{
-		title: "SEO for Beginners",
-		image: "../wp-content/plugins/wordpress-seo/images/sample_course_card_header.png",
-		description: "In this free course, you'll get quick wins to make your site rank higher in \n" +
-		             "Google, Bing, and Yahoo.",
-		courseUrl: "https://yoast.com/academy/keyword-research-training/",
-		shopUrl: "https://yoast.com/cart/?add-to-cart=1311259",
-		banner: {
-			text: "Free",
-		},
-	},
-	{
-		title: "Keyword Research",
-		image: "../wp-content/plugins/wordpress-seo/images/sample_course_card_header.png",
-		description: "Do you know the essential first step of good SEO? It’s keyword research. In this training, \n" +
-		             "you’ll learn how to research and select the keywords that will guide searchers to your pages.",
-		courseUrl: "https://yoast.com/academy/keyword-research-training/",
-		shopUrl: "https://yoast.com/cart/?add-to-cart=1311259",
-		banner: {
-			text: "Free trial available",
-		},
-	},
-	{
-		title: "Structured Data",
-		image: "../wp-content/plugins/wordpress-seo/images/sample_course_card_header.png",
-		description: "Get your site ready for rich search results that bring in loads of customers. \n " +
-		             "Make your site stand out from the crowd by adding structured data!",
-		courseUrl: "https://yoast.com/academy/keyword-research-training/",
-		shopUrl: "https://yoast.com/cart/?add-to-cart=1311259",
-		banner: {
-			text: "Free trial available",
-		},
-	},
-];
+class CoursesOverview extends React.Component {
+	/**
+	 * Creates the components and initializes its state.
+	 */
+	constructor() {
+		super();
 
-/**
- * Converts the relevant data in a course to a header object.
- *
- * @param {string} course to create a header for.
- *
- * @returns {Object} the header object.
- */
-const getHeaderData = ( course ) => {
-	return {
-		image: course.image,
-		title: course.title,
-		link: course.courseUrl,
+		this.state = {
+			courses: null,
+		};
+
+		this.getFeed();
+	}
+
+	/**
+	 * Fetches data from the yoast.com feed, parses it and sets it to the state.
+	 *
+	 * @returns {void}
+	 */
+	getFeed() {
+		// Developer note: this link should -not- be converted to a shortlink.
+		getFeed( "https://yoast.com/?feed=courses" )
+			.then( ( feed ) => {
+				feed.items = feed.items.map( ( item ) => {
+					return item;
+				} );
+				this.setState( { courses: feed.items } );
+			} )
+			/* eslint-disable-next-line no-console */
+			.catch( error => console.log( error ) );
+	}
+
+	/**
+	 * Converts the relevant data in a course to a header object.
+	 *
+	 * @param {string} course to create a header for.
+	 *
+	 * @returns {Object} the header object.
+	 */
+	getHeaderData( course ) {
+		return {
+			image: course.image,
+			title: course.title,
+			link: course.link,
+		};
 	};
-};
 
-render(
-	<OuterContainer>
-		{ courses.map( ( course, i ) =>
-			<CourseListItem key={ i }>
-				<FullHeightCard
-					className={ "CourseCard" }
-					id={ i }
-					header={ getHeaderData( course ) }
-					banner={ course.banner }
-				>
-					<CardDetails
-						title={ course.title }
-						description={ course.description }
-						courseUrl={ course.courseUrl }
-						shopUrl={ course.shopUrl }
-					/>
-				</FullHeightCard>
-			</CourseListItem>
-		) }
-	</OuterContainer>,
-	element
-);
+	render() {
+		const courses = this.state.courses;
+		if ( courses === null ) {
+			return null;
+		}
+		return (
+			<OuterContainer>
+				{ courses.map( ( course, i ) =>
+					<CourseListItem key={ i }>
+						<FullHeightCard
+							className={ "CourseCard" }
+							id={ i }
+							header={ this.getHeaderData( course ) }
+							banner={ course.banner }
+						>
+							<CardDetails
+								title={ course.title }
+								description={ course.content }
+								courseUrl={ course.link }
+								shopUrl={ course.shopUrl }
+							/>
+						</FullHeightCard>
+					</CourseListItem>
+				) }
+			</OuterContainer>
+		);
+	}
+}
+
+const element = document.getElementById( "yoast-courses-overview" );
+
+if ( element ) {
+	ReactDOM.render( <CoursesOverview />, element );
+}
