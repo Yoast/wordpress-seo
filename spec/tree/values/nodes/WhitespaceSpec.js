284767573edeec1adff18829eb59671a041a5a08
Natalia <natalia@yoast.com>
2018-12-07 15:07:30 +0100

    Add Node inheritance

--- spec/tree/values/nodes/WhitespaceSpec.js
+++ spec/tree/values/nodes/WhitespaceSpec.js
@@ -2,7 +2,11 @@ import Whitespace from "../../../../src/tree/values/nodes/Whitespace";
 
 describe( "Whitespace", () => {
 	it( "can make a Whitespace node", () => {
-		const whitespaceElement = new Whitespace( "\t     \n" );
+		const whitespaceElement = new Whitespace( 0, 8, "\t     \n" );
+
+		expect( whitespaceElement.type ).toEqual( "whitespace" );
+		expect( whitespaceElement.startIndex ).toEqual( 0 );
+		expect( whitespaceElement.endIndex ).toEqual( 8 );
 		expect( whitespaceElement.content ).toEqual( "\t     \n" );
 	} );
 } );
--- src/tree/values/nodes/Whitespace.js
+++ src/tree/values/nodes/Whitespace.js
@@ -1,15 +1,19 @@
+import Node from "./Node.js";
 /**
  * Represents a piece of whitespace that cannot be represented by any other structured element.
  */
-class Whitespace {
+class Whitespace extends Node {
 	/**
 	 * Represents a piece of whitespace that cannot be represented by any other structured element.
 	 *
-	 * @param {string} content The type of the whitespace (e.g., any combination of tabs, spaces, line-feeds).
+	 * @param {number} startIndex	The index in the original text-string where this Whitespace begins.
+	 * @param {number} endIndex	    The index in the original text-string where this Whitespace ends.
+	 * @param {string} content      The content of the Whitespace (e.g., any combination of tabs, spaces, line-feeds).
 	 *
 	 * @returns {void}
 	 */
-	constructor( content ) {
+	constructor( startIndex, endIndex, content ) {
+		super( "whitespace", startIndex, endIndex );
 		this.content = content;
 	}
 }
