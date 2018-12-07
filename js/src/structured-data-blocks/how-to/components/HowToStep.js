2161432bbfd8d76fca10306660d6f103381fcbff
Alexander Botteram <a.botteram@gmail.com>
2018-12-07 14:05:57 +0100

    Implemented shouldComponentUpdate in HowToStep and removed function declarations from render functions

--- js/src/structured-data-blocks/how-to/components/HowToStep.js
+++ js/src/structured-data-blocks/how-to/components/HowToStep.js
@@ -2,6 +2,7 @@
 import PropTypes from "prop-types";
 import { __ } from "@wordpress/i18n";
 import appendSpace from "../../../components/higherorder/appendSpace";
+import isShallowEqual from "@wordpress/is-shallow-equal/objects";
 
 const { Component } = window.wp.element;
 const { IconButton } = window.wp.components;
@@ -24,19 +25,169 @@ export default class HowToStep extends Component {
 	constructor( props ) {
 		super( props );
 
-		this.onSelectImage = this.onSelectImage.bind( this );
+		this.onSelectImage  = this.onSelectImage.bind( this );
+		this.onInsertStep   = this.onInsertStep.bind( this );
+		this.onRemoveStep   = this.onRemoveStep.bind( this );
+		this.onMoveStepUp   = this.onMoveStepUp.bind( this );
+		this.onMoveStepDown = this.onMoveStepDown.bind( this );
+		this.setTextRef     = this.setTextRef.bind( this );
+		this.setTitleRef    = this.setTitleRef.bind( this );
+		this.onFocusText    = this.onFocusText.bind( this );
+		this.onFocusTitle   = this.onFocusTitle.bind( this );
+		this.onChangeTitle  = this.onChangeTitle.bind( this );
+		this.onChangeText   = this.onChangeText.bind( this );
+	}
+
+	/**
+	 * Handles the insert step button action.
+	 *
+	 * @returns {void}
+	 */
+	onInsertStep() {
+		this.props.insertStep( this.props.index );
+	}
+
+	/**
+	 * Handles the remove step button action.
+	 *
+	 * @returns {void}
+	 */
+	onRemoveStep() {
+		this.props.removeStep( this.props.index );
+	}
+
+	/**
+	 * Handles the move step up button action.
+	 *
+	 * @returns {void}
+	 */
+	onMoveStepUp() {
+		if ( this.props.isFirst ) {
+			return;
+		}
+		this.props.onMoveUp( this.props.index );
+	}
+
+	/**
+	 * Handles the move step down button action.
+	 *
+	 * @returns {void}
+	 */
+	onMoveStepDown() {
+		if ( this.props.isLast ) {
+			return;
+		}
+		this.props.onMoveDown( this.props.index );
+	}
+
+	/**
+	 * Pass the title editor reference down to the parent component.
+	 *
+	 * @param {object} ref Reference to the title editor.
+	 *
+	 * @returns {void}
+	 */
+	setTitleRef( ref ) {
+		this.props.editorRef( this.props.index, "name", ref );
+	}
+
+	/**
+	 * Pass the text editor reference down to the parent component.
+	 *
+	 * @param {object} ref Reference to the text editor.
+	 *
+	 * @returns {void}
+	 */
+	setTextRef( ref ) {
+		this.props.editorRef( this.props.index, "text", ref );
+	}
+
+	/**
+	 * Handles the focus event on the title editor.
+	 *
+	 * @returns {void}
+	 */
+	onFocusTitle() {
+		this.props.onFocus( this.props.index, "name" );
+	}
+
+	/**
+	 * Handles the focus event on the text editor.
+	 *
+	 * @returns {void}
+	 */
+	onFocusText() {
+		this.props.onFocus( this.props.index, "text" );
+	}
+
+	/**
+	 * Handles the on change event on the title editor.
+	 *
+	 * @param {string} value The new title.
+	 *
+	 * @returns {void}
+	 */
+	onChangeTitle( value ) {
+		const {
+			onChange,
+			index,
+			step: {
+				text,
+				name,
+			},
+		} = this.props;
+
+		onChange( value, text, name, text, index );
+	}
+
+	/**
+	 * Handles the on change event on the text editor.
+	 *
+	 * @param {string} value The new text.
+	 *
+	 * @returns {void}
+	 */
+	onChangeText( value ) {
+		const {
+			onChange,
+			index,
+			step: {
+				text,
+				name,
+			},
+		} = this.props;
+
+		onChange( name, value, name, text, index );
+	}
+
+	/**
+	 * Renders the media upload button.
+	 *
+	 * @param {object} props      The receive props.
+	 * @param {func}   props.open Opens the media upload dialog.
+	 *
+	 * @returns {ReactElement} The media upload button.
+	 */
+	getMediaUploadButton( props ) {
+		return (
+			<IconButton
+				className="schema-how-to-step-button editor-inserter__toggle how-to-step-add-media"
+				icon="insert"
+				onClick={ props.open }
+			>
+				{ __( "Add image", "wordpress-seo" ) }
+			</IconButton>
+		);
 	}
 
 	/**
 	 * The insert and remove step buttons.
 	 *
-	 * @returns {Component} The buttons.
+	 * @returns {ReactElement} The buttons.
 	 */
 	getButtons() {
 		const {
 			step,
-			removeStep,
-			insertStep,
 		} = this.props;
 
 		return <div className="schema-how-to-step-button-container">
@@ -45,28 +196,20 @@ export default class HowToStep extends Component {
 				onSelect={ this.onSelectImage }
 				type="image"
 				value={ step.id }
-				render={ ( { open } ) => (
-					<IconButton
-						className="schema-how-to-step-button editor-inserter__toggle how-to-step-add-media"
-						icon="insert"
-						onClick={ open }
-					>
-						{ __( "Add image", "wordpress-seo" ) }
-					</IconButton>
-				) }
+				render={ this.getMediaUploadButton }
 			/>
 			}
 			<IconButton
 				className="schema-how-to-step-button editor-inserter__toggle"
 				icon="trash"
 				label={ __( "Delete step", "wordpress-seo" ) }
-				onClick={ removeStep }
+				onClick={ this.onRemoveStep }
 			/>
 			<IconButton
 				className="schema-how-to-step-button editor-inserter__toggle"
 				icon="insert"
 				label={ __( "Insert step", "wordpress-seo" ) }
-				onClick={ insertStep }
+				onClick={ this.onRemoveStep }
 			/>
 		</div>;
 	}
@@ -80,14 +223,14 @@ export default class HowToStep extends Component {
 		return <div className="schema-how-to-step-mover">
 			<IconButton
 				className="editor-block-mover__control"
-				onClick={ this.props.isFirst ? null : this.props.onMoveUp }
+				onClick={ this.onMoveStepUp }
 				icon="arrow-up-alt2"
 				label={ __( "Move step up", "wordpress-seo" ) }
 				aria-disabled={ this.props.isFirst }
 			/>
 			<IconButton
 				className="editor-block-mover__control"
-				onClick={ this.props.isLast ? null : this.props.onMoveDown }
+				onClick={ this.onMoveStepDown }
 				icon="arrow-down-alt2"
 				label={ __( "Move step down", "wordpress-seo" ) }
 				aria-disabled={ this.props.isLast }
@@ -139,12 +282,27 @@ export default class HowToStep extends Component {
 	}
 
 	/**
+	 * Perform a shallow equal to prevent every step from being rerendered.
+	 *
+	 * @param {object} nextProps The next props the component will receive.
+	 *
+	 * @returns {boolean} Whether or not the component should perform an update.
+	 */
+	shouldComponentUpdate( nextProps ) {
+		if ( ! isShallowEqual( nextProps, this.props ) ) {
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
 	 * Returns the component of the given How-to step to be rendered in a WordPress post
 	 * (e.g. not in the editor).
 	 *
 	 * @param {object} step The how-to step.
 	 *
-	 * @returns {Component} The component to be rendered.
+	 * @returns {ReactElement} The component to be rendered.
 	 */
 	static Content( step ) {
 		return (
@@ -168,17 +326,14 @@ export default class HowToStep extends Component {
 	/**
 	 * Renders this component.
 	 *
-	 * @returns {Component} The how-to step editor.
+	 * @returns {ReactElement} The how-to step editor.
 	 */
 	render() {
 		const {
 			index,
 			step,
-			onChange,
-			onFocus,
 			isSelected,
 			subElement,
-			editorRef,
 			isUnorderedList,
 		} = this.props;
 
@@ -195,26 +350,26 @@ export default class HowToStep extends Component {
 				<RichTextWithAppendedSpace
 					className="schema-how-to-step-name"
 					tagName="p"
-					unstableOnSetup={ ( ref ) => editorRef( "name", ref ) }
+					unstableOnSetup={ this.setTitleRef }
 					key={ `${ id }-name` }
 					value={ name }
-					onChange={ ( value ) => onChange( value, text, name, text ) }
+					onChange={ this.onChangeTitle }
 					isSelected={ isSelected && subElement === "name" }
 					placeholder={ __( "Enter a step title", "wordpress-seo" ) }
-					setFocusedElement={ () => onFocus( "name" ) }
+					setFocusedElement={ this.onFocusTitle }
 					keepPlaceholderOnFocus={ true }
 					formattingControls={ [ "italic", "strikethrough", "link" ] }
 				/>
 				<RichTextWithAppendedSpace
 					className="schema-how-to-step-text"
 					tagName="p"
-					unstableOnSetup={ ( ref ) => editorRef( "text", ref ) }
+					unstableOnSetup={ this.setTextRef }
 					key={ `${ id }-text` }
 					value={ text }
-					onChange={ ( value ) => onChange( name, value, name, text ) }
+					onChange={ this.onChangeText }
 					isSelected={ isSelected && subElement === "text" }
 					placeholder={ __( "Enter a step description", "wordpress-seo" ) }
-					setFocusedElement={ () => onFocus( "text" ) }
+					setFocusedElement={ this.onFocusText }
 					keepPlaceholderOnFocus={ true }
 				/>
 				{ isSelected &&
@@ -238,9 +393,10 @@ HowToStep.propTypes = {
 	editorRef: PropTypes.func.isRequired,
 	onMoveUp: PropTypes.func.isRequired,
 	onMoveDown: PropTypes.func.isRequired,
-	subElement: PropTypes.string,
-	isSelected: PropTypes.bool,
-	isFirst: PropTypes.bool,
-	isLast: PropTypes.bool,
-	isUnorderedList: PropTypes.bool,
+	subElement: PropTypes.string.isRequired,
+	isSelected: PropTypes.bool.isRequired,
+	isFirst: PropTypes.bool.isRequired,
+	isLast: PropTypes.bool.isRequired,
+	isUnorderedList: PropTypes.bool.isRequired,
 };
+
