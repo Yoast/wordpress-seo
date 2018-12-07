5a64757fbbb8fb7a1757558f1a96c0e9cadeed73
Hans-Christiaan <hans-christiaan@yoast.com>
2018-12-07 15:28:35 +0100

    Chsnged code to match new documentation / plan.

--- spec/tree/values/nodes/ParagraphSpec.js
+++ spec/tree/values/nodes/ParagraphSpec.js
@@ -3,34 +3,107 @@ import FormattingElement from "../../../../src/tree/values/FormattingElement";
 import Paragraph from "../../../../src/tree/values/nodes/Paragraph";
 
 describe( "Paragraph tree node", () => {
-	it( "can make a new Paragraph tree node with paragraph opening tags", () => {
-		const formattingElements = [
-			new FormattingElement( "a", 25, 29 ),
-		];
-		const text = "This is some text with a link.";
-		const textElement = new TextContainer( text, formattingElements );
+	describe( "constructor", () => {
+		it( "can make a new Paragraph tree node with paragraph opening tags", () => {
+			const formattingElements = [
+				new FormattingElement( "a", 25, 29 ),
+			];
+			const text = "This is some text with a link.";
+			const textElement = new TextContainer( text, formattingElements );
 
-		const start = "/n/n";
-		const end = "";
+			const startIndex = 0;
+			const endIndex = 30;
+			const tag = "p";
 
-		const paragraphElement = new Paragraph( textElement, start, end );
+			const paragraphElement = new Paragraph( textElement, startIndex, endIndex, tag );
 
-		expect( paragraphElement.textContainer ).toEqual( textElement );
-		expect( paragraphElement.start ).toEqual( "/n/n" );
-		expect( paragraphElement.end ).toEqual( "" );
+			expect( paragraphElement.textContainer ).toEqual( textElement );
+			expect( paragraphElement.startIndex ).toEqual( 0 );
+			expect( paragraphElement.endIndex ).toEqual( 30 );
+		} );
+
+		it( "can make a new Paragraph tree node", () => {
+			const formattingElements = [
+				new FormattingElement( "a", 25, 29 ),
+			];
+			const text = "This is some text with a link.";
+			const textElement = new TextContainer( text, formattingElements );
+
+			const startIndex = 0;
+			const endIndex = 30;
+
+			const paragraphElement = new Paragraph( textElement, startIndex, endIndex );
+
+			expect( paragraphElement.textContainer ).toEqual( textElement );
+			expect( paragraphElement.startIndex ).toEqual( 0 );
+			expect( paragraphElement.endIndex ).toEqual( 30 );
+			expect( paragraphElement.tag ).toEqual( "" );
+		} );
 	} );
 
-	it( "can make a new Paragraph tree node", () => {
-		const formattingElements = [
-			new FormattingElement( "a", 25, 29 ),
-		];
-		const text = "This is some text with a link.";
-		const textElement = new TextContainer( text, formattingElements );
+	describe( "map function", () => {
+		it( "returns a changed paragraph when map is called", () => {
+			const formattingElements = [
+				new FormattingElement( "a", 25, 29 ),
+			];
+			const text = "This is some text with a link";
+			const textElement = new TextContainer( text, formattingElements );
 
-		const paragraphElement = new Paragraph( textElement );
+			const startIndex = 0;
+			const endIndex = 30;
 
-		expect( paragraphElement.textContainer ).toEqual( textElement );
-		expect( paragraphElement.start ).toEqual( "<p>" );
-		expect( paragraphElement.end ).toEqual( "</p>" );
+			const paragraphElement = new Paragraph( textElement, startIndex, endIndex );
+
+			const mappedParagraph = paragraphElement.map( node => {
+				if ( node.type === "paragraph" ) {
+					node.textContainer.text = node.textContainer.text + "!!!!";
+				}
+				return node;
+			} );
+
+			expect( mappedParagraph.textContainer.text ).toEqual( "This is some text with a link!!!!" );
+		} );
+	} );
+
+	describe( "filter function", () => {
+		it( "returns this paragraph in an array when the predicate returns true", () => {
+			const formattingElements = [
+				new FormattingElement( "a", 25, 29 ),
+			];
+			const text = "This is some text with a link";
+			const textElement = new TextContainer( text, formattingElements );
+
+			const startIndex = 0;
+			const endIndex = 30;
+
+			const paragraphElement = new Paragraph( textElement, startIndex, endIndex );
+
+			const filteredParagraph = paragraphElement.filter( node => {
+				// Should return true.
+				return node.startIndex === 0;
+			} );
+
+			expect( filteredParagraph ).toEqual( [ paragraphElement ] );
+		} );
+
+		it( "returns this paragraph in an array when the predicate returns false", () => {
+			const formattingElements = [
+				new FormattingElement( "a", 25, 29 ),
+			];
+			const text = "This is some text with a link";
+			const textElement = new TextContainer( text, formattingElements );
+
+			const startIndex = 0;
+			const endIndex = 30;
+
+			const paragraphElement = new Paragraph( textElement, startIndex, endIndex );
+
+			const filteredParagraph = paragraphElement.filter( node => {
+				// Should return false.
+				return node.startIndex === 15;
+			} );
+
+			expect( filteredParagraph ).toEqual( [ ] );
+		} );
 	} );
 } );
--- src/tree/values/nodes/Paragraph.js
+++ src/tree/values/nodes/Paragraph.js
@@ -1,21 +1,51 @@
+import Node from "./Node";
 /**
- * Represents a paragraph with text within an HTML-document that can be read by a reader.
+ * Represents a paragraph with text within a document.
  */
-class Paragraph {
+class Paragraph extends Node {
 	/**
-	 * A paragraph within an HTML-document that can be read by a reader.
+	 * A paragraph within a document.
 	 *
 	 * @param {TextContainer} textContainer That includes the text without any formatting and phrasing elements if any.
-	 * @param {string} [start = "<p>"] The opening tag/marking of the paragraph.
-	 * @param {string} [end = "</p>"] The closing tag/marking of the paragraph.
+	 * @param {number} startIndex 			Start position of this paragraph in the raw text-string.
+	 * @param {number} endIndex			End position of this paragraph in the raw text-string.
+	 * @param {string} [tag=""]			Optional tag to use for opening / closing this paragraph.
 	 */
-	constructor( textContainer, start = "<p>", end = "</p>" ) {
+	constructor( textContainer, startIndex, endIndex, tag = "" ) {
+		super( "paragraph", startIndex, endIndex );
 		// The text with markup inside the paragraph.
 		this.textContainer = textContainer;
-		// The opening tag/marking of the paragraph.
-		this.start = start;
-		// The closing tag/marking of the paragraph.
-		this.end = end;
+		this.tag = tag;
+	}
+
+	/**
+	 * Maps the given function to this Paragraph.
+	 *
+	 * @param {mapCallback} callback	The function that should be mapped to this Paragraph.
+	 * @returns {Node} A clone of this Paragraph after the given function has been applied to it.
+	 */
+	map( callback ) {
+		const clone = new Paragraph(
+			this.textContainer,
+			this.startIndex,
+			this.endIndex,
+			this.tag
+		);
+		return callback( clone );
+	}
+
+	/**
+	 * Filters all the elements out of the tree for which the given predicate function returns `false`
+	 * and returns them as an array of Nodes.
+	 *
+	 * @param {filterCallback} predicate		The predicate to check each Node against.
+	 * @returns {Node[]} An array with this Paragraph, if the predicate returns true for this Paragraph, an empty array if not.
+	 */
+	filter( predicate ) {
+		if ( predicate( this ) ) {
+			return [ this ];
+		}
+		return [ ];
 	}
 }
 
