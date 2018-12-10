1d1452efae885d0880e44b745b620c616d96a775
Hans-Christiaan <hans-christiaan@yoast.com>
2018-12-10 10:41:17 +0100

    Added tests for map and filter function.

--- spec/tree/values/nodes/StructuredNodeSpec.js
+++ spec/tree/values/nodes/StructuredNodeSpec.js
@@ -14,4 +14,44 @@ describe( "StructuredNode", () => {
 		expect( structuredTextNode.children ).toEqual( children );
 		expect( structuredTextNode.tag ).toEqual( tag );
 	} );
+
+	describe( "map and filter function", () => {
+		let structuredNode;
+
+		beforeEach( () => {
+			const startIndex = 0;
+			const endIndex = 5;
+			const children = [
+				new StructuredNode( 8, 12, [
+					new StructuredNode( 16, 18, [], "div" ),
+				], "section" ),
+				new StructuredNode( 16, 18, [], "nav" ),
+			];
+			const tag = "div";
+
+			structuredNode = new StructuredNode( startIndex, endIndex, children, tag );
+		} );
+
+		it( "can apply a mapping function to a tree with multiple nodes.", () => {
+			const nodeAfterMapping = structuredNode.map( node => {
+				node.endIndex = 666;
+				return node;
+			} );
+
+			expect( nodeAfterMapping.endIndex ).toEqual( 666 );
+			expect( nodeAfterMapping.children[ 0 ].endIndex ).toEqual( 666 );
+			expect( nodeAfterMapping.children[ 1 ].endIndex ).toEqual( 666 );
+			expect( nodeAfterMapping.children[ 0 ].children[ 0 ].endIndex ).toEqual( 666 );
+		} );
+
+		it( "can apply a filter function to a tree with multiple nodes.", () => {
+			const nodeAfterFiltering = structuredNode.filter( node => {
+				return node.startIndex === 16;
+			} );
+
+			expect( nodeAfterFiltering.length ).toEqual( 2 );
+			expect( nodeAfterFiltering[ 0 ].tag ).toEqual( "div" );
+			expect( nodeAfterFiltering[ 1 ].tag ).toEqual( "nav" );
+		} );
+	} );
 } );
