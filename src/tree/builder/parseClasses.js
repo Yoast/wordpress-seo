63e0a384867a14bf0d095c868f5e43ec42daf6f5
Hans-Christiaan <hans-christiaan@yoast.com>
2018-12-11 11:32:02 +0100

    Removed clone, map and filter functions for later implementation (with more context).

---
+++ src/tree/builder/parseClasses.js
@@ -0,0 +1,12 @@
+/**
+ * The html-elements that are irrelevant for further analysis.
+ * @type {string[]}
+ */
+export const irrelevantTypes = [ "code", "style", "link" ];
+
+/**
+ * The html-element that should be parsed as formatting elements ("phrasing content").
+ * @type {string[]}
+ */
+export const formattingTypes = [ "a", "b", "emph" ];
+
--- src/tree/values/nodes/StructuredNode.js
+++ src/tree/values/nodes/StructuredNode.js
@@ -19,74 +19,10 @@ class StructuredNode extends Node {
 	 *
 	 * @returns {void}
 	 */
-	constructor( startIndex, endIndex, children, tag  ) {
+	constructor( startIndex, endIndex, children = [], tag  ) {
 		super( "structuredNode", startIndex, endIndex );
 		this.children = children;
 		this.tag = tag;
-	}
-
-	/**
-	 * Clones this StructuredNode.
-	 *
-	 * @returns {Node} The cloned Node.
-	 */
-	clone() {
-		return new StructuredNode(
-			this.startIndex,
-			this.endIndex,
-			this.children,
-			this.tag
-		);
-	}
-
-	/**
-	 * Maps the given function to each Node in this tree.
-	 *
-	 * @param {mapCallback} mappingFunction 	The function that should be mapped to each Node in the tree.
-	 *
-	 * @returns {Node} A new tree, after the given function has been mapped on each Node.
-	 *
-	 * @abstract
-	 */
-	map( mappingFunction ) {
-		// Clone this node to avoid changing the original.
-		let clone = this.clone();
-
-		// Apply mapping function to clone.
-		clone = mappingFunction( clone );
-		// Apply mapping function to each child.
-		if ( clone.children && clone.children.length > 0 ) {
-			clone.children = clone.children.map(
-				node => node.map( mappingFunction )
-			);
-		}
-
-		return clone;
-	}
-
-	/**
-	 * Filters all the elements out of the tree for which the given predicate function returns `false`
-	 * and returns them as an array of Nodes.
-	 *
-	 * @param {filterCallback} filterFunction 	The predicate to check each Node against.
-	 *
-	 * @returns {Node[]} An array of all the Nodes in the tree for which the given predicate function returns `true`.
-	 *
-	 * @abstract
-	 */
-	filter( filterFunction ) {
-		// Clone this Node.
-		const clone = this.clone();
-
-		// Apply filter function to this Node.
-		const filtered = filterFunction( clone ) ? [ clone ] : [];
-		// Apply filter function to each child and concatenate the results.
-		if ( clone.children && clone.children.length > 0 ) {
-			return this.children.reduce( ( filterArray, child ) => {
-				return filterArray.concat( child.filter( filterFunction ) );
-			}, filtered );
-		}
-		return filtered;
 	}
 }
 export default StructuredNode;
