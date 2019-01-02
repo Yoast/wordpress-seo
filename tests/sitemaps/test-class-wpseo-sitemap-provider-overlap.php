28f9d41d6c33deb1eb5bb36cbaf740cc1866104d
Jip <jhp.moors@gmail.com>
2019-01-02 08:45:05 +0100

    Add test to check for author entry in author sitemap

--- tests/sitemaps/test-class-wpseo-sitemap-provider-overlap.php
+++ tests/sitemaps/test-class-wpseo-sitemap-provider-overlap.php
@@ -59,5 +59,32 @@ class Test_WPSEO_Sitemap_Provider_Overlap extends WPSEO_UnitTestCase {
 		$this->expectOutputContains(
 			'<loc>http://example.org/author-sitemap.xml</loc>'
 		);
+
+		unregister_taxonomy( 'author' );
+	}
+
+	/**
+	 * Makes sure the private taxonomy "author" does not override the "Author" sitemap.
+	 */
+	public function test_private_taxonomy_author_overlap_author_in_sitemap() {
+		// Create private taxonomy "author", overlapping the "author" sitemap.
+		register_taxonomy( 'author', array( 'post' ), array( 'public' => false ) );
+
+		// Create a user with a post.
+		$user_id = $this->factory->user->create( array( 'role' => 'author' ) );
+		$this->factory->post->create_many( 1, array( 'post_author' => $user_id ) );
+
+		// Fetch the global sitemap.
+		set_query_var( 'sitemap', 'author' );
+
+		// Load the sitemap.
+		self::$class_instance->redirect( $GLOBALS['wp_the_query'] );
+
+		// Expect the author-sitemap to be present in the index.
+		$this->expectOutputContains(
+			'<loc>' . get_author_posts_url( $user_id ) . '</loc>'
+		);
+
+		unregister_taxonomy( 'author' );
 	}
 }
