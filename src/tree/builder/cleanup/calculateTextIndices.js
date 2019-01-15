9acae90f26a9ad13e94796b3364fac9fc33db4a6
Hans-Christiaan <hans-christiaan@yoast.com>
2019-01-15 12:52:55 +0100

    Refactored calculateTestIndices some more.

--- src/tree/builder/cleanup/calculateTextIndices.js
+++ src/tree/builder/cleanup/calculateTextIndices.js
@@ -1,19 +1,9 @@
-import { pull } from "lodash-es";
+import { pullAll } from "lodash-es";
 
-import getElementContent from "./getElementContent";
 import { ignoredHtmlElements } from "../htmlConstants";
 
-const gatherElementsToBeClosed = function( currentElement, openElements ) {
-
-};
-
 /**
- * Closes the elements that can be closed given the position of the current element within the source code.
- *
- * This does two things:
- *  1. The closed element's text end index is calculated based on the current offset.
- *  2. The closed element's end tag lengths are counted towards the current offset, to make sure that the computed position
- * of the formatting elements are still correct.
+ * Gathers all elements that can be closed given the position of the current element in the source code.
  *
  * Elements that can be closed are all elements that are opened before this element, but in which this element is
  * not nested.
@@ -24,41 +14,71 @@ const gatherElementsToBeClosed = function( currentElement, openElements ) {
  * with `<b>!!!</b>` as the current element,
  * means that the `<strong>` needs to be closed, but `<em>` **not**.
  *
- * @param {module:tree/structure.FormattingElement} currentElement The current element of which the text position is parsed
- * @param {Object[]} openElements                                    The current list of open elements (of which some will be closed)
- * @param {number} currentOffset                                   The current offset when parsing the formatting elements
+ * @param {module:tree/structure.FormattingElement} currentElement The current element.
+ * @param {module:tree/structure.FormattingElement[]} openElements The elements that are currently open.
+ *
+ * @returns {module:tree/structure.FormattingElement[]} The elements that can be closed.
+ */
+const elementsThatCanBeClosed = function( currentElement, openElements ) {
+	return openElements.filter( el => {
+		const endTag = el.location.endTag;
+		return endTag.endOffset <= currentElement.location.startOffset;
+	} );
+};
+
+/**
+ * Closes the elements that can be closed given the position of the current element within the source code.
+ *
+ * This does two things:
+ *  1. The closed element's text end index is calculated based on the current offset.
+ *  2. The closed element's end tag lengths are counted towards the current offset, to make sure that the computed position
+ * of the formatting elements are still correct.
+ *
+ * @param {module:tree/structure.FormattingElement[]} elementsToClose The list of open elements that need to be closed
+ * @param {number} currentOffset                                      The current offset when parsing the formatting elements
  *
  * @returns {number} The updated current offset
  *
  * @private
  */
-const closeElements = function( currentElement, openElements, currentOffset ) {
-	/*
-	  Gather all the elements that can be closed
-	  (in other words when the current element is not nested inside the element to be closed).
-	 */
-	const elementsToClose = openElements.filter( el => {
-		const endTag = el.location.endTag;
-		return endTag.endOffset <= currentElement.location.startOffset;
-	} );
-
+const closeElements = function( elementsToClose, currentOffset ) {
 	// Sort, so we close all elements in the right order.
 	elementsToClose.sort( ( a, b ) => a.location.endTag.endOffset - b.location.endTag.endOffset );
 
 	elementsToClose.forEach( elementToClose => {
 		const endTag = elementToClose.location.endTag;
-		// For example: "</strong>".length
-		const endTagLength = endTag.endOffset - endTag.startOffset;
-
+		// Set the end position as seen in the text.
 		elementToClose.textEndIndex = endTag.startOffset - currentOffset;
 		/*
 		  Add the end tag length of the to be closed element to the total offset,
 		  and remove the element from the stack.
 		 */
+		// For example: "</strong>".length
+		const endTagLength = endTag.endOffset - endTag.startOffset;
 		currentOffset += endTagLength;
-		const index = openElements.indexOf( elementToClose );
-		openElements.splice( index, 1 );
 	} );
+
+	return currentOffset;
+};
+
+/**
+ * Adds the content length of the given element (the part between the tags) to the current offset.
+ *
+ * @param {module:tree/structure.FormattingElement} element The element of which to add the content length.
+ * @param {number} currentOffset                            The current offset to which to add the length to.
+ *
+ * @returns {number} The updated current offset
+ */
+const addIgnoredContentToOffset = function( element, currentOffset ) {
+	// Has 0 length in text, so end = start.
+	element.textEndIndex = element.textStartIndex;
+
+	// Update current offset.
+	const location = element.location;
+	const start = location.startTag ? location.startTag.endOffset : location.startOffset;
+	const end = location.endTag ? location.endTag.startOffset : location.endOffset;
+
+	currentOffset += end - start;
 
 	return currentOffset;
 };
@@ -67,13 +87,12 @@ const closeElements = function( currentElement, openElements, currentOffset ) {
  * Sets the start and end position of the formatting elements in the given node's text.
  *
  * @param {module:tree/structure.LeafNode} node The node containing a TextContainer
- * @param {string} html                         The original html source code
  *
  * @returns {void}
  *
  * @private
  */
-const calculateTextIndices = function( node, html ) {
+const calculateTextIndices = function( node ) {
 	if ( ! node.textContainer.formatting || node.textContainer.formatting.length === 0 ) {
 		return;
 	}
@@ -88,7 +107,10 @@ const calculateTextIndices = function( node, html ) {
 
 	node.textContainer.formatting.forEach( element => {
 		// Close elements that can be closed.
-		currentOffset = closeElements( element, openElements, currentOffset );
+		const elementsToClose = elementsThatCanBeClosed( element, openElements );
+		currentOffset = closeElements( elementsToClose, currentOffset );
+		// Remove closed elements from the list.
+		pullAll( openElements, elementsToClose );
 
 		const startTag = element.location.startTag;
 		const endTag = element.location.endTag;
@@ -102,13 +124,13 @@ const calculateTextIndices = function( node, html ) {
 		element.textStartIndex = startTag.endOffset - currentOffset;
 
 		if ( endTag ) {
-			// Keep track of the elements that needs to be closed.
+			// Keep track of the elements that need to be closed.
 			openElements.push( element );
 		} else {
 			/*
 			  Some elements have no end tags,
 			  e.g. void elements like <img/> or self-closing elements.
-			  We can close them immediately (with length of 0).
+			  We can close them immediately (with length of 0, since it has no content).
 			 */
 			element.textEndIndex = element.textStartIndex;
 		}
@@ -119,25 +141,11 @@ const calculateTextIndices = function( node, html ) {
 		  so the current offset should be updated.
 		 */
 		if ( ignoredHtmlElements.includes( element.type ) ) {
-			// Has 0 length in text, so end = start.
-			element.textEndIndex = element.textStartIndex;
-			const rawContent = getElementContent( element, html );
-			currentOffset += rawContent.length;
+			currentOffset = addIgnoredContentToOffset( element, currentOffset );
 		}
 	} );
-
-	// Sort, so we close all elements in the right order.
-	openElements.sort( ( a, b ) => a.location.endTag.endOffset - b.location.endTag.endOffset );
-
-	openElements.forEach( element => {
-		const endTag = element.location.endTag;
-		// For example: "</strong>".length
-		const endTagLength = endTag.endOffset - endTag.startOffset;
-
-		element.textEndIndex = endTag.startOffset - currentOffset;
-
-		currentOffset += endTagLength;
-	} );
+	// Close all remaining elements.
+	closeElements( openElements, currentOffset );
 };
 
 export default calculateTextIndices;
