28c38aeda6a1e6348e6aba956b08ccecb7b8fbb5
Anton Timmermans <email@atimmer.com>
2019-01-17 15:20:43 +0100

    Autofix codestyle in create-react-app files
    This makes these files easier to work with for us.

--- examples/webpack/config/jest/fileTransform.js
+++ examples/webpack/config/jest/fileTransform.js
@@ -1,16 +1,16 @@
-'use strict';
 
-const path = require('path');
+
+const path = require( "path" );
 
 // This is a custom Jest transformer turning file imports into filenames.
 // http://facebook.github.io/jest/docs/en/webpack.html
 
 module.exports = {
-  process(src, filename) {
-    const assetFilename = JSON.stringify(path.basename(filename));
+	process( src, filename ) {
+		const assetFilename = JSON.stringify( path.basename( filename ) );
 
-    if (filename.match(/\.svg$/)) {
-      return `module.exports = {
+		if ( filename.match( /\.svg$/ ) ) {
+			return `module.exports = {
         __esModule: true,
         default: ${assetFilename},
         ReactComponent: (props) => ({
@@ -23,8 +23,8 @@ module.exports = {
           })
         }),
       };`;
-    }
+		}
 
-    return `module.exports = ${assetFilename};`;
-  },
+		return `module.exports = ${assetFilename};`;
+	},
 };
