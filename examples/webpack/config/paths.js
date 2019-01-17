fb5673718fa378b6be5b41da0c59441bb51a2272
Anton Timmermans <email@atimmer.com>
2019-01-17 15:57:31 +0100

    Add yoastseo path
    Original commits:
    
    * 7bac241bf0f059b62b540694a7cf623057fce4d7
    * 218a816a16133d9c3be23e55a4defcf6b99d909d
    * 3a99972ae7d805d6d5e4dae1ceda4bf3b18410fc

--- examples/webpack/config/paths.js
+++ examples/webpack/config/paths.js
@@ -81,6 +81,7 @@ module.exports = {
 	appNodeModules: resolveApp( "node_modules" ),
 	publicUrl: getPublicUrl( resolveApp( "package.json" ) ),
 	servedPath: getServedPath( resolveApp( "package.json" ) ),
+	yoastSrc: resolveApp( "../../src" ),
 };
 
 
--- examples/webpack/config/webpack.config.js
+++ examples/webpack/config/webpack.config.js
@@ -264,6 +264,8 @@ module.exports = function( webpackEnv ) {
 				// Support React Native Web
 				// https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
 				"react-native": "react-native-web",
+
+				"yoastsrc": paths.yoastSrc,
 			},
 			plugins: [
 				// Adds support for installing with Plug'n'Play, leading to faster installs and adding
