db3ae2354be540c57de613ea2fd6709ffd84ba76
Igor <igor@yoast.com>
2019-01-21 10:41:54 +0100

    Add grunt webpack:recalibrationDev task
    To make it possible to also have a dev build without the webpack dev server.
    This is needed since a change in the way the analysis worker minified or not.
    See: https://github.com/Yoast/wordpress-seo/pull/11872 and
    https://github.com/Yoast/wordpress-seo/pull/11738

--- grunt/config/webpack.js
+++ grunt/config/webpack.js
@@ -2,7 +2,8 @@
 const webpackConfig = require( "../../webpack/webpack.config" );
 
 module.exports = {
-	buildDev: webpackConfig( { environment: "development" } ),
-	buildProd: webpackConfig(),
-	recalibration: webpackConfig( { recalibration: "enabled" } ),
+	buildDev: () => webpackConfig( { environment: "development" } ),
+	buildProd: () => webpackConfig(),
+	recalibrationDev: () => webpackConfig( { environment: "development", recalibration: "enabled" } ),
+	recalibration: () => webpackConfig( { recalibration: "enabled" } ),
 };
