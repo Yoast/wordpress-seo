c529bc8d38a75b290a6d6be15a8ebac5aada485e
Andy Meerwaldt <andy@yoast.com>
2019-01-22 14:39:13 +0100

    Move notice request back

--- admin/notifiers/class-subscription-notifier.php
+++ admin/notifiers/class-subscription-notifier.php
@@ -14,10 +14,20 @@ class WPSEO_Subscription_Notifier implements WPSEO_Notifier {
 	}
 
 	public function notify() {
+		$notification  = '<div class="yoast-container yoast-container__configuration-wizard">';
+		$notification .= sprintf(
+			'<img src="%1$s" height="%2$s" width="%3$d" />',
+			esc_url( plugin_dir_url( WPSEO_FILE ) . 'images/subscription-notification.svg' ),
+			60,
+			60
+		);
+		$notification .= '<div class="yoast-container__configuration-wizard--content">';
 		// translators: %1$s expands to Yoast
-		$title = sprintf(  __( 'One or more %1$s plugins are about to expire!', 'wordpress-seo' ), 'Yoast' );
-		$image_url = esc_url( plugin_dir_url( WPSEO_FILE ) . 'images/subscription-notification.svg' );
+		$notification .= '<h3>' . sprintf( esc_html__( 'One or more %1$s plugins are about to expire!', 'wordpress-seo' ), 'Yoast' ) . '</h3>';
 
-		return WPSEO_Notification::display( $title, '', $image_url );
+		$notification .= '</div>';
+		$notification .= '</div>';
+
+		return $notification;
 	}
 }
