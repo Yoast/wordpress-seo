579164ce1ab4a94a2caec09837ca4effcfae6f83
Jip <jhp.moors@gmail.com>
2019-01-22 16:59:36 +0100

    Implement fail-over for `allow_url_fopen` disabled servers
    Load WordPress to use `wp_remote_get` when the PHP `allow_url_fopen` configuration is set to `Off`.
    This allows those servers to load the file from MyYoast.
    For servers which don't have this configuration, it saves a lot of trees to directly load the URL from MyYoast skipping the load of entire WordPress.

--- admin/my-yoast-proxy.php
+++ admin/my-yoast-proxy.php
@@ -15,12 +15,38 @@ switch ( filter_input( INPUT_GET, 'file', FILTER_SANITIZE_STRING ) ) {
 }
 
 if ( empty( $my_yoast_url ) ) {
+	header( 'HTTP/1.0 501 Requested file not implemented' );
 	exit;
 }
 
-header( 'Content-Type: ' . $my_yoast_url_content_type );
-header( 'Cache-Control: max-age=86400' );
+if ( ini_get('allow_url_fopen' ) ) {
+	header( 'Content-Type: ' . $my_yoast_url_content_type );
+	header( 'Cache-Control: max-age=86400' );
+	readfile( $my_yoast_url );
+	exit;
+}
 
-readfile( $my_yoast_url );
+// Try to load WordPress, to be able to use `wp_remote_get`.
+if ( ! is_file( $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php' ) ) {
+	header( 'HTTP/1.0 500 Server configuration not compatible' );
+	exit;
+}
+
+require $_SERVER['DOCUMENT_ROOT'] . '/wp-load.php';
+
+$content = wp_remote_get( $my_yoast_url );
+if ( $content instanceof WP_Error ) {
+	header( 'HTTP/1.0 500 Unable to retrieve file from MyYoast' );
+	exit;
+}
+
+$status_code = $content['http_response']->get_status();
+if ( $status_code === 200 ) {
+	header( 'Content-Type: ' . $my_yoast_url_content_type );
+	header( 'Cache-Control: max-age=86400' );
+	echo $content['body'];
+}
+
+header( 'HTTP/1.0 500 Recieved unexpected response from MyYoast' );
 
 exit;
