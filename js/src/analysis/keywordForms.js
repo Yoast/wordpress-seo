a70ffc965cf163b93bebdfa618c4c25bc127d620
Natalia <natalia@yoast.com>
2019-01-23 14:11:59 +0100

    Remove console messages

--- js/src/analysis/keywordForms.js
+++ js/src/analysis/keywordForms.js
@@ -8,20 +8,10 @@ import { updateKeywordForms } from "../redux/actions/snippetEditor";
  */
 export function requestKeywordForms( keyword ) {
 	YoastSEO.analysis.worker.runResearch( "morphology", new Paper( "", { keyword } ) ).then( result => {
-		console.log("!! request keyword forms result: ", result);
 		if ( result.hasOwnProperty( "result" ) ) {
-			console.log( "!! taking result.result only: ", result.result );
 			if ( result.result.hasOwnProperty( "keyphraseForms" ) ) {
-				console.log( "!! taking keyphraseforms only: ", result.result.keyphraseForms );
-				result = result.result.keyphraseForms;
-			} else {
-				console.log( "!! Ha, looks like there are no keyphrase forms (1)!" )
-				result = [];
+				YoastSEO.store.dispatch( updateKeywordForms( result.result.keyphraseForms ) );
 			}
-		} else {
-			console.log("!! Ha, looks like there are no keyphrase forms (2)!")
-			result = [];
 		}
-		YoastSEO.store.dispatch( updateKeywordForms( result ) );
 	} );
 }
