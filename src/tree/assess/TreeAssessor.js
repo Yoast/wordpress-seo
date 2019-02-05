c62c34cdd1c33057fa1872af221a7f7afa7699a4
Hans-Christiaan <hans-christiaan@yoast.com>
2019-02-05 12:02:26 +0100

    Tweaks the jsdoc of the TreeAssessor constructor.

--- src/tree/assess/TreeAssessor.js
+++ src/tree/assess/TreeAssessor.js
@@ -1,16 +1,18 @@
 /**
- * Analyzes a paper by doing a list of assessments on a tree representation of a text.
- * Aggregates the scores on each individual assessment in to an overall score.
+ * Analyzes a paper by doing a list of assessments on a tree representation of a text and its metadata.
+ * Aggregates the scores on each individual assessment into an overall score.
+ *
+ * This score can represent anything from the readability to the SEO of the given text and metadata.
  */
 class TreeAssessor {
 	/**
 	 * Creates a new assessor.
 	 *
-	 * @param {Object} options                                           Assessor options.
-	 * @param {Jed} options.i18n                                         A Jed object to use for translations.
-	 * @param {module:tree/researcher.TreeResearcher} options.researcher Supplies the assessments with researches and their (cached) results.
-	 * @param {Object} options.scoreAggregator                           Aggregates the scores on the individual assessments to one overall score.
-	 * @param {Object[]|null} options.assessments                        The list of assessments to do.
+	 * @param {Object} options                                         Assessor options.
+	 * @param {Jed} options.i18n                                       A Jed object to use for translations.
+	 * @param {module:tree/research.TreeResearcher} options.researcher Supplies the assessments with researches and their (cached) results.
+	 * @param {Object} options.scoreAggregator                         Aggregates the scores on the individual assessments into one overall score.
+	 * @param {Object[]} [options.assessments]                         The list of assessments to do.
 	 */
 	constructor( options ) {
 		this.i18n = options.i18n;
