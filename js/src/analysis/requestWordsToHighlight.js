a06989d143476495a752d7973dc8ea2db422acc3
Natalia <natalia@yoast.com>
2019-02-05 12:08:22 +0100

    Flatten keyphrase array before saving it to the store

--- js/src/analysis/requestWordsToHighlight.js
+++ js/src/analysis/requestWordsToHighlight.js
@@ -1,5 +1,7 @@
 import { Paper } from "yoastseo";
 import { updateWordsToHighlight } from "../redux/actions/snippetEditor";
+import flatten from "lodash/flatten";
+import uniq from "lodash/uniq";
 
 /**
  * Requests morphological forms of the focus keyword and makes them ready for the snippet editor.
@@ -12,7 +14,9 @@ import { updateWordsToHighlight } from "../redux/actions/snippetEditor";
  */
 export default function( runResearch, store, keyword ) {
 	runResearch( "morphology", new Paper( "", { keyword } ) ).then( researchResult => {
-		store.dispatch( updateWordsToHighlight( researchResult.result.keyphraseForms ) );
+		const keyphraseForms = researchResult.result.keyphraseForms;
+		// Prepare the original array to be used for highlighting in SnippetPreview and dispatch it to the store.
+		store.dispatch( updateWordsToHighlight( uniq( flatten( keyphraseForms ) ) ) );
 	} ).catch( () => {
 		store.dispatch( updateWordsToHighlight( [] ) );
 	} );
