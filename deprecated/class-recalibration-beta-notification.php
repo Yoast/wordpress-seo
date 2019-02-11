dcb80912499530c78511eed75e1550e066b569e0
Andy Meerwaldt <andy@yoast.com>
2019-02-11 14:01:51 +0100

    More cleanup.

--- deprecated/class-recalibration-beta-notification.php
+++ deprecated/class-recalibration-beta-notification.php
@@ -5,15 +5,19 @@
  * @package WPSEO\Admin\Notifiers
  */
 
+_deprecated_file( __FILE__, 'WPSEO 9.6' );
+
 /**
  * Represents the logic for showing recalibration beta notice.
+ *
+ * @codeCoverageIgnore Ignore, because this class has been deprecated.
  *
  * @deprecated 9.6
  */
 class WPSEO_Recalibration_Beta_Notification implements WPSEO_WordPress_Integration {
 
 	/**
-	 * The name of the notifier.
+	 * The name of the notifier.s
 	 *
 	 * @var string
 	 */
@@ -25,37 +29,24 @@ class WPSEO_Recalibration_Beta_Notification implements WPSEO_WordPress_Integrati
 	 * @deprecated 9.6
 	 */
 	public function __construct() {
-		_deprecated_constructor( 'WPSEO_Recalibration_Beta_Notification', '9.6' );
+		_deprecated_constructor( 'WPSEO_Recalibration_Beta_Notification', 'WPSEO 9.6' );
 	}
 
 	/**
-	 * Registers all hooks to WordPress
-	 *
-	 * @codeCoverageIgnore
+	 * Registers all hooks to WordPress.
 	 *
 	 * @return void
 	 */
 	public function register_hooks() {
-		add_action( 'admin_init', array( $this, 'handle_notice' ), 15 );
+		// Do nothing.
 	}
 
 	/**
 	 * Shows the notification when applicable.
 	 *
-	 * @return void.
+	 * @return void
 	 */
 	public function handle_notice() {
-		$this->get_notification_center()->remove_notification_by_id( 'wpseo-' . $this->notification_identifier );
-	}
-
-	/**
-	 * Retrieves an instance of the notification center.
-	 *
-	 * @codeCoverageIgnore
-	 *
-	 * @return Yoast_Notification_Center Instance of the notification center.
-	 */
-	protected function get_notification_center() {
-		return Yoast_Notification_Center::get();
+		// Do nothing.
 	}
 }
