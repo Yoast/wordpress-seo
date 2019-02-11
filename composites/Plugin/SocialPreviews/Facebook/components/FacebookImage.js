f0de2adf0b1beda1a9a06a92c394a674ec9b1bc7
Andrea Fercia <a.fercia@gmail.com>
2019-02-11 17:02:03 +0100

    Fix translate syntax for IE11.

--- composites/Plugin/SocialPreviews/Facebook/components/FacebookImage.js
+++ composites/Plugin/SocialPreviews/Facebook/components/FacebookImage.js
@@ -33,7 +33,7 @@ const StyledImage = styled.img`
 	position: absolute;
 	top: 50%;
 	left: 50%;
-	transform: translate( -50%, -50% );
+	transform: translate(-50%, -50%);
 `;
 
 const ErrorImage = styled.div`
