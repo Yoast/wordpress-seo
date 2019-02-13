dc17893cb860dd34edfafe4bddaf03198f373461
Anton Timmermans <email@atimmer.com>
2019-02-13 11:45:27 +0100

    Refactor plopfile to be more extensible

--- plopfile.js
+++ plopfile.js
@@ -7,9 +7,7 @@ const { paramCase } = require( "change-case" );
 const path = require( "path" );
 
 /* Internal dependencies */
-const configuration = require( "./scripts/plop-code-generation/configuration.js" );
-
-const blocks = [];
+const mainConfiguration = require( "./scripts/plop-code-generation/configuration.js" );
 
 const BLOCK_PREFIX = "yoast/";
 const DISCLAIMER = "// THIS FILE IS GENERATED, DO NOT MANUALLY EDIT!";
@@ -62,38 +60,71 @@ function mapDefinitionToData( definition ) {
 	};
 }
 
-const generators = _flatMap( configuration, ( definitions, definitionPath ) => {
-	return _flatMap( definitions, definition => {
+const blocksToGenerate = [];
+
+function walkDefinitions( definitions, definitionPath ) {
+	definitions.forEach( definition => {
 		const data = mapDefinitionToData( definition );
 
-		blocks.push( {
-			block: data.type,
-			path: definitionPath + "/",
+		blocksToGenerate.push( {
+			data,
+			definitionPath,
 		} );
-
-		return {
-			name: data.type,
-			description: "Generate code for " + data.type + " block",
-			prompts: [],
-			actions: [
-				{
-					type: "add",
-					path: path.join( basePath, definitionPath, `register-${data.type}.js` ),
-					templateFile: path.join( templatesPath, "structural-block-registration.hbs" ),
-					data: data,
-					force: true,
-				},
-				{
-					type: "add",
-					path: path.join( basePath, definitionPath, `${data.type}.js` ),
-					templateFile: path.join( templatesPath, "structural-block-component.hbs" ),
-					data: data,
-					force: true,
-				},
-			],
-		};
 	} );
-} );
+}
+
+function walkConfiguration( configuration ) {
+	Object.keys( configuration ).forEach( definitionPath => {
+		const definitions = configuration[ definitionPath ];
+
+		walkDefinitions( definitions, definitionPath );
+	} );
+}
+
+function mapBlockToGenerator( block ) {
+	const { data, definitionPath } = block;
+
+	return {
+		name: data.type,
+		description: "Generate code for " + data.type + " block",
+		prompts: [],
+		actions: [
+			{
+				type: "add",
+				path: path.join( basePath, definitionPath, `register-${data.type}.js` ),
+				templateFile: path.join( templatesPath, "structural-block-registration.hbs" ),
+				data: data,
+				force: true,
+			},
+			{
+				type: "add",
+				path: path.join( basePath, definitionPath, `${data.type}.js` ),
+				templateFile: path.join( templatesPath, "structural-block-component.hbs" ),
+				data: data,
+				force: true,
+			},
+		],
+	};
+}
+
+function mapBlocksToGenerators( blocks ) {
+	return blocks.map( mapBlockToGenerator );
+}
+
+function mapBlockToImport( block ) {
+	return {
+		block: block.data.type,
+		path: block.definitionPath + "/",
+	};
+}
+
+function mapBlocksToImports( blocks ) {
+	return blocks.map( mapBlockToImport );
+}
+
+walkConfiguration( mainConfiguration );
+const generators = mapBlocksToGenerators( blocksToGenerate );
+const blockImports = mapBlocksToImports( blocksToGenerate );
 
 // Required viewing: https://www.youtube.com/watch?v=o3YWv4pWqk4.
 module.exports = function( plop ) {
@@ -110,7 +141,7 @@ module.exports = function( plop ) {
 				path: path.join( basePath, "index.js" ),
 				templateFile: path.join( templatesPath, "structural-blocks-list.hbs" ),
 				data: {
-					blocks: blocks,
+					blocks: blockImports,
 					disclaimer: DISCLAIMER,
 				},
 				force: true,
