bafad68e49fff2c2ffd523a19c5b985685e15a0b
Hans-Christiaan <hans-christiaan@yoast.com>
2019-02-15 10:45:45 +0100

    Made some small code style changes (const instead of var, length of lines).

--- spec/assessments/textImagesSpec.js
+++ spec/assessments/textImagesSpec.js
@@ -1,15 +1,15 @@
 import ImageCountAssessment from "../../src/assessments/seo/TextImagesAssessment";
 import Paper from "../../src/values/Paper.js";
 import Factory from "../specHelpers/factory.js";
-var i18n = Factory.buildJed();
+const i18n = Factory.buildJed();
 
 const imageCountAssessment = new ImageCountAssessment();
 
 describe( "An image count assessment for Recalibration analysis", function() {
 	it( "assesses no images", function() {
-		var mockPaper = new Paper( "sample" );
+		const mockPaper = new Paper( "sample" );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 0,
 			altTagCount: {
 				noAlt: 0,
@@ -20,13 +20,14 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 3 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: No images appear on this page. <a href='https://yoa.st/33d' target='_blank'>Add some</a>!" );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"No images appear on this page. <a href='https://yoa.st/33d' target='_blank'>Add some</a>!" );
 	} );
 
 	it( "assesses a single image, without a keyword and alt-tag set", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' />" );
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' />" );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 1,
 			altTagCount: {
 				noAlt: 1,
@@ -37,13 +38,15 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Images on this page do not have alt attributes that reflect the topic of your text. <a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of relevant images</a>!" );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Images on this page do not have alt attributes that reflect the topic of your text. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of relevant images</a>!" );
 	} );
 
 	it( "assesses a single image, without a keyword, but with an alt-tag set", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='image' />" );
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='image' />" );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 1,
 			altTagCount: {
 				noAlt: 0,
@@ -54,13 +57,16 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Images on this page have alt attributes, but you have not set your keyphrase. <a href='https://yoa.st/33d' target='_blank'>Fix that</a>!" );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Images on this page have alt attributes, but you have not set your keyphrase. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Fix that</a>!" );
 	} );
 
 	it( "assesses 4 images, without a keyword, but with an alt-tag set", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='image' /> <img src='image.jpg' alt='image2' /> <img src='image.jpg' alt='image3' /> <img src='image.jpg' alt='image4' />" );
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='image' /> " +
+			"<img src='image.jpg' alt='image2' /> <img src='image.jpg' alt='image3' /> <img src='image.jpg' alt='image4' />" );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 4,
 			altTagCount: {
 				noAlt: 0,
@@ -71,15 +77,17 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Images on this page have alt attributes, but you have not set your keyphrase. <a href='https://yoa.st/33d' target='_blank'>Fix that</a>!" );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Images on this page have alt attributes, but you have not set your keyphrase. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Fix that</a>!" );
 	} );
 
 	it( "assesses a single image, with a keyword and alt-tag set, but with a non-keyword alt-tag", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='keyword' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='keyword' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 1,
 			altTagCount: {
 				noAlt: 0,
@@ -90,15 +98,18 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Images on this page do not have alt attributes that reflect the topic of your text. <a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of relevant images</a>!" );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Images on this page do not have alt attributes that reflect the topic of your text. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of relevant images</a>!" );
 	} );
 
 	it( "assesses four images, with a keyword and alt-tag set, but with a non-keyword alt-tag", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='image' /> <img src='image.jpg' alt='image2' /> <img src='image.jpg' alt='image3' /> <img src='image.jpg' alt='image4' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='image' /> " +
+			"<img src='image.jpg' alt='image2' /> <img src='image.jpg' alt='image3' /> <img src='image.jpg' alt='image4' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 4,
 			altTagCount: {
 				noAlt: 0,
@@ -109,15 +120,17 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Images on this page do not have alt attributes that reflect the topic of your text. <a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of relevant images</a>!" );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Images on this page do not have alt attributes that reflect the topic of your text. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of relevant images</a>!" );
 	} );
 
 	it( "assesses a single image, with a keyword and alt-tag set to keyword", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 1,
 			altTagCount: {
 				noAlt: 0,
@@ -133,11 +146,11 @@ describe( "An image count assessment for Recalibration analysis", function() {
 	} );
 
 	it( "assesses two images, with a keyword and alt-tag set to keyword for 1 of 2 images", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 2,
 			altTagCount: {
 				noAlt: 0,
@@ -152,11 +165,11 @@ describe( "An image count assessment for Recalibration analysis", function() {
 	} );
 
 	it( "assesses 6 images, with a keyword and alt-tag set to keyword for 2 images, alt-tag set to non-keyword for 1 image and 3 images without alt tags", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 6,
 			altTagCount: {
 				noAlt: 3,
@@ -171,11 +184,11 @@ describe( "An image count assessment for Recalibration analysis", function() {
 	} );
 
 	it( "assesses 6 images, with a keyword and alt-tag set to keyword for all 6 images", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 6,
 			altTagCount: {
 				noAlt: 0,
@@ -186,15 +199,18 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Out of 6 images on this page, 6 have alt attributes with words from your keyphrase or synonyms. That's a bit much. <a href='https://yoa.st/33d' target='_blank'>Only include the keyphrase or its synonyms when it really fits the image</a>." );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Out of 6 images on this page, 6 have alt attributes with words from your keyphrase or synonyms. That's a bit much. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Only include the keyphrase or its synonyms when it really " +
+			"fits the image</a>." );
 	} );
 
 	it( "assesses 6 images, with a keyword and alt-tag set to keyword for only 1 image", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 6,
 			altTagCount: {
 				noAlt: 1,
@@ -205,15 +221,18 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Out of 6 images on this page, only 1 has an alt attribute that reflects the topic of your text. <a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of more relevant images</a>!" );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Out of 6 images on this page, only 1 has an alt attribute that reflects the topic of your text. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of more " +
+			"relevant images</a>!" );
 	} );
 
 	it( "assesses 5 images, with a keyword and alt-tag set to keyword for 2 images, alt-tag set to non-keyword for 1 image and 2 images without alt tags", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 5,
 			altTagCount: {
 				noAlt: 2,
@@ -228,11 +247,11 @@ describe( "An image count assessment for Recalibration analysis", function() {
 	} );
 
 	it( "assesses 5 images, with a keyword and alt-tag set to keyword for 4 images", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 5,
 			altTagCount: {
 				noAlt: 1,
@@ -247,11 +266,11 @@ describe( "An image count assessment for Recalibration analysis", function() {
 	} );
 
 	it( "assesses 5 images, with a keyword and alt-tag set to keyword for all 5 images", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 5,
 			altTagCount: {
 				noAlt: 0,
@@ -262,15 +281,18 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Out of 5 images on this page, 5 have alt attributes with words from your keyphrase or synonyms. That's a bit much. <a href='https://yoa.st/33d' target='_blank'>Only include the keyphrase or its synonyms when it really fits the image</a>." );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Out of 5 images on this page, 5 have alt attributes with words from your keyphrase or synonyms. That's a bit much. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Only include the keyphrase or its synonyms when it " +
+			"really fits the image</a>." );
 	} );
 
 	it( "assesses 5 images, with a keyword and alt-tag set to keyword for only 1 image", function() {
-		var mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
+		const mockPaper = new Paper( "These are just five words <img src='image.jpg' alt='sample' />", {
 			keyword: "Sample",
 		} );
 
-		var assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
+		const assessment = imageCountAssessment.getResult( mockPaper, Factory.buildMockResearcher( {
 			imageCount: 5,
 			altTagCount: {
 				noAlt: 1,
@@ -281,6 +303,9 @@ describe( "An image count assessment for Recalibration analysis", function() {
 		}, true ), i18n );
 
 		expect( assessment.getScore() ).toEqual( 6 );
-		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Out of 5 images on this page, only 1 has an alt attribute that reflects the topic of your text. <a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of more relevant images</a>!" );
+		expect( assessment.getText() ).toEqual( "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: " +
+			"Out of 5 images on this page, only 1 has an alt attribute that reflects the topic of your text. " +
+			"<a href='https://yoa.st/33d' target='_blank'>Add your keyphrase or synonyms to the alt tags of " +
+			"more relevant images</a>!" );
 	} );
 } );
