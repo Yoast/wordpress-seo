a4646bafdf53ee017ef9b16c9ef2f80a7232531c
Dieter <gargamelk@hotmail.com>
2019-02-15 16:09:10 +0100

    Added failing test

--- tests/admin/test-class-my-yoast-route.php
+++ tests/admin/test-class-my-yoast-route.php
@@ -10,7 +10,44 @@
  * @group MyYoast
  */
 class WPSEO_MyYoast_Route_Test extends WPSEO_UnitTestCase {
+	/**
+	 * @covers WPSEO_MyYoast_Route::connect()
+	 */
+	public function test_connect_with_saved_clientId() {
+		/** @var WPSEO_MyYoast_Route_Double $instance */
+		$instance = $this
+			->getMockBuilder( 'WPSEO_MyYoast_Route_Double' )
+			->setMethods( array( 'redirect', 'get_client' ) )
+			->getMock();
 
+		/** @var Client $instance */
+		$client = $this
+			->getMockBuilder( '\Yoast\Tests\Doubles\Oauth\Client' )
+			->setMethods( array( 'get_configuration', 'save_configuration' ) )
+			->getMock();
 
+		$config_test_data = array(
+			'clientId' => "9740f9cf-608e-4327-8a16-24e3ff6a4c0d",
+			'secret'   => NULL,
+		);
 
+		$instance ->expects( $this->once() )
+		          ->method( 'get_client' )
+		          ->will( $this->returnValue( $client) );
+
+		$client  ->expects( $this->once() )
+		         ->method( 'get_configuration' )
+		         ->will( $this->returnValue( $config_test_data) );
+
+		// If the clientId is set no configuration should be saved.
+		$client  ->expects( $this->never() )
+		         ->method( 'save_configuration' );
+
+		// Regardless of clientId, redirect should be called.
+		$instance ->expects( $this->once() )
+		          ->method( 'redirect' );
+
+		$instance->connect();
+	}
 }
+
--- tests/doubles/class-my-yoast-route-double.php
+++ tests/doubles/class-my-yoast-route-double.php
@@ -16,4 +16,18 @@ class WPSEO_MyYoast_Route_Double extends WPSEO_MyYoast_Route {
 	public function get_action() {
 		return parent::get_action();
 	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function connect() {
+		return parent::connect();
+	}
+
+	/**
+	 * @inheritdoc
+	 */
+	public function redirect( $url, $query_args ) {
+		return parent::redirect( $url, $query_args );
+	}
 }
