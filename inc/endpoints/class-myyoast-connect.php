a9ff6822ca0a83552d1e9912651db2d20172140f
Andy Meerwaldt <andy@yoast.com>
2019-02-19 13:44:17 +0100

    Adds the myyoast connect endpoint

---
+++ inc/endpoints/class-myyoast-connect.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * WPSEO plugin file.
+ *
+ * @package WPSEO\Endpoint
+ */
+
+/**
+ * Represents an implementation of the WPSEO_Endpoint interface to register one or multiple endpoints.
+ */
+class WPSEO_Endpoint_MyYoast_Connect implements WPSEO_Endpoint {
+
+	/**
+	 * The namespace to use.
+	 *
+	 * @var string
+	 */
+	const REST_NAMESPACE = 'yoast/v1/myyoast';
+
+	/**
+	 * Registers the REST routes that are available on the endpoint.
+	 *
+	 * @return void
+	 */
+	public function register() {
+		register_rest_route(
+			self::REST_NAMESPACE,
+			'connect',
+			array(
+				'methods'             => 'POST',
+				'callback'            => array( $this, 'handle_request' ),
+				'permission_callback' => array( $this, 'can_retrieve_data' ),
+			)
+		);
+	}
+
+	/**
+	 * Determines whether or not data can be retrieved for the registered endpoints.
+	 *
+	 * @param WP_REST_Request $request The current request.
+	 *
+	 * @return WP_REST_Response The response.
+	 */
+	public function handle_request( WP_REST_Request $request ) {
+		if ( $request->get_param( 'url' ) !== WPSEO_Utils::get_home_url() ) {
+			return new WP_REST_Response(
+				'Bad request: URL mismatch.', 403
+			);
+		}
+
+		$client_id = $this->get_client_id();
+
+		if ( $request->get_param( 'clientId' ) !== $client_id ) {
+			return new WP_REST_Response(
+				'Bad request: ClientID mismatch.', 403
+			);
+		}
+
+		$client_secret = $request->get_param('clientSecret');
+		if ( empty( $client_secret ) ) {
+			return new WP_REST_Response(
+				'Bad request: ClientSecret missing.', 403
+			);
+		}
+
+		$this->save_secret( $client_secret );
+
+		return new WP_REST_Response( 'Connection successful established.' );
+	}
+
+	/**
+	 * Determines whether or not data can be retrieved for the registered endpoints.
+	 *
+	 * @return bool Whether or not data can be retrieved.
+	 */
+	public function can_retrieve_data() {
+		return true;
+	}
+
+	/**
+	 * Saves the client secret.
+	 *
+	 * @param string $clientSecret The secret to save.
+	 *
+	 * @return void
+	 */
+	protected function save_secret( $clientSecret ) {
+		$this->get_client()->save_configuration(
+			array(
+				'secret' => $clientSecret,
+			)
+		);
+	}
+
+	/**
+	 * Retrieves the current client id.
+	 *
+	 * @return array The client id.
+	 */
+	protected function get_client_id() {
+		$config = $this->get_client()->get_configuration();
+
+		return $config['clientId'];
+	}
+
+	/**
+	 * Retrieves an instance of the client.
+	 *
+	 * @return WPSEO_MyYoast_Client Instance of client.
+	 */
+	protected function get_client() {
+		static $client;
+
+		if ( ! $client ) {
+			$client = new WPSEO_MyYoast_Client();
+		}
+
+		return $client;
+	}
+}
