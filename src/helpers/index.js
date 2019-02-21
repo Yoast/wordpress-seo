99c56c0873fad1125e011199114d49da80f09924
Igor <igor@yoast.com>
2019-02-21 08:42:15 +0100

    Export getFormsLanguages to the helpers index

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,10 +1,11 @@
-import { measureTextWidth } from "./createMeasurementElement";
-
 import scoreToRating from "../interpreters/scoreToRating";
+import { measureTextWidth } from "./createMeasurementElement";
+import getFormsLanguages from "./getFormsLanguages";
 
 export {
 	measureTextWidth,
 	scoreToRating,
+	getFormsLanguages,
 };
 
 export * from "./inRange";
