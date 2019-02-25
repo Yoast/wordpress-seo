456f164b61f6852c08b0a42d79cb521ade883f2c
Hans-Christiaan <hans-christiaan@yoast.com>
2019-02-25 13:19:24 +0100

    Added asterisks before each new line in added multiline comment blocks.

--- js/src/analysis/handleWorkerError.js
+++ js/src/analysis/handleWorkerError.js
@@ -8,13 +8,13 @@
  */
 export default function handleWorkerError() {
 	/*
-	   This function should be implemented to catch and handle errors
-	   in the analysis web worker.
-
-	   For your information: the line below can be used to dispatch a warning message. Where the
-	   warning message is a React component (for example made by `interpolateComponents`).
-	   ```js
-	     window.YoastSEO.store.dispatch( setWarningMessage( message ) );
-	   ```
+	 * This function should be implemented to catch and handle errors
+	 * in the analysis web worker.
+	 *
+	 * For your information: the line below can be used to dispatch a warning message. Where the
+	 * warning message is a React component (for example made by `interpolateComponents`).
+	 * ```js
+	 *    window.YoastSEO.store.dispatch( setWarningMessage( message ) );
+	 * ```
 	 */
 }
--- js/tests/analysis/handleWorkerError.test.js
+++ js/tests/analysis/handleWorkerError.test.js
@@ -4,8 +4,8 @@ import handleWorkerError from "../../src/analysis/handleWorkerError";
 describe( "handleWorkerError", () => {
 	it( "does not do anything yet", () => {
 		/*
-		  Note: this test suite should be properly implemented when `handleWorkerError`
-		  has been implemented as well.
+		 * Note: this test suite should be properly implemented when `handleWorkerError`
+		 * has been implemented as well.
 		 */
 		window.YoastSEO = {
 			store: {
