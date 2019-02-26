aadb6263501c4016301cef3447d0ff1c19ac8188
Dieter <gargamelk@hotmail.com>
2019-02-26 11:48:33 +0100

    Fixed some CS

--- wp-seo-main.php
+++ wp-seo-main.php
@@ -535,9 +535,10 @@ register_deactivation_hook( WPSEO_FILE, 'wpseo_deactivate' );
 
 // Wpmu_new_blog has been deprecated in 5.1 and replaced by wp_insert_site.
 global $wp_version;
-if( version_compare($wp_version,'5.1', '<') ) {
+if ( version_compare( $wp_version,'5.1', '<' ) ) {
 	add_action( 'wpmu_new_blog', 'wpseo_on_activate_blog' );
-} else {
+}
+else {
 	add_action( 'wp_insert_site', 'wpseo_on_activate_blog' );
 }
 
