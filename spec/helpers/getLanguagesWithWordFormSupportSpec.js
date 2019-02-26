4d8ec759e173d3be1420f983b44f6bf04a31581c
Manuel <manuel@yoast.com>
2019-02-26 15:38:40 +0100

    Add German to languages with word form support unit test, improve naming in unit tests

--- spec/helpers/getLanguagesWithWordFormSupportSpec.js
+++ spec/helpers/getLanguagesWithWordFormSupportSpec.js
@@ -1,7 +1,7 @@
-import getFormsLanguages from "../../src/helpers/getLanguagesWithWordFormSupport";
+import getLanguagesWithWordFormSupport from "../../src/helpers/getLanguagesWithWordFormSupport";
 
 describe( "Checks which languages have morphology support in YoastSEO.js", function() {
 	it( "returns an array of languages that have morphology support", function() {
-		expect( getFormsLanguages() ).toEqual( [ "en" ] );
+		expect( getLanguagesWithWordFormSupport() ).toEqual( [ "en", "de" ] );
 	} );
 } );
