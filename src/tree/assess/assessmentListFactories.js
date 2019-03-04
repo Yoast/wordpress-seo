152ec7154f522da087953ab912ec4f9297e83d2b
Hans-Christiaan <hans-christiaan@yoast.com>
2019-03-04 17:24:16 +0100

    JSDoc of taxonomy assessments now correctly says taxonomy assessments, instead of readability assments.

--- src/tree/assess/assessmentListFactories.js
+++ src/tree/assess/assessmentListFactories.js
@@ -18,9 +18,9 @@ const constructReadabilityAssessments = () => [
 ];
 
 /**
- * Creates a new list of readability assessments.
+ * Creates a new list of SEO assessments for taxonomy pages.
  *
- * @returns {module:tree/assess.Assessment[]} The list of readability assessments.
+ * @returns {module:tree/assess.Assessment[]} The list of SEO assessments.
  */
 const constructTaxonomyAssessments = () => [
 	// Needs to be populated by fancy new assessments that work on the tree representation of the text.
