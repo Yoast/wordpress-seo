f8e7eda6c23f5a814432af7191de9f7e337713a4
Andy Meerwaldt <andy@yoast.com>
2019-03-06 14:56:44 +0100

    Improve the docs

--- admin/config-ui/fields/class-field-profile-url-wikipedia.php
+++ admin/config-ui/fields/class-field-profile-url-wikipedia.php
@@ -21,9 +21,11 @@ class WPSEO_Config_Field_Profile_URL_Wikipedia extends WPSEO_Config_Field {
 	}
 
 	/**
-	 * Set adapter
+	 * Sets the adapter.
 	 *
 	 * @param WPSEO_Configuration_Options_Adapter $adapter Adapter to register lookup on.
+	 *
+	 * @return void.
 	 */
 	public function set_adapter( WPSEO_Configuration_Options_Adapter $adapter ) {
 		$adapter->add_option_lookup( $this->get_identifier(), 'wikipedia_url' );
