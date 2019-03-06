00c4d1dae375402661f86a29e782f97f33b64e9f
Andy <andy@yoast.com>
2019-03-06 15:00:45 +0100

    Removal of the full stop

--- admin/config-ui/fields/class-field-profile-url-wikipedia.php
+++ admin/config-ui/fields/class-field-profile-url-wikipedia.php
@@ -25,7 +25,7 @@ class WPSEO_Config_Field_Profile_URL_Wikipedia extends WPSEO_Config_Field {
 	 *
 	 * @param WPSEO_Configuration_Options_Adapter $adapter Adapter to register lookup on.
 	 *
-	 * @return void.
+	 * @return void
 	 */
 	public function set_adapter( WPSEO_Configuration_Options_Adapter $adapter ) {
 		$adapter->add_option_lookup( $this->get_identifier(), 'wikipedia_url' );
