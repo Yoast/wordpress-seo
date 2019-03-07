f736a29c2c7ab644f7fded41c5b80ed735af8a82
Johanna de Vos <johanna@yoast.com>
2019-03-07 13:34:16 +0100

    URL in capital letters (like URI).

--- inc/sitemaps/class-sitemaps-router.php
+++ inc/sitemaps/class-sitemaps-router.php
@@ -120,7 +120,7 @@ class WPSEO_Sitemaps_Router {
 		$base = apply_filters( 'wpseo_sitemaps_base_url', $base );
 
 		/*
-		 * Get the scheme from the configured home url instead of letting WordPress
+		 * Get the scheme from the configured home URL instead of letting WordPress
 		 * determine the scheme based on the requested URI.
 		 */
 		return home_url( $base . $page, wp_parse_url( get_option( 'home' ), PHP_URL_SCHEME ) );
