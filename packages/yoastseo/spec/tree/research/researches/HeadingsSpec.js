5d48b5c312052f90f3fe3d3b19eb249f0534a3f5
Hans-Christiaan <hans-christiaan@yoast.com>
2019-03-11 11:23:35 +0100

    Finished making basic test case.
    It fails now, but this is the master way of test=driven development.

--- packages/yoastseo/spec/tree/research/researches/HeadingsSpec.js
+++ packages/yoastseo/spec/tree/research/researches/HeadingsSpec.js
@@ -1,5 +1,6 @@
 import Headings from "../../../../src/tree/research/researches/Headings";
 import TreeResearcher from "../../../../src/tree/research/TreeResearcher";
+import Heading from "../../../../src/tree/structure/nodes/Heading";
 import buildTreeFromYaml from "../../../specHelpers/buildTreeFromYaml";
 
 describe( "Headings", () => {
@@ -15,22 +16,52 @@ Structured:
         level: 1
         sourceStartIndex: 25
         sourceEndIndex: 30
-        text: This is a header level 1.
+        text: This is a heading level 1.
     - Paragraph:
         tag: p
         sourceStartIndex: 23
         sourceEndIndex: 50
         text: This is a paragraph.
+    - Structured:
+        tag: section
+        sourceStartIndex: 23
+        sourceEndIndex: 50
+        children:
+          - Heading:
+              level: 2
+              sourceStartIndex: 23
+              sourceEndIndex: 50
+              text: This is a heading level 2.
+          - Paragraph:
+              tag: p
+              sourceStartIndex: 23
+              sourceEndIndex: 50
+              text: This is another paragraph.
 `;
 	} );
 
 	it( "gives back a list of headings when applying the research to a text with the researcher.", done => {
 		const researcher = new TreeResearcher();
 		const headings = new Headings();
+
+		const heading1 = new Heading( 1 );
+		heading1.text = "This is a heading level 1";
+		heading1.level = 1;
+		heading1.sourceStartIndex = 23;
+		heading1.sourceEndIndex = 50;
+
+		const heading2 = new Heading( 1 );
+		heading2.text = "This is a heading level 2";
+		heading2.level = 2;
+		heading2.sourceStartIndex = 23;
+		heading2.sourceEndIndex = 50;
+
+		const expected = [ heading1, heading2 ];
+
 		researcher.addResearch( "headings", headings );
 		researcher.doResearch( "headings", tree ).then(
-			result => {
-				console.log( result );
+			results => {
+				expect( results ).toEqual( expected );
 				done();
 			}
 		);
