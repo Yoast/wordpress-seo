2935903112005dba8811ab93111e7a99307fbaea
Hans-Christiaan <hans-christiaan@yoast.com>
2019-03-11 12:37:46 +0100

    Created Headdings researcher.

--- packages/yoastseo/spec/tree/research/researches/HeadingsSpec.js
+++ packages/yoastseo/spec/tree/research/researches/HeadingsSpec.js
@@ -45,16 +45,16 @@ Structured:
 		const headings = new Headings();
 
 		const heading1 = new Heading( 1 );
-		heading1.text = "This is a heading level 1";
-		heading1.level = 1;
-		heading1.sourceStartIndex = 23;
-		heading1.sourceEndIndex = 50;
+		heading1.text = "This is a heading level 1.";
+		heading1.sourceStartIndex = 25;
+		heading1.sourceEndIndex = 30;
+		heading1.setResearchResult( "headings", heading1 );
 
-		const heading2 = new Heading( 1 );
-		heading2.text = "This is a heading level 2";
-		heading2.level = 2;
+		const heading2 = new Heading( 2 );
+		heading2.text = "This is a heading level 2.";
 		heading2.sourceStartIndex = 23;
 		heading2.sourceEndIndex = 50;
+		heading2.setResearchResult( "headings", heading2 );
 
 		const expected = [ heading1, heading2 ];
 
--- packages/yoastseo/src/tree/research/researches/Headings.js
+++ packages/yoastseo/src/tree/research/researches/Headings.js
@@ -1,3 +1,8 @@
+import { flatten } from "lodash-es";
+
+/* Internal dependencies */
+import { Heading } from "../../structure";
+import LeafNode from "../../structure/nodes/LeafNode";
 import Research from "./Research";
 
 /**
@@ -11,11 +16,9 @@ class Headings extends Research {
 	 * @param {module:tree/structure.Node} node The node to calculate the research for.
 	 *
 	 * @returns {Promise<*>} The result of the research.
-	 *
-	 * @abstract
 	 */
 	calculateFor( node ) {
-		return undefined;
+		return node instanceof Heading ? Promise.resolve( node ) : Promise.resolve( null );
 	}
 
 	/**
@@ -24,11 +27,9 @@ class Headings extends Research {
 	 * @param {module:tree/structure.Node} node The node to check.
 	 *
 	 * @returns {boolean} If the given node is considered a leaf node for this research.
-	 *
-	 * @abstract
 	 */
 	isLeafNode( node ) {
-		return false;
+		return node instanceof LeafNode;
 	}
 
 	/**
@@ -37,11 +38,9 @@ class Headings extends Research {
 	 * @param {Array<*>} results The results of this research to merge.
 	 *
 	 * @returns {*} The merged results.
-	 *
-	 * @abstract
 	 */
 	mergeChildrenResults( results ) {
-		return undefined;
+		return flatten( results ).filter( node => node );
 	}
 }
 
