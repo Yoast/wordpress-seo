6e701b7a315673d7b099a27d1ee6448ae083c761
Andy Meerwaldt <andy@yoast.com>
2019-03-13 13:59:20 +0100

    Exports all components

--- packages/components/index.js
+++ packages/components/index.js
@@ -6,10 +6,20 @@ export {
 	StyledHeading,
 };
 
-export { default as Textfield } from "composites/Textfield";
 export { default as Button } from "Button";
+export { default as ErrorBoundary } from "ErrorBoundary";
+export { default as Heading } from "Heading";
+export { default as IFrame } from "IFrame";
 export { default as Input } from "Input";
 export { default as Label } from "Label";
+export { default as Loader } from "Loader";
+export { default as Notification } from "Notification";
+export { default as Paper } from "Paper";
 export { default as Progressbar } from "Progressbar";
 export { default as Section } from "Section";
 export { default as Textarea } from "Textarea";
+export { default as Textfield } from "composites/Textfield";
+export { default as YoastLogo } from "YoastLogo";
+export { default as YoastSeoIcon } from "YoastSeoIcon";
+export { default as YouTubeVideo } from "YouTubeVideo";
+
