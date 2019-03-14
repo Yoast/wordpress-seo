929eb37ec3b105dada025f7fc2a29d1a3bd935fb
Igor <igor@yoast.com>
2019-03-14 12:07:07 +0100

    Add FacebookAuthorName component

---
+++ packages/yoast-components/composites/Plugin/SocialPreviews/Facebook/components/FacebookAuthorName.js
@@ -0,0 +1,33 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const FacebookAuthorNameWrapper = styled.span`
+    color: #3b5998;
+    font-size: 12px;
+    line-height: 11px;
+    position: relative;
+    text-decoration: none;
+    cursor: auto;
+`;
+
+/**
+ * Renders a FacebookAuthorName component.
+ *
+ * @param {object} props The props.
+ *
+ * @returns {React.Element} The rendered element.
+ */
+const FacebookAuthorName = ( props ) => {
+	return (
+		<FacebookAuthorNameWrapper>
+			{ props.authorName }
+		</FacebookAuthorNameWrapper>
+	);
+};
+
+FacebookAuthorName.propTypes = {
+	authorName: PropTypes.string.isRequired,
+};
+
+export default FacebookAuthorName;
