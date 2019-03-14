a726d0495aa99580fbe8d45e49ba497fc1e30c04
Andy Meerwaldt <andy@yoast.com>
2019-03-14 11:37:39 +0100

    Changes the references by adding ./ before each of them.

--- packages/components/index.js
+++ packages/components/index.js
@@ -1,4 +1,4 @@
-import StyledSection, { StyledSectionBase, StyledHeading } from "composites/StyledSection";
+import StyledSection, { StyledSectionBase, StyledHeading } from "./composites/StyledSection";
 
 export {
 	StyledSection,
@@ -6,20 +6,20 @@ export {
 	StyledHeading,
 };
 
-export { default as Button } from "Button";
-export { default as ErrorBoundary } from "ErrorBoundary";
-export { default as Heading } from "Heading";
-export { default as IFrame } from "IFrame";
-export { default as Input } from "Input";
-export { default as Label } from "Label";
-export { default as Loader } from "Loader";
-export { default as Notification } from "Notification";
-export { default as Paper } from "Paper";
-export { default as Progressbar } from "Progressbar";
-export { default as Section } from "Section";
-export { default as Textarea } from "Textarea";
-export { default as Textfield } from "composites/Textfield";
-export { default as YoastLogo } from "YoastLogo";
-export { default as YoastSeoIcon } from "YoastSeoIcon";
-export { default as YouTubeVideo } from "YouTubeVideo";
+export { default as Button } from "./Button";
+export { default as ErrorBoundary } from "./ErrorBoundary";
+export { default as Heading } from "./Heading";
+export { default as IFrame } from "./IFrame";
+export { default as Input } from "./Input";
+export { default as Label } from "./Label";
+export { default as Loader } from "./Loader";
+export { default as Notification } from "./Notification";
+export { default as Paper } from "./Paper";
+export { default as Progressbar } from "./Progressbar";
+export { default as Section } from "./Section";
+export { default as Textarea } from "./Textarea";
+export { default as Textfield } from "./composites/Textfield";
+export { default as YoastLogo } from "./YoastLogo";
+export { default as YoastSeoIcon } from "./YoastSeoIcon";
+export { default as YouTubeVideo } from "./YouTubeVideo";
 
