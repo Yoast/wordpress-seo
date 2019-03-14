6a8b0811ed65b293b61627772147e3929e05436d
Andrea Fercia <a.fercia@gmail.com>
2019-03-14 12:14:28 +0100

    Minor coding standards.

--- packages/yoast-components/composites/Plugin/SocialPreviews/Facebook/components/FacebookDescription.js
+++ packages/yoast-components/composites/Plugin/SocialPreviews/Facebook/components/FacebookDescription.js
@@ -1,17 +1,18 @@
+/* External dependencies */
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
 
 const FacebookDescriptionWrapper = styled.p`
-    color: #606770;
-    font-size: 14px;
-    line-height: 20px;
-    word-break: break-word;
-    max-height: 80px;
-    overflow: hidden;
-    text-overflow: ellipsis;
-    white-space: normal;
-    margin-top: 3px;
+	color: #606770;
+	font-size: 14px;
+	line-height: 20px;
+	word-break: break-word;
+	max-height: 80px;
+	overflow: hidden;
+	text-overflow: ellipsis;
+	white-space: normal;
+	margin-top: 3px;
 `;
 
 /**
