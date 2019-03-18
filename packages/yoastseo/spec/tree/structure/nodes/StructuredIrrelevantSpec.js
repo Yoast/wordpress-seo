98fd02d49fea286e55b15e5f18df571608b3952d
Hans-Christiaan <hans-christiaan@yoast.com>
2019-03-18 15:47:16 +0100

    Renamed StructuredIrrelevant to Igonred in test after refactor in Ignored class.

--- packages/yoastseo/spec/tree/structure/nodes/StructuredIrrelevantSpec.js
+++ packages/yoastseo/spec/tree/structure/nodes/StructuredIrrelevantSpec.js
@@ -4,7 +4,7 @@ describe( "StructuredIrrelevant", () => {
 	describe( "constructor", () => {
 		it( "creates a new StructuredIrrelevant node", () => {
 			const structuredIrrelevant = new Ignored( "script" );
-			expect( structuredIrrelevant.type ).toEqual( "StructuredIrrelevant" );
+			expect( structuredIrrelevant.type ).toEqual( "Ignored" );
 			expect( structuredIrrelevant.tag ).toEqual( "script" );
 			expect( structuredIrrelevant instanceof Ignored ).toEqual( true );
 		} );
