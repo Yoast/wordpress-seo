d716e3609f07d168f9ff2ece5cd1e4b9ffead8b3
Johanna de Vos <johanna@yoast.com>
2019-03-18 14:36:43 +0100

    Created the FacebookTitle component.

---
+++ packages/yoast-components/composites/Plugin/SocialPreviews/Facebook/components/FacebookTitle.js
@@ -0,0 +1,39 @@
+import React from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+
+const FacebookTitleWrapper = styled.span`
+	color: #1d2129;
+	font-weight: 600;
+	overflow: hidden;
+	font-size: 16px;
+	line-height: 20px;
+	margin: 5px 0 0;
+	max-height: 110px;
+	word-wrap: break-word;
+	letter-spacing: normal;
+	text-overflow: ellipsis;
+	white-space: normal;
+	cursor: pointer;
+`;
+
+/**
+ * Renders a FacebookTitle component.
+ *
+ * @param {object} props The props.
+ *
+ * @returns {React.Element} The rendered element.
+ */
+const FacebookTitle = ( props ) => {
+	return (
+		<FacebookTitleWrapper>
+			{ props.title }
+		</FacebookTitleWrapper>
+	);
+};
+
+FacebookTitle.propTypes = {
+	title: PropTypes.string.isRequired,
+};
+
+export default FacebookTitle;
