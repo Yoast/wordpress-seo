3ee64843545b8722b8c4c9f657b1105689faa548
Bram <bintzandt@users.noreply.github.com>
2019-03-19 15:52:34 +0100

    Update the script

--- scripts/link_monorepo_to_plugin.sh
+++ scripts/link_monorepo_to_plugin.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env bash
 
-# Go to the root directory of the plugin
-# cd ..
-
 # Get the current branch of the plugin
 CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2-)
 
 # Clone the monorepo to the javascript directory in the root directory of the plugin
-if [ -d javascript ]; then
+if [[ -d javascript ]]; then
   cd javascript
   git pull
 else
@@ -15,24 +12,34 @@ else
   cd javascript
 fi
 
-if [ "$CURRENT_BRANCH" = "trunk" ]; then
-	echo current branch is trunk
-else
-	echo current branch is bla
+JS_BRANCH=${CURRENT_BRANCH}
+
+if [[ "$CURRENT_BRANCH" = "trunk" ]]; then
+	JS_BRANCH="develop"
 fi
 
-exit 0
+git checkout ${JS_BRANCH}
+if [[ $? -ne 0 ]]; then
+	echo -e "${CURRENT_BRANCH} does not exist\nFallback to develop"
+	git checkout develop
+	git pull
+fi
 
-# Run yarn link-all inside the monorepo
+# Just to be sure.
+yarn install
+
+# Run yarn link-all inside the monorepo.
 yarn link-all
-
-# Get all the packages inside the yoastseo package.json that are scoped using @yoast
-packages=($(cat packages/yoastseo/package.json | tr '"' '\n' | grep @yoast))
 
 # Go back to the root directory of the plugin
 cd ..
 
+# Get all the packages inside the yoastseo package.json that are scoped using @yoast
+packages=($(cat package.json | tr '"' '\n' | grep -w @yoast))
+
 for package in ${packages[@]}
 do
-   yarn link $package
+   echo -e "Linking ${package}\n"
+   # We don't want to exit on errors since there are some packages that we simply can't link (yet).
+   yarn link ${package} || true
 done
