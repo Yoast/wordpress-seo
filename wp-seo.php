6b8d9f78b8af577325e2969fe141027ecd1fb247
Andy Meerwaldt <andy@yoast.com>
2019-03-19 08:58:18 +0100

    Bump the version

--- wp-seo.php
+++ wp-seo.php
@@ -8,7 +8,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast SEO
- * Version:     10.0
+ * Version:     10.0.1
  * Plugin URI:  https://yoa.st/1uj
  * Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
  * Author:      Team Yoast
