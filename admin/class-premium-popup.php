6e314c05a3d4899fee915f11694fa354eaa3726a
IreneStr <irene@yoast.com>
2019-03-19 15:49:48 +0100

    Change description below upsell button

--- admin/class-premium-popup.php
+++ admin/class-premium-popup.php
@@ -85,7 +85,7 @@ class WPSEO_Premium_Popup {
 		if ( $popup ) {
 			$classes = ' hidden';
 		}
-		$micro_copy = __( '1 year free updates and upgrades included!', 'wordpress-seo' );
+		$micro_copy = __( '1 year free support and updates included!', 'wordpress-seo' );
 
 		$popup = <<<EO_POPUP
 <div id="wpseo-{$this->identifier}-popup" class="wpseo-premium-popup wp-clearfix$classes">
