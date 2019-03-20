5b5d50a9b952c4a6f720193c744f1a9ea9976aff
Bram <bintzandt@users.noreply.github.com>
2019-03-20 10:37:45 +0100

    Make some changes to the bash script to get the correct variables when running in Travis

--- scripts/link_monorepo_to_plugin.sh
+++ scripts/link_monorepo_to_plugin.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
-# Get the current branch of the plugin
-CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2-)
+# Check to make sure that we get the correct branch if this is being run in TravisCI.
+if [[ ${CI} = "1" ]]; then
+    if [[ ${TRAVIS_PULL_REQUEST_BRANCH} = "" ]]; then
+        CURRENT_BRANCH=${TRAVIS_BRANCH}
+    else
+        CURRENT_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}
+    fi
+else
+    # Get the current branch of the plugin
+    CURRENT_BRANCH=$(git branch | grep \* | cut -d ' ' -f2-)
+fi
 
 # Clone the monorepo to the javascript directory in the root directory of the plugin
 if [[ -d javascript ]]; then
@@ -18,12 +27,14 @@ if [[ "$CURRENT_BRANCH" = "trunk" ]]; then
 	JS_BRANCH="develop"
 fi
 
+# Try to checkout the right branch with a fallback to develop.
 git checkout ${JS_BRANCH}
 if [[ $? -ne 0 ]]; then
 	echo -e "${CURRENT_BRANCH} does not exist\nFallback to develop"
 	git checkout develop
-	git pull
 fi
+
+git pull
 
 # Just to be sure.
 yarn install
@@ -43,3 +54,7 @@ do
    # We don't want to exit on errors since there are some packages that we simply can't link (yet).
    yarn link ${package} || true
 done
+
+# Manually link yoast-components and yoastseo because they don't adhere to the new naming conventions (yet).
+yarn link yoast-components
+yarn link yoastseo
