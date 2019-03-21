f5de998f33eda9f75e60e6067c9b3b04552f33d9
Bram <bintzandt@users.noreply.github.com>
2019-03-21 13:09:05 +0100

    Allow developers to use their own branch for premium-configuration.

--- packages/yoastseo/grunt/config/shell.js
+++ packages/yoastseo/grunt/config/shell.js
@@ -11,18 +11,30 @@ module.exports = function( grunt ) {
 				files = grunt.file.expand( files );
 
 				return "xgettext" +
-				       " --default-domain=<%= shell.makepot.textdomain %>" +
-				       " -o <%= shell.makepot.potFile %>" +
-				       " --package-version=<%= pkg.version %> --package-name=<%= pkg.name %>" +
-				       " --force-po" +
-				       " --from-code=UTF-8" +
-				       " --add-comments=\"translators: \"" +
-				       " --add-comments=\"Translators: \"" +
-				       " " + files.join( " " );
+					" --default-domain=<%= shell.makepot.textdomain %>" +
+					" -o <%= shell.makepot.potFile %>" +
+					" --package-version=<%= pkg.version %> --package-name=<%= pkg.name %>" +
+					" --force-po" +
+					" --from-code=UTF-8" +
+					" --add-comments=\"translators: \"" +
+					" --add-comments=\"Translators: \"" +
+					" " + files.join( " " );
 			},
 		},
 		"get-current-branch": {
-			command: "git branch | grep \\* | cut -d ' ' -f2",
+			command: function() {
+				const commands = [];
+
+				if ( grunt.file.exists( "premium-configuration" ) ) {
+					commands.push( "cd premium-configuration" );
+					commands.push( "git branch | grep \\* | cut -d ' ' -f2" );
+					commands.push( "cd .." );
+				} else {
+					commands.push( "git branch | grep \\* | cut -d ' ' -f2" );
+				}
+
+				return commands.join( "&&" );
+			},
 			options: {
 				// eslint-disable-next-line handle-callback-err
 				callback: function( err, stdout, stderr, cb ) {
@@ -48,6 +60,7 @@ module.exports = function( grunt ) {
 
 				commands.push( "cd premium-configuration" );
 				commands.push( "git fetch" );
+				commands.push( "echo Done" );
 
 				return commands.join( "&&" );
 			},
