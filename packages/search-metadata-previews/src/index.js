f7a10f97bf3a974bfd6b9b92e9e9fc074edf0f5f
Diede Exterkate <Diede@yoast.com>
2019-03-22 15:46:44 +0100

    Dont abbreviate ReplacementVariableEditor

--- packages/search-metadata-previews/src/index.js
+++ packages/search-metadata-previews/src/index.js
@@ -4,6 +4,6 @@ export { default as HelpTextWrapper } from "./snippet-preview/HelpTextWrapper";
 export { default as SnippetPreview } from "./snippet-preview/SnippetPreview";
 
 // Snippet editor exports.
-export { default as ReplaceVarEditor } from "./snippet-editor/ReplacementVariableEditor";
+export { default as ReplacementVariableEditor } from "./snippet-editor/ReplacementVariableEditor";
 export { default as SettingsSnippetEditor } from "./snippet-editor/SettingsSnippetEditor";
 export { default as SnippetEditor } from "./snippet-editor/SnippetEditor";
