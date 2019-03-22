78cbec5bafbe8af8a02b59146c3ac53bec527a29
harmwin <harmwinnemuller@gmail.com>
2019-03-22 15:37:59 +0100

    Implement Twitter background color for image container

--- packages/social-metadata-previews/src/twitter/TwitterImage.js
+++ packages/social-metadata-previews/src/twitter/TwitterImage.js
@@ -19,7 +19,7 @@ const TwitterImageContainer = styled.div`
 	height: ${ props => props.dimensions.height };
 	width: ${ props => props.dimensions.width };
 	overflow: hidden;
-	background-color: ${ colors.$color_white };
+	background-color: #E1E8ED;
 `;
 
 const StyledImage = styled.img`
