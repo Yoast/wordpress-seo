51e9da879c7a5fabfbaa68c6a98d0a6d7704638c
Diede Exterkate <Diede@yoast.com>
2019-03-22 14:29:34 +0100

    Add missing semicolons

--- packages/search-metadata-previews/src/index.js
+++ packages/search-metadata-previews/src/index.js
@@ -1,9 +1,9 @@
 // Snippet preview exports.
-export { default as FixedWidthContainer } from "./snippet-preview/FixedWidthContainer"
+export { default as FixedWidthContainer } from "./snippet-preview/FixedWidthContainer";
 export { default as HelpTextWrapper } from "./snippet-preview/HelpTextWrapper";
 export { default as SnippetPreview } from "./snippet-preview/SnippetPreview";
 
 // Snippet editor exports.
-export { default as ReplaceVarEditor } from "./snippet-editor/ReplacementVariableEditor"
-export { default as SettingsSnippetEditor } from "./snippet-editor/SettingsSnippetEditor"
-export { default as SnippetEditor } from "./snippet-editor/SnippetEditor"
+export { default as ReplaceVarEditor } from "./snippet-editor/ReplacementVariableEditor";
+export { default as SettingsSnippetEditor } from "./snippet-editor/SettingsSnippetEditor";
+export { default as SnippetEditor } from "./snippet-editor/SnippetEditor";
