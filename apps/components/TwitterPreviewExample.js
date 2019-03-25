a370429a7c8da5fa813ac3b926daf8c721269fa5
Johanna de Vos <johanna@yoast.com>
2019-03-25 11:31:06 +0100

    Change headers and descriptions in TwitterPreviewExample, and add an extra example with a portrait image

--- apps/components/TwitterPreviewExample.js
+++ apps/components/TwitterPreviewExample.js
@@ -11,14 +11,23 @@ import TwitterPreview from "../../packages/social-metadata-previews/src/twitter/
 const TwitterPreviewExample = () => {
 	return (
 		<ExamplesContainer backgroundColor="transparent">
-			<h2>TwitterPreview summary with large image card</h2>
+			<h2>Summary with large image card</h2>
 			<TwitterPreview
+				// Try out a landscape picture that isn't yet 2:1
 				src="https://yoast.com/app/uploads/2019/03/Storytelling_FI.jpg"
 				title="YoastCon Workshops &bull; Yoast"
 				siteName="yoast.com"
 			/>
-			<h2>TwitterPreview summary card</h2>
+			<h2>Summary with large image card</h2>
 			<TwitterPreview
+				// Try out a portrait picture
+				src="https://yoast.com/app/uploads/2012/10/yoast-video.png"
+				title="YoastCon Workshops &bull; Yoast"
+				siteName="yoast.com"
+			/>
+			<h2>Summary card</h2>
+			<TwitterPreview
+				// Try out a perfectly square picture (1:1)
 				src="https://yoast.com/app/uploads/2015/09/Avatar_Marieke_500x500-250x250.png"
 				title="YoastCon Workshops &bull; Yoast"
 				siteName="yoast.com"
