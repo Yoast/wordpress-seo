4efc0641fa334386db0c69cb11a71f4f5ec8235f
Irene <irene@yoast.com>
2019-03-25 09:28:45 +0100

    Revert comment style change

--- scripts/maybe-restore-composer-lock.php
+++ scripts/maybe-restore-composer-lock.php
@@ -1,5 +1,5 @@
 <?php
-/** Windows compatible way of executing this conditionally. **/
+// Windows compatible way of executing this conditionally.
 if ( ! getenv( 'TRAVIS' ) ) {
 	exec( 'git checkout composer.lock' );
 }
