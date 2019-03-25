2f7ab077581b9f99bff8bef7da680fe9d7759adf
Johanna de Vos <johanna@yoast.com>
2019-03-25 13:29:32 +0100

    Adapt preview example: show each card type with different original image modes (landscape, portrait, square)

--- apps/components/TwitterPreviewExample.js
+++ apps/components/TwitterPreviewExample.js
@@ -22,8 +22,32 @@ const TwitterPreviewExample = () => {
 			<h2>Summary with large image card (feed portrait)</h2>
 			<TwitterPreview
 				// Try out a portrait picture
-				src="https://yoast.com/app/uploads/2012/10/yoast-video.png"
+				src="https://yoast.com/app/uploads/2018/11/Network_referrals_to_identify_social_traffic_in_Google_Analytics-444x800.png"
 				type="summary-large-image"
+				title="YoastCon Workshops &bull; Yoast"
+				siteName="yoast.com"
+			/>
+			<h2>Summary with large image card (feed square)</h2>
+			<TwitterPreview
+				// Try out a perfectly square picture (1:1)
+				src="https://yoast.com/app/uploads/2015/09/Avatar_Marieke_500x500-250x250.png"
+				type="summary-large-image"
+				title="YoastCon Workshops &bull; Yoast"
+				siteName="yoast.com"
+			/>
+			<h2>Summary card (feed landscape)</h2>
+			<TwitterPreview
+				// Try out a landscape picture that isn't yet 2:1
+				src="https://yoast.com/app/uploads/2019/03/Storytelling_FI.jpg"
+				type="summary"
+				title="YoastCon Workshops &bull; Yoast"
+				siteName="yoast.com"
+			/>
+			<h2>Summary card (feed portrait)</h2>
+			<TwitterPreview
+				// Try out a portrait picture
+				src="https://yoast.com/app/uploads/2018/11/Network_referrals_to_identify_social_traffic_in_Google_Analytics-444x800.png"
+				type="summary"
 				title="YoastCon Workshops &bull; Yoast"
 				siteName="yoast.com"
 			/>
