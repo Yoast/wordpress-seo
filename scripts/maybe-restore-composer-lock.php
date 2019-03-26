df34dc44687991e24423ab96babc18213eea1c59
Irene <irene@yoast.com>
2019-03-26 11:35:19 +0100

    Add file comment

--- scripts/maybe-restore-composer-lock.php
+++ scripts/maybe-restore-composer-lock.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * WPSEO plugin file.
+ *
+ * @package WPSEO\Scripts
+ */
+
 // Windows compatible way of executing this conditionally.
 if ( ! getenv( 'TRAVIS' ) ) {
 	exec( 'git checkout composer.lock' );
