4b3ad1c51cc99bac39a5614307a462a9827102fa
Alexander Botteram <a.botteram@gmail.com>
2019-03-27 11:33:58 +0100

    Refactored URL logic and andded translator comments

--- admin/notifiers/class-subscription-notifier.php
+++ admin/notifiers/class-subscription-notifier.php
@@ -112,7 +112,7 @@ class WPSEO_Subscription_Notifier implements WPSEO_WordPress_Integration {
 	/**
 	 * Initialize notification.
 	 *
-	 * @returns void
+	 * @return void
 	 */
 	public function init() {
 		$this->current_notification_id = get_option( self::EXPIRATION_NOTIFICATION_ID );
@@ -151,36 +151,41 @@ class WPSEO_Subscription_Notifier implements WPSEO_WordPress_Integration {
 		if ( $days_to_expiration < 28 && $days_to_expiration > 7 ) {
 			$this->set_current_notification( self::EXPIRATION_4_WEEKS );
 
-			$message = sprintf( __( 'Your %1$s plugin(s) will expire within 4 weeks. When plugins expire, you will no longer receive updates or support. %2$sRenew now to get a 25%% discount!%3$s', 'wordpress-seo' ), 'Yoast', '<a href="' . esc_url( $this->first_expiring_subscription->renewal_url ) . '"">'  , '</a>' );
+			/* translators: %1$s expands to Yoast, %2$s expands to an opening anchor tag, %3$s expands to a percentage, %4$s expands to an closing anchor tag. */
+			$message = sprintf( __( 'Your %1$s plugin(s) will expire within 4 weeks. When plugins expire, you will no longer receive updates or support. %2$sRenew now to get a %3$s discount!%4$s', 'wordpress-seo' ), 'Yoast', '<a href="' . $this->get_url() . '"">', '25%', '</a>' );
 			$this->show_notification( $message );
 			return;
 		}
 		if ( $days_to_expiration <= 7 && $days_to_expiration > 1 ) {
 			$this->set_current_notification( self::EXPIRATION_1_WEEK );
 
-			$formatted_date = '<b>' . date_i18n( 'F jS, Y', strtotime( $this->first_expiring_subscription->expiry_date ) ) . '</b>';
-			$message        = sprintf( esc_html__( 'Your %1$s plugins are expired! When plugins expire, you will no longer receive updates or support. You have until %2$s to renew with a 25%% discount. %3$sRenew now!%4$s', 'wordpress-seo' ), 'Yoast', $formatted_date, '<a href="' . esc_url( $this->first_expiring_subscription->renewal_url ) . '">', '</a>' );
+			$formatted_date = '<b>' . date_i18n( 'F jS, HH:MM', strtotime( $this->first_expiring_subscription->expiry_date ) ) . '</b>';
+			/* translators: %1$s expands to Yoast, %2$s expands to a formatted date and time (e.g. "Februari 8th 15:00"), %3$s expands to a percentage, %4$s expands to an opening anchor tag, %5$s expands to an closing anchor tag. */
+			$message        = sprintf( esc_html__( 'Your %1$s plugins are about to expire! When plugins expire, you will no longer receive updates or support. You have until %2$s to renew with a %3%s discount. %4$sRenew now!%5$s', 'wordpress-seo' ), 'Yoast', $formatted_date, '25%', '<a href="' . $this->get_url() . '">', '</a>' );
 			$this->show_notification( $message );
 			return;
 		}
 		if ( $days_to_expiration === 1 ) {
 			$this->set_current_notification( self::EXPIRATION_1_DAY );
 
-			$message = sprintf( __( 'Your %1$s plugin(s) will expire in 1 day. When plugins expire, you will no longer receive updates or support. %2$sRenew now to get a 25%% discount!%3$s', 'wordpress-seo' ), 'Yoast', '<a href="' . esc_url( $this->first_expiring_subscription->renewal_url ) . '"">'  , '</a>' );
+			/* translators: %1$s expands to Yoast, %2$s expands to an opening anchor tag, %3$s expands to a percentage, %4$s expands to an closing anchor tag. */
+			$message = sprintf( __( 'Your %1$s plugin(s) will expire in 1 day. When plugins expire, you will no longer receive updates or support. %2$sRenew now to get a %3%s discount!%4$s', 'wordpress-seo' ), 'Yoast', '<a href="' . $this->get_url() . '"">', '25%', '</a>' );
 			$this->show_notification( $message );
 			return;
 		}
 		if ( $days_to_expiration === 0 ) {
 			$this->set_current_notification( self::EXPIRED_1_DAY );
 
-			$message = sprintf( __( 'Your %1$s plugin(s) are expired. When plugins expire you will no longer receive updates or support. Hereby, we give you the opportunity to renew your license with a %2$s25%% discount for a few more days!%3$s', 'wordpress-seo' ), 'Yoast', '<a href="' . esc_url( $this->first_expiring_subscription->renewal_url ) . '">', '</a>' );
+			/* translators: %1$s expands to Yoast, %2$s expands to an opening anchor tag, %3$s expands to a percentage, %4$s expands to an closing anchor tag. */
+			$message = sprintf( __( 'Your %1$s plugin(s) are expired. When plugins expire you will no longer receive updates or support. Hereby, we give you the opportunity to %2$srenew your license with a %3$s discount for a few more days!%3$s', 'wordpress-seo' ), 'Yoast', '<a href="' . $this->get_url() . '">', '25%', '</a>' );
 			$this->show_notification( $message );
 			return;
 		}
 		if ( $days_to_expiration < 0 && $days_to_expiration > -30 ) {
 			$this->set_current_notification( self::EXPIRED_MORE_THAN_1_DAY );
 
-			$message = sprintf( esc_html__( 'When plugins expire, you will no longer receive updates or support. %3$sRenew now!%4$s', 'wordpress-seo' ), 'Yoast', '<a href="' . esc_url( $this->first_expiring_subscription->renewal_url ) . '">', '</a>' );
+			/* translators: %1$s expands to Yoast, %2$s expands to an opening anchor tag, %3$s expands to a percentage, %4$s expands to an closing anchor tag. */
+			$message = sprintf( esc_html__( 'Your %1$s plugin(s) has expired within the last month. When plugins expire, you will no longer receive updates or support. Grab your last chance to %2$srenew with a %3$s discount!%4$s', 'wordpress-seo' ), 'Yoast', '<a href="' . $this->get_url() . '">', '25%', '</a>' );
 			$this->show_notification( $message );
 			return;
 		}
@@ -189,11 +194,107 @@ class WPSEO_Subscription_Notifier implements WPSEO_WordPress_Integration {
 	}
 
 	/**
-	 * Set the current notification id, and if it has changed opposed to the saved id set notification_has_changed.
+	 * Gets the URL for the expiration notification message.
+	 *
+	 * For implementation details see https://github.com/Yoast/wordpress-seo/pull/11170#issuecomment-477026724.
+	 *
+	 * @return string Escaped URL string.
+	 */
+	private function get_url() {
+		if ( ! empty( $this->first_expiring_subscription->renewal_url ) ) {
+			$url = $this->apply_utm_tags( $this->first_expiring_subscription->renewal_url );
+		} else {
+			$url = $this->get_shortlink();
+		}
+
+		return esc_url( $url );
+	}
+
+	/**
+	 * Applies UTM tags to a URL.
+	 *
+	 * @param string $url The url to apply to.
+	 *
+	 * @return string The URL with applied UTM tags.
+	 */
+	private function apply_utm_tags( $url ) {
+		return $url  . $this->get_utm_tags();
+	}
+
+	/**
+	 * Get get UTM tags.
+	 *
+	 * @return string The UTM tags.
+	 */
+	private function get_utm_tags() {
+		$tags = array(
+			'utm_source'   => 'yoast-seo',
+			'utm_medium'   => 'software',
+			'utm_content'  => 'renewal-notification',
+			'utm_campaign' => 'wordpress-ad',
+			'utm_term'     => $this->get_utm_term(),
+		);
+
+		$utm_tags = '#';
+
+		foreach ( $tags as $key => $value ) {
+			$utm_tags .= "${ $key }=${ $value }";
+		}
+
+		return $utm_tags;
+	}
+
+	/**
+	 * Determines the UTM term that should be output.
+	 *
+	 * @return string The UTM term.
+	 */
+	private function get_utm_term () {
+		switch ( $this->current_notification_id ) {
+			case self::EXPIRATION_4_WEEKS:
+				return '4-weeks-before';
+			case self::EXPIRATION_1_WEEK:
+				return '1-week-before';
+			case self::EXPIRATION_1_DAY:
+				return '1-day-before';
+			case self::EXPIRED_1_DAY:
+				return '1-day-after';
+			case self::EXPIRED_MORE_THAN_1_DAY:
+				return '29-days-after';
+		}
+		return '';
+	}
+
+	/**
+	 * Gets the appropriate shortlink based on the expiration date.
+	 *
+	 * @return string The shortlink.
+	 *
+	 * @codeCoverageIgnore
+	 */
+	private function get_shortlink() {
+		switch ( $this->current_notification_id ) {
+			case self::EXPIRATION_4_WEEKS:
+				return WPSEO_Shortlinker::get( 'https://yoa.st/3me' );
+			case self::EXPIRATION_1_WEEK:
+				return WPSEO_Shortlinker::get( 'https://yoa.st/3mf' );
+			case self::EXPIRATION_1_DAY:
+				return WPSEO_Shortlinker::get( 'https://yoa.st/3mg' );
+			case self::EXPIRED_1_DAY:
+				return WPSEO_Shortlinker::get( 'https://yoa.st/3mh' );
+			case self::EXPIRED_MORE_THAN_1_DAY:
+				return WPSEO_Shortlinker::get( 'https://yoa.st/3mi' );
+			default:
+				return '';
+		}
+	}
+
+	/**
+	 * Sets the current notification id, and if it has changed opposed to the saved id set notification_has_changed.
 	 *
 	 * @param string $notification_id Notification id.
 	 *
-	 * @returns void
+	 * @return void
 	 */
 	private function set_current_notification( $notification_id ) {
 		if ( $this->current_notification_id === $notification_id ) {
@@ -207,16 +308,17 @@ class WPSEO_Subscription_Notifier implements WPSEO_WordPress_Integration {
 	/**
 	 * Clean up all persistent data related to this class.
 	 *
-	 * @returns void
+	 * @return void
 	 */
 	private function clean_up() {
+		delete_option( self::EXPIRATION_NOTIFICATION_ID );
 		$this->notification_center->remove_notification_by_id( self::EXPIRATION_NOTIFICATION_ID );
 	}
 
 	/**
 	 * Gets the first add-on that will expire from teh add-on manager.
 	 *
-	 * @returns stdClass|null Object representing a subscription.
+	 * @return stdClass|null Object representing a subscription.
 	 */
 	protected function get_first_expiring_subscription() {
 		// Required to make sure we only get the latest data.
@@ -266,7 +368,7 @@ class WPSEO_Subscription_Notifier implements WPSEO_WordPress_Integration {
 	 *
 	 * @param string $message Message to be shown.
 	 *
-	 * @returns void
+	 * @return void
 	 */
 	private function show_notification( $message ) {
 		if ( $this->notification_has_changed ) {
