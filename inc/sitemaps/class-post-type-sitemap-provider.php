f433adcd75a7119cf27346ac62221ed0760f561e
stodorovic <sasa.todorovic@gmx.com>
2019-03-27 14:40:27 +0100

    Removes inherit for parent posts in attachment-sitemap

--- inc/sitemaps/class-post-type-sitemap-provider.php
+++ inc/sitemaps/class-post-type-sitemap-provider.php
@@ -586,18 +586,19 @@ class WPSEO_Post_Type_Sitemap_Provider implements WPSEO_Sitemap_Provider {
 		$join   = '';
 
 		$post_statuses = array_map( 'esc_sql', WPSEO_Sitemaps::get_post_statuses( $post_type ) );
-
-		$status = "{$wpdb->posts}.post_status IN ('" . implode( "','", $post_statuses ) . "')";
+		$status_where  = "{$wpdb->posts}.post_status IN ('" . implode( "','", $post_statuses ) . "')";
 
 		// Based on WP_Query->get_posts(). R.
 		if ( 'attachment' === $post_type ) {
 			$join   = " LEFT JOIN {$wpdb->posts} AS p2 ON ({$wpdb->posts}.post_parent = p2.ID) ";
-			$status = "p2.post_status IN ('" . implode( "','", $post_statuses ) . "') AND p2.post_password = ''";
+
+			$parent_statuses = array_diff( $post_statuses, array( 'inherit' ) );
+			$status_where    = "p2.post_status IN ('" . implode( "','", $post_statuses ) . "') AND p2.post_password = ''";
 		}
 
 		$where_clause = "
 			{$join}
-			WHERE {$status}
+			WHERE {$status_where}
 				AND {$wpdb->posts}.post_type = %s
 				AND {$wpdb->posts}.post_password = ''
 				AND {$wpdb->posts}.post_date != '0000-00-00 00:00:00'
