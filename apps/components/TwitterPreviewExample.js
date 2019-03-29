dc84c72afa0d01c0eabceceefc452da9dccffb16
Johanna de Vos <johanna@yoast.com>
2019-03-29 10:08:17 +0100

    Add preview examples for both card types, for both short and long descriptions

--- apps/components/TwitterPreviewExample.js
+++ apps/components/TwitterPreviewExample.js
@@ -11,15 +11,41 @@ import TwitterPreview from "../../packages/social-metadata-previews/src/twitter/
 const TwitterPreviewExample = () => {
 	return (
 		<ExamplesContainer backgroundColor="transparent">
-			<h2>TwitterPreview</h2>
+			<h2>Summary with large image card, short description</h2>
 			<TwitterPreview
 				title="YoastCon Workshops &bull; Yoast"
-				description="This is a description"
+				type="summary-large-image"
+				description="This is a description."
 				siteName="yoast.com"
 			/>
-			<h2>TwitterPreview with long description</h2>
+			<h2>Summary with large image card, long description</h2>
 			<TwitterPreview
 				title="YoastCon Workshops &bull; Yoast"
+				type="summary-large-image"
+				description={
+					"A very long description. A very long description. A very long description. A very long description. " +
+					"A very long description. A very long description. A very long description. A very long description. " +
+					"A very long description. A very long description. A very long description. A very long description. " +
+					"A very long description. A very long description. A very long description. A very long description. " +
+					"A very long description. A very long description. A very long description. A very long description. " +
+					"A very long description. A very long description. A very long description. A very long description. " +
+					"A very long description. A very long description. A very long description. A very long description. " +
+					"A very long description. A very long description. A very long description. A very long description. " +
+					"A very long description. A very long description. A very long description. A very long description."
+				}
+				siteName="yoast.com"
+			/>
+			<h2>Summary card, short description</h2>
+			<TwitterPreview
+				title="YoastCon Workshops &bull; Yoast"
+				type="summary"
+				description="This is a description."
+				siteName="yoast.com"
+			/>
+			<h2>Summary card, long description</h2>
+			<TwitterPreview
+				title="YoastCon Workshops &bull; Yoast"
+				type="summary"
 				description={
 					"A very long description. A very long description. A very long description. A very long description. " +
 					"A very long description. A very long description. A very long description. A very long description. " +
@@ -34,7 +60,7 @@ const TwitterPreviewExample = () => {
 				siteName="yoast.com"
 			/>
 		</ExamplesContainer>
-	);
+	)
 };
 
 export default TwitterPreviewExample;
