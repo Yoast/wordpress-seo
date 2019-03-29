acb644f250e5a31cd03b2e96c9550cd712949cc5
Alexander Botteram <a.botteram@gmail.com>
2019-03-29 15:42:26 +0100

    Fixed comsole error

--- admin/config-ui/fields/class-field-profile-url-twitter.php
+++ admin/config-ui/fields/class-field-profile-url-twitter.php
@@ -14,7 +14,7 @@ class WPSEO_Config_Field_Profile_URL_Twitter extends WPSEO_Config_Field {
 	 * WPSEO_Config_Field_Profile_URL_Twitter constructor.
 	 */
 	public function __construct() {
-		parent::__construct( 'profileUrlTwitter', 'LabeledInput' );
+		parent::__construct( 'profileUrlTwitter', 'Input' );
 
 		$this->set_property( 'label', __( 'Twitter Username', 'wordpress-seo' ) );
 	}
