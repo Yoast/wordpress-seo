37e587725fd1688c9e3d75ed3f6d8c8802a98455
Alexander Botteram <a.botteram@gmail.com>
2019-04-01 16:30:45 +0200

    Added initial person selector

--- admin/config-ui/fields/class-field-person.php
+++ admin/config-ui/fields/class-field-person.php
@@ -17,8 +17,23 @@ class WPSEO_Config_Field_Person extends WPSEO_Config_Field {
 		parent::__construct( 'publishingEntityPersonId', 'WordPressUserSelector' );
 
 		$this->set_property( 'label', __( 'The person', 'wordpress-seo' ) );
+		$this->set_property( 'user', $this->map_user_for_input( get_user_by( 'ID', WPSEO_Options::get( 'company_or_person_user_id' ) ) ) );
 
 		$this->set_requires( 'publishingEntityType', 'person' );
+	}
+
+	/**
+	 * Maps the user to be used by the input.
+	 *
+	 * @param WP_User $user The user to be mapped.
+	 *
+	 * @return array The mapped user.
+	 */
+	public function map_user_for_input( WP_User $user ) {
+		return array(
+			'value' => (int) $user->get( 'ID' ),
+			'label' => $user->get( 'display_name' ),
+		);
 	}
 
 	/**
