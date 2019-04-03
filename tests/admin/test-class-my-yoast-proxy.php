76e5169a54cb05c6508936f22817f4efac0151bb
jrfnl <jrfnl@users.noreply.github.com>
2019-04-03 15:11:22 +0200

    Test MyYoastProxy: remove incorrect @expectedException annotation
    These tests should _not_ in actual fact expect an `Exception` as the `Exception` is caught and handled in the `WPSEO_MyYoast_Proxy::render_proxy_page()` method.

--- tests/admin/test-class-my-yoast-proxy.php
+++ tests/admin/test-class-my-yoast-proxy.php
@@ -111,9 +111,6 @@ class WPSEO_MyYoast_Proxy_Test extends WPSEO_UnitTestCase {
 
 	/**
 	 * @covers WPSEO_MyYoast_Proxy::render_proxy_page()
-	 *
-	 * @expectedException        Exception
-	 * @expectedExceptionMessage Received unexpected response from MyYoast
 	 */
 	public function test_render_proxy_page_for_the_research_webworker_file_errored_and_wordpress_not_found() {
 		/** @var WPSEO_MyYoast_Proxy $instance */
@@ -208,9 +205,6 @@ class WPSEO_MyYoast_Proxy_Test extends WPSEO_UnitTestCase {
 
 	/**
 	 * @covers WPSEO_MyYoast_Proxy::render_proxy_page()
-	 *
-	 * @expectedException        Exception
-	 * @expectedExceptionMessage Unable to retrieve file from MyYoast
 	 */
 	public function test_render_proxy_page_via_wordpress_errored() {
 		/** @var WPSEO_MyYoast_Proxy $instance */
