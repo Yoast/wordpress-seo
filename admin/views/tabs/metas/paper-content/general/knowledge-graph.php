34bfa9bc11121832af6986f2276652d57f92399e
Alexander Botteram <a.botteram@gmail.com>
2019-04-04 08:31:56 +0200

    Changed mentions of company to organization

--- admin/views/tabs/metas/paper-content/general/knowledge-graph.php
+++ admin/views/tabs/metas/paper-content/general/knowledge-graph.php
@@ -12,7 +12,7 @@ $knowledge_graph_help = new WPSEO_Admin_Help_Panel(
 	__( 'Learn more about the knowledge graph setting', 'wordpress-seo' ),
 	sprintf(
 	/* translators: %1$s opens the link to the Yoast.com article about Google's Knowledge Graph, %2$s closes the link, */
-		__( 'This data is shown as metadata in your site. It is intended to appear in %1$sGoogle\'s Knowledge Graph%2$s. You can be either a company, or a person.', 'wordpress-seo' ),
+		__( 'This data is shown as metadata in your site. It is intended to appear in %1$sGoogle\'s Knowledge Graph%2$s. You can be either an organization, or a person.', 'wordpress-seo' ),
 		'<a href="' . esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/1-p' ) ) . '" target="_blank" rel="noopener noreferrer">',
 		'</a>'
 	),
@@ -23,21 +23,21 @@ $knowledge_graph_help = new WPSEO_Admin_Help_Panel(
 	<h2 class="help-button-inline"><?php echo esc_html__( 'Knowledge Graph', 'wordpress-seo' ) . $knowledge_graph_help->get_button_html(); ?></h2>
 	<?php echo $knowledge_graph_help->get_panel_html(); ?>
 	<p>
-		<?php esc_html_e( 'Choose whether the site represents a company or a person.', 'wordpress-seo' ); ?>
+		<?php esc_html_e( 'Choose whether the site represents an organization or a person.', 'wordpress-seo' ); ?>
 	</p>
 	<?php
 	$yoast_free_kg_select_options = array(
-		''        => __( 'Choose whether you\'re a company or person', 'wordpress-seo' ),
-		'company' => __( 'Company', 'wordpress-seo' ),
+		''        => __( 'Choose whether you\'re an organization or a person', 'wordpress-seo' ),
+		'company' => __( 'Organization', 'wordpress-seo' ),
 		'person'  => __( 'Person', 'wordpress-seo' ),
 	);
-	$yform->select( 'company_or_person', __( 'Company or person', 'wordpress-seo' ), $yoast_free_kg_select_options, 'styled' );
+	$yform->select( 'company_or_person', __( 'Organization or person', 'wordpress-seo' ), $yoast_free_kg_select_options, 'styled' );
 	?>
 	<div id="knowledge-graph-company">
-		<h3><?php esc_html_e( 'Company', 'wordpress-seo' ); ?></h3>
+		<h3><?php esc_html_e( 'Organization', 'wordpress-seo' ); ?></h3>
 		<?php
-		$yform->textinput( 'company_name', __( 'Company name', 'wordpress-seo' ), array( 'autocomplete' => 'organization' ) );
-		$yform->media_input( 'company_logo', __( 'Company logo', 'wordpress-seo' ) );
+		$yform->textinput( 'company_name', __( 'Organization name', 'wordpress-seo' ), array( 'autocomplete' => 'organization' ) );
+		$yform->media_input( 'company_logo', __( 'Organization logo', 'wordpress-seo' ) );
 		?>
 	</div>
 	<div id="knowledge-graph-person">
