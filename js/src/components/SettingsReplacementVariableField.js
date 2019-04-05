ed60971add7c6ffabe1a54a6229e547740af4093
Diede Exterkate <Diede@yoast.com>
2019-04-05 11:20:36 +0200

    Add missing comma

--- js/src/components/SettingsReplacementVariableField.js
+++ js/src/components/SettingsReplacementVariableField.js
@@ -2,7 +2,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {
-	ReplacementVariableEditor
+	ReplacementVariableEditor,
 	replacementVariablesShape,
 	recommendedReplacementVariablesShape,
 } from "@yoast/search-metadata-previews";
