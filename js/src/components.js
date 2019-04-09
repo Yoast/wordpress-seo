b9d6cd88eb2acac8fa967f56b55b240a6dad6e4b
IreneStr <irene@yoast.com>
2019-04-09 16:10:46 +0200

    Change package order because of dependencies

--- js/src/components.js
+++ js/src/components.js
@@ -1,8 +1,9 @@
+// Don't change this order because some packages depend on each other.
 window.yoast = window.yoast || {};
-window.yoast.components = require( "yoast-components/index.js" );
-window.yoast.helpers = require( "@yoast/helpers" );
-window.yoast.configurationWizard = require( "@yoast/configuration-wizard" );
 window.yoast.styleGuide = require( "@yoast/style-guide" );
+window.yoast.helpers = require( "@yoast/helpers" );
 window.yoast.componentsNew = require( "@yoast/components" );
+window.yoast.configurationWizard = require( "@yoast/configuration-wizard" );
 window.yoast.algoliaSearchBox = require( "@yoast/algolia-search-box" );
 window.yoast.searchMetadataPreviews = require( "@yoast/search-metadata-previews" );
+window.yoast.components = require( "yoast-components/index.js" );
