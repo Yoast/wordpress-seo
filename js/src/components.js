6bb8825526f537732f2a86ec12a1e7b3d09189b5
IreneStr <irene@yoast.com>
2019-04-09 15:29:39 +0200

    Add more yoast packages to window

--- js/src/components.js
+++ js/src/components.js
@@ -4,3 +4,5 @@ window.yoast.helpers = require( "@yoast/helpers" );
 window.yoast.configurationWizard = require( "@yoast/configuration-wizard" );
 window.yoast.styleGuide = require( "@yoast/style-guide" );
 window.yoast.componentsNew = require( "@yoast/components" );
+window.yoast.algoliaSearchBox = require( "@yoast/algolia-search-box" );
+window.yoast.searchMetadataPreview = require( "@yoast/search-metadata-preview" );
