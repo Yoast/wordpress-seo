5727f8cfffd8baa95bca7fabbaeb58d607614a9b
IreneStr <irene@yoast.com>
2019-04-09 15:46:42 +0200

    Fix typo

--- js/src/components.js
+++ js/src/components.js
@@ -5,4 +5,4 @@ window.yoast.configurationWizard = require( "@yoast/configuration-wizard" );
 window.yoast.styleGuide = require( "@yoast/style-guide" );
 window.yoast.componentsNew = require( "@yoast/components" );
 window.yoast.algoliaSearchBox = require( "@yoast/algolia-search-box" );
-window.yoast.searchMetadataPreview = require( "@yoast/search-metadata-preview" );
+window.yoast.searchMetadataPreviews = require( "@yoast/search-metadata-previews" );
