0129041e1981ef63c8f8c13881c26001095f84b2
IreneStr <irene@yoast.com>
2019-04-09 11:29:50 +0200

    Add own webpack config for 'components' which doesn't need all @yoast externals

--- js/src/components.js
+++ js/src/components.js
@@ -1,2 +1,6 @@
 window.yoast = window.yoast || {};
 window.yoast.components = require( "yoast-components/index.js" );
+window.yoast.helpers = require( "@yoast/helpers" );
+window.yoast.configurationWizard = require( "@yoast/configuration-wizard" );
+window.yoast.styleGuide = require( "@yoast/style-guide" );
+window.yoast.componentsNew = require( "@yoast/components" );
