85ab241690e7da30a7d8c5934349d5689d02051c
Alexander Botteram <a.botteram@gmail.com>
2019-04-09 11:08:35 +0200

    Make sure the saved Authohr is loaded when going back to the Organization or person page in the onboarding wizard

--- admin/config-ui/fields/class-field-person.php
+++ admin/config-ui/fields/class-field-person.php
@@ -17,25 +17,8 @@ class WPSEO_Config_Field_Person extends WPSEO_Config_Field {
 		parent::__construct( 'publishingEntityPersonId', 'WordPressUserSelector' );
 
 		$this->set_property( 'label', __( 'The person', 'wordpress-seo' ) );
-		$this->set_property( 'user', $this->get_user_id() );
 
 		$this->set_requires( 'publishingEntityType', 'person' );
-	}
-
-	/**
-	 * Get the entity person id.
-	 *
-	 * @return int|null User id.
-	 */
-	public function get_user_id() {
-		$user = get_user_by( 'ID', WPSEO_Options::get( 'company_or_person_user_id' ) );
-
-		if ( $user === false ) {
-			return null;
-		}
-
-		return (int) $user->get( 'ID' );
-
 	}
 
 	/**
