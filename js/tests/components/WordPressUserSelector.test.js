8e139b4ecab64c932aaa0c5cfd9168c1e247ddaf
Alexander Botteram <a.botteram@gmail.com>
2019-04-11 08:19:21 +0200

    Written additional unit test

--- js/tests/components/WordPressUserSelector.test.js
+++ js/tests/components/WordPressUserSelector.test.js
@@ -1,6 +1,17 @@
 import WordPressUserSelector from "../../src/components/WordPressUserSelector";
 
 describe( "addQueryParams", () => {
+	it( "adds a first parameter", () => {
+		// Decoded: http://local.wordpress.test?search=test post
+		const expected = "http://local.wordpress.test?search=test%20post";
+
+		const actual = WordPressUserSelector.addQueryParams( "http://local.wordpress.test", {
+			search: "test post",
+		} );
+
+		expect( actual ).toBe( expected );
+	} );
+
 	it( "adds an additional parameter", () => {
 		// Decoded: http://local.wordpress.test?search=test post&rest_route=/wp/v2/posts
 		const expected = "http://local.wordpress.test?search=test%20post&rest_route=%2Fwp%2Fv2%2Fposts";
