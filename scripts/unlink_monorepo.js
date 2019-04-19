326698bb70a8dfb59f050861e53d128348524cc6
Diede Exterkate <Diede@yoast.com>
2019-04-19 11:15:14 +0200

    Only unlink project. Keep monorepo packages linked in yarn

--- scripts/unlink_monorepo.js
+++ scripts/unlink_monorepo.js
@@ -1,19 +1,10 @@
 #!/usr/local/bin/node
 const execSync = require( "child_process" ).execSync;
 
-// We cannot use "~" in the cwd field so we have to use a little workaround.
-const homeDirectory = execSync( `echo $HOME` ).toString().split( "\n" )[ 0 ];
-const yarnLinkDir = homeDirectory + "/.config/yarn/link";
-
-const toRemove = execSync( `ls`, { cwd: yarnLinkDir } ).toString().split( "\n" ).filter( value => value.includes( "yoast" ) );
-
-// Remove the symlinks from the yarn link directory.
-var x;
-for ( x in toRemove ) {
-	execSync( `rm -rf ${ toRemove[ x ] }`, { cwd: yarnLinkDir } );
-}
-
 // Remove the symlinks from node_modules.
 execSync( `rm -rf *yoast*`, { cwd: "./node_modules/" } );
-
 console.log( "All previously linked yoast packages have been unlinked." );
+
+console.log( "Reinstalling unlinked packages." );
+// Reinstall the removed yoast packages with Yarn.
+execSync( `yarn install --check-files` );
