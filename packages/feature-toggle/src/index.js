0b4d2c7a9b7b092bacb46d3481958d207a7982fc
Hans-Christiaan <hans-christiaan@yoast.com>
2019-04-23 14:28:21 +0200

    Refactors the feature toggle package to make use of a global variable on the window / worker for saving enabled features.

--- packages/feature-toggle/src/index.js
+++ packages/feature-toggle/src/index.js
@@ -1,39 +1,32 @@
 /**
- * Which features are currently enabled.
- *
- * @type {Object<string,string[]>}
- * @private
- */
-const _enabledFeatures = { };
-
-/**
  * Checks whether the given feature is enabled.
  *
  * @param {string} featureName      The name of the feature to check.
- * @param {string} [namespace=""]   An optional namespace to prepend to the feature name. To avoid possible conflicts between packages.
  *
  * @returns {boolean} `true` when the feature is enabled, `false` if not.
  */
-const isFeatureEnabled = function( featureName, namespace = "" ) {
-	return _enabledFeatures[ namespace ].includes( featureName );
+const isFeatureEnabled = function( featureName ) {
+	if ( self.wpseoFeatureFlags ) {
+		return self.wpseoFeatureFlags.includes( featureName );
+	}
+	return false;
 };
 
 /**
  * Enables the features with the given names.
  *
  * @param {string[]} featureNames   A list of names of the features to enable.
- * @param {string} [namespace=""]   An optional namespace to prepend to each feature name. To avoid possible conflicts between packages.
  *
  * @returns {void}
  */
-const enableFeatures = function( featureNames, namespace = "" ) {
+const enableFeatures = function( featureNames ) {
+	if ( ! self.wpseoFeatureFlags ) {
+		self.wpseoFeatureFlags = featureNames;
+	}
 	// Check whether the features are already enabled, if not: add them.
 	featureNames.forEach( name => {
-		// Create the namespace if it does not exist yet.
-		_enabledFeatures[ namespace ] = _enabledFeatures[ namespace ] || [];
-
-		if ( ! _enabledFeatures[ namespace ].includes( name ) ) {
-			_enabledFeatures[ namespace ].push( name );
+		if ( ! self.wpseoFeatureFlags.includes( name ) ) {
+			self.wpseoFeatureFlags.push( name );
 		}
 	} );
 };
@@ -41,12 +34,10 @@ const enableFeatures = function( featureNames, namespace = "" ) {
 /**
  * Returns the list of enabled features.
  *
- * @param {string} [namespace=""] An optional namespace. If not provided, only the features on the global namespace are returned.
- *
  * @returns {string[]} The list of enabled features.
  */
-const enabledFeatures = function( namespace = "" ) {
-	return _enabledFeatures[ namespace ] || [];
+const enabledFeatures = function() {
+	return self.wpseoFeatureFlags || [];
 };
 
 
