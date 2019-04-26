e70f0a09833e7ea8b9a70cc499d71ee03be4d000
Rami Yushuvaev <r_a_m_i@hotmail.com>
2019-04-26 23:52:03 +0300

    i18n: Merge similar translation strings in admin sidebar

--- admin/views/sidebar.php
+++ admin/views/sidebar.php
@@ -21,7 +21,7 @@ $new_tab_message      = WPSEO_Admin_Utils::get_new_tab_message();
 			<h2>
 				<?php
 				/* translators: %1$s expands to the plugin name */
-				printf( esc_html__( 'Upgrade to %1$s', 'wordpress-seo' ), 'Yoast SEO Premium' );
+				printf( esc_html__( 'Upgrade to %s', 'wordpress-seo' ), 'Yoast SEO Premium' );
 				?>
 			</h2>
 			<ul>
