c31b1fd2866d881f6e24a97f3177bcab68649fa5
Igor <igor@yoast.com>
2019-05-07 12:42:42 +0200

    Convert MyYoast Proxy test to Brain Monkey
    Copy the `expectOutput` and `expectOutputContains` helper functions from `WPSEO_UnitTestCase` to the `Yoast\Tests\TestCase` class.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -52,4 +52,37 @@ abstract class TestCase extends PHPUnit_Framework_TestCase {
 		Monkey\tearDown();
 		parent::tearDown();
 	}
+
+	/**
+	 * Tests for expected output.
+	 *
+	 * @param string $expected    Expected output.
+	 * @param string $description Explanation why this result is expected.
+	 */
+	protected function expectOutput( $expected, $description = '' ) {
+		$output = ob_get_contents();
+		ob_clean();
+
+		$output   = preg_replace( '|\R|', "\r\n", $output );
+		$expected = preg_replace( '|\R|', "\r\n", $expected );
+
+		$this->assertEquals( $expected, $output, $description );
+	}
+
+	/**
+	 * @param string|array $expected Expected output.
+	 */
+	protected function expectOutputContains( $expected ) {
+		$output = preg_replace( '|\R|', "\r\n", ob_get_contents() );
+		ob_clean();
+
+		if ( ! is_array( $expected ) ) {
+			$expected = array( $expected );
+		}
+
+		foreach ( $expected as $needle ) {
+			$found = strpos( $output, $needle );
+			$this->assertTrue( $found !== false, sprintf( 'Expected "%s" to be found in "%s" but couldn\'t find it.', $needle, $output ) );
+		}
+	}
 }
