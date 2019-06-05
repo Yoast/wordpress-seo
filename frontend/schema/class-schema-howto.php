15b2e08c406e40542e4dcaf47240f4f0e2edfafd
Alexander Botteram <a.botteram@gmail.com>
2019-06-05 15:31:55 +0200

    Changed itemListElement in HowToStep schema from object to array

--- frontend/schema/class-schema-howto.php
+++ frontend/schema/class-schema-howto.php
@@ -103,7 +103,7 @@ class WPSEO_Schema_HowTo implements WPSEO_Graph_Piece {
 	 *
 	 * @return string A reference URL.
 	 */
-	private function get_main_schema_id() {
+	protected function get_main_schema_id() {
 		if ( WPSEO_Schema_Article::is_article_post_type() ) {
 			return $this->context->canonical . WPSEO_Schema_IDs::ARTICLE_HASH;
 		}
@@ -154,7 +154,10 @@ class WPSEO_Schema_HowTo implements WPSEO_Graph_Piece {
 		if ( empty( $step['jsonText'] ) ) {
 			return;
 		}
-		$schema_step['itemListElement'] = array(
+
+		$schema_step['itemListElement'] = array();
+
+		$schema_step['itemListElement'][] = array(
 			'@type' => 'HowToDirection',
 			'text'  => $step['jsonText'],
 		);
