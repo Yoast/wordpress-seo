5e434c12f57228514d080c312c0c226b81a0b114
Igor <igor@yoast.com>
2019-07-03 09:53:56 +0200

    Howto schema: mainEntityOfPage fallback to webpage when the site is not represented

--- frontend/schema/class-schema-howto.php
+++ frontend/schema/class-schema-howto.php
@@ -117,7 +117,7 @@ class WPSEO_Schema_HowTo implements WPSEO_Graph_Piece {
 	 * @codeCoverageIgnore
 	 */
 	protected function get_main_schema_id() {
-		if ( WPSEO_Schema_Article::is_article_post_type() ) {
+		if ( $this->context->site_represents !== false && WPSEO_Schema_Article::is_article_post_type() ) {
 			return $this->context->canonical . WPSEO_Schema_IDs::ARTICLE_HASH;
 		}
 
