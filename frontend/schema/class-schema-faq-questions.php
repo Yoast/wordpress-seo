2ac0a4056a36f4cadf6435c2b51fd995bd03ee93
IreneStr <irene@yoast.com>
2019-07-03 13:11:25 +0200

    Strip all HTML tags except for allowed tags from faq question

--- frontend/schema/class-schema-faq-questions.php
+++ frontend/schema/class-schema-faq-questions.php
@@ -80,11 +80,11 @@ class WPSEO_Schema_FAQ_Questions {
 			'@id'            => $this->context->canonical . '#' . $question['id'],
 			'position'       => $this->position ++,
 			'url'            => $this->context->canonical . '#' . $question['id'],
-			'name'           => $question['jsonQuestion'],
+			'name'           => strip_tags( $question['jsonQuestion'] ),
 			'answerCount'    => 1,
 			'acceptedAnswer' => array(
 				'@type' => 'Answer',
-				'text'  => $question['jsonAnswer'],
+				'text'  => strip_tags( $question['jsonAnswer'], '<h1><h2><h3><h4><h5><h6><br><ol><ul><li><a><p><b><strong><i><em>' ),
 			),
 		);
 	}
