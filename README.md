9c471739fbbbaa968b3946bead86cd54a9a1e759
Ashley Hitchcock <ashhitch@users.noreply.github.com>
2019-07-08 13:34:30 +0100

    Feature/dynamic data (#4)
    * Generated Meta title and descriptions
    
    * Code tidy

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 This is an extension to the WPGraphQL plugin (https://github.com/wp-graphql/wp-graphql) that returns Yoast SEO data
 
+## Quick Install
+
+1. Install & activate [WPGraphQL](https://www.wpgraphql.com/)
+2. Clone or download the zip of this repository into your WordPress plugin directory & activate the **WP GraphQL Yoast SEO** plugin
+
+## Usage
+
 To query for the SEO Data:
 
 ```
@@ -30,4 +37,11 @@ To query for the SEO Data:
   }
 }
 
+
+## Notes
+This can be used in production, however it is still under active development.
 ```
+
+## Support
+
+[Open an issue](https://github.com/ashhitch/wp-graphql-yoast-seo/issues)
--- wp-graphql-yoast-seo.php
+++ wp-graphql-yoast-seo.php
@@ -7,71 +7,77 @@
  * Author URI:      https://www.ashleyhitchcock.com
  * Text Domain:     wp-graphql-yoast-seo
  * Domain Path:     /languages
- * Version:         1.0.1
+ * Version:         2.0.0
  *
  * @package         WP_Graphql_YOAST_SEO
  */
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (!defined('ABSPATH')) {
+  exit();
 }
 
-add_action( 'graphql_register_types', function() {
+add_action('graphql_register_types', function () {
+  $post_types = \WPGraphQL::$allowed_post_types;
 
-	$post_types = \WPGraphQL::$allowed_post_types;
-
-	register_graphql_object_type( 'SEO', [
+  register_graphql_object_type('SEO', [
     'fields' => [
-      'title' => [ 'type' => 'String' ],
-      'metaDesc'   => [ 'type' => 'String' ],
-      'focuskw'   => [ 'type' => 'String' ],
-      'metaKeywords'   => [ 'type' => 'String' ],
-      'metaRobotsNoindex'   => [ 'type' => 'String' ],
-      'metaRobotsNofollow'   => [ 'type' => 'String' ],
-      'opengraphTitle'   => [ 'type' => 'String' ],
-      'opengraphDescription'   => [ 'type' => 'String' ],
-      'opengraphImage'   => [ 'type' => 'String' ],
-      'twitterTitle'   => [ 'type' => 'String' ],
-      'twitterDescription'   => [ 'type' => 'String' ],
-      'twitterImage'   => [ 'type' => 'String' ],
-    ],
-  ] );
-    
-	if ( ! empty( $post_types ) && is_array( $post_types ) ) {
-		foreach ( $post_types as $post_type ) {
-			 $post_type_object = get_post_type_object( $post_type );
+      'title' => ['type' => 'String'],
+      'metaDesc' => ['type' => 'String'],
+      'focuskw' => ['type' => 'String'],
+      'metaKeywords' => ['type' => 'String'],
+      'metaRobotsNoindex' => ['type' => 'String'],
+      'metaRobotsNofollow' => ['type' => 'String'],
+      'opengraphTitle' => ['type' => 'String'],
+      'opengraphDescription' => ['type' => 'String'],
+      'opengraphImage' => ['type' => 'String'],
+      'twitterTitle' => ['type' => 'String'],
+      'twitterDescription' => ['type' => 'String'],
+      'twitterImage' => ['type' => 'String']
+    ]
+  ]);
 
-			 if ( isset( $post_type_object->graphql_single_name ) ):
-			 
-					register_graphql_field( $post_type_object->graphql_single_name, 'seo', [
-							'type' => 'SEO',
-							'description' => __( 'The Yoast SEO data of the '.$post_type_object->graphql_single_name, 'wp-graphql' ),
-							'resolve' => function( $post ) {
-								// Base array
-								$seo = array();
+  if (!empty($post_types) && is_array($post_types)) {
+    foreach ($post_types as $post_type) {
+      $post_type_object = get_post_type_object($post_type);
 
-								// Get data					
-								$seo = array(
-									'title' => get_post_meta($post->ID, '_yoast_wpseo_title', true),
-									'metaDesc' => get_post_meta($post->ID, '_yoast_wpseo_metadesc', true),
-									'focuskw' => get_post_meta($post->ID,'_yoast_wpseo_focuskw', true),
-									'metaKeywords' => get_post_meta($post->ID, '_yoast_wpseo_metakeywords', true),
-									'metaRobotsNoindex' => get_post_meta($post->ID, '_yoast_wpseo_meta-robots-noindex', true),
-									'metaRobotsNofollow' => get_post_meta($post->ID, '_yoast_wpseo_meta-robots-nofollow', true),
-									'opengraphTitle' => get_post_meta($post->ID, '_yoast_wpseo_opengraph-title', true),
-									'opengraphDescription' => get_post_meta($post->ID, '_yoast_wpseo_opengraph-description', true),
-									'opengraphImage' => get_post_meta($post->ID, '_yoast_wpseo_opengraph-image', true),
-									'twitterTitle' => get_post_meta($post->ID, '_yoast_wpseo_twitter-title', true),
-									'twitterDescription' => get_post_meta($post->ID, '_yoast_wpseo_twitter-description', true),
-									'twitterImage' => get_post_meta($post->ID, '_yoast_wpseo_twitter-image', true)
-							);
+      if (isset($post_type_object->graphql_single_name)):
+        register_graphql_field($post_type_object->graphql_single_name, 'seo', [
+          'type' => 'SEO',
+          'description' => __('The Yoast SEO data of the ' . $post_type_object->graphql_single_name, 'wp-graphql'),
+          'resolve' => function ($post) {
+            // Connect to Yoast
+            $wpseo_frontend = WPSEO_Frontend::get_instance();
+            $wpseo_frontend->reset();
 
-								return ! empty( $seo ) ? $seo : null;
-							}
-					]);
+            // Base array
+            $seo = array();
 
-				endif;
-			 
-		} 
-	}
+            query_posts(array(
+              'p' => $post->ID,
+              'post_type' => 'any'
+            ));
+            the_post();
 
-} );
+            // Get data
+            $seo = array(
+              'title' => trim($wpseo_frontend->get_content_title($post)),
+              'metaDesc' => trim($wpseo_frontend->metadesc(false)),
+              'focuskw' => trim(get_post_meta($post->ID, '_yoast_wpseo_focuskw', true)),
+              'metaKeywords' => trim(get_post_meta($post->ID, '_yoast_wpseo_metakeywords', true)),
+              'metaRobotsNoindex' => trim(get_post_meta($post->ID, '_yoast_wpseo_meta-robots-noindex', true)),
+              'metaRobotsNofollow' => trim(get_post_meta($post->ID, '_yoast_wpseo_meta-robots-nofollow', true)),
+              'opengraphTitle' => trim(get_post_meta($post->ID, '_yoast_wpseo_opengraph-title', true)),
+              'opengraphDescription' => trim(get_post_meta($post->ID, '_yoast_wpseo_opengraph-description', true)),
+              'opengraphImage' => trim(get_post_meta($post->ID, '_yoast_wpseo_opengraph-image', true)),
+              'twitterTitle' => trim(get_post_meta($post->ID, '_yoast_wpseo_twitter-title', true)),
+              'twitterDescription' => trim(get_post_meta($post->ID, '_yoast_wpseo_twitter-description', true)),
+              'twitterImage' => trim(get_post_meta($post->ID, '_yoast_wpseo_twitter-image', true))
+            );
+            wp_reset_query();
+
+            return !empty($seo) ? $seo : null;
+          }
+        ]);
+      endif;
+    }
+  }
+});
