e096166a825d7faf241e94de6a9e51e4f110c255
Prince Ahmed <36558734+princeahmed@users.noreply.github.com>
2019-07-28 10:55:16 +0600

    fix notice showing
    Notice: Trying to get property of non-object in /var/www/montreal.webpublisherpro.com/htdocs/wp-content/plugins/wordpress-seo/frontend/schema/class-schema-utils.php on line 26
    
    Notice fixed.

--- inc/class-wpseo-image-utils.php
+++ inc/class-wpseo-image-utils.php
@@ -243,7 +243,11 @@ class WPSEO_Image_Utils {
 		// If the file size for the file is over our limit, we're going to go for a smaller version.
 		// @todo Save the filesize to the image metadata.
 		// phpcs:ignore Generic.PHP.NoSilencedErrors.Discouraged -- If file size doesn't properly return, we'll not fail.
-		return @filesize( self::get_absolute_path( $image['path'] ) );
+		if(file_exists(self::get_absolute_path( $image['path'] ))) {
+			return @filesize( self::get_absolute_path( $image['path'] ) );
+		}
+		
+		return false;
 	}
 
 	/**
