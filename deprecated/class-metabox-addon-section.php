a17f2c5fbea596c1eda72ae57b14e7465c2c7c53
IreneStr <irene@yoast.com>
2019-07-30 12:06:49 +0200

    Set deprecated version to 11.9 instead of 11.8

--- deprecated/class-metabox-addon-section.php
+++ deprecated/class-metabox-addon-section.php
@@ -25,7 +25,7 @@ class WPSEO_Metabox_Addon_Tab_Section extends WPSEO_Metabox_Tab_Section {
 	 * @param array  $options      Optional link attributes.
 	 */
 	public function __construct( $name, $link_content, array $tabs = array(), array $options = array() ) {
-		_deprecated_constructor( 'WPSEO_Metabox_Addon_Tab_Section', '11.8' );
+		_deprecated_constructor( 'WPSEO_Metabox_Addon_Tab_Section', '11.9' );
 		parent::__construct( $name, $link_content, $tabs, $options );
 	}
 
@@ -35,19 +35,19 @@ class WPSEO_Metabox_Addon_Tab_Section extends WPSEO_Metabox_Tab_Section {
 	 * @deprecated 11.9
 	 */
 	public function display_content() {
-		_deprecated_function( __METHOD__, '11.8' );
+		_deprecated_function( __METHOD__, '11.9' );
 		?>
 		<div role="tabpanel" id="wpseo-meta-section-addons" aria-labelledby="wpseo-meta-tab-addons" tabindex="0" class="wpseo-meta-section">
 			<div class="wpseo-metabox-tabs-div">
 				<ul class="wpseo-metabox-tabs">
 					<?php
-					// @deprecated 11.8 This functionality has been replaced by the filter: `yoast_free_additional_metabox_sections`.
-					do_action_deprecated( 'wpseo_tab_header', array(), '11.8' );
+					// @deprecated 11.9 This functionality has been replaced by the filter: `yoast_free_additional_metabox_sections`.
+					do_action_deprecated( 'wpseo_tab_header', array(), '11.9' );
 					?>
 				</ul>
 				<?php
-				// @deprecated 11.8 This functionality has been replaced by the filter: `yoast_free_additional_metabox_sections`.
-				do_action_deprecated( 'wpseo_tab_content', array(), '11.8' );
+				// @deprecated 11.9 This functionality has been replaced by the filter: `yoast_free_additional_metabox_sections`.
+				do_action_deprecated( 'wpseo_tab_content', array(), '11.9' );
 				?>
 			</div>
 		</div>
@@ -63,7 +63,7 @@ class WPSEO_Metabox_Addon_Tab_Section extends WPSEO_Metabox_Tab_Section {
 	 * @return bool
 	 */
 	protected function has_sections() {
-		_deprecated_function( __METHOD__, '11.8' );
+		_deprecated_function( __METHOD__, '11.9' );
 		return true;
 	}
 }
