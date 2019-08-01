3f798705eeb5fe54308a3b5f011e76dcadbde14a
Igor <igor@yoast.com>
2019-08-01 10:08:06 +0200

    Output escaping: admin/views/class-yoast-form-fieldset.php
    Deprecated because it is not used.

--- deprecated/class-yoast-form-fieldset.php
+++ deprecated/class-yoast-form-fieldset.php
@@ -52,11 +52,16 @@ class Yoast_Form_Fieldset implements Yoast_Form_Element {
 	/**
 	 * Constructor.
 	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
+	 *
 	 * @param string $id             ID for the fieldset.
 	 * @param string $legend_content The translatable legend text.
 	 * @param string $content        The grouped form elements for the fieldset.
 	 */
 	public function __construct( $id, $legend_content, $content ) {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		$this->id             = $id;
 		$this->legend_content = $legend_content;
 		$this->content        = $content;
@@ -64,8 +69,13 @@ class Yoast_Form_Fieldset implements Yoast_Form_Element {
 
 	/**
 	 * Render the view.
+	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
 	 */
 	public function render_view() {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		/*
 		 * Extract because we want values accessible via variables for later use
 		 * in the view instead of accessing them as an array.
@@ -77,24 +87,39 @@ class Yoast_Form_Fieldset implements Yoast_Form_Element {
 
 	/**
 	 * Start output buffering to catch the form elements to wrap in the fieldset.
+	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
 	 */
 	public function start() {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		ob_start();
 	}
 
 	/**
 	 * Return output buffering with the form elements to wrap in the fieldset.
+	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
 	 */
 	public function end() {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		$this->content = ob_get_clean();
 	}
 
 	/**
 	 * Return the rendered view.
 	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
+	 *
 	 * @return string
 	 */
 	public function get_html() {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		ob_start();
 		$this->render_view();
 		return ob_get_clean();
@@ -102,33 +127,53 @@ class Yoast_Form_Fieldset implements Yoast_Form_Element {
 
 	/**
 	 * Output the rendered view.
+	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
 	 */
 	public function html() {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		echo $this->get_html();
 	}
 
 	/**
 	 * Add attributes to the fieldset default attributes.
 	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
+	 *
 	 * @param array $attributes Array of attributes names and values to merge with the defaults.
 	 */
 	public function add_attributes( $attributes ) {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		$this->attributes = wp_parse_args( $attributes, $this->attributes );
 	}
 
 	/**
 	 * Add attributes to the fieldset legend default attributes.
 	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
+	 *
 	 * @param array $attributes Array of attributes names and values to merge with the defaults.
 	 */
 	public function legend_add_attributes( $attributes ) {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		$this->legend_attributes = wp_parse_args( $attributes, $this->legend_attributes );
 	}
 
 	/**
 	 * Visually hide the fieldset legend but keep it available to assistive technologies.
+	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
 	 */
 	public function legend_hide() {
+		_deprecated_function( __METHOD__, '11.9' );
+
 		$this->legend_attributes = wp_parse_args(
 			array( 'class' => 'screen-reader-text' ),
 			$this->legend_attributes
@@ -137,6 +182,9 @@ class Yoast_Form_Fieldset implements Yoast_Form_Element {
 
 	/**
 	 * Return the set of attributes and content for the fieldset.
+	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
 	 *
 	 * @return array
 	 */
@@ -152,6 +200,9 @@ class Yoast_Form_Fieldset implements Yoast_Form_Element {
 
 	/**
 	 * Return HTML formatted attributes as a string, when there are attributes set.
+	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
 	 *
 	 * @param array $attributes Fieldset or legend attributes.
 	 *
@@ -170,6 +221,9 @@ class Yoast_Form_Fieldset implements Yoast_Form_Element {
 
 	/**
 	 * Escape and format an attribute as an HTML attribute.
+	 *
+	 * @deprecated         11.9
+	 * @codeCoverageIgnore
 	 *
 	 * @param string $value     The value of the attribute.
 	 * @param string $attribute The attribute to look for.
