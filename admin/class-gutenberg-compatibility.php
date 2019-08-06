28a993293c0849e29530508526da74b2771bc002
Igor <igor@yoast.com>
2019-08-06 08:23:32 +0200

    Release 11.8 preparations

--- admin/class-gutenberg-compatibility.php
+++ admin/class-gutenberg-compatibility.php
@@ -15,14 +15,14 @@ class WPSEO_Gutenberg_Compatibility {
 	 *
 	 * @var string
 	 */
-	const CURRENT_RELEASE = '6.1.1';
+	const CURRENT_RELEASE = '6.2.0';
 
 	/**
 	 * The minimally supported version of Gutenberg by the plugin.
 	 *
 	 * @var string
 	 */
-	const MINIMUM_SUPPORTED = '6.1.1';
+	const MINIMUM_SUPPORTED = '6.2.0';
 
 	/**
 	 * Holds the current version.
