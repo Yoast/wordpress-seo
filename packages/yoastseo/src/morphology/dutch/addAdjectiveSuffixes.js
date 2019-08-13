4196b9d627dd4c71defeaa03e08b0076c50e4370
Agnieszka Szuba <agnieszka@yoast.com>
2019-08-13 11:08:52 +0200

    Remove blank line

--- packages/yoastseo/src/morphology/dutch/addAdjectiveSuffixes.js
+++ packages/yoastseo/src/morphology/dutch/addAdjectiveSuffixes.js
@@ -124,7 +124,6 @@ export function addInflectedSuffix( morphologyDataAdjectives, morphologyDataStem
 	stemmedWord = findAndApplyModifications( stemmedWord, inflectedSuffix, morphologyDataStemModifications );
 
 	return stemmedWord.concat( inflectedSuffix );
-
 }
 
 /**
