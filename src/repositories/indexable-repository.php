596e15067e92268fe0b0a3cca970a816cd799b2a
Manuel <manuel@yoast.com>
2019-08-13 14:57:56 +0200

    Add find_by_indexable_id method in indexable repository

--- src/repositories/indexable-repository.php
+++ src/repositories/indexable-repository.php
@@ -87,7 +87,7 @@ class Indexable_Repository extends ORMWrapper {
 	}
 
 	/**
-	 * Retrieves an indexable by its ID and type.
+	 * Retrieves an indexable by the ID and type of the indexable object.
 	 *
 	 * @param int    $object_id   The indexable object ID.
 	 * @param string $object_type The indexable object type.
@@ -148,5 +148,16 @@ class Indexable_Repository extends ORMWrapper {
 		);
 
 		return $indexable;
+	}
+
+	/**
+	 * Retrieves indexables by their IDs.
+	 *
+	 * @param array $indexable_ids The indexable IDs.
+	 *
+	 * @return array|\IdiormResultSet Instances of indexables.
+	 */
+	public function find_by_indexable_ids( $indexable_ids ) {
+		return $this->where_id_in( $indexable_ids )->find_many();
 	}
 }
