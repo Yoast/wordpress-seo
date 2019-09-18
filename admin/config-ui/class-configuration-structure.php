ecffa8db435f015a5ff621f4f13afe14519f8a1f
hwinne <harmwinnemuller@yoast.com>
2019-09-18 18:10:24 +0200

    revert commenting out

--- admin/config-ui/class-configuration-structure.php
+++ admin/config-ui/class-configuration-structure.php
@@ -45,10 +45,10 @@ class WPSEO_Configuration_Structure {
 			'profileUrlWikipedia',
 		),
 		'multipleAuthors'            => array( 'multipleAuthors' ),
-		//'connectGoogleSearchConsole' => array(
-		//	'googleSearchConsoleIntro',
-		//	'connectGoogleSearchConsole',
-		//),
+		 'connectGoogleSearchConsole' => array(
+			'googleSearchConsoleIntro',
+			'connectGoogleSearchConsole',
+		 ),
 		'titleTemplate'              => array(
 			'titleIntro',
 			'siteName',
