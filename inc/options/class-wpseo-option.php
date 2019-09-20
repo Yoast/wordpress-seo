e746d274eb1cbef376f97296a6d6870233726ae4
Andy Meerwaldt <andy@yoast.com>
2019-09-20 11:44:28 +0200

    Sanitize the value that got validated

--- inc/options/class-wpseo-option.php
+++ inc/options/class-wpseo-option.php
@@ -335,7 +335,7 @@ abstract class WPSEO_Option {
 		if ( isset( $dirty[ $key ] ) && $dirty[ $key ] !== '' ) {
 
 			$submitted_url = trim( htmlspecialchars( $dirty[ $key ] ) );
-			$validated_url = filter_var( $submitted_url, FILTER_VALIDATE_URL );
+			$validated_url = filter_var( WPSEO_Utils::sanitize_url( $submitted_url ), FILTER_VALIDATE_URL );
 
 			if ( $validated_url === false ) {
 				if ( function_exists( 'add_settings_error' ) ) {
