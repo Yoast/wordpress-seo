f037753828fe391f9adbb4d57acea9cf31e3b9d3
Herre Groen <herre@yoast.com>
2019-09-23 17:12:12 +0200

    Do not replace replace vars twice in the fallback presenter

--- src/presenters/fallback/meta-description-presenter.php
+++ src/presenters/fallback/meta-description-presenter.php
@@ -14,22 +14,6 @@ use Yoast\WP\Free\Presenters\Abstract_Meta_Description_Presenter;
 class Meta_Description_Presenter extends Abstract_Meta_Description_Presenter {
 
 	/**
-	 * @var \WPSEO_Replace_Vars
-	 */
-	protected $replacement_variables_helper;
-
-	/**
-	 * Meta_Description_Presenter constructor.
-	 *
-	 * @param \WPSEO_Replace_Vars  $replacement_variables_helper The replacement variables helper.
-	 */
-	public function __construct(
-		WPSEO_Replace_Vars $replacement_variables_helper
-	) {
-		$this->replacement_variables_helper = $replacement_variables_helper;
-	}
-
-	/**
 	 * Generates the meta description for an indexable.
 	 *
 	 * @param Indexable $indexable The indexable.
@@ -37,16 +21,10 @@ class Meta_Description_Presenter extends Abstract_Meta_Description_Presenter {
 	 * @return string The meta description.
 	 */
 	protected function generate( Indexable $indexable ) {
-		$meta_description = $indexable->description;
-
-		if ( empty( $meta_description ) ) {
-			$meta_description = '';
+		if ( $indexable->description ) {
+			return $indexable->description;
 		}
 
-		if ( $meta_description !== '' ) {
-			$meta_description = $this->replacement_variables_helper->replace( $meta_description, \get_post( $indexable->object_id ) );
-		}
-
-		return $meta_description;
+		return '';
 	}
 }
