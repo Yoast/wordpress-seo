a69d1c9ffab13681de3047a65b64fb042675b603
omarreiss <omar.reiss@gmail.com>
2019-09-24 13:13:28 +0200

    Add documentation and use prop instead of constant in settings tracking class

--- admin/tracking/class-tracking-settings-data.php
+++ admin/tracking/class-tracking-settings-data.php
@@ -6,15 +6,15 @@
  */
 
 /**
- * Collects anonimized settings data.
+ * Collects anonymized settings data.
  */
 class WPSEO_Tracking_Settings_Data implements WPSEO_Collection {
 
 	/**
-	 * @var array ANONYMOUS_SETTINGS contains all of the option_names which need to be
+	 * @var array $anonymous_settings contains all of the option_names which need to be
 	 * anonimized before they can be sent elsewhere.
 	 */
-	const ANONYMOUS_SETTINGS = [
+	private $anonymous_settings = [
 		"baiduverify", "googleverify", "msverify", "yandexverify", "website_name",
 		"alternate_website_name", "company_logo", "company_name", "person_name",
 		"person_logo", "person_logo_id", "company_logo_id", "facebook_site",
@@ -31,12 +31,18 @@ class WPSEO_Tracking_Settings_Data implements WPSEO_Collection {
 	public function get() {
 
 		return array(
-			'settings' => $this->anonimize_settings( WPSEO_Options::get_all() ),
+			'settings' => $this->anonymize_settings( WPSEO_Options::get_all() ),
 		);
 	}
 
-	private function anonimize_settings( $settings ) {
-		foreach( $this::ANONYMOUS_SETTINGS as $setting ) {
+	/**
+	 * Anonimizes the WPSEO_Options array by replacing all $anonymous_settings values to 'used'.
+	 *
+	 * @param array $settings The settings.
+	 * @return array The anonymized settings.
+	 */
+	private function anonymize_settings( $settings ) {
+		foreach( $this->anonymous_settings as $setting ) {
 			if ( ! empty ( $settings[ $setting ] ) ) {
 				$settings[ $setting ] = 'used';
 			}
