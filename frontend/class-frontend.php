b9878887d27c30c161e4c3c07b718e85b3adc724
Igor <igor@yoast.com>
2019-09-24 14:11:46 +0200

    Add abstract and post-type robots presenter

--- frontend/class-frontend.php
+++ frontend/class-frontend.php
@@ -12,6 +12,7 @@
 class WPSEO_Frontend {
 
 	const METADESC_PRIORITY = 6;
+	const ROBOTS_PRIORITY   = 10;
 
 	/**
 	 * Instance of this class.
@@ -96,7 +97,7 @@ class WPSEO_Frontend {
 		// The head function here calls action wpseo_head, to which we hook all our functionality.
 		add_action( 'wpseo_head', array( $this, 'debug_mark' ), 2 );
 		add_action( 'wpseo_head', array( $this, 'metadesc' ), self::METADESC_PRIORITY );
-		add_action( 'wpseo_head', array( $this, 'robots' ), 10 );
+		add_action( 'wpseo_head', array( $this, 'robots' ), self::ROBOTS_PRIORITY );
 		add_action( 'wpseo_head', array( $this, 'canonical' ), 20 );
 		add_action( 'wpseo_head', array( $this, 'adjacent_rel_links' ), 21 );
 
