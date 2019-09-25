2809dff3e4d3a56b1a55bfaa31b263bf4ee30c1c
Johanna de Vos <johanna@yoast.com>
2019-09-25 14:41:20 +0200

    Remove double that is no longer needed because code was moved

--- tests/doubles/presenters/post-type/twitter-image-presenter-double.php
+++ tests/doubles/presenters/post-type/twitter-image-presenter-double.php
@@ -3,23 +3,11 @@
 namespace Yoast\WP\Free\Tests\Doubles\Presenters\Post_Type;
 
 use Yoast\WP\Free\Presenters\Post_Type\Twitter_Image_Presenter;
-use Yoast\WP\Free\Models\Indexable;
 
 /**
  * Test Helper Class.
  */
 class Twitter_Image_Presenter_Double extends Twitter_Image_Presenter {
-
-	/**
-	 * Retrieves twitter_image from the indexable. If not available, defaults to og_image.
-	 *
-	 * @param Indexable $indexable The indexable.
-	 *
-	 * @return string The image url or an empty string when not found.
-	 */
-	public function retrieve_social_image( Indexable $indexable ) {
-		return parent::retrieve_social_image( $indexable );
-	}
 
 	/**
 	 * Retrieves an attachment page's attachment url.
