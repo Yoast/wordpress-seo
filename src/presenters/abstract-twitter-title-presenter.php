ffd8a7f3648e28009e3e5b00b4c3aab4d21a0fd4
Johanna de Vos <johanna@yoast.com>
2019-09-27 09:18:17 +0200

    Reorder class attributes and methods

--- src/presenters/abstract-twitter-title-presenter.php
+++ src/presenters/abstract-twitter-title-presenter.php
@@ -19,6 +19,15 @@ abstract class Abstract_Twitter_Title_Presenter implements Presenter_Interface {
 	protected $replace_vars_helper;
 
 	/**
+	 * Generates the Twitter title for an indexable.
+	 *
+	 * @param Indexable $indexable The indexable.
+	 *
+	 * @return string The Twitter title.
+	 */
+	abstract public function generate( Indexable $indexable );
+
+	/**
 	 * Returns the Twitter title.
 	 *
 	 * @param Indexable $indexable The indexable.
@@ -34,6 +43,43 @@ abstract class Abstract_Twitter_Title_Presenter implements Presenter_Interface {
 		}
 
 		return '';
+	}
+
+	/**
+	 * @required
+	 *
+	 * Sets the replace vars helper, used by DI.
+	 *
+	 * @param \WPSEO_Replace_Vars $replace_vars_helper The replace vars helper.
+	 */
+	public function set_replace_vars_helper( \WPSEO_Replace_Vars $replace_vars_helper ) {
+		$this->replace_vars_helper = $replace_vars_helper;
+	}
+
+	/**
+	 * Retrieves twitter_title from the indexable.
+	 *
+	 * @param Indexable $indexable The indexable.
+	 *
+	 * @return string The Twitter title or an empty string when not found.
+	 */
+	protected function retrieve_twitter_title( Indexable $indexable ) {
+		if ( $indexable->twitter_title ) {
+			return $indexable->twitter_title;
+		}
+
+		return '';
+	}
+
+	/**
+	 * Gets an object to be used as a source of replacement variables.
+	 *
+	 * @param Indexable $indexable The indexable.
+	 *
+	 * @return array A key => value array of variables that may be replaced.
+	 */
+	protected function get_replace_vars_object( Indexable $indexable ) {
+		return \get_post( $indexable->object_id );
 	}
 
 	/**
@@ -53,41 +99,6 @@ abstract class Abstract_Twitter_Title_Presenter implements Presenter_Interface {
 	}
 
 	/**
-	 * Generates the Twitter title for an indexable.
-	 *
-	 * @param Indexable $indexable The indexable.
-	 *
-	 * @return string The Twitter title.
-	 */
-	abstract public function generate( Indexable $indexable );
-
-	/**
-	 * Retrieves twitter_title from the indexable.
-	 *
-	 * @param Indexable $indexable The indexable.
-	 *
-	 * @return string The Twitter title or an empty string when not found.
-	 */
-	protected function retrieve_twitter_title( Indexable $indexable ) {
-		if ( $indexable->twitter_title ) {
-			return $indexable->twitter_title;
-		}
-
-		return '';
-	}
-
-	/**
-	 * @required
-	 *
-	 * Sets the replace vars helper, used by DI.
-	 *
-	 * @param \WPSEO_Replace_Vars $replace_vars_helper The replace vars helper.
-	 */
-	public function set_replace_vars_helper( \WPSEO_Replace_Vars $replace_vars_helper ) {
-		$this->replace_vars_helper = $replace_vars_helper;
-	}
-
-	/**
 	 * Replace replacement variables in the Twitter title.
 	 *
 	 * @param string    $twitter_title The Twitter title.
@@ -97,16 +108,5 @@ abstract class Abstract_Twitter_Title_Presenter implements Presenter_Interface {
 	 */
 	private function replace_vars( $twitter_title, Indexable $indexable ) {
 		return $this->replace_vars_helper->replace( $twitter_title, $this->get_replace_vars_object( $indexable ) );
-	}
-
-	/**
-	 * Gets an object to be used as a source of replacement variables.
-	 *
-	 * @param Indexable $indexable The indexable.
-	 *
-	 * @return array A key => value array of variables that may be replaced.
-	 */
-	protected function get_replace_vars_object( Indexable $indexable ) {
-		return \get_post( $indexable->object_id );
 	}
 }
