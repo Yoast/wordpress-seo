2f8f5664334738ff266171555a7f09b4604bf84f
Johanna de Vos <johanna@yoast.com>
2019-09-27 14:24:26 +0200

    Port Twitter title to new format

--- src/presentations/indexable-post-type-presentation.php
+++ src/presentations/indexable-post-type-presentation.php
@@ -5,6 +5,8 @@
 
 namespace Yoast\WP\Free\Presentations;
 
+use Yoast\WP\Free\Helpers\Current_Page_Helper;
+use Yoast\WP\Free\Helpers\Meta_Helper;
 use Yoast\WP\Free\Helpers\Options_Helper;
 
 /**
@@ -18,14 +20,29 @@ class Indexable_Post_Type_Presentation extends Indexable_Presentation {
 	private $options_helper;
 
 	/**
+	 * @var Meta_Helper
+	 */
+	private $meta_helper;
+	/**
+	 * @var Current_Page_Helper
+	 */
+	private $current_page_helper;
+
+	/**
 	 * Indexable_Post_Type_Presentation constructor.
 	 *
-	 * @param Options_Helper $options_helper The options helper.
+	 * @param Options_Helper      $options_helper The options helper.
+	 * @param Meta_Helper         $meta_helper
+	 * @param Current_Page_Helper $current_page_helper
 	 */
 	public function __construct(
-		Options_Helper $options_helper
+		Options_Helper $options_helper,
+		Meta_Helper $meta_helper,
+		Current_Page_Helper $current_page_helper
 	) {
 		$this->options_helper = $options_helper;
+		$this->meta_helper    = $meta_helper;
+		$this->current_page_helper = $current_page_helper;
 	}
 
 	/**
@@ -55,6 +72,22 @@ class Indexable_Post_Type_Presentation extends Indexable_Presentation {
 	 */
 	public function generate_og_type() {
 		return 'article';
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function generate_twitter_title() {
+		if ( $this->model->twitter_title ) {
+			return $this->model->twitter_title;
+		}
+
+		$title = $this->meta_helper->get_value( 'twitter-title', $this->current_page_helper->get_simple_page_id() );
+		if ( ! is_string( $title ) ) {
+			return '';
+		}
+
+		return $this->title;
 	}
 
 	/**
