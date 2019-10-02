9d7b408df61e8b989baa7d132e0c3a86c702632d
Johanna de Vos <johanna@yoast.com>
2019-10-02 16:11:28 +0200

    Added more unit tests

---
+++ tests/presentations/indexable-presentation/og-type-test.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Yoast\WP\Free\Tests\Presentations\Indexable_Presentation;
+
+use Yoast\WP\Free\Tests\TestCase;
+
+/**
+ * Class OG_Type_Test
+ *
+ * @coversDefaultClass \Yoast\WP\Free\Presentations\Indexable_Presentation
+ *
+ * @group presentations2
+ */
+class OG_Type_Test extends TestCase {
+	use Presentation_Instance_Builder;
+
+	/**
+	 * Sets up the test class.
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->setInstance();
+	}
+
+	/**
+	 * Tests the situation where the OG type is given.
+	 *
+	 * ::covers generate_og_type
+	 */
+	public function test_generate_og_type() {
+		$this->assertEquals( 'website', $this->instance->generate_og_type() );
+	}
+}
