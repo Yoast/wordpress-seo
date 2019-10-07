a5659dcd35a5305c4477b52492d7359d500f8a04
Shivam Agarwal <gr8shivam25@gmail.com>
2019-10-08 01:42:18 +0530

    Update class-wpseo-image-utils.php
    Made uploads variable static

--- inc/class-wpseo-image-utils.php
+++ inc/class-wpseo-image-utils.php
@@ -24,7 +24,11 @@ class WPSEO_Image_Utils {
 		 */
 		$url = preg_replace( '/(.*)-\d+x\d+\.(jpg|png|gif)$/', '$1.$2', $url );
 		
-		$uploads = wp_get_upload_dir();
+		static $uploads;
+
+		if ( $uploads === null ) {
+			$uploads = wp_get_upload_dir();
+		}
 
 		// Don't try to do this for external URLs.
 		if ( strpos( $url, $uploads['baseurl'] ) !== 0 ) {
