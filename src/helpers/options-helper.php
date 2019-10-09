38e547f7d9db12a3c6f04e200671b8420d1e191b
Johanna de Vos <johanna@yoast.com>
2019-10-09 12:30:48 +0200

    Add twitter title for date archives

--- src/helpers/options-helper.php
+++ src/helpers/options-helper.php
@@ -7,13 +7,16 @@
 
 namespace Yoast\WP\Free\Helpers;
 
+use WPSEO_Option_Titles;
+use WPSEO_Options;
+
 /**
  * Class Options_Helper
  */
 class Options_Helper {
 
 	/**
-	 * Retrieve a single field from any option for the SEO plugin. Keys are always unique.
+	 * Retrieves a single field from any option for the SEO plugin. Keys are always unique.
 	 *
 	 * @param string $key     The key it should return.
 	 * @param mixed  $default The default value that should be returned if the key isn't set.
@@ -21,6 +24,15 @@ class Options_Helper {
 	 * @return mixed|null Returns value if found, $default if not.
 	 */
 	public function get( $key, $default = null ) {
-		return \WPSEO_Options::get( $key, $default );
+		return WPSEO_Options::get( $key, $default );
+	}
+
+	/**
+	 * Retrieves the defaults of the title options.
+	 *
+	 * @return array The title option defaults.
+	 */
+	public function get_title_defaults() {
+		return WPSEO_Option_Titles::get_instance()->get_defaults();
 	}
 }
--- src/presentations/indexable-date-archive-presentation.php
+++ src/presentations/indexable-date-archive-presentation.php
@@ -42,4 +42,21 @@ class Indexable_Date_Archive_Presentation extends Indexable_Presentation {
 
 		return $this->robots_helper->after_generate( $robots );
 	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function generate_twitter_title() {
+		$twitter_title_option = 'title-archive-wpseo';
+		$twitter_title        = $this->options_helper->get( $twitter_title_option );
+
+		if ( empty( $twitter_title ) ) {
+			$default_options = $this->options_helper->get_title_defaults();
+			if ( ! empty( $default_options[ $twitter_title_option ] ) ) {
+				$twitter_title = $default_options[ $twitter_title_option ];
+			}
+		}
+
+		return $twitter_title;
+	}
 }
