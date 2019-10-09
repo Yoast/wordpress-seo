e3513bae89f4bfd2824d318ef7457045f6c20ad5
Andy Meerwaldt <andy@yoast.com>
2019-10-09 11:22:57 +0200

    Fix the failing test

--- tests/builders/indexable-post-builder-test.php
+++ tests/builders/indexable-post-builder-test.php
@@ -5,6 +5,7 @@ namespace Yoast\WP\Free\Tests\Builders;
 use Mockery;
 use Brain\Monkey;
 use Yoast\WP\Free\Builders\Indexable_Post_Builder;
+use Yoast\WP\Free\Helpers\Image_Helper;
 use Yoast\WP\Free\Models\Indexable;
 use Yoast\WP\Free\ORM\ORMWrapper;
 use Yoast\WP\Free\Repositories\SEO_Meta_Repository;
@@ -66,10 +67,20 @@ class Indexable_Post_Builder_Test extends TestCase {
 		$indexable_mock->orm->expects( 'set' )->with( 'description', 'description' );
 		$indexable_mock->orm->expects( 'set' )->with( 'og_title', 'og_title' );
 		$indexable_mock->orm->expects( 'set' )->with( 'og_image', 'og_image' );
+		$indexable_mock->orm->expects( 'set' )->with( 'og_image', null );
 		$indexable_mock->orm->expects( 'set' )->with( 'og_image_id', 'og_image_id' );
+		$indexable_mock->orm->expects( 'set' )->with( 'og_image_id', null );
+		$indexable_mock->orm->expects( 'set' )->with( 'og_image_id', 1 );
+		$indexable_mock->orm->expects( 'set' )->with( 'og_image_source', null );
+		$indexable_mock->orm->expects( 'set' )->with( 'og_image_source', 'featured-image' );
 		$indexable_mock->orm->expects( 'set' )->with( 'og_description', 'og_description' );
 		$indexable_mock->orm->expects( 'set' )->with( 'twitter_title', 'twitter_title' );
 		$indexable_mock->orm->expects( 'set' )->with( 'twitter_image', 'twitter_image' );
+		$indexable_mock->orm->expects( 'set' )->with( 'twitter_image', null );
+		$indexable_mock->orm->expects( 'set' )->with( 'twitter_image_id', null );
+		$indexable_mock->orm->expects( 'set' )->with( 'twitter_image_id', 1 );
+		$indexable_mock->orm->expects( 'set' )->with( 'twitter_image_source', null );
+		$indexable_mock->orm->expects( 'set' )->with( 'twitter_image_source', 'featured-image' );
 		$indexable_mock->orm->expects( 'set' )->with( 'twitter_description', 'twitter_description' );
 		$indexable_mock->orm->expects( 'set' )->with( 'is_cornerstone', true );
 		$indexable_mock->orm->expects( 'set' )->with( 'is_robots_noindex', true );
@@ -83,6 +94,14 @@ class Indexable_Post_Builder_Test extends TestCase {
 		$indexable_mock->orm->expects( 'set' )->with( 'link_count', 5 );
 		$indexable_mock->orm->expects( 'set' )->with( 'incoming_link_count', 2 );
 
+		$indexable_mock->orm->expects( 'get' )->once()->with( 'og_image' );
+		$indexable_mock->orm->expects( 'get' )->twice()->with( 'og_image_id' );
+		$indexable_mock->orm->expects( 'get' )->twice()->with( 'og_image_source' );
+		$indexable_mock->orm->expects( 'get' )->twice()->with( 'twitter_image' );
+		$indexable_mock->orm->expects( 'get' )->twice()->with( 'twitter_image_id' );
+		$indexable_mock->orm->expects( 'get' )->with( 'object_sub_type' );
+		$indexable_mock->orm->expects( 'get' )->with( 'object_id' );
+
 		$seo_meta_repository = Mockery::mock( SEO_Meta_Repository::class );
 		$seo_meta_repository->expects( 'find_by_post_id' )->once()->with( 1 )->andReturn(
 			(object) [
@@ -91,7 +110,13 @@ class Indexable_Post_Builder_Test extends TestCase {
 			]
 		);
 
-		$builder = new Indexable_Post_Builder( $seo_meta_repository );
+		$image_helper = Mockery::mock( Image_Helper::class );
+		$image_helper
+			->expects( 'get_featured_image_id' )
+			->once()
+			->andReturn( 1 );
+
+		$builder = new Indexable_Post_Builder( $seo_meta_repository, $image_helper );
 		$builder->build( 1, $indexable_mock );
 	}
 }
