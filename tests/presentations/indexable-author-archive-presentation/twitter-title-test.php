eae25271da7bb628d28eff5ae6db90fabd551a21
Johanna de Vos <johanna@yoast.com>
2019-10-10 13:14:52 +0200

    Add unit tests for author and date archives

--- tests/presentations/indexable-author-archive-presentation/twitter-title-test.php
+++ tests/presentations/indexable-author-archive-presentation/twitter-title-test.php
@@ -1,18 +1,18 @@
 <?php
 
-namespace Yoast\WP\Free\Tests\Presentations\Indexable_Date_Archive_Presentation;
+namespace Yoast\WP\Free\Tests\Presentations\Indexable_Author_Archive_Presentation;
 
 use Yoast\WP\Free\Tests\TestCase;
 
 /**
  * Class Twitter_Title_Test.
  *
- * @coversDefaultClass \Yoast\WP\Free\Presentations\Indexable_Date_Archive_Presentation
+ * @coversDefaultClass \Yoast\WP\Free\Presentations\Indexable_Author_Archive_Presentation
  *
  * @group presentations
  * @group twitter
  *
- * @package Yoast\Tests\Presentations\Indexable_Date_Archive_Presentation
+ * @package Yoast\Tests\Presentations\Indexable_Author_Archive_Presentation
  */
 class Twitter_Title_Test extends TestCase {
 	use Presentation_Instance_Builder;
