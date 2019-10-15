9b839332c8b80cb2c18c1fb2645d141476d29f64
hwinne <harm@thedesigncompany.nl>
2019-10-15 16:53:13 +0200

    Converted snippet preview desktop and mobile mode buttons to radio buttons

--- packages/search-metadata-previews/src/snippet-editor/ModeSwitcher.js
+++ packages/search-metadata-previews/src/snippet-editor/ModeSwitcher.js
@@ -4,99 +4,75 @@ import styled from "styled-components";
 import { __ } from "@wordpress/i18n";
 import PropTypes from "prop-types";
 
-// Yoast dependencies.
-import { SvgIcon, Button, ScreenReaderText } from "@yoast/components";
-import { colors } from "@yoast/style-guide";
-
 // Internal dependencies.
 import { MODE_DESKTOP, MODE_MOBILE, MODES } from "../snippet-preview/constants";
 
-/**
- * Renders a switcher button.
- *
- * @param {Object}  props The props for this component.
- * @param {boolean} props.isActive Whether or not this button is currently active.
- *
- * @returns {ReactComponent} The rendered component.
- */
-const SwitcherButton = styled( Button )`
-	border: none;
-	border-bottom: 4px solid transparent;
-
-	width: 31px;
-	height: 31px;
-
-	border-color: ${ ( props ) => props.isActive ? colors.$color_snippet_active : "transparent" };
-	color: ${ colors.$color_snippet_active };
-
-	transition: 0.15s color ease-in-out,0.15s background-color ease-in-out,0.15s border-color ease-in-out;
-	transition-property: border-color;
-
-	&:hover, &:focus {
-		background-color: ${ colors.$color_white };
-		border: none;
-		border-bottom: 4px solid transparent;
-		border-color: ${ colors.$color_snippet_focus };
-		color: ${ colors.$color_snippet_focus };
-		box-shadow: none;
-	}
-`;
-
-const MobileButton = styled( SwitcherButton )`
-	border-radius: 3px 0 0 3px;
-`;
-
-const DesktopButton = styled( SwitcherButton )`
-	border-radius: 0 3px 3px 0;
-`;
-
 const Switcher = styled.div`
 	display: inline-block;
-	margin-top: 10px;
-	border: 1px solid #dbdbdb;
-	border-radius: 4px;
-	background-color: #f7f7f7;
+	margin-bottom: 16px;
 	vertical-align: top;
+`;
+
+const ModeLabel = styled.label`
+	font: 400 14px/24px "Open Sans",sans-serif;
+	margin-right: 18px;
+	cursor: pointer;
+	color: #3c4043;
+`;
+
+const ModeRadio = styled.input`
+	margin-right: 10px;
+`;
+
+const SwitcherTitle = styled.h4`
+	margin: .5em 0;
+	color: #3c4043;
 `;
 
 /**
  * Renders a mode switcher between mobile and desktop.
  *
- * @param {Object} props The props for this component.
+ * @param {Object}   props          The props for this component.
  * @param {Function} props.onChange Callback that is called when the mode switches.
  * @param {boolean}  props.active   Which mode is currently active.
  *
  * @returns {ReactElement} The rendered element.
  */
 const ModeSwitcher = ( { onChange, active } ) => {
-	return <Switcher>
-		<MobileButton
-			onClick={ () => onChange( MODE_MOBILE ) }
-			isActive={ active === MODE_MOBILE }
-			aria-pressed={ active === MODE_MOBILE }
-		>
-			<SvgIcon icon="mobile" size="22px" color="currentColor" />
-			<ScreenReaderText>
-				{ __( "Mobile preview", "yoast-components" ) }
-			</ScreenReaderText>
-		</MobileButton>
-
-		<DesktopButton
-			onClick={ () => onChange( MODE_DESKTOP ) }
-			isActive={ active === MODE_DESKTOP }
-			aria-pressed={ active === MODE_DESKTOP }
-		>
-			<SvgIcon icon="desktop" size="18px" color="currentColor" />
-			<ScreenReaderText>
-				{ __( "Desktop preview", "yoast-components" ) }
-			</ScreenReaderText>
-		</DesktopButton>
-	</Switcher>;
+	return ( <Switcher>
+		<SwitcherTitle>{ __( "Preview as", "yoast-components" ) + ":" }</SwitcherTitle>
+		<ModeLabel>
+			<ModeRadio
+				onChange={ () => onChange( MODE_MOBILE ) }
+				checked={ active === MODE_MOBILE }
+				aria-pressed={ active === MODE_MOBILE }
+				type="radio"
+				name="screen"
+				value="mobile"
+			/>
+			<span>{ __( "Mobile result", "yoast-components" ) }</span>
+		</ModeLabel>
+		<ModeLabel>
+			<ModeRadio
+				onChange={ () => onChange( MODE_DESKTOP ) }
+				checked={ active === MODE_DESKTOP }
+				aria-pressed={ active === MODE_DESKTOP }
+				type="radio"
+				name="screen"
+				value="desktop"
+			/>
+			<span>{ __( "Desktop result", "yoast-components" ) }</span>
+		</ModeLabel>
+	</Switcher> );
 };
 
 ModeSwitcher.propTypes = {
 	onChange: PropTypes.func.isRequired,
 	active: PropTypes.oneOf( MODES ),
+};
+
+ModeSwitcher.defaultProps = {
+	active: MODE_MOBILE,
 };
 
 export default ModeSwitcher;
