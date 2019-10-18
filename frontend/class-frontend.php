200bacc2cf250e911aba047f027f74e918d04481
Chris Harmoney <chris@cornershopcreative.com>
2019-10-18 12:21:29 -0400

    Correct copy/paste variable name issue

--- frontend/class-frontend.php
+++ frontend/class-frontend.php
@@ -1105,14 +1105,14 @@ class WPSEO_Frontend {
 
 		// Maybe output the prev link.
 		if ( $paged > 1 ) {
-			$this->adjacent_rel_link( 'prev', previous_posts( false ), ( $page - 1 ), 'page' );
+			$this->adjacent_rel_link( 'prev', previous_posts( false ), ( $paged - 1 ), 'page' );
 		}
 
 		$nextpage = ( intval( $paged ) + 1 );
 
 		// Maybe output the next link.
 		if ( $nextpage <= $max_page ) {
-			$this->adjacent_rel_link( 'next', next_posts( $max_page, false ), ( $page + 1 ), 'page' );
+			$this->adjacent_rel_link( 'next', next_posts( $max_page, false ), ( $paged + 1 ), 'page' );
 		}
 	}
 
