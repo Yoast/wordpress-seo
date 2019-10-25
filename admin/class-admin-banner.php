d1babbab990bc4b7b7f6c2ce0d915a151aa32289
Joost de Valk <joost@yoast.com>
2019-10-25 15:18:04 +0200

    Make sure the banner doesn't show on premium

--- admin/class-admin-banner.php
+++ admin/class-admin-banner.php
@@ -24,16 +24,42 @@ class WPSEO_Admin_Banner implements WPSEO_WordPress_Integration {
 			}
 		}
 
-		if ( WPSEO_Options::get( 'bf_banner_2019_dismissed' ) ) {
+		if ( ! $this->should_run_banner() ) {
 			return;
 		}
 
 		$close_url = add_query_arg( array( 'yst_dismiss_bf' => 1 ) );
+
 		?>
         <div class="yoast_bf_sale">
             <a class="close" href="<?php echo $close_url; ?>" aria-label="Dismiss the Yoast Black Friday Banner">X</a>
             <a class="target" href="https://yoa.st/bf-sale-2019"></a>
         </div>
 		<?php
+	}
+
+	/**
+	 * Determines whether the banner should be shown or not.
+	 *
+	 * @return bool True if it should be shown, false if not.
+	 */
+	private function should_run_banner() {
+		if ( WPSEO_Utils::is_yoast_seo_premium() ) {
+			return false;
+		}
+
+		if ( WPSEO_Options::get( 'bf_banner_2019_dismissed' ) ) {
+			return false;
+		}
+
+		$time = time();
+
+		// 1574938800 is November 28th 12:00 CET.
+		// 1575381600 is December 3rd 15:00 CET.
+		if ( $time >= 1574938800 && $time < 1575381600 ) {
+			return true;
+		}
+
+		return false;
 	}
 }
