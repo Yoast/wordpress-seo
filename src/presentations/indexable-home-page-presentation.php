2af31660644b823b6aa86b7ccf81f5018b455c56
Igor <igor@yoast.com>
2019-10-25 12:38:40 +0200

    Add canonical to home page

--- src/presentations/indexable-home-page-presentation.php
+++ src/presentations/indexable-home-page-presentation.php
@@ -7,10 +7,52 @@
 
 namespace Yoast\WP\Free\Presentations;
 
+use Yoast\WP\Free\Helpers\Rel_Adjacent_Helper;
+
 /**
  * Class Indexable_Presentation
  */
 class Indexable_Home_Page_Presentation extends Indexable_Presentation {
+
+	/**
+	 * Holds the Rel_Adjacent_Helper instance.
+	 *
+	 * @var Rel_Adjacent_Helper
+	 */
+	protected $rel_adjacent;
+
+	/**
+	 * Indexable_Date_Archive_Presentation constructor.
+	 *
+	 * @param Rel_Adjacent_Helper $rel_adjacent The rel adjacent helper.
+	 *
+	 * @codeCoverageIgnore
+	 */
+	public function __construct(
+		Rel_Adjacent_Helper $rel_adjacent
+	) {
+		$this->rel_adjacent = $rel_adjacent;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function generate_canonical() {
+		if ( $this->model->canonical ) {
+			return $this->model->canonical;
+		}
+
+		if ( ! $this->model->permalink ) {
+			return '';
+		}
+
+		$current_page = $this->current_page->get_current_archive_page();
+		if ( $current_page > 1 ) {
+			return $this->rel_adjacent->get_paginated_url( $this->model->permalink, $current_page );
+		}
+
+		return $this->model->permalink;
+	}
 
 	/**
 	 * @inheritDoc
--- tests/presentations/indexable-home-page-presentation/presentation-instance-builder.php
+++ tests/presentations/indexable-home-page-presentation/presentation-instance-builder.php
@@ -3,6 +3,7 @@
 namespace Yoast\WP\Free\Tests\Presentations\Indexable_Home_Page_Presentation;
 
 use Mockery;
+use Yoast\WP\Free\Helpers\Rel_Adjacent_Helper;
 use Yoast\WP\Free\Presentations\Indexable_Home_Page_Presentation;
 use Yoast\WP\Free\Tests\Mocks\Indexable;
 use Yoast\WP\Free\Tests\Presentations\Presentation_Instance_Dependencies;
@@ -24,12 +25,21 @@ trait Presentation_Instance_Builder {
 	protected $instance;
 
 	/**
+	 * Holds the Rel_Adjacent_Helper instance.
+	 *
+	 * @var Rel_Adjacent_Helper
+	 */
+	protected $rel_adjacent;
+
+	/**
 	 * Builds an instance of Indexable_Home_Page_Presentation.
 	 */
 	protected function setInstance() {
 		$this->indexable = new Indexable();
 
-		$instance = Mockery::mock( Indexable_Home_Page_Presentation::class )
+		$this->rel_adjacent = Mockery::mock( Rel_Adjacent_Helper::class );
+
+		$instance = Mockery::mock( Indexable_Home_Page_Presentation::class, [ $this->rel_adjacent ] )
 			->makePartial()
 			->shouldAllowMockingProtectedMethods();
 
