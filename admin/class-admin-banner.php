03f07ae6e6cb8b4e2697ecb30b1ee5d1b5722739
Joost de Valk <joost@yoast.com>
2019-10-25 14:59:08 +0200

    Implement Black Friday 2019 banner

--- admin/class-admin-banner.php
+++ admin/class-admin-banner.php
@@ -1,11 +1,39 @@
 <?php
 
 
-class WPSEO_Admin_Banner {
+class WPSEO_Admin_Banner implements WPSEO_WordPress_Integration {
+	/**
+	 * Registers all hooks to WordPress.
+	 *
+	 * @return void
+	 */
+	public function register_hooks() {
+		add_action( 'admin_notices', array( $this, 'banner' ) );
+	}
 
 	/**
-	 * WPSEO_Admin_Banner constructor.
+	 * Render the admin banner.
 	 */
-	public function __construct() {
+	public function banner() {
+		if ( isset( $_GET['yst_dismiss_bf'] ) ) {
+			if ( $_GET['yst_dismiss_bf'] === '1' ) {
+				WPSEO_Options::set( 'bf_banner_2019_dismissed', true );
+			}
+			if ( $_GET['yst_dismiss_bf'] === '0' ) {
+				WPSEO_Options::set( 'bf_banner_2019_dismissed', false );
+			}
+		}
+
+		if ( WPSEO_Options::get( 'bf_banner_2019_dismissed' ) ) {
+			return;
+		}
+
+		$close_url = add_query_arg( array( 'yst_dismiss_bf' => 1 ) );
+		?>
+        <div class="yoast_bf_sale">
+            <a class="close" href="<?php echo $close_url; ?>" aria-label="Dismiss the Yoast Black Friday Banner">X</a>
+            <a class="target" href="https://yoa.st/bf-sale-2019"></a>
+        </div>
+		<?php
 	}
 }
