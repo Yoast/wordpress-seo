f7e0ba06f4e85dee889c84bbb2a5be284762d4fa
Natalia <natalia@yoast.com>
2019-10-29 11:26:38 +0100

    Add a migration for prominent word indexable column

--- src/models/indexable.php
+++ src/models/indexable.php
@@ -59,6 +59,8 @@ use Yoast\WP\Free\ORM\Yoast_Model;
  * @property string  $twitter_image_id
  * @property string  $twitter_image_source
  * @property string  $twitter_card
+ *
+ * @property int     $prominent_words_version
  */
 class Indexable extends Yoast_Model {
 
