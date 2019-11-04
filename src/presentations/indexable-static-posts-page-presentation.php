03a61f77523bc72409e63b556c96a7685717b8c3
Alexander Botteram <a.botteram@gmail.com>
2019-11-04 16:13:48 +0100

    Fixed paginated canonical for static posts pages

--- src/presentations/indexable-static-posts-page-presentation.php
+++ src/presentations/indexable-static-posts-page-presentation.php
@@ -12,4 +12,21 @@ namespace Yoast\WP\Free\Presentations;
  */
 class Indexable_Static_Posts_Page_Presentation extends Indexable_Post_Type_Presentation {
 	use Archive_Adjacent;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function generate_canonical() {
+		if ( $this->model->canonical ) {
+			return $this->model->canonical;
+		}
+
+		$current_page = $this->pagination->get_current_archive_page_number();
+
+		if ( $current_page > 1 ) {
+			return $this->pagination->get_paginated_url( $this->model->permalink, $current_page );
+		}
+
+		return $this->model->permalink;
+	}
 }
