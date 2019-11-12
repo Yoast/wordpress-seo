073e4eeb0d030a06cd58a6203a3a31aecc21fc83
Andy Meerwaldt <andy@yoast.com>
2019-11-12 11:20:45 +0100

    Adds logic to have bc fallbacks for the most common methods

--- src/backwards-compatibility/frontend.php
+++ src/backwards-compatibility/frontend.php
@@ -8,6 +8,10 @@
 use Yoast\WP\Free\Initializers\Initializer_Interface;
 use Yoast\WP\Free\Memoizer\Meta_Tags_Context_Memoizer;
 use Yoast\WP\Free\Presenters\Canonical_Presenter;
+use Yoast\WP\Free\Presenters\Meta_Description_Presenter;
+use Yoast\WP\Free\Presenters\Rel_Next_Presenter;
+use Yoast\WP\Free\Presenters\Rel_Prev_Presenter;
+use Yoast\WP\Free\Presenters\Robots_Presenter;
 
 /**
  * Class WPSEO_Frontend
@@ -28,6 +32,13 @@ class WPSEO_Frontend implements Initializer_Interface {
 	private $context_memoizer;
 
 	/**
+	 * The replace vars helper.
+	 *
+	 * @var WPSEO_Replace_Vars
+	 */
+	private $replace_vars;
+
+	/**
 	 * @inheritDoc
 	 */
 	public function initialize() {
@@ -45,19 +56,39 @@ class WPSEO_Frontend implements Initializer_Interface {
 	 * WPSEO_Breadcrumbs constructor.
 	 *
 	 * @param Meta_Tags_Context_Memoizer $context_memoizer The context memoizer.
+	 * @param \WPSEO_Replace_Vars        $replace_vars     The replace vars helper.
 	 */
-	public function __construct( Meta_Tags_Context_Memoizer $context_memoizer ) {
+	public function __construct(
+		Meta_Tags_Context_Memoizer $context_memoizer,
+		WPSEO_Replace_Vars $replace_vars
+	) {
 		$this->context_memoizer = $context_memoizer;
+		$this->replace_vars     = $replace_vars;
 	}
 
 	/**
 	 * Catches call to methods that don't exist and might deprecated.
 	 *
-	 * @param string $name      The called method.
+	 * @param string $method    The called method.
 	 * @param array  $arguments The given arguments.
 	 */
-	public function __call( $name, $arguments ) {
-		_deprecated_function( $name, 'WPSEO xx.x' );
+	public function __call( $method, $arguments ) {
+		_deprecated_function( $method, 'WPSEO xx.x' );
+
+		$title_methods = [
+			'title',
+			'fix_woo_title',
+			'get_content_title',
+			'get_seo_title',
+			'get_taxonomy_title',
+			'get_author_title',
+			'get_title_from_options',
+			'get_default_title',
+			'force_wp_title',
+		];
+		if ( in_array( $method, $title_methods, true ) ) {
+			return $this->get_title();
+		}
 
 		return null;
 	}
@@ -69,5 +100,155 @@ class WPSEO_Frontend implements Initializer_Interface {
 	 */
 	public static function get_instance() {
 		return self::$instance;
+	}
+
+	/**
+	 * @param bool $echo        Whether or not to output the canonical element.
+	 * @param bool $un_paged    Whether or not to return the canonical with or without pagination added to the URL.
+	 * @param bool $no_override Whether or not to return a manually overridden canonical.
+	 *
+	 * @return string|void
+	 */
+	public function canonical( $echo = true, $un_paged = false, $no_override = false ) {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		$context = $this->context_memoizer->for_current_page();
+		if ( ! $echo ) {
+			return $context->presentation->canonical;
+		}
+
+		$canonical_presenter = new Canonical_Presenter();
+		echo $canonical_presenter->present( $context->presentation );
+	}
+
+	/**
+	 * Retrieves the meta robots value.
+	 *
+	 * @return string
+	 */
+	public function get_robots() {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		$context = $this->context_memoizer->for_current_page();
+
+		return $context->presentation->robots;
+	}
+
+	/**
+	 * Outputs the meta robots value.
+	 *
+	 * @return string The robots.
+	 */
+	public function robots() {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		$context   = $this->context_memoizer->for_current_page();
+		$presenter = new Robots_Presenter();
+		$presenter->present( $context->presentation );
+	}
+
+	/**
+	 * Determine $robots values for a single post.
+	 *
+	 * @param array $robots  Robots data array.
+	 * @param int   $post_id The post ID for which to determine the $robots values, defaults to current post.
+	 *
+	 * @return array
+	 */
+	public function robots_for_single_post( $robots, $post_id = 0 ) {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		$context = $this->context_memoizer->for_current_page();
+
+		return $context->presentation->robots;
+	}
+
+	/**
+	 * Used for static home and posts pages as well as singular titles.
+	 *
+	 * @param object|null $object If filled, object to get the title for.
+	 *
+	 * @return string The content title.
+	 */
+	private function get_title( $object = null ) {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		$context = $this->context_memoizer->for_current_page();
+		$title   = $context->presentation->title;
+
+		return $this->replace_vars->replace( $title, $context->presentation->replace_vars_object );
+	}
+
+	/**
+	 * This function adds paging details to the title.
+	 *
+	 * @param string $sep         Separator used in the title.
+	 * @param string $seplocation Whether the separator should be left or right.
+	 * @param string $title       The title to append the paging info to.
+	 *
+	 * @return string
+	 */
+	public function add_paging_to_title( $sep, $seplocation, $title ) {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		return $title;
+	}
+
+	/**
+	 * Add part to title, while ensuring that the $seplocation variable is respected.
+	 *
+	 * @param string $sep         Separator used in the title.
+	 * @param string $seplocation Whether the separator should be left or right.
+	 * @param string $title       The title to append the title_part to.
+	 * @param string $title_part  The part to append to the title.
+	 *
+	 * @return string
+	 */
+	public function add_to_title( $sep, $seplocation, $title, $title_part ) {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		if ( 'right' === $seplocation ) {
+			return $title . $sep . $title_part;
+		}
+
+		return $title_part . $sep . $title;
+	}
+
+	/**
+	 * Adds 'prev' and 'next' links to archives.
+	 *
+	 * @link  http://googlewebmastercentral.blogspot.com/2011/09/pagination-with-relnext-and-relprev.html
+	 * @since 1.0.3
+	 */
+	public function adjacent_rel_links() {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		$context = $this->context_memoizer->for_current_page();
+
+		$rel_prev_presenter = new Rel_Prev_Presenter();
+		$rel_prev_presenter->present( $context->presentation );
+
+		$rel_next_presenter = new Rel_Next_Presenter();
+		$rel_next_presenter->present( $context->presentation );
+	}
+
+	/**
+	 * Outputs the meta description element or returns the description text.
+	 *
+	 * @param bool $echo Echo or return output flag.
+	 *
+	 * @return string
+	 */
+	public function metadesc( $echo = true ) {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
+		$context = $this->context_memoizer->for_current_page();
+
+		if ( ! $echo ) {
+			return $context->presentation->meta_description;
+		}
+
+		$presenter = new Meta_Description_Presenter();
+		$presenter->present( $context->presentation );
 	}
 }
