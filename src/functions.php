00f4cf9d1bd2abbd0b9cace9646c231e504513a5
Andy Meerwaldt <andy@yoast.com>
2019-11-15 12:58:01 +0100

    Moves the use-statement.

--- src/functions.php
+++ src/functions.php
@@ -5,13 +5,13 @@
  * @package WPSEO\Internals
  */
 
-use Yoast\WP\Free\Main;
-
 if ( ! defined( 'WPSEO_VERSION' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
 	exit();
 }
+
+use Yoast\WP\Free\Main;
 
 /**
  * Retrieves the main instance.
