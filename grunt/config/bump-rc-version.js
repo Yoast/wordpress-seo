993b6aa8ae2a7ed26d9b3b6db70e3d5a2c02a41a
Johanna de Vos <johanna@yoast.com>
2019-12-05 11:01:50 +0100

    Remove unnecessary regex and add documentation

--- grunt/config/bump-rc-version.js
+++ grunt/config/bump-rc-version.js
@@ -1,9 +1,10 @@
 const fs = require( "fs" );
 
 /**
- * ...
+ * Grunt task to bump the RC version.
  *
  * @param {Object} grunt The grunt helper object.
+ *
  * @returns {void}
  */
 module.exports = function( grunt ) {
@@ -12,22 +13,31 @@ module.exports = function( grunt ) {
 		"Bumps the versions to the next RC",
 		function() {
 			let pluginVersionFlag = grunt.option( "plugin-version" );
+
+			// Retrieve the current plugin version from package.json.
 			const packageJson = JSON.parse( fs.readFileSync( "package.json", "utf8" ) ) || {};
 			const pluginVersionPackageJson = packageJson.yoast.pluginVersion;
 
-			const versionRegex = /\d+.\d+/gm;
-			const strippedVersion = pluginVersionPackageJson.match( versionRegex )[ 0 ];
+			// Strip off the RC part from the current plugin version.
+			const parsedVersion = pluginVersionPackageJson.split( "-RC" );
 
-			// If the passed version matches the version in package.json.
+			// From the resulting array, get the first value (the second value is the RC number).
+			const strippedVersion = parsedVersion[ 0 ];
+
+			// If the flagged version matches the version in package.json, increment the RC version.
 			if ( pluginVersionFlag === strippedVersion ) {
-				const currentRCVersion = pluginVersionPackageJson.split( "-RC" )[ 1 ];
+				const currentRCVersion = parsedVersion[ 1 ] ? parsedVersion[ 1 ] : "0";
 				const bumpedRCVersion = parseInt( currentRCVersion, 10 ) + 1;
 				pluginVersionFlag = pluginVersionFlag + "-RC" + bumpedRCVersion;
-			} else {
+			}
+
+			// Else, the RC is 1.
+			else {
 				pluginVersionFlag = pluginVersionFlag + "-RC1";
 			}
-			console.log( pluginVersionFlag );
 
+			// eslint-disable-next-line no-console
+			console.log( "Bumped the plugin version to ".concat( pluginVersionFlag ).concat( "." ) );
 		}
 	);
 };
