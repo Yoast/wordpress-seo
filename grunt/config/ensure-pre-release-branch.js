0f81005f771df9e502eeaba85f34e6d938e91368
IreneStr <irene@yoast.com>
2019-12-05 14:56:27 +0100

    Set force to true

--- grunt/config/ensure-pre-release-branch.js
+++ grunt/config/ensure-pre-release-branch.js
@@ -34,7 +34,7 @@ module.exports = function( grunt ) {
 
 			grunt.config( "gitcheckout.releaseBranch.options", {
 				branch: branchname,
-				overwrite: false,
+				force: true,
 				create: true,
 			} );
 
