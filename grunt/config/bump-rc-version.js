e1213410748d9e7d94f8cbf036fbf3978ce3cca2
Johanna de Vos <johanna@yoast.com>
2019-12-05 10:36:41 +0100

    Initial commit to bump the RC version number

--- grunt/config/bump-rc-version.js
+++ grunt/config/bump-rc-version.js
@@ -1,3 +1,5 @@
+const fs = require( "fs" );
+
 /**
  * ...
  *
@@ -9,6 +11,23 @@ module.exports = function( grunt ) {
 		"bump-rc-version",
 		"Bumps the versions to the next RC",
 		function() {
+			let pluginVersionFlag = grunt.option( "plugin-version" );
+			const packageJson = JSON.parse( fs.readFileSync( "package.json", "utf8" ) ) || {};
+			const pluginVersionPackageJson = packageJson.yoast.pluginVersion;
+
+			const versionRegex = /\d+.\d+/gm;
+			const strippedVersion = pluginVersionPackageJson.match( versionRegex )[ 0 ];
+
+			// If the passed version matches the version in package.json.
+			if ( pluginVersionFlag === strippedVersion ) {
+				const currentRCVersion = pluginVersionPackageJson.split( "-RC" )[ 1 ];
+				const bumpedRCVersion = parseInt( currentRCVersion, 10 ) + 1;
+				pluginVersionFlag = pluginVersionFlag + "-RC" + bumpedRCVersion;
+			} else {
+				pluginVersionFlag = pluginVersionFlag + "-RC1";
+			}
+			console.log( pluginVersionFlag );
+
 		}
 	);
 };
