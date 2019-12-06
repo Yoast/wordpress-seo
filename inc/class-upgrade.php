5f9c8ea5a191a38e7f815eefdf867a9756523678
Joost de Valk <joost@yoast.com>
2019-12-06 15:43:35 +0100

    Remove BlackFriday option

--- inc/class-upgrade.php
+++ inc/class-upgrade.php
@@ -51,17 +51,18 @@ class WPSEO_Upgrade {
 			'12.1-RC0'  => 'clean_all_notifications',
 			'12.3-RC0'  => 'upgrade_123',
 			'12.4-RC0'  => 'upgrade_124',
+			'12.5-RC0'  => 'upgrade_125',
 		];
 
 		array_walk( $routines, [ $this, 'run_upgrade_routine' ], $version );
 
-		if ( version_compare( $version, '12.5-RC0', '<' ) ) {
+		if ( version_compare( $version, '12.7-RC0', '<' ) ) {
 			/*
 			 * We have to run this by hook, because otherwise:
 			 * - the theme support check isn't available.
 			 * - the notification center notifications are not filled yet.
 			 */
-			add_action( 'init', [ $this, 'upgrade_125' ] );
+			add_action( 'init', [ $this, 'upgrade_127' ] );
 		}
 
 		// Since 3.7.
@@ -677,6 +678,14 @@ class WPSEO_Upgrade {
 		// Removes the WordPress update notification, because it is no longer necessary when WordPress 5.3 is released.
 		$center = Yoast_Notification_Center::get();
 		$center->remove_notification_by_id( 'wpseo-dismiss-wordpress-upgrade' );
+	}
+
+	/**
+	 * Performs the 12.7 upgrade.
+	 */
+	public function upgrade_127() {
+		// Re-save wpseo to make sure bf_banner_2019_dismissed key is gone.
+		$this->cleanup_option_data( 'wpseo' );
 	}
 
 	/**
