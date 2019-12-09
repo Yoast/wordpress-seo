a16dc4fb4740d7ce9b579f792b1812cf3250ca1e
Andy Meerwaldt <andy@yoast.com>
2019-12-09 16:08:13 +0100

    Escapes the translated strings

--- inc/health-check-page-comments.php
+++ inc/health-check-page-comments.php
@@ -32,21 +32,21 @@ class WPSEO_Health_Check_Page_Comments extends WPSEO_Health_Check {
 			return;
 		}
 
-		$this->label          = __( 'Paging comments enabled', 'wordpress-seo' );
+		$this->label          = esc_html__( 'Paging comments enabled', 'wordpress-seo' );
 		$this->status         = self::STATUS_RECOMMENDED;
 		$this->badge['color'] = 'red';
 
-		$this->description  = __( 'Paging comments is enabled. As this is not needed in 999 out of 1000 cases, we recommend you disable it.', 'wordpress-seo' );
+		$this->description  = esc_html__( 'Paging comments is enabled. As this is not needed in 999 out of 1000 cases, we recommend you disable it.', 'wordpress-seo' );
 		$this->description .= '<br />';
 		$this->description .= sprintf(
 			/* translators: %1$s resolves to the opening tag of the link to the comment setting page, %2$s resolves to the closing tag of the link */
-			__( 'To fix this, uncheck the box in front of the "Break comments into pages..." on the %1$sComment settings page%2$s.', 'wordpress-seo' ),
+			esc_html__( 'To fix this, uncheck the box in front of the "Break comments into pages..." on the %1$sComment settings page%2$s.', 'wordpress-seo' ),
 			'<a href="' . esc_url( admin_url( 'options-discussion.php' ) ) . '">',
 			'</a>'
 		);
 
 		$this->actions  = '<a href="' . esc_url( admin_url( 'options-discussion.php' ) ) . '">';
-		$this->actions .= __( 'Go to the comment settings page', 'wordpress-seo' );
+		$this->actions .= esc_html__( 'Go to the comment settings page', 'wordpress-seo' );
 		$this->actions .= '</a>';
 	}
 
