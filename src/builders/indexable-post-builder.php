ce10c8fc070c81191cc3565cf2151cfbff9f1b7c
Igor <igor@yoast.com>
2019-12-11 16:19:07 +0100

    Add is_public to indexable table

--- src/builders/indexable-post-builder.php
+++ src/builders/indexable-post-builder.php
@@ -40,9 +40,11 @@ class Indexable_Post_Builder {
 	 * @return \Yoast\WP\Free\Models\Indexable The extended indexable.
 	 */
 	public function build( $post_id, $indexable ) {
+		$post = \get_post( $post_id );
+
 		$indexable->object_id       = $post_id;
 		$indexable->object_type     = 'post';
-		$indexable->object_sub_type = \get_post_type( $post_id );
+		$indexable->object_sub_type = $post->post_type;
 		$indexable->permalink       = \get_permalink( $post_id );
 
 		$indexable->primary_focus_keyword_score = $this->get_keyword_score(
@@ -80,6 +82,10 @@ class Indexable_Post_Builder {
 		$indexable = $this->set_link_count( $post_id, $indexable );
 
 		$indexable->number_of_pages = $this->get_number_of_pages_for_post( $post_id );
+
+		if ( $post->post_status !== 'publish' || $post->post_password !== '' ) {
+			$indexable->is_public = false;
+		}
 
 		return $indexable;
 	}
