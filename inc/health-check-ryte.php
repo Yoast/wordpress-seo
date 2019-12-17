a805df6d4facab9c88fc02a4330aa8676a70ee6f
Karlijn Bok <karlijn@yoast.com>
2019-12-17 11:49:39 +0100

    Improve switch and add missing translators

--- inc/health-check-ryte.php
+++ inc/health-check-ryte.php
@@ -23,17 +23,20 @@ class WPSEO_Health_Check_Ryte extends WPSEO_Health_Check {
 	public function run() {
 
 		$onpage_option = new WPSEO_OnPage_Option();
-		$status = $onpage_option->get_status();
 
-		switch($status) {
+		switch($onpage_option->get_status()) {
 			case WPSEO_OnPage_Option::IS_NOT_INDEXABLE:
+				// If development mode is on or the blog is not public, don't show this response.
+				if( WPSEO_Utils::is_development_mode() || ( '0' === get_option( 'blog_public' ) ) ) {
+					break;
+				}
 				$this->not_indexable_response();
 				break;
 			case WPSEO_OnPage_Option::CANNOT_FETCH:
 			case WPSEO_OnPage_Option::NOT_FETCHED:
 				$this->unknown_indexability_response();
 				break;
-			case WPSEO_OnPage_Option::IS_INDEXABLE: // $onpage_option->is_enabled() ?
+			case WPSEO_OnPage_Option::IS_INDEXABLE:
 				$this->is_indexable_response();
 				break;
 			default:
@@ -54,7 +57,7 @@ class WPSEO_Health_Check_Ryte extends WPSEO_Health_Check {
 
 		$this->description = esc_html__('Ryte offers a free indexability check for Yoast SEO users, and it has determined that your site cannot be found by search engines. If this site is live or about to become live, this should be fixed.', 'wordpress-seo');
 		$this->description .= sprintf(
-		/* translators: %1$s resolves to the opening tag of the link to the reading setting page, %2$s resolves to the closing tag of the link, %3$s resolves to the opening bold tag, %4$s resolves to the closing bold tag, %5$s resolves to two line breaks */
+		/* translators: %1$s: opening tag of the link to the reading setting page, %2$s:closing tag of the link, %3$s: opening bold tag, %4$s: closing bold tag, %5$s: two line breaks */
 			esc_html__('%5$s As a first step, %1$s go to your site\'s Reading Settings %2$s and make sure the option to discourage search engine visibility is %3$snot enabled%4$s, then re-analyze your site indexability. %5$s', 'wordpress-seo'),
 			'<a href="' . esc_url('/wp-admin/options-reading.php') . '">',
 			'</a>',
@@ -62,13 +65,13 @@ class WPSEO_Health_Check_Ryte extends WPSEO_Health_Check {
 			'</b>',
 			'<br /><br />');
 		$this->description .= sprintf(
-		/* translators: %1$s resolves to the opening tag of the link to the Yoast knowledge base troubleshooting page, %2$s resolves to the closing tag of the link */
+		/* translators: %1$s: opening tag of the link to the Yoast knowledge base troubleshooting page, %2$s: closing tag of the link */
 			esc_html__('If that did not help, %1$s read more about troubleshooting search engine visibility. %2$s', 'wordpress-seo'),
 			'<a href="' . esc_url('https://kb.yoast.com/kb/your-site-isnt-indexable/') . ' "target="_blank">',
 			'</a><br />');
 
 		$this->actions = sprintf(
-		/* translators: %1$s resolves to the opening tag of the link to the Yoast Ryte website, %2$s resolves to the closing tag of the link */
+		/* translators: %1$s: opening tag of the link to the Yoast Ryte website, %2$s: closing tag of the link */
 			esc_html__('%1$s Re-analyze site indexability %2$s', 'wordpress-seo'),
 			'<a class="landing-page button" href= "' . esc_url('https://yoa.st/rytelp') . '" target="_blank">',
 			'</a>');
@@ -83,7 +86,9 @@ class WPSEO_Health_Check_Ryte extends WPSEO_Health_Check {
 
 		$this->description = esc_html__('Ryte offers a free indexability check for Yoast SEO users, and right now it has trouble determining whether search engines can find your site. 
 				 This could have several (legitimate) reasons, and is not a problem in itself, but if this is a live site, it is recommended that you figure out why the Ryte check failed.');
+
 		$this->actions = sprintf(
+		/* translators: %1$s: opening tag of the link to the Yoast knowledge base indexability check doesn't work page, %2$s: closing tag of the link */
 			esc_html__('%1$s Read more about troubleshooting Ryte\'s search engine visibility %2$s' ),
 			'<a href="' . esc_url('https://kb.yoast.com/kb/indexability-check-doesnt-work/') . '"target="_blank">',
 			'</a>' );
@@ -104,7 +109,7 @@ class WPSEO_Health_Check_Ryte extends WPSEO_Health_Check {
 	 */
 	protected function add_yoast_label() {
 		$this->actions .= sprintf(
-		/* translators: %1$s resolves to the start of the paragraph, beginning with the Yoast icon, %2$s resolves to the opening tag of the small italic text, %3$s resolves to the closing tag of the small italic text and the paragraph.  */
+		/* translators: %1$s: start of the paragraph, beginning with the Yoast icon, %2$s: opening tag of the small italic text, %3$s: closing tag of the small italic text and the paragraph.  */
 			esc_html__('%1$s %2$s This issue was reported by the Yoast SEO plugin %3$s', 'wordpress-seo'),
 			'<p><br /><img src="http://one.wordpress.test/wp-content/plugins/wordpress-seo/images/Yoast_Icon_RGB.png" height="20" width="20" align="left"> &nbsp',
 			'<small><i>',
