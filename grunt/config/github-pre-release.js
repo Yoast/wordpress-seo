ba0dda09153ce0ec9a6021c0cd824ce8d773e847
Alexander Botteram <a.botteram@gmail.com>
2019-12-20 13:04:44 +0100

    * Changed GITHUB_TOKEN to GITHUB_ACCESS_TOKEN in .env.example * Reused code to open a visual editor * Various tweaks and CS fixes.

--- grunt/config/github-pre-release.js
+++ grunt/config/github-pre-release.js
@@ -1,26 +1,27 @@
-const spawn = require( "child_process" ).spawnSync;
 const fetch = require( "node-fetch" );
+const getUserInput = require( "./tools/get-user-input" );
 
-function openChangelogEditor( grunt ) {
-	const editor = process.env.VISUAL || process.env.EDITOR || "vim" || "code" || "subl";
+/**
+ * Throws an error.
+ *
+ * @param {Object} response The response object.
+ * @param {Object} grunt    The grunt object.
+ *
+ * @returns {void}
+ */
+async function logError( response, grunt ) {
+	const responseObject = await response.json();
 
-	// Spawn editor and save to changelog_buffer.txt
-	const { status } = spawn( editor, [ "changelog_buffer.txt" ], { stdio: "inherit" } );
-	if ( status !== 0 ) {
-		grunt.fail.fatal( "Something went wrong while editing the changelog." );
+	grunt.log.error( `Status: ${ response.status }` );
+	grunt.log.error( `Message: ${ responseObject.message }` );
+
+	if ( responseObject.errors ) {
+		responseObject.errors.forEach( error => {
+			grunt.log.error( JSON.stringify( error ) );
+		} );
 	}
 
-	// Read out the changelog_buffer.txt contents.
-	const data = grunt.file.read( "changelog_buffer.txt" );
-
-	if ( data.length === 0 ) {
-		grunt.file.delete( "changelog_buffer.txt" );
-		grunt.fail.fatal( "The changelog cannot be empty." );
-	}
-
-	grunt.file.delete( "changelog_buffer.txt" );
-
-	return data;
+	grunt.fail.fatal( "Failed to create a pre release on github." );
 }
 
 /**
@@ -37,29 +38,30 @@ module.exports = function( grunt ) {
 			const done = this.async();
 
 			// Open a text editor to get the changelog.
-			const changelog = openChangelogEditor( grunt );
+			const changelog = await getUserInput( { initialContent: grunt.option( "changelog" ) } );
 			const pluginVersion = grunt.file.readJSON( "package.json" ).yoast.pluginVersion;
 
 			// Creating the release on github through an API request.
 			const github = {};
 			github.apiRoot = "https://api.github.com";
-			github.accesToken = process.env.GITHUB_TOKEN;
+			github.accesToken = process.env.GITHUB_ACCESS_TOKEN;
 
 			// Note: do not uncomment the /Yoast/wordpress-seo URL unless you want to create a real tag. Use a personal fork for testing, instead:
-			//github.api_url = github.apiRoot + "/repos/{ YOUR PERSONAL REPO }/wordpress-seo/releases?access_token=" + github.accesToken;
+			github.api_url = github.apiRoot + "/repos/Xyfi/wordpress-seo/releases?access_token=" + github.accesToken;
 
 			// github.api_url = github.apiRoot + "/repos/Yoast/wordpress-seo/releases?access_token=" + github.accesToken;
 
+			/* eslint-disable camelcase */
 			const releaseData = {
 				tag_name: "v" + pluginVersion,
-				target_commitish: "release/12.8",
+				target_commitish: "master",
 				name: "v" + pluginVersion,
 				body: changelog,
 				draft: false,
 				prerelease: true,
 			};
+			/* eslint-enable camelcase */
 
-			let responseObject;
 			try {
 				const response = await fetch( github.api_url, {
 					method: "POST",
@@ -68,18 +70,12 @@ module.exports = function( grunt ) {
 					},
 					body: JSON.stringify( releaseData ),
 				} );
-				responseObject = await response.json();
 
-				// If there are error(s), throw them into the catch.
-				if ( responseObject.errors && responseObject.errors.length > 0 ) {
-					throw responseObject;
+				if ( ! response.ok ) {
+					await logError( response, grunt );
 				}
 			} catch ( error ) {
-				grunt.log.error( error.message );
-				error.errors.forEach( error => {
-					grunt.log.error( JSON.stringify( error ) );
-				} );
-				grunt.fail.fatal( "Quit due to the errors above" );
+				grunt.fail.fatal( "An error occurred" );
 			}
 
 			// Slack notifier logic.
