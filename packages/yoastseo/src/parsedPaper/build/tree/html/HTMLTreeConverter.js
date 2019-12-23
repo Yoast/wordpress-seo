31c29a468b5e1dca363620c2e320cf3d187fab67
Hans-Christiaan <hans-christiaan@yoast.com>
2019-12-23 11:11:42 +0100

    Refactored HTMLTreeConverter, added JSDoc.

--- packages/yoastseo/src/parsedPaper/build/tree/html/HTMLTreeConverter.js
+++ packages/yoastseo/src/parsedPaper/build/tree/html/HTMLTreeConverter.js
@@ -2,28 +2,34 @@ import { FormattingElement, Heading, Paragraph, StructuredNode, List, ListItem }
 import LeafNode from "../../../structure/tree/nodes/LeafNode";
 import { formattingElements, headings, ignoredHtmlElements } from "./htmlConstants";
 
+/**
+ * Converts a parse5 tree to a tree that can be analyzed.
+ */
 class HTMLTreeConverter {
 	/**
+	 * Converts the parse5 tree to a Yoast tree.
 	 *
-	 * @param parse5Tree
+	 * @param {Object} parse5Tree The parse5 tree to convert.
+	 *
+	 * @returns {module:parsedPaper/structure.Node} The converted tree.
 	 */
 	convert( parse5Tree ) {
-		const root = new StructuredNode( "root" );
-		this._convert( parse5Tree, root, null );
+		const root = new StructuredNode( "root", null );
+		this._convert( parse5Tree, root );
 		return root;
 	}
 
 	/**
 	 * Converts the tree from a parse5 implementation to a Yoast tree.
-	 * @param {Object}   parse5Tree    The parse5 tree to convert.
-	 * @param {Node}     parent The tree in progress.
-	 * @param {LeafNode} lastLeafNode  The last leaf node that was added to the tree.
+	 *
+	 * @param {Object}                                parse5Tree    The parse5 tree to convert.
+	 * @param {module:parsedPaper/structure.Node}     parent        The tree in progress.
 	 *
 	 * @returns {void}
 	 *
 	 * @private
 	 */
-	_convert( parse5Tree, parent, lastLeafNode ) {
+	_convert( parse5Tree, parent ) {
 		if ( parse5Tree.childNodes ) {
 			for ( const node of parse5Tree.childNodes ) {
 				const nodeType = node.nodeName;
@@ -32,7 +38,7 @@ class HTMLTreeConverter {
 
 				if ( ignoredHtmlElements.includes( nodeType ) ) {
 					const formatting = new FormattingElement( nodeType, node.sourceCodeLocation, this._parseAttributes( node.attrs ) );
-					this._addContent( formatting, this._addFormatting, parent, node.sourceCodeLocation );
+					this._addLeafNodeContent( formatting, this._addFormatting, parent, node.sourceCodeLocation );
 				} else if ( nodeType === "p" ) {
 					// Paragraph.
 					child = new Paragraph( node.sourceCodeLocation );
@@ -46,7 +52,7 @@ class HTMLTreeConverter {
 					// List node.
 					child = new List( nodeType === "ol", node.sourceCodeLocation );
 				} else if ( nodeType === "#text" ) {
-					const newParagraph = this._addContent( node.value, this._addText, parent, node.sourceCodeLocation );
+					const newParagraph = this._addLeafNodeContent( node.value, this._addText, parent, node.sourceCodeLocation );
 
 					if ( newParagraph ) {
 						child = newParagraph;
@@ -54,7 +60,7 @@ class HTMLTreeConverter {
 				} else if ( formattingElements.includes( nodeType ) ) {
 					const formatting = new FormattingElement( nodeType, node.sourceCodeLocation, this._parseAttributes( node.attrs ) );
 
-					const newParagraph = this._addContent( formatting, this._addFormatting, parent, node.sourceCodeLocation );
+					const newParagraph = this._addLeafNodeContent( formatting, this._addFormatting, parent, node.sourceCodeLocation );
 
 					if ( newParagraph ) {
 						child = newParagraph;
@@ -66,16 +72,37 @@ class HTMLTreeConverter {
 				if ( child ) {
 					parent.addChild( child );
 					parent = child;
-					lastLeafNode = child instanceof LeafNode ? child : lastLeafNode;
 				}
 
-				this._convert( node, parent, lastLeafNode );
+				this._convert( node, parent );
 			}
 		}
 	}
 
-	_addContent( contentToAdd, add, parent, location ) {
+	/**
+	 * Adds leaf node content (text or formatting) to the tree according to a predefined strategy:
+	 *
+	 * If the content has a leaf node (paragraph, heading, list item) parent or ancestor: add it to this leaf node.
+	 * Else: wrap the content in an implicit paragraph, and add the content to it in such a way that runs
+	 * of phrasing content make up one implicit paragraph.
+	 *
+	 * @param {string|FormattingElement} contentToAdd The content to add.
+	 * @param {function}                 add          A function that adds the content to the specified leaf node.
+	 * @param {Node}                     parent       The parent to which to try to add the content.
+	 * @param {Object}                   location     The location of the content as parsed by parse5.
+	 *
+	 * @returns {null|module:parsedPaper/structure.Paragraph} The implicit paragraph, if one needed to be created.
+	 *
+	 * @private
+	 */
+	_addLeafNodeContent( contentToAdd, add, parent, location ) {
 		const previousChild = this._previousChild( parent );
+
+		if ( parent instanceof LeafNode ) {
+			add( parent, contentToAdd );
+			return null;
+		}
+
 		const leafNodeAncestor = this._leafNodeAncestor( parent );
 
 		if ( leafNodeAncestor instanceof Paragraph ) {
@@ -91,10 +118,30 @@ class HTMLTreeConverter {
 		return child;
 	}
 
+	/**
+	 * Add text to the parent leaf node.
+	 *
+	 * @param {module:parsedPaper/structure.LeafNode} parent The leaf node to which to add the text.
+	 * @param {string}                                text   The text to add to the node.
+	 *
+	 * @returns {void}
+	 *
+	 * @private
+	 */
 	_addText( parent, text ) {
 		parent.appendText( text );
 	}
 
+	/**
+	 * Add a formatting element to the parent leaf node.
+	 *
+	 * @param {module:parsedPaper/structure.LeafNode}          parent     The leaf node to which to add the formatting.
+	 * @param {module:parsedPaper/structure.FormattingElement} formatting The formatting element to add.
+	 *
+	 * @returns {void}
+	 *
+	 * @private
+	 */
 	_addFormatting( parent, formatting ) {
 		parent.addFormatting( formatting );
 	}
@@ -138,23 +185,27 @@ class HTMLTreeConverter {
 		return null;
 	}
 
+	/**
+	 * Try to find the leaf node ancestor of this node. Returns `null` if the node does not have one.
+	 *
+	 * @param {module:parsedPaper/structure.Node} element The node for which to find the leaf node ancestor.
+	 *
+	 * @returns {null|module:parsedPaper/structure.Node} The leaf node ancestor, if it has one, `null` if not.
+	 *
+	 * @private
+	 */
 	_leafNodeAncestor( element ) {
-		const parent = element.parentNode;
+		const parent = element.parent;
 
 		if ( ! parent ) {
 			return null;
 		}
 
-		if ( this._isLeafNode( parent ) ) {
+		if ( element instanceof LeafNode ) {
 			return parent;
 		}
 
 		return this._leafNodeAncestor( parent );
-	}
-
-	_isLeafNode( parse5Node ) {
-		const nodeType = parse5Node.nodeName;
-		return nodeType === "p" || headings.includes( nodeType ) || nodeType === "li";
 	}
 }
 
