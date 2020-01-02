cb9e75ab9e9f84b7da13606a625c4014fb861245
Hans-Christiaan <hans-christiaan@yoast.com>
2020-01-02 16:15:31 +0100

    Removed unneccesary clean up code for after parsing the HTML code.

--- packages/yoastseo/src/parsedPaper/build/tree/cleanup/postParsing.js
+++ packages/yoastseo/src/parsedPaper/build/tree/cleanup/postParsing.js
@@ -1,41 +1,5 @@
 import { LeafNode } from "../../../structure/tree";
-import Ignored from "../../../structure/tree/nodes/Ignored";
 import calculateTextIndices from "./calculateTextIndices";
-import getElementContent from "./getElementContent";
-
-/**
- * Calculates the start and end index of the given node or formatting element,
- * based on its source code location as parsed by `parse5`.
- *
- * @param {module:parsedPaper/structure.Node|module:parsedPaper/structure.FormattingElement} element  The element to set the start and end index of
- *
- * @returns {void}
- *
- * @private
- */
-const calculateSourceIndices = function( element ) {
-	if ( element.location ) {
-		element.sourceStartIndex = element.location.startOffset;
-		element.sourceEndIndex = element.location.endOffset;
-	}
-};
-
-/**
- * Deletes parameters from the element that are used during parsing,
- * but are not needed for analysis.
- *
- * @param {Object} element  The element to delete parameters of.
- *
- * @returns {void}
- *
- * @private
- */
-const deleteParseParameters = function( element ) {
-	delete element.location;
-	delete element.namespace;
-	delete element.tagName;
-	delete element.documentMode;
-};
 
 /**
  * Cleans up a node in the tree.
@@ -48,35 +12,17 @@ const deleteParseParameters = function( element ) {
  * @private
  */
 const cleanUpNode = function( node, html ) {
-	// Set content of ignored node, based on original source code.
-	if ( node instanceof Ignored ) {
-		node.content = getElementContent( node, html );
-	}
-
 	// Clean up formatting elements in headings and paragraphs.
 	if ( node instanceof LeafNode ) {
 		// Start and end position in leaf node's (header's or paragraph's) text without formatting.
 		calculateTextIndices( node, html );
-		node.textContainer.formatting = node.textContainer.formatting.map( element => {
-			// Start and end position in text **with** formatting.
-			calculateSourceIndices( element );
-			deleteParseParameters( element );
-
-			return element;
-		} );
 	}
-
-	calculateSourceIndices( node );
-	deleteParseParameters( node );
-
 	return node;
 };
 
 /**
- * Cleans up the given tree after parsing of the HTML source code.
- *
- * These steps are setting the start and end index of each node and
- * deleting attributes needed for parsing, but not needed for further analysis.
+ * Cleans up the given tree after parsing of the HTML source code
+ * by setting the start and end index of each formatting element in a leaf node's text.
  *
  * @param {module:parsedPaper/structure.Node} tree The tree structure to be cleaned.
  * @param {string} html                     The original HTML source code.
