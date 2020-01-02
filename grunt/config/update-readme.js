7ef91174c67b905d861192dba6ad40553ad91748
Jip <jhp.moors@gmail.com>
2020-01-02 11:49:57 +0100

    Update changelog when version entry exists
    Feeds the entire changelog to the editor when the current version exists, instead of asking for specific input.
    
    Also only remove major versions which do not match the current version.

--- grunt/config/update-readme.js
+++ grunt/config/update-readme.js
@@ -1,4 +1,5 @@
 const getUserInput = require( "./tools/get-user-input" );
+let _isEmpty = require( "lodash/isEmpty" );
 
 /**
  * Class representing a version number.
@@ -44,6 +45,7 @@ class VersionNumber {
 		return this.versionNumber().patch > 0;
 	}
 }
+
 /**
  * ...
  *
@@ -57,45 +59,100 @@ module.exports = function( grunt ) {
 		function() {
 			const done = this.async();
 
-			let changelog = grunt.file.read( "./readme.txt" );
 			const newVersion = grunt.option( "plugin-version" );
-			getUserInput( { initialContent: `= ${newVersion} =` } ).then( newChangelog => {
-				grunt.option( "changelog", newChangelog );
-				const versionNumber = new VersionNumber( newVersion );
+			const versionNumber = new VersionNumber( newVersion );
 
-				// Only if the version is not a patch we remove old changelog entries.
-				if ( ! versionNumber.isPatch() ) {
-					const releaseInChangelog = /[=] \d+\.\d+(\.\d+)? =/g;
-					const allReleasesInChangelog = changelog.match( releaseInChangelog );
-					const sanitizedVersionNumbers = allReleasesInChangelog.map(
-						element => new VersionNumber( element.slice( 2, element.length - 2 ) )
+			let changelog = grunt.file.read( "./readme.txt" );
+
+			const releaseInChangelog = /[=] \d+\.\d+(\.\d+)? =/g;
+			const allReleasesInChangelog = changelog.match( releaseInChangelog );
+			const changelogVersions = allReleasesInChangelog.map(
+				element => new VersionNumber( element.slice( 2, element.length - 2 ) )
+			);
+
+			// Only if the version is not a patch we remove old changelog entries.
+			if ( ! versionNumber.isPatch() ) {
+				let cleanedChangelog = changelog;
+
+				// Remove the current version from the list; this should not be removed.
+				const relevantChangelogVersions = changelogVersions.filter( version => {
+					return ! (
+						versionNumber.major === version.major &&
+						versionNumber.minor === version.minor &&
+						versionNumber.patch === version.patch
 					);
-					const highestMajor = Math.max( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.major ) );
-					const lowestMajor = Math.min( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.major ) );
+				} );
 
-					if ( highestMajor !== lowestMajor ) {
-						// If there are multiple major versions in the current changelog, remove all entries from the oldest major version.
-						changelog = changelog.replace( new RegExp( "= " + lowestMajor + "(.|\\n)*= Earlier versions =" ), "= Earlier versions =" );
-					} else {
-						// If there are only multiple minor versions of the same major version, remove all entries from the oldest minor version.
-						const lowestMinor = Math.min( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.minor ) );
-						const lowestVersion = `${lowestMajor}.${lowestMinor}`;
-						changelog = changelog.replace( new RegExp( "= " + lowestVersion + "(.|\\n)*= Earlier versions =" ), "= Earlier versions =" );
-					}
+				const highestMajor = Math.max( ...relevantChangelogVersions.map( version => version.major ) );
+				const lowestMajor = Math.min( ...relevantChangelogVersions.map( version => version.major ) );
+
+				if ( highestMajor === lowestMajor ) {
+					// If there are only multiple minor versions of the same major version, remove all entries from the oldest minor version.
+					const lowestMinor = Math.min( ...relevantChangelogVersions.map( version => version.minor ) );
+					const lowestVersion = `${lowestMajor}.${lowestMinor}`;
+					cleanedChangelog = changelog.replace(
+						new RegExp( "= " + lowestVersion + "(.|\\n)*= Earlier versions =" ),
+						"= Earlier versions ="
+					);
+				} else {
+					// If there are multiple major versions in the current changelog, remove all entries from the oldest major version.
+					cleanedChangelog = changelog.replace(
+						new RegExp( "= " + lowestMajor + "(.|\\n)*= Earlier versions =" ),
+						"= Earlier versions ="
+					);
 				}
-				// Add the user input to the changelog.
-				changelog = changelog.replace( /[=]= Changelog ==/ig, "== Changelog ==\n\n" + newChangelog.trim() );
-				grunt.file.write( "./readme.txt", changelog );
-				done();
-			} );
+
+				if ( cleanedChangelog !== changelog ) {
+					changelog = cleanedChangelog;
+
+					grunt.option( "changelog", changelog );
+
+					// Write changes to the file.
+					grunt.file.write( "./readme.txt", changelog );
+				}
+			}
+
+			// Check if the current version already exists in the changelog.
+			const containsCurrentVersion = ! _isEmpty(
+				changelogVersions.filter( version => {
+					return (
+						versionNumber.major === version.major &&
+						versionNumber.minor === version.minor &&
+						versionNumber.patch === version.patch
+					);
+				} )
+			);
+
+			if ( containsCurrentVersion ) {
+				// Present the user with the entire changelog file.
+				getUserInput( { initialContent: changelog } ).then( newChangelog => {
+					grunt.option( "changelog", newChangelog );
+
+					// Write changes to the file.
+					grunt.file.write( "./readme.txt", newChangelog );
+					done();
+				} );
+			} else {
+				// Present the user with only the version number.
+				getUserInput( { initialContent: `= ${newVersion} =` } ).then( newChangelog => {
+					grunt.option( "changelog", newChangelog );
+
+					// Add the user input to the changelog.
+					changelog = changelog.replace( /[=]= Changelog ==/ig, "== Changelog ==\n\n" + newChangelog.trim() );
+
+					// Write changes to the file.
+					grunt.file.write( "./readme.txt", changelog );
+					done();
+				} );
+			}
 
 			// Stage the changed readme.txt.
 			grunt.config( "gitadd.addChangelog.files", { src: [ "./readme.txt" ] } );
-			grunt.task.run( "gitadd:addChangelog" );
+			// grunt.task.run( "gitadd:addChangelog" );
 
 			// Commit the changed readme.txt.
 			grunt.config( "gitcommit.commitChangelog.options.message", "Add changelog" );
-			grunt.task.run( "gitcommit:commitChangelog" );
+			// grunt.task.run( "gitcommit:commitChangelog" );
 		}
 	);
 };
