ef20c7e4f2cccb10a3588c13366a7074b5ee528a
Jip <jhp.moors@gmail.com>
2020-01-02 10:56:13 +0100

    Improve docs

--- grunt/config/tools/parse-version.js
+++ grunt/config/tools/parse-version.js
@@ -1,3 +1,17 @@
+/**
+ * @typedef {Object} Version
+ * @property {number} major Major version
+ * @property {number} minor Minor version
+ * @property {number} patch Patch version
+ */
+
+/**
+ * Splits up the version parts from a version string.
+ *
+ * @param {string} versionNumberString The version number as a string.
+ *
+ * @returns {Version} The parsed version number.
+ */
 function parseVersion( versionNumberString ) {
 	const versionNumber = ( /(\d+).(\d+).?(\d+)?/g ).exec( versionNumberString );
 
