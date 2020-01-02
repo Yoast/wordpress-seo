427d1313a03633a5d2ec267918e2e0f3a3841847
Jip <jhp.moors@gmail.com>
2020-01-02 10:58:21 +0100

    Improve variable naming
    For better readability

--- grunt/config/update-readme.js
+++ grunt/config/update-readme.js
@@ -18,28 +18,29 @@ module.exports = function( grunt ) {
 			const newVersion = grunt.option( "plugin-version" );
 			getUserInput( { initialContent: `= ${newVersion} =` } ).then( newChangelog => {
 				grunt.option( "changelog", newChangelog );
-				const versionNumber = parseVersion( newVersion );
+				const newVersionNumber = parseVersion( newVersion );
 
 				// Only if the version is not a patch we remove old changelog entries.
-				if ( versionNumber.patch !== 0 ) {
+				if ( newVersionNumber.patch !== 0 ) {
 					const releaseInChangelog = /[=] \d+\.\d+(\.\d+)? =/g;
 					const allReleasesInChangelog = changelog.match( releaseInChangelog );
-					const sanitizedVersionNumbers = allReleasesInChangelog.map(
-						element => parseVersion( element.slice( 2, element.length - 2 ) )
+					const parsedVersionNumbers = allReleasesInChangelog.map(
+						changelogEntryVersion => parseVersion( changelogEntryVersion.slice( 2, changelogEntryVersion.length - 2 ) )
 					);
-					const highestMajor = Math.max( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.major ) );
-					const lowestMajor = Math.min( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.major ) );
+					const highestMajor = Math.max( ...parsedVersionNumbers.map( versionNumber => versionNumber.major ) );
+					const lowestMajor = Math.min( ...parsedVersionNumbers.map( versionNumber => versionNumber.major ) );
 
-					if ( highestMajor !== lowestMajor ) {
-						// If there are multiple major versions in the current changelog, remove all entries from the oldest major version.
-						changelog = changelog.replace( new RegExp( "= " + lowestMajor + "(.|\\n)*= Earlier versions =" ), "= Earlier versions =" );
-					} else {
+					if ( highestMajor === lowestMajor ) {
 						// If there are only multiple minor versions of the same major version, remove all entries from the oldest minor version.
-						const lowestMinor = Math.min( ...sanitizedVersionNumbers.map( sanitizedVersionNumber => sanitizedVersionNumber.minor ) );
+						const lowestMinor = Math.min( ...parsedVersionNumbers.map( versionNumber => versionNumber.minor ) );
 						const lowestVersion = `${lowestMajor}.${lowestMinor}`;
 						changelog = changelog.replace( new RegExp( "= " + lowestVersion + "(.|\\n)*= Earlier versions =" ), "= Earlier versions =" );
+					} else {
+						// If there are multiple major versions in the current changelog, remove all entries from the oldest major version.
+						changelog = changelog.replace( new RegExp( "= " + lowestMajor + "(.|\\n)*= Earlier versions =" ), "= Earlier versions =" );
 					}
 				}
+
 				// Add the user input to the changelog.
 				changelog = changelog.replace( /[=]= Changelog ==/ig, "== Changelog ==\n\n" + newChangelog.trim() );
 				grunt.file.write( "./readme.txt", changelog );
