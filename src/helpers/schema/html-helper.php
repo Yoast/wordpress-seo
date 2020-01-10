223fea00bef9741cf71571d8c4267db0e7c34fbb
Joost de Valk <joost@yoast.com>
2020-01-10 13:00:45 +0100

    Add tests, improve code as a result of adding tests

--- src/helpers/schema/html-helper.php
+++ src/helpers/schema/html-helper.php
@@ -38,14 +38,17 @@ class HTML_Helper {
 		$html = preg_replace( '/<br(\s*)?\/?>/i', ' ', $html );
 
 		// Replace closing </p> and other tags with the same tag with a space after it, so we don't end up connecting words when we remove them later.
-		$html = preg_replace( '/<\/(p|li|div|h\d)>/i', '</$1> ', $html );
+		$html = preg_replace( '/<\/(p|div|h\d)>/i', '</$1> ', $html );
 
 		// Replace list items with list identifiers so it still looks natural.
-		$html = preg_replace( '/<li([^>]+)>/i', '&#149; ', $html );
+		$html = preg_replace( '/(<li[^>]*>)/i', '$1• ', $html );
+
+		// Strip tags.
+		$html = \wp_strip_all_tags( $html );
 
 		// Replace multiple spaces with one space.
-		$html = preg_replace( '/\s+/', ' ', $html );
+		$html = preg_replace( '!\s+!', ' ', $html );
 
-		return trim( \wp_strip_all_tags( $html ) );
+		return trim( $html );
 	}
 }
