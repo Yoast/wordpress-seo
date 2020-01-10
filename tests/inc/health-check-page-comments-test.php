a216adad53b2092a1bc8f231e2ae5b4c9c04b718
Karlijn Bok <karlijn@yoast.com>
2020-01-10 13:39:37 +0100

    Fix test

--- tests/inc/health-check-page-comments-test.php
+++ tests/inc/health-check-page-comments-test.php
@@ -22,8 +22,12 @@ class WPSEO_Health_Check_Page_Comments_Test extends TestCase {
 			->once()
 			->with( 'page_comments' )
 			->andReturn( '0' );
+
+		Monkey\Functions\expect( 'plugin_dir_url' )->andReturn( '' );
+
 		$health_check = new \WPSEO_Health_Check_Page_Comments();
 		$health_check->run();
+		
 		// We just want to verify that the label attribute is the "passed" message.
 		$this->assertAttributeEquals( 'Paging comments is properly disabled', 'label', $health_check );
 	}
@@ -40,8 +44,11 @@ class WPSEO_Health_Check_Page_Comments_Test extends TestCase {
 			->andReturn( '1' );
 		Monkey\Functions\expect( 'esc_url' )->andReturn( '' );
 		Monkey\Functions\expect( 'admin_url' )->andReturn( '' );
+		Monkey\Functions\expect( 'plugin_dir_url' )->andReturn( '' );
+
 		$health_check = new \WPSEO_Health_Check_Page_Comments();
 		$health_check->run();
+
 		// We just want to verify that the label attribute is the "not passed" message.
 		$this->assertAttributeEquals( 'Paging comments is enabled', 'label', $health_check );
 	}
