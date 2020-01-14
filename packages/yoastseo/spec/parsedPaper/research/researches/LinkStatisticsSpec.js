2a343129253fae3e657bd9bfaca1565edc067e01
Hans-Christiaan <hans-christiaan@yoast.com>
2020-01-14 16:54:40 +0100

    Added unit tests for LinkStatistics research.

--- packages/yoastseo/spec/parsedPaper/research/researches/LinkStatisticsSpec.js
+++ packages/yoastseo/spec/parsedPaper/research/researches/LinkStatisticsSpec.js
@@ -3,32 +3,9 @@ import buildTreeFromYaml from "../../../specHelpers/buildTreeFromYaml";
 
 
 describe( "LinkStatistics research", () => {
-	let paragraph;
 	let metadata;
 
 	beforeEach( () => {
-		const paragraphYaml = `
-Paragraph:
-  formatting:
-    - a:
-        attributes:
-          rel: "noopener nofollow"
-        sourceCodeLocation:
-          startTag:
-          endTag:
-        textStartIndex: 20
-        textEndIndex: 28
-    - a:
-        attributes:
-          href: "https://yoast.com/how-to-yoast-your-post"
-        sourceCodeLocation:
-          startTag:
-          endTag:
-        textStartIndex: 33
-        textEndIndex: 44
-		`;
-		paragraph = buildTreeFromYaml( paragraphYaml );
-
 		const metadataYaml = `
 Structured:
   children:
@@ -43,15 +20,205 @@ Structured:
 		metadata = buildTreeFromYaml( metadataYaml );
 	} );
 
-	it( "can identify whether a link is `follow` or `nofollow`.", done => {
-		const linkStatistics = new LinkStatistics();
+	describe( "follow vs nofollow", () => {
+		it( "identifies links that have no 'rel' attribute as being allowed to follow.", done => {
+			const yaml = `
+Paragraph:
+  formatting:
+    - a:
+        attributes:
+          href: ""
+        sourceCodeLocation:
+          startTag:
+          endTag:
+        textStartIndex: 20
+        textEndIndex: 28
+		`;
 
-		linkStatistics.calculateFor( paragraph, metadata ).then(
-			result => {
-				const noFollowLinks = result.filter( link => link.noFollow );
-				expect( noFollowLinks ).toHaveLength( 1 );
-				done();
-			}
-		);
+			const linkStatistics = new LinkStatistics();
+
+			linkStatistics.calculateFor( buildTreeFromYaml( yaml ), metadata ).then(
+				result => {
+					expect( result[ 0 ].noFollow ).toEqual( false );
+					done();
+				}
+			);
+		} );
+
+		it( "can identify whether a link is `follow` or `nofollow`.", done => {
+			const yaml = `
+Paragraph:
+  formatting:
+    - a:
+        attributes:
+          href: "ftp://yoast.com/some-ftp-endpoint"
+          rel: "noopener nofollow"
+        sourceCodeLocation:
+          startTag:
+          endTag:
+        textStartIndex: 20
+        textEndIndex: 28
+		`;
+
+			const linkStatistics = new LinkStatistics();
+
+			linkStatistics.calculateFor( buildTreeFromYaml( yaml ), metadata ).then(
+				result => {
+					expect( result[ 0 ].noFollow ).toEqual( true );
+					done();
+				}
+			);
+		} );
+	} );
+
+	describe( "internal vs external vs other", () => {
+		it( "correctly identifies whether links are internal.", done => {
+			const yaml = `
+Paragraph:
+  formatting:
+    - a:
+        attributes:
+          href: "https://yoast.com/how-to-write-an-awesome-title"
+          rel: "noopener nofollow"
+        sourceCodeLocation:
+          startTag:
+          endTag:
+        textStartIndex: 20
+        textEndIndex: 28
+		`;
+
+			const linkStatistics = new LinkStatistics();
+
+			linkStatistics.calculateFor( buildTreeFromYaml( yaml ), metadata ).then(
+				result => {
+					expect( result[ 0 ].target ).toEqual( "internal" );
+					done();
+				}
+			);
+		} );
+
+		it( "correctly identifies whether links are external.", done => {
+			const yaml = `
+Paragraph:
+  formatting:
+    - a:
+        attributes:
+          href: "https://example.org/some-post-on-another-website"
+          rel: "noopener nofollow"
+        sourceCodeLocation:
+          startTag:
+          endTag:
+        textStartIndex: 20
+        textEndIndex: 28
+		`;
+
+			const linkStatistics = new LinkStatistics();
+
+			linkStatistics.calculateFor( buildTreeFromYaml( yaml ), metadata ).then(
+				result => {
+					expect( result[ 0 ].target ).toEqual( "external" );
+					done();
+				}
+			);
+		} );
+
+		it( "correctly identifies links using the ftp protocol as being of target 'other'.", done => {
+			const yaml = `
+Paragraph:
+  formatting:
+    - a:
+        attributes:
+          href: "ftp://yoast.com/some-ftp-endpoint"
+          rel: "noopener nofollow"
+        sourceCodeLocation:
+          startTag:
+          endTag:
+        textStartIndex: 20
+        textEndIndex: 28
+		`;
+
+			const linkStatistics = new LinkStatistics();
+
+			linkStatistics.calculateFor( buildTreeFromYaml( yaml ), metadata ).then(
+				result => {
+					expect( result[ 0 ].target ).toEqual( "other" );
+					done();
+				}
+			);
+		} );
+
+		it( "correctly identifies empty links as being of target 'other'.", done => {
+			const yaml = `
+Paragraph:
+  formatting:
+    - a:
+        attributes:
+          href: ""
+          rel: "noopener nofollow"
+        sourceCodeLocation:
+          startTag:
+          endTag:
+        textStartIndex: 20
+        textEndIndex: 28
+		`;
+
+			const linkStatistics = new LinkStatistics();
+
+			linkStatistics.calculateFor( buildTreeFromYaml( yaml ), metadata ).then(
+				result => {
+					expect( result[ 0 ].target ).toEqual( "other" );
+					done();
+				}
+			);
+		} );
+
+		it( "correctly identifies invalid links as being of target 'other'.", done => {
+			const yaml = `
+Paragraph:
+  formatting:
+    - a:
+        attributes:
+          href: "an invalid link"
+          rel: "noopener nofollow"
+        sourceCodeLocation:
+          startTag:
+          endTag:
+        textStartIndex: 20
+        textEndIndex: 28
+		`;
+
+			const linkStatistics = new LinkStatistics();
+
+			linkStatistics.calculateFor( buildTreeFromYaml( yaml ), metadata ).then(
+				result => {
+					expect( result[ 0 ].target ).toEqual( "other" );
+					done();
+				}
+			);
+		} );
+
+		it( "identifies links that have no 'href' attribute as being of target 'other'.", done => {
+			const yaml = `
+Paragraph:
+  formatting:
+    - a:
+        attributes:
+        sourceCodeLocation:
+          startTag:
+          endTag:
+        textStartIndex: 20
+        textEndIndex: 28
+		`;
+
+			const linkStatistics = new LinkStatistics();
+
+			linkStatistics.calculateFor( buildTreeFromYaml( yaml ), metadata ).then(
+				result => {
+					expect( result[ 0 ].target ).toEqual( "other" );
+					done();
+				}
+			);
+		} );
+
 	} );
 } );
