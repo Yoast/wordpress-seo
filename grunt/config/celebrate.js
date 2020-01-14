b44398d75541c2a3b015c8f596059324f677a381
Maarten Leenders <maarten@yoast.com>
2020-01-14 15:12:01 +0100

    Update documentation

--- grunt/config/celebrate.js
+++ grunt/config/celebrate.js
@@ -1,5 +1,5 @@
 /**
- * ...
+ * Celebrates with emoji in the log.
  *
  * @param {Object} grunt The grunt helper object.
  * @returns {void}
@@ -7,7 +7,7 @@
 module.exports = function( grunt ) {
 	grunt.registerTask(
 		"celebrate",
-		"Celebration when all tasks are done",
+		"Celebration with emoji in the log",
 		function() {
 			grunt.log.writeln( "🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉🎉" );
 		}
