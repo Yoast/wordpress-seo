31e09ea1a1bf93be020d96994c3b7b8153f7ecad
Igor <igor@yoast.com>
2020-01-15 09:06:20 +0100

    Move br tag from formatting elements to ignored html elements

--- packages/yoastseo/spec/parsedPaper/build/tree/html/buildTreeSpec.js
+++ packages/yoastseo/spec/parsedPaper/build/tree/html/buildTreeSpec.js
@@ -774,6 +774,45 @@ Structured:
 		expect( tree.toString() ).toEqual( expected.toString() );
 	} );
 
+	it( "ignores any br tags instead of creating an implicit paragraph", () => {
+		const input = "<article><br><h1>The Stranger in the Night</h1></article>";
+
+		const expectedYaml = `
+Structured:
+  tag: root
+  children:
+    - Structured:
+        tag: article
+        sourceCodeLocation:
+          startTag:
+            startOffset: 0
+            endOffset: 9
+          endTag:
+            startOffset: 47
+            endOffset: 57
+          startOffset: 0
+          endOffset: 57
+        children:
+          - Heading:
+              level: 1
+              sourceCodeLocation:
+                startTag:
+                  startOffset: 13
+                  endOffset: 17
+                endTag:
+                  startOffset: 42
+                  endOffset: 47
+                startOffset: 13
+                endOffset: 47
+              text: The Stranger in the Night
+		`;
+
+		const expected = buildTreeFromYaml( expectedYaml );
+		const tree = buildTree( input );
+
+		expect( tree.toString() ).toEqual( expected.toString() );
+	} );
+
 	it.skip( "parses a paragraph within a heading", () => {
 		// We need to decide whether we want to support invalid HTML or let it crash and burn.
 		const input = "<h2>This is a <p>paragraph within a</p> heading.</h2>";
