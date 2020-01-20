052d8471fec79b018e2011a085f58be225f57d0c
Andy Meerwaldt <andy@yoast.com>
2020-01-20 16:40:49 +0100

    Updates the indexables relations for post change.

--- src/integrations/watchers/indexable-post-watcher.php
+++ src/integrations/watchers/indexable-post-watcher.php
@@ -9,7 +9,9 @@ namespace Yoast\WP\SEO\Integrations\Watchers;
 
 use Yoast\WP\SEO\Conditionals\Migrations_Conditional;
 use Yoast\WP\SEO\Builders\Indexable_Builder;
+use Yoast\WP\SEO\Generators\Related_Indexables_Generator;
 use Yoast\WP\SEO\Integrations\Integration_Interface;
+use Yoast\WP\SEO\Models\Indexable;
 use Yoast\WP\SEO\Repositories\Indexable_Hierarchy_Repository;
 use Yoast\WP\SEO\Repositories\Indexable_Repository;
 
@@ -63,6 +65,7 @@ class Indexable_Post_Watcher implements Integration_Interface {
 	public function register_hooks() {
 		\add_action( 'wp_insert_post', [ $this, 'build_indexable' ], \PHP_INT_MAX );
 		\add_action( 'delete_post', [ $this, 'delete_indexable' ] );
+		\add_action( 'transition_post_status', [ $this, 'status_transition' ], \PHP_INT_MAX, 3 );
 
 		\add_action( 'edit_attachment',[ $this, 'build_indexable' ], \PHP_INT_MAX );
 		\add_action( 'add_attachment', [ $this, 'build_indexable' ], \PHP_INT_MAX );
@@ -81,6 +84,10 @@ class Indexable_Post_Watcher implements Integration_Interface {
 
 		if ( ! $indexable ) {
 			return;
+		}
+
+		if ( $indexable->is_public ) {
+			$this->update_relations( get_post( $post_id ) );
 		}
 
 		$this->hierarchy_repository->clear_ancestors( $indexable->id );
@@ -107,6 +114,10 @@ class Indexable_Post_Watcher implements Integration_Interface {
 		$indexable = $this->repository->find_by_id_and_type( $post_id, 'post', false );
 		$indexable = $this->builder->build_for_id_and_type( $post_id, 'post', $indexable );
 
+		if ( $indexable->is_public ) {
+			$this->update_relations( get_post( $post_id ) );
+		}
+
 		$indexable->save();
 	}
 
@@ -127,5 +138,59 @@ class Indexable_Post_Watcher implements Integration_Interface {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Checks if the relations should be updated on status transition.
+	 *
+	 * @param string   $new_status The new status.
+	 * @param string   $old_status The old status.
+	 * @param \WP_Post $post       The post object.
+	 */
+	public function status_transition( $new_status, $old_status, $post ) {
+		/**
+		 * Filter: 'wpseo_public_post_statuses' - List of public post statuses.
+		 *
+		 * @apo array $post_statuses Post status list, defaults to array( 'publish' ).
+		 */
+		$public_statuses = \apply_filters( 'wpseo_public_post_statuses', [ 'publish' ] );
+
+		if ( in_array( $new_status, $public_statuses, true ) || in_array( $old_status, $public_statuses, true ) ) {
+			$this->update_relations( $post );
+		}
+	}
+
+	/**
+	 * Updates the relations on post save or post status change.
+	 *
+	 * @param \WP_Post $post The post that has been updated.
+	 */
+	protected function update_relations( $post ) {
+		/**
+		 * The related indexables.
+		 *
+		 * @var Indexable[] $related_indexables.
+		 */
+		$related_indexables   = [];
+		$related_indexables[] = $this->repository->find_by_id_and_type( $post->post_author, 'user', false );
+		$related_indexables[] = $this->repository->find_for_post_type_archive( $post->post_type, false );
+		$related_indexables[] = $this->repository->find_for_home_page( false );
+
+		$taxonomies = get_post_taxonomies( $post->ID );
+		$taxonomies = array_filter( $taxonomies, 'is_taxonomy_viewable' );
+		foreach ( $taxonomies as $taxonomy ) {
+			$terms = get_the_terms( $post->ID, $taxonomy );
+			foreach( $terms as $term ) {
+				$related_indexables[] = $this->repository->find_by_id_and_type( $term->term_id, 'term', false );
+			}
+		}
+
+		$related_indexables = \array_filter( $related_indexables );
+
+		$updated_at = \gmdate( 'Y-m-d H:i:s' );
+		foreach ( $related_indexables as $indexable ) {
+			$indexable->updated_at = $updated_at;
+			$indexable->save();
+		}
 	}
 }
