7d3d82d71716cf94878d96c3c71fb604bac10f73
Maarten Leenders <maarten@yoast.com>
2020-01-24 12:15:04 +0100

    Add initial structure

--- src/integrations/watchers/indexable-permalink-watcher.php
+++ src/integrations/watchers/indexable-permalink-watcher.php
@@ -10,21 +10,13 @@ namespace Yoast\WP\SEO\Integrations\Watchers;
 
 use Yoast\WP\SEO\Conditionals\Migrations_Conditional;
 use Yoast\WP\SEO\Integrations\Integration_Interface;
-use Yoast\WP\SEO\Repositories\Indexable_Repository;
+use Yoast\WP\SEO\WordPress\Wrapper;
 
 /**
  * Handles updates to the permalink_structure for the Indexables table.
  */
 class Indexable_Permalink_Watcher implements Integration_Interface {
 
-	/**
-	 * Indexable_Permalink_Watcher constructor.
-	 *
-	 * @param Indexable_Repository $repository The repository to use.
-	 */
-	public function __construct( Indexable_Repository $repository ) {
-		$this->repository = $repository;
-	}
 
 	/**
 	 * @inheritdoc
@@ -35,20 +27,44 @@ class Indexable_Permalink_Watcher implements Integration_Interface {
 
 	public function register_hooks() {
 		\add_action( 'update_option_permalink_structure', [ $this, 'reset_permalinks' ], 10, 3 );
-		\add_action( 'update_option_category_base', [ $this, 'reset_permalinks' ], 10, 3 );
-		\add_action( 'update_option_tag_base', [ $this, 'reset_permalinks' ], 10, 3 );
+		\add_action( 'update_option_category_base', [ $this, 'reset_permalinks_term' ], 10, 3 );
+		\add_action( 'update_option_tag_base', [ $this, 'reset_permalinks_term' ], 10, 3 );
+
+		//todo I have yet to find which actions are done for custom categories
+//		\add_action( 'update_option_woocommerce_product_category_slug', [ $this, 'reset_permalinks_term' ], 10, 3 );
 	}
 
 	public function reset_permalinks( $old, $new, $type ) {
-		var_dump( $old  );
-		var_dump( $new );
-		var_dump( $type );
-		if( $type !== "tag_base" ) {
-			return;
+		echo $type;
+		exit;
+		$this->reset_permalink_indexables( 'post', 'post' );
+		$this->reset_permalink_indexables( 'post', 'page' );
+	}
+
+	public function reset_permalinks_term( $old, $new, $type ) {
+		echo $type;
+		exit;
+		$subtype = $type;
+		if ( strstr( $subtype, '_base') ) {
+			$subtype = substr( $type, 0, -5 ); // 'strips _base' from the field.
 		}
 
-		//todo set indexables to null for posts, category base, or tag base?
-//		$this-><indexable->reset_permalinks( $type );
-		wp_die();
+		//todo actually do the request
+//		$this->reset_permalink_indexables( 'term', $subtype );
+
+	}
+
+	private function reset_permalink_indexables( $type, $subtype ) {
+		Wrapper::get_wpdb()->update(
+			Yoast_Model::get_table_name( 'Indexable' ),
+			[
+				'permalink' => null,
+			],
+			[
+				'object_type'    => $type,
+				'object_subtype' => $subtype,
+			]
+
+		);
 	}
 }
