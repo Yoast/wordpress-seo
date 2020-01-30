d7eb661afdf759feaf8a3775c78847dddb620c4c
Andrea Fercia <a.fercia@gmail.com>
2020-01-30 09:35:11 +0100

    Pass the post ID.

--- frontend/schema/class-schema-article.php
+++ frontend/schema/class-schema-article.php
@@ -66,7 +66,7 @@ class WPSEO_Schema_Article implements WPSEO_Graph_Piece {
 			'@id'              => $this->context->canonical . WPSEO_Schema_IDs::ARTICLE_HASH,
 			'isPartOf'         => [ '@id' => $this->context->canonical . WPSEO_Schema_IDs::WEBPAGE_HASH ],
 			'author'           => [ '@id' => WPSEO_Schema_Utils::get_user_schema_id( $post->post_author, $this->context ) ],
-			'headline'         => WPSEO_Schema_Utils::get_post_title_with_fallback(),
+			'headline'         => WPSEO_Schema_Utils::get_post_title_with_fallback( $this->context->id ),
 			'datePublished'    => $this->date->format( $post->post_date_gmt ),
 			'dateModified'     => $this->date->format( $post->post_modified_gmt ),
 			'commentCount'     => $comment_count['approved'],
