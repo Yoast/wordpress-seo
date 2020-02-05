390dd7024ad6efbbed58b0521fe487a3a7a81be0
Alexander Botteram <a.botteram@gmail.com>
2020-02-05 11:06:46 +0100

    Removed redundant function

--- js/src/edit-social.js
+++ js/src/edit-social.js
@@ -3,27 +3,21 @@ import { Collapsible } from "@yoast/components";
 import { Slot } from "@wordpress/components";
 import { registerPlugin } from "@wordpress/plugins";
 
-const props = {
-	siteName: "My awesome site",
-	title: "My awesome title",
-	src: "https://yoast.com/app/uploads/2019/03/Quick_wins_Hero_480.png",
-};
-
 const Social = () => {
 	return createPortal(
 		<Fragment>
 			<Collapsible hasSeparator={ true } title="Facebook">
 				<Slot name="YoastFacebookPreview" />
+				<b>Facebook form</b>
 			</Collapsible>
 			<Collapsible hasSeparator={ true } title="Twitter">
 				<Slot name="YoastTwitterPreview" />
+				<b>Twitter form</b>
 			</Collapsible>
 		</Fragment>,
 		document.getElementById( "wpseo-section-social" ) );
 };
 
 registerPlugin( "yoast-seo-social-previews", {
-	render: () => {
-		return <Social />;
-	},
+	render: Social,
 } );
