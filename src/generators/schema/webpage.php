c03464312b8c8b47c044e25a857766a59b7a8e1d
Hans-Christiaan <hans-christiaan@yoast.com>
2020-02-10 17:08:06 +0100

    WebSite graph piece is not put out when the page is a 404 page.

--- src/generators/schema/webpage.php
+++ src/generators/schema/webpage.php
@@ -60,7 +60,7 @@ class WebPage extends Abstract_Schema_Piece {
 	 * @return bool
 	 */
 	public function is_needed( Meta_Tags_Context $context ) {
-		return $context->indexable->object_type !== 'error-page';
+		return $context->indexable->object_type !== 'system-page' && $context->indexable->object_sub_type !== '404';
 	}
 
 	/**
