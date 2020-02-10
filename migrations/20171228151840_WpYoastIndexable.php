b7312b3cccc8d3e4da1ab0c4b5c57e882f955a88
Hans-Christiaan <hans-christiaan@yoast.com>
2020-02-10 14:58:51 +0100

    Removes permalink index. It got removed in a later migration before.

--- migrations/20171228151840_WpYoastIndexable.php
+++ migrations/20171228151840_WpYoastIndexable.php
@@ -124,17 +124,6 @@ class WpYoastIndexable extends Ruckusing_Migration_Base {
 	 * @param string $indexable_table_name The name of the indexable table.
 	 */
 	private function add_indexes( $indexable_table_name ) {
-		$this->add_index(
-			$indexable_table_name,
-			[
-				'permalink',
-			],
-			[
-				'name'   => 'unique_permalink',
-				'unique' => true,
-			]
-		);
-
 		$this->add_index( $indexable_table_name, 'permalink_hash' );
 
 		$this->add_index(
