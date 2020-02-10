3a0420aaa6aff68413c3b724d80a5af3970e1004
Hans-Christiaan <hans-christiaan@yoast.com>
2020-02-10 14:37:24 +0100

    Fixed spacing issues within function calls in indexable table migration.

--- migrations/20171228151840_WpYoastIndexable.php
+++ migrations/20171228151840_WpYoastIndexable.php
@@ -45,8 +45,8 @@ class WpYoastIndexable extends Ruckusing_Migration_Base {
 
 		// Object type and ID.
 		$indexable_table->column( 'object_id', 'integer', [ 'unsigned' => true, 'null' => true, 'limit' => 11 ] );
-		$indexable_table->column( 'object_type', 'string', [ 'null' => false, 'limit' => 191 ]);
-		$indexable_table->column( 'object_sub_type', 'string', [ 'null' => true, 'limit' => 191 ]  );
+		$indexable_table->column( 'object_type', 'string', [ 'null' => false, 'limit' => 191 ] );
+		$indexable_table->column( 'object_sub_type', 'string', [ 'null' => true, 'limit' => 191 ] );
 
 		// Title and description.
 		$indexable_table->column( 'title', 'string', [ 'null' => true, 'limit' => 191 ] );
