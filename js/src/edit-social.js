5ca4754d0b8c8c4bfb8eb6666944b247d2d6c321
Alexander Botteram <a.botteram@gmail.com>
2020-02-11 13:54:07 +0100

    Added SocialMetadataPreviewForm components

--- js/src/edit-social.js
+++ js/src/edit-social.js
@@ -2,17 +2,36 @@ import { Fragment, createPortal } from "@wordpress/element";
 import { Collapsible } from "@yoast/components";
 import { Slot } from "@wordpress/components";
 import { registerPlugin } from "@wordpress/plugins";
+import { SocialMetadataPreviewForm } from "@yoast/social-metadata-previews";
 
 const Social = () => {
 	return createPortal(
 		<Fragment>
-			<Collapsible hasSeparator={ true } title="Facebook">
+			<Collapsible hasPadding={ true } hasSeparator={ true } title="Facebook">
 				<Slot name="YoastFacebookPreview" />
-				<b>Facebook form</b>
+				<SocialMetadataPreviewForm
+					socialMediumName="Facebook"
+					replacementVariables={[]}
+					recommendedReplacementVariables={[]}
+					description="Facebook"
+					title="The facebook title"
+					onTitleChange={ () => {} }
+					onDescriptionChange={ () => {} }
+					selectFileClick={ () => {} }
+				/>
 			</Collapsible>
-			<Collapsible hasSeparator={ true } title="Twitter">
+			<Collapsible hasPadding={ true } hasSeparator={ true } title="Twitter">
 				<Slot name="YoastTwitterPreview" />
-				<b>Twitter form</b>
+				<SocialMetadataPreviewForm
+					socialMediumName="Twitter"
+					replacementVariables={[]}
+					recommendedReplacementVariables={[]}
+					description="Twitter"
+					title="The Twitter title"
+					onTitleChange={ () => {} }
+					onDescriptionChange={ () => {} }
+					selectFileClick={ () => {} }
+				/>
 			</Collapsible>
 		</Fragment>,
 		document.getElementById( "wpseo-section-social" ) );
