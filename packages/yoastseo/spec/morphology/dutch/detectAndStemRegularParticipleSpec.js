600b16febdbb8176c3be731c71c6bb0e8d9dd3d2
Agnieszka Szuba <agnieszka@yoast.com>
2020-02-11 15:45:13 +0100

    Add a check for whether to stem -t/-d suffix
    Add functionality that checks whether the final -t/-d at the end of a detected participle should be stemmed or not, and stems the participle accordingly.

--- packages/yoastseo/spec/morphology/dutch/detectAndStemRegularParticipleSpec.js
+++ packages/yoastseo/spec/morphology/dutch/detectAndStemRegularParticipleSpec.js
@@ -6,37 +6,61 @@ const morphologyDataNL = getMorphologyData( "nl" ).nl;
 
 describe( "Detects and stems participles", () => {
 	it( "does not stem a word if it is on an exception list of non-participles", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "geduld" ) ).toEqual( "" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "geduld" ) ).toEqual( "" );
 	} );
 	it( "does not stem a word if it ends with one of the non-participle endings", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "geaardheid" ) ).toEqual( "" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "geaardheid" ) ).toEqual( "" );
 	} );
 	it( "does not stem a word that does not match any of the participle stemming regexes", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "afgeladen" ) ).toEqual( null );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "afgeladen" ) ).toEqual( null );
 	} );
 	it( "correctly stems a regular participle without prefixes", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "geaaid" ) ).toEqual( "aai" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "geaaid" ) ).toEqual( "aai" );
 	} );
 	it( "correctly stems a regular participle with a separable prefix", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "aangebeld" ) ).toEqual( "aanbel" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "aangebeld" ) ).toEqual( "aanbel" );
 	} );
 	it( "correctly stems a regular participle with an inseparable prefix", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "herhaald" ) ).toEqual( "herhaal" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "herhaald" ) ).toEqual( "herhaal" );
 	} );
 	it( "correctly stems a regular participle with an inseparable prefix that is usually separable", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "omwikkeld" ) ).toEqual( "omwikkel" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "omwikkeld" ) ).toEqual( "omwikkel" );
 	} );
 	it( "returns the participle if it is the same as the stem", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "misleid" ) ).toEqual( "misleid" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "misleid" ) ).toEqual( "misleid" );
 	} );
 	it( "if the stem of the participle begins in ge-, the ge- does not get stemmed", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "gebruikt" ) ).toEqual( "gebruik" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "gebruikt" ) ).toEqual( "gebruik" );
 	} );
 	it( "if the ge- following a separable prefix is part of the stem, it should not be stemmed", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "opgebruikt" ) ).toEqual( "opgebruik" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "opgebruikt" ) ).toEqual( "opgebruik" );
 	} );
 	it( "correctly stems a participle with a separable prefix beginning in her- (herop- in this case), even though the her- " +
 		"prefix on its own is inseparable", () => {
-		expect( detectAndStemRegularParticiple( morphologyDataNL.verbs, "heropgebouwd" ) ).toEqual( "heropbouw" );
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "heropgebouwd" ) ).toEqual( "heropbouw" );
+	} );
+	it( "correctly stems a participle that is found on the list of verbs for which -t should be stemmed (exception to a rule)", () => {
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "gestretcht" ) ).toEqual( "stretch" );
+	} );
+	it( "correctly stems a participle that matches the regex for when -t should not be stemmed.", () => {
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "gegroet" ) ).toEqual( "groet" );
+	} );
+	it( "correctly stems a participle that is found on the list of verbs for which -t should not be stemmed.", () => {
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "getest" ) ).toEqual( "test" );
+	} );
+	it( "correctly stems a compound participle that is matches the regex for when -t should not be stemmed.", () => {
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "verzoet" ) ).toEqual( "verzoet" );
+	} );
+	it( "correctly stems a compound participle that is found on the list of verbs for which -t should not be stemmed.", () => {
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "aangevat" ) ).toEqual( "aanvat" );
+	} );
+	it( "correctly stems a participle that is found on the list of verbs for which -d should not be stemmed.", () => {
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "geleid" ) ).toEqual( "leid" );
+	} );
+	it( "correctly stems a participle of a compound verb that is found on the list of verbs for which -d should not be stemmed.", () => {
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "aangemeld" ) ).toEqual( "aanmeld" );
+	} );
+	it( "correctly stems a participle of a compound verb that is found on the list of verbs for which -t should not be stemmed.", () => {
+		expect( detectAndStemRegularParticiple( morphologyDataNL, "afgemest" ) ).toEqual( "afmest" );
 	} );
 } );
--- packages/yoastseo/src/morphology/dutch/stem.js
+++ packages/yoastseo/src/morphology/dutch/stem.js
@@ -2,6 +2,7 @@ import { checkIfWordEndingIsOnExceptionList } from "../morphoHelpers/exceptionLi
 import { removeSuffixFromFullForm } from "../morphoHelpers/stemHelpers";
 import { isVowelDoublingAllowed } from "./stemModificationHelpers";
 import { generateCorrectStemWithTAndDEnding } from "./getStemWordsWithTAndDEnding.js";
+import { flatten } from "lodash-es";
 
 /**
  * @file Dutch stemming algorithm. Adapted from:
@@ -193,7 +194,8 @@ const removeSuffixFromFullForms = function( exceptionsRemoveSuffixFromFullForms,
  */
 export default function stem( word, morphologyDataNL ) {
 	// Check whether the word is on an exception list of words that shouldn't be stemmed. If it is, return the word.
-	if ( checkIfWordEndingIsOnExceptionList( word, morphologyDataNL.stemming.stemExceptions.wordsNotToBeStemmedExceptions ) ) {
+	const wordsThatShouldNotBeStemmed = flatten( Object.values( morphologyDataNL.stemming.stemExceptions.wordsNotToBeStemmedExceptions ) );
+	if ( checkIfWordEndingIsOnExceptionList( word, wordsThatShouldNotBeStemmed ) ) {
 		return word;
 	}
 
