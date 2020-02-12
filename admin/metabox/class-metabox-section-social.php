6796f098770bd2684ca9de18f1f725f1bc19772c
Alexander Botteram <a.botteram@gmail.com>
2020-02-12 14:17:09 +0100

    Fixed PHPCS errors

--- admin/metabox/class-metabox-section-social.php
+++ admin/metabox/class-metabox-section-social.php
@@ -1,7 +1,18 @@
 <?php
+/**
+ * WPSEO plugin file.
+ *
+ * @package WPSEO\Admin
+ */
 
+/**
+ * Generates and displays the React root element for a metabox social section.
+ */
 class WPSEO_Metabox_Section_Social extends WPSEO_Metabox_Section_Additional {
 
+	/**
+	 * Constructor.
+	 */
 	public function __construct() {
 		$name = 'wpseo-section-social';
 		$link_content = '<span class="dashicons dashicons-share"></span>' . __( 'Social React', 'wordpress-seo' );
