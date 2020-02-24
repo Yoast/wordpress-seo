b647a3ce149d8057546f26591b24875fead69fb0
Guido Offermans <guidooffermans@hotmail.com>
2020-02-24 16:44:55 +0100

    changed paths in index.js

--- packages/social-metadata-forms/src/index.js
+++ packages/social-metadata-forms/src/index.js
@@ -1,6 +1,6 @@
-import * as actions from "./redux/actions/formActions";
-import * as reducers from "./redux/reducers/index";
-import * as selectors from "./redux/selectors";
+import * as actions from "../src/redux/actions/formActions";
+import * as reducers from "../src/redux/reducers/index";
+import * as selectors  from "../src/redux/selectors/formSelectors";
 
 export { default as SocialMetadataPreviewForm } from "./SocialMetadataPreviewForm";
 export  { actions, reducers, selectors };
