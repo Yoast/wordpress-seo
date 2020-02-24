b8bdb04c31e3e6f7f69fdaa9c8b69147e602609c
Guido Offermans <guidooffermans@hotmail.com>
2020-02-24 15:22:52 +0100

    created new package, moved form, imageselect, redux and tests

---
+++ packages/social-metadata-forms/src/index.js
@@ -0,0 +1,1 @@
+export { default as SocialMetadataPreviewForm } from "./SocialMetadataPreviewForm";
--- packages/social-metadata-previews/src/index.js
+++ packages/social-metadata-previews/src/index.js
@@ -1,3 +1,2 @@
 export { default as FacebookPreview } from "./facebook/FacebookPreview";
 export { default as TwitterPreview } from "./twitter/TwitterPreview";
-export { default as SocialMetadataPreviewForm } from "./shared/SocialMetadataPreviewForm";
