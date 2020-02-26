bfff7fcba96b01867cb87fe1e6809f8c5e42e834
guido offermans <guidooffermans@hotmail.com>
2020-02-26 16:23:49 +0100

    add fallback selectors for imageUrl, title and description

--- js/src/redux/selectors/index.js
+++ js/src/redux/selectors/index.js
@@ -1,4 +1,4 @@
-import { socialSelectors } from "./socialSelectors";
+import { socialSelectors, fallbackSelectors } from "./socialSelectors";
 import * as results from "./results";
 import * as primaryTaxonomies from "./primaryTaxonomies";
 import * as activeMarker from "./activeMarker";
@@ -6,6 +6,7 @@ import * as markerPauseStatus from "./markerPauseStatus";
 
 const selectors = {
 	...socialSelectors,
+	...fallbackSelectors,
 	...results,
 	...primaryTaxonomies,
 	...activeMarker,
--- js/src/redux/selectors/socialSelectors.js
+++ js/src/redux/selectors/socialSelectors.js
@@ -2,4 +2,14 @@ import { selectorsFactory } from "@yoast/social-metadata-forms";
 
 const socialSelectors = selectorsFactory( "socialReducer" );
 
-export { socialSelectors };
+const fallbackSelectors = {
+
+	getTitleFallback: state => state.analysisData.snippet.title,
+
+	getDescriptionFallback: state => state.analysisData.snippet.description,
+
+	getImageUrlFallback: state => state.settings.socialpreviews.sideWideImage,
+
+};
+
+export { socialSelectors, fallbackSelectors };
