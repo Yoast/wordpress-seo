f4e0d464eb8c5d36baef108f217eab6b4d6efeb5
guido offermans <guidooffermans@hotmail.com>
2020-02-26 13:35:30 +0100

    changed imports/exports

--- packages/social-metadata-forms/src/index.js
+++ packages/social-metadata-forms/src/index.js
@@ -1,6 +1,7 @@
 import * as actions from "../src/redux/actions/formActions";
-export { socialReducer } from "../src/redux/reducers/index";
-import * as selectors  from "../src/redux/selectors/formSelectors";
+import selectorsFactory from "../src/redux/selectors/formSelectors";
 
+export { socialReducer } from "../src/redux/reducers/index";
 export { default as SocialMetadataPreviewForm } from "./SocialMetadataPreviewForm";
-export  { actions, selectors };
+export  { actions };
+export { selectorsFactory };
