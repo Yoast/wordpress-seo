fcfab9e4b3027a65f6ac1238c09920e0393d774d
guido offermans <guidooffermans@hotmail.com>
2020-02-26 16:58:50 +0100

    start on tests

---
+++ js/tests/redux/selectors/socialSelectors.test.js
@@ -0,0 +1,15 @@
+import { fallbackSelectors } from "../../../src/redux/selectors/socialSelectors";
+
+const testState = {
+	
+};
+
+describe( "fallbackSelectors", () => {
+	it( "", () => {
+		const actual = "";
+		const expected = "";
+
+		expect( actual ).toEqual( expected );
+	} );
+} );
+
