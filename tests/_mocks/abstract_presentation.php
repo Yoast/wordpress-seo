7b072b4d719e0703ad51eab9f56affc10be97fb0
Hans-Christiaan <hans-christiaan@yoast.com>
2020-02-28 09:40:09 +0100

    Added docblock for generate_some_mock_data method in abstract presentation mock.

--- tests/_mocks/abstract_presentation.php
+++ tests/_mocks/abstract_presentation.php
@@ -9,6 +9,15 @@ class Abstract_Presentation extends \Yoast\WP\SEO\Presentations\Abstract_Present
 		return parent::is_prototype();
 	}
 
+	/**
+	 * Generator for 'some_mock_data'.
+	 * This is to test the generator functionality of the abstract presentation.
+	 *
+	 * E.g. that, when the `some_mock_data` property is used, the
+	 * `generate_some_mock_data` method is called under the hood.
+	 *
+	 * @return string Some mock value.
+	 */
 	public function generate_some_mock_data() {
 		return 'some_mock_value';
 	}
