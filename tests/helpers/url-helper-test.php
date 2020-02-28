ade60deefbf03cfb9cd146a339a58cdd4d7da2ad
Andy Meerwaldt <andy@yoast.com>
2020-02-28 13:54:43 +0100

    Adds test for the url helper

--- tests/helpers/url-helper-test.php
+++ tests/helpers/url-helper-test.php
@@ -3,6 +3,7 @@
 namespace Yoast\WP\SEO\Tests\Helpers;
 
 use Brain\Monkey;
+use Mockery;
 use Yoast\WP\SEO\Helpers\Url_Helper;
 use Yoast\WP\SEO\Tests\TestCase;
 
@@ -16,7 +17,9 @@ use Yoast\WP\SEO\Tests\TestCase;
 class Url_Helper_Test extends TestCase {
 
 	/**
-	 * @var Url_Helper
+	 * The instance to test.
+	 *
+	 * @var Url_Helper|Mockery::mock
 	 */
 	private $instance;
 
@@ -26,10 +29,12 @@ class Url_Helper_Test extends TestCase {
 	public function setUp() {
 		parent::setUp();
 
-		$this->instance = new Url_Helper();
+		$this->instance = Mockery::mock( Url_Helper::class )->makePartial();
 	}
 
 	/**
+	 * Tests the building of an absolute url.
+	 *
 	 * @covers ::build_absolute_url
 	 */
 	public function test_build_absolute_url() {
@@ -54,6 +59,8 @@ class Url_Helper_Test extends TestCase {
 	}
 
 	/**
+	 * Tests the building of an absolute url with a relative url given.
+	 *
 	 * @covers ::build_absolute_url
 	 */
 	public function test_build_absolute_url_relative_url() {
@@ -78,6 +85,8 @@ class Url_Helper_Test extends TestCase {
 	}
 
 	/**
+	 * Tests retrieving of the file extension.
+	 *
 	 * @covers ::get_extension_from_url
 	 * @covers ::get_url_path
 	 */
@@ -93,6 +102,8 @@ class Url_Helper_Test extends TestCase {
 	}
 
 	/**
+	 * Tests retrieving of the file extension with no path present.
+	 *
 	 * @covers ::get_extension_from_url
 	 * @covers ::get_url_path
 	 */
@@ -108,6 +119,8 @@ class Url_Helper_Test extends TestCase {
 	}
 
 	/**
+	 * Tests retrieving of the file extension with no extension present.
+	 *
 	 * @covers ::get_extension_from_url
 	 * @covers ::get_url_path
 	 */
@@ -120,5 +133,59 @@ class Url_Helper_Test extends TestCase {
 		$actual   = $this->instance->get_extension_from_url( 'https://example.com/path' );
 
 		$this->assertEquals( $expected, $actual );
+	}
+
+	/**
+	 * Tests the ensure absolute url with an array given as argument.
+	 *
+	 * @covers ::ensure_absolute_url
+	 */
+	public function test_ensure_absolute_url_with_array_given() {
+		$this->assertEquals( [ 'array' ], $this->instance->ensure_absolute_url( [ 'array' ] ) );
+	}
+
+	/**
+	 * Tests the ensure absolute url with an array given as argument.
+	 *
+	 * @covers ::ensure_absolute_url
+	 */
+	public function test_ensure_absolute_url_with_empty_string_given() {
+		$this->assertEquals( '', $this->instance->ensure_absolute_url( '' ) );
+	}
+
+	/**
+	 * Tests the ensure absolute url with a relative url given as argument.
+	 *
+	 * @covers ::ensure_absolute_url
+	 */
+	public function test_ensure_absolute_url_with_relative_url_given() {
+		$this->instance
+			->expects( 'is_relative' )
+			->once()
+			->with( 'page' )
+			->andReturnTrue();
+
+		$this->instance
+			->expects( 'build_absolute_url' )
+			->once()
+			->with( 'page' )
+			->andReturn( 'https://example.org/page' );
+
+		$this->assertEquals( 'https://example.org/page', $this->instance->ensure_absolute_url( 'page' ) );
+	}
+
+	/**
+	 * Tests the ensure absolute url with an absolute url given as argument.
+	 *
+	 * @covers ::ensure_absolute_url
+	 */
+	public function test_ensure_absolute_url_with_absolute_url_given() {
+		$this->instance
+			->expects( 'is_relative' )
+			->once()
+			->with( 'https://example.org/page' )
+			->andReturnFalse();
+
+		$this->assertEquals( 'https://example.org/page', $this->instance->ensure_absolute_url( 'https://example.org/page' ) );
 	}
 }
