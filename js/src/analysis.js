ee8ffedc3f23429328fc46c1bb3025b41b9044a2
omarreiss <omar.reiss@gmail.com>
2020-03-02 19:45:24 +0100

    Export JS modules Premium add-on needs on `plugin` namespace.

--- js/src/analysis.js
+++ js/src/analysis.js
@@ -1,2 +1,16 @@
 window.yoast = window.yoast || {};
-window.yoast.analysis = require( "yoastseo/index.js" );
+import * as analysis from "yoastseo";
+import getL10nObject from "./analysis/getL10nObject";
+import getContentLocale from "./analysis/getContentLocale";
+import { refreshDelay } from "./analysis/constants";
+import { sortResultsByIdentifier } from "./analysis/refreshAnalysis";
+import getIndicatorForScore from "./analysis/getIndicatorForScore";
+
+window.yoast.analysis = analysis;
+window.yoast.plugin = window.yoast.plugin || {};
+window.yoast.plugin.analysis = {};
+window.yoast.plugin.analysis.getL10nObject = getL10nObject;
+window.yoast.plugin.analysis.getContentLocale = getContentLocale;
+window.yoast.plugin.analysis.refreshDelay = refreshDelay;
+window.yoast.plugin.analysis.sortResultsByIdentifier = sortResultsByIdentifier;
+window.yoast.plugin.analysis.getIndicatorForScore = getIndicatorForScore;
--- js/src/components.js
+++ js/src/components.js
@@ -1,9 +1,45 @@
+import * as styleGuide from "@yoast/style-guide";
+import * as helpers from "@yoast/helpers";
+import * as componentsNew from "@yoast/components";
+import * as configurationWizard from "@yoast/configuration-wizard";
+import * as searchMetadataPreviews from "@yoast/search-metadata-previews";
+import * as analysisReport from "@yoast/analysis-report";
+import * as components from "yoast-components";
+import Collapsible from "./components/SidebarCollapsible";
+import SidebarItem from "./components/SidebarItem";
+import withSidebarPriority from "./components/higherorder/withYoastSidebarPriority";
+import HelpLink from "./components/contentAnalysis/HelpLink";
+import TopLevelProviders from "./components/TopLevelProviders";
+import { getIconForScore } from "./components/contentAnalysis/mapResults";
+import Results from "./components/contentAnalysis/Results";
+import { LocationConsumer } from "./components/contexts/location";
+import SidebarCollapsible from "./components/SidebarCollapsible";
+import isGutenbergDataAvailable from "./helpers/isGutenbergDataAvailable";
+import { setTextdomainL10n, setYoastComponentsL10n } from "./helpers/i18n";
+
 // Don't change this order because some packages depend on each other.
 window.yoast = window.yoast || {};
-window.yoast.styleGuide = require( "@yoast/style-guide" );
-window.yoast.helpers = require( "@yoast/helpers" );
-window.yoast.componentsNew = require( "@yoast/components" );
-window.yoast.configurationWizard = require( "@yoast/configuration-wizard" );
-window.yoast.searchMetadataPreviews = require( "@yoast/search-metadata-previews" );
-window.yoast.analysisReport = require( "@yoast/analysis-report" );
-window.yoast.components = require( "yoast-components/index.js" );
+window.yoast.styleGuide = styleGuide;
+window.yoast.helpers = helpers;
+window.yoast.componentsNew = componentsNew;
+window.yoast.configurationWizard = configurationWizard;
+window.yoast.searchMetadataPreviews = searchMetadataPreviews;
+window.yoast.analysisReport = analysisReport;
+window.yoast.components = components;
+
+window.yoast.plugin = window.yoast.plugin || {};
+window.yoast.plugin.components = {};
+window.yoast.plugin.components.Collapsible = Collapsible;
+window.yoast.plugin.components.SidebarItem = SidebarItem;
+window.yoast.plugin.components.withSidebarPriority = withSidebarPriority;
+window.yoast.plugin.components.HelpLink = HelpLink;
+window.yoast.plugin.components.TopLevelProviders = TopLevelProviders;
+window.yoast.plugin.components.getIconForScore = getIconForScore;
+window.yoast.plugin.components.Results = Results;
+window.yoast.plugin.components.LocationConsumer = LocationConsumer;
+window.yoast.plugin.components.SidebarCollapsible = SidebarCollapsible;
+
+window.yoast.plugin.helpers = {};
+window.yoast.plugin.helpers.isGutenbergDataAvailable = isGutenbergDataAvailable;
+window.yoast.plugin.helpers.setTextdomainL10n = setTextdomainL10n;
+window.yoast.plugin.helpers.setYoastComponentsL10n = setYoastComponentsL10n;
