275a9415938a2894d23426213cf80fc9f40a32bd
Aida Marfuaty <48715883+FAMarfuaty@users.noreply.github.com>
2020-03-04 11:02:39 +0100

    Update packages/yoastseo/spec/morphology/dutch/getStemWordsWithTAndDEndingSpecs.js
    Co-Authored-By: nataliashitova <36954173+nataliashitova@users.noreply.github.com>

--- packages/yoastseo/spec/morphology/dutch/getStemWordsWithTAndDEndingSpecs.js
+++ packages/yoastseo/spec/morphology/dutch/getStemWordsWithTAndDEndingSpecs.js
@@ -48,7 +48,7 @@ describe( "Creates stem from words with ambiguous endings", () => {
 	it( "Returns undefined if a word is not matched in any of the checks", () => {
 		expect( generateCorrectStemWithTAndDEnding( morphologyDataNL.stemming, "poolt" ) ).toEqual( null );
 	} );
-	it( "Creates the stem of a word which is in the list of words ending in -den that should only have -en stemmed.", () => {
+	it( "Creates the stem of a word which is in the list of words ending in -end.", () => {
 		expect( generateCorrectStemWithTAndDEnding( morphologyDataNL.stemming, "sportend" ) ).toEqual(
 			"sport" );
 	} );
