7ec977b61d945cfb56e3d419ccb2e3bb1ee2cde8
Aida Marfuaty <48715883+FAMarfuaty@users.noreply.github.com>
2020-03-04 11:05:18 +0100

    Update packages/yoastseo/spec/morphology/dutch/stemTOrDFromEndOfWordSpec.js
    Co-Authored-By: nataliashitova <36954173+nataliashitova@users.noreply.github.com>

--- packages/yoastseo/spec/morphology/dutch/stemTOrDFromEndOfWordSpec.js
+++ packages/yoastseo/spec/morphology/dutch/stemTOrDFromEndOfWordSpec.js
@@ -3,7 +3,7 @@ import getMorphologyData from "../../specHelpers/getMorphologyData";
 
 const morphologyDataNL = getMorphologyData( "nl" ).nl;
 
-describe( "Creates forms form stem from words with ambiguous endings", () => {
+describe( "Creates stems of words with ambiguous endings -d/-t", () => {
 	it( "Removes the -t from the end of a stem which was not matched in a previous check", () => {
 		expect( stemTOrDFromEndOfWord( morphologyDataNL, "dreunt", "dreunt" ) ).toEqual( "dreun" );
 	} );
