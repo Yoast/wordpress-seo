223395208b63a00281785ca7c326a34d1fb76384
nataliashitova <36954173+nataliashitova@users.noreply.github.com>
2020-03-04 14:45:43 +0100

    Update test comments

--- packages/yoastseo/spec/morphology/morphoHelpers/exceptionListHelpersSpec.js
+++ packages/yoastseo/spec/morphology/morphoHelpers/exceptionListHelpersSpec.js
@@ -12,13 +12,13 @@ describe( "Returns true if a word has the same ending as one of the entries in a
 	it( "Returns false if a word does not end with an entry from the array", () => {
 		expect( checkIfWordEndingIsOnExceptionList( "huisarts", [ "huis", "keuken" ] ) ).toEqual( false );
 	} );
-	it( "Returns true if a word is in the array list with two stems", () => {
+	it( "Returns the canonical stem if a word is in the array list with two stems", () => {
 		expect( checkExceptionListWithTwoStems( [ [ "glas", "glaas" ], [ "vat", "vaat" ] ], "glaas" ) ).toEqual( "glas" );
 	} );
-	it( "Returns true if a word ends in an entry of the array list with two stems", () => {
+	it( "Returns the canonical stem for compound words (if a word ends in an entry of the array list with two stems)", () => {
 		expect( checkExceptionListWithTwoStems( [ [ "glas", "glaas" ], [ "vat", "vaat" ] ], "bierglaas" ) ).toEqual( "bierglas" );
 	} );
-	it( "Returns false if a word is not in the array list with two stems", () => {
+	it( "Returns undefined if a word is not in the array list with two stems", () => {
 		expect( checkExceptionListWithTwoStems( [ [ "glas", "glaas" ], [ "vat", "vaat" ] ], "boek" ) ).toBeUndefined();
 	} );
 } );
