5e75769b6afaa62461324510e2d4c56383d190bb
Hans-Christiaan <hans-christiaan@yoast.com>
2020-03-10 11:35:46 +0100

    Refactored HowTo tests to make it more readable.

--- tests/generators/schema/howto-test.php
+++ tests/generators/schema/howto-test.php
@@ -57,17 +57,111 @@ class HowToTest extends TestCase {
 	private $language;
 
 	/**
+	 * The base Gutenberg blocks.
+	 *
+	 * @var array
+	 */
+	private $base_blocks = [
+		'yoast/how-to-block' => [
+			[
+				'blockName' => 'yoast/how-to-block',
+				'attrs'     => [
+					'jsonDescription' => 'An How To block description <em>with markup</em>.',
+					'hasDuration'     => true,
+					'days'            => '50',
+					'hours'           => '23',
+					'minutes'         => '27',
+					'steps'           => [
+						[
+							'id'       => 'how-to-step-1583764039837',
+							'name'     => [
+								[
+									'type'  => 'strong',
+									'props' => [
+										'children' => [
+											'Step 1 title',
+										],
+									],
+								],
+							],
+							'text'     => [
+								'Step 1 description',
+							],
+							'jsonName' => '<strong>Step 1 title</strong>',
+							'jsonText' => 'Step 1 description',
+						],
+					],
+				],
+			],
+		],
+	];
+
+	/**
+	 * The base expected schema.
+	 *
+	 * @var array
+	 */
+	private $base_expected_schema = [
+		[
+			'@type'            => 'HowTo',
+			'@id'              => '#howto-1',
+			'name'             => 'post title',
+			'mainEntityOfPage' => [
+				'@id' => 'https://example.com/post-1#main-schema-id',
+			],
+			'description'      => 'An How To block description <em>with markup</em>.',
+			'totalTime'        => 'P50DT23H27M',
+			'step'             => [
+				[
+					'@type'           => 'HowToStep',
+					'url'             => '#how-to-step-1583764039837',
+					'name'            => '<strong>Step 1 title</strong>',
+					'itemListElement' => [
+						[
+							'@type' => 'HowToDirection',
+							'text'  => 'Step 1 description',
+						],
+					],
+				],
+			],
+			'inLanguage'       => 'language',
+		],
+	];
+
+	/**
 	 * @inheritDoc
 	 */
 	public function setUp() {
 		parent::setUp();
 
+		$id = 1234;
+
 		$this->meta_tags_context = \Mockery::mock( Meta_Tags_Context::class );
+
+		$this->meta_tags_context->id             = $id;
+		$this->meta_tags_context->main_schema_id = 'https://example.com/post-1#main-schema-id';
 
 		$this->html     = \Mockery::mock( HTML_Helper::class );
 		$this->image    = \Mockery::mock( Image_Helper::class );
 		$this->post     = \Mockery::mock( Post_Helper::class );
 		$this->language = \Mockery::mock( Language_Helper::class );
+
+		$this->html
+			->shouldReceive( 'smart_strip_tags' )
+			->andReturnArg( 0 );
+
+		$this->html
+			->shouldReceive( 'sanitize' )
+			->andReturnArg( 0 );
+
+		$this->language
+			->shouldReceive( 'add_piece_language' )
+			->andReturnUsing( [ $this, 'set_language' ] );
+
+		$this->post
+			->shouldReceive( 'get_post_title_with_fallback' )
+			->with( $id )
+			->andReturn( 'post title' );
 
 		$this->instance = new HowTo( $this->html, $this->image, $this->post, $this->language );
 	}
@@ -90,104 +184,48 @@ class HowToTest extends TestCase {
 	}
 
 	/**
-	 * Tests the 'happy path' of the How-to Schema generation.
+	 * Tests the generation of HowTo Schema pieces.
 	 *
-	 * @covers ::generate
-	 * @covers ::add_steps
-	 * @covers ::add_duration
-	 * @covers ::add_step_description
-	 * @covers ::add_step_image
+	 * @param array $blocks          The blocks in the post's content.
+	 * @param array $expected_schema The expected schema.
+	 *
+	 * @dataProvider generate_test_provider
 	 */
-	public function test_generate() {
-		$id             = 1234;
-		$main_schema_id = 'https://example.com/post-1#main-schema-id';
-		$post_title     = 'post title';
+	public function test_generate( $blocks, $expected_schema ) {
+		$this->meta_tags_context->blocks = $blocks;
 
-		$this->meta_tags_context->blocks = [
-			'yoast/how-to-block' => [
-				[
-					'blockName' => 'yoast/how-to-block',
-					'attrs'     => [
-						'jsonDescription' => 'An How To block description <em>with markup</em>.',
-						'hasDuration'     => true,
-						'days'            => '50',
-						'hours'           => '23',
-						'minutes'         => '27',
-						'steps'           => [
-							[
-								'id'       => 'how-to-step-1583764039837',
-								'name'     => [
-									[
-										'type'  => 'strong',
-										'props' => [
-											'children' => [
-												'Step 1 title',
-											],
-										],
-									],
-								],
-								'text'     => [
-									'Step 1 description',
-								],
-								'jsonName' => '<strong>Step 1 title</strong>',
-								'jsonText' => 'Step 1 description',
-							],
-						],
-					],
-				],
-			],
+		$actual_schema = $this->instance->generate( $this->meta_tags_context );
+
+		$this->assertEquals( $expected_schema, $actual_schema );
+	}
+
+	/**
+	 * Provides the test_generate method with test data.
+	 *
+	 * @return array The provided test data.
+	 */
+	public function generate_test_provider() {
+		$no_name_blocks = $this->base_blocks;
+		// Remove the name.
+		$no_name_blocks['yoast/how-to-block'][0]['attrs']['steps'][0]['jsonName'] = '';
+
+		$no_name_expected_schema               = $this->base_expected_schema;
+		$no_name_expected_schema[0]['step'][0] = [
+			'@type' => 'HowToStep',
+			'url'   => '#how-to-step-1583764039837',
+			'text'  => 'Step 1 description',
 		];
 
-		$this->meta_tags_context->id             = $id;
-		$this->meta_tags_context->main_schema_id = $main_schema_id;
-
-		$this->post
-			->expects( 'get_post_title_with_fallback' )
-			->with( $id )
-			->andReturn( $post_title );
-
-		$this->html
-			->shouldReceive( 'smart_strip_tags' )
-			->andReturnArg( 0 );
-
-		$this->html
-			->shouldReceive( 'sanitize' )
-			->andReturnArg( 0 );
-
-		$this->language
-			->shouldReceive( 'add_piece_language' )
-			->andReturnUsing( [ $this, 'set_language' ] );
-
-		$actual = $this->instance->generate( $this->meta_tags_context );
-
-		$expected = [
-			[
-				'@type'            => 'HowTo',
-				'@id'              => '#howto-1',
-				'name'             => 'post title',
-				'mainEntityOfPage' => [
-					'@id' => 'https://example.com/post-1#main-schema-id',
-				],
-				'description'      => 'An How To block description <em>with markup</em>.',
-				'totalTime'        => 'P50DT23H27M',
-				'step'             => [
-					[
-						'@type'           => 'HowToStep',
-						'url'             => '#how-to-step-1583764039837',
-						'name'            => '<strong>Step 1 title</strong>',
-						'itemListElement' => [
-							[
-								'@type' => 'HowToDirection',
-								'text'  => 'Step 1 description',
-							],
-						],
-					],
-				],
-				'inLanguage'       => 'language',
+		return [
+			'happy path'         => [
+				$this->base_blocks,
+				$this->base_expected_schema,
+			],
+			'with an empty name' => [
+				$no_name_blocks,
+				$no_name_expected_schema,
 			],
 		];
-
-		$this->assertEquals( $expected, $actual );
 	}
 
 	/**
