6f2fcd4fd5c70a65912379c85577ce4ed9cbb8d2
Hans-Christiaan <hans-christiaan@yoast.com>
2020-03-12 13:07:53 +0100

    Stub class for primary term builder.

---
+++ src/builders/primary-term-bulder.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+ * Creates an indexable for the primary term.
+ */
+class Primary_Term_Builder {
+
+	/**
+	 * Primary_Term_Builder constructor.
+	 */
+	public function __construct() {
+
+	}
+
+
+	/**
+	 * Formats the data.
+	 *
+	 * @param int $post_id The post ID.
+	 */
+	public function build( $post_id ) {
+
+	}
+}
