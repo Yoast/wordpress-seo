8ae85687718b88e6d4e658c5024bb9f7b1556d42
Bram <bintzandt@users.noreply.github.com>
2020-03-16 14:51:48 +0100

    Update the Input and the FieldGroup component to match the HTML
    Rename Input to TextInput because it can only be used for text

--- apps/components/ReactifiedComponentsWrapper.js
+++ apps/components/ReactifiedComponentsWrapper.js
@@ -1,13 +1,19 @@
 import React from "react";
-import Input from "@yoast/components/src/inputs/Input";
+import { TextInput } from "@yoast/components/src/inputs/Input";
 
 const ReactifiedComponentsWrapper = () => {
 	return (
 		<div>
-			<Input
+			<TextInput
 				label="This is the input label"
 				value="This is the input value"
 				type="text"
+			/>
+			<TextInput
+				label="This input has a description"
+				description="Great description!"
+				type="text"
+				placeholder="The best placeholder ev4h"
 			/>
 		</div>
 	);
--- packages/components/src/inputs/Input.js
+++ packages/components/src/inputs/Input.js
@@ -1,41 +1,48 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FieldGroupLabelIcon } from "../field-group/FieldGroup";
+import FieldGroup from "../field-group/FieldGroup";
 
 // Import the required CSS.
 import "./input.css";
 
-const Input = ( { id, label, type, name, value, ariaDescribedBy } ) => {
+export const TextInput = ( props ) => {
 	return (
-		<FieldGroupLabelIcon htmlFor={ id } label={ label }>
+		<FieldGroup
+			htmlFor={ props.id }
+			label={ props.label }
+			description={ props.description }
+			linkText={ props.linkText }
+			linkTo={ props.linkTo }
+		>
 			<input
-				type={ type }
-				value={ value }
-				name={ name }
-				id={ id }
-				aria-describedby={ ariaDescribedBy }
+				type="text"
+				value={ props.value }
+				className="yoast-field-group__inputfield"
+				name={ props.name }
+				id={ props.id }
+				aria-describedby={ props.ariaDescribedBy }
+				placeholder={ props.placeholder }
 			/>
-		</FieldGroupLabelIcon>
+		</FieldGroup>
 	);
 };
 
-Input.propTypes = {
+TextInput.propTypes = {
 	id: PropTypes.string,
 	label: PropTypes.string,
-	type: PropTypes.string,
 	name: PropTypes.name,
 	value: PropTypes.string,
 	ariaDescribedBy: PropTypes.string,
+	description: PropTypes.string,
+	placeholder: PropTypes.string,
 };
 
-Input.defaultProps = {
+TextInput.defaultProps = {
 	// Generate a unique string of length 8.
 	id: (+new Date).toString(36).slice(-8),
 	label: "",
-	type: "text",
 	name: "",
 	value: "",
 	ariaDescribedBy: "",
+	placeholder: "",
 };
-
-export default Input;
