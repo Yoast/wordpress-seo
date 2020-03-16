ab488365d2c84ded61bedf0e21528b55e3796044
Herre Groen <herre@yoast.com>
2020-03-16 10:00:34 +0100

    Handle themes that do not support title-tag

--- src/presenters/title-presenter.php
+++ src/presenters/title-presenter.php
@@ -37,17 +37,21 @@ class Title_Presenter extends Abstract_Indexable_Presenter {
 	 * Returns the document title.
 	 *
 	 * @param Indexable_Presentation $presentation The presentation of an indexable.
+	 * @param bool                   $output_html  Optional. Whether or not to output HTML. Defaults to true.
 	 *
 	 * @return string The document title tag.
 	 */
-	public function present( Indexable_Presentation $presentation ) {
+	public function present( Indexable_Presentation $presentation, $output_html = true ) {
 		$title = $this->replace_vars( $presentation->title, $presentation );
 		$title = $this->filter( $title, $presentation );
 		$title = $this->string->strip_all_tags( \stripslashes( $title ) );
 		$title = \trim( $title );
 
 		if ( \is_string( $title ) && $title !== '' ) {
-			return '<title>' . \esc_html( $title ) . '</title>';
+			if ( $output_html ) {
+				return '<title>' . \esc_html( $title ) . '</title>';
+			}
+			return \esc_html( $title );
 		}
 
 		return '';
