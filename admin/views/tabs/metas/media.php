3fdce8e43fd79d9e8961c0862b259b1e31d46218
jdevalk <joost@yoast.com>
2020-03-18 15:29:46 +0100

    Introduce nu Admin_Help_Button class, to replace Admin_Help_Panel

--- admin/views/tabs/metas/media.php
+++ admin/views/tabs/metas/media.php
@@ -15,12 +15,8 @@ $wpseo_media_presenter = new WPSEO_Paper_Presenter(
 	esc_html__( 'Media & attachment URLs', 'wordpress-seo' ),
 	dirname( __FILE__ ) . '/paper-content/media-content.php',
 	[
-		'help_text' => new WPSEO_Admin_Help_Panel(
-			'search-appearance-media',
-			__( 'Learn more about the Media and attachment URLs setting', 'wordpress-seo' ),
-			__( 'When you upload media (an image or video for example) to WordPress, it doesn\'t just save the media, it creates an attachment URL for it. These attachment pages are quite empty: they contain the media item and maybe a title if you entered one. Because of that, if you never use these attachment URLs, it\'s better to disable them, and redirect them to the media item itself.', 'wordpress-seo' ),
-			'has-wrapper'
-		),
+		// @todo fix the shortlink below to be a real yoa.st shortlink to the correct page!
+		'help_text' => new WPSEO_Admin_Help_Button( 'shortlink', __( 'Learn more about the Media and attachment URLs setting', 'wordpress-seo' ) ),
 		'paper_id'  => 'settings-media-attachment-url',
 		'class'     => 'search-appearance',
 	]
