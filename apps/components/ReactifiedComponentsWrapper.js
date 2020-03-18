8af61ef5158ecb98483258b0b821da6748218eb8
Bram <bintzandt@users.noreply.github.com>
2020-03-18 13:30:46 +0100

    Add an place where we can render 'new' components

---
+++ apps/components/ReactifiedComponentsWrapper.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Input from "@yoast/components/src/inputs/Input";
+
+const ReactifiedComponentsWrapper = () => {
+	return (
+		<div>
+			<Input
+				label="This is the input label"
+				value="This is the input value"
+				type="text"
+			/>
+		</div>
+	);
+};
+export default ReactifiedComponentsWrapper;
