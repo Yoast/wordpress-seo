c52d37a51c11025b9d189099af8444e86acf28e6
Maarten Leenders <maarten@yoast.com>
2020-03-23 14:32:57 +0100

    enqueue monorepo styles on admin pages

--- admin/class-config.php
+++ admin/class-config.php
@@ -56,7 +56,7 @@ class WPSEO_Admin_Pages {
 		wp_enqueue_style( 'global' );
 		wp_enqueue_style( 'wp-admin' );
 		$this->asset_manager->enqueue_style( 'select2' );
-
+		$this->asset_manager->enqueue_style( 'monorepo');
 		$this->asset_manager->enqueue_style( 'admin-all' );
 	}
 
