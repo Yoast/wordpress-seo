7fe503bd9552cfc67817238cb74be30d6a36c1ed
Igor <igor@yoast.com>
2020-03-23 10:04:59 +0100

    Add Organization schema generate and is_needed backwards compatibility

--- deprecated/frontend/schema/class-schema-organization.php
+++ deprecated/frontend/schema/class-schema-organization.php
@@ -5,6 +5,9 @@
  * @package WPSEO\Frontend\Schema
  */
 
+use Yoast\WP\SEO\Generators\Schema\Organization;
+use Yoast\WP\SEO\Memoizer\Meta_Tags_Context_Memoizer;
+
 /**
  * Returns schema Organization data.
  *
@@ -15,13 +18,30 @@
 class WPSEO_Schema_Organization implements WPSEO_Graph_Piece {
 
 	/**
+	 * Holds the Organization schema generator.
+	 *
+	 * @var Organization
+	 */
+	private $organization;
+
+	/**
+	 * Holds a memoizer for the meta tag context.
+	 *
+	 * @var Meta_Tags_Context_Memoizer
+	 */
+	private $memoizer;
+
+	/**
 	 * WPSEO_Schema_Organization constructor.
 	 *
 	 * @codeCoverageIgnore
 	 * @deprecated xx.x
 	 */
 	public function __construct() {
-		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+		_deprecated_function( __METHOD__, 'WPSEO xx.x', 'Yoast\WP\SEO\Generators\Schema\Organization' );
+
+		$this->memoizer = YoastSEO()->classes->get( Meta_Tags_Context_Memoizer::class );
+		$this->organization = YoastSEO()->classes->get( Organization::class );
 	}
 
 	/**
@@ -33,9 +53,11 @@ class WPSEO_Schema_Organization implements WPSEO_Graph_Piece {
 	 * @return bool
 	 */
 	public function is_needed() {
-		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+		_deprecated_function( __METHOD__, 'WPSEO xx.x', 'Yoast\WP\SEO\Generators\Schema\Organization::is_needed' );
 
-		return false;
+		$context = $this->memoizer->for_current_page();
+
+		return $this->organization->is_needed( $context );
 	}
 
 	/**
@@ -47,8 +69,10 @@ class WPSEO_Schema_Organization implements WPSEO_Graph_Piece {
 	 * @return array $data The Organization schema.
 	 */
 	public function generate() {
-		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+		_deprecated_function( __METHOD__, 'WPSEO xx.x', 'Yoast\WP\SEO\Generators\Schema\Organization::generate' );
 
-		return array();
+		$context = $this->memoizer->for_current_page();
+
+		return $this->organization->generate( $context );
 	}
 }
