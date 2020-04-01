ab88ec3c1436124ea4ae42465d18a0a68d04fbab
Alexander Botteram <a.botteram@gmail.com>
2020-04-01 11:43:58 +0200

    Added prop to determine what field is hovered over

--- apps/components/SocialPreviewEditorWrapper.js
+++ apps/components/SocialPreviewEditorWrapper.js
@@ -1,6 +1,12 @@
 import React  from "react";
+import styled from "styled-components";
 
 import SocialPreviewEditor from "@yoast/social-metadata-previews/src/editor/SocialPreviewEditor";
+const Container = styled.div`
+	background-color: white;
+	margin: 20px;
+`;
+
 
 class SocialPreviewEditorWrapper extends React.Component {
 	constructor( props ) {
@@ -15,6 +21,7 @@ class SocialPreviewEditorWrapper extends React.Component {
 		this.setTitle = this.setStateAttribute.bind( this, "title" );
 		this.setDescription = this.setStateAttribute.bind( this, "description" );
 		this.setImage = this.setStateAttribute.bind( this, "image" );
+		this.removeImage = this.setStateAttribute.bind( this, "image", "" );
 	}
 
 	setStateAttribute( attr, value ) {
@@ -27,17 +34,19 @@ class SocialPreviewEditorWrapper extends React.Component {
 	render() {
 		console.log( this.state );
 		return (
-			<SocialPreviewEditor
-				title={ this.state.title }
-				onTitleChange={ this.setTitle }
-				description={ this.state.description }
-				onDescriptionChange={ this.setDescription }
-				siteName="Site name"
-				onRemoveImageClick={ () => this.setImage( "" ) }
-				alt="Alt text"
-				image={ this.state.image }
-				onSelectImageClick={ () => this.setImage( "https://www.yarrah.com/en/wp-content/uploads/sites/10/2019/01/Puppy-aanschaffen-header-800x600.png" ) }
-			/>
+			<Container>
+				<SocialPreviewEditor
+					title={ this.state.title }
+					onTitleChange={ this.setTitle }
+					description={ this.state.description }
+					onDescriptionChange={ this.setDescription }
+					siteName="Site name"
+					onRemoveImageClick={ this.removeImage }
+					alt="Alt text"
+					image={ this.state.image }
+					onSelectImageClick={ () => this.setImage( "https://www.yarrah.com/en/wp-content/uploads/sites/10/2019/01/Puppy-aanschaffen-header-800x600.png" ) }
+				/>
+			</Container>
 		);
 	}
 
--- packages/social-metadata-forms/src/SocialMetadataPreviewForm.js
+++ packages/social-metadata-forms/src/SocialMetadataPreviewForm.js
@@ -3,6 +3,38 @@ import ImageSelect from "./ImageSelect";
 import PropTypes from "prop-types";
 import { ReplacementVariableEditor, replacementVariablesShape } from "@yoast/replacement-variable-editor";
 import { __, sprintf } from "@wordpress/i18n";
+import styled from "styled-components";
+import { getDirectionalStyle } from "@yoast/helpers";
+import { angleLeft, angleRight, colors } from "@yoast/style-guide";
+
+/**
+ * Adds caret styles to a component.
+ *
+ * @param {ReactComponent} WithoutCaret The component without caret styles.
+ * @param {string} color The color to render the caret in.
+ * @param {string} mode The mode the snippet preview is in.
+ *
+ * @returns {ReactComponent} The component with caret styles.
+ */
+function addCaretStyle( WithoutCaret ) {
+	return styled( WithoutCaret )`
+		&::before {
+			display: block;
+			position: absolute;
+			top: 0;
+			${ getDirectionalStyle( "left", "right" ) }: ${ () => "-22px" };
+			width: 22px;
+			height: 22px;
+			background-image: url( ${ getDirectionalStyle( angleRight( colors.$color_white ), angleLeft( colors.$color_snippet_hover ) ) } );
+			background-size: 24px;
+			background-repeat: no-repeat;
+			background-position: center;
+			content: "";
+		}
+	`;
+}
+
+const CaretContainer = addCaretStyle( styled.div`` );
 
 /**
  * A form with an image selection button, a title input field and a description field.
@@ -36,23 +68,27 @@ const SocialMetadataPreviewForm = ( props ) => {
 				imageUrl={ props.imageUrl }
 				isPremium={ props.isPremium }
 			/>
-			<ReplacementVariableEditor
-				onChange={ props.onTitleChange }
-				content={ props.title }
-				replacementVariables={ props.replacementVariables }
-				recommendedReplacementVariables={ props.recommendedReplacementVariables }
-				type="title"
-				label={ titleEditorTitle }
-			/>
-			<ReplacementVariableEditor
-				onChange={ props.onDescriptionChange }
-				content={ props.description }
-				placeholder={ descEditorPlaceholder }
-				replacementVariables={ props.replacementVariables }
-				recommendedReplacementVariables={ props.recommendedReplacementVariables }
-				type="description"
-				label={ descEditorTitle }
-			/>
+			<CaretContainer>
+				<ReplacementVariableEditor
+					onChange={ props.onTitleChange }
+					content={ props.title }
+					replacementVariables={ props.replacementVariables }
+					recommendedReplacementVariables={ props.recommendedReplacementVariables }
+					type="title"
+					label={ titleEditorTitle }
+				/>
+			</CaretContainer>
+			<CaretContainer>
+				<ReplacementVariableEditor
+					onChange={ props.onDescriptionChange }
+					content={ props.description }
+					placeholder={ descEditorPlaceholder }
+					replacementVariables={ props.replacementVariables }
+					recommendedReplacementVariables={ props.recommendedReplacementVariables }
+					type="description"
+					label={ descEditorTitle }
+				/>
+			</CaretContainer>
 		</Fragment>
 	);
 }
