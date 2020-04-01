8a06a96b19f372f4e711556c55a4ade21355aa5d
jdevalk <joost@yoast.com>
2020-04-01 23:30:41 +0200

    More documentation

--- src/surfaces/current-page-surface.php
+++ src/surfaces/current-page-surface.php
@@ -12,50 +12,50 @@ use Yoast\WP\SEO\Memoizer\Meta_Tags_Context_Memoizer;
 /**
  * Class Current_Page_Surface
  *
- * @property string      $canonical
- * @property string      $description
- * @property string      $title
- * @property string      $id
- * @property string      $site_name
- * @property string      $wordpress_site_name
- * @property string      $site_url
- * @property string      $company_name
- * @property int         $company_logo_id
- * @property int         $site_user_id
- * @property string      $site_represents
- * @property array|false $site_represents_reference
- * @property bool        $breadcrumbs_enabled
- * @property string      $schema_page_type
- * @property string      $main_schema_id
- * @property string      $page_type
- * @property string      $meta_description
- * @property array       $robots
- * @property array       $googlebot
- * @property string      $rel_next
- * @property string      $rel_prev
- * @property bool        $open_graph_enabled
- * @property string      $open_graph_publisher
- * @property string      $open_graph_type
- * @property string      $open_graph_title
- * @property string      $open_graph_description
- * @property array       $open_graph_images
- * @property string      $open_graph_url
- * @property string      $open_graph_site_name
- * @property string      $open_graph_article_publisher
- * @property string      $open_graph_article_author
- * @property string      $open_graph_article_published_time
- * @property string      $open_graph_article_modified_time
- * @property string      $open_graph_locale
- * @property string      $open_graph_fb_app_id
- * @property array       $schema
- * @property string      $twitter_card
- * @property string      $twitter_title
- * @property string      $twitter_description
- * @property string      $twitter_image
- * @property string      $twitter_creator
- * @property string      $twitter_site
- * @property array       $source
- * @property array       $breadcrumbs
+ * @property string      $canonical                         The canonical URL for the current page.
+ * @property string      $description                       The meta description for the current page, if set.
+ * @property string      $title                             The SEO title for the current page.
+ * @property string      $id                                The requested object ID.
+ * @property string      $site_name                         The site name from the Yoast SEO settings.
+ * @property string      $wordpress_site_name               The site name from the WordPress settings.
+ * @property string      $site_url                          The site's main URL.
+ * @property string      $company_name                      The company name from the Knowledge graph settings.
+ * @property int         $company_logo_id                   The attachment ID for the company logo.
+ * @property int         $site_user_id                      If the site represents a 'person', this is the ID of the accompanying user profile.
+ * @property string      $site_represents                   Whether the site represents a 'person' or a 'company'.
+ * @property array|false $site_represents_reference         The schema reference ID for what this site represents.
+ * @property bool        $breadcrumbs_enabled               Whether breadcrumbs are enabled or not.
+ * @property string      $schema_page_type                  The Schema page type.
+ * @property string      $main_schema_id                    Schema ID that points to the main Schema thing on the page, usually the webpage or article Schema piece.
+ * @property string      $page_type                         The Schema page type.
+ * @property string      $meta_description                  The meta description for the current page, if set.
+ * @property array       $robots                            An array of the robots values set for the current page.
+ * @property array       $googlebot                         The meta robots values we specifically output for Googlebot on this page.
+ * @property string      $rel_next                          The next page in the series, if any.
+ * @property string      $rel_prev                          The previous page in the series, if any.
+ * @property bool        $open_graph_enabled                Whether OpenGraph is enabled on this site.
+ * @property string      $open_graph_publisher              The OpenGraph publisher reference.
+ * @property string      $open_graph_type                   The og:type.
+ * @property string      $open_graph_title                  The og:title.
+ * @property string      $open_graph_description            The og:description.
+ * @property array       $open_graph_images                 The array of images we have for this page.
+ * @property string      $open_graph_url                    The og:url.
+ * @property string      $open_graph_site_name              The og:site_name.
+ * @property string      $open_graph_article_publisher      The article:publisher value.
+ * @property string      $open_graph_article_author         The article:author value.
+ * @property string      $open_graph_article_published_time The article:published_time value.
+ * @property string      $open_graph_article_modified_time  The article:modified_time value.
+ * @property string      $open_graph_locale                 The og:locale for the current page.
+ * @property string      $open_graph_fb_app_id              The Facebook App ID.
+ * @property array       $schema                            The entire Schema array for the current page.
+ * @property string      $twitter_card                      The Twitter card type for the current page.
+ * @property string      $twitter_title                     The Twitter card title for the current page.
+ * @property string      $twitter_description               The Twitter card description for the current page.
+ * @property string      $twitter_image                     The Twitter card image for the current page.
+ * @property string      $twitter_creator                   The Twitter card author for the current page.
+ * @property string      $twitter_site                      The Twitter card site reference for the current page.
+ * @property array       $source                            The source object for most of this page's data.
+ * @property array       $breadcrumbs                       The breadcrumbs array for the current page.
  */
 class Current_Page_Surface {
 
