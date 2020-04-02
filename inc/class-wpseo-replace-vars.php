f2e78a9ed9e5858a054efb23d38e581752850b62
Marco Almeida <webdados@users.noreply.github.com>
2020-04-02 14:54:04 +0100

    Fix elseif on a new line

--- inc/class-wpseo-replace-vars.php
+++ inc/class-wpseo-replace-vars.php
@@ -705,7 +705,8 @@ class WPSEO_Replace_Vars {
 				if ( $name !== '' ) {
 					$replacement = $name;
 				}
-			} elseif ( is_category() || is_tag() || is_tax() ) {
+			}
+			elseif ( is_category() || is_tag() || is_tax() ) {
 				$term = $GLOBALS['wp_query']->get_queried_object();
 				$name = get_term_meta( $term->term_id, $field, true );
 				if ( $name !== '' ) {
