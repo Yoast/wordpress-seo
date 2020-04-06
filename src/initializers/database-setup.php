8d2a4f9b936975671864aa7d53d82481ab46b9f1
Herre Groen <herre@yoast.com>
2020-04-06 18:39:33 +0200

    Build connection string with logic matching wpdb

--- src/initializers/database-setup.php
+++ src/initializers/database-setup.php
@@ -39,11 +39,48 @@ class Database_Setup implements Initializer_Interface {
 	 * Initializes the database setup.
 	 */
 	public function initialize() {
-		ORM::configure( 'mysql:host=' . \DB_HOST . ';dbname=' . \DB_NAME );
+		ORM::configure( $this->get_connection_string() );
 		ORM::configure( 'username', \DB_USER );
 		ORM::configure( 'password', \DB_PASSWORD );
 
 		Yoast_Model::$auto_prefix_models = '\\Yoast\\WP\\SEO\\Models\\';
 		Yoast_Model::$logger             = $this->logger;
+	}
+
+	/**
+	 * Builds a connection string from wpdb
+	 *
+	 * @return string The connection string.
+	 */
+	private function get_connection_string() {
+		global $wpdb;
+
+		$host    = \DB_HOST;
+		$port    = null;
+		$socket  = null;
+		$is_ipv6 = false;
+
+		$host_data = $wpdb->parse_db_host( \DB_HOST );
+		if ( $host_data ) {
+			list( $host, $port, $socket, $is_ipv6 ) = $host_data;
+		}
+		if ( $is_ipv6 && extension_loaded( 'mysqlnd' ) ) {
+			$host = "[$host]";
+		}
+		if ( empty( $port ) ) {
+			$port = ini_get( 'mysqli.default_port' );
+		}
+
+		$connection_string = 'mysql:host=' . $host . ';dbname=' . \DB_NAME . ';';
+		if ( ! empty( $port ) ) {
+			$connection_string .= 'port=' . $port . ';';
+		}
+		if ( ! empty( $socket ) ) {
+			$connection_string .= 'unix_socket=' . $socket . ';';
+		}
+		if ( ! empty( \DB_CHARSET ) ) {
+			$connection_string .= 'charset=' . \DB_CHARSET . ';';
+		}
+		return $connection_string;
 	}
 }
