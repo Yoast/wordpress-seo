6a98be7c5e183017b11c5a80e66b47b115c0b2ba
agnieszkaszuba <agnieszka@yoast.com>
2020-04-09 18:07:34 +0200

    Add some missing suffixes and specs for those suffixes

--- packages/yoastseo/src/morphology/polish/stem.js
+++ packages/yoastseo/src/morphology/polish/stem.js
@@ -20,90 +20,25 @@
  * SOFTWARE.
  */
 
-/**
- * Loops through an array of word endings and returns the longest ending that was matched at the end of the word.
- *
- * @param {string}      word       The word to check.
- * @param {string[]}    endings    The word endings to check.
- * @returns {string}    The longest matched ending.
- */
-const endsInArr = function( word, endings ) {
-	const matches = [];
-	for ( const i in endings ) {
-		if ( word.endsWith( endings[ i ] ) ) {
-			matches.push( endings[ i ] );
-		}
-	}
-
-	const longest = matches.sort( function( a, b ) {
-		return b.length - a.length;
-	} )[ 0 ];
-
-	if ( longest ) {
-		return longest;
-	}
-	return "";
-};
-
-/**
- * Checks if a word is longer than the inputted word length. If it is, it checks whether the word ends with one of the endings
- * from the inputted array. If it does, it removes the suffix from the end of the word (the length of the actual suffix is not always
- * equal to the length of the checked ending, so suffix length is a separate parameter).
- *
- * @param {string} word            The word to stem.
- * @param {number} wordLength      The length of the word.
- * @param {string[]} wordEndings   The suffix group.
- * @param {number} suffixLength    The length of the suffix.
- *
- * @returns {string}               The stemmed word.
- */
-const findSuffixInGroupAndStem = function( word, wordLength, wordEndings, suffixLength ) {
-	if ( word.length > wordLength ) {
-		const longestMatchedWordEnding = endsInArr( word, wordEndings );
-
-		if ( longestMatchedWordEnding !== "" ) {
-			return word.slice( 0, -suffixLength );
-		}
-	}
-};
-
-/**
- * Goes through all the suffix groups in a given class (i.e., diminutive, noun, verb, adjective/adverb or general) and
- * stems the word if a suffix is found and should be stemmed.
- *
- * @param {string}      word            The word to stem.
- * @param {Object}      suffixClass     The class of suffixes to check.
- * @returns {string}    The stemmed word.
- */
-const findSuffixInClassAndStem = function( word, suffixClass ) {
-	for ( const suffixGroup in suffixClass ) {
-		if ( suffixClass.hasOwnProperty( suffixGroup ) ) {
-			const wordShouldBeLongerThan = suffixClass[ suffixGroup ].wordShouldBeLongerThan;
-			const wordEndings = suffixClass[ suffixGroup ].wordEndings;
-			const suffixLength = suffixClass[ suffixGroup ].suffixLength;
-
-			const stemmedWord = findSuffixInGroupAndStem( word, wordShouldBeLongerThan, wordEndings, suffixLength );
-
-			if ( stemmedWord ) {
-				return stemmedWord;
-			}
-		}
-	}
-};
-
 const diminutiveSuffixes = {
 	suffixes1: {
-		wordEndings: [ "eczek", "iczek", "iszek", "aszek", "uszek" ],
+		wordEndings: [ "eczek", "eczka", "eczki", "iczek", "iczka", "iczki", "iszek", "iszka", "iszki", "aszek", "aszki",
+			"aszka", "uszek", "uszka", "uszki", "aczek", "aczka", "aczki" ],
 		wordShouldBeLongerThan: 6,
 		suffixLength: 5,
 	},
 	suffixes2: {
-		wordEndings: [ "enek", "ejek", "erek" ],
+		wordEndings: [ "ątko", "unio", "usia" ],
+		wordShouldBeLongerThan: 6,
+		suffixLength: 4,
+	},
+	suffixes3: {
+		wordEndings: [ "enek", "enka", "enki", "enką", "ejek", "ejka", "ejki", "ejką", "erek", "erki" ],
 		wordShouldBeLongerThan: 6,
 		suffixLength: 2,
 	},
-	suffixes3: {
-		wordEndings: [ "ek", "ak" ],
+	suffixes4: {
+		wordEndings: [ "ek", "ak", "ka", "ko", "uś" ],
 		wordShouldBeLongerThan: 4,
 		suffixLength: 2,
 	},
@@ -111,16 +46,46 @@ const diminutiveSuffixes = {
 
 const verbSuffixes = {
 	suffixes1: {
-		wordEndings: [ "bym", "esz", "asz", "cie", "eść", "aść", "łem", "amy", "emy" ],
+		wordEndings: [ "łybyście", "libyście" ],
+		wordShouldBeLongerThan: 10,
+		suffixLength: 8,
+	},
+	suffixes2: {
+		wordEndings: [ "łybyśmy", "libyśmy" ],
+		wordShouldBeLongerThan: 9,
+		suffixLength: 7,
+	},
+	suffixes3: {
+		wordEndings: [ "liście", "łyśmy", "liśmy" ],
+		wordShouldBeLongerThan: 8,
+		suffixLength: 6,
+	},
+	suffixes4: {
+		wordEndings: [ "łabym", "łabyś" ],
+		wordShouldBeLongerThan: 7,
+		suffixLength: 5,
+	},
+	suffixes5: {
+		wordEndings: [ "łyby", "liby", "łaby", "łszy", "wszy" ],
+		wordShouldBeLongerThan: 5,
+		suffixLength: 4,
+	},
+	suffixes6: {
+		wordEndings: [ "bym", "byś", "esz", "asz", "cie", "eść", "eźć", "aść", "łem", "łam", "łeś", "amy", "emy", "ała", "iła" ],
 		wordShouldBeLongerThan: 5,
 		suffixLength: 3,
 	},
-	suffixes2: {
+	suffixes7: {
+		wordEndings: [ "łby" ],
+		wordShouldBeLongerThan: 4,
+		suffixLength: 3,
+	},
+	suffixes8: {
 		wordEndings: [ "esz", "asz", "eść", "aść", "ać", "em", "am", "ał", "ił", "ić", "ąc", "eć" ],
 		wordShouldBeLongerThan: 3,
 		suffixLength: 2,
 	},
-	suffixes3: {
+	suffixes9: {
 		wordEndings: [ "aj" ],
 		wordShouldBeLongerThan: 3,
 		suffixLength: 1,
@@ -167,26 +132,36 @@ const nounSuffixes = {
 
 const adjectiveAndAdverbSuffixes = {
 	suffixes1: {
-		wordEndings: [ "szych" ],
+		wordEndings: [ "ejszych", "ejszego", "owszych", "owszego" ],
+		wordShouldBeLongerThan: 9,
+		suffixLength: 7,
+	},
+	suffixes2: {
+		wordEndings: [ "cznych", "ejszej", "owszej", "cznego" ],
+		wordShouldBeLongerThan: 8,
+		suffixLength: 6,
+	},
+	suffixes3: {
+		wordEndings: [ "szych", "ejsze", "ejszy", "ejsza", "ejszą", "owsze", "owszy", "owsza", "owszą", "cznej" ],
 		wordShouldBeLongerThan: 7,
 		suffixLength: 5,
 	},
-	suffixes2: {
-		wordEndings: [ "sze", "szy", "owy", "owa", "owe", "ych", "ego" ],
+	suffixes4: {
+		wordEndings: [ "sze", "szy", "sza", "owy", "owa", "owe", "ych", "ego" ],
 		wordShouldBeLongerThan: 5,
 		suffixLength: 3,
 	},
-	suffixes3: {
-		wordEndings: [ "czny" ],
+	suffixes5: {
+		wordEndings: [ "czny", "czna", "czne" ],
 		wordShouldBeLongerThan: 6,
 		suffixLength: 4,
 	},
-	suffixes4: {
+	suffixes6: {
 		wordEndings: [ "ej" ],
 		wordShouldBeLongerThan: 5,
 		suffixLength: 2,
 	},
-	suffixes5: {
+	suffixes7: {
 		wordEndings: [ "nie", "wie", "rze" ],
 		wordShouldBeLongerThan: 4,
 		suffixLength: 2,
@@ -207,8 +182,85 @@ const generalSuffixes = {
 };
 
 /**
+ * Loops through an array of word endings and returns the longest ending that was matched at the end of the word.
+ *
+ * @param {string}      word       The word to check.
+ * @param {string[]}    endings    The word endings to check.
+ * @returns {string}    The longest matched ending.
+ */
+const endsInArr = function( word, endings ) {
+	const matches = [];
+	for ( const i in endings ) {
+		if ( word.endsWith( endings[ i ] ) ) {
+			matches.push( endings[ i ] );
+		}
+	}
+
+	const longest = matches.sort( function( a, b ) {
+		return b.length - a.length;
+	} )[ 0 ];
+
+	if ( longest ) {
+		return longest;
+	}
+	return "";
+};
+
+/**
+ * Checks if a word is longer than the word length threshold specified for a given suffix group. If the word length does not
+ * meet the threshold, it is not stemmed.
+ *
+ * If it does meet the threshold, checks whether the word ends with one of the endings from the inputted array. If it does,
+ * it removes the suffix from the end of the word.
+ *
+ * The length of the actual suffix is not always equal to the length of the checked ending, so suffix length is a separate
+ * parameter. For example, the word 'rowerek' ends with -erek, one of the endings from the diminutive group. However, for
+ * that ending, the suffix length is defined as 2 so only -ek is stemmed.
+ *
+ * @param {string} word            The word to stem.
+ * @param {number} wordLength      The length of the word.
+ * @param {string[]} wordEndings   The suffix group.
+ * @param {number} suffixLength    The length of the suffix.
+ *
+ * @returns {string}               The stemmed word.
+ */
+const findSuffixInGroupAndStem = function( word, wordLength, wordEndings, suffixLength ) {
+	if ( word.length > wordLength ) {
+		const longestMatchedWordEnding = endsInArr( word, wordEndings );
+
+		if ( longestMatchedWordEnding !== "" ) {
+			return word.slice( 0, -suffixLength );
+		}
+	}
+};
+
+/**
+ * Goes through all the suffix groups in a given class (i.e., diminutive, noun, verb, adjective/adverb or general) and
+ * stems the word if a suffix is found and should be stemmed.
+ *
+ * @param {string}      word            The word to stem.
+ * @param {Object}      suffixClass     The class of suffixes to check.
+ * @returns {string}    The stemmed word.
+ */
+const findSuffixInClassAndStem = function( word, suffixClass ) {
+	for ( const suffixGroup in suffixClass ) {
+		if ( suffixClass.hasOwnProperty( suffixGroup ) ) {
+			const wordShouldBeLongerThan = suffixClass[ suffixGroup ].wordShouldBeLongerThan;
+			const wordEndings = suffixClass[ suffixGroup ].wordEndings;
+			const suffixLength = suffixClass[ suffixGroup ].suffixLength;
+
+			const stemmedWord = findSuffixInGroupAndStem( word, wordShouldBeLongerThan, wordEndings, suffixLength );
+
+			if ( stemmedWord ) {
+				return stemmedWord;
+			}
+		}
+	}
+};
+
+/**
  * Stems adjective and adverb suffixes. After stemming the suffixes, looks for the superlative prefix 'naj' and stems it
- * as well if found. For example, in 'najsilniejsze', first the 'sze' is stemmed and then the 'naj'.
+ * as well if found. For example, in 'najsilniejsze', first the 'ejsze' is stemmed and then the 'naj'.
  *
  * @param {string}  word    The word to stem.
  * @returns {string}        The word with removed adjective/adverb suffixes
@@ -234,7 +286,7 @@ const stemAdjectivesAndAdverbs = function( word ) {
 export default function stem( word ) {
 	word.toLowerCase();
 
-	// If a word is three characters long or shorter, the stem should be the same as the word.
+	// If the word is three characters long or shorter, the stem should be the same as the word.
 	if ( word.length < 4 ) {
 		return word;
 	}
