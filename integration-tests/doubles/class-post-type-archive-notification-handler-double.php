61ab4522f2c7ed2e256545ba2a26520230d263de
Karlijn Bok <karlijn@yoast.com>
2020-04-16 12:17:32 +0200

    Deprecate WPSEO_Post_Type_Archive_Notification_Handler and _Double

--- integration-tests/doubles/class-post-type-archive-notification-handler-double.php
+++ integration-tests/doubles/class-post-type-archive-notification-handler-double.php
@@ -7,15 +7,21 @@
 
 /**
  * Test Helper Class.
+ *
+ * @deprecated xx.x
  */
 class WPSEO_Post_Type_Archive_Notification_Handler_Double extends WPSEO_Post_Type_Archive_Notification_Handler {
 
 	/**
 	 * Checks if the notice should be shown.
 	 *
+	 * @deprecated xx.x
+	 *
 	 * @return bool True when applicable.
 	 */
 	public function is_applicable() {
+		_deprecated_function( __METHOD__, 'WPSEO xx.x' );
+
 		return parent::is_applicable();
 	}
 }
