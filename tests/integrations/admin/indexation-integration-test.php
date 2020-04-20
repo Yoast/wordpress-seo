3cb9200243fab8e8b69c03332cb308500656396c
Hans-Christiaan <hans-christiaan@yoast.com>
2020-04-20 09:44:04 +0200

    Added scaffold for indexation integration test.

---
+++ tests/integrations/admin/indexation-integration-test.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Yoast\WP\SEO\Tests\Integrations\Admin;
+
+use Mockery;
+use Yoast\WP\SEO\Actions\Indexation\Indexable_General_Indexation_Action;
+use Yoast\WP\SEO\Actions\Indexation\Indexable_Post_Indexation_Action;
+use Yoast\WP\SEO\Actions\Indexation\Indexable_Post_Type_Archive_Indexation_Action;
+use Yoast\WP\SEO\Actions\Indexation\Indexable_Term_Indexation_Action;
+use Yoast\WP\SEO\Helpers\Options_Helper;
+use Yoast\WP\SEO\Tests\TestCase;
+
+use Yoast\WP\SEO\Integrations\Admin\Indexation_Integration;
+
+class Indexation_Integration_Test extends TestCase {
+
+	/**
+	 * Holds indexation integration.
+	 *
+	 * @var Indexation_Integration
+	 */
+	private $instance;
+
+	/**
+	 * Holds the post indexation action mock.
+	 *
+	 * @var Mockery\MockInterface|Indexable_Post_Indexation_Action
+	 */
+	private $post_indexation;
+
+	/**
+	 * Holds the term indexation action mock.
+	 *
+	 * @var Mockery\MockInterface|Indexable_Term_Indexation_Action
+	 */
+	private $term_indexation;
+
+	/**
+	 * Holds the post type archive indexation action mock.
+	 *
+	 * @var Mockery\MockInterface|Indexable_Post_Type_Archive_Indexation_Action
+	 */
+	private $post_type_archive_indexation;
+
+	/**
+	 * Holds the general indexation action mock.
+	 *
+	 * @var Mockery\MockInterface|Indexable_General_Indexation_Action
+	 */
+	private $general_indexation;
+
+	/**
+	 * Holds the options helper mock.
+	 *
+	 * @var Mockery\MockInterface|Options_Helper
+	 */
+	private $options;
+
+
+	/**
+	 * @inheritDoc
+	 */
+	public function setUp() {
+		$this->post_type_archive_indexation = Mockery::mock( Indexable_Post_Type_Archive_Indexation_Action::class );
+		$this->post_indexation              = Mockery::mock( Indexable_Post_Indexation_Action::class );
+		$this->term_indexation              = Mockery::mock( Indexable_Term_Indexation_Action::class );
+		$this->general_indexation           = Mockery::mock( Indexable_General_Indexation_Action::class );
+		$this->options                      = Mockery::mock( Options_Helper::class );
+
+		$this->instance = new Indexation_Integration(
+			$this->post_indexation,
+			$this->term_indexation,
+			$this->post_type_archive_indexation,
+			$this->general_indexation,
+			$this->options
+		);
+	}
+
+	public function test_get_conditionals() {
+
+	}
+
+	public function test_render_indexation_warning() {
+
+	}
+
+	public function test_register_hooks() {
+
+	}
+
+	public function test_render_indexation_modal() {
+
+	}
+
+	public function test_enqueue_scripts() {
+
+	}
+
+	public function test_construct() {
+
+	}
+}
