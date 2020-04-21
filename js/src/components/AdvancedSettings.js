6353d0d799702bce4bd13e6c32435b2c692ba661
Maarten Leenders <maarten@yoast.com>
2020-04-21 13:11:36 +0200

    Remove memoization because it didn't work

--- js/src/components/AdvancedSettings.js
+++ js/src/components/AdvancedSettings.js
@@ -22,16 +22,12 @@ const isPost = () => window.wpseoAdminL10n.postType === "post";
  * @returns {void} Array Returns an array of options for the noIndex setting.
  */
 const getNoIndexOptions = () => {
-	if ( this.options ) {
-		return this.options;
-	}
-
 	const translatedNo = __( "No", "wordpress-seo" );
 	const translatedYes = __( "Yes", "wordpress-seo" );
 	const noIndex = window.wpseoAdminL10n.noIndex ? translatedNo : translatedYes;
 
 	if ( isPost() ) {
-		this.options = [
+		return [
 			{
 				name: sprintf(
 					/* Translators: %s translates to the Post Label in plural form, %s translates to "yes" or "no" */
@@ -44,23 +40,20 @@ const getNoIndexOptions = () => {
 			{ name: translatedNo, value: "1" },
 			{ name: translatedYes, value: "2" },
 		];
-	} else {
-		this.options = [
-			{
-				name: sprintf(
-					/* Translators: %s translates to the Post Label in plural form, %s translates to the "yes" or "no" */
-					__( "Default for %s, currently: %s", "wordpress-seo" ),
-					window.wpseoAdminL10n.postTypeNamePlural,
-					noIndex,
-				),
-				value: "default",
-			},
-			{ name: translatedYes, value: "index" },
-			{ name: translatedNo, value: "noindex" },
-		];
 	}
-
-	return this.options;
+	return [
+		{
+			name: sprintf(
+				/* Translators: %s translates to the Post Label in plural form, %s translates to the "yes" or "no" */
+				__( "Default for %s, currently: %s", "wordpress-seo" ),
+				window.wpseoAdminL10n.postTypeNamePlural,
+				noIndex,
+			),
+			value: "default",
+		},
+		{ name: translatedYes, value: "index" },
+		{ name: translatedNo, value: "noindex" },
+	];
 };
 
 
