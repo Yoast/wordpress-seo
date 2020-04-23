e5d26236b1b18977484a2c0506b7f03506fa9c53
Herre Groen <herre@yoast.com>
2020-04-23 10:47:35 +0200

    Fix failing presenter test

--- tests/presenters/admin/indexation-warning-presenter-test.php
+++ tests/presenters/admin/indexation-warning-presenter-test.php
@@ -32,11 +32,11 @@ class Indexation_Warning_Presenter_Test extends TestCase {
 
 		$presenter = new Indexation_Warning_Presenter();
 
-		$expected  = '<div id="yoast-indexation-warning" class="notice notice-warning"><p>';
-		$expected .= '<strong>NEW:</strong> Yoast SEO can speed up your website! Please ';
-		$expected .= '<button type="button" class="button-link yoast-open-indexation" data-title="Your content is being indexed">click here</button> ';
-		$expected .= 'to run our indexing process. Or <button type="button" id="yoast-indexation-dismiss-button" class="button-link hide-if-no-js" data-nonce="123456789">';
-		$expected .= 'dismiss this warning</button>.</p></div>';
+		$expected  = '<div id="yoast-indexation-warning" class="notice notice-success"><p>';
+		$expected .= '<strong>NEW:</strong> Yoast SEO can now store your site’s SEO data in a smarter way!</p>';
+		$expected .= '<button type="button" class="button yoast-open-indexation">Click here to speed up your site now</button>';
+		$expected .= '<p>Or <button type="button" id="yoast-indexation-dismiss-button" class="button-link hide-if-no-js" data-nonce="123456789">hide this notice</button> ';
+		$expected .= '(everything will continue to function as normal).</p></div>';
 
 		$this->assertEquals( $expected, $presenter->present() );
 	}
