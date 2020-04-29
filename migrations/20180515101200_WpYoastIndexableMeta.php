47850443716d91a22202622d68b491391f6b90b3
omarreiss <omar.reiss@gmail.com>
2020-04-29 20:43:47 +0200

    Make sure removed migrations that were once in the zip are still included empty to override them on servers.

---
+++ migrations/20180515101200_WpYoastIndexableMeta.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Yoast SEO Plugin File.
+ *
+ * @package WPSEO\Migrations
+ */
+
+use YoastSEO_Vendor\Ruckusing_Migration_Base;
+
+/**
+ * Removed migration.
+ */
+class WpYoastIndexableMeta extends Ruckusing_Migration_Base {
+
+	/**
+	 * Migration up.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		// do nothing.
+	}
+
+	/**
+	 * Migration down.
+	 */
+	public function down() {
+		// do nothing.
+	}
+}
