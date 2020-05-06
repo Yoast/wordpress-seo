a372ef6ffbb1a31a44604c06527619e53ea2bd9d
Andy Meerwaldt <andy@yoast.com>
2020-05-06 12:26:45 +0200

    Don't show errors when there is a migration error

--- lib/orm.php
+++ lib/orm.php
@@ -8,6 +8,7 @@
 namespace Yoast\WP\Lib;
 
 use wpdb;
+use Yoast\WP\SEO\Config\Migration_Status;
 
 /**
  *
@@ -264,12 +265,21 @@ class ORM implements \ArrayAccess {
 		 */
 		global $wpdb;
 
+		$show_errors = $wpdb->show_errors;
+
+		if ( YoastSEO()->classes->get( Migration_Status::class )->get_error( 'free' ) ) {
+			$wpdb->show_errors = false;
+		}
+
 		$parameters = \array_filter( $parameters, function ( $parameter ) {
 			return $parameter !== null;
 		} );
-		$query = $wpdb->prepare( $query, $parameters );
+		$query  = $wpdb->prepare( $query, $parameters );
+		$result = $wpdb->query( $query );
 
-		return $wpdb->query( $query );
+		$wpdb->show_errors = $show_errors;
+
+		return $result;
 	}
 
 	// ------------------------ //
