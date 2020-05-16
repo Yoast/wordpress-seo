a2756b78a801a1e12e7b70ced79d41a13f0c092e
jrfnl <jrfnl@users.noreply.github.com>
2020-05-16 16:12:55 +0200

    CS: boolean operator placement

--- src/generators/schema/person.php
+++ src/generators/schema/person.php
@@ -262,8 +262,8 @@ class Person extends Abstract_Schema_Piece {
 
 		// Article post from the same user as the site represents.
 		if (
-			$this->context->indexable->object_type === 'post' &&
-			$this->helpers->schema->article->is_article_post_type( $this->context->indexable->object_sub_type )
+			$this->context->indexable->object_type === 'post'
+			&& $this->helpers->schema->article->is_article_post_type( $this->context->indexable->object_sub_type )
 		) {
 			return $this->context->site_user_id === $this->context->indexable->author_id;
 		}
