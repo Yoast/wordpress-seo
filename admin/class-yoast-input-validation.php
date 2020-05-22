8cf59cbaaacb38d38fbdff37c276d86322ca950a
Jesser Hulshof <jesserh16@gmail.com>
2020-05-22 10:16:07 +0200

    Escape social value to prevent xss scripting

--- admin/class-yoast-input-validation.php
+++ admin/class-yoast-input-validation.php
@@ -308,7 +308,7 @@ class Yoast_Input_Validation {
 	 * @return string The error invalid value message or empty string.
 	 */
 	public static function get_dirty_value_message( $error_code ) {
-		$dirty_value = self::get_dirty_value( $error_code );
+		$dirty_value = esc_html(self::get_dirty_value( $error_code ));
 
 		if ( $dirty_value ) {
 			return sprintf(
