286d36fb57a5a09a2885e2f864d7db410b57cd6d
Hanna <48948921+hannaw93@users.noreply.github.com>
2020-05-25 13:46:02 +0200

    Update generateStems.js

--- packages/yoastseo/spec/morphology/russian/stemmerCoverage/generateStems.js
+++ packages/yoastseo/spec/morphology/russian/stemmerCoverage/generateStems.js
@@ -3,7 +3,7 @@ import stem from "../../../../src/morphology/russian/stem";
 import getMorphologyData from "../../../specHelpers/getMorphologyData";
 import filterFunctionWordsFromArray from "../../../../src/helpers/filterFunctionWordsFromArray.js";
 import sampleVocabulary from "../stemmerCoverage/sampleVocabulary.json";
-const wordsToStemWithoutFunctionWords = filterFunctionWordsFromArray( sampleVocabulary.words, "fr" );
+const wordsToStemWithoutFunctionWords = filterFunctionWordsFromArray( sampleVocabulary.words, "ru" );
 const morphologyDataRU = getMorphologyData( "ru" ).ru;
 describe( "Generate stems for Russian words", () => {
 	const corpusWithStems = wordsToStemWithoutFunctionWords.map( word => [ word, stem( word, morphologyDataRU ) ] );
