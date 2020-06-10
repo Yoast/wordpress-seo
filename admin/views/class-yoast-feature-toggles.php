d94d8b2675a5a451f6125aab93cc039fb3a61dc2
IreneStr <irene@yoast.com>
2020-06-10 16:31:47 +0200

    Add read_more_url for head endpoint settings toggle

--- admin/views/class-yoast-feature-toggles.php
+++ admin/views/class-yoast-feature-toggles.php
@@ -135,7 +135,7 @@ class Yoast_Feature_Toggles {
 					__( 'This %1$s REST API endpoint gives you all the metadata you need for a specific URL. This will make it very easy for headless WordPress sites to use %1$s for all their SEO meta output.', 'wordpress-seo' ),
 					'Yoast SEO'
 				),
-				'read_more_url'   => 'https://example.com',// @todo add a URL once this article has been written.
+				'read_more_url'   => 'https://yoa.st/40s',
 				'order'   => 100,
 			],
 		];
