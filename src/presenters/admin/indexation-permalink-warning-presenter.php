5ec254efd15ddbb27b1c4d946a631753efb60ac1
Johanna de Vos <johanna@yoast.com>
2020-06-12 14:47:19 +0200

    Extend the indexation-warning-presenter class to prevent a lot of duplicate code

--- src/presenters/admin/indexation-permalink-warning-presenter.php
+++ src/presenters/admin/indexation-permalink-warning-presenter.php
@@ -14,32 +14,7 @@ use Yoast\WP\SEO\Presenters\Abstract_Presenter;
 /**
  * Indexation_Permalink_Warning_Presenter class.
  */
-class Indexation_Permalink_Warning_Presenter extends Abstract_Presenter {
-
-	/**
-	 * The number of objects that needs to be indexed.
-	 *
-	 * @var int
-	 */
-	protected $total_unindexed;
-
-	/**
-	 * The options helper.
-	 *
-	 * @var Options_Helper
-	 */
-	private $options_helper;
-
-	/**
-	 * Indexation_Permalink_Warning_Presenter constructor.
-	 *
-	 * @param int            $total_unindexed The number of objects that needs to be indexed.
-	 * @param Options_Helper $options_helper  The options helper.
-	 */
-	public function __construct( $total_unindexed, Options_Helper $options_helper ) {
-		$this->total_unindexed = $total_unindexed;
-		$this->options_helper  = $options_helper;
-	}
+class Indexation_Permalink_Warning_Presenter extends Indexation_Warning_Presenter {
 
 	/**
 	 * Presents the warning that your site's content is not fully indexed.
@@ -84,49 +59,5 @@ class Indexation_Permalink_Warning_Presenter extends Abstract_Presenter {
 		);
 
 		return $output;
-	}
-
-	/**
-	 * Determines the message given for the estimation of the time that calculation might take.
-	 *
-	 * @return string The message.
-	 */
-	private function get_estimate() {
-		if ( $this->total_unindexed > 2500 ) {
-			$estimate = '<p>';
-			$estimate .= \esc_html__( 'We estimate this could take a long time, due to the size of your site. As an alternative to waiting, you could:', 'wordpress-seo' );
-			$estimate .= '<ul class="ul-disc">';
-			$estimate .= '<li>';
-			$estimate .= \sprintf(
-			/* translators: 1: Expands to Yoast SEO, 2: Button start tag for the reminder, 3: Button closing tag */
-				\esc_html__( 'Wait for a week or so, until %1$s automatically processes most of your content in the background. %2$sRemind me in a week.%3$s', 'wordpress-seo' ),
-				'Yoast SEO',
-				\sprintf(
-					'<button type="button" id="yoast-indexation-remind-button" class="button-link hide-if-no-js" data-nonce="%s">',
-					\esc_js( \wp_create_nonce( 'wpseo-indexation-remind' ) )
-				),
-				'</button>'
-			);
-			$estimate .= '</li>';
-			$estimate .= '<li>';
-			$estimate .= \sprintf(
-			/* translators: 1: Link to article about indexation command, 2: Anchor closing tag, 3: Link to WP CLI. */
-				\esc_html__( '%1$sRun the indexation process on your server%2$s using %3$sWP CLI%2$s', 'wordpress-seo' ),
-				'<a href="' . \esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/3-w' ) ) . '" target="_blank">',
-				'</a>',
-				'<a href="https://wp-cli.org/" target="_blank">'
-			);
-			$estimate .= '</li>';
-			$estimate .= '</ul>';
-			$estimate .= '</p>';
-
-			return $estimate;
-		}
-
-		if ( $this->total_unindexed >= 400 ) {
-			return '<p>' . \esc_html__( 'We estimate this will take a couple of minutes.', 'wordpress-seo' ) . '</p>';
-		}
-
-		return '<p>' . \esc_html__( 'We estimate this will take less than a minute.', 'wordpress-seo' ) . '</p>';
 	}
 }
