1f1e47a89b2629fc9d1040f9f66439eab2814cc1
FAMarfuaty <aida@yoast.com>
2020-06-18 16:06:55 +0200

    Up the version number to v9

--- packages/yoastseo/spec/specHelpers/getMorphologyData.js
+++ packages/yoastseo/spec/specHelpers/getMorphologyData.js
@@ -1,10 +1,10 @@
 import en from "../../premium-configuration/data/morphologyData-v3.json";
-import de from "../../premium-configuration/data/morphologyData-de-v8.json";
-import nl from "../../premium-configuration/data/morphologyData-nl-v8.json";
-import es from "../../premium-configuration/data/morphologyData-es-v8.json";
-import fr from "../../premium-configuration/data/morphologyData-fr-v8.json";
-import ru from "../../premium-configuration/data/morphologyData-ru-v8.json";
-import it from "../../premium-configuration/data/morphologyData-it-v8.json";
+import de from "../../premium-configuration/data/morphologyData-de-v9.json";
+import nl from "../../premium-configuration/data/morphologyData-nl-v9.json";
+import es from "../../premium-configuration/data/morphologyData-es-v9.json";
+import fr from "../../premium-configuration/data/morphologyData-fr-v9.json";
+import ru from "../../premium-configuration/data/morphologyData-ru-v9.json";
+import it from "../../premium-configuration/data/morphologyData-it-v9.json";
 
 
 const morphologyData = {
