f91d479ef0315d7a2c617350716e4d491e27182f
jrfnl <jrfnl@users.noreply.github.com>
2020-06-21 09:39:02 +0200

    CS: array double arrow alignment

--- admin/class-config.php
+++ admin/class-config.php
@@ -86,11 +86,11 @@ class WPSEO_Admin_Pages {
 			];
 
 			$script_data['searchAppearance'] = [
-				'isRtl'                    => is_rtl(),
-				'userEditUrl'              => add_query_arg( 'user_id', '{user_id}', admin_url( 'user-edit.php' ) ),
-				'brushstrokeBackgroundURL' => plugins_url( 'images/brushstroke_background.svg', WPSEO_FILE ),
-				'showLocalSEOUpsell'       => $this->should_show_local_seo_upsell(),
-				'localSEOUpsellURL'        => WPSEO_Shortlinker::get( 'https://yoa.st/3mp' ),
+				'isRtl'                            => is_rtl(),
+				'userEditUrl'                      => add_query_arg( 'user_id', '{user_id}', admin_url( 'user-edit.php' ) ),
+				'brushstrokeBackgroundURL'         => plugins_url( 'images/brushstroke_background.svg', WPSEO_FILE ),
+				'showLocalSEOUpsell'               => $this->should_show_local_seo_upsell(),
+				'localSEOUpsellURL'                => WPSEO_Shortlinker::get( 'https://yoa.st/3mp' ),
 				'knowledgeGraphCompanyInfoMissing' => WPSEO_Language_Utils::get_knowledge_graph_company_info_missing_l10n(),
 			];
 
