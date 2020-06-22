efaefdaa69a5f1c9c212c4a532a667a84d675fe2
Rolf Siebers <rolf@yoast.com>
2020-06-22 11:16:38 +0200

    Remove javascript function that isn't used anymore

--- js/src/admin-global.js
+++ js/src/admin-global.js
@@ -96,16 +96,6 @@
 			return false;
 		} );
 
-		jQuery( ".yoast-help-button" ).on( "click", function() {
-			var $button = jQuery( this ),
-				helpPanel = jQuery( "#" + $button.attr( "aria-controls" ) ),
-				isPanelVisible = helpPanel.is( ":visible" );
-
-			jQuery( helpPanel ).slideToggle( 200, function() {
-				$button.attr( "aria-expanded", ! isPanelVisible );
-			} );
-		} );
-
 		// Dismiss the "search engines discouraged" admin notice.
 		jQuery( "#robotsmessage button" ).on( "click", function() {
 			wpseoSetIgnore( "search_engines_discouraged_notice", "robotsmessage", jQuery( this ).data( "nonce" ) );
