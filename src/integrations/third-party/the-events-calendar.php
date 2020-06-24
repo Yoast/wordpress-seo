97b9d65f9798a29de1450f337f2cab5e8507e8f1
Hendrik Luehrsen <hendrik@luehrsen.de>
2020-06-24 12:21:53 +0200

    Bootstrap the integration

---
+++ src/integrations/third-party/the-events-calendar.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * WPSEO plugin file.
+ *
+ * @package Yoast\WP\SEO\Integrations\Third_Party
+ */
+
+namespace Yoast\WP\SEO\Integrations\Third_Party;
+
+use Yoast\WP\SEO\Conditionals\Front_End_Conditional;
+use Yoast\WP\SEO\Conditionals\The_Events_Calendar_Conditional;
+use Yoast\WP\SEO\Conditionals\Open_Graph_Conditional;
+use Yoast\WP\SEO\Integrations\Integration_Interface;
+
+/**
+ * Class Jetpack
+ */
+class The_Events_Calendar implements Integration_Interface {
+
+	/**
+	 * @inheritDoc
+	 */
+	public static function get_conditionals() {
+		return [ Front_End_Conditional::class, The_Events_Calendar_Conditional::class, Open_Graph_Conditional::class ];
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function register_hooks() {
+	}
+}
