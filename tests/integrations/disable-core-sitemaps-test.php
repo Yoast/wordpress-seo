ae01119b4abaa0ecf4875191c7d7feaa5ffd63e2
jdevalk <joost@yoast.com>
2020-06-24 16:47:11 +0200

    Disable WP core sitemaps
    Fixes PROD-190

---
+++ tests/integrations/disable-core-sitemaps-test.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Yoast SEO Plugin File.
+ *
+ * @package Yoast\YoastSEO\Integrations
+ */
+
+namespace Yoast\WP\SEO\Tests\Integrations;
+
+use Mockery;
+use Yoast\WP\SEO\Integrations\Disable_Core_Sitemaps;
+use Yoast\WP\SEO\Tests\TestCase;
+
+/**
+ * Unit Test Class.
+ *
+ * @coversDefaultClass \Yoast\WP\SEO\Integrations\Disable_Core_Sitemaps
+ *
+ * @group integrations
+ */
+class Disable_Core_Sitemaps_Test extends TestCase {
+	/**
+	 * Represents the instance we are testing.
+	 *
+	 * @var Mockery\MockInterface|Disable_Core_Sitemaps
+	 */
+	private $instance;
+
+	/**
+	 * @inheritDoc
+	 */
+	public function setUp() {
+		parent::setUp();
+
+		$this->instance = Mockery::mock( Disable_Core_Sitemaps::class )->makePartial();
+	}
+
+	/**
+	 * Tests the situation when primary term id isn't to the category id, the id should get updated.
+	 *
+	 * @covers ::register_hooks
+	 */
+	public function test_register_hooks() {
+		$this->instance->register_hooks();
+
+		$this->assertTrue( \has_filter( 'wp_sitemaps_is_enabled', '__return_false' ), 'Does not have expected wp_sitemaps_is_enabled filter' );
+	}
+
+}
