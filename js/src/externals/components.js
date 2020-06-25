4d75cac3835c67a781d62d85d1ba9c21fdda8c14
Igor <igor@yoast.com>
2020-06-25 13:17:07 +0200

    Add @yoast/replacement-variable-editor and @yoast/feature-flag as external

--- js/src/externals/components.js
+++ js/src/externals/components.js
@@ -2,8 +2,10 @@
 window.yoast = window.yoast || {};
 window.yoast.styleGuide = require( "@yoast/style-guide" );
 window.yoast.helpers = require( "@yoast/helpers" );
+window.yoast.featureFlag = require( "@yoast/feature-flag" );
 window.yoast.componentsNew = require( "@yoast/components" );
 window.yoast.configurationWizard = require( "@yoast/configuration-wizard" );
+window.yoast.replacementVariableEditor = require( "@yoast/replacement-variable-editor" );
 window.yoast.searchMetadataPreviews = require( "@yoast/search-metadata-previews" );
 window.yoast.analysisReport = require( "@yoast/analysis-report" );
 window.yoast.components = require( "yoast-components/index.js" );
