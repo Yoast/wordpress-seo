f3ed70b072758d934b613bf38737ee1ec0e95ecc
Jip <jhp.moors@gmail.com>
2020-06-30 15:26:53 +0200

    Special pages spacing

--- admin/views/tabs/metas/paper-content/special-pages.php
+++ admin/views/tabs/metas/paper-content/special-pages.php
@@ -21,7 +21,7 @@ $special_pages_help = new WPSEO_Admin_Help_Button(
 $editor = new WPSEO_Replacevar_Field( $yform, 'title-search-wpseo', __( 'Search pages', 'wordpress-seo' ), 'search', 'search' );
 $editor->render();
 
-echo '<br/>';
-
 $editor = new WPSEO_Replacevar_Field( $yform, 'title-404-wpseo', __( '404 pages', 'wordpress-seo' ), '404', '404' );
 $editor->render();
+
+echo '<br class="clear">';
