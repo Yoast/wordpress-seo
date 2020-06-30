ca54309e884a779f1555351565224831fd8b5500
Jip <jhp.moors@gmail.com>
2020-06-30 15:26:22 +0200

    Add spacing

--- admin/views/tabs/metas/paper-content/date-archives-settings.php
+++ admin/views/tabs/metas/paper-content/date-archives-settings.php
@@ -18,33 +18,37 @@ $yform->light_switch(
 	true
 );
 
-?>
-<div id='date-archives-titles-metas-content' class='archives-titles-metas-content'>
-	<?php
-	$date_archives_help = new WPSEO_Admin_Help_Button(
-		'https://yoa.st/show-x',
-		esc_html__( 'Help on the date archives search results setting', 'wordpress-seo' )
-	);
+echo '<div id="date-archives-titles-metas-content" class="archives-titles-metas-content">';
 
-	$yform->index_switch(
-		'noindex-archive-wpseo',
-		__( 'date archives', 'wordpress-seo' ),
-		$date_archives_help
-	);
+echo '<div class="yoast-space-after">';
 
-	$recommended_replace_vars     = new WPSEO_Admin_Recommended_Replace_Vars();
-	$editor_specific_replace_vars = new WPSEO_Admin_Editor_Specific_Replace_Vars();
+$date_archives_help = new WPSEO_Admin_Help_Button(
+	'https://yoa.st/show-x',
+	esc_html__( 'Help on the date archives search results setting', 'wordpress-seo' )
+);
 
-	$editor = new WPSEO_Replacevar_Editor(
-		$yform,
-		[
-			'title'                 => 'title-archive-wpseo',
-			'description'           => 'metadesc-archive-wpseo',
-			'page_type_recommended' => $recommended_replace_vars->determine_for_archive( 'date' ),
-			'page_type_specific'    => $editor_specific_replace_vars->determine_for_archive( 'date' ),
-			'paper_style'           => false,
-		]
-	);
-	$editor->render();
-	?>
-</div>
+$yform->index_switch(
+	'noindex-archive-wpseo',
+	__( 'date archives', 'wordpress-seo' ),
+	$date_archives_help
+);
+
+echo '</div>'; // space-after
+
+$recommended_replace_vars     = new WPSEO_Admin_Recommended_Replace_Vars();
+$editor_specific_replace_vars = new WPSEO_Admin_Editor_Specific_Replace_Vars();
+
+$editor = new WPSEO_Replacevar_Editor(
+	$yform,
+	[
+		'title'                 => 'title-archive-wpseo',
+		'description'           => 'metadesc-archive-wpseo',
+		'page_type_recommended' => $recommended_replace_vars->determine_for_archive( 'date' ),
+		'page_type_specific'    => $editor_specific_replace_vars->determine_for_archive( 'date' ),
+		'paper_style'           => false,
+	]
+);
+$editor->render();
+
+echo '</div>'; // archives-content
+
