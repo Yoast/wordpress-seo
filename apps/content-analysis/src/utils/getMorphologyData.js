Yoast/javascript

Move Portuguese stemming data to morphology data file

apps/content-analysis/src/utils/getMorphologyData.js
@@ -6,7 +6,7 @@ let morphologyData = null;
  * @returns {Object} The morphology data, or an empty object if not available.
  */
 function loadLocalMorphologyData() {
-	let data, dataDe, dataNL, dataES, dataFR, dataRU = {};
+	let data, dataDe, dataNL, dataES, dataFR, dataRU, dataPT = {};
 	try {
 		// Disabling global require to be able to fail.
 		// eslint-disable-next-line global-require
@@ -21,11 +21,13 @@ function loadLocalMorphologyData() {
 		dataFR = require( "../../../../packages/yoastseo/premium-configuration/data/morphologyData-fr-v6.json" );
 		// eslint-disable-next-line global-require
 		dataRU = require( "../../../../packages/yoastseo/premium-configuration/data/morphologyData-ru-v6.json" );
+		// eslint-disable-next-line global-require
+		dataPT = require( "../../../../packages/yoastseo/premium-configuration/data/morphologyData-pt-v6.json" );
 	} catch ( error ) {
 		// Falling back to empty data.
 	}
 
-	return merge( data, dataDe, dataNL, dataES, dataFR, dataRU );
+	return merge( data, dataDe, dataNL, dataES, dataFR, dataRU, dataPT );
 }
 
 /**

packages/yoastseo/spec/specHelpers/getMorphologyData.js
@@ -4,6 +4,7 @@ import nl from "../../premium-configuration/data/morphologyData-nl-v6.json";
 import es from "../../premium-configuration/data/morphologyData-es-v6.json";
 import fr from "../../premium-configuration/data/morphologyData-fr-v6.json";
 import ru from "../../premium-configuration/data/morphologyData-ru-v6.json";
+import pt from "../../premium-configuration/data/morphologyData-pt-v6.json";
 
 
 const morphologyData = {
@@ -13,6 +14,7 @@ const morphologyData = {
 	es,
 	fr,
 	ru,
+	pt,
 };
 
 /**

Yoast/wordpress-seo

apps/content-analysis/src/utils/getMorphologyData.js
@@ -6,7 +6,7 @@ let morphologyData = null;
  * @returns {Object} The morphology data, or an empty object if not available.
  */
 function loadLocalMorphologyData() {
-	let data, dataDe, dataNL, dataES, dataFR, dataRU, dataIT = {};
+	let data, dataDe, dataNL, dataES, dataFR, dataRU, dataIT, dataPT = {};
 	try {
 		// Disabling global require to be able to fail.
 		// eslint-disable-next-line global-require
@@ -23,11 +23,12 @@ function loadLocalMorphologyData() {
 		dataRU = require( "../../../../packages/yoastseo/premium-configuration/data/morphologyData-ru-v8.json" );
 		// eslint-disable-next-line global-require
 		dataIT = require( "../../../../packages/yoastseo/premium-configuration/data/morphologyData-it-v8.json" );
+		// eslint-disable-next-line global-require
+		dataPT = require( "../../../../packages/yoastseo/premium-configuration/data/morphologyData-pt-v8.json" );
 	} catch ( error ) {
 		// Falling back to empty data.
 	}
-
-	return merge( data, dataDe, dataNL, dataES, dataFR, dataRU, dataIT );
+	return merge( data, dataDe, dataNL, dataES, dataFR, dataRU, dataIT, dataPT );
 }
 
 /**

packages/yoastseo/spec/specHelpers/getMorphologyData.js
@@ -5,6 +5,7 @@ import es from "../../premium-configuration/data/morphologyData-es-v8.json";
 import fr from "../../premium-configuration/data/morphologyData-fr-v8.json";
 import ru from "../../premium-configuration/data/morphologyData-ru-v8.json";
 import it from "../../premium-configuration/data/morphologyData-it-v8.json";
+import pt from "../../premium-configuration/data/morphologyData-pt-v8.json";
 
 
 const morphologyData = {
@@ -15,6 +16,7 @@ const morphologyData = {
 	fr,
 	ru,
 	it,
+	pt,
 };
 
 /**