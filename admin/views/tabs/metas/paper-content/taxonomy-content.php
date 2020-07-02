762d547e8ba1e04d10fd32b203cf5640c908c522
Jip <jhp.moors@gmail.com>
2020-07-02 12:19:55 +0200

    Replace labels Show/Hide with Yes/No

--- admin/views/tabs/metas/paper-content/taxonomy-content.php
+++ admin/views/tabs/metas/paper-content/taxonomy-content.php
@@ -31,10 +31,14 @@ $yform->index_switch(
 );
 
 if ( $wpseo_taxonomy->name !== 'post_format' ) {
-	$yform->show_hide_switch(
+	$yform->light_switch(
 		'display-metabox-tax-' . $wpseo_taxonomy->name,
 		/* translators: %s expands to an indexable object's name, like a post type or taxonomy */
-		sprintf( __( 'Show SEO settings for %1$s', 'wordpress-seo' ), $title )
+		sprintf( __( 'Show SEO settings for %1$s', 'wordpress-seo' ), $title ),
+		[
+			__( 'No', 'wordpress-seo' ),
+			__( 'Yes', 'wordpress-seo' ),
+		]
 	);
 }
 
