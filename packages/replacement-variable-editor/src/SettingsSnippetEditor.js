ef1271e72a1d12b034419a5e2f413251f165f6ed
Jip <jhp.moors@gmail.com>
2020-07-10 15:38:20 +0200

    Move components to the replacement variable package
    These components are not bound with "yoastseo" and use the replacement-variable-editor directly.
    
    This solves two problems; decoupling of "yoastseo" for the replacement variable component and no longer having to import from the search metadata previews.

--- packages/replacement-variable-editor/src/SettingsSnippetEditor.js
+++ packages/replacement-variable-editor/src/SettingsSnippetEditor.js
@@ -4,13 +4,12 @@ import PropTypes from "prop-types";
 
 // Yoast dependencies.
 import { ErrorBoundary } from "@yoast/components";
+
+import SettingsSnippetEditorFields from "./SettingsSnippetEditorFields";
 import {
 	replacementVariablesShape,
 	recommendedReplacementVariablesShape,
-} from "@yoast/replacement-variable-editor";
-
-// Internal dependencies.
-import SettingsSnippetEditorFields from "./SettingsSnippetEditorFields";
+} from "./constants";
 
 /**
  * The snippet editor settings.
