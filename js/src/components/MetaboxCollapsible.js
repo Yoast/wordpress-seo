85e657935c924f2dfbc19d6097395a7b246947cb
Maarten Leenders <maarten@yoast.com>
2020-07-14 15:58:04 +0200

    Fix collapsible coloring, reintroduce outline and fix separator

--- js/src/components/MetaboxCollapsible.js
+++ js/src/components/MetaboxCollapsible.js
@@ -5,16 +5,9 @@ const MetaboxCollapsible = styled( SidebarCollapsible )`
 	h2 > button {
 		padding-left: 24px;
 		padding-top: 16px;
-		span > span {
-			color: #A4286A;
-		}
-
-		:focus {
-			outline: none;
-		}
 	}
 
-	div[class^="Collapsible__Content"] {
+	div[class^="collapsible_content"] {
 		padding: 24px 0;
 		margin: 0 24px;
 		border-top: 1px solid rgba(0,0,0,0.2);
