791e814ed6c180e9e947b6f6762c12a4beb1fa21
Hans-Christiaan <hans-christiaan@yoast.com>
2020-07-20 11:30:35 +0200

    Imported Mockery in test.

--- tests/actions/indexation/indexable-prepare-indexation-action-test.php
+++ tests/actions/indexation/indexable-prepare-indexation-action-test.php
@@ -2,6 +2,7 @@
 
 namespace Yoast\WP\SEO\Tests\Actions\Indexation;
 
+use Mockery;
 use Yoast\WP\SEO\Actions\Indexation\Indexable_Prepare_Indexation_Action;
 use Yoast\WP\SEO\Helpers\Date_Helper;
 use Yoast\WP\SEO\Helpers\Options_Helper;
