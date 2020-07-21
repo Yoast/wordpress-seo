56bf2593ede93170670083af466fa56cba9b06b3
omarreiss <omar.reiss@gmail.com>
2020-07-21 15:41:56 +0200

    Move webpack config to config directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@
 var timeGrunt = require( "time-grunt" );
 var path = require( "path" );
 var loadGruntConfig = require( "load-grunt-config" );
-const { flattenVersionForFile } = require( "./webpack/paths" );
+const { flattenVersionForFile } = require( "./config/webpack/paths" );
 require( "dotenv" ).config();
 
 module.exports = function( grunt ) {
