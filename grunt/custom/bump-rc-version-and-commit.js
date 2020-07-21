b4decf7e4e2d37f70548fa1c65d60d9fc3dcb461
Johanna de Vos <johanna@yoast.com>
2020-07-21 14:09:43 +0200

    Set upstream to true

--- grunt/custom/bump-rc-version-and-commit.js
+++ grunt/custom/bump-rc-version-and-commit.js
@@ -12,7 +12,7 @@ module.exports = function( grunt ) {
 			grunt.config( "gitcommit.versionBump.options.message", "Update the plugin version to " + grunt.config.data.pluginVersion );
 			grunt.task.run( "gitcommit:versionBump" );
 
-			grunt.config( "gitpush.versionBump.options", { remote: "origin" } );
+			grunt.config( "gitpush.versionBump.options", { remote: "origin", upstream: true } );
 			grunt.task.run( "gitpush:versionBump" );
 		}
 	);
