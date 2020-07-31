3a050e2686832312280372c7573c967ca98488f1
Hans-Christiaan <hans-christiaan@yoast.com>
2020-07-31 10:10:02 +0200

    Fixes asking for the QA / GitHub changelog.

--- grunt/custom/github-pre-release.js
+++ grunt/custom/github-pre-release.js
@@ -40,12 +40,14 @@ module.exports = function( grunt ) {
 
 			const pluginVersion = grunt.file.readJSON( "package.json" ).yoast.pluginVersion;
 
-			// Open a text editor to get the changelog. For the first RC use the full changelog, for follow-up RCs only use the changes.
+			let initialContent = `Changes compared to ${grunt.config.get( "previousPluginVersion" )}:\n`;
+			// For the first RC use the full changelog, for follow-up RCs only use the changes.
 			if ( pluginVersion.substr( pluginVersion.length - 3 ) === "RC1" ) {
-				const changelog = await getUserInput( { initialContent: grunt.option( "changelog" ) } );
-			} else {
-				const changelog = await getUserInput( { initialContent: "Changes to " + grunt.config.get( 'previousPluginVersion' ) + ":\n" } );
+				initialContent = `Changes in this RC (${pluginVersion}):\n`;
 			}
+
+			// Open a text editor to get the changelog.
+			const changelog = await getUserInput( { initialContent } );
 
 			/* eslint-disable camelcase */
 			const releaseData = {
