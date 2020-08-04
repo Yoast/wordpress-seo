5dc26571bc956a64bc8ef893d0c87a20fb3dde0c
iolandasequino <iolanda@yoast.com>
2020-08-04 11:25:33 +0200

    add extra transition owrds

--- packages/yoastseo/src/researches/arabic/twoPartTransitionWords.js
+++ packages/yoastseo/src/researches/arabic/twoPartTransitionWords.js
@@ -5,5 +5,5 @@
  * @returns {Array}                     The array filled with two-part transition words.
  */
 export default function() {
-	return [ [ "لا", "ولا" ], [ "إما", "أو" ], [ "ربما", "ربما" ], [ "حينئذ", "عندئذ" ] ];
+	return [ [ "لا", "ولا" ], [ "إما", "أو" ], [ "ربما", "ربما" ], [ "حينئذ", "عندئذ" ], [ "إما", "وإما" ], [ "كل من", "و" ] ];
 }
