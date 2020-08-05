45d8bfca8892276fdd0517f63b28cc1d8fc0c382
Igor <igor@yoast.com>
2020-08-05 08:56:01 +0200

    Add postSettingsModal selector

---
+++ js/src/redux/selectors/postSettingsModal.js
@@ -0,0 +1,8 @@
+/**
+ * Gets isOpen from the state.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {boolean} Whether the post settings modal is open or not.
+ */
+export const getPostSettingsModalIsOpen = state => state.postSettingsModal.isOpen;
