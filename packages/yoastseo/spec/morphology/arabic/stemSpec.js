6e538c7965d108e3b740fae18389a74113878fca
FAMarfuaty <aida@yoast.com>
2020-08-18 11:57:45 +0200

    Add more details to the failing specs comments

--- packages/yoastseo/spec/morphology/arabic/stemSpec.js
+++ packages/yoastseo/spec/morphology/arabic/stemSpec.js
@@ -15,35 +15,39 @@ const wordsToStem = [
 	// Three letter words which is in the list of threeLetterRoots
 	[ "رحل", "رحل" ],
 	[ "طمو", "طمو" ],
-	// Three letter word with initial letter ا/ ؤ/ ئ, change the initial letter to أ
+	// Three letter word with initial letter ا/ ؤ/ ئ (yeh_hamza/waw_hamza/alef), change the initial letter to أ (alef_hamza_above)
 	[ "ؤكد", "أكد" ],
-	// Three letter words ending in weak letter و/ي/ا/ى/ء/ئ and the root is in the list of word with the last weak letter or hamza removed.
+	// Three letter words ending in weak letter و/ي/ا/ى/ء/ئ (yeh_hamza/hamza/yeh_maksorah/alef/yeh/waw)
+	// And the root is in the list of word with the last weak letter or hamza removed.
 	// The current stem is قا as قوا is matched by regexRemoveMiddleWeakLetterOrHamza instead of this regex regexRemoveLastWeakLetterOrHamza
 	// [ "قوا", "قوا" ],
 	[ "دعا", "دعا" ],
 	[ "بدء", "بدأ" ],
 	[ "كرى", "كرى" ],
-	// Three letter words ending in weak letter و/ي/ا/ى/ء/ئ and the root is NOT in the list of word with the last weak letter or hamza removed.
-	// The current stem is وفي, as وف is also detected in wordsWithLastYahRemoved list. For words found in the list, ي is added to the root
+	// Three letter words ending in weak letter و/ي/ا/ى/ء/ئ (yeh_hamza/hamza/yeh_maksorah/alef/yeh/waw)
+	// And the root is NOT in the list of word with the last weak letter or hamza removed.
+	// The current stem is وفي, as وف is also detected in wordsWithLastYahRemoved list. For words found in the list, ي (yeh) is added to the root
 	// [ "وفى", "وف" ],
 	[ "باء", "با" ],
-	// The current stem is برا, as بر is also detected in wordsWithLastHamzaRemoved list. For words found in the list, ا is added to the root
+	// The current stem is برا, as بر is also detected in wordsWithLastHamzaRemoved list. For words found in the list, ا (alef) is added to the root
 	// [ "برئ", "بر" ],
-	// The current stem is بلي, as بل is also detected in wordsWithLastYahRemoved list. For words found in the list, ي is added to the root
+	// The current stem is بلي, as بل is also detected in wordsWithLastYahRemoved list. For words found in the list, ي (yeh) is added to the root
 	// [ "بلا", "بل" ],
 	[ "رؤي", "رؤ" ],
-	// Three letter words with و/ي as their second letter and the root is in the exception list after و/ي removal
+	// Three letter words with و/ي (yeh/waw) as their second letter and the root is in the exception list after و/ي (yeh/waw) removal
 	[ "أيد", "أيد" ],
 	[ "أوز", "أوز" ],
-	// Three letter words with و/ي/ا/ئ as their second letter and the root is NOT in the exception list after و/ي removal
+	// Three letter words with و/ي/ا/ئ (yeh_hamza/alef/yeh/waw) as their second letter
+	// And the root is NOT in the exception list after the deletion of و/ي/ا/ئ (yeh_hamza/alef/yeh/waw)
 	[ "موظ", "مظ" ],
 	[ "فئة", "فة" ],
-	// The current stem is بور, as بر is also detected in wordsWithMiddleWawRemoved list. For words found in the list, و is added to the root.
+	// The current stem is بور, as بر is also detected in wordsWithMiddleWawRemoved list. For words found in the list, و (waw) is added to the root.
 	// [ "بار", "بر" ],
-	// Three letter words with ئ/ؤ as their second letter and end in ر/ز/ن, change ئ/ؤ to ا
+	// Three letter words with ئ/ؤ (yeh_hamza/waw_hamza) as their second letter and end in ر/ز/ن (noon/zai/reh),
+	// Change ئ/ؤ (yeh_hamza/waw_hamza) to ا (alef)
 	// The current stem is ين, it is because يئن is matched first by this regex regexRemoveMiddleWeakLetterOrHamza.
 	// [ "يئن", "يان" ],
-	// The current stem is بور, as بر is also detected in wordsWithMiddleWawRemoved list. For words found in the list, و is added to the root.
+	// The current stem is بور, as بر is also detected in wordsWithMiddleWawRemoved list. For words found in the list, و (waw) is added to the root.
 	// [ "بئر", "بار" ],
 	// Three letter words that ends in a shadda, duplicate the second character and the root is in threeLetterRoots list
 	[ "ودّ", "ودد" ],
