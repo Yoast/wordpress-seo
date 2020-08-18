c3a0ff2348fe30a2200b55b87b3fe532c3b285e4
Johanna de Vos <johanna@yoast.com>
2020-08-18 12:59:21 +0200

    Remove unused argument

--- config/grunt/task-config/webpack.js
+++ config/grunt/task-config/webpack.js
@@ -1,6 +1,6 @@
 /* global require */
 const webpackConfig = require( "../../webpack/webpack.config" );
-module.exports = ( grunt ) => {
+module.exports = () => {
 	return {
 		buildDev: () => webpackConfig( { environment: "development", pluginVersion: "<%= pluginVersionSlug %>" } ),
 		buildProd: () => webpackConfig( { environment: "production", pluginVersion: "<%= pluginVersionSlug %>" } ),
