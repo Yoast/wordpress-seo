32d497463dc13031b55db76ba6e9af597f5cbf95
Andy Meerwaldt <andy@yoast.com>
2020-08-19 09:28:41 +0200

    Revert "Remove unused argument"
    This reverts commit c3a0ff2348fe30a2200b55b87b3fe532c3b285e4.

--- config/grunt/task-config/webpack.js
+++ config/grunt/task-config/webpack.js
@@ -1,6 +1,6 @@
 /* global require */
 const webpackConfig = require( "../../webpack/webpack.config" );
-module.exports = () => {
+module.exports = ( grunt ) => {
 	return {
 		buildDev: () => webpackConfig( { environment: "development", pluginVersion: "<%= pluginVersionSlug %>" } ),
 		buildProd: () => webpackConfig( { environment: "production", pluginVersion: "<%= pluginVersionSlug %>" } ),
