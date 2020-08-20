671d23377ad23b40634111780dc2aaad3ad1bb8d
Herre Groen <herre@yoast.com>
2020-08-20 13:02:36 +0200

    Avoid duplicate queries on the posts overview

--- tests/unit/helpers/indexable-helper-test.php
+++ tests/unit/helpers/indexable-helper-test.php
@@ -38,7 +38,7 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_post_indexable( ) {
+	public function test_get_permalink_for_post_indexable() {
 		$indexable              = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_type = 'post';
 
@@ -75,7 +75,7 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_homepage_indexable( ) {
+	public function test_get_permalink_for_homepage_indexable() {
 		$indexable              = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_type = 'home-page';
 
@@ -93,12 +93,12 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_term_indexable( ) {
+	public function test_get_permalink_for_term_indexable() {
 		$indexable              = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_id = 2;
 		$indexable->object_type = 'term';
 
-		$term =  (object) [
+		$term = (object) [
 			'taxonomy' => 'category',
 		];
 
@@ -111,7 +111,7 @@ class Indexable_Helper_Test extends TestCase {
 			->andReturn( false );
 
 		Monkey\Functions\expect( 'get_term_link' )
-			->with( $term, 'category'  )
+			->with( $term, 'category' )
 			->andReturn( 'https://example.org/term' );
 
 		$this->assertEquals(
@@ -125,7 +125,7 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_term_indexable_term_not_found( ) {
+	public function test_get_permalink_for_term_indexable_term_not_found() {
 		$indexable              = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_id = 2;
 		$indexable->object_type = 'term';
@@ -145,12 +145,12 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_term_indexable_term_is_wp_error( ) {
+	public function test_get_permalink_for_term_indexable_term_is_wp_error() {
 		$indexable              = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_id = 2;
 		$indexable->object_type = 'term';
 
-		$term =  (object) [
+		$term = (object) [
 			'taxonomy' => 'category',
 		];
 
@@ -172,7 +172,7 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_search_page_indexable( ) {
+	public function test_get_permalink_for_search_page_indexable() {
 		$indexable                  = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_type     = 'system-page';
 		$indexable->object_sub_type = 'search-page';
@@ -191,7 +191,7 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_post_type_archive_indexable( ) {
+	public function test_get_permalink_for_post_type_archive_indexable() {
 		$indexable                  = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_type     = 'post-type-archive';
 		$indexable->object_sub_type = 'post-type';
@@ -211,7 +211,7 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_user_indexable( ) {
+	public function test_get_permalink_for_user_indexable() {
 		$indexable              = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_type = 'user';
 		$indexable->object_id   = 1;
@@ -231,12 +231,66 @@ class Indexable_Helper_Test extends TestCase {
 	 *
 	 * @covers ::get_permalink_for_indexable
 	 */
-	public function test_get_permalink_for_unknown_type_indexable( ) {
+	public function test_get_permalink_for_unknown_type_indexable() {
 		$indexable              = Mockery::mock( Indexable_Mock::class );
 		$indexable->object_type = 'unknown';
 
 		$this->assertNull(
 			$this->instance->get_permalink_for_indexable( $indexable )
 		);
+	}
+
+	/**
+	 * Tests the get_page_type_for_indexable_provider function.
+	 *
+	 * @param int    $object_type        The object type.
+	 * @param int    $object_sub_type    The object sub type.
+	 * @param bool   $is_front_page      Whether or not the indexable is the front page.
+	 * @param bool   $is_posts_page      Whether or not the indexable is the posts page.
+	 * @param string $expected_page_type The expected page type.
+	 *
+	 * @covers ::get_page_type_for_indexable
+	 * @dataProvider get_page_type_for_indexable_provider
+	 */
+	public function test_get_page_type_for_indexable( $object_type, $object_sub_type, $is_front_page, $is_posts_page, $expected_page_type ) {
+		if ( $object_type === 'post' ) {
+			Monkey\Functions\expect( 'get_option' )
+				->once()
+				->with( 'page_on_front' )
+				->andReturn( ( $is_front_page ) ? 1 : 0 );
+
+			if ( ! $is_front_page ) {
+				Monkey\Functions\expect( 'get_option' )
+					->once()
+					->with( 'page_for_posts' )
+					->andReturn( ( $is_posts_page ) ? 1 : 0 );
+			}
+		}
+
+		$indexable                  = Mockery::mock( Indexable_Mock::class );
+		$indexable->object_id       = 1;
+		$indexable->object_type     = $object_type;
+		$indexable->object_sub_type = $object_sub_type;
+
+		$this->assertEquals( $expected_page_type, $this->instance->get_page_type_for_indexable( $indexable ) );
+	}
+
+	/**
+	 * Data provider for the test_get_page_type_for_indexable_provider function.
+	 *
+	 * @return array The test data.
+	 */
+	public function get_page_type_for_indexable_provider() {
+		return [
+			[ 'post', 'page', true, false, 'Static_Home_Page' ],
+			[ 'post', 'page', false, true, 'Static_Posts_Page' ],
+			[ 'post', 'post', false, false, 'Post_Type' ],
+			[ 'term', 'tag', false, false, 'Term_Archive' ],
+			[ 'user', null, false, false, 'Author_Archive' ],
+			[ 'home-page', null, false, false, 'Home_Page' ],
+			[ 'post-type-archive', 'post', false, false, 'Post_Type_Archive' ],
+			[ 'system-page', 'search-result', false, false, 'Search_Result_Page' ],
+			[ 'system-page', '404', false, false, 'Error_Page' ],
+		];
 	}
 }
