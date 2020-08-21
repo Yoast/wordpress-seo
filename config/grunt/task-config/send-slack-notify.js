1124c928d956bc5a921fb0200c9c5f7ddde66135
arnoud thörig <arnoud@yoast.com>
2020-08-21 13:54:18 +0200

    fixed eslint errors

--- config/grunt/task-config/send-slack-notify.js
+++ config/grunt/task-config/send-slack-notify.js
@@ -8,7 +8,7 @@ module.exports =  {
 			enable: true,
 		},
 	},
-	'add-git-issue-slack-message': {
+	"add-git-issue-slack-message": {
 		options: {
 			slackToken: process.env.SLACK_DEV_PLUGIN_CHANNEL_TOKEN || process.env.SLACK_TEST_CHANNEL_TOKEN,
 			message: "<%= slackMessage %>",
