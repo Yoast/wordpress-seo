202cbcd61d3c23741114f4b9a5bc9e28fc60faa4
Bram <bintzandt@users.noreply.github.com>
2020-08-24 10:43:57 +0200

    Add redux to the Schema-tab.

---
+++ js/src/redux/reducers/schemaTab.js
@@ -0,0 +1,42 @@
+import {
+	GET_SCHEMA_PAGE_DATA,
+	GET_SCHEMA_ARTICLE_DATA,
+	SET_PAGE_TYPE,
+	SET_ARTICLE_TYPE,
+} from "../actions/schemaTab";
+
+const initialState = {
+	pageType: "",
+	defaultPageType: "",
+	articleType: "",
+	defaultArticleType: "",
+	isLoading: true,
+};
+
+/**
+ * A reducer for the SchemaTab object.
+ *
+ * @param {Object} state The current state of the object.
+ * @param {Object} action The current action received.
+ *
+ * @returns {Object} The updated SchemaTab results object.
+ */
+const schemaTabReducer = ( state = initialState, action ) => {
+	switch ( action.type ) {
+		case SET_PAGE_TYPE: return { ...state, pageType: action.pageType };
+		case SET_ARTICLE_TYPE: return { ...state, articleType: action.articleType };
+		case GET_SCHEMA_PAGE_DATA: return {
+			...state,
+			pageType: action.pageType,
+			defaultPageType: action.defaultPageType,
+			isLoading: false,
+		};
+		case GET_SCHEMA_ARTICLE_DATA: return {
+			...state,
+			articleType: action.articleType,
+			defaultArticleType: action.defaultArticleType,
+		};
+	}
+};
+
+export default schemaTabReducer;
---
+++ js/src/redux/selectors/schemaTab.js
@@ -0,0 +1,46 @@
+import { get } from "lodash";
+
+/**
+ * Gets the defaultPageType from the state.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {String} Default page type.
+ */
+export const getDefaultPageType = state => get( state, "schemaTab.defaultPageType", "" );
+
+/**
+ * Gets the pageType from the state.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {String} Page type.
+ */
+export const getPageType = state => get( state, "schemaTab.pageType", "" );
+
+/**
+ * Gets the defaultArticleType from the state.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {String} Default article type.
+ */
+export const getDefaultArticleType = state => get( state, "schemaTab.defaultArticleType", "" );
+
+/**
+ * Gets the articleType from the state.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {String} Article type.
+ */
+export const getArticleType = state => get( state, "schemaTab.articleType", "" );
+
+/**
+ * Gets the isLoading prop from the state.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {bool} Whether the schema tab is loading.
+ */
+export const getIsLoading = state => get( state, "schemaTab.isLoading", true );
