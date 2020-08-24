2d21669713e9178d1db79746d68935ce6d38f44d
Hans-Christiaan <hans-christiaan@yoast.com>
2020-08-24 13:29:25 +0200

    Changed documentation of filter argument to use param instead of api tag.

--- src/context/meta-tags-context.php
+++ src/context/meta-tags-context.php
@@ -442,9 +442,8 @@ class Meta_Tags_Context extends Abstract_Presentation {
 		/**
 		 * Filter: 'wpseo_schema_article_type' - Allow changing the Article type.
 		 *
-		 * @param Indexable $indexable The indexable.
-		 *
-		 * @api string|array $type The Article type.
+		 * @param string|string[] $type      The Article type.
+		 * @param Indexable       $indexable The indexable.
 		 */
 		return \apply_filters( 'wpseo_schema_article_type', $type, $this->indexable );
 	}
