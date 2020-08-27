b5d6d757a6bc437a97dd5ae2ab296977df711de1
Andrea Fercia <a.fercia@gmail.com>
2020-08-27 10:21:27 +0200

    Backfill wp_get_environment_type.

--- inc/wpseo-functions.php
+++ inc/wpseo-functions.php
@@ -250,3 +250,72 @@ function wpseo_get_capabilities() {
 	}
 	return WPSEO_Capability_Manager_Factory::get()->get_capabilities();
 }
+
+if ( ! function_exists( 'wp_get_environment_type' ) ) {
+	/**
+	 * Retrieves the current environment type.
+	 *
+	 * The type can be set via the `WP_ENVIRONMENT_TYPE` global system variable,
+	 * or a constant of the same name.
+	 *
+	 * Possible values include 'local', 'development', 'staging', 'production'.
+	 * If not set, the type defaults to 'production'.
+	 *
+	 * Backfill for `wp_get_environment_type()` introduced in WordPress 5.5.
+	 *
+	 * @return string The current environment type.
+	 */
+	function wp_get_environment_type() {
+		static $current_env = '';
+
+		if ( $current_env ) {
+			return $current_env;
+		}
+
+		// phpcs:disable Generic.Arrays.DisallowLongArraySyntax.Found -- Reason: This is copied from WP core.
+		$wp_environments = array(
+			'local',
+			'development',
+			'staging',
+			'production',
+		);
+		// phpcs:enable Generic.Arrays.DisallowLongArraySyntax.Found
+
+		// Check if the environment variable has been set, if `getenv` is available on the system.
+		if ( function_exists( 'getenv' ) ) {
+			$has_env = getenv( 'WP_ENVIRONMENT_TYPES' );
+			// phpcs:disable Generic.ControlStructures.DisallowYodaConditions.Found -- Reason: This is copied from WP core.
+			if ( false !== $has_env ) {
+				$wp_environments = explode( ',', $has_env );
+			}
+			// phpcs:enable Generic.ControlStructures.DisallowYodaConditions.Found
+		}
+
+		// Fetch the environment types from a constant, this overrides the global system variable.
+		if ( defined( 'WP_ENVIRONMENT_TYPES' ) ) {
+			$wp_environments = WP_ENVIRONMENT_TYPES;
+		}
+
+		// Check if the environment variable has been set, if `getenv` is available on the system.
+		if ( function_exists( 'getenv' ) ) {
+			$has_env = getenv( 'WP_ENVIRONMENT_TYPE' );
+			// phpcs:disable Generic.ControlStructures.DisallowYodaConditions.Found -- Reason: This is copied from WP core.
+			if ( false !== $has_env ) {
+				$current_env = $has_env;
+			}
+			// phpcs:enable Generic.ControlStructures.DisallowYodaConditions.Found
+		}
+
+		// Fetch the environment from a constant, this overrides the global system variable.
+		if ( defined( 'WP_ENVIRONMENT_TYPE' ) ) {
+			$current_env = WP_ENVIRONMENT_TYPE;
+		}
+
+		// Make sure the environment is an allowed one, and not accidentally set to an invalid value.
+		if ( ! in_array( $current_env, $wp_environments, true ) ) {
+			$current_env = 'production';
+		}
+
+		return $current_env;
+	}
+}
