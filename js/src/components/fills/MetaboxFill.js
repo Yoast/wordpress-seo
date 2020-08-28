535acf63497fe79dcfbc781694bc12930d318348
Maarten Leenders <maarten@yoast.com>
2020-08-29 01:05:49 +0200

    Use providers around slot rather than around fill

--- js/src/components/fills/MetaboxFill.js
+++ js/src/components/fills/MetaboxFill.js
@@ -13,7 +13,6 @@ import ReadabilityAnalysis from "../contentAnalysis/ReadabilityAnalysis";
 import SeoAnalysis from "../contentAnalysis/SeoAnalysis";
 import MetaboxCollapsible from "../MetaboxCollapsible";
 import SidebarItem from "../SidebarItem";
-import TopLevelProviders from "../TopLevelProviders";
 import AdvancedSettings from "../../containers/AdvancedSettings";
 import SocialMetadataPortal from "../portals/SocialMetadataPortal";
 import SchemaTabContainer from "../../containers/SchemaTab";
@@ -30,53 +29,45 @@ import SchemaTabContainer from "../../containers/SchemaTab";
 export default function MetaboxFill( { settings, store, theme } ) {
 	return (
 		<Fill name="YoastMetabox">
-			<TopLevelProviders
-				store={ store }
-				theme={ theme }
-				location={ "metabox" }
+			<SidebarItem renderPriority={ 1 }>
+				<Warning />
+			</SidebarItem>
+			{ settings.isKeywordAnalysisActive && <SidebarItem renderPriority={ 8 }>
+				<KeywordInput />
+			</SidebarItem> }
+			<SidebarItem renderPriority={ 9 }>
+				<MetaboxCollapsible
+					id={ "yoast-snippet-editor-metabox" }
+					title={ __( "Google preview", "wordpress-seo" ) } initialIsOpen={ true }
+				>
+					<SnippetEditor hasPaperStyle={ false } />
+				</MetaboxCollapsible>
+			</SidebarItem>
+			{ settings.isContentAnalysisActive && <SidebarItem renderPriority={ 10 }>
+				<ReadabilityAnalysis />
+			</SidebarItem> }
+			{ settings.isKeywordAnalysisActive && <SidebarItem renderPriority={ 20 }>
+				<SeoAnalysis
+					shouldUpsell={ settings.shouldUpsell }
+					shouldUpsellWordFormRecognition={ settings.isWordFormRecognitionActive }
+				/>
+			</SidebarItem> }
+			{ settings.isCornerstoneActive && <SidebarItem renderPriority={ 30 }>
+				<CollapsibleCornerstone />
+			</SidebarItem> }
+			{ settings.displayAdvancedTab && <SidebarItem renderPriority={ 40 }>
+				<MetaboxCollapsible id={ "collapsible-advanced-settings" } title={ __( "Advanced", "wordpress-seo" ) }>
+					<AdvancedSettings />
+				</MetaboxCollapsible>
+			</SidebarItem> }
+			{ settings.displaySchemaSettings && <SidebarItem renderPriority={ 50 }>
+				<SchemaTabContainer />
+			</SidebarItem> }
+			<Fragment
+				renderPriority={ -1 }
 			>
-				<Fragment>
-					<SidebarItem renderPriority={ 1 }>
-						<Warning />
-					</SidebarItem>
-					{ settings.isKeywordAnalysisActive && <SidebarItem renderPriority={ 8 }>
-						<KeywordInput />
-					</SidebarItem> }
-					<SidebarItem renderPriority={ 9 }>
-						<MetaboxCollapsible
-							id={ "yoast-snippet-editor-metabox" }
-							title={ __( "Google preview", "wordpress-seo" ) } initialIsOpen={ true }
-						>
-							<SnippetEditor hasPaperStyle={ false } />
-						</MetaboxCollapsible>
-					</SidebarItem>
-					{ settings.isContentAnalysisActive && <SidebarItem renderPriority={ 10 }>
-						<ReadabilityAnalysis />
-					</SidebarItem> }
-					{ settings.isKeywordAnalysisActive && <SidebarItem renderPriority={ 20 }>
-						<SeoAnalysis
-							shouldUpsell={ settings.shouldUpsell }
-							shouldUpsellWordFormRecognition={ settings.isWordFormRecognitionActive }
-						/>
-					</SidebarItem> }
-					{ settings.isCornerstoneActive && <SidebarItem renderPriority={ 30 }>
-						<CollapsibleCornerstone />
-					</SidebarItem> }
-					{ settings.displayAdvancedTab && <SidebarItem renderPriority={ 40 }>
-						<MetaboxCollapsible id={ "collapsible-advanced-settings" } title={ __( "Advanced", "wordpress-seo" ) }>
-							<AdvancedSettings />
-						</MetaboxCollapsible>
-					</SidebarItem> }
-					{ settings.displaySchemaSettings && <SidebarItem renderPriority={ 50 }>
-						<SchemaTabContainer />
-					</SidebarItem> }
-					<Fragment
-						renderPriority={ -1 }
-					>
-						<SocialMetadataPortal target="wpseo-section-social" />
-					</Fragment>
-				</Fragment>
-			</TopLevelProviders>
+				<SocialMetadataPortal target="wpseo-section-social" />
+			</Fragment>
 		</Fill>
 	);
 }
