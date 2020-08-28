b98cfd104bc115012d72a1b22624a1ea36662be2
jrfnl <jrfnl@users.noreply.github.com>
2020-08-28 13:46:11 +0200

    PHPCS: fix incorrect errorcode reference in ignore annotation

--- tests/unit/TestCase.php
+++ tests/unit/TestCase.php
@@ -74,7 +74,7 @@ abstract class TestCase extends BaseTestCase {
 				},
 				'wp_strip_all_tags'    => function( $string, $remove_breaks = false ) {
 					$string = \preg_replace( '@<(script|style)[^>]*?>.*?</\\1>@si', '', $string );
-					// phpcs:ignore WordPress.WP.AlternativeFunctions.strip_tags -- We are stubbing the wp_strip_all_tags.
+					// phpcs:ignore WordPress.WP.AlternativeFunctions.strip_tags_strip_tags -- We are stubbing the wp_strip_all_tags.
 					$string = \strip_tags( $string );
 
 					if ( $remove_breaks ) {
