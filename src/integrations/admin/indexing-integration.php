22bb7b3b4bdc8e71bc028f640d4970f8dedcf87c
Hans-Christiaan <hans-christiaan@yoast.com>
2020-09-01 11:03:48 +0200

    Added indexables indexing actions to indexing integration.

--- src/integrations/admin/indexing-integration.php
+++ src/integrations/admin/indexing-integration.php
@@ -2,6 +2,11 @@
 
 namespace Yoast\WP\SEO\Integrations\Admin;
 
+use Yoast\WP\SEO\Actions\Indexation\Indexable_Complete_Indexation_Action;
+use Yoast\WP\SEO\Actions\Indexation\Indexable_General_Indexation_Action;
+use Yoast\WP\SEO\Actions\Indexation\Indexable_Post_Indexation_Action;
+use Yoast\WP\SEO\Actions\Indexation\Indexable_Post_Type_Archive_Indexation_Action;
+use Yoast\WP\SEO\Actions\Indexation\Indexable_Term_Indexation_Action;
 use Yoast\WP\SEO\Conditionals\Admin_Conditional;
 use Yoast\WP\SEO\Conditionals\Migrations_Conditional;
 use Yoast\WP\SEO\Conditionals\Yoast_Admin_And_Dashboard_Conditional;
@@ -11,11 +16,46 @@ use Yoast\WP\SEO\Presenters\Admin\Indexing_List_Item_Presenter;
 class Indexing_Integration implements Integration_Interface {
 
 	/**
-	 * @inheritDoc
+	 * The post indexation action.
+	 *
+	 * @var Indexable_Post_Indexation_Action
 	 */
-	public function register_hooks() {
-		\add_action( 'wpseo_tools_overview_list_items', [ $this, 'render_indexing_list_item' ], 10 );
-	}
+	protected $post_indexation;
+
+	/**
+	 * The term indexation action.
+	 *
+	 * @var Indexable_Term_Indexation_Action
+	 */
+	protected $term_indexation;
+
+	/**
+	 * The post type archive indexation action.
+	 *
+	 * @var Indexable_Post_Type_Archive_Indexation_Action
+	 */
+	protected $post_type_archive_indexation;
+
+	/**
+	 * Represents the general indexation.
+	 *
+	 * @var Indexable_General_Indexation_Action
+	 */
+	protected $general_indexation;
+
+	/**
+	 * Represented the indexation completed action.
+	 *
+	 * @var Indexable_Complete_Indexation_Action
+	 */
+	protected $complete_indexation_action;
+
+	/**
+	 * The total amount of unindexed objects.
+	 *
+	 * @var int
+	 */
+	protected $total_unindexed;
 
 	/**
 	 * @inheritDoc
@@ -28,6 +68,45 @@ class Indexing_Integration implements Integration_Interface {
 		];
 	}
 
+	public function __construct(
+		Indexable_Post_Indexation_Action $post_indexation,
+		Indexable_Term_Indexation_Action $term_indexation,
+		Indexable_Post_Type_Archive_Indexation_Action $post_type_archive_indexation,
+		Indexable_General_Indexation_Action $general_indexation,
+		Indexable_Complete_Indexation_Action $complete_indexation_action
+	) {
+		$this->post_indexation              = $post_indexation;
+		$this->term_indexation              = $term_indexation;
+		$this->post_type_archive_indexation = $post_type_archive_indexation;
+		$this->general_indexation           = $general_indexation;
+		$this->complete_indexation_action   = $complete_indexation_action;
+	}
+
+	/**
+	 * @inheritDoc
+	 */
+	public function register_hooks() {
+		\add_action( 'wpseo_tools_overview_list_items', [ $this, 'render_indexing_list_item' ], 10 );
+		\add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts' ], 10 );
+	}
+
+	/**
+	 * Enqueues the required scripts.
+	 *
+	 * @return void
+	 */
+	public function enqueue_scripts() {
+		/*
+		 * We aren't able to determine whether or not anything needs to happen at register_hooks,
+		 * as post types aren't registered yet. So we do most of our add_action calls here.
+		 */
+		if ( $this->get_total_unindexed() === 0 ) {
+			$this->complete_indexation_action->complete();
+
+			return;
+		}
+	}
+
 	/**
 	 * Renders the indexation list item.
 	 *
@@ -37,5 +116,21 @@ class Indexing_Integration implements Integration_Interface {
 		if ( \current_user_can( 'manage_options' ) ) {
 			echo new Indexing_List_Item_Presenter();
 		}
+	}
+
+	/**
+	 * Returns the total number of unindexed objects.
+	 *
+	 * @return int
+	 */
+	public function get_total_unindexed() {
+		if ( \is_null( $this->total_unindexed ) ) {
+			$this->total_unindexed  = $this->post_indexation->get_total_unindexed();
+			$this->total_unindexed += $this->term_indexation->get_total_unindexed();
+			$this->total_unindexed += $this->general_indexation->get_total_unindexed();
+			$this->total_unindexed += $this->post_type_archive_indexation->get_total_unindexed();
+		}
+
+		return $this->total_unindexed;
 	}
 }
