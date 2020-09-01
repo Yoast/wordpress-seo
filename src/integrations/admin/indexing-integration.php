93e1cb66d460da4fc596933f582db9f783bf14d1
Hans-Christiaan <hans-christiaan@yoast.com>
2020-09-01 11:24:04 +0200

    Indexing integration now enqueues JavaScript and styling.

--- src/integrations/admin/indexing-integration.php
+++ src/integrations/admin/indexing-integration.php
@@ -2,6 +2,7 @@
 
 namespace Yoast\WP\SEO\Integrations\Admin;
 
+use WPSEO_Admin_Asset_Manager;
 use Yoast\WP\SEO\Actions\Indexation\Indexable_Complete_Indexation_Action;
 use Yoast\WP\SEO\Actions\Indexation\Indexable_General_Indexation_Action;
 use Yoast\WP\SEO\Actions\Indexation\Indexable_Post_Indexation_Action;
@@ -14,6 +15,8 @@ use Yoast\WP\SEO\Conditionals\Migrations_Conditional;
 use Yoast\WP\SEO\Conditionals\Yoast_Admin_And_Dashboard_Conditional;
 use Yoast\WP\SEO\Integrations\Integration_Interface;
 use Yoast\WP\SEO\Presenters\Admin\Indexing_List_Item_Presenter;
+use Yoast\WP\SEO\Routes\Indexable_Indexation_Route;
+use Yoast\WP\SEO\Routes\Link_Indexing_Route;
 
 class Indexing_Integration implements Integration_Interface {
 
@@ -74,6 +77,13 @@ class Indexing_Integration implements Integration_Interface {
 	protected $total_unindexed;
 
 	/**
+	 * Represents the admin asset manager.
+	 *
+	 * @var WPSEO_Admin_Asset_Manager
+	 */
+	protected $asset_manager;
+
+	/**
 	 * @inheritDoc
 	 */
 	public static function get_conditionals() {
@@ -91,7 +101,8 @@ class Indexing_Integration implements Integration_Interface {
 		Indexable_General_Indexation_Action $general_indexation,
 		Indexable_Complete_Indexation_Action $complete_indexation_action,
 		Post_Link_Indexing_Action $post_link_indexing_action,
-		Term_Link_Indexing_Action $term_link_indexing_action
+		Term_Link_Indexing_Action $term_link_indexing_action,
+		WPSEO_Admin_Asset_Manager $asset_manager
 	) {
 		$this->post_indexation              = $post_indexation;
 		$this->term_indexation              = $term_indexation;
@@ -100,6 +111,7 @@ class Indexing_Integration implements Integration_Interface {
 		$this->complete_indexation_action   = $complete_indexation_action;
 		$this->post_link_indexing_action    = $post_link_indexing_action;
 		$this->term_link_indexing_action    = $term_link_indexing_action;
+		$this->asset_manager                = $asset_manager;
 	}
 
 	/**
@@ -125,6 +137,29 @@ class Indexing_Integration implements Integration_Interface {
 
 			return;
 		}
+
+		$this->asset_manager->enqueue_script( 'indexation' );
+		$this->asset_manager->enqueue_style( 'admin-css' );
+
+		$data = [
+			'amount'  => $this->get_total_unindexed(),
+			'restApi' => [
+				'root'      => \esc_url_raw( \rest_url() ),
+				'endpoints' => [
+					'prepare'    => Indexable_Indexation_Route::FULL_PREPARE_ROUTE,
+					'posts'      => Indexable_Indexation_Route::FULL_POSTS_ROUTE,
+					'terms'      => Indexable_Indexation_Route::FULL_TERMS_ROUTE,
+					'archives'   => Indexable_Indexation_Route::FULL_POST_TYPE_ARCHIVES_ROUTE,
+					'general'    => Indexable_Indexation_Route::FULL_GENERAL_ROUTE,
+					'link-posts' => Link_Indexing_Route::FULL_POSTS_ROUTE,
+					'link-terms' => Link_Indexing_Route::FULL_TERMS_ROUTE,
+					'complete'   => Indexable_Indexation_Route::FULL_COMPLETE_ROUTE,
+				],
+				'nonce'     => \wp_create_nonce( 'wp_rest' ),
+			],
+		];
+
+		\wp_localize_script( WPSEO_Admin_Asset_Manager::PREFIX . 'indexation', 'yoastIndexingData', $data );
 	}
 
 	/**
@@ -145,7 +180,7 @@ class Indexing_Integration implements Integration_Interface {
 	 */
 	public function get_total_unindexed() {
 		if ( \is_null( $this->total_unindexed ) ) {
-			$this->total_unindexed  = $this->post_indexation->get_total_unindexed();
+			$this->total_unindexed = $this->post_indexation->get_total_unindexed();
 			$this->total_unindexed += $this->term_indexation->get_total_unindexed();
 			$this->total_unindexed += $this->general_indexation->get_total_unindexed();
 			$this->total_unindexed += $this->post_type_archive_indexation->get_total_unindexed();
