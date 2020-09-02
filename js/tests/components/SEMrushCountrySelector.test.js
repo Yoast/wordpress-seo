41ef16e46cd007017560b89f89d72b754c4e4e09
Jesser Hulshof <jesserh16@gmail.com>
2020-09-02 14:16:30 +0200

    CS test cleanup

--- js/tests/components/SEMrushCountrySelector.test.js
+++ js/tests/components/SEMrushCountrySelector.test.js
@@ -1,29 +1,34 @@
-import { mount, shallow } from "enzyme";
+import { shallow } from "enzyme";
 
 import SEMrushCountrySelector
 	from "../../../js/src/components/modals/SEMrushCountrySelector";
 import React from "react";
+import { noop } from "lodash";
 
-window.jQuery = () => ( { select2: () => {}, on: () => {} } );
+window.jQuery = () => ( {
+	select2: () => {
+	}, on: () => {
+	},
+} );
 
 describe( "SEMrushCountrySelector", () => {
 	it( "successfully calls the associated newRequest function when the change country button is clicked", () => {
 		const onClickMock = jest.fn();
-		const component = shallow( <SEMrushCountrySelector setCountry={() => {console.log("setCountry")}} newRequest={onClickMock}
-		setNoResultsFoundnewRequest={() => {}} setRequestSucceeded={() => {}} setRequestLimitReached={() => {}}
-		setRequestFailed={() => {}} setNoResultsFound={() => {}} /> );
+		const component = shallow( <SEMrushCountrySelector
+			setCountry={ noop } newRequest={ onClickMock } setNoResultsFoundnewRequest={ noop }
+			setRequestSucceeded={ noop } setRequestLimitReached={ noop } setRequestFailed={ noop } setNoResultsFound={ noop }
+		/> );
 
-		component.find("button").simulate("click");
+		component.find( "button" ).simulate( "click" );
 
 		expect( onClickMock ).toHaveBeenCalled();
 	} );
 	it( "validates the amount of options loaded within the select", () => {
-		const onClickMock = jest.fn();
-		const component = shallow( <SEMrushCountrySelector setCountry={() => {console.log("setCountry")}} newRequest={onClickMock}
-		setNoResultsFoundnewRequest={() => {}} setRequestSucceeded={() => {}} setRequestLimitReached={() => {}}
-		setRequestFailed={() => {}} setNoResultsFound={() => {}} /> );
+		const component = shallow( <SEMrushCountrySelector
+			setCountry={ noop } newRequest={ noop } setNoResultsFoundnewRequest={ noop }
+			setRequestSucceeded={ noop } setRequestLimitReached={ noop } setRequestFailed={ noop } setNoResultsFound={ noop }
+		/> );
 
-		expect( component.find("select").getElement().props.children.length ).toEqual( 117 );
+		expect( component.find( "select" ).getElement().props.children.length ).toEqual( 117 );
 	} );
-
 } );
