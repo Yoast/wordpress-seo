ef2974fc0f3e810dc4f53e8ca28403c0dd0798b0
Johanna de Vos <johanna@yoast.com>
2020-09-11 11:20:15 +0200

    Disable the indexing button when the indexables should not be indexed

--- src/integrations/admin/indexation-integration.php
+++ src/integrations/admin/indexation-integration.php
@@ -245,7 +245,7 @@ class Indexation_Integration implements Integration_Interface {
 	 */
 	public function render_indexation_list_item() {
 		if ( \current_user_can( 'manage_options' ) ) {
-			echo new Indexation_List_Item_Presenter( $this->get_total_unindexed() );
+			echo new Indexation_List_Item_Presenter( $this->get_total_unindexed(), $this->indexable_helper );
 		}
 	}
 
