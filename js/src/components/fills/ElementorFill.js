080e74c65ea9bb7ad76c6963e383e5b0ee65e634
Bram in 't Zandt <bintzandt@users.noreply.github.com>
2020-09-11 11:53:16 +0200

    Fix ElementorFill after merge

--- js/src/components/fills/ElementorFill.js
+++ js/src/components/fills/ElementorFill.js
@@ -1,5 +1,9 @@
+// External dependencies.
 import { Fill } from "@wordpress/components";
+import { __ } from "@wordpress/i18n";
 import PropTypes from "prop-types";
+
+// Internal dependencies.
 import CollapsibleCornerstone from "../../containers/CollapsibleCornerstone";
 import Warning from "../../containers/Warning";
 import KeywordInput from "../contentAnalysis/KeywordInput";
@@ -9,7 +13,9 @@ import SidebarItem from "../SidebarItem";
 import GooglePreviewModal from "../modals/editorModals/GooglePreviewModal";
 import TwitterPreviewModal from "../modals/editorModals/TwitterPreviewModal";
 import FacebookPreviewModal from "../modals/editorModals/FacebookPreviewModal";
-
+import SidebarCollapsible from "../SidebarCollapsible";
+import SchemaTabContainer from "../../containers/SchemaTab";
+import AdvancedSettings from "../../containers/AdvancedSettings";
 
 /**
  * Creates the ElementorFill component.
