950504a75e97335dff4aed8d443ffe3bdb8c2bca
Arnoud Thörig <39429596+noud-github@users.noreply.github.com>
2020-09-14 09:28:56 +0200

    Update config/grunt/custom-tasks/deploy-to-wordpress.js
    Co-authored-by: Jip <jhp.moors@gmail.com>

--- config/grunt/custom-tasks/deploy-to-wordpress.js
+++ config/grunt/custom-tasks/deploy-to-wordpress.js
@@ -13,7 +13,7 @@ module.exports = function( grunt ) {
 			const options = this.options({
                 enable: false,
                 branch: '',
-            });
+			});
 
 
             if ( options.enable ) {
