ec3aac9f4157320c5c948394416a7f08c448a42c
Joost Boomkamp <joostboomkamp@yoast.com>
2020-09-21 14:32:17 +0200

    fix cs errors

--- config/dependency-injection/interface-injection-pass.php
+++ config/dependency-injection/interface-injection-pass.php
@@ -28,6 +28,7 @@ class Interface_Injection_Pass implements CompilerPassInterface {
 		 * We need reflection methods introduced in PHP 7.1 to run this compiler pass.
 		 */
 		if ( PHP_VERSION_ID < 70100 ) {
+			echo 'Cannot use interface array injection; PHP version is ' . PHP_VERSION . ', but v7.1 is required.';
 			return;
 		}
 
@@ -38,65 +39,26 @@ class Interface_Injection_Pass implements CompilerPassInterface {
 
 			/**
 			 * We only want to execute for classes that match:
-			 * - the class must be defined in our projects. ( e.g. don't process Wordpress code )
+			 * - the class must be defined in our projects. ( e.g. don't process WordPress code )
 			 * - the class must have a constructor with a typed splat argument, e.g.
+			 *
 			 * @example public function __construct( Fruit ...$allFruit )
 			 */
 			foreach ( $definitions as $definition ) {
 
-				// The goal is to limit the process to classes from our project.
-				$definition_class = $definition->getClass();
-				if ( ! \class_exists( $definition_class ) ) {
-					// Do not process classes outside our project.
-					continue;
-				}
-
-				// Apply reflection to the class definition, to retrieve the constructor.
-				$class_reflection_info = new ReflectionClass( $definition_class );
-				$class_constructor     = $class_reflection_info->getConstructor();
-				if ( ! $class_constructor ) {
-					/*
-					 * The class has no constructor (i.e. it only has a default constructor with no arguments).
-					 * We do not need to process this class.
-					 */
-					continue;
-				}
-
-				// Get the constructor's last argument.
-				// We only care about the last constructor argument; the '...' splat operator is always last.
-				$constructor_arguments = $class_constructor->getParameters();
-				$splat_argument        = end( $constructor_arguments );
-
-				if ( ! $splat_argument || // No arguments at all
-					 ! $splat_argument->isVariadic() ) { // isVariadic means "is it a 'splat' argument".
-					// Only continue for splat arguments.
-					continue;
-				}
-
-				// Needs PHP 7.0.
-				$splat_argument_type = $splat_argument->getType();
-				// Analyse the type of the splat argument. Needs PHP 7.1.
-				if ( ! is_a( $splat_argument_type, ReflectionNamedType::class ) ) {
-					// If the argument is not a class, we cannot inject it as a dependency.
+				$definition_class = $this->get_variadic_constructor( $definition );
+				if ( $definition_class == null ) {
 					continue;
 				}
 
 				// We've found a class that has a splat operator in its constructor!
-				echo( "Found a constructor with a splat argument for class " . $definition_class );
+				echo( 'Found a constructor with a splat argument for class ' . $definition_class->target_class_name );
 				// This is the type of the splat argument:
-				$splat_argument_classname = $splat_argument_type->getName();
-				echo( "Going to inject all implementations of " . $splat_argument_classname );
+				$splat_argument_classname = $definition_class->splat_argument_type;
+				echo( 'Going to inject all implementations of ' . $splat_argument_classname );
 
 				// Find all subclasses of the requested type in our DI definition
-				$subclasses_of_splat_type = \array_filter( $definitions,
-					function( $other_definition ) use ( $splat_argument_classname, $definition ) {
-						if ( $other_definition === $definition ) {
-							// Never inject itself to itself
-							return false;
-						}
-
-						return \is_subclass_of( $other_definition->getClass(), $splat_argument_classname );
-					} );
+				$subclasses_of_splat_type = get_registered_subclasses( $definition_class, $definitions );
 
 				/*
 				 * The constructor needs to be called with a comma separated enumeration of objects.
@@ -105,9 +67,9 @@ class Interface_Injection_Pass implements CompilerPassInterface {
 				 * We have to start this array at the position of the splat argument, and inject the next
 				 * matching type on the next position, and so on, until all matching types have been injected.
 				 */
-				$argument_index = $splat_argument->getPosition();
+				$argument_index = $definition_class->splat_argument->getPosition();
 				foreach ( $subclasses_of_splat_type as $subclass ) {
-					echo ( "Injecting " . $subclass );
+					echo ( 'Injecting ' . $subclass );
 					$definition->setArgument( $argument_index, new Reference( $subclass->getClass() ) );
 
 					$argument_index += 1;
@@ -117,4 +79,100 @@ class Interface_Injection_Pass implements CompilerPassInterface {
 			var_dump( $e );
 		}
 	}
+
+	/**
+	 * Analyses a class definition and looks for a constructor with a typed variadic argument
+	 *
+	 * @param Definition $definition The class definition we are inspecting.
+	 *
+	 * @return Constructor_Details|null
+	 *
+	 * @throws \ReflectionException
+	 * @example If we had a class Fruit_Basket with this constructor:
+	 * __construct(Fruit ...$all_our_fruit), then this method would find it.
+	 */
+	function get_variadic_constructor( $definition ) {
+		// Limit the processing to classes from our project.
+		$definition_class = $definition->getClass();
+		if ( ! \class_exists( $definition_class ) ) {
+			return null;
+		}
+
+		// Apply reflection to the class definition, to retrieve the constructor.
+		$class_reflection_info = new \ReflectionClass( $definition_class );
+		$class_constructor     = $class_reflection_info->getConstructor();
+		if ( ! $class_constructor ) {
+			/*
+			 * The class has no constructor (i.e. it only has a default constructor, with no arguments).
+			 * We do not need to process this class.
+			 */
+			return null;
+		}
+
+		// Get the constructor's last argument.
+		// We only care about the last constructor argument; the '...' splat operator is always last.
+		$constructor_arguments = $class_constructor->getParameters();
+		$splat_argument        = end( $constructor_arguments );
+
+		if ( ! $splat_argument ||
+			 ! $splat_argument->isVariadic() ) { // isVariadic means "is it a 'splat' argument".
+			return null;
+		}
+
+		// Needs PHP 7.0.
+		$splat_argument_type = $splat_argument->getType();
+
+		// Analyse the type of the splat argument.
+		// Needs PHP 7.1.
+		if ( ! is_a( $splat_argument_type, ReflectionNamedType::class ) ) {
+			// If the argument is not a class, we cannot inject it as a dependency.
+			return null;
+		}
+
+		$result                      = new Constructor_Details();
+		$result->target_class_name   = $definition_class;
+		$result->constructor         = $class_constructor;
+		$result->splat_argument_type = $splat_argument_type->getName();
+		$result->splat_argument      = $splat_argument;
+
+		return $result;
+	}
+
+	function get_registered_subclasses_of( $all_definitions, Constructor_Details $definition_class ) {
+		\array_filter(
+			$all_definitions,
+			function( $other_definition ) use ( $definition_class ) {
+				$other_class = $other_definition->getClass();
+				if ( $other_class === $definition_class->target_class_name ) {
+					// Never inject itself to itself
+					return false;
+				}
+
+				return \is_subclass_of( $other_class, $definition_class->splat_argument_type );
+			}
+		);
+	}
+}
+
+class Constructor_Details {
+
+	/**
+	 * @var string Represents the class containing a constructor with a splat parameter.
+	 */
+	public $target_class_name;
+
+	/**
+	 * @var \ReflectionMethod Represents the constructor function.
+	 */
+	public $constructor;
+
+	/**
+	 * @var ReflectionParameter Represents the constructor parameter.
+	 */
+	public $splat_argument;
+
+	/**
+	 * @var ReflectionParameterType Represents the constructor parameter's type.
+	 */
+	public $splat_argument_type;
 }
--- fruit-manager.php
+++ fruit-manager.php
@@ -2,17 +2,21 @@
 
 namespace Yoast\WP\SEO;
 
-interface Fruit_Interface {}
-
-class Fruit_Manager {
-    public $fruit;
-
-    public function __construct( Fruit_Interface ...$fruit ) {
-        $this->fruit = $fruit;
-        var_dump($this->fruit);
-    }
+interface Fruit_Interface {
 }
 
-class Apple implements Fruit_Interface {};
+class Fruit_Manager {
 
-class Pear implements Fruit_Interface {};
+	public $fruit;
+
+	public function __construct( Fruit_Interface ...$fruit ) {
+		$this->fruit = $fruit;
+		var_dump( $this->fruit );
+	}
+}
+
+class Apple implements Fruit_Interface {
+};
+
+class Pear implements Fruit_Interface {
+};
