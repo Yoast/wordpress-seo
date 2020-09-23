0be63dddd7039e201f30230ee5658ad9acffe801
Andy Meerwaldt <andy@yoast.com>
2020-09-23 15:42:24 +0200

    Proper deprecation of the classes

--- src/deprecated/link-count-indexing-list-item-presenter.php
+++ src/deprecated/link-count-indexing-list-item-presenter.php
@@ -13,13 +13,6 @@ use Yoast\WP\SEO\Presenters\Abstract_Presenter;
 class Link_Count_Indexing_List_Item_Presenter extends Abstract_Presenter {
 
 	/**
-	 * The number of objects that need to be reindexed.
-	 *
-	 * @var int
-	 */
-	protected $total_unindexed;
-
-	/**
 	 * Link_Count_Indexing_List_Item_Presenter constructor.
 	 *
 	 * @deprecated 15.1
@@ -28,30 +21,18 @@ class Link_Count_Indexing_List_Item_Presenter extends Abstract_Presenter {
 	 * @param int $total_unindexed The number of objects that need to be indexed.
 	 */
 	public function __construct( $total_unindexed ) {
-		$this->total_unindexed = $total_unindexed;
+		_deprecated_function( __METHOD__, 'WPSEO 15.1' );
 	}
 
 	/**
-	 * @inheritDoc
+	 * Returns the output as string.
 	 *
 	 * @deprecated 15.1
 	 * @codeCoverageIgnore
 	 */
 	public function present() {
-		$list_item = '<li><strong>' . esc_html__( 'Text link counter', 'wordpress-seo' ) . '</strong><br/>';
+		_deprecated_function( __METHOD__, 'WPSEO 15.1' );
 
-		if ( $this->total_unindexed === 0 ) {
-			$list_item .= '<span class="wpseo-checkmark-ok-icon"></span>' . esc_html__( 'Good job! All the links in your texts have been counted.', 'wordpress-seo' );
-		}
-		else {
-			$list_item .= sprintf(
-				'<span id="yoast-link-indexing"><button type="button" class="button yoast-open-indexation" data-title="%1$s" data-settings="yoastLinkIndexingData">%1$s</button></span>',
-				esc_attr__( 'Count links in your texts', 'wordpress-seo' )
-			);
-		}
-
-		$list_item .= '</li>';
-
-		return $list_item;
+		return '';
 	}
 }
