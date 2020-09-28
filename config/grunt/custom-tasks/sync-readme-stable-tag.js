df713f4800816846e0169740eef0a1ed3f958938
IreneStr <irene@yoast.com>
2020-09-28 10:42:39 +0200

    Fix typos and eslint warnings in sync-readme-stable-tag.js

--- config/grunt/custom-tasks/sync-readme-stable-tag.js
+++ config/grunt/custom-tasks/sync-readme-stable-tag.js
@@ -1,42 +1,17 @@
 const wordpressApi = require( "../lib/wordpress-api" );
 
 module.exports = function( grunt ) {
-	grunt.registerTask(
-		"sync-readme-stable-tag",
-		"Makes sure the Stable version is set to the latest released on wordpress.org.",
-		async function() {
-			const done = this.async();
-
-			const targetFile = "readme.txt";
-			const stableVerion = await wordpressApi.getPluginStableVersionFromWordPressApi( grunt.config.data.pluginSlug );
-			if ( stableVerion === null ){
-				grunt.fail.fatal(
-					"The Stable tag for plugin: " + grunt.config.data.pluginSlug + " could not be retrieved from api.wordpress.org\n" +
-					"The release process has been stopped."
-				);
-			}
-
-			const currentVersion = getVersion( targetFile, /(\n?)(Stable tag: )(\d+(\.\d+){0,3})(\n)/, 3 );
-			//grunt.verbose.writeln ( "stable Tag version in readme.txt: " + currentVersion + "\nretrieved verion: " + stableVerion );
-			// Only change the file if the version does not match.
-			if ( currentVersion !== stableVerion ) {
-				grunt.verbose.writeln ( "stable Tag version in readme.txt: " + currentVersion + "\nretrieved version: " + stableVerion + "\nWill set it to the retrieved worpress.org value now." );
-				setVersion( targetFile, /(\n?)(Stable tag: )(\d+(\.\d+){0,3})(\n)/, "$1$2" + stableVerion + "\n" );
-			}
-
-			return done();
-		}
-	);
-
 	/**
 	 * Helper function to update the content of the file.
 	 *
 	 * @param {string} file Target file.
 	 * @param {RegExp} pattern Pattern to search for.
 	 * @param {string} version Version to insert.
+	 *
+	 * @returns {void}
 	 */
 	function setVersion( file, pattern, version ) {
-		let contents = grunt.file.read( file ).replace(
+		const contents = grunt.file.read( file ).replace(
 			pattern,
 			version
 		);
@@ -51,9 +26,38 @@ module.exports = function( grunt ) {
 	 * @param {RegExp} pattern Pattern to search for.
 	 * @param {int} index Index to return.
 	 *
-	 * @return {string} The version from the file.
+	 * @returns {string} The version from the file.
 	 */
 	function getVersion( file, pattern, index ) {
 		return pattern.exec( grunt.file.read( file ) )[ index ];
 	}
+
+	grunt.registerTask(
+		"sync-readme-stable-tag",
+		"Makes sure the Stable version is set to the latest released on wordpress.org.",
+		async function() {
+			const done = this.async();
+
+			const targetFile = "readme.txt";
+			const stableVersion = await wordpressApi.getPluginStableVersionFromWordPressApi( grunt.config.data.pluginSlug );
+			if ( stableVersion === null ) {
+				grunt.fail.fatal(
+					"The Stable tag for plugin: " + grunt.config.data.pluginSlug + " could not be retrieved from api.wordpress.org\n" +
+					"The release process has been stopped."
+				);
+			}
+
+			const currentVersion = getVersion( targetFile, /(\n?)(Stable tag: )(\d+(\.\d+){0,3})(\n)/, 3 );
+			//grunt.verbose.writeln ( "stable Tag version in readme.txt: " + currentVersion + "\nretrieved verion: " + stableVersion );
+			// Only change the file if the version does not match.
+			if ( currentVersion !== stableVersion ) {
+				grunt.verbose.writeln( "Stable tag version in readme.txt: " + currentVersion + "\n" +
+					"Retrieved version from wordpress.org: " + stableVersion + "\n" +
+					"Will set the version in readme.txt to the retrieved wordpress.org value now." );
+				setVersion( targetFile, /(\n?)(Stable tag: )(\d+(\.\d+){0,3})(\n)/, "$1$2" + stableVersion + "\n" );
+			}
+
+			return done();
+		}
+	);
 };
