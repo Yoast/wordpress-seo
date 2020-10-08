41ed2b05ff2a40c936d7d46c5d6368c7e91deccb
Agnieszka Szuba <agnieszka@yoast.com>
2020-10-08 16:25:00 +0200

    Add missing languages to getMorphologyData file

--- packages/yoastseo/spec/specHelpers/getMorphologyData.js
+++ packages/yoastseo/spec/specHelpers/getMorphologyData.js
@@ -5,6 +5,8 @@ import es from "../../premium-configuration/data/morphologyData-es-v9.json";
 import fr from "../../premium-configuration/data/morphologyData-fr-v9.json";
 import ru from "../../premium-configuration/data/morphologyData-ru-v9.json";
 import it from "../../premium-configuration/data/morphologyData-it-v9.json";
+import pt from "../../premium-configuration/data/morphologyData-pt-v9.json";
+import id from "../../premium-configuration/data/morphologyData-id-v9.json";
 import pl from "../../premium-configuration/data/morphologyData-pl-v9.json";
 import ar from "../../premium-configuration/data/morphologyData-ar-v9.json";
 
