96b6470ff507b56544ed5907d13381f69341cab3
Igor <35524806+igorschoester@users.noreply.github.com>
2020-10-14 12:31:43 +0200

    Collect missing data in the getAnalysisData selector
    The titleWidth is still calculated here, we should move this to updating along with the SEO title.

--- js/src/redux/selectors/analysis.js
+++ js/src/redux/selectors/analysis.js
@@ -1,21 +1,63 @@
-import { getEditorDataContent, getEditorDataTitle } from "./editorData";
+import measureTextWidth from "../../helpers/measureTextWidth";
+import { getContentLocale } from "./editorContext";
+import { getEditorDataContent } from "./editorData";
 import { getFocusKeyphrase } from "./focusKeyPhrase";
+import { get } from "lodash";
+
+/**
+ * Gets the SEO title.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {string} The SEO title.
+ */
+export const getSeoTitle = ( state ) => get( state, "analysisData.snippet.title", "" );
+
+/**
+ * Gets the description.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {string} The description.
+ */
+export const getDescription = ( state ) => get( state, "analysisData.snippet.description", "" );
+
+/**
+ * Gets the permalink.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {string} The permalink.
+ */
+export const getPermalink = ( state ) => get( state, "analysisData.snippet.url", "" );
+
+/**
+ * Gets the slug.
+ *
+ * @param {Object} state The state object.
+ *
+ * @returns {string} The slug.
+ */
+export const getSlug = state => get( state, "snippetEditor.data.slug", "" );
 
 /**
  * Gets the analysis data.
  *
- * @param {Object} state The state
+ * @param {Object} state The state.
  *
  * @returns {Object} The analysis results.
  */
 export const getAnalysisData = ( state ) => {
+	const title = getSeoTitle( state );
+
 	return {
 		text: getEditorDataContent( state ),
-		title: getEditorDataTitle( state ),
+		title,
 		keyword: getFocusKeyphrase( state ),
-		synonyms: "",
-		description: "",
-		locale: "",
-		titleWidth: 0,
+		description: getDescription( state ),
+		locale: getContentLocale( state ),
+		titleWidth: measureTextWidth( title ),
+		url: getSlug( state ),
+		permalink: getPermalink( state ),
 	};
 };
