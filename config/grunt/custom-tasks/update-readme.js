b29b362bf13924854396466c3138970640c6d43a
Hans-Christiaan <hans-christiaan@yoast.com>
2020-10-19 15:06:23 +0200

    Automatic RC process: Before adding and committing the changelog, check if enything has changed first with git status.

--- config/grunt/custom-tasks/update-readme.js
+++ config/grunt/custom-tasks/update-readme.js
@@ -100,13 +100,22 @@ module.exports = function( grunt ) {
 				} );
 			}
 
-			// Stage the changed readme.txt.
-			grunt.config( "gitadd.addChangelog.files", { src: [ "./readme.txt" ] } );
-			grunt.task.run( "gitadd:addChangelog" );
+			// Check if there is something to stage and commit with `git status` first.
+			grunt.config( "gitstatus.checkChangelog.options.callback", function( changedFiles ) {
+				if ( changedFiles.length > 0 ) {
+					// Stage the changed readme.txt.
+					grunt.config( "gitadd.addChangelog.files", { src: [ "./readme.txt" ] } );
+					grunt.task.run( "gitadd:addChangelog" );
 
-			// Commit the changed readme.txt.
-			grunt.config( "gitcommit.commitChangelog.options.message", "Add changelog" );
-			grunt.task.run( "gitcommit:commitChangelog" );
+					// Commit the changed readme.txt.
+					grunt.config( "gitcommit.commitChangelog.options.message", "Add changelog" );
+					grunt.task.run( "gitcommit:commitChangelog" );
+				} else {
+					grunt.log.writeln( "Changelog is unchanged. Nothing to commit." );
+				}
+			} );
+
+			grunt.task.run( "gitstatus:checkChangelog" );
 		}
 	);
 };
