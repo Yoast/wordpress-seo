2234c50889b8abe4807128263eec1cc9ceed04e0
Johanna de Vos <johanna@yoast.com>
2020-10-22 14:27:17 +0200

    Simplify the No_Tool_Selected_Conditional and call both the tools page conditionals in the indexing tool integration

--- src/conditionals/no-tool-selected-conditional.php
+++ src/conditionals/no-tool-selected-conditional.php
@@ -3,26 +3,16 @@
 namespace Yoast\WP\SEO\Conditionals;
 
 /**
- * Conditional that is only met when current page is the main tools page (but not a specific tool).
+ * Conditional that is only met when current page is not a specific tool's page.
  */
 class No_Tool_Selected_Conditional implements Conditional {
 
 	/**
 	 * Returns whether or not this conditional is met.
 	 *
-	 * @return boolean Whether or not the conditional is met.
+	 * @return bool Whether or not the conditional is met.
 	 */
 	public function is_met() {
-		global $pagenow;
-
-		if ( $pagenow !== 'admin.php' ) {
-			return false;
-		}
-
-		if ( isset( $_GET['page'] ) && $_GET['page'] === 'wpseo_tools' && ! isset( $_GET['tool'] ) ) {
-			return true;
-		}
-
-		return false;
+		return ! isset( $_GET['tool'] );
 	}
 }
