b35ba671b16bd33c1200202ed141b8e3f0e8391d
Johanna de Vos <johanna@yoast.com>
2020-10-23 09:55:29 +0200

    Also ping the product team

--- config/grunt/task-config/notify-slack.js
+++ config/grunt/task-config/notify-slack.js
@@ -2,6 +2,6 @@
 module.exports = {
 	rc: {
 		slackToken: process.env.SLACK_TEST_CHANNEL_TOKEN,
-		message: "@plugin-testers An RC has been created: ",
+		message: "@plugin-testers @product-team An RC has been created: ",
 	},
 };
