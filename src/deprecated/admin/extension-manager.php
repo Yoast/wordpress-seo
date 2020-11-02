d4ec375cbc341749b4cdec9ab50186d6eb21fede
Herre Groen <herre@yoast.com>
2020-11-02 12:23:21 +0100

    Fix deprecation versions for the license manager

--- src/deprecated/admin/extension-manager.php
+++ src/deprecated/admin/extension-manager.php
@@ -10,7 +10,7 @@
 /**
  * Represents the class that contains the available extensions for Yoast SEO.
  *
- * @deprecated 15.2
+ * @deprecated 15.4
  * @codeCoverageIgnore
  */
 class WPSEO_Extension_Manager {
@@ -18,7 +18,7 @@ class WPSEO_Extension_Manager {
 	/**
 	 * Adds an extension to the manager.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 *
 	 * @param string          $extension_name The extension name.
@@ -27,13 +27,13 @@ class WPSEO_Extension_Manager {
 	 * @return void
 	 */
 	public function add( $extension_name, WPSEO_Extension $extension = null ) {
-		_deprecated_function( __METHOD__, '15.2' );
+		_deprecated_function( __METHOD__, '15.4' );
 	}
 
 	/**
 	 * Removes an extension from the manager.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 *
 	 * @param string $extension_name The name of the extension to remove.
@@ -41,13 +41,13 @@ class WPSEO_Extension_Manager {
 	 * @return void
 	 */
 	public function remove( $extension_name ) {
-		_deprecated_function( __METHOD__, '15.2' );
+		_deprecated_function( __METHOD__, '15.4' );
 	}
 
 	/**
 	 * Returns the extension for the given extension name.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 *
 	 * @param string $extension_name The name of the extension to get.
@@ -55,7 +55,7 @@ class WPSEO_Extension_Manager {
 	 * @return null|WPSEO_Extension The extension object or null when it doesn't exist.
 	 */
 	public function get( $extension_name ) {
-		_deprecated_function( __METHOD__, '15.2' );
+		_deprecated_function( __METHOD__, '15.4' );
 
 		return null;
 	}
@@ -63,13 +63,13 @@ class WPSEO_Extension_Manager {
 	/**
 	 * Returns all set extension.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 *
 	 * @return WPSEO_Extension[] Array with the extensions.
 	 */
 	public function get_all() {
-		_deprecated_function( __METHOD__, '15.2' );
+		_deprecated_function( __METHOD__, '15.4' );
 
 		return [];
 	}
@@ -77,7 +77,7 @@ class WPSEO_Extension_Manager {
 	/**
 	 * Checks if the plugin is activated within My Yoast.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 *
 	 * @param string $extension_name The extension name to check.
@@ -85,7 +85,7 @@ class WPSEO_Extension_Manager {
 	 * @return bool True when the plugin is activated.
 	 */
 	public function is_activated( $extension_name ) {
-		_deprecated_function( __METHOD__, '15.2' );
+		_deprecated_function( __METHOD__, '15.4' );
 
 		return false;
 	}
--- src/deprecated/admin/license-page-manager.php
+++ src/deprecated/admin/license-page-manager.php
@@ -22,27 +22,27 @@ class WPSEO_License_Page_Manager implements WPSEO_WordPress_Integration {
 	/**
 	 * Registers all hooks to WordPress.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 */
 	public function register_hooks() {
-		_deprecated_function( __METHOD__, 'WPSEO 15.2' );
+		_deprecated_function( __METHOD__, 'WPSEO 15.4' );
 	}
 
 	/**
 	 * Removes the faulty set notifications.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 */
 	public function remove_faulty_notifications() {
-		_deprecated_function( __METHOD__, 'WPSEO 15.2' );
+		_deprecated_function( __METHOD__, 'WPSEO 15.4' );
 	}
 
 	/**
 	 * Handles the response.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 *
 	 * @param array  $response          HTTP response.
@@ -52,7 +52,7 @@ class WPSEO_License_Page_Manager implements WPSEO_WordPress_Integration {
 	 * @return array The response array.
 	 */
 	public function handle_response( array $response, $request_arguments, $url ) {
-		_deprecated_function( __METHOD__, 'WPSEO 15.2' );
+		_deprecated_function( __METHOD__, 'WPSEO 15.4' );
 
 		return $response;
 	}
@@ -60,13 +60,13 @@ class WPSEO_License_Page_Manager implements WPSEO_WordPress_Integration {
 	/**
 	 * Returns the license page to use based on the version number.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 *
 	 * @return string The page to use.
 	 */
 	public function get_license_page() {
-		_deprecated_function( __METHOD__, '15.2' );
+		_deprecated_function( __METHOD__, '15.4' );
 
 		return 'licenses';
 	}
@@ -74,11 +74,11 @@ class WPSEO_License_Page_Manager implements WPSEO_WordPress_Integration {
 	/**
 	 * Validates the extensions and show a notice for the invalid extensions.
 	 *
-	 * @deprecated 15.2
+	 * @deprecated 15.4
 	 * @codeCoverageIgnore
 	 */
 	public function validate_extensions() {
-		_deprecated_function( __METHOD__, '15.2' );
+		_deprecated_function( __METHOD__, '15.4' );
 	}
 }
 // phpcs:enable
