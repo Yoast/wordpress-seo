828a8241400d47a691bf59937f460f11e103c85c
Igor <35524806+igorschoester@users.noreply.github.com>
2020-11-05 11:56:12 +0100

    Remove elementor init develop comments

--- js/src/elementor.js
+++ js/src/elementor.js
@@ -42,22 +42,3 @@ domReady( () => {
 
 // Initialize the editor integration.
 initElementorEditorIntegration();
-
-// STORE INIT
-// Register our store to WP data.
-// Added:
-// - editorData (title, description, slug)
-//		Is slug really an editor data thing? Also, do we need the extra draft slug checks?
-// - searchMetadata (seo title, seo description, focus keyphrase)
-
-// WATCHER INIT
-// Only for non-component data flow into the store.
-// Added: editorData for Elementor
-
-// PAGE INIT
-// - Render React root.
-// > PER COMPONENT INIT
-//   Example: analysis: subscribe to store and refresh when data changed
-
-// Create replacement variable initalization that replaces the `fillReplacementVariables` in the analysis data.
-// Use the new searchMetadata.keyphrase in the KeywordInput component.
