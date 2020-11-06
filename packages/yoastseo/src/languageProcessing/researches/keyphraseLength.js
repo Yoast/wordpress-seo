51864a73a6214f3d065c41d002a112803805fa67
FAMarfuaty <aida@yoast.com>
2020-11-06 10:29:18 +0100

    Adjust a function description

--- packages/yoastseo/src/languageProcessing/researches/keyphraseLength.js
+++ packages/yoastseo/src/languageProcessing/researches/keyphraseLength.js
@@ -4,7 +4,7 @@
  * @param {Paper} paper 			The paper to research
  * @param {Researcher} researcher 	The researcher to use for analysis
  *
- * @returns {Object} The length of the keyphrase and whether the function words list is available or not.
+ * @returns {Object} The length of the keyphrase and the function words list.
  */
 export default function( paper, researcher ) {
 	const topicForms = researcher.getResearch( "morphology" );
