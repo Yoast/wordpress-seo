0cbd828789b1498ef1faa12b597f2e6129a89c10
Igor <35524806+igorschoester@users.noreply.github.com>
2020-11-11 14:37:28 +0100

    Add selector for isKeywordAnalysisActive

--- js/src/redux/selectors/preferences.js
+++ js/src/redux/selectors/preferences.js
@@ -1,10 +1,23 @@
+import { get } from "lodash";
+
 /**
  * Gets the preferences.
  *
- * @param {Object} state    The state.
+ * @param {Object} state The state.
  *
- * @returns {string} the preferences.
+ * @returns {string} The preferences.
  */
 export function getPreferences( state ) {
 	return state.preferences;
+}
+
+/**
+ * Gets the isKeywordAnalysisActive.
+ *
+ * @param {Object} state The state.
+ *
+ * @returns {boolean} The isKeywordAnalysisActive.
+ */
+export function getIsKeywordAnalysisActive( state ) {
+	return get( state, "preferences.isKeywordAnalysisActive", false );
 }
