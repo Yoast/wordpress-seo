6eaa0457f28756ba03887e2f061185c8b76dbcfb
Andy Meerwaldt <andy@yoast.com>
2020-11-11 10:42:09 +0100

    Fix CS

--- config/dependency-injection/schema-templates-loader.php
+++ config/dependency-injection/schema-templates-loader.php
@@ -15,7 +15,7 @@ class Schema_Templates_Loader {
 	public function get_templates() {
 		// When we have minimal PHP 7.0 support, we can replace the nested dirname call by using the levels argument.
 		$root_directory = dirname( dirname( __DIR__ ) );
-		$templates = glob( $root_directory . '/src/schema-templates/*.php' );
+		$templates      = glob( $root_directory . '/src/schema-templates/*.php' );
 		if ( ! $templates ) {
 			$templates = [];
 		}
