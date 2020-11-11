8a11f8505f65d0030fe56b33f69022e8a3f99be6
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2020-11-11 15:21:33 +0100

    Fix tests related to store changes and Action Creator AnalysisFields side-effects

--- js/tests/redux/reducers/cornerstoneContent.test.js
+++ js/tests/redux/reducers/cornerstoneContent.test.js
@@ -1,13 +1,15 @@
 /* global describe, it, expect */
 
-import { toggleCornerstoneContent } from "../../../src/redux/actions/cornerstoneContent";
+import { TOGGLE_CORNERSTONE_CONTENT } from "../../../src/redux/actions/cornerstoneContent";
 import cornerstoneContentReducer from "../../../src/redux/reducers/cornerstoneContent";
 
 describe( "cornerstone reducers", () => {
 	describe( "cornerstoneContentReducer on receiving the TOGGLE_CORNERSTONE_CONTENT action", () => {
 		it( "should set isCornerstoneContent's current value to true if it was false", () => {
 			const state = false;
-			const action = toggleCornerstoneContent();
+			const action = {
+				type: TOGGLE_CORNERSTONE_CONTENT,
+			};
 			const expected = true;
 			const actual = cornerstoneContentReducer( state, action );
 
@@ -16,7 +18,9 @@ describe( "cornerstone reducers", () => {
 
 		it( "should set isCornerstoneContent's current value to false if it was true", () => {
 			const state = true;
-			const action = toggleCornerstoneContent();
+			const action = {
+				type: TOGGLE_CORNERSTONE_CONTENT,
+			};
 			const expected = false;
 			const actual = cornerstoneContentReducer( state, action );
 
