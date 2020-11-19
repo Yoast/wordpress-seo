ff7b9d7d534f5e4333fd2ba5340507fd361d76d6
Igor <35524806+igorschoester@users.noreply.github.com>
2020-11-19 13:37:58 +0100

    Add conditional for new or edit post (type)

---
+++ src/conditionals/admin/post-conditional.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Yoast\WP\SEO\Conditionals\Admin;
+
+use Yoast\WP\SEO\Conditionals\Conditional;
+
+/**
+ * Conditional that is only met when on a post edit or new post page.
+ */
+class Post_Conditional implements Conditional {
+
+	/**
+	 * Returns whether or not this conditional is met.
+	 *
+	 * @return boolean Whether or not the conditional is met.
+	 */
+	public function is_met() {
+		global $pagenow;
+
+		$get_action = \filter_input( INPUT_GET, 'action', FILTER_SANITIZE_STRING );
+
+		// Current page is the creation of a new post (type, i.e. post, page, custom post or attachment).
+		if ( $pagenow === 'post-new.php' ) {
+			return true;
+		}
+
+		// Current page is the edit page of an existing post (type, i.e. post, page, custom post or attachment).
+		if ( $pagenow === 'post.php' || $get_action === 'edit' ) {
+			return true;
+		}
+
+		return false;
+	}
+}
