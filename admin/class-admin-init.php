c338028d2a4cc9863c4be3e480013c17be4d4dab
Jimmy Comack <jimmy@yoast.com>
2020-11-24 14:22:22 +0100

    Fixed codestyle

--- admin/class-admin-init.php
+++ admin/class-admin-init.php
@@ -435,23 +435,23 @@ class WPSEO_Admin_Init {
 				'version'     => '9.4',
 				'alternative' => null,
 			],
-			'wpseo_opengraph'                       => [
+			'wpseo_opengraph' => [
 				'version'     => '14.0',
 				'alternative' => null,
 			],
-			'wpseo_twitter'                         => [
+			'wpseo_twitter' => [
 				'version'     => '14.0',
 				'alternative' => null,
 			],
-			'wpseo_twitter_taxonomy_image'          => [
+			'wpseo_twitter_taxonomy_image' => [
 				'version'     => '14.0',
 				'alternative' => null,
 			],
-			'wpseo_twitter_metatag_key'             => [
+			'wpseo_twitter_metatag_key' => [
 				'version'     => '14.0',
 				'alternative' => null,
 			],
-			'wp_seo_get_bc_ancestors'               => [
+			'wp_seo_get_bc_ancestors' => [
 				'version'     => '14.0',
 				'alternative' => 'wpseo_breadcrumb_links',
 			],
