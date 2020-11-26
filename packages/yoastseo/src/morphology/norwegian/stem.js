11699ab3abb0905164b1c8060ab0d807399f17bb
FAMarfuaty <aida@yoast.com>
2020-11-26 11:41:26 +0100

    refactor stemmer

--- packages/yoastseo/src/morphology/norwegian/stem.js
+++ packages/yoastseo/src/morphology/norwegian/stem.js
@@ -28,6 +28,7 @@
  * R1 is the region after the first non-vowel following a vowel. It should include at least 3 letters.
  *
  * @param {string} word The word for which to determine the R1 region.
+ *
  * @returns {number} The start index of the R1 region.
  */
 const determineR1 = function( word ) {
@@ -48,11 +49,10 @@ const determineR1 = function( word ) {
 
 /**
  *  Searches and stems these suffixes if in R1: hetene, hetens, hetens, heten, heter, endes, ande, ende, edes, enes, ede,
- *  ane, ene, ens, ers, ets, het, ast, en, ar, er, as, es, et, a, e.
- *  Searches and stems suffix -els if in R1.
+ *  ane, ene, ens, ers, ets, het, ast, en, ar, er, as, es, et, a, e. After stemming the suffix, it checks if the word
+ *  ends in -ert, if it is stem -t.
  *  Searches and stems suffix -s if in R1 and if preceded by a valid precedence: b, c, d, f, g, h, j, l, m, n, o, p, r, t, v, y, z,
  *  or k not preceded by a vowel.
- *  Searches for suffixes -erte/-ert and change it to -er if in R1.
  *
  * @param {string} word             The word to check.
  * @param {number} r1Index          The start index of the R1.
@@ -63,31 +63,25 @@ const determineR1 = function( word ) {
 const removeSuffixesStep1 = function( word, r1Index, morphologyData ) {
 	const suffixes1aIndex = word.search( new RegExp( morphologyData.externalStemmer.regexSuffixes1a ) );
 	if ( suffixes1aIndex >= r1Index && r1Index !== -1 ) {
-		word = word.substring( 0, suffixes1aIndex );
-	}
-
-	const suffix1bIndex = word.search( new RegExp( morphologyData.externalStemmer.regexSuffix1b ) );
-	if ( suffix1bIndex >= r1Index && r1Index !== -1 ) {
-		word = word.substring( 0, suffix1bIndex );
+		let wordAfterStemming = word.substring( 0, suffixes1aIndex );
+		if ( /ert$/i.test( wordAfterStemming ) ) {
+			wordAfterStemming = wordAfterStemming.slice( 0, -1 );
+		}
+		return wordAfterStemming;
 	}
 
 	const suffixSIndex = word.search( /s$/ );
-	const suffixes1cIndex = word.search( new RegExp( morphologyData.externalStemmer.regexSuffixes1c ) );
-	if ( suffixSIndex >= r1Index && suffixes1cIndex !== -1 && r1Index !== -1 ) {
-		word = word.slice( 0, -1 );
+	const suffixes1bIndex = word.search( new RegExp( morphologyData.externalStemmer.regexSuffixes1b ) );
+	if ( suffixSIndex >= r1Index && suffixes1bIndex !== -1 && r1Index !== -1 ) {
+		return word.slice( 0, -1 );
 	}
 
-	const suffixes1d = morphologyData.externalStemmer.regexSuffixes1d;
-	const suffixes1dIndex = word.search( new RegExp( suffixes1d[ 0 ] ) );
-	if ( suffixes1dIndex >= r1Index && r1Index !== -1 ) {
-		word = word.replace( new RegExp( suffixes1d[ 0 ] ), ( suffixes1d[ 1 ] ) );
-	}
 
 	return word;
 };
 
 /**
- * Searches for suffixes -dt/-vt and stems -t if the suffix is in R1.
+ * Searches for suffixes -ert/-dt/-vt and stems -t if the suffix is in R1.
  *
  * @param {string} word             The word to check.
  * @param {number} r1Index          The start index of the R1.
@@ -129,17 +123,11 @@ const removeSuffixesStep3 = function( word, r1Index, morphologyData ) {
  * @returns {string}    The stemmed word.
  */
 export default function stem( word, morphologyData ) {
-	// Remove suffixes step 1
-	let r1Index = determineR1( word );
-	word =  removeSuffixesStep1( word, r1Index, morphologyData );
-
-	// Remove suffixes step 2
-	r1Index = determineR1( word );
-	word =  removeSuffixesStep2( word, r1Index, morphologyData );
-
-	// Remove suffixes step 3
-	r1Index = determineR1( word );
-	word = removeSuffixesStep3( word, r1Index, morphologyData );
+	let r1Index = -1;
+	for ( const step of [ removeSuffixesStep1, removeSuffixesStep2, removeSuffixesStep3 ] ) {
+		r1Index = determineR1( word );
+		word = step( word, r1Index, morphologyData );
+	}
 
 	return word;
 }
