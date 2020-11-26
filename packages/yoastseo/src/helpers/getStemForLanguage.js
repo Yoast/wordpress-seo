3c20130204be8023d2675f3efeefee1d9ccfc43a
FAMarfuaty <aida@yoast.com>
2020-11-26 12:33:17 +0100

    Add norwegian stemmer

--- packages/yoastseo/src/helpers/getStemForLanguage.js
+++ packages/yoastseo/src/helpers/getStemForLanguage.js
@@ -11,6 +11,7 @@ import polishDetermineStem from "../morphology/polish/stem";
 import arabicDetermineStem from "../morphology/arabic/stem";
 import swedishDetermineStem from "../morphology/swedish/stem";
 import hebrewDetermineStem from "../morphology/hebrew/stem";
+import norwegianDetermineStem from "../morphology/norwegian/stem";
 
 /**
  * Collects all functions for determining a stem per language and returns this collection to a Researcher
@@ -32,5 +33,6 @@ export default function() {
 		ar: arabicDetermineStem,
 		sv: swedishDetermineStem,
 		he: hebrewDetermineStem,
+		nb: norwegianDetermineStem,
 	};
 }
