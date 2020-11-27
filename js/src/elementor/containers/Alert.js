186bf075a248f2286330e6fb7f2e49a18745505d
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2020-11-27 13:53:47 +0100

    Add JSdoc

--- js/src/elementor/containers/Alert.js
+++ js/src/elementor/containers/Alert.js
@@ -1,6 +1,15 @@
 import { Alert } from "@yoast/components";
 import { withSelect } from "@wordpress/data";
 
+/**
+ * Wraps the Alert as a quick fix for the way it is used in Elementor.
+ * Message is sometimes an empty array or an empty string,
+ * in which case the Alert should not be shown.
+ *
+ * @param {Object} props The props.
+ *
+ * @returns {ReactElement|Null} The Alert or Null.
+ */
 function WrappedAlert( props ) {
 	return ( props.message.length === 0 ? null : <Alert type={ props.type }>{ props.message }</Alert> );
 }
