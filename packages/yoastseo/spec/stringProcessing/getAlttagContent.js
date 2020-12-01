8fb9c758750c962f27a88c501ee5dba1907407b6
Agnieszka Szuba <agnieszka@yoast.com>
2020-12-01 14:31:39 +0100

    Add specs for getAlttagContent and imageInText

--- packages/yoastseo/spec/stringProcessing/getAlttagContent.js
+++
@@ -1,8 +0,0 @@
-import imageAlt from "../../src/researches/stringProcessing/getAlttagContent.js";
-
-describe( "Checks for alttag in an image", function() {
-	it( "returns the contents of the alttag", function() {
-		expect( imageAlt( "<img src='img.com' alt='a test' />" ) ).toBe( "a test" );
-		expect( imageAlt( "<img src='img.com' alt='ä test' />" ) ).toBe( "ä test" );
-	} );
-} );
