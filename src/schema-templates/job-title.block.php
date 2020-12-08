324b0679a6c0335735b82bd42b56e23d56b4e248
Johanna de Vos <johanna@yoast.com>
2020-12-08 09:52:44 +0100

    Create the job title block template

---
+++ src/schema-templates/job-title.block.php
@@ -0,0 +1,5 @@
+<?php // phpcs:ignore Internal.NoCodeFound ?>
+{{block name="yoast/job-title" title="Job title" category="common" parent=[ "yoast/job-posting" ] }}
+<div class={{class-name}}>
+	{{variable-tag-rich-text name="title" tags=[ "h2", "h3", "h4", "h5", "h6", "strong" ] placeholder="Enter job title"}}
+</div>
