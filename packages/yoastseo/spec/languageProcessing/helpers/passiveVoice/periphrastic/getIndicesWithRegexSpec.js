d7a93e4ac479b6dd2f7310197b2278c74dd84bbb
FAMarfuaty <aida@yoast.com>
2020-12-08 11:44:26 +0100

    Add spec for getIndicesWithRegex

--- packages/yoastseo/spec/languageProcessing/helpers/passiveVoice/periphrastic/getIndicesWithRegexSpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/passiveVoice/periphrastic/getIndicesWithRegexSpec.js
@@ -1,12 +1,9 @@
 import getIndicesWithRegex from "../../../../../src/languageProcessing/helpers/passiveVoice/periphrastic/getIndicesWithRegex";
 
-describe( "splits German sentences into parts", function() {
-	it( "returns all sentence parts", function() {
-		const sentence =  "Zwischen 1927 und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron.";
-		expect( getIndicesWithRegex( sentence, "de" )[ 0 ].getSentencePartText() ).toBe( "Zwischen 1927" );
-	} );
-	it( "splits German sentences that begin with a stopword into sentence parts", function() {
-		const sentence =  "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron.";
-		expect( getIndicesWithRegex( sentence, "de" )[ 0 ].getSentencePartText() ).toBe( "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron." );
+describe( "Matches words from a list in sentence parts and returns them and their indices", function() {
+	it( "returns the match and the index of the match ", function() {
+		const sentencePart =  "It is a hands-free, voice-controlled device.";
+		const regex = /([:,]|('ll)|('ve))(?=[ \n\r\t'"+\-»«‹›<>])/ig;
+		expect( getIndicesWithRegex( sentencePart, regex ) ).toEqual( [ { index: 18, match: "," } ] );
 	} );
 } );
