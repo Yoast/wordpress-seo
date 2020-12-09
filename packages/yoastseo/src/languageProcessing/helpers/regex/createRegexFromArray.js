7437bd48cf2db512a932e29bb339f5ffd0fe0824
FAMarfuaty <aida@yoast.com>
2020-12-09 16:58:45 +0100

    Improve code style for regex helper files

--- packages/yoastseo/src/languageProcessing/helpers/regex/createRegexFromArray.js
+++ packages/yoastseo/src/languageProcessing/helpers/regex/createRegexFromArray.js
@@ -8,9 +8,9 @@ import sanitizeString from "../sanitize/sanitizeString";
 /**
  * Creates a regex of combined strings from the input array.
  *
- * @param {string[]} array The array with strings
+ * @param {string[]} array                      The array with strings
  * @param {boolean} [disableWordBoundary=false] Boolean indicating whether or not to disable word boundaries.
- * @param {string} [extraBoundary=""] A string that is used as extra boundary for the regex.
+ * @param {string} [extraBoundary=""]           A string that is used as extra boundary for the regex.
  * @param {boolean} [doReplaceDiacritics=false] If set to true, it replaces diacritics. Defaults to false.
  *
  * @returns {RegExp} regex The regex created from the array.
--- packages/yoastseo/src/languageProcessing/helpers/regex/createRegexFromDoubleArray.js
+++ packages/yoastseo/src/languageProcessing/helpers/regex/createRegexFromDoubleArray.js
@@ -4,10 +4,12 @@ import addWordBoundary from "../word/addWordboundary.js";
 
 /**
  * Creates a regex string of combined strings from the input array.
+ *
  * @param {array} array The array containing the various parts of a transition word combination.
+ *
  * @returns {array} The array with replaced entries.
  */
-var wordCombinationToRegexString = function( array ) {
+const wordCombinationToRegexString = function( array ) {
 	array = array.map( function( word ) {
 		return addWordBoundary( word );
 	} );
@@ -16,13 +18,17 @@ var wordCombinationToRegexString = function( array ) {
 
 /**
  * Creates a regex of combined strings from the input array, containing arrays with two entries.
+ *
  * @param {array} array The array containing arrays.
+ *
  * @returns {RegExp} The regex created from the array.
  */
 export default function( array ) {
 	array = array.map( function( wordCombination ) {
 		return wordCombinationToRegexString( wordCombination );
 	} );
-	var regexString = "(" + array.join( ")|(" ) + ")";
+
+	const regexString = "(" + array.join( ")|(" ) + ")";
+
 	return new RegExp( regexString, "ig" );
 }
