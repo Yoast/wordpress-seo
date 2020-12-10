7014d8f785356ae2e5c2199b21105f81893e0e0f
FAMarfuaty <aida@yoast.com>
2020-12-10 13:21:42 +0100

    Adjust the function comment in regexHelpers

--- packages/yoastseo/src/languageProcessing/helpers/morphology/regexHelpers.js
+++ packages/yoastseo/src/languageProcessing/helpers/morphology/regexHelpers.js
@@ -3,6 +3,7 @@
  *
  * @param {string} word		The word to check.
  * @param {string} regex	The regex.
+ *
  * @returns {boolean}		Whether or not there was a match.
  */
 export function doesWordMatchRegex( word, regex ) {
@@ -13,8 +14,9 @@ export function doesWordMatchRegex( word, regex ) {
 /**
  * Loops through a nested array with pairs of regexes and replacements, and performs the needed replacement if a match is found.
  *
- * @param {string} word 							The word that may need to be modified.
+ * @param {string} word 						The word that may need to be modified.
  * @param {[][]} groupOfRegexAndReplacements 	The array with the regexes and the required replacements.
+ *
  * @returns {?string} The modified stem or null if no match was found.
  */
 export function searchAndReplaceWithRegex( word, groupOfRegexAndReplacements ) {
@@ -29,8 +31,8 @@ export function searchAndReplaceWithRegex( word, groupOfRegexAndReplacements ) {
 /**
  * Loops through a nested array of regexes and replacement pairs, and applies all replacements for which a match is found.
  *
- * @param {string}      word                        The word that may need to be modified.
- * @param {string[]}    listOfRegexAndReplacement   The array with the regexes and the replacements.
+ * @param {string}  word                        The word that may need to be modified.
+ * @param {[][]}    listOfRegexAndReplacement   The array with the regexes and the replacements.
  *
  * @returns {string} The modified word.
  */
