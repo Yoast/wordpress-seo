76be158dbbdc5486fd9a1d5df96bea3ddced775e
jrfnl <jrfnl@users.noreply.github.com>
2020-12-10 07:51:55 +0100

    Tests: use `expectOutputContains()`
    WP Test Utils introduces a new `expectOutputContains()` method to complement the PHPUnit native `expectOutput*()` methods as it appears testing whether output contains a certain text string is a common pattern in the Yoast tests.
    
    By default this method will ignore differences in line endings to improve the cross-OS compatibility of the tests.
    
    This switches out the custom code to do the same in this test suite in favour of using the `expectOutputContains()` method instead.
    
    **Important**: each test should only have one (1) output expectation. Subsequent expectations being set will overwrite the original expectation. In effect, that means that only the last set expectation will ever be tested.
    
    This goes for both the PHPUnit native `expectOutput*()` methods as well as the WP Test Utils `expectOutputContains()` method.
    
    To that end, when there were multiple expectations for one test, these tests have been refactored to be run multiple times in combination with a data provider. That way, each expectation will be tested separately.

--- tests/integration/framework/class-wpseo-unit-test-case.php
+++ tests/integration/framework/class-wpseo-unit-test-case.php
@@ -27,23 +27,4 @@ abstract class WPSEO_UnitTestCase extends TestCase {
 		$migration_runner = YoastSEO()->classes->get( Migration_Runner::class );
 		$migration_runner->run_migrations( 'free' );
 	}
-
-	/**
-	 * Tests whether the output contains the expected value.
-	 *
-	 * @param string|array $expected Expected output.
-	 */
-	protected function expectOutputContains( $expected ) {
-		$output = preg_replace( '|\R|', "\r\n", ob_get_contents() );
-		ob_clean();
-
-		if ( ! is_array( $expected ) ) {
-			$expected = [ $expected ];
-		}
-
-		foreach ( $expected as $needle ) {
-			$found = strpos( $output, $needle );
-			$this->assertTrue( $found !== false, sprintf( 'Expected "%s" to be found in "%s" but couldn\'t find it.', $needle, $output ) );
-		}
-	}
 }
