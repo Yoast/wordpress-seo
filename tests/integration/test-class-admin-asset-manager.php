c95acde8f743016259b536dc90f08520b7cb905b
jrfnl <jrfnl@users.noreply.github.com>
2020-12-10 09:27:36 +0100

    WPSEO_Admin_Asset_Manager_Test: refactor tests using `MockBuilder->at()`
    The `at()` matcher has been deprecated in PHPUnit 9 and will be removed in PHPUnit 10. It is strongly recommended not to rely on the order in which methods are invoked.
    
    Replacing the expectations create with the `at()` matcher, by a slightly different expectation which still tests that the `register_(script|style)()` function is called 1) the expected number of times and 2) with specific arguments and 3) in a certain order.
    
    This stabilizes these tests for the future.

--- tests/integration/test-class-admin-asset-manager.php
+++ tests/integration/test-class-admin-asset-manager.php
@@ -247,17 +247,11 @@ class WPSEO_Admin_Asset_Manager_Test extends WPSEO_UnitTestCase {
 			->getMock();
 
 		$class_instance
-			->expects( $this->at( 0 ) )
+			->expects( $this->exactly( 2 ) )
 			->method( 'register_script' )
-			->with(
-				$this->equalTo( new WPSEO_Admin_Asset( $asset_args[0] ) )
-			);
-
-		$class_instance
-			->expects( $this->at( 1 ) )
-			->method( 'register_script' )
-			->with(
-				$this->equalTo( new WPSEO_Admin_Asset( $asset_args[1] ) )
+			->withConsecutive(
+				[ $this->equalTo( new WPSEO_Admin_Asset( $asset_args[0] ) ) ],
+				[ $this->equalTo( new WPSEO_Admin_Asset( $asset_args[1] ) ) ]
 			);
 
 		$class_instance->register_scripts( $asset_args );
@@ -291,17 +285,11 @@ class WPSEO_Admin_Asset_Manager_Test extends WPSEO_UnitTestCase {
 			->getMock();
 
 		$class_instance
-			->expects( $this->at( 0 ) )
+			->expects( $this->exactly( 2 ) )
 			->method( 'register_style' )
-			->with(
-				$this->equalTo( new WPSEO_Admin_Asset( $asset_args[0] ) )
-			);
-
-		$class_instance
-			->expects( $this->at( 1 ) )
-			->method( 'register_style' )
-			->with(
-				$this->equalTo( new WPSEO_Admin_Asset( $asset_args[1] ) )
+			->withConsecutive(
+				[ $this->equalTo( new WPSEO_Admin_Asset( $asset_args[0] ) ) ],
+				[ $this->equalTo( new WPSEO_Admin_Asset( $asset_args[1] ) ) ]
 			);
 
 		$class_instance->register_styles( $asset_args );
