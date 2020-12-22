ee61b9b4ed94b5a4a140166f3b6b0abe44ca38ac
Andy Meerwaldt <andy@yoast.com>
2020-12-22 08:54:42 +0100

    Adds additional tests

--- packages/schema-blocks/tests/blocks/RequiredBlocks.test.tsx
+++ packages/schema-blocks/tests/blocks/RequiredBlocks.test.tsx
@@ -2,18 +2,67 @@ import { BlockInstance } from "@wordpress/blocks";
 import { RequiredBlock } from "../../src/instructions/blocks/dto";
 import RequiredBlocks from "../../src/blocks/RequiredBlocks";
 
+jest.mock( "../../src/functions/blocks", () => {
+	return {
+		getBlockType: jest.fn( ( blockName: string )  => {
+			if ( blockName === "yoast/nonexisting" ) {
+				return undefined;
+			}
+
+			return {
+				title: "The required block",
+			};
+		} ),
+	};
+} );
+
+jest.mock( "../../src/functions/innerBlocksHelper", () => {
+	return {
+		getInnerblocksByName: jest.fn( ()  => {
+			return [
+				{
+					name: "yoast/added-to-content",
+				},
+				{
+					name: "yoast/not-added-to-content",
+				},
+			];
+		} ),
+	};
+} );
+
 describe( "The required blocks in the sidebar", () => {
-	it( "haha", () => {
-		const block = {} as BlockInstance;
+	it( "doesn't have the required block being registered as a block", () => {
+		const block = { innerBlocks: [] } as BlockInstance;
 		const requiredBlocks = [
 			{
-				name: "yoast/innerblock",
+				name: "yoast/nonexisting",
 				option: 1,
 			} as RequiredBlock,
 		];
 
 		const actual = RequiredBlocks( block, requiredBlocks );
-		const expected = "lala";
+
+		expect( actual ).toBe( null );
+	} );
+
+	it( "renders the required block as an added one", () => {
+		const block = { innerBlocks: [] } as BlockInstance;
+		const requiredBlocks = [
+			{
+				name: "yoast/added-to-content",
+				option: 1,
+			} as RequiredBlock,
+		];
+
+		const actual = RequiredBlocks( block, requiredBlocks );
+		const expected = <PanelBody>
+
+			<div className="yoast-block-sidebar-title">Required blocks</div>
+			<ul className="yoast-block-suggestions">
+
+			</ul>
+		</PanelBody>;
 
 		expect( actual ).toEqual( expected );
 	} );
