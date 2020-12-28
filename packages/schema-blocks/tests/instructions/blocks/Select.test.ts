4d111c55dba5f63d376220de8be6f84fa7b35756
Hans-Christiaan <hans-christiaan@yoast.com>
2020-12-28 11:16:00 +0100

    Fixed import of react-test-renderer in schema-blocks unit tests.

--- packages/schema-blocks/tests/instructions/blocks/Select.test.ts
+++ packages/schema-blocks/tests/instructions/blocks/Select.test.ts
@@ -1,4 +1,4 @@
-import renderer from "react-test-renderer";
+import * as renderer from "react-test-renderer";
 import { shallow } from "enzyme";
 
 import { ReactElement } from "@wordpress/element";
