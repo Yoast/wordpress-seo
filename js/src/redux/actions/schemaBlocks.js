f7060d0565d520e016af7001c11e4910cb3de096
Joost Boomkamp <joostboomkamp@yoast.com>
2020-12-31 16:09:48 +0100

    improve redux action

--- js/src/redux/actions/schemaBlocks.js
+++ js/src/redux/actions/schemaBlocks.js
@@ -2,36 +2,6 @@ const PREFIX = "WPSEO_";
 
 export const SET_BLOCK_VALID = `${ PREFIX }SET_BLOCK_VALIDATION`;
 
-export class SetBlockValidation {
-	/**
-	 * The type of action, always set to SET_BLOCK_VALIDATION.
-	 * @type {string}
-	 */
-	type;
-	/**
-	 * The clientId of the validated BlockInstance.
-	 * @type {string}
-	 */
-	clientId;
-
-	/**
-	 * The validation results.
-	 * @type {BlockValidation[]}
-	 */
-	validation;
-
-	/**
-	 * Creates a new SetBlockValidation action.
-	 * @param {string} clientId              The clientId of the validated BlockInstance.
-	 * @param {BlockValidation[]} validation The validation results.
-	 */
-	constructor( clientId, validation ) {
-		this.type = SET_BLOCK_VALID;
-		this.clientId = clientId;
-		this.validation = validation;
-	}
-}
-
 /**
  * Updates whether a block is valid.
  *
@@ -41,5 +11,9 @@ export class SetBlockValidation {
  * @returns {SetBlockValidation} An action for redux.
  */
 export function setBlockValidation( clientID, validation ) {
-	return new SetBlockValidation( clientID, validation );
+	return {
+		type: SET_BLOCK_VALID,
+		clientId: clientID,
+		validation: validation,
+	};
 }
--- js/src/redux/reducers/schemaBlocks.js
+++ js/src/redux/reducers/schemaBlocks.js
@@ -2,6 +2,8 @@ import { SET_BLOCK_VALID } from "../actions/schemaBlocks";
 
 const initialState = {};
 
+let counter = 1;
+
 /**
  * A reducer for the Schema blocks.
  *
@@ -14,6 +16,7 @@ export default function schemaBlocksReducer( state = initialState, action ) {
 	switch ( action.type ) {
 		case SET_BLOCK_VALID: {
 			const validationResult = { [ action.clientId ]: action.validation };
+			console.log( counter++ + " : " + validationResult );
 			return Object.assign( {}, state, validationResult );
 		}
 		default: {
