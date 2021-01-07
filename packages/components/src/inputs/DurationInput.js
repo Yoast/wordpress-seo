375e54f6377749e4e16005ef86216698ed8bef05
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2021-01-07 15:27:58 +0100

    Add DurationInput including css and test

--- packages/components/src/inputs/DurationInput.js
+++ packages/components/src/inputs/DurationInput.js
@@ -1,41 +1,13 @@
+// External dependencies
 import React, { useCallback } from "react";
 import PropTypes from "prop-types";
-import FieldGroup, { FieldGroupDefaultProps, FieldGroupProps } from "../field-group/FieldGroup";
-// Import the required CSS.
+import { clamp, get } from "lodash";
+import { __ } from "@wordpress/i18n";
+
+// Internal dependencies
 import "./input.css";
+import FieldGroup, { FieldGroupDefaultProps, FieldGroupProps } from "../field-group/FieldGroup";
 import { getId } from "../GenerateId";
-import { clamp } from "lodash";
-
-const getValidTimeValue = ( value, min, max ) => {
-	console.log();
-	if ( min && value < min ) {
-		return min;
-	}
-	if ( max && value > max ) {
-		return max;
-	}
-	return value;
-}
-
-/**
- * Handles the onChange event.
- *
- * @param {func} onChange The onChange function.
- *
- * @returns {func} Function that call the onChange function with event.target.value.
- */
-const onChangeHandler = ( onChange, field, min = 0, max = Number.MAX_VALUE ) => {
-	return event => {
-		let value = parseInt( event.target.value, 10 );
-		console.log( event.target );
-		value = getValidTimeValue( value, min, max );
-		onChange( value, field );
-	};
-};
-
-// useCallback (ALS DAT MAG VAN MENEER REACT )
-// clamp
-// getAttr van target
 
 /**
  * Renders a input field of type text for use in our forms.
@@ -45,11 +17,18 @@ const onChangeHandler = ( onChange, field, min = 0, max = Number.MAX_VALUE ) => 
  * @returns {React.Component} Component that can be used inside a form.
  */
 const DurationInput = ( props ) => {
-	const memoizedOnChange = useCallback( ( event, field ) => {
-		let value = parseInt( event.target.value, 10 );
-		// value = getValidTimeValue( value, min, max );
-		// onChange( value, field );
-	} );
+	const onChangeHandler = useCallback(
+		( event ) => {
+			let value = event.target.value;
+			value = parseInt( event.target.value, 10 ) || 0;
+			const min = get( event, "target.attributes.min.value", Number.MIN_VALUE );
+			const max = get( event, "target.attributes.max.value", Number.MAX_VALUE );
+			const name = get( event, "target.attributes.name.value", "" );
+			value = clamp( value, min, max );
+			props.onChange( value, name );
+		},
+		[ props.onChange ]
+	);
 
 	const id = getId( props.id );
 	const fieldGroupProps = {
@@ -57,69 +36,74 @@ const DurationInput = ( props ) => {
 		htmlFor: id,
 	};
 
-	console.log( typeof props.hours, typeof props.minutes, typeof props.seconds );
-	console.log( props );
-
 	return (
 		<FieldGroup { ...fieldGroupProps }>
-			<div
-				style={ {
-					display: "flex",
-					flexDirection: "row",
-				} }
-			>
-				<input
-					id={ id + "-hours" }
-					name={ "hours" }
-					value={ props.hours }
-					type={ "number" }
-					className="yoast-field-group__inputfield duration-input"
-					aria-describedby={ props.ariaDescribedBy }
-					placeholder={ "HH" }
-					readOnly={ props.readOnly }
-					min={ 0 }
-					max={ props.max }
-					step={ props.step }
-					onChange={ onChangeHandler( props.onChange, "hours", 0 ) }
-				/>
-				<input
-					id={ id + "-minutes" }
-					name={ "minutes" }
-					value={ props.minutes }
-					type={ "number" }
-					className="yoast-field-group__inputfield duration-input"
-					aria-describedby={ props.ariaDescribedBy }
-					placeholder={ "MM" }
-					readOnly={ props.readOnly }
-					min={ 0 }
-					max={ 59 }
-					step={ props.step }
-					onChange={ onChangeHandler( props.onChange, "minutes", 0, 59 ) }
-				/>
-				<input
-					id={ id + "-seconds" }
-					name={ "seconds" }
-					value={ props.seconds }
-					type={ "number" }
-					className="yoast-field-group__inputfield duration-input"
-					aria-describedby={ props.ariaDescribedBy }
-					placeholder={ "SS" }
-					readOnly={ props.readOnly }
-					min={ 0 }
-					max={ 59 }
-					step={ props.step }
-					onChange={ onChangeHandler( props.onChange, "seconds", 0, 59 ) }
-				/>
+			<div className="duration-inputs-wrapper">
+				<div className="duration-input-label-pair">
+					<label htmlFor="duration-input-hours">{ __( "hours", "yoast-components" ) }</label>
+					<input
+						id={ id + "-hours" }
+						name={ "hours" }
+						value={ props.hours }
+						type={ "number" }
+						className="yoast-field-group__inputfield duration-input"
+						aria-describedby={ props.hoursAriaDescribedBy }
+						readOnly={ props.readOnly }
+						min={ 0 }
+						onChange={ onChangeHandler }
+					/>
+				</div>
+				<div className="duration-input-label-pair">
+					<label htmlFor="duration-input-minutes">{ __( "minutes", "yoast-components" ) }</label>
+					<input
+						id={ id + "-minutes" }
+						name={ "minutes" }
+						value={ props.minutes }
+						type={ "number" }
+						className="yoast-field-group__inputfield duration-input"
+						aria-describedby={ props.minutesAriaDescribedBy }
+						readOnly={ props.readOnly }
+						min={ 0 }
+						max={ 59 }
+						onChange={ onChangeHandler }
+					/>
+				</div>
+				<div className="duration-input-label-pair">
+					<label htmlFor="duration-input-seconds">{ __( "seconds", "yoast-components" ) }</label>
+					<input
+						id={ id + "-seconds" }
+						name={ "seconds" }
+						value={ props.seconds }
+						type={ "number" }
+						className="yoast-field-group__inputfield duration-input"
+						aria-describedby={ props.secondsAriaDescribedBy }
+						readOnly={ props.readOnly }
+						min={ 0 }
+						max={ 59 }
+						onChange={ onChangeHandler }
+					/>
+				</div>
 			</div>
 		</FieldGroup>
 	);
 };
 
 DurationInput.propTypes = {
+	hours: PropTypes.number.isRequired,
+	minutes: PropTypes.number.isRequired,
+	seconds: PropTypes.number.isRequired,
+	hoursAriaDescribedBy: PropTypes.string,
+	minutesAriaDescribedBy: PropTypes.string,
+	secondsAriaDescribedBy: PropTypes.string,
+	id: PropTypes.string,
 	...FieldGroupProps,
 };
 
 DurationInput.defaultProps = {
+	id: "",
+	hoursAriaDescribedBy: "",
+	minutesAriaDescribedBy: "",
+	secondsAriaDescribedBy: "",
 	...FieldGroupDefaultProps,
 };
 
