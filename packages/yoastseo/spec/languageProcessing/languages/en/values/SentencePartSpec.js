339d0c64fdf1936781db8a8495207ee866efe15b
FAMarfuaty <aida@yoast.com>
2021-01-08 15:24:20 +0100

    Add specs for English values files
    And improve the code style of the respected values files

--- packages/yoastseo/spec/languageProcessing/languages/en/values/SentencePartSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/en/values/SentencePartSpec.js
@@ -1,20 +1,19 @@
-import EnglishSentencePart from "../../../../src/languages/legacy/researches/english/passiveVoice/SentencePart.js";
+import EnglishSentencePart from "../../../../../src/languageProcessing/languages/en/values/SentencePart.js";
 
 describe( "creates a English sentence part", function() {
 	it( "makes sure the English sentence part inherits all functions", function() {
-		var mockPart = new EnglishSentencePart( "English texts are great.", [ "are" ], "en_US" );
+		const mockPart = new EnglishSentencePart( "English texts are great.", [ "are" ] );
 		expect( mockPart.getSentencePartText() ).toBe( "English texts are great." );
 		expect( mockPart.getAuxiliaries() ).toEqual( [ "are" ] );
-		expect( mockPart.getLocale() ).toBe( "en_US" );
 	} );
 	it( "returns a irregular participle for an English sentence part", function() {
-		var mockPart = new EnglishSentencePart( "English texts are written.", [ "are" ], "en_US" );
+		const mockPart = new EnglishSentencePart( "English texts are written.", [ "are" ] );
 		expect( mockPart.getParticiples()[ 0 ].getParticiple() ).toBe( "written" );
 		expect( mockPart.getParticiples()[ 0 ].getType() ).toBe( "irregular" );
 		expect( mockPart.getParticiples()[ 0 ].determinesSentencePartIsPassive() ).toBe( true );
 	} );
 	it( "returns a regular participle for an English sentence part", function() {
-		var mockPart = new EnglishSentencePart( "The kitchen cabinets were nailed to the wall.", [ "are" ], "en_US" );
+		const mockPart = new EnglishSentencePart( "The kitchen cabinets were nailed to the wall.", [ "are" ] );
 		expect( mockPart.getParticiples()[ 0 ].getParticiple() ).toBe( "nailed" );
 		expect( mockPart.getParticiples()[ 0 ].getType() ).toBe( "regular" );
 		expect( mockPart.getParticiples()[ 0 ].determinesSentencePartIsPassive() ).toBe( true );
