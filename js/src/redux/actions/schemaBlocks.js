9f8f68d1aa1523a5043be7f6236fbf5b75566fcf
Joost Boomkamp <joostboomkamp@yoast.com>
2021-01-08 11:34:19 +0100

    Revert "New iteration on setting the validation on the Redux store."
    This reverts commit b2dd75f0a7f84ede36657564e6eb90ac0f341abd.

--- js/src/redux/actions/schemaBlocks.js
+++ js/src/redux/actions/schemaBlocks.js
@@ -1,31 +1,19 @@
 const PREFIX = "WPSEO_";
 
 export const SET_BLOCK_VALID = `${ PREFIX }SET_BLOCK_VALIDATION`;
-export const SET_INNER_BLOCK_VALID = `${ PREFIX }SET_BLOCK_VALIDATION`;
 
 /**
  * Updates whether a block is valid.
  *
  * @param {string} clientID   The client ID of the block.
- * @param {string} blockName  The block name.
- * @param {object} validation The validation result for the block.
+ * @param {BlockValidation} validation The validation result for the block.
  *
- * @returns {object} An action for redux.
+ * @returns {SetBlockValidation} An action for redux.
  */
-export function setBlockValidation( clientID, blockName, validation ) {
+export function setBlockValidation( clientID, validation ) {
 	return {
 		type: SET_BLOCK_VALID,
 		clientId: clientID,
-		blockName: blockName,
 		validation: validation,
-	};
-}
-
-export function setInnerBlockValidation( parentClientId, blockName, validation ) {
-	return {
-		type: SET_INNER_BLOCK_VALID,
-		parentClientId,
-		blockName,
-		validation,
 	};
 }
--- js/src/redux/reducers/schemaBlocks.js
+++ js/src/redux/reducers/schemaBlocks.js
@@ -1,4 +1,4 @@
-import { SET_BLOCK_VALID } from "../actions";
+import { SET_BLOCK_VALID } from "../actions/schemaBlocks";
 
 const initialState = {};
 
@@ -6,24 +6,15 @@ const initialState = {};
  * A reducer for the Schema blocks.
  *
  * @param {Object} state  The current state of the object.
- * @param {Object} action The received action.
+ * @param {SetBlockValidation} action The received action.
  *
  * @returns {Object} The state.
  */
 export default function schemaBlocksReducer( state = initialState, action ) {
 	switch ( action.type ) {
 		case SET_BLOCK_VALID: {
-			const blockResults = Object.assign( {}, state[ action.clientId ] );
-
-			if ( ! blockResults[ action.blockName ] ) {
-				blockResults[ action.blockName ] = [];
-			}
-
-			if ( ! blockResults[ action.blockName ].includes( action.validation ) ) {
-				blockResults[ action.blockName ].push( action.validation );
-			}
-
-			return Object.assign( {}, state, { [ action.clientId ]: blockResults } );
+			const validationResult = { [ action.clientId ]: action.validation };
+			return Object.assign( {}, state, validationResult );
 		}
 		default: {
 			return state;
