cbda9d2e31b9910b77e1f2e8dff5e8cbb21deec0
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2021-01-11 11:36:49 +0100

    Provide option of showing Alert in components app

--- apps/components/ImageSelectWrapper.js
+++ apps/components/ImageSelectWrapper.js
@@ -16,22 +16,39 @@ class ImageSelectWrapper extends React.Component {
 		this.state = {
 			imageUrl: "",
 			hasPreview: true,
+			warnings: [],
 		};
 		this.setImageUrl = this.setStateAttribute.bind( this, "imageUrl" );
 		this.changePreview = this.setStateAttribute.bind( this, "hasPreview" );
+		this.toggleWarning = this.toggleWarning.bind( this );
 	}
 
 	/**
-	 * @returns {void} Void.
+	 * State setting function.
 	 *
 	 * @param {Object} attr Attributes.
 	 * @param {string} value Value.
+	 *
+	 * @returns {void} Void.
 	 */
 	setStateAttribute( attr, value ) {
 		this.setState( state => ( {
 			...state,
 			[ attr ]: value,
 		} ) );
+	}
+
+	/**
+	 * Set filled array if warnings is empty. Empty array if warnings is full.
+	 *
+	 * @returns {void} Void.
+	 */
+	toggleWarning() {
+		if ( this.state.warnings.length === 0 ) {
+			this.setState( { warnings: [ "Oops! Something's wrong!" ] } );
+			return;
+		}
+		this.setState( { warnings: [] } );
 	}
 
 	/**
@@ -42,6 +59,7 @@ class ImageSelectWrapper extends React.Component {
 	render() {
 		return (
 			<Fragment>
+				<button onClick={ () => this.toggleWarning() }>Toggle warning</button>
 				<button onClick={ () => this.changePreview( ! this.state.hasPreview ) }>Toggle preview</button>
 				<div className="yoast">
 					<h2>ImageSelect</h2>
@@ -56,7 +74,7 @@ class ImageSelectWrapper extends React.Component {
 						removeImageButtonId="bla3"
 						onClick={ () => this.setImageUrl( "http://placekitten.com/g/200/300" ) }
 						onRemoveImageClick={ () => this.setImageUrl( "" ) }
-						warnings={ [ "Oepsiepoepsie!" ] }
+						warnings={ this.state.warnings }
 					/>
 				</div>
 			</Fragment>
