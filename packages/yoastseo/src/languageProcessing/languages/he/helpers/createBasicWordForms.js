77609d51d0d56ae50184b22e2acbe78b4cf3114f
FAMarfuaty <aida@yoast.com>
2021-01-12 11:56:41 +0100

    Improve code style in basic stemmer

--- packages/yoastseo/src/languageProcessing/languages/he/helpers/createBasicWordForms.js
+++ packages/yoastseo/src/languageProcessing/languages/he/helpers/createBasicWordForms.js
@@ -3,9 +3,9 @@
  *
  * @param {string} word     The word for which to create basic word forms.
  *
- * @returns {string}        Prefixed and de-prefixed variations of a word.
+ * @returns {string[]} Prefixed and de-prefixed variations of a word.
  */
-export function createBasicWordForms( word ) {
+export default function createBasicWordForms( word ) {
 	const prefixes = [ "ב", "ה", "ו", "כ", "ל", "מ", "ש" ];
 	const forms = [];
 
