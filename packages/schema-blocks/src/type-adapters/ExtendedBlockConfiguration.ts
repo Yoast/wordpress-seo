87e8faf22f7379534ebd5a9dca835d4b4a69697a
Johanna de Vos <johanna@yoast.com>
2021-01-14 17:28:32 +0100

    Move the ExtendedBlockConfiguration to another folder

--- packages/schema-blocks/src/type-adapters/ExtendedBlockConfiguration.ts
+++ packages/schema-blocks/src/type-adapters/ExtendedBlockConfiguration.ts
@@ -1,6 +1,8 @@
 import { BlockConfiguration } from "@wordpress/blocks";
 
 /*
+ * Adds the `variations` property to the BlockConfiguration type.
+ *
  * See https://developer.wordpress.org/block-editor/developers/block-api/block-registration/#variations-optional
  * for other options.
  */
