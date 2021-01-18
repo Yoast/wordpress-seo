ea73d97023c22edb68c638f6bc5163cf19b2d460
FAMarfuaty <aida@yoast.com>
2021-01-18 17:09:03 +0100

    Add specs for Researcher

--- packages/yoastseo/spec/languageProcessing/languages/pt/ResearcherSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/pt/ResearcherSpec.js
@@ -1,203 +1,115 @@
-import Researcher from "../../../../src/languageProcessing/languages/en/Researcher.js";
+import Researcher from "../../../../src/languageProcessing/languages/pt/Researcher.js";
 import Paper from "../../../../src/values/Paper.js";
 import getMorphologyData from "../../../specHelpers/getMorphologyData";
-const morphologyDataEN = getMorphologyData( "en" );
+const morphologyDataPT = getMorphologyData( "pt" );
 
-describe( "a test for the English Researcher", function() {
+describe( "a test for the Portuguese Researcher", function() {
 	const researcher = new Researcher( new Paper( "This is another paper!" ) );
+	const functionWordExamples = [ 	"aquilo", "àquele", "àquela", "àqueles", "àquelas", "àquilo", "este", "estes", "esta", "estas",
+		"àqueles", "aqueles", "aquele", "aquela", "aquelas", "aquilo", "esse", "esses", "essa", "essas", "isto", "isso" ];
 
 	it( "returns true if the inherited Abstract Researcher has a specific research", function() {
 		expect( researcher.hasResearch( "getParagraphLength" ) ).toBe( true );
 	} );
 
-	it( "returns true if the English Researcher has a specific research", function() {
+	it( "returns true if the Portuguese Researcher has a specific research", function() {
 		expect( researcher.hasResearch( "getPassiveVoice" ) ).toBe( true );
 	} );
 
-	it( "returns the English function words filtered at the end", function() {
-		expect( researcher.getConfig( "functionWords" ).filteredAtEnding ).toEqual(
-			[ "first", "second", "third", "fourth", "fifth", "sixth", "seventh", "eighth", "ninth", "tenth", "eleventh",
-				"twelfth", "thirteenth", "fourteenth", "fifteenth", "sixteenth", "seventeenth", "eighteenth", "nineteenth",
-				"twentieth", "doing", "daring", "having", "appearing", "becoming", "coming", "keeping", "remaining", "staying",
-				"saying", "asking", "stating", "seeming", "letting", "making", "setting", "showing", "putting", "adding", "going",
-				"using", "trying", "containing", "new", "newer", "newest", "old", "older", "oldest", "previous", "good",
-				"well", "better", "best", "big", "bigger", "biggest", "easy", "easier", "easiest", "fast", "faster", "fastest",
-				"far", "hard", "harder", "hardest", "least", "own", "large", "larger", "largest", "long", "longer", "longest",
-				"low", "lower", "lowest", "high", "higher", "highest", "regular", "simple", "simpler", "simplest", "small",
-				"smaller", "smallest", "tiny", "tinier", "tiniest", "short", "shorter", "shortest", "main", "actual", "nice",
-				"nicer", "nicest", "real", "same", "able", "certain", "usual", "so-called", "mainly", "mostly", "recent",
-				"anymore", "complete", "lately", "possible", "commonly", "constantly", "continually", "directly", "easily",
-				"nearly", "slightly", "somewhere", "estimated", "latest", "different", "similar", "widely", "bad", "worse",
-				"worst", "great", "specific", "available", "average", "awful", "awesome", "basic", "beautiful", "busy", "current",
-				"entire", "everywhere", "important", "major", "multiple", "normal", "necessary", "obvious", "partly", "special",
-				"last", "early", "earlier", "earliest", "young", "younger", "youngest", "" ]
-		);
+	it( "returns Portuguese function words", function() {
+		expect( researcher.getConfig( "functionWords" ).all ).toEqual( ( expect.arrayContaining( functionWordExamples ) ) );
 	} );
 
-	it( "returns the English first word exceptions", function() {
+	it( "returns the Portuguese first word exceptions", function() {
 		expect( researcher.getConfig( "firstWordExceptions" ) ).toEqual(
-			[ "the", "a", "an", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
-				"this", "that", "these", "those" ]
+			[ "o", "a", "os", "as", "um", "uma", "uns", "umas", "um", "dois", "três", "quatro", "cinco", "seis", "sete",
+				"oito", "nove", "dez", "este", "estes", "esta", "estas", "esse", "esses", "essa", "essas", "aquele", "aqueles",
+				"aquela", "aquelas", "isto", "isso", "aquilo" ]
 		);
 	} );
 
-	it( "returns the English transition words", function() {
-		expect( researcher.getConfig( "transitionWords" ) ).toEqual( [
-			"accordingly", "additionally", "afterward", "afterwards", "albeit", "also", "although", "altogether",
-			"another", "basically", "because", "before", "besides", "but", "certainly", "chiefly", "comparatively",
-			"concurrently", "consequently", "contrarily", "conversely", "correspondingly", "despite", "doubtedly", "during",
-			"e.g.", "earlier", "emphatically", "equally", "especially", "eventually", "evidently", "explicitly", "finally",
-			"firstly", "following", "formerly", "forthwith", "fourthly", "further", "furthermore", "generally", "hence",
-			"henceforth", "however", "i.e.", "identically", "indeed", "initially", "instead", "last", "lastly", "later", "lest",
-			"likewise", "markedly", "meanwhile", "moreover", "nevertheless", "nonetheless", "nor",  "notwithstanding", "obviously",
-			"occasionally", "otherwise", "overall", "particularly", "presently", "previously", "rather", "regardless", "secondly",
-			"shortly", "significantly", "similarly", "simultaneously", "since", "so", "soon", "specifically", "still", "straightaway",
-			"subsequently", "surely", "surprisingly", "than", "then", "thereafter", "therefore", "thereupon", "thirdly", "though",
-			"thus", "till", "undeniably", "undoubtedly", "unless", "unlike", "unquestionably", "until", "when", "whenever",
-			"whereas", "while", "above all", "after all", "after that", "all in all", "all of a sudden", "all things considered",
-			"analogous to", "although this may be true", "analogous to", "another key point", "as a matter of fact", "as a result",
-			"as an illustration", "as can be seen", "as has been noted", "as I have noted", "as I have said", "as I have shown",
-			"as long as", "as much as", "as opposed to", "as shown above", "as soon as", "as well as", "at any rate", "at first", "at last",
-			"at least", "at length", "at the present time", "at the same time", "at this instant", "at this point", "at this time",
-			"balanced against", "being that", "by all means", "by and large", "by comparison", "by the same token", "by the time",
-			"compared to", "be that as it may", "coupled with", "different from", "due to", "equally important", "even if",
-			"even more", "even so", "even though", "first thing to remember", "for example", "for fear that", "for instance",
-			"for one thing", "for that reason", "for the most part", "for the purpose of", "for the same reason", "for this purpose",
-			"for this reason", "from time to time", "given that", "given these points", "important to realize", "in a word", "in addition",
-			"in another case", "in any case", "in any event", "in brief", "in case", "in conclusion", "in contrast",
-			"in detail", "in due time", "in effect", "in either case", "in essence", "in fact", "in general", "in light of",
-			"in like fashion", "in like manner", "in order that", "in order to", "in other words", "in particular", "in reality",
-			"in short", "in similar fashion", "in spite of", "in sum", "in summary", "in that case", "in the event that",
-			"in the final analysis", "in the first place", "in the fourth place", "in the hope that", "in the light of",
-			"in the long run", "in the meantime", "in the same fashion", "in the same way", "in the second place",
-			"in the third place", "in this case", "in this situation", "in time", "in truth", "in view of", "inasmuch as",
-			"most compelling evidence", "most important", "must be remembered", "not only", "not to mention", "note that",
-			"now that", "of course", "on account of", "on balance", "on condition that", "on one hand", "on the condition that", "on the contrary",
-			"on the negative side", "on the other hand", "on the positive side", "on the whole", "on this occasion", "once",
-			"once in a while", 	"only if", "owing to", "point often overlooked", "prior to", "provided that", "seeing that",
-			"so as to", "so far", "so long as", "so that", "sooner or later", "such as", "summing up", "take the case of",
-			"that is", "that is to say", "then again", "this time", "to be sure", "to begin with", "to clarify", "to conclude",
-			"to demonstrate", "to emphasize", "to enumerate", "to explain", "to illustrate", "to list", "to point out",
-			"to put it another way", "to put it differently", "to repeat", "to rephrase it", "to say nothing of", "to sum up",
-			"to summarize", "to that end", "to the end that", "to this end", "together with", "under those circumstances", "until now",
-			"up against", "up to the present time", "vis a vis", "what's more", "while it may be true", "while this may be true",
-			"with attention to", "with the result that", "with this in mind", "with this intention", "with this purpose in mind",
-			"without a doubt", "without delay", "without doubt", "without reservation",
-		] );
+	it( "returns the Portuguese transition words", function() {
+		expect( researcher.getConfig( "transitionWords" ) ).toEqual(
+			[ "ademais", "afinal", "aliás", "analogamente", "anteriormente", "assim", "certamente", "conforme",
+				"conquanto", "contudo", "decerto", "embora", "enfim", "enquanto", "então", "entretanto", "eventualmente",
+				"igualmente", "inegavelmente", "inesperadamente", "mas", "outrossim", "pois", "porquanto", "porque", "portanto",
+				"posteriormente", "precipuamente", "primeiramente", "primordialmente", "principalmente", "salvo",
+				"semelhantemente", "similarmente", "sobretudo", "surpreendentemente", "todavia", "a fim de", "a fim de que",
+				"a menos que", "a princípio", "a saber", "acima de tudo", "ainda assim", "ainda mais", "ainda que", "além disso",
+				"antes de mais nada", "antes de tudo", "antes que", "ao mesmo tempo", "ao passo que", "ao propósito", "apesar de",
+				"às vezes", "assim como", "assim que", "assim sendo", "assim também", "bem como", "com a finalidade de",
+				"com efeito", "com o fim de", "com o intuito de", "com o propósito de", "com toda a certeza", "como resultado",
+				"como se", "da mesma forma", "de acordo com", "de conformidade com", "de fato", "de maneira idêntica",
+				"de tal forma que", "de tal sorte que", "depois que", "desde que", "dessa forma", "dessa maneira", "desse modo",
+				"do mesmo modo", "é provável", "em conclusão", "em contrapartida", "em contraste com", "em outras palavras",
+				"em primeiro lugar", "em princípio", "em resumo", "em seguida", "em segundo lugar", "em síntese", "em suma",
+				"em terceiro lugar", "em virtude de", "finalmente agora atualmente", "isto é", "já que", "logo após", "logo depois",
+				"logo que", "mesmo que", "não apenas", "nesse hiato", "nesse ínterim", "nesse meio tempo", "nesse sentido",
+				"no entanto", "no momento em que", "ou por outra", "ou seja", "para que", "pelo contrário", "por analogia",
+				"por causa de", "por certo", "por conseguinte", "por conseqüência", "por exemplo", "por fim", "por isso",
+				"por mais que", "por menos que", "por outro lado", "posto que", "se acaso", "se bem que", "seja como for",
+				"sem dúvida", "só para exemplificar", "só para ilustrar", "só que", "sob o mesmo ponto de vista",
+				"talvez provavelmente", "tanto quanto", "uma vez que", "visto que" ]
+		);
 	} );
 
-	it( "returns the English two part transition word", function() {
+	it( "returns the Portuguese two part transition word", function() {
 		expect( researcher.getConfig( "twoPartTransitionWords" ) ).toEqual(
-			[ [ "both", "and" ], [ "if", "then" ], [ "not only", "but also" ], [ "neither", "nor" ], [ "either", "or" ], [ "not", "but" ],
-				[ "whether", "or" ], [ "no sooner", "than" ] ]
+			[ [ "não apenas", "como também" ], [ "não só", "bem como" ], [ "não só", "como também" ], [ "não só", "mas também" ],
+				[ "ora", "ora" ], [ "ou", "ou" ], [ "quer", "quer" ] ]
 		);
 	} );
 
-	it( "returns a specified part of the English syllables data", function() {
-		expect( researcher.getConfig( "syllables" ).deviations.words.fragments ).toEqual(
-			{
-				global: [
-					{
-						word: "coyote",
-						syllables: 3,
-					},
-					{
-						word: "graveyard",
-						syllables: 2,
-					},
-					{
-						word: "lawyer",
-						syllables: 2,
-					},
-				],
-			}
-		);
-	} );
-
-	it( "returns the English stop words", function() {
-		expect( researcher.getConfig( "stopWords" ) ).toEqual(
+	it( "returns a specified part of the Portuguese syllables data", function() {
+		expect( researcher.getConfig( "syllables" ).deviations.words.full ).toEqual(
 			[
-				"to",
-				"which",
-				"who",
-				"whom",
-				"that",
-				"whose",
-				"after",
-				"although",
-				"as",
-				"because",
-				"before",
-				"even if",
-				"even though",
-				"how",
-				"if",
-				"in order that",
-				"inasmuch",
-				"lest",
-				"once",
-				"provided",
-				"since",
-				"so that",
-				"than",
-				"though",
-				"till",
-				"unless",
-				"until",
-				"when",
-				"whenever",
-				"where",
-				"whereas",
-				"wherever",
-				"whether",
-				"while",
-				"why",
-				"by the time",
-				"supposing",
-				"no matter",
-				"how",
-				"what",
-				"won't",
-				"do",
-				"does",
-				"–",
-				"and",
-				"but",
-				"or",
-			]
+				{ word: "delegacia", syllables: 5 },
+				{ word: "democracia", syllables: 5 },
+				{ word: "parceria", syllables: 4 },
+				{ word: "secretaria", syllables: 5 },
+			],
 		);
 	} );
 
-	it( "returns the English locale", function() {
-		expect( researcher.getConfig( "language" ) ).toEqual( "en" );
+	it( "returns the Portuguese stop words", function() {
+		expect( researcher.getConfig( "stopWords" ) ).toEqual(
+			[ "que ", "como", "e", "nem", "se", "caso", "conforme", "consoante", "porque", "pois", "segundo ", "enquanto",
+				"embora", "conquanto", "quanto menos", "quanto mais", "quando", "mal ", "apenas", "ora", "seja", "quer", "já",
+				"logo", "portanto", "por isso", "pois", "рог conseguinte", "ou seja ", "isto é", "quer dizer", "a saber",
+				"ou melhor", "mas", "também", "sim", "porém", "contudo", "senão", "todavia", "mas ainda", "no entanto", "entretanto" ]
+		);
 	} );
 
-	it( "returns the English passive construction type", function() {
+	it( "returns the Portuguese locale", function() {
+		expect( researcher.getConfig( "language" ) ).toEqual( "pt" );
+	} );
+
+	it( "returns the Portuguese passive construction type", function() {
 		expect( researcher.getConfig( "isPeriphrastic" ) ).toEqual( true );
 	} );
 
-	it( "stems a word using the English stemmer", function() {
-		researcher.addResearchData( "morphology", morphologyDataEN );
-		expect( researcher.getHelper( "getStemmer" )( researcher )( "cats" ) ).toEqual( "cat" );
+	it( "stems a word using the Portuguese stemmer", function() {
+		researcher.addResearchData( "morphology", morphologyDataPT );
+		expect( researcher.getHelper( "getStemmer" )( researcher )( "gatas" ) ).toEqual( "gat" );
 	} );
 
-	it( "splits English sentence into parts", function() {
-		const sentence =  "The English are still having a party.";
-		expect( researcher.getHelper( "getSentenceParts" )( sentence )[ 0 ].getSentencePartText() ).toBe( "are still" );
-		expect( researcher.getHelper( "getSentenceParts" )( sentence )[ 1 ].getSentencePartText() ).toBe( "having a party." );
+	it( "splits Portuguese sentence into parts", function() {
+		const sentence =  "Eles eram três amigos quando eram crianças.";
+		expect( researcher.getHelper( "getSentenceParts" )( sentence )[ 0 ].getSentencePartText() ).toBe( "eram três amigos" );
+		expect( researcher.getHelper( "getSentenceParts" )( sentence )[ 1 ].getSentencePartText() ).toBe( "eram crianças." );
 	} );
 
-	it( "checks if a English sentence is passive or not", function() {
-		expect( researcher.getHelper( "isPassiveSentencePart" )( "The cats are vaccinated.", [ "are" ] ) ).toEqual( true );
-		expect( researcher.getHelper( "isPassiveSentencePart" )( "The girl loves her cat.", [] ) ).toEqual( false );
+	it( "checks if a Portuguese sentence is passive or not", function() {
+		expect( researcher.getHelper( "isPassiveSentencePart" )( "Os gatos são amados.", [ "são" ] ) ).toEqual( true );
+		expect( researcher.getHelper( "isPassiveSentencePart" )( "A garota ama seus gatos.", [] ) ).toEqual( false );
 	} );
 
-	it( "calculates the Flesch reading score using the formula for English", function() {
+	it( "calculates the Flesch reading score using the formula for Portuguese", function() {
 		const statistics = {
-			numberOfWords: 400,
-			numberOfSyllables: 800,
-			averageWordsPerSentence: 20,
+			numberOfWords: 600,
+			numberOfSyllables: 1200,
+			averageWordsPerSentence: 19,
 		};
-		expect( researcher.getHelper( "fleschReadingScore" )( statistics ) ).toBe( 17.3 );
+		expect( researcher.getHelper( "fleschReadingScore" )( statistics ) ).toBe( 60.4 );
 	} );
 } );
