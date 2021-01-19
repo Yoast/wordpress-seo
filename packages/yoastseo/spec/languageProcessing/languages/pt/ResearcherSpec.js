350173cd3e8391a91465e8383e277f475fc84e4f
FAMarfuaty <aida@yoast.com>
2021-01-19 16:51:24 +0100

    Adjust the Researcher specs

--- packages/yoastseo/spec/languageProcessing/languages/pt/ResearcherSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/pt/ResearcherSpec.js
@@ -1,12 +1,16 @@
 import Researcher from "../../../../src/languageProcessing/languages/pt/Researcher.js";
 import Paper from "../../../../src/values/Paper.js";
 import getMorphologyData from "../../../specHelpers/getMorphologyData";
+import functionWords from "../../../../src/languageProcessing/languages/pt/config/functionWords";
+import firstWordExceptions from "../../../../src/languageProcessing/languages/pt/config/firstWordExceptions";
+import transitionWords from "../../../../src/languageProcessing/languages/pt/config/transitionWords";
+import twoPartTransitionWords from "../../../../src/languageProcessing/languages/pt/config/twoPartTransitionWords";
+import syllables from "../../../../src/languageProcessing/languages/pt/config/syllables.json";
+import stopWords from "../../../../src/languageProcessing/languages/pt/config/stopWords";
 const morphologyDataPT = getMorphologyData( "pt" );
 
 describe( "a test for the Portuguese Researcher", function() {
 	const researcher = new Researcher( new Paper( "This is another paper!" ) );
-	const functionWordExamples = [ 	"aquilo", "àquele", "àquela", "àqueles", "àquelas", "àquilo", "este", "estes", "esta", "estas",
-		"àqueles", "aqueles", "aquele", "aquela", "aquelas", "aquilo", "esse", "esses", "essa", "essas", "isto", "isso" ];
 
 	it( "returns true if the inherited Abstract Researcher has a specific research", function() {
 		expect( researcher.hasResearch( "getParagraphLength" ) ).toBe( true );
@@ -17,67 +21,27 @@ describe( "a test for the Portuguese Researcher", function() {
 	} );
 
 	it( "returns Portuguese function words", function() {
-		expect( researcher.getConfig( "functionWords" ).all ).toEqual( ( expect.arrayContaining( functionWordExamples ) ) );
+		expect( researcher.getConfig( "functionWords" ).all ).toEqual( functionWords.all );
 	} );
 
 	it( "returns the Portuguese first word exceptions", function() {
-		expect( researcher.getConfig( "firstWordExceptions" ) ).toEqual(
-			[ "o", "a", "os", "as", "um", "uma", "uns", "umas", "um", "dois", "três", "quatro", "cinco", "seis", "sete",
-				"oito", "nove", "dez", "este", "estes", "esta", "estas", "esse", "esses", "essa", "essas", "aquele", "aqueles",
-				"aquela", "aquelas", "isto", "isso", "aquilo" ]
-		);
+		expect( researcher.getConfig( "firstWordExceptions" ) ).toEqual( firstWordExceptions );
 	} );
 
 	it( "returns the Portuguese transition words", function() {
-		expect( researcher.getConfig( "transitionWords" ) ).toEqual(
-			[ "ademais", "afinal", "aliás", "analogamente", "anteriormente", "assim", "certamente", "conforme",
-				"conquanto", "contudo", "decerto", "embora", "enfim", "enquanto", "então", "entretanto", "eventualmente",
-				"igualmente", "inegavelmente", "inesperadamente", "mas", "outrossim", "pois", "porquanto", "porque", "portanto",
-				"posteriormente", "precipuamente", "primeiramente", "primordialmente", "principalmente", "salvo",
-				"semelhantemente", "similarmente", "sobretudo", "surpreendentemente", "todavia", "a fim de", "a fim de que",
-				"a menos que", "a princípio", "a saber", "acima de tudo", "ainda assim", "ainda mais", "ainda que", "além disso",
-				"antes de mais nada", "antes de tudo", "antes que", "ao mesmo tempo", "ao passo que", "ao propósito", "apesar de",
-				"às vezes", "assim como", "assim que", "assim sendo", "assim também", "bem como", "com a finalidade de",
-				"com efeito", "com o fim de", "com o intuito de", "com o propósito de", "com toda a certeza", "como resultado",
-				"como se", "da mesma forma", "de acordo com", "de conformidade com", "de fato", "de maneira idêntica",
-				"de tal forma que", "de tal sorte que", "depois que", "desde que", "dessa forma", "dessa maneira", "desse modo",
-				"do mesmo modo", "é provável", "em conclusão", "em contrapartida", "em contraste com", "em outras palavras",
-				"em primeiro lugar", "em princípio", "em resumo", "em seguida", "em segundo lugar", "em síntese", "em suma",
-				"em terceiro lugar", "em virtude de", "finalmente agora atualmente", "isto é", "já que", "logo após", "logo depois",
-				"logo que", "mesmo que", "não apenas", "nesse hiato", "nesse ínterim", "nesse meio tempo", "nesse sentido",
-				"no entanto", "no momento em que", "ou por outra", "ou seja", "para que", "pelo contrário", "por analogia",
-				"por causa de", "por certo", "por conseguinte", "por conseqüência", "por exemplo", "por fim", "por isso",
-				"por mais que", "por menos que", "por outro lado", "posto que", "se acaso", "se bem que", "seja como for",
-				"sem dúvida", "só para exemplificar", "só para ilustrar", "só que", "sob o mesmo ponto de vista",
-				"talvez provavelmente", "tanto quanto", "uma vez que", "visto que" ]
-		);
+		expect( researcher.getConfig( "transitionWords" ).allWords ).toEqual( transitionWords.allWords );
 	} );
 
 	it( "returns the Portuguese two part transition word", function() {
-		expect( researcher.getConfig( "twoPartTransitionWords" ) ).toEqual(
-			[ [ "não apenas", "como também" ], [ "não só", "bem como" ], [ "não só", "como também" ], [ "não só", "mas também" ],
-				[ "ora", "ora" ], [ "ou", "ou" ], [ "quer", "quer" ] ]
-		);
+		expect( researcher.getConfig( "twoPartTransitionWords" ) ).toEqual( twoPartTransitionWords );
 	} );
 
 	it( "returns a specified part of the Portuguese syllables data", function() {
-		expect( researcher.getConfig( "syllables" ).deviations.words.full ).toEqual(
-			[
-				{ word: "delegacia", syllables: 5 },
-				{ word: "democracia", syllables: 5 },
-				{ word: "parceria", syllables: 4 },
-				{ word: "secretaria", syllables: 5 },
-			],
-		);
+		expect( researcher.getConfig( "syllables" ) ).toEqual( syllables );
 	} );
 
 	it( "returns the Portuguese stop words", function() {
-		expect( researcher.getConfig( "stopWords" ) ).toEqual(
-			[ "que ", "como", "e", "nem", "se", "caso", "conforme", "consoante", "porque", "pois", "segundo ", "enquanto",
-				"embora", "conquanto", "quanto menos", "quanto mais", "quando", "mal ", "apenas", "ora", "seja", "quer", "já",
-				"logo", "portanto", "por isso", "pois", "рог conseguinte", "ou seja ", "isto é", "quer dizer", "a saber",
-				"ou melhor", "mas", "também", "sim", "porém", "contudo", "senão", "todavia", "mas ainda", "no entanto", "entretanto" ]
-		);
+		expect( researcher.getConfig( "stopWords" ) ).toEqual( stopWords );
 	} );
 
 	it( "returns the Portuguese locale", function() {
