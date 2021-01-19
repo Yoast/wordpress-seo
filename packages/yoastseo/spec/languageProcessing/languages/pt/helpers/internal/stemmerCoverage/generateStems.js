48241ce7bb707e41584d83a96ab4d174581a65dc
FAMarfuaty <aida@yoast.com>
2021-01-19 10:56:32 +0100

    Add the missing morphology data and function words to generateStems file

--- packages/yoastseo/spec/languageProcessing/languages/pt/helpers/internal/stemmerCoverage/generateStems.js
+++ packages/yoastseo/spec/languageProcessing/languages/pt/helpers/internal/stemmerCoverage/generateStems.js
@@ -1,12 +1,16 @@
 /* eslint-disable no-console */
 import stem from "../../../../../../../src/languageProcessing/languages/pt/helpers/internal/stem";
 import filterFunctionWordsFromArray from "../../../../../../../src/languageProcessing/helpers/word/filterWordsFromArray.js";
+import getMorphologyData from "../../../../../../specHelpers/getMorphologyData";
 import sampleVocabulary from "./sampleVocabulary.json";
+import { all as functionWords } from "../../../../../../../src/languageProcessing/languages/pt/config/functionWords";
 
-const wordsToStemWithoutFunctionWords = filterFunctionWordsFromArray( sampleVocabulary.words, "pt" );
+const morphologyData = getMorphologyData( "pt" ).pt;
+
+const wordsToStemWithoutFunctionWords = filterFunctionWordsFromArray( sampleVocabulary.words, functionWords );
 
 describe( "Generate stems for Portuguese words", () => {
-	const corpusWithStems = wordsToStemWithoutFunctionWords.map( word => [ word, stem( word ) ] );
+	const corpusWithStems = wordsToStemWithoutFunctionWords.map( word => [ word, stem( word, morphologyData ) ] );
 
 	console.log( JSON.stringify( corpusWithStems ) );
 } );
