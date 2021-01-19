1b22a977cdc342dad1103b3b4120bdceb20c6fb4
FAMarfuaty <aida@yoast.com>
2021-01-19 17:13:09 +0100

    Fix small typo

--- packages/yoastseo/spec/languageProcessing/languages/nl/helpers/internal/stemTOrDFromEndOfWordSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/nl/helpers/internal/stemTOrDFromEndOfWordSpec.js
@@ -10,7 +10,7 @@ describe( "Creates stems of words with ambiguous endings -d/-t", () => {
 	it( "Removes the -d from the end of a stem which was not matched in a previous check", () => {
 		expect( stemTOrDFromEndOfWord( morphologyDataNL, "wend", "wend" ) ).toEqual( "wen" );
 	} );
-	it( "Does not remove the -d from the end of a stem if the word is matched in detectAndStemRegularParticiple", () => {
+	it( "Does not remove the -t from the end of a stem if the word is matched in detectAndStemRegularParticiple", () => {
 		expect( stemTOrDFromEndOfWord( morphologyDataNL, "gebruikt", "gebruikt" ) ).toEqual( null );
 	} );
 	it( "Does not remove the -d from the end of a stem if the word ends in -heid", () => {
