28f32fab8c573af53ac4253fd793dcd33371210a
FAMarfuaty <aida@yoast.com>
2021-01-20 13:15:12 +0100

    Adjust the specs for meta description assessments

--- packages/yoastseo/spec/scoring/assessments/seo/MetaDescriptionKeywordAssessmentSpec.js
+++ packages/yoastseo/spec/scoring/assessments/seo/MetaDescriptionKeywordAssessmentSpec.js
@@ -15,23 +15,29 @@ describe( "the metadescription keyword assessment", function() {
 		const assessment = new MetaDescriptionKeywordAssessment().getResult( mockPaper, mockResearcherNoMatches, i18n );
 
 		expect( assessment.getScore() ).toBe( 3 );
-		expect( assessment.getText() ).toBe( "<a href='https://yoa.st/33k' target='_blank'>Keyphrase in meta description</a>: The meta description has been specified, but it does not contain the keyphrase. <a href='https://yoa.st/33l' target='_blank'>Fix that</a>!" );
+		expect( assessment.getText() ).toBe( "<a href='https://yoa.st/33k' target='_blank'>Keyphrase in meta description</a>: " +
+			"The meta description has been specified, but it does not contain the keyphrase. " +
+			"<a href='https://yoa.st/33l' target='_blank'>Fix that</a>!" );
 	} );
 
-	it( "returns a good result and an appropriate feedback message when at least one sentence contains every keyword term at least once in the same sentence.", function() {
+	it( "returns a good result and an appropriate feedback message when at least one sentence contains every keyword term " +
+		"at least once in the same sentence.", function() {
 		const mockPaper = new Paper();
 		const assessment = new MetaDescriptionKeywordAssessment().getResult( mockPaper, mockResearcherOneMatch, i18n );
 
 		expect( assessment.getScore() ).toBe( 9 );
-		expect( assessment.getText() ).toBe( "<a href='https://yoa.st/33k' target='_blank'>Keyphrase in meta description</a>: Keyphrase or synonym appear in the meta description. Well done!" );
+		expect( assessment.getText() ).toBe( "<a href='https://yoa.st/33k' target='_blank'>Keyphrase in meta description</a>: " +
+			"Keyphrase or synonym appear in the meta description. Well done!" );
 	} );
 
-	it( "returns a good result and an appropriate feedback message when the meta description contains the keyword two times in the same sentence", function() {
+	it( "returns a good result and an appropriate feedback message when the meta description contains the keyword " +
+		"two times in the same sentence", function() {
 		const mockPaper = new Paper();
 		const assessment = new MetaDescriptionKeywordAssessment().getResult( mockPaper, mockResearcherTwoMatches, i18n );
 
 		expect( assessment.getScore() ).toBe( 9 );
-		expect( assessment.getText() ).toBe( "<a href='https://yoa.st/33k' target='_blank'>Keyphrase in meta description</a>: Keyphrase or synonym appear in the meta description. Well done!" );
+		expect( assessment.getText() ).toBe( "<a href='https://yoa.st/33k' target='_blank'>Keyphrase in meta " +
+			"description</a>: Keyphrase or synonym appear in the meta description. Well done!" );
 	} );
 
 	it( "returns a bad result when the meta description contains the keyword three times in the same sentence", function() {
@@ -39,7 +45,9 @@ describe( "the metadescription keyword assessment", function() {
 		const assessment = new MetaDescriptionKeywordAssessment().getResult( mockPaper, mockResearcherThreeMatches, i18n );
 
 		expect( assessment.getScore() ).toBe( 3 );
-		expect( assessment.getText() ).toBe( "<a href='https://yoa.st/33k' target='_blank'>Keyphrase in meta description</a>: The meta description contains the keyphrase 3 times, which is over the advised maximum of 2 times. <a href='https://yoa.st/33l' target='_blank'>Limit that</a>!" );
+		expect( assessment.getText() ).toBe( "<a href='https://yoa.st/33k' target='_blank'>Keyphrase in meta " +
+			"description</a>: The meta description contains the keyphrase 3 times, which is over the advised maximum " +
+			"of 2 times. <a href='https://yoa.st/33l' target='_blank'>Limit that</a>!" );
 	} );
 
 	it( "is not applicable when the paper doesn't have a keyword", function() {
@@ -53,7 +61,8 @@ describe( "the metadescription keyword assessment", function() {
 	} );
 
 	it( "is  applicable when the paper has a meta description and a keyword", function() {
-		const isApplicableResult = new MetaDescriptionKeywordAssessment().isApplicable( new Paper( "text", { keyword: "keyword", description: "description" } ) );
+		const isApplicableResult = new MetaDescriptionKeywordAssessment().isApplicable( new Paper( "text",
+			{ keyword: "keyword", description: "description" } ) );
 		expect( isApplicableResult ).toBe( true );
 	} );
 } );
