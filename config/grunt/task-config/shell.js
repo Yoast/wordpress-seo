9373c717943c7cb9f05c1287e5fe6e16709f4f2c
Joost Boomkamp <joostboomkamp@yoast.com>
2021-01-21 15:26:26 +0100

    fix line length cs error

--- config/grunt/task-config/shell.js
+++ config/grunt/task-config/shell.js
@@ -206,7 +206,8 @@ module.exports = function( grunt ) {
 
 		"install-schema-blocks": {
 			// If a src directory exists in the schema-blocks but not dist directory then it needs to be built.
-			command: "echo schema blocks is still in development. run [yarn build] in the linked monorepo folder /src/schemablocks if you want to use schema blocks.",
+			command: "echo schema blocks is still in development. run [yarn build] " +
+					 " in the linked monorepo folder /src/schemablocks if you want to use schema blocks.",
 		},
 
 		"check-for-uncommitted-changes": {
