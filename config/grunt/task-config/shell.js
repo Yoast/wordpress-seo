a8fa253b9519b38a20594d975acf5c8c6bcce807
Hans-Christiaan <hans-christiaan@yoast.com>
2021-01-26 11:41:04 +0100

    Reintroduce install-schema-blocks shell command.

--- config/grunt/task-config/shell.js
+++ config/grunt/task-config/shell.js
@@ -206,8 +206,9 @@ module.exports = function( grunt ) {
 
 		"install-schema-blocks": {
 			// If a src directory exists in the schema-blocks but not dist directory then it needs to be built.
-			command: "echo schema blocks is still in development. run [yarn build] " +
-					 " in the linked monorepo folder /src/schemablocks if you want to use schema blocks.",
+			command: "if [ -d node_modules/@yoast/schema-blocks/src ] && [ ! -d node_modules/@yoast/schema-blocks/dist ]; then " +
+					 "cd node_modules/@yoast/schema-blocks && yarn install && yarn build; " +
+					 "fi",
 		},
 
 		"check-for-uncommitted-changes": {
