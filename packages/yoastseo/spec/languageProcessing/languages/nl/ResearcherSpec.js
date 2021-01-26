7dcfbd2f56aac7db4d75558bd459f350756f207b
FAMarfuaty <aida@yoast.com>
2021-01-26 09:56:44 +0100

    Adjust the way the Dutch function words are imported in the Researcher
    Also adjust the specs

--- packages/yoastseo/spec/languageProcessing/languages/nl/ResearcherSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/nl/ResearcherSpec.js
@@ -21,7 +21,7 @@ describe( "a test for the Dutch Researcher", function() {
 	} );
 
 	it( "returns the Dutch function words", function() {
-		expect( researcher.getConfig( "functionWords" ).all ).toEqual( functionWords.all );
+		expect( researcher.getConfig( "functionWords" ) ).toEqual( functionWords.all );
 	} );
 
 	it( "returns the Dutch stopwords", function() {
@@ -33,7 +33,7 @@ describe( "a test for the Dutch Researcher", function() {
 	} );
 
 	it( "returns the Dutch transition words", function() {
-		expect( researcher.getConfig( "transitionWords" ).allWords ).toEqual( transitionWords.allWords );
+		expect( researcher.getConfig( "transitionWords" ) ).toEqual( transitionWords );
 	} );
 
 	it( "returns the Dutch two part transition word", function() {
