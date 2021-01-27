0d3c69fff239974bfbcd23600e875c4ae795238d
Hans-Christiaan <hans-christiaan@yoast.com>
2021-01-27 15:29:59 +0100

    Adds tests for DocumentSidebar container.

---
+++ js/tests/containers/DocumentSidebar.test.js
@@ -0,0 +1,73 @@
+import { mapSelectToProps, mapDispatchToProps } from "../../src/containers/DocumentSidebar";
+
+describe( "The DocumentSidebar container", () => {
+	it( "maps the select function to props", () => {
+		const getResultsForFocusKeyword = jest.fn().mockReturnValue( {
+			overallScore: 60,
+		} );
+
+		const getReadabilityResults = jest.fn().mockReturnValue( {
+			overallScore: 100,
+		} );
+
+		const getPreferences = jest.fn().mockReturnValue( {
+			isKeywordAnalysisActive: true,
+			isContentAnalysisActive: true,
+		} );
+
+		const getSchemaBlocksValidationResults = jest.fn().mockReturnValue( {
+			"1234-abcde": {
+				result: 1,
+			},
+		} );
+
+		const selectors = {
+			getResultsForFocusKeyword,
+			getReadabilityResults,
+			getPreferences,
+			getSchemaBlocksValidationResults,
+		};
+
+		const select = jest.fn();
+
+		select.mockReturnValue( selectors );
+
+		const props = mapSelectToProps( select );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "Readability analysis:",
+			score: "good",
+			scoreValue: "Good",
+		} );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "SEO analysis:",
+			score: "ok",
+			scoreValue: "OK",
+		} );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "Schema analysis:",
+			score: "bad",
+			scoreValue: "Needs improvement",
+		} );
+
+		expect( props.shouldShowIntro ).toEqual( false );
+	} );
+
+	it( "maps the dispatch function to props", () => {
+		const dispatchers = {
+			openGeneralSidebar: jest.fn(),
+		};
+
+		const dispatch = jest.fn();
+
+		dispatch.mockReturnValue( dispatchers );
+
+		const { onClick } = mapDispatchToProps( dispatch );
+
+		onClick();
+
+		expect( dispatchers.openGeneralSidebar ).toHaveBeenCalledWith( "yoast-seo/seo-sidebar" );
+	} );
+} );
