0761d1512d3a9f0d34dc2f6897e9572606b9bf3a
marijnyoast <marijn@yoast.com>
2021-02-02 16:29:32 +0100

    introduce isAlertDismissed selector

--- js/src/redux/selectors/dismissedAlerts.js
+++ js/src/redux/selectors/dismissedAlerts.js
@@ -5,8 +5,23 @@ import { get } from "lodash";
  *
  * @param {Object} state The state.
  *
- * @returns {Array} The dismissedAlerts object.
+ * @returns {Object} The dismissedAlerts object.
  */
 export function getDismissedAlerts( state ) {
 	return get( state, "editor.dismissedAlerts", true );
+}
+
+/**
+ * Checks whether a certain Alert is dismissed.
+ *
+ * @param {Object} state The state.
+ * @param {string} alertKey The key of the Alert.
+ *
+ * @returns {Boolean} Whether or not the Alert is dismissed.
+ */
+export function isAlertDismissed( state, alertKey ) {
+	if ( state.dismissedAlerts[ alertKey ] ) {
+		return true;
+	}
+	return false;
 }
