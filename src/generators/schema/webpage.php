0af4366c07a3c2e968dbf5e06bf6b744f021497c
Omar Reiss <omar@yoast.com>
2021-02-03 22:57:37 +0100

    Always add breadcrumbs for webpage schema unless home page is static

--- src/generators/schema/webpage.php
+++ src/generators/schema/webpage.php
@@ -105,11 +105,7 @@ class WebPage extends Abstract_Schema_Piece {
 			return false;
 		}
 
-		if ( $this->context->breadcrumbs_enabled ) {
-			return true;
-		}
-
-		return false;
+		return true;
 	}
 
 	/**
