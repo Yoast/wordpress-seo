b6216415477e0e929a8ac84b50e61c1e00af228b
noud-github <arnoud@yoast.com>
2021-02-09 13:30:28 +0100

    moved edit distance

--- config/grunt/custom-tasks/update-readme-jenkins.js
+++ config/grunt/custom-tasks/update-readme-jenkins.js
@@ -2,46 +2,7 @@ function escapeRegExp(string) {
 	return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
   }
 
-function similarity(s1, s2) {
-	var longer = s1;
-	var shorter = s2;
-	if (s1.length < s2.length) {
-	  longer = s2;
-	  shorter = s1;
-	}
-	var longerLength = longer.length;
-	if (longerLength == 0) {
-	  return 1.0;
-	}
-	return (longerLength - editDistance(longer, shorter)) / parseFloat(longerLength);
-  }
 
-  function editDistance(s1, s2) {
-	s1 = s1.toLowerCase();
-	s2 = s2.toLowerCase();
-  
-	var costs = new Array();
-	for (var i = 0; i <= s1.length; i++) {
-	  var lastValue = i;
-	  for (var j = 0; j <= s2.length; j++) {
-		if (i == 0)
-		  costs[j] = j;
-		else {
-		  if (j > 0) {
-			var newValue = costs[j - 1];
-			if (s1.charAt(i - 1) != s2.charAt(j - 1))
-			  newValue = Math.min(Math.min(newValue, lastValue),
-				costs[j]) + 1;
-			costs[j - 1] = lastValue;
-			lastValue = newValue;
-		  }
-		}
-	  }
-	  if (i > 0)
-		costs[s2.length] = lastValue;
-	}
-	return costs[s2.length];
-  }
 
 class ChangelogBuilder {
 	constructor(grunt , changelogIn) {
@@ -130,12 +91,12 @@ class Unique {
 			var arrlen = this.items.length;
 			for (var j = i+1; j<arrlen; j++) {
 				
-				if (similarity(this.items[i], this.items[j]) > 0.9) {
+				if (thsi.#similarity(this.items[i], this.items[j]) > 0.9) {
 					toBeRemoved.push(j)
 					this.grunt.verbose.writeln ("---------------")
 					this.grunt.verbose.writeln (`${j}: ${this.items[j]}`)
 					this.grunt.verbose.writeln (`${i}: ${this.items[i]}`)
-					this.grunt.verbose.writeln (`${similarity(this.items[i], this.items[j])}`)
+					this.grunt.verbose.writeln (`${this.#similarity(this.items[i], this.items[j])}`)
 					this.grunt.verbose.writeln ("---------------")
 				}
 			}
@@ -149,6 +110,47 @@ class Unique {
 		}
 		
 	}
+
+	#similarity(s1, s2) {
+		var longer = s1;
+		var shorter = s2;
+		if (s1.length < s2.length) {
+		  longer = s2;
+		  shorter = s1;
+		}
+		var longerLength = longer.length;
+		if (longerLength == 0) {
+		  return 1.0;
+		}
+		return (longerLength - this.#editDistance(longer, shorter)) / parseFloat(longerLength);
+	  }
+	
+	  #editDistance(s1, s2) {
+		s1 = s1.toLowerCase();
+		s2 = s2.toLowerCase();
+	  
+		var costs = new Array();
+		for (var i = 0; i <= s1.length; i++) {
+		  var lastValue = i;
+		  for (var j = 0; j <= s2.length; j++) {
+			if (i == 0)
+			  costs[j] = j;
+			else {
+			  if (j > 0) {
+				var newValue = costs[j - 1];
+				if (s1.charAt(i - 1) != s2.charAt(j - 1))
+				  newValue = Math.min(Math.min(newValue, lastValue),
+					costs[j]) + 1;
+				costs[j - 1] = lastValue;
+				lastValue = newValue;
+			  }
+			}
+		  }
+		  if (i > 0)
+			costs[s2.length] = lastValue;
+		}
+		return costs[s2.length];
+	  }
 
  };
 
