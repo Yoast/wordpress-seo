0e665f6c38ac4ec84fb9bb663fb9bf05df462c81
Andrea Fercia <a.fercia@gmail.com>
2021-02-10 09:16:08 +0100

    Add test for the Help Link Presenter.

--- src/presenters/admin/help-link-presenter.php
+++ src/presenters/admin/help-link-presenter.php
@@ -72,7 +72,7 @@ class Help_Link_Presenter extends Abstract_Presenter {
 
 		if ( $this->opens_in_new_browser_tab ) {
 			$target_blank_attribute = ' target="_blank"';
-			$new_tab_message        = ' ' . __( '(Opens in a new browser tab)', 'wordpress-seo' );
+			$new_tab_message        = ' ' . \__( '(Opens in a new browser tab)', 'wordpress-seo' );
 		}
 
 		return sprintf(
