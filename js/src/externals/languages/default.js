d123b4f41fa3aef3e569564a5ac0456d990680de
Agnieszka Szuba <agnieszkaszuba@Agnieszkas-MacBook-Pro.local>
2021-02-16 13:08:01 +0100

    Fix typo in path

--- js/src/externals/languages/default.js
+++ js/src/externals/languages/default.js
@@ -1,2 +1,2 @@
 window.yoast = window.yoast || {};
-window.yoast.Researcher = require( "yoastseo/src/languageProcessing/languages/default/Researcher" );
+window.yoast.Researcher = require( "yoastseo/src/languageProcessing/languages/_default/Researcher" );
