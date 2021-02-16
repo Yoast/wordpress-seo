3174f9b7d3c8160b4554619585f3495be3dd1de1
noud-github <arnoud@yoast.com>
2021-02-16 15:55:15 +0100

    changed to multitask

---
+++ config/grunt/task-config/update-changelog-with-latest-pr-texts.js
@@ -0,0 +1,12 @@
+// Custom task
+module.exports = {
+	options: {
+		version: "<%= pluginVersion %>",
+		matchGroupIndex: 1,
+		readmePath: "readme.txt",
+		useEditDistanceComapair: true,
+	},
+	defualt: {
+		
+	}
+};
