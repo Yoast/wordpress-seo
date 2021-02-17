5bce5240b5303ee1eb25fdc7df212ccee755afac
Joost Boomkamp <joostboomkamp@yoast.com>
2021-02-17 08:06:13 +0100

    suggestie van Herre

--- packages/schema-blocks/src/core/Instruction.ts
+++ packages/schema-blocks/src/core/Instruction.ts
@@ -109,6 +109,7 @@ export default abstract class Instruction {
 
 		if ( ! klass ) {
 			logger.error( "Invalid instruction: ", name );
+			logger.debug( "if you just added a new instruction class, did you export that class?" );
 		}
 
 		return new klass( id, options );
