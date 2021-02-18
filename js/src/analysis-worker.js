fbd6b743b810ba83669ccf18e79adc27cbcd6df3
Manuel <manuel@yoast.com>
2021-02-18 13:57:45 +0100

    Fix loading of language-specific researcher in webworker

--- js/src/analysis-worker.js
+++ js/src/analysis-worker.js
@@ -3,14 +3,13 @@ self.window = self;
 
 import "babel-polyfill";
 
-self.onmessage = ( data ) => {
+self.onmessage = ( event ) => {
     const originalUrl = self.yoastOriginalUrl || self.location.href;
-
     // We only know the URL of the worker script, so base all other files names on that.
     self.importScripts( originalUrl.replace( "analysis-worker", "commons" ) );
     self.importScripts( originalUrl.replace( "analysis-worker", "yoast/feature-flag" ) );
     self.importScripts( originalUrl.replace( "analysis-worker", "analysis" ) );
-    self.importScripts( originalUrl.replace( "analysis-worker", "languages/" + data.language ) );
+	self.importScripts( originalUrl.replace( "analysis-worker", "languages/" + event.data.language ) );
 
     const worker = new self.yoast.analysis.AnalysisWebWorker( self, new self.yoast.Researcher() );
     worker.register();
