0593edf93412d859c4fc646687c6f89373cd6878
FAMarfuaty <aida@yoast.com>
2021-02-19 14:43:14 +0100

    Add Norwegian and Turkish to the supported language list

--- src/config/researcher-languages.php
+++ src/config/researcher-languages.php
@@ -6,5 +6,5 @@ namespace Yoast\WP\SEO\Config;
  * Holds all languages supported with specific researches for our readability analysis.
  */
 class Researcher_Languages {
-	const SUPPORTED_LANGUAGES = [ 'ar', 'ca', 'de', 'en', 'es', 'fa', 'fr', 'he', 'hu', 'id', 'it', 'nl', 'pl', 'pt', 'ru', 'sv' ];
+	const SUPPORTED_LANGUAGES = [ 'ar', 'ca', 'de', 'en', 'es', 'fa', 'fr', 'he', 'hu', 'id', 'it', 'nl', 'pl', 'pt', 'ru', 'sv', 'nb', 'tr' ];
 }
