b19773326bb4937095c6a024785d2d3e8ca7bb86
Andy <andy@yoast.com>
2021-02-23 10:40:46 +0100

    Update src/presenters/admin/auto-update-notification-presenter.php

--- src/presenters/admin/auto-update-notification-presenter.php
+++ src/presenters/admin/auto-update-notification-presenter.php
@@ -19,7 +19,7 @@ class Auto_Update_Notification_Presenter extends Abstract_Presenter {
 		$notification_text .= \sprintf(
 			/* Translators: %1$s expands to 'Yoast SEO', %2$s to an opening anchor tag for a link leading to the Plugins page, and %3$s to a closing anchor tag. */
 			\esc_html__(
-				'We see that you enabled automatic updates for WordPress. We recommend that you do this for Yoast SEO as well. This way we can guarantee that WordPress and %1$s will continue to run smoothly together. %2$sGo to your plugins overview to enable auto-updates for %1$s.%3$s',
+				'We see that you enabled automatic updates for WordPress. We recommend that you do this for %1$s as well. This way we can guarantee that WordPress and %1$s will continue to run smoothly together. %2$sGo to your plugins overview to enable auto-updates for %1$s.%3$s',
 				'wordpress-seo'
 			),
 			'Yoast SEO',
