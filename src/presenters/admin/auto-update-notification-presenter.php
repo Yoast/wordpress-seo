bf7472c4c879396ccaa52ec631ea0ff42bf4c9d3
Johanna de Vos <johanna@yoast.com>
2021-02-23 09:59:59 +0100

    Don't translate "Yoast SEO"

--- src/presenters/admin/auto-update-notification-presenter.php
+++ src/presenters/admin/auto-update-notification-presenter.php
@@ -17,11 +17,12 @@ class Auto_Update_Notification_Presenter extends Abstract_Presenter {
 	public function present() {
 		$notification_text  = '<p>';
 		$notification_text .= \sprintf(
-			/* Translators: %1$s expands to an opening anchor tag for a link leading to the Plugins page, %2$s expands to a closing anchor tag. */
+			/* Translators: %1$s expands to 'Yoast SEO', %2$s to an opening anchor tag for a link leading to the Plugins page, and %3$s to a closing anchor tag. */
 			\esc_html__(
-				'We see that you enabled automatic updates for WordPress. We recommend that you do this for Yoast SEO as well. This way we can guarantee that WordPress and Yoast SEO will continue to run smoothly together. %1$sGo to your plugins overview to enable auto-updates for Yoast SEO.%2$s',
+				'We see that you enabled automatic updates for WordPress. We recommend that you do this for Yoast SEO as well. This way we can guarantee that WordPress and %1$s will continue to run smoothly together. %2$sGo to your plugins overview to enable auto-updates for %1$s.%3$s',
 				'wordpress-seo'
 			),
+			'Yoast SEO',
 			'<a href="' . \esc_url( \get_admin_url( null, 'plugins.php' ) ). '">',
 			'</a>'
 		);
