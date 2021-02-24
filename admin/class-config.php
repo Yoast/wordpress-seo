bfa711d095210459ddc6b465b6dd3deff6ffe304
Herre Groen <herre@yoast.com>
2021-02-24 15:56:29 +0100

    Fix should_show_local_seo_upsell function

--- admin/class-config.php
+++ admin/class-config.php
@@ -167,9 +167,7 @@ class WPSEO_Admin_Pages {
 	 * @return bool Whether the Local SEO upsell should be shown.
 	 */
 	private function should_show_local_seo_upsell() {
-		$addon_manager = new WPSEO_Addon_Manager();
-
-		return ! YoastSEO()->helpers->product->is_premium()()
+		return ! YoastSEO()->helpers->product->is_premium()
 			&& ! ( defined( 'WPSEO_LOCAL_FILE' ) );
 	}
 
