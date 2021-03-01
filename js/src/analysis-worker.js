892e193d0b12967af8a5fabfcdf89cec23055e9e
Manuel <manuel@yoast.com>
2021-03-01 10:07:37 +0100

    Make sure the researcher is correctly loaded in the web worker

--- js/src/analysis-worker.js
+++ js/src/analysis-worker.js
@@ -4,18 +4,21 @@ self.window = self;
 import "babel-polyfill";
 
 self.onmessage = ( event ) => {
+	// Only construct a new web worker if a data object with a language entry is sent.
 	if( ! event.data.language ) {
 		return;
 	}
+
     const originalUrl = self.yoastOriginalUrl || self.location.href;
     // We only know the URL of the worker script, so base all other files names on that.
 	self.importScripts( event.data.lodashURL );
+	self.lodash = _.noConflict();
+
 	self.importScripts( originalUrl.replace( "analysis-worker", "commons" ) );
     self.importScripts( originalUrl.replace( "analysis-worker", "yoast/feature-flag" ) );
     self.importScripts( originalUrl.replace( "analysis-worker", "analysis" ) );
 	self.importScripts( originalUrl.replace( "analysis-worker", "languages/" + event.data.language ) );
 
-
-    const worker = new self.yoast.analysis.AnalysisWebWorker( self, new self.yoast.Researcher() );
+    const worker = new self.yoast.analysis.AnalysisWebWorker( self, new self.yoast.Researcher.default() );
     worker.register();
 }
