a5fc55b580158b98cf9a62208494ab465cb4ac72
Agnieszka Szuba <agnieszkaszuba@Agnieszkas-MacBook-Pro.local>
2021-03-03 10:09:36 +0100

    Fix failing spec

--- packages/yoastseo/spec/fullTextTests/testTexts/pt/portuguesePaper3.js
+++ packages/yoastseo/spec/fullTextTests/testTexts/pt/portuguesePaper3.js
@@ -144,7 +144,7 @@ const expectedResults = {
 	passiveVoice: {
 		isApplicable: true,
 		score: 6,
-		resultText: "<a href='https://yoa.st/34t' target='_blank'>Passive voice</a>: 15.2% of the sentences contain passive voice, which is more" +
+		resultText: "<a href='https://yoa.st/34t' target='_blank'>Passive voice</a>: 14.4% of the sentences contain passive voice, which is more" +
 			" than the recommended maximum of 10%. <a href='https://yoa.st/34u' target='_blank'>Try to use their active counterparts</a>.",
 	},
 	textPresence: {
