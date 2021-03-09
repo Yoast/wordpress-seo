fddfb537f84a766d37ade749d8bad313432b6bbb
Andy Meerwaldt <andy@yoast.com>
2021-03-09 09:35:23 +0100

    Add global settings model

---
+++ src/models/settings/global-settings-model.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Yoast\WP\SEO\Models\Settings;
+
+use Yoast\WP\SEO\Models\Settings_Model;
+
+/**
+ * Class Global_Settings_Model.
+ */
+abstract class Global_Settings_Model extends Settings_Model {
+
+}
