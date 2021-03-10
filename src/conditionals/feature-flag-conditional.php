0d0fd7ba29ecb3d983802712a52004078fdbbd74
marijnyoast <marijn@yoast.com>
2021-03-10 10:57:28 +0100

    fix typo in filter name

--- src/conditionals/feature-flag-conditional.php
+++ src/conditionals/feature-flag-conditional.php
@@ -14,7 +14,7 @@ abstract class Feature_Flag_Conditional implements Conditional {
 	 */
 	public function is_met() {
 		$feature_flag = \strtoupper( static::get_feature_flag() );
-		if ( in_array( $feature_flag, apply_filters( 'wpseo_enable_features', [] ) ) ) {
+		if ( in_array( $feature_flag, apply_filters( 'wpseo_enable_feature', [] ) ) ) {
 			return true;
 		}
 		return \defined( 'YOAST_SEO_' . $feature_flag ) && \constant( 'YOAST_SEO_' . $feature_flag ) === true;
