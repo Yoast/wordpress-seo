fd1f2f365ce0b9ccbb245f5c385f418480d26510
iolandasequino <iolanda@yoast.com>
2021-03-15 15:30:09 +0100

    change Czech locale in file path

--- js/src/externals/languages/cs.js
+++ js/src/externals/languages/cs.js
@@ -1,2 +1,2 @@
 window.yoast = window.yoast || {};
-window.yoast.Researcher = require( "yoastseo/src/languageProcessing/languages/cz/Researcher" );
+window.yoast.Researcher = require( "yoastseo/src/languageProcessing/languages/cs/Researcher" );
