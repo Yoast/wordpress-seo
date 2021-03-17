c7a8a7ac03f8adfd0a962ff2ec7e400283222ced
noud-github <arnoud@yoast.com>
2021-03-17 09:19:55 +0100

    code styling

--- config/grunt/task-config/update-changelog-with-latest-pr-texts.js
+++ config/grunt/task-config/update-changelog-with-latest-pr-texts.js
@@ -1,16 +1,18 @@
+/* eslint-disable no-useless-escape */
+/* eslint-disable no-useless-concat */
 // Custom task
 module.exports = {
 	"wordpress-seo": {
 		options: {
-			// free header:
+			// Free header:
 			// = 15.7 =
 			// Release Date: January 26th, 2021
 			//
 			// Enhancements:
 			readmeFile: "./readme.txt",
 			releaseInChangelog: /[=] \d+\.\d+(\.\d+)? =/g,
-			matchChangelogHeader:  /[=]= Changelog ==\n\n/ig,
-			newHeadertemplate: "== Changelog ==\n\n" +"= " + "VERSIONNUMBER" + " =\nRelease Date: " + "DATESTRING"  + "\n\n",
+			matchChangelogHeader: /[=]= Changelog ==\n\n/ig,
+			newHeadertemplate: "== Changelog ==\n\n" + "= " + "VERSIONNUMBER" + " =\nRelease Date: " + "DATESTRING"  + "\n\n",
 			matchCorrectHeader: "= " + "VERSIONNUMBER" + "(.|\\n)*?\\n(?=(\\w\+?:\\n|= \\d+[\.\\d]+ =|= Earlier versions =))",
 			matchCorrectLines: "= " + "VERSIONNUMBER" + "(.|\\n)*?(?=(= \\d+[\.\\d]+ =|= Earlier versions =))",
 			matchCleanedChangelog: "= " + "VERSIONNUMBER" + "(.|\\n)*= Earlier versions =",
@@ -21,6 +23,6 @@ module.exports = {
 			commitChangelog: true,
 			useEditDistanceComapair: true,
 		},
-	},	
-	
+	},
+
 };
