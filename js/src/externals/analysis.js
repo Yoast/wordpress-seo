cbf5c49ce44322fcd60e265f8c9db0105e5110c8
Agnieszka Szuba <agnieszkaszuba@Agnieszkas-MBP.fritz.box>
2021-03-18 16:16:53 +0100

    Add buffer and htmlparser2 as externals

--- js/src/externals/analysis.js
+++ js/src/externals/analysis.js
@@ -1,2 +1,4 @@
 window.yoast = window.yoast || {};
 window.yoast.analysis = require( "yoastseo/index.js" );
+window.yoast.buffer = require( "buffer" );
+window.yoast.htmlparser2 = require( "htmlparser2/lib" );
