cfbb1b90fde62ce14f9834f5be2bcdcbb5b9cb01
marijnyoast <marijn@yoast.com>
2021-03-24 21:41:35 +0100

    remove faulty snapshot tests

--- packages/search-metadata-previews/tests/SnippetEditorTest.js
+++ packages/search-metadata-previews/tests/SnippetEditorTest.js
@@ -19,21 +19,6 @@ const defaultArgs = {
 };
 
 /**
- * Renders a snapshot with changed arguments.
- *
- * @param {Object} changedArgs The changed arguments.
- *
- * @returns {void}
- */
-const renderSnapshotWithArgs = ( changedArgs ) => {
-	const args = { ...defaultArgs, ...changedArgs };
-	const tree = renderer.create( <SnippetEditor { ...args } />, { locale: "en" } )
-		.toJSON();
-
-	expect( tree ).toMatchSnapshot();
-};
-
-/**
  * Mounts the snippet editor component with changed arguments.
  *
  * @param {Object} changedArgs The changed arguments.
@@ -59,29 +44,6 @@ const shallowWithArgs = ( changedArgs ) => {
 
 
 describe( "SnippetEditor", () => {
-	it( "shows the editor", () => {
-		renderSnapshotWithArgs( {} );
-	} );
-
-	it( "highlights a hovered field", () => {
-		renderSnapshotWithArgs( { hoveredField: "title" } );
-	} );
-
-	it( "highlights a focused field", () => {
-		renderSnapshotWithArgs( { activeField: "slug" } );
-	} );
-
-	it( "renders in desktop mode", () => {
-		renderSnapshotWithArgs( { mode: MODE_DESKTOP } );
-	} );
-
-	it( "passes the date prop", () => {
-		renderSnapshotWithArgs( { date: "date string" } );
-	} );
-
-	it( "renders the snippet editor without a close button when showCloseButton is false", () => {
-		renderSnapshotWithArgs( { showCloseButton: false } );
-	} );
 
 	it( "accepts a custom data mapping function", () => {
 		const mapper = jest.fn( () => {
@@ -99,29 +61,10 @@ describe( "SnippetEditor", () => {
 		const context = {
 			shortenedBaseUrl: "example.org/",
 		};
-		const replacementVariables = [
-			{
-				name: "replacement_variable",
-				value: "replacement value",
-				description: "replacement value",
-				label: "replacement value",
-			},
-		];
-
-		renderSnapshotWithArgs( { mapEditorDataToPreview: mapper, replacementVariables } );
 
 		// The mapper is called both in the constructor, as well as the render function.
 		expect( mapper ).toHaveBeenCalledTimes( 2 );
 		expect( mapper ).toHaveBeenCalledWith( defaultMappedData, context );
-	} );
-
-	it( "opens when calling open()", () => {
-		const editor = mountWithArgs( {} );
-
-		editor.instance().open();
-		editor.update();
-
-		expect( editor ).toMatchSnapshot();
 	} );
 
 	it( "closes when calling close()", () => {
@@ -133,36 +76,6 @@ describe( "SnippetEditor", () => {
 
 		editor.instance().setFieldFocus( "title" );
 		expect( focus ).toHaveBeenCalledTimes( 1 );
-
-		expect( editor ).toMatchSnapshot();
-
-		editor.instance().close();
-		editor.update();
-
-		expect( editor ).toMatchSnapshot();
-	} );
-
-	it( "highlights the hovered field when onMouseEnter() is called", () => {
-		const editor = mountWithArgs( {} );
-
-		editor.instance().open();
-		editor.instance().onMouseEnter( "description" );
-		editor.update();
-
-		expect( editor ).toMatchSnapshot();
-	} );
-
-	it( "removes the highlight from the hovered field on calling onMouseLeave()", () => {
-		const editor = shallowWithArgs( {} );
-
-		editor.instance().open();
-		editor.instance().onMouseEnter( "description" );
-		editor.update();
-
-		editor.instance().onMouseLeave( "description" );
-		editor.update();
-
-		expect( editor ).toMatchSnapshot();
 	} );
 
 	it( "highlights the active ReplacementVariableEditor when calling setFieldFocus", () => {
@@ -176,31 +89,6 @@ describe( "SnippetEditor", () => {
 		editor.update();
 
 		expect( focus ).toHaveBeenCalledTimes( 2 );
-		expect( editor ).toMatchSnapshot();
-	} );
-
-	it( "removes the highlight when calling unsetFieldFocus", () => {
-		focus.mockClear();
-
-		const editor = mountWithArgs( {} );
-
-		editor.instance().open();
-		editor.instance().setFieldFocus( "title" );
-		editor.update();
-
-		editor.instance().unsetFieldFocus();
-		editor.update();
-
-		expect( editor ).toMatchSnapshot();
-	} );
-
-	it( "activates a field on onMouseUp() and opens the editor", () => {
-		const editor = shallowWithArgs( {} );
-
-		editor.instance().onMouseUp( "title" );
-		editor.update();
-
-		expect( editor ).toMatchSnapshot();
 	} );
 
 	it( "switches modes when changing mode switcher input", () => {
@@ -216,67 +104,6 @@ describe( "SnippetEditor", () => {
 		editor.find( "ModeSwitcher__Switcher" ).find( "input" ).at( 1 ).simulate( "change" );
 
 		expect( onChange ).toBeCalledWith( "mode", "desktop" );
-	} );
-
-	it( "passes replacement variables to the title and description editor", () => {
-		const editor = mountWithArgs( {
-			replacementVariables: [
-				{
-					name: "title",
-					value: "Title!!!",
-					label: "Title",
-					description: "Title",
-				},
-				{
-					name: "excerpt",
-					value: "Excerpt!!!",
-					label: "Excerpt",
-					description: "Excerpt",
-				},
-			],
-		} );
-
-		editor.instance().open();
-		editor.update();
-
-		expect( editor ).toMatchSnapshot();
-	} );
-
-	describe( "colored progress bars", () => {
-		it( "can handle scores of 3 and 9", () => {
-			const editor = mountWithArgs( {
-				titleLengthProgress: {
-					max: 550,
-					actual: 100,
-					score: 3,
-				},
-				descriptionLengthProgress: {
-					max: 650,
-					actual: 330,
-					score: 9,
-				},
-			} );
-
-			editor.instance().open();
-			editor.update();
-
-			expect( editor ).toMatchSnapshot();
-		} );
-
-		it( "can handle a score of 6", () => {
-			const editor = mountWithArgs( {
-				titleLengthProgress: {
-					max: 550,
-					actual: 361,
-					score: 6,
-				},
-			} );
-
-			editor.instance().open();
-			editor.update();
-
-			expect( editor ).toMatchSnapshot();
-		} );
 	} );
 
 	describe( "shallowCompareData", () => {
