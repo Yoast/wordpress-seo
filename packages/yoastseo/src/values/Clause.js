8b514b528dd47ce34905631550fead49624c37b2
FAMarfuaty <aida@yoast.com>
2021-03-29 09:37:17 +0200

    Add SentenceNew and Clause classes and their specs

---
+++ packages/yoastseo/src/values/Clause.js
@@ -0,0 +1,122 @@
+/**
+ * Sentence clause which should be checked for passiveness.
+ *
+ */
+class Clause {
+	/**
+	 * Constructs a clause object.
+	 *
+	 * @param {string} clauseText The text in the clause.
+	 * @param {Array} auxiliaries The auxiliaries in the clause.
+	 *
+	 * @constructor
+	 */
+	constructor( clauseText, auxiliaries ) {
+		this._clauseText = clauseText;
+		this._auxiliaries = auxiliaries;
+		this._isPassive = false;
+		this._participles = [];
+	}
+	/**
+	 * Returns the clause text.
+	 *
+	 * @returns {string} The clause text.
+	 */
+	getClauseText() {
+		return this._clauseText;
+	}
+
+	/**
+	 * Returns true if the clause is passive.
+	 *
+	 * @returns {boolean} Whether the clause is passive.
+	 */
+	isPassive() {
+		return this._isPassive;
+	}
+
+	/**
+	 * Returns the auxiliaries of the clause.
+	 *
+	 * @returns {Array} The auxiliaries present in the clause.
+	 */
+	getAuxiliaries() {
+		return this._auxiliaries;
+	}
+
+	/**
+	 * Sets the passiveness of the clause.
+	 *
+	 * @param {boolean} passive	 Whether the clause is passive.
+	 *
+	 * @returns {void}
+	 */
+	setPassive( passive ) {
+		this._isPassive = passive;
+	}
+
+	/**
+	 * Sets the participles.
+	 *
+	 * @param {Array} participles	The participles.
+	 *
+	 * @returns {void}
+	 */
+	setParticiples( participles ) {
+		this._participles = participles;
+	}
+
+	/**
+	 * Returns the found participles.
+	 *
+	 * @returns {Array} The participles
+	 */
+	getParticiples() {
+		return this._participles;
+	}
+
+	/**
+	 * Applies language-specific checks to determine whether the clause is passive.
+	 * A clause is passive if it contains at least one passive participle.
+	 *
+	 * @param {function} isParticiplePassive	The language-specific function for checking whether the clause is passive.
+	 *
+	 * @returns {void}
+	 */
+	setClausePassiveness( isParticiplePassive ) {
+		const passiveParticiples = this.getParticiples().filter( participle => isParticiplePassive( this.getClauseText(), participle ) );
+
+		this.setPassive( passiveParticiples.length > 0 );
+	}
+
+	/**
+	 * Serializes the Clause instance to an object.
+	 *
+	 * @returns {Object} The serialized Clause.
+	 */
+	serialize() {
+		return {
+			_parseClass: "Clause",
+			clauseText: this._clauseText,
+			auxiliaries: this._auxiliaries,
+			isPassive: this._isPassive,
+			participles: this._participles,
+		};
+	}
+
+	/**
+	 * Parses the object to a Clause.
+	 *
+	 * @param {Object} serialized The serialized object.
+	 *
+	 * @returns {Clause} The parsed Clause.
+	 */
+	parse( serialized ) {
+		const clause = new Clause( serialized.clauseText, serialized.auxiliaries );
+		clause.setPassive( serialized.isPassive );
+
+		return clause;
+	}
+}
+
+export default Clause;
