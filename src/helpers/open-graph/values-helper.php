ba60f51b264de86c2972adfb2a94dabd996add4a
Enrico Battocchi <enrico.battocchi@gmail.com>
2021-03-29 11:14:30 +0200

    Document the new filters

--- src/helpers/open-graph/values-helper.php
+++ src/helpers/open-graph/values-helper.php
@@ -17,7 +17,13 @@ class Values_Helper {
 	 * @return string The open graph title.
 	 */
 	public function get_open_graph_title( $title, $object_type, $object_subtype ) {
-		return \apply_filters( "Yoast\WP\SEO\open_graph_title_{$object_type}", $title, $object_subtype );
+		/**
+		 * Allow changing the Open Graph title.
+		 *
+		 * @param string $title          The default title.
+		 * @param string $object_subtype The object subtype.
+		 */
+		return \apply_filters( 'Yoast\WP\SEO\open_graph_title_' . $object_type, $title, $object_subtype );
 	}
 
 	/**
@@ -27,10 +33,16 @@ class Values_Helper {
 	 * @param string $object_type    The object type.
 	 * @param string $object_subtype The object subtype.
 	 *
-	 * @return string The open graph title.
+	 * @return string The open graph description.
 	 */
 	public function get_open_graph_description( $description, $object_type, $object_subtype ) {
-		return \apply_filters( "Yoast\WP\SEO\open_graph_description_{$object_type}", $description, $object_subtype );
+		/**
+		 * Allow changing the Open Graph description.
+		 *
+		 * @param string $description    The default description.
+		 * @param string $object_subtype The object subtype.
+		 */
+		return \apply_filters( 'Yoast\WP\SEO\open_graph_description_' . $object_type, $description, $object_subtype );
 	}
 
 	/**
@@ -40,10 +52,16 @@ class Values_Helper {
 	 * @param string $object_type    The object type.
 	 * @param string $object_subtype The object subtype.
 	 *
-	 * @return string The open graph title.
+	 * @return string The open graph image ID.
 	 */
 	public function get_open_graph_image_id( $image_id, $object_type, $object_subtype ) {
-		return \apply_filters( "Yoast\WP\SEO\open_graph_image_id_{$object_type}", $image_id, $object_subtype );
+		/**
+		 * Allow changing the Open Graph image ID.
+		 *
+		 * @param int    $image_id       The default image ID.
+		 * @param string $object_subtype The object subtype.
+		 */
+		return \apply_filters( 'Yoast\WP\SEO\open_graph_image_id_' . $object_type, $image_id, $object_subtype );
 	}
 
 	/**
@@ -53,9 +71,15 @@ class Values_Helper {
 	 * @param string $object_type    The object type.
 	 * @param string $object_subtype The object subtype.
 	 *
-	 * @return string The open graph title.
+	 * @return string The open graph image URL.
 	 */
 	public function get_open_graph_image( $image, $object_type, $object_subtype ) {
-		return \apply_filters( "Yoast\WP\SEO\open_graph_image_{$object_type}", $image, $object_subtype );
+		/**
+		 * Allow changing the Open Graph image URL.
+		 *
+		 * @param string $image          The default image URL.
+		 * @param string $object_subtype The object subtype.
+		 */
+		return \apply_filters( 'Yoast\WP\SEO\open_graph_image_' . $object_type, $image, $object_subtype );
 	}
 }
