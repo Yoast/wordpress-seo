a42974ba66ea78386ded938966d5b34961df5922
Joost Boomkamp <3604126+increddibelly@users.noreply.github.com>
2021-03-30 09:10:49 +0200

    fix cs errors

--- js/tests/containers/DocumentSidebar.test.js
+++ js/tests/containers/DocumentSidebar.test.js
@@ -1,76 +1,11 @@
 import { mapSelectToProps, mapDispatchToProps } from "../../src/containers/DocumentSidebar";
 
-describe( "The DocumentSidebar container", () => {
-	it( "maps the select function to props", () => {
-		const select = getSelectors( name );
-
-		const props = mapSelectToProps( select );
-
-		expect( props.checklist ).toContainEqual( {
-			label: "Readability analysis:",
-			score: "good",
-			scoreValue: "Good",
-		} );
-
-		expect( props.checklist ).toContainEqual( {
-			label: "SEO analysis:",
-			score: "ok",
-			scoreValue: "OK",
-		} );
-
-		expect( props.checklist ).toContainEqual( {
-			label: "Schema analysis:",
-			score: "bad",
-			scoreValue: "Needs improvement",
-		} );
-
-		expect( props.intro ).toEqual( undefined );
-	} );
-
-	it( "maps the select function to props when no schema validation results are present", () => {
-
-		const select = getSelectors( name, { "1234-abcde": { result: 0, }, } );
-
-		const props = mapSelectToProps( select );
-
-		expect( props.checklist ).toContainEqual( {
-			label: "Readability analysis:",
-			score: "good",
-			scoreValue: "Good",
-		} );
-
-		expect( props.checklist ).toContainEqual( {
-			label: "SEO analysis:",
-			score: "ok",
-			scoreValue: "OK",
-		} );
-
-		expect( props.checklist ).toContainEqual( {
-			label: "Schema analysis:",
-			score: "good",
-			scoreValue: "Good",
-		} );
-
-		expect( props.intro ).toEqual( undefined );
-	} );
-
-	it( "maps the dispatch function to props", () => {
-		const dispatchers = {
-			openGeneralSidebar: jest.fn(),
-		};
-
-		const dispatch = jest.fn();
-
-		dispatch.mockReturnValue( dispatchers );
-
-		const { onClick } = mapDispatchToProps( dispatch );
-
-		onClick();
-
-		expect( dispatchers.openGeneralSidebar ).toHaveBeenCalledWith( "yoast-seo/seo-sidebar" );
-	} );
-} );
-
+/**
+ * Setup the redux selectors
+ * @param name (
+ * @param schemaBlocksValidationresults Overrides default (invalid) schema blocks validation results
+ * @returns The Mocked selectors
+ */
 function getSelectors( name, schemaBlocksValidationresults = null ) {
 	const getResultsForFocusKeyword = jest.fn().mockReturnValue( {
 		overallScore: 60,
@@ -84,7 +19,6 @@ function getSelectors( name, schemaBlocksValidationresults = null ) {
 		isKeywordAnalysisActive: true,
 		isContentAnalysisActive: true,
 	} );
-
 
 	const getSchemaBlocksValidationResults = jest.fn().mockReturnValue( schemaBlocksValidationresults || {
 		"1234-abcde": {
@@ -133,6 +67,75 @@ function getSelectors( name, schemaBlocksValidationresults = null ) {
 				return schemaBlocksSelectors;
 		}
 	} );
-
 	return select;
 }
+
+describe( "The DocumentSidebar container", () => {
+	it( "maps the select function to props", () => {
+		const select = getSelectors( name );
+
+		const props = mapSelectToProps( select );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "Readability analysis:",
+			score: "good",
+			scoreValue: "Good",
+		} );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "SEO analysis:",
+			score: "ok",
+			scoreValue: "OK",
+		} );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "Schema analysis:",
+			score: "bad",
+			scoreValue: "Needs improvement",
+		} );
+
+		expect( props.intro ).toEqual( undefined );
+	} );
+
+	it( "maps the select function to props when no schema validation results are present", () => {
+		const select = getSelectors( name, { "1234-abcde": { result: 0 } } );
+
+		const props = mapSelectToProps( select );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "Readability analysis:",
+			score: "good",
+			scoreValue: "Good",
+		} );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "SEO analysis:",
+			score: "ok",
+			scoreValue: "OK",
+		} );
+
+		expect( props.checklist ).toContainEqual( {
+			label: "Schema analysis:",
+			score: "good",
+			scoreValue: "Good",
+		} );
+
+		expect( props.intro ).toEqual( undefined );
+	} );
+
+	it( "maps the dispatch function to props", () => {
+		const dispatchers = {
+			openGeneralSidebar: jest.fn(),
+		};
+
+		const dispatch = jest.fn();
+
+		dispatch.mockReturnValue( dispatchers );
+
+		const { onClick } = mapDispatchToProps( dispatch );
+
+		onClick();
+
+		expect( dispatchers.openGeneralSidebar ).toHaveBeenCalledWith( "yoast-seo/seo-sidebar" );
+	} );
+} );
