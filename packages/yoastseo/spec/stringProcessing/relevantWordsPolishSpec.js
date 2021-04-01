c0ea06e292b996fd0efa5b4fd7c4c4da93d8b78b
Manuel <manuel@yoast.com>
2021-04-01 17:49:49 +0200

    Fix order relevant words specs

--- packages/yoastseo/spec/stringProcessing/relevantWordsPolishSpec.js
+++ packages/yoastseo/spec/stringProcessing/relevantWordsPolishSpec.js
@@ -16,26 +16,24 @@ describe( "gets Polish word combinations", function() {
 			"odczuwa ból w klatce piersiowej, to należy natychmiast dzwonić po karetkę. W zasadzie każdy z nas wie, że " +
 			"gdy ktoś odczuwa ból w klatce piersiowej, to należy natychmiast dzwonić po karetkę. W zasadzie każdy z nas" +
 			" wie, że gdy ktoś odczuwa ból w klatce piersiowej, to należy natychmiast dzwonić po karetkę.";
-
-
 		const expected = [
 			new WordCombination( [ "odczuwa", "ból", "w", "klatce", "piersiowej" ], 8, polishFunctionWords ),
-			new WordCombination( [ "natychmiast", "dzwonić", "po", "karetkę" ], 8, polishFunctionWords ),
-			new WordCombination( [ "ból", "w", "klatce", "piersiowej" ], 8, polishFunctionWords ),
 			new WordCombination( [ "odczuwa", "ból", "w", "klatce" ], 8, polishFunctionWords ),
-			new WordCombination( [ "dzwonić", "po", "karetkę" ], 8, polishFunctionWords ),
+			new WordCombination( [ "ból", "w", "klatce", "piersiowej" ], 8, polishFunctionWords ),
+			new WordCombination( [ "natychmiast", "dzwonić", "po", "karetkę" ], 8, polishFunctionWords ),
 			new WordCombination( [ "ból", "w", "klatce" ], 8, polishFunctionWords ),
+			new WordCombination( [ "dzwonić", "po", "karetkę" ], 8, polishFunctionWords ),
 			new WordCombination( [ "odczuwa", "ból" ], 8, polishFunctionWords ),
-			new WordCombination( [ "natychmiast", "dzwonić" ], 8, polishFunctionWords ),
 			new WordCombination( [ "klatce", "piersiowej" ], 8, polishFunctionWords ),
-			new WordCombination( [ "odczuwa" ], 8, polishFunctionWords ),
-			new WordCombination( [ "karetkę" ], 8, polishFunctionWords ),
-			new WordCombination( [ "dzwonić" ], 8, polishFunctionWords ),
-			new WordCombination( [ "natychmiast" ], 8, polishFunctionWords ),
-			new WordCombination( [ "piersiowej" ], 8, polishFunctionWords ),
-			new WordCombination( [ "klatce" ], 8, polishFunctionWords ),
-			new WordCombination( [ "ból" ], 8, polishFunctionWords ),
+			new WordCombination( [ "natychmiast", "dzwonić" ], 8, polishFunctionWords ),
 			new WordCombination( [ "zasadzie" ], 8, polishFunctionWords ),
+			new WordCombination( [ "odczuwa" ], 8, polishFunctionWords ),
+			new WordCombination( [ "ból" ], 8, polishFunctionWords ),
+			new WordCombination( [ "klatce" ], 8, polishFunctionWords ),
+			new WordCombination( [ "piersiowej" ], 8, polishFunctionWords ),
+			new WordCombination( [ "natychmiast" ], 8, polishFunctionWords ),
+			new WordCombination( [ "dzwonić" ], 8, polishFunctionWords ),
+			new WordCombination( [ "karetkę" ], 8, polishFunctionWords ),
 		];
 
 		// Make sure our words aren't filtered by density.
