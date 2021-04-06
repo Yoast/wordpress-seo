24304947c43d45d9061b3b70b0c76972d3aed86d
Alexander Botteram <a.botteram@gmail.com>
2021-04-06 09:52:53 +0200

    Changed recipe name placeholder to match designs

--- src/schema-templates/recipe-name.block.php
+++ src/schema-templates/recipe-name.block.php
@@ -1,2 +1,2 @@
 {{block name="yoast/recipe-name" title="Recipe name" category="yoast-structured-data-blocks" parent=[ "yoast/recipe" ] }}
-{{variable-tag-rich-text name="name" tags=[ "h2", "h3", "h4", "h5", "h6", "strong" ] placeholder="<?php esc_attr_e( 'Enter recipe name', 'wordpress-seo' ); ?>" }}
+{{variable-tag-rich-text name="name" tags=[ "h2", "h3", "h4", "h5", "h6", "strong" ] placeholder="<?php esc_attr_e( 'Enter a recipe name', 'wordpress-seo' ); ?>" }}
