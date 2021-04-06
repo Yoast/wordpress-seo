efe3539c6f357f101e780f1b57f2a453e5ab3e2d
Johanna de Vos <johanna@yoast.com>
2021-04-06 16:00:49 +0200

    Simplify the isValidResult function based on the new BlockValidation codes

--- packages/schema-blocks/src/functions/validators/isValidResult.ts
+++ packages/schema-blocks/src/functions/validators/isValidResult.ts
@@ -2,11 +2,12 @@ import { BlockValidation } from "../../core/validation";
 
 /**
  * Determines if a specific validation result is essentially invalid or not.
+ * 'Valid' means that Schema can be output, 'invalid' means that it should not be output.
  *
  * @param result The source value.
  *
- * @returns {boolean} Whether the result is Valid or Invalid.
+ * @returns Whether the result is Valid or Invalid.
 */
 export default function isValidResult( result: BlockValidation ): boolean {
-	return result === BlockValidation.Valid || result === BlockValidation.Unknown || result === BlockValidation.MissingRecommendedBlock;
+	return result < 300;
 }
