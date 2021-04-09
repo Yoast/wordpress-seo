50ba5999a8cc14fb9f9eaa45af53534498ef0168
FAMarfuaty <aida@yoast.com>
2021-04-09 17:12:59 +0200

    Adjust getParticiples spec

--- packages/yoastseo/spec/languageProcessing/languages/en/helpers/internal/getParticiplesSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/en/helpers/internal/getParticiplesSpec.js
@@ -1,34 +1,22 @@
-import getAndCheckParticiples from "../../../../../../src/languageProcessing/languages/en/helpers/internal/getParticiples";
+import getParticiples from "../../../../../../src/languageProcessing/languages/en/helpers/internal/getParticiples";
 
 describe( "Test for matching English participles", function() {
 	it( "returns matched regular participles.", function() {
-		const sentencePartText = "The cats are loved.";
-		const auxiliaries = [ "are", "is" ];
-		const foundParticiples = getAndCheckParticiples( sentencePartText, auxiliaries );
-		expect( foundParticiples[ 0 ].getParticiple() ).toEqual(  "loved" );
-		expect( foundParticiples[ 0 ].getType() ).toEqual( "regular" );
-		expect( foundParticiples[ 0 ].getSentencePart() ).toEqual( "The cats are loved." );
-		expect( foundParticiples[ 0 ].getAuxiliaries() ).toEqual( [ "are", "is" ] );
-		expect( foundParticiples[ 0 ].getLanguage() ).toEqual( "en" );
-		expect( foundParticiples[ 0 ].determinesSentencePartIsPassive() ).toEqual( true );
+		const clauseText = "The cats are loved.";
+
+		const foundParticiples = getParticiples( clauseText );
+		expect( foundParticiples ).toEqual(  [ "loved" ] );
 	} );
 
 	it( "returns matched irregular participles.", function() {
-		const sentencePartText = "The cats were given.";
-		const auxiliaries = [ "were", "was" ];
-		const foundParticiples = getAndCheckParticiples( sentencePartText, auxiliaries );
-		expect( foundParticiples[ 0 ].getParticiple() ).toEqual( "given" );
-		expect( foundParticiples[ 0 ].getType() ).toEqual( "irregular" );
-		expect( foundParticiples[ 0 ].getSentencePart() ).toEqual( "The cats were given." );
-		expect( foundParticiples[ 0 ].getAuxiliaries() ).toEqual( [ "were", "was" ] );
-		expect( foundParticiples[ 0 ].getLanguage() ).toEqual( "en" );
-		expect( foundParticiples[ 0 ].determinesSentencePartIsPassive() ).toEqual( true );
+		const clauseText = "The cats were given.";
+		const foundParticiples = getParticiples( clauseText );
+		expect( foundParticiples ).toEqual( [ "given" ] );
 	} );
 
 	it( "returns an empty array when there is no participle or when the sentence is empty.", function() {
-		const sentencePartText = "The cats are special.";
-		const auxiliaries = [ "are", "is" ];
-		expect( getAndCheckParticiples( sentencePartText, auxiliaries ) ).toEqual( [] );
-		expect( getAndCheckParticiples( "", auxiliaries ) ).toEqual( [] );
+		const clauseText = "The cats are special.";
+		expect( getParticiples( clauseText ) ).toEqual( [] );
+		expect( getParticiples( "" ) ).toEqual( [] );
 	} );
 } );
