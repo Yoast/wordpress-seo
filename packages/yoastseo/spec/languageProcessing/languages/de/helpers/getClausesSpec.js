b82e18cc142a6ba3e53466ad8e682811f1c0e72b
FAMarfuaty <aida@yoast.com>
2021-04-12 17:31:35 +0200

    Create German Clause and specs and adjust getClauses helper

---
+++ packages/yoastseo/spec/languageProcessing/languages/de/helpers/getClausesSpec.js
@@ -0,0 +1,14 @@
+import getClauses from "../../../../../src/languageProcessing/languages/de/helpers/getClauses.js";
+
+describe( "splits German sentences into clauses", function() {
+	it( "returns all clauses", function() {
+		const sentence =  "Zwischen 1927 und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron.";
+		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "Zwischen 1927" );
+		expect( getClauses( sentence )[ 1 ].getClauseText() ).toBe( "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron." );
+		expect( getClauses( sentence ).length ).toBe( 2 );
+	} );
+	it( "splits German sentences that begin with a stopword into clauses", function() {
+		const sentence =  "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron.";
+		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron." );
+	} );
+} );
