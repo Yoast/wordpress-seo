07a7b210bfc9376baf7fc21efe07a81b8ff25ede
Andy Meerwaldt <andy@yoast.com>
2021-04-12 12:30:30 +0200

    Fixes a typo in the key

--- config/grunt/task-config/copy.js
+++ config/grunt/task-config/copy.js
@@ -81,7 +81,7 @@ module.exports = {
 	},
 	"makepot-yoast-js-schema-blocks": {
 		src: "gettext.pot",
-		dest: "<%= files.pot.yoastSchemaBocks %>",
+		dest: "<%= files.pot.yoastSchemaBlocks %>",
 	},
 
 	// The default de_CH is formal on WordPress.org, but that one is not translated enough for wordpress-seo.
