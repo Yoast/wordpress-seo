6fb1c8f4da0143c9cd255a7dee777d08a0d2516f
Agnieszka Szuba <agnieszkaszuba@Agnieszkas-MacBook-Pro.local>
2021-04-13 10:47:07 +0200

    Fix casing inconsistency in file names

--- packages/yoastseo/spec/languageProcessing/helpers/passiveVoice/periphrastic/getClausesSplitOnStopWordsSpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/passiveVoice/periphrastic/getClausesSplitOnStopWordsSpec.js
@@ -1,4 +1,4 @@
-import getClausesSplitOnStopwords from "../../../../../src/languageProcessing/helpers/passiveVoice/periphrastic/getClausesSplitOnStopwords.js";
+import getClausesSplitOnStopwords from "../../../../../src/languageProcessing/helpers/passiveVoice/periphrastic/getClausesSplitOnStopWords.js";
 import arrayToRegex from "../../../../../src/languageProcessing/helpers/regex/createRegexFromArray";
 import Clause from "../../../../../src/values/Clause";
 
