e7c0aa5a456f6b00edd0534ca55b462de0ebedbc
Hans-Christiaan <hans-christiaan@yoast.com>
2021-04-13 10:38:41 +0200

    Simplified block appender.

--- packages/schema-blocks/src/functions/presenters/BlockAppender.tsx
+++ packages/schema-blocks/src/functions/presenters/BlockAppender.tsx
@@ -1,8 +1,5 @@
 import { createElement } from "@wordpress/element";
-import TextareaAutosize from "react-autosize-textarea";
 import { Inserter } from "@wordpress/block-editor";
-import { __ } from "@wordpress/i18n";
-import { dispatch } from "@wordpress/data";
 
 /**
  * Properties for the BlockAppender.
@@ -21,39 +18,14 @@ export interface BlockAppenderProps {
  * @constructor
  */
 export default function BlockAppender( { clientId, label }: BlockAppenderProps ) {
-	const { insertDefaultBlock, startTyping } = dispatch(
-		"core/block-editor",
-	);
-
-	/**
-	 * Function called when a block is appended.
-	 */
-	const onAppend = () => {
-		insertDefaultBlock( {}, clientId );
-		startTyping();
-	};
-
-	/* The appender "button" is in-fact a text field so as to support
-	 * transitions by WritingFlow occurring by arrow key press. WritingFlow
-	 * only supports tab transitions into text fields and to the block focus
-	 * boundary.
-	 *
-	 * See: https://github.com/WordPress/gutenberg/issues/4829#issuecomment-374213658
-	 */
-
 	return (
 		<div
 			data-root-client-id={ clientId || "" }
 			className="wp-block block-editor-default-block-appender"
 		>
-			<TextareaAutosize
-				role="button"
-				aria-label={ __( "Add block", "yoast-schema-blocks" ) }
-				className="block-editor-default-block-appender__content"
-				readOnly={ true }
-				onFocus={ onAppend }
-				value={ label }
-			/>
+			<p className="block-editor-default-block-appender__content yoast-schema-block-appender__content">
+				{ label }
+			</p>
 			<Inserter
 				rootClientId={ clientId }
 				position="bottom right"
