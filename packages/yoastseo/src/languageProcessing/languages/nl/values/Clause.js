7d972e8e7f75e7efbb1ca1eb5f6e1bf8a4454fb4
FAMarfuaty <aida@yoast.com>
2021-04-14 09:31:20 +0200

    Add Dutch Clause class

---
+++ packages/yoastseo/src/languageProcessing/languages/nl/values/Clause.js
@@ -0,0 +1,61 @@
+import { includes } from "lodash-es";
+import { values, languageProcessing } from "yoastseo";
+const { Clause } = values;
+const { directPrecedenceException } = languageProcessing;
+
+import { cannotDirectlyPrecedePassiveParticiple } from "../config/functionWords";
+import nonParticiples from "../config/internal/nonParticiples";
+import passiveAuxiliaries from "../config/internal/passiveVoiceAuxiliaries";
+import getParticiples from "../helpers/internal/getParticiples";
+
+/**
+ * Creates a Clause object for the Dutch language.
+ */
+class DutchClause extends Clause {
+	/**
+	 * Constructor.
+	 *
+	 * @param {string} clauseText   The text of the clause.
+	 * @param {Array} auxiliaries   The auxiliaries.
+	 *
+	 * @constructor
+	 */
+	constructor( clauseText, auxiliaries ) {
+		super( clauseText, auxiliaries );
+		this._participles = getParticiples( this.getClauseText() );
+		this.checkParticiples();
+	}
+
+	/**
+	 * Checks if any exceptions are applicable to this participle that would result in the clause not being passive.
+	 * If no exceptions are found and there is an auxiliary present, the sentence part is passive.
+	 *
+	 * @returns {void}
+	 */
+	checkParticiples() {
+		const foundParticiples = this.getParticiples().filter( participle => {
+			return ! includes( nonParticiples, participle ) &&
+				! this.hasNonParticipleEnding( participle ) &&
+				! directPrecedenceException( this.getClauseText(), participle, cannotDirectlyPrecedePassiveParticiple );
+		} );
+
+		this.setPassive( foundParticiples.length > 0 );
+
+		if ( ! this.getAuxiliaries().some( auxiliary => passiveAuxiliaries.includes( auxiliary ) ) ) {
+			this.setPassive( false );
+		}
+	}
+
+	/**
+	 * Checks whether a found participle has a non-participle ending and is therefore not really a participle.
+	 *
+	 * @param {string} participle   The participle to check.
+	 *
+	 * @returns {boolean} Returns true if the participle has a non-participle ending, otherwise returns false.
+	 */
+	hasNonParticipleEnding( participle ) {
+		return ( /\S+(heid|teit|tijd)($|[ \n\r\t.,'()"+\-;!?:/»«‹›<>])/ig ).test( participle );
+	}
+}
+
+export default DutchClause;
