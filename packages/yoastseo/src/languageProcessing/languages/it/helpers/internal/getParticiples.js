8a11bd5b1edd2faa55fc89bec969d03595aa68e4
iolandasequino <iolanda@yoast.com>
2021-04-14 16:38:40 +0200

    replace sentence parts by clause

--- packages/yoastseo/src/languageProcessing/languages/it/helpers/internal/getParticiples.js
+++ packages/yoastseo/src/languageProcessing/languages/it/helpers/internal/getParticiples.js
@@ -1,22 +1,22 @@
 import { forEach, includes } from "lodash-es";
 import { languageProcessing } from "yoastseo";
-const { matchRegularParticiples, getWords } = languageProcessing;
+const { getWords } = languageProcessing;
+
+import participles from "../../config/internal/passiveVoiceParticiples";
 
 /**
- * Creates participle array for the participles found in a clause.
+ * Creates a participle list for the participles found in a clause.
  *
- * @param {string} clauseText       The sentence part to find participles in.
+ * @param {string} clauseText The clause to find participles in.
  *
  * @returns {Array} The list with participles.
  */
-export default function getParticiples( clauseText) {
+export default function getParticiples( clauseText ) {
 	const words = getWords( clauseText );
 	const foundParticiples = [];
 
 	forEach( words, function( word ) {
-		const regex = [ /\w+ed($|[ \n\r\t.,'()"+\-;!?:/»«‹›<>])/ig ];
-		if ( matchRegularParticiples( word, regex ).length !== 0 || includes(
-			irregularParticiples, word ) ) {
+		if ( includes( participles, word ) ) {
 			foundParticiples.push( word );
 		}
 	} );
