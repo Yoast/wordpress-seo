d7773970e517f71889bdf8cefcc3bb8c2fe78994
Johanna de Vos <johanna@yoast.com>
2021-04-15 15:13:29 +0200

    Add a type and a link to the question mark icon

--- packages/schema-blocks/src/functions/presenters/InnerBlocksSidebar.tsx
+++ packages/schema-blocks/src/functions/presenters/InnerBlocksSidebar.tsx
@@ -92,12 +92,14 @@ interface WarningListProps {
  * @returns A ReactElement containing the sidebar header.
  */
 function SidebarHeader(): ReactElement {
-	const questionMarkIcon =
+	const questionMarkIcon: JSX.Element =
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -4.5 20 22" fill="currentColor" height="15" width="22">
-			<path
-				fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113
-			8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"
-			/>
+			<a href="https://yoast.com/help/required-recommended-optional-fields/" rel="noopener noreferrer" target="_blank">
+				<path
+					fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113
+				8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"
+				/>
+			</a>
 		</svg>;
 
 	return (
