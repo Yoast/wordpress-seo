2cf38cafcbd18803cfa08c146213b4a11cae4ecd
dennis <dennis@yoast.com>
2021-04-15 13:15:19 +0200

    Explain to Psalm how `apply_filters` works

--- config/psalm/overrides.php
+++ config/psalm/overrides.php
@@ -1,6 +1,14 @@
 <?php
 
 /**
+ * @template TFilterValue
+ * @param string $a
+ * @psalm-param TFilterValue $value
+ * @return TFilterValue
+ */
+function apply_filters( string $a, $value, ...$args ) {}
+
+/**
  * @param WP_Error|mixed $error
  * @psalm-assert-if-true WP_Error $error
  */
