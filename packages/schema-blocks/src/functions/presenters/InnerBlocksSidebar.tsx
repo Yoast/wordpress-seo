6f9ce056dc0385d7b77e39240f539a245ccaad6e
Johanna de Vos <johanna@yoast.com>
2021-04-15 14:40:43 +0200

    Add the sidebar header and the question mark icon

--- packages/schema-blocks/src/functions/presenters/InnerBlocksSidebar.tsx
+++ packages/schema-blocks/src/functions/presenters/InnerBlocksSidebar.tsx
@@ -67,6 +67,7 @@ export function InnerBlocksSidebar( props: InnerBlocksSidebarProps ): ReactEleme
 	}
 
 	return <Fragment>
+		<SidebarHeader />
 		<WarningList warnings={ warnings } />
 		<BlockSuggestions
 			title={ __( "Required Blocks", "yoast-schema-blocks" ) }
@@ -83,6 +84,30 @@ export function InnerBlocksSidebar( props: InnerBlocksSidebarProps ): ReactEleme
 
 interface WarningListProps {
 	warnings: SidebarWarning[];
+}
+
+/**
+ * Renders a ReactElement containing the sidebar header.
+ *
+ * @returns A ReactElement containing the sidebar header.
+ */
+function SidebarHeader(): ReactElement {
+	const questionMarkIcon =
+		<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 -4.5 20 22" fill="currentColor" height="15" width="22">
+			<path
+				fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113
+			8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clipRule="evenodd"
+			/>
+		</svg>;
+
+	return (
+		<div className="yoast-block-sidebar-header">
+			<div className="yoast-block-sidebar-title">
+				{ __( "Blocks for Schema output", "yoast-schema-blocks" ) }
+				<span>{ questionMarkIcon }</span>
+			</div>
+		</div>
+	);
 }
 
 /**
