7196bff964d21024c7357698c2a3c4b91226bff2
Johanna de Vos <johanna@yoast.com>
2021-04-16 10:47:36 +0200

    Change the list from variable-tag-rich-text to rich-text
    We don't need to option to have an ordered list

--- src/schema-templates/ingredients.block.php
+++ src/schema-templates/ingredients.block.php
@@ -1,6 +1,6 @@
 <?php // phpcs:ignore Internal.NoCodeFound ?>
 {{block name="yoast/ingredients" title="Ingredients" category="common" parent=[ "yoast/recipe" ] }}
 <div class={{class-name}}>
-	{{variable-tag-rich-text name="ingredients" tags=["ul", "ol"] multiline="li" placeholder="Ingredient 1" }}
 	{{heading name="title" default-tag="h3" tags=[ "h2", "h3", "h4", "h5", "h6", "strong" ] placeholder="Ingredients title" }}
+	{{rich-text name="ingredients" tag=["ul"] multiline="li" placeholder="Ingredient 1" }}
 </div>
