c83077fb5917270bb7d3a2275bca2653242d4f2b
Johanna de Vos <johanna@yoast.com>
2021-04-16 10:28:44 +0200

    Reorder the imports (externals above)

--- packages/schema-blocks/src/instructions/blocks/Heading.ts
+++ packages/schema-blocks/src/instructions/blocks/Heading.ts
@@ -1,9 +1,10 @@
-import BlockInstruction from "../../core/blocks/BlockInstruction";
-import { RenderSaveProps, RenderEditProps } from "../../core/blocks/BlockDefinition";
-import RichTextBase, { RichTextEditProps, RichTextSaveProps } from "./abstract/RichTextBase";
 import { BlockConfiguration, BlockEditProps } from "@wordpress/blocks";
 import { createElement } from "@wordpress/element";
 import { SelectControl } from "@wordpress/components";
+
+import BlockInstruction from "../../core/blocks/BlockInstruction";
+import { RenderSaveProps, RenderEditProps } from "../../core/blocks/BlockDefinition";
+import RichTextBase, { RichTextEditProps, RichTextSaveProps } from "./abstract/RichTextBase";
 import { arrayOrObjectToOptions } from "../../functions/select";
 
 /**
