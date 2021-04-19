e520d15211394a50729a32a50cd20a212430fac6
dennis <dennis@yoast.com>
2021-04-19 10:03:50 +0200

    Add group support to the Badge_Presenter

--- src/presenters/admin/badge-presenter.php
+++ src/presenters/admin/badge-presenter.php
@@ -3,6 +3,7 @@
 namespace Yoast\WP\SEO\Presenters\Admin;
 
 use WPSEO_Admin_Asset_Manager;
+use Yoast\WP\SEO\Config\Badge_Group_Names;
 use Yoast\WP\SEO\Presenters\Abstract_Presenter;
 
 /**
@@ -25,6 +26,15 @@ class Badge_Presenter extends Abstract_Presenter {
 	private $link;
 
 	/**
+	 * Optional group which the badge belongs to.
+	 *
+	 * Each group has a fixed period after which the the group will no longer be considered new and the badges will disappear.
+	 *
+	 * @var string
+	 */
+	private $group;
+
+	/**
 	 * An instance of the WPSEO_Admin_Asset_Manager class.
 	 *
 	 * @var WPSEO_Admin_Asset_Manager
@@ -34,12 +44,14 @@ class Badge_Presenter extends Abstract_Presenter {
 	/**
 	 * New_Badge_Presenter constructor.
 	 *
-	 * @param string $id   Id of the badge.
-	 * @param string $link Optional link of the badge.
+	 * @param string $id    Id of the badge.
+	 * @param string $link  Optional link of the badge.
+	 * @param string $group Optional group which the badge belongs to.
 	 */
-	public function __construct( $id, $link = '' ) {
+	public function __construct( $id, $link = '', $group = '' ) {
 		$this->id   = $id;
 		$this->link = $link;
+		$this->group = $group;
 
 		if ( ! $this->asset_manager ) {
 			$this->asset_manager = new WPSEO_Admin_Asset_Manager();
@@ -55,6 +67,10 @@ class Badge_Presenter extends Abstract_Presenter {
 	 * @return string The styled New Badge.
 	 */
 	public function present() {
+		if( ! $this->is_group_still_new() ) {
+			return '';
+		}
+
 		if ( $this->link !== '' ) {
 			return sprintf(
 				'<a class="yoast-badge yoast-badge__is-link yoast-new-badge" id="%1$s-new-badge" href="%2$s">%3$s</a>',
@@ -69,5 +85,20 @@ class Badge_Presenter extends Abstract_Presenter {
 			\esc_attr( $this->id ),
 			\esc_html__( 'New', 'wordpress-seo' )
 		);
+	}
+
+	/**
+	 * Check whether the new badge should be shown according to the group it is in.
+	 *
+	 * @return bool True if still new.
+	 */
+	public function is_group_still_new() {
+		// If there's no group configured, the new badge is always active
+		if( ! $this->group ) {
+			return true;
+		}
+
+		$badge_group_names  = new Badge_Group_Names();
+		return $badge_group_names->is_still_eligible_for_new_badge( $this->group );
 	}
 }
