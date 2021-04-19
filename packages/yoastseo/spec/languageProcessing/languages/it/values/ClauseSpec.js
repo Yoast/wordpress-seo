811c69fc5688211d3699bbdeb8d582ef640962f9
iolandasequino <iolanda@yoast.com>
2021-04-19 16:25:00 +0200

    add Italian specs for precedence exception

--- packages/yoastseo/spec/languageProcessing/languages/it/values/ClauseSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/it/values/ClauseSpec.js
@@ -7,9 +7,23 @@ describe( "creates an Italian clause", function() {
 		expect( mockClause.getAuxiliaries() ).toEqual( [ "stati" ] );
 	} );
 
-	it( "returns a participle for an Italian clause", function() {
+	it( "returns a participle for an Italian passive clause", function() {
 		const mockClause = new ItalianClause( "Il testo è stato corretto.", [ "stato" ] );
 		expect( mockClause.getParticiples() ).toEqual( [ "corretto" ] );
 		expect( mockClause.isPassive() ).toBe( true );
+	} );
+
+	it( "checks an Italian clause with a precedence exception between the passive auxiliary and the participle" +
+		"and checks its passiveness", function() {
+		const mockClause = new ItalianClause( "Ciò che ebbe l'aveva costruito suo padre.", [ "fue" ] );
+		expect( mockClause.getParticiples() ).toEqual( [ "costruito" ] );
+		expect( mockClause.isPassive() ).toBe( false );
+	} );
+
+	xit( "checks an Italian clause with a precedence exception where the word from the list directly precedes the participle" +
+		"and checks its passiveness", function() {
+		const mockClause = new ItalianClause( "Era il fatto che le avesse portato dei fiori a farle piacere.", [ "era" ] );
+		expect( mockClause.getParticiples() ).toEqual( [ "fatto" ] );
+		expect( mockClause.isPassive() ).toBe( false );
 	} );
 } );
