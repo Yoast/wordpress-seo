a43af7d3eb31cf56e2d4a56467fbbbc3952f2370
FAMarfuaty <aida@yoast.com>
2021-04-19 14:32:36 +0200

    Adjust some Clause specs files

--- packages/yoastseo/spec/languageProcessing/languages/cs/values/ClauseSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/cs/values/ClauseSpec.js
@@ -2,34 +2,34 @@ import CzechClause from "../../../../../src/languageProcessing/languages/cs/valu
 
 describe( "A test for checking the Czech clause", function() {
 	it( "checks the properties of the Czech clause object with an auxiliary and a participle", function() {
-		const mockParticiple = new CzechClause( "již bylo napsáno.", [ "bylo" ] );
+		const mockClause = new CzechClause( "již bylo napsáno.", [ "bylo" ] );
 
-		expect( mockParticiple.getParticiples() ).toEqual( [ "napsáno" ] );
-		expect( mockParticiple.isPassive() ).toBe( true );
+		expect( mockClause.getParticiples() ).toEqual( [ "napsáno" ] );
+		expect( mockClause.isPassive() ).toBe( true );
 	} );
 
 	it( "checks the properties of the Czech clause object with a participle but without an auxiliary", function() {
-		const mockParticiple = new CzechClause( "adoptována kočka", [] );
+		const mockClause = new CzechClause( "adoptována kočka", [] );
 
-		expect( mockParticiple.getParticiples() ).toEqual( [ "adoptována" ] );
-		expect( mockParticiple.isPassive() ).toBe( false );
+		expect( mockClause.getParticiples() ).toEqual( [ "adoptována" ] );
+		expect( mockClause.isPassive() ).toBe( false );
 	} );
 
 	it( "checks the properties of the Czech clause object with an auxiliary but without a participle", function() {
-		const mockParticiple = new CzechClause( "Kočky jsou krásné.", [ "jsou" ] );
+		const mockClause = new CzechClause( "Kočky jsou krásné.", [ "jsou" ] );
 
-		expect( mockParticiple.isPassive() ).toBe( false );
+		expect( mockClause.isPassive() ).toBe( false );
 	} );
 
 	it( "checks the properties of the Czech clause object without an auxiliary and a participle", function() {
-		const mockParticiple = new CzechClause( "Dorazil doktor.", [] );
+		const mockClause = new CzechClause( "Dorazil doktor.", [] );
 
-		expect( mockParticiple.isPassive() ).toBe( false );
+		expect( mockClause.isPassive() ).toBe( false );
 	} );
 
 	it( "checks the properties of the Czech clause object without an auxiliary and a participle", function() {
-		const mockParticiple = new CzechClause( "Kat je člověk, který vykonává popravy lidí.", [] );
+		const mockClause = new CzechClause( "Kat je člověk, který vykonává popravy lidí.", [] );
 
-		expect( mockParticiple.isPassive() ).toBe( false );
+		expect( mockClause.isPassive() ).toBe( false );
 	} );
 } );
