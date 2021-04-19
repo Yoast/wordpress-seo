70bbf22fd8b21aaa2ee62dec6a939e7f8b3b0ddf
FAMarfuaty <aida@yoast.com>
2021-04-19 16:16:44 +0200

    Adjust Spanish specs descriptions

--- packages/yoastseo/spec/languageProcessing/languages/es/helpers/getClausesSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/es/helpers/getClausesSpec.js
@@ -14,20 +14,20 @@ describe( "splits Spanish sentences into clauses", function() {
 		expect( getClauses( sentence ).length ).toBe( 1 );
 	} );
 
-	it( "doesn't return clauses when there is a sentence breaker within a word", function() {
+	it( "splits Spanish sentence with a sentence breaker within a word", function() {
 		// Sentence breaker: 'es' in 'responsable' and 'acciones'.
 		const sentence = "De aquí en adelante ella es responsable de sus acciones.";
 		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "es responsable de sus acciones." );
 		expect( getClauses( sentence ).length ).toBe( 1 );
 	} );
 
-	it( "returns clauses when there is a stop characters followed by a space/punctuation mark", function() {
+	it( "splits Spanish sentence with a stop character followed by a space/punctuation mark", function() {
 		const sentence = "Esto es especialmente y principalmente una cuestión de dinero, resumió el primer ministro holandés, Mark Rutte.";
 		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "es especialmente y principalmente una cuestión de dinero" );
 		expect( getClauses( sentence ).length ).toBe( 1 );
 	} );
 
-	it( "doesn't return clauses when there is a stop characters that is not followed by a space/punctuation mark", function() {
+	it( "splits Spanish sentence with a stop character that is not followed by a space/punctuation mark", function() {
 		const sentence = "Afortunadamente será Navidad en 2,5 semanas.";
 		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "será Navidad en 2,5 semanas." );
 		expect( getClauses( sentence ).length ).toBe( 1 );
