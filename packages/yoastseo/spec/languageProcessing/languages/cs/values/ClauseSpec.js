b588f576106e1357f1c362af1ddec639734c6893
Agnieszka Szuba <agnieszkaszuba@Agnieszkas-MacBook-Pro.local>
2021-04-20 11:08:41 +0200

    Delete unnecessary specs after refactoring getClausesSplitOnStopWords

--- packages/yoastseo/spec/languageProcessing/languages/cs/values/ClauseSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/cs/values/ClauseSpec.js
@@ -8,13 +8,6 @@ describe( "A test for checking the Czech clause", function() {
 		expect( mockClause.isPassive() ).toBe( true );
 	} );
 
-	it( "checks the properties of the Czech clause object with a participle but without an auxiliary", function() {
-		const mockClause = new CzechClause( "adoptována kočka", [] );
-
-		expect( mockClause.getParticiples() ).toEqual( [ "adoptována" ] );
-		expect( mockClause.isPassive() ).toBe( false );
-	} );
-
 	it( "checks the properties of the Czech clause object with an auxiliary but without a participle", function() {
 		const mockClause = new CzechClause( "Kočky jsou krásné.", [ "jsou" ] );
 
@@ -23,12 +16,6 @@ describe( "A test for checking the Czech clause", function() {
 
 	it( "checks the properties of the Czech clause object without an auxiliary and a participle", function() {
 		const mockClause = new CzechClause( "Dorazil doktor.", [] );
-
-		expect( mockClause.isPassive() ).toBe( false );
-	} );
-
-	it( "checks the properties of the Czech clause object without an auxiliary and a participle", function() {
-		const mockClause = new CzechClause( "Kat je člověk, který vykonává popravy lidí.", [] );
 
 		expect( mockClause.isPassive() ).toBe( false );
 	} );
