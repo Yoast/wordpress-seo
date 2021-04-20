92aa0a67a8aecc53ba9d03fe4bd34a9b7610445e
FAMarfuaty <aida@yoast.com>
2021-04-20 10:31:14 +0200

    Adjust German getClausesSpec

--- packages/yoastseo/spec/languageProcessing/languages/de/helpers/getClausesSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/de/helpers/getClausesSpec.js
@@ -2,15 +2,19 @@ import getClauses from "../../../../../src/languageProcessing/languages/de/helpe
 
 describe( "splits German sentences into clauses", function() {
 	it( "returns all clauses and their passiveness", function() {
-		const sentence =  "Zwischen 1927 und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron.";
-		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "Zwischen 1927" );
-		expect( getClauses( sentence )[ 0 ].isPassive() ).toBe( false );
-		expect( getClauses( sentence )[ 1 ].getClauseText() ).toBe( "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron." );
-		expect( getClauses( sentence )[ 1 ].isPassive() ).toBe( false );
+		const sentence =  "Die Katze wurde adoptiert, sobald sie gesehen wurde.";
+		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "Die Katze wurde adoptiert," );
+		expect( getClauses( sentence )[ 0 ].isPassive() ).toBe( true );
+		expect( getClauses( sentence )[ 1 ].getClauseText() ).toBe( "sobald sie gesehen wurde." );
+		expect( getClauses( sentence )[ 1 ].isPassive() ).toBe( true );
 		expect( getClauses( sentence ).length ).toBe( 2 );
 	} );
 	it( "splits German sentences that begin with a stopword into clauses", function() {
-		const sentence =  "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron.";
-		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron." );
+		const sentence =  "und bekommt seinen letzten Lohn.";
+		expect( getClauses( sentence )[ 0 ].getClauseText() ).toBe( "und bekommt seinen letzten Lohn." );
+	} );
+	it( "returns an empty array if there is no auxiliary in the sentence", function() {
+		const sentence =  "Zwischen 1927 und 1933 hatte sie intensiven Kontakt zur Erzabtei Beuron.";
+		expect( getClauses( sentence ) ).toEqual( [] );
 	} );
 } );
