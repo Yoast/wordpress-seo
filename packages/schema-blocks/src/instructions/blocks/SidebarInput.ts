cff21702d6d659d0ba2bf9c23c7284ae239cf6c0
Johanna de Vos <johanna@yoast.com>
2021-04-20 16:48:18 +0200

    Set the className attribute
    So we can pass a class name from the template in order to have styling

--- packages/schema-blocks/src/instructions/blocks/SidebarInput.ts
+++ packages/schema-blocks/src/instructions/blocks/SidebarInput.ts
@@ -24,6 +24,7 @@ class SidebarInput extends SidebarBase {
 		const attributes: TextControl.Props = {
 			label: this.options.label,
 			value: props.attributes[ this.options.name ] as string,
+			className: this.options.className,
 			onChange: value => props.setAttributes( { [ this.options.name ]: this.options.type === "number" ? parseInt( value, 10 ) : value } ),
 			key: i,
 		};
