70a4fcad6c81750701065a20a543eacc4a920e19
FAMarfuaty <aida@yoast.com>
2021-04-21 16:21:46 +0200

    Delete Sentence Class

--- packages/yoastseo/src/values/index.js
+++ packages/yoastseo/src/values/index.js
@@ -1,13 +1,11 @@
 import AssessmentResult from "./AssessmentResult";
 import Mark from "./Mark";
 import Paper from "./Paper";
-import Sentence from "./Sentence";
 import SentencePart from "./SentencePart";
 
 export {
 	AssessmentResult,
 	Mark,
 	Paper,
-	Sentence,
 	SentencePart,
 };
