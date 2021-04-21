7d52ea8c16c6a0572ecc8bebf8c6be3f81b0c49d
FAMarfuaty <aida@yoast.com>
2021-04-21 16:23:58 +0200

    Delete SentencePart Class

--- packages/yoastseo/src/values/index.js
+++ packages/yoastseo/src/values/index.js
@@ -1,11 +1,9 @@
 import AssessmentResult from "./AssessmentResult";
 import Mark from "./Mark";
 import Paper from "./Paper";
-import SentencePart from "./SentencePart";
 
 export {
 	AssessmentResult,
 	Mark,
 	Paper,
-	SentencePart,
 };
