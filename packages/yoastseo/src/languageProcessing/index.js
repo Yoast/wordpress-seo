3ae0157a3dddc0ec493d6f0e27c8a7c1aea70058
FAMarfuaty <aida@yoast.com>
2021-04-21 15:51:17 +0200

    delete obsolete helpers from languageProcessing/index.js

--- packages/yoastseo/src/languageProcessing/index.js
+++ packages/yoastseo/src/languageProcessing/index.js
@@ -6,15 +6,11 @@ import stripSpaces from "./helpers/sanitize/stripSpaces";
 import baseStemmer from "./helpers/morphology/baseStemmer";
 import getWords from "./helpers/word/getWords";
 import AbstractResearcher from "./AbstractResearcher";
-import splitSentence from "./helpers/passiveVoice/periphrastic/freeAuxiliaryParticipleOrder/getSentencePartsSplitOnStopwords";
-import getPeriphrasticSentenceParts from "./helpers/passiveVoice/periphrastic/getSentenceParts";
-import determineSentencePartIsPassive from "./helpers/passiveVoice/periphrastic/determineSentencePartIsPassive";
 import flattenSortLength from "./helpers/morphology/flattenSortLength";
 import indices from "./helpers/word/indices";
 import buildFormRule from "./helpers/morphology/buildFormRule";
 import createRulesFromArrays, { createSingleRuleFromArray } from "./helpers/morphology/createRulesFromArrays";
 import matchRegularParticiples from "./helpers/passiveVoice/periphrastic/matchRegularParticiples";
-import checkException from "./helpers/passiveVoice/periphrastic/checkException.js";
 import directPrecedenceException from "./helpers/passiveVoice/periphrastic/directPrecedenceException";
 import precedenceException from "./helpers/passiveVoice/periphrastic/precedenceException";
 import nonDirectPrecedenceException from "./helpers/passiveVoice/periphrastic/freeAuxiliaryParticipleOrder/nonDirectParticiplePrecedenceException";
@@ -36,16 +32,12 @@ export {
 	stripSpaces,
 	baseStemmer,
 	getWords,
-	splitSentence,
-	getPeriphrasticSentenceParts,
-	determineSentencePartIsPassive,
 	flattenSortLength,
 	indices,
 	buildFormRule,
 	createRulesFromArrays,
 	createSingleRuleFromArray,
 	matchRegularParticiples,
-	checkException,
 	directPrecedenceException,
 	precedenceException,
 	nonDirectPrecedenceException,
