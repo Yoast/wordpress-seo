7d524fdea3bcdbae68cc88fcaa540c35387185ad
Andy Meerwaldt <andy@yoast.com>
2021-04-22 11:10:22 +0200

    Pass the tag as a string

--- src/schema-templates/ingredients.block.php
+++ src/schema-templates/ingredients.block.php
@@ -2,5 +2,5 @@
 {{block name="yoast/ingredients" title="Ingredients" category="common" parent=[ "yoast/recipe" ] }}
 <div class={{class-name}}>
 	{{heading name="title" defaultHeadingLevel=3 placeholder="Ingredients title" }}
-	{{rich-text name="ingredients" tag=["ul"] multiline="li" placeholder="Ingredient 1" }}
+	{{rich-text name="ingredients" tag="ul" multiline="li" placeholder="Ingredient 1" }}
 </div>
