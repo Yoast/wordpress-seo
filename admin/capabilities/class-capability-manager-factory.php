a227cf018ca2c04f2e863ab4af3d87d2c18df6ad
Andrea Fercia <a.fercia@gmail.com>
2021-04-23 15:08:02 +0200

    Allow Free and Premium to use separate instances of the Capability Manager Factory.

--- admin/capabilities/class-capability-manager-factory.php
+++ admin/capabilities/class-capability-manager-factory.php
@@ -13,21 +13,23 @@ class WPSEO_Capability_Manager_Factory {
 	/**
 	 * Returns the Manager to use.
 	 *
+	 * @param string $plugin_type Whether it's Free or Premium.
+	 *
 	 * @return WPSEO_Capability_Manager Manager to use.
 	 */
-	public static function get() {
-		static $manager = null;
+	public static function get( $plugin_type = 'free' ) {
+		static $manager = [];
 
-		if ( $manager === null ) {
+		if ( ! array_key_exists( $plugin_type, $manager ) ) {
 			if ( function_exists( 'wpcom_vip_add_role_caps' ) ) {
-				$manager = new WPSEO_Capability_Manager_VIP();
+				$manager[ $plugin_type ] = new WPSEO_Capability_Manager_VIP();
 			}
 
 			if ( ! function_exists( 'wpcom_vip_add_role_caps' ) ) {
-				$manager = new WPSEO_Capability_Manager_WP();
+				$manager[ $plugin_type ] = new WPSEO_Capability_Manager_WP();
 			}
 		}
 
-		return $manager;
+		return $manager[ $plugin_type ];
 	}
 }
