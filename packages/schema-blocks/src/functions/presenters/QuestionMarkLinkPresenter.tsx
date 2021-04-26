5485a6cf2d5e230167afbe4c9a782564508e2faf
Johanna de Vos <johanna@yoast.com>
2021-04-26 11:20:24 +0200

    Change the colors of the question mark icon

--- packages/schema-blocks/src/functions/presenters/QuestionMarkLinkPresenter.tsx
+++ packages/schema-blocks/src/functions/presenters/QuestionMarkLinkPresenter.tsx
@@ -14,7 +14,7 @@ interface QuestionMarkLinkProps {
 export function QuestionMarkLink( props: QuestionMarkLinkProps ): JSX.Element {
 	return (
 		<svg xmlns="http://www.w3.org/2000/svg" viewBox="-2 0 20 18" fill="currentColor" height="15" width="22">
-			<a href={ props.URL } rel="noopener noreferrer" target="_blank">
+			<a className="yoast-question-mark-icon" href={ props.URL } rel="noopener noreferrer" target="_blank">
 				<path
 					fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113
 				8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z"
