3e5e68b86894ec5e41d57ae96a936fb9b8782f25
guido offermans <guidooffermans@hotmail.com>
2021-05-10 16:08:44 +0200

    add licences page conditional
    Co-authored-by: Diede Exterkate <diede@yoast.com>

---
+++ src/conditionals/admin/licenses_page_conditional.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Yoast\WP\SEO\Conditionals\Admin;
+
+use Yoast\WP\SEO\Conditionals\Conditional;
+
+/**
+ * Conditional that is only met when current page is the tools page.
+ */
+class Licenses_Page_Conditional implements Conditional {
+
+	/**
+	 * Returns whether or not this conditional is met.
+	 *
+	 * @return boolean Whether or not the conditional is met.
+	 */
+	public function is_met() {
+		global $pagenow;
+
+		if ( $pagenow !== 'admin.php' ) {
+			return false;
+		}
+
+		if ( isset( $_GET['page'] ) && $_GET['page'] === 'wpseo_licenses' ) {
+			return true;
+		}
+
+		return false;
+	}
+}
