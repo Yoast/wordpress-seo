fa4d9afabdec743e27965c4febf63570a7c576d1
Hans-Christiaan <hans-christiaan@yoast.com>
2021-05-12 16:06:38 +0200

    Removed unused import.

--- js/src/containers/SchemaTab.js
+++ js/src/containers/SchemaTab.js
@@ -1,7 +1,7 @@
 import { compose } from "@wordpress/compose";
 import { withDispatch, withSelect } from "@wordpress/data";
 import { useEffect } from "@wordpress/element";
-import { __, sprintf } from "@wordpress/i18n";
+import { __ } from "@wordpress/i18n";
 import PropTypes from "prop-types";
 import SchemaTab from "../components/SchemaTab";
 import SchemaFields from "../helpers/fields/SchemaFields";
