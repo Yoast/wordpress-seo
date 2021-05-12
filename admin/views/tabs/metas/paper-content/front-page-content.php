a6fbea626610d8890877a52a84d147e9f77254f9
dennis <dennis@yoast.com>
2021-05-12 16:37:16 +0200

    Removed old frontpage, introduced notification based on #17011

--- admin/views/tabs/metas/paper-content/front-page-content.php
+++ admin/views/tabs/metas/paper-content/front-page-content.php
@@ -11,27 +11,36 @@
  */
 
 
-echo '<div class="yoast-settings-section">';
-echo '<p>' . esc_html__( 'These are the image, title and description used when a link to your homepage is shared on social media.', 'wordpress-seo' ) . '</p>';
+$view_utils                   = new Yoast_View_Utils();
+$opengraph_disabled_alert     = $view_utils->generate_opengraph_disabled_alert( 'homepage' );
 
-echo '<div id="yoast-og-frontpage-image-select"></div>';
+if( ! empty( $opengraph_disabled_alert ) ) {
+	// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Is correctly escaped in the generate_opengraph_disabled_alert() method.
+	echo $opengraph_disabled_alert;
+} else {
 
-$yform->hidden( 'open_graph_frontpage_image', 'open_graph_frontpage_image' );
-$yform->hidden( 'open_graph_frontpage_image_id', 'open_graph_frontpage_image_id' );
+	echo '<div class="yoast-settings-section">';
+	echo '<p>' . esc_html__( 'These are the image, title and description used when a link to your homepage is shared on social media.', 'wordpress-seo' ) . '</p>';
 
-$editor = new WPSEO_Replacevar_Editor(
-	$yform,
-	[
-		'title'                   => 'open_graph_frontpage_title',
-		'description'             => 'open_graph_frontpage_desc',
-		'page_type_recommended'   => 'homepage',
-		'page_type_specific'      => 'page',
-		'paper_style'             => false,
-		'label_title'             => __( 'Social title', 'wordpress-seo' ),
-		'label_description'       => __( 'Social description', 'wordpress-seo' ),
-		'description_placeholder' => __( 'Modify your social description by editing it right here.', 'wordpress-seo' ),
-		'has_new_badge'           => true,
-	]
-);
-$editor->render();
-echo '</div>';
+	echo '<div id="yoast-og-frontpage-image-select"></div>';
+
+	$yform->hidden( 'open_graph_frontpage_image', 'open_graph_frontpage_image' );
+	$yform->hidden( 'open_graph_frontpage_image_id', 'open_graph_frontpage_image_id' );
+
+	$editor = new WPSEO_Replacevar_Editor(
+		$yform,
+		[
+			'title'                   => 'open_graph_frontpage_title',
+			'description'             => 'open_graph_frontpage_desc',
+			'page_type_recommended'   => 'homepage',
+			'page_type_specific'      => 'page',
+			'paper_style'             => false,
+			'label_title'             => __( 'Social title', 'wordpress-seo' ),
+			'label_description'       => __( 'Social description', 'wordpress-seo' ),
+			'description_placeholder' => __( 'Modify your social description by editing it right here.', 'wordpress-seo' ),
+			'has_new_badge'           => true,
+		]
+	);
+	$editor->render();
+	echo '</div>';
+}
