eeb4c39eb743c0f84642b1657db019d7ccea3b3e
dennis <dennis@yoast.com>
2021-05-12 15:36:44 +0200

    fixup! Separate papers for Search Appearance > General

--- admin/views/tabs/metas/general.php
+++ admin/views/tabs/metas/general.php
@@ -11,50 +11,49 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 	exit();
 }
 
-$title_separator_title = \esc_html__( 'Title Separator', 'wordpress-seo' );
+$wpseo_title_separator_title     = \esc_html__( 'Title Separator', 'wordpress-seo' );
 $wpseo_title_separator_presenter = new WPSEO_Paper_Presenter(
-	$title_separator_title,
+	$wpseo_title_separator_title,
 	__DIR__ . '/paper-content/general/title-separator.php',
 	[
 		'collapsible' => true,
 		'expanded'    => true,
 		'paper_id'    => 'settings-general-title-separator',
-		'title'       => $title_separator_title,
+		'title'       => $wpseo_title_separator_title,
 		'class'       => 'search-appearance'
 	]
 );
 echo $wpseo_title_separator_presenter->get_output();
 
 if ( get_option( 'show_on_front' ) === 'posts' ) {
-	$homepage_title = \esc_html__( 'Homepage', 'wordpress-seo' );
+	$wpseo_homepage_title = \esc_html__( 'Homepage', 'wordpress-seo' );
 } else {
-	$homepage_title = \esc_html__( 'Homepage &amp; Posts page', 'wordpress-seo' );
+	$wpseo_homepage_title = \esc_html__( 'Homepage &amp; Posts page', 'wordpress-seo' );
 }
 
 $wpseo_homepage_presenter = new WPSEO_Paper_Presenter(
-	$homepage_title,
+	$wpseo_homepage_title,
 	__DIR__ . '/paper-content/general/homepage.php',
 	[
 		'collapsible' => true,
 		'expanded'    => true,
 		'paper_id'    => 'settings-general-homepage',
-		'title'       => $homepage_title,
+		'title'       => $wpseo_homepage_title,
 		'class'       => 'search-appearance'
 	]
 );
 echo $wpseo_homepage_presenter->get_output();
 
-$knowledge_graph_title = esc_html__( 'Knowledge Graph & Schema.org', 'wordpress-seo' );
-
+$wpseo_knowledge_graph_title     = esc_html__( 'Knowledge Graph & Schema.org', 'wordpress-seo' );
 $wpseo_knowledge_graph_presenter = new WPSEO_Paper_Presenter(
-	$knowledge_graph_title,
+	$wpseo_knowledge_graph_title,
 	__DIR__ . '/paper-content/general/knowledge-graph.php',
 	[
 		'collapsible' => true,
 		'expanded'    => true,
-		'paper_id' => 'settings-general-knowledge-graph',
-		'title'       => $knowledge_graph_title,
-		'class' => 'search-appearance'
+		'paper_id'    => 'settings-general-knowledge-graph',
+		'title'       => $wpseo_knowledge_graph_title,
+		'class'       => 'search-appearance'
 	]
 );
 echo $wpseo_knowledge_graph_presenter->get_output();
