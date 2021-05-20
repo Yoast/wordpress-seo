7312870eb1c1078a0cd79da81a5b96ae387122bd
Andrea Fercia <a.fercia@gmail.com>
2021-05-20 11:03:57 +0200

    Add missing question marks.

--- admin/views/tabs/metas/paper-content/taxonomy-content.php
+++ admin/views/tabs/metas/paper-content/taxonomy-content.php
@@ -36,7 +36,7 @@ if ( $wpseo_taxonomy->name !== 'post_format' ) {
 	$yform->show_hide_switch(
 		'display-metabox-tax-' . $wpseo_taxonomy->name,
 		/* translators: %s: Expands to an indexable object's name, like a post type or taxonomy. */
-		sprintf( __( 'Show SEO settings for %1$s', 'wordpress-seo' ), $title )
+		sprintf( __( 'Show SEO settings for %1$s?', 'wordpress-seo' ), $title )
 	);
 }
 
