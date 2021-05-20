d6ca97220d12ef2a49728af6b3471ac860238a5c
JustinyAhin <justiny.ahinon@gmail.com>
2021-05-20 12:02:40 +0100

    Add e2e tests for Yoast blocks

---
+++ packages/e2e-tests/specs/check-yoast-seo-blocks.test.js
@@ -0,0 +1,46 @@
+/**
+ * WordPress e2e utilities
+ */
+import {
+	createNewPost,
+	trashAllPosts,
+	insertBlock,
+	getEditedPostContent
+} from "@wordpress/e2e-test-utils";
+
+describe( "Yoast SEO blocks", () => {
+
+	const title = "Test Post";
+
+	beforeEach( async () => {
+		await trashAllPosts();
+	} );
+
+	it( "shows correctly Yoast SEO FAQ block", async () => {
+		await createNewPost( { title } );
+
+		await insertBlock( "Yoast FAQ" );
+		expect( 
+			await page.$( '[data-type="yoast/faq-block"]' ) 
+		).not.toBeNull();
+	} );
+
+	it( "shows correctly Yoast Breadcrumbs block", async () => {
+		await createNewPost( { title } );
+
+		await insertBlock( "Yoast Breadcrumbs" );
+		expect( 
+			await page.$( '[data-type="yoast-seo/breadcrumbs"]' ) 
+		).not.toBeNull();
+	} );
+
+	it( "shows correctly Yoast How-to block", async () => {
+		await createNewPost( { title } );
+
+		await insertBlock( "Yoast How-to" );
+		expect( 
+			await page.$( '[data-type="yoast/how-to-block"]' ) 
+		).not.toBeNull();
+	} );
+
+} );
