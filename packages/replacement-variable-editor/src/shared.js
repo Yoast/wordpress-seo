c5c0a43bd0d7334d194c349ecd2ea2c553506400
Andrea Fercia <a.fercia@gmail.com>
2021-05-20 17:50:27 +0200

    Fix draft.js input fields height bug.

--- packages/replacement-variable-editor/src/shared.js
+++ packages/replacement-variable-editor/src/shared.js
@@ -7,8 +7,7 @@ const greyPlaceholderColor = "#707070";
 
 export const TitleInputContainer = styled( VariableEditorInputContainer )`
 	.public-DraftStyleDefault-block {
-		// Don't use properties that trigger hasLayout in IE11.
-		line-height: 24px;
+		line-height: 1.85714285; // 26px based on 14px font-size
 	}
 
 	.public-DraftEditorPlaceholder-root {
@@ -22,8 +21,8 @@ export const TitleInputContainer = styled( VariableEditorInputContainer )`
 
 export const DescriptionInputContainer = styled( VariableEditorInputContainer )`
 	min-height: 72px;
-	padding: 2px 6px;
-	line-height: 24px;
+	padding: 4px 5px;
+	line-height: 1.85714285; // 26px based on 14px font-size
 
 	.public-DraftEditorPlaceholder-root {
 		color: ${greyPlaceholderColor};
