3afe53b4c775b93eabff2bf8d930f04a847f4f94
JustinyAhin <justiny.ahinon@gmail.com>
2021-05-20 09:06:24 +0100

    Update yoast metabox check test

--- packages/e2e-tests/specs/check-yoast-seo-metabox.test.js
+++ packages/e2e-tests/specs/check-yoast-seo-metabox.test.js
@@ -3,27 +3,14 @@
  */
 import {
 	createNewPost,
-	installPlugin,
-	activatePlugin
-} from '@wordpress/e2e-test-utils';
+} from "@wordpress/e2e-test-utils";
 
-describe( 'Yoast SEO plugin metabox', () => {
-	beforeEach( async () => {
-		// Testing this with Yoast plugin installed and activated
-		// on the docker WordPress installation.
-		// If the plugin is not installed and activated, the next two lines
-		// need to be uncommented
-
-		// await installPlugin( 'wordpress-seo' );
-		// await activatePlugin( 'wordpress-seo' );
-	} );
-
-	it( 'should contains the Yoast SEO plugin metabox', async () => {
+describe( "Yoast SEO plugin metabox", () => {
+	it( "should contains the Yoast SEO plugin metabox", async () => {
 		await createNewPost();
-
-			await page.waitForSelector( '.postbox.yoast.wpseo-metabox' );
-			const yoastSeoMetabox = await page.$x(
-			`//*[contains(@class, 'postbox yoast wpseo-metabox')]`
+		await page.waitForSelector( ".postbox.yoast.wpseo-metabox" );
+		const yoastSeoMetabox = await page.$x(
+		`//*[contains(@class, "postbox yoast wpseo-metabox")]`
 		);
 		expect( yoastSeoMetabox.length ).toBe( 1 );
 	} );
