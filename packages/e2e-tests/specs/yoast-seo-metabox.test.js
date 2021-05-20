b03d6b10bb8b20a9d31543e40d4362eb3d9bc532
JustinyAhin <justiny.ahinon@gmail.com>
2021-05-20 14:13:41 +0100

    Update yoast-seo-metabox.test.js

--- packages/e2e-tests/specs/yoast-seo-metabox.test.js
+++ packages/e2e-tests/specs/yoast-seo-metabox.test.js
@@ -10,10 +10,9 @@ describe( "Yoast SEO plugin metabox", () => {
 	it( "should contains the Yoast SEO plugin metabox", async () => {
 		await createNewPost();
 		await page.waitForSelector( ".postbox.yoast.wpseo-metabox" );
-		const yoastSeoMetabox = await page.$x(
-		`//*[contains( @class, "postbox yoast wpseo-metabox" )]`
-		);
-		expect( yoastSeoMetabox.length ).toBe( 1 );
+		expect(
+			`//*[contains( @class, "postbox yoast wpseo-metabox" )]`
+		).not.toBeNull();
 	} );
 
 } );
