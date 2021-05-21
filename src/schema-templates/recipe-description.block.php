c53c460a423216c897638de694b10baf67d42c95
Alexander Botteram <a.botteram@gmail.com>
2021-05-21 09:12:14 +0200

    Changed translations domain to wordpress-seo

--- src/schema-templates/recipe-description.block.php
+++ src/schema-templates/recipe-description.block.php
@@ -9,7 +9,7 @@ use Yoast\WP\SEO\Schema_Templates\Assets\Icons;
 
 // phpcs:disable WordPress.Security.EscapeOutput -- Reason: The Icons contains safe svg.
 ?>
-{{block name="yoast/recipe-description" title="<?php esc_attr_e( 'Recipe description', 'wordpress-seo-premium' ); ?>" category="yoast-required-recipe-blocks" description="<?php esc_attr_e( 'The description of the recipe.', 'wordpress-seo-premium' ); ?>" icon="<?php echo Icons::heroicons_identification(); ?>" supports={"multiple": false} }}
+{{block name="yoast/recipe-description" title="<?php esc_attr_e( 'Recipe description', 'wordpress-seo' ); ?>" category="yoast-required-recipe-blocks" description="<?php esc_attr_e( 'The description of the recipe.', 'wordpress-seo' ); ?>" icon="<?php echo Icons::heroicons_identification(); ?>" supports={"multiple": false} }}
 <div class="yoast-recipe-block__description {{class-name}}">
-	{{rich-text name="description" required=true tag="p" keepPlaceholderOnFocus=true placeholder="<?php esc_attr_e( 'Enter a recipe description...', 'wordpress-seo-premium' ); ?>"}}
+	{{rich-text name="description" required=true tag="p" keepPlaceholderOnFocus=true placeholder="<?php esc_attr_e( 'Enter a recipe description...', 'wordpress-seo' ); ?>"}}
 </div>
