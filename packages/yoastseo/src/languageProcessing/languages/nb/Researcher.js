84adc1166db5385fbd8890aa626b450b8be3f05d
FAMarfuaty <aida@yoast.com>
2021-05-28 12:28:09 +0200

    Initial implementation of feature flag

--- packages/yoastseo/src/languageProcessing/languages/nb/Researcher.js
+++ packages/yoastseo/src/languageProcessing/languages/nb/Researcher.js
@@ -1,11 +1,19 @@
 import { languageProcessing } from "yoastseo";
 const { AbstractResearcher } = languageProcessing;
+import { enableFeatures, isFeatureEnabled } from "@yoast/feature-flag";
+import firstWordExceptions from "../en/config/firstWordExceptions";
+import getSentenceParts from "../hu/helpers/getSentenceParts";
+import isPassiveSentence from "../hu/helpers/isPassiveSentence";
+import isPassiveSentencePart from "../hu/helpers/isPassiveSentencePart";
 
 // All config
 import functionWords from "./config/functionWords";
 
 // All helpers
 import getStemmer from "./helpers/getStemmer";
+
+// // The list of the feature flag names
+// const featureNames = [ 'NORWEGIAN_READABILITY' ];
 
 /**
  * The researches contains all the researches
@@ -19,10 +27,30 @@ export default class Researcher extends AbstractResearcher {
 	constructor( paper ) {
 		super( paper );
 
+		// Delete Flesch Reading Ease research since Norwegian doesn't have the support for it
 		delete this.defaultResearches.getFleschReadingScore;
-		delete this.defaultResearches.getPassiveVoice;
-		delete this.defaultResearches.getSentenceBeginnings;
-		delete this.defaultResearches.findTransitionWords;
+
+		// Enable the other Norwegian readability features
+		enableFeatures( [ 'NORWEGIAN_READABILITY' ] );
+
+		// Delete the transition words, passive voice and getSentenceBeginning researches if the Norwegian readability is not enabled
+		if ( isFeatureEnabled( 'NORWEGIAN_READABILITY' ) ) {
+			Object.assign( this.config, {
+				passiveConstructionType,
+				transitionWords,
+				twoPartTransitionWords,
+				firstWordExceptions,
+			} );
+			Object.assign( this.helpers, {
+				getSentenceParts,
+				isPassiveSentencePart,
+				isPassiveSentence,
+			} );
+		} else {
+			delete this.defaultResearches.findTransitionWords;
+			delete this.defaultResearches.getPassiveVoice;
+			delete this.defaultResearches.getSentenceBeginnings;
+		}
 
 		Object.assign( this.config, {
 			language: "nb",
