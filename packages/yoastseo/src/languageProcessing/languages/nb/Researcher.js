b4aac158226db64922a37f395a0512666c07177e
FAMarfuaty <aida@yoast.com>
2021-05-31 11:46:57 +0200

    Adjust the Norwegian researcher

--- packages/yoastseo/src/languageProcessing/languages/nb/Researcher.js
+++ packages/yoastseo/src/languageProcessing/languages/nb/Researcher.js
@@ -1,15 +1,11 @@
 import { languageProcessing } from "yoastseo";
 const { AbstractResearcher } = languageProcessing;
-import { enableFeatures, isFeatureEnabled, enabledFeatures } from "@yoast/feature-flag";
 
 // All config
 import functionWords from "./config/functionWords";
 
 // All helpers
 import getStemmer from "./helpers/getStemmer";
-
-// // The list of the feature flag names
-// const featureNames = [ 'NORWEGIAN_READABILITY' ];
 
 /**
  * The researches contains all the researches
@@ -25,28 +21,6 @@ export default class Researcher extends AbstractResearcher {
 
 		// Delete Flesch Reading Ease research since Norwegian doesn't have the support for it
 		delete this.defaultResearches.getFleschReadingScore;
-
-		// Enable the other Norwegian readability features
-		//enableFeatures( [ 'NORWEGIAN_READABILITY' ] );
-
-		if ( isFeatureEnabled("norwegian-readability" ) ) {
-			Object.assign( this.config, {
-				passiveConstructionType: "morphologicalAndPeriphrastic",
-				transitionWords: ["of"],
-				twoPartTransitionWords: [["of", "and"]],
-				firstWordExceptions: ["the"],
-			} );
-		/*	Object.assign( this.helpers, {
-				getSentenceParts,
-				isPassiveSentencePart,
-				isPassiveSentence,
-			} );*/
-		} else {
-			// Delete the researches for other readability assessments when the feature is not enabled.
-			delete this.defaultResearches.findTransitionWords;
-			delete this.defaultResearches.getPassiveVoice;
-			delete this.defaultResearches.getSentenceBeginnings;
-		}
 
 		Object.assign( this.config, {
 			language: "nb",
