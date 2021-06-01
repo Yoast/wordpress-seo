060659cc4f131e9ef9154bd25a9b583f9729d50f
Alexander Botteram <a.botteram@gmail.com>
2021-06-01 12:38:13 +0200

    Made sure ingredients and instructions for the recipe block have the same mark-up. Also added translations

--- src/schema-templates/steps.block.php
+++
@@ -1,6 +0,0 @@
-<?php // phpcs:ignore Internal.NoCodeFound ?>
-{{block name="yoast/steps" title="Steps" category="common" parent=[ "yoast/recipe" ] }}
-<ul class={{class-name}}>
-	{{heading name="title" defaultHeadingLevel=3 placeholder="Steps title" }}
-	{{inner-blocks allowed-blocks=["yoast/step"] appender="button" appenderLabel="Add step" }}
-</ul>
