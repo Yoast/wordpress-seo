d23981dc8a4a441d02c19242cf20965b10e132c1
Andrea Fercia <a.fercia@gmail.com>
2021-06-02 15:31:32 +0200

    Remove unused action hook.

--- admin/views/tabs/metas/paper-content/post_type/post-type.php
+++ admin/views/tabs/metas/paper-content/post_type/post-type.php
@@ -48,14 +48,6 @@ $editor->render();
 
 echo '</div>';
 
-/**
- * Allow adding custom fields to the admin meta page - Content Types tab.
- *
- * @param Yoast_Form $yform The Yoast_Form object.
- * @param string     $name  The post type name.
- */
-do_action( 'Yoast\WP\SEO\admin_post_types_meta', $yform, $wpseo_post_type->name );
-
 echo '<div class="yoast-settings-section">';
 
 // Schema settings.
