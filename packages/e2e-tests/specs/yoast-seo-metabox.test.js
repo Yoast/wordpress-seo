bf76778ce106a613f8690847d5e763a11cdc650b
JustinyAhin <justiny.ahinon@gmail.com>
2021-06-03 10:10:19 +0100

    Check if the Yoast metabox is present when editing pages

--- packages/e2e-tests/specs/yoast-seo-metabox.test.js
+++ packages/e2e-tests/specs/yoast-seo-metabox.test.js
@@ -4,17 +4,49 @@
 import {
 	trashAllPosts,
 	createNewPost,
+	visitAdminPage,
 } from "@wordpress/e2e-test-utils";
+import { addQueryArgs } from '@wordpress/url';
 
 describe( "Yoast SEO plugin metabox", () => {
 
-	it( "should contains the Yoast SEO plugin metabox", async () => {
+	it( "shows correctly the Yoast SEO metabox should be present when editing a post", async () => {
 		await trashAllPosts();
 		await createNewPost();
+
 		await page.waitForSelector( ".postbox.yoast.wpseo-metabox" );
 		expect(
-			`//*[contains( @class, "postbox yoast wpseo-metabox" )]`
+			`//div[contains( @class, "postbox yoast wpseo-metabox" )]`
 		).not.toBeNull();
 	} );
+
+	it( "shows correctly the Yoast SEO metabox should be present when editing a page", async () => {
+		await trashAllPosts( "page" );
+		await createNewPost( { postType: "page" } );
+
+		await page.waitForSelector( ".postbox.yoast.wpseo-metabox" );
+		expect(
+			`//div[contains( @class, "postbox yoast wpseo-metabox" )]`
+		).not.toBeNull();
+	} );
+
+/* 	it( "shows correctly the Yoast SEO metabox should be present when editing a post category page", async () => {
+		const postCategoryQuery = addQueryArgs( '', {
+			taxonomy: 'category',
+		} );
+
+		await visitAdminPage( 'edit-tags.php', postCategoryQuery );
+		await page.waitForSelector( '#the-list tr' );
+		
+		const [ uncathe ] = await page.$x(
+			`//a[contains( @class, "row-title" )][contains( text(), "${ categoryTitle }" )]`
+		);
+		await editLink.click();
+
+		await page.waitForSelector( ".postbox.yoast.wpseo-taxonomy-metabox-postbox" );
+		expect(
+			`//div[contains( @class, "postbox yoast wpseo-taxonomy-metabox-postbox" )]`
+		).not.toBeNull();
+	} ); */
 
 } );
