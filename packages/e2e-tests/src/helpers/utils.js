bc819b3e082a4033f453b659da894b2e2600925f
JustinyAhin <justiny.ahinon@gmail.com>
2021-06-03 17:02:27 +0100

    Create utils.js

---
+++ packages/e2e-tests/src/helpers/utils.js
@@ -0,0 +1,15 @@
+/**
+ * WordPress e2e utilities
+ */
+import {
+    installPlugin,
+    activatePlugin,
+    visitAdminPage,
+    pressKeyWithModifier,
+} from "@wordpress/e2e-test-utils";
+
+import { addQueryArgs } from '@wordpress/url';
+
+export const createCustomPostType = async( slug ) => {
+   
+}
