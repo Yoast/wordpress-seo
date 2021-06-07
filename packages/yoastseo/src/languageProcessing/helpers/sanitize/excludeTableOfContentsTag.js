9ff32ee8f58f1d3913beb74050434d16c572c1d5
FAMarfuaty <aida@yoast.com>
2021-06-07 12:49:40 +0200

    Improve function description

--- packages/yoastseo/src/languageProcessing/helpers/sanitize/excludeTableOfContentsTag.js
+++ packages/yoastseo/src/languageProcessing/helpers/sanitize/excludeTableOfContentsTag.js
@@ -1,11 +1,11 @@
 const tableOfContentsTagRegex = new RegExp( "(<div class='wp-block-yoast-seo-table-of-contents yoast-table-of-contents'>).*?(</div>)", "igs" );
 
 /**
- * Excludes table of contents text
+ * Excludes table of contents from text.
  *
- * @param {String} text The text to check
+ * @param {String} text The text to check.
  *
- * @returns {String}    The stripped text
+ * @returns {String} The text without table of contents.
  */
 export default function excludeTableOfContentsTag( text ) {
 	text = text.replace( tableOfContentsTagRegex, "" );
