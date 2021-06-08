554fcd523fe1aac9ea369460b0daab15439fc2d2
Joost Boomkamp <3604126+increddibelly@users.noreply.github.com>
2021-06-08 09:59:45 +0200

    initial commit

--- src/presenters/admin/indexing-failed-notification-presenter.php
+++ src/presenters/admin/indexing-failed-notification-presenter.php
@@ -2,8 +2,10 @@
 
 namespace Yoast\WP\SEO\Presenters\Admin;
 
+use WPSEO_Addon_Manager;
 use Yoast\WP\SEO\Helpers\Product_Helper;
 use Yoast\WP\SEO\Presenters\Abstract_Presenter;
+
 
 /**
  * Class Indexing_Failed_Notification_Presenter.
@@ -20,12 +22,21 @@ class Indexing_Failed_Notification_Presenter extends Abstract_Presenter {
 	protected $product_helper;
 
 	/**
+	 * The license manager.
+	 *
+	 * @var WPSEO_Addon_Manager
+	 */
+	protected $class_addon_manager;
+
+	/**
 	 * Indexing_Failed_Notification_Presenter constructor.
 	 *
-	 * @param Product_Helper $product_helper The product helper.
+	 * @param Product_Helper      $product_helper      The product helper.
+	 * @param WPSEO_Addon_Manager $class_addon_manager The license manager.
 	 */
-	public function __construct( $product_helper ) {
-		$this->product_helper = $product_helper;
+	public function __construct( $product_helper, $class_addon_manager ) {
+		$this->class_addon_manager = $class_addon_manager;
+		$this->product_helper      = $product_helper;
 	}
 
 	/**
@@ -42,11 +53,26 @@ class Indexing_Failed_Notification_Presenter extends Abstract_Presenter {
 			'</a>'
 		);
 		if ( $this->product_helper->is_premium() ) {
-			$notification_text .= ' ';
-			$notification_text .= \esc_html__( 'If the problem persists, please contact support.', 'wordpress-seo' );
+			if ( $this->has_premium_license() ) {
+				$notification_text .= ' ';
+				$notification_text .= \esc_html__('If the problem persists, please contact support.', 'wordpress-seo');
+			} else {
+				// no premium license
+				$notification_text = \sprintf(
+					\esc_html__( 'Oops, something has gone wrong and we couldn\'t complete the optimization of your SEO data. Please make sure your license is active in MyYoast by completing %1$sthe provided steps here%2$s.',
+						'wordpress-seo',
+						'<a href="'. \esc_url( $this->short_link_helper->get( 'https://yoa.st/3wv' ) ) . '">',
+						'</a>'
+					);
+
+			}
 		}
 		$notification_text .= '</p>';
 
 		return $notification_text;
+	}
+
+	protected function has_premium_license() {
+		return $this->class_addon_manager->has_valid_subscription( WPSEO_Addon_Manager::PREMIUM_SLUG );
 	}
 }
