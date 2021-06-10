bce3fd4302924089b909dd7a24c73eb1636de791
JustinyAhin <justiny.ahinon@gmail.com>
2021-06-10 11:01:59 +0100

    Add tests for Yoast SEO metabox and sidebar
    Add tests for checking Yoast SEO metabox and sidebar when editing a custom post and a custom post taxonomy

--- packages/e2e-tests/specs/yoast-seo-metabox.test.js
+++ packages/e2e-tests/specs/yoast-seo-metabox.test.js
@@ -94,74 +94,53 @@ describe( "Yoast SEO plugin metabox", () => {
 		expect( yoastSeoTaxonomyMetabox.length ).toBe( 1 );
 	} );
 
-	it( "shows correctly the Yoast SEO metabox should be present when editing a custom post type", async() => {
-        const cptPluginSlug = "custom-post-type-ui";
-        const cptuiCreatePostType = addQueryArgs('', {
-            page: "cptui_manage_post_types",
-        });
-        const cptuiDeletePostType = addQueryArgs('', {
-            page: "cptui_manage_post_types",
-            action: "edit"
-		});
-		const slug = "yoast_post_type";
-		const plural_label = "Yoast Simple Posts"
-		const singular_label = "Yoast Simple Post"
-		
-		// If the plugin is already installed but not activated, activate it
-		// If the plugin is already installed and activated, move to the CPT management
-		// If the plugin is not installed, install and activate it
-		await visitAdminPage( "plugins.php" );
-		const cptuiPluginRow = await page.$x(
-			`//tr[contains( @data-plugin, "custom-post-type-ui/custom-post-type-ui.php" )]`
-		);
-		if( cptuiPluginRow.length == 1 ) {
-			const [ activatePluginLink ] = await page.$x(
-				`//a[contains( @id, "activate-custom-post-type-ui" )]`
-			);
-			if( activatePluginLink.length == 1 ) {
-				activatePluginLink.click()
-			}
-		} else{
-			await installPlugin(cptPluginSlug);
-			await activatePlugin(cptPluginSlug);
-		}
-
-		// When the tests are run multiple times, it is necessary
-		// to delete created custom post types before creating them again
-		await visitAdminPage( "admin.php", cptuiDeletePostType );
-		const selectCptElement = await page.$$( "#post_type" );
-		if( selectCptElement.length > 0 ) {
-			await page.click( "#cpt_submit_delete" );
-			await page.keyboard.press( "Enter" );
-		}
-
-		// Create the new custom post type
-		await visitAdminPage( "admin.php", cptuiCreatePostType );
-
-		await page.focus( "#name" );
-		await pressKeyWithModifier( "primary", "a" );
-		await page.type( "#name", slug );
-
-		await page.focus( "#label" );
-		await pressKeyWithModifier( "primary", "a" );
-		await page.type( "#label", plural_label );
-
-		await page.focus( "#singular_label" );
-		await pressKeyWithModifier( "primary", "a" );
-		await page.type( "#singular_label", singular_label );
-
-		await page.click( "#auto-populate" )
-		await page.click( ".button-primary" );
-
-		// Check if the Yoast SEO metabox is present when editing the custom post type
-		await trashAllPosts( slug );
-   		await createNewPost( { postType: slug } );
+	it( "shows correctly the Yoast SEO metabox should be present when editing a custom post", async () => {
+		await trashAllPosts( "yoast_post_type" );
+		await createNewPost( { postType: "yoast_post_type" } );
 
 		await page.waitForSelector( ".postbox.yoast.wpseo-metabox" );
-		const yoastSeoCustomPostMetabox = await page.$x(
+		const yoastSeoPostMetabox = await page.$x(
 			`//div[contains( @class, "wpseo-metabox" )][contains( .//h2, "Yoast SEO" )]`
 		);
-		expect( yoastSeoCustomPostMetabox.length ).toBe( 1 );
-    });
+		expect( yoastSeoPostMetabox.length ).toBe( 1 );
+	} );
+
+	it( "shows correctly the Yoast SEO metabox should be present when editing a custom post taxonomy page", async () => {
+		const customPostTaxomomyQuery = addQueryArgs( '', {
+			taxonomy: 'yoast_simple_posts_taxonomy',
+		} );
+		const taxonomyTitle = "New Taxonomy";
+	
+		await visitAdminPage( 'edit-tags.php', customPostTaxomomyQuery );
+	
+		// Trash all existing custom post taxonomies
+		await page.waitForSelector( '#the-list tr' );
+		const taxonomiesRows = await page.$$( '#the-list tr' );
+		if( taxonomiesRows.length !== null ) {
+			await page.click( '[id^=cb-select-all-]' );
+			await page.select( '#bulk-action-selector-top', 'delete' );
+			await page.focus( '#doaction' );
+			await page.keyboard.press( 'Enter' );
+		}
+	
+		// Create a new post taxonomy
+		await page.waitForSelector('#tag-name');
+		await page.focus( '#tag-name' );
+		await pressKeyWithModifier( 'primary', 'a' );
+		await page.type( '#tag-name', taxonomyTitle );
+		await page.click( '#submit' );
+	
+		// Go to the new created post taxonomy page
+		const [ newCreatedTaxonomy ] = await page.$x(
+			`//a[contains( @class, "row-title" )][contains( text(), "${ taxonomyTitle }" )]`
+		);
+		await newCreatedTaxonomy.click();
+	
+		await page.waitForSelector( ".postbox.yoast.wpseo-taxonomy-metabox-postbox" );
+		const yoastSeoTaxonomyMetabox = await page.$x(
+			`//div[contains( @class, "wpseo-taxonomy-metabox-postbox" )][contains( .//h2, "Yoast SEO" )]`
+		);
+		expect( yoastSeoTaxonomyMetabox.length ).toBe( 1 );
+	} );
 
 } );
--- packages/e2e-tests/specs/yoast-sidebar.test.js
+++ packages/e2e-tests/specs/yoast-sidebar.test.js
@@ -44,4 +44,22 @@ describe( "Yoast SEO sidebar", () => {
 		expect( yoastSeoSidebarTitle.length ).toBe( 1 )
 	} );
 
+	it( "shows correctly Yoast SEO sidebar when editing a custom post", async () => {
+		await trashAllPosts( "yoast_post_type" );
+   		await createNewPost( { postType: "yoast_post_type" } );
+
+		await page.waitForSelector( "aria/Yoast SEO" );
+		const yoastSeoSidebarButton = await page.$x(
+			`//button[contains( @aria-label, "Yoast SEO" )]`
+		);
+		expect( yoastSeoSidebarButton.length ).toBe( 1 );
+
+		await page.click( "aria/Yoast SEO" );
+
+		const yoastSeoSidebarTitle = await page.$x(
+			`//div[contains( @class, "components-panel__header" )][contains( .//strong, "Yoast SEO" )]`
+		);
+		expect( yoastSeoSidebarTitle.length ).toBe( 1 )
+	} );
+
 } );
