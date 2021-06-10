dce08157608b2668a91a265d58ecbee935b93d25
JustinyAhin <justiny.ahinon@gmail.com>
2021-06-10 15:31:43 +0100

    Update utils.js

--- packages/e2e-tests/src/helpers/utils.js
+++ packages/e2e-tests/src/helpers/utils.js
@@ -2,10 +2,23 @@
  * WordPress e2e utilities
  */
 import {
-    installPlugin,
-    activatePlugin,
     visitAdminPage,
-    pressKeyWithModifier,
 } from "@wordpress/e2e-test-utils";
 
 import { addQueryArgs } from '@wordpress/url';
+
+export const deleteExistingTaxonomies = async ( taxonomySlug ) => {
+	const taxonomyPageQuery = addQueryArgs( '', {
+		taxonomy: taxonomySlug,
+	} );
+	
+	await visitAdminPage( 'edit-tags.php', taxonomyPageQuery );
+	
+	const noTaxonomyFoundRow = await page.$( 'tr.no-items' );
+	if( noTaxonomyFoundRow == null ) {
+		await page.click( '[id^=cb-select-all-]' );
+		await page.select( '#bulk-action-selector-top', 'delete' );
+		await page.focus( '#doaction' );
+		await page.keyboard.press( 'Enter' );
+	}
+}
