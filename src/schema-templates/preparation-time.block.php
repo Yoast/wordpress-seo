86c5277d889fc92c64c4ce2f1cb908716328c321
Johanna de Vos <johanna@yoast.com>
2021-06-10 15:41:49 +0200

    Change the editable heading 'Prep time' according to the designs

--- src/schema-templates/preparation-time.block.php
+++ src/schema-templates/preparation-time.block.php
@@ -10,6 +10,6 @@ use Yoast\WP\SEO\Schema_Templates\Assets\Icons;
 ?>
 {{block name="yoast/preparation-time" title="<?php \esc_attr_e( 'Recipe prep time', 'wordpress-seo' ); ?>" category="yoast-structured-data-blocks" parent=[ "yoast/recipe" ] description="<?php \esc_html_e( 'The time it takes to prepare the items to be used in the instructions.', 'wordpress-seo' ); ?>" icon="<?php echo Icons::heroicons_clock(); ?>" supports={"multiple": false} }}
 <div class={{class-name}}>
-	{{heading name="title" default-heading-level=3 tags=[ "h2", "h3", "h4", "h5", "h6", "strong" ] default="<?php \esc_attr_e( 'Preparation time', 'wordpress-seo' ); ?>" }}
+	{{heading name="title" default-heading-level=3 tags=[ "h2", "h3", "h4", "h5", "h6", "strong" ] default="<?php \esc_attr_e( 'Prep time', 'wordpress-seo' ); ?>" }}
 	{{duration name="preparation-time" }}
 </div>
