607bffc2362dab79098ea2cd05d4c9f48f08beae
FAMarfuaty <aida@yoast.com>
2021-06-17 16:17:08 +0200

    Delete unnecessary console.log statement

--- packages/yoastseo/src/helpers/getLanguagesWithWordFormSupport.js
+++ packages/yoastseo/src/helpers/getLanguagesWithWordFormSupport.js
@@ -12,7 +12,6 @@ export function getLanguagesWithWordFormSupport() {
 	if ( isFeatureEnabled( "SLOVAK_SUPPORT" ) ) {
 		supportedLanguages.push( "sk" );
 	}
-	console.log( supportedLanguages );
 
 	return supportedLanguages;
 }
