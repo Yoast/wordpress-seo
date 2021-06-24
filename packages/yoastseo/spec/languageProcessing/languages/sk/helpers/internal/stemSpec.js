caa9d4e2b0f9c83f278b6d69ba82873fb75ddc22
FAMarfuaty <aida@yoast.com>
2021-06-24 13:46:26 +0200

    Add more specs

--- packages/yoastseo/spec/languageProcessing/languages/sk/helpers/internal/stemSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/sk/helpers/internal/stemSpec.js
@@ -7,11 +7,48 @@ const morphologyDataSK = getMorphologyData( "sk" ).sk;
 const wordsToStem = [
 	// Input a word ending in case suffix -eho.
 	[ "teplejšieho", "tep" ],
-
+	// Input a word ending in case suffix -atoch
+	// [ "demokratoch", "demokrat" ],
+	[ "sampionatoch", "sampio" ],
+	// Input a word ending in case suffix -aťom
+	[ "arcikniežaťom", "arcikniež" ],
+	// Input a word ending in case suffix -aťom
+	[ "arcikniežaťom", "arcikniež" ],
+	// Input a word ending in case suffix -och
+	[ "mesiacoch", "mesia" ],
+	// Input a word ending in case suffix -ému
+	[ "samotnému", "samot" ],
+	// Input a word ending in case suffix -ých
+	[ "ktorých", "ktor" ],
+	// Input a word ending in case suffix -ých and a derivational suffix -č
+	[ "zahraničných", "zahrani" ],
+	// Input a word ending in case suffix -ata
+	[ "zvierata", "zvier" ],
+	// Input a word ending in case suffix -om and a derivational suffix -ob
+	[ "spôsobom", "spôs" ],
+	// Input a word ending in case suffix -om
+	[ "ľuďom", "ľuď" ],
+	// Input a word ending in case suffix -es
+	[ "najdes", "naj" ],
+	// Input a word ending in case suffix -ím
+	[ "hovorím", "hov" ],
+	// Input a word ending in case suffix -úm
+	[ "chartúm", "char" ],
+	// Input a word ending in case suffix -ej
+	[ "súčasnej", "súčas" ],
+	// Input a word ending in possessive suffix -ov
+	[ "problémov", "problém" ],
+	// Input a word ending in possessive suffix -in
+	[ "problémin", "problém" ],
+	// Input a word ending in comparative suffix -ejš
+	[ "mokrejš", "mokr" ],
+	// Input a word ending in a case suffix -í and comparative suffix -ejš
+	[ "skúpejší", "skúp" ],
+	// Input a word ending in a case suffix -í and comparative suffix -ějš
+	[ "nejkrásnější", "nejkrás" ],
 ];
 
 const paradigms = [
-	// A paradigm of a masculine animate noun (hard declension).
 	{ stem: "tep", forms: [
 		"teplý",
 		"teplého",
@@ -24,6 +61,31 @@ const paradigms = [
 		"teplí",
 		"teplých",
 		"teplými",
+	] },
+	// Paradigm of a verb
+	{ stem: "pochop", forms: [
+		// "pochopiť",
+		// "pochopiac",
+		// "pochopiaci",
+		// "pochopiaca",
+		// "pochopiace",
+		"pochopený",
+		"pochopená",
+		"pochopené",
+		"pochopení",
+		// "pochopím",
+		// "pochopíš",
+		"pochopí",
+		// "pochopíme",
+		// "pochopíte",
+		// "pochopia",
+		// "pochopili",
+		// "pochopil",
+		// "pochopila",
+		// "pochopilo",
+		// "pochopme",
+		"pochopte",
+		// "pochopenie",
 	] },
 ];
 
