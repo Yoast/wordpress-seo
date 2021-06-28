1a0e690446e5b973fce0975fed016c0214d29d53
jdevalk <joost@yoast.com>
2021-06-28 16:26:22 +0200

    Simplify code

--- admin/views/class-yoast-integration-toggles.php
+++ admin/views/class-yoast-integration-toggles.php
@@ -110,12 +110,7 @@ class Yoast_Integration_Toggles {
 				'name'            => \sprintf( \esc_html__( '%s integration', 'wordpress-seo' ), 'Algolia' ),
 				'premium'         => true,
 				'setting'         => 'algolia_integration_active',
-				'label'           => \sprintf(
-				/* translators: 1: Yoast SEO, 2: Algolia. */
-					\__( 'Improve the quality of your site search! Automatically helps your users find your cornerstone and most important content in your internal search results. It also removes noindexed posts & pages from your site’s search results.', 'wordpress-seo' ),
-					'Yoast SEO',
-					'WP Search with Algolia'
-				),
+				'label'           => __( 'Improve the quality of your site search! Automatically helps your users find your cornerstone and most important content in your internal search results. It also removes noindexed posts & pages from your site’s search results.', 'wordpress-seo' ),
 				/* translators: %s: Algolia. */
 				'read_more_label' => \sprintf( \__( 'Find out more about our %s integration.', 'wordpress-seo' ), 'Algolia' ),
 				'read_more_url'   => 'https://yoa.st/4eu',
