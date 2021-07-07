a5f20d4e4aff5e439f466b92a39b54a8d58acf03
Hans-Christiaan <hans-christiaan@yoast.com>
2021-07-07 15:33:51 +0200

    Silenced two CS warnings.

--- src/helpers/wordpress-helper.php
+++ src/helpers/wordpress-helper.php
@@ -2,11 +2,10 @@
 
 namespace Yoast\WP\SEO\Helpers;
 
+// phpcs:disable WordPress.WP.CapitalPDangit.MisspelledClassName -- It is spelled like `Wordpress_Helper` because of Yoast's naming conventions for classes, which would otherwise break dependency injection in some cases.
+
 /**
  * A helper object for WordPress matters.
- *
- * Note: it is spelled like `Wordpress_Helper` because of Yoast's naming conventions for classes,
- * which would otherwise break dependency injection in some cases.
  */
 class Wordpress_Helper {
 
