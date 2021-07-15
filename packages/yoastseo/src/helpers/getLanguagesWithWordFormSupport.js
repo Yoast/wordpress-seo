d8c2b2d774809dc9bcaa6048560b7d9762150652
Hanna Worku <hanna@yoast.com>
2021-07-15 13:06:52 +0200

    add feature flag for Farsi language support

--- packages/yoastseo/src/helpers/getLanguagesWithWordFormSupport.js
+++ packages/yoastseo/src/helpers/getLanguagesWithWordFormSupport.js
@@ -1,4 +1,4 @@
-const supportedLanguages = [ "en", "de", "es", "fr", "it", "nl", "ru", "id", "pt", "pl", "ar", "sv", "he", "hu", "nb", "tr", "cs", "sk" ];
+import { isFeatureEnabled } from "@yoast/feature-flag";
 
 /**
  * Checks which languages have morphology support inside YoastSEO.js.
@@ -6,5 +6,11 @@ const supportedLanguages = [ "en", "de", "es", "fr", "it", "nl", "ru", "id", "pt
  * @returns {string[]} A list of languages that have morphology support.
  */
 export function getLanguagesWithWordFormSupport() {
+	const supportedLanguages = [ "en", "de", "es", "fr", "it", "nl", "ru", "id", "pt", "pl", "ar", "sv", "he", "hu", "nb", "tr", "cs", "sk" ];
+
+	// Add Farsi to the supported languages list if the feature is enabled.
+	if ( isFeatureEnabled( "FARSI_SUPPORT" ) ) {
+		supportedLanguages.push( "fa" );
+	}
 	return supportedLanguages;
 }
