b21dda0c999d4a67d399748fadb85e876ec024da
Manuel <manuel@yoast.com>
2021-07-19 09:57:18 +0200

    Add product pages assessors

---
+++ packages/yoastseo/src/scoring/productPages/cornerstone/contentAssessor.js
@@ -0,0 +1,56 @@
+import Assessor from "../../assessor.js";
+import ContentAssessor from "../../contentAssessor";
+import fleschReadingEase from "../../assessments/readability/fleschReadingEaseAssessment.js";
+import paragraphTooLong from "../../assessments/readability/paragraphTooLongAssessment.js";
+import SentenceLengthInText from "../../assessments/readability/sentenceLengthInTextAssessment.js";
+import SubheadingDistributionTooLong from "../../assessments/readability/subheadingDistributionTooLongAssessment.js";
+import transitionWords from "../../assessments/readability/transitionWordsAssessment.js";
+import passiveVoice from "../../assessments/readability/passiveVoiceAssessment.js";
+import sentenceBeginnings from "../../assessments/readability/sentenceBeginningsAssessment.js";
+import textPresence from "../../assessments/readability/textPresenceAssessment.js";
+
+/*
+ Temporarily disabled:
+
+ var wordComplexity = require( "./assessments/readability/wordComplexityAssessment.js" );
+ var sentenceLengthInDescription = require( "./assessments/readability/sentenceLengthInDescriptionAssessment.js" );
+ */
+
+/**
+ * Creates the Assessor
+ *
+ * @param {object} i18n The i18n object used for translations.
+ * @param {Object} options The options for this assessor.
+ * @param {Object} options.marker The marker to pass the list of marks to.
+ *
+ * @constructor
+ */
+const productCornerstoneContentAssessor = function( i18n, options = {} ) {
+	Assessor.call( this, i18n, options );
+	this.type = "ProductCornerstoneContentAssessor";
+
+	this._assessments = [
+
+		fleschReadingEase,
+		new SubheadingDistributionTooLong( {
+			parameters:	{
+				slightlyTooMany: 250,
+				farTooMany: 300,
+				recommendedMaximumWordCount: 250,
+			},
+		} ),
+		paragraphTooLong,
+		new SentenceLengthInText( true ),
+		transitionWords,
+		passiveVoice,
+		textPresence,
+		sentenceBeginnings,
+		// Temporarily disabled: wordComplexity,
+	];
+};
+
+require( "util" ).inherits( productCornerstoneContentAssessor, ContentAssessor );
+
+
+export default productCornerstoneContentAssessor;
+
