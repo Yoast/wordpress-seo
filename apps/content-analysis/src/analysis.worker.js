38d06eb5f3a1e3fa68dfa9b671bb1fc6fe545a55
Manuel <manuel@yoast.com>
2021-07-21 14:14:07 +0200

    Set product page assessors in content analysis app (commented out)

--- apps/content-analysis/src/analysis.worker.js
+++ apps/content-analysis/src/analysis.worker.js
@@ -1,6 +1,12 @@
 import "babel-polyfill";
 import AnalysisWebWorker from "../../../packages/yoastseo/src/worker/AnalysisWebWorker";
 import getResearcher from "../../../packages/yoastseo/spec/specHelpers/getResearcher";
+// import productSEOAssessor from "yoastseo/src/scoring/productPages/seoAssessor";
+// import productCornerstoneSEOAssessor from "yoastseo/src/scoring/productPages/cornerstone/seoAssessor";
+// import productContentAssessor from "yoastseo/src/scoring/productPages/contentAssessor";
+// import productCornerstoneContentAssessor from "yoastseo/src/scoring/productPages/cornerstone/contentAssessor";
+// import productRelatedKeywordAssessor from "yoastseo/src/scoring/productPages/relatedKeywordAssessor";
+// import productCornerstoneRelatedKeywordAssessor from "yoastseo/src/scoring/productPages/cornerstone/relatedKeywordAssessor";
 
 self.onmessage = ( event ) => {
 	const language = event.data.language;
@@ -12,5 +18,17 @@ self.onmessage = ( event ) => {
 	const Researcher = getResearcher( language );
 
 	const worker = new AnalysisWebWorker( self, new Researcher() );
+
+	/*
+	 * Uncomment the following lines to use the specific assessors for product pages:
+	 * (Later we should implement a toggle for this.)
+	 */
+	// worker.setCustomSEOAssessorClass( productSEOAssessor );
+	// worker.setCustomCornerstoneSEOAssessorClass( productCornerstoneSEOAssessor );
+	// worker.setCustomContentAssessorClass( productContentAssessor );
+	// worker.setCustomCornerstoneContentAssessorClass( productCornerstoneContentAssessor );
+	// worker.setCustomRelatedKeywordAssessorClass( productRelatedKeywordAssessor );
+	// worker.setCustomCornerstoneRelatedKeywordAssessorClass( productCornerstoneRelatedKeywordAssessor );
+
 	worker.register();
 };
