c6295728ba7b040b78e7005931e436be44869da4
FAMarfuaty <aida@yoast.com>
2021-07-21 16:29:17 +0300

    Adjust Norwegian and Slovak papers

--- packages/yoastseo/spec/fullTextTests/testTexts/sk/slovakPaper2.js
+++ packages/yoastseo/spec/fullTextTests/testTexts/sk/slovakPaper2.js
@@ -58,13 +58,6 @@ const expectedResults = {
 		resultText: "<a href='https://yoa.st/34l' target='_blank'>Link keyphrase</a>: You're linking to another page with " +
 			"the words you want this page to rank for. <a href='https://yoa.st/34m' target='_blank'>Don't do that</a>!",
 	},
-	textImages: {
-		isApplicable: true,
-		score: 6,
-		resultText: "<a href='https://yoa.st/33c' target='_blank'>Image alt attributes</a>: Images on this page do not have" +
-			" alt attributes that reflect the topic of your text. <a href='https://yoa.st/33d' target='_blank'>Add your " +
-			"keyphrase or synonyms to the alt tags of relevant images</a>!",
-	},
 	textLength: {
 		isApplicable: true,
 		score: 9,
@@ -155,6 +148,17 @@ const expectedResults = {
 		score: 3,
 		resultText: "<a href='https://yoa.st/35f' target='_blank'>Consecutive sentences</a>: The text contains 3 instances where 3 or " +
 			"more consecutive sentences start with the same word. <a href='https://yoa.st/35g' target='_blank'>Try to mix things up</a>!",
+	},
+	imageKeyphrase: {
+		isApplicable: true,
+		score: 6,
+		resultText: "<a href='https://yoa.st/33c' target='_blank'>Image Keyphrase</a>: Images on this page do not have alt attributes " +
+			"with at least half of the words from your keyphrase.<a href='https://yoa.st/33d' target='_blank'>Fix that!</a>!",
+	},
+	imageCount: {
+		isApplicable: true,
+		score: 9,
+		resultText: "<a href='https://yoa.st/33c' target='_blank'>Images</a>: Good job!",
 	},
 };
 
