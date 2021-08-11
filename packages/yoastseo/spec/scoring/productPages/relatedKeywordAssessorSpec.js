eff7eb89430e5ec9a917e12a28b067146d1d3bd4
Agnieszka Szuba <agnieszkaszuba@agnieszkas-mbp.kpn>
2021-08-11 11:37:15 +0200

    Adjust specs for the related keyword product assessor

--- packages/yoastseo/spec/scoring/productPages/relatedKeywordAssessorSpec.js
+++ packages/yoastseo/spec/scoring/productPages/relatedKeywordAssessorSpec.js
@@ -4,7 +4,22 @@ import Paper from "../../../src/values/Paper";
 import factory from "../../specHelpers/factory";
 import getResults from "../../specHelpers/getListOfAssessmentResults";
 const i18n = factory.buildJed();
-const assessor = new Assessor( i18n, new EnglishResearcher() );
+const assessor = new Assessor( i18n, new EnglishResearcher(), {
+	introductionKeyphraseUrlTitle: "https://yoast.com/1",
+	introductionKeyphraseCTAUrl: "https://yoast.com/2",
+	keyphraseLengthUrlTitle: "https://yoast.com/3",
+	keyphraseLengthCTAUrl: "https://yoast.com/4",
+	keyphraseDensityUrlTitle: "https://yoast.com/5",
+	keyphraseDensityCTAUrl: "https://yoast.com/6",
+	metaDescriptionKeyphraseUrlTitle: "https://yoast.com/7",
+	metaDescriptionKeyphraseCTAUrl: "https://yoast.com/8",
+	textCompetingLinksUrlTitle: "https://yoast.com/9",
+	textCompetingLinksCTAUrl: "https://yoast.com/10",
+	functionWordsInKeyphraseUrlTitle: "https://yoast.com/11",
+	functionWordsInKeyphraseCTAUrl: "https://yoast.com/12",
+	imageKeyphraseUrlTitle: "https://yoast.com/13",
+	imageKeyphraseCTAUrl: "https://yoast.com/14",
+} );
 
 describe( "running assessments in the related keyword product assessor", function() {
 	it( "runs assessments without any specific requirements", function() {
@@ -81,8 +96,8 @@ describe( "has configuration overrides", () => {
 
 		expect( assessment ).toBeDefined();
 		expect( assessment._config ).toBeDefined();
-		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoa.st/shopify8' target='_blank'>" );
-		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoa.st/shopify9' target='_blank'>" );
+		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoast.com/1' target='_blank'>" );
+		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoast.com/2' target='_blank'>" );
 	} );
 
 	test( "KeyphraseLengthAssessment", () => {
@@ -90,8 +105,8 @@ describe( "has configuration overrides", () => {
 
 		expect( assessment ).toBeDefined();
 		expect( assessment._config ).toBeDefined();
-		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoa.st/shopify10' target='_blank'>" );
-		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoa.st/shopify11' target='_blank'>" );
+		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoast.com/3' target='_blank'>" );
+		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoast.com/4' target='_blank'>" );
 	} );
 
 	test( "MetaDescriptionKeywordAssessment", () => {
@@ -99,8 +114,8 @@ describe( "has configuration overrides", () => {
 
 		expect( assessment ).toBeDefined();
 		expect( assessment._config ).toBeDefined();
-		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoa.st/shopify14' target='_blank'>" );
-		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoa.st/shopify15' target='_blank'>" );
+		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoast.com/7' target='_blank'>" );
+		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoast.com/8' target='_blank'>" );
 	} );
 
 	test( "KeywordDensityAssessment", () => {
@@ -108,8 +123,8 @@ describe( "has configuration overrides", () => {
 
 		expect( assessment ).toBeDefined();
 		expect( assessment._config ).toBeDefined();
-		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoa.st/shopify12' target='_blank'>" );
-		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoa.st/shopify13' target='_blank'>" );
+		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoast.com/5' target='_blank'>" );
+		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoast.com/6' target='_blank'>" );
 	} );
 
 	test( "FunctionWordsInKeyphrase", () => {
@@ -117,8 +132,8 @@ describe( "has configuration overrides", () => {
 
 		expect( assessment ).toBeDefined();
 		expect( assessment._config ).toBeDefined();
-		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoa.st/shopify50' target='_blank'>" );
-		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoa.st/shopify51' target='_blank'>" );
+		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoast.com/11' target='_blank'>" );
+		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoast.com/12' target='_blank'>" );
 	} );
 
 	test( "TextCompetingLinks", () => {
@@ -126,8 +141,8 @@ describe( "has configuration overrides", () => {
 
 		expect( assessment ).toBeDefined();
 		expect( assessment._config ).toBeDefined();
-		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoa.st/shopify18' target='_blank'>" );
-		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoa.st/shopify19' target='_blank'>" );
+		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoast.com/9' target='_blank'>" );
+		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoast.com/10' target='_blank'>" );
 	} );
 
 	test( "ImageKeyphrase", () => {
@@ -135,7 +150,7 @@ describe( "has configuration overrides", () => {
 
 		expect( assessment ).toBeDefined();
 		expect( assessment._config ).toBeDefined();
-		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoa.st/shopify22' target='_blank'>" );
-		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoa.st/shopify23' target='_blank'>" );
+		expect( assessment._config.urlTitle ).toBe( "<a href='https://yoast.com/13' target='_blank'>" );
+		expect( assessment._config.urlCallToAction ).toBe( "<a href='https://yoast.com/14' target='_blank'>" );
 	} );
 } );
