ca1ff6aad4ed836bbe2546dc6f37153be7f6b4a7
FAMarfuaty <aida@yoast.com>
2021-08-16 13:22:11 +0200

    Add feature flag before returning the Farsi stemmer

--- packages/yoastseo/src/languageProcessing/languages/fa/helpers/getStemmer.js
+++ packages/yoastseo/src/languageProcessing/languages/fa/helpers/getStemmer.js
@@ -1,11 +1,25 @@
+import { isFeatureEnabled } from "@yoast/feature-flag";
+import { get } from "lodash-es";
 import { languageProcessing } from "yoastseo";
+import determineStem from "../../fa/helpers/internal/stem";
 const { baseStemmer } = languageProcessing;
 
 /**
  * Returns the stemmer for a researcher.
  *
+ * @param {Researcher} researcher The researcher.
+ *
  * @returns {Function} The stemmer.
  */
-export default function getStemmer() {
+export default function getStemmer( researcher ) {
+	if ( isFeatureEnabled( "FARSI_SUPPORT" ) ) {
+		const morphologyData = get( researcher.getData( "morphology" ), "fa", false );
+
+		if ( morphologyData ) {
+			return word => determineStem( word, morphologyData );
+		}
+		return baseStemmer;
+	}
+
 	return baseStemmer;
 }
