fa9ca10ef95e5d46c1a2b6dd9e7e80664d7f3d41
Alexander Botteram <a.botteram@gmail.com>
2021-08-26 09:26:27 +0200

    Run first cron job one houw from planning it

--- src/integrations/cleanup-integration.php
+++ src/integrations/cleanup-integration.php
@@ -130,7 +130,7 @@ class Cleanup_Integration implements Integration_Interface {
 	private function start_cron_job( $task_name ) {
 		\update_option( self::CURRENT_TASK_OPTION, $task_name );
 		\wp_schedule_event(
-			time(),
+			time() + HOUR_IN_SECONDS,
 			'hourly',
 			self::CRON_HOOK
 		);
