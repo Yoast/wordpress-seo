07a6f1ad1a4aac6fe871d8fa758d42c331d758a9
Alexander Botteram <a.botteram@gmail.com>
2021-09-01 08:43:46 +0200

    Run start cleanup 1 hour after upgrade routine

--- inc/class-upgrade.php
+++ inc/class-upgrade.php
@@ -838,7 +838,7 @@ class WPSEO_Upgrade {
 	private function upgrade_172() {
 		\wp_unschedule_hook( 'wpseo_cleanup_orphaned_indexables' );
 
-		\wp_schedule_single_event( time(), \Yoast\WP\SEO\Integrations\Cleanup_Integration::START_HOOK );
+		\wp_schedule_single_event( time() + HOUR_IN_SECONDS, \Yoast\WP\SEO\Integrations\Cleanup_Integration::START_HOOK );
 	}
 
 	/**
