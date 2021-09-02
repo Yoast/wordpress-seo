24ed66c34ce395fafe0e3485caf668c79fd1d460
Agnieszka Szuba <agnieszka@yoast.com>
2021-09-02 14:10:32 +0200

    Change version number in deprecation doc

--- src/conditionals/farsi-support-conditional.php
+++ src/conditionals/farsi-support-conditional.php
@@ -5,7 +5,7 @@ namespace Yoast\WP\SEO\Conditionals;
 /**
  * Checks if the YOAST_SEO_FARSI_SUPPORT constant is set.
  *
- * @deprecated 17.2
+ * @deprecated 17.1
  * @codeCoverageIgnore
  */
 class Farsi_Support_Conditional extends Feature_Flag_Conditional {
@@ -14,7 +14,7 @@ class Farsi_Support_Conditional extends Feature_Flag_Conditional {
 	 * Returns the name of the feature flag.
 	 * 'YOAST_SEO_' is automatically prepended to it and it will be uppercased.
 	 *
-	 * @deprecated 17.2
+	 * @deprecated 17.1
 	 * @codeCoverageIgnore
 	 *
 	 * @return string the name of the feature flag.
