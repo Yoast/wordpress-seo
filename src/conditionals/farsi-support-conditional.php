1636b44cb68d5dc8ef84a316f86fac02505e1973
Agnieszka Szuba <agnieszka@yoast.com>
2021-09-02 12:45:16 +0200

    Properly deprecate Farsi feature flag conditional

--- src/conditionals/farsi-support-conditional.php
+++ src/conditionals/farsi-support-conditional.php
@@ -14,9 +14,13 @@ class Farsi_Support_Conditional extends Feature_Flag_Conditional {
 	 * Returns the name of the feature flag.
 	 * 'YOAST_SEO_' is automatically prepended to it and it will be uppercased.
 	 *
+	 * @deprecated 17.2
+	 * @codeCoverageIgnore
+	 *
 	 * @return string the name of the feature flag.
 	 */
 	public function get_feature_flag() {
+		_deprecated_function( __METHOD__, 'WPSEO 17.2' );
 		return 'FARSI_SUPPORT';
 	}
 }
