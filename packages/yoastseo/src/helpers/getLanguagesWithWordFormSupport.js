ed7b56e3346263ebf19e00ddca55e65d9e4a8893
Hanna Worku <hanna@yoast.com>
2021-09-07 13:53:00 +0200

    add feature flag for Japanese support

--- packages/yoastseo/src/helpers/getLanguagesWithWordFormSupport.js
+++ packages/yoastseo/src/helpers/getLanguagesWithWordFormSupport.js
@@ -1,4 +1,4 @@
-
+import { isFeatureEnabled } from "@yoast/feature-flag";
 /**
  * Checks which languages have morphology support inside YoastSEO.js.
  *
@@ -6,5 +6,10 @@
  */
 export function getLanguagesWithWordFormSupport() {
 	const supportedLanguages = [ "en", "de", "es", "fr", "it", "nl", "ru", "id", "pt", "pl", "ar", "sv", "he", "hu", "nb", "tr", "cs", "sk" ];
+
+	// Add Slovak to the supported languages list if the feature is enabled.
+	if ( isFeatureEnabled( "JAPANESE_SUPPORT" ) ) {
+		supportedLanguages.push( "ja" );
+	}
 	return supportedLanguages;
 }
