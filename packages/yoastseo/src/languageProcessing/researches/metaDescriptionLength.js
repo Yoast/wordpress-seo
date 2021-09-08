6f1d413248b2ce1ee5ba1f1f57e6ff4da33d6bfc
FAMarfuaty <aida@yoast.com>
2021-09-08 15:34:26 +0200

    Include date when calculating the length of meta description

--- packages/yoastseo/src/languageProcessing/researches/metaDescriptionLength.js
+++ packages/yoastseo/src/languageProcessing/researches/metaDescriptionLength.js
@@ -1,8 +1,18 @@
 /**
  * Check the length of the description.
+ *
  * @param {Paper} paper The paper object containing the description.
+ *
  * @returns {number} The length of the description.
  */
 export default function( paper ) {
-	return paper.getDescription().length;
+	let descriptionLength = paper.getDescription().length;
+
+	/* If the meta description is preceded by a date, two spaces and a hyphen (" - ") are added as well. Therefore,
+	three needs to be added to the total length. */
+	if ( paper.hasDate() && descriptionLength > 0 ) {
+		descriptionLength += paper.getDate().length + 3;
+	}
+
+	return descriptionLength;
 }
