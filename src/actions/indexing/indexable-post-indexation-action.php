f57ab86b2d97d49daa8fa73c95cc555c345eb939
Alexander Botteram <a.botteram@gmail.com>
2021-09-16 09:46:47 +0200

    Exclude auto-draft indexables from indexation action

--- src/actions/indexing/indexable-post-indexation-action.php
+++ src/actions/indexing/indexable-post-indexation-action.php
@@ -4,6 +4,7 @@ namespace Yoast\WP\SEO\Actions\Indexing;
 
 use wpdb;
 use Yoast\WP\Lib\Model;
+use Yoast\WP\SEO\Helpers\Post_Helper;
 use Yoast\WP\SEO\Values\Indexables\Indexable_Builder_Versions;
 use Yoast\WP\SEO\Helpers\Post_Type_Helper;
 use Yoast\WP\SEO\Models\Indexable;
@@ -38,6 +39,13 @@ class Indexable_Post_Indexation_Action extends Abstract_Indexing_Action {
 	protected $post_type_helper;
 
 	/**
+	 * The post helper.
+	 *
+	 * @var Post_Helper
+	 */
+	protected $post_helper;
+
+	/**
 	 * The indexable repository.
 	 *
 	 * @var Indexable_Repository
@@ -70,12 +78,14 @@ class Indexable_Post_Indexation_Action extends Abstract_Indexing_Action {
 		Post_Type_Helper $post_type_helper,
 		Indexable_Repository $repository,
 		wpdb $wpdb,
-		Indexable_Builder_Versions $builder_versions
+		Indexable_Builder_Versions $builder_versions,
+		Post_Helper $post_helper
 	) {
 		$this->post_type_helper = $post_type_helper;
 		$this->repository       = $repository;
 		$this->wpdb             = $wpdb;
 		$this->version          = $builder_versions->get_latest_version_for_type( 'post' );
+		$this->post_helper      = $post_helper;
 	}
 
 	/**
@@ -130,9 +140,14 @@ class Indexable_Post_Indexation_Action extends Abstract_Indexing_Action {
 	protected function get_count_query() {
 		$indexable_table = Model::get_table_name( 'Indexable' );
 
-		$replacements    = $this->get_post_types();
-		$post_type_count = \count( $replacements );
-		$replacements[]  = $this->version;
+		$post_types              = $this->get_post_types();
+		$excluded_post_statusses = $this->post_helper->get_excluded_post_statuses();
+		$replacements            = array_merge(
+			$post_types,
+			$excluded_post_statusses
+		);
+	
+		$replacements[]       = $this->version;
 
 		// Warning: If this query is changed, makes sure to update the query in get_select_query as well.
 		// @phpcs:ignore WordPress.DB.PreparedSQLPlaceholders.ReplacementsWrongNumber
@@ -140,7 +155,8 @@ class Indexable_Post_Indexation_Action extends Abstract_Indexing_Action {
 			"
 			SELECT COUNT(P.ID)
 			FROM {$this->wpdb->posts} AS P
-			WHERE P.post_type IN (" . \implode( ', ', \array_fill( 0, $post_type_count, '%s' ) ) . ")
+			WHERE P.post_type IN (" . \implode( ', ', \array_fill( 0, \count( $post_types ), '%s' ) ) . ")
+			AND I.post_status NOT IN (" . \implode( ', ', \array_fill( 0, \count( $excluded_post_statusses ), '%s' ) ) . ")
 			AND P.ID not in (
 				SELECT I.object_id from $indexable_table as I
 				WHERE I.object_type = 'post'
@@ -158,9 +174,15 @@ class Indexable_Post_Indexation_Action extends Abstract_Indexing_Action {
 	 */
 	protected function get_select_query( $limit = false ) {
 		$indexable_table = Model::get_table_name( 'Indexable' );
-		$post_types      = $this->get_post_types();
-		$replacements    = $post_types;
-		$replacements[]  = $this->version;
+
+		$post_types              = $this->get_post_types();
+		$excluded_post_statusses = $this->post_helper->get_excluded_post_statuses();
+		$replacements            = array_merge(
+			$post_types,
+			$excluded_post_statusses
+		);
+	
+		$replacements[]       = $this->version;
 
 		$limit_query = '';
 		if ( $limit ) {
@@ -175,6 +197,7 @@ class Indexable_Post_Indexation_Action extends Abstract_Indexing_Action {
 			SELECT P.ID
 			FROM {$this->wpdb->posts} AS P
 			WHERE P.post_type IN (" . \implode( ', ', \array_fill( 0, \count( $post_types ), '%s' ) ) . ")
+			AND P.post_status NOT IN (" . \implode( ', ', \array_fill( 0, \count( $excluded_post_statusses ), '%s' ) ) . ")
 			AND P.ID not in (
 				SELECT I.object_id from $indexable_table as I
 				WHERE I.object_type = 'post'
