83b75a05deb42b10c2912a5c80120c6ec7552cdb
Leonidas Milosis <leonidas.milossis@gmail.com>
2021-09-22 17:03:43 +0300

    Improve comment with new regex

--- admin/import/plugins/class-import-aioseo-v4.php
+++ admin/import/plugins/class-import-aioseo-v4.php
@@ -151,7 +151,7 @@ class WPSEO_Import_AIOSEO_V4 extends WPSEO_Plugin_Importer {
 
 			/*
 			 * `$matches[1]` contain the captured matches of the
-			 * first capturing group (the `(\w+)` in the regex above).
+			 * first capturing group (the `([\w-]+)` in the regex above).
 			 */
 			$custom_fields_or_taxonomies = $matches[1];
 
