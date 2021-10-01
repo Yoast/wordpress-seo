da55821401b04f6257550d3bae0cc00f0ff8a9c6
FAMarfuaty <aida@yoast.com>
2021-10-01 13:32:31 +0200

    Add comments and improve code

--- packages/yoastseo/src/languageProcessing/languages/el/helpers/internal/getParticiples.js
+++ packages/yoastseo/src/languageProcessing/languages/el/helpers/internal/getParticiples.js
@@ -1,29 +1,33 @@
 import { languageProcessing } from "yoastseo";
 const { getWords } = languageProcessing;
 
+const passiveParticipleEndings = "(ούμενους|ημένους|ούμενος|ούμενου|ούμενον|ούμενης|ούμενοι|ούμενων|ούμενες|μένους|" +
+	"ημένος|ημένου|ημένον|ημένοι|ημένων|ημένης|ημένες|ούμενη|ούμενο|ούμενα|μένος|μένου|μένον|μένοι|μένης|μένες|" +
+	"μένων|ημένη|ημένο|ημένα|μένη|μένο|μένα)$";
+const passiveInfinitiveEndings = "(ηθεί|φθεί|θει|τει)$";
+
 /**
  * Creates an array of participles found in a clause.
  *
  * @param {string} clauseText   The clause to finds participles in.
  *
- * @returns {Object|Array} The array with the participles.
+ * @returns {Array} The array with the participles object.
  */
 export default function( clauseText ) {
 	const words = getWords( clauseText );
-	const participleEndings = "(ούμενους|ημένους|ούμενος|ούμενου|ούμενον|ούμενης|ούμενοι|ούμενων|ούμενες|μένους|" +
-		"ημένος|ημένου|ημένον|ημένοι|ημένων|ημένης|ημένες|ούμενη|ούμενο|ούμενα|μένος|μένου|μένον|μένοι|μένης|μένες|" +
-		"μένων|ημένη|ημένο|ημένα|μένη|μένο|μένα)$";
-	const participleEndingsRegex = new RegExp( participleEndings );
-	const infinitiveEndingsRegex = new RegExp( "(ηθεί|φθεί|θει|τει)$" );
-	const participle = words.filter( word => participleEndingsRegex.test( word ) );
-	const infinitive = words.filter( word => infinitiveEndingsRegex.test( word ) );
-	const foundParticiple = [];
 
-	if ( participle.length > 0 ) {
-		foundParticiple.push( { participle: participle, type: "participle" } );
-	} else if ( infinitive.length > 0 ) {
-		foundParticiple.push( { participle: infinitive, type: "infinitive" } );
+	// Find the passive participles
+	const getParticiples = words.filter( word => new RegExp( passiveParticipleEndings ).test( word ) );
+	// Find the passive infinitives
+	const getInfinitives = words.filter( word => new RegExp( passiveInfinitiveEndings ).test( word ) );
+
+	const foundPassives = [];
+
+	if ( getParticiples.length > 0 ) {
+		foundPassives.push( { participle: getParticiples, type: "participle" } );
+	} else if ( getInfinitives.length > 0 ) {
+		foundPassives.push( { participle: getInfinitives, type: "infinitive" } );
 	}
 
-	return foundParticiple;
+	return foundPassives;
 }
--- packages/yoastseo/src/languageProcessing/languages/el/helpers/isPassiveSentence.js
+++ packages/yoastseo/src/languageProcessing/languages/el/helpers/isPassiveSentence.js
@@ -4,24 +4,23 @@ const { getWords } = languageProcessing;
 import nonPassiveVerbStems from "../config/internal/nonPassiveVerbStems";
 import { passiveSuffixes } from "../config/internal/mophologicalPassiveSuffixes.js";
 
-
-
 /**
  * Checks the passed sentence to see if it contains Greek passive verb-forms.
  *
  * @param {string} sentence     The sentence to match against.
  *
- * @returns {Boolean}           Whether the sentence contains Greek passive voice.
+ * @returns {Boolean}           Whether the sentence is passive or not.
  */
 export default function isPassiveSentence( sentence ) {
 	const words = getWords( sentence );
-	// Loop over the words array.
+
 	for ( const word of words ) {
-		// For each word we need to get the stem, if the word ends in one of the suffixes.
 		for ( const suffix of passiveSuffixes ) {
-			// If the stem is on the nonPassive stem list, the word is not a passive. Otherwise, it is.
 			if ( word.endsWith( suffix ) && word.length > 4 ) {
+				// Get the stem of the word.
 				const stem = word.slice( 0, -suffix.length );
+				// Return true if the word ends with one of the passive suffixes, if the word is more than 4 characters long
+				// And if the word stem is not in the non-passive exception list.
 				return ! nonPassiveVerbStems.includes( stem );
 			}
 		}
