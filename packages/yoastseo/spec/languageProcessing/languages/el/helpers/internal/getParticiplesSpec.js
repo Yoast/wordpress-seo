b92553bbc3964f87b16de18c531f41e7f8fd45d9
FAMarfuaty <aida@yoast.com>
2021-10-01 14:29:17 +0200

    add specs for getParticiples

--- packages/yoastseo/spec/languageProcessing/languages/el/helpers/internal/getParticiplesSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/el/helpers/internal/getParticiplesSpec.js
@@ -1,17 +1,25 @@
 import getParticiples from "../../../../../../src/languageProcessing/languages/el/helpers/internal/getParticiples";
 
 describe( "Test for matching Greek participles", function() {
-	it( "returns matched participles.", function() {
-		const sentence = "η γάτα εγκαταλείφμένος.";
+	it( "returns matched passive participles.", function() {
+		// Passive participle: αγαπημένη.
+		const sentence = "Η γάτα είναι αγαπημένη.";
 		const foundParticiples = getParticiples( sentence );
-		expect( foundParticiples.participle ).toEqual( [ "εγκαταλείφμένος" ] );
-		expect( foundParticiples.type ).toEqual( "participle" );
-
+		expect( foundParticiples[ 0 ].participle ).toEqual( [ "αγαπημένη" ] );
+		expect( foundParticiples[ 0 ].type ).toEqual( "participle" );
 	} );
 
-	it( "returns no matched participles.", function() {
+	it( "returns matched passive infinitive.", function() {
+		// Passive infinitive: αγαπηθεί.
+		const sentence = "Ο σκύλος έχει αγαπηθεί.";
+		const foundParticiples = getParticiples( sentence );
+		expect( foundParticiples[ 0 ].participle ).toEqual( [ "αγαπηθεί" ] );
+		expect( foundParticiples[ 0 ].type ).toEqual( "infinitive" );
+	} );
+
+	it( "returns an empty array when no participle/passive is matched.", function() {
 		const sentence = "To καινουργιο αυτοκίνητο μου τρέχει με ενενήντα χιλιόμετρα την ώρα.";
 		const foundParticiples = getParticiples( sentence );
-		expect( foundParticiples ).toEqual( null );
+		expect( foundParticiples ).toEqual( [] );
 	} );
 } );
