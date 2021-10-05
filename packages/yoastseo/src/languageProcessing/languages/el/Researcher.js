d0c3f32d191234497b7321b9fb02f81b3031cac4
FAMarfuaty <aida@yoast.com>
2021-10-05 16:01:28 +0200

    Add function word list and add spec

--- packages/yoastseo/src/languageProcessing/languages/el/Researcher.js
+++ packages/yoastseo/src/languageProcessing/languages/el/Researcher.js
@@ -5,6 +5,7 @@ const { AbstractResearcher } = languageProcessing;
 import firstWordExceptions from "./config/firstWordExceptions";
 import transitionWords from "./config/transitionWords";
 import twoPartTransitionWords from "./config/twoPartTransitionWords";
+import functionWords from "./config/functionWords";
 
 // All helpers
 import getStemmer from "./helpers/getStemmer";
@@ -24,11 +25,10 @@ export default class Researcher extends AbstractResearcher {
 		// Deletes researches that are not available for languages that we haven't supported yet.
 		delete this.defaultResearches.getFleschReadingScore;
 		delete this.defaultResearches.getPassiveVoiceResult;
-		delete this.defaultResearches.functionWordsInKeyphrase;
 
 		Object.assign( this.config, {
 			language: "el",
-			functionWords: [],
+			functionWords,
 			transitionWords,
 			twoPartTransitionWords,
 			firstWordExceptions: firstWordExceptions.firstWords,
