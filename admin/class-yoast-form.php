e0d07e411c1d46bf5a852c8a74ac553c52512438
Enrico Battocchi <enrico.battocchi@gmail.com>
2021-10-12 15:59:06 +0200

    Address CR feedback

--- admin/class-yoast-form.php
+++ admin/class-yoast-form.php
@@ -522,12 +522,13 @@ class Yoast_Form {
 	 *
 	 * @param string $var The variable within the option to create the hidden input for.
 	 * @param string $id  The ID of the element.
-	 * @param mixed  $val The value to set in the input field. Optional: the value from the options will be used otherwise.
+	 * @param mixed  $val Optional. The value to set in the input field. Otherwise the value from the options will be used.
 	 */
 	public function hidden( $var, $id = '', $val = null ) {
 		if ( is_null( $val ) ) {
 			$val = $this->get_field_value( $var, '' );
 		}
+
 		if ( is_bool( $val ) ) {
 			$val = ( $val === true ) ? 'true' : 'false';
 		}
