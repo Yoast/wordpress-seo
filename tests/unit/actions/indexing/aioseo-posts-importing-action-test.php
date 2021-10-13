cce03504d51060cde76db77a9725a649501f2dea
Leonidas Milosis <leonidas.milossis@gmail.com>
2021-10-13 17:08:21 +0300

    Move unit tests from P2-1269

---
+++ tests/unit/actions/indexing/aioseo-posts-importing-action-test.php
@@ -0,0 +1,206 @@
+<?php
+
+namespace Yoast\WP\SEO\Tests\Unit\Actions\Indexing;
+
+use Mockery;
+use Yoast\WP\Lib\ORM;
+use Yoast\WP\SEO\Actions\Indexing\Aioseo_Posts_Importing_Action;
+use Yoast\WP\SEO\Helpers\Meta_Helper;
+use Yoast\WP\SEO\Helpers\Options_Helper;
+use Yoast\WP\SEO\Repositories\Indexable_Repository;
+use Yoast\WP\SEO\Tests\Unit\Doubles\Models\Indexable_Mock;
+use Yoast\WP\SEO\Tests\Unit\TestCase;
+
+/**
+ * Aioseo_Posts_Importing_Action_Test class
+ *
+ * @group actions
+ * @group indexing
+ *
+ * @coversDefaultClass \Yoast\WP\SEO\Actions\Indexing\Aioseo_Posts_Importing_Action
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded
+ */
+class Aioseo_Posts_Importing_Action_Test extends TestCase {
+
+	/**
+	 * Represents the instance to test.
+	 *
+	 * @var Aioseo_Posts_Importing_Action
+	 */
+	protected $instance;
+
+	/**
+	 * Represents the indexable repository.
+	 *
+	 * @var Mockery\MockInterface|Indexable_Repository
+	 */
+	protected $indexable_repository;
+
+	/**
+	 * The mocked WordPress database object.
+	 *
+	 * @var Mockery\MockInterface|\wpdb
+	 */
+	protected $wpdb;
+
+	/**
+	 * The mocked meta helper.
+	 *
+	 * @var Mockery\MockInterface|Meta_Helper
+	 */
+	protected $meta;
+
+	/**
+	 * The mocked options helper.
+	 *
+	 * @var Mockery\MockInterface|Options_Helper
+	 */
+	protected $options;
+
+	/**
+	 * Sets up the test class.
+	 */
+	protected function set_up() {
+		parent::set_up();
+
+		$this->indexable_repository = Mockery::mock( Indexable_Repository::class );
+		$this->wpdb                 = Mockery::mock( 'wpdb' );
+		$this->meta                 = Mockery::mock( Meta_Helper::class );
+		$this->options              = Mockery::mock( Options_Helper::class );
+		$this->instance             = new Aioseo_Posts_Importing_Action( $this->indexable_repository, $this->wpdb, $this->meta, $this->options );
+	}
+
+	/**
+	 * Tests the mapping of indexable data to postmeta.
+	 *
+	 * @covers ::map_to_postmeta
+	 */
+	public function test_map_postmeta_with_full_yoast_indexable() {
+		$indexable      = Mockery::mock( Indexable_Mock::class );
+		$indexable->orm = Mockery::mock( ORM::class );
+
+		$indexable->title                  = 'title1';
+		$indexable->description            = 'description1';
+		$indexable->open_graph_title       = 'open_graph_title1';
+		$indexable->open_graph_description = 'open_graph_description1';
+		$indexable->twitter_title          = 'twitter_title1';
+		$indexable->twitter_description    = 'twitter_description1';
+		$indexable->object_id              = 123;
+
+		$this->meta->expects( 'set_value' )
+			->with( 'title', 'title1', 123 )
+			->andReturn( true );
+		$this->meta->expects( 'set_value' )
+			->with( 'metadesc', 'description1', 123 )
+			->andReturn( true );
+		$this->meta->expects( 'set_value' )
+			->with( 'opengraph-title', 'open_graph_title1', 123 )
+			->andReturn( true );
+		$this->meta->expects( 'set_value' )
+			->with( 'opengraph-description', 'open_graph_description1', 123 )
+			->andReturn( true );
+		$this->meta->expects( 'set_value' )
+			->with( 'twitter-title', 'twitter_title1', 123 )
+			->andReturn( true );
+		$this->meta->expects( 'set_value' )
+			->with( 'twitter-description', 'twitter_description1', 123 )
+			->andReturn( true );
+
+
+		$this->instance->map_to_postmeta( $indexable );
+	}
+
+	/**
+	 * Tests the mapping of indexable data to postmeta, when the indexable is empty.
+	 *
+	 * @covers ::map_to_postmeta
+	 */
+	public function test_map_postmeta_with_empty_yoast_indexable() {
+		$indexable      = Mockery::mock( Indexable_Mock::class );
+		$indexable->orm = Mockery::mock( ORM::class );
+
+		$indexable->object_id = 123;
+
+		$this->meta->expects( 'set_value' )
+			->never();
+
+		$this->instance->map_to_postmeta( $indexable );
+	}
+
+	/**
+	 * Tests the mapping of indexable data when we have an empty Yoast indexable.
+	 *
+	 * @covers ::map
+	 */
+	public function test_map_with_empty_yoast_indexable() {
+		$indexable      = Mockery::mock( Indexable_Mock::class );
+		$indexable->orm = Mockery::mock( ORM::class );
+
+		$aioseio_indexable = [
+			'title'                => 'title1',
+			'description'          => 'description1',
+			'og_title'             => 'og_title1',
+			'og_description'       => 'og_description1',
+			'twitter_title'        => 'twitter_title1',
+			'twitter_description'  => 'twitter_description1',
+		];
+
+		$indexable = $this->instance->map( $indexable, $aioseio_indexable );
+
+		$this->assertEquals( 'title1', $indexable->title );
+		$this->assertEquals( 'description1', $indexable->description );
+		$this->assertEquals( 'og_title1', $indexable->open_graph_title );
+		$this->assertEquals( 'og_description1', $indexable->open_graph_description );
+		$this->assertEquals( 'twitter_title1', $indexable->twitter_title );
+		$this->assertEquals( 'twitter_description1', $indexable->twitter_description );
+	}
+
+	/**
+	 * Tests the mapping of indexable data when we have existing data in the Yoast indexable.
+	 *
+	 * @covers ::map
+	 */
+	public function test_map_with_existing_yoast_indexable() {
+		$indexable      = Mockery::mock( Indexable_Mock::class );
+		$indexable->orm = Mockery::mock( ORM::class );
+
+		$indexable->title       = 'existing_title';
+		$indexable->description = 'existing_dsc';
+
+		$aioseio_indexable = [
+			'title'                => 'title1',
+			'description'          => 'description1',
+			'og_title'             => 'og_title1',
+			'og_description'       => 'og_description1',
+			'twitter_title'        => 'twitter_title1',
+			'twitter_description'  => 'twitter_description1',
+		];
+
+		$indexable = $this->instance->map( $indexable, $aioseio_indexable );
+
+		$this->assertEquals( 'existing_title', $indexable->title );
+		$this->assertEquals( 'existing_dsc', $indexable->description );
+		$this->assertEquals( 'og_title1', $indexable->open_graph_title );
+		$this->assertEquals( 'og_description1', $indexable->open_graph_description );
+		$this->assertEquals( 'twitter_title1', $indexable->twitter_title );
+		$this->assertEquals( 'twitter_description1', $indexable->twitter_description );
+	}
+
+	/**
+	 * Tests the mapping of indexable data when we have missing data from the AIOSEO indexable.
+	 *
+	 * @covers ::map
+	 */
+	public function test_map_with_missing_aioseo_data() {
+		$indexable      = Mockery::mock( Indexable_Mock::class );
+		$indexable->orm = Mockery::mock( ORM::class );
+
+		$indexable->twitter_description = null;
+
+		$aioseio_indexable = [];
+
+		$indexable = $this->instance->map( $indexable, $aioseio_indexable );
+
+		$this->assertNull( $indexable->twitter_description );
+	}
+}
