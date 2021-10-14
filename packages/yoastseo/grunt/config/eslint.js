f8d06914a450f400886deaa85b425dbb577ec534
Hans-Christiaan <hans-christiaan@yoast.com>
2021-10-14 13:10:24 +0200

    do stuff.

--- packages/yoastseo/grunt/config/eslint.js
+++ packages/yoastseo/grunt/config/eslint.js
@@ -1,6 +1,8 @@
 // See https://github.com/sindresorhus/grunt-eslint
 module.exports = function( grunt ) {
 	const fix = grunt.option( "fix" ) || false;
+	const outputFileSrc = grunt.option( "output-file-src" ) || false;
+	const outputFileTests = grunt.option( "output-file-tests" ) || false;
 
 	return {
 		target: {
@@ -8,6 +10,7 @@ module.exports = function( grunt ) {
 			options: {
 				maxWarnings: 22,
 				fix: fix,
+				outputFile: outputFileSrc,
 			},
 		},
 		tests: {
@@ -16,6 +19,7 @@ module.exports = function( grunt ) {
 				configFile: ".eslintrc-tests",
 				maxWarnings: 19,
 				fix: fix,
+				outputFile: outputFileTests,
 			},
 		},
 	};
