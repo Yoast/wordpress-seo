d75fdb98f05d5598429f8435a3e720e6e78b4dbd
Leonidas Milosis <leonidas.milossis@gmail.com>
2021-10-15 14:23:08 +0300

    Add unit tests

--- tests/unit/actions/indexing/aioseo-posts-importing-action-test.php
+++ tests/unit/actions/indexing/aioseo-posts-importing-action-test.php
@@ -8,6 +8,7 @@ use Yoast\WP\SEO\Actions\Indexing\Aioseo_Posts_Importing_Action;
 use Yoast\WP\SEO\Helpers\Meta_Helper;
 use Yoast\WP\SEO\Helpers\Options_Helper;
 use Yoast\WP\SEO\Repositories\Indexable_Repository;
+use Yoast\WP\SEO\Tests\Unit\Doubles\Actions\Aioseo_Posts_Importing_Action_Double;
 use Yoast\WP\SEO\Tests\Unit\Doubles\Models\Indexable_Mock;
 use Yoast\WP\SEO\Tests\Unit\TestCase;
 
@@ -28,6 +29,13 @@ class Aioseo_Posts_Importing_Action_Test extends TestCase {
 	 * @var Aioseo_Posts_Importing_Action
 	 */
 	protected $instance;
+
+	/**
+	 * Represents the mock instance to test.
+	 *
+	 * @var Aioseo_Posts_Importing_Action_Double
+	 */
+	protected $mock_instance;
 
 	/**
 	 * Represents the indexable repository.
@@ -68,6 +76,103 @@ class Aioseo_Posts_Importing_Action_Test extends TestCase {
 		$this->meta                 = Mockery::mock( Meta_Helper::class );
 		$this->options              = Mockery::mock( Options_Helper::class );
 		$this->instance             = new Aioseo_Posts_Importing_Action( $this->indexable_repository, $this->wpdb, $this->meta, $this->options );
+		$this->mock_instance        = Mockery::mock(
+			Aioseo_Posts_Importing_Action_Double::class,
+			[
+				$this->indexable_repository,
+				$this->wpdb,
+				$this->meta,
+				$this->options,
+			]
+		)->makePartial();
+
+		$this->wpdb->prefix = 'wp_';
+	}
+
+	/**
+	 * Tests the getting of unimported AIOSEO data.
+	 *
+	 * @covers ::get_total_unindexed
+	 */
+	public function test_get_total_unindexed() {
+		$this->mock_instance->expects( 'get_cursor' )
+			->once()
+			->andReturn( 0 );
+
+		$expected_query = 'SELECT * FROM wp_aioseo_posts WHERE id > %d ORDER BY id LIMIT %d';
+
+		$this->wpdb->expects( 'prepare' )
+			->once()
+			->with(
+				$expected_query,
+				0,
+				25
+			)
+			->andReturn(
+				'
+				SELECT *
+				FROM wp_aioseo_posts
+				WHERE id > 0
+				ORDER BY id
+				LIMIT 25'
+			);
+
+		$this->wpdb->expects( 'get_col' )
+			->once()
+			->with(
+				'
+				SELECT *
+				FROM wp_aioseo_posts
+				WHERE id > 0
+				ORDER BY id
+				LIMIT 25'
+			)
+			->andReturn( [ '1', '2', '3' ] );
+
+		$limited_unimported_rows = $this->mock_instance->get_total_unindexed();
+		$this->assertEquals( 3, $limited_unimported_rows );
+	}
+
+	/**
+	 * Tests the importing of AIOSEO data.
+	 *
+	 * @covers ::index
+	 */
+	public function test_index() {
+		// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedConstantFound
+		define( 'ARRAY_A', 'ARRAY_A' );
+
+		$this->mock_instance->expects( 'get_cursor' )
+			->once()
+			->andReturn( 0 );
+
+		$expected_query = 'SELECT * FROM wp_aioseo_posts WHERE id > %d ORDER BY id LIMIT %d';
+
+		$this->wpdb->expects( 'prepare' )
+			->once()
+			->with(
+				$expected_query,
+				0,
+				25
+			)
+			->andReturn(
+				'
+				SELECT *
+				FROM wp_aioseo_posts
+				WHERE id > 0
+				ORDER BY id
+				LIMIT 25'
+			);
+
+			$this->wpdb->expects( 'get_results' )
+				->once()
+				->andReturn( [] );
+
+			$this->mock_instance->expects( 'set_cursor' )
+				->once()
+				->with( $this->options, 0 );
+
+			$this->mock_instance->index();
 	}
 
 	/**
