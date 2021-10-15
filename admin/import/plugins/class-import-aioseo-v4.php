9b4faf990673f23b67fb5369972b34f6668ae5ba
Leonidas Milosis <leonidas.milossis@gmail.com>
2021-10-15 10:37:33 +0300

    Add the new importer behind a feature flag (from P2-1269)

--- admin/import/plugins/class-import-aioseo-v4.php
+++ admin/import/plugins/class-import-aioseo-v4.php
@@ -5,6 +5,7 @@
  * @package WPSEO\Admin\Import\Plugins
  */
 
+use Yoast\WP\SEO\Actions\Indexing\Aioseo_Posts_Importing_Action;
 /**
  * Class with functionality to import & clean All in One SEO Pack post metadata, versions 4 and up.
  */
@@ -212,4 +213,24 @@ class WPSEO_Import_AIOSEO_V4 extends WPSEO_Plugin_Importer {
 	}
 
 	// phpcs:enable WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+
+	/**
+	 * Detects whether there is AIOSEO data to import from their custom table.
+	 *
+	 * @return bool Boolean indicating whether there is something to import.
+	 */
+	protected function detect() {
+		$aioseo_posts_import_action = YoastSEO()->classes->get( Aioseo_Posts_Importing_Action::class );
+		return ( $aioseo_posts_import_action->get_total_unindexed() > 0 );
+	}
+
+	/**
+	 * Actually AIOSEO data from their custom table.
+	 *
+	 * @return void
+	 */
+	protected function import() {
+		$aioseo_posts_import_action = YoastSEO()->classes->get( Aioseo_Posts_Importing_Action::class );
+		$aioseo_posts_import_action->index();
+	}
 }
