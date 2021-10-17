71f9286b9831d787662f97f87a8f87ca9a189e19
JustinyAhin <justiny.ahinon@gmail.com>
2021-10-17 14:55:38 +0200

    Update CPT plugin link

--- packages/e2e-tests/e2e-test-env-setup.sh
+++ packages/e2e-tests/e2e-test-env-setup.sh
@@ -4,7 +4,7 @@
 cd ../..
 
 # Start the development environment.
-wp-env start
+#wp-env start
 
 # Check if the plugin is active or not.
 # If the plugin is not active, check if it is installed. If the plugin is active, do nothing. 
@@ -13,7 +13,7 @@ wp-env run tests-cli wp plugin is-active yoast-simple-custom-post-type
 if [ $? -eq 1 ]; then
 	wp-env run tests-cli wp plugin is-installed yoast-simple-custom-post-type
 	if [ $? -eq 1 ]; then
-		wp-env run tests-cli wp plugin install "https://github.com/Yoast/wordpress-seo/blob/try/e2e-tests-package/packages/e2e-tests/data/yoast-simple-custom-post-type.zip?raw=true"
+		wp-env run tests-cli wp plugin install "https://github.com/Yoast/wordpress-seo/blob/trunk/packages/e2e-tests/data/yoast-simple-custom-post-type.zip?raw=true"
 		wp-env run tests-cli wp plugin activate yoast-simple-custom-post-type
 	else
 		wp-env run tests-cli wp plugin activate yoast-simple-custom-post-type
