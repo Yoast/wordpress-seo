f1f0bf6b7fb743335a5201e445fbfdaf0b5461f2
FAMarfuaty <aida@yoast.com>
2021-10-18 17:06:55 +0200

    Add more specs

--- packages/yoastseo/spec/languageProcessing/languages/ja/helpers/internal/createWordFormsSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/ja/helpers/internal/createWordFormsSpec.js
@@ -6,14 +6,16 @@ const morphologyDataJA = getMorphologyData( "ja" ).ja;
 
 describe( "test for creating word forms for Japanese", function() {
 	it( "returns an array with the original word if the word is one character long", function() {
-		const words = createWordForms( "来", morphologyDataJA );
-
+		let words = createWordForms( "来", morphologyDataJA );
 		expect( words ).toEqual( [ "来" ] );
+
+		words = createWordForms( "バラ", morphologyDataJA );
+		expect( words ).toEqual( [ "バラ" ] );
 	} );
 	it( "returns an array with the word forms for a input word that ends in vowel group ending, e.g. -わせる", function() {
 		const words = createWordForms( "会わせる", morphologyDataJA );
 
-		expect( words ).toEqual(   [ "会う", "会い", "会わ", "会え", "会お", "会っ", "会える", "会わせ", "会わせる", "会われ", "会われる", "会おう" ]
+		expect( words ).toEqual( [ "会う", "会い", "会わ", "会え", "会お", "会っ", "会える", "会わせ", "会わせる", "会われ", "会われる", "会おう" ]
 		);
 	} );
 	xit( "returns an array with the word forms for a input word that ends in T group ending", function() {
@@ -56,15 +58,15 @@ describe( "test for creating word forms for Japanese", function() {
 
 		expect( words ).toEqual( [ "来" ] );
 	} );
-	xit( "returns an additional forms without る ending if the input word ends in -る", function() {
-		const words = createWordForms( "会わせる", morphologyDataJA );
+	it( "returns an additional forms without る ending if the input word ends in -る", function() {
+		const words = createWordForms( "見る", morphologyDataJA );
 
-		expect( words ).toEqual( [ "来" ] );
+		expect( words ).toEqual( [ "見る", "見り", "見ら", "見れ", "見ろ", "見っ", "見れる", "見らせ", "見らせる", "見られ", "見られる", "見ろう", "見" ] );
 	} );
 	it( "returns an array of word forms for an input word that ends in an ending that present in multiple ending groups, i.e. い", function() {
 		const words = createWordForms( "会い", morphologyDataJA );
 
-		expect( words ).toEqual(    [ "会う", "会い", "会わ", "会え", "会お", "会っ", "会える", "会わせ", "会わせる", "会われ", "会われる", "会おう", "会く", "会き",
+		expect( words ).toEqual( [ "会う", "会い", "会わ", "会え", "会お", "会っ", "会える", "会わせ", "会わせる", "会われ", "会われる", "会おう", "会く", "会き",
 			"会か", "会け", "会こ", "会ける", "会かせ", "会かせる", "会かれ", "会かれる", "会こう", "会かっ", "会ぐ", "会ぎ", "会が", "会げ", "会ご", "会げる", "会がせ",
 			"会がせる", "会がれ", "会がれる", "会ごう" ]
 		);
@@ -72,7 +74,7 @@ describe( "test for creating word forms for Japanese", function() {
 	it( "returns an array of word forms for an input word that ends in an ending that present in multiple ending groups, i.e. っ", function() {
 		const words = createWordForms( "会っ", morphologyDataJA );
 
-		expect( words ).toEqual(   [ "会う", "会い", "会わ", "会え", "会お", "会っ", "会える", "会わせ", "会わせる", "会われ", "会われる", "会おう", "会つ", "会ち",
+		expect( words ).toEqual( [ "会う", "会い", "会わ", "会え", "会お", "会っ", "会える", "会わせ", "会わせる", "会われ", "会われる", "会おう", "会つ", "会ち",
 			"会た", "会て", "会と", "会てる", "会たせ", "会たせる", "会たれ", "会たれる", "会とう", "会る", "会り", "会ら", "会れ", "会ろ", "会れる", "会らせ", "会らせる",
 			"会られ", "会られる", "会ろう" ]
 		);
@@ -80,7 +82,7 @@ describe( "test for creating word forms for Japanese", function() {
 	it( "returns an array of word forms for an input word that ends in an ending that present in multiple ending groups, i.e. ん", function() {
 		const words = createWordForms( "読ん", morphologyDataJA );
 
-		expect( words ).toEqual(   [ "読ぬ", "読に", "読な", "読ね", "読の", "読ん", "読ねる", "読なせ", "読なせる", "読なれ", "読なれる", "読のう", "読む", "読み",
+		expect( words ).toEqual( [ "読ぬ", "読に", "読な", "読ね", "読の", "読ん", "読ねる", "読なせ", "読なせる", "読なれ", "読なれる", "読のう", "読む", "読み",
 			"読ま", "読め", "読も", "読める", "読ませ", "読ませる", "読まれ", "読まれる", "読もう", "読ぶ", "読び", "読ば", "読べ", "読ぼ", "読べる", "読ばせ", "読ばせる",
 			"読ばれ", "読ばれる", "読ぼう" ]
 		);
