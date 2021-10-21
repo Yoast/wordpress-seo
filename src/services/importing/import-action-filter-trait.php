21827427bff96dbc6e85198b588ab4acfb7a7286
Joost Boomkamp <3604126+increddibelly@users.noreply.github.com>
2021-10-21 11:50:01 +0200

    create filter trait and import detector
    (cherry picked from commit 797a3ca6fea6051eef468960cafb2392b0cc5cd5)

---
+++ src/services/importing/import-action-filter-trait.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Yoast\WP\SEO\Services\Importing;
+
+/**
+ * Trait for filtering a set of importer actions based on plugin and type.
+ *
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded
+ */
+trait Importer_Action_Filter_Trait {
+
+	/**
+	 * Filters all import actions from a list that do not match the given Plugin or Type.
+	 *
+	 * @param $all_actions Import_Action The complete list of actions.
+	 * @param $plugin      string        The Plugin name whose actions to keep.
+	 * @param $type        string        The type of actions to keep.
+	 *
+	 * @return array
+	 */
+	public function filter_actions( $all_actions, $plugin = null, $type = null ){
+		$actions = $all_actions;
+
+		if ( $plugin ) {
+			$actions = array_filter( $actions, function( $detector, $plugin ) {
+				return $detector->plugin === $plugin;
+			} );
+		}
+
+		if ( $type ) {
+			$actions = array_filter( $actions, function( $detector, $type ) {
+				return $detector->plugin === $type;
+			} );
+		}
+
+		return $actions;
+	}
+}
