951c3b2da5f4bb0422f4d44757c77a97904da5ea
iolandasequino <iolanda@yoast.com>
2021-10-22 13:14:27 +0200

    create a new collectStems file for Japanese and relocate getWordForms

---
+++ packages/yoastseo/src/languageProcessing/languages/ja/helpers/getWordForms.js
@@ -0,0 +1,19 @@
+//get all the words from a paper
+//get all the words in a text
+//create wordforms for a keyphrase
+// find matches in the paper
+//return an object with an array of matched forms of keyphrases and synonyms
+// {
+// *                     keyphraseForms: [[ form1, form2, ... ], [ form1, form2, ... ]],
+// *                     synonymsForms: [
+// *                          [[ form1, form2, ... ], [ form1, form2, ... ]],
+// *                          [[ form1, form2, ... ], [ form1, form2, ... ]],
+// *                          [[ form1, form2, ... ], [ form1, form2, ... ]],
+// *                     ],
+// *
+//}
+
+import getWords from "./getWords";
+import getContentWords from "./getContentWords";
+import getStemmer from "./getStemmer";
+
