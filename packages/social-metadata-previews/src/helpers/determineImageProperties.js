8ed36c6571af016b4b079d4ea4f4f6a9042a59f4
Simon <simon@yoast.com>
2021-10-22 13:19:21 +0200

    Add aspect ratio to the corresponding platform

--- packages/social-metadata-previews/src/helpers/determineImageProperties.js
+++ packages/social-metadata-previews/src/helpers/determineImageProperties.js
@@ -3,6 +3,7 @@ export const TWITTER_IMAGE_SIZES = {
 	squareHeight: 125,
 	landscapeWidth: 506,
 	landscapeHeight: 265,
+	aspectRatio: ( 0.502 / 1 ) * 100,
 };
 
 export const FACEBOOK_IMAGE_SIZES = {
@@ -12,6 +13,7 @@ export const FACEBOOK_IMAGE_SIZES = {
 	landscapeHeight: 273,
 	portraitWidth: 158,
 	portraitHeight: 237,
+	aspectRatio: ( 0.522 / 1 ) * 100,
 };
 
 /**
