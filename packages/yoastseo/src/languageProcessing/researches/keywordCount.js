90a26696a43090a1b104c4c33c8f7d50a610279c
FAMarfuaty <aida@yoast.com>
2021-10-25 17:09:55 +0200

    Adapt keywordCount for Japanese

--- packages/yoastseo/src/languageProcessing/researches/keywordCount.js
+++ packages/yoastseo/src/languageProcessing/researches/keywordCount.js
@@ -7,14 +7,21 @@ import getSentences from "../helpers/sentence/getSentences";
 import { markWordsInSentences } from "../helpers/word/markWordsInSentences";
 
 /**
- * Calculates the keyword count, takes morphology into account.
+ * Calculates the keyword count, takes morphology into account. Also calculates the total characters of all the keyword forms
+ * that are found in the text if the custom helper for it is available in the researcher.
  *
- * @param {object} paper The paper containing keyword and text.
- * @param {object} researcher The researcher
- * @returns {number} The keyword count.
+ * @param {object} paper        The paper containing keyword and text.
+ * @param {object} researcher   The researcher.
+ *
+ * @returns {object} An object containing the count for the keyword occurrences, an array of all the matches,
+ * markings and the keyphrase length. if the custom helper for it is available in the researcher.
  */
 export default function( paper, researcher ) {
 	const topicForms = researcher.getResearch( "morphology" );
+
+	// A helper to calculate the characters length of words in an array that is needed for some languages.
+	const keywordCharacterCount = researcher.getHelper( "wordsCharacterCount" );
+
 	const text = paper.getText();
 	const locale = paper.getLocale();
 
@@ -26,11 +33,15 @@ export default function( paper, researcher ) {
 		sentencesWithKeywords: [],
 	};
 
+	let charactersCount = 0;
+
 	/*
 	 * Count the amount of key phrase occurrences in the sentences.
-	 * An occurrence is counted when all keywords of the key phrase are contained within the sentence.
-	 * Each sentence can contain multiple key phrases
+	 * An occurrence is counted when all keywords of the key phrase are contained within the sentence. Each sentence can contain multiple key phrases
 	 * (e.g. "The apple potato is an apple and a potato." has two occurrences of the key phrase "apple potato").
+	 *
+	 * If a custom helper to calculate word characters is available, we also calculate the total characters of all the keyword forms
+	 * that are found in the text.
 	 */
 	sentences.forEach( sentence => {
 		const matchesInSentence = topicForms.keyphraseForms.map( keywordForms => matchWords( sentence, keywordForms, locale ) );
@@ -39,6 +50,13 @@ export default function( paper, researcher ) {
 		if ( hasAllKeywords ) {
 			const counts = matchesInSentence.map( match => match.count );
 			const foundWords = flattenDeep( matchesInSentence.map( match => match.matches ) );
+
+			// Check if a custom helper to calculate the characters length of all the keyword forms that are found is available.
+			if ( keywordCharacterCount ) {
+				// If the custom helper is available, also calculate the characters length of all the keyword forms that are found.
+				charactersCount += keywordCharacterCount( foundWords );
+			}
+
 			keywordsFound.count += Math.min( ...counts );
 			keywordsFound.matches.push( foundWords );
 			keywordsFound.sentencesWithKeywords.push( sentence );
@@ -47,10 +65,16 @@ export default function( paper, researcher ) {
 
 	const matches = unique( flattenDeep( keywordsFound.matches ) ).sort( ( a, b ) => b.length - a.length );
 
-	return {
+	const keywordCountObject = {
 		count: keywordsFound.count,
 		matches: matches,
 		markings: markWordsInSentences( matches, keywordsFound.sentencesWithKeywords, locale ),
 		length: topicForms.keyphraseForms.length,
 	};
+
+	if ( keywordCharacterCount ) {
+		keywordCountObject.charactersCount = charactersCount;
+	}
+
+	return keywordCountObject;
 }
