2520813c291e4e4c9367f5c7251e9931e0d0f737
Herre Groen <herre@yoast.com>
2021-10-27 14:44:01 +0200

    Update timestamps of terms, authors and post type archives when these are edited

--- src/integrations/watchers/indexable-post-type-archive-watcher.php
+++ src/integrations/watchers/indexable-post-type-archive-watcher.php
@@ -121,6 +121,11 @@ class Indexable_Post_Type_Archive_Watcher implements Integration_Interface {
 	 */
 	public function build_indexable( $post_type ) {
 		$indexable = $this->repository->find_for_post_type_archive( $post_type, false );
-		$this->builder->build_for_post_type_archive( $post_type, $indexable );
+		$indexable = $this->builder->build_for_post_type_archive( $post_type, $indexable );
+
+		if ( $indexable ) {
+			$indexable->object_last_modified = \max( $indexable->object_last_modified, \current_time( 'mysql' ) );
+			$indexable->save();
+		}
 	}
 }
