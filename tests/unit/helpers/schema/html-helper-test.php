e1e1248713deaca68ace3918870403839ea817d3
jrfnl <jrfnl@users.noreply.github.com>
2021-10-29 06:05:37 +0200

    HTML_Helper_Test: refactor the tests to use data providers
    This refactors both the `test_sanitize()` and `test_smart_strip_tags()`  tests to use data providers.
    
    This:
    1. Makes it easier to debug the test as the data provider will show a descriptive name for the failing test case and the test method itself now only contains one assertion instead of multiple.
    2. Auto-documents the test cases by using named data providers.
    3. Allows for more easily adding additional test cases.
    
    Note: The actual test cases themselves have not been changed by this refactor, though the order in which they are run has (to more closely match the logic order in the method under test).
    
    Includes changing the used assertion from `assertEquals()` (loose type check) to `assertSame()` (strict type check).

--- tests/unit/helpers/schema/html-helper-test.php
+++ tests/unit/helpers/schema/html-helper-test.php
@@ -35,53 +35,80 @@ class HTML_Helper_Test extends TestCase {
 	 * Test whether sanitize sanitizes properly.
 	 *
 	 * @covers ::sanitize
+	 *
+	 * @dataProvider data_sanitize
+	 *
+	 * @param mixed  $input    The input to sanitize.
+	 * @param string $expected The expected return value.
 	 */
-	public function test_sanitize() {
-		// Test whether sanitize properly removes script tags.
-		$input    = 'Test <script>alert(0)</script> bla';
-		$expected = 'Test alert(0) bla';
-		$this->assertEquals( $expected, $this->instance->sanitize( $input ) );
+	public function test_sanitize( $input, $expected ) {
+		$this->assertSame( $expected, $this->instance->sanitize( $input ) );
+	}
 
-		// Test whether sanitize leaves allowed <p> tags intact.
-		$input    = 'Test <p>Paragraph</p> bla';
-		$expected = 'Test <p>Paragraph</p> bla';
-		$this->assertEquals( $expected, $this->instance->sanitize( $input ) );
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_sanitize() {
+		return [
+			'Text containing script tags should have the script tags stripped' => [
+				'input'    => 'Test <script>alert(0)</script> bla',
+				'expected' => 'Test alert(0) bla',
+			],
+			'Text containing allowed tag should be returned unchanged' => [
+				'input'    => 'Test <p>Paragraph</p> bla',
+				'expected' => 'Test <p>Paragraph</p> bla',
+			],
+		];
 	}
 
 	/**
 	 * Test whether smart strips tags strips tags in a smart way.
 	 *
 	 * @covers ::smart_strip_tags
+	 *
+	 * @dataProvider data_smart_strip_tags
+	 *
+	 * @param mixed  $input    The input to sanitize.
+	 * @param string $expected The expected return value.
 	 */
-	public function test_smart_strip_tags() {
-		// Test removing script tags.
-		$input    = 'Test </script>bla';
-		$expected = 'Test bla';
-		$this->assertEquals( $expected, $this->instance->smart_strip_tags( $input ) );
+	public function test_smart_strip_tags( $input, $expected ) {
+		$this->assertSame( $expected, $this->instance->smart_strip_tags( $input ) );
+	}
 
-		// Test removing script tags.
-		$input    = 'Test </script ><script>alert(0)</script><script>';
-		$expected = 'Test';
-		$this->assertEquals( $expected, $this->instance->smart_strip_tags( $input ) );
-
-		// Test replacing `<br>` tags with spaces.
-		$input    = 'Test<br>word';
-		$expected = 'Test word';
-		$this->assertEquals( $expected, $this->instance->smart_strip_tags( $input ) );
-
-		// Test replacing closing heading tags with spaces.
-		$input    = '<h1>Heading</h1>
-First words';
-		$expected = 'Heading First words';
-		$this->assertEquals( $expected, $this->instance->smart_strip_tags( $input ) );
-
-		// Test replacing tags li with • and new lines with spaces.
-		$input    = 'I am:
+	/**
+	 * Data provider.
+	 *
+	 * @return array
+	 */
+	public function data_smart_strip_tags() {
+		return [
+			'Test replacing `<br>` tag with space' => [
+				'input'    => 'Test<br>word',
+				'expected' => 'Test word',
+			],
+			'Test adding a space when replacing heading close tags' => [
+				'input'    => '<h1>Heading</h1>
+First words',
+				'expected' => 'Heading First words',
+			],
+			'Test replacing tags li with • and new lines with spaces' => [
+				'input'    => 'I am:
 <ul>
 <li>smart</li>
 <li>beautiful</li>
-</ul>';
-		$expected = 'I am: • smart • beautiful';
-		$this->assertEquals( $expected, $this->instance->smart_strip_tags( $input ) );
+</ul>',
+				'expected' => 'I am: • smart • beautiful',
+			],
+			'Test removing incomplete set of script tags' => [
+				'input'    => 'Test </script>bla',
+				'expected' => 'Test bla',
+			],
+			'Test removing incomplete + complete set of script tags' => [
+				'input'    => 'Test </script ><script>alert(0)</script><script>',
+				'expected' => 'Test',
+			],
+		];
 	}
 }
