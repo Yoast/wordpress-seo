1b2cdc62749e08e060e82ece11f7ae89770eef01
nolledgeable <nolle@yoast.com>
2021-11-02 16:30:21 +0100

    re-add initial state gist

--- packages/seo-store/gists/initial-state.js
+++ packages/seo-store/gists/initial-state.js
@@ -1,48 +1,53 @@
 // Proposed state:
 export const state = {
-	data: {
-		// Do we need to send locale here?
-		seoTitle: "Title",
-		metaDescription: "",
+	analysis: {
+		options: {
+			analysisType: "post",
+			isSeoActive: true,
+			isReadabilityActive: true,
+			researches: [ "morphology" ],
+		},
+		results: {
+			status: "idle",
+			error: "",
+			seo: {
+				focus: {},
+				rej2oirj: {},
+			},
+			readability: {},
+			research: {
+				morphology: {},
+			},
+		},
+	},
+	editor: {
+		title: "",
 		date: "",
 		permalink: "",
-		slug: "",
-		content: "",
-		// Editor data we could use, for example in replacevars or for fallbacks.
-		title: "",
 		excerpt: "",
+		content: "",
 		featuredImage: {},
-		// Fallbacks? Make a solution similar to replacevars.
 	},
-	keyphrases: {
-		focus: {
-			keyphrase: "focus",
-			synonyms: "",
-		},
-		// Have a nanoid as key?
-		rej2oirj: {
-			id: "rej2oirj",
-			keyphrase: "a keyphrase",
-			synonyms: "a synonym",
-		},
-	},
-	config: {
-		// Do we need other config here?
-		analysisType: "post",
-		isSeoActive: true,
-		isReadabilityActive: true,
-		researches: [ "morphology" ],
-	},
-	results: {
-		status: "idle",
-		error: "",
+	form: {
 		seo: {
-			focus: {},
-			rej2oirj: {},
+			title: "Title",
+			description: "",
+			slug: "",
 		},
-		readability: {},
-		research: {
-			morphology: {},
+		keyphrases: {
+			focus: {
+				id: "focus",
+				keyphrase: "focus",
+				synonyms: "",
+			},
+			rej2oirj: {
+				id: "rej2oirj",
+				keyphrase: "a keyphrase",
+				synonyms: "a synonym",
+			},
 		},
+		schema: {},
+		social: {},
+		robots: {},
 	},
 };
