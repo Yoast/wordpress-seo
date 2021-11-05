cab20ecc2f43c2f9f226ac50417c7c5a218083bc
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2021-11-05 16:12:06 +0100

    Register a new minimal card

---
+++ packages/js/src/workouts/components/ConfigurationWorkoutCard.js
@@ -0,0 +1,52 @@
+// External dependencies.
+import PropTypes from "prop-types";
+import { __, sprintf } from "@wordpress/i18n";
+import { useSelect } from "@wordpress/data";
+// Internal dependencies.
+import WorkoutCard from "./WorkoutCard";
+
+/**
+ * The ConfigurationWorkoutCard component.
+ *
+ * @param {Object} props The props object.
+ *
+ * @returns {WPElement} The ConfigurationWorkoutCard component.
+ */
+export default function ConfigurationWorkoutCard( {
+	workout,
+	badges,
+} ) {
+	const finishedSteps = useSelect( "yoast-seo/workouts" ).getFinishedSteps( "configuration" );
+	return <WorkoutCard
+		name={ "configuration" }
+		title={ __( "Configuration", "wordpress-seo" ) }
+		// translators: %s translates to Yoast SEO.
+		subtitle={ sprintf( __( "Configure %s with the optimal SEO settings for your site", "wordpress-seo" ), "Yoast SEO" ) }
+		usps={ [
+			sprintf( __(
+				"Configure %s in a few steps",
+				"wordpress-seo"
+			), "Yoast SEO" ),
+			__(
+				"Apply the optimal SEO settings to your site",
+				"wordpress-seo"
+			),
+		] }
+		image={ "" }
+		finishableSteps={ [ "step1", "step2", "step3", "step4", "step5" ] }
+		finishedSteps={ finishedSteps }
+		upsell={ null }
+		workout={ workout }
+		badges={ badges }
+	/>;
+}
+
+ConfigurationWorkoutCard.propTypes = {
+	workout: PropTypes.func,
+	badges: PropTypes.arrayOf( PropTypes.element ),
+};
+
+ConfigurationWorkoutCard.defaultProps = {
+	workout: null,
+	badges: [],
+};
