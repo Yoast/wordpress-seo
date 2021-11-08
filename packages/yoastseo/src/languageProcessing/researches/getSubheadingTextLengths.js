903ce0c56ceba3578d9181bc85ada6721210cf71
Manuel <manuel@yoast.com>
2021-11-08 11:52:21 +0100

    Adapt getSubheadingTextLengths for Japanese

--- packages/yoastseo/src/languageProcessing/researches/getSubheadingTextLengths.js
+++ packages/yoastseo/src/languageProcessing/researches/getSubheadingTextLengths.js
@@ -5,18 +5,24 @@ import { forEach } from "lodash-es";
 
 /**
  * Gets the subheadings from the text and returns the length of these subheading in an array.
- * @param {Paper} paper The Paper object to get the text from.
+ *
+ * @param {Paper}       paper       The Paper object to get the text from.
+ * @param {Researcher}  researcher  The researcher to use for analysis.
+ *
  * @returns {Array} The array with the length of each subheading.
  */
-export default function( paper ) {
+export default function( paper, researcher ) {
 	const text = excludeTableOfContentsTag( paper.getText() );
 	const matches = getSubheadingTexts( text );
+
+	// A helper to count characters for languages that don't count number of words for text length.
+	const countCharacters = researcher.getHelper( "countCharacters" );
 
 	const subHeadingTexts = [];
 	forEach( matches, function( subHeading ) {
 		subHeadingTexts.push( {
 			text: subHeading,
-			wordCount: countWords( subHeading ),
+			countLength: countCharacters ? countCharacters( subHeading ) : countWords( subHeading ),
 		} );
 	} );
 	return subHeadingTexts;
