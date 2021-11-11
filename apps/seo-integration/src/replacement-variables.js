f589ce8cd84df7131c2605a33b7b7ab9a1c937e3
Igor <35524806+igorschoester@users.noreply.github.com>
2021-11-11 12:45:47 +0100

    Seo-integration: add replacement variables
    Refactor to cache the variables themselves on register and apply them in the filter (which still gets the current value when a replacevar is found)

--- apps/seo-integration/src/replacement-variables.js
+++ apps/seo-integration/src/replacement-variables.js
@@ -4,8 +4,8 @@ import createReplacementVariables from "@yoast/replacement-variables";
 import { SEO_STORE_NAME } from "@yoast/seo-store";
 import { reduce } from "lodash";
 
-const applyReplacementVariables = ( paper ) => {
-	const replacementVariables = createReplacementVariables( [
+const registerReplacementVariables = () => {
+	const { replacementVariables, apply } = createReplacementVariables( [
 		{
 			name: "title",
 			label: "Title",
@@ -15,19 +15,101 @@ const applyReplacementVariables = ( paper ) => {
 				return replacement;
 			},
 		},
+		{
+			name: "permalink",
+			label: "Permalink",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectPermalink();
+				console.log( "getReplacement/permalink", replacement );
+				return replacement;
+			},
+		},
+		{
+			name: "excerpt",
+			label: "Excerpt",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectExcerpt();
+				console.log( "getReplacement/excerpt", replacement );
+				return replacement;
+			},
+		},
+		{
+			name: "date",
+			label: "Date",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectDate();
+				console.log( "getReplacement/date", replacement );
+				return replacement;
+			},
+		},
+		{
+			name: "featuredImage",
+			label: "Featured image",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectFeaturedImage()?.url ?? "";
+				console.log( "getReplacement/featuredImage", replacement );
+				return replacement;
+			},
+		},
+		{
+			name: "keyphrase",
+			label: "Keyphrase",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectKeyphrase();
+				console.log( "getReplacement/keyphrase", replacement );
+				return replacement;
+			},
+		},
+		{
+			name: "synonyms",
+			label: "Synonyms",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectSynonyms();
+				console.log( "getReplacement/synonyms", replacement );
+				return replacement;
+			},
+		},
+		{
+			name: "seoTitle",
+			label: "SEO title",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectSeoTitle();
+				console.log( "getReplacement/seoTitle", replacement );
+				return replacement;
+			},
+		},
+		{
+			name: "metaDescription",
+			label: "Meta description",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectSeoDescription();
+				console.log( "getReplacement/metaDescription", replacement );
+				return replacement;
+			},
+		},
+		{
+			name: "slug",
+			label: "Slug",
+			getReplacement: () => {
+				const replacement = select( SEO_STORE_NAME ).selectSlug();
+				console.log( "getReplacement/slug", replacement );
+				return replacement;
+			},
+		},
 	] );
+	console.log("you can try the following replacement variables:", replacementVariables.map( replacevar => replacevar.name ) );
 
-	return reduce(
-		paper,
-		( acc, value, key ) => ( {
-			...acc,
-			[ key ]: replacementVariables.apply( value ),
-		} ),
-		{},
-	);
-};
+	const applyReplacementVariables = ( paper ) => {
+		return reduce(
+			paper,
+			( acc, value, key ) => ( {
+				...acc,
+				[ key ]: apply( value ),
+			} ),
+			{},
+		);
+	};
 
-const registerReplacementVariables = () => {
 	// Applying the replacement variables with a priority of 10, so that other functions can go before (< 10) or after (> 10) the replaced variables.
 	addFilter(
 		"yoast.seoStore.analysis.preparePaper",
