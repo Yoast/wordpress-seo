9265e669926010b68577da7de0cd8031b915fab2
Igor <35524806+igorschoester@users.noreply.github.com>
2021-11-11 12:14:15 +0100

    Seo-integration: add editor fields

--- apps/seo-integration/src/app.js
+++ apps/seo-integration/src/app.js
@@ -15,20 +15,41 @@ const useDebounce = ( callback, dependencies, debounceTimeInMs = 500 ) => {
 // First candidate classic editor with WooCommerce
 
 const Editor = () => {
-	const { updateContent, updateTitle } = useDispatch( SEO_STORE_NAME );
-	const handleContentChange = useDebounce( ( event ) => {
-		updateContent( event.target.value );
-	}, [ updateContent ] );
-	const handleTitleChange = useDebounce( ( event ) => {
-		updateTitle( event.target.value );
-	}, [ updateTitle ] );
+	const { updateContent, updateTitle, updatePermalink, updateExcerpt, updateDate, updateFeaturedImage } = useDispatch( SEO_STORE_NAME );
+
+	const handleContentChange = useDebounce( event => updateContent( event.target.value ), [ updateContent ] );
+	const handleTitleChange = useDebounce( event => updateTitle( event.target.value ), [ updateTitle ] );
+	const handlePermalinkChange = useDebounce( event => updatePermalink( event.target.value ), [ updatePermalink ] );
+	const handleExcerptChange = useDebounce( event => updateExcerpt( event.target.value ), [ updateExcerpt ] );
+	const handleDateChange = useDebounce( event => updateDate( event.target.value ), [ updateDate ] );
+	const handleFeaturedImageChange = useDebounce( event => updateFeaturedImage( { url: event.target.value } ), [ updateFeaturedImage ] );
 
 	return (
-		<main className="editor">
+		<main className="editor inline-flex flex-column">
+			<h1>Editor</h1>
 			<label htmlFor="editor-title">Title</label>
-			<input id="editor-title" name="title" onChange={ handleTitleChange } />
+			<div className="inline-flex">
+				<input id="editor-title" name="title" onChange={ handleTitleChange } />
+			</div>
 			<label htmlFor="editor-content">Content</label>
-			<textarea id="editor-content" name="editor" rows="16" onChange={ handleContentChange } />
+			<textarea id="editor-content" name="content" rows="16" onChange={ handleContentChange } />
+			<label htmlFor="editor-permalink">Permalink</label>
+			<div className="inline-flex">
+				<input id="editor-permalink" name="permalink" type="url" pattern="https?://.*" onChange={ handlePermalinkChange } />
+				<span className="validity" />
+			</div>
+			<label htmlFor="editor-excerpt">Excerpt</label>
+			<textarea id="editor-excerpt" name="excerpt" rows="3" onChange={ handleExcerptChange } />
+			<label htmlFor="editor-date">Date</label>
+			<div className="inline-flex">
+				<input id="editor-date" name="date" type="datetime-local" onChange={ handleDateChange } />
+				<span className="validity" />
+			</div>
+			<label htmlFor="editor-featured-image">Featured image URL</label>
+			<div className="inline-flex">
+				<input id="editor-featured-image" name="featured-image" type="url" pattern="https?://.*" onChange={ handleFeaturedImageChange } />
+				<span className="validity" />
+			</div>
 		</main>
 	);
 };
@@ -41,7 +62,7 @@ const Keyphrases = () => {
 	}, [ addKeyphraseEntry ] );
 
 	return (
-		<div className="keyphrases">
+		<div className="inline-flex flex-column">
 			{ ids.map( id => <Keyphrase key={ `keyphrase-entry-${ id }` } id={ id } /> ) }
 			<button onClick={ handleAddKeyphraseEntry }>Add keyphrase</button>
 		</div>
@@ -61,7 +82,7 @@ const Keyphrase = ( props ) => {
 	}, [ props.id, updateSynonyms ] );
 
 	return (
-		<fieldset className="keyphrase-entry">
+		<fieldset className="inline-flex flex-column">
 			<label htmlFor={ `keyphrase-${ props.id }` }>
 				{ props.id === "focus" ? "Focus keyphrase" : "Related keyphrase" }
 			</label>
@@ -89,6 +110,7 @@ function App() {
 		<>
 			<Editor />
 			<aside className="sidebar">
+				<h2>Sidebar</h2>
 				<Keyphrases />
 				<h4>SEO Results</h4>
 				<div>...</div>
