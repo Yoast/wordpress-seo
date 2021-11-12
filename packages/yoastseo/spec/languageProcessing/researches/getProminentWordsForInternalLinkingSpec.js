37c2fd2e1f8de35944107fd2b99e9b18730a7373
FAMarfuaty <aida@yoast.com>
2021-11-12 11:10:02 +0100

    adjust and add unit tests

--- packages/yoastseo/spec/languageProcessing/researches/getProminentWordsForInternalLinkingSpec.js
+++ packages/yoastseo/spec/languageProcessing/researches/getProminentWordsForInternalLinkingSpec.js
@@ -221,7 +221,9 @@ describe( "relevantWords research", function() {
 } );
 
 enableFeatures( [ "JAPANESE_SUPPORT" ] );
-describe( "test for prominent words research for Japanese", function() {
+describe( "test for prominent words research for languages that have custom helpers", function() {
+	// Japanese has custom helpers for getting words from the text, for counting text length
+	// And for returning custom function to return the stem of a word.
 	it( "returns no prominent words for texts under 100 words", function() {
 		const paper = new Paper( "東海道新幹線の開業前、東西の大動脈である東海道本線は高度経済成長下で線路容量が逼迫しており、抜本的な輸送力増強を迫られていた。" );
 
@@ -239,7 +241,7 @@ describe( "test for prominent words research for Japanese", function() {
 		expect( words ).toEqual( expected );
 	} );
 
-	it( "returns prominent words for texts with more than 300 words", function() {
+	it( "returns prominent words for texts with more than 300 words, in which the morphology data is available", function() {
 		const paper = new Paper( "私の美しい猫" + "の美しい猫".repeat( 180 ), { title: "題名" } );
 
 		const researcher = new JapaneseResearcher( paper );
@@ -249,6 +251,26 @@ describe( "test for prominent words research for Japanese", function() {
 			prominentWords: [
 				new ProminentWord( "猫", "猫", 181 ),
 				new ProminentWord( "美しい", "美しい", 181 ),
+			],
+			hasMetaDescription: false,
+			hasTitle: true,
+		};
+
+		const words = prominentWordsResearch( paper, researcher );
+
+		expect( words ).toEqual( expected );
+	} );
+
+	it( "returns prominent words for texts with more than 300 words, in which the morphology data is not available", function() {
+		const paper = new Paper( "私の美しい猫" + "の美しい猫".repeat( 180 ), { title: "題名" } );
+
+		const researcher = new JapaneseResearcher( paper );
+
+		const expected = {
+			prominentWords: [
+				new ProminentWord( "猫", "猫", 181 ),
+				new ProminentWord( "美しい", "美しい", 181 ),
+				new ProminentWord( "題名", "題名", 3 ),
 			],
 			hasMetaDescription: false,
 			hasTitle: true,
@@ -316,19 +338,42 @@ describe( "test for prominent words research for Japanese", function() {
 				new ProminentWord( "トラ", "トラ", 6 ),
 				new ProminentWord( "なき", "ない", 6 ),
 				new ProminentWord( "ペット", "ペット", 6 ),
+				new ProminentWord( "ヤマネコ", "ヤマネコ", 6 ),
 				new ProminentWord( "ライオン", "ライオン", 6 ),
-				new ProminentWord( "一方", "一方", 6 ),
 				new ProminentWord( "世界", "世界", 6 ),
 				new ProminentWord( "並ぶ", "並ば", 6 ),
 				new ProminentWord( "代表", "代表", 6 ),
 				new ProminentWord( "以下", "以下", 6 ),
-				new ProminentWord( "加え", "加い", 6 ),
-				new ProminentWord( "包括", "包括", 6 ),
+				new ProminentWord( "分類", "分類", 6 ),
 				new ProminentWord( "化", "化", 6 ),
 				new ProminentWord( "同様", "同様", 6 ),
+				new ProminentWord( "含め", "含ま", 6 ),
 				new ProminentWord( "含む全て", "含む全た", 6 ),
 				new ProminentWord( "大型", "大型", 6 ),
-				new ProminentWord( "学術", "学術", 6 ),
+				new ProminentWord( "家猫", "家猫", 6 ),
+				new ProminentWord( "家畜", "家畜", 6 ),
+				new ProminentWord( "属", "属", 6 ),
+				new ProminentWord( "広く", "広い", 6 ),
+				new ProminentWord( "広義", "広義", 6 ),
+				new ProminentWord( "後述", "後述", 6 ),
+				new ProminentWord( "日本", "日本", 6 ),
+				new ProminentWord( "本項", "本項", 6 ),
+				new ProminentWord( "物全", "物全", 6 ),
+				new ProminentWord( "特記", "特記", 6 ),
+				new ProminentWord( "犬", "犬", 6 ),
+				new ProminentWord( "狭義", "狭義", 6 ),
+				new ProminentWord( "目", "目", 6 ),
+				new ProminentWord( "知見", "知見", 6 ),
+				new ProminentWord( "科ネコ", "科ネコ", 6 ),
+				new ProminentWord( "科動", "科動", 6 ),
+				new ProminentWord( "科動物", "科動物", 6 ),
+				new ProminentWord( "種", "種", 6 ),
+				new ProminentWord( "般", "般", 6 ),
+				new ProminentWord( "解説", "解説", 6 ),
+				new ProminentWord( "通称", "通称", 6 ),
+				new ProminentWord( "限り", "限っ", 6 ),
+				new ProminentWord( "食肉", "食肉", 6 ),
+				new ProminentWord( "飼わ", "飼い", 6 ),
 			],
 			hasMetaDescription: true,
 			hasTitle: true,
