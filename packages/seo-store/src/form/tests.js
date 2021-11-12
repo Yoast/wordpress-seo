327967a3e999bdd5a0fbbf910e27b91735522dad
Simon <simon@yoast.com>
2021-11-12 14:16:35 +0100

    Add seo reducer tests

---
+++ packages/seo-store/src/form/tests.js
@@ -0,0 +1,63 @@
+import seoReducer, { seoActions } from "./slice/seo";
+
+describe( "Seo slice", () => {
+	// eslint-disable-next-line no-undefined
+	const previousState = undefined;
+
+	const initialState = {
+		title: "",
+		description: "",
+		slug: "",
+		isCornerstone: false,
+	};
+
+	describe( "Reducer", () => {
+		test( "should return the seo slice its initial state", () => {
+			expect( seoReducer( previousState, {} ) ).toEqual( initialState );
+		} );
+
+		test( "should update the title", () => {
+			const { updateSeoTitle } = seoActions;
+
+			const result = seoReducer( initialState, updateSeoTitle( "test" ) );
+
+			expect( result ).toEqual( {
+				...initialState,
+				title: "test",
+			} );
+		} );
+
+		test( "should update the description", () => {
+			const { updateSeoDescription } = seoActions;
+
+			const result = seoReducer( initialState, updateSeoDescription( "test" ) );
+
+			expect( result ).toEqual( {
+				...initialState,
+				description: "test",
+			} );
+		} );
+
+		test( "should update the slug", () => {
+			const { updateSlug } = seoActions;
+
+			const result = seoReducer( initialState, updateSlug( "test" ) );
+
+			expect( result ).toEqual( {
+				...initialState,
+				slug: "test",
+			} );
+		} );
+
+		test( "should update the isCornerstone", () => {
+			const { updateIsCornerstone } = seoActions;
+
+			const result = seoReducer( initialState, updateIsCornerstone( false ) );
+
+			expect( result ).toEqual( {
+				...initialState,
+				isCornerstone: false,
+			} );
+		} );
+	} );
+} );
