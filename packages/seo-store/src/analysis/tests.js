9cf19347b3c19823dfb9a7001d703ddd2b04c52e
Igor <35524806+igorschoester@users.noreply.github.com>
2021-11-15 15:24:50 +0100

    Seo-store: add analysis config reducer tests

--- packages/seo-store/src/analysis/tests.js
+++ packages/seo-store/src/analysis/tests.js
@@ -12,11 +12,11 @@ describe( "Config slice", () => {
 	};
 
 	describe( "Reducer", () => {
-		test( "should return the analysis config initial state", () => {
+		test( "should return the initial state", () => {
 			expect( configReducer( previousState, {} ) ).toEqual( initialState );
 		} );
 
-		test( "should update the analysisType", () => {
+		test( "should update the analysis type", () => {
 			const { updateAnalysisType } = configActions;
 
 			const result = configReducer( previousState, updateAnalysisType( "term" ) );
@@ -24,6 +24,50 @@ describe( "Config slice", () => {
 			expect( result ).toEqual( {
 				...initialState,
 				analysisType: "term",
+			} );
+		} );
+
+		test( "should update whether the SEO analysis is active", () => {
+			const { updateIsSeoActive } = configActions;
+
+			const result = configReducer( previousState, updateIsSeoActive( false ) );
+
+			expect( result ).toEqual( {
+				...initialState,
+				isSeoActive: false,
+			} );
+		} );
+
+		test( "should update whether the readability analysis is active", () => {
+			const { updateIsReadabilityActive } = configActions;
+
+			const result = configReducer( previousState, updateIsReadabilityActive( false ) );
+
+			expect( result ).toEqual( {
+				...initialState,
+				isReadabilityActive: false,
+			} );
+		} );
+
+		test( "should add a research", () => {
+			const { addResearch } = configActions;
+
+			const result = configReducer( previousState, addResearch( "test" ) );
+
+			expect( result ).toEqual( {
+				...initialState,
+				researches: [ "morphology", "test" ],
+			} );
+		} );
+
+		test( "should remove a research", () => {
+			const { removeResearch } = configActions;
+
+			const result = configReducer( previousState, removeResearch( "morphology" ) );
+
+			expect( result ).toEqual( {
+				...initialState,
+				researches: [],
 			} );
 		} );
 	} );
