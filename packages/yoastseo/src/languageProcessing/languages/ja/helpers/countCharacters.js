a58aaafa319fba450217d67d4c299321d04f1a46
FAMarfuaty <aida@yoast.com>
2021-11-22 12:01:55 +0100

    Also exclude ToC in sanitation helper

--- packages/yoastseo/src/languageProcessing/languages/ja/helpers/countCharacters.js
+++ packages/yoastseo/src/languageProcessing/languages/ja/helpers/countCharacters.js
@@ -1,4 +1,4 @@
-import { stripFullTags as stripTags } from "../../../helpers/sanitize/stripHTMLTags.js";
+import sanitizeString from "../../../helpers/sanitize/sanitizeString";
 
 /**
  * Calculates the character count of a text, including punctuation and numbers. Is used to determine length of text.
@@ -8,6 +8,7 @@ import { stripFullTags as stripTags } from "../../../helpers/sanitize/stripHTMLT
  * @returns {int} The word count of the given text.
  */
 export default function( text ) {
-	text = stripTags( text );
+	text = sanitizeString( text );
+
 	return text.length;
 }
