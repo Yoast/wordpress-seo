1991affd195a45e8eca029b279f9709d881686c4
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2021-11-24 09:47:20 +0100

    Some wordpress-seo-premium language domains remained in free

--- packages/js/src/workouts/components/WorkoutsPage.js
+++ packages/js/src/workouts/components/WorkoutsPage.js
@@ -102,7 +102,7 @@ export default function WorkoutsPage( props ) {
 	return (
 		<div>
 			<h1>
-				{ __( "SEO workouts", "wordpress-seo-premium" ) }
+				{ __( "SEO workouts", "wordpress-seo" ) }
 			</h1>
 			<p>
 				{ __(
