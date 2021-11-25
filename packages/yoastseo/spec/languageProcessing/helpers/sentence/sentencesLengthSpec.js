c772265d0398ef1bdffea4dbd7c900f73ffe6bde
Hanna Worku <hanna@yoast.com>
2021-11-25 16:51:59 +0100

    adapt wordCountInText research for Japanese

--- packages/yoastseo/spec/languageProcessing/helpers/sentence/sentencesLengthSpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/sentence/sentencesLengthSpec.js
@@ -1,19 +1,29 @@
 import sentencesLength from "../../../../src/languageProcessing/helpers/sentence/sentencesLength";
+import JapaneseResearcher from "../../../../src/languageProcessing/languages/ja/Researcher";
+import EnglishResearcher from "../../../../src/languageProcessing/languages/en/Researcher";
 
 describe( "A test to count the sentence length.", function() {
 	it( "doesn't return a length for an empty sentence", function() {
 		const sentences = [ "", "A sentence" ];
 
-		const lengths = sentencesLength( sentences );
+		const lengths = sentencesLength( sentences, new EnglishResearcher() );
 
-		expect( lengths ).toEqual( [ { sentence: "A sentence", sentenceLength: 2 } ] );
+		expect( lengths ).toEqual( [ { sentence: "A sentence", countLength: 2 } ] );
 	} );
 
 	it( "returns the string and the length of each sentence (the HTML tags should be stripped if present)", function() {
 		const sentences = [ "A good text", "this is a <span>textstring</span>" ];
 
-		const lengths = sentencesLength( sentences );
+		const lengths = sentencesLength( sentences, new EnglishResearcher() );
 
-		expect( lengths ).toEqual( [ { sentence: "A good text", sentenceLength: 3 }, { sentence: "this is a textstring", sentenceLength: 4 } ] );
+		expect( lengths ).toEqual( [ { sentence: "A good text", countLength: 3 }, { sentence: "this is a textstring", countLength: 4 } ] );
+	} );
+
+	it( "returns the string and the length of each sentence (the HTML tags should be stripped if present) in Japanese", function() {
+		const sentences = [ "自然おのずから存在しているもの", "歩くさわやかな森 <span>自然</span>" ];
+
+		const lengths = sentencesLength( sentences, new JapaneseResearcher() );
+
+		expect( lengths ).toEqual( [ { sentence: "自然おのずから存在しているもの", countLength: 15 }, { sentence: "歩くさわやかな森自然", countLength: 10 } ] );
 	} );
 } );
