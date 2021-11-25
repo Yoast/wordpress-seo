a3605e1ebf43bd5f27f7ca4539e2ef536301aa73
Jannes <jannes@yoast.com>
2021-11-25 11:02:36 +0100

    Move the code to the actual getter and optimize it

--- src/repositories/seo-links-repository.php
+++ src/repositories/seo-links-repository.php
@@ -109,12 +109,27 @@ class SEO_Links_Repository {
 	 * @return array An array of associative arrays, each containing a indexable id and incoming property.
 	 */
 	public function get_incoming_link_counts_for_indexable_ids( $indexable_ids ) {
-		return $this->query()
+		$indexable_counts = $this->query()
 			->select_expr( 'COUNT( id )', 'incoming' )
 			->select( 'target_indexable_id' )
 			->where_in( 'target_indexable_id', $indexable_ids )
 			->group_by( 'target_indexable_id' )
 			->find_array();
+
+		// The query only returns ID's with an incoming count > 0. We need to restore any ID's with 0 incoming links.
+		// Get all ID's returned from the query:
+		$returned_ids = array_column($indexable_counts, 'target_indexable_id');
+
+		// Loop over the original ID's and search them in the returned ID's. If they don't exist, add them with an incoming count of 0:
+		foreach ( $indexable_ids as $id ){
+			// Cast the ID to string, as the arrays only contain stringified versions of the ID:
+			$id = strval($id);
+			if ( array_search($id, $returned_ids, true) === false ) {
+				$indexable_counts[] = [ 'incoming' => "0", 'target_indexable_id' => $id ];
+			}
+		}
+
+		return $indexable_counts;	
 	}
 
 	/**
