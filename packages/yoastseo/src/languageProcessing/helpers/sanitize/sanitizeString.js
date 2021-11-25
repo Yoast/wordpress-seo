7a56d61ae797a39535984fb45b5d790c147edaed
FAMarfuaty <aida@yoast.com>
2021-11-25 14:49:53 +0100

    Remove duplicate logic in sanitizing a string

--- packages/yoastseo/src/languageProcessing/helpers/sanitize/sanitizeString.js
+++ packages/yoastseo/src/languageProcessing/helpers/sanitize/sanitizeString.js
@@ -1,8 +1,6 @@
 import excludeTableOfContentsTag from "./excludeTableOfContentsTag";
 import { stripFullTags as stripTags } from "./stripHTMLTags.js";
 
-import stripSpaces from "./stripSpaces.js";
-
 /**
  * Sanitizes the text before we use the text for the analysis.
  *
@@ -13,7 +11,6 @@ import stripSpaces from "./stripSpaces.js";
 export default function( text ) {
 	text = excludeTableOfContentsTag( text );
 	text = stripTags( text );
-	text = stripSpaces( text );
 
 	return text;
 }
