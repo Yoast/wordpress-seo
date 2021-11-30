d557ae56567bc1506d26776dc2e6eccb7145346b
Leonidas Milosis <leonidas.milossis@gmail.com>
2021-11-30 16:42:47 +0200

    Add unit tests for taxonomy settings import

---
+++ tests/unit/actions/importing/aioseo-taxonomy-settings-importing-action-test.php
@@ -0,0 +1,260 @@
+<?php
+
+namespace Yoast\WP\SEO\Tests\Unit\Actions\Importing;
+
+use Mockery;
+use Brain\Monkey;
+use Yoast\WP\SEO\Actions\Importing\Aioseo_Taxonomy_Settings_Importing_Action;
+use Yoast\WP\SEO\Helpers\Options_Helper;
+use Yoast\WP\SEO\Tests\Unit\TestCase;
+use Yoast\WP\SEO\Tests\Unit\Doubles\Actions\Importing\Aioseo_Taxonomy_Settings_Importing_Action_Double;
+
+/**
+ * Aioseo_Taxonomy_Settings_Importing_Action_Test class
+ *
+ * @group actions
+ * @group importing
+ *
+ * @coversDefaultClass \Yoast\WP\SEO\Actions\Importing\Aioseo_Taxonomy_Settings_Importing_Action
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded, Yoast.Yoast.AlternativeFunctions.json_encode_json_encode
+ */
+class Aioseo_Taxonomy_Settings_Importing_Action_Test extends TestCase {
+
+	/**
+	 * Represents the instance to test.
+	 *
+	 * @var Aioseo_Taxonomy_Settings_Importing_Action
+	 */
+	protected $instance;
+
+	/**
+	 * Represents the mock instance to test.
+	 *
+	 * @var Aioseo_Taxonomy_Settings_Importing_Action_Double
+	 */
+	protected $mock_instance;
+
+	/**
+	 * The mocked options helper.
+	 *
+	 * @var Mockery\MockInterface|Options_Helper
+	 */
+	protected $options;
+
+	/**
+	 * An array of the total Default Archive Settings we can import.
+	 *
+	 * @var Mockery\MockInterface|Options_Helper
+	 */
+	protected $full_settings_to_import = [
+		'category'      => [
+			'show'            => true,
+			'title'           => 'Category Title',
+			'metaDescription' => 'Category Desc',
+			'advanced'        => [
+				'showDateInGooglePreview' => true,
+			],
+		],
+		'post_tag'      => [
+			'show'            => true,
+			'title'           => 'Tag Title',
+			'metaDescription' => 'Tag Desc',
+			'advanced'        => [
+				'showDateInGooglePreview' => true,
+			],
+		],
+		'book-category' => [
+			'show'            => true,
+			'title'           => 'Category Title',
+			'metaDescription' => 'Category Desc',
+			'advanced'        => [
+				'showDateInGooglePreview' => true,
+			],
+		],
+	];
+
+	/**
+	 * Sets up the test class.
+	 */
+	protected function set_up() {
+		parent::set_up();
+
+		$this->options       = Mockery::mock( Options_Helper::class );
+		$this->instance      = new Aioseo_Taxonomy_Settings_Importing_Action( $this->options );
+		$this->mock_instance = Mockery::mock(
+			Aioseo_Taxonomy_Settings_Importing_Action_Double::class,
+			[ $this->options ]
+		)->makePartial()->shouldAllowMockingProtectedMethods();
+	}
+
+	/**
+	 * Tests the getting of the source option_name.
+	 *
+	 * @covers ::get_source_option_name
+	 */
+	public function test_get_source_option_name() {
+		$source_option_name = $this->instance->get_source_option_name();
+		$this->assertEquals( $source_option_name, 'aioseo_options_dynamic' );
+	}
+
+	/**
+	 * Tests retrieving unimported AiOSEO settings.
+	 *
+	 * @param array $query_results The results from the query.
+	 * @param bool  $expected      The expected retrieved data.
+	 *
+	 * @dataProvider provider_query
+	 * @covers ::query
+	 */
+	public function test_query( $query_results, $expected ) {
+		Monkey\Functions\expect( 'get_option' )
+			->once()
+			->andReturn( $query_results );
+
+		$this->mock_instance->shouldReceive( 'get_unimported_chunk' )
+			->with( $expected, null )
+			->zeroOrMoreTimes()
+			->andReturn( $expected );
+
+		$settings_to_import = $this->mock_instance->query();
+		$this->assertTrue( $settings_to_import === $expected );
+	}
+
+	/**
+	 * Tests flattening AIOSEO default archive settings.
+	 *
+	 * @covers ::flatten_settings
+	 */
+	public function test_flatten_settings() {
+		$flattened_sesttings = $this->mock_instance->flatten_settings( $this->full_settings_to_import );
+		$expected_result     = [
+			'/category/show'                                  => true,
+			'/category/title'                                 => 'Category Title',
+			'/category/metaDescription'                       => 'Category Desc',
+			'/category/advanced/showDateInGooglePreview'      => true,
+			'/post_tag/show'                                  => true,
+			'/post_tag/title'                                 => 'Tag Title',
+			'/post_tag/metaDescription'                       => 'Tag Desc',
+			'/post_tag/advanced/showDateInGooglePreview'      => true,
+			'/book-category/show'                             => true,
+			'/book-category/title'                            => 'Category Title',
+			'/book-category/metaDescription'                  => 'Category Desc',
+			'/book-category/advanced/showDateInGooglePreview' => true,
+		];
+
+		$this->assertTrue( $expected_result === $flattened_sesttings );
+	}
+
+	/**
+	 * Tests mapping AIOSEO default archive settings.
+	 *
+	 * @param string $setting       The setting at hand, eg. post or movie-category, separator etc.
+	 * @param string $setting_value The value of the AIOSEO setting at hand.
+	 *
+	 * @dataProvider provider_map
+	 * @covers ::map
+	 */
+	public function test_map( $setting, $setting_value ) {
+		$taxonomies = [
+			(object) [
+				'name' => 'category',
+			],
+			(object) [
+				'name' => 'post_tag',
+			],
+			(object) [
+				'name' => 'book-category',
+			],
+		];
+		Monkey\Functions\expect( 'get_taxonomies' )
+			->once()
+			->andReturn( $taxonomies );
+
+		$this->mock_instance->build_mapping();
+
+		$aioseo_options_to_yoast_map = $this->mock_instance->get_aioseo_options_to_yoast_map();
+
+		if ( isset( $aioseo_options_to_yoast_map[ $setting ] ) ) {
+			$this->mock_instance->shouldReceive( 'import_single_setting' )
+				->with( $setting, $setting_value, $aioseo_options_to_yoast_map[ $setting ] )
+				->once();
+		}
+		else {
+			$this->mock_instance->shouldReceive( 'import_single_setting' )
+				->never();
+		}
+
+		$this->mock_instance->map( $setting_value, $setting );
+	}
+
+	/**
+	 * Data provider for test_map().
+	 *
+	 * @return array
+	 */
+	public function provider_map() {
+		return [
+			[ '/category/title', 'Category Title' ],
+			[ '/category/metaDescription', 'Category Desc' ],
+			[ '/post_tag/show', 'Tag Title' ],
+			[ '/post_tag/metaDescription', 'Tag Title' ],
+			[ '/book-category/title', 'Category Title' ],
+			[ '/book-category/metaDescription', 'Category Desc' ],
+			[ '/randomSetting', 'randomeValue' ],
+		];
+	}
+
+	/**
+	 * Data provider for test_query().
+	 *
+	 * @return string
+	 */
+	public function provider_query() {
+		$full_settings = [
+			'searchAppearance' => [
+				'taxonomies'   => $this->full_settings_to_import,
+				'postypes'     => [
+					'post' => [
+						'title'           => 'title1',
+						'metaDescription' => 'desc1',
+					],
+				],
+			],
+		];
+
+		$full_settings_expected = $this->full_settings_to_import;
+
+		$missing_settings = [
+			'searchAppearance' => [
+				'postypes' => [
+					'post' => [
+						'title'           => 'title1',
+						'metaDescription' => 'desc1',
+					],
+				],
+			],
+		];
+
+		$missing_settings_expected = [];
+
+		$malformed_settings = [
+			'searchAppearance' => [
+				'taxonomies' => 'not_array',
+				'postypes'   => [
+					'post' => [
+						'title'           => 'title1',
+						'metaDescription' => 'desc1',
+					],
+				],
+			],
+		];
+
+		$malformed_settings_expected = [];
+
+		return [
+			[ \json_encode( $full_settings ), $full_settings_expected ],
+			[ \json_encode( $missing_settings ), $missing_settings_expected ],
+			[ \json_encode( $missing_settings ), $missing_settings_expected ],
+		];
+	}
+}
