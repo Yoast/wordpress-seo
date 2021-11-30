b6abe14e19458f323fe469692c317f94851a4876
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2021-11-30 12:54:48 +0100

    Add logic for userselect and update copy

--- packages/js/src/workouts/components/ConfigurationWorkout.js
+++ packages/js/src/workouts/components/ConfigurationWorkout.js
@@ -17,6 +17,7 @@ import { PersonSection } from "./PersonSection";
 import { NewsletterSignup } from "./NewsletterSignup";
 import { WorkoutIndexation } from "./WorkoutIndexation";
 import SocialInputSection from "./SocialInputSection";
+import SocialInputPersonSection from "./SocialInputPersonSection";
 import { scrollToStep } from "../helpers";
 
 window.wpseoScriptData = window.wpseoScriptData || {};
@@ -700,55 +701,7 @@ export function ConfigurationWorkout( { finishSteps, reviseStep, toggleWorkout, 
 						errorFields={ state.errorFields }
 						setErrorFields={ setErrorFields }
 					/> }
-					{ siteRepresentsPerson && <div>
-						<p>
-							{
-								createInterpolateElement(
-									sprintf(
-										__(
-											// translators: %1$s and %2$s are replaced by opening and closing <b> tags, %3$s and %4$s are replaced by opening and closing anchor tags
-											"%1$sNote%2$s: In this step, you need to add the personal social profiles of the person your site represents. To do that, you should go to the %3$sUsers%4$s > Profile page in a new browser tab.",
-											"wordpress-seo"
-										),
-										"<b>",
-										"</b>",
-										"<a>",
-										"</a>"
-									),
-									{
-										b: <b />,
-										// eslint-disable-next-line jsx-a11y/anchor-has-content
-										a: <a id="yoast-configuration-workout-user-page-link-1" href={ window.wpseoWorkoutsData.usersPageUrl } target="_blank" rel="noopener noreferrer" />,
-									}
-								)
-							}
-						</p>
-						<p>
-							{
-								addLinkToString(
-									sprintf(
-										__(
-											// translators: %1$s and %2$s are replaced by opening and closing anchor tags.
-											"On the %1$sUsers%2$s page, hover your mouse over the username you want to edit. Click ‘Edit’ to access the user’s profile. Then, scroll down to the ‘Contact info’ section and fill in the URLs of the personal social profiles you want to add.",
-											"wordpress-seo"
-										),
-										"<a>",
-										"</a>"
-									),
-									window.wpseoWorkoutsData.usersPageUrl,
-									"yoast-configuration-workout-user-page-link-2"
-								)
-							}
-						</p>
-						<p>
-							<b>{ __( "Screenshot:", "wordpress-seo" ) }</b>
-							<img
-								src={ window.wpseoWorkoutsData.pluginUrl + "/images/profile-social-fields.png" }
-								alt={ __( "A screenshot of the Contact Info section of a user's Profile page", "wordpress-seo" ) }
-							/>
-						</p>
-					</div>
-					}
+					{ siteRepresentsPerson && <SocialInputPersonSection personId={ state.personId } /> }
 					<FinishButtonSection
 						buttonId="yoast-configuration-workout-step-social-profiles-button"
 						stepNumber={ 3 }
