ecc5771436d2bea99baf4559576061b300e902f7
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2021-12-03 21:46:02 +0100

    Add prototype for steppers copied from tailwind ui and translated css properties

--- packages/js/src/installation-success.js
+++ packages/js/src/installation-success.js
@@ -7,14 +7,45 @@ import { ButtonStyledLink } from "@yoast/components";
 setWordPressSeoL10n();
 
 /**
- *
+ * @returns {WPElement} The fake steppers.
+ */
+export default function Steppers() {
+	return (
+		<ul className="step-list">
+			<li className={ "step-complete" }>
+				<div className="step-first-div" aria-hidden="true">
+					<div className="step-second-div" />
+				</div>
+				<span
+					className="step-list-complete-circle"
+				>
+					{/* <CheckIcon className="w-5 h-5 text-white" aria-hidden="true" /> */}
+					<span className="complete-check-icon" />
+				</span>
+			</li>
+			<li className={ "step-current" }>
+				<span
+					href="#"
+					className="step-list-current-circle"
+					aria-current="step"
+				>
+					<span className="step-list-current-inner-circle" aria-hidden="true" />
+				</span>
+			</li>
+		</ul>
+	);
+}
+
+
+/**
  * @returns {WPElement} The installation success page.
  */
 function InstallationSuccessPage() {
 	return (
 		<div className="installation-success-page">
+			<h1 className="installation-success-title">{ __( "You've successfully installed Yoast SEO!", "wordpress-seo" ) }</h1>
 			<div className="installation-success-steps">
-				<p>stepper stuff</p>
+				<Steppers />
 			</div>
 			<div className="installation-success-cards">
 				<div className="installation-success-card">
