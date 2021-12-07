92090ac80ff8a59a81b3e802952574de43699aa8
Karlijn Bok <karlijn@yoast.com>
2021-12-07 15:46:40 +0100

    Fix cs

--- src/conditionals/wincher-conditional.php
+++ src/conditionals/wincher-conditional.php
@@ -7,12 +7,12 @@ namespace Yoast\WP\SEO\Conditionals;
  */
 class Wincher_Conditional extends Feature_Flag_Conditional {
 
-    /**
-     * Returns the name of the Wincher integration feature flag.
+	/**
+	 * Returns the name of the Wincher integration feature flag.
 	 *
 	 * @return string The name of the feature flag.
-     */
-    protected function get_feature_flag() {
-        return 'WINCHER_INTEGRATION';
-    }
+	 */
+	protected function get_feature_flag() {
+		return 'WINCHER_INTEGRATION';
+	}
 }
