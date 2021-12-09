ad0e59fd69db61befdd9e9001aa7a7239b977721
YoastBot CI <yoastbot-ci@yoast.com>
2021-12-09 13:27:31 +0000

    Bump Gutenberg supported version.

--- admin/class-gutenberg-compatibility.php
+++ admin/class-gutenberg-compatibility.php
@@ -15,14 +15,14 @@ class WPSEO_Gutenberg_Compatibility {
 	 *
 	 * @var string
 	 */
-	const CURRENT_RELEASE = '12.0.2';
+	const CURRENT_RELEASE = '12.1.0';
 
 	/**
 	 * The minimally supported version of Gutenberg by the plugin.
 	 *
 	 * @var string
 	 */
-	const MINIMUM_SUPPORTED = '12.0.2';
+	const MINIMUM_SUPPORTED = '12.1.0';
 
 	/**
 	 * Holds the current version.
