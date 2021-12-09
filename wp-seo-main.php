276cf3fb1c1f976b80001926494412be4319588e
YoastBot CI <yoastbot-ci@yoast.com>
2021-12-09 13:27:36 +0000

    Bump version to 17.8-RC6

--- wp-seo-main.php
+++ wp-seo-main.php
@@ -15,7 +15,7 @@ if ( ! function_exists( 'add_filter' ) ) {
  * {@internal Nobody should be able to overrule the real version number as this can cause
  *            serious issues with the options, so no if ( ! defined() ).}}
  */
-define( 'WPSEO_VERSION', '17.8-RC5' );
+define( 'WPSEO_VERSION', '17.8-RC6' );
 
 
 if ( ! defined( 'WPSEO_PATH' ) ) {
