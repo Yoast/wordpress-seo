8682b8060dc9f36071a600338f8b3c29ce0f5cf6
FAMarfuaty <aida@yoast.com>
2021-12-09 12:41:37 +0100

    Adjust Title keyword assessment for Japanese

--- packages/yoastseo/src/scoring/assessments/seo/TitleKeywordAssessment.js
+++ packages/yoastseo/src/scoring/assessments/seo/TitleKeywordAssessment.js
@@ -1,5 +1,6 @@
 import { __, sprintf } from "@wordpress/i18n";
 import { escape, merge } from "lodash-es";
+import getLanguage from "../../../languageProcessing/helpers/language/getLanguage";
 
 import Assessment from "../assessment";
 import { createAnchorOpeningTag } from "../../../helpers/shortlinker";
@@ -35,6 +36,10 @@ class TitleKeywordAssessment extends Assessment {
 			},
 			urlTitle: createAnchorOpeningTag( "https://yoa.st/33g" ),
 			urlCallToAction: createAnchorOpeningTag( "https://yoa.st/33h" ),
+			feedbackStrings: {
+				bad: "For the best SEO results write the exact match of your keyphrase in the SEO title, " +
+					"and put the keyphrase at the beginning of the title",
+			},
 		};
 
 		this.identifier = "titleKeyword";
@@ -50,12 +55,13 @@ class TitleKeywordAssessment extends Assessment {
 	 * @returns {AssessmentResult} The result of the assessment with text and score.
 	 */
 	getResult( paper, researcher ) {
+		const language = getLanguage( paper.getLocale() );
 		this._keywordMatches = researcher.getResearch( "findKeywordInPageTitle" );
 		this._keyword = escape( paper.getKeyword() );
 
 		const assessmentResult = new AssessmentResult();
 
-		const calculatedResult = this.calculateResult( this._keyword );
+		const calculatedResult = this.calculateResult( this._keyword, language );
 		assessmentResult.setScore( calculatedResult.score );
 		assessmentResult.setText( calculatedResult.resultText );
 
@@ -79,10 +85,16 @@ class TitleKeywordAssessment extends Assessment {
 	 * from the keyphrase are in the title (in any form). Returns BAD otherwise.
 	 *
 	 * @param {string}  keyword     The keyword of the paper (to be returned in the feedback strings).
+	 * @param {string}  language    The language to check.
 	 *
 	 * @returns {Object} Object with score and text.
 	 */
-	calculateResult( keyword ) {
+	calculateResult( keyword, language ) {
+		const feedbackStrings = this._config.feedbackStrings;
+		if ( language === "ja" ) {
+			feedbackStrings.bad = "For the best SEO results include all words of your keyphrase in the SEO title, " +
+					"and put the keyphrase at the beginning of the title";
+		}
 		const exactMatchFound = this._keywordMatches.exactMatchFound;
 		const position = this._keywordMatches.position;
 		const allWordsFound = this._keywordMatches.allWordsFound;
@@ -122,6 +134,38 @@ class TitleKeywordAssessment extends Assessment {
 		}
 
 		if ( allWordsFound ) {
+			if ( language === "ja" ) {
+				if ( position === 0 ) {
+					return {
+						score: this._config.scores.good,
+						resultText: sprintf(
+							/* Translators: %1$s expands to a link on yoast.com,
+							%2$s expands to the anchor end tag. */
+							__(
+								"%1$sKeyphrase in title%2$s: The focus keyphrase appears at the beginning of the SEO title. Good job!",
+								"wordpress-seo"
+							),
+							this._config.urlTitle,
+							"</a>"
+						),
+					};
+				}
+				return {
+					score: this._config.scores.okay,
+					resultText: sprintf(
+						/* Translators: %1$s and %2$s expand to a link on yoast.com,
+						%3$s expands to the anchor end tag. */
+						__(
+							// eslint-disable-next-line max-len
+							"%1$sKeyphrase in title%3$s: Title does not begin with the focus keyphrase. %2$sMove your focus keyphrase to the beginning of the title.%3$s.",
+							"wordpress-seo"
+						),
+						this._config.urlTitle,
+						this._config.urlCallToAction,
+						"</a>"
+					),
+				};
+			}
 			return {
 				score: this._config.scores.okay,
 				resultText: sprintf(
@@ -165,13 +209,14 @@ class TitleKeywordAssessment extends Assessment {
 				%3$s expands to the anchor end tag, %4$s expands to the keyword of the article. */
 				__(
 					// eslint-disable-next-line max-len
-					"%1$sKeyphrase in title%3$s: Not all the words from your keyphrase \"%4$s\" appear in the SEO title. %2$sFor the best SEO results write the exact match of your keyphrase in the SEO title, and put the keyphrase at the beginning of the title%3$s.",
+					"%1$sKeyphrase in title%3$s: Not all the words from your keyphrase \"%4$s\" appear in the SEO title. %2$s%5$s%3$s.",
 					"wordpress-seo"
 				),
 				this._config.urlTitle,
 				this._config.urlCallToAction,
 				"</a>",
-				keyword
+				keyword,
+				feedbackStrings.bad
 			),
 		};
 	}
