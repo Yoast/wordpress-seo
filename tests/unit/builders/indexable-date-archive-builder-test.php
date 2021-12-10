d8f754f0e2e28888aee931eafd4931983acd6b50
Diede Exterkate <diede@yoast.com>
2021-12-10 12:11:41 +0100

    Fix tests

--- tests/unit/builders/indexable-date-archive-builder-test.php
+++ tests/unit/builders/indexable-date-archive-builder-test.php
@@ -48,7 +48,7 @@ class Indexable_Date_Archive_Builder_Test extends TestCase {
 		Functions\expect( 'get_current_blog_id' )->once()->andReturn( 1 );
 		$indexable_mock->orm->expects( 'set' )->with( 'blog_id', 1 );
 		$indexable_mock->orm->expects( 'set' )->with( 'permalink', null );
-		$indexable_mock->orm->expects( 'set' )->with( 'version', 1 );
+		$indexable_mock->orm->expects( 'set' )->with( 'version', 2 );
 		$indexable_mock->orm->expects( 'set' )->with( 'is_publicly_viewable', 1 );
 
 		$builder = new Indexable_Date_Archive_Builder( $options_mock, new Indexable_Builder_Versions() );
