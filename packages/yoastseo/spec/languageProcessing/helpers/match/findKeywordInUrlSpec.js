1ac7e8ee0c60c7e3850b36f7ec11f850970973fa
FAMarfuaty <aida@yoast.com>
2021-12-15 13:27:11 +0100

    Add more unit tests

--- packages/yoastseo/spec/languageProcessing/helpers/match/findKeywordInUrlSpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/match/findKeywordInUrlSpec.js
@@ -8,19 +8,52 @@ describe( "findKeywordInUrl", function() {
 } );
 
 describe( "checks keyword occurences in links", function() {
+	let mockExactMatchRequestObject = {
+		exactMatchRequested: false,
+		keyphrase: "yoast",
+	};
 	it( "returns keywords found", function() {
-		expect( findKeywordInUrl( "<a href='http://yoast.com'>test</a>", { keyphraseForms: [ [ "yoast" ] ] } ) ).toBe( false );
+		expect( findKeywordInUrl( "<a href='http://yoast.com'>test</a>",
+			{ keyphraseForms: [ [ "yoast" ] ] },
+			"en_US",
+			false,
+			mockExactMatchRequestObject
+		) ).toBe( false );
 	} );
 
 	it( "returns keywords found", function() {
-		expect( findKeywordInUrl( "<a href='http://yoast.com'>yoast</a>", { keyphraseForms: [ [ "yoast" ] ] } ) ).toBe( true );
+		expect( findKeywordInUrl( "<a href='http://yoast.com'>yoast</a>",
+			{ keyphraseForms: [ [ "yoast" ] ] },
+			"en_US",
+			false,
+			mockExactMatchRequestObject
+		) ).toBe( true );
 	} );
 
 	it( "returns keywords found for a language that has a custom helper for matching words", function() {
 		const url = "<a href='http://yoast.com'>yoast</a>";
-		expect( findKeywordInUrl( url, { keyphraseForms: [ [ "yoast" ] ] }, "ja", matchWordCustomHelper ) ).toBe( true );
+		expect( findKeywordInUrl( url,
+			{ keyphraseForms: [ [ "yoast" ] ] },
+			"ja",
+			matchWordCustomHelper,
+			mockExactMatchRequestObject
+		) ).toBe( true );
 	} );
 
+	mockExactMatchRequestObject = {
+		exactMatchRequested: true,
+		keyphrase: "yoast",
+	};
+
+	it( "returns keywords found for when the exact match is requested", function() {
+		const url = "<a href='http://yoast.com'>yoast</a>";
+		expect( findKeywordInUrl( url,
+			{ keyphraseForms: [ [ "yoast" ] ] },
+			"en_US",
+			matchWordCustomHelper,
+			mockExactMatchRequestObject
+		) ).toBe( true );
+	} );
 	/*
 	// This test does not work with the current implementation of morphological researcher.
 	it( "returns keywords found", function() {
