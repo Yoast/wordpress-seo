6b1f91768e0f3115ced02f693e1215c78a749a3c
Agnieszka Szuba <agnieszka@yoast.com>
2021-12-20 13:12:54 +0100

    Adjust full text test results after adding Japanese punctuation

--- packages/yoastseo/spec/fullTextTests/testTexts/es/spanishPaper2.js
+++ packages/yoastseo/spec/fullTextTests/testTexts/es/spanishPaper2.js
@@ -54,7 +54,7 @@ const expectedResults = {
 	textLength: {
 		isApplicable: true,
 		score: 9,
-		resultText: "<a href='https://yoa.st/34n' target='_blank'>Text length</a>: The text contains 914 words. Good job!",
+		resultText: "<a href='https://yoa.st/34n' target='_blank'>Text length</a>: The text contains 913 words. Good job!",
 	},
 	externalLinks: {
 		isApplicable: true,
@@ -97,7 +97,7 @@ const expectedResults = {
 	fleschReadingEase: {
 		isApplicable: true,
 		score: 9,
-		resultText: "<a href='https://yoa.st/34r' target='_blank'>Flesch Reading Ease</a>: The copy scores 73.5 in the test, which is considered fairly easy to read. Good job!",
+		resultText: "<a href='https://yoa.st/34r' target='_blank'>Flesch Reading Ease</a>: The copy scores 73.3 in the test, which is considered fairly easy to read. Good job!",
 	},
 	subheadingsTooLong: {
 		isApplicable: true,
