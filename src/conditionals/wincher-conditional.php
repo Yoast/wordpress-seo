e1140b7fe3b18435c89e150774b4f24e045c6318
Enrico Battocchi <enrico.battocchi@gmail.com>
2021-12-20 17:03:39 +0100

    Remove unnecessary override of the `is_met` method

--- src/conditionals/wincher-conditional.php
+++ src/conditionals/wincher-conditional.php
@@ -5,14 +5,4 @@ namespace Yoast\WP\SEO\Conditionals;
 /**
  * Conditional for the Wincher integration.
  */
-class Wincher_Conditional extends Non_Multisite_Conditional {
-
-	/**
-	 * Override is_met to also make sure this isn't a multisite installation.
-	 *
-	 * @return bool
-	 */
-	public function is_met() {
-		return parent::is_met();
-	}
-}
+class Wincher_Conditional extends Non_Multisite_Conditional {}
