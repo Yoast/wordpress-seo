c439e0349a259570a5482131c9cc171df4828e2b
FAMarfuaty <aida@yoast.com>
2021-12-24 12:50:37 +0100

    Add a helper to sanitize line break tag

---
+++ packages/yoastseo/src/languageProcessing/helpers/sanitize/sanitizeLineBreakTag.js
@@ -0,0 +1,11 @@
+/**
+ * Replaces line break tag that contains an attribute with paragraph tag.
+ * Line break tags with attribute aren't properly sanitized using wordpress `autop` function.
+ *
+ * @param {string} text The text to sanitize.
+ *
+ * @returns {string} The sanitized text.
+ */
+export default function( text ) {
+	return text.replace( /<br.*?>\s*<br.*?>/gm, "</p><p>" );
+}
