d0401dec2174fc3556f5fbc1aafc2a2da88c8275
Marijn Bent <marijn@wordproof.com>
2021-12-25 14:13:30 +0700

    add wpseo data to editor pages

--- src/integrations/third-party/wordproof.php
+++ src/integrations/third-party/wordproof.php
@@ -2,6 +2,7 @@
 
 namespace Yoast\WP\SEO\Integrations\Third_Party;
 
+use WordProof\SDK\Support\Authentication;
 use WordProof\SDK\WordPressSDK;
 use Yoast\WP\SEO\Conditionals\Third_Party\WordProof_Core_Inactive_Conditional;
 use Yoast\WP\SEO\Helpers\Options_Helper;
@@ -56,9 +57,32 @@ class WordProof implements Integration_Interface {
 	 */
 	public function register_hooks() {
 		\add_action( 'plugins_loaded', [ $this, 'init' ] );
+		\add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_assets' ] );
 	}
 
 	public function init() {
 		$this->SDK->certificate();
+	}
+
+	public function enqueue_assets() {
+		global $pagenow;
+
+		if ( ! \WPSEO_Metabox::is_post_edit( $pagenow ) ) {
+			return;
+		}
+
+		$asset_manager = new \WPSEO_Admin_Asset_Manager();
+
+		$data = [
+			'data' => [
+				'is_authenticated' => Authentication::isAuthenticated(),
+				'popup_redirect_authentication_url' => \admin_url('admin.php?page=wordproof-redirect-authenticate'),
+				'popup_redirect_settings_url' => \admin_url('admin.php?page=wordproof-redirect-settings'),
+				'rest_is_authenticated_url' => get_rest_url(null, '')
+			]
+		];
+
+		$asset_manager->localize_script( 'post-edit', 'wpseoWordProof', $data );
+		$asset_manager->localize_script( 'post-edit-classic', 'wpseoWordProof', $data );
 	}
 }
