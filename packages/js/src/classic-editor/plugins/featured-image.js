d1bd80ffdcaa36e830cf4f643e684d62a83749e2
Hans-Christiaan <hans-christiaan@yoast.com>
2022-01-03 16:51:37 +0100

    Simplified featured image plugin now that the featured image can be retrieved from the SEO store.

--- packages/js/src/classic-editor/plugins/featured-image.js
+++ packages/js/src/classic-editor/plugins/featured-image.js
@@ -1,47 +1,6 @@
 import { addFilter } from "@wordpress/hooks";
+import { select } from "@wordpress/data";
 import { SEO_STORE_NAME } from "@yoast/seo-integration";
-import { dispatch } from "@wordpress/data";
-
-/**
- * Creates image HTML from the given WordPress media frame.
- *
- * @param {Object} mediaFrame The WordPress media frame.
- *
- * @returns {string} The image HTML.
- */
-function getSelectedImage( mediaFrame ) {
-	if ( ! ( mediaFrame && mediaFrame.state() ) ) {
-		return "";
-	}
-	const selectedImage = mediaFrame.state().get( "selection" ).first();
-
-	const src = selectedImage.get( "url" );
-	const alt = selectedImage.get( "alt" );
-	const width = selectedImage.get( "width" );
-	const height = selectedImage.get( "height" );
-
-	return `<img src="${ src }" alt="${ alt }" width="${ width }" height="${ height }"/>`;
-}
-
-/**
- * Gets the featured image that is currently set within the editor.
- *
- * @returns {string} The featured image that is currently set, as an HTML string.
- */
-function getCurrentFeaturedImage() {
-	const featuredImageElement = document.querySelector( "#set-post-thumbnail > img" );
-
-	if ( ! featuredImageElement ) {
-		return "";
-	}
-
-	const src = featuredImageElement.getAttribute( "src" );
-	const alt = featuredImageElement.getAttribute( "alt" );
-	const width = featuredImageElement.getAttribute( "width" );
-	const height = featuredImageElement.getAttribute( "height" );
-
-	return `<img src="${ src }" alt="${ alt }" width="${ width }" height="${ height }"/>`;
-}
 
 /**
  * Initializes the featured image plugin.
@@ -49,32 +8,6 @@ function getCurrentFeaturedImage() {
  * @returns {void}
  */
 function initFeaturedImagePlugin() {
-	let featuredImageHtml = "";
-
-	const { analyze } = dispatch( SEO_STORE_NAME );
-
-	const frame = window.wp.media.featuredImage.frame();
-
-	// Set the featured image currently selected in the editor.
-	featuredImageHtml = getCurrentFeaturedImage();
-
-	// Change the featured image when one is selected in the editor.
-	frame.on( "select", () => {
-		featuredImageHtml = getSelectedImage( frame );
-		// Trigger a new analysis.
-		analyze();
-	} );
-
-	// Remove the featured image when it is removed in the editor.
-	const removeFeaturedImageLink = document.getElementById( "remove-post-thumbnail" );
-	if ( removeFeaturedImageLink ) {
-		removeFeaturedImageLink.addEventListener( "click", () => {
-			featuredImageHtml = "";
-			// Trigger a new analysis.
-			analyze();
-		} );
-	}
-
 	/**
 	 * Adds the featured image to the paper.
 	 *
@@ -83,7 +16,17 @@ function initFeaturedImagePlugin() {
 	 * @returns {Object} The paper with the featured image added.
 	 */
 	function addFeaturedImageToPaperContents( paper ) {
+		const { src, alt, width, height } = select( SEO_STORE_NAME ).selectFeaturedImage();
+
+		if ( ! src ) {
+			// Do not add the image to the paper's content if no featured image is set.
+			return paper;
+		}
+
+		const featuredImageHtml = `<img src="${ src }" alt="${ alt }" width="${ width }" height="${ height }"/>`;
+
 		paper.content += featuredImageHtml;
+
 		return paper;
 	}
 
