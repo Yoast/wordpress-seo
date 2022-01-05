00a108575dc555ffeb2f4f6c896a0b2abad7cd6c
Marijn Bent <marijn@wordproof.com>
2022-01-05 22:34:56 +0700

    use wordproof defined as conditional

--- src/conditionals/third-party/wordproof-core-inactive.php
+++ src/conditionals/third-party/wordproof-core-inactive.php
@@ -10,18 +10,11 @@ use Yoast\WP\SEO\Conditionals\Conditional;
 class WordProof_Core_Inactive_Conditional implements Conditional {
 
 	/**
-	 * Path to the WordProof Timestamp plugin file.
-	 *
-	 * @internal
-	 */
-	const PATH_TO_WORDPROOF_TIMESTAMP_PLUGIN_FILE = 'wordproof-timestamp/wordproof-timestamp.php';
-
-	/**
 	 * Returns whether or not the WordProof Timestamp plugin is active.
 	 *
 	 * @return bool Whether or not the WordProof Timestamp plugin is active.
 	 */
 	public function is_met() {
-		return !\is_plugin_active( self::PATH_TO_WORDPROOF_TIMESTAMP_PLUGIN_FILE );
+		return !\defined( 'WORDPROOF_VERSION' );
 	}
 }
