96ece94b9ce59e31b2c0f6ac95efd0ff5f9e0f59
FAMarfuaty <aida@yoast.com>
2022-01-07 12:24:27 +0100

    Remove unnecessary double quotes array

--- packages/yoastseo/src/languageProcessing/helpers/match/processExactMatchRequest.js
+++ packages/yoastseo/src/languageProcessing/helpers/match/processExactMatchRequest.js
@@ -6,11 +6,10 @@
  * @returns {Object} Whether the exact match functionality is requested and the keyword stripped from double quotes.
  */
 export default function processExactMatchRequest( keyphrase ) {
-	const doubleQuotes = [ "“", "”", "〝", "〞", "〟", "‟", "„", "\"", "\u300c", "\u300d", "\u300e", "\u300f" ];
 	const exactMatchRequest = { exactMatchRequested: false, keyphrase: keyphrase };
 
 	// Check if only exact match processing is requested by the user. If so, strip the quotation marks from the keyphrase.
-	if ( doubleQuotes.includes( keyphrase[ 0 ] ) && doubleQuotes.includes( keyphrase[ keyphrase.length - 1 ] ) ) {
+	if ( keyphrase[ 0 ] === "\"" && keyphrase[ keyphrase.length - 1 ] === "\"" ) {
 		exactMatchRequest.keyphrase = keyphrase.substring( 1, keyphrase.length - 1 );
 		exactMatchRequest.exactMatchRequested = true;
 	}
