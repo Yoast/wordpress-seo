ecb33a161c26a0ce7aa57406813d9395ad58a98e
FAMarfuaty <aida@yoast.com>
2022-01-07 10:01:04 +0100

    Add unit tests

--- packages/yoastseo/spec/languageProcessing/helpers/match/processExactMatchRequestSpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/match/processExactMatchRequestSpec.js
@@ -3,18 +3,30 @@ import processExactMatchRequest from "../../../../src/languageProcessing/helpers
 describe( "a test for processing exact match request for a keyphrase", function() {
 	it( "returns the result for when the keyphrase is not enclosed in double quotes", function() {
 		const keyphrase = "keyphrase";
-		const doubleQuotes = [ "“", "”", "〝", "〞", "〟", "‟", "„", "\"" ];
-		expect( processExactMatchRequest( keyphrase, doubleQuotes ) ).toEqual( {
+		expect( processExactMatchRequest( keyphrase ) ).toEqual( {
 			exactMatchRequested: false,
 			keyphrase: "keyphrase",
 		} );
 	} );
 	it( "returns the result for when the keyphrase is enclosed in double quotes", function() {
 		const keyphrase = "“keyphrase”";
-		const doubleQuotes = [ "“", "”", "〝", "〞", "〟", "‟", "„", "\"" ];
-		expect( processExactMatchRequest( keyphrase, doubleQuotes ) ).toEqual( {
+		expect( processExactMatchRequest( keyphrase ) ).toEqual( {
 			exactMatchRequested: true,
 			keyphrase: "keyphrase",
+		} );
+	} );
+	it( "returns the result for when the keyphrase is enclosed in Japanese double quotes", function() {
+		const keyphrase = "「小さい花の刺繍」";
+		expect( processExactMatchRequest( keyphrase ) ).toEqual( {
+			exactMatchRequested: true,
+			keyphrase: "小さい花の刺繍",
+		} );
+	} );
+	it( "returns the result for when the Japanese keyphrase is enclosed in latin double quotes", function() {
+		const keyphrase = "\"小さい花の刺繍\"";
+		expect( processExactMatchRequest( keyphrase ) ).toEqual( {
+			exactMatchRequested: true,
+			keyphrase: "小さい花の刺繍",
 		} );
 	} );
 } );
