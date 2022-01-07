15f419651bd2a17a49058d525be37174ad6fd374
FAMarfuaty <aida@yoast.com>
2022-01-07 12:23:54 +0100

    Add japanese double quotes to the array

--- packages/yoastseo/src/languageProcessing/helpers/sanitize/quotes.js
+++ packages/yoastseo/src/languageProcessing/helpers/sanitize/quotes.js
@@ -15,7 +15,7 @@ function normalizeSingleQuotes( text ) {
  * @returns {string} The normalized text.
  */
 function normalizeDoubleQuotes( text ) {
-	return text.replace( /[“”〝〞〟‟„]/g, "\"" );
+	return text.replace( /[“”〝〞〟‟„『』]/g, "\"" );
 }
 
 /**
