f1878bb8f204d51e34fa36615ae5c1063bfd1d2c
Diede Exterkate <diede@yoast.com>
2022-01-10 14:22:44 +0100

    Consider authors to be publicly viewable
    The author visibility hangs on a couple of global facors, which can't
    be tracked in the indexable model.
    Using the number_of_publicly_viewable posts causes problems in combination with
    the noindex-author-noposts-wpseo setting set to false in the query_where_noindex
    query, because this query always needs to check for visibility.

--- src/builders/indexable-author-builder.php
+++ src/builders/indexable-author-builder.php
@@ -88,14 +88,13 @@ class Indexable_Author_Builder {
 		$indexable->is_robots_noimageindex = null;
 		$indexable->is_robots_nosnippet    = null;
 		$indexable->blog_id                = \get_current_blog_id();
+		$indexable->is_publicly_viewable   = true;
 		$indexable->set_deprecated_property( 'is_public', ( $indexable->is_robots_noindex ) ? false : null );
 
 		$this->reset_social_images( $indexable );
 		$this->handle_social_images( $indexable );
 
 		$indexable = $this->set_aggregate_values( $indexable );
-
-		$indexable->is_publicly_viewable = $indexable->number_of_publicly_viewable_posts > 0;
 
 		$indexable->version = $this->version;
 
