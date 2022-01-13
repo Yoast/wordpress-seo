7e90e7004027d29cf508e843f677af49d33c504c
YoastBot CI <yoastbot-ci@yoast.com>
2022-01-13 13:29:31 +0000

    Bump version to 18.0-RC3

--- wp-seo.php
+++ wp-seo.php
@@ -8,7 +8,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast SEO
- * Version:     18.0-RC2
+ * Version:     18.0-RC3
  * Plugin URI:  https://yoa.st/1uj
  * Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
  * Author:      Team Yoast
