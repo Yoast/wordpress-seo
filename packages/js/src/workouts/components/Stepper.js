c3c892df81466a56d59ca974fc247997208f5add
Karlijn Bok <karlijn@yoast.com>
2022-01-17 14:03:42 +0100

    Combine separate components for statuses into one

--- packages/js/src/workouts/components/Stepper.js
+++ packages/js/src/workouts/components/Stepper.js
@@ -66,6 +66,32 @@ StepButtons.defaultProps = {
 	finishStepper: () => {},
 };
 
+const classnames = {
+	complete: {
+		line: "yst-top-4 yst-bg-primary-500",
+		bullet: {
+			border: "yst-bg-primary-500 yst-group-hover:bg-primary-700",
+			content: "",
+		},
+		name: "",
+	},
+	current: {
+		line: "yst-top-8 yst-bg-gray-300",
+		bullet: {
+			border: "yst-bg-white yst-border-2 yst-border-primary-500",
+			content: "yst-bg-primary-500",
+		},
+		name: "yst-text-primary-500",
+	},
+	upcoming: {
+		line: "yst-top-4 yst-bg-gray-300",
+		bullet: {
+			border: "yst-bg-white yst-border-2 yst-border-gray-300 yst-group-hover:border-gray-400",
+			content: "yst-bg-transparent yst-group-hover:bg-gray-300",
+		},
+		name: "yst-text-gray-500",
+	},
+};
 /**
  * Example stepper.
  *
@@ -76,87 +102,41 @@ export default function Stepper( { steps, setActiveStep, saveStep, finishStepper
 		<ol className="yst-overflow-hidden">
 			{ /* eslint-disable-next-line complexity */ }
 			{ steps.map( ( step, stepIdx ) => (
-				 <li key={ step.name } className={ stepIdx === steps.length - 1 ? "" : "yst-pb-10", "yst-relative" }>
-					{ ( step.status === "complete" ) &&
-						<Fragment>
-							{ stepIdx !== steps.length - 1 &&
-								<div
-									className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-4 yst-left-4 yst-w-0.5 yst-h-full yst-bg-primary-500"
-									aria-hidden="true"
-								/>
-							}
-							<div className="yst-relative yst-flex yst-items-start yst-group">
-								<span className="yst-h-9 yst-flex yst-items-center">
-									{ /* eslint-disable-next-line max-len */ }
-									<span className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-primary-500 yst-rounded-full yst-group-hover:bg-primary-700">
-										<CheckIcon className="yst-w-5 yst-h-5 yst-text-white" aria-hidden="true" />
-									</span>
+				 <li key={ step.name } className={ ( stepIdx === steps.length - 1 ? "" : "yst-pb-10" ) + " yst-relative" }>
+					 <Fragment>
+						 <div
+							 className={ ( stepIdx === steps.length - 1 ? "hidden " : "" ) +
+							 "yst--ml-px yst-absolute yst-mt-0.5 yst-left-4 yst-w-0.5 yst-h-full " + classnames[ step.status ].line }
+							 aria-hidden="true"
+						 />
+						 <div className="yst-relative yst-flex yst-items-start yst-group" aria-current={ step.status === "current" ? "step" : null }>
+							 <span className="yst-h-9 yst-flex yst-items-center" aria-hidden={ step.status === "current" ? "true" : null }>
+								 { /* eslint-disable-next-line max-len */ }
+								 <span className={ "yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-rounded-full " + classnames[ step.status ].bullet.border }>
+									{ ( step.status === "complete" )
+										? <CheckIcon className="yst-w-5 yst-h-5 yst-text-white" aria-hidden="true" />
+										: <span className={ "yst-h-2.5 yst-w-2.5 yst-rounded-full " + classnames[ step.status ].bullet.content } />
+									}
 								</span>
-								<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col yst-self-center">
-									<span className="yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase">{ step.name }</span>
-									{ step.description && <span className="yst-text-sm yst-text-gray-500">{ step.description }</span> }
+							 </span>
+							 <span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col yst-self-center">
+								<span className={ "yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase " + classnames[ step.status ].name }>
+									{ step.name }
 								</span>
-							</div>
-						</Fragment>
-					}
-					{ ( step.status === "current" ) &&
-						<Fragment>
-							{ stepIdx === steps.length - 1 ? null : (
-								<div
-									className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-8 yst-left-4 yst-w-0.5 yst-h-full yst-bg-gray-300"
-									aria-hidden="true"
-								/>
-							) }
-							<div className="yst-relative yst-flex yst-items-start yst-group" aria-current="step">
-								<span className="yst-h-9 yst-flex yst-items-center" aria-hidden="true">
-									{ /* eslint-disable-next-line max-len */ }
-									<span className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-white yst-border-2 yst-border-primary-500 yst-rounded-full">
-										<span className="yst-h-2.5 yst-w-2.5 yst-bg-primary-500 yst-rounded-full" />
-									</span>
-								</span>
-								<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col yst-self-center">
-									<span className="yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase yst-text-primary-500">
-										{ step.name }
-									</span>
-									{ step.description && <span className="yst-text-sm yst-text-gray-500">{ step.description }</span> }
-								</span>
-							</div>
-							<div className="yst-ml-12 yst-mb-8 yst-mt-4">
-								{ step.component }
-								<StepButtons
-									stepIdx={ stepIdx }
-									lastIndex={ steps.length - 1 }
-									setActiveStep={ setActiveStep }
-									finishStepper={ finishStepper }
-									saveStep={ saveStep }
-								/>
-							</div>
-						</Fragment>
-					}
-					{ ( step.status !== "complete" && step.status !== "current" ) &&
-						<Fragment>
-							{ ( stepIdx !== steps.length - 1 ) &&
-								<div
-									className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-4 yst-left-4 yst-w-0.5 yst-h-full yst-bg-gray-300"
-									aria-hidden="true"
-								/>
-							}
-							<div className="yst-relative yst-flex yst-items-start yst-group">
-								<span className="yst-h-9 yst-flex yst-items-center" aria-hidden="true">
-									{ /* eslint-disable-next-line max-len */ }
-									<span className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-white yst-border-2 yst-border-gray-300 yst-rounded-full yst-group-hover:border-gray-400">
-										<span className="yst-h-2.5 yst-w-2.5 yst-bg-transparent yst-rounded-full yst-group-hover:bg-gray-300" />
-									</span>
-								</span>
-								<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col yst-self-center">
-									<span className="yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase yst-text-gray-500">
-										{ step.name }
-									</span>
-									{ step.description && <span className="yst-text-sm yst-text-gray-500">{ step.description }</span> }
-								</span>
-							</div>
-						</Fragment>
-					}
+								{ step.description && <span className="yst-text-sm yst-text-gray-500">{ step.description }</span> }
+							</span>
+						 </div>
+						<div className={ ( step.status === "current" ? "" : "hidden " ) + "yst-ml-12 yst-mb-8 yst-mt-4" }>
+							{ step.component }
+							<StepButtons
+								stepIdx={ stepIdx }
+								lastIndex={ steps.length - 1 }
+								setActiveStep={ setActiveStep }
+								finishStepper={ finishStepper }
+								saveStep={ saveStep }
+							/>
+						</div>
+					</Fragment>
 				</li>
 			) ) }
 		</ol>
