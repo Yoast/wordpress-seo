5d08bdac621d53f7f6852db34c9b0e9c63bd9376
Karlijn Bok <karlijn@yoast.com>
2022-01-17 14:46:18 +0100

    Add tailwind step component

--- packages/js/src/workouts/components/Stepper.js
+++ packages/js/src/workouts/components/Stepper.js
@@ -92,51 +92,100 @@ const classnames = {
 		name: "yst-text-gray-500",
 	},
 };
+
 /**
- * Example stepper.
+ * The (Tailwind) Step component
  *
- * @returns {WPElement} The example stepper.
+ * @param {Object} props The props.
+ *
+ * @returns {WPElement} The Step component.
+ */
+// eslint-disable-next-line complexity, require-jsdoc
+function TailwindStep( { step, stepIdx, lastStepIdx, setActiveStep, saveStep, finishStepper  } ) {
+	return (
+		<Fragment>
+			{ /* Line. */ }
+			<div
+				className={ ( stepIdx === lastStepIdx ? "hidden " : "" ) +
+				"yst--ml-px yst-absolute yst-mt-0.5 yst-left-4 yst-w-0.5 yst-h-full " + classnames[ step.status ].line }
+				aria-hidden="true"
+			/>
+			<div className="yst-relative yst-flex yst-items-start yst-group" aria-current={ step.status === "current" ? "step" : null }>
+				{ /* Bullet. */ }
+				<span className="yst-h-9 yst-flex yst-items-center" aria-hidden={ step.status === "current" ? "true" : null }>
+					{ /* eslint-disable-next-line max-len */ }
+					<span
+						className={ "yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-rounded-full " +
+						classnames[ step.status ].bullet.border }
+					>
+						{ ( step.status === "complete" )
+							? <CheckIcon className="yst-w-5 yst-h-5 yst-text-white" aria-hidden="true" />
+							: <span className={ "yst-h-2.5 yst-w-2.5 yst-rounded-full " + classnames[ step.status ].bullet.content } />
+						}
+					</span>
+				</span>
+				{ /* Name and description. */ }
+				<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col yst-self-center">
+					<span className={ "yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase " + classnames[ step.status ].name }>
+						{ step.name }
+					</span>
+					{ step.description && <span className="yst-text-sm yst-text-gray-500">{ step.description }</span> }
+				</span>
+			</div>
+			{ /* Child component and buttons. */ }
+			<div className={ ( step.status === "current" ? "" : "hidden " ) + "yst-ml-12 yst-mb-8 yst-mt-4" }>
+				{ step.component }
+				<StepButtons
+					stepIdx={ stepIdx }
+					lastIndex={ lastStepIdx }
+					setActiveStep={ setActiveStep }
+					finishStepper={ finishStepper }
+					saveStep={ saveStep }
+				/>
+			</div>
+		</Fragment>
+	);
+}
+
+TailwindStep.propTypes = {
+	step: PropTypes.objectOf( PropTypes.shape( {
+		name: PropTypes.string.isRequired,
+		description: PropTypes.string,
+		component: PropTypes.element.isRequired,
+		status: PropTypes.oneOf( [ "complete", "current", "upcoming" ] ).isRequired,
+	} ) ).isRequired,
+	stepIdx: PropTypes.number.isRequired,
+	lastStepIdx: PropTypes.number.isRequired,
+	setActiveStep: PropTypes.func.isRequired,
+	saveStep: PropTypes.func,
+	finishStepper: PropTypes.func,
+};
+
+TailwindStep.defaultProps = {
+	saveStep: () => {},
+	finishStepper: () => {},
+};
+
+/**
+ * The Tailwind Stepper component.
+ *
+ * @param {Object} props The props.
+ *
+ * @returns {WPElement} The Stepper component.
  */
 export default function Stepper( { steps, setActiveStep, saveStep, finishStepper } ) {
 	return (
 		<ol className="yst-overflow-hidden">
-			{ /* eslint-disable-next-line complexity */ }
 			{ steps.map( ( step, stepIdx ) => (
 				 <li key={ step.name } className={ ( stepIdx === steps.length - 1 ? "" : "yst-pb-10" ) + " yst-relative" }>
-					 <Fragment>
-						 <div
-							 className={ ( stepIdx === steps.length - 1 ? "hidden " : "" ) +
-							 "yst--ml-px yst-absolute yst-mt-0.5 yst-left-4 yst-w-0.5 yst-h-full " + classnames[ step.status ].line }
-							 aria-hidden="true"
-						 />
-						 <div className="yst-relative yst-flex yst-items-start yst-group" aria-current={ step.status === "current" ? "step" : null }>
-							 <span className="yst-h-9 yst-flex yst-items-center" aria-hidden={ step.status === "current" ? "true" : null }>
-								 { /* eslint-disable-next-line max-len */ }
-								 <span className={ "yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-rounded-full " + classnames[ step.status ].bullet.border }>
-									{ ( step.status === "complete" )
-										? <CheckIcon className="yst-w-5 yst-h-5 yst-text-white" aria-hidden="true" />
-										: <span className={ "yst-h-2.5 yst-w-2.5 yst-rounded-full " + classnames[ step.status ].bullet.content } />
-									}
-								</span>
-							 </span>
-							 <span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col yst-self-center">
-								<span className={ "yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase " + classnames[ step.status ].name }>
-									{ step.name }
-								</span>
-								{ step.description && <span className="yst-text-sm yst-text-gray-500">{ step.description }</span> }
-							</span>
-						 </div>
-						<div className={ ( step.status === "current" ? "" : "hidden " ) + "yst-ml-12 yst-mb-8 yst-mt-4" }>
-							{ step.component }
-							<StepButtons
-								stepIdx={ stepIdx }
-								lastIndex={ steps.length - 1 }
-								setActiveStep={ setActiveStep }
-								finishStepper={ finishStepper }
-								saveStep={ saveStep }
-							/>
-						</div>
-					</Fragment>
+					 <TailwindStep
+						 step={ step }
+						 stepIdx={ stepIdx }
+						 lastStepIdx={ steps.length - 1 }
+						 setActiveStep={ setActiveStep }
+						 saveStep={ saveStep }
+						 finishStepper={ finishStepper }
+					 />
 				</li>
 			) ) }
 		</ol>
