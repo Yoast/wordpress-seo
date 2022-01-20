dc0df4642d7e2505f0b03ba288121df9e6bd122f
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-01-20 15:22:34 +0200

    Improve helper docblock

--- src/helpers/utils-helper.php
+++ src/helpers/utils-helper.php
@@ -12,9 +12,11 @@ class Utils_Helper {
 	/**
 	 * Emulate the WP native sanitize_text_field function in a %%variable%% safe way.
 	 *
+	 * @codeCoverageIgnore We have to write test when this method contains own code.
+	 *
 	 * @param string $value String value to sanitize.
 	 *
-	 * @return string
+	 * @return string The sanitized string.
 	 */
 	public function sanitize_text_field( $value ) {
 		return WPSEO_Utils::sanitize_text_field( $value );
