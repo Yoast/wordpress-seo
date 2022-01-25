19f4ed308f0cc3566667a7b716feaa80831232f5
Diede Exterkate <diede@yoast.com>
2022-01-25 15:51:25 +0100

    Use the most recent lastmod date of a sitemap entry in the sitemap index for authors

--- inc/sitemaps/class-author-sitemap-provider.php
+++ inc/sitemaps/class-author-sitemap-provider.php
@@ -66,54 +66,18 @@ class WPSEO_Author_Sitemap_Provider implements WPSEO_Sitemap_Provider {
 			return [];
 		}
 
-		$noindex_authors_with_no_posts = WPSEO_Options::get( 'noindex-author-noposts-wpseo' );
-		$query                         = $this->repository
-			->query_where_noindex( false, 'user', null, $noindex_authors_with_no_posts )
-			->select( 'id' )
-			->order_by_asc( 'object_last_modified' );
-
-		$users_to_exclude = $this->exclude_users();
-		if ( is_array( $users_to_exclude ) && count( $users_to_exclude ) > 0 ) {
-			$query->where_not_in( 'object_id', $users_to_exclude );
-		}
-
-		$table_name = Model::get_table_name( 'Indexable' );
-		// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared -- Query is prepared by our ORM.
-		$raw_query = $wpdb->prepare( $query->get_sql(), $query->get_values() );
-
-		// phpcs:ignore WordPress.DB.DirectDatabaseQuery -- Complex query is not possible without a direct query.
-		$last_modified_per_page = $wpdb->get_col(
-			// phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared -- Variables are secure.
-			$wpdb->prepare(
-				// This query pulls only every Nth last_modified from the database.
-				"
-					SELECT i.object_last_modified
-					FROM $table_name AS i
-					INNER JOIN (
-						SELECT id
-						FROM (
-							SELECT @row:=@row+1 AS rownum, id
-							FROM ( $raw_query ) AS sorted, ( SELECT @row:=-1 ) AS init
-						) AS ranked
-						WHERE rownum MOD %d = 0
-					) AS subset
-					ON subset.id = i.id
-				",
-				$max_entries
-			)
-			// phpcs:enable
-		);
+		$last_object_per_page = $this->get_last_object_per_page( $max_entries );
 
 		$page = 1;
-		if ( count( $last_modified_per_page ) === 1 ) {
+		if ( count( $last_object_per_page ) === 1 ) {
 			$page = '';
 		}
 
 		$index_links = [];
-		foreach ( $last_modified_per_page as $last_modified ) {
+		foreach ( $last_object_per_page as $object ) {
 			$index_links[] = [
 				'loc'     => WPSEO_Sitemaps_Router::get_base_url( 'author-sitemap' . $page . '.xml' ),
-				'lastmod' => $last_modified,
+				'lastmod' => $object->object_last_modified,
 			];
 
 			if ( is_int( $page ) ) {
@@ -164,6 +128,79 @@ class WPSEO_Author_Sitemap_Provider implements WPSEO_Sitemap_Provider {
 		}
 
 		return $this->xml_sitemap_helper->convert_indexables_to_sitemap_links( $indexables, 'user' );
+	}
+
+	/**
+	 * Gets a list of sitemap objects that represent the last entry on an individual sitemap page.
+	 *
+	 * @param int $max_entries_per_page The maximum allowed number of objects on a single page.
+	 *
+	 * @return array A list of sitemap objects that represent the last entry on an individual sitemap page.
+	 */
+	protected function get_last_object_per_page( $max_entries_per_page ) {
+		global $wpdb;
+
+		$noindex_authors_with_no_posts = WPSEO_Options::get( 'noindex-author-noposts-wpseo' );
+
+		$query = $this->repository
+			->query_where_noindex( false, 'user', null, $noindex_authors_with_no_posts )
+			->select( 'id' )
+			->order_by_asc( 'object_last_modified' );
+
+		$users_to_exclude = $this->exclude_users();
+		if ( is_array( $users_to_exclude ) && count( $users_to_exclude ) > 0 ) {
+			$query->where_not_in( 'object_id', $users_to_exclude );
+		}
+
+		$table_name = Model::get_table_name( 'Indexable' );
+		// phpcs:ignore WordPress.DB.PreparedSQL.NotPrepared -- Query is prepared by our ORM.
+		$raw_query = $wpdb->prepare( $query->get_sql(), $query->get_values() );
+
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery -- Complex query is not possible without a direct query.
+		$last_object_per_page = $wpdb->get_results(
+		// phpcs:disable WordPress.DB.PreparedSQL.InterpolatedNotPrepared -- Variables are secure.
+			$wpdb->prepare(
+			// This query pulls only every Nth last_modified from the database.
+				"
+					SELECT i.object_last_modified
+					FROM $table_name AS i
+					INNER JOIN (
+						SELECT id
+						FROM (
+							SELECT @row:=@row+1 AS rownum, id
+							FROM ( $raw_query ) AS sorted, ( SELECT @row:=-1 ) AS init
+						) AS ranked
+						WHERE rownum MOD %d = (%d-1) -- Get the last entry of all full pages. 
+					) AS subset
+					ON subset.id = i.id
+				",
+				$max_entries_per_page,
+				$max_entries_per_page
+			)
+		// phpcs:enable
+		);
+
+		// Ensure that author pages with fewer objects than the max_entries get a link.
+		$aggregate_query = $this->repository
+			->query_where_noindex( false, 'user', null, $noindex_authors_with_no_posts )
+			->select_expr( 'MAX(`object_last_modified`)', 'most_recently_modified' )
+			->select_expr( 'COUNT(`id`)', 'number_of_authors' )
+			->having_gt( 'number_of_authors', 0 );
+
+		if ( is_array( $users_to_exclude ) && count( $users_to_exclude ) > 0 ) {
+			$aggregate_query->where_not_in( 'object_id', $users_to_exclude );
+		}
+
+		// We must use find_array. Other functions will try to map the aggregates to the Indexable model.
+		$aggregates = (array) $aggregate_query->find_array();
+
+		if ( isset( $aggregates[0]['number_of_authors'] ) && ( $aggregates[0]['number_of_authors'] % $max_entries_per_page !== 0 ) ) {
+			$last_object_per_page[] = (object) [
+				'object_last_modified' => $aggregates[0]['most_recently_modified'],
+			];
+		}
+
+		return $last_object_per_page;
 	}
 
 	/**
