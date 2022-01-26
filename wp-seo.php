5eab860b7e62ad7c2061806551ffa75175fa34bf
IreneStr <irene@yoast.com>
2022-01-26 14:35:17 +0100

    Update copyright year in main plugin file

--- wp-seo.php
+++ wp-seo.php
@@ -3,7 +3,7 @@
  * Yoast SEO Plugin.
  *
  * @package   WPSEO\Main
- * @copyright Copyright (C) 2008-2021, Yoast BV - support@yoast.com
+ * @copyright Copyright (C) 2008-2022, Yoast BV - support@yoast.com
  * @license   http://www.gnu.org/licenses/gpl-3.0.html GNU General Public License, version 3 or higher
  *
  * @wordpress-plugin
