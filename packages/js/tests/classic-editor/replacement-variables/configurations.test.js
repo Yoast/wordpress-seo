d1a0db1e0e38c7c47222d3750f86fdf86de0e34f
FAMarfuaty <aida@yoast.com>
2022-01-28 12:21:15 +0100

    Add more unit test

--- packages/js/tests/classic-editor/replacement-variables/configurations.test.js
+++ packages/js/tests/classic-editor/replacement-variables/configurations.test.js
@@ -1,25 +1,5 @@
 import { primaryCategory } from "../../../src/classic-editor/replacement-variables/configurations";
-
-jest.mock( "@wordpress/data", () => {
-	return {
-		select: () => {
-			return {
-				getPrimaryTaxonomyId: jest.fn().mockReturnValue( 2 ),
-				selectCategories: jest.fn().mockReturnValue( [
-					{
-						id: "1",
-						name: "category 1",
-					},
-					{
-						id: "2",
-						name: "category 2",
-					},
-				] ),
-			};
-		},
-		combineReducers: jest.fn(),
-	};
-} );
+import * as data from "@wordpress/data";
 
 self.wpseoPrimaryCategoryL10n = {
 	taxonomies: {
@@ -29,8 +9,37 @@ self.wpseoPrimaryCategoryL10n = {
 	},
 };
 
+const categories = jest.fn().mockReturnValue( [
+	{
+		id: "1",
+		name: "category 1",
+	},
+	{
+		id: "2",
+		name: "category 2",
+	},
+] );
+
 describe( "a test for getting the replacement of the variables in classic editor", () => {
-	it( "should return the replacement for primary category variable", () => {
+	it( "should return the replacement for primary category variable when the id from the store is not undefined", () => {
+		jest.spyOn( data, "select" ).mockImplementation( () => {
+			return {
+				getPrimaryTaxonomyId: jest.fn().mockReturnValue( 2 ),
+				selectCategories: categories,
+			};
+		} );
+
 		expect( primaryCategory.getReplacement() ).toEqual( "category 2" );
+	} );
+
+	it( "should return the replacement for primary category variable when the id from the store is undefined", () => {
+		jest.spyOn( data, "select" ).mockImplementation( () => {
+			return {
+				getPrimaryTaxonomyId: jest.fn().mockReturnValue( undefined ),
+				selectCategories: categories,
+			};
+		} );
+
+		expect( primaryCategory.getReplacement() ).toEqual( "category 1" );
 	} );
 } );
