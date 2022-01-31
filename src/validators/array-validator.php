99e5e1f0f8fed26a8b5e1037d69937f3d94db1fb
Igor <35524806+igorschoester@users.noreply.github.com>
2022-01-31 15:28:38 +0100

    Add settings to validate interface

--- src/validators/array-validator.php
+++ src/validators/array-validator.php
@@ -10,11 +10,12 @@ class Array_Validator implements Validator_Interface {
 	/**
 	 * Validates if a value is an array.
 	 *
-	 * @param mixed $value The value to validate.
+	 * @param mixed $value    The value to validate.
+	 * @param array $settings Optional settings.
 	 *
 	 * @return bool Whether the value is an array.
 	 */
-	public function validate( $value ) {
+	public function validate( $value, array $settings = null ) {
 		return \is_array( $value );
 	}
 }
