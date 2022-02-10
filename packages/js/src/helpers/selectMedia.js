be20fb4262f6b40aab20a5292f554d0479c01c7c
Igor <35524806+igorschoester@users.noreply.github.com>
2022-02-10 15:02:46 +0100

    Add fetchAttachment
    Fetch the same image details from attachment ID as from media selection
    So we can fill in the alt text on load too
    
    Note: needed to delay the settings script to have access to the attachment API

--- packages/js/src/helpers/selectMedia.js
+++ packages/js/src/helpers/selectMedia.js
@@ -1,28 +1,60 @@
 /**
+ * Strips an attachment to an object with what we want.
+ *
+ * @param {wp.media.model.Attachment} attachment A WordPress media attachment.
+ *
+ * @returns {Object} Image attributes.
+ */
+const toImage = ( attachment ) => ( {
+	type: attachment.subtype,
+	width: attachment.width,
+	height: attachment.height,
+	url: attachment.url,
+	id: attachment.id,
+	sizes: attachment.sizes,
+	// Use fallbacks: the media title or the filename without extension.
+	alt: attachment.alt || attachment.title || attachment.name,
+} );
+
+/**
  * Function to get the media object and hook the right action dispatchers.
  *
  * @param {Function} onSelect Callback function received from openMedia. Gets object image' as an argument.
  *
- * @returns {void}
+ * @returns {wp.media.view.MediaFrame} A media workflow.
  */
 function getMedia( onSelect ) {
-	let media = null;
-	media = window.wp.media();
+	const media = window.wp.media();
+
 	// Listens for the selection of an image. Then gets the right data and dispatches the data to the store.
 	media.on( "select", () => {
 		const selected = media.state().get( "selection" ).first();
-		const image = {
-			type: selected.attributes.subtype,
-			width: selected.attributes.width,
-			height: selected.attributes.height,
-			url: selected.attributes.url,
-			id: selected.attributes.id,
-			sizes: selected.attributes.sizes,
-		};
-		onSelect( image );
+
+		onSelect( toImage( selected.attributes ) );
 	} );
 
 	return media;
+}
+
+/**
+ * Fetches the attachment via WP media.
+ *
+ * @param {number|string} id The attachment ID to fetch.
+ *
+ * @returns {Promise} The promise of an attachment. Can be rejected.
+ */
+export function fetchAttachment( id ) {
+	return new Promise( ( resolve, reject ) => {
+		if ( ! window.wp.media.attachment ) {
+			reject();
+		}
+
+		window.wp.media.attachment( id ).fetch()
+			.then( attachment => {
+				resolve( toImage( attachment ) );
+			} )
+			.catch( () => reject() );
+	} );
 }
 
 /**
@@ -33,5 +65,5 @@ function getMedia( onSelect ) {
  * @returns {void}
  */
 export function openMedia( onSelect ) {
-	return getMedia( onSelect ).open();
+	getMedia( onSelect ).open();
 }
