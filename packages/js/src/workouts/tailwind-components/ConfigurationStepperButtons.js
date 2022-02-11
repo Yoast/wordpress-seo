a487533b19b830fe1f62c527e55f41303371e6a5
Paolo L. Scala <paolo.scala@gmail.com>
2022-02-11 14:51:34 +0100

    Add EditButton and ConfigurationStepButtons for convenience

--- packages/js/src/workouts/tailwind-components/ConfigurationStepperButtons.js
+++ packages/js/src/workouts/tailwind-components/ConfigurationStepperButtons.js
@@ -1,6 +1,7 @@
+import { __ } from "@wordpress/i18n";
+import { useCallback } from "@wordpress/element";
 import PropTypes from "prop-types";
 import { Step } from "./Stepper";
-import { __ } from "@wordpress/i18n";
 
 /**
  * A ContinueButton that always goes to the next step.
@@ -34,12 +35,43 @@ ContinueButton.defaultProps = {
 };
 
 /**
- * A ContinueButton that always goes to the next step.
+ * A EditButton that opens the current step.
  *
  * @param {Object}   props    The props.
  * @param {NodeList} children The Component children.
  *
- * @returns {WPElement} The ContinueButton, that always goes to the next step.
+ * @returns {WPElement} The EditButton, that always goes to the next step.
+ */
+export function EditButton( { beforeGo, children, additionalClasses, ...restProps } ) {
+	return ( <Step.GoButton
+		className={ `yst-button--secondary yst-button--small ${ additionalClasses }` }
+		destination={ 0 }
+		beforeGo={ beforeGo }
+		{ ...restProps }
+	>
+		{ children }
+	</Step.GoButton> );
+}
+
+EditButton.propTypes = {
+	additionalClasses: PropTypes.string,
+	beforeGo: PropTypes.func,
+	children: PropTypes.node,
+};
+
+EditButton.defaultProps = {
+	additionalClasses: "",
+	beforeGo: null,
+	children: null,
+};
+
+/**
+ * A BackButton that always goes to the previous step.
+ *
+ * @param {Object}   props    The props.
+ * @param {NodeList} children The Component children.
+ *
+ * @returns {WPElement} The BackButton, that always goes to the previous step.
  */
 export function BackButton( { beforeGo, children, additionalClasses, ...restProps } ) {
 	return ( <Step.GoButton
@@ -94,4 +126,37 @@ StepButtons.defaultProps = {
 	continueLabel: __( "Continue", "wordpress-seo" ),
 	beforeBack: null,
 	backLabel: __( "Go Back", "wordpress-seo" ),
+};
+
+/**
+ * A convenience class for the ConfigurationWorkout buttons:
+ * "Save and continue" and "Go back" when the stepper is in progress, "Save changes" when the Stepper has been completed once.
+ *
+ * @param {Object}   props                     The props for the StepButtons.
+ * @param {boolean}  props.stepperFinishedOnce Wether the stepper has been completed once.
+ * @param {function} props.saveFunction        A function to call upon clicking the "Save Changes" button.
+ * @param {string}   props.setEditState        A function to set the edit state of the Stepper.
+ *
+ * @returns {WPElement} The most common stepper buttons: continue and back.
+ */
+export function ConfigurationStepButtons( { stepperFinishedOnce, saveFunction, setEditState } ) {
+	const onSaveClick = useCallback( () => {
+		saveFunction();
+		setEditState( false );
+		return true;
+	} );
+
+	if ( stepperFinishedOnce ) {
+		return <Step.GoButton className="yst-button--primary yst-mt-12" destination="last" beforeGo={ onSaveClick }>
+			{ __( "Save changes", "wordpress-seo" ) }
+		</Step.GoButton>;
+	}
+
+	return <StepButtons beforeContinue={ saveFunction } continueLabel={ __( "Save and continue", "wordpress-seo" ) } />;
+}
+
+ConfigurationStepButtons.propTypes = {
+	stepperFinishedOnce: PropTypes.bool.isRequired,
+	saveFunction: PropTypes.func.isRequired,
+	setEditState: PropTypes.func.isRequired,
 };
