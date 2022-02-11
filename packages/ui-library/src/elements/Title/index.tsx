5d6b1c4c871e89c15b12ec7abb14bfa43c90e6e8
Simon <simon@yoast.com>
2022-02-11 16:47:10 +0100

    Introduce Title stories

--- packages/ui-library/src/elements/Title/index.tsx
+++ packages/ui-library/src/elements/Title/index.tsx
@@ -1,36 +1,46 @@
-import React, { ElementType, FC } from  "react";
+import React, { FC } from "react";
 import cx from "classnames";
 
+export const propertyMap = {
+	size: {
+		"1": "yst-title-1",
+		"2": "yst-title-2",
+		"3": "yst-title-3",
+		"4": "yst-title-4",
+	},
+	element: {
+		h1: 'h1',
+		h2: 'h2',
+		h3: 'h3',
+		h4: 'h4',
+	}
+}
+
 interface TitleProps {
-    as?: ElementType;
-	size?: "1" | "2" | "3" | "4" | "5" | "6";
+	as?: keyof typeof propertyMap.element;
+	size?: keyof typeof propertyMap.size;
 	className?: string;
 	// Handlers
 	onClick?: () => void;
-    [k: string]: unknown;
+	[k: string]: unknown;
 }
 
-const sizeClassMap = {
-	1: "",
-	2: "",
-	3: "",
-	4: "",
-	5: "",
-	6: "",
-}
-
-const Title: FC<TitleProps> = ( {
+const Title: FC<TitleProps> = ({
 	children,
 	as: Component = "h1",
 	size = "1",
 	className = "",
 	...props
-} ) => {
-	const sizeClassName = sizeClassMap[ size ]
+}) => {
+	const sizeClassName = propertyMap.size[ size ]
 	return (
 		<Component
-			className={ cx( sizeClassName, className ) }
-			{ ...props }
+			className={cx(
+				'yst-title',
+				sizeClassName,
+				className
+			)}
+			{...props}
 		>
 			{ children }
 		</Component>
