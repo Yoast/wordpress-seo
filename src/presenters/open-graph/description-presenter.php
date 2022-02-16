34ffbb451e71ac17390c861e812e97d8153e0c92
Jannes <jannes@yoast.com>
2022-02-16 10:33:06 +0100

    Change var name to better differentiate from default description.

--- src/presenters/open-graph/description-presenter.php
+++ src/presenters/open-graph/description-presenter.php
@@ -30,7 +30,7 @@ class Description_Presenter extends Abstract_Indexable_Tag_Presenter {
 	 * @return string The filtered description.
 	 */
 	public function get() {
-		$meta_description = $this->replace_vars( $this->presentation->open_graph_description );
+		$meta_og_description = $this->replace_vars( $this->presentation->open_graph_description );
 
 		/**
 		 * Filter: 'wpseo_opengraph_desc' - Allow changing the Yoast SEO generated Open Graph description.
@@ -39,8 +39,8 @@ class Description_Presenter extends Abstract_Indexable_Tag_Presenter {
 		 *
 		 * @param Indexable_Presentation $presentation The presentation of an indexable.
 		 */
-		$meta_description = \apply_filters( 'wpseo_opengraph_desc', $meta_description, $this->presentation );
-		$meta_description = $this->helpers->string->strip_all_tags( \stripslashes( $meta_description ) );
-		return \trim( $meta_description );
+		$meta_og_description = \apply_filters( 'wpseo_opengraph_desc', $meta_og_description, $this->presentation );
+		$meta_og_description = $this->helpers->string->strip_all_tags( \stripslashes( $meta_og_description ) );
+		return \trim( $meta_og_description );
 	}
 }
