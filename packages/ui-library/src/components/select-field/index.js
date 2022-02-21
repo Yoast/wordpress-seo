ba6aa72980e97cf4dc8d3513c159aad9489c71b5
nolledgeable <nolle@yoast.com>
2022-02-21 22:12:45 +0100

    Add select field component

---
+++ packages/ui-library/src/components/select-field/index.js
@@ -0,0 +1,68 @@
+/* eslint-disable no-undefined */
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+import Label from "../../elements/label";
+import Select from "../../elements/select";
+
+/**
+ * @param {string} id Identifier.
+ * @param {{ value, label }[]} options Options to choose from.
+ * @param {Function} onChange Change callback.
+ * @param {JSX.Element} error Error node.
+ * @param {string} [className] CSS class.
+ * @returns {JSX.Element} SelectField component.
+ */
+const SelectField = ( {
+	children,
+	id,
+	value,
+	options,
+	label,
+	onChange,
+	error,
+	className,
+	...props
+} ) => {
+	return (
+		<div className={ classNames( "yst-select-field", className ) }>
+			{ label && <Label className="yst-select-field__label">{ label }</Label> }
+			<Select
+				id={ id }
+				value={ value }
+				options={ options }
+				onChange={ onChange }
+				isError={ Boolean( error ) }
+				className="yst-select-field__select"
+				{ ...props }
+			/>
+			{ children && <div className="yst-select-field__description">{ children }</div> }
+			{ error && <p className="yst-select-field__error">{ error }</p> }
+		</div>
+	);
+};
+
+SelectField.propTypes = {
+	children: PropTypes.node,
+	id: PropTypes.string.isRequired,
+	name: PropTypes.string.isRequired,
+	value: PropTypes.string,
+	label: PropTypes.node,
+	options: PropTypes.arrayOf( PropTypes.shape( {
+		value: PropTypes.string.isRequired,
+		label: PropTypes.string.isRequired,
+	} ) ).isRequired,
+	onChange: PropTypes.func.isRequired,
+	error: PropTypes.node,
+	className: PropTypes.string,
+};
+
+SelectField.defaultProps = {
+	children: null,
+	value: undefined,
+	label: null,
+	error: null,
+	className: "",
+};
+
+export default SelectField;
