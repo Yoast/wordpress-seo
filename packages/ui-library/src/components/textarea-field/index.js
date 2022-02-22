b9addceaa2c9a55ecbfe16d6c24b2852ea642e63
Igor <35524806+igorschoester@users.noreply.github.com>
2022-02-22 08:50:51 +0100

    Add textarea field component

---
+++ packages/ui-library/src/components/textarea-field/index.js
@@ -0,0 +1,60 @@
+import { ExclamationCircleIcon } from "@heroicons/react/solid";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+import Label from "../../elements/label";
+import Textarea from "../../elements/textarea";
+
+/**
+ * @param {string} id The ID of the input.
+ * @param {function} onChange The input change handler.
+ * @param {JSX.node} [label] A label.
+ * @param {string} [className] The HTML class.
+ * @param {JSX.node} [description] A description.
+ * @param {JSX.node} [error] An error "message".
+ * @param {Object} [props] Any extra properties for the Textarea.
+ * @returns {JSX.Element} The textarea field.
+ */
+const TextareaField = ( {
+	id,
+	label,
+	className,
+	description,
+	error,
+	...props
+} ) => (
+	<div className={ classNames( "yst-textarea-field", className ) }>
+		{ label && <Label className="yst-textarea-field__label" htmlFor={ id }>{ label }</Label> }
+		<div className="yst-relative yst-inline-flex">
+			<Textarea
+				id={ id }
+				className={ classNames(
+					"yst-textarea-field__input",
+					error && "yst-textarea-field__input--error",
+				) }
+				{ ...props }
+			/>
+			{ error && <div className="yst-textarea-field__error-icon">
+				<ExclamationCircleIcon />
+			</div> }
+		</div>
+		{ description && <p className="yst-textarea-field__description">{ description }</p> }
+		{ error && <p className="yst-textarea-field__error">{ error }</p> }
+	</div>
+);
+
+TextareaField.propTypes = {
+	id: PropTypes.string.isRequired,
+	label: PropTypes.node,
+	className: PropTypes.string,
+	description: PropTypes.node,
+	error: PropTypes.node,
+};
+
+TextareaField.defaultProps = {
+	label: null,
+	className: "",
+	description: null,
+	error: null,
+};
+
+export default TextareaField;
---
+++ packages/ui-library/src/components/textarea-field/stories.js
@@ -0,0 +1,50 @@
+import TextareaField from ".";
+
+export default {
+	title: "2. Components/Textarea field",
+	component: TextareaField,
+	argTypes: {
+		label: { control: "text" },
+		description: { control: "text" },
+		error: { control: "text" },
+	},
+	parameters: {
+		docs: {
+			description: {
+				component: "A simple textarea field component.",
+			},
+		},
+	},
+	args: {
+		id: "textarea-field",
+	},
+};
+
+export const Factory = {
+	component: args => <TextareaField { ...args } />,
+	parameters: {
+		controls: { disable: false },
+	},
+};
+
+export const WithLabelAndDescription = {
+	component: Factory.component.bind( {} ),
+	args: {
+		id: "textarea-field-1",
+		label: "Textarea field with a label",
+		description: "Textarea field with a description.",
+	},
+};
+
+export const WithError = {
+	component: Factory.component.bind( {} ),
+	parameters: {
+		controls: { disable: false },
+	},
+	args: {
+		id: "textarea-field-2",
+		label: "Textarea field with a label",
+		description: "Textarea field with a description.",
+		error: "Please enter a valid text.",
+	},
+};
