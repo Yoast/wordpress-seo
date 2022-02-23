fbfde94bbab0b3dba9358efe5bdbd8d7f765b032
FAMarfuaty <aida@yoast.com>
2022-02-23 13:27:31 +0100

    add unit test for tag replacement variable

--- packages/js/tests/classic-editor/helpers/dom.test.js
+++ packages/js/tests/classic-editor/helpers/dom.test.js
@@ -90,3 +90,35 @@ describe( "a test for retrieving categories from the DOM", () => {
 		expect( dom.getPostCategories() ).toEqual( [ ] );
 	} );
 } );
+
+describe( "a test for retrieving tags from the document", () => {
+	const tag1 = document.createElement( "li" );
+	const tag1ChildNode1 = document.createElement( "button" );
+	const tag1ChildNode2 = document.createTextNode( "" );
+	const tag1ChildNode3 = document.createTextNode( "cat food" );
+
+	tag1.appendChild( tag1ChildNode1 );
+	tag1.appendChild( tag1ChildNode2 );
+	tag1.appendChild( tag1ChildNode3 );
+
+	const tag2 = document.createElement( "li" );
+	const tag2ChildNode1 = document.createElement( "button" );
+	const tag2ChildNode2 = document.createTextNode( "" );
+	const tag2ChildNode3 = document.createTextNode( "cat snack" );
+
+	tag2.appendChild( tag2ChildNode1 );
+	tag2.appendChild( tag2ChildNode2 );
+	tag2.appendChild( tag2ChildNode3 );
+
+	const tagsListElement = document.createElement( "ul" );
+	tagsListElement.setAttribute( "class", "tagchecklist" );
+	tagsListElement.appendChild( tag1 );
+	tagsListElement.appendChild( tag2 );
+
+	document.body.appendChild( tagsListElement );
+
+	it( "should return the tags from the post", () => {
+		expect( dom.getTagsList() ).toEqual( [ tag1, tag2 ] );
+		expect( dom.getPostTags() ).toEqual( [ "cat food", "cat snack" ] );
+	} );
+} );
