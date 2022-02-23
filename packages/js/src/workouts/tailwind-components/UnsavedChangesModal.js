6dbcb63f89e1e368a8ae4b7133334a616f3e6255
Paolo L. Scala <paolo.scala@gmail.com>
2022-02-23 10:45:57 +0100

    Refactor component to accept additional parameters

--- packages/js/src/workouts/tailwind-components/UnsavedChangesModal.js
+++ packages/js/src/workouts/tailwind-components/UnsavedChangesModal.js
@@ -9,11 +9,16 @@ import TailwindModal from "./TailwindModal";
  *
  * Works per route, it saves the last location.
  *
- * @param {Object} props The props.
+ * @param {Object} props                    The props.
+ * @param {bool}   props.hasUnsavedChanges  If the modal popup must be shown.
+ * @param {string} props.title              The modal popup title.
+ * @param {string} props.description        The message displayed in the modal popup.
+ * @param {string} props.okButtonLabel      The text label for the "OK" button.
+ * @param {string} props.cancelButtonLabel  The text label for the "Cancel" button.
  *
  * @returns {JSX.Element} The UnsavedChangesModal.
  */
-export default function UnsavedChangesModal( { hasUnsavedChanges } ) {
+export default function UnsavedChangesModal( { hasUnsavedChanges, title, description, okButtonLabel, cancelButtonLabel } ) {
 	const [ modalIsOpen, setModalIsOpen ] = useState( false );
 	const [ targetUrl, setTargetUrl ] = useState( "empty" );
 
@@ -123,11 +128,11 @@ export default function UnsavedChangesModal( { hasUnsavedChanges } ) {
 				</div>
 				<div className="yst-mt-3 yst-text-center sm:yst-mt-0 sm:yst-ml-4 sm:yst-text-left">
 					<TailwindModal.Title as="h3" className="yst-text-lg yst-leading-6 yst-font-medium yst-text-gray-900">
-						{ __( "Unsaved changes", "wordpress-seo" ) }
+						{ __( title, "wordpress-seo" ) }
 					</TailwindModal.Title>
 					<div className="yst-mt-2">
 						<p className="yst-text-sm yst-text-gray-500">
-							{ __( "There are unsaved changes in this step. Leaving means that those changes will be lost. Are you sure you want to leave this page?", "wordpress-seo" ) }
+							{ __( description, "wordpress-seo" ) }
 						</p>
 					</div>
 				</div>
@@ -139,14 +144,14 @@ export default function UnsavedChangesModal( { hasUnsavedChanges } ) {
 					className="yst-button yst-button--danger yst-w-full yst-inline-flex sm:yst-w-auto sm:yst-ml-3"
 					onClick={ continueNavigation }
 				>
-					{ __( "Yes, leave page", "wordpress-seo" ) }
+					{ __( okButtonLabel, "wordpress-seo" ) }
 				</button>
 				<button
 					type="button"
 					className="yst-button yst-button--secondary yst-w-full yst-inline-flex sm:yst-w-auto sm:yst-mt-0"
 					onClick={ closeModal }
 				>
-					{ __( "No, continue editing", "wordpress-seo" ) }
+					{ __( cancelButtonLabel, "wordpress-seo" ) }
 				</button>
 			</div>
 		</TailwindModal>
@@ -155,4 +160,13 @@ export default function UnsavedChangesModal( { hasUnsavedChanges } ) {
 
 UnsavedChangesModal.propTypes = {
 	hasUnsavedChanges: PropTypes.bool.isRequired,
+	title: PropTypes.string.isRequired,
+	description: PropTypes.string.isRequired,
+	okButtonLabel: PropTypes.string,
+	cancelButtonLabel: PropTypes.string,
+};
+
+UnsavedChangesModal.defaultProps = {
+	okButtonLabel: "Ok",
+	cancelButtonLabel: "Cancel",
 };
