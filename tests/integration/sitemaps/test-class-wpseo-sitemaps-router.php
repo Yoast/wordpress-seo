36017f15cece07a87b10aec0f51ba27f64323047
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-02-24 16:32:08 +0200

    Fix result for integration test

--- tests/integration/sitemaps/test-class-wpseo-sitemaps-router.php
+++ tests/integration/sitemaps/test-class-wpseo-sitemaps-router.php
@@ -71,8 +71,8 @@ class WPSEO_Sitemaps_Router_Test extends WPSEO_UnitTestCase {
 		$this->assertNotEquals( 'http://example.org/sitemap.xml', WPSEO_Sitemaps_Router::get_base_url( 'sitemap.xml' ) );
 
 		update_option( 'home', 'example.org' );
-		$this->assertEquals( 'example.org/sitemap.xml', WPSEO_Sitemaps_Router::get_base_url( 'sitemap.xml' ) );
-		$this->assertNotEquals( 'example.org/sitemap.xml', WPSEO_Sitemaps_Router::get_base_url( 'sitemap.xml' ) );
+		$this->assertEquals( 'https://example.org/sitemap.xml', WPSEO_Sitemaps_Router::get_base_url( 'sitemap.xml' ) );
+		$this->assertNotEquals( 'http://example.org/sitemap.xml', WPSEO_Sitemaps_Router::get_base_url( 'sitemap.xml' ) );
 	}
 
 	/**
