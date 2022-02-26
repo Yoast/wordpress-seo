86f77568d6cc2e39d047643a3ce5a474cda544ac
Marijn Bent <marijn@wordproof.com>
2022-02-26 14:41:41 +0700

    add webhook failed string

--- src/config/wordproof-translations.php
+++ src/config/wordproof-translations.php
@@ -12,15 +12,21 @@ class WordProofTranslations implements TranslationsInterface
 		return sprintf(__('You are out of timestamps. Please upgrade your account by opening the %s settings.', 'wordpress-seo'), 'WordProof');
 	}
 
-	public function getTimestampFailedNotice()
+	public function getTimestampSuccessNotice()
 	{
 		/* translators: %s expands to WordProof. */
 		return sprintf(__('%s has successfully timestamped this page.', 'wordpress-seo'), 'WordProof');
 	}
 
-	public function getTimestampSuccessNotice()
+	public function getTimestampFailedNotice()
 	{
 		/* translators: %s expands to WordProof. */
 		return sprintf(__('%1$s failed to timestamp this page. Please check if you\'re correctly authenticated with %1$s and try to save this page again.', 'wordpress-seo'), 'WordProof');
+	}
+
+	public function getWebhookFailedNotice()
+	{
+		/* translators: %s expands to WordProof. */
+		return sprintf(__('The timestamp is not retrieved by your site. Please try again or contact %1$s support.', 'wordpress-seo'), 'WordProof');
 	}
 }
