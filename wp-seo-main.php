1361f4145703ad380196a65b58947f98e5c3edde
YoastBot CI <yoastbot-ci@yoast.com>
2022-03-08 12:01:02 +0000

    Bump version to 18.4-RC1

--- wp-seo-main.php
+++ wp-seo-main.php
@@ -15,7 +15,7 @@ if ( ! function_exists( 'add_filter' ) ) {
  * {@internal Nobody should be able to overrule the real version number as this can cause
  *            serious issues with the options, so no if ( ! defined() ).}}
  */
-define( 'WPSEO_VERSION', '18.3' );
+define( 'WPSEO_VERSION', '18.4-RC1' );
 
 
 if ( ! defined( 'WPSEO_PATH' ) ) {
