73c3ba2a70fe7b4b2f252ccc3fab4748c1bac4fb
Marijn Bent <marijn@wordproof.com>
2022-03-09 17:11:26 +0700

    add non_multisite conditional

--- src/helpers/wordproof-helper.php
+++ src/helpers/wordproof-helper.php
@@ -57,13 +57,20 @@ class WordProof_Helper {
 	/**
 	 * Returns if conditionals are met. If not, the integration should be disabled.
 	 *
-	 * @return bool Returns if the integration should be disabled.
+	 * @param bool $returnConditional If the conditional class name that was unmet should be returned.
+	 *
+	 * @return bool|string Returns if the integration should be disabled.
 	 */
-	public function integration_is_disabled() {
-		$conditionals = [ new WordProof_Plugin_Inactive_Conditional() ];
+	public function integration_is_disabled( $returnConditional = false ) {
+		$conditionals = [ new WordProof_Plugin_Inactive_Conditional(), new Non_Multisite_Conditional() ];
 
 		foreach ( $conditionals as $conditional ) {
 			if ( ! $conditional->is_met() ) {
+
+				if ( $returnConditional === true ) {
+					return (new \ReflectionClass( $conditional ))->getShortName();
+				}
+
 				return true;
 			}
 		}
