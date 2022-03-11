74e606ab55f6636da89834f03ca4ec4015acfff1
YoastBot CI <yoastbot-ci@yoast.com>
2022-03-11 11:37:21 +0000

    Bump version to 18.4-RC3

--- wp-seo-main.php
+++ wp-seo-main.php
@@ -15,7 +15,7 @@ if ( ! function_exists( 'add_filter' ) ) {
  * {@internal Nobody should be able to overrule the real version number as this can cause
  *            serious issues with the options, so no if ( ! defined() ).}}
  */
-define( 'WPSEO_VERSION', '18.4-RC2' );
+define( 'WPSEO_VERSION', '18.4-RC3' );
 
 
 if ( ! defined( 'WPSEO_PATH' ) ) {
@@ -35,7 +35,7 @@ define( 'YOAST_VENDOR_DEFINE_PREFIX', 'YOASTSEO_VENDOR__' );
 define( 'YOAST_VENDOR_PREFIX_DIRECTORY', 'vendor_prefixed' );
 
 define( 'YOAST_SEO_PHP_REQUIRED', '5.6' );
-define( 'YOAST_SEO_WP_TESTED', '5.9.1' );
+define( 'YOAST_SEO_WP_TESTED', '5.9.2' );
 define( 'YOAST_SEO_WP_REQUIRED', '5.8' );
 
 if ( ! defined( 'WPSEO_NAMESPACES' ) ) {
