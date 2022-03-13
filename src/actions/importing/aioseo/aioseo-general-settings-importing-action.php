ed5741b125965ef18bdb1eadcff4095710b3a183
jrfnl <jrfnl@users.noreply.github.com>
2022-03-13 06:28:41 +0100

    CS/QA: alphabetically sorted import `use` statements

--- src/actions/importing/aioseo/aioseo-general-settings-importing-action.php
+++ src/actions/importing/aioseo/aioseo-general-settings-importing-action.php
@@ -3,10 +3,10 @@
 // phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Given it's a very specific case.
 namespace Yoast\WP\SEO\Actions\Importing\Aioseo;
 
+use Yoast\WP\SEO\Helpers\Image_Helper;
 use Yoast\WP\SEO\Helpers\Import_Cursor_Helper;
 use Yoast\WP\SEO\Helpers\Options_Helper;
 use Yoast\WP\SEO\Helpers\Sanitization_Helper;
-use Yoast\WP\SEO\Helpers\Image_Helper;
 use Yoast\WP\SEO\Services\Importing\Aioseo\Aioseo_Replacevar_Service;
 use Yoast\WP\SEO\Services\Importing\Aioseo\Aioseo_Robots_Provider_Service;
 use Yoast\WP\SEO\Services\Importing\Aioseo\Aioseo_Robots_Transformer_Service;
