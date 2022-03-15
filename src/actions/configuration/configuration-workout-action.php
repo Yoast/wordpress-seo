778f050323b5265dfcab4832c9ebc07d462b910c
Paolo L. Scala <paolo.scala@gmail.com>
2022-03-15 19:38:08 +0100

    Add other_urls field to the social profiles payload

--- src/actions/configuration/configuration-workout-action.php
+++ src/actions/configuration/configuration-workout-action.php
@@ -42,6 +42,7 @@ class Configuration_Workout_Action {
 		'pinterest_url',
 		'youtube_url',
 		'wikipedia_url',
+        'other_urls',
 	];
 
 	/**
@@ -105,6 +106,7 @@ class Configuration_Workout_Action {
 		$failures = [];
 
 		foreach ( self::SOCIAL_PROFILES_FIELDS as $field_name ) {
+            if ( $field_name === "other_urls" ) continue;
 			if ( isset( $params[ $field_name ] ) ) {
 				$result = $this->options_helper->set( $field_name, $params[ $field_name ] );
 				if ( ! $result ) {
