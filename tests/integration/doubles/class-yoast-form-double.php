607269a6e40ad3e3adcab29f0fb01a891ea5f385
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-03-18 13:41:32 +0200

    Fix PHPCS

--- tests/integration/doubles/class-yoast-form-double.php
+++ tests/integration/doubles/class-yoast-form-double.php
@@ -22,7 +22,7 @@ class Yoast_Form_Double extends Yoast_Form {
 	/**
 	 * Wrapper calling the parent method to make it public and unit-testable.
 	 *
-	 * @param string $var The variable within the option to check whether its control should be disabled.
+	 * @param string $variable The variable within the option to check whether its control should be disabled.
 	 *
 	 * @return bool True if control should be disabled, false otherwise.
 	 */
