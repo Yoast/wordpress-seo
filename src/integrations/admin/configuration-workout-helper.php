157c928a5ec63068f5dcdbda6e87c79788a7b3a8
Paolo L. Scala <paolo.scala@gmail.com>
2022-03-22 17:18:46 +0100

    Add array of social media names

---
+++ src/integrations/admin/configuration-workout-helper.php
@@ -0,0 +1,6 @@
+<?php
+namespace Yoast\WP\SEO\Integrations\Admin;
+
+class Configuration_workout_helper {
+    public static $person_social_profiles = [ "facebook", "instagram", "linkedin", "myspace", "pinterest", "soundcloud", "tumblr", "twitter", "youtube", "wikipedia" ];
+}
