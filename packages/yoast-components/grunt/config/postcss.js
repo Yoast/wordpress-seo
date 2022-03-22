fbc4ab2331a60aa03c5bbe9566163f273c351b56
nolledgeable <nolle@yoast.com>
2022-03-22 16:35:08 +0100

    Revert "Replace grunt-sass"
    This reverts commit 14228afcd13fd1d91dfa99e7fec7a089e535907c.

--- packages/yoast-components/grunt/config/postcss.js
+++ packages/yoast-components/grunt/config/postcss.js
@@ -1,21 +1,13 @@
-const cssnano = require( "cssnano" );
-const autoprefixer = require( "autoprefixer" );
-const postcssImport = require( "postcss-import" );
+var autoprefixer = require( "autoprefixer" );
 
 // See https://github.com/nDmitry/grunt-postcss
 module.exports = {
 	options: {
 		processors: [
-			postcssImport(),
 			autoprefixer( { browsers: "last 2 versions, IE >= 11" } ),
-			cssnano(),
 		],
 	},
 	build: {
-		cwd: "css/",
-		src: [ "all.css", "standalone.css" ],
-		dest: "css-dist/",
-		expand: true,
-		ext: ".css",
+		src: "css-dist/yoast-components.min.css",
 	},
 };
