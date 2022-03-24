f1a12253dff295faff5d3e01eb305371cbb35268
Paolo L. Scala <paolo.scala@gmail.com>
2022-03-24 16:35:52 +0100

    fix JSDoc

--- packages/js/src/workouts/tailwind-components/steps/social-profiles/social-profiles-step.js
+++ packages/js/src/workouts/tailwind-components/steps/social-profiles/social-profiles-step.js
@@ -10,7 +10,10 @@ import SocialInputPersonSection from "./social-input-person-section.js";
 /**
  * Social profiles step component
  *
- * @param {Object} props The props object.
+ * @param {Object}   props                The props object.
+ * @param {Object}   props.state          The container's state
+ * @param {function} props.dispatch       The function to update the container's state.
+ * @param {function} props.setErrorFields The function to keep track of which text fields are not valid
  *
  * @returns {WPElement} The social profiles step.
  */
