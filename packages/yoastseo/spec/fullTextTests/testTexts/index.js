86a02f601de5246e558423d92978e74fd774aa4a
Marina Koleva <marina@yoast.com>
2022-03-28 17:05:55 +0200

    removing superfluous full text tests

--- packages/yoastseo/spec/fullTextTests/testTexts/index.js
+++ packages/yoastseo/spec/fullTextTests/testTexts/index.js
@@ -1,176 +1,103 @@
-// English papers
-import englishPaper1 from "./en/englishPaper1";
-import englishPaper2 from "./en/englishPaper2";
-import englishPaper3 from "./en/englishPaper3";
-import englishPaper4 from "./en/englishPaper4";
+// AR: Arabic papers
+import arabicPaper from "./ar/arabicPaper";
 
-// Swedish papers
-import swedishPaper1 from "./sv/swedishPaper1";
-import swedishPaper2 from "./sv/swedishPaper2";
+// CA: Catalan papers
+import catalanPaper from "./ca/catalanPaper";
 
-// German papers
-import germanPaper1 from "./de/germanPaper1";
-import germanPaper2 from "./de/germanPaper2";
-import germanPaper3 from "./de/germanPaper3";
+// CS: Czech papers
+import czechPaper from "./cs/czechPaper";
 
-// French papers
-import frenchPaper2 from "./fr/frenchPaper2";
-import frenchPaper3 from "./fr/frenchPaper3";
+// DE: German papers
+import germanPaper from "./de/germanPaper";
 
-// Italian papers
-import italianPaper1 from "./it/italianPaper1";
-import italianPaper2 from "./it/italianPaper2";
-import italianPaper3 from "./it/italianPaper3";
-
-// Spanish papers
-import spanishPaper1 from "./es/spanishPaper1";
-import spanishPaper2 from "./es/spanishPaper2";
-import spanishPaper3 from "./es/spanishPaper3";
-
-// Polish papers
-import polishPaper1 from "./pl/polishPaper1";
-import polishPaper2 from "./pl/polishPaper2";
-import polishPaper3 from "./pl/polishPaper3";
-
-// Russian papers
-import russianPaper1 from "./ru/russianPaper1";
-import russianPaper2 from "./ru/russianPaper2";
-import russianPaper3 from "./ru/russianPaper3";
-
-// Arabic papers
-import arabicPaper1 from "./ar/arabicPaper1";
-
-// Papers for comparing performance of stemmers in different languages in the content analysis app.
-import polishPaperForPerformanceTest from "./pl/polishPaperForPerformanceTest";
-import spanishPaperForPerformanceTest from "./es/spanishPaperForPerformanceTest";
-import englishPaperForPerformanceTest from "./en/englishPaperForPerformanceTest";
-
-
-// Hebrew papers
-import hebrewPaper1 from "./he/hebrewPaper1";
-import hebrewPaper2 from "./he/hebrewPaper2";
-import hebrewPaper3 from "./he/hebrewPaper3";
-
-// Turkish papers
-import turkishPaper1 from "./tr/turkishPaper1";
-import turkishPaper2 from "./tr/turkishPaper2";
-import turkishPaper3 from "./tr/turkishPaper3";
-
-// Hungarian papers
-import hungarianPaper1 from "./hu/hungarianPaper1";
-import hungarianPaper2 from "./hu/hungarianPaper2";
-import hungarianPaper3 from "./hu/hungarianPaper3";
-
-// Catalan papers
-import catalanPaper1 from "./ca/catalanPaper1";
-import catalanPaper2 from "./ca/catalanPaper2";
-import catalanPaper3 from "./ca/catalanPaper3";
-
-// Dutch papers
-import dutchPaper1 from "./nl/dutchPaper1";
-import dutchPaper2 from "./nl/dutchPaper2";
-import dutchPaper3 from "./nl/dutchPaper3";
-
-// Indonesian papers
-import indonesianPaper1 from "./id/indonesianPaper1";
-import indonesianPaper2 from "./id/indonesianPaper2";
-import indonesianPaper3 from "./id/indonesianPaper3";
-
-// Farsi papers
-import farsiPaper1 from "./fa/farsiPaper1";
-import farsiPaper2 from "./fa/farsiPaper2";
-import farsiPaper3 from "./fa/farsiPaper3";
-
-// Portuguese papers
-import portuguesePaper1 from "./pt/portuguesePaper1";
-import portuguesePaper2 from "./pt/portuguesePaper2";
-
-// Czech papers
-import czechPaper1 from "./cs/czechPaper1";
-import czechPaper2 from "./cs/czechPaper2";
-import czechPaper3 from "./cs/czechPaper3";
-
-// Norwegian papers
-import norwegianPaper1 from "./nb/norwegianPaper1";
-import norwegianPaper2 from "./nb/norwegianPaper2";
-import norwegianPaper3 from "./nb/norwegianPaper3";
-
-// Slovak papers
-import slovakPaper1 from "./sk/slovakPaper1";
-import slovakPaper2 from "./sk/slovakPaper2";
-import slovakPaper3 from "./sk/slovakPaper3";
-
-// Greek paper
+// EL: Greek paper
 import greekPaper from "./el/greekPaper";
 
-// Japanese paper
+// EN: English papers
+import englishPaper from "./en/englishPaper";
+
+// ES: Spanish papers
+import spanishPaper from "./es/spanishPaper";
+
+// FA: Farsi papers
+import farsiPaper from "./fa/farsiPaper";
+
+// FR: French papers
+import frenchPaper from "./fr/frenchPaper";
+
+// HE: Hebrew papers
+import hebrewPaper from "./he/hebrewPaper";
+
+// HU: Hungarian papers
+import hungarianPaper from "./hu/hungarianPaper";
+
+// ID: Indonesian papers
+import indonesianPaper from "./id/indonesianPaper";
+
+// IT: Italian papers
+import italianPaper from "./it/italianPaper";
+
+// JA: Japanese paper
 import japanesePaper from "./ja/japanesePaper";
+
+// NB: Norwegian papers
+import norwegianPaper from "./nb/norwegianPaper";
+
+// NL: Dutch papers
+import dutchPaper from "./nl/dutchPaper";
+
+// PL: Polish papers
+import polishPaper from "./pl/polishPaper";
+
+// PT: Portuguese papers
+import portuguesePaper from "./pt/portuguesePaper";
+
+// RU: Russian papers
+import russianPaper from "./ru/russianPaper";
+
+// SK: Slovak papers
+import slovakPaper from "./sk/slovakPaper";
+
+// SV: Swedish papers
+import swedishPaper from "./sv/swedishPaper";
+
+// TR: Turkish papers
+import turkishPaper from "./tr/turkishPaper";
+
+// Papers for comparing performance of stemmers in different languages in the content analysis app.
+import englishPaperForPerformanceTest from "./en/englishPaperForPerformanceTest";
+import spanishPaperForPerformanceTest from "./es/spanishPaperForPerformanceTest";
+import polishPaperForPerformanceTest from "./pl/polishPaperForPerformanceTest";
 
 /**
  * FrenchPaper1 & portuguesePaper3 are temporarily disabled until we figure out why there are small differences
  * in passive voice detection since upgrading from node v10 to the lts version.
  */
 export default [
-	englishPaper1,
-	englishPaper2,
-	englishPaper3,
-	englishPaper4,
-	swedishPaper1,
-	swedishPaper2,
-	germanPaper1,
-	germanPaper2,
-	germanPaper3,
-	frenchPaper2,
-	frenchPaper3,
-	italianPaper1,
-	italianPaper2,
-	italianPaper3,
-	spanishPaper1,
-	spanishPaper2,
-	spanishPaper3,
-	polishPaper1,
-	polishPaper2,
-	polishPaper3,
-	russianPaper1,
-	russianPaper2,
-	russianPaper3,
-	arabicPaper1,
+	englishPaper,
+	arabicPaper,
+	catalanPaper,
+	czechPaper,
+	germanPaper,
+	greekPaper,
+	spanishPaper,
+	farsiPaper,
+	frenchPaper,
+	hebrewPaper,
+	hungarianPaper,
+	indonesianPaper,
+	italianPaper,
+	japanesePaper,
+	norwegianPaper,
+	dutchPaper,
+	polishPaper,
+	portuguesePaper,
+	russianPaper,
+	slovakPaper,
+	swedishPaper,
+	turkishPaper,
 	englishPaperForPerformanceTest,
 	spanishPaperForPerformanceTest,
 	polishPaperForPerformanceTest,
-	hebrewPaper1,
-	hebrewPaper2,
-	hebrewPaper3,
-	turkishPaper1,
-	turkishPaper2,
-	turkishPaper3,
-	hungarianPaper1,
-	hungarianPaper2,
-	hungarianPaper3,
-	catalanPaper1,
-	catalanPaper2,
-	catalanPaper3,
-	dutchPaper1,
-	dutchPaper2,
-	dutchPaper3,
-	indonesianPaper1,
-	indonesianPaper2,
-	indonesianPaper3,
-	portuguesePaper1,
-	portuguesePaper2,
-	czechPaper1,
-	czechPaper2,
-	czechPaper3,
-	norwegianPaper1,
-	norwegianPaper2,
-	norwegianPaper3,
-	slovakPaper1,
-	slovakPaper2,
-	slovakPaper3,
-	farsiPaper1,
-	farsiPaper2,
-	farsiPaper3,
-	greekPaper,
-	japanesePaper,
 ];
 
