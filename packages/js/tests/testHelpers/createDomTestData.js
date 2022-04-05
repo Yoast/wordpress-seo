8d3280d706ae421f53dc9250f86e9f0e60b17da9
FAMarfuaty <aida@yoast.com>
2022-04-05 17:21:01 +0200

    Improve the way we create dom test data

--- packages/js/tests/testHelpers/createDomTestData.js
+++ packages/js/tests/testHelpers/createDomTestData.js
@@ -1,207 +1,178 @@
-/* eslint-disable max-statements */
+/**
+ * Creates a category element.
+ *
+ * @param {string} id   The id of the category.
+ * @param {string} name The name of the category.
+ *
+ * @returns {{checkbox: HTMLInputElement, label: HTMLLabelElement}} The category checkbox element and the parent element of that checkbox.
+ */
+const createCategoryElement = ( id, name ) => {
+	const label = document.createElement( "label" );
+
+	const checkbox = document.createElement( "input" );
+	checkbox.setAttribute( "type", "checkbox" );
+	checkbox.setAttribute( "value", id );
+
+	const text = document.createTextNode( name );
+
+	label.append( checkbox, text );
+
+	return { checkbox, label };
+};
 
 /**
- * Creates category elements.
+ * Creates all category elements.
  *
- * @returns {{category1: HTMLInputElement, category2: HTMLInputElement, category3: HTMLInputElement,
- * category4: HTMLInputElement, category5: HTMLInputElement, allCategories: HTMLDivElement,
- * mostUsedCategories: HTMLDivElement}} The elements created for categories.
+ * @returns {{allCategoryCheckboxes:{}, allCategories: HTMLDivElement}}  The created elements.
  */
-const createCategoryElements = () => {
+const createAllCategoryElements = () => {
 	// Create regular category elements.
-	const cat1 = document.createElement( "input" );
-	cat1.setAttribute( "type", "checkbox" );
-	cat1.setAttribute( "value", "1" );
-	const cat1Name = document.createTextNode( "cat1" );
-
-	const parentCat1 = document.createElement( "label" );
-	parentCat1.appendChild( cat1 );
-	parentCat1.appendChild( cat1Name );
-
-	const cat2 = document.createElement( "input" );
-	cat2.setAttribute( "type", "checkbox" );
-	cat2.setAttribute( "value", "2" );
-	const cat2Name = document.createTextNode( "cat2" );
-
-	const parentCat2 = document.createElement( "label" );
-	parentCat2.appendChild( cat2 );
-	parentCat2.appendChild( cat2Name );
-
-	const cat3 = document.createElement( "input" );
-	cat3.setAttribute( "type", "checkbox" );
-	cat3.setAttribute( "value", "3" );
-	const cat3Name = document.createTextNode( "cat3" );
-
-	const parentCat3 = document.createElement( "label" );
-	parentCat3.appendChild( cat3 );
-	parentCat3.appendChild( cat3Name );
-
-	const cat4 = document.createElement( "input" );
-	cat4.setAttribute( "type", "checkbox" );
-	cat4.setAttribute( "value", "4" );
-	const cat4Name = document.createTextNode( "Birds" );
-
-	const parentCat4 = document.createElement( "label" );
-	parentCat4.appendChild( cat4 );
-	parentCat4.appendChild( cat4Name );
-
-	const cat5 = document.createElement( "input" );
-	cat5.setAttribute( "type", "checkbox" );
-	cat5.setAttribute( "value", "5" );
-	const cat5Name = document.createTextNode( "dogs" );
-
-	const parentCat5 = document.createElement( "label" );
-	parentCat5.appendChild( cat5 );
-	parentCat5.appendChild( cat5Name );
+	const catElement1 = createCategoryElement( "1", "cat1" );
+	const catElement2 = createCategoryElement( "2", "cat2" );
+	const catElement3 = createCategoryElement( "3", "cat3" );
+	const catElement4 = createCategoryElement( "4", "Birds" );
+	const catElement5 = createCategoryElement( "5", "dogs" );
 
 	const allCats = document.createElement( "div" );
 	allCats.setAttribute( "id", "categorychecklist" );
-	allCats.appendChild( parentCat1 );
-	allCats.appendChild( parentCat2 );
-	allCats.appendChild( parentCat3 );
-	allCats.appendChild( parentCat4 );
-	allCats.appendChild( parentCat5 );
+	allCats.append( catElement1.label, catElement2.label, catElement3.label, catElement4.label, catElement5.label );
 
-	const mostUsedCats = document.createElement( "div" );
-	mostUsedCats.setAttribute( "id", "categorychecklist-pop" );
-	mostUsedCats.appendChild( cat1.cloneNode() );
-	mostUsedCats.appendChild( cat2.cloneNode() );
+	const allCategoryCheckboxes = {
+		cat1: catElement1.checkbox,
+		cat2: catElement2.checkbox,
+		cat3: catElement3.checkbox,
+		cat4: catElement4.checkbox,
+		cat5: catElement5.checkbox,
+	};
 
 	return {
-		category1: cat1,
-		category2: cat2,
-		category3: cat3,
-		category4: cat4,
-		category5: cat5,
+		allCategoryCheckboxes,
 		allCategories: allCats,
-		mostUsedCategories: mostUsedCats,
 	};
 };
 
 /**
- * Creates tag elements.
+ * Creates the most used category elements.
  *
- * @returns {{parentTagElement: HTMLDivElement}} The element created for tags.
+ * @returns {HTMLDivElement} The created elements
  */
-const createTagElements = () => {
+const createMostUsedCategoryElements = () => {
+	const mostUsedCat1Checkbox = createCategoryElement( 1, "cat1" ).checkbox;
+	const mostUsedCat2Checkbox = createCategoryElement( 2, "cat2" ).checkbox;
+
+	const mostUsedCatsDiv = document.createElement( "div" );
+	mostUsedCatsDiv.setAttribute( "id", "categorychecklist-pop" );
+	mostUsedCatsDiv.append( mostUsedCat1Checkbox, mostUsedCat2Checkbox );
+
+	return mostUsedCatsDiv;
+};
+
+/**
+ * Creates a tag list element.
+ *
+ * @param {string} tag The tag text node.
+ *
+ * @returns {HTMLLIElement} The tag list element.
+ */
+const createTagElement = ( tag ) => {
 	// Create regular tag elements.
-	const tag1 = document.createElement( "li" );
-	const tag1ChildNode1 = document.createElement( "button" );
-	const tag1ChildNode2 = document.createTextNode( "" );
-	const tag1ChildNode3 = document.createTextNode( "cat food" );
+	const list = document.createElement( "li" );
+	const listChildNode1 = document.createElement( "button" );
+	const listChildNode2 = document.createTextNode( "" );
+	const listChildNode3 = document.createTextNode( tag );
 
-	tag1.appendChild( tag1ChildNode1 );
-	tag1.appendChild( tag1ChildNode2 );
-	tag1.appendChild( tag1ChildNode3 );
+	list.append( listChildNode1, listChildNode2, listChildNode3 );
 
-	const tag2 = document.createElement( "li" );
-	const tag2ChildNode1 = document.createElement( "button" );
-	const tag2ChildNode2 = document.createTextNode( "" );
-	const tag2ChildNode3 = document.createTextNode( "cat snack" );
+	return list;
+};
 
-	tag2.appendChild( tag2ChildNode1 );
-	tag2.appendChild( tag2ChildNode2 );
-	tag2.appendChild( tag2ChildNode3 );
+/**
+ * Creates all tag elements.
+ *
+ * @returns {HTMLDivElement} The parent element of the tags elements created.
+ */
+const createAllTagElements = () => {
+	// Create regular tag elements.
+	const tag1 = createTagElement( "cat food" );
+
+	const tag2 = createTagElement( "cat snack" );
 
 	const tagsListElement = document.createElement( "ul" );
 	tagsListElement.setAttribute( "class", "tagchecklist" );
-	tagsListElement.appendChild( tag1 );
-	tagsListElement.appendChild( tag2 );
+	tagsListElement.append( tag1, tag2 );
 
 	const parentTagElement = document.createElement( "div" );
 	parentTagElement.setAttribute( "id", "post_tag" );
 	parentTagElement.appendChild( tagsListElement );
 
+	return parentTagElement;
+};
+
+/**
+ * Creates all hierarchical custom taxonomy elements.
+ *
+ * @returns {{actor1: HTMLInputElement, actor2: HTMLInputElement, actor3: HTMLInputElement,
+ * mostUsedActors: HTMLDivElement, nonHierarchicalParentElement: HTMLDivElement, allActors: HTMLDivElement}} All custom taxonomy elements created.
+ */
+const createAllHierarchicalCTElements = () => {
+	// Set to the document the hierarchical custom taxonomy elements.
+	const actorElement1 = createCategoryElement( "1", "actor1" );
+	const actorElement2 = createCategoryElement( "2", "actor2" );
+	const actorElement3 = createCategoryElement( "3", "actor3" );
+
+	const allActors = document.createElement( "div" );
+	allActors.setAttribute( "id", "actorschecklist" );
+	allActors.append( actorElement1.label, actorElement2.label, actorElement3.label );
+
+	const allCTCheckboxes = {
+		actor1: actorElement1.checkbox,
+		actor2: actorElement2.checkbox,
+		actor3: actorElement3.checkbox,
+	};
+
 	return {
-		parentTagElement: parentTagElement,
+		allCTCheckboxes,
+		allCTElements: allActors,
 	};
 };
 
 /**
- * Creates custom taxonomy elements, both hierarchical and non-hierarchical.
+ * Creates most used hierarchical custom taxonomy elements.
  *
- * @returns {{actor1: HTMLInputElement, actor2: HTMLInputElement, actor3: HTMLInputElement,
- * mostUsedActors: HTMLDivElement, nonHierarchicalParentElement: HTMLDivElement, allActors: HTMLDivElement}} The created custom taxonomy elements.
+ * @returns {HTMLDivElement}    The created elements.
  */
-const createCustomTaxonomyElements = () => {
-	// Set to the document the hierarchical custom taxonomy elements.
-	const actor1 = document.createElement( "input" );
-	actor1.setAttribute( "type", "checkbox" );
-	actor1.setAttribute( "value", "1" );
-	const actor1Name = document.createTextNode( "actor1" );
-
-	const parentActor1 = document.createElement( "label" );
-	parentActor1.appendChild( actor1 );
-	parentActor1.appendChild( actor1Name );
-
-	const actor2 = document.createElement( "input" );
-	actor2.setAttribute( "type", "checkbox" );
-	actor2.setAttribute( "value", "2" );
-	const actor2Name = document.createTextNode( "actor2" );
-
-	const parentActor2 = document.createElement( "label" );
-	parentActor2.appendChild( actor2 );
-	parentActor2.appendChild( actor2Name );
-
-	const actor3 = document.createElement( "input" );
-	actor3.setAttribute( "type", "checkbox" );
-	actor3.setAttribute( "value", "3" );
-	const actor3Name = document.createTextNode( "actor3" );
-
-	const parentActor3 = document.createElement( "label" );
-	parentActor3.appendChild( actor3 );
-	parentActor3.appendChild( actor3Name );
-
-	const allActors = document.createElement( "div" );
-	allActors.setAttribute( "id", "actorschecklist" );
-	allActors.appendChild( parentActor1 );
-	allActors.appendChild( parentActor2 );
-	allActors.appendChild( parentActor3 );
+const createMostUsedCTElements = () => {
+	const mostUsedActor1 = createCategoryElement( "1", "actor1" ).checkbox;
+	const mostUsedActor2 = createCategoryElement( "2", "actor2" ).checkbox;
 
 	const mostUsedActors = document.createElement( "div" );
 	mostUsedActors.setAttribute( "id", "actorschecklist-pop" );
-	mostUsedActors.appendChild( actor1.cloneNode() );
-	mostUsedActors.appendChild( actor2.cloneNode() );
+	mostUsedActors.append( mostUsedActor1, mostUsedActor2 );
 
+	return mostUsedActors;
+};
+
+
+/**
+ * Creates non hierarchical custom taxonomy elements.
+ *
+ * @returns {HTMLDivElement} The non-hierarchical custom taxonomy elements created.
+ */
+const createNonHierarchicalCTElements = () => {
 	// Set to the document the non-hierarchical custom taxonomy elements.
-	const director1 = document.createElement( "li" );
-	const director1ChildNode1 = document.createElement( "button" );
-	const director1ChildNode2 = document.createTextNode( "" );
-	const director1ChildNode3 = document.createTextNode( "Steven Spielberg" );
-
-	director1.appendChild( director1ChildNode1 );
-	director1.appendChild( director1ChildNode2 );
-	director1.appendChild( director1ChildNode3 );
-
-	const director2 = document.createElement( "li" );
-	const director2ChildNode1 = document.createElement( "button" );
-	const director2ChildNode2 = document.createTextNode( "" );
-	const director2ChildNode3 = document.createTextNode( "Spike Lee" );
-
-	director2.appendChild( director2ChildNode1 );
-	director2.appendChild( director2ChildNode2 );
-	director2.appendChild( director2ChildNode3 );
+	const director1 = createTagElement( "Steven Spielberg" );
+	const director2 = createTagElement( "Spike Lee" );
 
 	const nonHierarchicalCTElement = document.createElement( "ul" );
 	nonHierarchicalCTElement.setAttribute( "class", "tagchecklist" );
-
-	// Add new elements for the non-hierarchical custom taxonomies.
-	nonHierarchicalCTElement.appendChild( director1 );
-	nonHierarchicalCTElement.appendChild( director2 );
+	nonHierarchicalCTElement.append( director1, director2 );
 
 	// Set the parent element for the non-hierarchical custom taxonomies.
 	const nonHierarchicalParentElement = document.createElement( "div" );
 	nonHierarchicalParentElement.setAttribute( "id", "directors" );
 	nonHierarchicalParentElement.appendChild( nonHierarchicalCTElement );
 
-	return {
-		actor1: actor1,
-		actor2: actor2,
-		actor3: actor3,
-		allActors: allActors,
-		mostUsedActors: mostUsedActors,
-		nonHierarchicalParentElement: nonHierarchicalParentElement,
-	};
+	return nonHierarchicalParentElement;
 };
 
 /**
@@ -247,8 +218,11 @@ const createSlugElements = () => {
 };
 
 export {
-	createCategoryElements,
-	createTagElements,
-	createCustomTaxonomyElements,
+	createAllCategoryElements,
+	createMostUsedCategoryElements,
+	createAllTagElements,
+	createAllHierarchicalCTElements,
+	createMostUsedCTElements,
+	createNonHierarchicalCTElements,
 	createSlugElements,
 };
