3f5c9b69f5490811eeb0667b2190e68447bfa129
FAMarfuaty <aida@yoast.com>
2022-04-06 10:24:35 +0200

    Adjust unit tests

--- packages/js/tests/helpers/getValuesFromCheckboxes.test.js
+++ packages/js/tests/helpers/getValuesFromCheckboxes.test.js
@@ -1,22 +1,49 @@
 import { getValuesFromCheckboxes } from "../../src/helpers/getValuesFromCheckboxes";
-import { getPostCategoryCheckboxes } from "../../src/classic-editor/helpers/dom";
-import { createCategoryElements } from "../testHelpers/createDomTestData";
+import { createAllCategoryElements, createCategoryElement } from "../testHelpers/createDomTestData";
 
-const checkboxesElements = createCategoryElements();
-document.body.appendChild( checkboxesElements.allCategories );
+const checkboxesElements = createAllCategoryElements();
+
+const allCheckboxes = checkboxesElements.allCategoryCheckboxes;
 
 describe( "a test for retrieving the values from checked checkboxes", () => {
 	it( "should return an empty array if no checkboxes is checked", () => {
-		const checkedCheckboxes = getPostCategoryCheckboxes().filter( checkbox => checkbox.checked );
+		const checkedCheckboxes = [];
 		expect( getValuesFromCheckboxes( checkedCheckboxes ) ).toEqual( [] );
 	} );
+
+	allCheckboxes.cat1.checked = true;
+	allCheckboxes.cat4.checked = true;
+	allCheckboxes.cat5.checked = true;
+
 	it( "should return the values of the checked checkboxes, ordered alphabetically", () => {
-		checkboxesElements.category1.checked = true;
-		checkboxesElements.category4.checked = true;
-		const checkedCheckboxes = getPostCategoryCheckboxes().filter( checkbox => checkbox.checked );
+		const checkedCheckboxes = [
+			allCheckboxes.cat1,
+			allCheckboxes.cat4,
+			allCheckboxes.cat5,
+		];
 		expect( getValuesFromCheckboxes( checkedCheckboxes ) ).toEqual( [
 			{ id: "4", name: "Birds" },
 			{ id: "1", name: "cat1" },
+			{ id: "5", name: "dogs" },
+		] );
+	} );
+
+	it( "should still return the values of the checked checkboxes ordered alphabetically when the name contains more than one word", () => {
+		const newCat = createCategoryElement( "6", "busy turtle" );
+		newCat.checkbox.checked = true;
+
+		const checkedCheckboxes = [
+			allCheckboxes.cat1,
+			allCheckboxes.cat4,
+			allCheckboxes.cat5,
+			newCat.checkbox,
+		];
+
+		expect( getValuesFromCheckboxes( checkedCheckboxes ) ).toEqual( [
+			{ id: "4", name: "Birds" },
+			{ id: "6", name: "busy turtle" },
+			{ id: "1", name: "cat1" },
+			{ id: "5", name: "dogs" },
 		] );
 	} );
 } );
