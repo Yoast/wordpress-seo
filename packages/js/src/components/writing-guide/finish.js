d22b851a6402928ea2477bec959bbc3f5421e2f5
Igor <35524806+igorschoester@users.noreply.github.com>
2022-04-21 11:52:01 +0200

    Initital steps

---
+++ packages/js/src/components/writing-guide/finish.js
@@ -0,0 +1,7 @@
+import { Title } from "@yoast/ui-library";
+
+const Finish = () => (
+	<Title>Finish!</Title>
+);
+
+export default Finish;
---
+++ packages/js/src/components/writing-guide/schema.js
@@ -0,0 +1,7 @@
+import { Title } from "@yoast/ui-library";
+
+const Schema = () => (
+	<Title>Schema!</Title>
+);
+
+export default Schema;
