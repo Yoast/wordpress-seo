9cf050b5d8d4f95d59dcfff664be483888a1af73
Igor <35524806+igorschoester@users.noreply.github.com>
2022-04-21 14:48:43 +0200

    Fix things

--- packages/js/src/components/writing-guide/index.js
+++ packages/js/src/components/writing-guide/index.js
@@ -2,18 +2,17 @@
 /* eslint-disable complexity */
 /* eslint-disable no-nested-ternary */
 /* eslint-disable require-jsdoc */
-import { useState, useCallback, useReducer } from "@wordpress/element";
-import { noop, indexOf, includes } from "lodash";
-import { Modal, Button } from "@yoast/ui-library";
-import classNames from "classnames";
 import { CheckIcon } from "@heroicons/react/outline";
-
-import Welcome from "./welcome";
+import { useCallback, useReducer, useState } from "@wordpress/element";
+import { Button, Modal } from "@yoast/ui-library";
+import classNames from "classnames";
+import { indexOf, noop } from "lodash";
 import About from "./about";
 import Audience from "./audience";
-import Schema from "./schema";
-import Layout from "./layout";
 import Finish from "./finish";
+import Layout from "./layout";
+import Schema from "./schema";
+import Welcome from "./welcome";
 
 const steps = {
 	welcome: {
@@ -54,12 +53,12 @@ const steps = {
 	},
 };
 
-const stepIdsOrder = [ "welcome", "about", "schema", "audience", "layout", "finish" ];
+const stepsOrder = [ "welcome", "about", "schema", "audience", "layout", "finish" ];
 
 const dataReducer = ( state = {}, action ) => {
 	switch ( action.type ) {
 		case "setData":
-			state =  {
+			state = {
 				...state,
 				...action.payload,
 			};
@@ -70,88 +69,108 @@ const dataReducer = ( state = {}, action ) => {
 
 const WritingGuide = () => {
 	const [ data, dispatch ] = useReducer( dataReducer );
-	const [ activeStepId, setActiveStepId ] = useState( "welcome" );
-	const [ completedStepIds, setCompletedStepIds ] = useState( [] );
+	const [ activeStep, setActiveStep ] = useState( "welcome" );
+	const [ completedSteps, setCompletedSteps ] = useState( [] );
 
-	const ActiveStep = steps[ activeStepId ].component;
+	const ActiveStep = steps[ activeStep ].component;
 
 	const handlePrev = useCallback( () => {
-		setActiveStepId( stepIdsOrder[ indexOf( stepIdsOrder, activeStepId ) - 1 ] );
-	}, [ setActiveStepId ] );
+		setActiveStep( stepsOrder[ indexOf( stepsOrder, activeStep ) - 1 ] );
+	}, [ setActiveStep, activeStep ] );
 
 	const handleNext = useCallback( () => {
-		setCompletedStepIds( [ ...completedStepIds, activeStepId ] );
-		setActiveStepId( stepIdsOrder[ indexOf( stepIdsOrder, activeStepId ) + 1 ] );
-	}, [ setCompletedStepIds, setActiveStepId ] );
+		setCompletedSteps( [ ...completedSteps, activeStep ] );
+		setActiveStep( stepsOrder[ indexOf( stepsOrder, activeStep ) + 1 ] );
+	}, [ setCompletedSteps, setActiveStep, activeStep ] );
 
 	return (
 		<Modal isOpen={ true } onClose={ noop }>
-			<nav aria-label="Progress" className="">
-				<ol className="yst-overflow-hidden">
-					{ stepIdsOrder.map( ( step, stepIdx ) => (
-						<li key={ step.id } className={ classNames( stepIdx !== steps.length - 1 ? "yst-pb-10" : "", "yst-relative" ) }>
-							{ includes( completedStepIds, step.id ) ? (
-								<>
-									{ stepIdx !== steps.length - 1 ? (
-										<div className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-4 yst-left-4 yst-w-0.5 yst-h-full yst-bg-indigo-600" aria-hidden="true" />
-									) : null }
-									<a href={ step.href } className="yst-relative yst-flex yst-items-start yst-group">
+			<div className="yst-flex">
+				<nav aria-label="Progress" className="yst-pr-2 yst-border-gray-300 yst-border-r">
+					<ol className="yst-overflow-hidden">
+						{ stepsOrder.map( ( stepKey, stepIdx ) => {
+							const step = steps[ stepKey ];
+
+							return (
+								<li key={ step.id }
+								    className={ classNames( stepIdx !== steps.length - 1 ? "yst-pb-10" : "", "yst-relative" ) }>
+									{ step.status === "complete" ? (
+										<>
+											{ stepIdx !== steps.length - 1 ? (
+												<div
+													className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-4 yst-left-4 yst-w-0.5 yst-h-full yst-bg-indigo-600"
+													aria-hidden="true" />
+											) : null }
+											<a href={ step.href } className="yst-relative yst-flex yst-items-start yst-group">
 										<span className="yst-h-9 yst-flex yst-items-center">
-											<span className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-indigo-600 yst-rounded-full yst-group-hover:bg-indigo-800">
+											<span
+												className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-indigo-600 yst-rounded-full yst-group-hover:bg-indigo-800">
 												<CheckIcon className="yst-w-5 yst-h-5 yst-text-white" aria-hidden="true" />
 											</span>
 										</span>
-										<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col">
+												<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col">
 											<span className="yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase">{ step.title }</span>
 											<span className="yst-text-sm yst-text-gray-500">{ step.description }</span>
 										</span>
-									</a>
-								</>
-							) : activeStepId === step.id ? (
-								<>
-									{ stepIdx !== steps.length - 1 ? (
-										<div className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-4 yst-left-4 yst-w-0.5 yst-h-full yst-bg-gray-300" aria-hidden="true" />
-									) : null }
-									<a href={ step.href } className="yst-relative yst-flex yst-items-start yst-group" aria-current="step">
+											</a>
+										</>
+									) : step.status === "current" ? (
+										<>
+											{ stepIdx !== steps.length - 1 ? (
+												<div
+													className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-4 yst-left-4 yst-w-0.5 yst-h-full yst-bg-gray-300"
+													aria-hidden="true" />
+											) : null }
+											<a href={ step.href } className="yst-relative yst-flex yst-items-start yst-group" aria-current="step">
 										<span className="yst-h-9 yst-flex yst-items-center" aria-hidden="true">
-											<span className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-white yst-border-2 yst-border-indigo-600 yst-rounded-full">
+											<span
+												className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-white yst-border-2 yst-border-indigo-600 yst-rounded-full">
 												<span className="yst-h-2.5 yst-w-2.5 yst-bg-indigo-600 yst-rounded-full" />
 											</span>
 										</span>
-										<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col">
-											<span className="yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase yst-text-indigo-600">{ step.title }</span>
+												<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col">
+											<span
+												className="yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase yst-text-indigo-600">{ step.title }</span>
 											<span className="yst-text-sm yst-text-gray-500">{ step.description }</span>
 										</span>
-									</a>
-								</>
-							) : (
-								<>
-									{ stepIdx !== steps.length - 1 ? (
-										<div className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-4 yst-left-4 yst-w-0.5 yst-h-full yst-bg-gray-300" aria-hidden="true" />
-									) : null }
-									<a href={ step.href } className="yst-relative yst-flex yst-items-start yst-group">
+											</a>
+										</>
+									) : (
+										<>
+											{ stepIdx !== steps.length - 1 ? (
+												<div
+													className="yst--ml-px yst-absolute yst-mt-0.5 yst-top-4 yst-left-4 yst-w-0.5 yst-h-full yst-bg-gray-300"
+													aria-hidden="true" />
+											) : null }
+											<a href={ step.href } className="yst-relative yst-flex yst-items-start yst-group">
 										<span className="yst-h-9 yst-flex yst-items-center" aria-hidden="true">
-											<span className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-white yst-border-2 yst-border-gray-300 yst-rounded-full yst-group-hover:border-gray-400">
-												<span className="yst-h-2.5 yst-w-2.5 yst-bg-transparent yst-rounded-full yst-group-hover:bg-gray-300" />
+											<span
+												className="yst-relative yst-z-10 yst-w-8 yst-h-8 yst-flex yst-items-center yst-justify-center yst-bg-white yst-border-2 yst-border-gray-300 yst-rounded-full yst-group-hover:border-gray-400">
+												<span
+													className="yst-h-2.5 yst-w-2.5 yst-bg-transparent yst-rounded-full yst-group-hover:bg-gray-300" />
 											</span>
 										</span>
-										<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col">
-											<span className="yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase yst-text-gray-500">{ step.title }</span>
+												<span className="yst-ml-4 yst-min-w-0 yst-flex yst-flex-col">
+											<span
+												className="yst-text-xs yst-font-semibold yst-tracking-wide yst-uppercase yst-text-gray-500">{ step.title }</span>
 											<span className="yst-text-sm yst-text-gray-500">{ step.description }</span>
 										</span>
-									</a>
-								</>
-							) }
-						</li>
-					) ) }
-				</ol>
-			</nav>
-			<ActiveStep data={ data } dispatch={ dispatch } />
-			<footer>
-				<Button variant="secondary" onClick={ handlePrev }>Prev</Button>
-				<Button onClick={ handleNext }>Next</Button>
-			</footer>
-
+											</a>
+										</>
+									) }
+								</li>
+							);
+						} ) }
+					</ol>
+				</nav>
+				<div className="yst-flex yst-grow yst-flex-col yst-justify-between yst-pl-2">
+					<ActiveStep data={ data } dispatch={ dispatch } />
+					<footer className="yst-flex yst-justify-between">
+						<Button variant="secondary" onClick={ handlePrev }>Prev</Button>
+						<Button onClick={ handleNext }>Next</Button>
+					</footer>
+				</div>
+			</div>
 		</Modal>
 	);
 };
