f9a6d910d20de42cdf71ec8d71a6ce2cd5971e44
YoastBot CI <yoastbot-ci@yoast.com>
2022-04-29 11:25:42 +0000

    Bump version to 18.7-RC3

--- wp-seo-main.php
+++ wp-seo-main.php
@@ -18,7 +18,7 @@ if ( ! function_exists( 'add_filter' ) ) {
  * {@internal Nobody should be able to overrule the real version number as this can cause
  *            serious issues with the options, so no if ( ! defined() ).}}
  */
-define( 'WPSEO_VERSION', '18.7-RC2' );
+define( 'WPSEO_VERSION', '18.7-RC3' );
 
 
 if ( ! defined( 'WPSEO_PATH' ) ) {
