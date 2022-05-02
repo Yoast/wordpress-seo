6e35174bf7bd46361f192d38a775a286439184f0
Paolo L. Scala <paolo.scala@gmail.com>
2022-05-02 10:20:06 +0200

    Continue tailwindification of the success page

--- packages/js/src/installation-success.js
+++ packages/js/src/installation-success.js
@@ -2,7 +2,7 @@ import domReady from "@wordpress/dom-ready";
 import { __, sprintf } from "@wordpress/i18n";
 import { createInterpolateElement } from "@wordpress/element";
 import { renderReactRoot } from "./helpers/reactRoot";
-import { ButtonStyledLink } from "@yoast/components";
+import { Button } from "@yoast/components";
 import { ActiveCircle, SavedCircle } from "./first-time-configuration/tailwind-components/step-circle";
 
 /**
@@ -39,7 +39,7 @@ export default function Steppers() {
 function InstallationSuccessPage() {
 	return (
 		<div className="yst-root yst-flex yst-flex-col yst-h-full yst-justify-start yst-pt-2">
-			<h1 className="yst-text-4xl yst-text-gray-900 yst-w-[350px] yst-font-extrabold yst-mx-auto yst-my-6 yst-text-center">
+			<h1 className="yst-text-4xl yst-text-gray-900 yst-w-[350px] yst-font-extrabold yst-mx-auto yst-my-6 yst-text-center yst-tracking-tight">
 				{
 					createInterpolateElement(
 						sprintf(
@@ -57,9 +57,9 @@ function InstallationSuccessPage() {
 				<div className="yst-block">
 					<Steppers />
 				</div>
-				<div className="installation-success-cards">
-					<div id="installation-success-card-optimized-site" className="installation-success-card">
-						<h2>{ __( "Your site is now easy to find for search engines!", "wordpress-seo" ) }</h2>
+				<div className="yst-flex yst-justify-center yst-gap-6">
+					<div id="installation-success-card-optimized-site" className="installation-success-card yst-shadow-md">
+						<h2 className="yst-tracking-tight">{ __( "Your site is already easier to find for search engines.", "wordpress-seo" ) }</h2>
 						<p>
 							{ sprintf(
 								/* translators: %s expands to Yoast SEO. */
@@ -76,33 +76,48 @@ function InstallationSuccessPage() {
 							/>
 						</div>
 					</div>
-					<div id="installation-success-card-configuration-workout" className="installation-success-card active">
-						<h2>
+					<div id="installation-success-card-configuration-workout" className="installation-success-card yst-h-[392px] yst-bg-primary-500 yst-yst-shadow-xl">
+						<h2 className=" yst-text-white">
 							{ sprintf(
 								/* translators: %s expands to Yoast SEO. */
-								__( "Configure %s!", "wordpress-seo" ),
+								__( "For the best ranking results: configure %s!", "wordpress-seo" ),
 								"Yoast SEO"
 							) }
 						</h2>
-						<p>
+						<p className="yst-font-medium yst-tracking-tight yst-text-white">
 							{ sprintf(
 								/* translators: %s expands to Yoast SEO. */
-								__( "Set the essential %s settings in a few steps.", "wordpress-seo" ),
+								__( "Set the essential %s settings for your site in a few steps.", "wordpress-seo" ),
 								"Yoast SEO"
 							) }
 						</p>
 						<img
-							src={ window.wpseoInstallationSuccess.pluginUrl + "/images/mirrored_fit_bubble_woman_1_optim.svg" }
+							src={ window.wpseoInstallationSuccess.pluginUrl + "/images/indexables_3_left_bubble_optm.svg" }
 							alt={ "" }
 						/>
-						<div className="card-button-section">
-							<ButtonStyledLink
+						<div className="flex yst-justify-center yst-grow-1 yst-mt-auto">
+							<Button
+								className="yst-flex yst-items-center yst-px-3 yst-py-0 yst-border yst-border-transparent yst-text-base yst-font-medium yst-rounded-md yst-shadow-sm yst-text-primary-500 yst-bg-white"
 								id="installation-successful-configuration-workout-link"
 								href={ window.wpseoInstallationSuccess.configurationWorkoutUrl }
 								variant="primary"
 							>
-								{ __( "Start configuration workout!", "wordpress-seo" ) }
-							</ButtonStyledLink>
+								{ __( "Start first-time configuration!", "wordpress-seo" ) }
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									className="yst-ml-3 yst-mr-1 yst-h-5 yst-w-5"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke="currentColor"
+								>
+									<path
+										strokeLinecap="round"
+										strokeLinejoin="round"
+										strokeWidth="2"
+										d="M14 5l7 7m0 0l-7 7m7-7H3"
+									/>
+								</svg>
+							</Button>
 						</div>
 					</div>
 				</div>
