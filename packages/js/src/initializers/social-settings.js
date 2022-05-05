432f1c4aac4eead4c03f726d88aca6789a71b36a
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2022-05-05 09:28:00 +0200

    Add onChangeHandler

--- packages/js/src/initializers/social-settings.js
+++ packages/js/src/initializers/social-settings.js
@@ -1,8 +1,35 @@
-import { render, Fragment, useState, useCallback } from "@wordpress/element";
+import apiFetch from "@wordpress/api-fetch";
+import { render, Fragment, useState, useCallback, useEffect } from "@wordpress/element";
 import ImageSelectPortal from "../components/portals/ImageSelectPortal";
 import Portal from "../components/portals/Portal";
 import { __ } from "@wordpress/i18n";
 import { SocialInputSection } from "../first-time-configuration/tailwind-components/steps/social-profiles/social-input-section";
+
+/**
+ * Updates the social profiles in the database.
+ *
+ * @param {String} facebookUrl     The facebook url to save.
+ * @param {String} twitterUsername The twitter username/url to save.
+ * @param {Array}  otherSocialUrls The other urls array to save.
+ *
+ * @returns {Promise|bool} A promise, or false if the call fails.
+ */
+async function updateSocialProfiles( facebookUrl, twitterUsername, otherSocialUrls ) {
+	const socialProfiles = {
+		/* eslint-disable camelcase */
+		facebook_site: facebookUrl,
+		twitter_site: twitterUsername,
+		other_social_urls: otherSocialUrls,
+		/* eslint-enable camelcase */
+	};
+
+	const response = await apiFetch( {
+		path: "yoast/v1/configuration/social_profiles",
+		method: "POST",
+		data: socialProfiles,
+	} );
+	return await response.json;
+}
 
 /**
  * Wraps the SocialInputSection to provide some state.
@@ -14,6 +41,31 @@ function SocialProfilesWrapper() {
 	const [ twitterValue, setTwitterValue ] = useState( "" );
 	const [ otherSocialUrls, setOtherSocialUrls ] = useState( [] );
 	const [ errorFields, setErrorFields ] = useState( [] );
+
+	// Clear errorFields when values are edited.
+	useEffect( () => {
+		setErrorFields( [] );
+	}, [ facebookValue, twitterValue, otherSocialUrls ] );
+
+	const onSaveHandler = useCallback( () => {
+		updateSocialProfiles( facebookValue, twitterValue, otherSocialUrls )
+			.then( ( response ) => {
+				if ( response.success === false ) {
+					setErrorFields( response.failures );
+					return Promise.reject( "There were errors saving social profiles" );
+				}
+				return response;
+			} )
+			.then( () => { setErrorFields( [] ) } )
+			.catch(
+				( e ) => {
+					if ( e.failures ) {
+						setErrorFields( e.failures );
+					}
+					return false;
+				}
+			)
+	}, [ updateSocialProfiles, setErrorFields, facebookValue, twitterValue, otherSocialUrls ] );
 
 	const onChangeHandler = useCallback( ( value, socialMedium ) => {
 		if ( socialMedium === "facebookUrl" ) {
@@ -57,6 +109,7 @@ function SocialProfilesWrapper() {
 			<button
 				className="yst-button yst-button--primary yst-mt-8"
 				type="button"
+				onClick={ onSaveHandler }
 			>{ __( "Save changes", "wordpress-seo" ) }</button>
 		</div>
 	);
