a39fb4acba306cc170a18734c7e7d4e99620524b
FAMarfuaty <aida@yoast.com>
2022-05-06 15:16:45 +0200

    Adjust unit tests

--- packages/js/tests/classic-editor/initial-state.test.js
+++ packages/js/tests/classic-editor/initial-state.test.js
@@ -5,21 +5,21 @@ jest.mock( "../../src/classic-editor/helpers/dom", () => ( {
 	...jest.requireActual( "../../src/classic-editor/helpers/dom" ),
 	getPostTitle: jest.fn( () => "Tortoiseshell cat" ),
 	getPostDate: jest.fn( () => "18 January 2022 12:17" ),
-	getPostPermalink: jest.fn( () => "www.sweetcat.com/123" ),
+	getPostPermalink: jest.fn( () => "www.example.com/123" ),
 	getPostExcerpt: jest.fn( () => "An example of excerpt from a content." ),
 	getPostContent: jest.fn( () => "Tortoiseshell is a cat coat coloring named for its similarity to tortoiseshell material." ),
 	getPostFeaturedImage: jest.fn( () => ( {} ) ),
 	getPostSeoTitle: jest.fn( () => "Tortoiseshell cat - All about cats" ),
 	getPostMetaDescription: jest.fn( () => "Cats with tortoiseshell coloration are believed to bring good luck." ),
-	getPostSlug: jest.fn( () => "www.sweetcat.com/tortoiseshell-cat" ),
+	getPostSlug: jest.fn( () => "www.example.com/tortoiseshell-cat" ),
 	getPostIsCornerstone: jest.fn( () => false ),
 	getPostFocusKeyphrase: jest.fn( () => "tortoiseshell cat" ),
 	getTermName: jest.fn( () => "cat" ),
-	getTermPermalink: jest.fn( () => "www.sweetcat.com/categories" ),
+	getTermPermalink: jest.fn( () => "www.example.com/categories" ),
 	getTermDescription: jest.fn( () => "This is to describe a cat, that deserves only good attributes to them." ),
 	getTermSeoTitle: jest.fn( () => "A title befitting a beautiful cat" ),
 	getTermMetaDescription: jest.fn( () => "An example of a description for a cat." ),
-	getTermSlug: jest.fn( () => "www.sweetcat.com/categories/cat" ),
+	getTermSlug: jest.fn( () => "www.example.com/categories/cat" ),
 	getTermIsCornerstone: jest.fn( () => false ),
 	getTermFocusKeyphrase: jest.fn( () => "panda" ),
 	getPostCategories: jest.fn( () => [
@@ -48,22 +48,22 @@ jest.mock( "../../src/classic-editor/helpers/dom", () => ( {
 			directors: [ "Spike Lee", "Steven Spielberg" ],
 		};
 	} ),
-	getPostFBTitle: jest.fn( () => "An FB title for post" ),
-	getPostFBDescription: jest.fn( () => "An FB meta description for post" ),
-	getPostFBImageID: jest.fn( () => 7 ),
-	getPostFBImageURL: jest.fn( () => "www.sweetcat.com/images/good-cat.jpeg" ),
+	getPostFacebookTitle: jest.fn( () => "An FB title for post" ),
+	getPostFacebookDescription: jest.fn( () => "An FB meta description for post" ),
+	getPostFacebookImageID: jest.fn( () => 7 ),
+	getPostFacebookImageUrl: jest.fn( () => "www.example.com/images/good-cat.jpeg" ),
 	getPostTwitterTitle: jest.fn( () => "A Twitter title for post" ),
 	getPostTwitterDescription: jest.fn( () => "A Twitter meta description for post" ),
 	getPostTwitterImageID: jest.fn( () => 10 ),
-	getPostTwitterImageURL: jest.fn( () => "www.sweetcat.com/images/good-cat-on-twitter.jpeg" ),
-	getTermFBTitle: jest.fn( () => "An FB title for term" ),
-	getTermFBDescription: jest.fn( () => "An FB meta description for term" ),
-	getTermFBImageID: jest.fn( () => 4 ),
-	getTermFBImageURL: jest.fn( () => "www.sweetcat.com/images/best-cat-on-fb.jpeg" ),
+	getPostTwitterImageUrl: jest.fn( () => "www.example.com/images/good-cat-on-twitter.jpeg" ),
+	getTermFacebookTitle: jest.fn( () => "An FB title for term" ),
+	getTermFacebookDescription: jest.fn( () => "An FB meta description for term" ),
+	getTermFacebookImageID: jest.fn( () => 4 ),
+	getTermFacebookImageUrl: jest.fn( () => "www.example.com/images/best-cat-on-fb.jpeg" ),
 	getTermTwitterTitle: jest.fn( () => "A Twitter title for term" ),
 	getTermTwitterDescription: jest.fn( () => "A Twitter meta description for term" ),
 	getTermTwitterImageID: jest.fn( () => 6 ),
-	getTermTwitterImageURL: jest.fn( () => "www.sweetcat.com/images/best-cat-on-twitter.jpeg" ),
+	getTermTwitterImageUrl: jest.fn( () => "www.example.com/images/best-cat-on-twitter.jpeg" ),
 } ) );
 
 self.wpseoScriptData = {
@@ -81,14 +81,14 @@ describe( "a test for getting the initial state of a post or a term", () => {
 		const actual = getInitialPostState();
 		expect( actual.editor.title ).toEqual( "Tortoiseshell cat" );
 		expect( actual.editor.date ).toEqual( "18 January 2022 12:17" );
-		expect( actual.editor.permalink ).toEqual( "www.sweetcat.com/123" );
+		expect( actual.editor.permalink ).toEqual( "www.example.com/123" );
 		expect( actual.editor.excerpt ).toEqual( "An example of excerpt from a content." );
 		expect( actual.editor.content ).toEqual( "Tortoiseshell is a cat coat coloring named for its similarity to tortoiseshell material." );
 		expect( actual.editor.featuredImage ).toEqual( {} );
 		expect( actual.editor.locale ).toEqual( "en_US" );
 		expect( actual.form.seo.title ).toEqual( "Tortoiseshell cat - All about cats" );
 		expect( actual.form.seo.description ).toEqual( "Cats with tortoiseshell coloration are believed to bring good luck." );
-		expect( actual.form.seo.slug ).toEqual( "www.sweetcat.com/tortoiseshell-cat" );
+		expect( actual.form.seo.slug ).toEqual( "www.example.com/tortoiseshell-cat" );
 		expect( actual.form.seo.isCornerstone ).toEqual( false );
 		expect( actual.form.keyphrases ).toEqual( { focus: { id: "focus", keyphrase: "tortoiseshell cat" } } );
 		expect( actual.editor.taxonomies.categories ).toEqual( [
@@ -116,11 +116,11 @@ describe( "a test for getting the initial state of a post or a term", () => {
 		expect( actual.form.social.facebook.title ).toEqual( "An FB title for post" );
 		expect( actual.form.social.facebook.description ).toEqual( "An FB meta description for post" );
 		expect( actual.form.social.facebook.image.id ).toEqual( 7 );
-		expect( actual.form.social.facebook.image.url ).toEqual( "www.sweetcat.com/images/good-cat.jpeg" );
+		expect( actual.form.social.facebook.image.url ).toEqual( "www.example.com/images/good-cat.jpeg" );
 		expect( actual.form.social.twitter.title ).toEqual( "A Twitter title for post" );
 		expect( actual.form.social.twitter.description ).toEqual( "A Twitter meta description for post" );
 		expect( actual.form.social.twitter.image.id ).toEqual( 10 );
-		expect( actual.form.social.twitter.image.url ).toEqual( "www.sweetcat.com/images/good-cat-on-twitter.jpeg" );
+		expect( actual.form.social.twitter.image.url ).toEqual( "www.example.com/images/good-cat-on-twitter.jpeg" );
 		expect( actual.form.social.template.description ).toEqual( "A social template for meta description" );
 		expect( actual.form.social.template.title ).toEqual( "A social template for title" );
 	} );
@@ -128,21 +128,21 @@ describe( "a test for getting the initial state of a post or a term", () => {
 	it( "returns the initial state of a term", () => {
 		const actual = getInitialTermState();
 		expect( actual.editor.title ).toEqual( "cat" );
-		expect( actual.editor.permalink ).toEqual( "www.sweetcat.com/categories" );
+		expect( actual.editor.permalink ).toEqual( "www.example.com/categories" );
 		expect( actual.editor.content ).toEqual( "This is to describe a cat, that deserves only good attributes to them." );
 		expect( actual.editor.locale ).toEqual( "en_US" );
 		expect( actual.form.seo.title ).toEqual( "A title befitting a beautiful cat" );
 		expect( actual.form.seo.description ).toEqual( "An example of a description for a cat." );
-		expect( actual.form.seo.slug ).toEqual( "www.sweetcat.com/categories/cat" );
+		expect( actual.form.seo.slug ).toEqual( "www.example.com/categories/cat" );
 		expect( actual.form.seo.isCornerstone ).toEqual( false );
 		expect( actual.form.social.facebook.title ).toEqual( "An FB title for term" );
 		expect( actual.form.social.facebook.description ).toEqual( "An FB meta description for term" );
 		expect( actual.form.social.facebook.image.id ).toEqual( 4 );
-		expect( actual.form.social.facebook.image.url ).toEqual( "www.sweetcat.com/images/best-cat-on-fb.jpeg" );
+		expect( actual.form.social.facebook.image.url ).toEqual( "www.example.com/images/best-cat-on-fb.jpeg" );
 		expect( actual.form.social.twitter.title ).toEqual( "A Twitter title for term" );
 		expect( actual.form.social.twitter.description ).toEqual( "A Twitter meta description for term" );
 		expect( actual.form.social.twitter.image.id ).toEqual( 6 );
-		expect( actual.form.social.twitter.image.url ).toEqual( "www.sweetcat.com/images/best-cat-on-twitter.jpeg" );
+		expect( actual.form.social.twitter.image.url ).toEqual( "www.example.com/images/best-cat-on-twitter.jpeg" );
 		expect( actual.form.social.template.description ).toEqual( "A social template for meta description" );
 		expect( actual.form.social.template.title ).toEqual( "A social template for title" );
 		expect( actual.form.keyphrases ).toEqual( { focus: { id: "focus", keyphrase: "panda" } } );
