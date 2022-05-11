6ee1c965780be127f0585cafbdf12f56b44f53f8
FAMarfuaty <aida@yoast.com>
2022-05-11 14:48:15 +0200

    Improve documentation

--- packages/yoastseo/src/languageProcessing/helpers/sentence/getSentences.js
+++ packages/yoastseo/src/languageProcessing/helpers/sentence/getSentences.js
@@ -35,9 +35,10 @@ const getSentencesFromBlockCached = memoize( getSentenceTokenizer );
  * @returns {Array} Sentences found in the text.
  */
 export default function( text ) {
+	// We don't remove the other HTML tags here since removing them might lead to incorrect results when running the sentence tokenizer.
 	// Remove Table of Contents.
 	text = excludeTableOfContentsTag( text );
-	// Unify whitespaces and non-breaking spaces.
+	// Unify only non-breaking spaces and not the other whitespaces since a whitespace could signify a sentence break or a new line.
 	text = unifyNonBreakingSpace( text );
 
 	let blocks = getBlocks( text );
