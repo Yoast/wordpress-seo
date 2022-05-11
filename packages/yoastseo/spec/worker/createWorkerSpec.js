02032fe768a4c7a934b7ff695662f591c5f178f9
Hans-Christiaan <hans-christiaan@yoast.com>
2022-05-11 13:02:10 +0200

    Added unit tests for createWorker file.

--- packages/yoastseo/spec/worker/createWorkerSpec.js
+++ packages/yoastseo/spec/worker/createWorkerSpec.js
@@ -1,78 +1,150 @@
 // Dependencies
-import { createExceptionHandler, isSameOrigin, createBlobURL } from "../../src/worker/createWorker";
+import { createExceptionHandler, isSameOrigin, createBlobURL, createWorkerFallback, createWorker } from "../../src/worker/createWorker";
 
-describe( "checks createExceptionHandler function", () => {
-	it( "outputs an error message containing the inputted string ", () => {
-		const exceptionMessage = createExceptionHandler( "poffertjes" );
-		expect( exceptionMessage ).toContain( "poffertjes" );
-	} );
-} );
+/**
+ * JSDom does not provide full support for Blob, so we have to provide a polyfill
+ * to be able to check the contents of blobs.
+ */
+import "blob-polyfill";
 
-describe( "checks isSameOrigin function", () => {
-	it( "checks if two different URLS from different sites have the same origin (hostname, port, protocol)", () => {
-		const sameURL = isSameOrigin( "https://jestjs.io/docs/mock-functions", "https://developer.mozilla.org/en-US/docs/Web/API/Location/origin" );
-		expect( sameURL ).toBeFalsy();
-	} );
-
-	it( "checks if two URLS of different posts on the same site have the same origin (hostname, port, protocol)", () => {
-		// eslint-disable-next-line max-len
-		const sameURL = isSameOrigin( "https://stackoverflow.com/questions/52968969/jest-url-createobjecturl-is-not-a-function", "https://stackoverflow.com/questions/41885841/how-can-i-mock-the-javascript-window-object-using-jest" );
-		expect( sameURL ).toBeTruthy();
-	} );
-
-	it( "checks if two URLS with different scheme (http vs https) have the same origin (hostname, port, protocol/ scheme)", () => {
-		const sameURL = isSameOrigin( "http://jestjs.io/docs/mock-functions", "https://jestjs.io/docs/mock-functions" );
-		expect( sameURL ).toBeFalsy();
-	} );
-} );
-
-describe( "checks createBlobURL function", () => {
-	it( "does something", () => {
-		global.URL.createObjectURL = jest.fn();
-		createBlobURL( "dogs" );
-		expect( global.URL.createObjectURL ).toBeCalledTimes( 1 );
-		const blob = new Blob( [ "test" ] );
-		expect( global.URL.createObjectURL ).toBeCalledWith( blob );
-		expect( blob ).toBeInstanceOf( Blob );
-		const blobAsText = blob.text;
-		console.log( blobAsText );
-		const blobType = blob.slice();
-		console.log( blobType );
-		/*
-		Const someTestValue = "DOGS";
-		const blobScript = createExceptionHandler( createBlobScript( someTestValue ) );
-
-		let blob;
-		const URL = window.URL || window.webkitURL;
-		try {
-			blob = new Blob( [ blobScript ], { type: "application/javascript" } );
-		} catch ( e1 ) {
-			const blobBuilder = new BlobBuilder();
-			blobBuilder.append( blobScript );
-			blob = blobBuilder.getBlob( "application/javascript" );
-		}
-		const urlOut = window.URL.createObjectURL( blob );
-		const supposedScript = fetch( urlOut ).then( function( body ) {
-			return body.text();
+describe( "The createWorker module", () => {
+	describe( "checks createExceptionHandler function", () => {
+		it( "outputs an error message containing the inputted string ", () => {
+			const exceptionMessage = createExceptionHandler( "poffertjes" );
+			expect( exceptionMessage ).toContain( "poffertjes" );
 		} );
-		expect( blobScript ).toBe( supposedScript );
-		*/
 	} );
-	/*
-	It( "does something else", () => {
-		const doc = new jsPDF();
-		const fileName = "test.pdf";
-		doc.text( "Hello world!" );
-		const blob = new Blob( [ doc.output() ] );
-		download( fileName, blob );
+
+	describe( "checks isSameOrigin function", () => {
+		it( "checks if two different URLS from different sites have the same origin (hostname, port, protocol)", () => {
+			const sameURL = isSameOrigin( "https://jestjs.io/docs/mock-functions", "https://developer.mozilla.org/en-US/docs/Web/API/Location/origin" );
+			expect( sameURL ).toBeFalsy();
+		} );
+
+		it( "checks if two URLS of different posts on the same site have the same origin (hostname, port, protocol)", () => {
+			// eslint-disable-next-line max-len
+			const sameURL = isSameOrigin( "https://stackoverflow.com/questions/52968969/jest-url-createobjecturl-is-not-a-function", "https://stackoverflow.com/questions/41885841/how-can-i-mock-the-javascript-window-object-using-jest" );
+			expect( sameURL ).toBeTruthy();
+		} );
+
+		it( "checks if two URLS with different scheme (http vs https) have the same origin (hostname, port, protocol/ scheme)", () => {
+			const sameURL = isSameOrigin( "http://jestjs.io/docs/mock-functions", "https://jestjs.io/docs/mock-functions" );
+			expect( sameURL ).toBeFalsy();
+		} );
 	} );
-	it( "createExceptionHandler fails with an error", async() => {
-	expect.assertions( 1 );
-	try {
-		await createExceptionHandler();
-	} catch ( error ) {
-		expect( error ).toMatch( "Error occurred during worker initialization:" );
-	}
+
+	describe( "checks createBlobURL function", () => {
+		it( "creates a blob", async() => {
+			global.URL.createObjectURL = jest.fn();
+			createBlobURL( "https://example.org/some/code.js" );
+			expect( global.URL.createObjectURL ).toBeCalledTimes( 1 );
+
+			const argumentsOfLastCall = global.URL.createObjectURL.mock.calls[ 0 ];
+			const blob = argumentsOfLastCall[ 0 ];
+
+			expect( blob.type ).toEqual( "application/javascript" );
+
+			const text = await blob.text();
+
+			expect( text ).toContain( "importScripts('https://example.org/some/code.js');" );
+			expect( text ).toContain( "try" );
+			expect( text ).toContain( "catch" );
+		} );
+
+		it( "uses the webkitURL as a fallback when URL is not available", async() => {
+			const URL = global.URL;
+
+			global.URL = undefined;
+
+			global.webkitURL = jest.fn();
+			global.webkitURL.createObjectURL = jest.fn();
+			createBlobURL( "https://example.org/some/code.js" );
+			expect( global.webkitURL.createObjectURL ).toBeCalledTimes( 1 );
+
+			global.URL = URL;
+		} );
+
+		it( "falls back to the BlobBuilder when creating a Blob fails", async() => {
+			global.URL.createObjectURL = jest.fn();
+			global.Blob = jest.fn( () => { throw "An error!"; } );
+
+			const append = jest.fn();
+			const getBlob = jest.fn();
+
+			global.BlobBuilder = jest.fn( () => (
+				{
+					append,
+					getBlob,
+				}
+			) );
+
+			createBlobURL( "https://example.org/some/code.js" );
+
+			expect( global.URL.createObjectURL ).toBeCalledTimes( 1 );
+
+			const text = append.mock.calls[ 0 ][ 0 ];
+
+			expect( text ).toContain( "importScripts('https://example.org/some/code.js');" );
+			expect( text ).toContain( "try" );
+			expect( text ).toContain( "catch" );
+
+			expect( getBlob ).toHaveBeenCalledWith( "application/javascript" );
+		} );
+	} );
+
+	describe( "The createWorkerFallback function", () => {
+		it( "creates a worker fallback", () => {
+			global.URL.createObjectURL = jest.fn();
+			global.Worker = jest.fn();
+
+			global.URL.createObjectURL.mockReturnValue( "https://example.org/url/to/a/blob" );
+
+			createWorkerFallback( "https://example.org/some/code.js" );
+
+			expect( global.Worker ).toHaveBeenCalledWith( "https://example.org/url/to/a/blob" );
+		} );
+	} );
+
+	describe( "The createWorker function", () => {
+		it( "creates a worker", () => {
+			global.URL.createObjectURL = jest.fn();
+			global.Worker = jest.fn();
+
+			const worker = createWorker( "http://localhost/some/code.js" );
+
+			expect( worker ).toBeInstanceOf( global.Worker );
+		} );
+
+		it( "creates a worker using the fallback when the worker script does not have the same origin", () => {
+			global.URL.createObjectURL = jest.fn();
+			global.Worker = jest.fn();
+
+			const worker = createWorker( "http://example.org/some/code.js" );
+
+			expect( worker ).toBeInstanceOf( global.Worker );
+		} );
+
+		it( "creates a worker using the fallback when the worker script cannot be created", () => {
+			global.URL.createObjectURL = jest.fn();
+			global.Worker = jest.fn( url => {
+				if ( url === "http://localhost/some/code.js" ) {
+					throw "error!";
+				}
+			} );
+
+			const worker = createWorker( "http://localhost/some/code.js" );
+
+			expect( worker ).toBeInstanceOf( global.Worker );
+		} );
+
+		it( "throws an error when the worker script cannot be created at all", () => {
+			global.URL.createObjectURL = jest.fn();
+			global.Worker = jest.fn( url => {
+				throw "error!";
+			} );
+
+			expect( () => { createWorker( "http://localhost/some/code.js" ) } ).toThrowError();
+		} );
+	} );
 } );
-	 */
-} );
+
--- packages/yoastseo/src/worker/createWorker.js
+++ packages/yoastseo/src/worker/createWorker.js
@@ -113,5 +113,6 @@ export {
 	createBlobURL,
 	createWorker,
 	createBlobScript,
+	createWorkerFallback,
 };
 
