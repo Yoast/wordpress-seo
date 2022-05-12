7b013e4aed6dfbb6e9e40ac8337a042945d9c52d
Enrico Battocchi <enrico.battocchi@gmail.com>
2022-05-12 08:39:24 +0200

    Gray out the usage tracking step in the FTC when it's not allowed at network level

--- src/integrations/admin/first-time-configuration-integration.php
+++ src/integrations/admin/first-time-configuration-integration.php
@@ -198,6 +198,7 @@ class First_Time_Configuration_Integration implements Integration_Interface {
 					},
 					"isPremium": %d,
 					"tracking": %d,
+					"isTrackingAllowedMultisite": %d,
 					"companyOrPersonOptions": %s,
 					"shouldForceCompany": %d,
 					"knowledgeGraphMessage": "%s",
@@ -224,6 +225,7 @@ class First_Time_Configuration_Integration implements Integration_Interface {
 				WPSEO_Utils::format_json_encode( $social_profiles['other_social_urls'] ),
 				$this->product_helper->is_premium(),
 				$this->has_tracking_enabled(),
+				$this->is_tracking_enabled_multisite(),
 				WPSEO_Utils::format_json_encode( $options ),
 				$this->should_force_company(),
 				$knowledge_graph_message,
@@ -381,6 +383,21 @@ class First_Time_Configuration_Integration implements Integration_Interface {
 		}
 
 		return $this->options_helper->get( 'tracking', $default );
+	}
+
+	/**
+	 * Checks whether tracking option is allowed at network level.
+	 *
+	 * @return bool True if option change is allowed, false otherwise.
+	 */
+	private function is_tracking_enabled_multisite() {
+		$default = true;
+
+		if ( ! \is_multisite() ) {
+			return $default;
+		}
+
+		return $this->options_helper->get( 'allow_tracking', $default );
 	}
 
 	/**
