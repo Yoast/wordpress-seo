65ded344875a5587256f8633017141f44bd44159
Enrico Battocchi <enrico.battocchi@gmail.com>
2022-05-17 10:22:24 +0200

    Strings i18n

--- admin/views/redirects.php
+++ admin/views/redirects.php
@@ -19,7 +19,7 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 		<div style="position: absolute;top: 0;bottom: 0;left: 0;right: 0;z-index: 100; display: flex;justify-content: center;align-items: center;background: radial-gradient(#ffffffcf 20%, #ffffff00 50%);">
 			<a class="yoast-button-upsell" href="<?php echo \esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/4e0/' ) ); ?>" target="_blank">
 				<?php
-				echo \esc_html__( 'Unlock with Premium', 'wordpress-seo' )
+				\esc_html_e( 'Unlock with Premium', 'wordpress-seo' )
 					// phpcs:ignore WordPress.Security.EscapeOutput -- Already escapes correctly.
 					. WPSEO_Admin_Utils::get_new_tab_message();
 				?>
@@ -27,34 +27,79 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 		</div>
 		<div class="wpseo_content_cell" id="wpseo_content_top" style="opacity: 0.5;">
 			<h2 class="nav-tab-wrapper" id="wpseo-tabs">
-				<a class="nav-tab nav-tab-active" id="tab-url-tab" href="#" tabindex="-1">Redirects</a>
-				<a class="nav-tab" id="tab-url-tab" href="" tabindex="-1">Regex Redirects</a>
-				<a class="nav-tab" id="tab-url-tab" href="#" tabindex="-1">Settings</a></h2>
-
+				<a class="nav-tab nav-tab-active" id="tab-url-tab" href="#" tabindex="-1">
+					<?php
+					\esc_html_e( 'Redirects', 'wordpress-seo' )
+					?>
+				</a>
+				<a class="nav-tab" id="tab-url-tab" href="" tabindex="-1">
+					<?php
+					\esc_html_e( 'Regex Redirects', 'wordpress-seo' )
+					?>
+				</a>
+				<a class="nav-tab" id="tab-url-tab" href="#" tabindex="-1">
+					<?php
+					\esc_html_e( 'Settings', 'wordpress-seo' )
+					?>
+					</a>
+			</h2>
 
 			<div id="table-plain" class="tab-url redirect-table-tab">
-				<h2>Plain redirects</h2>
+				<h2>
+					<?php
+					\esc_html_e( 'Plain redirects', 'wordpress-seo' )
+					?>
+				</h2>
 				<form class="wpseo-new-redirect-form" method="post">
 					<div class="wpseo_redirect_form">
 						<div class="redirect_form_row" id="row-wpseo_redirects_type">
 							<label class="textinput" for="wpseo_redirects_type">
-								<span class="title">Type</span>
+								<span class="title">
+									<?php
+									\esc_html_e( 'Type', 'wordpress-seo' )
+									?>
+								</span>
 							</label>
 							<select name="wpseo_redirects_type" id="wpseo_redirects_type" class="select select2-hidden-accessible" data-select2-id="wpseo_redirects_type" tabindex="-1" aria-hidden="true">
-								<option value="301" data-select2-id="2">301 Moved Permanently</option>
-								<option value="302">302 Found</option>
-								<option value="307">307 Temporary Redirect</option>
-								<option value="410">410 Content Deleted</option>
-								<option value="451">451 Unavailable For Legal Reasons</option>
-							</select><span class="select2 select2-container select2-container--default" dir="ltr" data-select2-id="1" style="width: 400px;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="-1" aria-disabled="false" aria-labelledby="select2-wpseo_redirects_type-container"><span class="select2-selection__rendered" id="select2-wpseo_redirects_type-container" role="textbox" aria-readonly="true" title="301 Moved Permanently">301 Moved Permanently</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+								<option value="301" data-select2-id="2">
+									<?php
+									\esc_html_e( '301 Moved Permanently', 'wordpress-seo' )
+									?>
+								</option>
+							</select>
+							<span class="select2 select2-container select2-container--default" dir="ltr" data-select2-id="1" style="width: 400px;">
+								<span class="selection">
+									<span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="-1" aria-disabled="false" aria-labelledby="select2-wpseo_redirects_type-container">
+										<span class="select2-selection__rendered" id="select2-wpseo_redirects_type-container" role="textbox" aria-readonly="true" title="301 Moved Permanently">
+											<?php
+											\esc_html_e( '301 Moved Permanently', 'wordpress-seo' )
+											?>
+										</span>
+										<span class="select2-selection__arrow" role="presentation">
+											<b role="presentation"></b>
+										</span>
+									</span>
+								</span>
+								<span class="dropdown-wrapper" aria-hidden="true"></span>
+							</span>
 						</div>
 
 						<p class="label desc description wpseo-redirect-clear">
-							The redirect type is the HTTP response code sent to the browser telling the browser what type of redirect is served. <a href="#" target="_blank" tabindex="-1">Learn more about redirect types</a>.</p>
-
+							<?php
+							printf(
+								/* translators: 1: opens a link. 2: closes the link. */
+								esc_html__( 'The redirect type is the HTTP response code sent to the browser telling the browser what type of redirect is served. %1$sLearn more about redirect types%2$s.', 'wordpress-seo' ),
+								'<a href="#" target="_blank">',
+								'</a>'
+							);
+							?>
 						<div class="redirect_form_row" id="row-wpseo_redirects_origin">
 							<label class="textinput" for="wpseo_redirects_origin">
-								<span class="title">Old URL</span>
+								<span class="title">
+									<?php
+									\esc_html_e( 'Old URL', 'wordpress-seo' )
+									?>
+								</span>
 							</label>
 							<input type="text" class="textinput" name="wpseo_redirects_origin" id="wpseo_redirects_origin" value="" tabindex="-1">
 						</div>
@@ -62,16 +107,24 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 
 						<div class="redirect_form_row wpseo_redirect_target_holder" id="row-wpseo_redirects_target">
 							<label class="textinput" for="wpseo_redirects_target">
-								<span class="title">URL</span>
+								<span class="title">
+									<?php
+									\esc_html_e( 'URL', 'wordpress-seo' )
+									?>
+								</span>
 							</label>
 							<input type="text" class="textinput" name="wpseo_redirects_target" id="wpseo_redirects_target" value="" tabindex="-1">
 						</div>
 						<br class="clear">
 
-						<button type="button" class="button button-primary" tabindex="-1">Add Redirect</button>
+						<button type="button" class="button button-primary" tabindex="-1">
+							<?php
+							\esc_html_e( 'Add Redirect', 'wordpress-seo' )
+							?>
+						</button>
 					</div>
 				</form>
-
+				<p class="desc">&nbsp;</p>
 				<form id="plain" class="wpseo-redirects-table-form" method="post" action="">
 					<input type="hidden" class="wpseo_redirects_ajax_nonce" name="wpseo_redirects_ajax_nonce" value="6ccb86df42">
 					<input type="hidden" id="_wpnonce" name="_wpnonce" value="4b02cca185">
@@ -79,23 +132,13 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 
 					<div class="alignleft actions">
 						<select name="redirect-type" id="filter-by-redirect" tabindex="-1">
-							<option selected="selected" value="0">All redirect types</option>
-							<option value="301">301 Moved Permanently</option>
-							<option value="302">302 Found</option>
-							<option value="307">307 Temporary Redirect</option>
-							<option value="410">410 Content Deleted</option>
-							<option value="451">451 Unavailable For Legal Reasons</option>
+							<option selected="selected" value="0">
+								<?php
+								\esc_html_e( 'All redirect types', 'wordpress-seo' )
+								?>
+							</option>
 						</select>
-						<input type="submit" name="filter_action" id="post-query-submit" class="button" value="Filter" tabindex="-1">
-					</div>
-					<div class="tablenav-pages no-pages"><span class="displaying-num">0 items</span>
-						<span class="pagination-links"><span class="tablenav-pages-navspan button disabled" aria-hidden="true">«</span>
-						<span class="tablenav-pages-navspan button disabled" aria-hidden="true">‹</span>
-						<span class="paging-input">
-							<input class="current-page" id="current-page-selector" type="text" name="paged" value="1" size="1" aria-describedby="table-paging"><span class="tablenav-paging-text"> of <span class="total-pages">0</span></span></span>
-							<a class="next-page button" href="#"><span aria-hidden="true">›</span></a>
-							<a class="last-page button" href="#"><span aria-hidden="true">»</span></a>
-						</span>
+						<input type="submit" name="filter_action" id="post-query-submit" class="button" value="<?php \esc_attr_e( 'Filter', 'wordpress-seo' ); ?>" tabindex="-1">
 					</div>
 						<br class="clear">
 					</div>
@@ -106,19 +149,46 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 									<input id="cb-select-all-1" type="checkbox"  tabindex="-1">
 								</td>
 								<th scope="col" id="type" class="manage-column column-type column-primary sortable desc">
-									<a href="#" tabindex="-1"><span>Type</span><span class="sorting-indicator"></span></a>
+									<a href="#" tabindex="-1">
+										<span>
+										<?php
+										\esc_html_e( 'Type', 'wordpress-seo' )
+										?>
+										</span>
+										<span class="sorting-indicator"></span>
+									</a>
 								</th>
 								<th scope="col" id="old" class="manage-column column-old sortable desc">
-									<a href="#" tabindex="-1"><span>Old URL</span><span class="sorting-indicator"></span></a>
+									<a href="#" tabindex="-1">
+										<span>
+											<?php
+											\esc_html_e( 'Old URL', 'wordpress-seo' )
+											?>
+										</span>
+										<span class="sorting-indicator"></span>
+									</a>
 								</th>
 								<th scope="col" id="new" class="manage-column column-new sortable desc">
-									<a href="#" tabindex="-1"><span>New URL</span><span class="sorting-indicator"></span></a>
+									<a href="#" tabindex="-1">
+										<span>
+											<?php
+											\esc_html_e( 'New URL', 'wordpress-seo' )
+											?>
+										</span>
+										<span class="sorting-indicator"></span>
+									</a>
 								</th>
 							</tr>
 						</thead>
 
 						<tbody id="the-list">
-							<tr class="no-items"><td class="colspanchange" colspan="4">No items found.</td></tr>
+							<tr class="no-items">
+								<td class="colspanchange" colspan="4">
+									<?php
+									\esc_html_e( 'No items found.', 'wordpress-seo' )
+									?>
+								</td>
+							</tr>
 						</tbody>
 
 						<tfoot>
@@ -127,28 +197,37 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 									<input id="cb-select-all-2" type="checkbox" tabindex="-1">
 								</td>
 								<th scope="col" class="manage-column column-type column-primary sortable desc">
-									<a href="#" tabindex="-1"><span>Type</span><span class="sorting-indicator"></span></a>
+									<a href="#" tabindex="-1">
+										<span>
+											<?php
+											\esc_html_e( 'Type', 'wordpress-seo' )
+											?>
+										</span>
+										<span class="sorting-indicator"></span></a>
 								</th>
 								<th scope="col" class="manage-column column-old sortable desc">
-									<a href="#" tabindex="-1"><span>Old URL</span><span class="sorting-indicator"></span></a>
+									<a href="#" tabindex="-1">
+										<span>
+											<?php
+											\esc_html_e( 'Old URL', 'wordpress-seo' )
+											?>
+										</span>
+										<span class="sorting-indicator"></span>
+									</a>
 								</th>
 								<th scope="col" class="manage-column column-new sortable desc">
-									<a href="#" tabindex="-1"><span>New URL</span><span class="sorting-indicator"></span></a>
+									<a href="#" tabindex="-1">
+										<span>
+											<?php
+											\esc_html_e( 'New URL', 'wordpress-seo' )
+											?>
+										</span>
+										<span class="sorting-indicator"></span></a>
 								</th>
 							</tr>
 						</tfoot>
 
 					</table>
-					<div class="tablenav bottom">
-
-						<div class="tablenav-pages no-pages"><span class="displaying-num">0 items</span>
-							<span class="pagination-links"><span class="tablenav-pages-navspan button disabled" aria-hidden="true">«</span>
-<span class="tablenav-pages-navspan button disabled" aria-hidden="true">‹</span>
-<span id="table-paging" class="paging-input"><span class="tablenav-paging-text">1 of <span class="total-pages">0</span></span></span>
-<a class="next-page button" href="#"><span aria-hidden="true">›</span></a>
-<a class="last-page button" href="#"><span aria-hidden="true">»</span></a></span></div>
-						<br class="clear">
-					</div>
 				</form>
 			</div>
 
