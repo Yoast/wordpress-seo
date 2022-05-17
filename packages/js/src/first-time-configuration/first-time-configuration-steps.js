84d5d39b3536b1af0ae0ea50cdcbc9843309d5fb
Paolo L. Scala <paolo.scala@gmail.com>
2022-05-17 11:29:28 +0200

    Prevents advancement from site representatio step if a network error occurs

--- packages/js/src/first-time-configuration/first-time-configuration-steps.js
+++ packages/js/src/first-time-configuration/first-time-configuration-steps.js
@@ -292,10 +292,17 @@ export default function FirstTimeConfigurationSteps() {
 			return false;
 		}
 		setSiteRepresentationEmpty( state.companyOrPerson === "emptyChoice" || isCompanyAndEmpty || isPersonAndEmpty );
-		updateSiteRepresentation( state )
+		return updateSiteRepresentation( state )
 			.then( () => setStepIsSaved( 2 ) )
-			.then( () => finishSteps( STEPS.siteRepresentation ) );
-		return true;
+			.then( () => {
+				setErrorFields( [] );
+				finishSteps( STEPS.siteRepresentation );
+				return true;
+			} )
+			.catch( ( e ) => {
+				setErrorFields( [ "site_representation", e.message ] );
+				return false;
+			} );
 	}
 
 	/**
