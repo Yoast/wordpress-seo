e76aca31a6ab52b803fba014737576ee993f80c2
Hans-Christiaan <hans-christiaan@yoast.com>
2022-05-17 13:58:26 +0200

    Made first time configuration work with refactored renderReactRoot function.

--- packages/js/src/first-time-configuration.js
+++ packages/js/src/first-time-configuration.js
@@ -2,10 +2,21 @@ import domReady from "@wordpress/dom-ready";
 
 import { renderReactRoot } from "./helpers/reactRoot";
 import FirstTimeConfigurationSteps from "./first-time-configuration/first-time-configuration-steps";
+import getL10nObject from "./analysis/getL10nObject";
 
 domReady( () => {
 	const rootId = "wpseo-first-time-configuration";
 	if ( document.getElementById( rootId ) ) {
-		renderReactRoot( rootId, <FirstTimeConfigurationSteps /> );
+		const localizedData = getL10nObject();
+
+		const theme = {
+			isRtl: localizedData.isRtl,
+		};
+
+		renderReactRoot( {
+			target: rootId,
+			children: [ <FirstTimeConfigurationSteps/> ],
+			theme,
+		} );
 	}
 } );
