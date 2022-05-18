dd6e653cbd38c4047856f5b18df57bacafab6a5b
hdvos <hdvos93@gmail.com>
2022-05-18 16:32:08 +0200

    add test to test changed isApplicable method

--- packages/yoastseo/spec/scoring/assessments/readability/wordComplexityAssessmentSpec.js
+++ packages/yoastseo/spec/scoring/assessments/readability/wordComplexityAssessmentSpec.js
@@ -3,6 +3,7 @@ import DefaultResearcher from "../../../../src/languageProcessing/languages/_def
 import wordComplexityAssessment from "../../../../src/scoring/assessments/readability/wordComplexityAssessment.js";
 import Paper from "../../../../src/values/Paper.js";
 import factory from "../../../specHelpers/factory.js";
+import fleschReadingAssessment from "../../../../src/scoring/assessments/readability/fleschReadingEaseAssessment";
 
 describe( "an assessment returning complex words", function() {
 	it( "runs a test with an empty text", function() {
@@ -179,6 +180,17 @@ describe( "tests for the assessment applicability", function() {
 	it( "returns false by default because the word complexity assessment is currently disabled", function() {
 		const paper = new Paper( "hallo" );
 		expect( wordComplexityAssessment.isApplicable( paper, new DefaultResearcher( paper ) ) ).toBe( false );
+	} );
+
+	it( "returns false for isApplicable for an English paper with only an image.", function() {
+		// eslint-disable-next-line max-len
+		const paper = new Paper( "<img src=\"https://yoast.com/cdn-cgi/image/width=466%2Cheight=244%2Cfit=crop%2Cf=auto%2Conerror=redirect//app/uploads/2017/12/Focus_keyword_FI.jpg\">", { locale: "en_US" } );
+		expect( fleschReadingAssessment.isApplicable( paper, new EnglishResearcher( paper ) ) ).toBe( false );
+	} );
+
+	it( "returns false for isApplicable for an English paper with only spaces.", function() {
+		const paper = new Paper( "        ", { locale: "en_US" } );
+		expect( fleschReadingAssessment.isApplicable( paper, new EnglishResearcher( paper ) ) ).toBe( false );
 	} );
 } );
 
