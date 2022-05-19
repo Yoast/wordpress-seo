a5a128a102b26bafad8b1d1546a2adc04a2981cd
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-05-19 12:57:03 +0300

    Call the WP function instead of duplicating it

--- src/integrations/front-end/crawl-cleanup-rss.php
+++ src/integrations/front-end/crawl-cleanup-rss.php
@@ -44,95 +44,25 @@ class Crawl_Cleanup_Rss implements Integration_Interface {
 	 * @codeCoverageIgnore
 	 */
 	public function register_hooks() {
-		if ( $this->options_helper->get( 'remove_feed_post_comments' ) === true ) {
-			// Replace core's feed_links_extra with our own which allows us control over which feeds to show.
-			remove_action( 'wp_head', 'feed_links_extra', 3 );
-			add_action( 'wp_head', [ $this, 'feed_links_extra_replacement' ], 3 );
-		}
+		// Replace core's feed_links_extra with our own which allows us control over which feeds to show.
+		remove_action( 'wp_head', 'feed_links_extra', 3 );
+		add_action( 'wp_head', [ $this, 'feed_links_extra_replacement' ], 3 );
 
 		// Redirect the ones we don't want to exist.
 		add_action( 'wp', [ $this, 'redirect_unwanted_feeds' ], - 10000 );
 	}
 
 	/**
-	 * Adapted from `feed_links_extra` in WP core, this is a version that allows us to control which feeds to show.
+	 * Output feed links the WP way, but not the ones we have disabled.
 	 *
 	 * @param array $args Optional arguments.
 	 */
 	public function feed_links_extra_replacement( $args = [] ) {
-		$defaults = [
-			/* translators: Separator between blog name and feed type in feed links. */
-			'separator'     => _x( '&raquo;', 'feed link', 'wordpress-seo' ),
-			/* translators: 1: Blog name, 2: Separator (raquo), 3: Post title. */
-			'singletitle'   => __( '%1$s %2$s %3$s Comments Feed', 'wordpress-seo' ),
-			/* translators: 1: Blog name, 2: Separator (raquo), 3: Category name. */
-			'cattitle'      => __( '%1$s %2$s %3$s Category Feed', 'wordpress-seo' ),
-			/* translators: 1: Blog name, 2: Separator (raquo), 3: Tag name. */
-			'tagtitle'      => __( '%1$s %2$s %3$s Tag Feed', 'wordpress-seo' ),
-			/* translators: 1: Blog name, 2: Separator (raquo), 3: Term name, 4: Taxonomy singular name. */
-			'taxtitle'      => __( '%1$s %2$s %3$s %4$s Feed', 'wordpress-seo' ),
-			/* translators: 1: Blog name, 2: Separator (raquo), 3: Author name. */
-			'authortitle'   => __( '%1$s %2$s Posts by %3$s Feed', 'wordpress-seo' ),
-			/* translators: 1: Blog name, 2: Separator (raquo), 3: Search query. */
-			'searchtitle'   => __( '%1$s %2$s Search Results for &#8220;%3$s&#8221; Feed', 'wordpress-seo' ),
-			/* translators: 1: Blog name, 2: Separator (raquo), 3: Post type name. */
-			'posttypetitle' => __( '%1$s %2$s %3$s Feed', 'wordpress-seo' ),
-		];
-
-		$args = \wp_parse_args( $args, $defaults );
-
-		if ( \is_singular() ) {
+		if ( \is_singular() && $this->options_helper->get( 'remove_feed_post_comments' ) === true ) {
 			return;
 		}
-		elseif ( \is_post_type_archive() ) {
-			$post_type = get_query_var( 'post_type' );
-			if ( \is_array( $post_type ) ) {
-				$post_type = reset( $post_type );
-			}
 
-			$post_type_obj = \get_post_type_object( $post_type );
-			$title         = \sprintf( $args['posttypetitle'], \get_bloginfo( 'name' ), $args['separator'], $post_type_obj->labels->name );
-			$href          = \get_post_type_archive_feed_link( $post_type_obj->name );
-		}
-		elseif ( \is_category() ) {
-			$term = \get_queried_object();
-
-			if ( $term ) {
-				$title = \sprintf( $args['cattitle'], \get_bloginfo( 'name' ), $args['separator'], $term->name );
-				$href  = \get_category_feed_link( $term->term_id );
-			}
-		}
-		elseif ( \is_tag() ) {
-			$term = \get_queried_object();
-
-			if ( $term ) {
-				$title = \sprintf( $args['tagtitle'], \get_bloginfo( 'name' ), $args['separator'], $term->name );
-				$href  = \get_tag_feed_link( $term->term_id );
-			}
-		}
-		elseif ( is_tax() ) {
-			$term = get_queried_object();
-
-			if ( $term ) {
-				$tax   = \get_taxonomy( $term->taxonomy );
-				$title = \sprintf( $args['taxtitle'], \get_bloginfo( 'name' ), $args['separator'], $term->name, $tax->labels->singular_name );
-				$href  = \get_term_feed_link( $term->term_id, $term->taxonomy );
-			}
-		}
-		elseif ( \is_author() ) {
-			$author_id = (int) \get_query_var( 'author' );
-
-			$title = \sprintf( $args['authortitle'], \get_bloginfo( 'name' ), $args['separator'], \get_the_author_meta( 'display_name', $author_id ) );
-			$href  = \get_author_feed_link( $author_id );
-		}
-		elseif ( \is_search() ) {
-			$title = \sprintf( $args['searchtitle'], \get_bloginfo( 'name' ), $args['separator'], \get_search_query( false ) );
-			$href  = \get_search_feed_link();
-		}
-
-		if ( isset( $title ) && isset( $href ) ) {
-			echo '<link rel="alternate" type="' . \esc_attr( \feed_content_type() ) . '" title="' . \esc_attr( $title ) . '" href="' . \esc_url( $href ) . '" />' . "\n";
-		}
+		\feed_links_extra( $args );
 	}
 
 	/**
