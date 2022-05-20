311948e104cbee9a7cd5dc86a8310e8f3985165f
Hanna Worku <hanna@yoast.com>
2022-05-20 14:12:05 +0200

    replace em dashes with spaces

--- packages/yoastseo/spec/languageProcessing/helpers/sanitize/removePunctuationSpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/sanitize/removePunctuationSpec.js
@@ -32,12 +32,15 @@ describe( "Removing punctuation at the begin and end of a word", function() {
 		expect( removePunctuation( "'word'" ) ).toBe( "word" );
 		expect( removePunctuation( "'word'!!!???!!!!" ) ).toBe( "word" );
 		expect( removePunctuation( "'word–" ) ).toBe( "word" );
-		expect( removePunctuation( "'word—" ) ).toBe( "word" );
+		expect( removePunctuation( "'word—" ) ).toBe( "word " );
 		expect( removePunctuation( "'word×" ) ).toBe( "word" );
 		expect( removePunctuation( "'word+" ) ).toBe( "word" );
 		expect( removePunctuation( "'word&" ) ).toBe( "word" );
 		expect( removePunctuation( "“word”" ) ).toBe( "word" );
 		expect( removePunctuation( "„word‟" ) ).toBe( "word" );
+		expect( removePunctuation( "„word‟" ) ).toBe( "word" );
+		expect( removePunctuation( "—word—" ) ).toBe( " word " );
+		expect( removePunctuation( "word—" ) ).toBe( "word " );
 	} );
 } );
 
