0de5dd24d52f79777e3a7b73c22d684b1ff062c7
Martijn van der Klis <martijnvanderklis@yoast.com>
2022-05-25 15:43:26 +0200

    Do not add a space when removing the em dash

--- packages/yoastseo/spec/languageProcessing/helpers/sanitize/removePunctuationSpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/sanitize/removePunctuationSpec.js
@@ -32,15 +32,14 @@ describe( "Removing punctuation at the begin and end of a word", function() {
 		expect( removePunctuation( "'word'" ) ).toBe( "word" );
 		expect( removePunctuation( "'word'!!!???!!!!" ) ).toBe( "word" );
 		expect( removePunctuation( "'word–" ) ).toBe( "word" );
-		expect( removePunctuation( "'word—" ) ).toBe( "word " );
+		expect( removePunctuation( "'word—" ) ).toBe( "word" );
 		expect( removePunctuation( "'word×" ) ).toBe( "word" );
 		expect( removePunctuation( "'word+" ) ).toBe( "word" );
 		expect( removePunctuation( "'word&" ) ).toBe( "word" );
 		expect( removePunctuation( "“word”" ) ).toBe( "word" );
 		expect( removePunctuation( "„word‟" ) ).toBe( "word" );
 		expect( removePunctuation( "„word‟" ) ).toBe( "word" );
-		expect( removePunctuation( "—word—" ) ).toBe( " word " );
-		expect( removePunctuation( "word—" ) ).toBe( "word " );
+		expect( removePunctuation( "—word—" ) ).toBe( "word" );
 	} );
 } );
 
