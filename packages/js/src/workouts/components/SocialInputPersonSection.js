8726c5806da2668f7093c4f0d01b84a0114a0eb9
Paolo L. Scala <paolo.scala@gmail.com>
2022-05-25 11:34:08 +0200

    Fix misplaced translators comment

--- packages/js/src/workouts/components/SocialInputPersonSection.js
+++ packages/js/src/workouts/components/SocialInputPersonSection.js
@@ -23,8 +23,8 @@ function SocialInputPersonSection( { personId } ) {
 						{
 							createInterpolateElement(
 								sprintf(
+									// translators: %1$s and %2$s are replaced by opening and closing <b> tags, %3$s and %4$s are replaced by opening and closing anchor tags
 									__(
-										// translators: %1$s and %2$s are replaced by opening and closing <b> tags, %3$s and %4$s are replaced by opening and closing anchor tags
 										"%1$sImportant%2$s: Please select a name in step 2 for this step to be effective.",
 										"wordpress-seo"
 									),
@@ -41,8 +41,8 @@ function SocialInputPersonSection( { personId } ) {
 						{
 							addLinkToString(
 								sprintf(
+									// translators: %1$s and %2$s are replaced by opening and closing anchor tags
 									__(
-										// translators: %1$s and %2$s are replaced by opening and closing anchor tags
 										"In this step, you need to add the personal social profiles of the person your site represents. To do that, you should go to the %1$sUsers%2$s > Profile page in a new browser tab. Alternatively, ask the user or an admin to do it if you are not allowed.",
 										"wordpress-seo"
 									),
@@ -58,8 +58,8 @@ function SocialInputPersonSection( { personId } ) {
 						{
 							addLinkToString(
 								sprintf(
+									// translators: %1$s and %2$s are replaced by opening and closing anchor tags.
 									__(
-										// translators: %1$s and %2$s are replaced by opening and closing anchor tags.
 										"On the %1$sUsers%2$s page, hover your mouse over the username you want to edit. Click ‘Edit’ to access the user’s profile. Then, scroll down to the ‘Contact info’ section (see screenshot below) and fill in the URLs of the personal social profiles you want to add.",
 										"wordpress-seo"
 									),
@@ -79,8 +79,8 @@ function SocialInputPersonSection( { personId } ) {
 						{
 							addLinkToString(
 								sprintf(
+									// translators: %1$s and %2$s are replaced by opening and closing <b> tags, %3$s and %4$s are replaced by opening and closing anchor tags
 									__(
-										// translators: %1$s and %2$s are replaced by opening and closing <b> tags, %3$s and %4$s are replaced by opening and closing anchor tags
 										"In this step, you need to add the personal social profiles of the person your site represents. To do that, you should go to the user’s %1$sProfile page%2$s (opens in a new browser tab). Then, scroll down to the ‘Contact info’ section (see screenshot below) and fill in the URLs of the personal social profiles you want to add. Alternatively, ask the user or an admin to do it if you are not allowed.",
 										"wordpress-seo"
 									),
