6cbae9eec16e13c740482aacfba741fa247f31c1
Martijn van der Klis <martijnvanderklis@yoast.com>
2022-05-25 15:56:27 +0200

    Keeping the same order and removing duplicate tests

--- packages/yoastseo/spec/languageProcessing/helpers/sanitize/removePunctuationSpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/sanitize/removePunctuationSpec.js
@@ -38,8 +38,6 @@ describe( "Removing punctuation at the begin and end of a word", function() {
 		expect( removePunctuation( "'word&" ) ).toBe( "word" );
 		expect( removePunctuation( "“word”" ) ).toBe( "word" );
 		expect( removePunctuation( "„word‟" ) ).toBe( "word" );
-		expect( removePunctuation( "„word‟" ) ).toBe( "word" );
-		expect( removePunctuation( "—word—" ) ).toBe( "word" );
 	} );
 } );
 
