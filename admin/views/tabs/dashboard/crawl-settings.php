e2f90d561479a11c93e06955b33f563f52a561fe
Paolo L. Scala <paolo.scala@gmail.com>
2022-05-26 10:57:29 +0200

    Add remove_atom_rdf_feeds toggle to crawl settings tab

--- admin/views/tabs/dashboard/crawl-settings.php
+++ admin/views/tabs/dashboard/crawl-settings.php
@@ -32,13 +32,6 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 	);
 	echo '</p>';
 
-	$help_text  = esc_html__( 'This removes the Post Comment Feed link.', 'wordpress-seo' );
-	$help_text .= ' ';
-	$help_text .= sprintf(
-		'<a href="#" target="_blank" rel="noopener noreferrer">%1$s</a>',
-		esc_html__( 'Find out how removing feeds can improve performance.', 'wordpress-seo' )
-	);
-
 	$yform->toggle_switch(
 		'remove_feed_post_comments',
 		[
@@ -46,6 +39,15 @@ if ( ! defined( 'WPSEO_VERSION' ) ) {
 			'on'  => __( 'Remove', 'wordpress-seo' ),
 		],
 		__( 'Post comment feeds', 'wordpress-seo' )
+	);
+
+	$yform->toggle_switch(
+		'remove_atom_rdf_feeds',
+		[
+			'off' => __( 'Keep', 'wordpress-seo' ),
+			'on'  => __( 'Remove', 'wordpress-seo' ),
+		],
+		__( 'All Atom and RDF feeds', 'wordpress-seo' )
 	);
 	?>
 </div>
