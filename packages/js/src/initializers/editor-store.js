94fc0791d08da7ff1313ed752b84d67f45280802
nolledgeable <nolle@yoast.com>
2022-06-01 18:22:49 +0200

    Add is premium boolean to Redux stores

--- packages/js/src/initializers/editor-store.js
+++ packages/js/src/initializers/editor-store.js
@@ -35,6 +35,7 @@ const populateStore = store => {
 	store.dispatch( actions.setWincherAutomaticKeyphaseTracking( window.wpseoScriptData.metabox.wincherAutoAddKeyphrases ) );
 
 	store.dispatch( actions.setDismissedAlerts( get( window, "wpseoScriptData.dismissedAlerts", {} ) ) );
+	store.dispatch( actions.setIsPremium( Boolean( get( window, "wpseoScriptData.metabox.isPremium", false ) ) ) );
 };
 
 /**
--- packages/js/src/initializers/settings-store.js
+++ packages/js/src/initializers/settings-store.js
@@ -3,9 +3,9 @@ import { reducers, selectors, actions } from "@yoast/externals/redux";
 import { get } from "lodash";
 import * as controls from "../redux/controls/dismissedAlerts";
 
-const { dismissedAlerts, settings, snippetEditor } = reducers;
-const { getRecommendedReplaceVars, getReplaceVars, isAlertDismissed } = selectors;
-const { dismissAlert, setDismissedAlerts, setSettings, updateReplacementVariable } = actions;
+const { dismissedAlerts, settings, snippetEditor, isPremium } = reducers;
+const { getRecommendedReplaceVars, getReplaceVars, isAlertDismissed, getIsPremium } = selectors;
+const { dismissAlert, setDismissedAlerts, setSettings, updateReplacementVariable, setIsPremium } = actions;
 
 /**
  * Populates the store.
@@ -37,6 +37,7 @@ function populateStore( store ) {
 	} );
 
 	store.dispatch( setDismissedAlerts( get( window, "wpseoScriptData.dismissedAlerts", {} ) ) );
+	store.dispatch( setIsPremium( Boolean( get( window, "wpseoScriptData.isPremium", false ) ) ) );
 }
 
 /**
@@ -50,15 +51,18 @@ export default function initSettingsStore() {
 			dismissedAlerts,
 			settings,
 			snippetEditor,
+			isPremium,
 		} ),
 		selectors: {
 			isAlertDismissed,
 			getReplaceVars,
 			getRecommendedReplaceVars,
+			getIsPremium,
 		},
 		actions: {
 			dismissAlert,
 			setDismissedAlerts,
+			setIsPremium,
 		},
 		controls,
 	} );
