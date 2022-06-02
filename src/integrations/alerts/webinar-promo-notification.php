aea188648de22018fe868d9f73e8861b2e07645e
nolledgeable <nolle@yoast.com>
2022-06-02 17:47:14 +0200

    Remove unused conditionals

--- src/integrations/alerts/webinar-promo-notification.php
+++ src/integrations/alerts/webinar-promo-notification.php
@@ -2,9 +2,6 @@
 
 namespace Yoast\WP\SEO\Integrations\Alerts;
 
-use Yoast\WP\SEO\Conditionals\Yoast_Admin_And_Dashboard_Conditional;
-use Yoast\WP\SEO\Conditionals\Premium_Inactive_Conditional;
-
 /**
  * Webinar_Promo_Notification class.
  */
