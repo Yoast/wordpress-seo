9704fcda6cc746a69e751135b68323f670be67b6
Martijn van der Klis <martijnvanderklis@yoast.com>
2022-06-08 14:34:11 +0200

    Improves the variable naming as well as the comments

--- packages/yoastseo/src/languageProcessing/helpers/html/htmlParser.js
+++ packages/yoastseo/src/languageProcessing/helpers/html/htmlParser.js
@@ -1,17 +1,16 @@
 // We use an external library, which can be found here: https://github.com/fb55/htmlparser2.
 import htmlparser from "htmlparser2";
 
-
 import { includes } from "lodash-es";
 
-// The array containing the text parts without the blocks defined in inlineTags.
+// The array containing the text parts without the blocks defined in ignoredTags.
 let textArray;
 
-// False when we are not in a block defined in inlineTags. True if we are.
-let inScriptBlock = false;
+// False when we are not in a block defined in ignoredTags. True if we are.
+let inIgnorableBlock = false;
 
 // The blocks we filter out of the text that needs to be parsed.
-const inlineTags = [ "script", "style", "code", "pre", "blockquote" ];
+const ignoredTags = [ "script", "style", "code", "pre", "blockquote" ];
 
 /**
  * Parses the text.
@@ -19,8 +18,8 @@ const inlineTags = [ "script", "style", "code", "pre", "blockquote" ];
 const parser = new htmlparser.Parser( {
 	/**
 	 * Handles the opening tag.
-	 * If we are in a script block, disregard the tag.
-	 * If the opening tag is included in the inlineTags array, set inScriptBlock to true.
+	 * If we are in an ignorable block, disregard the tag.
+	 * If the opening tag is included in the ignoredTags array, set inIgnorableBlock to true.
 	 * Otherwise, push the tag to the textArray.
 	 *
 	 * @param {string} tagName The tag name.
@@ -29,12 +28,12 @@ const parser = new htmlparser.Parser( {
 	 * @returns {void}
 	 */
 	onopentag: function( tagName, nodeValue ) {
-		if ( inScriptBlock ) {
+		if ( inIgnorableBlock ) {
 			return;
 		}
 
-		if ( includes( inlineTags, tagName ) ) {
-			inScriptBlock = true;
+		if ( includes( ignoredTags, tagName ) ) {
+			inIgnorableBlock = true;
 			return;
 		}
 
@@ -49,33 +48,34 @@ const parser = new htmlparser.Parser( {
 		textArray.push( "<" + tagName + nodeValueString + ">" );
 	},
 	/**
-	 * Handles the text that doesn't contain opening or closing tags. If inScriptBlock is false, the text gets pushed to the textArray array.
+	 * Handles the text that doesn't contain opening or closing tags.
+	 * If inIgnorableBlock is false, the text gets pushed to the textArray array.
 	 *
 	 * @param {string} text The text that doesn't contain opening or closing tags.
 	 *
 	 * @returns {void}
 	 */
 	ontext: function( text ) {
-		if ( ! inScriptBlock ) {
+		if ( ! inIgnorableBlock ) {
 			textArray.push( text );
 		}
 	},
 	/**
 	 * Handles the closing tag.
-	 * If the closing tag is included in the inlineTags array, set inScriptBlock to false.
-	 * Otherwise, if we are not currently in a script block, push the tag to the textArray.
+	 * If the closing tag is included in the ignoredTags array, set inIgnorableBlock to false.
+	 * Otherwise, if we are not currently in an ignorable block, push the tag to the textArray.
 	 *
 	 * @param {string} tagName The tag name.
 	 *
 	 * @returns {void}
 	 */
 	onclosetag: function( tagName ) {
-		if ( includes( inlineTags, tagName ) ) {
-			inScriptBlock = false;
+		if ( includes( ignoredTags, tagName ) ) {
+			inIgnorableBlock = false;
 			return;
 		}
 
-		if ( inScriptBlock ) {
+		if ( inIgnorableBlock ) {
 			return;
 		}
 
@@ -84,11 +84,11 @@ const parser = new htmlparser.Parser( {
 }, { decodeEntities: true } );
 
 /**
- * Calls the htmlparser and returns the text without the HTML blocks as defined in the inlineTags array.
+ * Calls the htmlparser and returns the text without the HTML blocks as defined in the ignoredTags array.
  *
  * @param {string} text The text to parse.
  *
- * @returns {string} The text without the HTML blocks as defined in the inlineTags array.
+ * @returns {string} The text without the HTML blocks as defined in the ignoredTags array.
  */
 export default function( text ) {
 	textArray = [];
