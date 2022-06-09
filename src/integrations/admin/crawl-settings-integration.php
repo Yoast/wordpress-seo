ec856b69b3d4ed6cb5f15801729314753109eb48
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-06-09 15:34:31 +0300

    Update feature copy/title

--- src/integrations/admin/crawl-settings-integration.php
+++ src/integrations/admin/crawl-settings-integration.php
@@ -132,7 +132,7 @@ class Crawl_Settings_Integration implements Integration_Interface {
 
 		$this->search_cleanup_settings = [
 			'search_cleanup'          => \__( 'Filter search terms', 'wordpress-seo' ),
-			'search_cleanup_emoji'    => \__( 'Filter searches with emoji', 'wordpress-seo' ),
+			'search_cleanup_emoji'    => \__( 'Filter searches with emoji and other special characters', 'wordpress-seo' ),
 			'search_cleanup_patterns' => \__( 'Filter searches with common spam patterns', 'wordpress-seo' ),
 		];
 	}
