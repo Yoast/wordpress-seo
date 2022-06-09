3bac445d74b3d9e915e1408e887ee5c431d60119
hdvos <hdvos93@gmail.com>
2022-06-09 15:14:38 +0200

    add hasEnoughContentForAssessment to isApplicable

--- packages/yoastseo/src/scoring/assessments/readability/TextPresenceAssessment.js
+++ packages/yoastseo/src/scoring/assessments/readability/TextPresenceAssessment.js
@@ -3,7 +3,7 @@ import { createAnchorOpeningTag } from "../../../helpers/shortlinker";
 import AssessmentResult from "../../../values/AssessmentResult";
 import Assessment from "../assessment";
 import { merge } from "lodash-es";
-import hasEnoughContent from "../../helpers/assessments/hasEnoughContent";
+
 
 /**
  * Represents the assessment that checks whether there is enough text in the paper.
@@ -38,7 +38,7 @@ export default class TextPresenceAssessment extends Assessment {
 	 */
 	// eslint-disable-next-line no-unused-vars
 	getResult( paper, researcher ) {
-		if ( ! hasEnoughContent( paper ) ) {
+		if ( ! this.hasEnoughContentForAssessment( paper ) ) {
 			const result = new AssessmentResult();
 
 			result.setText( sprintf(
