df2f7c7fad4d5082ff35f04507b272e19ee98506
Hans-Christiaan <hans-christiaan@yoast.com>
2022-06-09 12:48:29 +0200

    Initialize use keyphrase assessment in the classic editor integration.

---
+++ packages/js/src/classic-editor/plugins/used-keyphrases-assessment.js
@@ -0,0 +1,53 @@
+import { get } from "lodash-es";
+import { addFilter } from "@wordpress/hooks";
+
+import getL10nObject from "../../analysis/getL10nObject";
+import UsedKeywords from "../../analysis/usedKeywords";
+
+/**
+ * Initializes the used keyphrases assessment.
+ *
+ * @param {string} usedKeyphrasesEndpoint The endpoint to call to retrieve previously used keyphrases.
+ * @param {function} refreshAnalysis The function to call to refresh the analysis.
+ *
+ * @returns {void}
+ */
+export default function initializeUsedKeyphrasesAssessment( usedKeyphrasesEndpoint, refreshAnalysis ) {
+	const localizedData = getL10nObject();
+
+	if ( ! localizedData.previouslyUsedKeywordActive ) {
+		return;
+	}
+
+	const scriptUrl = get(
+		window,
+		[ "wpseoScriptData", "analysis", "worker", "keywords_assessment_url" ],
+		"used-keywords-assessment.js"
+	);
+
+	const usedKeywords = new UsedKeywords(
+		usedKeyphrasesEndpoint,
+		localizedData,
+		refreshAnalysis,
+		scriptUrl
+	);
+	usedKeywords.init();
+
+	let lastFocusKeyphrase = null;
+
+	addFilter(
+		"yoast.seoStore.analysis.processResults",
+		"yoast/free/usedKeyphrasesAssessment",
+		( results, { keyphrases } ) => {
+			if ( keyphrases.focus === lastFocusKeyphrase ) {
+				return results;
+			}
+
+			usedKeywords.setKeyword( keyphrases.focus.keyphrase );
+
+			lastFocusKeyphrase = keyphrases.focus;
+
+			return results;
+		}
+	)
+}
