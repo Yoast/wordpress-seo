b61cbc31c2cd70a65160b51d8642d4f9eb0258db
hdvos <hdvos93@gmail.com>
2022-06-09 15:16:31 +0200

    add hasEnoughContentForAssessment to isApplicable

--- packages/yoastseo/src/scoring/assessments/readability/ParagraphTooLongAssessment.js
+++ packages/yoastseo/src/scoring/assessments/readability/ParagraphTooLongAssessment.js
@@ -7,7 +7,6 @@ import { inRangeEndInclusive as inRange } from "../../helpers/assessments/inRang
 import AssessmentResult from "../../../values/AssessmentResult";
 import Mark from "../../../values/Mark";
 import Assessment from "../assessment";
-import hasEnoughContent from "../../helpers/assessments/hasEnoughContent";
 
 /**
  * Represents the assessment that will look if the text has too long paragraphs.
@@ -224,6 +223,6 @@ export default class ParagraphTooLongAssessment extends Assessment {
 	 * @returns {boolean} Returns true if the assessment is applicable to the paper.
 	 */
 	isApplicable( paper ) {
-		return hasEnoughContent( paper );
+		return this.hasEnoughContentForAssessment( paper );
 	}
 }
