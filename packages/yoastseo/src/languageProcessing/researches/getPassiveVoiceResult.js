9d8dfde57452a6f1f5b6288b164ea8b9e1cbc97e
hdvos <hdvos93@gmail.com>
2022-06-13 13:48:03 +0200

    pass memoized tokenizer to getsentences so that the language specific (German) sentence tokenizer is used.

--- packages/yoastseo/src/languageProcessing/researches/getPassiveVoiceResult.js
+++ packages/yoastseo/src/languageProcessing/researches/getPassiveVoiceResult.js
@@ -16,8 +16,8 @@ import { forEach } from "lodash-es";
 export const getMorphologicalPassives = function( paper, researcher ) {
 	const isPassiveSentence = researcher.getHelper( "isPassiveSentence" );
 	const text = paper.getText();
-	// It's not necessary to pass the memoized tokenizer from the researcher here, since only Japanese has the language specific tokenizer.
-	// Passive voice analysis is not supported in Japanese.
+	// It's not necessary to pass the memoized tokenizer from the researcher here, since only Japanese and German have the language specific tokenizer.
+	// Passive voice analysis is not supported in Japanese. In German, passive voice is periphrastic.
 	const sentences = getSentences( text )
 		.map( function( sentence ) {
 			return new Sentence( sentence );
@@ -53,7 +53,7 @@ export const getMorphologicalPassives = function( paper, researcher ) {
 export const getPeriphrasticPassives = function( paper, researcher ) {
 	const getClauses = researcher.getHelper( "getClauses" );
 	const text = paper.getText();
-	const sentences = getSentences( text )
+	const sentences = getSentences( text, researcher.helpers.memoizedTokenizer )
 		.map( function( sentence ) {
 			return new Sentence( sentence );
 		} );
