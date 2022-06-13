d0ac4ad1f677dffa11cb95c5b058ee059033eaae
hdvos <hdvos93@gmail.com>
2022-06-13 16:49:06 +0200

    add tests for endsWithOrdinalDot

--- packages/yoastseo/spec/languageProcessing/languages/de/helpers/internal/GermanSentenceTokenizerSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/de/helpers/internal/GermanSentenceTokenizerSpec.js
@@ -15,13 +15,28 @@ describe( "Test German extension to sentence tokenizer", () =>{
 		expect( sentenceTokenizer.getSentencesFromTokens( tokens )[ 0 ] ).toBe( "In den 66. Club der Stadt wird nachts getanzt." );
 	} );
 
-	it( "Recognizes when a full-stop is part of a german ordinal.", () =>{
-		const currentSentence = "In den 66.";
+	it( "Recognizes when a full-stop is part of a German ordinal with 1 digits.", () =>{
+		const currentSentence = "In den 1.";
+		expect( sentenceTokenizer.endsWithOrdinalDot( currentSentence ) ).toBe( true );
+	} );
+
+	it( "Recognizes when a full-stop is part of a German ordinal with 2 digits.", () =>{
+		const currentSentence = "In den 12.";
+		expect( sentenceTokenizer.endsWithOrdinalDot( currentSentence ) ).toBe( true );
+	} );
+
+	it( "Recognizes when a full-stop is part of a German ordinal with 3 digits.", () =>{
+		const currentSentence = "In den 123.";
+		expect( sentenceTokenizer.endsWithOrdinalDot( currentSentence ) ).toBe( true );
+	} );
+
+	xit( "Does not recognize when a full-stop is part of a German ordinal with 4 digits (or more). This is by design.", () =>{
+		const currentSentence = "In den 1234.";
 		expect( sentenceTokenizer.endsWithOrdinalDot( currentSentence ) ).toBe( true );
 	} );
 
 	it( "Recognizes when a full-stop is NOT part of a german ordinal.", () =>{
-		const currentSentence = "In den 66. Club der Stadt wird nachts getanzt.";
+		const currentSentence = "In den 12. Club der Stadt wird nachts getanzt.";
 		expect( sentenceTokenizer.endsWithOrdinalDot( currentSentence ) ).toBe( false );
 	} );
 } );
