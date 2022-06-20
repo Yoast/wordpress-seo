32747976c82f874f5856fc0908c6327825aabe1f
Joost de Valk <joost@altha.nl>
2022-06-20 13:23:15 +0200

    Change schema image logic

--- src/generators/schema/main-image.php
+++ src/generators/schema/main-image.php
@@ -29,24 +29,6 @@ class Main_Image extends Abstract_Schema_Piece {
 	public function generate() {
 		$image_id = $this->context->canonical . Schema_IDs::PRIMARY_IMAGE_HASH;
 
-		// The Open Graph image.
-		if ( isset( $this->context->indexable->open_graph_image_id ) && $this->context->indexable->open_graph_image_source === 'set-by-user' ) {
-			$generated_schema              = $this->helpers->schema->image->generate_from_attachment_id( $image_id, $this->context->indexable->open_graph_image_id );
-			$this->context->main_image_url = $generated_schema['url'];
-			$this->context->main_image_id  = $this->context->indexable->open_graph_image_id;
-
-			return $generated_schema;
-		}
-
-		// The Twitter image.
-		if ( isset( $this->context->indexable->twitter_image_id ) && $this->context->indexable->twitter_image_source === 'set-by-user' ) {
-			$generated_schema              = $this->helpers->schema->image->generate_from_attachment_id( $image_id, $this->context->indexable->twitter_image_id );
-			$this->context->main_image_url = $generated_schema['url'];
-			$this->context->main_image_id  = $this->context->indexable->twitter_image_id;
-
-			return $generated_schema;
-		}
-
 		// The featured image.
 		if ( $this->context->main_image_id ) {
 			$generated_schema              = $this->helpers->schema->image->generate_from_attachment_id( $image_id, $this->context->main_image_id );
