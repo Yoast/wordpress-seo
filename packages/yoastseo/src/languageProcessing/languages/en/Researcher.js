115d632cc87d3208b0db06f33058b054d41f77f5
FAMarfuaty <aida@yoast.com>
2022-06-21 11:36:15 +0200

    Should not remove word complexity research

--- packages/yoastseo/src/languageProcessing/languages/en/Researcher.js
+++ packages/yoastseo/src/languageProcessing/languages/en/Researcher.js
@@ -27,8 +27,6 @@ export default class Researcher extends AbstractResearcher {
 	constructor( paper ) {
 		super( paper );
 
-		delete this.defaultResearches.wordComplexity;
-
 		Object.assign( this.config, {
 			language: "en",
 			passiveConstructionType: "periphrastic",
