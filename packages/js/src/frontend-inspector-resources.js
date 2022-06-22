c43b59bc741155e4f65b000cb56e9d932b83fe14
Igor <35524806+igorschoester@users.noreply.github.com>
2022-06-22 16:58:35 +0200

    Remove unused resource

--- packages/js/src/frontend-inspector-resources.js
+++ packages/js/src/frontend-inspector-resources.js
@@ -1,12 +1,10 @@
-import { renderReactRoot } from "./helpers/reactRoot";
+import { ReactComponent as YoastIcon } from "../images/Yoast_icon_kader.svg";
 import getIndicatorForScore from "./analysis/getIndicatorForScore";
 import AnalysisCheck from "./components/AnalysisCheck";
-import { ReactComponent as YoastIcon } from "../images/Yoast_icon_kader.svg";
 
 window.yoast = window.yoast || {};
 
 window.yoast.frontend = {
-	renderReactRoot,
 	getIndicatorForScore,
 	AnalysisCheck,
 	YoastIcon,
