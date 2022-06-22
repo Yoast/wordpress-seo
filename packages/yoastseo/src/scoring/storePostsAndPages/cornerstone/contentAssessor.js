0067f98315f589c103e32f6df2052a079b69fb75
FAMarfuaty <aida@yoast.com>
2022-06-22 12:24:43 +0200

    Add the assessment to shopify blog post, pages and product assessors

--- packages/yoastseo/src/scoring/storePostsAndPages/cornerstone/contentAssessor.js
+++ packages/yoastseo/src/scoring/storePostsAndPages/cornerstone/contentAssessor.js
@@ -8,6 +8,7 @@ import TransitionWords from "../../assessments/readability/TransitionWordsAssess
 import PassiveVoice from "../../assessments/readability/PassiveVoiceAssessment.js";
 import SentenceBeginnings from "../../assessments/readability/SentenceBeginningsAssessment.js";
 import TextPresence from "../../assessments/readability/TextPresenceAssessment.js";
+import WordComplexityAssessment from "../../assessments/readability/WordComplexityAssessment";
 
 /*
  Temporarily disabled:
@@ -67,7 +68,13 @@ const StorePostsAndPagesCornerstoneContentAssessor = function( researcher, optio
 			urlTitle: createAnchorOpeningTag( "https://yoa.st/shopify5" ),
 			urlCallToAction: createAnchorOpeningTag( "https://yoa.st/shopify65" ),
 		} ),
-		// Temporarily disabled: wordComplexity,
+		new WordComplexityAssessment( {
+			scores: {
+				acceptableAmount: 3,
+			},
+			urlTitle: createAnchorOpeningTag( "https://yoa.st/shopify77" ),
+			urlCallToAction: createAnchorOpeningTag( "https://yoa.st/shopify78" ),
+		} ),
 	];
 };
 
