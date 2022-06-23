72521adcf91d8328180693f312165988eef52660
iolandasequino <iolanda@yoast.com>
2022-06-23 12:25:28 +0200

    add a function to check for variants and a test

--- packages/yoastseo/spec/languageProcessing/researches/productIdentifierDataSpec.js
+++ packages/yoastseo/spec/languageProcessing/researches/productIdentifierDataSpec.js
@@ -1,4 +1,4 @@
-import isIdentifierFilledIn from "../../../src/languageProcessing/researches/productIdentifierData.js";
+import isIdentifierFilledIn, { hasVariants } from "../../../src/languageProcessing/researches/productIdentifierData.js";
 
 
 describe( "A test to check if the product identifier is filled in", () => {
@@ -16,5 +16,23 @@ describe( "A test to check if the product identifier is filled in", () => {
 		};
 
 		expect( isIdentifierFilledIn( "elementID" ) ).toEqual( false );
+	} );
+} );
+
+describe( "A test to check if the product has variants", () => {
+	it( "returns true if the value field is not empty", function() {
+		document.getElementsByClassName.namedItem = function() {
+			return { "data-total": "2" };
+		};
+
+		expect( hasVariants() ).toEqual( true );
+	} );
+
+	it( "returns false if the value field is 0", function() {
+		document.getElementsByClassName.namedItem = function() {
+			return { "data-total": "" };
+		};
+		console.log(document.getElementsByClassName.namedItem(), "result");
+		expect( hasVariants() ).toEqual( false );
 	} );
 } );
