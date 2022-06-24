352afce615bdab78e62f13482b07189c5b5e5bb9
Hans-Christiaan <hans-christiaan@yoast.com>
2022-06-24 15:43:00 +0200

    Do not show Flesch reading ease inside of the insights tab when it is not available.

--- packages/js/tests/insights/redux/selectors.test.js
+++ packages/js/tests/insights/redux/selectors.test.js
@@ -4,17 +4,47 @@ import {
 	getFleschReadingEaseDifficulty,
 	getFleschReadingEaseScore,
 	getWordCount,
+	isFleschReadingEaseAvailable,
 } from "../../../src/insights/redux/selectors";
 import { DIFFICULTY } from "yoastseo";
 
 describe( "The insights selectors", () => {
-	it( "gets the Flesch reading ease score from the store", () => {
-		const state = set( {}, "insights.fleschReadingEaseScore", 42 );
-		expect( getFleschReadingEaseScore( state ) ).toEqual( 42 );
+	describe( "The Flesch reading ease score selector", () => {
+		it( "gets the score from the store", () => {
+			const state = set( {}, "insights.fleschReadingEaseScore", 42 );
+			expect( getFleschReadingEaseScore( state ) ).toEqual( 42 );
+		} );
+		it( "returns `null` when no score is available", () => {
+			const state = {};
+			expect( getFleschReadingEaseScore( state ) ).toEqual( null );
+		} );
 	} );
-	it( "gets the Flesch reading ease score from the store", () => {
-		const state = set( {}, "insights.fleschReadingEaseDifficulty", DIFFICULTY.EASY );
-		expect( getFleschReadingEaseDifficulty( state ) ).toEqual( DIFFICULTY.EASY );
+	describe( "The Flesch reading ease difficulty selector", () => {
+		it( "gets the difficulty from the store", () => {
+			const state = set( {}, "insights.fleschReadingEaseDifficulty", DIFFICULTY.EASY );
+			expect( getFleschReadingEaseDifficulty( state ) ).toEqual( DIFFICULTY.EASY );
+		} );
+		it( "returns `null` when no difficulty is available", () => {
+			const state = {};
+			expect( getFleschReadingEaseDifficulty( state ) ).toEqual( null );
+		} );
+	} );
+	describe( "the Flesch reading ease availability selector", () => {
+		it( "returns `true` when a score and difficulty are available", () => {
+			const state = {
+				insights: {
+					fleschReadingEaseScore: 42,
+					fleschReadingEaseDifficulty: DIFFICULTY.OKAY,
+				},
+			};
+			expect( isFleschReadingEaseAvailable( state ) ).toEqual( true );
+		} );
+		it( "returns `false` when score or difficulty are not available", () => {
+			const state = {
+				insights: {},
+			};
+			expect( isFleschReadingEaseAvailable( state ) ).toEqual( false );
+		} );
 	} );
 	it( "gets the estimated reading time from the store", () => {
 		const state = set( {}, "insights.estimatedReadingTime", 31 );
