c80424c027a43a175e1a824743911069368edc7d
Agnieszka Szuba <agnieszka@yoast.com>
2022-06-24 17:28:57 +0200

    Continue adding functions to get product identifier data

--- packages/yoastseo/spec/languageProcessing/researches/productIdentifierDataSpec.js
+++ packages/yoastseo/spec/languageProcessing/researches/productIdentifierDataSpec.js
@@ -1,38 +1,80 @@
-import isIdentifierFilledIn, { hasVariants } from "../../../src/languageProcessing/researches/productIdentifierData.js";
+import { hasVariants, hasGlobalIdentifier, allVariantsHaveIdentifier } from "../../../src/languageProcessing/researches/productIdentifierData.js";
 
+// Set up mock DOM data.
+document.body.innerHTML =
+	'<div>' +
+	'  <input type="text" id="yoast_identifier_gtin8" value="" >' +
+	'  <input type="text" id="yoast_identifier_gtin12" value="" >' +
+	'  <input type="text" id="yoast_identifier_gtin13" value="" >' +
+	'  <input type="text" id="yoast_identifier_gtin14" value="789" >' +
+	'  <input type="text" id="yoast_identifier_isbn" value="" >' +
+	'  <input type="text" id="yoast_identifier_mpn" value="" >' +
+	'</div>' +
+	'<div id="variable_product_options_inner" >' +
+	'  <div class="woocommerce_variations" data-total="2" >' +
+	'		<div class="woocommerce_variation">' +
+	'			<h3>' +
+	'				<input type="hidden" class="variable_post_id" value="12" >' +
+	'			</h3>' +
+	'			<div>' +
+	'				<p>' +
+	'					<input type="text" id="yoast_variation_identfier[12][gtin8]" value="777">' +
+	'				</p>' +
+	'				<p>' +
+	'					<input type="text" id="yoast_variation_identfier[12][gtin12]" value>' +
+	'				</p>' +
+	'			</div>' +
+	'		<div class="woocommerce_variation">' +
+	'			<h3>' +
+	'				<input type="hidden" class="variable_post_id" value="13" >' +
+	'			</h3>' +
+	'			<div>' +
+	'				<p>' +
+	'					<input type="text" id="yoast_variation_identfier[13][gtin8]" value>' +
+	'				</p>' +
+	'				<p>' +
+	'					<input type="text" id="yoast_variation_identfier[13][gtin12]" value="23">' +
+	'				</p>' +
+	'			</div>' +
+	'		</div>' +
+	'	</div>' +
+	'</div>';
 
-describe( "A test to check if the product identifier is filled in", () => {
-	it( "returns true if the element ID field is not empty", function() {
-		document.getElementById = function() {
-			return { value: "1234" };
-		};
-
-		expect( isIdentifierFilledIn( "elementID" ) ).toEqual( true );
+describe( "A test to check if at least one global product identifier is filled in", () => {
+	it( "returns true if at least one identifier is filled in", function() {
+		expect( hasGlobalIdentifier() ).toEqual( true );
 	} );
 
-	it( "returns false if the element ID field is empty", function() {
-		document.getElementById = function() {
-			return { value: "" };
-		};
+	it( "returns false if no identifiers are filled in", function() {
+		// Change value of the filled in attribute to empty string.
+		document.querySelector('#yoast_identifier_gtin14').setAttribute( 'value', '')
 
-		expect( isIdentifierFilledIn( "elementID" ) ).toEqual( false );
+		expect( hasGlobalIdentifier( ) ).toEqual( false );
 	} );
 } );
 
 describe( "A test to check if the product has variants", () => {
-	it( "returns true if the value field is not empty", function() {
-		document.getElementsByClassName.namedItem = function() {
-			return { "data-total": "2" };
-		};
-
+	it( "returns true if the data-total attribute of the woocommerce_variations class is not 0 or undefined", function() {
 		expect( hasVariants() ).toEqual( true );
 	} );
 
-	it( "returns false if the value field is 0", function() {
-		document.getElementsByClassName.namedItem = function() {
-			return { "data-total": "" };
-		};
-		console.log(document.getElementsByClassName.namedItem(), "result");
+	it( "returns false if the data-total attribute of the woocommerce_variations class is 0", function() {
+		// Change value of data-total to 0.
+		document.querySelector('.woocommerce_variations').setAttribute( 'data-total', '0')
+
 		expect( hasVariants() ).toEqual( false );
+	} );
+} );
+
+describe( "A test to check if all variants of a product have an identifier", () => {
+	it( "returns true if all variants have at least one identifier", function() {
+		expect( allVariantsHaveIdentifier() ).toEqual( true );
+	} );
+
+	it( "returns false one or more of the variants don't have an identifier", function() {
+		// Change one of the identifiers to an empty string.
+		document.querySelector("#yoast_variation_identfier\\[13\\]\\[gtin12\\]").setAttribute( 'value', '')
+
+		expect( allVariantsHaveIdentifier() ).toEqual( false );
 	} );
 } );
--- packages/yoastseo/src/languageProcessing/researches/productIdentifierData.js
+++ packages/yoastseo/src/languageProcessing/researches/productIdentifierData.js
@@ -1,57 +1,72 @@
-// The research will return the following information:
-//
-// Whether there are variants.
-//
-// (if there are variants) if the SKU/product identifier is filled in for each variant.
-//
-// If the global identifier/SKU is filled in.
-
 /**
- * Checks whether the identifiers field for a product are filled in.
+ * Checks whether a specific identifier field for a product is filled in.
  */
-export function isIdentifierFilledIn( elementID ) {
-	// If product has a global identifier it returns its value.
-	return document.getElementById( elementID ).value !== undefined && document.getElementById( elementID ).value !== "";
+const isIdentifierFilledIn = function( elementID ) {
+	const identifierValue = document.querySelector( '#' + elementID ).value;
+	//const identifierValue = document.getElementById("elementID" ).value;
+
+	return identifierValue !== undefined && identifierValue !== "";
 }
 
-// /**
-//  * Returns whether the product has at least one product identifier.
-//  *
-//  * @returns {boolean} Whether the product has an identifier.
-//  */
-// export function hasGlobalIdentifier() {
-// 	const identifierNames = [ "yoast_identifier_gtin8", "yoast_identifier_gtin12", "yoast_identifier_gtin13",
-// 		"yoast_identifier_gtin14", "yoast_identifier_isbn", "yoast_identifier_mpn" ];
-//
-// 	console.log( identifierNames, "identifierNames");
-//
-// 	const identifierValues = identifierNames.map( identifier => isIdentifierFilledIn( identifier ) );
-// 	console.log( identifierValues, "identifierValues");
-//
-//
-// 	return identifierValues.includes( true );
-// }
+/**
+ * Returns whether the product (or product variant) has at least one product identifier filled in.
+ *
+ * @returns {boolean} Whether the product/variant has at least one identifier.
+ */
+const hasAtLeastOneIdentifier = function( identifierNames ) {
+	const hasIdentifiers = identifierNames.map( identifier => isIdentifierFilledIn( identifier ) );
+
+	return hasIdentifiers.includes( true );
+}
+
+/**
+ *
+ * @returns {boolean}
+ */
+export function hasGlobalIdentifier() {
+	const identifierNames = [ "yoast_identifier_gtin8", "yoast_identifier_gtin12", "yoast_identifier_gtin13",
+		"yoast_identifier_gtin14", "yoast_identifier_isbn", "yoast_identifier_mpn" ];
+
+	return hasAtLeastOneIdentifier( identifierNames );
+}
+
 
 /**
  * Checks whether there are variants for a product.
  */
 export function hasVariants() {
-	const numberOfVariants = document.getElementsByClassName( "woocommerce_variations wc-metaboxes ui-sortable" ).namedItem( "data-total" );
-	if ( numberOfVariants !== "" ) {
-		return true;
-	} return false;
+	const variantsMetabox = document.querySelector( "#variable_product_options_inner" );
+	const variants = variantsMetabox.querySelector(".woocommerce_variations" );
+	const numberOfVariants = variants.getAttribute("data-total");
+
+	return numberOfVariants !== undefined && numberOfVariants !== "0";
 }
-// && numberOfVariants !== "" && numberOfVariants !== "0"
-//
-// /**
-//  * Checks whether all variants for a product have an identifier.
-//  */
-// const allVariantsHaveIdentifier = function() {
-// 	if (all variants have identifier) {
-// 		return true;
-// 	}
-// };
 
 
+/**
+ * Checks whether all variants of a product have an identifier.
+ */
+export function allVariantsHaveIdentifier() {
+	const allVariants = document.querySelectorAll( ".woocommerce_variation" );
 
+	for ( let i = 0; i < allVariants.length; i++ ) {
+		let variant = allVariants[ i ];
+		let variantID = variant.querySelector( ".variable_post_id" ).value
+
+		let identifierNames = [ `yoast_variation_identfier[${variantID}][gtin8]`,
+			`yoast_variation_identfier[${variantID}][gtin12]`, `yoast_variation_identfier[${variantID}][gtin13]`,
+			`yoast_variation_identfier[${variantID}][gtin14]`, `yoast_variation_identfier[${variantID}][isbn]`,
+			`yoast_variation_identfier[${variantID}][mpn]`,]
+
+		if ( ! hasAtLeastOneIdentifier( identifierNames ) ) {
+			return false;
+		}
+	}
+	return true;
+}
+
+// individual variant boxes are direct children of the div with .woocommerce-variations class (selected in hasVariants)
+// the boxes with variants have .woocommerce_variation class -> get all children with that class?
+// variation id can be retrieved from input element with class .variable_post_id nested inside h3 element under the .woocommerce_variation class
+// id of input field for identifier = yoast_variation_identifier[variation id][identifier name]
 
