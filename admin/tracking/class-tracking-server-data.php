73d1c86a6f3bb28cc5c7aa7ac3761a1a9f9ac281
Md Mazedul Islam Khan <mazedulislamkhan@gmail.com>
2022-06-24 22:40:44 +0200

    Get server OS only when the function exists

--- admin/tracking/class-tracking-server-data.php
+++ admin/tracking/class-tracking-server-data.php
@@ -36,7 +36,10 @@ class WPSEO_Tracking_Server_Data implements WPSEO_Collection {
 			$server_data['Hostname'] = gethostbyaddr( $ipaddress );
 		}
 
-		$server_data['os']            = php_uname();
+		if ( function_exists( 'php_uname' ) ) {
+			$server_data['os'] = php_uname();
+		}
+
 		$server_data['PhpVersion']    = PHP_VERSION;
 		$server_data['CurlVersion']   = $this->get_curl_info();
 		$server_data['PhpExtensions'] = $this->get_php_extensions();
