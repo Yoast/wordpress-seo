0189257c580db769aa72d85d6edb00f9d921a892
Igor <35524806+igorschoester@users.noreply.github.com>
2022-06-27 12:01:33 +0200

    Rename variable
    Include the space within the value to prevent an extra space when not have the admin bar
    Also prevents having to add a space in all other tests

--- src/presenters/open-graph/image-presenter.php
+++ src/presenters/open-graph/image-presenter.php
@@ -43,17 +43,16 @@ class Image_Presenter extends Abstract_Indexable_Presenter {
 		$return = '';
 		foreach ( $images as $image_index => $image_meta ) {
 			$image_url = $image_meta['url'];
+			$class     = \is_admin_bar_showing() ? ' class="yoast-seo-meta-tag"' : '';
 
-			$admin_bar_class = \is_admin_bar_showing() ? 'class="yoast-seo-meta-tag"' : '';
-
-			$return .= '<meta property="og:image" content="' . \esc_url( $image_url, null, 'attribute' ) . '" ' . $admin_bar_class . ' />';
+			$return .= '<meta property="og:image" content="' . \esc_url( $image_url, null, 'attribute' ) . '"' . $class . ' />';
 
 			foreach ( static::$image_tags as $key => $value ) {
 				if ( empty( $image_meta[ $key ] ) ) {
 					continue;
 				}
 
-				$return .= \PHP_EOL . "\t" . '<meta property="og:image:' . \esc_attr( $key ) . '" content="' . \esc_attr( $image_meta[ $key ] ) . '" ' . $admin_bar_class . ' />';
+				$return .= \PHP_EOL . "\t" . '<meta property="og:image:' . \esc_attr( $key ) . '" content="' . \esc_attr( $image_meta[ $key ] ) . '"' . $class . ' />';
 			}
 		}
 
