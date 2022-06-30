f766115120229d9633885b5ce1487e251bf66548
Maarten Leenders <26220788+maartenleenders@users.noreply.github.com>
2022-06-30 11:35:07 +0200

    Add news seo active check to utils and dont rely on news window object

--- packages/js/src/redux/reducers/preferences.js
+++ packages/js/src/redux/reducers/preferences.js
@@ -36,7 +36,7 @@ function getDefaultState() {
 		isWincherIntegrationActive: isWincherIntegrationActive(),
 		isWordProofIntegrationActive: isWordProofIntegrationActive(),
 		isInsightsEnabled: get( window, "wpseoScriptData.metabox.isInsightsEnabled", false ),
-		isNewsEnabled: ! isUndefined( window.wpseoNewsScriptData ),
+		isNewsEnabled: ! ! window.wpseoAdminL10n.news_seo_is_active,
 	};
 }
 
