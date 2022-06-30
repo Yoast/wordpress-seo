555e82ad289bb61179b83194c4368157f2ad694e
FAMarfuaty <aida@yoast.com>
2022-06-30 17:02:06 +0200

    Add unit tests for Shopify

--- packages/yoastseo/spec/scoring/assessments/seo/ProductIdentifiersAssessmentSpec.js
+++ packages/yoastseo/spec/scoring/assessments/seo/ProductIdentifiersAssessmentSpec.js
@@ -2,7 +2,7 @@ import ProductIdentifiersAssessment from "../../../../src/scoring/assessments/se
 import Paper from "../../../../src/values/Paper";
 import Factory from "../../../specHelpers/factory";
 
-const assessment = new ProductIdentifiersAssessment( { isWoo: true } );
+let assessment = new ProductIdentifiersAssessment( { isWoo: true } );
 
 describe( "An assessment to check whether the right research is called depending on the repository", function() {
 	it( "returns the default result when the paper doesn't contain an H1", function() {
@@ -39,6 +39,39 @@ describe( "An assessment to check whether the right research is called depending
 
 		expect( assessmentResult.getScore() ).toEqual( 6 );
 		expect( assessmentResult.getText() ).toEqual( "A different feedback for orange" );
+	} );
+} );
+
+describe( "a test for Product identifiers assessment for Shopify", () => {
+	assessment = new ProductIdentifiersAssessment( { isWoo: false } );
+	it( "returns with score 9 when the product has global identifier", () => {
+		const paper = new Paper( "" );
+		const assessmentResult = assessment.getResult( paper, Factory.buildMockResearcher( {
+			hasGlobalIdentifier: true,
+			hasVariants: false,
+		} ) );
+
+		expect( assessmentResult.getScore() ).toEqual( 9 );
+	} );
+
+	it( "returns with score 6 when the product doesn't have global identifier and variants", () => {
+		const paper = new Paper( "" );
+		const assessmentResult = assessment.getResult( paper, Factory.buildMockResearcher( {
+			hasGlobalIdentifier: false,
+			hasVariants: false,
+		} ) );
+
+		expect( assessmentResult.getScore() ).toEqual( 6 );
+	} );
+
+	it( "should not return a score if the product has variants", () => {
+		const paper = new Paper( "" );
+		const assessmentResult = assessment.getResult( paper, Factory.buildMockResearcher( {
+			hasGlobalIdentifier: false,
+			hasVariants: true,
+		} ) );
+
+		expect( assessmentResult.hasScore() ).toEqual( false );
 	} );
 } );
 
