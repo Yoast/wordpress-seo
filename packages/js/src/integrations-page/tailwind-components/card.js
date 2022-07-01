b75502d89ba8f0e0656dc6f7fd19b89a70777e3c
Paolo L. Scala <paolo.scala@gmail.com>
2022-07-01 12:53:17 +0200

    Callbacks implementation

--- packages/js/src/integrations-page/tailwind-components/card.js
+++ packages/js/src/integrations-page/tailwind-components/card.js
@@ -1,3 +1,6 @@
+
+import apiFetch from "@wordpress/api-fetch";
+import { useState, useCallback } from "@wordpress/element";
 import { PropTypes } from "prop-types";
 import { ToggleField } from "@yoast/ui-library";
 
@@ -11,18 +14,56 @@ import { ToggleField } from "@yoast/ui-library";
  * @returns {JSX.Element} The modal element.
  */
 export default function Card( { integration, children } ) {
+	const basePath = "yoast/v1/integrations";
+	const [ isActive, setIsActive ] = useState( integration.isActive );
+
+	/**
+	 * Updates an integration state.
+	 *
+	 * @param {string} integrationName The integration name.
+	 * @param {bool} active If the integration must be activated.
+	*
+	 * @returns {Promise|bool} A promise, or false if the call fails.
+	 */
+	async function updateIntegrationState( integrationName, setActive ) {
+		const response = await apiFetch( {
+			path: `${basePath}/set_${integrationName}_active`,
+			method: "POST",
+			data: { active: setActive },
+		} );
+		return await response.json;
+	}
+
+	const toggleActive = useCallback(
+		() => {
+			setIsActive( ! isActive );
+			updateIntegrationState( integration.name, ! isActive ).then( () => {
+				console.log("OK!");
+				return true;
+			} )
+				.catch( ( e ) => {
+					if ( e.message ) {
+						Error.log( e.message );
+					}
+					return false;
+				} );
+		},
+		[ isActive, setIsActive ]
+	);
+
 	return (
-		<div key={integration.title} className="yst-flex yst-flex-col yst-rounded-lg yst-shadow-lg yst-overflow-hidden yst-mr-3 yst-mb-4">
-			<div className="yst-flex-1 yst-bg-white yst-p-6 yst-flex yst-flex-col yst-justify-between">
+		<div key={ integration.title } className="yst-flex yst-flex-col yst-rounded-lg yst-shadow-lg yst-overflow-hidden yst-mr-3 yst-mb-4">
+
+			<div className={`yst-flex-1 yst-bg-white yst-p-6 yst-flex yst-flex-col yst-justify-between ${ isActive ? "yst-opacity-100" : "yst-opacity-50" }` }>
 				<div className="yst-flex-1">
-					<p className="yst-text-xl yst-font-semibold yst-text-gray-900">{integration.title}</p>
-					<p className="yst-mt-3 yst-text-base yst-text-gray-500">{integration.description}</p>
+					<p className="yst-text-xl yst-font-semibold yst-text-gray-900">{ integration.title }</p>
+					<p className="yst-mt-3 yst-text-base yst-text-gray-500">{ integration.description }</p>
 				</div>
 				<div className="yst-flex-1">
 					<ToggleField
-						checked
-						label={ "Enable" + integration.name }
-						onChange={function noRefCheck(){}}
+						checked={ isActive }
+						label={ `Enable ${ integration.name }` }
+						onChange={ toggleActive }
 					/>
 				</div>
 			</div>
@@ -33,8 +74,4 @@ export default function Card( { integration, children } ) {
 Card.propTypes = {
 	integration: PropTypes.object.isRequired,
 	children: PropTypes.node,
-};
-
-Card.defaultProps = {
-	integration: { name: "Semrush", description: "An integration" }
 };
