1912aceb42ae3483d9b8593348eb90ca513a0874
Igor <35524806+igorschoester@users.noreply.github.com>
2022-07-04 14:54:28 +0200

    Add initial values and submit per group

--- src/integrations/settings-integration.php
+++ src/integrations/settings-integration.php
@@ -3,6 +3,7 @@
 namespace Yoast\WP\SEO\Integrations;
 
 use WPSEO_Admin_Asset_Manager;
+use WPSEO_Options;
 use Yoast\WP\SEO\Conditionals\New_Settings_Ui_Conditional;
 use Yoast\WP\SEO\Conditionals\Premium_Active_Conditional;
 
@@ -13,6 +14,11 @@ class Settings_Integration implements Integration_Interface {
 
 	protected $asset_manager;
 
+	/**
+	 * Constructs Settings_Integration.
+	 *
+	 * @param WPSEO_Admin_Asset_Manager $asset_manager The WPSEO_Admin_Asset_Manager.
+	 */
 	public function __construct( WPSEO_Admin_Asset_Manager $asset_manager ) {
 		$this->asset_manager = $asset_manager;
 	}
@@ -73,12 +79,33 @@ class Settings_Integration implements Integration_Interface {
 		echo '<div id="yoast-seo-settings"></div>';
 	}
 
+	/**
+	 * Enqueues the assets.
+	 *
+	 * @return void
+	 */
 	public function enqueue_assets() {
 		$this->asset_manager->enqueue_script( 'new-settings' );
 		$this->asset_manager->enqueue_style( 'tailwind' );
+		$this->asset_manager->localize_script( 'new-settings', 'wpseoScriptData', $this->get_script_data() );
+	}
 
-		$this->asset_manager->localize_script( 'new-settings', 'wpseoScriptData', [
-			"nonce" => \wp_create_nonce( 'yoast_wpseo_options-options' ),
-		] );
+	/**
+	 * Creates the script data.
+	 *
+	 * @return array The script data.
+	 */
+	protected function get_script_data() {
+		$data = [
+			'options' => [],
+			'nonces'  => [],
+		];
+
+		foreach ( WPSEO_Options::$options as $name => $instance ) {
+			$data['options'][ $name ] = WPSEO_Options::get_option( $name );
+			$data['nonces'][ $name ]  = \wp_create_nonce( "yoast_${name}_options-options" );
+		}
+
+		return $data;
 	}
 }
