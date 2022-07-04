26e9a624b0480b158e2c3321b829c7deb0d5b081
Hans-Christiaan <hans-christiaan@yoast.com>
2022-07-04 16:03:13 +0200

    Simplified getInclusiveLanguageResults selector.

--- packages/js/src/redux/selectors/results.js
+++ packages/js/src/redux/selectors/results.js
@@ -51,9 +51,7 @@ export function getReadabilityResults( state ) {
  * @returns {object} The results and overall score for the inclusive language analysis.
  */
 export function getInclusiveLanguageResults( state ) {
-	const results = get( state, "analysis.inclusiveLanguage", {} );
-
-	return isEmpty( results ) ? { results: emptyArray, overallScore: null } : results;
+	return get( state, "analysis.inclusiveLanguage", { results: emptyArray, overallScore: null } );
 }
 
 /**
