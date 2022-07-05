d70f7c76b6577bb7589646fd151d7404902f1d00
Igor <35524806+igorschoester@users.noreply.github.com>
2022-07-05 10:32:10 +0200

    Add label suffix to toggle field
    Adjust some styles

--- packages/ui-library/src/components/toggle-field/index.js
+++ packages/ui-library/src/components/toggle-field/index.js
@@ -7,15 +7,17 @@ import Toggle from "../../elements/toggle";
 /**
  * @param {JSX.node} children Children are rendered below the checkbox group.
  * @param {string} label The Label.
+ * @param {JSX.node} [labelSuffix] Optional label suffix.
  * @param {boolean} [checked] Default state.
  * @param {Function} onChange Change callback.
- * @param {{ value, label }[]} options Options to choose from.
  * @param {string} [className] CSS class.
+ * @param {Object} props Other Toggle props.
  * @returns {JSX.Element} ToggleField component.
  */
 const ToggleField = ( {
 	children,
 	label,
+	labelSuffix,
 	checked,
 	onChange,
 	className,
@@ -24,7 +26,10 @@ const ToggleField = ( {
 	<Switch.Group as="div" className={ classNames( "yst-toggle-field", className ) }>
 		{ ( label || children ) && (
 			<div className="yst-toggle-field__text">
-				<Label as={ Switch.Label } className="yst-toggle-field__label" label={ label } />
+				<div className="yst-flex yst-items-center">
+					<Label as={ Switch.Label } className="yst-toggle-field__label" label={ label } />
+					{ labelSuffix }
+				</div>
 				{ children && <Switch.Description className="yst-toggle-field__description">{ children }</Switch.Description> }
 			</div>
 		) }
@@ -40,6 +45,7 @@ const ToggleField = ( {
 ToggleField.propTypes = {
 	children: PropTypes.node,
 	label: PropTypes.string.isRequired,
+	labelSuffix: PropTypes.node,
 	checked: PropTypes.bool.isRequired,
 	onChange: PropTypes.func.isRequired,
 	className: PropTypes.string,
@@ -48,6 +54,7 @@ ToggleField.propTypes = {
 ToggleField.defaultProps = {
 	children: null,
 	className: "",
+	labelSuffix: null,
 };
 
 export default ToggleField;
