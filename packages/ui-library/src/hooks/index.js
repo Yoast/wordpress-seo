5d2ff99d4898d22a4b7a146a8e15db8eecd42c2f
Igor <35524806+igorschoester@users.noreply.github.com>
2022-07-05 13:16:19 +0200

    Add toggle state hook

--- packages/ui-library/src/hooks/index.js
+++ packages/ui-library/src/hooks/index.js
@@ -1,3 +1,4 @@
 export { default as useDescribedBy } from "./use-described-by";
 export { default as useRootContext } from "./use-root-context";
 export { default as useSvgAria } from "./use-svg-aria";
+export { default as useToggleState } from "./use-toggle-state";
