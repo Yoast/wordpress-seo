e1ced1fe6d6783a4dd4f2acae74a70f6fb3cf148
Hanna Worku <hanna@yoast.com>
2022-07-06 14:42:37 +0200

    replace plural in specs with plural not found in the list

--- packages/yoastseo/spec/languageProcessing/languages/en/helpers/checkIfWordIsComplexSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/en/helpers/checkIfWordIsComplexSpec.js
@@ -1,27 +1,27 @@
 import checkIfWordIsComplex from "../../../../../src/languageProcessing/languages/en/helpers/checkIfWordIsComplex";
 
 describe( "a test checking if the word is complex in English",  function() {
-	it( "returns word as non-complex if its singular version is found in the list", function() {
-		expect( checkIfWordIsComplex( "examples" ) ).toEqual(
-			false
-		);
-	} );
-	it( "returns word as non-complex if it is found in the list", function() {
+	it( "returns singular word as non-complex if it is found in the list", function() {
 		expect( checkIfWordIsComplex( "example" ) ).toEqual(
 			false
 		);
 	} );
 	it( "returns word as non-complex if its singular version is found in the list", function() {
-		expect( checkIfWordIsComplex( "characters" ) ).toEqual(
+		expect( checkIfWordIsComplex( "examples" ) ).toEqual(
 			false
 		);
 	} );
-	it( "returns word as non-complex if it is found in the list", function() {
-		expect( checkIfWordIsComplex( "character" ) ).toEqual(
+	it( "returns singular word as non-complex if it is found in the list", function() {
+		expect( checkIfWordIsComplex( "release" ) ).toEqual(
 			false
 		);
 	} );
-	it( "returns word as complex if it ends on -s and is not in the list", function() {
+	it( "returns plural word as non-complex if its singular version is found in the list", function() {
+		expect( checkIfWordIsComplex( "releases" ) ).toEqual(
+			false
+		);
+	} );
+	it( "returns plural word as complex if it is not in the list", function() {
 		expect( checkIfWordIsComplex( "refrigerators" ) ).toEqual(
 			true
 		);
