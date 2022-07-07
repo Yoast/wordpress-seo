6c572a2f76f0ef4ca483a449307234babd449528
FAMarfuaty <aida@yoast.com>
2022-07-07 09:21:34 +0200

    add more documentation

--- packages/yoastseo/src/languageProcessing/languages/en/helpers/checkIfWordIsComplex.js
+++ packages/yoastseo/src/languageProcessing/languages/en/helpers/checkIfWordIsComplex.js
@@ -3,9 +3,9 @@ import wordComplexity from "../config/internal/wordComplexity";
 /**
  * Checks if a word is complex.
  *
- * @param {string} word     The word to check.
+ * @param {string} word The word to check.
  *
- * @returns {boolean}    Whether or not a word is complex.
+ * @returns {boolean} Whether or not a word is complex.
  */
 export default function checkIfWordIsComplex( word ) {
 	const wordComplexityConfig = wordComplexity;
@@ -14,23 +14,27 @@ export default function checkIfWordIsComplex( word ) {
 	// Whether uppercased beginning of a word decreases its complexity.
 	const doesUpperCaseDecreaseComplexity = wordComplexityConfig.doesUpperCaseDecreasesComplexity;
 
-	/*
-	 * Check for each word whether it is a complex word or not.
-	 * A word is complex if:
-	 * its length is longer than the limit, AND
-	 * the word is not in the frequency list, AND
-	 * if the word does NOT start with a capital letter
-	 * (for languages that see long words to be less complex if they start with a capital letter)
-	 */
+	// The word is not complex if it's less than the length limit, i.e. 7 characters for English.
 	if ( word.length <= lengthLimit ) {
 		return false;
 	}
+
+	// The word is not complex if it's in the frequency list.
 	if ( frequencyList.includes( word ) ) {
 		return false;
 	}
 
+	/*
+	 * In English where capital letter beginning decreases the complexity of a word,
+	 * word longer than 7 characters is not complex if it starts with capital letter.
+	 */
 	if ( doesUpperCaseDecreaseComplexity === true && word[ 0 ].toLowerCase() === word[ 0 ] ) {
-		// Check if the word ends on -s (possible plural noun), if it ends on -s remove -s and check for the word is in the list.
+		/*
+		 * If a word is longer than 7 characters and doesn't start with capital letter,
+		 * we check further whether it is a plural ending in -s. If it is, we remove the -s suffix
+		 * and check if the singular word can be found in the frequency list.
+		 * The word is not complex if the singular form is in the list.
+		 */
 		if ( word.endsWith( "s" ) ) {
 			word = word.substring( 0, word.length - 1 );
 			return ! frequencyList.includes( word );
