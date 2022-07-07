62151d0f153b3bca05d71b2c87fb2504d9695837
Paolo L. Scala <paolo.scala@gmail.com>
2022-07-07 16:00:15 +0200

    Refactor card to compound component

--- packages/js/src/integrations-page/tailwind-components/card.js
+++ packages/js/src/integrations-page/tailwind-components/card.js
@@ -1,100 +1,167 @@
-import apiFetch from "@wordpress/api-fetch";
-import { useState, useCallback } from "@wordpress/element";
-import { __ } from "@wordpress/i18n";
+import { useState, createContext, useContext } from "@wordpress/element";
 import { PropTypes } from "prop-types";
-import { ToggleField, Button, Alert } from "@yoast/ui-library";
+import { ToggleField } from "@yoast/ui-library";
 
-/* eslint-disable complexity */
+const CardContext = createContext( {
+	isActive: true,
+	toggleActive: () => {},
+} );
+
 /**
- * Modal component.
+ * A hook for getting the CardContext value, with an informative error message.
  *
- * @param {Object}   props                       The props.
- * @param {Object}   props.integration           Contents of the modal.
- * @param {JSX.node} props.children              Contents of the modal.
- *
- * @returns {JSX.Element} The modal element.
+ * @returns {*} The value provided to the CardContext provider.
  */
-export default function Card( { integration, children } ) {
-	const basePath = "yoast/v1/integrations";
-	const isPremiumInstalled = Boolean( window.wpseoScriptData.isPremium );
-	const isIntegrationAvailable = ( integration.isPremium && isPremiumInstalled ) || ! integration.isPremium;
-	const [ isActive, setIsActive ] = useState( integration.isActive );
-	const [ errorMessage, setErrorMessage ] = useState( "" );
-	const upsellLink = "https://yoa.st/workout-orphaned-content-upsell";
-
-	/**
-	 * Updates an integration state.
-	 *
-	 * @param {string} integrationName The integration name.
-	 * @param {bool} setActive If the integration must be activated.
-	*
-	 * @returns {Promise|bool} A promise, or false if the call fails.
-	 */
-	async function updateIntegrationState( integrationName, setActive ) {
-		const response = await apiFetch( {
-			path: `${basePath}/set_${integrationName}_active`,
-			method: "POST",
-			data: { active: setActive },
-		} );
-		return await response.json;
+export function useCardContext() {
+	const context = useContext( CardContext );
+	if ( ! context ) {
+	  throw new Error(
+			"Card compound components cannot be rendered outside the Card component"
+	  );
 	}
+	return context;
+}
+/* eslint-disable complexity */
 
-	const toggleActive = useCallback(
-		() => {
-			setIsActive( ! isActive );
-			updateIntegrationState( integration.name, ! isActive ).then( () => {
-				setErrorMessage( "" );
-				return true;
-			} )
-				.catch( ( e ) => {
-					if ( e.message ) {
-						setErrorMessage( e.message );
-					}
-					return false;
-				} );
-		},
-		[ isActive, setIsActive ]
-	);
+/**
+ * The header.
+ *
+ * @param {JSX.node} children The children.
+ *
+ * @returns {JSX.Element} The header.
+ */
+const Header = ( { children } ) => {
+	const { isActive } = useCardContext();
+
 	return (
-		<div className={ `yst-relative yst-flex yst-flex-col yst-bg-white yst-rounded-lg yst-border yst-p-6 yst-space-y-6 yst-overflow-hidden yst-transition-transform yst-ease-in-out yst-duration-200 ${ isIntegrationAvailable ? "yst-border-gray-200 yst-shadow-sm" : "yst-border-gray-200 yst-shadow-sm" }` }>
-			<header className={ `yst-relative yst-flex yst-items-center yst-justify-center yst-h-24 yst-bg-gray-100 yst--mx-6 yst--mt-6 yst-py-6 ${ isActive ? "" : "yst-opacity-50 yst-filter yst-grayscale" }` }>
-				{ integration.logo && integration.logo }
-			</header>
-			<div className={ `yst-flex-grow ${ isActive ? "" : "yst-opacity-50  yst-filter yst-grayscale" } ` }>
-				<h4 className="yst-flex yst-items-center yst-text-base yst-mb-3">
-					<span>{ integration.name }</span>
-				</h4>
-				{ integration.description && <p> { integration.description } </p> }
-				{ integration.usps && <ul className="yst-space-y-3">
-					{ integration.usps.map( ( usp, index ) => {
-						return (
-							<li key={ index } className="yst-flex yst-items-start">
-								<svg xmlns="http://www.w3.org/2000/svg" className="yst-h-5 yst-w-5 yst-mr-2 yst-text-green-400 yst-flex-shrink-0" viewBox="0 0 20 20" fill="currentColor">
-									<path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
-								</svg>
-								<span> { usp } </span>
-							</li>
-						);
-					} ) }
-				</ul> }
-			</div>
-			<footer className="yst-border-t yst-border-gray-200 yst-pt-6">
-				{ isIntegrationAvailable
-					? <ToggleField checked={ isActive } label={ `Enable ${ integration.name }` } onChange={ toggleActive } />
-					: <Button id={ `${integration.name}-upsell-button` } as="a" href={ upsellLink } variant="upsell" className="yst-w-full yst-text-gray-800">
-						<svg xmlns="http://www.w3.org/2000/svg" className="yst--ml-1 yst-mr-2 yst-h-5 yst-w-5 yst-text-yellow-900" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-							<path strokeLinecap="round" strokeLinejoin="round" d="M8 11V7a4 4 0 118 0m-4 8v2m-6 4h12a2 2 0 002-2v-6a2 2 0 00-2-2H6a2 2 0 00-2 2v6a2 2 0 002 2z" />
-						</svg>
-						{ __( "Unlock with Premium", "wordpress-seo") }
-					</Button>
-				}
-				{ errorMessage && <Alert variant="error" className="yst-mt-2">{ errorMessage }</Alert> }
-			</footer>
+		<header className={ `yst-relative yst-flex yst-items-center yst-justify-center yst-h-24 yst-bg-gray-100 yst--mx-6 yst--mt-6 yst-py-6 ${ isActive ? "" : "yst-opacity-50 yst-filter yst-grayscale" }` }>
+			{ children }
+		</header>
+	);
+};
+
+Header.propTypes = {
+	children: PropTypes.node,
+};
+
+/**
+ * The content.
+ *
+ * @param {JSX.node} children The children.
+ *
+ * @returns {JSX.Element} The content.
+ */
+const Content = ( { children } ) => {
+	const { isActive } = useCardContext();
+	return (
+		<div className={ `yst-flex-grow ${ isActive ? "" : "yst-opacity-50  yst-filter yst-grayscale" } ` }>
+			{ children }
+		</div>
+	);
+};
+
+Content.propTypes = {
+	children: PropTypes.node,
+};
+
+
+/**
+ * The footer.
+ *
+ * @param {JSX.node} children The children.
+ *
+ * @returns  {JSX.Element} The footer.
+ */
+const Footer = ( { children } ) => {
+	return (
+		<footer className="yst-border-t yst-border-gray-200 yst-pt-6">
+			{ children }
+		</footer>
+	);
+};
+
+Footer.propTypes = {
+	children: PropTypes.node,
+};
+
+/**
+ * The toggle footer.
+ *
+ * @param {String}   props.toggleLabel The label to be used by the toggle.
+ * @param {JSX.node} children          The children.
+
+ * @returns  {JSX.Element} The footer.
+ */
+const ToggleFooter = ( { toggleLabel, children } ) => {
+	const { isActive, toggleActive } = useCardContext();
+	return (
+		<footer className="yst-border-t yst-border-gray-200 yst-pt-6">
+			{ children }
+			<ToggleField checked={ isActive } label={ toggleLabel } onChange={ toggleActive } />
+		</footer>
+	);
+};
+
+ToggleFooter.propTypes = {
+	toggleLabel: PropTypes.string,
+	children: PropTypes.node,
+};
+
+/**
+ * Card component.
+ *
+ * @param {Boolean}  props.initialState    Toggle initial state.
+ * @param {String}   props.integrationSlug The integration slug.
+ * @param {Function} props.beforeToggle    Function to call before changing the state of the toggle.
+ * @param {JSX.node} props.children        The children.
+
+ *
+ * @returns {JSX.Element} The card element.
+ */
+export function Card( { initialState, integrationSlug, beforeToggle, children } ) {
+	const [ isActive, setIsActive ] = useState( initialState );
+	/**
+	 * The toggle.
+	 *
+	 * @returns  {Boolean} The footer.
+	 */
+	const toggleActive =
+		async() => {
+			let canToggle = true;
+			// Immediately switch the toggle for enhanced UX
+			setIsActive( ! isActive );
+
+			if ( beforeToggle ) {
+				canToggle = false;
+				canToggle = await beforeToggle( integrationSlug, ! isActive );
+			}
+			if ( ! canToggle ) {
+				// If something went wrong, switch the toggle back
+				setIsActive( ! isActive );
+			}
+		};
+
+	const value = { isActive, toggleActive };
+	return (
+		<div className="yst-relative yst-flex yst-flex-col yst-bg-white yst-rounded-lg yst-border yst-p-6 yst-space-y-6 yst-overflow-hidden yst-transition-transform yst-ease-in-out yst-duration-200">
+			<CardContext.Provider value={ value }>
+				{ children }
+			</CardContext.Provider>
 		</div>
 	);
 }
 
 Card.propTypes = {
-	integration: PropTypes.object.isRequired,
+	initialState: PropTypes.bool,
+	integrationSlug: PropTypes.string,
+	beforeToggle: PropTypes.func,
 	children: PropTypes.node,
 };
+
+Card.defaultProps = {
+	initialState: true,
+};
+Card.Header = Header;
+Card.Content = Content;
+Card.Footer = Footer;
+Card.ToggleFooter = ToggleFooter;
