e738dc28ca79b0b6770f17747836fe66d6392d92
FAMarfuaty <aida@yoast.com>
2022-07-08 15:55:27 +0200

    add unit tests

--- packages/yoastseo/spec/languageProcessing/languages/fr/helpers/checkIfWordIsComplexSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/fr/helpers/checkIfWordIsComplexSpec.js
@@ -33,15 +33,20 @@ describe( "a test checking if the word is complex in French",  function() {
 		expect( checkIfWordIsComplex( "ouvrir" ) ).toEqual( false );
 	} );
 
-	it( "returns words longer than 9 characters preceded by article l' as non complex if the words are in the list", function() {
+	it( "returns words longer than 9 characters preceded by article l' or preposition d' as non complex if the words are in the list", function() {
 		expect( checkIfWordIsComplex( "l'ambassadeur" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( "d'échantillon" ) ).toEqual( false );
 	} );
 
-	it( "returns word shorter than 9 characters as non complex even when it's preceded by an l' article that increases its length", function() {
+	it( "returns word shorter than 9 characters as non complex even when it's preceded by an l' article or " +
+		"a preposition d' that increases its length", function() {
 		expect( checkIfWordIsComplex( "l'occident" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( "d'extension" ) ).toEqual( false );
 	} );
 
-	it( "returns word longer than 9 characters which starts with capital letter as non complex even when it's preceded by an l' article", function() {
+	it( "returns word longer than 9 characters which starts with capital letter as non complex even when it's preceded by an l' article " +
+		"or a preposition d'", function() {
 		expect( checkIfWordIsComplex( "l'Orthodoxie" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( "d'Égalisation" ) ).toEqual( false );
 	} );
 } );
