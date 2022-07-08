0107bd206eab43e36440e8ca5f28f87197b200bd
Igor <35524806+igorschoester@users.noreply.github.com>
2022-07-08 08:03:46 +0200

    Fix some code style

--- packages/js/src/settings/components/formik-value-change-field.js
+++ packages/js/src/settings/components/formik-value-change-field.js
@@ -6,12 +6,12 @@ import { useField } from "formik";
 /**
  * Wraps a non-default change event field to be a Formik field.
  * @param {JSX.ElementClass} as The field component.
- * @param {function} transformValue Transforms the incoming change value before Formik receives it.
- * @param {Object} props The props to pass down to ToggleField component.
+ * @param {function} [transformValue] Transforms the incoming change value before Formik receives it.
+ * @param {Object} props The props to pass down to the component.
  * @param {string} props.name The field name.
- * @returns {JSX.Element} The Formik compatible ToggleField component.
+ * @returns {JSX.Element} The Formik compatible element.
  */
-const FormikValueChangeField = ( { as: Component, transformValue, ...props } ) => {
+const FormikValueChangeField = ( { as: Component, transformValue = identity, ...props } ) => {
 	const [ field, , { setTouched, setValue } ] = useField( props );
 
 	const handleChange = useCallback( ( value ) => {
@@ -32,10 +32,6 @@ FormikValueChangeField.propTypes = {
 	as: PropTypes.elementType.isRequired,
 	name: PropTypes.string.isRequired,
 	transformValue: PropTypes.func,
-};
-
-FormikValueChangeField.defaultProps = {
-	transformValue: identity,
 };
 
 export default FormikValueChangeField;
