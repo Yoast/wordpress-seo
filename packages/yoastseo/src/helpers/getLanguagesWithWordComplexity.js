30f3b86d9e627f392c5929a0d5cc2182b182997b
FAMarfuaty <aida@yoast.com>
2022-07-11 13:54:17 +0200

    Add a helper for languages with Word complexity support

---
+++ packages/yoastseo/src/helpers/getLanguagesWithWordComplexity.js
@@ -0,0 +1,8 @@
+/**
+ * Checks which languages have Word complexity support.
+ *
+ * @returns {string[]} A list of languages that have Word complexity support.
+ */
+export function getLanguagesWithWordComplexity() {
+	return [ "en", "de", "fr", "es" ];
+}
