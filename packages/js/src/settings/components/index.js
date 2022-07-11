72cac209372c2d9f27c11318051ebfb461c6ea1a
nolledgeable <nolle@yoast.com>
2022-07-11 16:42:02 +0200

    Add other social urls field array

--- packages/js/src/settings/components/index.js
+++ packages/js/src/settings/components/index.js
@@ -4,5 +4,6 @@ export { default as FormikFlippedToggleField } from "./formik-flipped-toggle-fie
 export { default as FormikMediaSelectField } from "./formik-media-select-field";
 export { default as FormikReplacementVariableEditorField } from "./formik-replacement-variable-editor-field";
 export { default as FormikValueChangeField } from "./formik-value-change-field";
+export { default as FormikWithErrorField } from "./formik-with-error-field";
 export { default as SidebarNavigation } from "./sidebar-navigation";
 export { ReactComponent as YoastLogo } from "./yoast-logo.svg";
