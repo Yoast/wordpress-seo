c6d2a94d72620b2da02ae3206175827b8494edf2
Enrico Battocchi <enrico.battocchi@gmail.com>
2022-07-11 13:27:55 +0200

    Add safelist for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -54,4 +54,13 @@ module.exports = {
 		preflight: false,
 	},
 	prefix: "yst-",
+	safelist: process.env.NODE_ENV === "development" ? [ { pattern: /.*/ } ] : [
+		"yst-border-l",
+		"yst-space-x-8",
+		"yst-pb-10",
+		"yst-space-y-6",
+		"yst--left-3",
+		"yst-ring-gray-200",
+		"yst-font-mono",
+	],
 };
