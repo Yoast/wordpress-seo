5a33bfbc378100a2701b6a820d3a11e291c92f76
Igor <35524806+igorschoester@users.noreply.github.com>
2022-07-11 10:44:11 +0200

    Breaking change: Select and SelectField label and options change
    * Select takes children to override the default options rendering
    * Select.Option is exposed
    * Select takes label and labelProps props to fix the label htmlFor, this has to be here due to Listbox' auto-generated ID
    * SelectField uses description prop instead of children, due to children being passed to Select for the options override
    * SelectField uses regular label with label class instead of Label element due to Listbox requiring a forwardRef

--- packages/ui-library/src/components/select-field/index.js
+++ packages/ui-library/src/components/select-field/index.js
@@ -1,74 +1,56 @@
-/* eslint-disable no-undefined */
 import classNames from "classnames";
 import PropTypes from "prop-types";
-import Label from "../../elements/label";
 import Select from "../../elements/select";
 import { useDescribedBy } from "../../hooks";
 
 /**
  * @param {string} id Identifier.
- * @param {string} value Selected value.
- * @param {{ value, label }[]} options Options to choose from.
- * @param {Function} onChange Change callback.
  * @param {JSX.Element} error Error node.
  * @param {string} [className] Optional CSS class.
  * @param {string} label Label.
- * @param {JSX.node} [children] Optional description.
+ * @param {JSX.node} [description] Optional description.
  * @param {Object} [props] Any extra props.
  * @returns {JSX.Element} SelectField component.
  */
 const SelectField = ( {
-	children,
 	id,
-	value,
-	options,
 	label,
-	onChange,
-	error,
-	className,
+	description,
+	error = null,
+	className = "",
 	...props
 } ) => {
-	const { ids, describedBy } = useDescribedBy( id, { error, description: children } );
+	const { ids, describedBy } = useDescribedBy( id, { error, description } );
 
 	return (
 		<div className={ classNames( "yst-select-field", className ) }>
-			<Label className="yst-select-field__label" label={ label } />
 			<Select
 				id={ id }
-				value={ value }
-				options={ options }
-				onChange={ onChange }
+				label={ label }
+				labelProps={ {
+					as: "label",
+					className: "yst-label yst-select-field__label",
+				} }
 				isError={ Boolean( error ) }
 				className="yst-select-field__select"
 				buttonProps={ { "aria-describedby": describedBy } }
 				{ ...props }
 			/>
 			{ error && <p id={ ids.error } className="yst-select-field__error">{ error }</p> }
-			{ children && <div id={ ids.description } className="yst-select-field__description">{ children }</div> }
+			{ description && <div id={ ids.description } className="yst-select-field__description">{ description }</div> }
 		</div>
 	);
 };
 
 SelectField.propTypes = {
-	children: PropTypes.node,
 	id: PropTypes.string.isRequired,
 	name: PropTypes.string.isRequired,
-	value: PropTypes.oneOfType( [ PropTypes.string, PropTypes.number, PropTypes.bool ] ),
 	label: PropTypes.string.isRequired,
-	options: PropTypes.arrayOf( PropTypes.shape( {
-		value: PropTypes.oneOfType( [ PropTypes.string, PropTypes.number, PropTypes.bool ] ).isRequired,
-		label: PropTypes.string.isRequired,
-	} ) ).isRequired,
-	onChange: PropTypes.func.isRequired,
+	description: PropTypes.node,
 	error: PropTypes.node,
 	className: PropTypes.string,
 };
 
-SelectField.defaultProps = {
-	children: null,
-	value: undefined,
-	error: null,
-	className: "",
-};
+SelectField.Option = Select.Option;
 
 export default SelectField;
