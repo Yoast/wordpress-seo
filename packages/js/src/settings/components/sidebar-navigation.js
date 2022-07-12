35b68c8713cd4d1eed26ae840756408356c750e1
Igor <35524806+igorschoester@users.noreply.github.com>
2022-07-12 13:24:59 +0200

    Add suffix to mobile menu IDs
    Preventing duplicate IDs

--- packages/js/src/settings/components/sidebar-navigation.js
+++ packages/js/src/settings/components/sidebar-navigation.js
@@ -1,18 +1,22 @@
 import { SidebarNavigation } from "@yoast/ui-library";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const PureSubmenuItem = SidebarNavigation.SubmenuItem;
 
 /**
  * @param {Object} props The props.
  * @param {string} to The path to link to.
+ * @param {string} [idSuffix] Extra id suffix. Can prevent double IDs on the page.
  * @returns {JSX.Element} The submenu item element.
  */
-const SubmenuItem = ( { to, ...props } ) => <PureSubmenuItem as={ Link } pathProp="to" id={ `link:${ to }` } to={ to } { ...props } />;
+const SubmenuItem = ( { to, idSuffix = "", ...props } ) => (
+	<PureSubmenuItem as={ Link } pathProp="to" id={ `link:${ to }${ idSuffix }` } to={ to } { ...props } />
+);
 
 SubmenuItem.propTypes = {
 	to: PropTypes.string.isRequired,
+	idSuffix: PropTypes.string,
 };
 
 SidebarNavigation.SubmenuItem = SubmenuItem;
