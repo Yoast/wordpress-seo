dfec994ecbf747eae5a9c89348cd056f4bf522b5
nolledgeable <nolle@yoast.com>
2022-07-13 13:44:50 +0200

    Concept: rewrite HOCs to `with` convention for composability

--- packages/js/src/settings/components/formik-with-error-field.js
+++ packages/js/src/settings/components/formik-with-error-field.js
@@ -4,6 +4,20 @@ import PropTypes from "prop-types";
 import { useFormikError } from "../hooks";
 
 /**
+ * A higher order component that adds an error prop to a specified component based on Formik error state and field name.
+ * @param {JSX.Element} Component The component to wrap.
+ * @returns {JSX.Element} The wrapped component.
+ */
+// eslint-disable-next-line no-unused-vars
+const withFormikError = ( Component ) => {
+	const ComponentWithFormikError = ( { name, ...props } ) => {
+		const { isTouched, error } = useFormikError( { name } );
+		return <Component { ...props } name={ name } error={ isTouched && error } />;
+	};
+	return ComponentWithFormikError;
+};
+
+/**
  * @param {Object} props The props.
  * @param {string} name The field name.
  * @returns {JSX.Element} A Formik Field element with error message support.
