374029b83b8bfacc842cdbe4ed86feafb2313dba
nolledgeable <nolle@yoast.com>
2022-07-14 11:05:19 +0200

    Improve id props for menu links

--- packages/js/src/settings/components/sidebar-navigation.js
+++ packages/js/src/settings/components/sidebar-navigation.js
@@ -1,5 +1,7 @@
 import { SidebarNavigation } from "@yoast/ui-library";
 import PropTypes from "prop-types";
+import { replace } from "lodash";
+import { useMemo } from "@wordpress/element";
 import { Link } from "react-router-dom";
 
 const PureSubmenuItem = SidebarNavigation.SubmenuItem;
@@ -10,9 +12,10 @@ const PureSubmenuItem = SidebarNavigation.SubmenuItem;
  * @param {string} [idSuffix] Extra id suffix. Can prevent double IDs on the page.
  * @returns {JSX.Element} The submenu item element.
  */
-const SubmenuItem = ( { to, idSuffix = "", ...props } ) => (
-	<PureSubmenuItem as={ Link } pathProp="to" id={ `link-${ to }${ idSuffix }` } to={ to } { ...props } />
-);
+const SubmenuItem = ( { to, idSuffix = "", ...props } ) => {
+	const id = useMemo( () => replace( replace( `link-${to}`, "/", "-" ), "--", "-" ), [ to ] );
+	return <PureSubmenuItem as={ Link } pathProp="to" id={ `${ id }${ idSuffix }` } to={ to } { ...props } />;
+};
 
 SubmenuItem.propTypes = {
 	to: PropTypes.string.isRequired,
