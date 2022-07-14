4210d667d20cff806c636e120fb1d4490abf5162
FAMarfuaty <aida@yoast.com>
2022-07-14 13:06:22 +0200

    Fix the props reference

--- packages/js/src/components/contentAnalysis/ReadabilityAnalysis.js
+++ packages/js/src/components/contentAnalysis/ReadabilityAnalysis.js
@@ -199,12 +199,12 @@ class ReadabilityAnalysis extends Component {
 			<AnalysisResult
 				icon="alert-info"
 				pressed={ false }
-				onButtonClick={ noop }
+				onButtonClickMarks={ noop }
 				bulletColor="gray"
-				buttonId={ "" }
+				buttonIdMarks={ "" }
 				text={ text }
 				hasMarksButton={ false }
-				ariaLabel={ "" }
+				ariaLabelMarks={ "" }
 			/>
 		</ul>;
 	}
