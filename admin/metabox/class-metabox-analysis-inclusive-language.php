dd156e64f20930315a0a20315c84393e2f4d5397
Hans-Christiaan <hans-christiaan@yoast.com>
2022-07-18 13:26:38 +0200

    Disable inclusive language analysis for locales that we do not support yet.

--- admin/metabox/class-metabox-analysis-inclusive-language.php
+++ admin/metabox/class-metabox-analysis-inclusive-language.php
@@ -16,7 +16,9 @@ class WPSEO_Metabox_Analysis_Inclusive_Language implements WPSEO_Metabox_Analysi
 	 * @return bool Whether or not this analysis is enabled.
 	 */
 	public function is_enabled() {
-		return $this->is_globally_enabled() && $this->is_user_enabled() && YoastSEO()->helpers->product->is_premium();
+		return $this->is_globally_enabled() && $this->is_user_enabled()
+		       && YoastSEO()->helpers->product->is_premium()
+		       && YoastSEO()->helpers->language->has_inclusive_language_support( \get_locale() );
 	}
 
 	/**
