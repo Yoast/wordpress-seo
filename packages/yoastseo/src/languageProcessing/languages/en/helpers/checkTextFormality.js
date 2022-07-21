71106e7f2d8b75bb1f70d5cd188e7a038126c486
FAMarfuaty <aida@yoast.com>
2022-07-21 17:02:59 +0200

    Initial implementation of the research file

--- packages/yoastseo/src/languageProcessing/languages/en/helpers/checkTextFormality.js
+++ packages/yoastseo/src/languageProcessing/languages/en/helpers/checkTextFormality.js
@@ -1,8 +1,35 @@
-export default function() {
+import getSentences from "../../../helpers/sentence/getSentences";
+import getWords from "../../../helpers/word/getWords";
+
+
+function calculateAverageLength( array ) {
+	return array.map( unit => unit.length ).reduce( ( a, b ) => a + b );
+}
+
+function getInformalPronouns( words ) {
 	const informalPronouns = [ "I", "you", "me", "my", "mine", "your", "yours", "myself", "yourself", "yourselves" ];
+
+	return words.map( word => informalPronouns.includes( word ) );
+}
+
+export default function( text, passiveSentences ) {
 	/*
-1. average informal pronouns
-2. average word length per sentence
-3. average word length
- */
+	 * 0 = average letter length per sentence
+	 * 1 = average word length per sentence
+	 * 2 = average word length
+	 * 3 = normalized passive voice size
+	 * 5 = average number of informal pronouns
+	 */
+	const sentences = getSentences( text );
+	const words = getWords( text );
+
+	// Calculate the average letter length per sentence = the total number of characters in the sentences in the text / the total number of the sentences.
+	const totalCharactersInSentences = calculateAverageLength( sentences );
+	const averageSentenceLength = totalCharactersInSentences / sentences.length;
+	// Calculate the average word length per sentence = the total number of words in the text divided by the total number of the sentences.
+	const averageWordLengthPerSentence = words.length / sentences.length;
+	// Calculate the average word length = the total number of characters in the words in the text / the total number of words in the text.
+	const averageWordLength = calculateAverageLength( words );
+	// Calculate the average number of informal pronouns = the number of occurrences of informal pronouns / the total number of words in the text.
+	const averageInformalPronouns = getInformalPronouns( words ) / words.length;
 }
--- packages/yoastseo/src/languageProcessing/researches/getTextFormalityLevel.js
+++ packages/yoastseo/src/languageProcessing/researches/getTextFormalityLevel.js
@@ -1,5 +1,8 @@
 export default function( paper, researcher ) {
 	const checkTextFormality = researcher.getHelper( "checkTextFormality" );
+	// Not sure if we should calculate the passives in the research file or in language specific helper.
+	// Not all languages might use passive constructions for calculating formality.
+	const passiveSentences = researcher.getResearch( "getPassiveVoiceResult" )( paper, researcher ).total;
 	const text = paper.getText();
-	return checkTextFormality( text );
+	return checkTextFormality( text, passiveSentences );
 }
