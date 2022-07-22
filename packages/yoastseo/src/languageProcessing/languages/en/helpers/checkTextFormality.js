3d048e69e8e58fedaefb2c8ef8193f19dbb943cd
FAMarfuaty <aida@yoast.com>
2022-07-22 11:45:42 +0200

    Add checks for the formality level of the text

--- packages/yoastseo/src/languageProcessing/languages/en/helpers/checkTextFormality.js
+++ packages/yoastseo/src/languageProcessing/languages/en/helpers/checkTextFormality.js
@@ -1,35 +1,90 @@
 import getSentences from "../../../helpers/sentence/getSentences";
 import getWords from "../../../helpers/word/getWords";
 
+const informalPronouns = [ "I", "you", "me", "my", "mine", "your", "yours", "myself", "yourself", "yourselves" ];
+const formalPronouns = [ "we", "they",   "their", "theirs", "themselves", "us", "our", "ours", "ourselves",  "it", "its", "itself" ];
 
-function calculateAverageLength( array ) {
+/**
+ * Calculates the average length of units in an array.
+ *
+ * @param {Array} array     The array to calculate.
+ * @returns {number} The total length of a units in the array.
+ */
+function calculateTotalLength( array ) {
 	return array.map( unit => unit.length ).reduce( ( a, b ) => a + b );
 }
 
-function getInformalPronouns( words ) {
-	const informalPronouns = [ "I", "you", "me", "my", "mine", "your", "yours", "myself", "yourself", "yourselves" ];
-
-	return words.map( word => informalPronouns.includes( word ) );
+/**
+ * Gets the array of pronouns occurrences in the text.
+ *
+ * @param {Array} wordsArray    The array of words in the text to check.
+ * @param {Array} pronouns      The array of pronouns.
+ * @returns {Array} The array of pronouns found.
+ */
+function getPronouns( wordsArray, pronouns ) {
+	return wordsArray.map( word => pronouns.includes( word ) );
 }
 
+/**
+ * Checks if a text is formal or informal.
+ *
+ * @param {string} text             The text to check.
+ * @param {number} passiveSentences The number of occurrences of passive voice in the text.
+ *
+ * @returns {string} The string "Formal" if the text is formal, and the string "Informal" if the text is informal.
+ */
 export default function( text, passiveSentences ) {
 	/*
 	 * 0 = average letter length per sentence
 	 * 1 = average word length per sentence
 	 * 2 = average word length
 	 * 3 = normalized passive voice size
+	 * 4 = average number of formal pronouns
 	 * 5 = average number of informal pronouns
 	 */
 	const sentences = getSentences( text );
 	const words = getWords( text );
 
-	// Calculate the average letter length per sentence = the total number of characters in the sentences in the text / the total number of the sentences.
-	const totalCharactersInSentences = calculateAverageLength( sentences );
+	// Calculate the average letter length per sentence.
+	// The total number of characters in the sentences in the text / the total number of the sentences.
+	const totalCharactersInSentences = calculateTotalLength( sentences );
 	const averageSentenceLength = totalCharactersInSentences / sentences.length;
 	// Calculate the average word length per sentence = the total number of words in the text divided by the total number of the sentences.
 	const averageWordLengthPerSentence = words.length / sentences.length;
 	// Calculate the average word length = the total number of characters in the words in the text / the total number of words in the text.
-	const averageWordLength = calculateAverageLength( words );
+	const averageWordLength = calculateTotalLength( words ) / words.length;
+	// Calculate normalized passive voice size = the number of passives occurrences in the text / the total number of sentences.
+	const averagePassives = passiveSentences / sentences.length;
+	// Calculate the average number of formal pronouns = the number of occurrences of formal pronouns / the total number of words in the text.
+	const averageFormalPronouns = getPronouns( words, formalPronouns ).length / words.length;
 	// Calculate the average number of informal pronouns = the number of occurrences of informal pronouns / the total number of words in the text.
-	const averageInformalPronouns = getInformalPronouns( words ) / words.length;
+	const averageInformalPronouns = getPronouns( words, informalPronouns ).length / words.length;
+	if ( averageWordLength <= 5.948 ) {
+		if ( averageInformalPronouns <= 0.029 ) {
+			if ( averageWordLengthPerSentence <= 31.439 ) {
+				if ( averageSentenceLength <= 169.375 ) {
+					return "Formal";
+				}
+				return "Informal";
+			}
+			return "Informal";
+		}
+		if ( averagePassives <= 0.25 ) {
+			return "Informal";
+		}
+		return "Formal";
+	}
+	if ( averageWordLengthPerSentence <= 12.731 || averageSentenceLength <= 88.721 ) {
+		return "Informal";
+	}
+	if ( averageInformalPronouns <= 0.006 ) {
+		return "Formal";
+	}
+	if ( averageWordLength <= 6.129 ) {
+		if ( averageFormalPronouns <= 0.024 ) {
+			return "Informal";
+		}
+		return "Formal";
+	}
+	return "Formal";
 }
