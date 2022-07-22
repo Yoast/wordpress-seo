2ba0d0535d6733f6db163e59640acf3b4055e966
FAMarfuaty <aida@yoast.com>
2022-07-22 13:37:45 +0200

    improves comments

--- packages/yoastseo/src/languageProcessing/languages/en/helpers/checkTextFormality.js
+++ packages/yoastseo/src/languageProcessing/languages/en/helpers/checkTextFormality.js
@@ -22,41 +22,41 @@ function calculateTotalLength( array ) {
  * @returns {Array} The array of pronouns found.
  */
 function getPronouns( wordsArray, pronouns ) {
-	return wordsArray.map( word => pronouns.includes( word ) );
+	return wordsArray.filter( word => pronouns.includes( word ) );
 }
 
 /**
  * Checks if a text is formal or informal.
  *
- * @param {string} text             The text to check.
- * @param {number} passiveSentences The number of occurrences of passive voice in the text.
+ * @param {Object} paper        The paper to analyze.
+ * @param {Object} researcher   The researcher object.
  *
  * @returns {string} The string "Formal" if the text is formal, and the string "Informal" if the text is informal.
  */
-export default function( text, passiveSentences ) {
-	/*
-	 * 0 = average letter length per sentence
-	 * 1 = average word length per sentence
-	 * 2 = average word length
-	 * 3 = normalized passive voice size
-	 * 4 = average number of formal pronouns
-	 * 5 = average number of informal pronouns
-	 */
+export default function( paper, researcher ) {
+	const passiveSentences = researcher.getResearch( "getPassiveVoiceResult" ).passives;
+	const text = paper.getText();
 	const sentences = getSentences( text );
 	const words = getWords( text );
 
 	// Calculate the average letter length per sentence.
 	// The total number of characters in the sentences in the text / the total number of the sentences.
+	// 0.
 	const totalCharactersInSentences = calculateTotalLength( sentences );
 	const averageSentenceLength = totalCharactersInSentences / sentences.length;
 	// Calculate the average word length per sentence = the total number of words in the text divided by the total number of the sentences.
+	// 1.
 	const averageWordLengthPerSentence = words.length / sentences.length;
 	// Calculate the average word length = the total number of characters in the words in the text / the total number of words in the text.
+	// 2.
 	const averageWordLength = calculateTotalLength( words ) / words.length;
 	// Calculate normalized passive voice size = the number of passives occurrences in the text / the total number of sentences.
-	const averagePassives = passiveSentences / sentences.length;
+	// 3.
+	const averagePassives = passiveSentences.length / sentences.length;
 	// Calculate the average number of formal pronouns = the number of occurrences of formal pronouns / the total number of words in the text.
+	// 4.
 	const averageFormalPronouns = getPronouns( words, formalPronouns ).length / words.length;
+	// 5.
 	// Calculate the average number of informal pronouns = the number of occurrences of informal pronouns / the total number of words in the text.
 	const averageInformalPronouns = getPronouns( words, informalPronouns ).length / words.length;
 	if ( averageWordLength <= 5.948 ) {
