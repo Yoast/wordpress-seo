33e438a900c1471c3b231062ad3b9127cb39f257
FAMarfuaty <aida@yoast.com>
2022-07-22 11:45:54 +0200

    add JSDoc

--- packages/yoastseo/src/languageProcessing/researches/getTextFormalityLevel.js
+++ packages/yoastseo/src/languageProcessing/researches/getTextFormalityLevel.js
@@ -1,3 +1,10 @@
+/**
+ * Gets the formality level of the text.
+ *
+ * @param {Object} paper        The paper to analyze.
+ * @param {Object} researcher   The researcher object.
+ * @returns {string} The string "Formal" if the text is formal, and the string "Informal" if the text is informal.
+ */
 export default function( paper, researcher ) {
 	const checkTextFormality = researcher.getHelper( "checkTextFormality" );
 	// Not sure if we should calculate the passives in the research file or in language specific helper.
