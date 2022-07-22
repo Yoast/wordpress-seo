ae67df1f95a36c6b418bbeefb49ca27ce749b734
FAMarfuaty <aida@yoast.com>
2022-07-22 16:31:06 +0200

    add upsell for text formality

--- packages/js/src/insights/components/text-formality.js
+++ packages/js/src/insights/components/text-formality.js
@@ -1,11 +1,58 @@
 import { useSelect } from "@wordpress/data";
+import { useMemo } from "@wordpress/element";
 import { __, sprintf } from "@wordpress/i18n";
 import { BetaBadge } from "@yoast/components";
+import { makeOutboundLink } from "@yoast/helpers";
+import createInterpolateElement from "../../helpers/createInterpolateElement";
 
+const OutboundLink = makeOutboundLink();
+
+/**
+ * TextFormality component.
+ * @returns {JSX.Element} The element.
+ */
 const TextFormality = () => {
 	const shouldUpsell = useSelect( select => select( "yoast-seo/editor" ).getPreference( "shouldUpsell", false ), [] );
 	const formalityLevel = useSelect( select => select( "yoast-seo/editor" ).getTextFormalityLevel() );
-	const textLength = useSelect( select => select( "yoast-seo/editor" ).getTextLength(), [] );
+	const textLength = useSelect( select => select( "yoast-seo/editor" ).getTextLength(), [] ).count;
+	// We need to determine the minimum text required before we return a feedback.
+	const upsellLink = "www.yoast.com";
+	const upsellDescription = useMemo( () => {
+		return createInterpolateElement(
+			sprintf(
+				// translators: %1$s expands to a starting `b` tag, %1$s expands to a closing `b` tag and %3$s expands to `Yoast SEO Premium`.
+				__( "%1$s%3$s%2$s will help you assess the level of formality of your text.", "wordpress-seo" ),
+				"<b>",
+				"</b>",
+				"Yoast SEO Premium"
+			),
+			{
+				b: <b />,
+			}
+		);
+	}, [] );
+
+	const infoText = shouldUpsell
+		? sprintf(
+			__( "%1$sRead more about text formality.%2$s", "wordpress-seo" ),
+			"<a>",
+			"</a>"
+		)
+		: sprintf(
+			__( "Read our %1$sarticle on text formality%2$s to learn more about how you can change the formality level of your text.", "wordpress-seo" ),
+			"<a>",
+			"</a>"
+		);
+
+	const textFormalityInfo = useMemo( () => {
+		const link = "www.yoast.com";
+		return createInterpolateElement(
+			infoText,
+			{
+				a: <OutboundLink href={ link } />,
+			}
+		);
+	}, [] );
 
 	return (
 		<div className="yoast-text-formality">
@@ -35,6 +82,16 @@ const TextFormality = () => {
 				</div>
 			</div>
 			}
+			{ shouldUpsell && <p>{ upsellDescription }</p> }
+			{ shouldUpsell && <OutboundLink href={ upsellLink } className="yoast-button yoast-button-upsell">
+				{ sprintf(
+					// translators: %s expands to `Premium` (part of add-on name).
+					__( "Unlock with %s", "wordpress-seo" ),
+					"Premium"
+				) }
+				<span aria-hidden="true" className="yoast-button-upsell__caret" />
+			</OutboundLink> }
+			<p>{ textFormalityInfo }</p>
 		</div>
 	);
 };
