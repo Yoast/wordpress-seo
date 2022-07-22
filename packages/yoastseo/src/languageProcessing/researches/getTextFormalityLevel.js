a6bfd84635dcd5606d308e4a059bfc5c86259cbf
FAMarfuaty <aida@yoast.com>
2022-07-22 13:36:53 +0200

    move the retrieval of passives to language specific helper

--- packages/yoastseo/src/languageProcessing/researches/getTextFormalityLevel.js
+++ packages/yoastseo/src/languageProcessing/researches/getTextFormalityLevel.js
@@ -7,9 +7,6 @@
  */
 export default function( paper, researcher ) {
 	const checkTextFormality = researcher.getHelper( "checkTextFormality" );
-	// Not sure if we should calculate the passives in the research file or in language specific helper.
-	// Not all languages might use passive constructions for calculating formality.
-	const passiveSentences = researcher.getResearch( "getPassiveVoiceResult" )( paper, researcher ).total;
-	const text = paper.getText();
-	return checkTextFormality( text, passiveSentences );
+
+	return checkTextFormality( paper, researcher );
 }
