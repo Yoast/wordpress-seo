18cccee3626a462909620cbd7f17029fae7414b6
Igor <35524806+igorschoester@users.noreply.github.com>
2022-07-27 13:50:28 +0200

    Fix styled components production build
    By changing the root to be in the shadow DOM
    It has to be something that is on the page

--- packages/js/src/settings/initialize.js
+++ packages/js/src/settings/initialize.js
@@ -80,12 +80,14 @@ domReady( () => {
 
 	const isRtl = select( STORE_NAME ).selectPreference( "isRtl", false );
 
-	// Prevent Styled Components' styles by adding the stylesheet to a div that is not on the page.
-	const styleDummy = document.createElement( "div" );
+	// Prevent Styled Components' styles by adding the stylesheet to a div that is in the shadow DOM.
+	const shadowHost = document.createElement( "div" );
+	const shadowRoot = shadowHost.attachShadow( { mode: "open" } );
+	document.body.appendChild( shadowHost );
 
 	render(
 		<Root context={ { isRtl } }>
-			<StyleSheetManager target={ styleDummy }>
+			<StyleSheetManager target={ shadowRoot }>
 				<HashRouter>
 					<Formik
 						initialValues={ getInitialValues() }
