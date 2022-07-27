d60697bdea51e580e5fd579a5eea6049dc739fa4
FAMarfuaty <aida@yoast.com>
2022-07-27 13:22:13 +0200

    skip the test papers from being run

--- packages/yoastseo/src/languageProcessing/researches/getTextFormalityLevel.js
+++ packages/yoastseo/src/languageProcessing/researches/getTextFormalityLevel.js
@@ -3,7 +3,8 @@
  *
  * @param {Object} paper        The paper to analyze.
  * @param {Object} researcher   The researcher object.
- * @returns {string} The string "Formal" if the text is formal, and the string "Informal" if the text is informal.
+ *
+ * @returns {string} The string "formal" if the text is formal, and the string "informal" if the text is informal.
  */
 export default function( paper, researcher ) {
 	const checkTextFormality = researcher.getHelper( "checkTextFormality" );
