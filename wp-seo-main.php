2553dcc76bcefefd802e59baf5ca3361153379d3
Enrico Battocchi <enrico.battocchi@gmail.com>
2022-07-29 10:31:50 +0200

    Set minimum WP version to 5.9

--- wp-seo-main.php
+++ wp-seo-main.php
@@ -39,7 +39,7 @@ define( 'YOAST_VENDOR_PREFIX_DIRECTORY', 'vendor_prefixed' );
 
 define( 'YOAST_SEO_PHP_REQUIRED', '5.6' );
 define( 'YOAST_SEO_WP_TESTED', '6.0.1' );
-define( 'YOAST_SEO_WP_REQUIRED', '5.8' );
+define( 'YOAST_SEO_WP_REQUIRED', '5.9' );
 
 if ( ! defined( 'WPSEO_NAMESPACES' ) ) {
 	define( 'WPSEO_NAMESPACES', true );
