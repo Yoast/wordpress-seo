867701ce594c59d69a17112ee6e6561ed7bb6c7c
nolledgeable <nolle@yoast.com>
2022-08-02 16:49:53 +0200

    UX feedback media fields

--- packages/js/src/settings/routes/media.js
+++ packages/js/src/settings/routes/media.js
@@ -37,7 +37,7 @@ const Media = () => {
 			__( "Recommended size for this image is %1$s%3$s%2$s", "wordpress-seo" ),
 			"<strong>",
 			"</strong>",
-			"1200x675"
+			"1200x675px"
 		),
 		{
 			strong: <strong className="yst-font-semibold" />,
