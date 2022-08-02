744cf2422a06d93e67cb0bc48ab6bc9210bb263c
Enrico Battocchi <enrico.battocchi@gmail.com>
2022-08-02 10:01:25 +0200

    Remove call to deprecated Ryte integration + remove unused imports

--- wp-seo-main.php
+++ wp-seo-main.php
@@ -5,9 +5,6 @@
  * @package WPSEO\Main
  */
 
-use Yoast\WP\SEO\Helpers\Options_Helper;
-use Yoast\WP\SEO\Integrations\Admin\Ryte_Integration;
-
 if ( ! function_exists( 'add_filter' ) ) {
 	header( 'Status: 403 Forbidden' );
 	header( 'HTTP/1.1 403 Forbidden' );
@@ -237,10 +234,6 @@ function _wpseo_activate() {
 
 	// Clear cache so the changes are obvious.
 	WPSEO_Utils::clear_cache();
-
-	// Schedule cronjob when it doesn't exists on activation.
-	$wpseo_ryte = YoastSEO()->classes->get( Ryte_Integration::class );
-	$wpseo_ryte->activate_hooks();
 
 	do_action( 'wpseo_activate' );
 }
