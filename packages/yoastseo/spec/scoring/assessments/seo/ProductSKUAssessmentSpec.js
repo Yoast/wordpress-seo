ab19d5392a6032609ee5e373434ccb0c8f2a7409
FAMarfuaty <aida@yoast.com>
2022-08-05 15:11:15 +0200

    Adjust SKU feedback strings

--- packages/yoastseo/spec/scoring/assessments/seo/ProductSKUAssessmentSpec.js
+++ packages/yoastseo/spec/scoring/assessments/seo/ProductSKUAssessmentSpec.js
@@ -17,7 +17,7 @@ describe( "a test for SKU assessment for WooCommerce", function() {
 		} ) );
 
 		expect( assessmentResult.getScore() ).toEqual( 9 );
-		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/4lw' target='_blank'>SKU</a>: Your product has a SKU. Good job!" );
+		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/4lw' target='_blank'>SKU</a>: Your product has an SKU. Good job!" );
 	} );
 
 	it( "returns the score 9 when a product has no global SKU, but has variants and all variants have a SKU", function() {
@@ -28,7 +28,8 @@ describe( "a test for SKU assessment for WooCommerce", function() {
 		} ) );
 
 		expect( assessmentResult.getScore() ).toEqual( 9 );
-		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/4lw' target='_blank'>SKU</a>: All your product variants have a SKU. Good job!" );
+		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/4lw' target='_blank'>SKU</a>: All your product variants " +
+			"have an SKU. Good job!" );
 	} );
 
 	it( "returns the score 6 when a product has no global SKU and no variants", function() {
@@ -40,7 +41,7 @@ describe( "a test for SKU assessment for WooCommerce", function() {
 
 		expect( assessmentResult.getScore() ).toEqual( 6 );
 		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/4lw' target='_blank'>SKU</a>:" +
-			" Your product is missing a SKU. <a href='https://yoa.st/4lx' target='_blank'>Include" +
+			" Your product is missing an SKU. <a href='https://yoa.st/4lx' target='_blank'>Include" +
 			" this if you can, as it will help search engines to better understand your content.</a>" );
 	} );
 
@@ -53,11 +54,11 @@ describe( "a test for SKU assessment for WooCommerce", function() {
 
 		expect( assessmentResult.getScore() ).toEqual( 6 );
 		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/4lw' target='_blank'>SKU</a>:" +
-			" Not all your product variants have a SKU. <a href='https://yoa.st/4lx' target='_blank'>Include" +
+			" Not all your product variants have an SKU. <a href='https://yoa.st/4lx' target='_blank'>Include" +
 			" this if you can, as it will help search engines to better understand your content.</a>" );
 	} );
 
-	it( "returns the score 6 when a product has no global SKU, but has variants and not all variants have a SKU", function() {
+	it( "returns the score 6 when a product has no global SKU, but has variants and not all variants have an SKU", function() {
 		const assessmentResult = assessment.getResult( paper, Factory.buildMockResearcher( {
 			hasGlobalSKU: false,
 			hasVariants: true,
@@ -66,7 +67,7 @@ describe( "a test for SKU assessment for WooCommerce", function() {
 
 		expect( assessmentResult.getScore() ).toEqual( 6 );
 		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/4lw' target='_blank'>SKU</a>:" +
-			" Not all your product variants have a SKU. <a href='https://yoa.st/4lx' target='_blank'>Include" +
+			" Not all your product variants have an SKU. <a href='https://yoa.st/4lx' target='_blank'>Include" +
 			" this if you can, as it will help search engines to better understand your content.</a>" );
 	} );
 } );
@@ -84,7 +85,8 @@ describe( "a test for SKU assessment for Shopify", () => {
 		} ) );
 
 		expect( assessmentResult.getScore() ).toEqual( 9 );
-		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/shopify79' target='_blank'>SKU</a>: Your product has a SKU. Good job!" );
+		expect( assessmentResult.getText() ).toEqual( "<a href='https://yoa.st/shopify79' target='_blank'>SKU</a>: " +
+			"Your product has an SKU. Good job!" );
 	} );
 
 	it( "returns with score 6 when the product doesn't have a global SKU nor variants", () => {
