30ddbf0969ca92978e9f086efae193396c4b729f
nolledgeable <nolle@yoast.com>
2022-08-15 14:12:54 +0200

    Add comment to explain why adding space after variable in replacement variable editor is needed

--- packages/js/src/settings/components/formik-replacement-variable-editor-field.js
+++ packages/js/src/settings/components/formik-replacement-variable-editor-field.js
@@ -20,6 +20,15 @@ const FormikReplacementVariableEditorField = ( { className = "", ...props } ) =>
 
 	const handleFocus = useCallback( () => editorRef?.focus(), [ editorRef ] );
 
+	/**
+	 * Preemptively add a space at the end.
+	 * Adding a space is a thing that the replacement variable editor does to auto-separate after a variable.
+	 * If we don't do that in our original data, it will trigger an update and then a save prompt.
+	 * Adding a space here will prevent this scenario, there is no need to actually save the space.
+	 * Being precise here is important, because otherwise a space is added when the user types `%%` and the editor
+	 * resets its focus because unexpected content was received. E.g. only add a space when it is `%%sitename%%`,
+	 * not when it is only `%%` or `%%sitename`.
+	 */
 	const value = useMemo( () => ( field.value?.match( /%%\w+%%$/ ) ? `${ field.value } ` : field.value ) || "", [ field.value ] );
 
 	return (
