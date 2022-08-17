5f4f7f4798d7d4028db6a6d80be152db8fd0f266
hdvos <hdvos93@gmail.com>
2022-08-17 12:48:19 +0200

    remove trailing spaces

--- packages/yoastseo/src/scoring/assessments/inclusiveLanguage/InclusiveLanguageAssessment.js
+++ packages/yoastseo/src/scoring/assessments/inclusiveLanguage/InclusiveLanguageAssessment.js
@@ -65,7 +65,7 @@ export default class InclusiveLanguageAssessment extends Assessment {
 		const memoizedTokenizer = researcher.getHelper( "memoizedTokenizer" );
 		const text = paper.getText();
 		const sentences = getSentences( text, memoizedTokenizer );
-		
+
 		// Also include the text title in the analysis as a separate sentence.
 		const textTitle = paper.getTextTitle();
 		sentences.push( textTitle );
