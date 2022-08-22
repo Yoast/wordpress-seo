18ed4a0d925f424b8c32d3abf5ce078e16ea1a5e
Herre Groen <herre@yoast.com>
2022-08-22 11:56:27 +0200

    Ensure compatibility with WordPress 5.9

--- inc/class-wpseo-image-utils.php
+++ inc/class-wpseo-image-utils.php
@@ -272,8 +272,16 @@ class WPSEO_Image_Utils {
 			return $image['filesize'];
 		}
 
+		if ( ! isset( $image['path'] ) ) {
+			return 0;
+		}
+
 		// If the file size for the file is over our limit, we're going to go for a smaller version.
-		return wp_filesize( self::get_absolute_path( $image['path'] ) );
+		if ( function_exists( 'wp_filesize' ) ) {
+			return wp_filesize( self::get_absolute_path( $image['path'] ) );
+		}
+
+		return file_exists( $image['path'] ) ? (int) filesize( $image['path'] ) : 0;
 	}
 
 	/**
