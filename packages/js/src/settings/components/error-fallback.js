66f503aeb5fe20e358e13e0ba9e27830aa18a6e6
nolledgeable <nolle@yoast.com>
2022-08-25 12:13:53 +0200

    Fix lint errors in error fallback component

--- packages/js/src/settings/components/error-fallback.js
+++ packages/js/src/settings/components/error-fallback.js
@@ -10,7 +10,7 @@ import { useSelectSettings } from "../store";
  */
 const ErrorFallback = ( { error } ) => {
 	const handleRefreshClick = useCallback( () => window?.location?.reload(), [] );
-    const supportLink = useSelectSettings( "selectLink", "", "https://yoast.com/help/support" );
+	const supportLink = useSelectSettings( "selectLink", "", "https://yoast.com/help/support" );
 
 	return (
 		<div role="alert" className="yst-max-w-screen-sm yst-p-8 yst-space-y-4">
@@ -18,12 +18,12 @@ const ErrorFallback = ( { error } ) => {
 			<p>{ __( "We're very sorry, but it seems the following error has halted our application:", "wordpress-seo" ) }</p>
 			<Alert variant="error">{ error?.message || "Undefined error message." }</Alert>
 			<p>{ __( "You can refresh to try again, but all your unsaved changes will be lost. If this error occurs again, please contact our support team and we'll get you sorted.", "wordpress-seo" ) }</p>
-            <div className="yst-flex yst-gap-2">
-                <Button onClick={ handleRefreshClick }>{ __( "Refresh", "wordpress-seo" ) }</Button>
-                <Button variant="secondary" as="a" href={ supportLink } target="blank" rel="noreferrer">
-                    { __( "Contact support", "wordpress-seo" ) }
-                </Button>
-            </div>
+			<div className="yst-flex yst-gap-2">
+				<Button onClick={ handleRefreshClick }>{ __( "Refresh", "wordpress-seo" ) }</Button>
+				<Button variant="secondary" as="a" href={ supportLink } target="blank" rel="noreferrer">
+					{ __( "Contact support", "wordpress-seo" ) }
+				</Button>
+			</div>
 		</div>
 	);
 };
