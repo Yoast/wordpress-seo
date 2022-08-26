c2335a6581d304d40dfe279eceadb608b2b45010
Martijn van der Klis <martijnvanderklis@yoast.com>
2022-08-26 10:20:35 +0200

    Refactored the specs for formality

--- packages/yoastseo/spec/languageProcessing/languages/en/helpers/checkTextFormalitySpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/en/helpers/checkTextFormalitySpec.js
@@ -1,45 +1,37 @@
 import checkTextFormality from "../../../../../src/languageProcessing/languages/en/helpers/checkTextFormality";
 import Paper from "../../../../../src/values/Paper";
 import Researcher from "../../../../../src/languageProcessing/languages/en/Researcher";
-import { informalText1, informalText2 } from "./testPapers/en/informalTexts";
-import { formalText1, formalText2 } from "./testPapers/en/formalTexts";
+import { informalText1, informalText2, informalText3 } from "./testPapers/en/informalTexts";
 import { semiFormalText1, semiFormalText2 } from "./testPapers/en/semiFormalTexts";
+import { formalText1, formalText2 } from "./testPapers/en/formalTexts";
 
 describe( "a test for checking the formality level of a text in English", () => {
-	xit( "returns an informal label if the text is recognized as informal", () => {
-		const paper = new Paper( informalText1 );
+	/**
+	 * Checks whether the given text matches the given formality.
+	 * @param {string} text The text.
+	 * @param {string} formality The formality level ("formal" or "informal").
+	 * @returns {void}
+	 */
+	function checkFormality( text, formality ) {
+		const paper = new Paper( text );
 		const researcher = new Researcher( paper );
-		expect( checkTextFormality( paper, researcher ) ).toEqual( "informal" );
-	} );
+		expect( checkTextFormality( paper, researcher ) ).toEqual( formality );
+	}
 
 	it( "returns an informal label if the text is recognized as informal", () => {
-		const paper = new Paper( informalText2 );
-		const researcher = new Researcher( paper );
-		expect( checkTextFormality( paper, researcher ) ).toEqual( "informal" );
+		checkFormality( informalText1, "informal" );
+		checkFormality( informalText2, "informal" );
+		checkFormality( informalText3, "informal" );
 	} );
 
 	it( "returns an informal label for semi-formal/semi-informal text", () => {
-		const paper = new Paper( semiFormalText1 );
-		const researcher = new Researcher( paper );
-		expect( checkTextFormality( paper, researcher ) ).toEqual( "informal" );
-	} );
-
-	it( "returns an informal label for semi-formal/semi-informal text", () => {
-		const paper = new Paper( semiFormalText2 );
-		const researcher = new Researcher( paper );
-		expect( checkTextFormality( paper, researcher ) ).toEqual( "informal" );
+		checkFormality( semiFormalText1, "informal" );
+		checkFormality( semiFormalText2, "informal" );
 	} );
 
 	it( "returns a formal label if the text is recognized as formal", () => {
-		const paper = new Paper( formalText1 );
-		const researcher = new Researcher( paper );
-		expect( checkTextFormality( paper, researcher ) ).toEqual( "formal" );
-	} );
-
-	it( "returns a formal label if the text is recognized as formal", () => {
-		const paper = new Paper( formalText2 );
-		const researcher = new Researcher( paper );
-		expect( checkTextFormality( paper, researcher ) ).toEqual( "formal" );
+		checkFormality( formalText1, "formal" );
+		checkFormality( formalText2, "formal" );
 	} );
 } );
 
