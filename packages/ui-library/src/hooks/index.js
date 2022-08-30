9a32cfb6d106a43397304efb035ec840c7e18fd3
Igor <35524806+igorschoester@users.noreply.github.com>
2022-08-30 10:51:03 +0200

    Add useBeforeUnload hook

--- packages/ui-library/src/hooks/index.js
+++ packages/ui-library/src/hooks/index.js
@@ -1,3 +1,4 @@
+export { default as useBeforeUnload } from "./use-before-unload";
 export { default as useDescribedBy } from "./use-described-by";
 export { default as usePrevious } from "./use-previous";
 export { default as useRootContext } from "./use-root-context";
