a26acb4b859e2f98098a7517250ca21c36eda700
Enrico Battocchi <enrico.battocchi@gmail.com>
2022-08-31 12:02:22 +0200

    Fix CS

--- inc/wpseo-functions-deprecated.php
+++ inc/wpseo-functions-deprecated.php
@@ -18,13 +18,14 @@ if ( ! function_exists( 'initialize_wpseo_front' ) ) {
 }
 
 if ( ! function_exists( 'wpseo_cli_init' ) ) {
+
+	// phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound
 	/**
 	 * Initialize the WP-CLI integration.
 	 *
 	 * @deprecated 19.6.1
 	 * @codeCoverageIgnore
 	 */
-	// phpcs:disable WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedFunctionFound
 	function wpseo_cli_init() {
 		_deprecated_function( __FUNCTION__, 'WPSEO 19.6.1' );
 	}
