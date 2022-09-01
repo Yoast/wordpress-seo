82b53cc3bcb83562182af0932c9c5ee49fbed670
Igor <35524806+igorschoester@users.noreply.github.com>
2022-09-01 11:08:31 +0200

    Uncomment pages

--- packages/js/src/settings/app.js
+++ packages/js/src/settings/app.js
@@ -2,7 +2,7 @@
 import { AdjustmentsIcon, ChevronDownIcon, ChevronUpIcon, ColorSwatchIcon, DesktopComputerIcon, NewspaperIcon } from "@heroicons/react/outline";
 import { useCallback, useMemo } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
-import { ChildrenLimiter, ErrorBoundary, useBeforeUnload } from "@yoast/ui-library";
+import { Badge, ChildrenLimiter, ErrorBoundary, useBeforeUnload } from "@yoast/ui-library";
 import { useFormikContext } from "formik";
 import { map } from "lodash";
 import PropTypes from "prop-types";
@@ -94,11 +94,15 @@ const Menu = ( { postTypes, taxonomies, idSuffix = "" } ) => {
 				icon={ DesktopComputerIcon }
 				label={ __( "General", "wordpress-seo" ) }
 			>
-				{ /* <SidebarNavigation.SubmenuItem to="/site-representation" label={ __( "Site representation", "wordpress-seo" ) } idSuffix={ idSuffix } /> */ }
+				<SidebarNavigation.SubmenuItem
+					to="/site-representation"
+					label={ __( "Site representation", "wordpress-seo" ) }
+					idSuffix={ idSuffix }
+				/>
 				<SidebarNavigation.SubmenuItem to="/site-defaults" label={ __( "Site defaults", "wordpress-seo" ) } idSuffix={ idSuffix } />
 				<SidebarNavigation.SubmenuItem to="/site-preferences" label={ __( "Site preferences", "wordpress-seo" ) } idSuffix={ idSuffix } />
 				<SidebarNavigation.SubmenuItem to="/webmaster-tools" label={ __( "Webmaster tools", "wordpress-seo" ) } idSuffix={ idSuffix } />
-				{ /* <SidebarNavigation.SubmenuItem to="/breadcrumbs" label={ __( "Breadcrumbs", "wordpress-seo" ) } idSuffix={ idSuffix } /> */ }
+				<SidebarNavigation.SubmenuItem to="/breadcrumbs" label={ __( "Breadcrumbs", "wordpress-seo" ) } idSuffix={ idSuffix } />
 			</SidebarNavigation.MenuItem>
 			<SidebarNavigation.MenuItem
 				id={ `menu-content-settings${ idSuffix && `-${ idSuffix }` }` }
@@ -129,7 +133,7 @@ const Menu = ( { postTypes, taxonomies, idSuffix = "" } ) => {
 				icon={ AdjustmentsIcon }
 				label={ __( "Advanced", "wordpress-seo" ) }
 			>
-				{ /* <SidebarNavigation.SubmenuItem
+				<SidebarNavigation.SubmenuItem
 					to="/crawl-optimization"
 					label={
 						<span className="yst-inline-flex yst-items-center yst-gap-1.5">
@@ -138,7 +142,7 @@ const Menu = ( { postTypes, taxonomies, idSuffix = "" } ) => {
 						</span>
 					}
 					idSuffix={ idSuffix }
-				/> */ }
+				/>
 				<SidebarNavigation.SubmenuItem to="/author-archives" label={ __( "Author archives", "wordpress-seo" ) } idSuffix={ idSuffix } />
 				<SidebarNavigation.SubmenuItem to="/date-archives" label={ __( "Date archives", "wordpress-seo" ) } idSuffix={ idSuffix } />
 				<SidebarNavigation.SubmenuItem to="/search-pages" label={ __( "Search pages", "wordpress-seo" ) } idSuffix={ idSuffix } />
@@ -169,7 +173,7 @@ const App = () => {
 	const { dirty } = useFormikContext();
 	useBeforeUnload(
 		dirty,
-		__( "There are unsaved changes on this page. Leaving means that those changes will be lost. Are you sure you want to leave this page?", "wordpress-seo" )
+		__( "There are unsaved changes on this page. Leaving means that those changes will be lost. Are you sure you want to leave this page?", "wordpress-seo" ),
 	);
 
 	return (
@@ -192,8 +196,8 @@ const App = () => {
 						<ErrorBoundary FallbackComponent={ ErrorFallback }>
 							<Routes>
 								<Route path="author-archives" element={ <AuthorArchives /> } />
-								{ /* <Route path="breadcrumbs" element={ <Breadcrumbs /> } /> */ }
-								{ /* <Route path="crawl-optimization" element={ <CrawlSettings /> } /> */ }
+								<Route path="breadcrumbs" element={ <Breadcrumbs /> } />
+								<Route path="crawl-optimization" element={ <CrawlSettings /> } />
 								<Route path="date-archives" element={ <DateArchives /> } />
 								<Route path="homepage" element={ <Homepage /> } />
 								<Route path="formats" element={ <Formats /> } />
@@ -202,7 +206,7 @@ const App = () => {
 								<Route path="rss" element={ <Rss /> } />
 								<Route path="search-pages" element={ <SearchPages /> } />
 								<Route path="site-defaults" element={ <SiteDefaults /> } />
-								{ /* <Route path="site-representation" element={ <SiteRepresentation /> } /> */ }
+								<Route path="site-representation" element={ <SiteRepresentation /> } />
 								<Route path="site-preferences" element={ <SitePreferences /> } />
 								<Route path="webmaster-tools" element={ <WebmasterTools /> } />
 								<Route path="post-type">
