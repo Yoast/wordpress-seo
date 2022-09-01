d0cc6286d330d7809a485f68ce9b428108e6cf1b
skall <blazekm1lan@seznam.cz>
2022-09-01 16:34:39 +0200

    Check if post is not its own parent

--- src/builders/indexable-post-builder.php
+++ src/builders/indexable-post-builder.php
@@ -250,7 +250,7 @@ class Indexable_Post_Builder {
 		}
 
 		// The attachment should have a post parent.
-		if ( empty( $indexable->post_parent ) ) {
+		if ( empty( $indexable->post_parent ) || $indexable->post_parent === $indexable->object_id ) {
 			return false;
 		}
 
