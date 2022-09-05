022a126f625f7d96965e86f9154a6ca2ea4845fc
Igor <35524806+igorschoester@users.noreply.github.com>
2022-09-05 15:42:39 +0200

    Initial crawl optimization links

--- packages/js/src/settings/routes/crawl-optimization.js
+++ packages/js/src/settings/routes/crawl-optimization.js
@@ -1,14 +1,82 @@
+import { createInterpolateElement, useMemo } from "@wordpress/element";
 import { __, sprintf } from "@wordpress/i18n";
-import { Alert, Badge, TextField, ToggleField } from "@yoast/ui-library";
+import { Alert, Badge, Code, TextField, ToggleField } from "@yoast/ui-library";
 import { Field, useFormikContext } from "formik";
 import { addLinkToString } from "../../helpers/stringHelpers";
 import { FieldsetLayout, FormikFlippedToggleField, FormikValueChangeField, FormLayout } from "../components";
 import { useSelectSettings } from "../store";
 
 /**
- * @returns {JSX.Element} The site preferences route.
+ * @returns {JSX.Element} The crawl optimization route.
  */
-const CrawlSettings = () => {
+const CrawlOptimization = () => {
+	const crawlSettingsLink = useSelectSettings( "selectLink", [], "https://yoa.st/crawl-settings" );
+	const permalinkCleanupLink = useSelectSettings( "selectLink", [], "https://yoa.st/permalink-cleanup" );
+
+	const description = useMemo( () => addLinkToString(
+		sprintf(
+			/* translators: %1$s and %2$s are replaced by opening and closing <a> tags. */
+			__( "Make your site more efficient and more environmentally friendly by preventing search engines from crawling things they don’t need to, and by removing unused WordPress features. %1$sLearn more about crawl settings and how they could benefit your site%2$s.", "wordpress-seo" ),
+			"<a>",
+			"</a>"
+		),
+		crawlSettingsLink,
+		"link-crawl-settings-info"
+	), [] );
+	const removeUnwantedMetadataDescription = useMemo( () => createInterpolateElement(
+		sprintf(
+			/* translators: %1$s expands to `<head>` within a code tag. */
+			__( "WordPress adds a lot of links and content to your site's %1$s and HTTP headers. Most people can safely disable all of these, which can help to save bytes, electricity, and trees.", "wordpress-seo" ),
+			"<code/>"
+		),
+		{
+			code: <code className="yst-text-xs">{ "<head>" }</code>,
+		}
+	), [] );
+
+	const codeExample = useMemo( () => sprintf(
+		/* translators: %1$s expands to an example within a code tag. */
+		__( "E.g., %1$s", "wordpress-seo" ),
+		"<code/>"
+	), [] );
+	const codeExamples = useMemo( () => sprintf(
+		/* translators: %1$s and %2$s both expand to an example within a code tag. */
+		__( "E.g., %1$s and %2$s", "wordpress-seo" )
+		,
+		"<code1/>",
+		"<code2/>"
+	), [] );
+	const descriptions = useMemo( () => ( {
+		removeShortlinks: createInterpolateElement( codeExample, {
+			code: <Code>{ "<link rel=\"shortlink\" href=\"https://www.example.com/?p=1\" />" }</Code>,
+		} ),
+		removeRestApiLinks: createInterpolateElement( codeExample, {
+			code: <Code>{ "<link rel=\"https://api.w.org/\" href=\"https://www.example.com/wp-json/\" />" }</Code>,
+		} ),
+		removeRsdWlwLinks: createInterpolateElement( codeExamples, {
+			code1: <Code>
+				{ "<link rel=\"EditURI\" type=\"application/rsd+xml\" title=\"RSD\" href=\"https://www.example.com/xmlrpc.php?rsd\" />" }
+			</Code>,
+			code2: <Code>
+				{ "<link rel=\"wlwmanifest\" type=\"application/wlwmanifest+xml\" href=\"https://www.example.com/wp-includes/wlwmanifest.xml\" />" }
+			</Code>,
+		} ),
+		removeOembedLinks: createInterpolateElement( codeExample, {
+			code: <Code>
+				{ "<link rel=\"alternate\" type=\"application/json+oembed\" href=\"https://www.example.com/wp-json/oembed/1.0/embed?url=https%3A%2F%2Fwww.example.com%2Fexample-post%2F\" />" }
+			</Code>,
+		} ),
+		removeGenerator: createInterpolateElement( codeExample, {
+			code: <Code>{ "<meta name=\"generator\" content=\"WordPress 6.0.1\" />" }</Code>,
+		} ),
+		removePingbackHeader: createInterpolateElement( codeExample, {
+			code: <Code>{ "X-Pingback: https://www.example.com/xmlrpc.php" }</Code>,
+		} ),
+		removePoweredByHeader: createInterpolateElement( codeExample, {
+			code: <Code>{ "X-Powered-By: PHP/7.4.1" }</Code>,
+		} ),
+	} ), [] );
+
 	const { values } = useFormikContext();
 	const {
 		remove_feed_global_comments: removeFeedGlobalComments,
@@ -16,8 +84,6 @@ const CrawlSettings = () => {
 		search_cleanup: searchCleanup,
 		clean_permalinks: cleanPermalinks,
 	} = values.wpseo;
-	const crawlSettingsLink = useSelectSettings( "selectLink", [], "https://yoa.st/crawl-settings" );
-	const permalinkCleanupLink = useSelectSettings( "selectLink", [], "https://yoa.st/permalink-cleanup" );
 
 	return (
 		<FormLayout
@@ -27,60 +93,75 @@ const CrawlSettings = () => {
 					<Badge variant="info">{ __( "Beta", "wordpress-seo" ) }</Badge>
 				</span>
 			}
-			description={ addLinkToString(
-				sprintf(
-					// translators: %1$s and %2$s are replaced by opening and closing <a> tags.
-					__( "To make the crawling of your site more efficient and environmental friendly, Yoast SEO Premium allows you to remove URLs (added by WordPress) that might not be needed for your site. %1$sLearn more about crawl settings and how they could benefit your site%2$s.", "wordpress-seo" ),
-					"<a>",
-					"</a>"
-				),
-				crawlSettingsLink
-			) }
+			description={ description }
 		>
 			<FieldsetLayout
-				title={ __( "Crawl efficiency", "wordpress-seo" ) }
-				description={ __( "Disable settings to remove links added by WordPress to the header and <head>.", "wordpress-seo" ) }
+				title={ __( "Remove unwanted metadata", "wordpress-seo" ) }
+				description={ removeUnwantedMetadataDescription }
 			>
 				<FormikFlippedToggleField
 					name="wpseo.remove_shortlinks"
 					data-id="input-wpseo-remove_shortlinks"
 					label={ __( "Shortlinks", "wordpress-seo" ) }
-				/>
+				>
+					{ __( "Remove links to WordPress' internal 'shortlink' URLs for your posts.", "wordpress-seo" ) }
+					&nbsp;
+					{ descriptions.removeShortlinks }
+				</FormikFlippedToggleField>
 				<FormikFlippedToggleField
 					name="wpseo.remove_rest_api_links"
 					data-id="input-wpseo-remove_rest_api_links"
 					label={ __( "Rest API links", "wordpress-seo" ) }
-				/>
+				>
+					{ __( "Remove links to the location of your site’s REST API endpoints.", "wordpress-seo" ) }
+					&nbsp;
+					{ descriptions.removeRestApiLinks }
+				</FormikFlippedToggleField>
 				<FormikFlippedToggleField
 					name="wpseo.remove_rsd_wlw_links"
 					data-id="input-wpseo-remove_rsd_wlw_links"
 					label={ __( "RSD / WLW links", "wordpress-seo" ) }
-				/>
+				>
+					{ __( "Remove links used by external systems for publishing content to your blog.", "wordpress-seo" ) }
+					&nbsp;
+					{ descriptions.removeRsdWlwLinks }
+				</FormikFlippedToggleField>
 				<FormikFlippedToggleField
 					name="wpseo.remove_oembed_links"
 					data-id="input-wpseo-remove_oembed_links"
 					label={ __( "oEmbed links", "wordpress-seo" ) }
-				/>
+				>
+					{ __( "Remove links used for embedding your content on other sites.", "wordpress-seo" ) }
+					&nbsp;
+					{ descriptions.removeOembedLinks }
+				</FormikFlippedToggleField>
 				<FormikFlippedToggleField
 					name="wpseo.remove_generator"
 					data-id="input-wpseo-remove_generator"
 					label={ __( "Generator tag", "wordpress-seo" ) }
-				/>
-				<FormikFlippedToggleField
-					name="wpseo.remove_emoji_scripts"
-					data-id="input-wpseo-remove_emoji_scripts"
-					label={ __( "Emoji scripts", "wordpress-seo" ) }
-				/>
+				>
+					{ __( "Remove information about the plugins and software used by your site.", "wordpress-seo" ) }
+					&nbsp;
+					{ descriptions.removeGenerator }
+				</FormikFlippedToggleField>
 				<FormikFlippedToggleField
 					name="wpseo.remove_pingback_header"
 					data-id="input-wpseo-remove_pingback_header"
 					label={ __( "Pingback HTTP header", "wordpress-seo" ) }
-				/>
+				>
+					{ __( "Remove links which allow others sites to ‘ping’ yours when they link to you.", "wordpress-seo" ) }
+					&nbsp;
+					{ descriptions.removePingbackHeader }
+				</FormikFlippedToggleField>
 				<FormikFlippedToggleField
 					name="wpseo.remove_powered_by_header"
 					data-id="input-wpseo-remove_powered_by_header"
 					label={ __( "Powered by HTTP header", "wordpress-seo" ) }
-				/>
+				>
+					{ __( "Remove information about the plugins and software used by your site.", "wordpress-seo" ) }
+					&nbsp;
+					{ descriptions.removePoweredByHeader }
+				</FormikFlippedToggleField>
 			</FieldsetLayout>
 			<hr className="yst-my-8" />
 			<FieldsetLayout
@@ -143,6 +224,18 @@ const CrawlSettings = () => {
 			</FieldsetLayout>
 			<hr className="yst-my-8" />
 			<FieldsetLayout
+				title={ __( "Unused resources", "wordpress-seo" ) }
+				description={ __( "WordPress loads lots of resources, some of which your site might not need. If you’re not using these, removing them can speed up your pages and save resources.", "wordpress-seo" ) }
+			>
+				<FormikFlippedToggleField
+					name="wpseo.remove_emoji_scripts"
+					data-id="input-wpseo-remove_emoji_scripts"
+					label={ __( "Emoji scripts", "wordpress-seo" ) }
+					description={ __( "Remove JavaScript used for converting emoji characters in older browsers.", "wordpress-seo" ) }
+				/>
+			</FieldsetLayout>
+			<hr className="yst-my-8" />
+			<FieldsetLayout
 				title={ __( "Search cleanup", "wordpress-seo" ) }
 				description={ __( "Enable settings to clean up and filter searches to prevent search spam.", "wordpress-seo" ) }
 			>
@@ -183,7 +276,8 @@ const CrawlSettings = () => {
 							"<a>",
 							"</a>"
 						),
-						permalinkCleanupLink
+						permalinkCleanupLink,
+						"link-permalink-cleanup-info"
 					) }
 				</Alert>
 				<FormikFlippedToggleField
@@ -210,4 +304,4 @@ const CrawlSettings = () => {
 	);
 };
 
-export default CrawlSettings;
+export default CrawlOptimization;
