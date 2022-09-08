665a1d038ee6c06cecd8574178e489084bf804cd
Igor <35524806+igorschoester@users.noreply.github.com>
2022-09-08 16:31:54 +0200

    Show save only when form is dirty

--- packages/js/src/settings/components/form-layout.js
+++ packages/js/src/settings/components/form-layout.js
@@ -4,6 +4,7 @@ import { Button, Title } from "@yoast/ui-library";
 import { Form, useFormikContext } from "formik";
 import { includes, values } from "lodash";
 import PropTypes from "prop-types";
+import AnimateHeight from "react-animate-height";
 import { useSelectSettings } from "../store";
 
 /**
@@ -14,7 +15,7 @@ const FormLayout = ( {
 	title,
 	description = null,
 } ) => {
-	const { isSubmitting, status } = useFormikContext();
+	const { isSubmitting, status, dirty } = useFormikContext();
 	const isMediaLoading = useSelectSettings( "selectIsMediaLoading" );
 	const isStatusBlocked = useMemo( () => includes( values( status ), true ), [ status ] );
 
@@ -30,17 +31,24 @@ const FormLayout = ( {
 				{ children }
 			</div>
 			<footer className="yst-sticky yst-bottom-0">
-				<div className="yst-h-8 yst-bg-gradient-to-t yst-from-white" />
-				<div className="yst-p-8 yst-bg-gray-50 yst-rounded-b-lg">
-					<Button
-						id="button-submit-settings"
-						type="submit"
-						isLoading={ isSubmitting }
-						disabled={ isSubmitting || isMediaLoading || isStatusBlocked }
-					>
-						{ __( "Save changes", "wordpress-seo" ) }
-					</Button>
-				</div>
+				<AnimateHeight
+					easing="ease-in-out"
+					duration={ 300 }
+					height={ dirty ? "auto" : 0 }
+					animateOpacity={ true }
+				>
+					<div className="yst-h-8 yst-bg-gradient-to-t yst-from-white" />
+					<div className="yst-p-8 yst-bg-gray-50 yst-rounded-b-lg">
+						<Button
+							id="button-submit-settings"
+							type="submit"
+							isLoading={ isSubmitting }
+							disabled={ isSubmitting || isMediaLoading || isStatusBlocked }
+						>
+							{ __( "Save changes", "wordpress-seo" ) }
+						</Button>
+					</div>
+				</AnimateHeight>
 			</footer>
 		</Form>
 	);
