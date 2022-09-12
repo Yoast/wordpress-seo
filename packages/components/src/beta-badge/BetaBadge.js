bb5b1d70c7d675fb8a112a70ff407b7e611a8179
FAMarfuaty <aida@yoast.com>
2022-09-12 13:53:25 +0200

    remove prop from beta badge

--- packages/components/src/beta-badge/BetaBadge.js
+++ packages/components/src/beta-badge/BetaBadge.js
@@ -1,27 +1,15 @@
 import React from "react";
 import { __ } from "@wordpress/i18n";
-import PropTypes from "prop-types";
 
 /**
  * A component for adding a badge to a beta feature.
  *
- * @param {Object} props            The props.
- * @param {String} props.className  The class name.
- *
  * @returns {React.Component} The BetaBadge.
  */
-const BetaBadge = ( props ) => (
-	<span className={ `yoast-badge ${ props.className }` }>
+const BetaBadge = () => (
+	<span className={ "yoast-badge yoast-beta-badge" }>
 		{ __( "Beta", "wordpress-seo" ) }
 	</span>
 );
-
-BetaBadge.propTypes = {
-	className: PropTypes.string,
-};
-
-BetaBadge.defaultProps = {
-	className: "yoast-beta-badge",
-};
 
 export default BetaBadge;
