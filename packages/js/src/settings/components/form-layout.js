e28720548001ed7c2c7e2611a41872a046e23abd
Igor <35524806+igorschoester@users.noreply.github.com>
2022-09-19 13:54:51 +0200

    Add discard changes modal
    Instead of discarding all the changes right away, now there is an are you sure step

--- packages/js/src/settings/components/form-layout.js
+++ packages/js/src/settings/components/form-layout.js
@@ -1,6 +1,7 @@
+import { ExclamationIcon } from "@heroicons/react/outline";
 import { useCallback, useMemo } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
-import { Button, Title } from "@yoast/ui-library";
+import { Button, Modal, Title, useSvgAria, useToggleState } from "@yoast/ui-library";
 import { Form, useFormikContext } from "formik";
 import { includes, values } from "lodash";
 import PropTypes from "prop-types";
@@ -18,10 +19,13 @@ const FormLayout = ( {
 	const { isSubmitting, status, dirty, resetForm, initialValues } = useFormikContext();
 	const isMediaLoading = useSelectSettings( "selectIsMediaLoading" );
 	const isStatusBlocked = useMemo( () => includes( values( status ), true ), [ status ] );
+	const svgAriaProps = useSvgAria();
 
+	const [ isRequestUndo, , , setRequestUndo, unsetRequestUndo ] = useToggleState( false );
 	const handleUndo = useCallback( () => {
+		unsetRequestUndo();
 		resetForm( { values: initialValues } );
-	}, [ resetForm, initialValues ] );
+	}, [ resetForm, initialValues, unsetRequestUndo ] );
 
 	return (
 		<Form className="yst-flex yst-flex-col yst-h-full yst-min-h-[75vh]">
@@ -56,10 +60,35 @@ const FormLayout = ( {
 								type="button"
 								variant="secondary"
 								disabled={ ! dirty }
-								onClick={ handleUndo }
+								onClick={ setRequestUndo }
 							>
 								{ __( "Discard changes", "wordpress-seo" ) }
 							</Button>
+							<Modal onClose={ unsetRequestUndo } isOpen={ isRequestUndo }>
+								<div className="sm:yst-flex sm:yst-items-start">
+									<div
+										className="yst-mx-auto yst-flex-shrink-0 yst-flex yst-items-center yst-justify-center yst-h-12 yst-w-12 yst-rounded-full yst-bg-red-100 sm:yst-mx-0 sm:yst-h-10 sm:yst-w-10"
+									>
+										<ExclamationIcon className="yst-h-6 yst-w-6 yst-text-red-600" { ...svgAriaProps } />
+									</div>
+									<div className="yst-mt-3 yst-text-center sm:yst-mt-0 sm:yst-ml-4 sm:yst-text-left">
+										<Modal.Title as="h3" className="yst-text-lg yst-leading-6 yst-font-medium yst-text-gray-900">
+											{ __( "Discard all changes", "wordpress-seo" ) }
+										</Modal.Title>
+										<Modal.Description className="yst-text-sm yst-text-gray-500">
+											{ __( "You are about to discard all unsaved changes. All of your settings will be reset to the point where you last saved. Are you sure you want to do this?", "wordpress-seo" ) }
+										</Modal.Description>
+									</div>
+								</div>
+								<div className="yst-flex yst-flex-col sm:yst-flex-row-reverse yst-gap-3 yst-mt-6">
+									<Button type="button" variant="error" onClick={ handleUndo }>
+										{ __( "Yes, discard changes", "wordpress-seo" ) }
+									</Button>
+									<Button type="button" variant="secondary" onClick={ unsetRequestUndo }>
+										{ __( "No, continue editing", "wordpress-seo" ) }
+									</Button>
+								</div>
+							</Modal>
 						</div>
 					</div>
 				</AnimateHeight>
