08b4f5032cb9c0e8db7d545bb7f00fd03c7f0555
FAMarfuaty <aida@yoast.com>
2022-09-19 15:24:58 +0200

    adjusted unit tests

--- packages/js/tests/insights/components/text-formality.test.js
+++ packages/js/tests/insights/components/text-formality.test.js
@@ -2,17 +2,45 @@ import TextFormality from "../../../src/insights/components/text-formality";
 import React from "react";
 import { shallow } from "enzyme";
 import TextFormalityUpsell from "../../../src/insights/components/text-formality-upsell";
+import { useSelect } from "@wordpress/data";
 
 window.wpseoAdminL10n = {
 	"shortlinks-insights-text_formality_info_free": "https://yoa.st/formality-free",
 	"shortlinks-insights-text_formality_info_premium": "https://yoa.st/formality",
 };
 
+jest.mock( "@wordpress/data", () => (
+	{
+		useSelect: jest.fn(),
+	}
+) );
+
+/**
+ * Mocks the WordPress `useSelect` hook.
+ *
+ * @param {boolean} isFormalitySupported    Whether Formality feature is available.
+ *
+ * @returns {void}
+ */
+function mockSelect( isFormalitySupported ) {
+	const select = jest.fn(
+		() => (
+			{
+				isFormalitySupported: jest.fn( () => isFormalitySupported ),
+			}
+		)
+	);
+
+	useSelect.mockImplementation(
+		selectFunction => selectFunction( select )
+	);
+}
+
 describe( "a test for TextFormality component", () => {
 	it( "should not render the component if the locale is non-English", () => {
+		mockSelect( false );
 		window.wpseoScriptData = {
 			metabox: {
-				contentLocale: "nl_NL",
 				isPremium: false,
 			},
 		};
@@ -21,9 +49,10 @@ describe( "a test for TextFormality component", () => {
 		expect( render.find( TextFormalityUpsell ) ).toHaveLength( 0 );
 	} );
 	it( "renders the component in sidebar in Free when the locale is English", () => {
+		mockSelect( true );
+
 		window.wpseoScriptData = {
 			metabox: {
-				contentLocale: "en_US",
 				isPremium: false,
 			},
 		};
