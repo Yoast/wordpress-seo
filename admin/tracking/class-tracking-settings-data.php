c440b4be47cc037362c4b4ab053fde6183335a0c
Enrico Battocchi <enrico.battocchi@gmail.com>
2022-09-20 09:43:49 +0200

    Introduce the new option `redirect_search_pretty_urls`

--- admin/tracking/class-tracking-settings-data.php
+++ admin/tracking/class-tracking-settings-data.php
@@ -214,6 +214,7 @@ class WPSEO_Tracking_Settings_Data implements WPSEO_Collection {
 		'search_cleanup_emoji',
 		'search_cleanup_patterns',
 		'search_character_limit',
+		'redirect_search_pretty_urls',
 		'wordproof_integration_active',
 		'indexables_overview_state',
 		'deny_search_crawling',
