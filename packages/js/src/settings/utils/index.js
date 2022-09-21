444d74f017c9834c5fb030c0bcee6e96889b0d98
nolledgeable <nolle@yoast.com>
2022-09-21 22:55:06 +0200

    Fix jsdoc

--- packages/js/src/settings/utils/index.js
+++ packages/js/src/settings/utils/index.js
@@ -3,7 +3,7 @@ import { reduce, isObject, isArray, join, filter } from "lodash";
 /**
  * Flattens nested object to a single level. Props will be keyed by dot syntax path.
  * @param {Object} object The object to flatten.
- * @param {string} parentPath The parent object path..
+ * @param {string} parentPath The parent object path.
  * @returns {Object} Flattened object.
  */
 export const flattenObject = ( object, parentPath = "" ) => reduce(
