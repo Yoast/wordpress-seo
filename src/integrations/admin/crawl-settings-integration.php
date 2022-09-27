7b3eaa87c403fcdb084dbf3c53c05818b95959ac
Enrico Battocchi <enrico.battocchi@gmail.com>
2022-09-27 12:10:53 +0200

    Update copy for the toggle

--- src/integrations/admin/crawl-settings-integration.php
+++ src/integrations/admin/crawl-settings-integration.php
@@ -142,7 +142,7 @@ class Crawl_Settings_Integration implements Integration_Interface {
 			'search_cleanup_emoji'        => \__( 'Filter searches with emojis and other special characters', 'wordpress-seo' ),
 			'search_cleanup_patterns'     => \__( 'Filter searches with common spam patterns', 'wordpress-seo' ),
 			'deny_search_crawling'        => \__( 'Prevent search engines from crawling site search URLs', 'wordpress-seo' ),
-			'redirect_search_pretty_urls' => \__( 'Redirect pretty search URLs to raw format', 'wordpress-seo' ),
+			'redirect_search_pretty_urls' => \__( 'Redirect pretty URLs for search pages to raw format', 'wordpress-seo' ),
 		];
 
 		$this->unused_resources_settings = [
