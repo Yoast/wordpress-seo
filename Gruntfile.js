b85dff6e7f4173f21e1f804da5e87ca05f8f13df
Jannes <jannes@yoast.com>
2022-09-29 08:02:56 +0200

    Revert a few

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function( grunt ) {
 
 	/* Used to switch between development and release builds.
 	Switches based on the grunt command (which is the third 'argv', after node and grunt,  so index 2).*/
-	const developmentBuild = ! [ "create-rc", "release", "release:js", "artifact", "deploy:trunk", "deploy:main" ].includes( process.argv[ 2 ] );
+	const developmentBuild = ! [ "create-rc", "release", "release:js", "artifact", "deploy:trunk", "deploy:master" ].includes( process.argv[ 2 ] );
 
 	// Define project configuration.
 	const project = {
