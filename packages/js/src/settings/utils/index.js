226f9a7033c352c36d33d4e7cbc7490a28918452
Igor <35524806+igorschoester@users.noreply.github.com>
2022-10-03 17:14:27 +0200

    Improve flexibility of flattenObject util

--- packages/js/src/settings/utils/index.js
+++ packages/js/src/settings/utils/index.js
@@ -1,19 +1,31 @@
-import { reduce, isObject, isArray, join, filter } from "lodash";
+import { filter, isArray, isObject, join, reduce } from "lodash";
+
+/**
+ * Determines if the given value should flatten.
+ * @param {*} value The value.
+ * @returns {boolean} True if the value is an object or an array.
+ */
+const shouldFlatten = value => isObject( value ) || isArray( value );
 
 /**
  * Flattens nested object to a single level. Props will be keyed by dot syntax path.
  * @param {Object} object The object to flatten.
  * @param {string} parentPath The parent object path.
+ * @param {function} getShouldFlatten Determines whether the entry should be flattened.
  * @returns {Object} Flattened object.
  */
-export const flattenObject = ( object, parentPath = "" ) => reduce(
+export const flattenObject = ( object, parentPath = "", getShouldFlatten = shouldFlatten ) => reduce(
 	object,
-	( acc, value, key ) => isObject( value ) || isArray( value ) ? {
-		...acc,
-		...flattenObject( value, join( filter( [ parentPath, key ], Boolean ), "." ) ),
-	} : {
-		...acc,
-		[ `${parentPath}.${key}` ]: value,
+	( acc, value, key ) => {
+		const flatKey = join( filter( [ parentPath, key ], Boolean ), "." );
+
+		return getShouldFlatten( value, key ) ? {
+			...acc,
+			...flattenObject( value, flatKey, getShouldFlatten ),
+		} : {
+			...acc,
+			[ flatKey ]: value,
+		};
 	},
 	{}
 );
