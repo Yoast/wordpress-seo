31eb77a1ea162c3e96d641cbd672d6d8326511ae
nolledgeable <nolle@yoast.com>
2022-10-06 17:14:59 +0200

    Update user select

--- packages/js/src/settings/components/formik-user-select-field.js
+++ packages/js/src/settings/components/formik-user-select-field.js
@@ -10,9 +10,9 @@ import { debounce, find, isEmpty, map, values } from "lodash";
 import PropTypes from "prop-types";
 import { ASYNC_ACTION_STATUS } from "../constants";
 import { useDispatchSettings, useSelectSettings } from "../hooks";
-
+​
 let abortController;
-
+​
 /**
  * @param {JSX.node} children The children.
  * @param {string} [className] The className.
@@ -23,12 +23,12 @@ const UserSelectOptionsContent = ( { children, className = "" } ) => (
 		{ children }
 	</div>
 );
-
+​
 UserSelectOptionsContent.propTypes = {
 	children: PropTypes.node.isRequired,
 	className: PropTypes.string,
 };
-
+​
 /**
  * @param {Object} props The props object.
  * @param {string} props.name The field name.
@@ -42,12 +42,12 @@ const FormikUserSelectField = ( { name, id, className = "", ...props } ) => {
 	const [ { value, ...field }, , { setTouched, setValue } ] = useField( { type: "select", name, id, ...props } );
 	const [ status, setStatus ] = useState( ASYNC_ACTION_STATUS.idle );
 	const [ queriedUserIds, setQueriedUserIds ] = useState( [] );
-
+​
 	const selectedUser = useMemo( () => {
 		const userObjects = values( users );
 		return find( userObjects, [ "id", value ] );
 	}, [ value, users ] );
-
+​
 	const debouncedFetchUsers = useCallback( debounce( async search => {
 		try {
 			setStatus( ASYNC_ACTION_STATUS.loading );
@@ -56,13 +56,13 @@ const FormikUserSelectField = ( { name, id, className = "", ...props } ) => {
 				abortController?.abort();
 			}
 			abortController = new AbortController();
-
+​
 			const response = await apiFetch( {
 				// eslint-disable-next-line camelcase
 				path: `/wp/v2/users?${ buildQueryString( { search, per_page: 20 } ) }`,
 				signal: abortController?.signal,
 			} );
-
+​
 			setQueriedUserIds( map( response, "id" ) );
 			addManyUsers( response );
 			setStatus( ASYNC_ACTION_STATUS.success );
@@ -73,22 +73,22 @@ const FormikUserSelectField = ( { name, id, className = "", ...props } ) => {
 			}
 			setQueriedUserIds( [] );
 			setStatus( ASYNC_ACTION_STATUS.error );
-
+​
 			console.error( error.message );
 		}
 	}, 200 ), [ setQueriedUserIds, addManyUsers, setStatus ] );
-
+​
 	const handleChange = useCallback( newValue => {
 		setTouched( true, false );
 		setValue( newValue );
 	}, [ setValue ] );
 	const handleQueryChange = useCallback( event => debouncedFetchUsers( event.target.value ), [ debouncedFetchUsers ] );
-
+​
 	useEffect( () => {
 		// Get initial options.
 		debouncedFetchUsers( "" );
 	}, [] );
-
+​
 	return (
 		<AutocompleteField
 			{ ...field }
@@ -98,7 +98,7 @@ const FormikUserSelectField = ( { name, id, className = "", ...props } ) => {
 			value={ selectedUser ? value : 0 }
 			onChange={ handleChange }
 			placeholder={ __( "Select a user...", "wordpress-seo" ) }
-			selectedLabel={ trim( selectedUser?.name ) || selectedUser?.username }
+			selectedLabel={ selectedUser?.name }
 			onQueryChange={ handleQueryChange }
 			className={ className }
 			{ ...props }
@@ -135,11 +135,11 @@ const FormikUserSelectField = ( { name, id, className = "", ...props } ) => {
 		</AutocompleteField>
 	);
 };
-
+​
 FormikUserSelectField.propTypes = {
 	name: PropTypes.string.isRequired,
 	id: PropTypes.string.isRequired,
 	className: PropTypes.string,
 };
-
+​
 export default FormikUserSelectField;
