57cf7c8c8211a497176895065c3da1eb2e68e3b5
nolledgeable <nolle@yoast.com>
2022-10-26 17:23:12 +0200

    Improve search algorithm

--- packages/js/src/settings/components/search.js
+++ packages/js/src/settings/components/search.js
@@ -3,23 +3,11 @@ import { SearchIcon } from "@heroicons/react/outline";
 import { useCallback, useRef, useState } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
 import { Modal, useSvgAria, useToggleState, TextInput, Title } from "@yoast/ui-library";
-import { debounce, filter, first, isEmpty, map, reduce, trim, includes } from "lodash";
+import { debounce, max, first, isEmpty, map, reduce, trim, includes, split, values, groupBy } from "lodash";
 import { Link } from "react-router-dom";
 import { useSelectSettings } from "../hooks";
 
 const QUERY_MIN_CHARS = 3;
-
-/**
- * @param {Object} items The search index items.
- * @returns {Object} The grouped items.
- */
-const groupItemsByRoute = items => reduce( items, ( acc, value ) => ( {
-	...acc,
-	[ value.route ]: [
-		...( acc[ value.route ] || [] ),
-		value,
-	],
-} ), {} );
 
 /**
  * @returns {JSX.Element} The element.
@@ -27,6 +15,7 @@ const groupItemsByRoute = items => reduce( items, ( acc, value ) => ( {
 const Search = () => {
 	// eslint-disable-next-line no-unused-vars
 	const [ isOpen, , , setOpen, setClose ] = useToggleState( false );
+	// const [ isLoading, setIsLoading ] = useState( false );
 	const [ query, setQuery ] = useState( "" );
 	const searchIndex = useSelectSettings( "selectSearchIndex" );
 	const queryableSearchIndex = useSelectSettings( "selectQueryableSearchIndex" );
@@ -37,22 +26,59 @@ const Search = () => {
 	const handleNavigate = useCallback( () => {
 		setClose();
 		setQuery( "" );
+		setResults( [] );
 	}, [ setClose, setQuery ] );
 
-	const debouncedSearch = useCallback( debounce( newQuery => {
+	const search = useCallback( newQuery => {
 		const trimmedQuery = trim( newQuery );
 
-		if ( trimmedQuery.length > QUERY_MIN_CHARS ) {
-			setResults( groupItemsByRoute(
-				filter( queryableSearchIndex, item => includes( item?.keywords, trimmedQuery ) )
-			) );
+		// Bail if query is too short.
+		if ( trimmedQuery.length < QUERY_MIN_CHARS ) {
+			return false;
 		}
-	}, 500 ), [ queryableSearchIndex, searchIndex ] );
+
+		// Split query into words.
+		const splitQuery = split( trimmedQuery, " " );
+
+		// Filter search index by split query and store number of hits.
+		// A hit is registered if a single word from split query in found in a fields keywords.
+		const queryResults = reduce( queryableSearchIndex, ( queryResultsAcc, item ) => {
+			const hits = reduce( splitQuery, ( hitsAcc, queryWord ) => includes( item?.keywords, queryWord ) ? ++hitsAcc : hitsAcc, 0 );
+
+			if ( hits > 0 ) {
+				return [
+					...queryResultsAcc,
+					{
+						...item,
+						hits,
+					},
+				];
+			}
+
+			return queryResultsAcc;
+		}, [] );
+
+		// Sort query results by number of hits on field, highest number of hits first.
+		const sortedQueryResults = queryResults.sort( ( a, b ) => b.hits - a.hits );
+
+		// Group query results by route.
+		const groupedQueryResults = groupBy( sortedQueryResults, "route" );
+
+		// Sort route groups by max hits of single field, highest number of max hits first.
+		// Ie. if group A contains a field with 2 hits and group B contains 2 fields with 1 hit, group A will be sorted first.
+		const sortedGroupedQueryResults = values( groupedQueryResults ).sort( ( a, b ) => {
+			const aMaxHits = reduce( a, ( maxHitsAcc, queryResult ) => max( [ maxHitsAcc, queryResult.hits ] ), 0 );
+			const bMaxHits = reduce( b, ( maxHitsAcc, queryResult ) => max( [ maxHitsAcc, queryResult.hits ] ), 0 );
+			return bMaxHits - aMaxHits;
+		} );
+
+		setResults( sortedGroupedQueryResults );
+	}, [ queryableSearchIndex, searchIndex ] );
 
 	const handleQueryChange = useCallback( event => {
 		setQuery( event.target.value );
-		debouncedSearch( event.target.value );
-	}, [ setQuery, debouncedSearch ] );
+		search( event.target.value );
+	}, [ setQuery, search ] );
 
 	return <>
 		<button
@@ -85,10 +111,10 @@ const Search = () => {
 						className="yst-h-12 yst-w-full yst-border-0 yst-bg-transparent yst-px-11 yst-text-slate-800 yst-placeholder-slate-400 focus:yst-ring-0 sm:yst-text-sm"
 					/>
 				</div>
-				{ query.length > QUERY_MIN_CHARS && ! isEmpty( results ) && (
+				{ query.length >= QUERY_MIN_CHARS && ! isEmpty( results ) && (
 					<ul className="yst-max-h-80 yst-scroll-pt-11 yst-scroll-pb-2 yst-overflow-y-auto yst-pb-2">
-						{ map( results, ( groupedItems, route ) => (
-							<li key={ route }>
+						{ map( results, ( groupedItems, index ) => (
+							<li key={ groupedItems?.[ 0 ]?.route || `group-${ index }` }>
 								<Title as="h4" size="3" className="yst-bg-slate-100 yst-py-3 yst-px-4">{ first( groupedItems ).routeLabel }</Title>
 								<ul>
 									{ map( groupedItems, ( item, name ) => (
@@ -107,13 +133,13 @@ const Search = () => {
 						) ) }
 					</ul>
 				) }
-				{ query.length <= QUERY_MIN_CHARS && (
+				{ query.length < QUERY_MIN_CHARS && (
 					<div className="yst-border-t yst-border-slate-100 yst-p-6 yst-space-3 yst-text-center yst-text-sm sm:yst-px-14">
 						<p className="yst-font-semibold yst-text-slate-900">{ __( "Search", "wordpress-seo" ) }</p>
 						<p className="yst-text-slate-500">{ __( "Please enter a search term that is longer than 3 characters.", "wordpress-seo" ) }</p>
 					</div>
 				) }
-				{ query.length > QUERY_MIN_CHARS && isEmpty( results ) && (
+				{ query.length >= QUERY_MIN_CHARS && isEmpty( results ) && (
 					<div className="yst-border-t yst-border-slate-100 yst-p-6 yst-space-3 yst-text-center yst-text-sm sm:yst-px-14">
 						<p className="yst-font-semibold yst-text-slate-900">{ __( "No results found", "wordpress-seo" ) }</p>
 						<p className="yst-text-slate-500">{ __( "We couldn’t find anything with that term.", "wordpress-seo" ) }</p>
