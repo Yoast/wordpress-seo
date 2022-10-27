21706a79e5292362e0446261b0f36c86e309a75e
Hans-Christiaan <hans-christiaan@yoast.com>
2022-10-27 14:46:17 +0200

    Adapted image alt tags assessment to use the HTML tree.

--- packages/yoastseo/src/languageProcessing/researches/altTagCount.js
+++ packages/yoastseo/src/languageProcessing/researches/altTagCount.js
@@ -1,68 +1,28 @@
 /** @module researches/imageAltTags */
 
-import imageInText from "../helpers/image/imageInText";
-import imageAlttag from "../helpers/image/getAlttagContent";
-import { findTopicFormsInString } from "../helpers/match/findKeywordFormsInString";
-import { isEmpty } from "lodash-es";
+import findAllInTree from "../../parse/findAllTree";
+import hasAttribute from "../../parse/helpers/hasAttribute";
+import attributeEmpty from "../../parse/helpers/attributeEmpty";
 
 /**
- * Matches the alt-tags in the images found in the text.
- * Returns an object with the totals and different alt-tags.
+ * Checks the number of images with no alt text.
  *
- * @param {Array}       imageMatches        	Array with all the matched images in the text
- * @param {Object}      topicForms          	The object with the keyphrase and the synonyms forms from the paper.
- * @param {string}      locale              	The locale used for transliteration.
- * @param {function}    matchWordCustomHelper 	A language-specific helper function to match word in text.
+ * @param {Paper} paper The paper to check for images.
  *
- * @returns {object} altProperties Object with all alt-tags that were found.
+ * @returns {object} An object containing the total number of images and the number of images that have no alt text.
  */
-const matchAltProperties = function( imageMatches, topicForms, locale, matchWordCustomHelper ) {
-	const altProperties = {
-		noAlt: 0,
-		withAlt: 0,
-		withAltKeyword: 0,
-		withAltNonKeyword: 0,
+export default function altTagCount( paper ) {
+	const images = findAllInTree(
+		paper.getTree(),
+		node => node.nodeName === "img"
+	);
+
+	const imagesWithNoAltText = images.filter(
+		image => ! hasAttribute( image, "alt" ) || attributeEmpty( image, "alt" )
+	);
+
+	return {
+		totalNrOfImages: images.length,
+		nrOfImagesWithNoAlt: imagesWithNoAltText.length,
 	};
-
-	for ( let i = 0; i < imageMatches.length; i++ ) {
-		const alttag = imageAlttag( imageMatches[ i ] );
-
-		// If no alt-tag is set
-		if ( alttag === "" ) {
-			altProperties.noAlt++;
-			continue;
-		}
-
-		// If no keyword is set, but the alt-tag is
-		if ( isEmpty( topicForms.keyphraseForms ) ) {
-			altProperties.withAlt++;
-			continue;
-		}
-
-		// If the keyword is matched in the alt tag
-		const keywordMatchedInAltTag = findTopicFormsInString( topicForms, alttag, true, locale, matchWordCustomHelper );
-		if ( keywordMatchedInAltTag.percentWordMatches >= 50 ) {
-			altProperties.withAltKeyword++;
-			continue;
-		}
-
-		altProperties.withAltNonKeyword++;
-	}
-
-	return altProperties;
-};
-
-/**
- * Checks the text for images, checks the type of each image and alt attributes for containing keywords
- *
- * @param {Paper}       paper       The paper to check for images.
- * @param {Researcher}  researcher  The researcher to use for analysis.
- *
- * @returns {object} Object containing all types of found images
- */
-export default function altTagCount( paper, researcher ) {
-	const topicForms = researcher.getResearch( "morphology" );
-	const matchWordCustomHelper = researcher.getHelper( "matchWordCustomHelper" );
-
-	return matchAltProperties( imageInText( paper.getText() ), topicForms, paper.getLocale(), matchWordCustomHelper );
 }
