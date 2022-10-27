e406e6c2257dfb0410ac98619f7d773cdcbbfa5b
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-10-27 14:11:09 +0300

    Start implementing grunt build:js in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,12 @@ const importCSS = require( "postcss-import" );
 const rtlcss = require( "gulp-rtlcss" );
 const tailwind = require( "tailwindcss" );
 const fs = require( "fs" );
+const exec = require('child_process').exec
+const del = require( "del" );
 
 const cssFolder = "css/src/";
 const cssDest = "csstest/";
+const cssDestLive = "css/dist/";
 const tailwindConfig = "tailwind.config.js";
 
 const files = fs.readdirSync( "css/src" );
@@ -59,5 +62,19 @@ gulp.task( "watch", function() {
 		gulp.watch( cssFolder + task, gulp.parallel( task + "-rtl" ) );
 	} );
 } );
+
+gulp.task( "clean", function() {
+	return del(
+		[ "js/dist", cssDestLive + "monorepo*.css" ]
+	);
+} );
+
+gulp.task('shell-webpack', function (cb) {
+	exec('cross-env NODE_ENV=development yarn run wp-scripts build --config config/webpack/webpack.config.js', function (err, stdout, stderr) {
+	  console.log(stdout);
+	  console.log(stderr);
+	  cb(err);
+	});
+  })
 
 gulp.task( "default", gulp.parallel( css ) );
