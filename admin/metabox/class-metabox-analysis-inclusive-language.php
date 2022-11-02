cb15688bbb9362bf3e21fa63e465a9bea7d84144
Agnieszka Szuba <agnieszka@yoast.com>
2022-11-02 12:10:04 +0100

    Don't load inclusive language in Free when a version of Premium that still has inclusive language is active

--- admin/metabox/class-metabox-analysis-inclusive-language.php
+++ admin/metabox/class-metabox-analysis-inclusive-language.php
@@ -16,7 +16,7 @@ class WPSEO_Metabox_Analysis_Inclusive_Language implements WPSEO_Metabox_Analysi
 	 * @return bool Whether or not this analysis is enabled.
 	 */
 	public function is_enabled() {
-		return $this->is_globally_enabled() && $this->is_user_enabled()
+		return $this->is_globally_enabled() && $this->is_user_enabled() && $this->is_current_version_supported()
 				&& YoastSEO()->helpers->language->has_inclusive_language_support( \WPSEO_Language_Utils::get_language( \get_locale() ) );
 	}
 
@@ -36,5 +36,16 @@ class WPSEO_Metabox_Analysis_Inclusive_Language implements WPSEO_Metabox_Analysi
 	 */
 	public function is_globally_enabled() {
 		return WPSEO_Options::get( 'inclusive_language_analysis_active', false );
+	}
+
+	private function is_current_version_supported() {
+		$is_premium      = YoastSEO()->helpers->product->is_premium();
+		$premium_version = YoastSEO()->helpers->product->get_premium_version();
+
+		/*
+		 * In Premium version 19.7, inclusive language analysis is moved from Premium to Free. If the user has an older
+		 * version of Premium, inclusive language should not be loaded in Free, since it is already loaded in Premium.
+		 */
+		return ! $is_premium || \version_compare( $premium_version, '19.7', '>=' );
 	}
 }
