ab1b26e47b7eb7933ee35f2023389ababa4b86e4
jrfnl <jrfnl@users.noreply.github.com>
2022-11-06 20:51:39 +0100

    CS/QA: import all used classes

--- src/values/robots/user-agent-list.php
+++ src/values/robots/user-agent-list.php
@@ -2,6 +2,8 @@
 
 namespace Yoast\WP\SEO\Values\Robots;
 
+use Yoast\WP\SEO\Values\Robots\User_Agent;
+
 /**
  * Class User_Agent_List
  */
@@ -37,7 +39,7 @@ class User_Agent_List {
 	 *
 	 * @param string $user_agent The user agent identifier.
 	 *
-	 * @return \Yoast\WP\SEO\Values\Robots\User_Agent
+	 * @return User_Agent
 	 */
 	public function get_user_agent( $user_agent ) {
 		if ( $this->has_user_agent( $user_agent ) ) {
--- src/values/robots/user-agent.php
+++ src/values/robots/user-agent.php
@@ -2,6 +2,8 @@
 
 namespace Yoast\WP\SEO\Values\Robots;
 
+use Yoast\WP\SEO\Values\Robots\Directive;
+
 /**
  * Class Directive
  */
@@ -17,14 +19,14 @@ class User_Agent {
 	/**
 	 * All directives that are allowed for this user agent.
 	 *
-	 * @var \Yoast\WP\SEO\Values\Robots\Directive $allow_directive
+	 * @var Directive $allow_directive
 	 */
 	private $allow_directive;
 
 	/**
 	 * All directives that are disallowed for this user agent.
 	 *
-	 * @var \Yoast\WP\SEO\Values\Robots\Directive $disallow_directive
+	 * @var Directive $disallow_directive
 	 */
 	private $disallow_directive;
 
