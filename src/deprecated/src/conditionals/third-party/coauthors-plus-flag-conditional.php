4c9e0704cad82391b1b763db4496c0571ef75ab0
Paolo Luigi Scala <paolo@yoast.com>
2022-11-08 16:37:43 +0100

    Deprecate CoAuthorsPlus Conditionals

--- src/deprecated/src/conditionals/third-party/coauthors-plus-flag-conditional.php
+++ src/deprecated/src/conditionals/third-party/coauthors-plus-flag-conditional.php
@@ -6,15 +6,20 @@ use Yoast\WP\SEO\Conditionals\Feature_Flag_Conditional;
 
 /**
  * Feature flag conditional for the CoAuthors Plus integration.
+ * 
+ * @deprecated 19.11
  */
 class CoAuthors_Plus_Flag_Conditional extends Feature_Flag_Conditional {
 
 	/**
 	 * Returns the name of the CoAuthors Plus integration feature flag.
+	 * 
+	 * @deprecated 19.11
 	 *
 	 * @return string The name of the feature flag.
 	 */
 	protected function get_feature_flag() {
+		\_deprecated_function( __METHOD__, 'WPSEO 19.11' );
 		return 'COAUTHORS_PLUS';
 	}
 }
