4a81cdfc2f762165face764b0055e53c642f7357
Lars van Rhijn <lars@yoast.com>
2022-11-09 10:47:21 +0100

    Added CS comments

--- admin/ajax/class-yoast-plugin-conflict-ajax.php
+++ admin/ajax/class-yoast-plugin-conflict-ajax.php
@@ -38,9 +38,11 @@ class Yoast_Plugin_Conflict_Ajax {
 		\check_ajax_referer( 'dismiss-plugin-conflict' );
 
 		if ( ! isset( $_POST['data'] ) || ! is_array( $_POST['data'] ) ) {
+			// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Reason: WPSEO_Utils::format_json_encode is considered safe.
 			\wp_die( WPSEO_Utils::format_json_encode( [] ) );
 		}
 
+		// phpcs:ignore WordPress.Security.ValidatedSanitizedInput.InputNotSanitized -- Reason: $conflict_data is getting sanitized later.
 		$conflict_data = \wp_unslash( $_POST['data'] );
 
 		$conflict_data = [
