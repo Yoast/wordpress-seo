a2488c5b30ae898ff109c75a61a16e1b8f2ac285
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-11-14 13:59:06 +0200

    Add unit tests for the post type change watcher

--- tests/unit/integrations/watchers/indexable-post-type-change-watcher-test.php
+++ tests/unit/integrations/watchers/indexable-post-type-change-watcher-test.php
@@ -4,7 +4,6 @@ namespace Yoast\WP\SEO\Tests\Unit\Integrations\Watchers;
 
 use Brain\Monkey\Functions;
 use Mockery;
-use Yoast_Notification;
 use Yoast_Notification_Center;
 use Yoast\WP\SEO\Actions\Indexing\Indexable_Post_Indexation_Action;
 use Yoast\WP\SEO\Conditionals\Admin_Conditional;
@@ -117,27 +116,22 @@ class Indexable_Post_Type_Change_Watcher_Test extends TestCase {
 	}
 
 	/**
-	 * Tests check option with false given as old_value. This happens when the value is updated
-	 * the first time.
+	 * Tests checking if one or more post types change visibility.
 	 *
 	 * @dataProvider provider_check_post_types_public_availability
 	 *
 	 * @covers ::check_post_types_public_availability
-	 * @covers ::maybe_add_notification
-	 * @covers ::add_notification
 	 *
-	 * @param bool                    $is_json_request              Whether it's a JSON request.
-	 * @param array                   $public_post_types            The public post types.
-	 * @param int                     $get_public_post_types_times  The times we get the public post types.
-	 * @param array                   $last_known_public_post_types The last known public post types.
-	 * @param int                     $set_public_post_types_times  The times we get the last known public post types.
-	 * @param int                     $delete_transient_times       The times we delete the transients.
-	 * @param Yoast_Notification|null $notification                 A Yoast_Notification if valid id.
-	 * @param int                     $add_notification_times       The times we add notification.
-	 * @param int                     $schedule_cleanup_times       The times we schedule cleanup.
+	 * @param bool  $is_json_request              Whether it's a JSON request.
+	 * @param array $public_post_types            The public post types.
+	 * @param int   $get_public_post_types_times  The times we get the public post types.
+	 * @param array $last_known_public_post_types The last known public post types.
+	 * @param int   $set_public_post_types_times  The times we get the last known public post types.
+	 * @param int   $delete_transient_times       The times we delete the transients.
+	 * @param int   $schedule_cleanup_times       The times we schedule cleanup.
 	 */
 	public function test_check_post_types_public_availability(
-		$is_json_request, $public_post_types, $get_public_post_types_times, $last_known_public_post_types, $set_public_post_types_times, $delete_transient_times, $notification, $add_notification_times, $schedule_cleanup_times
+		$is_json_request, $public_post_types, $get_public_post_types_times, $last_known_public_post_types, $set_public_post_types_times, $delete_transient_times, $schedule_cleanup_times
 	) {
 		Functions\expect( 'wp_is_json_request' )
 			->once()
@@ -157,7 +151,7 @@ class Indexable_Post_Type_Change_Watcher_Test extends TestCase {
 		$this->options
 			->expects( 'set' )
 			->times( $set_public_post_types_times )
-			->with( $public_post_types );
+			->with( 'last_known_public_post_types', \array_keys( $public_post_types ) );
 
 		Functions\expect( 'delete_transient' )
 			->times( $delete_transient_times )
@@ -176,33 +170,7 @@ class Indexable_Post_Type_Change_Watcher_Test extends TestCase {
 			->expects( 'get_notification_by_id' )
 			->times( $delete_transient_times )
 			->with( 'post-types-made-public' )
-			->andReturn( $notification );
-
-		Functions\expect( 'admin_url' )
-			->times( $add_notification_times )
-			->with( 'admin.php?page=wpseo_titles#top#post-types' )
-			->andReturn( 'https://example.com/admin.php?page=wpseo_titles#top#post-types' );
-
-		if ( $add_notification_times > 0 ) {
-			$new_notification = new Yoast_Notification(
-				'It looks like you\'ve added a new type of content to your website. We recommend that you review your <a href="https://example.com/admin.php?page=wpseo_titles#top#post-types">Search appearance settings</a>.',
-				[
-					'type'         => Yoast_Notification::WARNING,
-					'id'           => 'post-types-made-public',
-					'capabilities' => 'wpseo_manage_options',
-					'priority'     => 0.8,
-				]
-			);
-		}
-
-		Functions\expect( 'wp_get_current_user' )
-			->times( $add_notification_times )
-			->andReturn( 1 );
-
-		$this->notification_center
-			->expects( 'add_notification' )
-			->times( $add_notification_times )
-			->with( 'post-types-made-public' );
+			->andReturn( 'not_null' );
 
 		Functions\expect( 'wp_next_scheduled' )
 			->times( $schedule_cleanup_times * 2 )
@@ -223,8 +191,51 @@ class Indexable_Post_Type_Change_Watcher_Test extends TestCase {
 	public function provider_check_post_types_public_availability() {
 
 		return [
-			[ true, ['irrelevant'], 0, ['irrelevant'], 0, 0, null, 0, 0 ],
-			[ false, [], 1, ['irrelevant'], 1, 0, null, 0, 0 ],
+			[ true, [ 'irrelevant' ], 0, [ 'irrelevant' ], 0, 0, 0 ],
+			[ false, [], 1, [], 1, 0, 0 ],
+			[
+				false,
+				[
+					'post' => 'post',
+					'page' => 'page',
+				],
+				1,
+				[
+					'post' => 'post',
+					'page' => 'page',
+				],
+				0,
+				0,
+				0,
+			],
+			[
+				false,
+				[
+					'post' => 'post',
+					'page' => 'page',
+				],
+				1,
+				[
+					'post' => 'post',
+				],
+				1,
+				1,
+				0,
+			],
+			[
+				false,
+				[
+					'post' => 'post',
+				],
+				1,
+				[
+					'post' => 'post',
+					'page' => 'page',
+				],
+				1,
+				0,
+				1,
+			],
 		];
 	}
 }
