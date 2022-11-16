15760f2f13601f3c910a5d728eb489460ef86cb2
vraja-pro <vraja.pro@gmail.com>
2022-11-16 17:16:22 +0000

    test: added 4 tests for search

---
+++ packages/js/tests/settings/search.test.js
@@ -0,0 +1,28 @@
+import Search from '../../src/settings/components/search';
+import {shallow} from 'enzyme';
+
+describe('Search test', () => {
+    let wrapper;
+    beforeEach(()=>{
+        wrapper = shallow(<Search />);
+    })
+
+    it('Should have "Quick Search" text on search button', () => { 
+        expect(wrapper.find('button').text()).toContain('Quick search...');
+    });
+
+    it('Input should have placeholder', () => {
+    expect(wrapper.find('#input-search').prop('placeholder')).toContain("Search...");
+    });
+
+    it('Should not search under 3 characters', async () => {
+        wrapper.find('#input-search').at(0).simulate('change', { target: {value: 'se' } });
+        expect(wrapper.find('#error-message').text()).toContain("Please enter a search term that is longer than 3 characters.");
+    });
+
+    it('Results not found', async () => {
+        wrapper.find('#input-search').at(0).simulate('change', { target: {value: 'random' } });
+        expect(wrapper.find('#error-message').text()).toContain("We couldn’t find anything with that term.");
+    });
+
+});
