784b82586f1293ea9e0335c788862979c6b81afc
vraja-pro <vraja.pro@gmail.com>
2022-11-17 20:32:31 +0000

    fix: eslint errors, except LF lines and tabs spacing

--- packages/js/tests/settings/search.test.js
+++ packages/js/tests/settings/search.test.js
@@ -1,32 +1,31 @@
-import Search from '../../src/settings/components/search';
-import {shallow} from 'enzyme';
+import Search from "../../src/settings/components/search";
+import { shallow } from "enzyme";
 
-describe('Search test', () => {
+describe( "Search test", () => {
     let wrapper;
-    beforeEach(()=>{
-        wrapper = shallow(<Search />);
-    })
+    beforeEach( () => {
+        wrapper = shallow( <Search /> );
+    } );
 
-    it('Should have "Quick Search" text on search button', () => { 
-        expect(wrapper.find('button').text()).toContain('Quick search...');
-    });
+    it( "Should have 'Quick Search' text on search button", () => {
+        expect( wrapper.find( "button" ).text() ).toContain( "Quick search..." );
+    } );
 
-    it('Input should have placeholder', () => {
-    expect(wrapper.find('#input-search').prop('placeholder')).toContain("Search...");
-    });
+    it( "Input should have placeholder", () => {
+        expect( wrapper.find( "#input-search" ).prop( "placeholder" ) ).toContain( "Search..." );
+    } );
 
-    it('Should not search under 3 characters', async () => {
-        wrapper.find('#input-search').at(0).simulate('change', { target: {value: 'se' } });
-        expect(wrapper.find('SearchNoResultsContent').find('p').text()).toContain("Please enter a search term that is longer than 3 characters.");
-    });
+    it( "Should not search under 3 characters", () => {
+        wrapper.find( "#input-search" ).at( 0 ).simulate( "change", { target: { value: "se" } } );
+        expect( wrapper.find( "SearchNoResultsContent" ).find( "p" ).text() ).toContain( "Please enter a search term that is longer than 3 characters." );
+    } );
 
-    it('Results not found', async () => {
-        wrapper.find('#input-search').at(0).simulate('change', { target: {value: 'random' } });
-        expect(wrapper.find('SearchNoResultsContent').find('p').text()).toContain("We couldn’t find anything with that term.");
-    });
+    it( "Results not found", () => {
+        wrapper.find( "#input-search" ).at( 0 ).simulate( "change", { target: { value: "random" } } );
+        expect( wrapper.find( "SearchNoResultsContent" ).find( "p" ).text() ).toContain( "We couldn’t find anything with that term." );
+    } );
 
-    it('should match search snapshot', () => {
-        expect(wrapper.debug()).toMatchSnapshot();
-    });
-
-});
+    it( "should match search snapshot", () => {
+        expect( wrapper.debug() ).toMatchSnapshot();
+    } );
+} );
