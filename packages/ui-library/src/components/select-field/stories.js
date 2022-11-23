1c313117e2a7a65c17afe5490765a9fd6290f466
Vraja Das <vraja.pro@gmail.com>
2022-11-23 16:02:52 +0000

    docs: added stories to Select an SelectFields
    Regarding the use of option props and children props.

--- packages/ui-library/src/components/select-field/stories.js
+++ packages/ui-library/src/components/select-field/stories.js
@@ -17,14 +17,25 @@ export default {
 	},
 };
 
+const options = [
+	{ value: "1", label: "Option 1" },
+	{ value: "2", label: "Option 2" },
+	{ value: "3", label: "Option 3" },
+	{ value: "4", label: "Option 4" }];
+
 const Template = ( args ) => {
-	const [ value, setValues ] = useState( args.value || "" );
-	const handleChange = useCallback( setValues, [ setValues ] );
+	const [ value, setValue ] = useState( args.value || "" );
+	const [ selectedLabel, setSelectedLabel ] = useState(value ? options.find(option=> option.value === value).label : "");
+	const handleChange = useCallback( (val)=>{
+		const selected = options.find(option=> option.value === val);
+		setSelectedLabel( selected.label );
+		setValue( val )}, [ setValue ] );
 
 	return (
 		// Min height to make room for options dropdown.
 		<div style={ { minHeight: 200 } }>
-			<SelectField { ...args } value={ value } onChange={ handleChange } />
+			<SelectField { ...args } value={ value } onChange={ handleChange } selectedLabel={selectedLabel} />
+			
 		</div>
 	);
 };
@@ -34,15 +45,10 @@ Factory.parameters = {
 	controls: { disable: false },
 };
 Factory.args = {
-	id: "select-field",
-	name: "name",
+	id: "select-field-0",
+	name: "name-0",
 	value: "1",
-	options: [
-		{ value: "1", label: "Option 1" },
-		{ value: "2", label: "Option 2" },
-		{ value: "3", label: "Option 3" },
-		{ value: "4", label: "Option 4" },
-	],
+	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
 	label: "A Select Field",
 };
 
@@ -50,14 +56,10 @@ export const WithLabelAndDescription = Template.bind( {} );
 WithLabelAndDescription.args = {
 	id: "select-field-1",
 	name: "name-1",
+	value: "3",
 	label: "Select field with a label",
 	description: "Select field with a description.",
-	options: [
-		{ value: "1", label: "Option 1" },
-		{ value: "2", label: "Option 2" },
-		{ value: "3", label: "Option 3" },
-		{ value: "4", label: "Option 4" },
-	],
+	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
 };
 
 export const WithError = Template.bind( {} );
@@ -67,12 +69,7 @@ WithError.args = {
 	value: "2",
 	label: "Select field with a label",
 	error: "Select field with an error.",
-	options: [
-		{ value: "1", label: "Option 1" },
-		{ value: "2", label: "Option 2" },
-		{ value: "3", label: "Option 3" },
-		{ value: "4", label: "Option 4" },
-	],
+	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
 };
 
 
@@ -83,6 +80,15 @@ WithLabelSuffix.args = {
 	value: "3",
 	label: "Select field with a label suffix",
 	labelSuffix: <Badge className="yst-ml-1.5" size="small">Beta</Badge>,
+	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
+};
+
+export const OptionsProp = Template.bind( {} );
+OptionsProp.args = {
+	id: "select-field-4",
+	name: "name-4",
+	value: "3",
+	label: "Select field with a options as array",
 	options: [
 		{ value: "1", label: "Option 1" },
 		{ value: "2", label: "Option 2" },
@@ -91,3 +97,19 @@ WithLabelSuffix.args = {
 	],
 };
 
+OptionsProp.parameters = {
+	docs: { description: { story: "Add options as an array of objects with `options` prop. Each object must contain `value` and `label` parameters. The displayed selected label will be updated automaticlly on change." } },
+}
+
+export const ChildrenProp = Template.bind( {} );
+ChildrenProp.args = {
+	id: "select-field-5",
+	name: "name-5",
+	value: "3",
+	label: "Select field with options as exposed React components",
+	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
+};
+
+ChildrenProp.parameters = {
+	docs: { description: { story: "Add options as an array of React components with `children` prop, using the exposed option component `SelectField.Option`. In this case changing the `selectedLabel` should be done manually in the handleChange function" } },
+}
