6b0a083c163df63035578aa6201182a8756088e6
nolledgeable <nolle@yoast.com>
2022-11-23 17:39:13 +0100

    Rename useBrowser hook to useUserAgentParser hook

--- packages/js/src/settings/hooks/use-user-agent-parser.js
+++ packages/js/src/settings/hooks/use-user-agent-parser.js
@@ -2,12 +2,12 @@ import { useMemo } from "@wordpress/element";
 import Bowser from "bowser";
 
 /**
- * @returns {Object} The browser object.
+ * @returns {Object} The Bowser parsed result object.
  * @see https://lancedikson.github.io/bowser/docs/global.html#ParsedResult
  */
-const useBrowser = () => {
+const useUserAgentParser = () => {
 	const browser = useMemo( () => Bowser.parse( window?.navigator?.userAgent ), [] );
 	return browser;
 };
 
-export default useBrowser;
+export default useUserAgentParser;
