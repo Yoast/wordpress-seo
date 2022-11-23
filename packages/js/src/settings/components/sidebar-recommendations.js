1fe6f4859dc739e4fa0977d9621b66abe89f3835
nolledgeable <nolle@yoast.com>
2022-11-23 12:15:04 +0100

    Better fluid font sizes for settings UI

--- packages/js/src/settings/components/sidebar-recommendations.js
+++ packages/js/src/settings/components/sidebar-recommendations.js
@@ -94,7 +94,7 @@ const SidebarRecommendations = () => {
 	), [] );
 
 	return (
-		<div className="xl:yst-max-w-3xl xl:yst-fixed xl:yst-right-8 xl:yst-w-[16rem]">
+		<div className="xl:yst-max-w-3xl xl:yst-fixed yst-z-10 xl:yst-right-8 xl:yst-w-[16rem]">
 			<div className="yst-grid yst-grid-cols-1 sm:yst-grid-cols-2 min-[783px]:yst-grid-cols-1 lg:yst-grid-cols-2 xl:yst-grid-cols-1 yst-gap-4">
 				<PremiumUpsellCard />
 				<div className="yst-p-6 yst-space-y-3 yst-rounded-lg yst-bg-white yst-shadow">
