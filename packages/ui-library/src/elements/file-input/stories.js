e8683f535f2bd3ee349084c17452e4f8bda39f35
Vraja Das <vraja.pro@gmail.com>
2022-11-25 14:10:02 +0000

    docs: add stories for description, disable and icon

--- packages/ui-library/src/elements/file-input/stories.js
+++ packages/ui-library/src/elements/file-input/stories.js
@@ -1,5 +1,6 @@
 import { noop } from "lodash";
 import FileInput from ".";
+import { DesktopComputerIcon } from "@heroicons/react/outline";
 
 export default {
 	title: "1. Elements/File Input",
@@ -14,20 +15,65 @@ export default {
 	},
 };
 
-export const Factory = ( args ) => (
-	<FileInput { ...args } />
-);
+const Template = ( args ) => <FileInput
+	value=""
+	selectLabel="Select label"
+	dropLabel="or drag and drop label"
+	screenReaderLabel="Select a file"
+	onChange={ noop }
+	{ ...args }
+/>;
+
+export const Factory = Template.bind( {} );
+
 Factory.parameters = {
 	controls: { disable: false },
 };
 Factory.args = {
-	id: "file-input",
-	name: "name",
-	value: "",
-	selectLabel: "Select label",
-	dropLabel: "drag and drop label",
-	screenReaderLabel: "Select a file",
+	id: "factory",
+	name: "factory",
+	isDisabled: false,
+};
+
+export const WithDescription = Template.bind( {} );
+
+WithDescription.parameters = {
+	controls: { disable: false },
+	docs: { description: { story: "A file input with a description using `selectDescription` prop." } },
+};
+
+WithDescription.args = {
+	id: "with-description",
+	name: "with-description",
 	selectDescription: "File input description",
-	disabled: false,
-	onChange: noop,
+	isDisabled: false,
+};
+
+export const Disabled = Template.bind( {} );
+
+Disabled.parameters = {
+	controls: { disable: true },
+	docs: { description: { story: "Disabled state using `isDisabled` prop." } },
+};
+
+Disabled.args = {
+	id: "file-input-disabled",
+	name: "disabled",
+	selectDescription: "File input description",
+	isDisabled: true,
+};
+
+export const DifferentIcon = Template.bind( {} );
+
+DifferentIcon.parameters = {
+	controls: { disable: false },
+	docs: { description: { story: "A file input with different icon using `iconAs` prop. The icon should be a react component." } },
+};
+
+DifferentIcon.args = {
+	id: "icon-as",
+	name: "icon-as",
+	selectDescription: "File input description",
+	isDisabled: false,
+	iconAs: DesktopComputerIcon,
 };
