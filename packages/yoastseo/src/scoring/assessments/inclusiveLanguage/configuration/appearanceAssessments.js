98d5cd404a94ab017b259709ec9ea194125d70f4
iolandasequino <iolanda@yoast.com>
2022-11-28 15:07:06 +0100

    edit the configuration and feedback of two appearance assessments to improve readbility and actionability

--- packages/yoastseo/src/scoring/assessments/inclusiveLanguage/configuration/appearanceAssessments.js
+++ packages/yoastseo/src/scoring/assessments/inclusiveLanguage/configuration/appearanceAssessments.js
@@ -1,7 +1,16 @@
-import { potentiallyHarmful, potentiallyHarmfulUnless, preferredDescriptorIfKnown } from "./feedbackStrings";
+import {
+	potentiallyHarmful,
+	potentiallyHarmfulUnless,
+	potentiallyHarmfulUnlessReclaimed,
+	preferredDescriptorIfKnown,
+} from "./feedbackStrings";
 import { SCORES } from "./scores";
 
 const learnMoreUrl = "https://yoa.st/inclusive-language-appearance";
+
+// This string says "Alternatively, if talking about a specific person, use their preferred descriptor if known, for example <i>plus-size</i>.
+const preferredDescriptorIfKnownPlusSize = [ preferredDescriptorIfKnown( 0, -1 ) + ", for example <i>plus-size</i>" ];
+
 
 const appearanceAssessments = [
 	{
@@ -15,19 +24,28 @@ const appearanceAssessments = [
 	{
 		identifier: "obese",
 		nonInclusivePhrases: [ "obese", "overweight" ],
-		inclusiveAlternatives: "<i>has/have a higher weight, " +
-			"higher-weight person/people, person/people in higher weight body/bodies, heavier person/people</i>",
+		inclusiveAlternatives: "<i>has a higher weight, " +
+			"higher-weight person, person in higher weight body, heavier person</i>",
 		score: SCORES.POTENTIALLY_NON_INCLUSIVE,
-		feedbackFormat: [ potentiallyHarmful, preferredDescriptorIfKnown ].join( " " ),
+		feedbackFormat: [ potentiallyHarmful, preferredDescriptorIfKnownPlusSize ].join( " " ),
 		learnMoreUrl: learnMoreUrl,
 	},
 	{
-		identifier: "obesity",
-		nonInclusivePhrases: [ "person with obesity", "people with obesity", "fat person", "fat people" ],
-		inclusiveAlternatives: "<i>person/people who has/have a higher weight, " +
-			"higher-weight person/people, person/people in higher weight body/bodies, heavier person/people</i>",
+		identifier: "obesitySingular",
+		nonInclusivePhrases: [ "person with obesity", "fat person" ],
+		inclusiveAlternatives: "<i>person who has a higher weight, " +
+			"higher-weight person, person in higher weight body, heavier person</i>",
 		score: SCORES.POTENTIALLY_NON_INCLUSIVE,
-		feedbackFormat: [ potentiallyHarmful, preferredDescriptorIfKnown ].join( " " ),
+		feedbackFormat: [ potentiallyHarmfulUnless, preferredDescriptorIfKnownPlusSize ].join( " " ),
+		learnMoreUrl: learnMoreUrl,
+	},
+	{
+		identifier: "obesityPlural",
+		nonInclusivePhrases: [ "people with obesity", "fat people" ],
+		inclusiveAlternatives: "<i>people who have a higher weight, " +
+			"higher-weight people, people in higher weight bodies, heavier people</i>",
+		score: SCORES.POTENTIALLY_NON_INCLUSIVE,
+		feedbackFormat: [ potentiallyHarmfulUnless ].join( " " ),
 		learnMoreUrl: learnMoreUrl,
 	},
 	{
