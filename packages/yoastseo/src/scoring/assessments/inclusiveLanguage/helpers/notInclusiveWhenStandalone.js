f4cf6826fcfeb23c104562b37b1496af7a74627e
hdvos <hdvos93@gmail.com>
2022-11-28 16:08:23 +0100

    add jsdoc.

--- packages/yoastseo/src/scoring/assessments/inclusiveLanguage/helpers/notInclusiveWhenStandalone.js
+++ packages/yoastseo/src/scoring/assessments/inclusiveLanguage/helpers/notInclusiveWhenStandalone.js
@@ -5,10 +5,12 @@ import { punctuationList } from "../../../../languageProcessing/helpers/sanitize
 
 
 /**
- * A
- * @param {string[]} words A
- * @param {string[]} nonInclusivePhrase A
- * @returns {function} A
+ * Returns a callback that checks whether a non inclusive word is standalone:
+ * "The undocumented are there". is not inclusive, "The undocumented" is standalone.
+ * "The undocumented people are there." is inclusive, in "The undocumented people", "the undocumented" is not standalon.
+ * @param {string[]} words A list of words that is being queried.
+ * @param {string[]} nonInclusivePhrase A list of words that are the non inclusive phrase.
+ * @returns {function} A callback that checks whether a non inclusive term is standalon.
  */
 export default function notInclusiveWhenStandalone( words, nonInclusivePhrase ) {
 	return ( index ) => {
