cc3179c6ef0f345a8dd31ec61ad5d8f71e5516e2
Agnieszka Szuba <agnieszka@yoast.com>
2022-11-29 16:38:25 +0100

    Remove spaces from Japanese text before counting characters

--- packages/yoastseo/src/languageProcessing/languages/ja/helpers/countCharacters.js
+++ packages/yoastseo/src/languageProcessing/languages/ja/helpers/countCharacters.js
@@ -3,7 +3,9 @@ const { sanitizeString } = languageProcessing;
 import removeURLs from "../../../helpers/sanitize/removeURLs.js";
 
 /**
- * Calculates the character count of a text, including punctuation and numbers. Is used to determine length of text.
+ * Calculates the character count which serves as a measure of text length.
+ * The character count includes letters, punctuation, and numbers. It doesn't include URLs, HTML tags, spaces, and the
+ * content of the Table of Contents and Estimated Reading Time blocks.
  *
  * @param {string} text The text to be counted.
  *
@@ -12,6 +14,7 @@ import removeURLs from "../../../helpers/sanitize/removeURLs.js";
 export default function( text ) {
 	text = removeURLs( text );
 	text = sanitizeString( text );
+	text = text.replace( /\s/g, "" );
 
 	return text.length;
 }
