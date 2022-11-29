01aabcee314fe78031743599d88d00c70a26841f
Vraja Das <vraja.pro@gmail.com>
2022-11-29 16:10:45 +0000

    fix: renaming terms in docs.

--- packages/ui-library/src/components/notifications/stories.js
+++ packages/ui-library/src/components/notifications/stories.js
@@ -30,7 +30,7 @@ export default {
 	parameters: {
 		docs: {
 			description: {
-				component: "The Notifications component shows notifications on a specified position on the screen. Switch location in the control panel.",
+				component: "The Notifications component shows notifications on a specified position on the screen. Switch `position` in the control panel to view.",
 			},
 		},
 	},
