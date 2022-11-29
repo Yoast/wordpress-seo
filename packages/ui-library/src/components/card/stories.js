4e6b3b4c81f9577b5593e40d11cee78ada8f1456
Vraja Das <vraja.pro@gmail.com>
2022-11-29 09:18:20 +0000

    docs: add story for car component

---
+++ packages/ui-library/src/components/card/stories.js
@@ -0,0 +1,42 @@
+import { Card } from "../../index";
+
+export default {
+	title: "2. Components/Card",
+	component: Card,
+	parameters: {
+		docs: {
+			description: {
+				component: "A card component.",
+			},
+		},
+	},
+	argTypes: {
+		children: { control: false },
+	},
+	args: {
+		children: ( <><Card.Header>This is Card header!</Card.Header>
+			<Card.Content>This is Card content!</Card.Content>
+			<Card.Footer>This is Card footer!</Card.Footer></> ),
+		className: "yst-w-1/3",
+	},
+
+};
+
+const Template = ( { className, children } ) => {
+	return (
+		<div className="yst-flex yst-gap-5 yst-justify-center">
+			<Card className={ className }>
+				{ children }
+			</Card>
+		</div>	);
+};
+
+export const Factory = Template.bind( {} );
+
+Factory.parameters = {};
+
+Factory.args = {
+	children: ( <><Card.Header>This is Card header!</Card.Header>
+		<Card.Content>This is Card content!</Card.Content>
+		<Card.Footer>This is Card footer!</Card.Footer></> ),
+};
