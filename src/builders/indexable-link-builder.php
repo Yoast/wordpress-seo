d0e91d3c3e4036b053d500ab7c966e58ffa9774f
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-12-02 15:34:25 +0200

    Keep building image links the old way if we don't disable attachments

--- src/builders/indexable-link-builder.php
+++ src/builders/indexable-link-builder.php
@@ -4,6 +4,7 @@ namespace Yoast\WP\SEO\Builders;
 
 use WPSEO_Image_Utils;
 use Yoast\WP\SEO\Helpers\Image_Helper;
+use Yoast\WP\SEO\Helpers\Options_Helper;
 use Yoast\WP\SEO\Helpers\Post_Helper;
 use Yoast\WP\SEO\Helpers\Url_Helper;
 use Yoast\WP\SEO\Models\Indexable;
@@ -45,6 +46,13 @@ class Indexable_Link_Builder {
 	protected $post_helper;
 
 	/**
+	 * The options helper.
+	 *
+	 * @var Options_Helper
+	 */
+	protected $options_helper;
+
+	/**
 	 * The indexable repository.
 	 *
 	 * @var Indexable_Repository
@@ -57,15 +65,18 @@ class Indexable_Link_Builder {
 	 * @param SEO_Links_Repository $seo_links_repository The SEO links repository.
 	 * @param Url_Helper           $url_helper           The URL helper.
 	 * @param Post_Helper          $post_helper          The post helper.
+	 * @param Options_Helper       $options_helper       The options helper.
 	 */
 	public function __construct(
 		SEO_Links_Repository $seo_links_repository,
 		Url_Helper $url_helper,
-		Post_Helper $post_helper
+		Post_Helper $post_helper,
+		Options_Helper $options_helper
 	) {
 		$this->seo_links_repository = $seo_links_repository;
 		$this->url_helper           = $url_helper;
 		$this->post_helper          = $post_helper;
+		$this->options_helper       = $options_helper;
 	}
 
 	/**
@@ -261,67 +272,77 @@ class Indexable_Link_Builder {
 
 		$model->parsed_url = $parsed_url;
 
-		// For internal links, we store the target indexable ids as well.
 		if ( $model->type === SEO_Links::TYPE_INTERNAL ) {
-			$permalink = $this->get_permalink( $url, $home_url );
-			if ( $this->url_helper->is_relative( $permalink ) ) {
-				// Make sure we're checking against the absolute URL, and add a trailing slash if the site has a trailing slash in its permalink settings.
-				$permalink = $this->url_helper->ensure_absolute_url( \user_trailingslashit( $permalink ) );
-			}
-			$target = $this->indexable_repository->find_by_permalink( $permalink );
+			$permalink = $this->build_permalink( $url, $home_url );
 
-			if ( ! $target ) {
-				// If target indexable cannot be found, create one based on the post's post ID.
-				$post_id = \url_to_postid( $permalink );
-				if ( $post_id && $post_id !== 0 ) {
-					$target = $this->indexable_repository->find_by_id_and_type( $post_id, 'post' );
-				}
-			}
-
-			if ( ! $target ) {
-				return $model;
-			}
-
-			$model->target_indexable_id = $target->id;
-			if ( $target->object_type === 'post' ) {
-				$model->target_post_id = $target->object_id;
-			}
+			return $this->enhance_link_from_indexable( $model, $permalink );
 		}
 
-		// For internal images, we store just the ids, as attachments dont have indexables.
 		if ( $model->type === SEO_Links::TYPE_INTERNAL_IMAGE ) {
-			$permalink = $this->get_permalink( $url, $home_url );
-			if ( $this->url_helper->is_relative( $permalink ) ) {
-				// Make sure we're checking against the absolute URL, and add a trailing slash if the site has a trailing slash in its permalink settings.
-				$permalink = $this->url_helper->ensure_absolute_url( \user_trailingslashit( $permalink ) );
-			}
+			$permalink = $this->build_permalink( $url, $home_url );
 
-			$target_post_id = WPSEO_Image_Utils::get_attachment_by_url( $permalink );
-
-			if ( ! empty( $target_post_id ) ) {
-				$model->target_post_id = $target_post_id;
-			}
-		}
-
-		if ( $is_image && $model->target_post_id ) {
-			$file = \get_attached_file( $model->target_post_id );
-			if ( $file ) {
-				list( , $width, $height ) = \wp_get_attachment_image_src( $model->target_post_id, 'full' );
-
-				$model->width  = $width;
-				$model->height = $height;
-				if ( \file_exists( $file ) ) {
-					$model->size = \filesize( $file );
-				}
-				else {
-					$model->size = null;
-				}
+			if ( ! $this->options_helper->get( 'disable-attachment' ) ) {
+				$model = $this->enhance_link_from_indexable( $model, $permalink );
 			}
 			else {
-				$model->width  = 0;
-				$model->height = 0;
-				$model->size   = 0;
+				$target_post_id = WPSEO_Image_Utils::get_attachment_by_url( $permalink );
+
+				if ( ! empty( $target_post_id ) ) {
+					$model->target_post_id = $target_post_id;
+				}
 			}
+
+			if ( $model->target_post_id ) {
+				$file = \get_attached_file( $model->target_post_id );
+				if ( $file ) {
+					list( , $width, $height ) = \wp_get_attachment_image_src( $model->target_post_id, 'full' );
+
+					$model->width  = $width;
+					$model->height = $height;
+					if ( \file_exists( $file ) ) {
+						$model->size = \filesize( $file );
+					}
+					else {
+						$model->size = null;
+					}
+				}
+				else {
+					$model->width  = 0;
+					$model->height = 0;
+					$model->size   = 0;
+				}
+			}
+		}
+
+		return $model;
+	}
+
+	/**
+	 * Enhances the link model with information from its indexable.
+	 *
+	 * @param SEO_Links $model     The link's model.
+	 * @param string    $permalink The link's permalink.
+	 *
+	 * @return SEO_Links The enhanced link model.
+	 */
+	protected function enhance_link_from_indexable( $model, $permalink ) {
+		$target = $this->indexable_repository->find_by_permalink( $permalink );
+
+		if ( ! $target ) {
+			// If target indexable cannot be found, create one based on the post's post ID.
+			$post_id = \url_to_postid( $permalink );
+			if ( $post_id && $post_id !== 0 ) {
+				$target = $this->indexable_repository->find_by_id_and_type( $post_id, 'post' );
+			}
+		}
+
+		if ( ! $target ) {
+			return $model;
+		}
+
+		$model->target_indexable_id = $target->id;
+		if ( $target->object_type === 'post' ) {
+			$model->target_post_id = $target->object_id;
 		}
 
 		if ( $model->target_indexable_id ) {
@@ -330,6 +351,25 @@ class Indexable_Link_Builder {
 		}
 
 		return $model;
+	}
+
+	/**
+	 * Builds the link's permalink.
+	 *
+	 * @param string $url      The url of the link.
+	 * @param array  $home_url The home url, as parsed by wp_parse_url.
+	 *
+	 * @return string The link's permalink.
+	 */
+	protected function build_permalink( $url, $home_url ) {
+		$permalink = $this->get_permalink( $url, $home_url );
+
+		if ( $this->url_helper->is_relative( $permalink ) ) {
+			// Make sure we're checking against the absolute URL, and add a trailing slash if the site has a trailing slash in its permalink settings.
+			$permalink = $this->url_helper->ensure_absolute_url( \user_trailingslashit( $permalink ) );
+		}
+
+		return $permalink;
 	}
 
 	/**
