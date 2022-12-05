bd1346a1149a88e9812f7cb1349585f877760002
Vraja Das <vraja.pro@gmail.com>
2022-12-05 11:44:38 +0100

    doc: improved variant for isLabelDangerousHtml prop

--- packages/ui-library/src/elements/radio/stories.js
+++ packages/ui-library/src/elements/radio/stories.js
@@ -23,23 +23,25 @@ Factory.args = {
 	id: "radio",
 	name: "name",
 	value: "value",
-	label: "I am a radio button.",
+	label: "&bull; I am a radio button.",
+	isLabelDangerousHtml: true,
 };
 
 export const Variants = ( args ) => (
 	<div className="yst-flex yst-flex-col yst-gap-4">
 		<Radio id="radio-1" name="name" value="1" label="I am a radio button." />
 		<hr />
-		<Radio id="radio-2" name="name" value="2" screenReaderLabel="Option #2" label="2" variant="inline-block" />
+		<Radio id="radio-a" name="name" value="A" screenReaderLabel="Option #A" label="A" variant="inline-block" />
+		<Radio id="radio-b" name="name" value="B" screenReaderLabel="Option #B" label="B" variant="inline-block" />
 		<hr />
 		<Radio
 			id="radio-3"
 			name="name"
 			value="&bull;"
 			screenReaderLabel="Option bullet"
-			label={ "&bull;" }
+			label={ "&bull; This Radio has `isLabelDangerousHtml` prop set to true, the bullet is encoded." }
 			isLabelDangerousHtml={ true }
-			variant="inline-block"
+			// variant="inline-block"
 		/>
 	</div>
 );
