ab8328a682b16da961d3c5626f7fe9c9197a85bd
Igor <35524806+igorschoester@users.noreply.github.com>
2022-12-05 16:39:35 +0100

    Fix code style

--- packages/ui-library/src/components/select-field/stories.js
+++ packages/ui-library/src/components/select-field/stories.js
@@ -21,21 +21,23 @@ const options = [
 	{ value: "1", label: "Option 1" },
 	{ value: "2", label: "Option 2" },
 	{ value: "3", label: "Option 3" },
-	{ value: "4", label: "Option 4" }];
+	{ value: "4", label: "Option 4" },
+];
 
 const Template = ( args ) => {
 	const [ value, setValue ] = useState( args.value || "" );
-	const [ selectedLabel, setSelectedLabel ] = useState(value ? options.find(option=> option.value === value).label : "");
-	const handleChange = useCallback( (val)=>{
-		const selected = options.find(option=> option.value === val);
+	const [ selectedLabel, setSelectedLabel ] = useState( value ? options.find( option => option.value === value ).label : "" );
+	const handleChange = useCallback( ( val ) => {
+		const selected = options.find( option => option.value === val );
 		setSelectedLabel( selected.label );
-		setValue( val )}, [ setValue ] );
+		setValue( val );
+	}, [ setValue ] );
 
 	return (
 		// Min height to make room for options dropdown.
 		<div style={ { minHeight: 200 } }>
-			<SelectField { ...args } value={ value } onChange={ handleChange } selectedLabel={selectedLabel} />
-			
+			<SelectField { ...args } value={ value } onChange={ handleChange } selectedLabel={ selectedLabel } />
+
 		</div>
 	);
 };
@@ -48,7 +50,7 @@ Factory.args = {
 	id: "select-field-0",
 	name: "name-0",
 	value: "1",
-	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
+	children: options.map( option => <SelectField.Option key={ option.value } { ...option } /> ),
 	label: "A Select Field",
 };
 
@@ -59,7 +61,7 @@ WithLabelAndDescription.args = {
 	value: "3",
 	label: "Select field with a label",
 	description: "Select field with a description.",
-	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
+	children: options.map( option => <SelectField.Option key={ option.value } { ...option } /> ),
 };
 
 export const WithError = Template.bind( {} );
@@ -69,7 +71,7 @@ WithError.args = {
 	value: "2",
 	label: "Select field with a label",
 	error: "Select field with an error.",
-	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
+	children: options.map( option => <SelectField.Option key={ option.value } { ...option } /> ),
 };
 
 
@@ -80,7 +82,7 @@ WithLabelSuffix.args = {
 	value: "3",
 	label: "Select field with a label suffix",
 	labelSuffix: <Badge className="yst-ml-1.5" size="small">Beta</Badge>,
-	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
+	children: options.map( option => <SelectField.Option key={ option.value } { ...option } /> ),
 };
 
 export const OptionsProp = Template.bind( {} );
@@ -98,8 +100,8 @@ OptionsProp.args = {
 };
 
 OptionsProp.parameters = {
-	docs: { description: { story: "Add options as an array of objects with `options` prop. Each object must contain `value` and `label` parameters. The displayed selected label will be updated automaticlly on change." } },
-}
+	docs: { description: { story: "Add options as an array of objects with `options` prop. Each object must contain `value` and `label` parameters. The displayed selected label will be updated automatically on change." } },
+};
 
 export const ChildrenProp = Template.bind( {} );
 ChildrenProp.args = {
@@ -107,9 +109,9 @@ ChildrenProp.args = {
 	name: "name-5",
 	value: "3",
 	label: "Select field with options as exposed React components",
-	children: options.map(option=><SelectField.Option key={option.value} {...option}/>),
+	children: options.map( option => <SelectField.Option key={ option.value } { ...option } /> ),
 };
 
 ChildrenProp.parameters = {
 	docs: { description: { story: "Add options as an array of React components with `children` prop, using the exposed option component `SelectField.Option`. In this case changing the `selectedLabel` should be done manually in the handleChange function" } },
-}
+};
