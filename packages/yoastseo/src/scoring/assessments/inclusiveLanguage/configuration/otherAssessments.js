16cbd8f014ccd0cb2517aea06a9748084525b0de
Hanna Worku <hanna@yoast.com>
2022-12-07 13:09:56 +0100

    add phrases with normal and abnormal to non-inclusive phrases

--- packages/yoastseo/src/scoring/assessments/inclusiveLanguage/configuration/otherAssessments.js
+++ packages/yoastseo/src/scoring/assessments/inclusiveLanguage/configuration/otherAssessments.js
@@ -2,6 +2,7 @@ import { SCORES } from "./scores";
 import { potentiallyHarmful, potentiallyHarmfulCareful } from "./feedbackStrings";
 import { includesConsecutiveWords } from "../helpers/includesConsecutiveWords";
 import notInclusiveWhenStandalone from "../helpers/notInclusiveWhenStandalone";
+import { isNotFollowedByException } from "../helpers/isFollowedByException";
 
 const otherAssessments = [
 	{
@@ -19,6 +20,24 @@ const otherAssessments = [
 		score: SCORES.POTENTIALLY_NON_INCLUSIVE,
 		feedbackFormat: "Be careful when using <i>%1$s</i> as it is potentially overgeneralizing. " +
 						"Consider using an alternative, such as %2$s, %3$s or specific minorities, such as %4$s.",
+	},
+	{
+		identifier: "normal",
+		nonInclusivePhrases: [ "normal person", "normal people", "normal behaviour", "normal behavior", "mentally normal" ] +
+			[ "behaviorally normal", "psychologically normal"  ],
+		inclusiveAlternatives: [ "<i>typical</i>" ],
+		score: SCORES.NON_INCLUSIVE,
+		feedbackFormat: "Be careful when using <i>normal</i> as it is potentially harmful. " +
+			"Consider using an alternative, such as %2$sm or a specific characteristic or experience if it is known.",
+	},
+	{
+		identifier: "abnormal",
+		nonInclusivePhrases: [ "abnormal person", "abnormal people", "abnormal behaviour", "abnormal behavior", "mentally abnormal" ] +
+			[ "behaviorally abnormal", "psychologically abnormal"  ],
+		inclusiveAlternatives: [ "<i>atypical</i>" ],
+		score: SCORES.NON_INCLUSIVE,
+		feedbackFormat: "Be careful when using <i>abnormal</i> as it is potentially harmful. " +
+			"Consider using an alternative, such as %2$sm or a specific characteristic or experience if it is known.",
 	},
 	{
 		identifier: "theMinority",
