c3cd8ea8a63a6d603ad141e29f9fc6c7bc7ec0b5
FAMarfuaty <aida@yoast.com>
2022-12-08 12:04:45 +0100

    add function description

--- packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/testHelpers/testHelpers.js
+++ packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/testHelpers/testHelpers.js
@@ -4,6 +4,15 @@ import InclusiveLanguageAssessment
 import Paper from "../../../../../src/values/Paper";
 import Factory from "../../../../specHelpers/factory";
 
+/**
+ * Tests multiple identifiers of one category. For example a singular noun and its plural forms.
+ *
+ * @param {Array} texts    The array of texts to check for each entry.
+ * @param {Array} identifiers The array of identifiers.
+ * @param {Array} feedbacks The array of the feedbacks for each non-inclusive phrase.
+ * @param {Number} score    The expected assessment score for this category.
+ * @returns {void}
+ */
 export function testMultipleForms( texts, identifiers, feedbacks, score ) {
 	identifiers.forEach( ( identifier, i ) => {
 		const assessment = new InclusiveLanguageAssessment( assessments.find( obj => obj.identifier === identifier ) );
