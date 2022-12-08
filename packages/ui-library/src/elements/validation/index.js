c19390d2376c026b01a4c4c0192b968f72b40b00
nolledgeable <nolle@yoast.com>
2022-12-08 23:07:51 +0100

    Add validation components

--- packages/ui-library/src/elements/validation/index.js
+++ packages/ui-library/src/elements/validation/index.js
@@ -1,2 +1,10 @@
+import PropTypes from "prop-types";
+
 export { default as ValidationIcon } from "./validation-icon";
+export { default as ValidationInput } from "./validation-input";
 export { default as ValidationMessage } from "./validation-message";
+
+export const validationPropType = PropTypes.shape( {
+	variant: PropTypes.string,
+	message: PropTypes.node,
+} );
