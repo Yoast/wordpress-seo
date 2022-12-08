72689d230752896db7c430f703f8c61c124f14ee
FAMarfuaty <aida@yoast.com>
2022-12-08 13:15:03 +0100

    add extra parameter

--- packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/testHelpers/testHelpers.js
+++ packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/testHelpers/testHelpers.js
@@ -1,4 +1,3 @@
-import assessments from "../../../../../src/scoring/assessments/inclusiveLanguage/configuration/cultureAssessments";
 import InclusiveLanguageAssessment
 	from "../../../../../src/scoring/assessments/inclusiveLanguage/InclusiveLanguageAssessment";
 import Paper from "../../../../../src/values/Paper";
@@ -7,13 +6,14 @@ import Factory from "../../../../specHelpers/factory";
 /**
  * Tests multiple identifiers of one category. For example a singular noun and its plural forms.
  *
+ * @param {Array} assessments The array of assessments to use.
  * @param {Array} texts    The array of texts to check for each entry.
  * @param {Array} identifiers The array of identifiers.
  * @param {Array} feedbacks The array of the feedbacks for each non-inclusive phrase.
  * @param {Number} score    The expected assessment score for this category.
  * @returns {void}
  */
-export function testMultipleForms( texts, identifiers, feedbacks, score ) {
+export function testMultipleForms( assessments, texts, identifiers, feedbacks, score ) {
 	identifiers.forEach( ( identifier, i ) => {
 		const assessment = new InclusiveLanguageAssessment( assessments.find( obj => obj.identifier === identifier ) );
 		const mockPaper = new Paper( texts[ i ] );
