c5e5bacded708cfb88d9b3cef60cec848e551627
nolledgeable <nolle@yoast.com>
2022-12-08 23:13:35 +0100

    Add forwardRef support to label element

--- packages/ui-library/src/elements/label/index.js
+++ packages/ui-library/src/elements/label/index.js
@@ -1,3 +1,4 @@
+import { forwardRef } from "@wordpress/element";
 import classNames from "classnames";
 import PropTypes from "prop-types";
 
@@ -8,20 +9,21 @@ import PropTypes from "prop-types";
  * @param {string} [className] CSS class.
  * @returns {JSX.Element} Label component.
  */
-const Label = ( {
+const Label = forwardRef( ( {
 	as: Component = "label",
 	className = "",
 	label = "",
 	children = "",
 	...props
-} ) => (
+}, ref ) => (
 	<Component
+		ref={ ref }
 		className={ classNames( "yst-label", className ) }
 		{ ...props }
 	>
 		{ label || children || null }
 	</Component>
-);
+) );
 
 Label.propTypes = {
 	label: PropTypes.string,
