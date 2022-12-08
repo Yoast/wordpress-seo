8d51a4d5509e0439241f531a6cdf7a1bdbbf33ec
nolledgeable <nolle@yoast.com>
2022-12-08 23:11:09 +0100

    Add validation variants to select & select field

--- packages/ui-library/src/elements/select/stories.js
+++ packages/ui-library/src/elements/select/stories.js
@@ -1,5 +1,7 @@
 import { useCallback, useState } from "@wordpress/element";
+import { noop, map } from "lodash";
 import Select from ".";
+import { VALIDATION_VARIANTS } from "../validation/constants";
 
 export default {
 	title: "1) Elements/Select",
@@ -54,14 +56,6 @@ Factory.args = {
 
 };
 
-export const States = Template.bind( {} );
-States.args = {
-	id: "select",
-	value: "1",
-	isError: true,
-	children: options.map( option => <Select.Option key={ option.value } { ...option } /> ),
-};
-
 export const OptionsProp = Template.bind( {} );
 OptionsProp.args = {
 	id: "select-field-4",
@@ -92,3 +86,33 @@ ChildrenProp.args = {
 ChildrenProp.parameters = {
 	docs: { description: { story: "Add options as an array of React components with `children` prop, using the exposed option component `Select.Option`. In this case changing the `selectedLabel` should be done manually in the handleChange function" } },
 };
+
+export const Validation = () => (
+	<div className="yst-space-y-8">
+		{ map( VALIDATION_VARIANTS, variant => (
+			<Select
+				key={ variant }
+				id={ `validation-${ variant }` }
+				name={ `validation-${ variant }` }
+				label={ `With validation of variant ${ variant }` }
+				value="The quick brown fox jumps over the lazy dog"
+				onChange={ noop }
+				options={ [
+					{ value: "1", label: "Option 1" },
+					{ value: "2", label: "Option 2" },
+					{ value: "3", label: "Option 3" },
+					{ value: "4", label: "Option 4" },
+				] }
+				validation={ {
+					variant,
+					message: {
+						success: "Looks like you are nailing it!",
+						warning: "Looks like you could do better!",
+						info: <>Looks like you could use some <a href="https://yoast.com" target="_blank" rel="noreferrer">more info</a>!</>,
+						error: "Looks like you are doing it wrong!",
+					}[ variant ],
+				} }
+			/>
+		) ) }
+	</div>
+);
