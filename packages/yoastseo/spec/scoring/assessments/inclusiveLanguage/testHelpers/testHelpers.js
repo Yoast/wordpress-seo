4ca021ade0b886d8be5bf109325896de3dfd6bca
FAMarfuaty <aida@yoast.com>
2022-12-09 17:31:03 +0100

    adapted the test helper

--- packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/testHelpers/testHelpers.js
+++ packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/testHelpers/testHelpers.js
@@ -2,25 +2,23 @@ import InclusiveLanguageAssessment
 	from "../../../../../src/scoring/assessments/inclusiveLanguage/InclusiveLanguageAssessment";
 import Paper from "../../../../../src/values/Paper";
 import Factory from "../../../../specHelpers/factory";
+import assessments from "../../../../../src/scoring/assessments/inclusiveLanguage/configuration";
 
 /**
  * Tests multiple identifiers of one category. For example a singular noun and its plural forms.
  *
- * @param {Array} assessments The array of assessments to use.
- * @param {Array} texts    The array of texts to check for each entry.
- * @param {Array} identifiers The array of identifiers.
- * @param {Array} feedbacks The array of the feedbacks for each non-inclusive phrase.
- * @param {Number} score    The expected assessment score for this category.
+ * @param {Array} testDataArray The array of test data to test.
+ *
  * @returns {void}
  */
-export function testMultipleForms( assessments, texts, identifiers, feedbacks, score ) {
-	identifiers.forEach( ( identifier, i ) => {
-		const assessment = new InclusiveLanguageAssessment( assessments.find( obj => obj.identifier === identifier ) );
-		const mockPaper = new Paper( texts[ i ] );
-		const mockResearcher = Factory.buildMockResearcher( [ texts[ i ] ] );
+export function testInclusiveLanguageAssessment( testDataArray ) {
+	testDataArray.forEach( ( testData ) => {
+		const assessment = new InclusiveLanguageAssessment( assessments.find( obj => obj.identifier === testData.identifier ) );
+		const mockPaper = new Paper( testData.text );
+		const mockResearcher = Factory.buildMockResearcher( [ testData.text ] );
 
 		expect( assessment.isApplicable( mockPaper, mockResearcher ) ).toBe( true );
-		expect( assessment.getResult().score ).toBe( score );
-		expect( assessment.getResult().text ).toBe( feedbacks[ i ] );
+		expect( assessment.getResult().score ).toBe( testData.expectedScore );
+		expect( assessment.getResult().text ).toBe( testData.expectedFeedback );
 	} );
 }
