d246b9dd7f9dfffdc31da1647ca49f38bb55e7a9
Agnieszka Szuba <agnieszka@yoast.com>
2022-12-12 10:57:58 +0100

    Add back imports after merge conflict

--- packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/configuration/otherAssessmentsSpec.js
+++ packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/configuration/otherAssessmentsSpec.js
@@ -2,6 +2,8 @@ import Paper from "../../../../../src/values/Paper";
 import EnglishResearcher from "../../../../../src/languageProcessing/languages/en/Researcher";
 import InclusiveLanguageAssessment from "../../../../../src/scoring/assessments/inclusiveLanguage/InclusiveLanguageAssessment";
 import assessments from "../../../../../src/scoring/assessments/inclusiveLanguage/configuration/otherAssessments";
+import Factory from "../../../../specHelpers/factory.js";
+import Mark from "../../../../../src/values/Mark";
 
 describe( "Other assessments", function() {
 	it( "should target potentially non-inclusive phrases",
