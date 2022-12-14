87d62ae181a90d170311364168cdb20b729e1ddd
Vraja Das <vraja.pro@gmail.com>
2022-12-14 10:17:58 +0100

    fix: Link forwardRef props support for storybook

--- packages/ui-library/src/elements/link/stories.js
+++ packages/ui-library/src/elements/link/stories.js
@@ -1,8 +1,8 @@
-import Link from ".";
+import { StoryComponent } from ".";
 
 export default {
 	title: "1) Elements/Link",
-	component: Link,
+	component: StoryComponent,
 	argTypes: {
 		children: { control: "text" },
 		as: { options: [ "a", "button" ] },
@@ -18,7 +18,7 @@ export default {
 };
 
 export const Factory = ( { children, ...args } ) => (
-	<Link { ...args }>{ children }</Link>
+	<StoryComponent { ...args }>{ children }</StoryComponent>
 );
 Factory.parameters = {
 	controls: { disable: false },
