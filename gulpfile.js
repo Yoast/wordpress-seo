be23856b8df5d2689e788990c586ae00d22fe409
Leonidas Milosis <leonidas.milossis@gmail.com>
2022-12-15 13:15:28 +0200

    Add composer tasks and more

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ const rename = require( "gulp-rename" );
 const sourcemaps = require( "gulp-sourcemaps" );
 const rtlcss = require( "gulp-rtlcss" );
 const fs = require( "fs" );
-const exec = require( "child_process" ).exec;
 const spawn = require('cross-spawn');
 const del = require( "del" );
 const paths = require( "./config/webpack/paths" );
@@ -57,7 +56,7 @@ files.forEach( function( task ) {
 /**
  * Clean up the old css files.
  */
-gulp.task( "clean:css", function() {
+gulp.task( "clean:build-assets-css", function() {
 	return del(
 		[ cssDestLive + "*.css*", "!" + cssDestLive + "monorepo*.css" ]
 	);
@@ -66,18 +65,20 @@ gulp.task( "clean:css", function() {
 /**
  * Clean up the old js files.
  */
-gulp.task( "clean:js", function() {
+gulp.task( "clean:build-assets-js", function() {
 	return del(
 		[ "js/dist", cssDestLive + "monorepo*.css" ]
 	);
 } );
+
+gulp.task( "clean:build-assets", gulp.parallel( "clean:build-assets-js", "clean:build-assets-css" ) );
 
 /**
  * Clean up the old css files.
  */
 gulp.task( "clean:vendor-prefixed", function() {
 	return del(
-		[ vendorPrefixed + "**", '!' + vendorPrefixed, "!" + vendorPrefixed + "*.gitignore" ]
+		[ vendorPrefixed + "**", "!" + vendorPrefixed, "!" + vendorPrefixed + "*.gitignore" ]
 	);
 } );
 
@@ -85,25 +86,46 @@ gulp.task( "clean:vendor-prefixed", function() {
  * Run `composer install`.
  */
 gulp.task( "shell:composer-install", function( cb ) {
-	spawn( 'composer', [ 'install' ], { stdio: 'inherit' })
-		.on('error', cb)
-		.on('close', code => code ? cb(new Error(code)) : cb());
+	spawn( "composer", [ "install" ], { stdio: "inherit" } )
+		.on( "error", cb )
+		.on( "close", code => code ? cb( new Error( code ) ) : cb() );
+} );
+
+/**
+ * Run `composer compile-di`.
+ */
+gulp.task( "shell:compile-dependency-injection-container", function( cb ) {
+	spawn( "composer", [ "compile-di" ], { stdio: "inherit" } )
+		.on( "error", cb )
+		.on( "close", code => code ? cb( new Error( code ) ) : cb() );
+} );
+
+/**
+ * Run `rm ./src/generated/container.php.meta`.
+ */
+gulp.task( "shell:remove-dependency-injection-meta", function( cb ) {
+	spawn( "rm", [ "./src/generated/container.php.meta" ], { stdio: "inherit" } )
+		.on( "error", cb )
+		.on( "close", code => code ? cb( new Error( code ) ) : cb() );
+} );
+
+/**
+ * Run `composer install --prefer-dist --optimize-autoloader --no-dev --no-scripts`.
+ */
+gulp.task( "shell:composer-install-production", function( cb ) {
+	spawn( "composer", [ "install", "--prefer-dist", "--optimize-autoloader", "--no-dev", "--no-scripts" ], { stdio: "inherit" } )
+		.on( "error", cb )
+		.on( "close", code => code ? cb( new Error( code ) ) : cb() );
 } );
 
 /**
  * Build the js files through webpack.
  */
 gulp.task( "shell:webpack", function( cb ) {
-	exec( "yarn cross-env NODE_ENV=development", function( err, stdout, stderr ) {
-		console.log( stdout );
-		console.log( stderr );
-		cb( err );
-	} );
-	exec( "yarn run wp-scripts build --config config/webpack/webpack.config.js", function( err, stdout, stderr ) {
-		console.log( stdout );
-		console.log( stderr );
-		cb( err );
-	} );
+	spawn.sync( "yarn", [ "cross-env", "NODE_ENV=development" ], { stdio: "inherit" } );
+	spawn( "yarn", [ "run", "wp-scripts", "build", "--config", "config/webpack/webpack.config.js" ], { stdio: "inherit" } )
+		.on( "error", cb )
+		.on( "close", code => code ? cb( new Error( code ) ) : cb() );
 } );
 
 /**
@@ -116,6 +138,6 @@ gulp.task( "watch", function() {
 	} );
 } );
 
-gulp.task( "build:js", gulp.series( "clean:js", "shell:webpack" ) );
-gulp.task( "build:css", gulp.series( "clean:css", gulp.parallel( css ) ) );
+gulp.task( "build:js", gulp.series( "clean:build-assets-js", "shell:webpack" ) );
+gulp.task( "build:css", gulp.series( "clean:build-assets-css", gulp.parallel( css ) ) );
 gulp.task( "default", gulp.parallel( "build:js", "build:css" ) );
