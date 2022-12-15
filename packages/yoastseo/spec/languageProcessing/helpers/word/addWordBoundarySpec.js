1c2e8bbae5cf33e2dd5eb146db2d17c09a106d08
hdvos <hdvos93@gmail.com>
2022-12-15 14:53:03 +0100

    adapt addwordboundarySpec

--- packages/yoastseo/spec/languageProcessing/helpers/word/addWordBoundarySpec.js
+++ packages/yoastseo/spec/languageProcessing/helpers/word/addWordBoundarySpec.js
@@ -1,42 +1,43 @@
 import addWordBoundary from "../../../../src/languageProcessing/helpers/word/addWordboundary";
+import { wordBoundariesStringForRegex } from "../../../../src/config/punctuation";
 
 describe( "a test adding word boundaries to a string", function() {
-	const wordBoundary = " \\u00a0\\u2014\\u06d4\\u061f\\u060C\\u061B\\n\\r\\t.,()”“〝〞〟‟„\"+\\-;!¡?¿:/»«‹›";
-
+	// const wordBoundary = "(^|[\\ \\\\    \\\\ \\\"\\»\\«\\”\\“\\〝\\〞\\〟\\‟\\„\\『\\』\\‘\\’\\‛\\`\\.\\,\\(\\)\\+\\;\\!\\?\\:\\/\\<\\>\\¡\\¿\\—\\۔\\؟\\،\\؛\\ '‘’‛`])keyword($|([\\ \\\\    \\\\ \\\"\\»\\«\\”\\“\\〝\\〞\\〟\\‟\\„\\『\\』\\‘\\’\\‛\\`\\.\\,\\(\\)\\+\\;\\!\\?\\:\\/\\<\\>\\¡\\¿\\—\\۔\\؟\\،\\؛\\ ])|((['‘’‛`])([\\ \\\\    \\\\ \\\"\\»\\«\\”\\“\\〝\\〞\\〟\\‟\\„\\『\\』\\‘\\’\\‛\\`\\.\\,\\(\\)\\+\\;\\!\\?\\:\\/\\<\\>\\¡\\¿\\—\\۔\\؟\\،\\؛\\
+	const wordBoundary = wordBoundariesStringForRegex;
 	it( "adds start and end boundaries", function() {
 		expect( addWordBoundary( "keyword" ) ).toEqual(
-			"(^|[" + wordBoundary + "<>'‘’‛`])" +
-			"keyword($|([" + wordBoundary + "<>])|((['‘’‛`])" +
-			"([" + wordBoundary + "<>])))"
+			"(^|[" + wordBoundary + "'‘’‛`])" +
+			"keyword($|([" + wordBoundary + "])|((['‘’‛`])" +
+			"([" + wordBoundary + "])))"
 		);
 	} );
 	it( "adds start and end boundaries and an extra boundary", function() {
 		expect( addWordBoundary( "keyword", false, "#" ) ).toEqual(
-			"(^|[" + wordBoundary + "#<>'‘’‛`])" +
-			"keyword($|([" + wordBoundary + "#<>])|((['‘’‛`])" +
-			"([" + wordBoundary + "#<>])))"
+			"(^|[" + wordBoundary + "#'‘’‛`])" +
+			"keyword($|([" + wordBoundary + "#])|((['‘’‛`])" +
+			"([" + wordBoundary + "#])))"
 		);
 	} );
 	it( "adds start boundaries, and end boundaries with positive lookahead", function() {
 		expect( addWordBoundary( "keyword", true, "" ) ).toEqual(
-			"(^|[" + wordBoundary + "<>'‘’‛`])" +
-			"keyword($|((?=[" + wordBoundary + "<>]))|((['‘’‛`])" +
-			"([" + wordBoundary + "<>])))"
+			"(^|[" + wordBoundary + "'‘’‛`])" +
+			"keyword($|((?=[" + wordBoundary + "]))|((['‘’‛`])" +
+			"([" + wordBoundary + "])))"
 		);
 	} );
 	it( "adds start boundaries with an extra boundary, and end boundaries with positive lookahead and an extra boundary", function() {
 		expect( addWordBoundary( "keyword", true, "#" ) ).toEqual(
-			"(^|[" + wordBoundary + "#<>'‘’‛`])" +
-			"keyword($|((?=[" + wordBoundary + "#<>]))|((['‘’‛`])" +
-			"([" + wordBoundary + "#<>])))"
+			"(^|[" + wordBoundary + "#'‘’‛`])" +
+			"keyword($|((?=[" + wordBoundary + "#]))|((['‘’‛`])" +
+			"([" + wordBoundary + "#])))"
 		);
 	} );
 	it( "uses a word boundary excluding - when the locale is Indonesian", function() {
-		const idWordBoundary = " \\u00a0\\n\\r\\t.,()”“〝〞〟‟„\"+;!¡?¿:/»«‹›";
+		const idWordBoundary = wordBoundariesStringForRegex;
 		expect( addWordBoundary( "keyword", false, "", "id_ID" ) ).toEqual(
-			"(^|[" + idWordBoundary + "<>'‘’‛`])" +
-			"keyword($|([" + idWordBoundary + "<>])|((['‘’‛`])" +
-			"([" + idWordBoundary + "<>])))"
+			"(^|[" + idWordBoundary + "'‘’‛`])" +
+			"keyword($|([" + idWordBoundary + "])|((['‘’‛`])" +
+			"([" + idWordBoundary + "])))"
 		);
 	} );
 } );
