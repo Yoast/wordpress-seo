72c71da2769cfe8bd3d636e09694b374964b43f1
Filipe de Carvalho <filipe.decarvalho@yoast.com>
2022-12-22 15:02:32 +0100

    Make lines less than 150 characters

--- packages/yoastseo/src/languageProcessing/languages/pt/config/transitionWords.js
+++ packages/yoastseo/src/languageProcessing/languages/pt/config/transitionWords.js
@@ -18,8 +18,9 @@ export const multipleWords = [
 	"já que", "juntamente com", "logo após", "logo depois", "logo que", "mesmo que", "não apenas", "nesse hiato", "nesse ínterim", "nesse meio tempo",
 	"nesse sentido", "no entanto", "no momento em que", "ou por outra", "ou seja", "para que", "pelo contrário", "por analogia",
 	"por causa de", "por certo", "por conseguinte", "por conseqüência", "porém", "por exemplo", "por fim", "por isso", "por mais que",
-	"por menos que", "por outro lado", "por vezes", "posto que", "se acaso", "se bem que", "seja como for", "sem dúvida", "sempre que", "só para exemplificar",
-	"só para ilustrar", "só que", "sob o mesmo ponto de vista", "talvez provavelmente", "tanto quanto", "todas as vezes que", "todas as vezes em que", "uma vez que", "visto que",
+	"por menos que", "por outro lado", "por vezes", "posto que", "se acaso", "se bem que", "seja como for", "sem dúvida", "sempre que",
+	"só para exemplificar", "só para ilustrar", "só que", "sob o mesmo ponto de vista", "talvez provavelmente", "tanto quanto",
+	"todas as vezes que", "todas as vezes em que", "uma vez que", "visto que",
 ];
 
 export const allWords = singleWords.concat( multipleWords );
