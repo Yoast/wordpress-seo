805643fe1083408b4eb846c6a5ba1094c3ff695b
Filipe de Carvalho <filipe.decarvalho@yoast.com>
2022-12-29 15:03:05 +0100

    Simplifies check for Portuguese participles

--- packages/yoastseo/src/languageProcessing/languages/pt/helpers/internal/getParticiples.js
+++ packages/yoastseo/src/languageProcessing/languages/pt/helpers/internal/getParticiples.js
@@ -1,8 +1,32 @@
-import { includes } from "lodash-es";
 import { languageProcessing } from "yoastseo";
 const { getWords } = languageProcessing;
 
-import participles from "../../config/internal/passiveVoiceParticiples";
+import participleStems from "../../config/internal/passiveVoiceParticiples";
+
+/**
+ * Checks if a Portuguese word is a participle.
+ * @param {string} word A Portuguese word.
+ * @returns {boolean} Returns true if word is a Portuguese participle, and false otherwise.
+ */
+function isParticiple( word ) {
+	// Check if the word ends with one of the vowel endings
+	if ( word.endsWith( "a" ) || word.endsWith( "o" ) ) {
+		// Remove the vowel ending from the word
+		const stem = word.slice( 0, -1 );
+		// Check if the stem is in the list of participle roots
+		if ( participleStems.includes( stem ) ) {
+			return true;
+		}
+		// Check if the word ends with one of the vowel endings
+	} else if ( word.endsWith( "as" ) || word.endsWith( "os" ) ) {
+		// Remove the vowel ending from the word
+		const stem = word.slice( 0, -2 );
+		if ( participleStems.includes( stem ) ) {
+			return true;
+		}
+	}
+	return false;
+}
 
 /**
  * Creates an array of participles for the participles found in a clause.
@@ -13,6 +37,5 @@ import participles from "../../config/internal/passiveVoiceParticiples";
  */
 export default function getParticiples( clauseText ) {
 	const words = getWords( clauseText );
-
-	return words.filter( word => includes( participles, word ) );
+	return words.filter( word => isParticiple(word) );
 }
