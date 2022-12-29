c5a833b0293f748d777fc791559103a352e50eb3
Marina Koleva <marina@yoast.com>
2022-12-29 17:17:50 +0200

    tiny formatting fix

--- packages/yoastseo/src/languageProcessing/helpers/regex/matchStringWithRegex.js
+++ packages/yoastseo/src/languageProcessing/helpers/regex/matchStringWithRegex.js
@@ -3,8 +3,8 @@
 /**
  * Checks a string with a regex, return all matches found with that regex.
  *
- * @param {String} text         The text to match.
- * @param {object} regex  A compiled regular expression.
+ * @param {String} 		text        The text to match.
+ * @param {object} 		regex  		A compiled regular expression.
  *
  * @returns {Array} Array with matches, empty array if no matches found.
  */
