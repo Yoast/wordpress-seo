9511ff54e82a4e7bbc28d05fd2fae8947f7b3f5b
Igor <35524806+igorschoester@users.noreply.github.com>
2023-01-02 10:44:57 +0100

    Settings UI: fix show more/less menu button icon

--- packages/js/src/settings/app.js
+++ packages/js/src/settings/app.js
@@ -1,14 +1,7 @@
 /* eslint-disable react/jsx-max-depth */
 import { Transition } from "@headlessui/react";
-import {
-	AdjustmentsIcon,
-	ArrowNarrowRightIcon,
-	ChevronDownIcon,
-	ChevronUpIcon,
-	ColorSwatchIcon,
-	DesktopComputerIcon,
-	NewspaperIcon,
-} from "@heroicons/react/outline";
+import { AdjustmentsIcon, ArrowNarrowRightIcon, ColorSwatchIcon, DesktopComputerIcon, NewspaperIcon } from "@heroicons/react/outline";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/solid";
 import { useCallback, useMemo } from "@wordpress/element";
 import { __, sprintf } from "@wordpress/i18n";
 import { Badge, Button, ChildrenLimiter, ErrorBoundary, Title, useBeforeUnload, useSvgAria } from "@yoast/ui-library";
@@ -54,7 +47,7 @@ const Menu = ( { postTypes, taxonomies, idSuffix = "" } ) => {
 			<div className="yst-relative">
 				<hr className="yst-absolute yst-inset-x-0 yst-top-1/2 yst-bg-slate-200" />
 				<button
-					className="yst-relative yst-flex yst-items-center yst-gap-2 yst-px-2.5 yst-py-1 yst-mx-auto yst-text-xs yst-font-medium yst-text-slate-700 yst-bg-slate-50 yst-rounded-full yst-border yst-border-slate-300 hover:yst-bg-white hover:yst-text-slate-800 focus:yst-outline-none focus:yst-ring-2 focus:yst-ring-primary-500 focus:yst-ring-offset-2"
+					className="yst-relative yst-flex yst-items-center yst-gap-1.5 yst-px-2.5 yst-py-1 yst-mx-auto yst-text-xs yst-font-medium yst-text-slate-700 yst-bg-slate-50 yst-rounded-full yst-border yst-border-slate-300 hover:yst-bg-white hover:yst-text-slate-800 focus:yst-outline-none focus:yst-ring-2 focus:yst-ring-primary-500 focus:yst-ring-offset-2"
 					onClick={ toggle }
 					{ ...ariaProps }
 				>
