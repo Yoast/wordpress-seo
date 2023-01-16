d03fb832eaf87cd04dc335609719edea5d2c6725
Vraja Das <vraja.pro@gmail.com>
2023-01-16 10:01:37 +0100

    redo tailwind config ring size 3

--- packages/ui-library/tailwind.config.js
+++ packages/ui-library/tailwind.config.js
@@ -2,11 +2,4 @@
 module.exports = {
 	presets: [ require( "@yoast/tailwindcss-preset" ) ],
 	content: [ "./src/**/*.js" ],
-	theme: {
-		extend: {
-		  ringWidth: {
-				3: "3px",
-		  },
-		},
-	  },
 };
