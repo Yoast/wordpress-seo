f9e07cb6bde43df8cb1d33ec25d2b0bedf401b75
Vraja Das <vraja.pro@gmail.com>
2023-01-16 09:05:34 +0100

    fix: custom ring width for radio checked and focus

--- packages/ui-library/tailwind.config.js
+++ packages/ui-library/tailwind.config.js
@@ -2,4 +2,11 @@
 module.exports = {
 	presets: [ require( "@yoast/tailwindcss-preset" ) ],
 	content: [ "./src/**/*.js" ],
+	theme: {
+		extend: {
+		  ringWidth: {
+				3: "3px",
+		  },
+		},
+	  },
 };
