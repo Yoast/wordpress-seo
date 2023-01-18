a323e08f8d0a327bdcbc332562a1cd0358b27ef0
Paolo Luigi Scala <paolo@yoast.com>
2023-01-18 13:35:58 +0100

    Fix comments

--- src/conditionals/third-party/edd-conditional.php
+++ src/conditionals/third-party/edd-conditional.php
@@ -5,14 +5,14 @@ namespace Yoast\WP\SEO\Conditionals\Third_Party;
 use Yoast\WP\SEO\Conditionals\Conditional;
 
 /**
- * Conditional that is only met when WooCommerce is active.
+ * Conditional that is only met when Easy Digital Downloads is active.
  */
 class EDD_Conditional implements Conditional {
 
 	/**
-	 * Returns `true` when the WooCommerce plugin is installed and activated.
+	 * Returns `true` when the Easy Digital Downloads plugin is installed and activated.
 	 *
-	 * @return bool `true` when the WooCommerce plugin is installed and activated.
+	 * @return bool `true` when the Easy Digital Downloads plugin is installed and activated.
 	 */
 	public function is_met() {
 		return \class_exists( 'Easy_Digital_Downloads' );
