f716d70e016b107ea1eee48d8dd201ec9873f422
Filipe de Carvalho <filipe.decarvalho@yoast.com>
2023-01-31 12:17:54 +0100

    Refactoring of Analytical Ambiguity check

--- packages/yoastseo/spec/languageProcessing/researches/checkAnalyticalSpec.js
+++ packages/yoastseo/spec/languageProcessing/researches/checkAnalyticalSpec.js
@@ -1,23 +1,79 @@
 import Paper from "../../../src/values/Paper";
 import EnglishResearcher from "../../../src/languageProcessing/languages/en/Researcher";
 import checkAnalytical from "../../../src/languageProcessing/researches/checkAnalytical";
+import fetch from "node-fetch";
+import SearchKey from "../../../SearchKey.json";
 
-describe( "Check for sentences with analytical ambiguity", function() {
-	it( "should find that some sentences are ambiguous", function() {
-		const mockPaper = new Paper(
-			"John is an English grammar teacher. " +
-			"John is a teacher. "
-		);
-		const mockResearcher = new EnglishResearcher( mockPaper );
+it( "should find that some sentences are ambiguous", async function() {
+	/**
+	 * Checks if a reading is possible according to Google Search.
+	 * @param {string} reading The potentially ambiguous reading.
+	 * @returns {number} Returns the number of hits.
+	 */
+	async function getHits( reading ) {
+		const cx = SearchKey.cx;
+		const key = SearchKey.key;
+		const url = "https://www.googleapis.com/customsearch/v1?";
+		const parameters = { q: reading, exactTerms: reading, cx: cx, key: key, googlehost: "www.google.com" };
+		let hits = 0;
 
-		expect( checkAnalytical( mockPaper, mockResearcher ) ).toEqual(
-			[
-				{
-					sentence: "John is an English grammar teacher.",
-					reading1: "teacher of English grammar",
-					reading2: "English teacher of grammar",
-					construction: [ "English", "grammar", "teacher" ],
-				},
-			] );
-	} );
+		// Using the parameters above, fetch results from Google.com
+		await fetch( url + new URLSearchParams( parameters ), { method: "GET" } )
+			.then( response => {
+				// Return the HTTP response status code
+				console.log( response.status );
+				return response.json();
+			} )
+			// Return the number of hits for each query
+			.then( data => {
+				hits = parseInt( data.searchInformation.totalResults, 10 );
+			} )
+			// If there is any error, return error message
+			.catch( function( err ) {
+				console.log( err );
+			} );
+
+		return hits;
+	}
+	// Test sentences with targeted regex
+	const mockPaper = new Paper(
+		"John is an English grammar teacher. " +
+		"John is a teacher. "
+	);
+	const mockResearcher = new EnglishResearcher( mockPaper );
+	const analyticalResult = await checkAnalytical( mockPaper, mockResearcher );
+	const results = await Promise.all( analyticalResult.map( async( result ) => {
+		// Save results for each reading in a variable
+		// eslint-disable-next-line no-inline-comments
+		const hitsReading1 = 0; // await getHits( result.reading1 );
+		// eslint-disable-next-line no-inline-comments
+		const hitsReading2 = 0; // await getHits( result.reading2 );
+		// Return each reading and its number of hits
+		console.log( result.reading1, hitsReading1 );
+		console.log( result.reading2, hitsReading2 );
+		// Sort the hits in ascending order
+		const sortedReadings = [ hitsReading1, hitsReading2 ].sort( function( a, b ) {
+			return a - b;
+		} );
+		console.log( sortedReadings );
+		const ambFormula = sortedReadings[ 0 ] / sortedReadings[ 1 ] * 100;
+		if ( ! isNaN( ambFormula ) && sortedReadings[ 0 ] >= 80 ) {
+			console.log( "The construction", '"' + result.construction.join( " " ) + '"', "has two potential readings:",
+				"1:", result.reading1, "OR", "2:", result.reading2 );
+			console.log( "Ambiguity: " + Math.round( ambFormula ) + "%" );
+		} else {
+			console.log( "No ambiguity found. Ratio: " + Math.round( ambFormula ) + "%" );
+		}
+	} ) );
+
+	console.log( results );
+	expect( analyticalResult ).toEqual(
+		[
+			{
+				sentence: "John is an English grammar teacher.",
+				reading1: "teacher of English grammar",
+				reading2: "English teacher of grammar",
+				construction: [ "English", "grammar", "teacher" ],
+			},
+		] );
 } );
--- packages/yoastseo/src/languageProcessing/researches/checkAnalytical.js
+++ packages/yoastseo/src/languageProcessing/researches/checkAnalytical.js
@@ -5,93 +5,129 @@ import getWords from "../helpers/word/getWords";
 const analyticalRegExp = new RegExp( "<JJ><NN.?><NN.?>" );
 
 /**
- * Checks whether a sentence has Analytical Ambiguity.
+ * An object containing the results of the Analytical Ambiguity research for a single sentence.
  *
- * @param {POSTagger} tagger The part-of-speech tagger.
- * @param {string} sentence The current sentence.
- * @returns {Object|null} The words composing the ambiguous construction (empty if none found), as well as the two alternative readings.
+ * The structure of the data is:
+ * @example
+ * {
+ * sentence: "John is an English grammar teacher.",
+ * reading1: "teacher of English grammar",
+ * reading2: "English teacher of grammar",
+ * construction: [ "English", "grammar", "teacher" ],
+ * }
+ *
+ * @typedef {Object} 	analyticalResult
+ * @property {string}	sentence		The sentence.
+ * @property {string}	reading1		The first reading, in which the adjective attaches to the first noun.
+ * @property {string}	reading2		The second reading, in which the adjective attaches to the second noun.
+ * @property {string[]}	construction	The ambiguous construction as a whole.
  */
 
-function isAmbiguous( tagger, sentence ) {
-	const words = getWords( sentence, false );
-	const tags = tagger.tag( words );
-
-	// Convert the tags to a string, so that we can more easily apply a regular expression.
-	const tagString = tags.map( tag => "<" + tag[ 1 ] + ">" ).join( "" );
-	// Try to match the the analyticalRegExp
-	const match = tagString.match( analyticalRegExp );
+/**
+ * Handles a match: extracts the construction and the two possible readings from the sentence.
+ *
+ * @param {string} sentence The current sentence.
+ * @param {Object[]} tags The assigned part-of-speech tags.
+ * @param {RegExpMatchArray} match The match.
+ * @returns {AnalyticalResult} The result.
+ */
+function handleMatch( sentence, tags, match ) {
 	const construction = [];
 	const reading1 = [];
 	const reading2 = [];
 	let firstNoun = "";
-	if ( match !== null ) {
-		const indexStart = match.index;
-		const indexFinal = indexStart + match[ 0 ].length;
-		let indexCurrent = 0;
-		tags.forEach( tag => {
-			if ( indexStart <= indexCurrent && indexCurrent < indexFinal ) {
-				const wordCurrent = tag[ 0 ];
-				const tagCurrent = tag[ 1 ];
-				construction.push( wordCurrent );
-				switch ( tagCurrent.slice( 0, 2 ) ) {
-					case "JJ":
+	const indexStart = match.index;
+	const indexFinal = indexStart + match[ 0 ].length;
+	let indexCurrent = 0;
+	tags.forEach( tag => {
+		const wordCurrent = tag[ 0 ];
+		const tagCurrent = tag[ 1 ];
+		if ( indexStart <= indexCurrent && indexCurrent < indexFinal ) {
+			construction.push( wordCurrent );
+			switch ( tagCurrent.slice( 0, 2 ) ) {
+				case "JJ":
+					reading1.push( wordCurrent );
+					reading2.push( wordCurrent );
+					break;
+				case "NN":
+					if ( reading1.length === 1 ) {
+						firstNoun = wordCurrent;
 						reading1.push( wordCurrent );
-						reading2.push( wordCurrent );
-						break;
-					case "NN":
-						if ( reading1.length === 1 ) {
-							firstNoun = wordCurrent;
-							reading1.push( wordCurrent );
-						}
-						else {
-							reading1.unshift( wordCurrent, "of" );
-							reading2.push( wordCurrent, "of", firstNoun );
-						}
-						break;
-					default:
-						break;
-				}
+					} else {
+						reading1.unshift( wordCurrent, "of" );
+						reading2.push( wordCurrent, "of", firstNoun );
+					}
+					break;
+				default:
+					break;
 			}
-			// Add 2 because we added brackets to the tagString above.
-			indexCurrent += tag[ 1 ].length + 2;
-		} );
+		}
+		// Add 2 because we added brackets to the tagString earlier.
+		indexCurrent += tagCurrent.length + 2;
+	} );
 
 
-		return {
-			sentence: sentence,
-			construction: construction,
-			reading1: reading1.join( " " ),
-			reading2: reading2.join( " " ),
-		};
+	return {
+		sentence: sentence,
+		construction: construction,
+		reading1: reading1.join( " " ),
+		reading2: reading2.join( " " ),
+	};
+	return null;
+}
+
+/**
+ * Obtains sentence readings if it matches regex for Analytical constructions.
+ *
+ * @param {POSTagger} tagger The Parts-Of-Speech tagger.
+ * @param {string} sentence The current sentence.
+ * @returns {AnalyticalResult|null} The ambiguous construction (empty if none found), as well as the two alternative readings.
+ */
+function getReadings( tagger, sentence ) {
+	const words = getWords( sentence, false );
+	const tags = tagger.tag( words );
+
+	// Convert the tags to a string so that we can more easily apply a regular expression.
+	const tagString = tags.map( tag => "<" + tag[ 1 ] + ">" ).join( "" );
+	// Checks if there is a match
+	const match = tagString.match( analyticalRegExp );
+
+	if ( match !== null ) {
+		return handleMatch( sentence, tags, match );
 	}
+
 	return null;
 }
 /**
- * Finds all sentences with (problematic) analytical constructions.
+ * Finds all sentences with (problematic) Analytical constructions.
  *
  * @param {string[]} sentences The sentences in the text.
  * @param {Researcher} researcher The researcher to use for analysis.
- * @returns {string[]} (potentially) syntactically ambiguous sentences.
+ * @returns {Promise<PPAttachmentResult[]>} (potentially) syntactically ambiguous sentences.
  */
-function findAnalytical( sentences, researcher ) {
+async function findAnalytical( sentences, researcher ) {
 	const ambiguousSentences = [];
 
 	const tagger = researcher.getHelper( "getTagger" )();
 
-	sentences.forEach( sentence => {
-		const result = isAmbiguous( tagger, sentence );
+	await Promise.all( sentences.map( async( sentence ) => {
+		const result = getReadings( tagger, sentence );
 		if ( result !== null ) {
+			// result.hitsReading1 = getHits( result.reading1 );
+			// result.hitsReading2 = getHits( result.reading2 );
+
 			ambiguousSentences.push( result );
 		}
-	} );
+	} ) );
 
 	return ambiguousSentences;
 }
 /**
- * Count sentences in the text.
- * @param {Paper} paper The Paper object to get text from.
- * @param {Researcher} 	researcher 	The researcher to use for analysis.
- * @returns {string[]} The ambiguous sentences from the text.
+ * Finds sentences in the text that (potentially) have Analytical Ambiguity.
+ *
+ * @param {Paper} paper The Paper object to obtain the text from.
+ * @param {Researcher} 	researcher 	The researcher to use for the analysis.
+ * @returns {Promise<AnalyticalResult[]>} The ambiguous constructions from the text.
  */
 export default function( paper, researcher ) {
 	const memoizedTokenizer = researcher.getHelper( "memoizedTokenizer" );
