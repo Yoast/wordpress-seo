6168420123c702325b64cd79402e7243ce536abf
marijnyoast <marijn@yoast.com>
2023-02-02 15:51:47 +0100

    Update CTB ID

--- packages/js/src/workouts/components/WorkoutCard.js
+++ packages/js/src/workouts/components/WorkoutCard.js
@@ -92,7 +92,7 @@ export default function WorkoutCard( {
 				{ /* eslint-disable-next-line max-len */ }
 				{ workout && <Button id={ `${ id }-action-button` } className={ `yoast-button yoast-button--${ isToggle ? "secondary" : "primary" }` } onClick={ onClick }>{ buttonText }</Button> }
 				{ ! workout &&
-					<UpsellButton id={ `${ id }-upsell-button` } href={ upsellLink } className="yoast-button yoast-button-upsell" data-action="load-nfd-ctb" data-ctb-id="57d6a568-783c-45e2-a388-847cff155897">
+					<UpsellButton id={ `${ id }-upsell-button` } href={ upsellLink } className="yoast-button yoast-button-upsell" data-action="load-nfd-ctb" data-ctb-id="f6a84663-465f-4cb5-8ba5-f7a6d72224b2">
 						{ actualUpsellText }
 						<span aria-hidden="true" className="yoast-button-upsell__caret" />
 					</UpsellButton>
