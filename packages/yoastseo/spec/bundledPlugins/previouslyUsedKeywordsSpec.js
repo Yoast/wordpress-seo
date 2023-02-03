0c35fb91912be15bec712a5cbedca818839d2422
hdvos <hdvos93@gmail.com>
2023-02-03 15:11:23 +0100

    adapt js test

--- packages/yoastseo/spec/bundledPlugins/previouslyUsedKeywordsSpec.js
+++ packages/yoastseo/spec/bundledPlugins/previouslyUsedKeywordsSpec.js
@@ -1,14 +1,14 @@
 import PreviouslyUsedKeywords from "../../src/bundledPlugins/previouslyUsedKeywords.js";
 
-var usedKeywords = { keyword: [ 1 ], test: [ 2, 3, 4 ] };
+let usedKeywords = { keyword: [ 1 ], test: [ 2, 3, 4 ] };
 import Paper from "../../src/values/Paper.js";
 
-var app = {};
+const app = {};
 
-var searchUrl =  "http://search?{keyword}";
-var postUrl = "http://post?{id}";
+const searchUrl = "http://search?{keyword}";
+const postUrl = "http://post?{id}";
 
-var args = {
+let args = {
 	usedKeywords: usedKeywords,
 	searchUrl: searchUrl,
 	postUrl: postUrl,
@@ -16,35 +16,38 @@ var args = {
 
 describe( "checks for keyword doubles", function() {
 	it( "returns array with keyword", function() {
-		var paper = new Paper( "text", { keyword: "keyword" } );
+		const paper = new Paper( "text", { keyword: "keyword" } );
 
-		var plugin = new PreviouslyUsedKeywords( app, args );
+		const plugin = new PreviouslyUsedKeywords( app, args );
 		expect( plugin.scoreAssessment( { id: 1, count: 1 }, paper ).score ).toBe( 6 );
-		expect( plugin.scoreAssessment( { id: 1, count: 1 }, paper ).text ).toBe( "<a href='https://yoa.st/33x' target='_blank'>Previously used keyphrase</a>: " +
-			"You've used this keyphrase <a href='http://post?1' target='_blank'>once before</a>. " +
+		expect( plugin.scoreAssessment( { id: 1, count: 1 }, paper ).text ).toBe( "<a href='https://yoa.st/33x' target='_blank'>" +
+			"Previously used keyphrase</a>: You've used this keyphrase <a href='http://post?1' target='_blank'>once before</a>. " +
 			"<a href='https://yoa.st/33y' target='_blank'>Do not use your keyphrase more than once</a>." );
 
 		expect( plugin.scoreAssessment( { id: 1, count: 2 }, paper ).score ).toBe( 1 );
-		expect( plugin.scoreAssessment( { id: 1, count: 2 }, paper ).text ).toBe( "<a href='https://yoa.st/33x' target='_blank'>Previously used keyphrase</a>: " +
-			"You've used this keyphrase <a href='http://search?keyword' target='_blank'>2 times before</a>. " +
-			"<a href='https://yoa.st/33y' target='_blank'>Do not use your keyphrase more than once</a>." );
+		expect( plugin.scoreAssessment( { id: 1, count: 2 }, paper ).text ).toBe("<a href='https://yoa.st/33x' " +
+			"target='_blank'>Previously used keyphrase</a>: You've used this keyphrase <a href='http://search?keyword&post_type=undefined' " +
+			"target='_blank'> multiple times before</a>. <a href='https://yoa.st/33y' target='_blank'>" +
+			"Do not use your keyphrase more than once</a>." );
 
 		expect( plugin.scoreAssessment( { id: 0, count: 0 }, paper ).score ).toBe( 9 );
-		expect( plugin.scoreAssessment( { id: 0, count: 0 }, paper ).text ).toBe( "<a href='https://yoa.st/33x' target='_blank'>Previously used keyphrase</a>: You've not used this keyphrase before, very good." );
+		expect( plugin.scoreAssessment( { id: 0, count: 0 }, paper ).text ).toBe( "<a href='https://yoa.st/33x' " +
+			"target='_blank'>Previously used keyphrase</a>: You've not used this keyphrase before, very good." );
 	} );
 
 	it( "escapes the keyword's special characters in the url", function() {
-		var paper = new Paper( "text", { keyword: "keyword/bla" } );
-		var plugin = new PreviouslyUsedKeywords( app, args );
-		expect( plugin.scoreAssessment( { id: 1, count: 2 }, paper ).text ).toBe( "<a href='https://yoa.st/33x' target='_blank'>Previously used keyphrase</a>: " +
-			"You've used this keyphrase <a href='http://search?keyword%2Fbla' target='_blank'>2 times before</a>. " +
+		const paper = new Paper( "text", { keyword: "keyword/bla" } );
+		const plugin = new PreviouslyUsedKeywords( app, args );
+		expect( plugin.scoreAssessment( { id: 1, count: 2 }, paper ).text ).toBe( "<a href='https://yoa.st/33x' " +
+			"target='_blank'>Previously used keyphrase</a>: You've used this keyphrase " +
+			"<a href='http://search?keyword%2Fbla&post_type=undefined' target='_blank'> multiple times before</a>. " +
 			"<a href='https://yoa.st/33y' target='_blank'>Do not use your keyphrase more than once</a>." );
 	} );
 } );
 
-describe( "checks for keyword doubles", function() {
-	it( "returns array with keyword", function() {
-		var plugin = new PreviouslyUsedKeywords( app, undefined ); // eslint-disable-line no-undefined
+describe( "checks for keyword doubles2", function() {
+	it( "returns array with keyword2", function() {
+		const plugin = new PreviouslyUsedKeywords( app, undefined ); // eslint-disable-line no-undefined
 		expect( plugin.searchUrl ).toBe( "" );
 	} );
 } );
