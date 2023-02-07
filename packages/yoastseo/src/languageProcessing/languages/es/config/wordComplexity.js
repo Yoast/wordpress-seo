3b52e131f5ae7cb87c0d49359b3c831b3ec99cf2
FAMarfuaty <aida@yoast.com>
2023-02-07 11:28:35 +0100

    Add comments

--- packages/yoastseo/src/languageProcessing/languages/es/config/wordComplexity.js
+++ packages/yoastseo/src/languageProcessing/languages/es/config/wordComplexity.js
@@ -1,5 +1,6 @@
 import frequencyList from "./internal/frequencyList.json";
 
+// This config is used for the premium assessment Word Complexity. As such, this config is registered from the premium repository.
 export default {
 	frequencyList: frequencyList.list,
 	wordLength: 7,
