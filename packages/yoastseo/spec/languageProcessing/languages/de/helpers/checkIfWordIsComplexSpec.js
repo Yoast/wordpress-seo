86407281ad9751f2f7a28bc5b5febb07ce4748a5
FAMarfuaty <aida@yoast.com>
2023-02-07 17:06:44 +0100

    adjust unit tests

--- packages/yoastseo/spec/languageProcessing/languages/de/helpers/checkIfWordIsComplexSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/de/helpers/checkIfWordIsComplexSpec.js
@@ -1,34 +1,36 @@
 import checkIfWordIsComplex from "../../../../../src/languageProcessing/languages/de/helpers/checkIfWordIsComplex";
+import wordComplexityConfig from "../../../../../src/languageProcessing/languages/de/config/wordComplexity";
+import functionWords from "../../../../../src/languageProcessing/languages/de/config/functionWords";
 
 describe( "a test checking if the word is complex in German",  function() {
 	it( "returns singular word form as non-complex if it is found in the list", function() {
-		expect( checkIfWordIsComplex( "präsident" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "präsident", functionWords.all ) ).toEqual( false );
 	} );
 
 	it( "returns plural word form as non-complex if its singular form is found in the list", function() {
-		expect( checkIfWordIsComplex( "Verstärkungen" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "Verstärkungen", functionWords.all ) ).toEqual( false );
 	} );
 
 	it( "returns plural word form as non-complex if its singular form is found in the list", function() {
-		expect( checkIfWordIsComplex( "Gouverneure" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "Gouverneure", functionWords.all ) ).toEqual( false );
 	} );
 	it( "returns word as non-complex if it is found in the function words list", function() {
-		expect( checkIfWordIsComplex( "verschiedenes" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "verschiedenes", functionWords.all ) ).toEqual( false );
 	} );
 
 	it( "returns plural word as complex if it (and its singular form) are not in the list", function() {
-		expect( checkIfWordIsComplex( "optimierungen" ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "optimierungen", functionWords.all ) ).toEqual( true );
 	} );
 
 	it( "returns word longer than 10 characters as complex if it's not in the list", function() {
-		expect( checkIfWordIsComplex( "architektonisch" ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "architektonisch", functionWords.all ) ).toEqual( true );
 	} );
 
 	it( "returns plural word as non complex if the word is less than 10 characters", function() {
-		expect( checkIfWordIsComplex( "boxen" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "boxen", functionWords.all ) ).toEqual( false );
 	} );
 
 	it( "recognized contractions when the contraction uses ’ (right single quotation mark) instead of ' (apostrophe)", function() {
-		expect( checkIfWordIsComplex( "l’histoire" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "l’histoire", functionWords.all ) ).toEqual( false );
 	} );
 } );
--- packages/yoastseo/spec/languageProcessing/languages/es/helpers/checkIfWordIsComplexSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/es/helpers/checkIfWordIsComplexSpec.js
@@ -1,39 +1,41 @@
 import checkIfWordIsComplex from "../../../../../src/languageProcessing/languages/es/helpers/checkIfWordIsComplex";
+import wordComplexityConfig from "../../../../../src/languageProcessing/languages/es/config/wordComplexity";
+import functionWords from "../../../../../src/languageProcessing/languages/es/config/functionWords";
 
 describe( "a test checking if the word is complex in Spanish",  function() {
 	it( "returns singular word form as non-complex if it is found in the list", function() {
-		expect( checkIfWordIsComplex( "original" ) ).toEqual( false );
+		expect( checkIfWordIsComplex(  wordComplexityConfig, "original", functionWords.all ) ).toEqual( false );
 	} );
 
 	// eslint-disable-next-line max-len
 	it( "returns plural word form as non-complex if its singular form is found in the list when the singular word form ends with a consonant", function() {
-		expect( checkIfWordIsComplex( "originales" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "originales", functionWords.all ) ).toEqual( false );
 	} );
 
 	// eslint-disable-next-line max-len
 	it( "returns plural word form as non-complex if its singular form is found in the list when the singular word form ends with a vowel", function() {
-		expect( checkIfWordIsComplex( "parecidos" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "parecidos", functionWords.all ) ).toEqual( false );
 	} );
 
 	it( "returns word as non-complex if it starts with a capital (even if non capitalized form is complex)", function() {
-		expect( checkIfWordIsComplex( "Alhambra" ) ).toEqual( false );
-		expect( checkIfWordIsComplex( "alhambra" ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "Alhambra", functionWords.all ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "alhambra", functionWords.all ) ).toEqual( true );
 	} );
 
 
 	it( "returns word as non-complex if it is found in the function words list", function() {
-		expect( checkIfWordIsComplex( "diecisiete" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "diecisiete", functionWords.all ) ).toEqual( false );
 	} );
 
 	it( "returns plural word as complex if it (and its singular form) are not in the list", function() {
-		expect( checkIfWordIsComplex( "situados" ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "situados", functionWords.all ) ).toEqual( true );
 	} );
 
 	it( "returns word longer than 7 characters as complex if it's not in the list", function() {
-		expect( checkIfWordIsComplex( "contemplada" ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "contemplada", functionWords.all ) ).toEqual( true );
 	} );
 
 	it( "returns plural word as non complex if the word is less than 7 characters", function() {
-		expect( checkIfWordIsComplex( "cosas" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "cosas", functionWords.all ) ).toEqual( false );
 	} );
 } );
