be8bc02cb56379ae78a8586340b8f690e4d62a4b
Igor <35524806+igorschoester@users.noreply.github.com>
2023-02-08 16:53:38 +0100

    Remove search appearance: taxonomies tab

--- admin/pages/metas.php
+++ admin/pages/metas.php
@@ -15,7 +15,6 @@ $yform = Yoast_Form::get_instance();
 $yform->admin_header( true, 'wpseo_titles' );
 
 $metas_tabs = new WPSEO_Option_Tabs( 'metas' );
-$metas_tabs->add_tab( new WPSEO_Option_Tab( 'taxonomies', __( 'Taxonomies', 'wordpress-seo' ) ) );
 $metas_tabs->add_tab( new WPSEO_Option_Tab( 'archives', __( 'Archives', 'wordpress-seo' ) ) );
 $metas_tabs->add_tab( new WPSEO_Option_Tab( 'breadcrumbs', __( 'Breadcrumbs', 'wordpress-seo' ) ) );
 $metas_tabs->add_tab( new WPSEO_Option_Tab( 'rss', __( 'RSS', 'wordpress-seo' ) ) );
