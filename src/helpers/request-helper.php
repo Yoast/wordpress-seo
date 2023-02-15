8b3983139ee8acb15818045679b608f7f6c531cb
Hans-Christiaan <hans-christiaan@yoast.com>
2023-02-15 14:34:36 +0100

    Allow filtering of the meta query used for filtering the post overview on keyphrase.

--- src/helpers/request-helper.php
+++ src/helpers/request-helper.php
@@ -15,4 +15,21 @@ class Request_Helper {
 	public function is_rest_request() {
 		return \defined( 'REST_REQUEST' ) && \REST_REQUEST === true;
 	}
+
+	/**
+	 * Returns a variable from the PHP superglobal query string, or `null` if
+	 * it does not exist or is not a string.
+	 *
+	 * @param string $variable_name The name of the query variable.
+	 *
+	 * @return string|null The value of the query variable.
+	 */
+	public function get_variable_from_query_string( $variable_name ) {
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Reason: We are not processing form information.
+		if ( isset( $_GET[ $variable_name ] ) && \is_string( $_GET[ $variable_name ] ) ) {
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Reason: We are not processing form information.
+			return \sanitize_text_field( \wp_unslash( $_GET[ $variable_name ] ) );
+		}
+		return null;
+	}
 }
