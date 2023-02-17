1c660d5a0b427cb55065509aac314a8a1de48b95
Hans-Christiaan <hans-christiaan@yoast.com>
2023-02-17 14:57:13 +0100

    Simplified way that related keyphrase filter is added.

--- src/helpers/request-helper.php
+++ src/helpers/request-helper.php
@@ -15,22 +15,4 @@ class Request_Helper {
 	public function is_rest_request() {
 		return \defined( 'REST_REQUEST' ) && \REST_REQUEST === true;
 	}
-
-	/**
-	 * Returns a variable from the PHP superglobal query string, or `null` if
-	 * it does not exist or is not a string.
-	 *
-	 * @param string      $variable_name The name of the query variable.
-	 * @param string|null $default_value The default value if the query variable is undefined.
-	 *
-	 * @return string|null The value of the query variable.
-	 */
-	public function get_variable_from_query_string( $variable_name, $default_value = null ) {
-		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Reason: We are not processing form information.
-		if ( isset( $_GET[ $variable_name ] ) && \is_string( $_GET[ $variable_name ] ) ) {
-			// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Reason: We are not processing form information.
-			return \sanitize_text_field( \wp_unslash( $_GET[ $variable_name ] ) );
-		}
-		return $default_value;
-	}
 }
