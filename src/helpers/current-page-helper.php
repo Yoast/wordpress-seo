c0849628981f9d2704b8f663f239c2c6c5e470da
Enrico Battocchi <enrico.battocchi@gmail.com>
2023-02-20 11:42:24 +0100

    Revert "revert remove is_yoast_seo"
    This reverts commit e9bbf38fbc4fda011f53a962b8906f175b1e381b.

--- src/helpers/current-page-helper.php
+++ src/helpers/current-page-helper.php
@@ -412,18 +412,13 @@ class Current_Page_Helper {
 	 * @return bool True when current page is a yoast seo plugin page.
 	 */
 	public function is_yoast_seo_page() {
-		private $is_yoast_seo;
-
-		if ( $is_yoast_seo === null ) {
-			// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Reason: We are not processing form information.
-			if ( isset( $_GET['page'] ) && \is_string( $_GET['page'] ) ) {
-				// phpcs:ignore WordPress.Security.NonceVerification.Recommended, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized -- Reason: We are not processing form information, We are only using the variable in the strpos function.
-				$current_page = \wp_unslash( $_GET['page'] );
-				$is_yoast_seo = \strpos( $current_page, 'wpseo_' ) === 0;
-			}
+		// phpcs:ignore WordPress.Security.NonceVerification.Recommended -- Reason: We are not processing form information.
+		if ( isset( $_GET['page'] ) && \is_string( $_GET['page'] ) ) {
+			// phpcs:ignore WordPress.Security.NonceVerification.Recommended, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized -- Reason: We are not processing form information, We are only using the variable in the strpos function.
+			$current_page = \wp_unslash( $_GET['page'] );
+			return \strpos( $current_page, 'wpseo_' ) === 0;
 		}
-
-		return $is_yoast_seo;
+		return null;
 	}
 
 	/**
