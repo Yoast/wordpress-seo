698646a65019efab379d567d0707376bf0aa5e21
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2023-02-21 13:14:24 +0200

    Update packages/yoastseo/spec/languageProcessing/languages/de/helpers/checkIfWordIsFunctionSpec.js
    Co-authored-by: Aida Marfuaty <48715883+FAMarfuaty@users.noreply.github.com>

--- packages/yoastseo/spec/languageProcessing/languages/de/helpers/checkIfWordIsFunctionSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/de/helpers/checkIfWordIsFunctionSpec.js
@@ -1,15 +1,15 @@
 import checkIfWordIsFunction from "../../../../../src/languageProcessing/languages/de/helpers/checkIfWordIsFunction";
 
-describe( "a test checking if the word is function in German",  function() {
-	it( "return word if function if word in lower case", function() {
+describe( "a test for checking if the word is a function word in German",  function() {
+	it( "returns true if the word is a function word in lowercase", function() {
 		expect( checkIfWordIsFunction( "verhältnismäßig" ) ).toEqual( true );
 	} );
 
-	it( "return word if function if word in upper case", function() {
+	it( "returns true if the word is a function word in uppercase", function() {
 		expect( checkIfWordIsFunction( "Wahrscheinlichkeit" ) ).toEqual( true );
 	} );
 
-	it( "returns word is not funtion if word is not in the function words list", function() {
+	it( "returns false if the word is not found in the list", function() {
 		expect( checkIfWordIsFunction( "gebildeten" ) ).toEqual( false );
 	} );
 } );
