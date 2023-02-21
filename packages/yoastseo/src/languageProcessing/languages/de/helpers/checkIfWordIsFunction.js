5c8883471a09a671b3524a68e1207765c543a0ad
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2023-02-21 13:12:27 +0200

    Update packages/yoastseo/src/languageProcessing/languages/de/helpers/checkIfWordIsFunction.js
    Co-authored-by: Aida Marfuaty <48715883+FAMarfuaty@users.noreply.github.com>

--- packages/yoastseo/src/languageProcessing/languages/de/helpers/checkIfWordIsFunction.js
+++ packages/yoastseo/src/languageProcessing/languages/de/helpers/checkIfWordIsFunction.js
@@ -1,11 +1,11 @@
 import { all as functionWords } from "../config/functionWords";
 
 /**
- * Checks if a word is function.
+ * Checks if a word is a function word.
  *
  * @param {string} word The word to check.
  *
- * @returns {boolean} Whether or not a word is function.
+ * @returns {boolean} Whether or not a word is a function word.
  */
 export default function checkIfWordIsFunction( word ) {
 	// All words are converted to lower case before processing to avoid excluding function words that start with a capital letter.
