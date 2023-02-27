9aa3684a99338880cd1932a9e9022bbc0f1167b4
Hans-Christiaan <hans-christiaan@yoast.com>
2023-02-27 17:33:00 +0100

    Add factory method to request helper for getting the current request based on the current superglobals.

--- src/helpers/request-helper.php
+++ src/helpers/request-helper.php
@@ -2,6 +2,8 @@
 
 namespace Yoast\WP\SEO\Helpers;
 
+use YoastSEO_Vendor\Symfony\Component\HttpFoundation\Request;
+
 /**
  * A helper object for the request state.
  */
@@ -14,5 +16,14 @@ class Request_Helper {
 	 */
 	public function is_rest_request() {
 		return \defined( 'REST_REQUEST' ) && \REST_REQUEST === true;
+	}
+
+	/**
+	 * Get a Request object based on the current state of the PHP superglobals.
+	 *
+	 * @return Request The request, based on the current state of the PHP superglobals.
+	 */
+	public function get_current_request() {
+		return Request::createFromGlobals();
 	}
 }
