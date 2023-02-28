d49bfa49053050ba85a685a75481afcfcf6f880d
FAMarfuaty <aida@yoast.com>
2023-02-28 12:14:36 +0100

    Adjust assessor

--- packages/yoastseo/src/scoring/storePostsAndPages/contentAssessor.js
+++ packages/yoastseo/src/scoring/storePostsAndPages/contentAssessor.js
@@ -1,7 +1,7 @@
 import { map, sum } from "lodash-es";
 import { inherits } from "util";
 
-import { Assessor, assessments, helpers, interpreters } from "yoastseo";
+import { Assessor, ContentAssessor, assessments, helpers, interpreters } from "yoastseo";
 const { createAnchorOpeningTag } = helpers;
 const { scoreToRating } = interpreters;
 
@@ -21,11 +21,10 @@ const {
  */
 
 /**
- * Creates the Assessor
+ * Creates the Assessor for e-commerce posts and pages content types.
  *
  * @param {object}  researcher      The researcher to use for the analysis.
  * @param {Object}  options         The options for this assessor.
- * @param {Object}  options.marker  The marker to pass the list of marks to.
  *
  * @constructor
  */
@@ -65,134 +64,7 @@ const StorePostsAndPagesContentAssessor = function( researcher, options = {} ) {
 	];
 };
 
-inherits( StorePostsAndPagesContentAssessor, Assessor );
-
-/**
- * Calculates the weighted rating for languages that have all assessments based on a given rating.
- *
- * @param {number} rating The rating to be weighted.
- * @returns {number} The weighted rating.
- */
-StorePostsAndPagesContentAssessor.prototype.calculatePenaltyPointsFullSupport = function( rating ) {
-	switch ( rating ) {
-		case "bad":
-			return 3;
-		case "ok":
-			return 2;
-		default:
-		case "good":
-			return 0;
-	}
-};
-
-/**
- * Calculates the weighted rating for languages that don't have all assessments based on a given rating.
- *
- * @param {number} rating The rating to be weighted.
- * @returns {number} The weighted rating.
- */
-StorePostsAndPagesContentAssessor.prototype.calculatePenaltyPointsPartialSupport = function( rating ) {
-	switch ( rating ) {
-		case "bad":
-			return 4;
-		case "ok":
-			return 2;
-		default:
-		case "good":
-			return 0;
-	}
-};
-
-/**
- * Determines whether a language is fully supported. If a language supports 8 content assessments
- * it is fully supported
- *
- * @returns {boolean} True if fully supported.
- */
-StorePostsAndPagesContentAssessor.prototype._allAssessmentsSupported = function() {
-	const numberOfAssessments = this._assessments.length;
-	const applicableAssessments = this.getApplicableAssessments();
-	return applicableAssessments.length === numberOfAssessments;
-};
-
-/**
- * Calculates the penalty points based on the assessment results.
- *
- * @returns {number} The total penalty points for the results.
- */
-StorePostsAndPagesContentAssessor.prototype.calculatePenaltyPoints = function() {
-	const results = this.getValidResults();
-
-	const penaltyPoints = map( results, function( result ) {
-		const rating = scoreToRating( result.getScore() );
-
-		if ( this._allAssessmentsSupported() ) {
-			return this.calculatePenaltyPointsFullSupport( rating );
-		}
-
-		return this.calculatePenaltyPointsPartialSupport( rating );
-	}.bind( this ) );
-
-	return sum( penaltyPoints );
-};
-
-/**
- * Rates the penalty points
- *
- * @param {number} totalPenaltyPoints The amount of penalty points.
- * @returns {number} The score based on the amount of penalty points.
- *
- * @private
- */
-StorePostsAndPagesContentAssessor.prototype._ratePenaltyPoints = function( totalPenaltyPoints ) {
-	if ( this.getValidResults().length === 1 ) {
-		// If we have only 1 result, we only have a "no content" result
-		return 30;
-	}
-
-	if ( this._allAssessmentsSupported() ) {
-		// Determine the total score based on the total penalty points.
-		if ( totalPenaltyPoints > 6 ) {
-			// A red indicator.
-			return 30;
-		}
-
-		if ( totalPenaltyPoints > 4 ) {
-			// An orange indicator.
-			return 60;
-		}
-	} else {
-		if ( totalPenaltyPoints > 4 ) {
-			// A red indicator.
-			return 30;
-		}
-
-		if ( totalPenaltyPoints > 2 ) {
-			// An orange indicator.
-			return 60;
-		}
-	}
-	// A green indicator.
-	return 90;
-};
-
-/**
- * Calculates the overall score based on the assessment results.
- *
- * @returns {number} The overall score.
- */
-StorePostsAndPagesContentAssessor.prototype.calculateOverallScore = function() {
-	const results = this.getValidResults();
-
-	// If you have no content, you have a red indicator.
-	if ( results.length === 0 ) {
-		return 30;
-	}
-
-	const totalPenaltyPoints = this.calculatePenaltyPoints();
-
-	return this._ratePenaltyPoints( totalPenaltyPoints );
-};
+inherits( StorePostsAndPagesContentAssessor, ContentAssessor );
 
 export default StorePostsAndPagesContentAssessor;
 
