13f5be77e8fe7e7365e7d1d03c445f67832ea4f5
hdvos <hdvos93@gmail.com>
2023-03-13 13:13:07 +0100

    add filter helpers

---
+++ packages/yoastseo/src/parse/build/private/filterHelpers.js
@@ -0,0 +1,29 @@
+/**
+ * Filter helpers fort the html treefilter.
+ * Are all functions that return a callback with the element as only argument.
+ */
+
+/**
+ * Returns a function that returns true if the element has a specific name.
+ * @param {string} name The name to check.
+ * @returns {function(*)} A function that returns true if the element has a specific name.
+ */
+export function elementHasName( name ) {
+	return ( elementThing ) => {
+		return elementThing.name === name;
+	};
+}
+
+/**
+ * Creates a callback that checks the class of an element.
+ * @param {string} className The classname to filter out.
+ * @returns {function(*): boolean} A function that returns true if a Node has a certain class.
+ */
+export function elementHasClass( className ) {
+	return ( elementThing ) => {
+		if ( elementThing.attributes ) {
+			return elementThing.attributes.class === className;
+		}
+		return false;
+	};
+}
