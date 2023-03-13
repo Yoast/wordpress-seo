30b1448e67578fed10b9dd0507cdc25b22b339f3
hdvos <hdvos93@gmail.com>
2023-03-13 15:08:17 +0100

    add filters for our gutenberg blocks

--- packages/yoastseo/src/parse/build/private/alwaysFilterElements.js
+++ packages/yoastseo/src/parse/build/private/alwaysFilterElements.js
@@ -2,13 +2,16 @@
  * A config file that contains filters that should always apply.
  */
 
-import { elementHasName } from "./filterHelpers";
+import { elementHasName, elementHasClass, isEstimatedReadingtimetag } from "./filterHelpers";
 
 const permanentFilters = [
 	elementHasName( "script" ),
 	elementHasName( "style" ),
 	elementHasName( "code" ),
 	elementHasName( "blockquote" ),
+	// Filter yoast TOC blocks
+	elementHasClass( "wp-block-yoast-seo-table-of-contents yoast-table-of-contents" ),
+	isEstimatedReadingtimetag(),
 ];
 
 export default permanentFilters;
