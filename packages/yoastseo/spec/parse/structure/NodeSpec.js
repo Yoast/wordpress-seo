0f75540a86f140c7b8c6bd001f49b7667b67636d
hdvos <hdvos93@gmail.com>
2023-03-14 13:37:06 +0100

    add spec for node.js

---
+++ packages/yoastseo/spec/parse/structure/NodeSpec.js
@@ -0,0 +1,53 @@
+import Node from "../../../src/parse/structure/Node";
+import LanguageProcessor from "../../../src/parse/language/LanguageProcessor";
+import Factory from "../../specHelpers/factory";
+import build from "../../../src/parse/build/build";
+
+describe( "A test for the Node object", () => {
+	it( "should correctly create a simple node", function() {
+		expect( new Node( "name", {}, [] ) ).toEqual( { name: "name", attributes: {}, childNodes: [] } );
+	} );
+} );
+
+describe( "A test for the findAll method", () => {
+	it( "should find all occurrences of a p tag", function() {
+		const html = "<div><p class='yoast'>Hello, world! </p><p class='yoast'>Hello, yoast!</p></div>";
+
+		const researcher = Factory.buildMockResearcher( {} );
+		const languageProcessor = new LanguageProcessor( researcher );
+
+		const tree = build( html, languageProcessor );
+
+		const searchResult = tree.findAll( ( node ) => node.name === "p" );
+
+		const expected = [ {
+			name: "p",
+			attributes: { "class": "yoast" },
+			childNodes: [ { name: "#text", value: "Hello, world! " } ],
+			isImplicit: false,
+			sentences: [],
+		},
+		{
+			name: "p",
+			attributes: { "class": "yoast" },
+			childNodes: [ { name: "#text", value: "Hello, yoast!" } ],
+			isImplicit: false,
+			sentences: [],
+		} ];
+
+		expect( searchResult ).toEqual( expected );
+	} );
+} );
+
+describe( "A test for the innerText methods", () => {
+	const html = "<div><p class='yoast'>Hello, world! </p><p class='yoast'>Hello, yoast!</p></div>";
+
+	const researcher = Factory.buildMockResearcher( {} );
+	const languageProcessor = new LanguageProcessor( researcher );
+
+	const tree = build( html, languageProcessor );
+
+	const innerText = tree.innerText();
+
+	expect( innerText ).toEqual( "Hello, world! Hello, yoast!" );
+} );
