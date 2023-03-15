c6c3def84f3897e1c75fe876960984e5be6beb21
Thijs van der Heijden <thijsvanderheijden2@gmail.com>
2023-03-15 12:15:46 +0100

    Improves copy of notifications.

--- inc/class-addon-manager.php
+++ inc/class-addon-manager.php
@@ -367,7 +367,13 @@ class WPSEO_Addon_Manager {
 		if ( $subscription && $this->has_subscription_expired( $subscription ) ) {
 			echo '<br><br>';
 			/* translators: %1$s is the plugin name, %2$s and %3$s are a link. */
-			echo '<strong><span class="wp-ui-text-notification alert dashicons dashicons-warning"></span> ' . sprintf( esc_html__( 'A new version of %1$s is available. %2$sRenew your subscription%3$s if you want to update to the latest version.', 'wordpress-seo' ), esc_html( $plugin_data['name'] ), '<a href="' . esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/4ey' ) ) . '">', '</a>' ) . '</strong>';
+			echo '<strong><span class="wp-ui-text-notification warning dashicons dashicons-warning"></span> ' .
+				sprintf(
+					esc_html__( '%1$s can\'t be updated because your product subscription is expired. %2$sRenew your product subscription%3$s to get updates again and use all the features of %1$s.', 'wordpress-seo' ),
+					esc_html( $plugin_data['name'] ),
+					'<a href="' . esc_url( WPSEO_Shortlinker::get( 'https://yoa.st/4ey' ) ) . '">',
+					'</a>'
+				) . '</strong>';
 		}
 	}
 
@@ -465,10 +471,13 @@ class WPSEO_Addon_Manager {
 
 		return new Yoast_Notification(
 			sprintf(
-			/* translators: %1$s expands to the product name. %2$s expands to a link to My Yoast  */
-				__( 'You are not receiving updates or support! Fix this problem by adding this site and enabling %1$s for it in %2$s.', 'wordpress-seo' ),
+			/* translators: %1$s expands to a strong tag, %2$s expands to the product name, %3$s expands to a closing strong tag, %4$s expands to the product name. %5$s expands to activate your product subscription in MyYoast  */
+				__( '%1$s %2$s isn\'t working as expected %3$s and you are not receiving updates or support! Make sure to %4$s to unlock all the features of %5$s.', 'wordpress-seo' ),
+				'<strong>',
 				$product_name,
-				'<a href="' . WPSEO_Shortlinker::get( 'https://yoa.st/13j' ) . '" target="_blank">My Yoast</a>'
+				'</strong>',
+				'<a href="' . WPSEO_Shortlinker::get( 'https://yoa.st/13j' ) . '" target="_blank">activate your product subscription in MyYoast</a>',
+				$product_name,
 			),
 			$notification_options
 		);
