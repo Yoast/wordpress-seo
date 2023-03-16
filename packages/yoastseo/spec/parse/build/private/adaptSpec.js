7a441dc4c5ce50b76ca2c83d87c5bb182754edfa
hdvos <hdvos93@gmail.com>
2023-03-16 15:49:12 +0100

    add adaptspec

---
+++ packages/yoastseo/spec/parse/build/private/adaptSpec.js
@@ -0,0 +1,44 @@
+import adapt from "../../../../src/parse/build/private/adapt";
+import { parseFragment } from "parse5";
+import Node from "../../../../src/parse/structure/Node";
+import  Paragraph from  "../../../../src/parse/structure/Paragraph";
+import Text from "../../../../src/parse/structure/Text";
+import Heading from "../../../../src/parse/structure/Heading";
+
+describe( "The adapt function",
+	() => {
+		it( "adapts a basic div element", () => {
+			const html = "<div><p class='yoast'>Hello, world!</p></div>";
+			const tree = parseFragment( html );
+
+			const adaptedTree = adapt( tree );
+
+			const expected = { attributes: {}, childNodes: [ { attributes: {}, childNodes: [ { attributes: { "class": new Set( [ "yoast" ] ) }, childNodes: [ { name: "#text", value: "Hello, world!" } ], isImplicit: false, name: "p" } ], name: "div" } ], name: "#document-fragment" };
+
+
+			expect( adaptedTree ).toEqual( expected );
+		} );
+
+		it( "adapts a heading element", () => {
+			const html = "<h1>Hello World!</h1>";
+			const tree = parseFragment( html );
+
+			const adaptedTree = adapt( tree );
+
+			const expected = new Node( "#document-fragment", {}, [
+				new Heading( 1, {}, [
+					new Text( "Hello World!" ),
+				] ),
+			] );
+			expect( adaptedTree ).toEqual( expected );
+		} );
+
+		it( "should correctly adapt a node with no childnodes.", () => {
+			const tree = { nodeName: "div" };
+			const adaptedTree = adapt( tree );
+
+			const expected = { attributes: {}, childNodes: [], name: "div" };
+			expect( adaptedTree ).toEqual( expected );
+		} );
+	}
+);
