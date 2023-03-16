fcbbbaf1959570c79500c5ddd682df72863ed28e
hdvos <hdvos93@gmail.com>
2023-03-16 16:22:42 +0100

    add spec for parseClassAttribute

--- packages/yoastseo/src/parse/build/private/helpers/parseClassAttribute.js
+++ packages/yoastseo/src/parse/build/private/helpers/parseClassAttribute.js
@@ -1,9 +1,9 @@
 
 /**
- * 
- * @param {string} classString the value of the class attribute
- * @returns A set containing all individual values of the class.
+ * When splits a string of html classes into a set html classes.
+ * @param {string} classString The value of the class attribute.
+ * @returns {Set} A set containing all individual values of the class.
  */
-export default function parseClassAttribute( classString ){
-	return new Set( classString.split( ' ' ) );
+export default function parseClassAttribute( classString ) {
+	return new Set( classString.split( " " ) );
 }
