5c63f0a5a352c3281695d6bd9cfae233beb90b0d
hdvos <hdvos93@gmail.com>
2023-03-16 14:08:35 +0100

    add jsdoc to parseClassAttribute

--- packages/yoastseo/src/parse/build/private/helpers/parseClassAttribute.js
+++ packages/yoastseo/src/parse/build/private/helpers/parseClassAttribute.js
@@ -1,4 +1,9 @@
 
+/**
+ * 
+ * @param {string} classString the value of the class attribute
+ * @returns A set containing all individual values of the class.
+ */
 export default function parseClassAttribute( classString ){
 	return new Set( classString.split( ' ' ) );
 }
