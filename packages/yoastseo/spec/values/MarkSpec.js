ba516a24da72303f44cce1d7f466adf0210cee7e
Martijn van der Klis <martijnvanderklis@yoast.com>
2023-03-17 15:00:32 +0100

    Adds prerequisites for position-based highlighting to Mark

--- packages/yoastseo/spec/values/MarkSpec.js
+++ packages/yoastseo/spec/values/MarkSpec.js
@@ -2,35 +2,45 @@ import Mark from "../../src/values/Mark";
 
 describe( "a mark value object", function() {
 	it( "should have default properties", function() {
-		var mark = new Mark( {} );
+		const mark = new Mark( {} );
 
 		expect( mark.getOriginal() ).toBe( "" );
 		expect( mark.getMarked() ).toBe( "" );
 	} );
 
 	it( "should allow new value via the constructor", function() {
-		var mark = new Mark( { original: "original", marked: "marked", fieldsToMark: [ "heading" ] } );
+		const mark = new Mark( { original: "original", marked: "marked", fieldsToMark: [ "heading" ] } );
 
 		expect( mark.getOriginal() ).toBe( "original" );
 		expect( mark.getMarked() ).toBe( "marked" );
 		expect( mark.getFieldsToMark() ).toEqual( [ "heading" ] );
 	} );
 
-	describe( "a simple application function", function() {
+	describe( "replacement-based application", function() {
 		it( "should be able to apply itself by replacing text", function() {
-			var mark = new Mark( { original: "original", marked: "marked" } );
-			var text = "original";
-			var expected = "marked";
+			const mark = new Mark( { original: "original", marked: "marked" } );
+			const text = "original";
+			const expected = "marked";
 
 			expect( mark.applyWithReplace( text ) ).toBe( expected );
 		} );
 
 		it( "should only apply all occurences", function() {
-			var mark = new Mark( { original: "original", marked: "marked" } );
-			var text = "original original original original original original original";
-			var expected = "marked marked marked marked marked marked marked";
+			const mark = new Mark( { original: "original", marked: "marked" } );
+			const text = "original original original original original original original";
+			const expected = "marked marked marked marked marked marked marked";
 
 			expect( mark.applyWithReplace( text ) ).toBe( expected );
+		} );
+	} );
+
+	describe( "position-based application", function() {
+		it( "should be able to apply itself by replacing at given positions", function() {
+			const mark = new Mark( { position: { start: 3, end: 28 } } );
+			const text = "<p>Hello <span>World!</span></p>";
+			const expected = "<p><yoastmark class='yoast-text-mark'>Hello <span>World!</span></yoastmark></p>";
+
+			expect( mark.applyWithPosition( text ) ).toBe( expected );
 		} );
 	} );
 } );
--- packages/yoastseo/src/values/Mark.js
+++ packages/yoastseo/src/values/Mark.js
@@ -1,19 +1,26 @@
 import { defaults } from "lodash-es";
 
 /**
- * Represents a marked piece of text.
+ * Represents a place where highlighting should be applied.
+ * We allow both replacement-based highlighting (through providing `original`, `marked`, and potentially `fieldsToMark`) and
+ * position-based highlighting (through providing a `position`).
  *
- * @param {{marked: (string|*), original: Array<string[]>}} properties The properties of this Mark.
- * @param {string}   properties.original       The original text that should be marked.
- * @param {string}   properties.marked         The new text including marks.
- * @param {array}    properties.fieldsToMark   The array that contains the information on which text section to mark.
+ * @param {Object}   properties                The properties of this Mark.
+ *
+ * @param {string?}  properties.original       The original text that should be marked.
+ * @param {string?}  properties.marked         The new text including marks.
+ * @param {array?}   properties.fieldsToMark   The array that specifies which text section(s) to mark.
+ *
+ * @param {Object?}  properties.position       The position object.
+ * @param {int}      properties.position.start The start position of the HTML that should be marked.
+ * @param {int}      properties.position.end   The end position of the HTML that should be marked.
+ *
  * @constructor
  */
 function Mark( properties ) {
 	defaults( properties, { original: "", marked: "", fieldsToMark: [] } );
 	this._properties = properties;
 }
-
 
 /**
  * Returns the original text.
@@ -36,14 +43,32 @@ Mark.prototype.getMarked = function() {
 /**
  * Returns the fields to mark.
  *
- * @returns {array} The replaced text.
+ * @returns {array} The fields to mark.
  */
 Mark.prototype.getFieldsToMark = function() {
 	return this._properties.fieldsToMark;
 };
 
 /**
- * Applies this mark to the given text.
+ * Returns the start position.
+ *
+ * @returns {int} The start position.
+ */
+Mark.prototype.getPositionStart = function() {
+	return this._properties.position.start;
+};
+
+/**
+ * Returns the end position.
+ *
+ * @returns {int} The end position.
+ */
+Mark.prototype.getPositionEnd = function() {
+	return this._properties.position.end;
+};
+
+/**
+ * Applies this mark to the given text with replacement-based highlighting.
  *
  * @param {string} text The original text without the mark applied.
  * @returns {string} A new text with the mark applied to it.
@@ -51,6 +76,24 @@ Mark.prototype.getFieldsToMark = function() {
 Mark.prototype.applyWithReplace = function( text ) {
 	// (=^ ◡ ^=) Cute method to replace everything in a string without using regex.
 	return text.split( this._properties.original ).join( this._properties.marked );
+};
+
+/**
+ * Applies this mark to the given text with position-based highlighting.
+ *
+ * @param {string} text The original text without the mark applied.
+ * @returns {string} A new text with the mark applied to it.
+ */
+Mark.prototype.applyWithPosition = function( text ) {
+	const markStart = "<yoastmark class='yoast-text-mark'>";
+	const markEnd = "</yoastmark>";
+
+	const newEndOffset = this.getPositionEnd() + markStart.length;
+
+	text = text.substring( 0, this.getPositionStart() ) + markStart + text.substring( this.getPositionStart() );
+	text = text.substring( 0, newEndOffset ) + markEnd + text.substring( newEndOffset );
+
+	return text;
 };
 
 /**
