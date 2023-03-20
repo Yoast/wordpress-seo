71b2f6f9691ef4cdf8231a979bb3c3374efd9159
Paolo Luigi Scala <paolo@yoast.com>
2023-03-20 12:04:37 +0100

    Fix linting

--- packages/search-metadata-previews/src/snippet-preview/SnippetPreview.js
+++ packages/search-metadata-previews/src/snippet-preview/SnippetPreview.js
@@ -373,7 +373,7 @@ function highlightWords( locale, wordsToHighlight, text, cleanText ) {
  */
 const VerticalDots = ( { fillColor } ) => {
 	/* eslint-disable max-len */
-	return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={ fillColor } style= { { width: "18px" } }>
+	return <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill={ fillColor } style={ { width: "18px" } }>
 		<path d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2-2 .9-2 2 .9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z" />
 	</svg>;
 	/* eslint-enable max-len */
