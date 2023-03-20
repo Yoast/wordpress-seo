094f8f6049071133fb801db6d28e4bedcfcae1c3
Marina Koleva <marina@yoast.com>
2023-03-20 13:33:40 +0100

    adapting stripSpaces in helpers

--- packages/helpers/src/strings/stripSpaces.js
+++ packages/helpers/src/strings/stripSpaces.js
@@ -10,6 +10,9 @@ export default function( text ) {
 	// Replaces multiple spaces with a single space.
 	text = text.replace( /\s{2,}/g, " " );
 
+	// Replaces space(s) followed by a period, if the period is the last character, with only a period.
+	text = text.replace( /\s\.$/, "." );
+
 	// Removes first/last character if it's a space.
 	text = text.replace( /^\s+|\s+$/g, "" );
 
--- packages/yoastseo/src/languageProcessing/helpers/sanitize/stripSpaces.js
+++ packages/yoastseo/src/languageProcessing/helpers/sanitize/stripSpaces.js
@@ -10,7 +10,7 @@
 export default function( text ) {
 	// Replaces multiple spaces with a single space.
 	text = text.replace( /\s{2,}/g, " " );
-	// Replaces space(s) followed by a period, if the period is the last character.
+	// Replaces space(s) followed by a period, if the period is the last character, with only the period.
 	text = text.replace( /\s\.$/, "." );
 	// Removes first/last character if it's a space.
 	text = text.replace( /^\s+|\s+$/g, "" );
