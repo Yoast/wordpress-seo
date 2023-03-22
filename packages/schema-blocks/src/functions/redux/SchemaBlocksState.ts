7d535ccd50fcc129aee0799212b2102155051e8f
Igor <35524806+igorschoester@users.noreply.github.com>
2023-03-22 10:22:03 +0100

    Remove schema-blocks package

--- packages/schema-blocks/src/functions/redux/SchemaBlocksState.ts
+++
@@ -1,9 +0,0 @@
-import { BlockValidationResult } from "../../core/validation";
-
-export interface SchemaBlocksState {
-	validations: Record<string, BlockValidationResult>;
-}
-
-export const SchemaBlocksDefaultState: SchemaBlocksState = {
-	validations: {},
-};
