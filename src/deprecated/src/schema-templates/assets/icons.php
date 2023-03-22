9556b2101f918f7da4389de623c17d64112c1fc1
Enrico Battocchi <enrico.battocchi@gmail.com>
2023-03-22 12:33:50 +0100

    Missing backslash for global function

--- src/deprecated/src/schema-templates/assets/icons.php
+++ src/deprecated/src/schema-templates/assets/icons.php
@@ -39,7 +39,7 @@ class Icons {
 	 * @return string
 	 */
 	public static function heroicons_academic_cap( $size = self::SIZE_DEFAULT ) {
-		_deprecated_function( __METHOD__, 'Yoast SEO 20.5' );
+		\_deprecated_function( __METHOD__, 'Yoast SEO 20.5' );
 		return self::svg(
 			"<path fill='#fff' d='M12 14l9-5-9-5-9 5 9 5z' />"
 			. "<path fill='#fff' d='M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z' />"
