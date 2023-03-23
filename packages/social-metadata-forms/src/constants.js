e8404728bd7fd6021caa81f2f0333fdd3cdf61b7
Igor <35524806+igorschoester@users.noreply.github.com>
2023-03-23 13:24:29 +0100

    Move social-metadata functions to other package
    * Introduce JS deprecation logging

---
+++ packages/social-metadata-forms/src/constants.js
@@ -0,0 +1,18 @@
+export const TWITTER_IMAGE_SIZES = {
+	squareWidth: 125,
+	squareHeight: 125,
+	landscapeWidth: 506,
+	landscapeHeight: 265,
+	aspectRatio: ( 0.502 / 1 ) * 100,
+};
+
+export const FACEBOOK_IMAGE_SIZES = {
+	squareWidth: 158,
+	squareHeight: 158,
+	landscapeWidth: 527,
+	landscapeHeight: 273,
+	portraitWidth: 158,
+	portraitHeight: 237,
+	aspectRatio: ( 0.522 / 1 ) * 100,
+	largeThreshold: { width: 446, height: 233 },
+};
--- packages/social-metadata-forms/src/index.js
+++ packages/social-metadata-forms/src/index.js
@@ -1,6 +1,8 @@
 import * as actions from "./redux/actions/formActions";
 import selectorsFactory from "./redux/selectors/formSelectors";
 
+export { FACEBOOK_IMAGE_SIZES, TWITTER_IMAGE_SIZES } from "./constants";
+export { default as determineFacebookImageMode } from "./helpers/determineFacebookImageMode";
 export { socialReducer } from "./redux/reducers/index";
 export { default as SocialMetadataPreviewForm } from "./SocialMetadataPreviewForm";
 export  { actions };
