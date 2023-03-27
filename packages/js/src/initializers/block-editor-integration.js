f25f1baf5b2ed033e2d26de20166b68a9be832ab
Enrico Battocchi <enrico.battocchi@gmail.com>
2023-03-27 09:56:18 +0200

    Remove dismissible box in the pre-publish sidebar

--- packages/js/src/initializers/block-editor-integration.js
+++ packages/js/src/initializers/block-editor-integration.js
@@ -17,7 +17,6 @@ import { actions } from "@yoast/externals/redux";
 import { get } from "lodash-es";
 import initializeWordProofForBlockEditor from "../../../../vendor_prefixed/wordproof/wordpress-sdk/resources/js/initializers/blockEditor";
 import getL10nObject from "../analysis/getL10nObject";
-import JetpackBoost from "../components/JetpackBoost";
 import YoastIcon from "../components/PluginIcon";
 import MetaboxPortal from "../components/portals/MetaboxPortal";
 import SidebarSlot from "../components/slots/SidebarSlot";
@@ -149,7 +148,6 @@ function registerFills( store ) {
 			>
 				<PrePublish />
 			</PluginPrePublishPanel> }
-			<JetpackBoost alertKey="get-jetpack-boost-pre-publish-notification" />
 			<PluginPostPublishPanel
 				className="yoast-seo-sidebar-panel"
 				title={ __( "Yoast SEO", "wordpress-seo" ) }
