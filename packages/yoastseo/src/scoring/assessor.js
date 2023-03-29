cbda7424146833bf8ffeaf736e0fb5e635a0f185
Martijn van der Klis <martijnvanderklis@yoast.com>
2023-03-29 11:50:32 +0200

    Updated the specs for all related keyphrase assessors

--- packages/yoastseo/src/scoring/assessor.js
+++ packages/yoastseo/src/scoring/assessor.js
@@ -17,9 +17,9 @@ const ScoreRating = 9;
 /**
  * Creates the Assessor.
  *
- * @param {Object} researcher       The researcher to use in the assessor.
- * @param {Object} options          The options for this assessor.
- * @param {Object} options.marker   The marker to pass the list of marks to.
+ * @param {Researcher} researcher   The researcher to use in the assessor.
+ * @param {Object?} options         The options for this assessor.
+ * @param {Function} options.marker The marker to pass the list of marks to.
  *
  * @constructor
  */
@@ -34,7 +34,7 @@ const Assessor = function( researcher, options ) {
 /**
  * Checks if the researcher is defined and sets it.
  *
- * @param   {Object} researcher The researcher to use in the assessor.
+ * @param   {Researcher} researcher The researcher to use in the assessor.
  *
  * @throws  {MissingArgument} Parameter needs to be a valid researcher object.
  * @returns {void}
@@ -55,7 +55,7 @@ Assessor.prototype.getAvailableAssessments = function() {
 };
 
 /**
- * Checks whether or not the Assessment is applicable.
+ * Checks whether the Assessment is applicable.
  *
  * @param {Object} assessment The Assessment object that needs to be checked.
  * @param {Paper} paper The Paper object to check against.
@@ -71,7 +71,7 @@ Assessor.prototype.isApplicable = function( assessment, paper, researcher ) {
 };
 
 /**
- * Determines whether or not an assessment has a marker.
+ * Determines whether an assessment has a marker.
  *
  * @param {Object} assessment The assessment to check for.
  * @returns {boolean} Whether or not the assessment has a marker.
@@ -107,7 +107,7 @@ Assessor.prototype.getPaper = function() {
  * @returns {Function} A function that can mark the given paper according to the given assessment.
  */
 Assessor.prototype.getMarker = function( assessment, paper, researcher ) {
-	var specificMarker = this._options.marker;
+	const specificMarker = this._options.marker;
 
 	return function() {
 		let marks = assessment.getMarks( paper, researcher );
@@ -118,7 +118,7 @@ Assessor.prototype.getMarker = function( assessment, paper, researcher ) {
 };
 
 /**
- * Runs the researches defined in the tasklist or the default researches.
+ * Runs the researches defined in the task list or the default researches.
  *
  * @param {Paper} paper The paper to run assessments on.
  * @returns {void}
@@ -126,7 +126,7 @@ Assessor.prototype.getMarker = function( assessment, paper, researcher ) {
 Assessor.prototype.assess = function( paper ) {
 	this._researcher.setPaper( paper );
 
-	var assessments = this.getAvailableAssessments();
+	let assessments = this.getAvailableAssessments();
 	this.results = [];
 
 	assessments = filter( assessments, function( assessment ) {
@@ -167,7 +167,7 @@ Assessor.prototype.hasMarkers = function() {
  * @returns {AssessmentResult} The result of the assessment.
  */
 Assessor.prototype.executeAssessment = function( paper, researcher, assessment ) {
-	var result;
+	let result;
 
 	try {
 		result = assessment.getResult( paper, researcher );
@@ -200,7 +200,7 @@ Assessor.prototype.executeAssessment = function( paper, researcher, assessment )
 };
 
 /**
- * Filters out all assessmentresults that have no score and no text.
+ * Filters out all assessment results that have no score and no text.
  *
  * @returns {Array<AssessmentResult>} The array with all the valid assessments.
  */
@@ -221,14 +221,14 @@ Assessor.prototype.isValidResult = function( assessmentResult ) {
 };
 
 /**
- * Returns the overallscore. Calculates the totalscore by adding all scores and dividing these
+ * Returns the overall score. Calculates the total score by adding all scores and dividing these
  * by the number of results times the ScoreRating.
  *
- * @returns {number} The overallscore
+ * @returns {number} The overall score.
  */
 Assessor.prototype.calculateOverallScore  = function() {
-	var results = this.getValidResults();
-	var totalScore = 0;
+	const results = this.getValidResults();
+	let totalScore = 0;
 
 	forEach( results, function( assessmentResult ) {
 		totalScore += assessmentResult.getScore();
@@ -261,7 +261,7 @@ Assessor.prototype.addAssessment = function( name, assessment ) {
  * @returns {void}
  */
 Assessor.prototype.removeAssessment = function( name ) {
-	var toDelete = findIndex( this._assessments, function( assessment ) {
+	const toDelete = findIndex( this._assessments, function( assessment ) {
 		return assessment.hasOwnProperty( "identifier" ) && name === assessment.identifier;
 	} );
 
@@ -288,7 +288,7 @@ Assessor.prototype.getAssessment = function( identifier ) {
  * @returns {Array} The array with applicable assessments.
  */
 Assessor.prototype.getApplicableAssessments = function() {
-	var availableAssessments = this.getAvailableAssessments();
+	const availableAssessments = this.getAvailableAssessments();
 	return filter(
 		availableAssessments,
 		function( availableAssessment ) {
