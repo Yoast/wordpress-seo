165806ae19ae21100e3f93a1757417d1ffb38bf0
Agnieszka Szuba <agnieszka@yoast.com>
2023-03-29 14:03:26 +0200

    Don't get sentence positions if there are no sentences in the node

--- packages/yoastseo/src/parse/build/private/tokenize.js
+++ packages/yoastseo/src/parse/build/private/tokenize.js
@@ -27,7 +27,9 @@ function getSentences( node, languageProcessor ) {
 	// Split text into sentences.
 	let sentences = languageProcessor.splitIntoSentences( node.innerText() );
 	// Add position information to the sentences.
-	sentences = getSentencePositions( node, sentences );
+	if ( sentences.length > 0 ) {
+		sentences = getSentencePositions( node, sentences );
+	}
 	// Tokenize sentences into tokens.
 	return sentences.map( sentence => getTokens( sentence, languageProcessor.splitIntoTokens ) );
 }
