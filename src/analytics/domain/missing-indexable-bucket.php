6d444ba835c5315bfc2e062d3d9dac0c2840a753
Thijs van der Heijden <thijsvanderheijden2@gmail.com>
2023-03-31 15:20:58 +0200

    Adds collector to gather missing indexables for easy indexing action.

---
+++ src/analytics/domain/missing-indexable-bucket.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Yoast\WP\SEO\Analytics\Domain;
+
+/**
+ * A collection domain object.
+ */
+class Missing_Indexable_Bucket {
+
+	/**
+	 * @var array<Missing_Indexable_Count> $missing_indexble_counts all the missing
+	 *                                                                                              indexable count
+	 *                                                                                              objects.
+	 */
+	private $missing_indexble_counts;
+
+	/**
+	 * The constructor.
+	 */
+	public function __construct() {
+		$this->missing_indexble_counts = []; }
+
+	/**
+	 * Adds a missing indexable count object to this bucket.
+	 *
+	 * @param Missing_Indexable_Count $missing_indexable_count The missing indexable count object.
+	 *
+	 * @return void
+	 */
+	public function add_missing_indexable_count( Missing_Indexable_Count $missing_indexable_count ): void {
+		$this->missing_indexble_counts[] = $missing_indexable_count;
+	}
+
+	/**
+	 * Returns the array representation of all indexable counts.
+	 *
+	 * @return array
+	 */
+	public function to_array(): array {
+		return array_map(
+			function ( $item ) {
+				return $item->to_array();
+			},
+			$this->missing_indexble_counts
+		);
+	}
+}
+
+
--- src/analytics/framework/missing-indexables-collector.php
+++ src/analytics/framework/missing-indexables-collector.php
@@ -1,23 +1,44 @@
 <?php
 
-namespace Yoast\WP\SEO\Analystics\Framework;
+namespace Yoast\WP\SEO\Analytics\Framework;
 
 use Yoast\WP\SEO\Actions\Indexing\Abstract_Indexing_Action;
+use Yoast\WP\SEO\Analytics\Domain\Missing_Indexable_Bucket;
+use Yoast\WP\SEO\Analytics\Domain\Missing_Indexable_Count;
 
+/**
+ * Manages the collection of the missing indexable data.
+ */
 class Missing_Indexables_Collector implements \WPSEO_Collection {
 
 	/**
-	 * @var array<Abstract_Indexing_Action> $indexation_actions all the indexation actions.
+	 * All the indexation actions.
+	 *
+	 * @var array<Abstract_Indexing_Action> $indexation_actions
 	 */
 	private $indexation_actions;
 
-	public function __construct( Abstract_Indexing_Action  ...$indexation_actions ) {
+	/**
+	 * The collector constructor.
+	 *
+	 * @param \Yoast\WP\SEO\Actions\Indexing\Abstract_Indexing_Action ...$indexation_actions All the Indexation actions.
+	 */
+	public function __construct( Abstract_Indexing_Action ...$indexation_actions ) {
 		$this->indexation_actions = $indexation_actions;
 	}
 
+	/**
+	 * Gets the data for the tracking collector.
+	 *
+	 * @return array The list of missing indexables.
+	 */
 	public function get() {
+		$missing_indexable_bucket = new Missing_Indexable_Bucket();
+		foreach ( $this->indexation_actions as $indexation_action ) {
+			$missing_indexable_count = new Missing_Indexable_Count( $indexation_action::UNINDEXED_COUNT_TRANSIENT, $indexation_action->get_total_unindexed() );
+			$missing_indexable_bucket->add_missing_indexable_count( $missing_indexable_count );
+		}
 
+		return $missing_indexable_bucket->to_array();
 	}
-
-
 }
