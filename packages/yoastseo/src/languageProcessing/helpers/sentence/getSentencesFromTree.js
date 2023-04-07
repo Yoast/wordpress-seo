ca068f4e5133f7b408ae775f89803a3295f3c926
FAMarfuaty <aida@yoast.com>
2023-04-07 17:18:43 +0200

    Create a helper to get sentences from tree nodes

---
+++ packages/yoastseo/src/languageProcessing/helpers/sentence/getSentencesFromTree.js
@@ -0,0 +1,17 @@
+import { flatten } from "lodash-es";
+import { Paragraph, Heading } from "../../../parse/structure";
+
+/**
+ * Gets all the sentences from paragraph and heading nodes.
+ * These two node types are the nodes that should contain sentences for the analysis.
+ *
+ * @param {Paper} paper The paper to get the sentences from.
+ *
+ * @returns {Object[]} The array of sentences retrieved from paragraph and heading nodes.
+ */
+export default function( paper ) {
+	const tree = paper.getTree().findAll( treeNode => treeNode instanceof Paragraph || treeNode instanceof Heading );
+
+	const sentences = tree.map( node => node.sentences );
+	return flatten( sentences );
+}
