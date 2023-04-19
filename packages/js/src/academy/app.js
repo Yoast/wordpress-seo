bfc954b6b0b9893ecaf4a2333847480358b7d83e
Vraja Das <vraja.pro@gmail.com>
2023-04-19 16:55:02 +0200

    show badge of premium course when it's a premium course and premium is activated.

--- packages/js/src/academy/app.js
+++ packages/js/src/academy/app.js
@@ -222,7 +222,7 @@ const App = () => {
 										decoding="async"
 									/>
 
-									{ isPremium && <div className="yst-absolute yst-top-2 yst-right-2 yst-flex yst-gap-1.5">
+									{ course.isPremium && isPremium && <div className="yst-absolute yst-top-2 yst-right-2 yst-flex yst-gap-1.5">
 										 <Badge size="small" variant="upsell">{ __( "Premium", "wordpress-seo" ) }</Badge>
 									</div> }
 
