3e1b777b286de6a4cac973d50432571db1b20dc3
Agnieszka Szuba <agnieszka@yoast.com>
2023-04-20 16:35:57 +0200

    Don't consider code and script elements phrasing content

--- packages/yoastseo/src/parse/build/private/isPhrasingContent.js
+++ packages/yoastseo/src/parse/build/private/isPhrasingContent.js
@@ -7,8 +7,8 @@
  * @type {string[]}
  */
 const phrasingContentTags = [
-	"b", "big", "i", "small", "tt", "abbr", "acronym", "cite", "code", "dfn", "em", "kbd", "strong",
-	"samp", "time", "var", "a", "bdo", "br", "img", "map", "object", "q", "script", "span", "sub", "sup", "button",
+	"b", "big", "i", "small", "tt", "abbr", "acronym", "cite", "dfn", "em", "kbd", "strong",
+	"samp", "time", "var", "a", "bdo", "br", "img", "map", "object", "q", "span", "sub", "sup", "button",
 	"input", "label", "select", "textarea",
 ];
 
