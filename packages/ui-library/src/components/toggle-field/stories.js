67715dc677a73dd4b611f299f58514e5b5e7a00c
dennis <dennis@yoast.com>
2023-04-21 14:17:54 +0200

    UI library documentation setup for ToggleField

--- packages/ui-library/src/components/toggle-field/stories.js
+++ packages/ui-library/src/components/toggle-field/stories.js
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "@wordpress/element";
 import { StoryComponent } from ".";
 import Badge from "../../elements/badge";
+import { component, withLabelAndDescription, checked, withLabelSuffix } from "./docs";
 
 export default {
 	title: "2) Components/Toggle field",
@@ -13,7 +14,7 @@ export default {
 	parameters: {
 		docs: {
 			description: {
-				component: "A simple toggle field component.",
+				component,
 			},
 		},
 	},
@@ -46,6 +47,7 @@ WithLabelAndDescription.args = {
 	label: "Toggle field with a label that spans multiple lines is still centered nicely with the toggle",
 	children: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a nisi egestas, accumsan ante quis, accumsan nisi. Duis lacinia pharetra luctus. Aliquam nisi orci, mattis quis lacus tristique, tempus pulvinar lectus. Nam rutrum vitae arcu at ullamcorper. Sed in felis blandit, consectetur nulla eu, congue justo. Suspendisse a augue a arcu lacinia tristique. Integer finibus dui sit amet pulvinar placerat. Phasellus a erat nec odio aliquet maximus id viverra nunc. Aliquam finibus malesuada est id dapibus. Curabitur suscipit lorem vitae sodales malesuada.",
 };
+WithLabelAndDescription.parameters = { docs: { description: { story: withLabelAndDescription } } };
 
 export const Checked = Template.bind( {} );
 Checked.args = {
@@ -54,6 +56,7 @@ Checked.args = {
 	checked: true,
 	label: "Checked toggle field",
 };
+Checked.parameters = { docs: { description: { story: checked } } };
 
 export const WithLabelSuffix = Template.bind( {} );
 WithLabelSuffix.storyName = "With label suffix";
@@ -64,3 +67,4 @@ WithLabelSuffix.args = {
 	label: "Label suffix toggle field",
 	labelSuffix: <Badge className="yst-ml-1.5" variant="upsell">Premium</Badge>,
 };
+WithLabelSuffix.parameters = { docs: { description: { story: withLabelSuffix } } };
