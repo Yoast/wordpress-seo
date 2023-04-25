6b9cbd5bfbb69a295b9b00f76539936efe533ea7
Hanna Worku <hanna@yoast.com>
2023-04-25 11:33:14 -0700

    remove text area element from content analysis

--- packages/yoastseo/src/languageProcessing/helpers/html/htmlParser.js
+++ packages/yoastseo/src/languageProcessing/helpers/html/htmlParser.js
@@ -10,7 +10,7 @@ let textArray;
 let inIgnorableBlock = false;
 
 // The blocks we filter out of the text that needs to be parsed.
-const ignoredTags = [ "script", "style", "code", "pre", "blockquote" ];
+const ignoredTags = [ "script", "style", "code", "pre", "blockquote", "textarea" ];
 
 /**
  * Parses the text.
