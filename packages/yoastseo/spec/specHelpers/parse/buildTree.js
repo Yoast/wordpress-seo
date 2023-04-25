498dd6d07a8597a5fdd7df766e94d8ea5696d9f0
hdvos <hdvos93@gmail.com>
2023-04-25 08:10:03 +0200

    Add helper for building tree without tokenizing in buildTree.js

--- packages/yoastseo/spec/specHelpers/parse/buildTree.js
+++ packages/yoastseo/spec/specHelpers/parse/buildTree.js
@@ -1,5 +1,9 @@
 import LanguageProcessor from "../../../src/parse/language/LanguageProcessor";
 import { build } from "../../../src/parse/build";
+import adapt from "../../../src/parse/build/private/adapt";
+import { parseFragment } from "parse5";
+import filterTree from "../../../src/parse/build/private/filterTree";
+import permanentFilters from "../../../src/parse/build/private/alwaysFilterElements";
 
 /**
  * Builds an HTML tree for a given paper and researcher, and adds it to the paper.
@@ -11,4 +15,27 @@ import { build } from "../../../src/parse/build";
 export default function buildTree( paper, researcher ) {
 	const languageProcessor = new LanguageProcessor( researcher );
 	paper.setTree( build( paper.getText(), languageProcessor ) );
+}
+
+/**
+ * Parses the HTML string to a tree representation of
+ * the HTML document.
+ *
+ * @param {string} htmlString The HTML string.
+ *
+ * @returns {Node} The tree representation of the HTML string.
+ */
+function buildWithoutTokenize( htmlString ) {
+	let tree = adapt( parseFragment( htmlString, { sourceCodeLocationInfo: true } ) );
+	tree = filterTree( tree, permanentFilters );
+	return tree;
+}
+
+/**
+ * Builds an HTML tree for a given paper and researcher, and adds it to the paper.
+ * @param {Paper} paper The paper to which the tree will be added.
+ * @returns {void}
+ */
+export function buildTreeNoTokenize( paper ) {
+	paper.setTree( buildWithoutTokenize( paper.getText() ) );
 }
