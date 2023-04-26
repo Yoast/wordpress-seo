60d5220f77d7c6f64db54959f25b0334a9f1b448
Vraja Das <vraja.pro@gmail.com>
2023-04-26 09:44:56 +0200

    refactor data provider

--- tests/unit/integrations/watchers/indexable-attachment-watcher-test.php
+++ tests/unit/integrations/watchers/indexable-attachment-watcher-test.php
@@ -102,104 +102,88 @@ class Indexable_Attachment_Watcher_Test extends TestCase {
 	 */
 	public function check_option_provider() {
 		return [
-			[
-				[
-					'old_value'                => 1,
-					'new_value'                => 2,
-					'delete_transient_times'   => 0,
-					'set_reason_times'         => 0,
-					'attachment_cleanup_times' => 0,
-					'wp_next_scheduled'        => null,
-					'schedule_event_times'     => 0,
-				],
+
+			'Old and new values are not arrays' => [
+				'old_value'                => 1,
+				'new_value'                => 2,
+				'delete_transient_times'   => 0,
+				'set_reason_times'         => 0,
+				'attachment_cleanup_times' => 0,
+				'wp_next_scheduled'        => null,
+				'schedule_event_times'     => 0,
 			],
-			[
-				[
-					'old_value'                => [],
-					'new_value'                => [],
-					'delete_transient_times'   => 0,
-					'set_reason_times'         => 0,
-					'attachment_cleanup_times' => 0,
-					'wp_next_scheduled'        => null,
-					'schedule_event_times'     => 0,
-				],
+
+			'Old and new values are empty arrays' => [
+				'old_value'                => [],
+				'new_value'                => [],
+				'delete_transient_times'   => 0,
+				'set_reason_times'         => 0,
+				'attachment_cleanup_times' => 0,
+				'wp_next_scheduled'        => null,
+				'schedule_event_times'     => 0,
 			],
-			[
-				[
-					'old_value'                => [],
-					'new_value'                => [ 'test' => 'test' ],
-					'delete_transient_times'   => 0,
-					'set_reason_times'         => 0,
-					'attachment_cleanup_times' => 0,
-					'wp_next_scheduled'        => null,
-					'schedule_event_times'     => 0,
-				],
+			'Old value is empty array and new doesnt have disable-attachment key' => [
+				'old_value'                => [],
+				'new_value'                => [ 'test' => 'test' ],
+				'delete_transient_times'   => 0,
+				'set_reason_times'         => 0,
+				'attachment_cleanup_times' => 0,
+				'wp_next_scheduled'        => null,
+				'schedule_event_times'     => 0,
 			],
-			[
-				[
-					'old_value'                => [ 'test' => 'test' ],
-					'new_value'                => [],
-					'delete_transient_times'   => 0,
-					'set_reason_times'         => 0,
-					'attachment_cleanup_times' => 0,
-					'wp_next_scheduled'        => null,
-					'schedule_event_times'     => 0,
-				],
+			'Old value doesnt have disable-attachment key and new value is empty array' => [
+				'old_value'                => [ 'test' => 'test' ],
+				'new_value'                => [],
+				'delete_transient_times'   => 0,
+				'set_reason_times'         => 0,
+				'attachment_cleanup_times' => 0,
+				'wp_next_scheduled'        => null,
+				'schedule_event_times'     => 0,
 			],
-			[
-				[
-					'old_value'                => [ 'test' => 'test' ],
-					'new_value'                => [ 'test' => 'test' ],
-					'delete_transient_times'   => 0,
-					'set_reason_times'         => 0,
-					'attachment_cleanup_times' => 0,
-					'wp_next_scheduled'        => null,
-					'schedule_event_times'     => 0,
-				],
+			'Old and new values doesnt have disable-attachment key' => [
+				'old_value'                => [ 'test' => 'test' ],
+				'new_value'                => [ 'test' => 'test' ],
+				'delete_transient_times'   => 0,
+				'set_reason_times'         => 0,
+				'attachment_cleanup_times' => 0,
+				'wp_next_scheduled'        => null,
+				'schedule_event_times'     => 0,
 			],
-			[
-				[
-					'old_value'                => false,
-					'new_value'                => [ 'test' => 'test' ],
-					'delete_transient_times'   => 0,
-					'set_reason_times'         => 0,
-					'attachment_cleanup_times' => 0,
-					'wp_next_scheduled'        => null,
-					'schedule_event_times'     => 0,
-				],
+			'Old value is false and new value doesnt have disable-attachment key' => [
+				'old_value'                => false,
+				'new_value'                => [ 'test' => 'test' ],
+				'delete_transient_times'   => 0,
+				'set_reason_times'         => 0,
+				'attachment_cleanup_times' => 0,
+				'wp_next_scheduled'        => null,
+				'schedule_event_times'     => 0,
 			],
-			[
-				[
-					'old_value'                => [ 'disable-attachment' => true ],
-					'new_value'                => [ 'disable-attachment' => false ],
-					'delete_transient_times'   => 1,
-					'set_reason_times'         => 1,
-					'attachment_cleanup_times' => 0,
-					'wp_next_scheduled'        => null,
-					'schedule_event_times'     => 0,
-				],
+			'Old and new values have disable-attachment key, old true, new false' => [
+				'old_value'                => [ 'disable-attachment' => true ],
+				'new_value'                => [ 'disable-attachment' => false ],
+				'delete_transient_times'   => 1,
+				'set_reason_times'         => 1,
+				'attachment_cleanup_times' => 0,
+				'wp_next_scheduled'        => null,
+				'schedule_event_times'     => 0,
 			],
-			[
-				[
-					'old_value'                => [ 'disable-attachment' => false ],
-					'new_value'                => [ 'disable-attachment' => true ],
-					'delete_transient_times'   => 1,
-					'set_reason_times'         => 0,
-					'attachment_cleanup_times' => 1,
-					'wp_next_scheduled'        => true,
-					'schedule_event_times'     => 0,
-				],
+			'Old and new values have disable-attachment key, old false, new true' => [
+				'old_value'                => [ 'disable-attachment' => false ],
+				'new_value'                => [ 'disable-attachment' => true ],
+				'delete_transient_times'   => 1,
+				'set_reason_times'         => 0,
+				'attachment_cleanup_times' => 1,
+				'wp_next_scheduled'        => true,
+				'schedule_event_times'     => 0,
 			],
-			[
-				[
-					'old_value'                => [ 'disable-attachment' => false ],
-					'new_value'                => [ 'disable-attachment' => true ],
-					'delete_transient_times'   => 1,
-					'set_reason_times'         => 0,
-					'attachment_cleanup_times' => 1,
-					'wp_next_scheduled'        => false,
-					'schedule_event_times'     => 1,
-				],
+			'Old and new values have disable-attachment key, old false, new true' => [
+				'old_value'                => [ 'disable-attachment' => false ],
+				'new_value'                => [ 'disable-attachment' => true ],
+				'delete_transient_times'   => 1,
+				'set_reason_times'         => 0,
+				'attachment_cleanup_times' => 1,
+				'wp_next_scheduled'        => false,
+				'schedule_event_times'     => 1,
 			],
 		];
 	}
@@ -211,42 +195,48 @@ class Indexable_Attachment_Watcher_Test extends TestCase {
 	 *
 	 * @dataProvider check_option_provider
 	 *
-	 * @param array $expected An array of expected results.
+	 * @param mixed $old_value                Old value.
+	 * @param mixed $new_value                New value.
+	 * @param int   $delete_transient_times   Number of times delete_transient should be called.
+	 * @param int   $set_reason_times         Number of times set_reason should be called.
+	 * @param int   $attachment_cleanup_times Number of times attachment_cleanup should be called.
+	 * @param mixed $wp_next_scheduled        Value of wp_next_scheduled.
+	 * @param int   $schedule_event_times     Number of times schedule_event should be called.
 	 */
-	public function test_check_option( $expected ) {
+	public function test_check_option( $old_value, $new_value, $delete_transient_times, $set_reason_times, $attachment_cleanup_times, $wp_next_scheduled, $schedule_event_times ) {
 
 		Monkey\Functions\expect( 'delete_transient' )
 			->with( Indexable_Post_Indexation_Action::UNINDEXED_COUNT_TRANSIENT )
-			->times( $expected['delete_transient_times'] );
+			->times( $delete_transient_times );
 
 		Monkey\Functions\expect( 'delete_transient' )
 			->with( Indexable_Post_Indexation_Action::UNINDEXED_LIMITED_COUNT_TRANSIENT )
-			->times( $expected['delete_transient_times'] );
+			->times( $delete_transient_times );
 
 		$this->indexing_helper
 			->expects( 'set_reason' )
 			->with( Indexing_Reasons::REASON_ATTACHMENTS_MADE_ENABLED )
-			->times( $expected['set_reason_times'] );
+			->times( $set_reason_times );
 
 		$this->attachment_cleanup
 			->expects( 'remove_attachment_indexables' )
 			->with( false )
-			->times( $expected['attachment_cleanup_times'] );
+			->times( $attachment_cleanup_times );
 
 		$this->attachment_cleanup
 			->expects( 'clean_attachment_links_from_target_indexable_ids' )
 			->with( false )
-			->times( $expected['attachment_cleanup_times'] );
+			->times( $attachment_cleanup_times );
 
 		Monkey\Functions\expect( 'wp_next_scheduled' )
 			->with( Cleanup_Integration::START_HOOK )
-			->times( $expected['attachment_cleanup_times'] )
-			->andReturn( $expected['wp_next_scheduled'] );
+			->times( $attachment_cleanup_times )
+			->andReturn( $wp_next_scheduled );
 
 			Monkey\Functions\expect( 'wp_schedule_single_event' )
 				->with( ( time() + ( \MINUTE_IN_SECONDS * 5 ) ), Cleanup_Integration::START_HOOK )
-				->times( $expected['schedule_event_times'] );
+				->times( $schedule_event_times );
 
-		$this->instance->check_option( $expected['old_value'], $expected['new_value'] );
+		$this->instance->check_option( $old_value, $new_value );
 	}
 }
