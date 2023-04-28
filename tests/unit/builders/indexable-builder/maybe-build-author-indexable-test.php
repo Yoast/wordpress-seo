e4e484884f9022cadefd30e4271f0494195d97ea
Vraja Das <vraja.pro@gmail.com>
2023-04-28 13:44:15 +0200

    Implemented indexable builder setup for maybe-build-author-indexable test

--- tests/unit/builders/indexable-builder/maybe-build-author-indexable-test.php
+++ tests/unit/builders/indexable-builder/maybe-build-author-indexable-test.php
@@ -3,25 +3,10 @@
 namespace Yoast\WP\SEO\Tests\Unit\Builders\Indexable_Builder;
 
 use Mockery;
-use Yoast\WP\SEO\Builders\Indexable_Author_Builder;
-use Yoast\WP\SEO\Builders\Indexable_Builder;
-use Yoast\WP\SEO\Builders\Indexable_Date_Archive_Builder;
-use Yoast\WP\SEO\Builders\Indexable_Hierarchy_Builder;
-use Yoast\WP\SEO\Builders\Indexable_Home_Page_Builder;
-use Yoast\WP\SEO\Builders\Indexable_Link_Builder;
-use Yoast\WP\SEO\Builders\Indexable_Post_Builder;
-use Yoast\WP\SEO\Builders\Indexable_Post_Type_Archive_Builder;
-use Yoast\WP\SEO\Builders\Indexable_System_Page_Builder;
-use Yoast\WP\SEO\Builders\Indexable_Term_Builder;
-use Yoast\WP\SEO\Builders\Primary_Term_Builder;
-use Yoast\WP\SEO\Helpers\Indexable_Helper;
 use Yoast\WP\SEO\Models\Indexable;
-use Yoast\WP\SEO\Repositories\Indexable_Repository;
-use Yoast\WP\SEO\Services\Indexables\Indexable_Version_Manager;
 use Yoast\WP\SEO\Tests\Unit\Doubles\Builders\Indexable_Builder_Double;
 use Yoast\WP\SEO\Tests\Unit\Doubles\Models\Indexable_Mock;
-use Yoast\WP\SEO\Tests\Unit\TestCase;
-
+use Yoast\WP\SEO\Tests\Unit\Builders\Indexable_Builder\Indexable_Builder_Setup;
 
 /**
  * Class Maybe_Build_Author_Indexable_Test.
@@ -31,138 +16,13 @@ use Yoast\WP\SEO\Tests\Unit\TestCase;
  *
  * @coversDefaultClass \Yoast\WP\SEO\Builders\Indexable_Builder
  */
-class Maybe_Build_Author_Indexable_Test extends TestCase {
-
-	/**
-	 * Represents the author builder.
-	 *
-	 * @var Mockery\MockInterface|Indexable_Author_Builder
-	 */
-	protected $author_builder;
-
-	/**
-	 * Represents the post builder.
-	 *
-	 * @var Mockery\MockInterface|Indexable_Post_Builder
-	 */
-	protected $post_builder;
-
-	/**
-	 * Represents the term builder.
-	 *
-	 * @var Mockery\MockInterface|Indexable_Term_Builder
-	 */
-	protected $term_builder;
-
-	/**
-	 * Represents the homepage builder.
-	 *
-	 * @var Mockery\MockInterface|Indexable_Home_Page_Builder
-	 */
-	protected $home_page_builder;
-
-	/**
-	 * Represents the post type archive builder.
-	 *
-	 * @var Mockery\MockInterface|Indexable_Post_Type_Archive_Builder
-	 */
-	protected $post_type_archive_builder;
-
-	/**
-	 * Represents the date archive builder.
-	 *
-	 * @var Mockery\MockInterface|Indexable_Date_Archive_Builder
-	 */
-	protected $date_archive_builder;
-
-	/**
-	 * Represents the system page builder.
-	 *
-	 * @var Mockery\MockInterface|Indexable_System_Page_Builder
-	 */
-	protected $system_page_builder;
-
-	/**
-	 * Represents the hierarchy builder.
-	 *
-	 * @var Mockery\MockInterface|Indexable_Hierarchy_Builder
-	 */
-	protected $hierarchy_builder;
-
-	/**
-	 * Represents the primary term builder.
-	 *
-	 * @var Mockery\MockInterface|Primary_Term_Builder
-	 */
-	protected $primary_term_builder;
-
-	/**
-	 * The link builder
-	 *
-	 * @var Mockery\MockInterface|Indexable_Link_Builder
-	 */
-	private $link_builder;
-
-	/**
-	 * Represents the indexable helper.
-	 *
-	 * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Indexable_Helper
-	 */
-	protected $indexable_helper;
-
-	/**
-	 * Represents the indexable repository.
-	 *
-	 * @var Mockery\MockInterface|Indexable_Repository
-	 */
-	protected $indexable_repository;
-
-	/**
-	 * Represents the indexable mock.
-	 *
-	 * @var Indexable_Mock
-	 */
-	protected $indexable;
-
-	/**
-	 * Represents the instance to test.
-	 *
-	 * @var Indexable_Builder
-	 */
-	protected $instance;
-
-	/**
-	 * The version manager.
-	 *
-	 * @var Mockery\LegacyMockInterface|Mockery\MockInterface|Indexable_Version_Manager
-	 */
-	protected $version_manager;
+class Maybe_Build_Author_Indexable_Test extends Indexable_Builder_Setup {
 
 	/**
 	 * Sets up the test.
 	 */
 	protected function set_up() {
 		parent::set_up();
-
-		$this->author_builder            = Mockery::mock( Indexable_Author_Builder::class );
-		$this->post_builder              = Mockery::mock( Indexable_Post_Builder::class );
-		$this->term_builder              = Mockery::mock( Indexable_Term_Builder::class );
-		$this->home_page_builder         = Mockery::mock( Indexable_Home_Page_Builder::class );
-		$this->post_type_archive_builder = Mockery::mock( Indexable_Post_Type_Archive_Builder::class );
-		$this->date_archive_builder      = Mockery::mock( Indexable_Date_Archive_Builder::class );
-		$this->system_page_builder       = Mockery::mock( Indexable_System_Page_Builder::class );
-		$this->hierarchy_builder         = Mockery::mock( Indexable_Hierarchy_Builder::class );
-		$this->primary_term_builder      = Mockery::mock( Primary_Term_Builder::class );
-		$this->link_builder              = Mockery::mock( Indexable_Link_Builder::class );
-		$this->indexable_helper          = Mockery::mock( Indexable_Helper::class );
-		$this->version_manager           = Mockery::mock( Indexable_Version_Manager::class );
-		$this->indexable_repository      = Mockery::mock( Indexable_Repository::class );
-
-		$this->indexable              = Mockery::mock( Indexable_Mock::class );
-		$this->indexable->author_id   = 1999;
-		$this->indexable->version     = 1;
-		$this->indexable->object_type = 'post';
-		$this->indexable->object_id   = 1337;
 
 		$this->instance = new Indexable_Builder_Double(
 			$this->author_builder,
@@ -211,7 +71,7 @@ class Maybe_Build_Author_Indexable_Test extends TestCase {
 			->andReturn( false );
 
 		// The rest of expectation are from build method.
-		$this->expect_ensure_indexable( $author_indexable, $author_defaults );
+		$this->expect_ensure_indexable( $author_defaults, $author_indexable );
 
 		$this->author_builder
 			->expects( 'build' )
@@ -219,7 +79,7 @@ class Maybe_Build_Author_Indexable_Test extends TestCase {
 			->with( $author_indexable->object_id, $author_indexable )
 			->andReturn( $author_indexable );
 
-		$this->expect_save_indexable();
+		$this->expect_save_indexable_skip();
 
 		$this->instance->exposed_maybe_build_author_indexable( $author_id );
 	}
@@ -268,7 +128,7 @@ class Maybe_Build_Author_Indexable_Test extends TestCase {
 			->with( $author_indexable->object_id, $author_indexable )
 			->andReturn( $author_indexable );
 
-		$this->expect_save_indexable();
+		$this->expect_save_indexable_skip();
 
 		$this->instance->exposed_maybe_build_author_indexable( $author_id );
 	}
@@ -299,62 +159,5 @@ class Maybe_Build_Author_Indexable_Test extends TestCase {
 			->andReturn( $needs_upgrade );
 
 		$this->assertEquals( $author_indexable, $this->instance->exposed_maybe_build_author_indexable( $author_id ) );
-	}
-
-	/**
-	 * Expect save_indexable execution inside build method.
-	 * Skipping saving indexable, outside testing scope.
-	 *
-	 * @return void
-	 */
-	public function expect_save_indexable() {
-
-		$this->indexable_helper
-			->expects( 'should_index_indexables' )
-			->once()
-			->withNoArgs()
-			->andReturnFalse();
-	}
-
-	/**
-	 * Expect deep_copy_indexable execution inside build method.
-	 *
-	 * @param Indexable_Mock $author_indexable The author indexable.
-	 */
-	public function expect_deep_copy_indexable( $author_indexable ) {
-
-		$author_indexable->expects( 'as_array' )
-			->once()
-			->andReturn( [] );
-
-		$this->indexable_repository
-			->expects( 'create' )
-			->once()
-			->andReturn( $author_indexable );
-
-		$this->indexable_repository
-			->expects( 'query' )
-			->once()
-			->andReturnSelf();
-	}
-
-	/**
-	 * Expect ensure_indexable execution inside build method.
-	 *
-	 * @param Indexable_Mock $author_indexable The author indexable.
-	 * @param array          $author_defaults  The author defaults.
-	 */
-	public function expect_ensure_indexable( $author_indexable, $author_defaults ) {
-
-		$this->indexable_repository
-			->expects( 'query' )
-			->once()
-			->andReturnSelf();
-
-		$this->indexable_repository
-			->expects( 'create' )
-			->once()
-			->with( $author_defaults )
-			->andReturn( $author_indexable );
 	}
 }
