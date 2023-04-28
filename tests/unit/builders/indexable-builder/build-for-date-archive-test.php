d463e0f1bb024d191a0d7b72664d2b1f2cf34b7d
Vraja Das <vraja.pro@gmail.com>
2023-04-28 14:34:56 +0200

    Added test and fixed annotation for test coverage

--- tests/unit/builders/indexable-builder/build-for-date-archive-test.php
+++ tests/unit/builders/indexable-builder/build-for-date-archive-test.php
@@ -3,6 +3,7 @@
 namespace Yoast\WP\SEO\Tests\Unit\Builders\Indexable_Builder;
 
 use Yoast\WP\SEO\Tests\Unit\Builders\Indexable_Builder\Indexable_Builder_Setup;
+use Yoast\WP\SEO\Builders\Indexable_Builder;
 
 /**
  * Build_For_Date_Archive_Test.
@@ -19,8 +20,7 @@ class Build_For_Date_Archive_Test extends Indexable_Builder_Setup {
 	 */
 	protected function set_up() {
 		parent::set_up();
-		$this->indexable->object_type     = 'date-archive';
-		$this->indexable->object_sub_type = 'post';
+		$this->indexable->object_type = 'date-archive';
 	}
 
 	/**
@@ -30,12 +30,38 @@ class Build_For_Date_Archive_Test extends Indexable_Builder_Setup {
 	 * @covers ::set_indexable_repository
 	 * @covers ::build_for_date_archive
 	 * @covers ::ensure_indexable
+	 * @covers ::build
 	 */
-	public function test_build_for_date_archive() {
+	public function test_build_for_date_archive_without_indexable() {
 
 		$this->expect_build( [ 'object_type' => 'date-archive' ] );
 
 		$this->assertSame( $this->indexable, $this->instance->build_for_date_archive( false ) );
+	}
+
+	/**
+	 * Tests building an indexable for the post type archive.
+	 *
+	 * @covers ::__construct
+	 * @covers ::set_indexable_repository
+	 * @covers ::build_for_date_archive
+	 * @covers ::ensure_indexable
+	 * @covers ::build
+	 */
+	public function test_build_for_date_archive_with_indexable() {
+
+		$this->expect_deep_copy_indexable( $this->indexable );
+
+		$this->date_archive_builder
+			->expects( 'build' )
+			->once()
+			->with( $this->indexable )
+			->andReturn( $this->indexable );
+
+		// Saving is outside the scope of this test.
+		$this->expect_save_indexable_skip();
+
+		$this->assertSame( $this->indexable, $this->instance->build_for_date_archive( $this->indexable ) );
 	}
 
 	/**
@@ -44,6 +70,7 @@ class Build_For_Date_Archive_Test extends Indexable_Builder_Setup {
 	 * @param array $defaults The defaults to expect.
 	 */
 	public function expect_build( $defaults ) {
+
 		$this->expect_ensure_indexable( $defaults, $this->indexable );
 
 		$this->date_archive_builder
