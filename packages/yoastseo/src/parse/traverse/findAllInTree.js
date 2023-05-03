f69fa176461c1d93e7bdd4142ff62c41a1dc4285
hdvos <hdvos93@gmail.com>
2023-05-03 13:44:38 +0200

    allow for recursing into found nodes in findAllInTree.js

--- packages/yoastseo/src/parse/traverse/findAllInTree.js
+++ packages/yoastseo/src/parse/traverse/findAllInTree.js
@@ -3,10 +3,11 @@
  *
  * @param {Object} tree The tree.
  * @param {function} condition The condition that a node should satisfy to end up in the list.
+ * @param {boolean} recurseFoundNodes=false Whether to recurse into found nodes to see if the condition also applies to sub-nodes of the found node.
  *
  * @returns {Object[]} The list of nodes that satisfy the condition.
  */
-export default function findAllInTree( tree, condition ) {
+export default function findAllInTree( tree, condition, recurseFoundNodes = false ) {
 	const nodes = [];
 
 	if ( ! tree.childNodes ) {
@@ -16,6 +17,9 @@ export default function findAllInTree( tree, condition ) {
 	tree.childNodes.forEach( child => {
 		if ( condition( child ) ) {
 			nodes.push( child );
+			if ( recurseFoundNodes ) {
+				nodes.push( ...findAllInTree( child, condition ) );
+			}
 		} else {
 			nodes.push( ...findAllInTree( child, condition ) );
 		}
