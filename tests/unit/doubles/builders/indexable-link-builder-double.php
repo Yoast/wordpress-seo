652e295fd896580947f172afc336ca343743b489
Vraja Das <vraja.pro@gmail.com>
2023-05-03 10:50:28 +0200

    php cs fix

--- tests/unit/doubles/builders/indexable-link-builder-double.php
+++ tests/unit/doubles/builders/indexable-link-builder-double.php
@@ -9,7 +9,7 @@ use Yoast\WP\SEO\Builders\Indexable_Link_Builder;
  */
 class Indexable_Link_Builder_Double extends Indexable_Link_Builder {
 
-    /**
+	/**
 	 * Returns a cleaned permalink for a given link.
 	 *
 	 * @param string $link     The raw URL.
@@ -18,18 +18,17 @@ class Indexable_Link_Builder_Double extends Indexable_Link_Builder {
 	 * @return string The cleaned permalink.
 	 */
 	public function exposed_get_permalink( $link, $home_url ) {
-        return $this->get_permalink( $link, $home_url );
-    }
+		return $this->get_permalink( $link, $home_url );
+	}
 
-    /**
+	/**
 	 * Updates incoming link counts for related indexables.
 	 *
 	 * @param int[] $related_indexable_ids The IDs of all related indexables.
 	 *
 	 * @return void
 	 */
-    public function exposed_update_incoming_links_for_related_indexables( $related_indexable_ids ) {
-        $this->update_incoming_links_for_related_indexables( $related_indexable_ids );
-    }
-
+	public function exposed_update_incoming_links_for_related_indexables( $related_indexable_ids ) {
+		$this->update_incoming_links_for_related_indexables( $related_indexable_ids );
+	}
 }
