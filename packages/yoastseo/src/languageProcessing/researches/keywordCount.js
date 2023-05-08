59a377bbc61398e10f3cd62bef618734763eb6cf
hdvos <hdvos93@gmail.com>
2023-05-08 15:17:02 +0200

    refactor matchTokenWithWordForms in keywordCount.js

--- packages/yoastseo/src/languageProcessing/researches/keywordCount.js
+++ packages/yoastseo/src/languageProcessing/researches/keywordCount.js
@@ -2,64 +2,13 @@ import { flatten, uniq } from "lodash-es";
 import matchTextWithArray from "../helpers/match/matchTextWithArray";
 import getSentencesFromTree from "../helpers/sentence/getSentencesFromTree";
 import Mark from "../../values/Mark";
-import addWordBoundary from "../helpers/word/addWordboundary";
-import { replaceTurkishIsMemoized } from "../helpers/transliterate/specialCharacterMappings";
 import { tokenizerSplitter } from "../../parse/language/LanguageProcessor";
 import { normalizeSingle } from "../helpers/sanitize/quotes";
 import Token from "../../parse/structure/Token";
 import { getLanguage } from "../index";
 import removeConsecutiveKeyphraseFromResult from "../helpers/keywordCount/removeConsecutiveKeyphraseMatches";
+import matchTokenWithWordForms from "../helpers/keywordCount/matchTokenWithWordForms";
 
-
-/**
- * Creates a regex from the keyword with included wordboundaries.
- *
- * @param {string} keyword  The keyword to create a regex from.
- * @param {string} locale   The locale.
- *
- * @returns {RegExp} Regular expression of the keyword with word boundaries.
- */
-const toRegex = function( keyword, locale ) {
-	keyword = addWordBoundary( keyword, false, "", locale );
-	return new RegExp( keyword, "ig" );
-};
-
-/**
- * Matches a string with and without transliteration.
- * @param {string} text The text to match.
- * @param {string} keyword The keyword to match in the text.
- * @param {string} locale The locale used for transliteration.
- * @returns {Array} All matches from the original as the transliterated text and keyword.
- */
-const matchTextWithTransliteration = function( text, keyword, locale ) {
-	let keywordRegex = toRegex( keyword, locale );
-
-	if ( locale === "tr_TR" ) {
-		const turkishMappings = replaceTurkishIsMemoized( keyword );
-		keywordRegex = new RegExp( turkishMappings.map( x => addWordBoundary( x ) ).join( "|" ), "ig" );
-	}
-	const matches = text.match( keywordRegex );
-
-	return !! matches;
-};
-
-/**
- * Checks if a token matches with different forms of a word.
- * @param {string[]} wordForms Different forms of a word to match the token against.
- * @param {Token} token The token to match.
- * @param {string} locale The locale used for transliteration.
- * @returns {boolean|*} True if the token matches with one of the word forms.
- */
-const matchTokenWithWordForms = ( wordForms, token, locale ) => {
-	// First try a literal match.
-	if ( wordForms.includes( token.text ) ) {
-		return true;
-	}
-	// Then try a match with transliteration.
-	return wordForms.some( keyword => {
-		return matchTextWithTransliteration( token.text, keyword, locale );
-	} );
-};
 
 /**
  * (re-)Tokenizes the keyphrase forms in the same way that the tokens in the text are splitted.
