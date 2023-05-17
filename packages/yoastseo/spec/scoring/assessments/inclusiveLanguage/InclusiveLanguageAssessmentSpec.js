edf0863deb220d185de9da401b47550574c84266
Agnieszka Szuba <agnieszkaszuba@Pixel-3a.yoast.local>
2023-05-17 16:20:01 +0200

    Adds unit tests for assessments

--- packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/InclusiveLanguageAssessmentSpec.js
+++ packages/yoastseo/spec/scoring/assessments/inclusiveLanguage/InclusiveLanguageAssessmentSpec.js
@@ -12,6 +12,17 @@ describe( "inclusive Language Assessments", () => {
 		expect( assessment.hasEnoughContentForAssessment( mockPaper ) ).toBe( false );
 	} );
 
+	it( "should signal it does not have enough content for long texts inside elements we want to exclude from the analysis", () => {
+		const assessment = new InclusiveLanguageAssessment( assessments.find( obj => obj.identifier === "firstWorld" ) );
+
+		const mockPaper = new Paper( "<blockquote>Alice was beginning to get very tired of sitting by her sister on the\n" +
+			"bank, and of having nothing to do: once or twice she had peeped into\n" +
+			"the book her sister was reading, but it had no pictures or\n" +
+			"conversations in it, “and what is the use of a book,” thought Alice\n" +
+			"“without pictures or conversations?”</blockquote>" );
+		expect( assessment.hasEnoughContentForAssessment( mockPaper ) ).toBe( false );
+	} );
+
 	it( "should signal it does have enough content for assessment for longer texts", () => {
 		const assessment = new InclusiveLanguageAssessment( assessments.find( obj => obj.identifier === "firstWorld" ) );
 
