61386f170b748c63164616ced7d4f3bdbadcdba6
Vraja Das <vraja.pro@gmail.com>
2023-05-25 12:17:24 +0200

    Added reducers and function to remove post type needs review badge

--- packages/js/src/settings/store/post-types.js
+++ packages/js/src/settings/store/post-types.js
@@ -1,15 +1,39 @@
 import { createSelector, createSlice } from "@reduxjs/toolkit";
 import { get, omit, pick } from "lodash";
+import apiFetch from "@wordpress/api-fetch";
 
 /**
  * @returns {Object} The initial state.
  */
 export const createInitialPostTypesState = () => get( window, "wpseoScriptData.postTypes", {} );
 
+const UPDATE_REVIEW_ACTION_NAME = "updatePostTypeReviewStatus";
+
+/**
+ * @param {String} postTypeName The query data.
+ * @returns {Object} Success or error action object.
+ */
+export function* updatePostTypeReviewStatus( postTypeName ) {
+	try {
+		yield{
+			type: UPDATE_REVIEW_ACTION_NAME,
+			payload: postTypeName,
+		};
+	} catch ( error ) {
+		// Empty.
+	}
+	return { type: `${ UPDATE_REVIEW_ACTION_NAME }/result`, payload: postTypeName };
+}
+
 const slice = createSlice( {
 	name: "postTypes",
 	initialState: createInitialPostTypesState(),
 	reducers: {},
+	extraReducers: ( builder ) => {
+		builder.addCase( `${ UPDATE_REVIEW_ACTION_NAME }/result`, ( state, { payload } ) => {
+			state[ payload ].needsReview = false;
+		} );
+	},
 } );
 
 export const postTypesSelectors = {
@@ -25,6 +49,17 @@ postTypesSelectors.selectPostTypes = createSelector(
 	postTypes => omit( postTypes, [ "attachment" ] )
 );
 
-export const postTypesActions = slice.actions;
+export const updatePostTypeReviewControls = {
+	[ UPDATE_REVIEW_ACTION_NAME ]: async( { payload } ) => apiFetch( {
+		path: "/yoast/v1/needs-review/dismiss-post-type",
+		method: "POST",
+		data: { postTypeName: payload },
+	} ),
+};
+
+export const postTypesActions = {
+	...slice.actions,
+	updatePostTypeReviewStatus,
+};
 
 export default slice.reducer;
