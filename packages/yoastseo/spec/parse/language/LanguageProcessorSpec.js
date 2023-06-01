d723f5003eed40523169b46cde3076a3146583bd
hdvos <hdvos93@gmail.com>
2023-06-01 16:40:00 +0200

    add testcases for the splitIntoTokens method

--- packages/yoastseo/spec/parse/language/LanguageProcessorSpec.js
+++ packages/yoastseo/spec/parse/language/LanguageProcessorSpec.js
@@ -29,17 +29,256 @@ describe( "A test for the splitIntoSentences method", () => {
 	} );
 } );
 
-describe( "A test for the splitIntoTokens method", () => {
-	it( "should return an array of tokens", function() {
-		const languageProcessor = new LanguageProcessor( researcher );
 
-		const tokens = languageProcessor.splitIntoTokens( new Sentence( "Hello, world!" ) );
-		expect( tokens ).toEqual( [
+const splitIntoTokensTestCases = [
+	{
+		description: "should correctly tokenize a sentence with a single token",
+		sentence: "Hello",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a single token and a trailing whitespace",
+		sentence: "Hello ",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+		],
+
+	},
+	{
+		description: "should correctly tokenize a sentence with a single token and a leading whitespace",
+		sentence: " Hello",
+		expectedTokens: [
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "Hello", sourceCodeRange: {} },
+		],
+
+	},
+	{
+		description: "should correctly tokenize a sentence with multiple tokens ending with a full stop",
+		sentence: "Hello, world.",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: ".", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with multiple tokens ending with a full stop and a trailing whitespace",
+		sentence: "Hello, world. ",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: ".", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with multiple punctuation marks",
+		sentence: "Hello, world!!!",
+		expectedTokens: [
 			{ text: "Hello", sourceCodeRange: {} },
 			{ text: ",", sourceCodeRange: {} },
 			{ text: " ", sourceCodeRange: {} },
 			{ text: "world", sourceCodeRange: {} },
 			{ text: "!", sourceCodeRange: {} },
-		] );
+			{ text: "!", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with multiple different punctuation marks",
+		sentence: "Hello, world!?!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+			{ text: "?", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a word containing a dash",
+		sentence: "Hello, world-wide!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "world-wide", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a word containing an underscore",
+		sentence: "Hello, world_wide!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "world_wide", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a word containing a forward slash",
+		sentence: "Hello, world/worlds!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: "/", sourceCodeRange: {} },
+			{ text: "worlds", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a word containing a backslash",
+		sentence: "Hello, world\\worlds!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: "\\", sourceCodeRange: {} },
+			{ text: "worlds", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a word containing an apostrophe",
+		sentence: "Hello, world's!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "world's", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a number with a decimal point",
+		sentence: "Hello, 3.14!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "3.14", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a number with a decimal comma",
+		sentence: "Hello, 3,14!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "3,14", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a token that starts with a punctuation mark",
+		sentence: "Hello, .world!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: ".", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a token between parentheses",
+		sentence: "Hello, (world)!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "(", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: ")", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with a phrase between parentheses",
+		sentence: "Hello, (world of worlds)!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "(", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "of", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "worlds", sourceCodeRange: {} },
+			{ text: ")", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence with nested parentheses",
+		sentence: "Hello, (world (of worlds))!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "(", sourceCodeRange: {} },
+			{ text: "world", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "(", sourceCodeRange: {} },
+			{ text: "of", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "worlds", sourceCodeRange: {} },
+			{ text: ")", sourceCodeRange: {} },
+			{ text: ")", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+	{
+		description: "should correctly tokenize a sentence containing an url",
+		sentence: "Hello, https://www.google.com!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "https://www.google.com", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+		skip: true,
+	},
+	{
+		description: "should correctly tokenize a sentence containing an email address",
+		sentence: "Hello, hugo@yoast.com!",
+		expectedTokens: [
+			{ text: "Hello", sourceCodeRange: {} },
+			{ text: ",", sourceCodeRange: {} },
+			{ text: " ", sourceCodeRange: {} },
+			{ text: "hugo@yoast.com", sourceCodeRange: {} },
+			{ text: "!", sourceCodeRange: {} },
+		],
+	},
+];
+
+describe.each( splitIntoTokensTestCases )( "A test for the tokenize method", ( { description, sentence, expectedTokens, skip } ) => {
+	const test = skip ? it.skip : it;
+
+	test( description, function() {
+		const languageProcessor = new LanguageProcessor( researcher );
+
+		const tokens = languageProcessor.splitIntoTokens( new Sentence( sentence ) );
+		expect( tokens ).toEqual( expectedTokens );
 	} );
 } );
