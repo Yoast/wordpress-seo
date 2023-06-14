8ee58d5e005428da69fe973f119937c5eefc68b1
hdvos <hdvos93@gmail.com>
2023-06-14 15:48:39 +0200

    fix es-lint errors.

--- packages/yoastseo/src/languageProcessing/helpers/word/getWordsForHTMLParser.js
+++ packages/yoastseo/src/languageProcessing/helpers/word/getWordsForHTMLParser.js
@@ -37,7 +37,6 @@ const getWordsForHTMLParser = ( text ) => {
 
 	const tokenTexts = [];
 	rawTokens.forEach( token => {
-
 		// Pretokens contains all that occurs before the first letter of the token.
 		const preTokens = [];
 		// Posttokens contains all that occurs after the last letter of the token.
