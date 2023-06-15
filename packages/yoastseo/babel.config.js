49dae815099d76f6ae6d43318586cacb18b20258
Martijn van der Klis <martijnvanderklis@yoast.com>
2023-06-15 10:20:40 +0200

    Major clean-up: removing a lot of the grunt aliases from yoastseo along with some unnecessary files

--- packages/yoastseo/babel.config.js
+++ packages/yoastseo/babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
 	presets: [ "@yoast/babel-preset" ],
+
+	// Remove the parsedPaper directory from the build, except for the scoreAggregators which are used in src/worker/AnalysisWebWorker.js
+	ignore: [ "src/parsedPaper/" ],
+	exclude: [ "src/parsedPaper/assess/scoreAggregators/" ],
 };
