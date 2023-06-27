1237e61a417d45fb8dfb39b0e04e5bba01e2314b
aidamarfuaty <aida@yoast.com>
2023-06-27 09:44:13 +0200

    uncomment tests

--- packages/yoastseo/spec/fullTextTests/testTexts/hu/hungarianPaper.js
+++ packages/yoastseo/spec/fullTextTests/testTexts/hu/hungarianPaper.js
@@ -31,7 +31,7 @@ const expectedResults = {
 	keywordDensity: {
 		isApplicable: true,
 		score: 9,
-		resultText: "<a href='https://yoa.st/33v' target='_blank'>Keyphrase density</a>: The keyphrase was found 12 times. This is great!",
+		resultText: "<a href='https://yoa.st/33v' target='_blank'>Keyphrase density</a>: The keyphrase was found 11 times. This is great!",
 	},
 	metaDescriptionKeyword: {
 		isApplicable: true,
