01283fc258d10b5467712f6f07b818cabb0be185
Enrico Battocchi <enrico.battocchi@gmail.com>
2023-06-28 14:29:57 +0200

    Extract Yoast (SEO) from translatable strings

--- packages/js/src/first-time-configuration/tailwind-components/steps/finish/finish-step.js
+++ packages/js/src/first-time-configuration/tailwind-components/steps/finish/finish-step.js
@@ -1,5 +1,5 @@
 import { ArrowNarrowRightIcon } from "@heroicons/react/outline";
-import { __ } from "@wordpress/i18n";
+import { __, sprintf } from "@wordpress/i18n";
 import { get } from "lodash";
 import { ReactComponent as ConfigurationFinishImage } from "../../../../../images/indexables_2_left_bubble_optm.svg";
 import { Button, Link } from "@yoast/ui-library";
@@ -25,7 +25,11 @@ export default function FinishStep() {
 		<div className="yst-flex yst-flex-row yst-justify-between yst-items-center yst--mt-4">
 			<div className="yst-mr-6">
 				<p className="yst-text-sm yst-mb-4">
-					{ __( "Good job! By providing this information, Yoast has improved your site and told search engines what your site is all about.", "wordpress-seo" ) }
+					{ sprintf(
+						/* translators: 1: Yoast. */
+						__( "Good job! By providing this information, %1$s has improved your site and told search engines what your site is all about.", "wordpress-seo" ),
+						"Yoast"
+					) }
 				</p>
 				<p className="yst-text-sm yst-mb-6">
 					{ __( "To increase your rankings, you need to regularly work on SEO. That can be overwhelming, so let’s start small!", "wordpress-seo" ) }
@@ -36,7 +40,11 @@ export default function FinishStep() {
 					href={ webinarIntroFirstTimeConfigUrl }
 					target="_blank"
 				>
-					{ __( "Learn how to increase your rankings with Yoast SEO", "wordpress-seo" ) }
+					{ sprintf(
+						/* translators: 1: Yoast SEO. */
+						__( "Learn how to increase your rankings with %1$s", "wordpress-seo" ),
+						"Yoast SEO"
+					) }
 					<ArrowNarrowRightIcon className="yst-w-4 yst-h-4 yst-icon-rtl yst-ml-2" />
 				</Button>
 				<p className="yst-mt-4">
