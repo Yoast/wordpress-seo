46a93df5265e2e2c31f8ce69e740109ce69ac009
Martijn van der Klis <martijnvanderklis@yoast.com>
2023-07-04 13:35:17 +0200

    Adds comma after "for example"

--- packages/js/src/settings/routes/crawl-optimization.js
+++ packages/js/src/settings/routes/crawl-optimization.js
@@ -234,7 +234,7 @@ const CrawlOptimization = () => {
 			sprintf(
 				/**
 				 * translators:
-				 * %1$s through %7$s each expand to a parameter name within a <code> tag. For example <code>gclid</code>.
+				 * %1$s through %7$s each expand to a parameter name within a <code> tag. For example, <code>gclid</code>.
 				 */
 				__( "Note that the following commonly-used parameters will not be removed: %1$s, %2$s, %3$s, %4$s, %5$s, %6$s, and %7$s.",
 					"wordpress-seo" ),
