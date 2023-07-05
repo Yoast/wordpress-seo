136decde8e03673b687e01b821cdcf7bd5b76ad8
hdvos <hdvos93@gmail.com>
2023-07-05 15:32:42 +0200

    expose stripFullTags

--- packages/yoastseo/src/languageProcessing/helpers/index.js
+++ packages/yoastseo/src/languageProcessing/helpers/index.js
@@ -1,9 +1,11 @@
 import matchStringWithRegex from "./regex/matchStringWithRegex";
 import { normalize } from "./sanitize/quotes";
 import removeHtmlBlocks from "./html/htmlParser";
+import { stripFullTags } from "./sanitize/stripHTMLTags";
 
 export {
 	matchStringWithRegex,
 	normalize,
 	removeHtmlBlocks,
+	stripFullTags,
 };
