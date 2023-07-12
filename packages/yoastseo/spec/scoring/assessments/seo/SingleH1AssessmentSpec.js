c7177f00ecd2a0c7971cedbfeba8e18125c27e3e
hdvos <hdvos93@gmail.com>
2023-07-12 17:45:37 +0200

    update SingleH1AssessmentSpec.js to new marks

--- packages/yoastseo/spec/scoring/assessments/seo/SingleH1AssessmentSpec.js
+++ packages/yoastseo/spec/scoring/assessments/seo/SingleH1AssessmentSpec.js
@@ -2,13 +2,16 @@ import SingleH1Assessment from "../../../../src/scoring/assessments/seo/SingleH1
 import Paper from "../../../../src/values/Paper.js";
 import Mark from "../../../../src/values/Mark.js";
 import EnglishResearcher from "../../../../src/languageProcessing/languages/en/Researcher";
+import buildTree from "../../../specHelpers/parse/buildTree";
 
 const h1Assessment = new SingleH1Assessment();
 
 describe( "An assessment to check whether there is more than one H1 in the text", function() {
 	it( "returns the default result when the paper doesn't contain an H1", function() {
 		const mockPaper = new Paper( "<p>a paragraph</p>" );
-		const assessment = h1Assessment.getResult( mockPaper, new EnglishResearcher( mockPaper ) );
+		const mockResearcher = new EnglishResearcher( mockPaper );
+		buildTree( mockPaper, mockResearcher );
+		const assessment = h1Assessment.getResult( mockPaper, mockResearcher );
 
 		expect( assessment.getScore() ).toEqual( 0 );
 		expect( assessment.getText() ).toEqual( "" );
@@ -17,7 +20,10 @@ describe( "An assessment to check whether there is more than one H1 in the text"
 
 	it( "returns the default result when there's an H1 at the beginning of the body", function() {
 		const mockPaper = new Paper( "<h1>heading</h1><p>a paragraph</p>" );
-		const assessment = h1Assessment.getResult( mockPaper, new EnglishResearcher( mockPaper ) );
+		const mockResearcher = new EnglishResearcher( mockPaper );
+		buildTree( mockPaper, mockResearcher );
+		const assessment = h1Assessment.getResult( mockPaper, mockResearcher );
+
 
 		expect( assessment.getScore() ).toEqual( 0 );
 		expect( assessment.getText() ).toEqual( "" );
@@ -26,7 +32,9 @@ describe( "An assessment to check whether there is more than one H1 in the text"
 
 	it( "also returns the default result when there's an H1 and it's not at the beginning of the body", function() {
 		const mockPaper = new Paper( "<p>Cool intro</p><h1>heading</h1><p>a paragraph</p>" );
-		const assessment = h1Assessment.getResult( mockPaper, new EnglishResearcher( mockPaper ) );
+		const mockResearcher = new EnglishResearcher( mockPaper );
+		buildTree( mockPaper, mockResearcher );
+		const assessment = h1Assessment.getResult( mockPaper, mockResearcher );
 
 		expect( assessment.getScore() ).toEqual( 0 );
 		expect( assessment.getText() ).toEqual( "" );
@@ -36,7 +44,9 @@ describe( "An assessment to check whether there is more than one H1 in the text"
 	it( "returns a bad score and appropriate feedback when there are multiple one superfluous (i.e., non-title) " +
 		"H1s in the body of the text", function() {
 		const mockPaper = new Paper( "<p>a paragraph</p><h1>heading 1</h1><p>a paragraph</p><h1>heading 2</h1>" );
-		const assessment = h1Assessment.getResult( mockPaper, new EnglishResearcher( mockPaper ) );
+		const mockResearcher = new EnglishResearcher( mockPaper );
+		buildTree( mockPaper, mockResearcher );
+		const assessment = h1Assessment.getResult( mockPaper, mockResearcher );
 
 		expect( assessment.getScore() ).toEqual( 1 );
 		expect( assessment.getText() ).toEqual(  "<a href='https://yoa.st/3a6' target='_blank'>Single title</a>: " +
@@ -48,11 +58,16 @@ describe( "An assessment to check whether there is more than one H1 in the text"
 describe( "A test for marking incorrect H1s in the body", function() {
 	it( "returns markers for incorrect H1s in the body", function() {
 		const mockPaper = new Paper( "<p>a paragraph</p><h1>heading</h1>" );
-		h1Assessment.getResult( mockPaper, new EnglishResearcher( mockPaper ) );
+		const mockResearcher = new EnglishResearcher( mockPaper );
+		buildTree( mockPaper, mockResearcher );
+		h1Assessment.getResult( mockPaper, mockResearcher );
 
 		const expected = [
-			new Mark( { original: "<h1>heading</h1>",
-				marked: "<h1><yoastmark class='yoast-text-mark'>heading</yoastmark></h1>" } ),
+			new Mark( {
+				original: "<h1>heading</h1>",
+				marked: "<h1><yoastmark class='yoast-text-mark'>heading</yoastmark></h1>",
+				position: { startOffset: 22, endOffset: 29 } }
+				 ),
 		];
 
 		expect( h1Assessment.getMarks() ).toEqual( expected );
@@ -60,10 +75,15 @@ describe( "A test for marking incorrect H1s in the body", function() {
 
 	it( "also returns markers for H1s in the first position of the body", function() {
 		const mockPaper = new Paper( "<h1>heading</h1><p>a paragraph</p>" );
-		h1Assessment.getResult( mockPaper, new EnglishResearcher( mockPaper ) );
+		const mockResearcher = new EnglishResearcher( mockPaper );
+		buildTree( mockPaper, mockResearcher );
+		h1Assessment.getResult( mockPaper, mockResearcher );
 		const expected = [
-			new Mark( { original: "<h1>heading</h1>",
-				marked: "<h1><yoastmark class='yoast-text-mark'>heading</yoastmark></h1>" } ),
+			new Mark( {
+				original: "<h1>heading</h1>",
+				marked: "<h1><yoastmark class='yoast-text-mark'>heading</yoastmark></h1>",
+				position: { startOffset: 4, endOffset: 11 },
+			} ),
 		];
 
 		expect( h1Assessment.getMarks() ).toEqual( expected );
@@ -71,7 +91,9 @@ describe( "A test for marking incorrect H1s in the body", function() {
 
 	it( "doesn't return markers when there are no H1s in the body", function() {
 		const mockPaper = new Paper( "<p>a paragraph</p>" );
-		const results = h1Assessment.getResult( mockPaper, new EnglishResearcher( mockPaper ) );
+		const mockResearcher = new EnglishResearcher( mockPaper );
+		buildTree( mockPaper, mockResearcher );
+		const results = h1Assessment.getResult( mockPaper, mockResearcher );
 
 		expect( results._hasMarks ).toEqual( false );
 	} );
@@ -87,6 +109,13 @@ describe( "Checks if the assessment is applicable", function() {
 
 	it( "is not applicable when there there is no text", function() {
 		const mockPaper = new Paper( "" );
+		const assessment = h1Assessment.isApplicable( mockPaper );
+
+		expect( assessment ).toBe( false );
+	} );
+
+	it( "is not applicable when there there is no text but there is an image", function() {
+		const mockPaper = new Paper( "<img src=\"img_girl.jpg\" alt=\"Girl in a jacket\" width=\"500\" height=\"600\">" );
 		const assessment = h1Assessment.isApplicable( mockPaper );
 
 		expect( assessment ).toBe( false );
