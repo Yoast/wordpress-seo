0affbc6eaa1b7b01a0d3e7d7c6b8216e20fb870a
hdvos <hdvos93@gmail.com>
2023-07-13 14:57:24 +0200

    update jsdoc

--- packages/yoastseo/src/languageProcessing/researches/h1s.js
+++ packages/yoastseo/src/languageProcessing/researches/h1s.js
@@ -1,7 +1,5 @@
-
-
 /**
- * Gets all H1s in a text, including their content and their position with regards to other HTML blocks.
+ * Gets all H1s in a text, including their content and their position information.
  *
  * @param {Paper} paper The paper for which to get the H1s.
  *
