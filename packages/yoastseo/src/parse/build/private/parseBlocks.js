6b5586c36e8a9c9fc071b5a0f67523a04a292028
aidamarfuaty <aida@yoast.com>
2023-07-21 17:26:16 +0200

    Add steps to parse blocks and set the client id to the tree

--- packages/yoastseo/src/parse/build/private/parseBlocks.js
+++ packages/yoastseo/src/parse/build/private/parseBlocks.js
@@ -1,77 +1,11 @@
-
 const blockTokenizer = /<!--\s+wp:([a-z][a-z0-9_-]*\/)?([a-z][a-z0-9_-]*)\s+({(?:(?=([^}]+|}+(?=})|(?!}\s+\/?-->)[^])*)\5|[^]*?)}\s+)?(\/)?-->/g;
 
-// /**
-//  * Sets the block index to the node.
-//  *
-//  * @param {number} index The block index.
-//  * @param {Node} node The node to check.
-//  * @returns {void}
-//  */
-// const setBlockIndexOnNode = ( index, node ) => {
-// 	node.blockIndex = index;
-// 	if ( node.childNodes && node.childNodes.length > 0 ) {
-// 		node.childNodes.map( childNode => setBlockIndexOnNode( index, childNode ) );
-// 	}
-// };
-//
-// /**
-//  * Retrieves the block index and sets them to Node.
-//  *
-//  * @param {Node} node The node to check.
-//  * @returns {void}
-//  */
-// export const getAnSetBlockIndex = ( node ) => {
-// 	let index = -1;
-// 	for ( const topNode of node.childNodes ) {
-// 		if ( topNode.name === "#comment" && topNode.data.trim().startsWith( "wp:" ) ) {
-// 			index++;
-// 		} else if ( topNode.name === "#comment" && topNode.data.trim().startsWith( "/wp:" ) ) {
-// 			continue;
-// 		} else {
-// 			setBlockIndexOnNode( index, topNode );
-// 		}
-// 	}
-// };
-
-
-const getBlockName = ( node ) => {
-	const supportedBlocks = [ "paragraph", "list", "list-item", "heading", "audio", "embed", "gallery", "image", "table", "video" ];
-	const blockNames = supportedBlocks.map( block => [ "wp:".concat( block ), "/wp:".concat( block ), "core/".concat( block ) ] );
-	const foundBlockName = { blockName: "", closingName: "" };
-	blockNames.forEach( name => {
-		if ( foundBlockName.blockName === "" && node.data && node.data.trim().startsWith( name[ 0 ] ) ) {
-			foundBlockName.blockName = name[ 2 ];
-		} else if ( foundBlockName.closingName === "" && node.data && node.data.trim().startsWith( name[ 1 ] ) ) {
-			foundBlockName.closingName = name[ 1 ];
-		}
-	} );
-	return foundBlockName;
-};
-
-
-const getYoastBlockName = ( node ) => {
-	const yoastBlocks = [ "yoast/faq-block", "yoast/how-to-block" ];
-	const foundBlockName = { blockName: "", closingName: "" };
-	yoastBlocks.forEach( block => {
-		if ( foundBlockName.blockName === "" && node.data && node.data.trim().startsWith( "wp:".concat( block ) ) ) {
-			foundBlockName.blockName = block;
-		} else if ( foundBlockName.closingName === "" && node.data && node.data.trim().startsWith( "/wp:".concat( block ) ) ) {
-			foundBlockName.closingName = block;
-		}
-	} );
-	return foundBlockName;
-};
-
-const setClientIdOnNode = ( index, blockName, node, blocks ) => {
-	if ( blocks && blocks.length > 0 && blocks[ index ].name === blockName ) {
-		node.clientId = blocks[ index ].clientId;
-		if ( node.childNodes && node.childNodes.length > 0 ) {
-			node.childNodes.map( childNode => setClientIdOnNode( index, blockName, childNode, blocks ) );
-		}
-	}
-};
-
+/**
+ * Get all the blocks including the inner blocks.
+ *
+ * @param {Paper} paper The paper to get all the blocks.
+ * @returns {Object[]} An array of blocks.
+ */
 const getAllBlocks = ( paper ) => {
 	const blocks = paper._attributes.wpBlocks;
 	const flattenBlocks = [];
@@ -91,7 +25,18 @@ const getAllBlocks = ( paper ) => {
 	return flattenBlocks;
 };
 
+
+/**
+ * Gets the offset of each block and set it to the tree.
+ *
+ * @param {Object[]} blocks An array of blocks.
+ * @param {string} text The text.
+ * @returns {void}
+ */
 function updateBlocksOffset( blocks, text ) {
+	if ( blocks.length === 0 ) {
+		return;
+	}
 	// eslint-disable-next-line no-constant-condition
 	blocks.forEach( currentBlock => {
 		const matches = blockTokenizer.exec( text );
@@ -114,7 +59,17 @@ function updateBlocksOffset( blocks, text ) {
 	} );
 }
 
-function updateClintIdForSubtree( rootNode, blocks, clientId ) {
+
+/**
+ * Gets the client id for each block and set it to the child nodes.
+ * Additionally, when a node has an attribute with 'id' key, also set this id to the first.
+ *
+ * @param {Node} rootNode The root node.
+ * @param {Object[]} blocks An array of blocks.
+ * @param {string} clientId The client id to set.
+ * @returns {void}
+ */
+function updateClientIdAndAttrIdForSubtree( rootNode, blocks, clientId ) {
 	if ( ! rootNode ) {
 		return;
 	}
@@ -123,84 +78,46 @@ function updateClintIdForSubtree( rootNode, blocks, clientId ) {
 	if ( rootNode.sourceCodeLocation && rootNode.sourceCodeLocation.startOffset ) {
 		const block = blocks.find( ( b ) => b.contentOffset === rootNode.sourceCodeLocation.startOffset );
 		if ( block ) {
-			console.log( "found block: ", block );
-
 			currentClientId = block.clientId;
 		}
 	}
 
+	// If the clientId is not undefined, also set this to the root node.
 	if ( currentClientId ) {
 		rootNode.clientId = currentClientId;
 	}
 
-	( rootNode.childNodes || [] ).forEach( ( node ) => updateClintIdForSubtree( node, blocks, currentClientId ) );
-}
-export default function( paper, node ) {
-	console.log( "parseBlocks", paper, node );
+	// If the node has children, update the clientId for them.
+	( rootNode.childNodes || [] ).forEach( ( node ) => {
+		if ( node.attributes && node.attributes.id ) {
+			// If the node's child has an attribute with 'id' key, also set this id to the first and third child node.
+			if ( node.childNodes && node.childNodes.length > 3 ) {
+				node.childNodes[ 0 ].attributeId = node.attributes.id;
+				node.childNodes[ 0 ].isFirstPair = true;
 
-	const blocks = paper._attributes.wpBlocks;
+				node.childNodes[ 2 ].attributeId = node.attributes.id;
+				node.childNodes[ 2 ].isFirstPair = false;
+			}
+		}
+		updateClientIdAndAttrIdForSubtree( node, blocks, currentClientId );
+	} );
+}
+
+/**
+ * Parses blocks and updates the clientId for the subtree.
+ * Additionally, when a node has an attribute with 'id' key, also set this id to the first and third child nod of that node.
+ *
+ * @param {Paper} paper The paper to parse.
+ * @param {Node} node	The node to parse.
+ * @returns {void}
+ */
+export default function( paper, node ) {
+	const blocks = paper._attributes.wpBlocks || [];
 	blockTokenizer.lastIndex = 0;
 	updateBlocksOffset( blocks, paper.getText() );
 
-	// console.log( "Paper attributes: ", attributes );
-
-	// const blocks = getAllBlocks( paper );
-	// const index = -1;
-	// const blockName = "";
-
-	// const getAndSetClientID = ( tree ) => {
-	// 	let blockName = "";
-	//
-	// 	for ( const topNode of tree.childNodes ) {
-	// 		const foundBlockName = getBlockName( topNode );
-	// 		const foundYoastBlockName = getYoastBlockName( topNode );
-	// 		if ( foundBlockName.blockName.length > 0 || foundYoastBlockName.blockName.length > 0 ) {
-	// 			blockName = foundBlockName.blockName;
-	// 			index++;
-	// 		} else if ( foundBlockName.closingName.length > 0 || foundYoastBlockName.closingName.length > 0 ) {
-	// 			continue;
-	// 		} else {
-	// 			if ( blocks.length > 0 && blocks[ index ].name === blockName ) {
-	// 				topNode.clientId = blocks[ index ].clientId;
-	// 				if ( topNode.childNodes && topNode.childNodes.length > 0 ) {
-	// 					getAndSetClientID( topNode );
-	// 				}
-	// 			}
-	// 			// setClientIdOnNode( index, blockName, topNode, blocks );
-	// 		}
-	// 	}
-	// };
-	// getAndSetClientID( node );
 	const rawBlocks = getAllBlocks( paper );
 
 	// eslint-disable-next-line no-undefined
-	updateClintIdForSubtree( node, rawBlocks, undefined );
-
-	console.log( "after updateClintIdForSubtree: ", node );
-	// for ( const topNode of node.childNodes ) {
-	// 	if ( ! topNode.sourceCodeLocation ) {
-	// 		continue;
-	// 	}
-	// 	if ( ! topNode.sourceCodeLocation.startOffset ) {
-	// 		continue;
-	// 	}
-	//
-	// 	const block = rawBlocks.find( ( b ) => b.contentOffset === topNode.sourceCodeLocation.startOffset );
-	// 	if ( block ) {
-	// 		console.log( "found block: ", block );
-	// 		topNode.clientId = block.clientId;
-	// 		// setClientIdOnNode( index, blockName, topNode, blocks );
-	// 	}
-	//
-	// 	// const foundBlockName = getBlockName( topNode );
-	// 	// const foundYoastBlockName = getYoastBlockName( topNode );
-	// 	// if ( foundBlockName.blockName.length > 0 || foundYoastBlockName.blockName.length > 0 ) {
-	// 	// 	blockName = foundBlockName.blockName;
-	// 	// 	index++;
-	// 	// } else if ( foundBlockName.closingName.length > 0 || foundYoastBlockName.closingName.length > 0 ) {
-	// 	// 	continue;
-	// 	// } else {
-	// 	// 	setClientIdOnNode( index, blockName, topNode, blocks );
-	// 	// }
-	// }
+	updateClientIdAndAttrIdForSubtree( node, rawBlocks, undefined );
 }
