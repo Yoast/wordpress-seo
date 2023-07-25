80397ee3536c53c2d4450d2d9dbcc8cfd7606036
hdvos <hdvos93@gmail.com>
2023-07-25 11:33:56 +0200

    Filter shortcodes from sentence text

--- packages/yoastseo/src/languageProcessing/helpers/sanitize/filterShortcodesFromTree.js
+++ packages/yoastseo/src/languageProcessing/helpers/sanitize/filterShortcodesFromTree.js
@@ -1,4 +1,13 @@
-const filterShortcodesFromSentence = ( sentence, shortcodeTags ) => {
+
+/**
+ * Filters shortcodes from a sentence.
+ * @param {Sentence} sentence The sentence to filter.
+ * @param {string[]} shortcodeTags The tags of the shortcodes to filter.
+ * @param {RegExp} shortcodeTagsRegex The regex to filter the shortcodes.
+ * @returns {void}
+ *
+ */
+const filterShortcodesFromSentence = ( sentence, shortcodeTags, shortcodeTagsRegex ) => {
 	const tokens = sentence.tokens;
 	// traverse through tokens backwards
 	for ( let i = tokens.length - 1; i >= 0; i-- ) {
@@ -11,26 +20,43 @@ const filterShortcodesFromSentence = ( sentence, shortcodeTags ) => {
 		}
 	}
 	sentence.tokens = tokens;
+
+	sentence.text = sentence.text.replace( shortcodeTagsRegex, "" );
 };
 
-const filterFromTokens = ( tree, shortcodeTags ) => {
+/**
+ * A recursive function that filters shortcodes from sentences.
+ * @param {Node} tree The tree to filter.
+ * @param {string[]} shortcodeTags The tags of the shortcodes to filter.
+ * @param {RegExp} shortcodeTagsRegex The regex to filter the shortcodes.
+ * @returns {void}
+ */
+const filterShortcodesFromSentences = ( tree, shortcodeTags, shortcodeTagsRegex ) => {
 	if ( tree.sentences ) {
 		tree.sentences.forEach( sentence => {
-			filterShortcodesFromSentence( sentence, shortcodeTags );
+			filterShortcodesFromSentence( sentence, shortcodeTags, shortcodeTagsRegex );
 		} );
 	}
 
 	if ( tree.childNodes ) {
 		tree.childNodes.forEach( childNode => {
-			filterFromTokens( childNode, shortcodeTags );
+			filterShortcodesFromSentences( childNode, shortcodeTags, shortcodeTagsRegex );
 		} );
 	}
-	return tree;
 };
 
+/**
+ * Filters shortcodes from the tree.
+ * @param {Node} tree The tree to filter.
+ * @param {string[]} shortcodeTags The tags of the shortcodes to filter.
+ * @returns {void}
+ */
 const filterShortcodesFromTree = ( tree, shortcodeTags ) => {
 	if ( shortcodeTags ) {
-		filterFromTokens( tree, shortcodeTags );
+		const sortcodeTagsRegexString = `\\[(${ shortcodeTags.join( "|" ) })[^\\]]*\\]`;
+		const shortcodeTagsRegex = new RegExp( sortcodeTagsRegexString, "g" );
+
+		filterShortcodesFromSentences( tree, shortcodeTags, shortcodeTagsRegex );
 	}
 };
 
