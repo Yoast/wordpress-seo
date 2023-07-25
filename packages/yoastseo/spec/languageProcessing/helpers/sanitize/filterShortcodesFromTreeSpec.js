072847fac2e2fdc81ba832c6ebe316332b0ba6b8
hdvos <hdvos93@gmail.com>
2023-07-25 10:51:10 +0200

    add specs for shortcode filtering function

---
+++ packages/yoastseo/spec/languageProcessing/helpers/sanitize/filterShortcodesFromTreeSpec.js
@@ -0,0 +1,327 @@
+import filterShortcodesFromTree from "../../../../src/languageProcessing/helpers/sanitize/filterShortcodesFromTree";
+
+
+describe( "filterShortcodesFromTree", function() {
+	it( "should filter a shortcode with no parameters from a tree", function() {
+		const tree = {
+			sentences: [
+				{
+					text: "This is a [shortcode] test.",
+					tokens: [
+						{ text: "This" },
+						{ text: " " },
+						{ text: "is" },
+						{ text: " " },
+						{ text: "a" },
+						{ text: " " },
+						{ text: "[" },
+						{ text: "shortcode" },
+						{ text: "]" },
+						{ text: " " },
+						{ text: "test" },
+						{ text: "." },
+					],
+				} ] };
+		const shortcodes = [ "shortcode" ];
+
+		filterShortcodesFromTree( tree, shortcodes );
+
+		expect( tree.sentences[ 0 ].tokens ).toEqual( [
+			{ text: "This" },
+			{ text: " " },
+			{ text: "is" },
+			{ text: " " },
+			{ text: "a" },
+			{ text: " " },
+			{ text: " " },
+			{ text: "test" },
+			{ text: "." },
+		] );
+	} );
+
+	it( "should filter a shortcode with parameters from a tree", function() {
+		const tree = {
+			sentences: [
+				{
+					text: "This is a [shortcode param1='value1' param2='value2'] test.",
+					tokens: [
+						{ text: "This" },
+						{ text: " " },
+						{ text: "is" },
+						{ text: " " },
+						{ text: "a" },
+						{ text: " " },
+						{ text: "[" },
+						{ text: "shortcode" },
+						{ text: " " },
+						{ text: "param1='value1" },
+						{ text: "'" },
+						{ text: " " },
+						{ text: "param2='value1" },
+						{ text: "'" },
+						{ text: "]" },
+						{ text: " " },
+						{ text: "test" },
+						{ text: "." },
+					],
+				} ] };
+		const shortcodes = [ "shortcode" ];
+
+		filterShortcodesFromTree( tree, shortcodes );
+
+		expect( tree.sentences[ 0 ].tokens ).toEqual( [
+			{ text: "This" },
+			{ text: " " },
+			{ text: "is" },
+			{ text: " " },
+			{ text: "a" },
+			{ text: " " },
+			{ text: " " },
+			{ text: "test" },
+			{ text: "." },
+		] );
+	} );
+
+	it( "should filter multiple shortcodes from the same sentence", function() {
+		const tree = {
+			sentences: [
+				{
+					text: "This is a [shortcode] test with [another_shortcode] in it.",
+					tokens: [
+						{ text: "This" },
+						{ text: " " },
+						{ text: "is" },
+						{ text: " " },
+						{ text: "a" },
+						{ text: " " },
+						{ text: "[" },
+						{ text: "shortcode" },
+						{ text: "]" },
+						{ text: " " },
+						{ text: "test" },
+						{ text: " " },
+						{ text: "with" },
+						{ text: " " },
+						{ text: "[" },
+						{ text: "another_shortcode" },
+						{ text: "]" },
+						{ text: " " },
+						{ text: "in" },
+						{ text: " " },
+						{ text: "it" },
+						{ text: "." },
+					],
+				},
+			] };
+		const shortcodes = [ "shortcode", "another_shortcode" ];
+
+		filterShortcodesFromTree( tree, shortcodes );
+
+		expect( tree.sentences[ 0 ].tokens ).toEqual( [
+			{ text: "This" },
+			{ text: " " },
+			{ text: "is" },
+			{ text: " " },
+			{ text: "a" },
+			{ text: " " },
+			{ text: " " },
+			{ text: "test" },
+			{ text: " " },
+			{ text: "with" },
+			{ text: " " },
+			{ text: " " },
+			{ text: "in" },
+			{ text: " " },
+			{ text: "it" },
+			{ text: "." },
+		] );
+	} );
+
+	it( "should filter multiple shortcodes from different sentences", function() {
+		const tree = {
+			sentences: [
+				{
+					text: "This is a [shortcode] test.",
+					tokens: [
+						{ text: "This" },
+						{ text: " " },
+						{ text: "is" },
+						{ text: " " },
+						{ text: "a" },
+						{ text: " " },
+						{ text: "[" },
+						{ text: "shortcode" },
+						{ text: "]" },
+						{ text: " " },
+						{ text: "test" },
+						{ text: "." },
+					],
+				},
+				{
+					text: "This is a [another_shortcode] test.",
+					tokens: [
+						{ text: "This" },
+						{ text: " " },
+						{ text: "is" },
+						{ text: " " },
+						{ text: "a" },
+						{ text: " " },
+						{ text: "[" },
+						{ text: "another_shortcode" },
+						{ text: "]" },
+						{ text: " " },
+						{ text: "test" },
+						{ text: "." },
+					],
+				},
+			],
+		};
+
+		const shortcodes = [ "shortcode", "another_shortcode" ];
+
+		filterShortcodesFromTree( tree, shortcodes );
+
+		expect( tree.sentences[ 0 ].tokens ).toEqual( [
+			{ text: "This" },
+			{ text: " " },
+			{ text: "is" },
+			{ text: " " },
+			{ text: "a" },
+			{ text: " " },
+			{ text: " " },
+			{ text: "test" },
+			{ text: "." },
+		] );
+		expect( tree.sentences[ 1 ].tokens ).toEqual( [
+			{ text: "This" },
+			{ text: " " },
+			{ text: "is" },
+			{ text: " " },
+			{ text: "a" },
+			{ text: " " },
+			{ text: " " },
+			{ text: "test" },
+			{ text: "." },
+		] );
+	} );
+	it( "should filter a shortcode from a childnode.", function() {
+		const tree = {
+			childNodes: [
+				{
+					sentences: [
+						{
+							text: "This is a [shortcode] test.",
+							tokens: [
+								{ text: "This" },
+								{ text: " " },
+								{ text: "is" },
+								{ text: " " },
+								{ text: "a" },
+								{ text: " " },
+								{ text: "[" },
+								{ text: "shortcode" },
+								{ text: "]" },
+								{ text: " " },
+								{ text: "test" },
+								{ text: "." },
+							],
+
+						},
+					],
+				},
+			],
+		};
+
+		const shortcodes = [ "shortcode" ];
+
+		filterShortcodesFromTree( tree, shortcodes );
+
+		expect( tree.childNodes[ 0 ].sentences[ 0 ].tokens ).toEqual( [
+			{ text: "This" },
+			{ text: " " },
+			{ text: "is" },
+			{ text: " " },
+			{ text: "a" },
+			{ text: " " },
+			{ text: " " },
+			{ text: "test" },
+			{ text: "." },
+		] );
+	} );
+
+	it( "should not filter a word between square brackets if it is not a shortcode", function() {
+		const tree = {
+			sentences: [
+				{
+					text: "This is a test.",
+					tokens: [
+						{ text: "This" },
+						{ text: " " },
+						{ text: "is" },
+						{ text: " " },
+						{ text: "a" },
+						{ text: " " },
+						{ text: "test" },
+						{ text: "." },
+					],
+				},
+			],
+		};
+
+		const shortcodes = [ "shortcode" ];
+
+		filterShortcodesFromTree( tree, shortcodes );
+
+		expect( tree.sentences[ 0 ].tokens ).toEqual( [
+			{ text: "This" },
+			{ text: " " },
+			{ text: "is" },
+			{ text: " " },
+			{ text: "a" },
+			{ text: " " },
+			{ text: "test" },
+			{ text: "." },
+		] );
+	} );
+	it( "should not filter if no shortcodes are available", function() {
+		const tree = {
+			sentences: [
+				{
+					text: "This is a [shortcode] test.",
+					tokens: [
+						{ text: "This" },
+						{ text: " " },
+						{ text: "is" },
+						{ text: " " },
+						{ text: "a" },
+						{ text: " " },
+						{ text: "[" },
+						{ text: "shortcode" },
+						{ text: "]" },
+						{ text: " " },
+						{ text: "test" },
+						{ text: "." },
+					],
+
+				},
+			],
+		};
+
+		filterShortcodesFromTree( tree );
+
+		expect( tree.sentences[ 0 ].tokens ).toEqual( [
+			{ text: "This" },
+			{ text: " " },
+			{ text: "is" },
+			{ text: " " },
+			{ text: "a" },
+			{ text: " " },
+			{ text: "[" },
+			{ text: "shortcode" },
+			{ text: "]" },
+			{ text: " " },
+			{ text: "test" },
+			{ text: "." },
+		] );
+	} );
+} );
