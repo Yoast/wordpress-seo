79a032dd5f1e09465605379b61aaa06aa54ddb22
aidamarfuaty <aida@yoast.com>
2023-08-16 11:51:23 +0200

    Create a helper for styled description

---
+++ packages/js/src/helpers/styledDescription.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { colors } from "@yoast/style-guide";
+
+const StyledDescription = styled.legend`
+	margin: 16px 0;
+	padding: 0;
+	color: ${ colors.$color_headings };
+	font-size: 12px;
+	font-weight: 300;
+`;
+
+export default StyledDescription;
