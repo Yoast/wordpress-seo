872b99b80775677c49576ad131a72b6098c4404b
Paolo Luigi Scala <paolo@yoast.com>
2023-08-29 11:21:16 +0200

    Work in progress implementation

--- src/presenters/admin/sidebar-presenter.php
+++ src/presenters/admin/sidebar-presenter.php
@@ -16,6 +16,13 @@ class Sidebar_Presenter extends Abstract_Presenter {
 	 * @return string The sidebar HTML.
 	 */
 	public function present() {
+		$time       = \time();
+		$time_start = \gmmktime( 11, 00, 00, 11, 22, 2022 );
+		$time_end   = \gmmktime( 11, 00, 00, 11, 29, 2023 );
+
+		$cyber_monday_start = \gmmktime( 11, 00, 00, 11, 27, 2024 );
+		$title              = ( $time > $cyber_monday_start ) ? \__( 'CYBER MONDAY - 30% OFF', 'wordpress-seo' ) : \__( 'COOL TEST BLA BLA BLA BLA BLA BLA BLA BLA', 'wordpress-seo' );
+
 		$assets_uri              = \trailingslashit( \plugin_dir_url( \WPSEO_FILE ) );
 		$buy_yoast_seo_shortlink = WPSEO_Shortlinker::get( 'https://yoa.st/jj' );
 		\ob_start();
@@ -42,6 +49,17 @@ class Sidebar_Presenter extends Abstract_Presenter {
 									sizes="(min-width: 1321px) 75px">
 							</figure>
 						</figure>
+						<?php if ( ( $time > $time_start ) && ( $time < $time_end ) ) : ?>
+							<div class="sidebar__sale_banner_container">
+								<div class="sidebar__sale_banner">
+									<div class="plstest">
+
+											<?php echo \esc_html( $title ); ?>
+
+									</div>
+								</div>
+							</div>
+						<?php endif; ?>
 						<h2 class="yoast-get-premium-title">
 							<?php
 							/* translators: %1$s and %2$s expand to a span wrap to avoid linebreaks. %3$s expands to "Yoast SEO Premium". */
@@ -51,30 +69,39 @@ class Sidebar_Presenter extends Abstract_Presenter {
 						<p>
 							<?php
 							/* translators: %1$s expands to an opening strong tag, %2$s expands to a closing strong tag */
-							\printf( \esc_html__( 'Be more efficient in creating titles and meta descriptions with the help of AI. %1$sGet 24/7 support%2$s and boost your website’s visibility.', 'wordpress-seo' ), '<strong>', '</strong>' );
+							\printf( \esc_html__( 'Enhance your website’s visibility with Yoast SEO Premium. Unlock %1$shigher traffic%2$s and %1$smaximize conversions%2$s!', 'wordpress-seo' ), '<strong>', '</strong>' );
 							?>
 						</p>
+						<?php if ( ( $time > $time_start ) && ( $time < $time_end ) ) : ?>
+							<div class="sidebar__sale_text">
+								<p>
+									<?php
+									/* translators: %1$s expands to an opening strong tag, %2$s expands to a closing strong tag */
+									\printf( \esc_html__( '%1$s SAVE 30%% %2$s on your 12 month subscription', 'wordpress-seo' ), '<strong>', '</strong>' );
+									?>
+								</p>
+							</div>
+						<?php endif; ?>
 						<p class="plugin-buy-button">
 							<a class="yoast-button-upsell" data-action="load-nfd-ctb" data-ctb-id="f6a84663-465f-4cb5-8ba5-f7a6d72224b2" target="_blank" href="<?php echo \esc_url( $buy_yoast_seo_shortlink ); ?>">
 								<?php
 								/* translators: %s expands to Yoast SEO Premium */
 								\printf( \esc_html__( 'Get %s', 'wordpress-seo' ), 'Yoast SEO Premium' );
 								?>
-								<span aria-hidden="true" class="yoast-button-upsell__caret"></span>
 							</a>
 						</p>
 						<div class="review-container">
 							<a href="https://www.g2.com/products/yoast-yoast/reviews" target="_blank" rel="noopener">
-								<h3 class="title">
-									<?php \esc_html_e( 'Read reviews from real users', 'wordpress-seo' ); ?>
-								</h3>
+									<span class="claim">
+										<?php \esc_html_e( 'Read reviews from real users', 'wordpress-seo' ); ?>
+									</span>
 								<span class="rating">
-									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="22" width="22" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/logo-g2-white.svg' ); ?>">
-									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="22" width="22" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-star.svg' ); ?>">
-									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="22" width="22" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-star.svg' ); ?>">
-									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="22" width="22" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-star.svg' ); ?>">
-									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="22" width="22" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-star.svg' ); ?>">
-									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="22" width="22" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-half.svg' ); ?>">
+									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="22" width="22" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/g2_logo_white_optm.svg' ); ?>">
+									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="20" width="20" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-star.svg' ); ?>">
+									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="20" width="20" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-star.svg' ); ?>">
+									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="20" width="20" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-star.svg' ); ?>">
+									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="20" width="20" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-star.svg' ); ?>">
+									<img alt="" loading="lazy" fetchpriority="low" decoding="async" height="20" width="20" src="<?php echo \esc_url( $assets_uri . 'packages/js/images/star-rating-half.svg' ); ?>">
 									<span class="rating-text">4.6 / 5</span>
 
 								</span>
