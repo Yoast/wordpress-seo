e8281e2c6a9f06fdac51301aa43ee822adc5c1a6
Paolo Luigi Scala <paolo@yoast.com>
2023-08-30 12:45:07 +0200

    The class representing the Black Friday promotion

---
+++ src/promotions/domain/black-friday-promotion.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Yoast\WP\SEO\Promotions\Domain;
+
+
+/**
+ * Class to manage the Black Friday promotion.
+ *
+ * @makePublic
+ */
+class Black_Friday_Promotion extends Abstract_Promotion implements Promotion_Interface{
+
+	public function __construct() {
+		parent::__construct(
+			'black_friday_2023',
+			new Time_Interval( \gmmktime( 11, 00, 00, 12, 23, 2021 ), \gmmktime( 11, 00, 00, 12, 28, 2025 ) )
+		);
+	
+	}
+}
