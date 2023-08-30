4ee5b43e75d8ff5b43fc980396111d11d303122e
Paolo Luigi Scala <paolo@yoast.com>
2023-08-30 14:11:49 +0200

    Fix cs

--- src/promotions/domain/black-friday-promotion.php
+++ src/promotions/domain/black-friday-promotion.php
@@ -2,19 +2,20 @@
 
 namespace Yoast\WP\SEO\Promotions\Domain;
 
-
 /**
  * Class to manage the Black Friday promotion.
  *
  * @makePublic
  */
-class Black_Friday_Promotion extends Abstract_Promotion implements Promotion_Interface{
+class Black_Friday_Promotion extends Abstract_Promotion implements Promotion_Interface {
 
+	/**
+	 * Class constructor.
+	 */
 	public function __construct() {
 		parent::__construct(
 			'black_friday_2023',
 			new Time_Interval( \gmmktime( 11, 00, 00, 12, 23, 2021 ), \gmmktime( 11, 00, 00, 12, 28, 2025 ) )
 		);
-	
 	}
 }
