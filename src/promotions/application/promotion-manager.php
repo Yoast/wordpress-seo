be995ef06e2c6615502df7d5ddcc08503ce04ff1
Paolo Luigi Scala <paolo@yoast.com>
2023-08-30 12:45:34 +0200

    The centralized class managing the promotions

---
+++ src/promotions/application/promotion-manager.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Yoast\WP\SEO\Promotions\Application;
+
+use Yoast\WP\SEO\Promotions\Domain\Promotion_Interface;
+
+/**
+ * Class to manage promotional promotions.
+ *
+ * @makePublic
+ */
+class Promotion_Manager implements Promotion_Manager_Interface {
+
+	/**
+	 * The centralized list of promotions: all promotions should be passed to the constructor.
+	 *
+	 * @var array
+	 */
+	private $promotions_list = [];
+
+	/**
+	 * Class constructor.
+	 */
+	public function __construct( Promotion_Interface ...$promotions ) {
+		$this->promotions_list = $promotions;
+	}
+
+	/**
+	 * Whether the promotion is effective.
+	 *
+	 * @param string $promotion_name The name of the promotion.
+	 *
+	 * @return bool Whether the promotion is effective.
+	 */
+	public function is( string $promotion_name ) {
+		$time = \time();
+
+		foreach ( $this->promotions_list as $promotion ) {
+			if ( $promotion->get_promotion_name() === $promotion_name ) {
+				return $promotion->get_time_interval()->contains( $time );
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * Get the list of promotions.
+	 *
+	 * @return array The list of promotions.
+	 */
+	public function get_promotions_list() {
+		return $this->promotions_list;
+	}
+}
