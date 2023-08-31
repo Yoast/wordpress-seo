ac38c3a04fdd40fe3cf846b1bd72f765a2b9711f
boblinthorst <bob@yoast.com>
2023-08-31 14:56:21 +0200

    Add another test, and improve comments

--- tests/integration/inc/test-class-addon-manager.php
+++ tests/integration/inc/test-class-addon-manager.php
@@ -12,15 +12,9 @@
  */
 class WPSEO_addon_manager_test extends WPSEO_UnitTestCase {
 
-//	/**
-//	 * @return void garbage
-//	 */
-//	public function test_called_at_all() {
-//		$this->assertEquals( 3, 1 + 2 );
-//	}
-
 	/**
-	 * @return void
+	 * This test assumes that, on my.yoast.com. There is no active license for a registered site with url:
+	 * https://thisDoesNotHaveAnyLicensesForeverNoBacksies.com
 	 *
 	 * @covers WPSEO_Addon_Manager::get_myyoast_site_information
 	 */
@@ -29,15 +23,35 @@ class WPSEO_addon_manager_test extends WPSEO_UnitTestCase {
 		update_option( 'home', $site  );
 
 		$addon_manager = new WPSEO_Addon_Manager();
-		$addons = $addon_manager->get_myyoast_site_information();
-
-		var_dump( $addons );
+		$actual = $addon_manager->get_myyoast_site_information();
 
 		$expected_site_information = (object) [
 			'url'           => $site,
 			'subscriptions' => [],
 		];
 
-		$this->assertEquals( $addons, $expected_site_information );
+		$this->assertEquals( $actual, $expected_site_information );
+	}
+
+	/**
+	 * This test assumes that, on my.yoast.com. There is an active license for all plugins for our blog.:
+	 *
+	 * @covers WPSEO_Addon_Manager::get_myyoast_site_information
+	 */
+	public function test_finds_all_licenses_for_yoast_dot_com() {
+		$site = 'https://yoast.com';
+		update_option( 'home', $site  );
+
+		$addon_manager = new WPSEO_Addon_Manager();
+		$actual = $addon_manager->get_myyoast_site_information();
+
+		// Might be nicer to put the entire expected blob here. But wanted to put something here that does not fail at the slightest change in interface.
+		$expected_slugs = [ "yoast-seo-local", "yoast-seo-video", "yoast-seo-woocommerce", "yoast-seo-news", "yoast-seo-wordpress-premium" ];
+
+		$slugs = array_map( function( $subscription ) {
+			return $subscription->product->slug;
+		}, $actual->subscriptions );
+
+		$this->assertEquals( $expected_slugs, $slugs );
 	}
 }
