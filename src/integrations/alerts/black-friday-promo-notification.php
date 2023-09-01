58c780f55b28b8b83fc47127d0c117a398af27fd
Vraja Das <vraja.pro@gmail.com>
2023-09-01 12:59:30 +0200

    Add the dismissal functionality

---
+++ src/integrations/alerts/black-friday-promo-notification.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Yoast\WP\SEO\Integrations\Alerts;
+
+/**
+ * Black_Friday_Promo_Notification class.
+ */
+class Black_Friday_Promo_Notification extends Abstract_Dismissable_Alert {
+
+	/**
+	 * Holds the alert identifier.
+	 *
+	 * @var string
+	 */
+	protected $alert_identifier = 'black-friday-promo-notification';
+}
