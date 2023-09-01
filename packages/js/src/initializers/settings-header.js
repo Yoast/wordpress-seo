3561db3d3fee7f468538828bbc67e0bd0fc276bf
Vraja Das <vraja.pro@gmail.com>
2023-09-01 14:11:31 +0200

    add BF ad for admin pages

--- packages/js/src/initializers/settings-header.js
+++ packages/js/src/initializers/settings-header.js
@@ -2,6 +2,8 @@ import { get } from "lodash";
 import { render } from "@wordpress/element";
 import { ThemeProvider } from "styled-components";
 import WebinarPromoNotification from "../components/WebinarPromoNotification";
+import BlackFridayPromoNotification from "../components/BlackFridayPromoNotification";
+
 
 /**
  * Initializes the React settings header, just below the title.
@@ -12,11 +14,16 @@ const initSettingsHeader = () => {
 	const reactRoot = document.getElementById( "yst-settings-header-root" );
 	const isRtl = Boolean( get( window, "wpseoScriptData.isRtl", false ) );
 	const webinarIntroSettingsUrl = get( window, "wpseoScriptData.webinarIntroSettingsUrl", "https://yoa.st/webinar-intro-settings" );
+	const blackFridayBlockEditorUrl = get( window, "wpseoScriptData.blackFridayBlockEditorUrl", "https://yoa.st/black-friday-checklist" );
+	const isWooCommerce = get( window, "wpseoScriptData.isWooCommerceActive", "" );
 
 	if ( reactRoot ) {
 		render(
 			<ThemeProvider theme={ { isRtl } }>
-				<WebinarPromoNotification store="yoast-seo/settings" url={ webinarIntroSettingsUrl } />
+				{ ! blackFridayBlockEditorUrl &&
+				<WebinarPromoNotification store="yoast-seo/settings" url={ webinarIntroSettingsUrl } /> }
+				{ isWooCommerce && blackFridayBlockEditorUrl &&
+				<BlackFridayPromoNotification store="yoast-seo/settings" hasIcon={ false } url={ blackFridayBlockEditorUrl } /> }
 			</ThemeProvider>,
 			reactRoot
 		);
