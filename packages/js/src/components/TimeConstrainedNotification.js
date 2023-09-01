7111a5d2beee5f8fab54b252fcef233f098cbddc
Paolo Luigi Scala <paolo@yoast.com>
2023-09-01 13:47:33 +0200

    A time-constrained notification

---
+++ packages/js/src/components/TimeConstrainedNotification.js
@@ -0,0 +1,37 @@
+import { __ } from "@wordpress/i18n";
+import PropTypes from "prop-types";
+
+import PersistentDismissableNotification from "../containers/PersistentDismissableNotification";
+
+/**
+ * @param {string} store The Redux store identifier from which to determine dismissed state.
+ * @param {string} url The URL for the register now link.
+ *
+ * @returns {JSX.Element} The TimeConstrainedNotification component.
+ */
+export const TimeConstrainedNotification = ( {
+	store = "yoast-seo/editor",
+	url,
+	...props
+} ) => {
+	return (
+		<PersistentDismissableNotification
+			alertKey="promotion-notification"
+			store={ store }
+			id="promotion-notification"
+			title={ __( "Is your WooCommerce store ready for Black Friday?", "wordpress-seo" ) }
+			url={ url }
+			{ ...props }
+		>
+			{ __( "Get help with maximizing your upcoming Black Friday sales! This ultimate Black Friday checklist helps you prepare for Black Friday and boost your sales. Get the checklist now!", "wordpress-seo" ) }
+			&nbsp;<a href={ url } target="_blank" rel="noreferrer">
+				{ __( "Get the checklist now!", "wordpress-seo" ) }
+			</a>
+		</PersistentDismissableNotification>
+	);
+};
+
+TimeConstrainedNotification.propTypes = {
+	store: PropTypes.string,
+	url: PropTypes.string.isRequired,
+};
