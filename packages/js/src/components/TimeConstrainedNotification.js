e60e76616af939d74763c52593ab889f06f807ef
Paolo Luigi Scala <paolo@yoast.com>
2023-09-04 13:52:47 +0200

    Make the notification reusable

--- packages/js/src/components/TimeConstrainedNotification.js
+++ packages/js/src/components/TimeConstrainedNotification.js
@@ -1,37 +1,45 @@
 import { __ } from "@wordpress/i18n";
 import PropTypes from "prop-types";
-
+import { select } from "@wordpress/data";
 import PersistentDismissableNotification from "../containers/PersistentDismissableNotification";
+import { ReactComponent as DefaultImage } from "../../../../images/succes_marieke_bubble_optm.svg";
 
 /**
- * @param {string} store The Redux store identifier from which to determine dismissed state.
- * @param {string} url The URL for the register now link.
+ * @param {string}      store The Redux store identifier from which to determine dismissed state.
+ * @param {JSX.Element} image The image or null if no image.
+ * @param {string}      title The title of the notification.
+ * @param {string}      promoId The promotion id.
+ * @param {JSX.node}    children The content of the notification.
  *
  * @returns {JSX.Element} The TimeConstrainedNotification component.
  */
 export const TimeConstrainedNotification = ( {
 	store = "yoast-seo/editor",
-	url,
+	image: Image = DefaultImage,
+	promoId,
+	title,
+	children,
 	...props
 } ) => {
+	const promotionActive = select( store ).isPromotionActive( promoId );
 	return (
-		<PersistentDismissableNotification
-			alertKey="time-constrained-notification"
+		promotionActive && <PersistentDismissableNotification
+			alertKey={ promoId }
 			store={ store }
 			id="time-constrained-notification"
-			title={ __( "Is your WooCommerce store ready for Black Friday?", "wordpress-seo" ) }
-			url={ url }
+			title={ title }
+			image={ Image }
 			{ ...props }
 		>
-			{ __( "Get help with maximizing your upcoming Black Friday sales! This ultimate Black Friday checklist helps you prepare for Black Friday and boost your sales. Get the checklist now!", "wordpress-seo" ) }
-			&nbsp;<a href={ url } target="_blank" rel="noreferrer">
-				{ __( "Get the checklist now!", "wordpress-seo" ) }
-			</a>
+			{ children }
 		</PersistentDismissableNotification>
 	);
 };
 
 TimeConstrainedNotification.propTypes = {
 	store: PropTypes.string,
-	url: PropTypes.string.isRequired,
+	image: PropTypes.elementType,
+	promoId: PropTypes.string.isRequired,
+	title: PropTypes.string.isRequired,
+	children: PropTypes.node,
 };
