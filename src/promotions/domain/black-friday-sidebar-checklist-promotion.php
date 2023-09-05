c0551734bacf3b30456758c760950a9dced43f33
Paolo Luigi Scala <paolo@yoast.com>
2023-09-05 12:34:27 +0200

    Use correct time frame for promotions

--- src/promotions/domain/black-friday-sidebar-checklist-promotion.php
+++ src/promotions/domain/black-friday-sidebar-checklist-promotion.php
@@ -3,18 +3,18 @@
 namespace Yoast\WP\SEO\Promotions\Domain;
 
 /**
- * Class to manage the Black Friday promotion.
+ * Class to manage the Black Friday promotion in the sidebar.
  *
  * @makePublic
  */
-class Black_Friday_Checklist_Promotion extends Abstract_Promotion implements Promotion_Interface {
+class Black_Friday_Sidebar_Checklist_Promotion extends Abstract_Promotion implements Promotion_Interface {
 
 	/**
 	 * Class constructor.
 	 */
 	public function __construct() {
 		parent::__construct(
-			'black_friday_checklist_promotion',
+			'bf_sidebar_checklist_promotion',
 			new Time_Interval(
 				\gmmktime( 11, 00, 00, 9, 19, 2023 ),
 				\gmmktime( 11, 00, 00, 11, 25, 2023 )
