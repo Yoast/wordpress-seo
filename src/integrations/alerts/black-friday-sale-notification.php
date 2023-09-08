0ca006ef4d8782f8717f9afc510e3efc7c337ea2
Paolo Luigi Scala <paolo@yoast.com>
2023-09-08 15:17:13 +0200

    Add integration to dismiss notification

---
+++ src/integrations/alerts/black-friday-sale-notification.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Yoast\WP\SEO\Integrations\Alerts;
+
+/**
+ * Black_Friday_Sale_Notification class.
+ */
+class Black_Friday_Sale_Notification extends Abstract_Dismissable_Alert {
+
+	/**
+	 * Holds the alert identifier.
+	 *
+	 * @var string
+	 */
+	protected $alert_identifier = 'black-friday-sale-notification';
+}
