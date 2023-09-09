9fc2b604909adaf4a4ed61228ac22eb0f7bfa872
Paolo Luigi Scala <paolo@yoast.com>
2023-09-10 01:14:47 +0200

    Remove unused prop

--- packages/js/src/components/BlackFridayProductEditorChecklistPromo.js
+++ packages/js/src/components/BlackFridayProductEditorChecklistPromo.js
@@ -17,7 +17,6 @@ export const BlackFridayProductEditorChecklistPromo = () => {
 	return (
 		<TimeConstrainedNotification
 			title={ __( "Is your WooCommerce store ready for Black Friday?", "wordpress-seo" ) }
-			shouldCheckForWarnings={ true }
 			promoId="black_friday_2023_checklist"
 			alertKey="black-friday-2023-product-editor-checklist"
 			store="yoast-seo/editor"
