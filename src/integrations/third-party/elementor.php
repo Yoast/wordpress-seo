e68ae5fd6c5e9361172c17a3ae0b9566259f904e
Paolo Luigi Scala <paolo@yoast.com>
2023-09-11 13:09:38 +0200

    Use service instead of injecting the Promotion_Manager

--- src/integrations/third-party/elementor.php
+++ src/integrations/third-party/elementor.php
@@ -26,7 +26,7 @@ use Yoast\WP\SEO\Helpers\Options_Helper;
 use Yoast\WP\SEO\Integrations\Integration_Interface;
 use Yoast\WP\SEO\Introductions\Infrastructure\Wistia_Embed_Permission_Repository;
 use Yoast\WP\SEO\Presenters\Admin\Meta_Fields_Presenter;
-use Yoast\WP\SEO\Promotions\Application\Promotion_Manager_Interface;
+use Yoast\WP\SEO\Promotions\Application\Promotion_Manager;
 
 /**
  * Integrates the Yoast SEO metabox in the Elementor editor.
@@ -102,13 +102,6 @@ class Elementor implements Integration_Interface {
 	protected $inclusive_language_analysis;
 
 	/**
-	 * The promotions manager.
-	 *
-	 * @var Promotion_Manager_Interface
-	 */
-	private $promotion_manager;
-
-	/**
 	 * Returns the conditionals based in which this loadable should be active.
 	 *
 	 * @return array
@@ -123,18 +116,15 @@ class Elementor implements Integration_Interface {
 	 * @param WPSEO_Admin_Asset_Manager   $asset_manager The asset manager.
 	 * @param Options_Helper              $options       The options helper.
 	 * @param Capability_Helper           $capability    The capability helper.
-	 * @param Promotion_Manager_Interface $promotion_manager The promotions manager.
 	 */
 	public function __construct(
 		WPSEO_Admin_Asset_Manager $asset_manager,
 		Options_Helper $options,
-		Capability_Helper $capability,
-		Promotion_Manager_Interface $promotion_manager
+		Capability_Helper $capability
 	) {
 		$this->asset_manager     = $asset_manager;
 		$this->options           = $options;
 		$this->capability        = $capability;
-		$this->promotion_manager = $promotion_manager;
 
 		$this->seo_analysis                 = new WPSEO_Metabox_Analysis_SEO();
 		$this->readability_analysis         = new WPSEO_Metabox_Analysis_Readability();
@@ -468,7 +458,7 @@ class Elementor implements Integration_Interface {
 			],
 			'dismissedAlerts'           => $dismissed_alerts,
 			'webinarIntroElementorUrl'  => WPSEO_Shortlinker::get( 'https://yoa.st/webinar-intro-elementor' ),
-			'blackFridayBlockEditorUrl' => ( $this->promotion_manager->is( 'black_friday_2023_checklist' ) ) ? WPSEO_Shortlinker::get( 'https://yoa.st/black-friday-checklist' ) : '',
+			'currentPromotions'          => YoastSEO()->classes->get( Promotion_Manager::class )->get_current_promotions(),
 			'usedKeywordsNonce'         => \wp_create_nonce( 'wpseo-keyword-usage-and-post-types' ),
 			'linkParams'                => WPSEO_Shortlinker::get_query_params(),
 			'pluginUrl'                 => \plugins_url( '', \WPSEO_FILE ),
