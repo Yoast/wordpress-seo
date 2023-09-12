ed141f45e5dd64e402dab2aad2be37889d218dd8
Paolo Luigi Scala <paolo@yoast.com>
2023-09-12 17:13:24 +0200

    Fix evaluating length on null

--- packages/js/src/helpers/checkForMetaboxWarnings.js
+++ packages/js/src/helpers/checkForMetaboxWarnings.js
@@ -7,7 +7,7 @@ import { select } from "@wordpress/data";
 export const checkForMetaboxWarnings = () => {
 	const isPremium = select( "yoast-seo/editor" ).getIsPremium();
 	const warningsFree = select( "yoast-seo/editor" ).getWarningMessage();
-	const warningsPremium = isPremium ? select( "yoast-seo-premium/editor" )?.getMetaboxWarning() ?? [] : false;
+	const warningsPremium = isPremium ? select( "yoast-seo-premium/editor" )?.getMetaboxWarning() ?? [] : [];
 
 	return warningsPremium.length > 0 || warningsFree.length > 0;
 };
