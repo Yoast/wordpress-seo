96a72b6a4c549f9f786fae60deb5c9ae18f84dbc
Paolo Luigi Scala <paolo@yoast.com>
2023-09-15 12:08:52 +0200

    Fix cs

--- config/webpack/externals.js
+++ config/webpack/externals.js
@@ -1,12 +1,12 @@
 const {
 	camelCaseDash,
-} = require("@wordpress/dependency-extraction-webpack-plugin/lib/util");
+} = require( "@wordpress/dependency-extraction-webpack-plugin/lib/util" );
 
 /**
  * Yoast dependencies, declared as such in the package.json.
  */
-const { dependencies } = require("../../packages/js/package");
-const legacyYoastPackages = ["yoast-components", "yoastseo"];
+const { dependencies } = require( "../../packages/js/package" );
+const legacyYoastPackages = [ "yoast-components", "yoastseo" ];
 const additionalPackages = [
 	"draft-js",
 	"styled-components",
@@ -21,20 +21,20 @@ const additionalPackages = [
 const YOAST_PACKAGE_NAMESPACE = "@yoast/";
 
 // Fetch all packages from the dependencies list.
-const yoastPackages = Object.keys(dependencies).filter(
-	(packageName) =>
-		packageName.startsWith(YOAST_PACKAGE_NAMESPACE) ||
-		legacyYoastPackages.includes(packageName) ||
-		additionalPackages.includes(packageName)
+const yoastPackages = Object.keys( dependencies ).filter(
+	( packageName ) =>
+		packageName.startsWith( YOAST_PACKAGE_NAMESPACE ) ||
+		legacyYoastPackages.includes( packageName ) ||
+		additionalPackages.includes( packageName )
 );
 
 /**
  * Convert Yoast packages to externals configuration.
  */
-const yoastExternals = yoastPackages.reduce((memo, packageName) => {
-	let useablePackageName = packageName.replace(YOAST_PACKAGE_NAMESPACE, "");
+const yoastExternals = yoastPackages.reduce( ( memo, packageName ) => {
+	let useablePackageName = packageName.replace( YOAST_PACKAGE_NAMESPACE, "" );
 
-	switch (useablePackageName) {
+	switch ( useablePackageName ) {
 		case "components":
 			useablePackageName = "components-new";
 			break;
@@ -49,9 +49,9 @@ const yoastExternals = yoastPackages.reduce((memo, packageName) => {
 			break;
 	}
 
-	memo[packageName] = camelCaseDash(useablePackageName);
+	memo[ packageName ] = camelCaseDash( useablePackageName );
 	return memo;
-}, {});
+}, {} );
 
 module.exports = {
 	YOAST_PACKAGE_NAMESPACE,
