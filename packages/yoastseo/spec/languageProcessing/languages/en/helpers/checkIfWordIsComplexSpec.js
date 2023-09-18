c2fb7c1bc6e19c7941073fce7356a73ac17cb45d
Martijn van der Klis <martijnvanderklis@yoast.com>
2023-09-18 10:13:30 +0200

    Adds the morphology data to the helper

--- packages/yoastseo/spec/languageProcessing/languages/en/helpers/checkIfWordIsComplexSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/en/helpers/checkIfWordIsComplexSpec.js
@@ -2,51 +2,54 @@ import checkIfWordIsComplex from "../../../../../src/languageProcessing/language
 import wordComplexityConfig from "../../../../../src/languageProcessing/languages/en/config/wordComplexity";
 import getMorphologyData from "../../../../specHelpers/getMorphologyData";
 
+const morphologyData = getMorphologyData( "en" ).en;
+
 describe( "a test checking if the word is complex in English",  function() {
 	it( "returns singular word as non-complex if it is found in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "example", getMorphologyData( "en" ) ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "example", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns word as non-complex if its singular version is found in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "examples", getMorphologyData( "en" ) ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "examples", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns singular word as non-complex if it is found in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "release", getMorphologyData( "en" ) ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "release", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns plural word as non-complex if its singular version is found in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "releases", getMorphologyData( "en" ) ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "releases", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns plural (with phonetical) change word as non-complex if its singular version is found in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "opportunities", getMorphologyData( "en" ) ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "opportunities" ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "opportunities", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns long irregular plural word as complex if its singular version is not found in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "metamorphoses", getMorphologyData( "en" ) ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "metamorphoses", morphologyData ) ).toEqual( true );
 	} );
 
 	it( "returns long plural word as complex if its singular version is not found in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "anesthesias", getMorphologyData( "en" ) ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "anesthesias", morphologyData ) ).toEqual( true );
 	} );
 
 	it( "returns plural word as complex if it is not in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "refrigerators", getMorphologyData( "en" ) ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "refrigerators", morphologyData ) ).toEqual( true );
 	} );
 
 	it( "returns plural word as non complex if the word starts with capital letter", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "Tortoiseshell", getMorphologyData( "en" ) ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "Outsiders", getMorphologyData( "en" ) ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "Tortoiseshell", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "Outsiders", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns words as non complex if the word is less than 7 characters", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "cat", getMorphologyData( "en" ) ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "rabbit", getMorphologyData( "en" ) ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "cat", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "rabbit", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns words as non complex if the word is less than 7 characters", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "cat", getMorphologyData( "en" ) ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "rabbit", getMorphologyData( "en" ) ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "cat", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "rabbit", morphologyData ) ).toEqual( false );
 	} );
 } );
--- packages/yoastseo/src/languageProcessing/languages/en/helpers/checkIfWordIsComplex.js
+++ packages/yoastseo/src/languageProcessing/languages/en/helpers/checkIfWordIsComplex.js
@@ -1,6 +1,4 @@
 import { languageProcessing } from "yoastseo";
-import { isUndefined } from "lodash-es";
-import { endsWithIng } from "./internal/getVerbStem";
 const { buildFormRule, createRulesFromArrays } = languageProcessing;
 
 /**
@@ -9,7 +7,7 @@ const { buildFormRule, createRulesFromArrays } = languageProcessing;
  *
  * @param {object} config The configuration needed for assessing the word's complexity, e.g., the frequency list.
  * @param {string} word The word to check.
- * @param {Object} morphologyData morphology data for the language
+ * @param {Object} [morphologyData] Optional morphology data.
  *
  * @returns {boolean} Whether or not a word is complex.
  */
@@ -24,34 +22,21 @@ export default function checkIfWordIsComplex( config, word, morphologyData ) {
 		return false;
 	}
 
+	// The word is not complex if it starts with a capital and thus is assumed to be a named entity.
+	if ( doesUpperCaseDecreaseComplexity && word[ 0 ].toLowerCase() !== word[ 0 ] ) {
+		return false;
+	}
+
 	// The word is not complex if it's in the frequency list.
 	if ( frequencyList.includes( word ) ) {
 		return false;
 	}
-	/*
-	 * In English where capital letter beginning decreases the complexity of a word,
-	 * word longer than 7 characters is not complex if it starts with capital letter.
-	 */
-	if ( doesUpperCaseDecreaseComplexity === true && word[ 0 ].toLowerCase() === word[ 0 ] ) {
-		/*
-		 * If a word is longer than 7 characters and doesn't start with capital letter,
-		 * we check further whether it is a plural ending in -s. If it is, we singularize it using the stemmer
-		 * and check if the singular word can be found in the frequency list.
-		 * The word is not complex if the singular form is in the list.
-		 */
-		const regexVerb = morphologyData.verbs.regexVerb;
-		const singularizeIfPlural = buildFormRule( word, createRulesFromArrays( morphologyData.nouns.regexNoun.singularize ) );
-		if ( ! isUndefined( singularizeIfPlural ) ) {
-			// Bring ing-nouns to base forms ("blessings" -> "bless").
-			if ( endsWithIng( singularizeIfPlural ) ) {
-				word = buildFormRule( singularizeIfPlural, createRulesFromArrays( regexVerb.ingFormToInfinitive ) );
-			}
-			word = singularizeIfPlural;
-		}
-		if ( ! frequencyList.includes( word ) ) {
-			return true;
-		}
-		return true;
+
+	// The word is not complex if it's singular form is in the frequency list.
+	if ( morphologyData ) {
+		const singular = buildFormRule( word, createRulesFromArrays( morphologyData.nouns.regexNoun.singularize ) );
+		return ! frequencyList.includes( singular );
 	}
-	return false;
+
+	return true;
 }
