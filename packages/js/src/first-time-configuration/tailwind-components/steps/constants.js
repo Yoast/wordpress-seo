0be2bfcb8af11e8d39ab72b98c8b250315900ca0
Paolo Luigi Scala <paolo@yoast.com>
2023-09-19 17:07:33 +0200

    Move steps names here and introduce HIIVE_STEPS_NAMES

---
+++ packages/js/src/first-time-configuration/tailwind-components/steps/constants.js
@@ -0,0 +1,12 @@
+export const STEPS = {
+	optimizeSeoData: "optimizeSeoData",
+	siteRepresentation: "siteRepresentation",
+	socialProfiles: "socialProfiles",
+	personalPreferences: "personalPreferences",
+};
+export const HIIVE_STEPS_NAMES = {
+	[ STEPS.optimizeSeoData ]: "data optimization",
+	[ STEPS.siteRepresentation ]: "Site representation",
+	[ STEPS.socialProfiles ]: "social profiles",
+	[ STEPS.personalPreferences ]: "personal preferences",
+};
