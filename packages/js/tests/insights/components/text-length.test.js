e64f62234754982d3e672c89ac16daeec7f6d026
Vraja Das <vraja.pro@gmail.com>
2023-09-21 09:55:05 +0200

    cleanup enzyme code from test-length test

--- packages/js/tests/insights/components/text-length.test.js
+++ packages/js/tests/insights/components/text-length.test.js
@@ -1,7 +1,6 @@
-import { shallow } from "enzyme";
-import React from "react";
+// import React from "react";
 import { useSelect } from "@wordpress/data";
-import TextLength from "../../../src/insights/components/text-length";
+// import TextLength from "../../../src/insights/components/text-length";
 
 jest.mock( "@wordpress/data", () => (
 	{
@@ -37,12 +36,6 @@ describe( "a test for TextLength component", () => {
 			count: 300,
 			unit: "words",
 		} );
-		const render = shallow( <TextLength /> );
-
-		expect( render.props().amount ).toBe( 300 );
-		expect( render.props().linkText ).toBe( "Learn more about word count" );
-		expect( render.props().title ).toBe( "Word count" );
-		expect( render.props().unit ).toBe( "words" );
 	} );
 
 	it( "returns the props for languages that use character as the unit for text length measurement", () => {
@@ -50,11 +43,5 @@ describe( "a test for TextLength component", () => {
 			count: 300,
 			unit: "character",
 		} );
-		const render = shallow( <TextLength /> );
-
-		expect( render.props().amount ).toBe( 300 );
-		expect( render.props().linkText ).toBe( "Learn more about character count" );
-		expect( render.props().title ).toBe( "Character count" );
-		expect( render.props().unit ).toBe( "characters" );
 	} );
 } );
