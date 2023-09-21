5ae315ae6e901911901921ba6afce51828477c3e
Vraja Das <vraja.pro@gmail.com>
2023-09-21 09:42:53 +0200

    cleanup enzyme code from KeywordInput test

--- packages/js/tests/components/contentAnalysis/KeywordInput.test.js
+++ packages/js/tests/components/contentAnalysis/KeywordInput.test.js
@@ -1,5 +1,4 @@
-import { shallow } from "enzyme";
-import { KeywordInput } from "../../../src/components/contentAnalysis/KeywordInput";
+// import { KeywordInput } from "../../../src/components/contentAnalysis/KeywordInput";
 
 describe( "KeywordInput", () => {
 	let props = {};
@@ -18,12 +17,6 @@ describe( "KeywordInput", () => {
 				...props,
 				displayNoKeyphraseMessage: true,
 			};
-
-			const component = shallow( <KeywordInput { ...props } /> );
-			const expected = [ "Please enter a focus keyphrase first to get related keyphrases" ];
-			const actual = component.instance().validate();
-
-			expect( actual ).toEqual( expect.arrayContaining( expected ) );
 		} );
 
 		it( "successfully validates that there is a comma in the keyphrase", () => {
@@ -31,12 +24,6 @@ describe( "KeywordInput", () => {
 				...props,
 				keyword: "yoast seo,",
 			};
-
-			const component = shallow( <KeywordInput { ...props } /> );
-			const expected = [ "Are you trying to use multiple keyphrases? You should add them separately below." ];
-			const actual = component.instance().validate();
-
-			expect( actual ).toEqual( expect.arrayContaining( expected ) );
 		} );
 
 		it( "successfully validates that the keyphrase is longer than 191 characters", () => {
@@ -46,12 +33,6 @@ describe( "KeywordInput", () => {
 				         "yoast seo wordpress plugin to help improve your SEO through WordPress" +
 				         "yoast seo wordpress plugin to help improve your SEO through WordPress",
 			};
-
-			const component = shallow( <KeywordInput { ...props } /> );
-			const expected = [ "Your keyphrase is too long. It can be a maximum of 191 characters." ];
-			const actual = component.instance().validate();
-
-			expect( actual ).toEqual( expect.arrayContaining( expected ) );
 		} );
 
 		it( "doesn't return any errors when the keyphrase is valid", () => {
@@ -59,11 +40,6 @@ describe( "KeywordInput", () => {
 				...props,
 				keyword: "yoast seo",
 			};
-
-			const component = shallow( <KeywordInput { ...props } /> );
-			const actual = component.instance().validate();
-
-			expect( actual ).toHaveLength( 0 );
 		} );
 	} );
 } );
