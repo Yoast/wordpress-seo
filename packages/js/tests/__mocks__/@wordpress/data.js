9b409cf1a26ef03895a568d43f78636a54ffb321
Vraja Das <vraja.pro@gmail.com>
2023-09-21 16:46:02 +0200

    remove @wordpress/data mock

--- packages/js/tests/__mocks__/@wordpress/data.js
+++
@@ -1,8 +0,0 @@
-let mockSelectors = {};
-
-module.exports = {
-	__setMockSelectors: selectors => {
-		mockSelectors = { ...selectors };
-	},
-	useSelect: select => select( () => mockSelectors ),
-};
