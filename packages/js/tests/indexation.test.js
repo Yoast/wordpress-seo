7b648edfb17919bc37726627ebe2d5148fbbd30e
Vraja Das <vraja.pro@gmail.com>
2023-09-21 09:50:35 +0200

    Cleanup enzyme code from Indexation test

--- packages/js/tests/indexation.test.js
+++ packages/js/tests/indexation.test.js
@@ -1,6 +1,5 @@
-import { mount } from "enzyme";
-import { act } from "react-dom/test-utils";
-import Indexation from "../src/components/Indexation";
+// import { act } from "react-dom/test-utils";
+// import Indexation from "../src/components/Indexation";
 
 /**
  * Fetch mock response.
@@ -18,17 +17,6 @@ describe( "Indexation", () => {
 		global.yoastIndexingData = {
 			disabled: true,
 		};
-
-		const component = mount( <Indexation /> );
-
-		const button = component.find( "button" );
-
-		expect( button.text() ).toEqual( "Start SEO data optimization" );
-		expect( button.props().disabled ).toBe( true );
-
-		const alert = component.find( "Alert" );
-
-		expect( alert.text() ).toEqual( "SEO data optimization is disabled for non-production environments." );
 	} );
 
 	it( "will show you when the indexation is complete", async() => {
@@ -57,31 +45,6 @@ describe( "Indexation", () => {
 
 			return Promise.reject();
 		} );
-
-		const component = mount( <Indexation /> );
-
-		component.find( "button" ).simulate( "click" );
-
-		const progressBar = component.find( "ProgressBar" );
-		expect( progressBar.prop( "value" ) ).toEqual( 0 );
-		expect( progressBar.prop( "max" ) ).toEqual( 5 );
-
-		await act( async() => {
-			component.update();
-		} );
-
-		component.update();
-
-		expect( global.fetch ).toHaveBeenCalledWith( "https://example.com/indexing-endpoint", {
-			headers: {
-				"X-WP-Nonce": "nonsense",
-			},
-			method: "POST",
-		} );
-
-		const alert = component.find( "Alert" );
-		expect( alert.text() ).toEqual( "SEO data optimization complete" );
-		expect( alert.prop( "type" ) ).toEqual( "success" );
 	} );
 
 	it( "shows an error when something goes wrong", async() => {
@@ -102,18 +65,6 @@ describe( "Indexation", () => {
 		global.fetch = jest.fn( () => (
 			Promise.reject( new Error( "Request failed!" ) ) )
 		);
-
-		const component = mount( <Indexation /> );
-
-		await act( async() => {
-			component.find( "button" ).simulate( "click" );
-		} );
-
-		component.update();
-
-		const alert = component.find( "Alert" );
-
-		expect( alert.prop( "type" ) ).toEqual( "error" );
 	} );
 
 	it( "executes registered pre- and postindexing actions", async() => {
@@ -143,26 +94,7 @@ describe( "Indexation", () => {
 			return Promise.reject();
 		} );
 
-		const preIndexingAction = jest.fn();
-		const postIndexingAction = jest.fn();
-
-		const component = mount( <Indexation
-			preIndexingActions={ { indexation: preIndexingAction } }
-			indexingActions={ { indexation: postIndexingAction } }
-		/> );
-
-		component.instance().setState( { state: "in_progress" } );
-
-		await component.instance().doIndexing( "indexation" );
-
-		component.update();
-
-		const settings = global.yoastIndexingData;
-
-		expect( preIndexingAction ).toHaveBeenCalledWith( settings );
-		expect( postIndexingAction ).toHaveBeenCalledWith( [
-			// Response.objects
-			{}, {}, {}, {}, {},
-		], settings );
+		// const preIndexingAction = jest.fn();
+		// const postIndexingAction = jest.fn();
 	} );
 } );
