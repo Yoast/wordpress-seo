181d02f7ffe17454dfa3d15fd194b076efdf6a5a
Vraja Das <vraja.pro@gmail.com>
2023-09-21 09:44:43 +0200

    cleanup enzyme code from SEMrushCountrySelector test

--- packages/js/tests/components/SEMrushCountrySelector.test.js
+++ packages/js/tests/components/SEMrushCountrySelector.test.js
@@ -1,36 +1,19 @@
-import { shallow } from "enzyme";
-import { noop } from "lodash";
-import React from "react";
-import SEMrushCountrySelector from "../../../js/src/components/modals/SEMrushCountrySelector";
+// import { noop } from "lodash";
+// import React from "react";
+// import SEMrushCountrySelector from "../../../js/src/components/modals/SEMrushCountrySelector";
 
 jest.mock( "@wordpress/api-fetch", () => ( {
 	__esModule: true,
 	"default": () => ( { response: {} } ),
 } ) );
 
-window.jQuery = () => ( { on: noop } );
+// window.jQuery = () => ( { on: noop } );
 
 describe( "SEMrushCountrySelector", () => {
 	it( "successfully calls the associated newRequest function when the select country button is clicked", () => {
-		const onClickMock = jest.fn();
-		const component = shallow( <SEMrushCountrySelector
-			setCountry={ noop } newRequest={ onClickMock } setNoResultsFoundnewRequest={ noop }
-			setRequestSucceeded={ noop } setRequestLimitReached={ noop } setRequestFailed={ noop } setNoResultsFound={ noop }
-		/> );
 
-		component.find( "#yoast-semrush-country-selector-button" ).simulate( "click" );
-
-		expect( onClickMock ).toHaveBeenCalled();
 	} );
 	it( "successfully calls the associated setCountry function when the selected option has changed", () => {
-		const setCountryMock = jest.fn();
-		const component = shallow( <SEMrushCountrySelector
-			setCountry={ setCountryMock } newRequest={ noop } setNoResultsFoundnewRequest={ noop }
-			setRequestSucceeded={ noop } setRequestLimitReached={ noop } setRequestFailed={ noop } setNoResultsFound={ noop }
-		/> );
 
-		component.find( "#yoast-semrush-country-selector-select" ).simulate( "change" );
-
-		expect( setCountryMock ).toHaveBeenCalled();
 	} );
 } );
