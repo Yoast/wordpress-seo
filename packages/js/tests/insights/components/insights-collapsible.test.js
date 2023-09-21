e36144973f3528692fc9c9160a0716dbf3f8ca9b
Vraja Das <vraja.pro@gmail.com>
2023-09-21 09:52:46 +0200

    cleanup enzyme code from insights collapsible test

--- packages/js/tests/insights/components/insights-collapsible.test.js
+++ packages/js/tests/insights/components/insights-collapsible.test.js
@@ -1,11 +1,10 @@
-import React from "react";
+// import React from "react";
 import { useSelect } from "@wordpress/data";
 import { enableFeatures } from "@yoast/feature-flag";
-import InsightsCollapsible from "../../../src/insights/components/insights-collapsible";
-import { shallow } from "enzyme";
-import FleschReadingEase from "../../../src/insights/components/flesch-reading-ease";
-import TextFormality from "../../../src/insights/components/text-formality";
-import TextLength from "../../../src/insights/components/text-length";
+// import InsightsCollapsible from "../../../src/insights/components/insights-collapsible";
+// import FleschReadingEase from "../../../src/insights/components/flesch-reading-ease";
+// import TextFormality from "../../../src/insights/components/text-formality";
+// import TextLength from "../../../src/insights/components/text-length";
 
 jest.mock( "@wordpress/data", () => (
 	{
@@ -37,32 +36,18 @@ function mockSelect( isFleschReadingEaseAvailable ) {
 describe( "The insights collapsible component", () => {
 	it( "renders the Flesch reading ease (FRE) component if the FRE score and difficulty are available", () => {
 		mockSelect( true );
-		const render = shallow( <InsightsCollapsible location={ "sidebar" } /> );
-
-		expect( render.find( FleschReadingEase ) ).toHaveLength( 1 );
 	} );
 	it( "does not render the FRE component if the FRE score and difficulty are not available", () => {
 		mockSelect( false );
-		const render = shallow( <InsightsCollapsible location={ "sidebar" } /> );
-
-		expect( render.find( FleschReadingEase ) ).toHaveLength( 0 );
 	} );
 	it( "renders the TextLength component", () => {
-		const render = shallow( <InsightsCollapsible location={ "sidebar" } /> );
 
-		expect( render.find( TextLength ) ).toHaveLength( 1 );
 	} );
 	it( "does not render the Text formality component when the feature is disabled", () => {
 		mockSelect( true );
-		const render = shallow( <InsightsCollapsible location={ "sidebar" } /> );
-
-		expect( render.find( TextFormality ) ).toHaveLength( 0 );
 	} );
 	it( "renders the Text formality component when the feature is enabled", () => {
 		enableFeatures( [ "TEXT_FORMALITY" ] );
 		mockSelect( true );
-		const render = shallow( <InsightsCollapsible location={ "sidebar" } /> );
-
-		expect( render.find( TextFormality ) ).toHaveLength( 1 );
 	} );
 } );
