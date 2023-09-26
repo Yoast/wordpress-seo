df6a9ef55c024634c27c425d9afa2b77c1dd9c12
Igor <35524806+igorschoester@users.noreply.github.com>
2023-09-26 12:06:45 +0200

    Migrate AnalysisCheckList tests
    To use the Testing library

--- packages/js/tests/components/AnalysisCheckList.test.js
+++ packages/js/tests/components/AnalysisCheckList.test.js
@@ -1,15 +1,78 @@
-// import AnalysisChecklist from "../../src/components/AnalysisChecklist";
+import AnalysisChecklist from "../../src/components/AnalysisChecklist";
+import { fireEvent, render, screen } from "../test-utils";
 
 describe( "The AnalysisChecklist component", () => {
 	it( "renders a checklist with a button when not all checks are good", () => {
+		const checks = [
+			{
+				label: "Readability analysis:",
+				score: "good",
+				scoreValue: "Good",
+			},
+			{
+				label: "SEO analysis:",
+				score: "bad",
+				scoreValue: "Needs improvement",
+			},
+			{
+				label: "Schema analysis:",
+				score: "good",
+				scoreValue: "Good",
+			},
+		];
+		const onClick = jest.fn();
 
+		render( <AnalysisChecklist checklist={ checks } onClick={ onClick } /> );
+
+		const checklistItems = document.querySelectorAll( ".yoast-analysis-check" );
+		expect( checklistItems ).toHaveLength( 3 );
+
+		const button = screen.getByRole( "button" );
+		expect( button ).toBeInTheDocument();
+		expect( button ).toHaveTextContent( "Improve your post with Yoast SEO" );
 	} );
 
 	it( "renders a checklist without a button when every check is good", () => {
+		const checks = [
+			{
+				label: "Readability analysis:",
+				score: "good",
+				scoreValue: "Good",
+			},
+			{
+				label: "SEO analysis:",
+				score: "good",
+				scoreValue: "Needs improvement",
+			},
+		];
+		const onClick = jest.fn();
 
+		render( <AnalysisChecklist checklist={ checks } onClick={ onClick } /> );
+
+		const checklistItems = document.querySelectorAll( ".yoast-analysis-check" );
+		expect( checklistItems ).toHaveLength( 2 );
+
+		expect( screen.queryByRole( "button" ) ).not.toBeInTheDocument();
 	} );
 
 	it( "calls the onClick handler when the button is clicked", () => {
+		const checks = [
+			{
+				label: "Readability analysis:",
+				score: "good",
+				scoreValue: "Good",
+			},
+			{
+				label: "SEO analysis:",
+				score: "bad",
+				scoreValue: "Needs improvement",
+			},
+		];
+		const onClick = jest.fn();
 
+		render( <AnalysisChecklist checklist={ checks } onClick={ onClick } /> );
+		fireEvent.click( screen.getByRole( "button" ) );
+
+		expect( onClick ).toHaveBeenCalled();
 	} );
 } );
