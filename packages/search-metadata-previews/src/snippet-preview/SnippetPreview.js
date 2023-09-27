704691ba15015713320acfdc7eab94062fec96d6
Vraja Das <vraja.pro@gmail.com>
2023-09-27 16:46:09 +0200

    Update lifecycle hook name to remove warning

--- packages/search-metadata-previews/src/snippet-preview/SnippetPreview.js
+++ packages/search-metadata-previews/src/snippet-preview/SnippetPreview.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 // External dependencies.
 import React, { PureComponent } from "react";
 import styled from "styled-components";
@@ -700,7 +701,7 @@ export default class SnippetPreview extends PureComponent {
 	 *
 	 * @returns {void}
 	 */
-	componentWillReceiveProps( nextProps ) {
+	UNSAFE_componentWillReceiveProps( nextProps ) {
 		const nextState = {};
 
 		if ( this.props.title !== nextProps.title ) {
