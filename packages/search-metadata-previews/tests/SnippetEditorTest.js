a35e6fa9173d5832895dd91b696d79add4ab85a2
Paolo Luigi Scala <paolo@yoast.com>
2023-09-27 15:24:11 +0200

    WIP refactoring

--- packages/search-metadata-previews/tests/SnippetEditorTest.js
+++ packages/search-metadata-previews/tests/SnippetEditorTest.js
@@ -1,8 +1,27 @@
-// import SnippetEditor from "../src/snippet-editor/SnippetEditor";
-// import React from "react";
+/**
+ * @jest-environment jsdom
+ */
+
+import React from "react";
+import SnippetEditor from "../src/snippet-editor/SnippetEditor";
+import { render, screen, fireEvent, userEvent } from "./test-utils";
+
 // eslint-disable-next-line import/named -- this concerns a mock
 import { focus } from "@yoast/replacement-variable-editor";
+import { set } from "lodash";
 
+const defaultData = {
+	title: "Test title",
+	slug: "test-slug",
+	description: "Test description, %%replacement_variable%%",
+};
+
+const defaultArgs = {
+	baseUrl: "http://example.org/",
+	siteName: "Test site name",
+	data: defaultData,
+	onChange: () => {},
+};
 
 describe( "SnippetEditor", () => {
 	it( "closes when calling close()", () => {
@@ -13,8 +32,19 @@ describe( "SnippetEditor", () => {
 		focus.mockClear();
 	} );
 
-	it( "switches modes when changing mode switcher input", () => {
+	it( "switches modes when changing mode switcher input", async() => {
+		const user = userEvent.setup();
+		render( <SnippetEditor { ...defaultArgs } /> );
 
+		expect( screen.getByText( "Desktop result" ) ).toBeInTheDocument();
+		expect( screen.getByText( "Mobile result" ) ).toBeInTheDocument();
+		expect( screen.getByLabelText( "Desktop result" ) ).not.toBeChecked();
+		expect( screen.getByLabelText( "Mobile result" ) ).toBeChecked();
+
+		await user.click( screen.getByLabelText( "Desktop result" ) );
+		await new Promise( resolve => setTimeout( resolve, 500 ) );
+
+		console.log( screen.getByLabelText( "Desktop result" ) );
 	} );
 
 	describe( "shallowCompareData", () => {
---
+++ packages/search-metadata-previews/tests/test-utils.js
@@ -0,0 +1,2 @@
+export * from "@testing-library/react";
+export * from "@testing-library/user-event";
