77f1bfa24ca79d69b0a5bf02c88df6b1baca744d
Vraja Das <vraja.pro@gmail.com>
2023-10-03 14:12:15 +0200

    refactor style

--- packages/js/src/components/modals/ModalBanner.js
+++ packages/js/src/components/modals/ModalBanner.js
@@ -7,7 +7,7 @@ import { __ } from "@wordpress/i18n";
  */
 const ModalBanner = () => {
 	return (
-		<div className="yst-flex yst-justify-between yst-leading-[33px] yst-text-lg yst-content-between yst-bg-black yst-text-amber-300 yst-h-9 yst-border-amber-300 yst-border-y yst-border-x-0 yst-border-solid yst-px-6">
+		<div className="yst-flex yst-justify-between yst-items-center yst-text-lg yst-content-between yst-bg-black yst-text-amber-300 yst-h-9 yst-border-amber-300 yst-border-y yst-border-x-0 yst-border-solid yst-px-6">
 			<div>{ __( "BLACK FRIDAY", "wordpress-seo" ) }</div>
 			<div>{ __( "30% OFF", "wordpress-seo" ) }</div>
 		</div>
