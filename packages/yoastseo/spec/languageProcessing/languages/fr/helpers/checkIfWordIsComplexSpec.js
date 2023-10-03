3424e1d08e3de0f77edce47e8f9264fffeb0006f
Hanna Worku <hanna@yoast.com>
2023-10-03 13:53:32 -0700

    add morphology data to each test

--- packages/yoastseo/spec/languageProcessing/languages/fr/helpers/checkIfWordIsComplexSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/fr/helpers/checkIfWordIsComplexSpec.js
@@ -6,8 +6,8 @@ const morphologyData = getMorphologyData( "fr" ).fr;
 
 describe( "a test checking if the word is complex in French",  function() {
 	it( "returns singular words as non-complex if it is found in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "résidence" ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "signature" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "résidence", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "signature", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns a plural words as non-complex if its singular version is found in the list", function() {
@@ -20,17 +20,17 @@ describe( "a test checking if the word is complex in French",  function() {
 	} );
 
 	it( "returns singular words longer than 9 characters as complex if it is not in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "dictionnaire" ) ).toEqual( true );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "dictionnaire", morphologyData ) ).toEqual( true );
 	} );
 
 	it( "returns words longer than 9 characters (including plurals) as non complex if the words start with capital letter", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "Opérations", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "Opérations", morphologyData, morphologyData ) ).toEqual( false );
 		expect( checkIfWordIsComplex( wordComplexityConfig, "Éclairage" ) ).toEqual( false );
 	} );
 
 	it( "returns words as non complex if the words are less than 9 characters", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "chanson" ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "ouvrir" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "chanson", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "ouvrir", morphologyData ) ).toEqual( false );
 	} );
 	it( "returns irregular plural as non-complex if its singular version is found in the list", function() {
 		expect( checkIfWordIsComplex( wordComplexityConfig,  "principaux", morphologyData ) ).toEqual( false );
@@ -47,21 +47,21 @@ describe( "a test checking if the word is complex in French",  function() {
 	} );
 
 	it( "returns words longer than 9 characters preceded by article l' or preposition d' as non complex if the words are in the list", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "l'ambassadeur" ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "d'échantillon" ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "s'appartient" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "l'ambassadeur", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "d'échantillon", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "s'appartient", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns word shorter than 9 characters as non complex even when it's preceded by an l' article or " +
 		"a contracted preposition that increases its length", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "l'occident" ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "d'extension" ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "c'extension" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "l'occident", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "d'extension", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "c'extension", morphologyData ) ).toEqual( false );
 	} );
 
 	it( "returns word longer than 9 characters which starts with capital letter as non complex even when it's preceded by an l' article " +
 		"or a contracted preposition", function() {
-		expect( checkIfWordIsComplex( wordComplexityConfig, "l'Orthodoxie" ) ).toEqual( false );
-		expect( checkIfWordIsComplex( wordComplexityConfig, "c'Égalisation" ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "l'Orthodoxie", morphologyData ) ).toEqual( false );
+		expect( checkIfWordIsComplex( wordComplexityConfig, "c'Égalisation", morphologyData ) ).toEqual( false );
 	} );
 } );
