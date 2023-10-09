c4a1368d113d4c42d7ebeb32b214e176eab78cf3
Paolo Luigi Scala <paolo@yoast.com>
2023-10-09 12:49:20 +0200

    Add missing test, and refactoring the existing one.

--- packages/search-metadata-previews/tests/HelpTextWrapperTest.js
+++ packages/search-metadata-previews/tests/HelpTextWrapperTest.js
@@ -1,6 +1,6 @@
 /* External dependencies */
 import React from "react";
-import renderer from "react-test-renderer";
+import { fireEvent, render, screen } from "./test-utils";
 
 /* Internal dependencies */
 import HelpTextWrapper from "../src/snippet-preview/HelpTextWrapper";
@@ -8,15 +8,14 @@ import HelpTextWrapper from "../src/snippet-preview/HelpTextWrapper";
 
 describe( "HelpTextWrapper", () => {
 	it( "matches the snapshot by default", () => {
-		const component = renderer.create(
-			<HelpTextWrapper helpTextButtonLabel="Open help" />
-		);
-
-		const tree = component.toJSON();
-		expect( tree ).toMatchSnapshot();
+		const { container } = render( <HelpTextWrapper helpTextButtonLabel="Open help" /> );
+		expect( container ).toMatchSnapshot();
 	} );
 
-	it( "matches the snapshot when the help text button is focused", () => {
+	it( "matches the snapshot when the help text button is focused", async() => {
+		const { container } = render( <HelpTextWrapper helpTextButtonLabel="Open help" /> );
 
+		fireEvent.click( screen.getByLabelText( "Open help" ) );
+		expect( container ).toMatchSnapshot();
 	} );
 } );
