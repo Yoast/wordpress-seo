5318833edb8ecceb6d05e5db14fc08942f04f192
Igor <35524806+igorschoester@users.noreply.github.com>
2023-10-16 14:39:44 +0200

    Fix @yoast/externals/contexts mocks

--- packages/js/tests/__mocks__/@yoast/externals/contexts.js
+++ packages/js/tests/__mocks__/@yoast/externals/contexts.js
@@ -1,7 +1,12 @@
 import { LocationConsumer, LocationContext, LocationProvider } from "../../../../src/components/contexts/location";
+import Root, { RootContext } from "../../../../src/components/contexts/root";
+import useRootContext from "../../../../src/components/contexts/use-root-context";
 
 export {
 	LocationContext,
 	LocationProvider,
 	LocationConsumer,
+	RootContext,
+	Root,
+	useRootContext,
 };
