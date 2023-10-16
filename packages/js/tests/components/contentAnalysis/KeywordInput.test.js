4a98e6933f4360728c21023a74afbd76a6b2929f
Igor <35524806+igorschoester@users.noreply.github.com>
2023-10-16 10:33:48 +0200

    Migrate KeywordInput tests

--- packages/js/tests/components/contentAnalysis/KeywordInput.test.js
+++ packages/js/tests/components/contentAnalysis/KeywordInput.test.js
@@ -1,7 +1,16 @@
-// import { KeywordInput } from "../../../src/components/contentAnalysis/KeywordInput";
+import { KeywordInput } from "../../../src/components/contentAnalysis/KeywordInput";
+import { render, screen } from "../../test-utils";
+
+beforeAll( () => {
+	global.wpseoAdminL10n = { "shortlinks.focus_keyword_info": "https://example.com/focus_keyword_info" };
+} );
+
+afterAll( () => {
+	delete global.wpseoAdminL10n;
+} );
 
 describe( "KeywordInput", () => {
-	let props = {};
+	let props;
 
 	beforeEach( () => {
 		props = {
@@ -13,33 +22,38 @@ describe( "KeywordInput", () => {
 
 	describe( "validate", () => {
 		it( "successfully validates that there is no keyphrase present", () => {
-			props = {
-				...props,
-				displayNoKeyphraseMessage: true,
-			};
+			render( <KeywordInput { ...props } displayNoKeyphraseMessage={ true } /> );
+
+			const alert = screen.getByRole( "alert" );
+			expect( alert ).toBeInTheDocument();
+			expect( alert.textContent ).toBe( "Please enter a focus keyphrase first to get related keyphrases" );
 		} );
 
 		it( "successfully validates that there is a comma in the keyphrase", () => {
-			props = {
-				...props,
-				keyword: "yoast seo,",
-			};
+			render( <KeywordInput { ...props } keyword="yoast seo," /> );
+
+			const alert = screen.getByRole( "alert" );
+			expect( alert ).toBeInTheDocument();
+			expect( alert.textContent ).toBe( "Are you trying to use multiple keyphrases? You should add them separately below." );
 		} );
 
 		it( "successfully validates that the keyphrase is longer than 191 characters", () => {
-			props = {
-				...props,
-				keyword: "yoast seo wordpress plugin to help improve your SEO through WordPress" +
-				         "yoast seo wordpress plugin to help improve your SEO through WordPress" +
-				         "yoast seo wordpress plugin to help improve your SEO through WordPress",
-			};
+			render( <KeywordInput
+				{ ...props }
+				keyword={ "yoast seo wordpress plugin to help improve your SEO through WordPress" +
+					"yoast seo wordpress plugin to help improve your SEO through WordPress" +
+					"yoast seo wordpress plugin to help improve your SEO through WordPress" }
+			/> );
+
+			const alert = screen.getByRole( "alert" );
+			expect( alert ).toBeInTheDocument();
+			expect( alert.textContent ).toBe( "Your keyphrase is too long. It can be a maximum of 191 characters." );
 		} );
 
 		it( "doesn't return any errors when the keyphrase is valid", () => {
-			props = {
-				...props,
-				keyword: "yoast seo",
-			};
+			render( <KeywordInput { ...props } keyword="yoast seo" /> );
+
+			expect( screen.queryByRole( "alert" ) ).not.toBeInTheDocument();
 		} );
 	} );
 } );
