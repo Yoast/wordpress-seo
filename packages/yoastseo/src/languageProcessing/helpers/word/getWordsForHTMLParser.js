bd4505015450d32961a0b2e5658a8c9833ddbdc5
Agnieszka Szuba <agnieszkaszuba@Agnieszkas-MacBook-Pro-2.local>
2023-10-18 16:29:04 +0200

    Move code to create punctuation tokens into a separate helper
    So that it can be reused for language-specific tokenizers

--- packages/yoastseo/src/languageProcessing/helpers/word/getWordsForHTMLParser.js
+++ packages/yoastseo/src/languageProcessing/helpers/word/getWordsForHTMLParser.js
@@ -1,5 +1,4 @@
-import { punctuationRegexEnd, punctuationRegexStart } from "../sanitize/removePunctuation";
-import { hashedHtmlEntitiesRegexEnd, hashedHtmlEntitiesRegexStart } from "../../../helpers/htmlEntities";
+import createPunctuationTokens from "./createPunctuationTokens";
 
 /*
  * The following regex matches a word separator. A word separator is either a whitespace, a tab or a non-breaking space.
@@ -38,35 +37,8 @@ const getWordsForHTMLParser = ( text, wordSeparatorsRegex =  wordSeparatorsRegex
 	// Split the sentence string into tokens. Those tokens are unrefined as they may contain punctuation.
 	const rawTokens = text.split( wordSeparatorsRegex ).filter( x => x !== "" );
 
-	const tokenTexts = [];
-	rawTokens.forEach( token => {
-		// Pretokens contains all that occurs before the first letter of the token.
-		const preTokens = [];
-		// Posttokens contains all that occurs after the last letter of the token.
-		const postTokens = [];
-
-		// Add all punctuation marks that occur before the first letter of the token to the pretokens array.
-		// Also, prevent matching with a hashed HTML entity in the beginning of the token.
-		while ( punctuationRegexStart.test( token ) && ! hashedHtmlEntitiesRegexStart.test( token ) ) {
-			preTokens.push( token[ 0 ] );
-			token = token.slice( 1 );
-		}
-		// Add all punctuation marks that occur after the last letter of the token to the posttokens array.
-		// Also, prevent matching with a hashed HTML entity at the end of the token.
-		while ( punctuationRegexEnd.test( token ) && ! hashedHtmlEntitiesRegexEnd.test( token ) ) {
-			// Using unshift here because we are iterating from the end of the string to the beginning,
-			// and we want to keep the order of the punctuation marks.
-			// Therefore, we add them to the start of the array.
-			postTokens.unshift( token[ token.length - 1 ] );
-			token = token.slice( 0, -1 );
-		}
-
-		let currentTokens = [ ...preTokens, token, ...postTokens ];
-		currentTokens = currentTokens.filter( x => x !== "" );
-		tokenTexts.push( ...currentTokens );
-	} );
-
-	return tokenTexts;
+	// Remove punctuation from the beginning and end of word tokens, and make them into separate tokens.
+	return createPunctuationTokens( rawTokens );
 };
 
 export default getWordsForHTMLParser;
