bc8d9b108dbba33d22806862e9614690a819893a
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2023-11-03 01:44:11 +0300

    Add unit tests for parseBlocks.js inside yoastseo#191 added tests for yoast blocks.

--- packages/yoastseo/spec/parse/build/private/parseBlocksSpec.js
+++ packages/yoastseo/spec/parse/build/private/parseBlocksSpec.js
@@ -1,234 +1,353 @@
 import parseBlocks from "../../../../src/parse/build/private/parseBlocks";
 import Paper from "../../../../src/values/Paper";
-import {htmlEntitiesRegex} from "../../../../src/helpers/htmlEntities";
 import adapt from "../../../../src/parse/build/private/adapt";
-import {parseFragment} from "parse5";
+import { parseFragment } from "parse5";
 
 describe( "The parseBlocks function", () => {
 	it( "parses an undefined node block should return undefined", () => {
-		const paper = new Paper( );
+		const paper = new Paper();
 
-		expect( parseBlocks( paper, undefined ) ).toBeUndefined();
-	});
+		expect( parseBlocks( paper ) ).toBeUndefined();
+	} );
 
 	it( "parses an empty blocks list should return undefined", () => {
-		const paper = new Paper( '', { wpBlocks: [] } );
+		const paper = new Paper( "", { wpBlocks: [] } );
+		const html = paper.getText();
+		const node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
 
+		expect( parseBlocks( paper, node ) ).toBeUndefined();
+	} );
 
-		expect( parseBlocks( paper, undefined ) ).toBeUndefined();
-	});
-
-	it( "offset correctly set for blocks", () => {
-		const paper = new Paper( '<!-- wp:paragraph -->\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:paragraph -->\n<p>Duis sagittis.</p>\n<!-- /wp:paragraph -->',
+	it( "offset not set if text has incorrect block structure", () => {
+		const paper = new Paper( "<!-- testwp:paragraph -->\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\n<!-- /testwp:paragraph -->",
 			{ wpBlocks: [
 				{
-					"clientId": "34e59165-166d-4663-a8b9-00349b193e36",
-					"name": "core/paragraph",
-					"isValid": true,
-					"originalContent": "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>",
-					"validationIssues": [],
-					"attributes": {
-						"content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-						"dropCap": false
+					clientId: "34e59165-166d-4663-a8b9-00349b193e36",
+					name: "core/paragraph",
+					isValid: true,
+					originalContent: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>",
+					validationIssues: [],
+					attributes: {
+						content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+						dropCap: false,
 					},
-					"innerBlocks": []
+					innerBlocks: [],
 				},
-				{
-					"clientId": "f0f597ef-f054-4831-8831-b8acc0dc6149",
-					"name": "core/paragraph",
-					"isValid": true,
-					"originalContent": "<p>Duis sagittis.</p>",
-					"validationIssues": [],
-					"attributes": {
-						"content": "Duis sagittis.",
-						"dropCap": false
-					},
-					"innerBlocks": []
-				}
 			] } );
 
-		let html = paper.getText();
-		let node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
+		const html = paper.getText();
+		const node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
+
+		parseBlocks( paper, node );
+
+		const firstBlock = paper._attributes.wpBlocks[ 0 ];
+
+		expect( firstBlock.startOffset ).toBeUndefined();
+		expect( firstBlock.contentOffset ).toBeUndefined();
+	} );
+
+	it( "offset correctly set for blocks", () => {
+		const paper = new Paper( "<!-- wp:paragraph -->\n<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\n<!-- /wp:paragraph -->\n\n<!-- wp:paragraph -->\n<p>Duis sagittis.</p>\n<!-- /wp:paragraph -->",
+			{ wpBlocks: [
+				{
+					clientId: "34e59165-166d-4663-a8b9-00349b193e36",
+					name: "core/paragraph",
+					isValid: true,
+					originalContent: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>",
+					validationIssues: [],
+					attributes: {
+						content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+						dropCap: false,
+					},
+					innerBlocks: [],
+				},
+				{
+					clientId: "f0f597ef-f054-4831-8831-b8acc0dc6149",
+					name: "core/paragraph",
+					isValid: true,
+					originalContent: "<p>Duis sagittis.</p>",
+					validationIssues: [],
+					attributes: {
+						content: "Duis sagittis.",
+						dropCap: false,
+					},
+					innerBlocks: [],
+				},
+			] } );
+
+		const html = paper.getText();
+		const node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
 		parseBlocks( paper, node );
 
 
 		parseBlocks( paper, node );
 
-		const firstBlock = paper._attributes.wpBlocks[0];
+		const firstBlock = paper._attributes.wpBlocks[ 0 ];
 
-		expect( firstBlock.startOffset ).toEqual(0);
-		expect( firstBlock.contentOffset ).toEqual(22);
+		expect( firstBlock.startOffset ).toEqual( 0 );
+		expect( firstBlock.contentOffset ).toEqual( 22 );
 
-		const secondBlock = paper._attributes.wpBlocks[1];
+		const secondBlock = paper._attributes.wpBlocks[ 1 ];
 
-		expect( secondBlock.startOffset ).toEqual(110);
-		expect( secondBlock.contentOffset ).toEqual(132);
-	});
+		expect( secondBlock.startOffset ).toEqual( 110 );
+		expect( secondBlock.contentOffset ).toEqual( 132 );
+	} );
 
 	it( "offset correctly set for blocks with inner blocks", () => {
-		const paper = new Paper( '<!-- wp:columns -->\n' +
+		const paper = new Paper( "<!-- wp:columns -->\n" +
 			'<div class="wp-block-columns"><!-- wp:column -->\n' +
 			'<div class="wp-block-column"><!-- wp:paragraph -->\n' +
-			'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\n' +
-			'<!-- /wp:paragraph --></div>\n' +
-			'<!-- /wp:column -->\n' +
-			'\n' +
-			'<!-- wp:column -->\n' +
+			"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\n" +
+			"<!-- /wp:paragraph --></div>\n" +
+			"<!-- /wp:column -->\n" +
+			"\n" +
+			"<!-- wp:column -->\n" +
 			'<div class="wp-block-column"><!-- wp:paragraph -->\n' +
-			'<p>Duis sagittis.</p>\n' +
-			'<!-- /wp:paragraph --></div>\n' +
-			'<!-- /wp:column --></div>\n' +
-			'<!-- /wp:columns -->',
-			{ wpBlocks: [
+			"<p>Duis sagittis.</p>\n" +
+			"<!-- /wp:paragraph --></div>\n" +
+			"<!-- /wp:column --></div>\n" +
+			"<!-- /wp:columns -->",
+		{ wpBlocks: [
+			{
+				clientId: "8cf41a06-d45e-4434-b1d3-4670c231afa7",
+				name: "core/columns",
+				isValid: true,
+				attributes: {
+					isStackedOnMobile: true,
+				},
+				innerBlocks: [
 					{
-						"clientId": "8cf41a06-d45e-4434-b1d3-4670c231afa7",
-						"name": "core/columns",
-						"isValid": true,
-						"attributes": {
-							"isStackedOnMobile": true
-						},
-						"innerBlocks": [
+						clientId: "06cc72bd-d6f0-42c3-a558-d338f855436c",
+						name: "core/column",
+						isValid: true,
+						attributes: {},
+						innerBlocks: [
 							{
-								"clientId": "06cc72bd-d6f0-42c3-a558-d338f855436c",
-								"name": "core/column",
-								"isValid": true,
-								"attributes": {},
-								"innerBlocks": [
-									{
-										"clientId": "78136cca-8160-4fe6-b221-a1575de1dbcd",
-										"name": "core/paragraph",
-										"isValid": true,
-										"attributes": {
-											"content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-											"dropCap": false
-										},
-										"innerBlocks": []
-									}
-								]
+								clientId: "78136cca-8160-4fe6-b221-a1575de1dbcd",
+								name: "core/paragraph",
+								isValid: true,
+								attributes: {
+									content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+									dropCap: false,
+								},
+								innerBlocks: [],
 							},
+						],
+					},
+					{
+						clientId: "a2b05c72-b677-4ed6-a46f-6154867d0ef1",
+						name: "core/column",
+						isValid: true,
+						attributes: {},
+						innerBlocks: [
 							{
-								"clientId": "a2b05c72-b677-4ed6-a46f-6154867d0ef1",
-								"name": "core/column",
-								"isValid": true,
-								"attributes": {},
-								"innerBlocks": [
-									{
-										"clientId": "a7f0c6a6-963f-48d4-b802-331900c4c645",
-										"name": "core/paragraph",
-										"isValid": true,
-										"attributes": {
-											"content": "Duis sagittis.",
-											"dropCap": false
-										},
-										"innerBlocks": []
-									}
-								]
-							}
-						]
-					}
-				] } );
+								clientId: "a7f0c6a6-963f-48d4-b802-331900c4c645",
+								name: "core/paragraph",
+								isValid: true,
+								attributes: {
+									content: "Duis sagittis.",
+									dropCap: false,
+								},
+								innerBlocks: [],
+							},
+						],
+					},
+				],
+			},
+		] } );
 
-		let html = paper.getText();
-		let node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
+		const html = paper.getText();
+		const node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
 
 
 		parseBlocks( paper, node );
 
 
-		const columnsBlock = paper._attributes.wpBlocks[0];
+		const columnsBlock = paper._attributes.wpBlocks[ 0 ];
 
-		expect( columnsBlock.startOffset ).toEqual(0);
-		expect( columnsBlock.contentOffset ).toEqual(20);
+		expect( columnsBlock.startOffset ).toEqual( 0 );
+		expect( columnsBlock.contentOffset ).toEqual( 20 );
 
-		const firstColumnBlock = columnsBlock.innerBlocks[0]
+		const firstColumnBlock = columnsBlock.innerBlocks[ 0 ];
 
-		expect( firstColumnBlock.startOffset ).toEqual(50);
-		expect( firstColumnBlock.contentOffset ).toEqual(69);
+		expect( firstColumnBlock.startOffset ).toEqual( 50 );
+		expect( firstColumnBlock.contentOffset ).toEqual( 69 );
 
-		const firstColumnParagraphBlock = firstColumnBlock.innerBlocks[0]
+		const firstColumnParagraphBlock = firstColumnBlock.innerBlocks[ 0 ];
 
-		expect( firstColumnParagraphBlock.startOffset ).toEqual(98);
-		expect( firstColumnParagraphBlock.contentOffset ).toEqual(120);
+		expect( firstColumnParagraphBlock.startOffset ).toEqual( 98 );
+		expect( firstColumnParagraphBlock.contentOffset ).toEqual( 120 );
 
-		const secondColumnBlock = columnsBlock.innerBlocks[1]
+		const secondColumnBlock = columnsBlock.innerBlocks[ 1 ];
 
-		expect( secondColumnBlock.startOffset ).toEqual(234);
-		expect( secondColumnBlock.contentOffset ).toEqual(253);
+		expect( secondColumnBlock.startOffset ).toEqual( 234 );
+		expect( secondColumnBlock.contentOffset ).toEqual( 253 );
 
-		const secondColumnParagraphBlock = secondColumnBlock.innerBlocks[0]
+		const secondColumnParagraphBlock = secondColumnBlock.innerBlocks[ 0 ];
 
-		expect( secondColumnParagraphBlock.startOffset ).toEqual(282);
-		expect( secondColumnParagraphBlock.contentOffset ).toEqual(304);
-	});
+		expect( secondColumnParagraphBlock.startOffset ).toEqual( 282 );
+		expect( secondColumnParagraphBlock.contentOffset ).toEqual( 304 );
+	} );
 
 	it( "clientId correctly set for blocks and inner blocks", () => {
 		const clientId = "8cf41a06-d45e-4434-b1d3-4670c231afa7";
-		const paper = new Paper( '<!-- wp:columns -->\n' +
+		const paper = new Paper( "<!-- wp:columns -->\n" +
 			'<div class="wp-block-columns"><!-- wp:column -->\n' +
 			'<div class="wp-block-column"><!-- wp:paragraph -->\n' +
-			'<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\n' +
-			'<!-- /wp:paragraph --></div>\n' +
-			'<!-- /wp:column -->\n' +
-			'\n' +
-			'<!-- wp:column -->\n' +
+			"<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>\n" +
+			"<!-- /wp:paragraph --></div>\n" +
+			"<!-- /wp:column -->\n" +
+			"\n" +
+			"<!-- wp:column -->\n" +
 			'<div class="wp-block-column"><!-- wp:paragraph -->\n' +
-			'<p>Duis sagittis.</p>\n' +
-			'<!-- /wp:paragraph --></div>\n' +
-			'<!-- /wp:column --></div>\n' +
-			'<!-- /wp:columns -->',
-			{ wpBlocks: [
+			"<p>Duis sagittis.</p>\n" +
+			"<!-- /wp:paragraph --></div>\n" +
+			"<!-- /wp:column --></div>\n" +
+			"<!-- /wp:columns -->",
+		{ wpBlocks: [
+			{
+				clientId: clientId,
+				name: "core/columns",
+				isValid: true,
+				attributes: {
+					isStackedOnMobile: true,
+				},
+				innerBlocks: [
 					{
-						"clientId": clientId,
-						"name": "core/columns",
-						"isValid": true,
-						"attributes": {
-							"isStackedOnMobile": true
-						},
-						"innerBlocks": [
+						name: "core/column",
+						isValid: true,
+						attributes: {},
+						innerBlocks: [
 							{
-								"name": "core/column",
-								"isValid": true,
-								"attributes": {},
-								"innerBlocks": [
-									{
-										"name": "core/paragraph",
-										"isValid": true,
-										"attributes": {
-											"content": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-											"dropCap": false
-										},
-										"innerBlocks": []
-									}
-								]
+								name: "core/paragraph",
+								isValid: true,
+								attributes: {
+									content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+									dropCap: false,
+								},
+								innerBlocks: [],
 							},
+						],
+					},
+					{
+						name: "core/column",
+						isValid: true,
+						attributes: {},
+						innerBlocks: [
 							{
-								"name": "core/column",
-								"isValid": true,
-								"attributes": {},
-								"innerBlocks": [
-									{
-										"name": "core/paragraph",
-										"isValid": true,
-										"attributes": {
-											"content": "Duis sagittis.",
-											"dropCap": false
-										},
-										"innerBlocks": []
-									}
-								]
-							}
-						]
-					}
-				] } );
+								name: "core/paragraph",
+								isValid: true,
+								attributes: {
+									content: "Duis sagittis.",
+									dropCap: false,
+								},
+								innerBlocks: [],
+							},
+						],
+					},
+				],
+			},
+		] } );
 
-		let html = paper.getText();
-		let node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
+		const html = paper.getText();
+		const node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
 
 		parseBlocks( paper, node );
 
-		const divNode = node.childNodes[2];
+		const divNode = node.childNodes[ 2 ];
 
 
-		expect( divNode.clientId ).toEqual(clientId);
-	});
+		expect( divNode.clientId ).toEqual( clientId );
+	} );
+
+	it( "id attribute is correctly set for yoast faq block", () => {
+		const attributeId = "faq-question-1698963671829";
+		const paper = new Paper( "<!-- wp:yoast/faq-block {\"questions\":[{\"id\":\"" + attributeId + "\",\"question\":[\"Question\"],\"answer\":[\"Answer\"],\"jsonQuestion\":\"Question\",\"jsonAnswer\":\"Answer\"}]} -->\n" +
+			"<div class=\"schema-faq wp-block-yoast-faq-block\"><div class=\"schema-faq-section\" id=\"" + attributeId + "\"><strong class=\"schema-faq-question\">Question</strong> <p class=\"schema-faq-answer\">Answer</p> </div> </div>\n" +
+			"<!-- /wp:yoast/faq-block -->",
+		{
+			wpBlocks: [
+				{
+					clientId: "61988c01-b912-42b7-9475-4886e77fac45",
+					name: "yoast/faq-block",
+					isValid: true,
+					attributes: {
+						questions: [
+							{
+								id: attributeId,
+								question: [
+									"Question",
+								],
+								answer: [
+									"Answer",
+								],
+								jsonQuestion: "Question",
+								jsonAnswer: "Answer",
+							},
+						],
+					},
+					innerBlocks: [],
+				},
+			],
+		} );
+
+		const html = paper.getText();
+		const node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
+
+		parseBlocks( paper, node );
+
+		const blockDiv = node.childNodes[ 2 ];
+		const sectionDiv = blockDiv.childNodes[ 0 ];
+
+		const strongNode = sectionDiv.childNodes[ 0 ];
+
+		expect( strongNode.attributeId ).toEqual( attributeId );
+		expect( strongNode.isFirstSection ).toBeTruthy();
+
+		const paragraphNode = sectionDiv.childNodes[ 2 ];
+
+		expect( paragraphNode.attributeId ).toEqual( attributeId );
+		expect( paragraphNode.isFirstSection ).toBeFalsy();
+	} );
+
+	it( "id attribute is not set if yoast faq block is not correctly", () => {
+		const attributeId = "faq-question-1698963671829";
+		const paper = new Paper( "<!-- wp:yoast/faq-block {\"questions\":[{\"id\":\"" + attributeId + "\",\"question\":[\"Question\"],\"jsonQuestion\":\"Question\"}]} -->\n" +
+			"<div class=\"schema-faq wp-block-yoast-faq-block\"><div class=\"schema-faq-section\" id=\"" + attributeId + "\"><strong class=\"schema-faq-question\">Question</strong> </div> </div>\n" +
+			"<!-- /wp:yoast/faq-block -->",
+		{
+			wpBlocks: [
+				{
+					clientId: "61988c01-b912-42b7-9475-4886e77fac45",
+					name: "yoast/faq-block",
+					isValid: true,
+					attributes: {
+						questions: [
+							{
+								id: attributeId,
+								question: [
+									"Question",
+								],
+								jsonQuestion: "Question",
+							},
+						],
+					},
+					innerBlocks: [],
+				},
+			],
+		} );
+
+		const html = paper.getText();
+		const node = adapt( parseFragment( html, { sourceCodeLocationInfo: true } ) );
+
+		parseBlocks( paper, node );
+
+		const blockDiv = node.childNodes[ 2 ];
+		const sectionDiv = blockDiv.childNodes[ 0 ];
+
+		const strongNode = sectionDiv.childNodes[ 0 ];
+
+		expect( strongNode.attributeId ).toBeUndefined();
+		expect( strongNode.isFirstSection ).toBeUndefined();
+	} );
 } );
