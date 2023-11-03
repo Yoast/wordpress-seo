4a1c30fdd08897d81043e42753d6d34a1937fc0c
aidamarfuaty <aida@yoast.com>
2023-11-03 09:36:57 +0100

    Add more unit tests

--- packages/js/tests/analysis/plugins/shortcode-plugin.test.js
+++ packages/js/tests/analysis/plugins/shortcode-plugin.test.js
@@ -1,19 +1,19 @@
 import YoastShortcodePlugin from "../../../src/analysis/plugins/shortcode-plugin";
 
+// Mock the functions and objects needed for the class
+const mockRegisterPlugin = jest.fn();
+const mockRegisterModification = jest.fn();
+const mockPluginReady = jest.fn();
+const mockPluginReloaded = jest.fn();
+const shortcodesToBeParsed = [ "caption", "wpseo_breadcrumb" ];
+
+// Mock the global objects and functions used in the class
+global.tinyMCE = {};
+global.wpseoScriptData = { analysis: { plugins: { shortcodes: { wpseo_filter_shortcodes_nonce: "nonce" } } } };
+global.ajaxurl = "http://example.com/ajax";
+global._ = { debounce: jest.fn() };
+
 describe( "YoastShortcodePlugin", () => {
-	// Mock the functions and objects needed for the class
-	const mockRegisterPlugin = jest.fn();
-	const mockRegisterModification = jest.fn();
-	const mockPluginReady = jest.fn();
-	const mockPluginReloaded = jest.fn();
-	const shortcodesToBeParsed = [ "caption", "wpseo_breadcrumb" ];
-
-	// Mock the global objects and functions used in the class
-	global.tinyMCE = {};
-	global.wpseoScriptData = { analysis: { plugins: { shortcodes: { wpseo_filter_shortcodes_nonce: "nonce" } } } };
-	global.ajaxurl = "http://example.com/ajax";
-	global._ = { debounce: jest.fn() };
-
 	beforeEach( () => {
 		mockRegisterPlugin.mockClear();
 		mockRegisterModification.mockClear();
@@ -90,22 +90,60 @@ describe( "YoastShortcodePlugin", () => {
 			shortcodesToBeParsed
 		);
 
-		// Test input text with shortcodes
+		// Test input text with shortcodes.
 		const inputText = "This is a sample [wpseo_breadcrumb] text with an image with caption [caption id=\"attachment_8\" align=\"alignnone\"" +
 			" width=\"230\"]<img class='size-medium wp-image-8' src='https://wacky-fowl.localsite.io/" +
 			"95fa1-230x300.jpg' alt='A tortie cat, not wayang kulit' width='230' height='300'>" +
 			"</img> A tortie cat, not a red panda.[/caption].";
 
-		// Call the getShortcodes method
+		// Call the getShortcodes method.
 		const shortcodes = plugin.getShortcodes( inputText );
 
-		// Expect the extracted shortcodes
+		// Expect the extracted shortcodes.
 		expect( shortcodes ).toEqual( [
 			"[caption id=\"attachment_8\" align=\"alignnone\" " +
 			"width=\"230\"]<img class='size-medium wp-image-8' src='https://wacky-fowl.localsite.io/" +
 			"95fa1-230x300.jpg' alt='A tortie cat, not wayang kulit' width='230' height='300'>" +
 			"</img> A tortie cat, not a red panda.[/caption]",
 			"[wpseo_breadcrumb]" ] );
+	} );
+
+	it( "should parse shortcodes through AJAX and save parsed shortcodes", ( done ) => {
+		const plugin = new YoastShortcodePlugin(
+			{
+				registerPlugin: mockRegisterPlugin,
+				registerModification: mockRegisterModification,
+				pluginReady: mockPluginReady,
+				pluginReloaded: mockPluginReloaded,
+			},
+			shortcodesToBeParsed
+		);
+
+		// Mock the jQuery.post function.
+		global.jQuery = {
+			post: jest.fn( ( url, data, callback ) => {
+				// Simulate an AJAX response with parsed shortcodes
+				const shortcodeResults = JSON.stringify( [
+					{ shortcode: "[shortcode1]", output: "Parsed Output 1" },
+					{ shortcode: "[shortcode2]", output: "Parsed Output 2" },
+				] );
+				callback( shortcodeResults );
+			} ),
+		};
+
+		// Mock the saveParsedShortcodes method for assertions.
+		const saveParsedShortcodes = jest.spyOn( plugin, "saveParsedShortcodes" );
+
+		// Call the parseShortcodes method.
+		plugin.parseShortcodes( [ "[shortcode1]", "[shortcode2]" ], () => {
+			// Expect that saveParsedShortcodes was called with the correct parsed shortcodes
+			expect( saveParsedShortcodes ).toHaveBeenCalledWith( JSON.stringify( [
+				{ shortcode: "[shortcode1]", output: "Parsed Output 1" },
+				{ shortcode: "[shortcode2]", output: "Parsed Output 2" },
+			] ), expect.any( Function ) );
+
+			done();
+		} );
 	} );
 } );
 
