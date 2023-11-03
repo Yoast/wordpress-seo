22b490fce993098c3330deb4385ee5baf0e92bff
Vraja Das <vraja.pro@gmail.com>
2023-11-03 10:39:46 +0100

    fix introduction modal for admin pages

--- packages/js/src/ai-generator/components/modal-content.js
+++ packages/js/src/ai-generator/components/modal-content.js
@@ -1,6 +1,8 @@
+/* eslint-disable complexity */
 import { useDispatch, useSelect } from "@wordpress/data";
 import { useMemo } from "@wordpress/element";
 import { AiGenerateTitlesAndDescriptionsUpsell } from "../../shared-admin/components";
+import { __, sprintf } from "@wordpress/i18n";
 
 const STORE = "yoast-seo/editor";
 
@@ -9,6 +11,52 @@ const STORE = "yoast-seo/editor";
  */
 export const ModalContent = () => {
 	const learnMoreLink = useSelect( select => select( STORE ).selectLink( "https://www.yoa.st/ai-generator-learn-more" ), [] );
+	const upsellLinkPremium = useSelect( select => select( STORE ).selectLink( "https://yoa.st/ai-generator-upsell" ), [] );
+	const upsellLinkWooPremiumBundle = useSelect( select => select( STORE ).selectLink( "https://yoa.st/ai-generator-upsell-woo-seo-premium-bundle" ), [] );
+	const upsellLinkWoo = useSelect( select => select( STORE ).selectLink( "https://yoa.st/ai-generator-upsell-woo-seo" ), [] );
+	const isPremium = useSelect( select => select( STORE ).getIsPremium(), [] );
+	const isWooSeoUpsell = useSelect( select => select( STORE ).getIsWooSeoUpsell(), [] );
+	const isProduct = useSelect( select => select( STORE ).getIsProduct(), [] );
+	const wooSeoNoPremium = isProduct && ! isWooSeoUpsell && ! isPremium;
+	const isProductCopy = isWooSeoUpsell || wooSeoNoPremium;
+	const postModalprops = {
+		isProductCopy,
+		upsellLink: upsellLinkPremium,
+	};
+
+	if ( isProductCopy ) {
+		const upsellPremiumWooLabel = sprintf(
+			/* translators: %1$s expands to Yoast SEO Premium, %2$s expands to Yoast WooCommerce SEO. */
+			__( "%1$s + %2$s", "wordpress-seo" ),
+			"Yoast SEO Premium",
+			"Yoast WooCommerce SEO"
+		);
+		postModalprops.newToText = sprintf(
+			/* translators: %1$s expands to Yoast SEO Premium and Yoast WooCommerce SEO. */
+			__( "New to %1$s", "wordpress-seo" ),
+			upsellPremiumWooLabel
+		);
+		postModalprops.title = __( "Generate product titles & descriptions with AI!", "wordpress-seo" );
+		if ( ! isPremium && isWooSeoUpsell ) {
+			postModalprops.upsellLabel = `${ sprintf(
+				/* translators: %1$s expands to Woo Premium bundle. */
+				__( "Unlock with the %1$s", "wordpress-seo" ),
+				"Woo Premium bundle"
+			)}*`;
+			postModalprops.bundleNote = <div className="yst-text-xs yst-text-slate-500 yst-mt-2">
+				{ `*${upsellPremiumWooLabel}` }
+			</div>;
+			postModalprops.upsellLink = upsellLinkWooPremiumBundle;
+		}
+		if ( isPremium ) {
+			postModalprops.upsellLabel = sprintf(
+				/* translators: %1$s expands to Yoast WooCommerce SEO. */
+				__( "Unlock with %1$s", "wordpress-seo" ),
+				"Yoast WooCommerce SEO"
+			);
+			postModalprops.upsellLink = upsellLinkWoo;
+		}
+	}
 
 
 	const imageLink = useSelect( select => select( STORE ).selectImageLink( "ai-generator-preview.png" ), [] );
@@ -29,6 +77,7 @@ export const ModalContent = () => {
 			learnMoreLink={ learnMoreLink }
 			thumbnail={ thumbnail }
 			wistiaEmbedPermission={ wistiaEmbedPermission }
+			{ ...postModalprops }
 		/>
 	);
 };
