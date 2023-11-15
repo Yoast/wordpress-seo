bd4447e37638543ffdf1273ad8af9a36779bf7e9
Thijs van der Heijden <thijsvanderheijden2@gmail.com>
2023-11-15 12:11:52 +0100

    Actually use exception as intended.

--- src/indexables/infrastructure/outdated-post-indexables-repository.php
+++ src/indexables/infrastructure/outdated-post-indexables-repository.php
@@ -7,6 +7,7 @@ use Yoast\WP\Lib\Model;
 use Yoast\WP\SEO\Helpers\Post_Helper;
 use Yoast\WP\SEO\Helpers\Post_Type_Helper;
 use Yoast\WP\SEO\Indexables\Application\Ports\Outdated_Post_Indexables_Repository_Interface;
+use Yoast\WP\SEO\Indexables\Domain\Exceptions\No_Outdated_Posts_Found_Exception;
 use Yoast\WP\SEO\Indexables\Domain\Last_Batch_Count;
 use Yoast\WP\SEO\Indexables\Domain\Outdated_Post_Indexables_List;
 use Yoast\WP\SEO\Repositories\Indexable_Repository;
@@ -69,6 +70,7 @@ class Outdated_Post_Indexables_Repository implements Outdated_Post_Indexables_Re
 	 *
 	 * @param Last_Batch_Count $count The last batch count domain object.
 	 *
+	 * @throws No_Outdated_Posts_Found_Exception When there are no outdated posts found.
 	 * @return Outdated_Post_Indexables_List
 	 */
 	public function get_outdated_post_indexables(
@@ -91,6 +93,10 @@ class Outdated_Post_Indexables_Repository implements Outdated_Post_Indexables_Re
 		$query->where_raw( 'P.post_status NOT IN (' . \implode( ', ', \array_fill( 0, \count( $excluded_post_statuses ), '%s' ) ) . ')', $excluded_post_statuses );
 
 		$indexables = $query->find_many();
+
+		if ( \count( $indexables ) === 0 ) {
+			throw No_Outdated_Posts_Found_Exception::because_no_outdated_posts_queried();
+		}
 
 		$indexable_list = new Outdated_Post_Indexables_List();
 		foreach ( $indexables as $indexable ) {
