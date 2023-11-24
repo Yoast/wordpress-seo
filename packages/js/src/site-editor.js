fe73602faf5dfc7548e1b7f5f27a4b776dc2906c
Vraja Das <vraja.pro@gmail.com>
2023-11-24 17:10:23 +0100

    access post id through core/edit-site store

--- packages/js/src/site-editor.js
+++ packages/js/src/site-editor.js
@@ -1,1 +1,16 @@
 
+import { select } from "@wordpress/data";
+/* eslint-disable no-unused-vars */
+import initEditorStore from "./site-editor/initializers/editor-store";
+import BlockEditorData from "./site-editor/analysis/blockEditorData";
+
+
+setTimeout( () => {
+	const postId = select( "core/edit-site" ).getEditedPostContext().postId;
+	window.yoast = window.yoast || {};
+
+	// window.yoast.EditorData = BlockEditorData;
+	// console.log( postId );
+
+	// window.YoastSEO.store = initEditorStore();
+}, 2000 );
