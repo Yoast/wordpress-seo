37d4895c413163ccede614a4c8a149f17cda4591
Paolo Luigi Scala <paolo@yoast.com>
2023-12-06 11:13:24 +0100

    Remove ad from admin bar menu

--- inc/class-wpseo-admin-bar-menu.php
+++ inc/class-wpseo-admin-bar-menu.php
@@ -10,7 +10,6 @@ use Yoast\WP\SEO\Helpers\Score_Icon_Helper;
 use Yoast\WP\SEO\Integrations\Support_Integration;
 use Yoast\WP\SEO\Models\Indexable;
 use Yoast\WP\SEO\Presenters\Admin\Premium_Badge_Presenter;
-use Yoast\WP\SEO\Promotions\Application\Promotion_Manager;
 use Yoast\WP\SEO\Repositories\Indexable_Repository;
 
 /**
@@ -591,23 +590,15 @@ class WPSEO_Admin_Bar_Menu implements WPSEO_WordPress_Integration {
 	 * @return void
 	 */
 	protected function add_premium_link( WP_Admin_Bar $wp_admin_bar ) {
-		$sale_percentage = '';
-		if ( YoastSEO()->classes->get( Promotion_Manager::class )->is( 'black-friday-2023-promotion' ) ) {
-			$sale_percentage = sprintf(
-				'<span class="admin-bar-premium-promotion">%1$s</span>',
-				__( '-30%', 'wordpress-seo' )
-			);
-		}
 		$wp_admin_bar->add_menu(
 			[
 				'parent' => self::MENU_IDENTIFIER,
 				'id'     => 'wpseo-get-premium',
 				// Circumvent an issue in the WP admin bar API in order to pass `data` attributes. See https://core.trac.wordpress.org/ticket/38636.
 				'title'  => sprintf(
-					'<a href="%1$s" target="_blank" data-action="load-nfd-ctb" data-ctb-id="f6a84663-465f-4cb5-8ba5-f7a6d72224b2" style="padding:0;">%2$s &raquo; %3$s</a>',
+					'<a href="%1$s" target="_blank" data-action="load-nfd-ctb" data-ctb-id="f6a84663-465f-4cb5-8ba5-f7a6d72224b2" style="padding:0;">%2$s &raquo;</a>',
 					$this->shortlinker->build_shortlink( 'https://yoa.st/admin-bar-get-premium' ),
-					__( 'Get Yoast SEO Premium', 'wordpress-seo' ),
-					$sale_percentage
+					__( 'Get Yoast SEO Premium', 'wordpress-seo' )
 				),
 				'meta'   => [
 					'tabindex' => '0',
