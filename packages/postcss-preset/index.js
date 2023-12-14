34641c93bdd4a772e9cf2d01282439f172a84fe4
Igor <35524806+igorschoester@users.noreply.github.com>
2023-12-14 21:36:57 +0100

    Add postcss-replace
    Optionally replaces both the --tw and the yst- prefixes

--- packages/postcss-preset/index.js
+++ packages/postcss-preset/index.js
@@ -9,6 +9,15 @@ module.exports = {
 		require( "tailwindcss/nesting" ),
 		require( "tailwindcss" ),
 		require( "autoprefixer" ),
+		...( process.env.TAILWIND_PREFIX ? [
+			require( "postcss-replace" )( {
+				pattern: /(--tw|yst-)/g,
+				data: {
+					"--tw": `--${ process.env.TAILWIND_PREFIX }`,
+					"yst-": `${ process.env.TAILWIND_PREFIX }-`,
+				},
+			} ),
+		] : [] ),
 		...( process.env.NODE_ENV === "production" ? [ require( "cssnano" ) ] : [] ),
 	],
 };
