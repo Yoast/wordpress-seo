d604ba70a629874aa8f8a8f221d6cefa7f4253e9
Paolo Luigi Scala <paolo@yoast.com>
2023-12-15 14:08:37 +0100

    Remove deleted deprecated classes

--- config/dependency-injection/deprecated-classes.php
+++ config/dependency-injection/deprecated-classes.php
@@ -23,31 +23,17 @@
 use Yoast\WP\SEO\Actions\Indexables_Page_Action;
 use Yoast\WP\SEO\Conditionals\Indexables_Page_Conditional;
 use Yoast\WP\SEO\Conditionals\Schema_Blocks_Conditional;
-use Yoast\WP\SEO\Conditionals\The_Events_Calendar_Conditional;
-use Yoast\WP\SEO\Conditionals\Third_Party\CoAuthors_Plus_Activated_Conditional;
-use Yoast\WP\SEO\Conditionals\Third_Party\CoAuthors_Plus_Flag_Conditional;
-use Yoast\WP\SEO\Generators\Schema\Third_Party\CoAuthor;
-use Yoast\WP\SEO\Generators\Schema\Third_Party\Events_Calendar_Schema;
 use Yoast\WP\SEO\Helpers\Indexables_Page_Helper;
 use Yoast\WP\SEO\Integrations\Admin\Indexables_Page_Integration;
 use Yoast\WP\SEO\Integrations\Admin\Old_Premium_Integration;
 use Yoast\WP\SEO\Integrations\Admin\Social_Templates_Integration;
 use Yoast\WP\SEO\Integrations\Schema_Blocks;
-use Yoast\WP\SEO\Integrations\Third_Party\CoAuthors_Plus;
-use Yoast\WP\SEO\Integrations\Third_Party\The_Events_Calendar;
 use Yoast\WP\SEO\Integrations\Third_Party\Wincher;
 use Yoast\WP\SEO\Integrations\Third_Party\Wordproof_Integration_Toggle;
 use Yoast\WP\SEO\Routes\Indexables_Page_Route;
 use Yoast\WP\SEO\Schema_Templates\Assets\Icons;
 
 $deprecated_classes = [
-	CoAuthors_Plus_Activated_Conditional::class => '19.12',
-	CoAuthors_Plus_Flag_Conditional::class      => '19.12',
-	CoAuthor::class                             => '19.12',
-	CoAuthors_Plus::class                       => '19.12',
-	The_Events_Calendar_Conditional::class      => '19.12',
-	Events_Calendar_Schema::class               => '19.12',
-	The_Events_Calendar::class                  => '19.12',
 	Social_Templates_Integration::class         => '20.3',
 	Indexables_Page_Integration::class          => '20.4',
 	Indexables_Page_Route::class                => '20.4',
