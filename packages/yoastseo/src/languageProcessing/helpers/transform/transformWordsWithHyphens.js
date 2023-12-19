29892d05dbd06f13a850a9a802447e429f92dd74
aidamarfuaty <aida@yoast.com>
2023-12-19 15:00:09 +0100

    Create a helper to split words from an array that contain hyphens and add the split words back to the array if they are not there yet

---
+++ packages/yoastseo/src/languageProcessing/helpers/transform/transformWordsWithHyphens.js
@@ -0,0 +1,19 @@
+import { flatten } from "lodash-es";
+
+/**
+ * Splits words from an array that contain hyphen and adds the elements to the array if they are not yet there.
+ *
+ * @param {Array} array The array to check and modify.
+ *
+ * @returns {Array} A new array with the elements containing hyphens split.
+ */
+export default function( array ) {
+	const newArray = array;
+	array.forEach( element => {
+		element = element.split( "-" );
+		if ( element.length > 0 && element.filter( unit => ! array.includes( unit ) ).length > 0 ) {
+			newArray.push( element );
+		}
+	} );
+	return flatten( newArray );
+}
