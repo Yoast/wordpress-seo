1e0f044167f9c14c9901e4f7476bcb8735b92873
Hanna Worku <hanna@Hannas-MacBook-Pro.local>
2023-12-20 12:30:12 +0100

    improve descriptions for Inclusive language helpers

--- packages/yoastseo/src/scoring/assessments/inclusiveLanguage/helpers/isFollowedByException.js
+++ packages/yoastseo/src/scoring/assessments/inclusiveLanguage/helpers/isFollowedByException.js
@@ -2,11 +2,11 @@ import { getWords } from "../../../../languageProcessing";
 import { includesWordsAtPosition } from "./includesWordsAtPosition";
 
 /**
- * Checks whether the given list of words contains another list of words in the given order,
- * but not when they are followed by one of the exceptions.
+ * Checks whether a list of words contains a sequence of words in the given order, excluding cases when
+ * they are followed by one of the exceptions.
  *
  * @param {string[]} words The list of words.
- * @param {string[]} consecutiveWords The list of words to find.
+ * @param {string[]} consecutiveWords The sequence of words in the given order to find in the list.
  * @param {string[]} exceptions The list of exception phrases.
  *
  * @returns {function} A function that checks whether the given list of words is contained in another list of words in the given order.
