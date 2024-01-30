6b936296b6f292b7c3532a30f45de2b853d6bf42
vraja-pro <vraja.pro@gmail.com>
2024-01-30 15:14:12 +0100

    Add test for full url

--- tests/WP/Frontend/Front_End_Integration_Test.php
+++ tests/WP/Frontend/Front_End_Integration_Test.php
@@ -155,6 +155,13 @@ final class Front_End_Integration_Test extends TestCase {
 				'next'     => '<a href="/?query-1-page=4">Next</a>',
 				'expected' => 'https://example.org/?query-1-page=2',
 			],
+			'full url' => [
+				'link'     => 'https://example.org/?query-1-page=3',
+				'rel'      => 'prev',
+				'prev'     => '<a href="https://example.org/?query-1-page=2">Prev</a>',
+				'next'     => '<a href="https://example.org/?query-1-page=4">Next</a>',
+				'expected' => 'https://example.org/?query-1-page=2',
+			],
 		];
 	}
 }
