563cc73bc04d9c55ba0cf8fdfdb7f95fad992761
Vraja Das <vraja.pro@gmail.com>
2024-02-16 16:27:02 +0100

    Add constants in separate folder

---
+++ packages/js/src/constants/sync.js
@@ -0,0 +1,9 @@
+export const METADATA_IDS = {
+	focusKeyphrase: "_yoast_wpseo_focuskw",
+	facebookImageId: "_yoast_wpseo_opengraph-image-id",
+};
+
+export const SYNC_TIME = {
+	wait: 1500,
+	max: 3000,
+};
