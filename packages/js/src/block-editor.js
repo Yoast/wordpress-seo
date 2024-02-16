acad0f6600588af96a0cbfcd768789448fd74510
Vraja Das <vraja.pro@gmail.com>
2024-02-16 18:02:54 +0100

    import helper functions from index

--- packages/js/src/block-editor.js
+++ packages/js/src/block-editor.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
 import initBlockEditorIntegration from "./initializers/block-editor-integration";
 import BlockEditorData from "./analysis/blockEditorData";
-import blockEditorSync from "./helpers/fields/blockEditorSync";
+import { blockEditorSync, hasHiddenFields } from "./helpers/fields";
 
 window.yoast = window.yoast || {};
 window.yoast.initEditorIntegration = initBlockEditorIntegration;
 window.yoast.EditorData = BlockEditorData;
 
+// console.log( hasHiddenFields() );
+// if ( hasHiddenFields() ) {
 blockEditorSync();
+// }
 /* eslint-enable no-unused-vars */
