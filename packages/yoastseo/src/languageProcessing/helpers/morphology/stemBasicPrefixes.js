9b9e12d427f7046b7c67b4a948076560f06d161e
aidamarfuaty <aida@yoast.com>
2024-02-19 16:59:21 +0100

    Create a separate helper for stemming basic prefixes

---
+++ packages/yoastseo/src/languageProcessing/helpers/morphology/stemBasicPrefixes.js
@@ -0,0 +1,30 @@
+/**
+ * An object containing the stem and the prefix.
+ *
+ * @typedef {Object} 	StemAndPrefixPair
+ * @property {string}	stem The word without the basic prefixes.
+ * @property {string}	prefix The prefix that was matched.
+ */
+
+/**
+ * Strips basic prefixes from a word.
+ *
+ * @param {string} word The word to strip the basic prefixes from.
+ * @param {RegExp} regex The regular expression to match the basic prefixes.
+ * @returns {StemAndPrefixPair} The word without the basic prefixes and the prefix that was stripped.
+ */
+export function stemBasicPrefixes( word, regex ) {
+	/*
+ 	 * If a word starts with one of the prefixes, we strip it.
+	 */
+	let stemmedWord = word;
+	let prefix = "";
+	const isPrefixed = word.match( regex );
+
+	if ( isPrefixed ) {
+		prefix = isPrefixed[ 0 ];
+		stemmedWord = word.slice( prefix.length );
+	}
+
+	return { stem: stemmedWord, prefix: prefix };
+}
