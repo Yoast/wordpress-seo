f221dd154b3fc86f553784192570b34e7f4cc028
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2024-03-06 10:17:25 +0200

    Replace .substr() with .substring() (or .slice()) in JS files since it's deprecated #20852

--- config/webpack/webpack.config.base.js
+++ config/webpack/webpack.config.base.js
@@ -51,10 +51,10 @@ module.exports = function( { entry, output, combinedOutputFile, cssExtractFileNa
 						return [ "yoast", yoastExternals[ request ] ];
 					}
 					if ( request.startsWith( "lodash/" ) ) {
-						return [ "lodash", request.substr( 7 ) ];
+						return [ "lodash", request.substring( 7 ) ];
 					}
 					if ( request.startsWith( "lodash-es/" ) ) {
-						return [ "lodash", request.substr( 10 ) ];
+						return [ "lodash", request.substring( 10 ) ];
 					}
 					if ( request === "react-select" ) {
 						return [ "yoast", "reactSelect" ];
@@ -63,7 +63,7 @@ module.exports = function( { entry, output, combinedOutputFile, cssExtractFileNa
 						return [ "yoast", "reactSelectAsync" ];
 					}
 					if ( request.startsWith( "@yoast/externals/" ) ) {
-						return [ "yoast", "externals", request.substr( 17 ) ];
+						return [ "yoast", "externals", request.substring( 17 ) ];
 					}
 				},
 				/**
@@ -85,7 +85,7 @@ module.exports = function( { entry, output, combinedOutputFile, cssExtractFileNa
 						return "yoast-seo-react-select";
 					}
 					if ( request.startsWith( "@yoast/externals/" ) ) {
-						return "yoast-seo-externals-" + request.substr( 17 );
+						return "yoast-seo-externals-" + request.substring( 17 );
 					}
 				},
 			} ),
