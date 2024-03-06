68b918ca584eb5ce2ec50fa70413947738cba56e
Vraja Das <vraja.pro@gmail.com>
2024-03-06 16:49:58 +0100

    relocate initialise of core editor store sync

--- packages/js/src/block-editor.js
+++ packages/js/src/block-editor.js
@@ -1,16 +1,7 @@
 /* eslint-disable no-unused-vars */
 import initBlockEditorIntegration from "./initializers/block-editor-integration";
 import BlockEditorData from "./analysis/blockEditorData";
-import { blockEditorSync, hasHiddenFields } from "./helpers/fields";
-import domReady from "@wordpress/dom-ready";
 
 window.yoast = window.yoast || {};
 window.yoast.initEditorIntegration = initBlockEditorIntegration;
 window.yoast.EditorData = BlockEditorData;
-
-domReady( () => {
-	if ( ! hasHiddenFields() ) {
-		blockEditorSync();
-	}
-} );
-
