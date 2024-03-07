011378d265549f8694421c511b388cf641560516
Vraja Das <vraja.pro@gmail.com>
2024-03-07 16:42:11 +0100

    add defensive coding to twitter fields

--- packages/js/src/helpers/fields/twitterFieldsStore.js
+++ packages/js/src/helpers/fields/twitterFieldsStore.js
@@ -1,27 +1,32 @@
 import { select } from "@wordpress/data";
 import { EDITOR_STORE } from "../../shared-admin/constants";
+import { defaultTo } from "lodash";
 
 /**
- * Retrieve twitter image id.
- * @returns {integer} The twitter image id.
+ * Retrieve twitter image id from the store.
+ *
+ * @returns {string} The twitter image id.
  */
-export const getTwitterImageId = () => select( EDITOR_STORE )?.getTwitterImageId();
+export const getTwitterImageId = () => String( defaultTo( select( EDITOR_STORE ).getTwitterImageId(), "" ) );
 
 /**
- * Get twitter title.
+ * Get twitter title from the store.
+ *
  * @returns {string} The twitter title.
  */
-export const getTwitterTitle = () => select( EDITOR_STORE )?.getTwitterTitle();
+export const getTwitterTitle = () => defaultTo( select( EDITOR_STORE ).getTwitterTitle(), "" );
 
 /**
- * Get twitter description.
+ * Get twitter description from the store.
+ *
  * @returns {string} The twitter description.
  */
-export const getTwitterDescription = () => select( EDITOR_STORE )?.getTwitterDescription();
+export const getTwitterDescription = () => defaultTo( select( EDITOR_STORE ).getTwitterDescription(), "" );
 
 /**
- * Get twitter image Url.
+ * Get twitter image Url from the store.
+ *
  * @returns {string} The twitter image Url.
  */
-export const getTwitterImageUrl = () => select( EDITOR_STORE )?.getTwitterImageUrl();
+export const getTwitterImageUrl = () => defaultTo( select( EDITOR_STORE ).getTwitterImageUrl(), "" );
 
