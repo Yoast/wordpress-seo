d53069a42c546270f68b7140f4852ec1cf9b2f12
Vraja Das <vraja.pro@gmail.com>
2024-03-07 16:34:46 +0100

    Add defensive coding to opengraph fields

--- packages/js/src/helpers/fields/facebookFieldsStore.js
+++ packages/js/src/helpers/fields/facebookFieldsStore.js
@@ -1,27 +1,28 @@
 import { select } from "@wordpress/data";
 import { EDITOR_STORE } from "../../shared-admin/constants";
+import { defaultTo } from "lodash";
 
 /**
  * Retrieve facebook image id.
  * @returns {integer} The facebook image id.
  */
-export const getFacebookImageId = () => select( EDITOR_STORE )?.getFacebookImageId();
+export const getFacebookImageId = () => String( defaultTo( select( EDITOR_STORE ).getFacebookImageId(), "" ) );
 
 /**
  * Get facebook title.
  * @returns {string} The facebook title.
  */
-export const getFacebookTitle = () => select( EDITOR_STORE )?.getFacebookTitle();
+export const getFacebookTitle = () => defaultTo( select( EDITOR_STORE ).getFacebookTitle(), "" );
 
 /**
  * Get facebook description.
  * @returns {string} The facebook description.
  */
-export const getFacebookDescription = () => select( EDITOR_STORE )?.getFacebookDescription();
+export const getFacebookDescription = () => defaultTo( select( EDITOR_STORE ).getFacebookDescription(), "" );
 
 /**
  * Get facebook image Url.
  * @returns {string} The facebook image Url.
  */
-export const getFacebookImageUrl = () => select( EDITOR_STORE )?.getFacebookImageUrl();
+export const getFacebookImageUrl = () => defaultTo( select( EDITOR_STORE )?.getFacebookImageUrl(), "" );
 
