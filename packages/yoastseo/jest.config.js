bf9d11e4389411f42cc40638f179f47887801f96
aidamarfuaty <aida@yoast.com>
2024-03-12 10:41:12 +0100

    Fix jest config and update yarn.lock

--- packages/yoastseo/jest.config.js
+++ packages/yoastseo/jest.config.js
@@ -19,9 +19,9 @@ module.exports = {
 	moduleNameMapper: {
 		"^lodash-es$": "lodash",
 		"^lodash-es/(.*)$": "lodash/$1",
-		// Without the next line, `import .. from "yoastseo"` statements would load from the build directory. In the
+		// Without the next line, `import ... from "yoastseo"` statements would load from the build directory. In the
 		// tests we always want to test the source.
-		"^yoastseo$": "<rootDir>/index.js",
+		"^yoastseo$": "<rootDir>/src/index.js",
 	},
 	testPathIgnorePatterns: [
 		"<rootDir>/node_modules/",
