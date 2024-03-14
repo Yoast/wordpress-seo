435fe5ca825177a650eca9993429974161cbb058
Paolo Luigi Scala <paolo@yoast.com>
2024-03-14 14:43:27 +0100

    Added last step

--- src/commands/First_Time_Configuration_Command.php
+++ src/commands/First_Time_Configuration_Command.php
@@ -2,15 +2,19 @@
 
 namespace Yoast\WP\SEO\Commands;
 
+use Exception;
 use WP_CLI;
 use WP_CLI\Utils;
 use Yoast\WP\SEO\Actions\Configuration\First_Time_Configuration_Action;
 use Yoast\WP\SEO\Main;
+use Yoast\WP\SEO\Helpers\Options_Helper;
+
 
 /**
  * Command to generate indexables for all posts and terms.
  */
 class First_Time_Configuration_Command implements Command_Interface {
+
 
 	/**
 	 *  The first tinme configuration action.
@@ -20,12 +24,22 @@ class First_Time_Configuration_Command implements Command_Interface {
 	private $first_time_configuration_action;
 
 	/**
+	 * The options helper.
+	 *
+	 * @var Options_Helper
+	 */
+	private $options_helper;
+
+	/**
 	 * The constructor.
 	 *
 	 * @param First_Time_Configuration_Action $first_time_configuration_action The first tinme configuration action.
 	 */
-	public function __construct( First_Time_Configuration_Action $first_time_configuration_action ) {
+	public function __construct(
+		First_Time_Configuration_Action $first_time_configuration_action,
+		Options_Helper $options_helper) {
 		$this->first_time_configuration_action = $first_time_configuration_action;
+		$this->options_helper                  = $options_helper;
 	}
 
 	/**
@@ -37,12 +51,14 @@ class First_Time_Configuration_Command implements Command_Interface {
 	 * @return void
 	 */
 	public function first_time_configuration() {
+		/// SEO data optimization
 		WP_CLI::confirm( \__( 'Do you want to run the SEO data optimization?', 'wordpress-seo' ) );
 
 		WP_CLI::runcommand( 'yoast index', $options = [] );
 		
 		WP_CLI::line( \__( "SEO data optimization completed successfully", 'wordpress-seo' ) );
 
+		/// Site representation
 		$options=['organization', 'person'];
 
 		$chosen_option = $this->ask( \__( 'Does your site represent an Organization or a Person?', 'wordpress-seo' ) , $options );
@@ -65,9 +81,68 @@ class First_Time_Configuration_Command implements Command_Interface {
 		}
 
 		$result = $this->first_time_configuration_action->set_site_representation( $representation_values );
-		
-		WP_CLI::line( var_dump($result) );
 
+		if ( $result->success === false ) {
+			WP_CLI::error( $result->message, false );
+		}
+		/// Social profiles
+
+		$facebook_profile = $this->ask( \__( 'Add your facebook profile or leave it empty.', 'wordpress-seo' ) );
+		$x_profile        = $this->ask( \__( 'Add your X profile or leave it empty.', 'wordpress-seo' ) );
+
+		$other_social_profiles = [];
+
+		do {
+			$skippable_confirm = $this->ask( \__( 'Do you want to add a social profile?', 'wordpress-seo' ) , ['y', 'n'] );
+
+			if ( $skippable_confirm === 'y' ) {
+				$other_social_profiles[] = $this->ask( \__( 'What is the URL of the social profile?', 'wordpress-seo' ) );
+			}
+		} while ( $skippable_confirm === 'y');
+
+		$result = $this->first_time_configuration_action->set_social_profiles(
+			[
+				'facebook_site' => $facebook_profile,
+				'twitter_site' => $x_profile,
+				'other_social_urls' => $other_social_profiles
+			]
+		);
+
+		// Personal preferences
+
+		$email = $this->ask( \__( 'Please specify your email if you want to receive our newsletter (leave blank otherwise)', 'wordpress-seo' ) );
+
+		if( ! empty( $email ) ) {
+			$customer_details = [
+				'customerDetails' => [
+					'firstName' => '',
+					'email' => $email
+				],
+					'list' => 'Yoast newsletter',
+					'source' => 'free'
+			];
+
+			// @todo: Please check if all the headers in packages/js/src/first-time-configuration/tailwind-components/steps/personal-preferences/newsletter-signup.js are needed here
+			$request_headers = [
+				'Content-Type' => 'application/json',
+				'Referrer-Policy' => 'no-referrer', 
+				'Cache-control' => 'no-cache',
+			];
+
+			$response = json_decode( $this->request( $customer_details, $request_headers ) );
+
+			if ( property_exists($response, 'error' ) || ( $response->status !== 'subscribed' ) ) {
+				WP_CLI::error( \__( "An error occurred while trying to subscribe to the newsletter.", "wordpress-seo" ), false );
+			} else {
+				WP_CLI::line( \__( "Thank you! Check your inbox for the confirmation email.", "wordpress-seo" ) );
+			}
+		}
+
+		$choice = $this->ask( \__( 'Do you want to be tracked?', 'wordpress-seo' ), ['y', 'n' ] );
+		
+		$tracking = $choice === 'y' ? true : false;
+		
+		$this->first_time_configuration_action->set_enable_tracking( ['tracking' => $tracking ] );
 	}
 	/**
 	 * Gets the namespace.
@@ -94,5 +169,24 @@ class First_Time_Configuration_Command implements Command_Interface {
 		fwrite( STDOUT, $question . \PHP_EOL );
 
 		return trim( fgets( STDIN ) );
+	}
+
+	public function request( $request_body = [], $request_headers = [] ) {
+		//$request_headers   = \array_merge( $request_headers, [ 'Content-Type' => 'application/json' ] );
+		$request_arguments = [
+			'timeout' => 30,
+			// phpcs:ignore Yoast.Yoast.JsonEncodeAlternative.Found -- Reason: We don't want the debug/pretty possibility.
+			'body'    => \wp_json_encode( $request_body ),
+			'headers' => $request_headers,
+		];
+
+		$response = \wp_remote_post( 'https://my.yoast.com/api/Mailing-list/subscribe', $request_arguments );
+
+		if ( \is_wp_error( $response ) ) {
+			// phpcs:ignore WordPress.Security.EscapeOutput.ExceptionNotEscaped -- false positive.
+			throw new Exception( $response->get_error_message(), $response->get_error_code() );
+		}
+
+		return \wp_remote_retrieve_body($response);
 	}
 }
