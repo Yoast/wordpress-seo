05ec046f4daa8268f19f13a60357fe361300b5bd
Paolo Luigi Scala <paolo@yoast.com>
2024-03-22 16:55:01 +0100

    Use proper methods instead of class attributes

--- src/helpers/current-page-helper.php
+++ src/helpers/current-page-helper.php
@@ -129,7 +129,7 @@ class Current_Page_Helper {
 	public function get_term_id() {
 		$wp_query = $this->wp_query_wrapper->get_main_query();
 
-		if ( $wp_query->is_tax || $wp_query->is_tag || $wp_query->is_category ) {
+		if ( $wp_query->is_tax() || $wp_query->is_tag() || $wp_query->is_category() ) {
 			$queried_object = $wp_query->get_queried_object();
 			if ( $queried_object && ! \is_wp_error( $queried_object ) ) {
 				return $queried_object->term_id;
