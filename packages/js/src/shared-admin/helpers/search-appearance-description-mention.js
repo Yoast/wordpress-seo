3c991aa056096d50a281757fcef57f141ecbb448
Jordi Prats Verdu <jordi.prats@yoast.com>
2024-03-25 15:47:07 +0100

    Find the correct css properties that match the design

--- packages/js/src/shared-admin/helpers/search-appearance-description-mention.js
+++ packages/js/src/shared-admin/helpers/search-appearance-description-mention.js
@@ -1,5 +1,6 @@
 import { Root, Badge } from "@yoast/ui-library";
 import { Fill } from "@wordpress/components";
+// import { useState, useCallback } from "@wordpress/element";
 import { get } from "lodash";
 import { addFilter } from "@wordpress/hooks";
 import { StoryComponent } from "@yoast/ui-library/build/elements/tooltip";
@@ -12,34 +13,59 @@ import { StoryComponent } from "@yoast/ui-library/build/elements/tooltip";
  *
  * @returns {JSX.node[]} The mentions.
  */
-const filterReplacementVariableEditorMentions = ( mentions, { fieldId } ) => {
-	const isRtl = get( window, "wpseoScriptData.metabox.isRtl", false );
+const filterReplacementVariableEditorMentions = (mentions, { fieldId }) => {
+	const isRtl = get(window, "wpseoScriptData.metabox.isRtl", false);
+	// const [isVisible, setIsVisible] = useState(false);
+	// const handleMouseEnter = useCallback(
+	// 	() => setIsVisible(true),
+	// 	[setIsVisible],
+	// );
+	// const handleMouseLeave = useCallback(
+	// 	() => setIsVisible(false),
+	// 	[setIsVisible],
+	// );
 	mentions.push(
 		<Fill
-			name={ `yoast.replacementVariableEditor.additionalMentions.${fieldId}` }
+			name={`yoast.replacementVariableEditor.additionalMentions.${fieldId}`}
 		>
-			<Root context={ { isRtl } }>
-				<Badge variant="plain" className="yst-text-slate-500" isVisible="true">
+			<Root context={{ isRtl }}>
+				<Badge
+					variant="plain"
+					className="yst-text-slate-500 yst-relative yst-cursor-pointer"
+					aria-describedby={id}
+					isVisible="true"
+				>
 					Date
-					<StoryComponent id={ fieldId } isVisible="true">
-						I am the date tooltip with a long text like
-						A grasshopper spent the summer hopping about in the sun and singing to his
-						heart's content. One day, an ant went hurrying by, looking very hot and weary.
+					<StoryComponent id={fieldId} isVisible="true" /*NOTES: the css properties of the toolpip component should be adjusted for this user case as follow: 
+					max-width: 576px; .yst-tooltip--top {yst--translate-x-11} &::before {transform: translateX(-14.8rem);} */>
+						The 'Date' variable is fixed and adds 14 chararacters to the length of your meta description.
 					</StoryComponent>
 				</Badge>
-				<Badge variant="plain" className="yst-text-slate-500">
+				{/* <Badge
+					variant="plain"
+					className="yst-text-slate-500"
+					aria-describedby={ id }
+					onMouseEnter={handleMouseEnter}
+					onMouseLeave={handleMouseLeave}
+				>
 					Separator
-					<StoryComponent id={ fieldId } position="left">
-						I am the separator tooltip
-					</StoryComponent>
-				</Badge>
+					{isVisible && (
+						<StoryComponent>
+							I am the separator tooltip
+						</StoryComponent>
+					)}
+				</Badge> */}
 			</Root>
-		</Fill>
+		</Fill>,
 	);
 
 	return mentions;
 };
 
 export const registerSearchAppearanceDescriptionMention = () => {
-	addFilter( "yoast.replacementVariableEditor.additionalMentions", "yoast/yoast-seo/Mentions", filterReplacementVariableEditorMentions );
+	addFilter(
+		"yoast.replacementVariableEditor.additionalMentions",
+		"yoast/yoast-seo/Mentions",
+		filterReplacementVariableEditorMentions,
+	);
 };
