bea6f2acbcfb70b2021c1eec4fe6e26084bcf8d3
Vraja Das <vraja.pro@gmail.com>
2024-03-26 13:56:35 +0100

    Added tests to analysisFieldsStore.js

---
+++ packages/js/tests/helpers/fields/analysisFieldsStore.test.js
@@ -0,0 +1,130 @@
+import {
+	getFocusKeyphrase,
+	getReadabilityScore,
+	getInclusiveLanguageScore,
+	getSeoScore,
+	getEstimatedReadingTime,
+	isCornerstoneContent,
+} from "../../../src/helpers/fields";
+import { EDITOR_STORE } from "../../../src/shared-admin/constants";
+import { select } from "@wordpress/data";
+
+jest.mock( "@wordpress/data", () => ( {
+	select: jest.fn(),
+} ) );
+
+
+const testCasesScoresInteger = [
+	{ method: "getReadabilityResults", getFunction: getReadabilityScore, returnValueSelect: { overallScore: 5 } },
+	{ method: "getInclusiveLanguageResults", getFunction: getInclusiveLanguageScore, returnValueSelect: { overallScore: 5 } },
+	{ method: "getSeoResults", getFunction: getSeoScore, returnValueSelect: { overallScore: 5 } },
+	{ method: "getEstimatedReadingTime", getFunction: getEstimatedReadingTime, returnValueSelect: 5 },
+];
+
+describe.each( testCasesScoresInteger )( "$method", ( { method, getFunction, returnValueSelect } ) => {
+	it( `should return string from ${method} when the score is an integer`, () => {
+		select.mockImplementation( ( store ) => {
+			if ( store === EDITOR_STORE ) {
+				return {
+					[ method ]: () => {
+						return returnValueSelect;
+					},
+				};
+			}
+		} );
+
+		const result = getFunction();
+		expect( result ).toBe( "5" );
+	  } );
+} );
+
+const testCasesScoresNull = [
+	{ method: "getReadabilityResults", getFunction: getReadabilityScore, returnValueSelect: { overallScore: null } },
+	{ method: "getInclusiveLanguageResults", getFunction: getInclusiveLanguageScore, returnValueSelect: { overallScore: null } },
+	{ method: "getSeoResults", getFunction: getSeoScore, returnValueSelect: { overallScore: null } },
+	{ method: "getEstimatedReadingTime", getFunction: getEstimatedReadingTime, returnValueSelect: null },
+];
+
+describe.each( testCasesScoresNull )( "$method", ( { method, getFunction, returnValueSelect } ) => {
+	it( `should return zero string from ${method} when null`, () => {
+	  select.mockImplementation( ( store ) => {
+			if ( store === EDITOR_STORE ) {
+				return {
+					[ method ]: () => {
+						return returnValueSelect;
+					},
+				};
+			}
+		} );
+
+		const result = getFunction();
+		expect( result ).toBe( "0" );
+	} );
+} );
+
+const testCasesScoresUndefined = [
+	{ method: "getReadabilityResults", getFunction: getReadabilityScore, returnValueSelect: { overallScore: undefined } },
+	{ method: "getInclusiveLanguageResults", getFunction: getInclusiveLanguageScore, returnValueSelect: { overallScore: undefined } },
+	{ method: "getSeoResults", getFunction: getSeoScore, returnValueSelect: { overallScore: undefined } },
+	{ method: "getEstimatedReadingTime", getFunction: getEstimatedReadingTime, returnValueSelect: undefined },
+];
+
+describe.each( testCasesScoresUndefined )( "$method", ( { method, getFunction, returnValueSelect } ) => {
+	it( `should return zero string from ${method} when undefined`, () => {
+	  select.mockImplementation( ( store ) => {
+			if ( store === EDITOR_STORE ) {
+				return {
+					[ method ]: () => {
+						return returnValueSelect;
+					},
+				};
+			}
+		} );
+
+		const result = getFunction();
+		expect( result ).toBe( "0" );
+	} );
+} );
+
+
+const boolenaTestCases = [
+	{ value: true, expected: "1" },
+	{ value: false, expected: "0" },
+	{ value: null, expected: "0" },
+	{ value: undefined, expected: "0" },
+];
+describe.each( boolenaTestCases )( "$method", ( { value, expected } ) => {
+	it( `should return ${expected} from isCornerstoneContent when value is ${value}`, () => {
+	  select.mockImplementation( ( store ) => {
+			if ( store === EDITOR_STORE ) {
+				return {
+					isCornerstoneContent: () => value,
+				};
+			}
+		} );
+
+		const result = isCornerstoneContent();
+		expect( result ).toBe( expected );
+	} );
+} );
+
+const focusKeyphraseTestCases = [
+	{ value: "test", expected: "test" },
+	{ value: null, expected: "" },
+	{ value: undefined, expected: "" },
+];
+
+describe.each( focusKeyphraseTestCases )( "$value", ( { value, expected } ) => {
+	it( `should return ${expected} from getFocusKeyphrase when value is ${value}`, () => {
+	  select.mockImplementation( ( store ) => {
+			if ( store === EDITOR_STORE ) {
+				return {
+					getFocusKeyphrase: () => value,
+				};
+			}
+		} );
+
+		const result = getFocusKeyphrase();
+		expect( result ).toBe( expected );
+	} );
+} );
