25026894beac937a032e99fbaee6ecc860f94980
Paolo Luigi Scala <paolo@yoast.com>
2024-03-29 16:33:34 +0100

    Change import

--- packages/js/src/components/ErrorBoundaryWithDefaultFallback.js
+++ packages/js/src/components/ErrorBoundaryWithDefaultFallback.js
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
+import { DefaultErrorFallback } from "./DefaultErrorFallback";
 import { ErrorBoundary } from "@yoast/ui-library";
-import DefaultErrorFallback from "./DefaultErrorFallback";
 
 /**
  * Wrapper around the ErrorBoundary component that sets the default fallback component to use.
  *
  * @param {JSX.Element} fallback The fallback component to render when an error occurs.
  * @param {JSX.Node} children The children
+ *
  * @returns {JSX.Element} The rendered component
  */
 const ErrorBoundaryWithDefaultFallback = ( { fallback = DefaultErrorFallback, children } ) => {
