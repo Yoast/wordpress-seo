5797a9aa6fb712363cb6e5e223823ca900bdd269
Paolo Luigi Scala <paolo@yoast.com>
2024-03-29 16:33:19 +0100

    Disable metabox tabs and render buttons according to location

--- packages/js/src/components/DefaultErrorFallback.js
+++ packages/js/src/components/DefaultErrorFallback.js
@@ -1,23 +1,31 @@
+import classNames from "classnames";
 import PropTypes from "prop-types";
 import { get } from "lodash";
 import { useCallback } from "@wordpress/element";
 import { __ } from "@wordpress/i18n";
 import { addQueryArgs } from "@wordpress/url";
 
+import { disableMetaboxTabs } from "../helpers/disableMetaboxTabs";
+import ScoreIconPortal from "./portals/ScoreIconPortal";
 import { Root, Title, Alert, Button } from "@yoast/ui-library";
+import { useRootContext } from "@yoast/externals/contexts";
 
 /**
  * The default error fallback component.
  *
  * @param {Object} error The error instance.
+ *
  * @returns {JSX.Element} The error fallback element.
  */
-const DefaultErrorFallback = ( { error } ) => {
+export const DefaultErrorFallback = ( { error } ) => {
 	const handleRefreshClick = useCallback( () => window?.location?.reload(), [] );
 	const linkParams = get( window, "wpseoScriptData.linkParams", [] );
 
 	const supportLink = addQueryArgs( "https://yoa.st/settings-error-support", linkParams );
 	const isRtl = get( window, "wpseoAdminGlobalL10n.isRtl", "" );
+	const { locationContext } = useRootContext();
+
+	disableMetaboxTabs();
 
 	return (
 		<Root context={ { isRtl } }>
@@ -26,13 +34,33 @@ const DefaultErrorFallback = ( { error } ) => {
 				<p>{ __( "We're very sorry, but it seems like the following error has interrupted our application:", "wordpress-seo" ) }</p>
 				<Alert variant="error">{ error?.message || __("Undefined error message.", "wordpress-seo" ) }</Alert>
 				<p>{ __( "Unfortunately, this means that all your unsaved changes will be lost. You can try and refresh this page to resolve the problem. If this error still occurs, please get in touch with our support team, and we'll get you all the help you need!", "wordpress-seo" ) }</p>
-				<div className="yst-flex yst-gap-2">
-					<Button onClick={ handleRefreshClick }>{ __( "Refresh this page", "wordpress-seo" ) }</Button>
+				<div
+					className={ classNames(
+						locationContext.includes( "metabox" ) && "yst-flex yst-gap-2",
+						locationContext.includes( "sidebar" ) && "yst-grid yst-grid-cols-1 yst-gap-y-2"
+					 ) }
+				>
+					<Button
+						className={ classNames(
+							locationContext.includes( "sidebar" ) && "yst-order-last"
+						) }
+						onClick={ handleRefreshClick }
+					>
+						{ __( "Refresh this page", "wordpress-seo" ) }
+					</Button>
 					<Button variant="secondary" as="a" href={ supportLink } target="_blank" rel="noopener">
 						{ __( "Contact support", "wordpress-seo" ) }
 					</Button>
 				</div>
 			</div>
+			<ScoreIconPortal
+				target="wpseo-readability-score-icon"
+				scoreIndicator={ "noindex" }
+			/>
+			<ScoreIconPortal
+				target="wpseo-seo-score-icon"
+				scoreIndicator={ "noindex" }
+			/>
 		</Root>
 	);
 };
@@ -40,5 +68,3 @@ const DefaultErrorFallback = ( { error } ) => {
 DefaultErrorFallback.propTypes = {
 	error: PropTypes.object.isRequired,
 };
-
-export default DefaultErrorFallback;
