7f57f797d9843b0a59223b4895a30069712efec7
Leonidas Milosis <leonidas.milossis@gmail.com>
2024-04-10 12:06:08 +0300

    Add unit tests for the custom meta framework

---
+++ tests/Unit/User_Meta/Framework/Custom_Meta/Author_Metadesc_Test.php
@@ -0,0 +1,50 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Tests\Unit\User_Meta\Framework\Custom_Meta;
+
+use Yoast\WP\SEO\Tests\Unit\TestCase;
+use Yoast\WP\SEO\User_Meta\Framework\Custom_Meta\Author_Metadesc;
+
+/**
+ * Class Author_Metadesc_Test
+ *
+ * @group user-meta
+ *
+ * @coversDefaultClass \Yoast\WP\SEO\User_Meta\Framework\Custom_Meta\Author_Metadesc
+ */
+final class Author_Metadesc_Test extends TestCase {
+
+	/**
+	 * Set up the test.
+	 *
+	 * @return void
+	 */
+	protected function set_up(): void {
+		parent::set_up();
+		$this->instance = new Author_Metadesc();
+	}
+
+	/**
+	 * Tests the getters.
+	 *
+	 * @covers ::get_key
+	 * @covers ::get_field_id
+	 *
+	 * @return void
+	 */
+	public function test_getters() {
+		$this->assertSame( 'wpseo_metadesc', $this->instance->get_key() );
+		$this->assertSame( 'wpseo_author_metadesc', $this->instance->get_field_id() );
+	}
+
+	/**
+	 * Tests getting if empty is allowed.
+	 *
+	 * @covers ::is_empty_allowed
+	 *
+	 * @return void
+	 */
+	public function test_is_empty_allowed() {
+		$this->assertSame( true, $this->instance->is_empty_allowed() );
+	}
+}
---
+++ tests/Unit/User_Meta/Framework/Custom_Meta/Inclusive_Language_Analysis_Disable_Test.php
@@ -0,0 +1,50 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Tests\Unit\User_Meta\Framework\Custom_Meta;
+
+use Yoast\WP\SEO\Tests\Unit\TestCase;
+use Yoast\WP\SEO\User_Meta\Framework\Custom_Meta\Inclusive_Language_Analysis_Disable;
+
+/**
+ * Class Inclusive_Language_Analysis_Disable_Test
+ *
+ * @group user-meta
+ *
+ * @coversDefaultClass \Yoast\WP\SEO\User_Meta\Framework\Custom_Meta\Inclusive_Language_Analysis_Disable
+ */
+final class Inclusive_Language_Analysis_Disable_Test extends TestCase {
+
+	/**
+	 * Set up the test.
+	 *
+	 * @return void
+	 */
+	protected function set_up(): void {
+		parent::set_up();
+		$this->instance = new Inclusive_Language_Analysis_Disable();
+	}
+
+	/**
+	 * Tests the getters.
+	 *
+	 * @covers ::get_key
+	 * @covers ::get_field_id
+	 *
+	 * @return void
+	 */
+	public function test_getters() {
+		$this->assertSame( 'wpseo_inclusive_language_analysis_disable', $this->instance->get_key() );
+		$this->assertSame( 'wpseo_inclusive_language_analysis_disable', $this->instance->get_field_id() );
+	}
+
+	/**
+	 * Tests getting if empty is allowed.
+	 *
+	 * @covers ::is_empty_allowed
+	 *
+	 * @return void
+	 */
+	public function test_is_empty_allowed() {
+		$this->assertSame( true, $this->instance->is_empty_allowed() );
+	}
+}
