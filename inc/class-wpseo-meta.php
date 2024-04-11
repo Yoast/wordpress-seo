7c03aee9326fd57a85e66957516db4b7e295f35f
vraja-pro <vraja.pro@gmail.com>
2024-04-11 10:46:33 +0200

    add_extra_wpseo_meta_fields should be used in an initialiser

--- inc/class-wpseo-meta.php
+++ inc/class-wpseo-meta.php
@@ -296,7 +296,7 @@ class WPSEO_Meta {
 
 		/**
 		 * Allow add-on plugins to register their meta fields for management by this class.
-		 * Calls to add_filter() must be made before plugins_loaded prio 14.
+		 * Calls to add_filter() must be made before plugins_loaded prio 14, use in an initialzer class.
 		 */
 		$extra_fields = apply_filters( 'add_extra_wpseo_meta_fields', [] );
 		if ( is_array( $extra_fields ) ) {
