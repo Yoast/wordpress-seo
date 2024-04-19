1c32db3d69fab37167786de189016f7d5c79694a
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2024-04-19 16:08:11 +0300

    Cleanup: Investigate if the files within yoastseo/src/snippetPreview can be removed #93
    Fix Insert variable dropdown.

--- packages/replacement-variable-editor/src/ReplacementVariableEditorStandalone.js
+++ packages/replacement-variable-editor/src/ReplacementVariableEditorStandalone.js
@@ -621,7 +621,7 @@ class ReplacementVariableEditorStandalone extends React.Component {
 	 */
 	render() {
 		const { MentionSuggestions } = this.pluginList.mentionsPlugin;
-		const { onFocus, onBlur, ariaLabelledBy, placeholder, theme, isDisabled, fieldId } = this.props;
+		const { onFocus, onBlur, ariaLabelledBy, placeholder, theme, isDisabled, fieldId, wrapperClassName } = this.props;
 		const { editorState, suggestions, isSuggestionsOpen } = this.state;
 
 		return (
@@ -650,7 +650,7 @@ class ReplacementVariableEditorStandalone extends React.Component {
 					fieldId
 				) }
 
-				<MentionSuggestionsStyleWrapper>
+				<MentionSuggestionsStyleWrapper className={wrapperClassName}>
 					<MentionSuggestions
 						onSearchChange={ this.onSearchChange }
 						suggestions={ suggestions }
@@ -676,6 +676,7 @@ ReplacementVariableEditorStandalone.propTypes = {
 	placeholder: PropTypes.string,
 	fieldId: PropTypes.string.isRequired,
 	isDisabled: PropTypes.bool,
+	wrapperClassName: PropTypes.string
 };
 
 ReplacementVariableEditorStandalone.defaultProps = {
