fd45e25a27f1a6f6d0f3079b66197306c81cc1eb
vraja-pro <vraja.pro@gmail.com>
2024-04-22 10:20:18 +0200

    rename selector import

--- packages/js/src/redux/selectors/index.js
+++ packages/js/src/redux/selectors/index.js
@@ -38,4 +38,4 @@ export * from "./WincherRequest";
 export * from "./WincherSEOPerformance";
 export * from "./isPremium";
 export * from "./isWooSEO";
-export * from "./postId";
+export * from "./post";
