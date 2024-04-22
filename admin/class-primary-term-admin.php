f7857b8ac257dbd3f85123a1e59699643ce5d89b
Vraja Das <vraja.pro@gmail.com>
2024-04-22 11:52:22 +0200

    removed the value from the primary taxonomy field, would be populated by the hidden fields sync

--- admin/class-primary-term-admin.php
+++ admin/class-primary-term-admin.php
@@ -65,10 +65,9 @@ class WPSEO_Primary_Term_Admin implements WPSEO_WordPress_Integration {
 	 */
 	protected function primary_term_field( $taxonomy_name ) {
 		return sprintf(
-			'<input class="yoast-wpseo-primary-term" type="hidden" id="%1$s" name="%2$s" value="%3$s" />',
+			'<input class="yoast-wpseo-primary-term" type="hidden" id="%1$s" name="%2$s"/>',
 			esc_attr( $this->generate_field_id( $taxonomy_name ) ),
 			esc_attr( $this->generate_field_name( $taxonomy_name ) ),
-			esc_attr( $this->get_primary_term( $taxonomy_name ) )
 		);
 	}
 
