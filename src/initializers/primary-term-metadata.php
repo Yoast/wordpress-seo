3857c7b6c701003cc8e6fb50c37044a01ffa0718
Vraja Das <vraja.pro@gmail.com>
2024-05-02 15:57:18 +0200

    php fix cs

--- src/initializers/primary-term-metadata.php
+++ src/initializers/primary-term-metadata.php
@@ -9,7 +9,7 @@ use Yoast\WP\SEO\Conditionals\No_Conditionals;
  */
 class Primary_Term_Metadata implements Initializer_Interface {
 
-    use No_Conditionals;
+	use No_Conditionals;
 
 	/**
 	 * Initializes the integration.
@@ -23,43 +23,41 @@ class Primary_Term_Metadata implements Initializer_Interface {
 		 * Called by WPSEO_Meta to add extra meta fields to the ones defined there.
 		 */
 		\add_filter( 'add_extra_wpseo_meta_fields', [ $this, 'add_meta_field' ] );
-        $taxonomies = $this->get_hierarchical_taxonomies_names();
-        foreach ( $taxonomies as $taxonomy_name ) {
-            \add_filter( 'wpseo_sanitize_post_meta_primary_' . $taxonomy_name, [ $this, 'sanitize_primary_term' ], 10, 4 );
-        }
-
+		$taxonomies = $this->get_hierarchical_taxonomies_names();
+		foreach ( $taxonomies as $taxonomy_name ) {
+			\add_filter( 'wpseo_sanitize_post_meta_primary_' . $taxonomy_name, [ $this, 'sanitize_primary_term' ], 10, 4 );
+		}
 	}
 
-    /**
-     * Get the hierarchical taxonomies.
-     * 
-     * @return array<WP_Taxonomy> The hierarchical taxonomies.
-     */
-    private function get_hierarchical_taxonomies_names() {
-        return get_taxonomies( [ 'hierarchical' => true ], 'names' );
-    }
+	/**
+	 * Get the hierarchical taxonomies.
+	 *
+	 * @return array<WP_Taxonomy> The hierarchical taxonomies.
+	 */
+	private function get_hierarchical_taxonomies_names() {
+		return \get_taxonomies( [ 'hierarchical' => true ], 'names' );
+	}
 
-    /**
-     * Sanitize the primary term.
-     * 
-     * @param mixed $value The value to sanitize.
-     * @param array<string> $field_def The field definition.
-     * @param string $meta_key The meta key.
-     * @param int $post_id The post ID.
-     * 
-     * @return mixed The sanitized value.
-     */
-    public function sanitize_primary_term( $clean, $meta_value, $field_def, $meta_key ) {
-        $clean = '';
-        if( strpos( $meta_key, WPSEO_Meta::$meta_prefix . 'primary_' ) === 0 ){
-            $int = WPSEO_Utils::validate_int( $meta_value );
-            if ( $int !== false && $int > 0 ) {
-                $clean = strval( $int );
-            }
-        }
-        return $clean;
-    }
-
+	/**
+	 * Sanitize the primary term.
+	 *
+	 * @param int|string    $clean      The clean value to sanitize.
+	 * @param int|string    $meta_value The meta value.
+	 * @param array<string> $field_def  The field definition.
+	 * @param string        $meta_key   The meta key.
+	 *
+	 * @return string The sanitized value.
+	 */
+	public function sanitize_primary_term( $clean, $meta_value, $field_def, $meta_key ) {
+		$clean = '';
+		if ( \strpos( $meta_key, WPSEO_Meta::$meta_prefix . 'primary_' ) === 0 ) {
+			$int = WPSEO_Utils::validate_int( $meta_value );
+			if ( $int !== false && $int > 0 ) {
+				$clean = \strval( $int );
+			}
+		}
+		return $clean;
+	}
 
 	/**
 	 * Adds the primary term to general tab.
@@ -70,7 +68,7 @@ class Primary_Term_Metadata implements Initializer_Interface {
 	 */
 	public function add_meta_field( $fields ) {
 
-		$taxonomies = get_taxonomies( [ 'hierarchical' => true ], 'names' );
+		$taxonomies = \get_taxonomies( [ 'hierarchical' => true ], 'names' );
 		foreach ( $taxonomies as $taxonomy_name ) {
 			$fields['primary_terms'][ 'primary_' . $taxonomy_name ] = [
 				'type'          => 'hidden',
