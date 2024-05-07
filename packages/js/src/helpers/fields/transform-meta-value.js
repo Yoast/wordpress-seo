5740f32b8d5f5f587ef77a54e37d39e78805aaa8
Vraja Das <vraja.pro@gmail.com>
2024-05-07 14:11:00 +0200

    prepare seo and social appearance with replacement variables

--- packages/js/src/helpers/fields/transform-meta-value.js
+++ packages/js/src/helpers/fields/transform-meta-value.js
@@ -1,4 +1,31 @@
 /* eslint-disable complexity */
+import { select } from "@wordpress/data";
+import { STORES } from "../../shared-admin/constants";
+
+/**
+ * Prepare twitter title to be saved in hidden field.
+ * @param {string} value The value to be saved.
+ * @returns {string} The value to be saved.
+ */
+const prepareSocialTitle = ( value ) => {
+	if ( value.trim() === select( STORES.editor ).getSocialTitleTemplate().trim() ) {
+		return "";
+	}
+	return value;
+};
+
+/**
+ * Prepare twitter and facebook description to be saved in hidden field.
+ * @param {string} value The value to be saved.
+ * @returns {string} The value to be saved.
+ */
+const prepareSocialDescription = ( value ) => {
+	if ( value.trim() === select( STORES.editor ).getSocialDescriptionTemplate().trim() ) {
+		return "";
+	}
+	return value;
+};
+
 /**
  * Prepare value to be saved in hidden field.
  *
@@ -22,6 +49,9 @@ export const transformMetaValue = ( key, value ) => {
 		"meta-robots-adv",
 	];
 
+	const titleKeys = [ "twitter-title", "opengraph-title", "title" ];
+	const descriptionKeys = [ "twitter-description", "opengraph-description", "desc", "metadesc" ];
+
 	switch ( true ) {
 		case integerKeys.includes( key ):
 		case key.startsWith( "primary_" ):
@@ -33,6 +63,10 @@ export const transformMetaValue = ( key, value ) => {
 			return value ? "1" : "0";
 		case arrayKeys.includes( key ):
 			return Array.isArray( value ) ? value.join() : value;
+		case titleKeys.includes( key ):
+			return prepareSocialTitle( value );
+		case descriptionKeys.includes( key ):
+			return prepareSocialDescription( value );
 		default:
 			return value;
 	}
