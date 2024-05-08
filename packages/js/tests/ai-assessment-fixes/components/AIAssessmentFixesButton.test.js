d06ec0d7891bc61e8f3c9b5c584ddf9faa98a2ea
aidamarfuaty <aida@yoast.com>
2024-05-08 14:59:53 +0200

    Update tests

--- packages/js/tests/ai-assessment-fixes/components/AIAssessmentFixesButton.test.js
+++ packages/js/tests/ai-assessment-fixes/components/AIAssessmentFixesButton.test.js
@@ -15,20 +15,20 @@ jest.mock( "@wordpress/data", () => {
 
 describe( "AIAssessmentFixesButton", () => {
 	test( "should find the correct aria-label in the document when hasAIFixes is true", () => {
-		render( <AIAssessmentFixesButton id="keyphraseDensity" hasAIFixes={ true } isPremium={ false } /> );
+		render( <AIAssessmentFixesButton id="keyphraseDensity" hasAIFixes={ true } isPremium={ false } isBlockEditor={ true } /> );
 
 		const labelText = document.querySelector( 'button[aria-label="Fix with AI"]' );
 		expect( labelText ).toBeInTheDocument();
 	} );
 
 	test( "should find the correct button id in the document when hasAIFixes is true", () => {
-		render( <AIAssessmentFixesButton id="keyphraseDensity" hasAIFixes={ true } isPremium={ false } /> );
+		render( <AIAssessmentFixesButton id="keyphraseDensity" hasAIFixes={ true } isPremium={ false } isBlockEditor={ true } /> );
 		const button = screen.getByRole( "button" );
 		expect( button ).toBeInTheDocument();
 	} );
 
 	test( "should NOT find the button id and the aria-label in the document when hasAIFixes is false", () => {
-		render( <AIAssessmentFixesButton id="keyphraseDensity" hasAIFixes={ false } isPremium={ false } /> );
+		render( <AIAssessmentFixesButton id="keyphraseDensity" hasAIFixes={ false } isPremium={ false } isBlockEditor={ true } /> );
 
 		const button = document.getElementById( "keyphraseDensityAIFixes" );
 		const labelText = document.querySelector( 'button[aria-label="Fix with AI"]' );
