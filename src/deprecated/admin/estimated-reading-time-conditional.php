7596d2042e378ba4c00a406428119b92d5a5b7ab
Vraja Das <vraja.pro@gmail.com>
2024-05-10 14:28:10 +0200

    deprecate estimate reading time

--- src/deprecated/admin/estimated-reading-time-conditional.php
+++ src/deprecated/admin/estimated-reading-time-conditional.php
@@ -6,6 +6,9 @@ use Yoast\WP\SEO\Conditionals\Conditional;
 
 /**
  * Conditional that is only when we want the Estimated Reading Time.
+ * 
+ * @codeCoverageIgnore
+ * @deprecated 22.8
  */
 class Estimated_Reading_Time_Conditional implements Conditional {
 
@@ -18,19 +21,27 @@ class Estimated_Reading_Time_Conditional implements Conditional {
 
 	/**
 	 * Constructs the Estimated Reading Time Conditional.
+	 * 
+	 * @codeCoverageIgnore
+	 * @deprecated 22.8
 	 *
 	 * @param Post_Conditional $post_conditional The post conditional.
 	 */
 	public function __construct( Post_Conditional $post_conditional ) {
+		_deprecated_function( __METHOD__, 'Yoast SEO 22.8' );
 		$this->post_conditional = $post_conditional;
 	}
 
 	/**
 	 * Returns whether this conditional is met.
+	 * 
+	 * @codeCoverageIgnore
+	 * @deprecated 22.8
 	 *
 	 * @return bool Whether the conditional is met.
 	 */
 	public function is_met() {
+		_deprecated_function( __METHOD__, 'Yoast SEO 22.8' );
 		// phpcs:disable WordPress.Security.NonceVerification.Recommended,WordPress.Security.NonceVerification.Missing -- Reason: Nonce verification should not be done in a conditional but rather in the classes using the conditional.
 		// Check if we are in our Elementor ajax request (for saving).
 		if ( \wp_doing_ajax() && isset( $_POST['action'] ) && \is_string( $_POST['action'] ) ) {
