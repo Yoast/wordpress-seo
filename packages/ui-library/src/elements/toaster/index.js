d97027713c5180ae58cfebbf34b3cea4c5d49e28
Paolo L. Scala <paolo@yoast.com>
2024-05-21 16:02:12 +0200

    Add the Close subcomponent and re-instate the position prop

--- packages/ui-library/src/elements/toaster/index.js
+++ packages/ui-library/src/elements/toaster/index.js
@@ -4,22 +4,58 @@ import { XIcon } from "@heroicons/react/outline";
 import classNames from "classnames";
 import { isArray, noop } from "lodash";
 import PropTypes from "prop-types";
-import React, { useCallback, useEffect } from "react";
+import React, { createContext, useCallback, useContext, useEffect } from "react";
 
-/**
- * @param {string} title The toaster title.
- * @returns {JSX.Element} The title.
- */
-const Title = ( {
-	title,
-} ) => {
-	return <p className="yst-text-sm yst-font-medium yst-text-slate-800">
-		{ title }
-	</p>;
+const ToasterContext = createContext( { onDismiss: noop } );
+
+const toasterClassNameMap = {
+	position: {
+		"bottom-center": "yst-translate-y-full",
+		"bottom-left": "yst-translate-y-full",
+		"top-center": "yst--translate-y-full",
+	},
 };
 
-Title.propTypes = {
-	title: PropTypes.string.isRequired,
+/**
+ * @param {JSX.node} children The children.
+ * @returns {JSX.Element} The content.
+ */
+const Content = ( {
+	children,
+} ) => {
+	return <div className="yst-w-0 yst-flex-1">
+		{ children }
+	</div>;
+};
+
+Content.propTypes = {
+	children: PropTypes.node.isRequired,
+};
+
+/**
+ * @param {string} dismissScreenReaderLabel The screen reader label for the dismiss button.
+ * @returns {JSX.Element} The close button.
+ */
+const Close = ( {
+	dismissScreenReaderLabel,
+} ) => {
+	const { handleDismiss } = useContext( ToasterContext );
+	return (
+		<div className="yst-flex-shrink-0 yst-flex">
+			<button
+				type="button"
+				onClick={ handleDismiss }
+				className="yst-bg-white yst-rounded-md yst-inline-flex yst-text-slate-400 hover:yst-text-slate-500 focus:yst-outline-none focus:yst-ring-2 focus:yst-ring-offset-2 focus:yst-ring-primary-500"
+			>
+				<span className="yst-sr-only">{ dismissScreenReaderLabel }</span>
+				<XIcon className="yst-h-5 yst-w-5" />
+			</button>
+		</div>
+	);
+};
+
+Close.propTypes = {
+	dismissScreenReaderLabel: PropTypes.string.isRequired,
 };
 
 /**
@@ -45,29 +81,29 @@ Description.propTypes = {
 };
 
 /**
- * @param {JSX.node} children The children.
- * @returns {JSX.Element} The content.
+ * @param {string} title The toaster title.
+ * @returns {JSX.Element} The title.
  */
-const Content = ( {
-	children,
+const Title = ( {
+	title,
 } ) => {
-	return <div className="yst-w-0 yst-flex-1">
-		{ children }
-	</div>;
+	return <p className="yst-text-sm yst-font-medium yst-text-slate-800">
+		{ title }
+	</p>;
 };
 
-Content.propTypes = {
-	children: PropTypes.node.isRequired,
+Title.propTypes = {
+	title: PropTypes.string.isRequired,
 };
 
 /**
  * @param {Object} props The props object.
  * @param {JSX.node} children The children.
  * @param {string} id The toaster ID.
- * @param {string} [className] Additional classes.
+ * @param {string} [className] The additional class names.
+ * @param {string} position The toaster position.
  * @param {Function} [onDismiss] Function to trigger on dismissal.
  * @param {number|null} [autoDismiss] Amount of milliseconds after which the message should auto dismiss, 0 indicating no auto dismiss.
- * @param {string} dismissScreenReaderLabel Screen reader label for dismiss button.
  * @param {boolean} isVisible Whether the notification is visible.
  * @param {Function} setIsVisible Function to set the visibility of the notification.
  * @returns {JSX.Element} The Toaster component.
@@ -76,9 +112,9 @@ const Toaster = ( {
 	children,
 	id,
 	className = "",
+	position = "bottom-left",
 	onDismiss = noop,
 	autoDismiss = null,
-	dismissScreenReaderLabel,
 	isVisible,
 	setIsVisible,
 } ) => {
@@ -104,36 +140,26 @@ const Toaster = ( {
 		return () => clearTimeout( timeout );
 	}, [] );
 	return (
-		<Transition
-			show={ isVisible }
-			enter={ "yst-transition yst-ease-in-out yst-duration-150" }
-			enterFrom={ "yst-opacity-0 yst-translate-y-full" }
-			enterTo="yst-translate-y-0"
-			leave={ "yst-transition yst-ease-in-out yst-duration-150" }
-			leaveFrom="yst-translate-y-0"
-			leaveTo={ "yst-opacity-0 yst-translate-y-full" }
-			className={ classNames(
-				"yst-toaster",
-				className,
-			) }
-			role="alert"
-		>
-			<div className="yst-flex yst-items-start yst-gap-3">
-				{ children }
-				{ onDismiss && (
-					<div className="yst-flex-shrink-0 yst-flex">
-						<button
-							type="button"
-							onClick={ handleDismiss }
-							className="yst-bg-white yst-rounded-md yst-inline-flex yst-text-slate-400 hover:yst-text-slate-500 focus:yst-outline-none focus:yst-ring-2 focus:yst-ring-offset-2 focus:yst-ring-primary-500"
-						>
-							<span className="yst-sr-only">{ dismissScreenReaderLabel }</span>
-							<XIcon className="yst-h-5 yst-w-5" />
-						</button>
-					</div>
+		<ToasterContext.Provider value={ { handleDismiss } }>
+			<Transition
+				show={ isVisible }
+				enter={ "yst-transition yst-ease-in-out yst-duration-150" }
+				enterFrom={ classNames( "yst-opacity-0", toasterClassNameMap.position[ position ] ) }
+				enterTo="yst-translate-y-0"
+				leave={ "yst-transition yst-ease-in-out yst-duration-150" }
+				leaveFrom="yst-translate-y-0"
+				leaveTo={ classNames( "yst-opacity-0", toasterClassNameMap.position[ position ] ) }
+				className={ classNames(
+					"yst-toaster",
+					className,
 				) }
-			</div>
-		</Transition>
+				role="alert"
+			>
+				<div className="yst-flex yst-items-start yst-gap-3">
+					{ children }
+				</div>
+			</Transition>
+		</ToasterContext.Provider>
 	);
 };
 
@@ -141,15 +167,16 @@ Toaster.propTypes = {
 	children: PropTypes.node,
 	id: PropTypes.string.isRequired,
 	className: PropTypes.string,
+	position: PropTypes.string,
 	onDismiss: PropTypes.func,
 	autoDismiss: PropTypes.number,
-	dismissScreenReaderLabel: PropTypes.string.isRequired,
 	isVisible: PropTypes.bool.isRequired,
 	setIsVisible: PropTypes.func.isRequired,
 };
 
-Toaster.Title = Title;
-Toaster.Description = Description;
 Toaster.Content = Content;
+Toaster.Close = Close;
+Toaster.Description = Description;
+Toaster.Title = Title;
 
 export default Toaster;
