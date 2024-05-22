11d318a1be2cf933042853b8eacb4fb21e467a67
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2024-05-22 16:26:29 +0200

    Revert "Only send canonical header if the header is for a non homepage page."
    This reverts commit 4e4d0a1edd9c7e57c1a777dd49252e0d62135def.

--- src/integrations/front-end/feed-improvements.php
+++ src/integrations/front-end/feed-improvements.php
@@ -99,11 +99,13 @@ class Feed_Improvements implements Integration_Interface {
 	 * @return void
 	 */
 	public function send_canonical_header( $for_comments ) {
+
 		if ( $for_comments || \headers_sent() ) {
 			return;
 		}
+
 		$url = $this->get_url_for_queried_object( $this->meta->for_home_page()->canonical );
-		if ( ! empty( $url ) && $url !== $this->meta->for_home_page()->canonical ) {
+		if ( ! empty( $url ) ) {
 			\header( \sprintf( 'Link: <%s>; rel="canonical"', $url ), false );
 		}
 	}
