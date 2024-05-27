4d9e2a9eee834d68ed24eb00198c4d074f7006d6
vraja-pro <vraja.pro@gmail.com>
2024-05-27 14:15:24 +0200

    fixed primary term initial state

--- packages/js/src/redux/initial-state/primaryTaxonomies.js
+++ packages/js/src/redux/initial-state/primaryTaxonomies.js
@@ -3,7 +3,7 @@ import { get, reduce } from "lodash";
 const primaryTerms = get( window, "wpseoPrimaryCategoryL10n.taxonomies", {} );
 
 export const primaryTaxonomies = reduce( primaryTerms, ( acc, value, key ) => {
-	acc[ key ] = value.primary || -1;
+	acc[ value.name ] = value.primary || -1;
 	return acc;
 }, {} );
 
