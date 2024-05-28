135ba8ff688d8aacf6f388ec54a2319a23458d26
vraja-pro <vraja.pro@gmail.com>
2024-05-28 14:03:13 +0200

    use name instead of key

--- packages/js/src/redux/initial-state/primaryTaxonomies.js
+++ packages/js/src/redux/initial-state/primaryTaxonomies.js
@@ -2,8 +2,8 @@ import { get, reduce } from "lodash";
 
 const primaryTerms = get( window, "wpseoPrimaryCategoryL10n.taxonomies", {} );
 
-export const primaryTaxonomies = reduce( primaryTerms, ( acc, value, key ) => {
-	acc[ key ] = value.primary || -1;
+export const primaryTaxonomies = reduce( primaryTerms, ( acc, value ) => {
+	acc[ value.name ] = value.primary || -1;
 	return acc;
 }, {} );
 
