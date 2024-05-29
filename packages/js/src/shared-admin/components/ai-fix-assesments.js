b28af624860d0b2b3c8b7eb9f09fca763126b3ff
vraja-pro <vraja.pro@gmail.com>
2024-05-29 16:39:57 +0200

    Replace introduction model

---
+++ packages/js/src/shared-admin/components/ai-fix-assesments.js
@@ -0,0 +1,148 @@
+import { LockOpenIcon } from "@heroicons/react/outline";
+import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
+import { createInterpolateElement } from "@wordpress/element";
+import { __, sprintf } from "@wordpress/i18n";
+import { Badge, Button, useModalContext } from "@yoast/ui-library";
+import PropTypes from "prop-types";
+import { OutboundLink, VideoFlow } from ".";
+
+/**
+ * @param {string} learnMoreLink The learn more link.
+ * @param {Object} thumbnail The thumbnail: img props.
+ * @param {Object} wistiaEmbedPermission The value, status and set for the Wistia embed permission.
+ * @param {string} upsellLink The upsell link.
+ * @param {boolean} isProductCopy Whether the upsell is for a product.
+ * @param {string} title The title.
+ * @param {string} upsellLabel The upsell label.
+ * @param {string} newToText The new to text.
+ * @param {string|JSX.Element } bundleNote The bundle note.
+ * @returns {JSX.Element} The element.
+ */
+export const AiFixAssesments = ( {
+	learnMoreLink,
+	thumbnail,
+	wistiaEmbedPermission,
+	upsellLink,
+	title,
+	upsellLabel,
+	newToText,
+	bundleNote,
+} ) => {
+	const { onClose, initialFocus } = useModalContext();
+
+	const learnMoreLinkStructure = {
+		// eslint-disable-next-line jsx-a11y/anchor-has-content
+		a: <OutboundLink
+			href={ learnMoreLink }
+			className="yst-inline-flex yst-items-center yst-gap-1 yst-no-underline yst-font-medium"
+			variant="primary"
+		/>,
+		ArrowNarrowRightIcon: <ArrowNarrowRightIcon className="yst-w-4 yst-h-4 rtl:yst-rotate-180" />,
+	};
+
+	return (
+		<div className="yst-flex yst-flex-col yst-items-center yst-p-10">
+			<div className="yst-relative yst-w-full">
+				<VideoFlow
+					videoId="vmrahpfjxp"
+					thumbnail={ thumbnail }
+					wistiaEmbedPermission={ wistiaEmbedPermission }
+				/>
+				<Badge className="yst-absolute yst-top-0 yst-right-2 yst-mt-2 yst-ml-2" variant="info">Beta</Badge>
+			</div>
+			<div className="yst-mt-6 yst-text-xs yst-font-medium">
+				<span className="yst-introduction-modal-uppercase">
+					{ newToText }
+				</span>
+			</div>
+			<div className="yst-mt-4 yst-mx-1.5 yst-text-center">
+				<h3 className="yst-text-slate-900 yst-text-lg yst-font-medium">
+					{ title }
+				</h3>
+				<div className="yst-mt-2 yst-text-slate-600 yst-text-sm">
+					{ createInterpolateElement(
+							sprintf(
+								/* translators: %1$s and %2$s are anchor tags; %3$s is the arrow icon. */
+								__(
+									"Let AI fix assesments. %1$sLearn more%2$s%3$s",
+									"wordpress-seo"
+								),
+								"<a>",
+								"<ArrowNarrowRightIcon />",
+								"</a>"
+							),
+							learnMoreLinkStructure
+						)
+					}
+				</div>
+			</div>
+			<div className="yst-w-full yst-flex yst-mt-10">
+				<Button
+					as="a"
+					className="yst-grow"
+					size="extra-large"
+					variant="upsell"
+					href={ upsellLink }
+					target="_blank"
+					ref={ initialFocus }
+				>
+					<LockOpenIcon className="yst--ml-1 yst-mr-2 yst-h-5 yst-w-5" />
+					{ upsellLabel }
+					<span className="yst-sr-only">
+						{
+							/* translators: Hidden accessibility text. */
+							__( "(Opens in a new browser tab)", "wordpress-seo" )
+						}
+					</span>
+				</Button>
+			</div>
+			{ bundleNote }
+			<Button
+				as="a"
+				className="yst-mt-4"
+				variant="tertiary"
+				onClick={ onClose }
+			>
+				{ __( "Close", "wordpress-seo" ) }
+			</Button>
+		</div>
+	);
+};
+AiFixAssesments.propTypes = {
+	learnMoreLink: PropTypes.string.isRequired,
+	upsellLink: PropTypes.string.isRequired,
+	thumbnail: PropTypes.shape( {
+		src: PropTypes.string.isRequired,
+		width: PropTypes.string,
+		height: PropTypes.string,
+	} ).isRequired,
+	wistiaEmbedPermission: PropTypes.shape( {
+		value: PropTypes.bool.isRequired,
+		status: PropTypes.string.isRequired,
+		set: PropTypes.func.isRequired,
+	} ).isRequired,
+	title: PropTypes.string,
+	upsellLabel: PropTypes.string,
+	newToText: PropTypes.string,
+	isProductCopy: PropTypes.bool,
+	bundleNote: PropTypes.oneOfType( [
+		PropTypes.string,
+		PropTypes.element,
+	] ),
+};
+
+AiFixAssesments.defaultProps = {
+	title: __( "Use AI to fix assesments!", "wordpress-seo" ),
+	upsellLabel: sprintf(
+		/* translators: %1$s expands to Yoast SEO Premium. */
+		__( "Unlock with %1$s", "wordpress-seo" ),
+		"Yoast SEO Premium"
+	),
+	newToText: sprintf(
+		/* translators: %1$s expands to Yoast SEO Premium. */
+		__( "New in %1$s", "wordpress-seo" ),
+		"Yoast SEO Premium"
+	),
+	isProductCopy: false,
+	bundleNote: "",
+};
---
+++ src/introductions/application/ai-fix-assesments-upsell.php
@@ -0,0 +1,94 @@
+<?php
+
+
+namespace Yoast\WP\SEO\Introductions\Application;
+
+use Yoast\WP\SEO\Helpers\Options_Helper;
+use Yoast\WP\SEO\Helpers\User_Helper;
+use Yoast\WP\SEO\Introductions\Application\User_Allowed_Trait;
+use Yoast\WP\SEO\Introductions\Domain\Introduction_Interface;
+
+/**
+ * Represents the introduction for the AI fix assessments feature.
+ *
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded
+ */
+class Ai_Fix_Assessments_Upsell implements Introduction_Interface {
+
+	use User_Allowed_Trait;
+
+	public const ID = 'ai-fix-assessments';
+
+	/**
+	 * Holds the options helper.
+	 *
+	 * @var Options_Helper
+	 */
+	private $options_helper;
+
+	/**
+	 * Holds the user helper.
+	 *
+	 * @var User_Helper
+	 */
+	private $user_helper;
+
+	/**
+	 * Constructs the introduction.
+	 *
+	 * @param Options_Helper $options_helper The options helper.
+	 * @param User_Helper    $user_helper    The user helper.
+	 */
+	public function __construct( Options_Helper $options_helper, User_Helper $user_helper ) {
+		$this->options_helper = $options_helper;
+		$this->user_helper    = $user_helper;
+	}
+
+	/**
+	 * Returns the ID.
+	 *
+	 * @return string The ID.
+	 */
+	public function get_id(): string {
+		return self::ID;
+	}
+
+	/**
+	 * Returns the name of the introdyction.
+	 *
+	 * @return string The name.
+	 */
+	public function get_name(): string {
+		\_deprecated_function( __METHOD__, 'Yoast SEO Premium 21.6', 'Please use get_id() instead' );
+
+		return self::ID;
+	}
+
+	/**
+	 * Returns the requested pagination priority. Lower means earlier.
+	 *
+	 * @return int The priority.
+	 */
+	public function get_priority(): int {
+		return 10;
+	}
+
+	/**
+	 * Returns whether this introduction should show.
+	 *
+	 * @return bool Whether this introduction should show.
+	 */
+	public function should_show(): bool {
+		// Get the current user ID, if no user is logged in we bail as this is needed for the next checks.
+		$current_user_id = $this->user_helper->get_current_user_id();
+		if ( $current_user_id === 0 ) {
+			return false;
+		}
+
+		if ( ! $this->is_user_allowed( [ 'edit_posts' ] ) ) {
+			return false;
+		}
+
+		return true;
+	}
+}
