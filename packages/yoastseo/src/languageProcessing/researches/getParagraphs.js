130cffc4b583ad5970e42c967b34496ba38a5a56
aidamarfuaty <aida@yoast.com>
2024-05-30 14:09:20 +0200

    Remove the possibility to filter paragraphs

--- packages/yoastseo/src/languageProcessing/researches/getParagraphs.js
+++ packages/yoastseo/src/languageProcessing/researches/getParagraphs.js
@@ -1,19 +1,14 @@
 import { reject } from "lodash";
-import filterTree from "../../parse/build/private/filterTree";
 
 /**
  * Returns all paragraphs in a given Paper.
  * Remove paragraphs that do not contain sentences or only consist of links.
  *
  * @param {Paper} paper The current paper.
- * @param {Function[]} 	filters 	An array of callbacks. If a callback returns true, the node is discarded.
  * @returns {Paragraph[]} All paragraphs in the paper.
  */
-export default function( paper, filters = [] ) {
-	let tree = paper.getTree();
-	if ( filters.length > 0 ) {
-		tree = filterTree( tree, filters );
-	}
+export default function( paper ) {
+	const tree = paper.getTree();
 	let paragraphs = tree.findAll( node => node.name === "p" );
 
 	// Remove empty paragraphs without sentences and paragraphs only consisting of links.
