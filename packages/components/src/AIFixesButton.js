e5270c718543b3098f7c9bcf57c4c8cb3b6e3a25
Jordi Prats Verdu <jordi.prats@yoast.com>
2024-06-05 13:41:33 +0200

    Corrects indentation

--- packages/components/src/AIFixesButton.js
+++ packages/components/src/AIFixesButton.js
@@ -5,12 +5,13 @@ import IconButtonBase from "./IconButtonBase";
 const yoastPrimary50 = "#FAF3F7";
 const yoastPrimary100 = "#F3E5ED";
 const yoastPrimary300 = "#CD82AB";
+const yoastPrimary400 = "#B94986";
 const yoastPrimary500 = "#A61E69";
 const blue50 = "#EFF6FF";
 const blue100 = "#DBEAFE";
 const blue300 = "#93C5FD";
 const blue500 = "#3B82F6";
-const yoastPrimary400 = "#B94986";
+
 const direction = "to bottom right";
 
 const gradientEffect = {
@@ -22,13 +23,16 @@ const gradientEffect = {
 
 const AIFixesButtonBase = styled( IconButtonBase )`
 	overflow: hidden;
-	border: ${ props => props.pressed
-		? "none" : "1px solid transparent" };
+	border: ${ props => props.pressed ? "none" : "1px solid transparent" };
 	border-image: ${ props => props.pressed ? "none" : gradientEffect.defaultStateBorder };
-	background-image: ${ props => props.pressed ? gradientEffect.pressedStateBackground : gradientEffect.defaultStateBackground } !important;
+	background-image: ${ props => props.pressed
+		? gradientEffect.pressedStateBackground
+		: gradientEffect.defaultStateBackground } !important;
 
 	&:hover {
-		background-image:  ${ props => props.pressed ? gradientEffect.pressedStateBackground : gradientEffect.hoverStateBackground } !important;
+		background-image:  ${ props => props.pressed
+			? gradientEffect.pressedStateBackground
+			: gradientEffect.hoverStateBackground } !important;
 	}
 
 	box-shadow: ${ props => props.pressed
