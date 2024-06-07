6053238824098912e6794b8e585404ccfa4a5e06
vraja-pro <vraja.pro@gmail.com>
2024-06-07 13:43:13 +0200

    add snapshot test

---
+++ packages/js/tests/ai-assessment-fixes/components/SparklesIcon.test.js
@@ -0,0 +1,23 @@
+import { render } from "../../test-utils";
+
+import { SparklesIcon } from "../../../src/ai-assessment-fixes/components/sparkles-icon";
+
+const testCases = [
+	{
+		name: "pressed is false",
+		pressed: false,
+	},
+	{
+		name: "pressed is true",
+		pressed: true,
+	},
+
+];
+
+describe.each( testCases )( "SparklesIcon", ( { name, pressed } ) => {
+	test( `should render the SparklesIcon component when ${name}`, () => {
+		const { container } = render( <SparklesIcon pressed={ pressed } /> );
+
+		expect( container ).toMatchSnapshot();
+	} );
+} );
