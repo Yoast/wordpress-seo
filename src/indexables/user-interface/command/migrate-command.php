8de475c44131b417596bb84d10bbb552de5e38df
Paolo L. Scala <paolo@yoast.com>
2024-06-20 14:41:52 +0200

    Added loop over all the migrations
    Added an error message and a summary of the migrated records

--- src/indexables/user-interface/command/migrate-command.php
+++ src/indexables/user-interface/command/migrate-command.php
@@ -4,21 +4,23 @@ namespace Yoast\WP\SEO\Indexables\User_Interface\Command;
 
 use WP_CLI\Utils;
 use Yoast\WP\SEO\Commands\Command_Interface;
-use Yoast\WP\SEO\Indexables\Application\Actions\Migration\Indexable_Action;
+use Yoast\WP\SEO\Indexables\Domain\Actions\Migration_Interface;
 use Yoast\WP\SEO\Main;
 
 /**
  * Command to generate indexables for all posts and terms.
  */
-class Migrate_Command implements Command_Interface {
+class Migrate_Command implements Command_Interface
+{
 
 	/**
-	 * @var Indexable_Action
+	 * @var array<Abstract_Migration>
 	 */
-	private $indexable_action;
+	private $migration_actions;
 
-	public function __construct(Indexable_Action $indexable_action ) {
-		$this->indexable_action = $indexable_action;
+	public function __construct(Migration_Interface  ...$migration_actions)
+	{
+		$this->migration_actions = $migration_actions;
 	}
 
 	/**
@@ -26,7 +28,8 @@ class Migrate_Command implements Command_Interface {
 	 *
 	 * @return string
 	 */
-	public static function get_namespace() {
+	public static function get_namespace()
+	{
 		return Main::WP_CLI_NAMESPACE;
 	}
 
@@ -43,27 +46,44 @@ class Migrate_Command implements Command_Interface {
 	 *
 	 * @when after_wp_load
 	 *
-	 * @param array|null $args       The arguments.
+	 * @param array|null $args The arguments.
 	 * @param array|null $assoc_args The associative arguments.
 	 *
 	 * @return void
 	 */
-	public function migrate( $args = null, $assoc_args = null ) {
-		if ( ! isset( $assoc_args['old-url'] ) || ! isset( $assoc_args['new-url'] )  ) {
-			return;
+	public function migrate($args = null, $assoc_args = null)
+	{
+		if (!isset($assoc_args['old-url']) || !isset($assoc_args['new-url'])) {
+			\WP_CLI::error( \__( 'Please provide both the old and new URL.', 'wordpress-seo' ) );
 		}
-		
-		$total = $this->indexable_action->get_total_unmigrated();
-		if ( $total > 0 ) {
-			$limit    = $this->indexable_action->get_limit();
-			$progress = Utils\make_progress_bar( 'Migrating indexables', $total );
-			do {
-				$indexables = $this->indexable_action->migrate( $assoc_args['old-url'], $assoc_args['new-url'] );
-				$progress->tick( $indexables );
-				\usleep( $interval );
-				Utils\wp_clear_object_cache();
-			} while ( $indexables >= $limit );
-			$progress->finish();
+
+		$total_migrated = 0;
+
+		foreach ($this->migration_actions as $migration_action) {
+			$total = $migration_action->get_total_unmigrated();
+			if ($total > 0) {
+				$limit = $migration_action->get_limit();
+				$progress = Utils\make_progress_bar('Migrating ' . $migration_action->get_name(), $total);
+				do {
+					$indexables = $migration_action->migrate($assoc_args['old-url'], $assoc_args['new-url']);
+					$progress->tick($indexables);
+					$total_migrated += $indexables;
+				} while ($indexables >= $limit);
+				$progress->finish();
+			}
 		}
+
+		\WP_CLI::success(
+			\sprintf(
+			/* translators: %1$d is the number of records that are removed. */
+				\_n(
+					'Migrated %1$d record.',
+					'Migrated %1$d records.',
+					$total_migrated,
+					'wordpress-seo'
+				),
+				$total_migrated
+			)
+		);
 	}
 }
