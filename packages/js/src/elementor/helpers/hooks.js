9b834894d270926ad778efa3af507aa1a6629fc0
Igor <35524806+igorschoester@users.noreply.github.com>
2024-07-26 09:27:03 +0200

    Add getConditions support to our Elementor hooks
    Return the callback like Elementor does -- not currently using

--- packages/js/src/elementor/helpers/hooks.js
+++ packages/js/src/elementor/helpers/hooks.js
@@ -1,6 +1,11 @@
 /* global $e */
 
 /**
+ * @returns {boolean} True.
+ */
+const returnTrue = () => true;
+
+/**
  * A class that conforms to the Elementor hooks format,
  * so that we can register its instance as a callback.
  *
@@ -10,15 +15,17 @@ class ElementorUIHook extends $e.modules.hookUI.Base {
 	/**
 	 * Constructs this class.
 	 *
-	 * @param {string}   hook     The hook to register to.
-	 * @param {string}   id       The id to register our callback behind.
+	 * @param {string} hook The hook to register to.
+	 * @param {string} id The id to register our callback behind.
 	 * @param {function} callback The function to call when the hook is fired.
+	 * @param {function} conditions The function to call to check if the hook should run. Defaults to a function that always returns true.
 	 */
-	constructor( hook, id, callback ) {
+	constructor( hook, id, callback, conditions = returnTrue ) {
 		super();
 		this.hook = hook;
 		this.id = id;
 		this.callback = callback;
+		this.conditions = conditions;
 	}
 
 	/**
@@ -27,7 +34,7 @@ class ElementorUIHook extends $e.modules.hookUI.Base {
 	 * @returns {string} The hook to register to.
 	 */
 	getCommand() {
-	  return this.hook;
+		return this.hook;
 	}
 
 	/**
@@ -36,7 +43,18 @@ class ElementorUIHook extends $e.modules.hookUI.Base {
 	 * @returns {string} The id to register behind.
 	 */
 	getId() {
-	  return this.id;
+		return this.id;
+	}
+
+	/**
+	 * A getter for the conditions function.
+	 *
+	 * @param {*} args The arguments passed to the hook.
+	 *
+	 * @returns {boolean} Whether the hook should run.
+	 */
+	getConditions( ...args ) {
+		return this.conditions( ...args );
 	}
 
 	/**
@@ -61,15 +79,17 @@ class ElementorDataHook extends $e.modules.hookData.Base {
 	/**
 	 * Constructs this class.
 	 *
-	 * @param {string}   hook     The hook to register to.
-	 * @param {string}   id       The id to register our callback behind.
+	 * @param {string} hook The hook to register to.
+	 * @param {string} id The id to register our callback behind.
 	 * @param {function} callback The function to call when the hook is fired.
+	 * @param {function} conditions The function to call to check if the hook should run. Defaults to a function that always returns true.
 	 */
-	constructor( hook, id, callback ) {
+	constructor( hook, id, callback, conditions = returnTrue ) {
 		super();
 		this.hook = hook;
 		this.id = id;
 		this.callback = callback;
+		this.conditions = conditions.bind( this );
 	}
 
 	/**
@@ -91,6 +111,17 @@ class ElementorDataHook extends $e.modules.hookData.Base {
 	}
 
 	/**
+	 * A getter for the conditions function.
+	 *
+	 * @param {*} args The arguments passed to the hook.
+	 *
+	 * @returns {boolean} Whether the hook should run.
+	 */
+	getConditions( ...args ) {
+		return this.conditions( ...args );
+	}
+
+	/**
 	 * This function is called when the hook is fired.
 	 *
 	 * @param {*} args The arguments passed to the hook.
@@ -105,39 +136,41 @@ class ElementorDataHook extends $e.modules.hookData.Base {
 /**
  * Initializes the Elementor UI after hooks and registers them.
  *
- * @param {string}   hook     The hook to register to.
- * @param {string}   id       The id to register our callback behind.
+ * @param {string} hook The hook to register to.
+ * @param {string} id The id to register our callback behind.
  * @param {function} callback The function to call when the hook is fired.
+ * @param {function} conditions The function to call to check if the hook should run. Defaults to a function that always returns true.
  *
- * @returns {void}
+ * @returns {{callback: *, id: *, isActive: boolean}} The Elementor callback function.
  */
-export function registerElementorUIHookAfter( hook, id, callback ) {
-	$e.hooks.registerUIAfter( new ElementorUIHook( hook, id, callback ) );
+export function registerElementorUIHookAfter( hook, id, callback, conditions = returnTrue ) {
+	return $e.hooks.registerUIAfter( new ElementorUIHook( hook, id, callback, conditions ) );
 }
-
 
 /**
  * Initializes the Elementor UI before hooks and registers them.
  *
- * @param {string}   hook     The hook to register to.
- * @param {string}   id       The id to register our callback behind.
+ * @param {string} hook The hook to register to.
+ * @param {string} id The id to register our callback behind.
  * @param {function} callback The function to call when the hook is fired.
+ * @param {function} conditions The function to call to check if the hook should run. Defaults to a function that always returns true.
  *
- * @returns {void}
+ * @returns {{callback: *, id: *, isActive: boolean}} The Elementor callback function.
  */
-export function registerElementorUIHookBefore( hook, id, callback ) {
-	$e.hooks.registerUIBefore( new ElementorUIHook( hook, id, callback ) );
+export function registerElementorUIHookBefore( hook, id, callback, conditions = returnTrue ) {
+	return $e.hooks.registerUIBefore( new ElementorUIHook( hook, id, callback, conditions ) );
 }
 
 /**
  * Initializes the Elementor Data after hooks and registers them.
  *
- * @param {string}   hook     The hook to register to.
- * @param {string}   id       The id to register our callback behind.
+ * @param {string} hook The hook to register to.
+ * @param {string} id The id to register our callback behind.
  * @param {function} callback The function to call when the hook is fired.
+ * @param {function} conditions The function to call to check if the hook should run. Defaults to a function that always returns true.
  *
- * @returns {void}
+ * @returns {{callback: *, id: *, isActive: boolean}} The Elementor callback function.
  */
-export function registerElementorDataHookAfter( hook, id, callback ) {
-	$e.hooks.registerDataAfter( new ElementorDataHook( hook, id, callback ) );
+export function registerElementorDataHookAfter( hook, id, callback, conditions = returnTrue ) {
+	return $e.hooks.registerDataAfter( new ElementorDataHook( hook, id, callback, conditions ) );
 }
