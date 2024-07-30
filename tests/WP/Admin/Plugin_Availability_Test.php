7e493beb6fbc089405acceea1466da4792cc5c4a
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2024-07-30 13:42:55 +0200

    Adjust wp tests.

--- tests/WP/Admin/Plugin_Availability_Test.php
+++ tests/WP/Admin/Plugin_Availability_Test.php
@@ -33,32 +33,10 @@ final class Plugin_Availability_Test extends TestCase {
 		self::$class_instance = $plugin_availability;
 	}
 
-	/**
-	 * Tests whether or not a plugin exists.
-	 *
-	 * @covers ::get_plugin
-	 *
-	 * @return void
-	 */
-	public function test_plugin_existence() {
-		$expected = [
-			'url'          => 'http://example.com/',
-			'title'        => 'Test Plugin',
-			'description'  => '',
-			'version'      => '3.3',
-			'installed'    => true,
-			'slug'         => 'test-plugin/test-plugin.php',
-			'version_sync' => true,
-		];
-
-		$this->assertEquals( self::$class_instance->get_plugin( 'test-plugin' ), $expected );
-		$this->assertEquals( self::$class_instance->get_plugin( 'non-exisiting-test-plugin' ), [] );
-	}
 
 	/**
 	 * Tests whether or not plugins are available.
 	 *
-	 * @covers ::get_plugin
 	 * @covers ::is_available
 	 *
 	 * @return void
@@ -78,7 +56,6 @@ final class Plugin_Availability_Test extends TestCase {
 	/**
 	 * Tests whether or not plugins are installed.
 	 *
-	 * @covers ::get_plugin
 	 * @covers ::is_installed
 	 *
 	 * @return void
@@ -93,42 +70,5 @@ final class Plugin_Availability_Test extends TestCase {
 		$installed = self::$class_instance->is_installed( $plugin );
 
 		$this->assertFalse( $installed );
-	}
-
-	/**
-	 * Tests for the plugin version.
-	 *
-	 * @covers ::get_plugin
-	 * @covers ::get_version
-	 *
-	 * @return void
-	 */
-	public function test_plugin_version() {
-		$plugin  = self::$class_instance->get_plugin( 'test-plugin' );
-		$version = self::$class_instance->get_version( $plugin );
-
-		$this->assertEquals( $version, '3.3' );
-
-		$plugin  = self::$class_instance->get_plugin( 'test-plugin-no-version' );
-		$version = self::$class_instance->get_version( $plugin );
-
-		$this->assertEquals( $version, '' );
-	}
-
-	/**
-	 * Tests for the detection of Premium plugins.
-	 *
-	 * @covers ::is_premium
-	 *
-	 * @return void
-	 */
-	public function test_plugin_is_premium() {
-		$is_premium_plugin = self::$class_instance->is_premium( self::$class_instance->get_plugin( 'test-plugin' ) );
-
-		$this->assertFalse( $is_premium_plugin );
-
-		$is_premium_plugin = self::$class_instance->is_premium( self::$class_instance->get_plugin( 'test-premium-plugin' ) );
-
-		$this->assertTrue( $is_premium_plugin );
 	}
 }
