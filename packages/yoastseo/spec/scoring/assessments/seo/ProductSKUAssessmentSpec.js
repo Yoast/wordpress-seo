f1d15fafa5b98f721bc353d73296c0281e0efb6e
aidamarfuaty <aida@yoast.com>
2024-09-03 16:59:01 +0200

    Adjust and add unit tests

--- packages/yoastseo/spec/scoring/assessments/seo/ProductSKUAssessmentSpec.js
+++ packages/yoastseo/spec/scoring/assessments/seo/ProductSKUAssessmentSpec.js
@@ -1,10 +1,10 @@
-import { createAnchorOpeningTag } from "../../../../src/helpers";
+import { __, sprintf } from "@wordpress/i18n";
 import ProductSKUAssessment from "../../../../src/scoring/assessments/seo/ProductSKUAssessment";
 import Paper from "../../../../src/values/Paper";
 
 
 describe( "a test for SKU assessment for WooCommerce", function() {
-	const assessment = new ProductSKUAssessment( { assessVariants: true, addSKULocation: true } );
+	const assessment = new ProductSKUAssessment( { assessVariants: true, shouldShowEditButton: true } );
 
 	it( "returns the score 9 when a product has a global SKU and no variants", function() {
 		const customData = {
@@ -153,13 +153,80 @@ describe( "a test for SKU assessment for WooCommerce", function() {
 			"<a href='https://yoa.st/4lx' target='_blank'>Include it if you can, as it will help search engines to " +
 			"better understand your content.</a>" );
 	} );
+
+	it( "returns `hasJumps` as true when the score is less than 9 and `shouldShowEditButton` is true", function() {
+		const customData = {
+			hasGlobalSKU: false,
+			hasVariants: false,
+			doAllVariantsHaveSKU: false,
+			productType: "simple",
+		};
+
+		const paperWithCustomData = new Paper( "", { customData } );
+		const assessmentResult = assessment.getResult( paperWithCustomData );
+
+		expect( assessmentResult.hasJumps() ).toBe( true );
+	} );
 } );
 
 describe( "a test for SKU assessment for Shopify", () => {
-	const assessment = new ProductSKUAssessment( { urlTitle: createAnchorOpeningTag( "https://yoa.st/shopify79" ),
-		urlCallToAction: createAnchorOpeningTag( "https://yoa.st/shopify80" ),
-		assessVariants: false,
-		addSKULocation: false,
+	/**
+	 * Returns the result texts for the Product SKU assessment.
+	 *
+	 * @param {string} urlTitleAnchorOpeningTag The anchor opening tag to the article about this assessment.
+	 * @param {string} urlActionAnchorOpeningTag The anchor opening tag to the call to action URL to the help article of this assessment.
+	 * @returns {{good: {withoutVariants: string, withVariants: string}, okay: {withoutVariants: string, withVariants: string}}} The feedback strings.
+	 */
+	const getResultTexts = ( { urlTitleAnchorOpeningTag, urlActionAnchorOpeningTag } ) => {
+		return {
+			good: {
+				withoutVariants: sprintf(
+					/* translators: %1$s expands to a link on yoast.com, %2$s expands to the anchor end tag. */
+					__(
+						"%1$sSKU%2$s: Your product has a SKU. Good job!",
+						"yoast-woo-seo"
+					),
+					urlTitleAnchorOpeningTag,
+					"</a>"
+				),
+				withVariants: sprintf(
+					/* translators: %1$s expands to a link on yoast.com, %2$s expands to the anchor end tag. */
+					__(
+						"%1$sSKU%2$s: All your product variants have a SKU. Good job!",
+						"yoast-woo-seo"
+					),
+					urlTitleAnchorOpeningTag,
+					"</a>"
+				),
+			},
+			okay: {
+				withoutVariants: sprintf(
+					/* translators: %1$s and %2$s expand to links on yoast.com, %3$s expands to the anchor end tag. */
+					__(
+						"%1$sSKU%3$s: Your product is missing a SKU. %2$sInclude it if you can, as it will help search engines to better understand your content.%3$s",
+						"yoast-woo-seo"
+					),
+					urlTitleAnchorOpeningTag,
+					urlActionAnchorOpeningTag,
+					"</a>"
+				),
+				withVariants: sprintf(
+					/* translators: %1$s and %2$s expand to links on yoast.com, %3$s expands to the anchor end tag. */
+					__(
+						"%1$sSKU%3$s: Not all your product variants have a SKU. You can add a SKU via the \"Variations\" tab in the Product data box. %2$sInclude it if you can, as it will help search engines to better understand your content.%3$s",
+						"yoast-woo-seo"
+					),
+					urlTitleAnchorOpeningTag,
+					urlActionAnchorOpeningTag,
+					"</a>"
+				),
+			},
+		};
+	};
+	const assessment = new ProductSKUAssessment( {
+		urlTitle: "https://yoa.st/shopify79",
+		urlCallToAction: "https://yoa.st/shopify80",
+		callbacks: { getResultTexts },
 	} );
 
 	it( "returns with score 9 when the product has global SKU and no variants", () => {
