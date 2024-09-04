924d33eaf7a2b30db3eaa14f0ef190fd4894b6f8
Vraja Das <vraja.pro@gmail.com>
2024-09-04 13:33:26 +0300

    remove unused variables

--- admin/class-config.php
+++ admin/class-config.php
@@ -6,7 +6,6 @@
  */
 
 use Yoast\WP\SEO\Actions\Alert_Dismissal_Action;
-use Yoast\WP\SEO\Conditionals\WooCommerce_Conditional;
 use Yoast\WP\SEO\Integrations\Academy_Integration;
 use Yoast\WP\SEO\Integrations\Settings_Integration;
 use Yoast\WP\SEO\Integrations\Support_Integration;
@@ -91,7 +90,6 @@ class WPSEO_Admin_Pages {
 
 		$alert_dismissal_action  = YoastSEO()->classes->get( Alert_Dismissal_Action::class );
 		$dismissed_alerts        = $alert_dismissal_action->all_dismissed();
-		$woocommerce_conditional = new WooCommerce_Conditional();
 
 		$script_data = [
 			'userLanguageCode'               => WPSEO_Language_Utils::get_language( get_user_locale() ),
