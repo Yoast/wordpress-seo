d857cc9b04f0ee604f5ee21910afcf603c2af005
Vraja Das <vraja.pro@gmail.com>
2024-09-12 13:52:52 +0300

    refactor: remove isPrivateBlog to the editors directory

--- src/editors/framework/site/base-site-information.php
+++ src/editors/framework/site/base-site-information.php
@@ -80,6 +80,7 @@ abstract class Base_Site_Information {
 			'isRtl'                 => \is_rtl(),
 			'isPremium'             => $this->product_helper->is_premium(),
 			'siteIconUrl'           => \get_site_icon_url(),
+			'isPrivateBlog'         => ! \get_option( 'blog_public' ),
 		];
 	}
 
@@ -95,6 +96,7 @@ abstract class Base_Site_Information {
 			'linkParams'            => $this->short_link_helper->get_query_params(),
 			'pluginUrl'             => \plugins_url( '', \WPSEO_FILE ),
 			'wistiaEmbedPermission' => $this->wistia_embed_permission_repository->get_value_for_user( \get_current_user_id() ),
+			'isPrivateBlog'         => ! \get_option( 'blog_public' ),
 			'metabox'               => [
 				'site_name'     => $this->meta->for_current_page()->site_name,
 				'contentLocale' => \get_locale(),
