9c33458d5d6286164eb9f92bb1a3218bc33ef2db
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2024-09-25 10:23:33 +0300

    "Keyphrase in slug" assessment won't ever make sense on a static homepage #20118 Fix front detection.

--- admin/metabox/class-metabox.php
+++ admin/metabox/class-metabox.php
@@ -881,7 +881,7 @@ class WPSEO_Metabox extends WPSEO_Meta {
 
 		$page_on_front 		= (int) get_option( 'page_on_front' );
 		$homepage_is_page 	= \get_option( 'show_on_front' ) === 'page';
-		$is_front_page 		= $homepage_is_page && $page_on_front === $post_id;
+		$is_front_page 		= $homepage_is_page && $page_on_front === (int) $post_id;
 
 		$script_data = [
 			'metabox'                    => $this->get_metabox_script_data(),
