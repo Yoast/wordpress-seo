fbc48df5e2ced9cfd956975f2774c45a846c748a
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2024-09-26 11:00:26 +0300

    "Keyphrase in slug" assessment won't ever make sense on a static homepage #20118 Fix linting errors.

--- admin/metabox/class-metabox.php
+++ admin/metabox/class-metabox.php
@@ -879,9 +879,9 @@ class WPSEO_Metabox extends WPSEO_Meta {
 			'log_level'               => WPSEO_Utils::get_analysis_worker_log_level(),
 		];
 
-		$page_on_front 		= (int) get_option( 'page_on_front' );
-		$homepage_is_page 	= \get_option( 'show_on_front' ) === 'page';
-		$is_front_page 		= $homepage_is_page && $page_on_front === (int) $post_id;
+		$page_on_front      = (int) get_option( 'page_on_front' );
+		$homepage_is_page   = \get_option( 'show_on_front' ) === 'page';
+		$is_front_page      = $homepage_is_page && $page_on_front === (int) $post_id;
 
 		$script_data = [
 			'metabox'                    => $this->get_metabox_script_data(),
