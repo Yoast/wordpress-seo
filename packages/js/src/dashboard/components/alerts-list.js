9e4abf649a88f36172ae5e0f63c9f18ed640b46f
Paolo L. Scala <paolo@yoast.com>
2024-09-30 15:41:29 +0200

    Add error notification

--- packages/js/src/dashboard/components/alerts-list.js
+++ packages/js/src/dashboard/components/alerts-list.js
@@ -1,11 +1,13 @@
-import { useContext, useCallback } from "@wordpress/element";
+import { Fragment, useContext, useCallback, useState } from "@wordpress/element";
 import { useDispatch } from "@wordpress/data";
+import { __ } from "@wordpress/i18n";
 import PropTypes from "prop-types";
-import { Button } from "@yoast/ui-library";
+import { Button, Notifications } from "@yoast/ui-library";
 import { EyeOffIcon, EyeIcon } from "@heroicons/react/outline";
 import classNames from "classnames";
+import { TOGGLE_ALERT_VISIBILITY } from "../constants";
 import { AlertsContext } from "../contexts/alerts-context";
-
+import { ASYNC_ACTION_NAMES } from "../../shared-admin/constants";
 
 /**
  * The alert item object.
@@ -18,34 +20,58 @@ import { AlertsContext } from "../contexts/alerts-context";
  * @returns {JSX.Element} The alert item component.
  */
 const AlertItem = ( { id, nonce, dismissed, message  } ) => {
-	const { bulletClass = "" } = useContext( AlertsContext );
+	const { bulletClass = "", type = "" } = useContext( AlertsContext );
 	const { toggleAlertStatus } = useDispatch( "@yoast/dashboard" );
 	const Eye = dismissed ? EyeOffIcon : EyeIcon;
+	const [ toggleError, setToggleError ] = useState( false );
 
-	const toggleAlert = useCallback( () => {
-		toggleAlertStatus( id, nonce, dismissed );
-	}, [ id, nonce, dismissed, toggleAlertStatus ] );
+	const onToggleErrorDismiss = useCallback( () => {
+		setToggleError( false );
+	}, [ setToggleError ] );
 
-	return <li key={ id } className="yst-border-b yst-border-slate-200 last:yst-border-b-0 yst-py-6 first:yst-pt-0 last:yst-pb-0">
-		<div className="yst-flex yst-justify-between yst-gap-5 yst-items-start">
-			<div className={ classNames( "yst-mt-1",  dismissed && "yst-opacity-50" ) }>
-				<svg width="11" height="11" className={ bulletClass }>
-					<circle cx="5.5" cy="5.5" r="5.5" />
-				</svg>
+	const toggleAlert = useCallback( async() => {
+		const status = await toggleAlertStatus( id, nonce, dismissed );
+		setToggleError( status.type === `${ TOGGLE_ALERT_VISIBILITY }/${ ASYNC_ACTION_NAMES.error }` );
+	}, [ id, nonce, dismissed, toggleAlertStatus, setToggleError ] );
+
+	return <Fragment>
+		<li key={ id } className="yst-border-b yst-border-slate-200 last:yst-border-b-0 yst-py-6 first:yst-pt-0 last:yst-pb-0">
+			<div className="yst-flex yst-justify-between yst-gap-5 yst-items-start">
+				<div className={ classNames( "yst-mt-1",  dismissed && "yst-opacity-50" ) }>
+					<svg width="11" height="11" className={ bulletClass }>
+						<circle cx="5.5" cy="5.5" r="5.5" />
+					</svg>
+				</div>
+				<div
+					className={ classNames(
+						"yst-text-sm yst-text-slate-600 yst-grow",
+						dismissed && "yst-opacity-50" ) }
+					dangerouslySetInnerHTML={ { __html: message } }
+				/>
+
+
+				<Button variant="secondary" size="small" className="yst-self-center yst-h-8" onClick={ toggleAlert }>
+					<Eye className="yst-w-4 yst-h-4 yst-text-neutral-700" />
+				</Button>
 			</div>
-			<div
-				className={ classNames(
-					"yst-text-sm yst-text-slate-600 yst-grow",
-					dismissed && "yst-opacity-50" ) }
-				dangerouslySetInnerHTML={ { __html: message } }
-			/>
-
-
-			<Button variant="secondary" size="small" className="yst-self-center yst-h-8" onClick={ toggleAlert }>
-				<Eye className="yst-w-4 yst-h-4 yst-text-neutral-700" />
-			</Button>
-		</div>
-	</li>;
+		</li>
+		<Notifications
+			className="yst-mx-[calc(50%-50vw)] yst-transition-all lg:yst-left-44"
+			position="bottom-left"
+		>
+			{	toggleError && <Notifications.Notification
+				id={ `toggle-alert-error-${ id }` }
+				title={ __( "Something went wrong", "wordpress-seo" ) }
+				variant="error"
+				dismissScreenReaderLabel={ __( "Dismiss", "wordpress-seo" ) }
+				size="large"
+				autoDismiss={ 5000 }
+				onDismiss={ onToggleErrorDismiss }
+			>
+				{ __( `This ${type} can't be hidden at this time. Please try again later.`, "wordpress-seo" ) }
+			</Notifications.Notification> }
+		</Notifications>
+	</Fragment>;
 };
 
 AlertItem.propTypes = {
