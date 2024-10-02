9e9c3aef20e0d180f62c7378b50bb2bcbdae719f
Leonidas Milosis <leonidas.milossis@gmail.com>
2024-10-02 11:52:46 +0300

    Fix tests

--- tests/WP/Frontend/Front_End_Integration_Test.php
+++ tests/WP/Frontend/Front_End_Integration_Test.php
@@ -5,7 +5,6 @@ namespace Yoast\WP\SEO\Tests\WP\Frontend;
 use Mockery;
 use WPSEO_Replace_Vars;
 use Yoast\WP\SEO\Helpers\Options_Helper;
-use Yoast\WP\SEO\Helpers\Request_Helper;
 use Yoast\WP\SEO\Memoizers\Meta_Tags_Context_Memoizer;
 use Yoast\WP\SEO\Presentations\Indexable_Presentation;
 use Yoast\WP\SEO\Surfaces\Helpers_Surface;
@@ -42,13 +41,6 @@ final class Front_End_Integration_Test extends TestCase {
 	protected $options;
 
 	/**
-	 * Represents the request helper.
-	 *
-	 * @var Request_Helper
-	 */
-	protected $request;
-
-	/**
 	 * The helpers surface.
 	 *
 	 * @var Helpers_Surface
@@ -79,7 +71,6 @@ final class Front_End_Integration_Test extends TestCase {
 		$this->context_memoizer = Mockery::mock( Meta_Tags_Context_Memoizer::class );
 		$this->container        = Mockery::mock( ContainerInterface::class );
 		$this->options          = Mockery::mock( Options_Helper::class );
-		$this->request          = Mockery::mock( Request_Helper::class );
 		$this->helpers          = Mockery::mock( Helpers_Surface::class );
 		$this->replace_vars     = Mockery::mock( WPSEO_Replace_Vars::class );
 
@@ -87,7 +78,6 @@ final class Front_End_Integration_Test extends TestCase {
 			$this->context_memoizer,
 			$this->container,
 			$this->options,
-			$this->request,
 			$this->helpers,
 			$this->replace_vars
 		);
