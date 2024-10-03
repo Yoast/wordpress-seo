02900ce075e03f5d1cf4a6af2abc97fa16c66fda
Paolo L. Scala <paolo@yoast.com>
2024-10-03 12:15:01 +0200

    Revert "Helper function to get an error message on the basis of the alert type"
    This reverts commit 944e17428ebc08722a7b5585c2263e851baedd8b.

--- packages/js/src/dashboard/helpers/get-toast-error-message.js
+++
@@ -1,8 +0,0 @@
-import { __ } from "@wordpress/i18n";
-
-export const getToastErrorMessage = ( alertType ) => {
-	if ( alertType === "error" ) {
-		return __( "This problem can't be hidden at this time. Please try again later.", "wordpress-seo" );
-	}
-	return __( "This notification can't be hidden at this time. Please try again later.", "wordpress-seo" );
-};
