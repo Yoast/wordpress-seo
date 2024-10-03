3827939a8a7c45014fe0ec0e51fab1be3d66c24d
Paolo L. Scala <paolo@yoast.com>
2024-10-03 12:41:24 +0200

    Remove error management

--- packages/js/src/dashboard/components/alerts-list.js
+++ packages/js/src/dashboard/components/alerts-list.js
@@ -1,13 +1,10 @@
-import { Fragment, useContext, useCallback, useState } from "@wordpress/element";
+import { Fragment, useContext, useCallback } from "@wordpress/element";
 import { useDispatch } from "@wordpress/data";
-import { __ } from "@wordpress/i18n";
 import PropTypes from "prop-types";
-import { Button, Notifications } from "@yoast/ui-library";
+import { Button } from "@yoast/ui-library";
 import { EyeOffIcon, EyeIcon } from "@heroicons/react/outline";
 import classNames from "classnames";
-import { TOGGLE_ALERT_VISIBILITY } from "../constants";
 import { AlertsContext } from "../contexts/alerts-context";
-import { ASYNC_ACTION_NAMES } from "../../shared-admin/constants";
 
 /**
  * The alert item object.
@@ -23,16 +20,10 @@ const AlertItem = ( { id, nonce, dismissed, message  } ) => {
 	const { bulletClass = "", type = "" } = useContext( AlertsContext );
 	const { toggleAlertStatus } = useDispatch( "@yoast/dashboard" );
 	const Eye = dismissed ? EyeOffIcon : EyeIcon;
-	const [ toggleError, setToggleError ] = useState( false );
-
-	const onToggleErrorDismiss = useCallback( () => {
-		setToggleError( false );
-	}, [ setToggleError ] );
 
 	const toggleAlert = useCallback( async() => {
-		const status = await toggleAlertStatus( id, nonce, dismissed );
-		setToggleError( status.type === `${ TOGGLE_ALERT_VISIBILITY }/${ ASYNC_ACTION_NAMES.error }` );
-	}, [ id, nonce, dismissed, toggleAlertStatus, setToggleError ] );
+		toggleAlertStatus( id, nonce, dismissed );
+	}, [ id, nonce, dismissed, toggleAlertStatus ] );
 
 	return <Fragment>
 		<li key={ id } className="yst-border-b yst-border-slate-200 last:yst-border-b-0 yst-py-6 first:yst-pt-0 last:yst-pb-0">
@@ -49,28 +40,11 @@ const AlertItem = ( { id, nonce, dismissed, message  } ) => {
 					dangerouslySetInnerHTML={ { __html: message } }
 				/>
 
-
 				<Button variant="secondary" size="small" className="yst-self-center yst-h-8" onClick={ toggleAlert }>
 					<Eye className="yst-w-4 yst-h-4 yst-text-neutral-700" />
 				</Button>
 			</div>
 		</li>
-		<Notifications
-			className="yst-mx-[calc(50%-50vw)] yst-transition-all lg:yst-left-44"
-			position="bottom-left"
-		>
-			{	toggleError && <Notifications.Notification
-				id={ `toggle-alert-error-${ id }` }
-				title={ __( "Something went wrong", "wordpress-seo" ) }
-				variant="error"
-				dismissScreenReaderLabel={ __( "Dismiss", "wordpress-seo" ) }
-				size="large"
-				autoDismiss={ 4000 }
-				onDismiss={ onToggleErrorDismiss }
-			>
-				{ __( `This ${type} can't be hidden at this time. Please try again later.`, "wordpress-seo" ) }
-			</Notifications.Notification> }
-		</Notifications>
 	</Fragment>;
 };
 
