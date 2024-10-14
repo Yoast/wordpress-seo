5dd99aebd75cdaf0b380d7feb39a4ff1cf198685
Vraja Das <vraja.pro@gmail.com>
2024-10-14 15:12:23 +0300

    remove notices on FTC tab

--- packages/js/src/dashboard/app.js
+++ packages/js/src/dashboard/app.js
@@ -102,7 +102,7 @@ const App = () => {
 						{ shouldShowWebinarPromotionNotificationInDashboard( STORE_NAME ) &&
 							<WebinarPromoNotification store={ STORE_NAME } url={ webinarIntroSettingsUrl } image={ null } />
 						}
-						{ notices.length > 0 && <div className="yst-space-y-3 yoast-new-dashboard-notices"> {
+						{ pathname !== "/first-time-configuration" && notices.length > 0 && <div className="yst-space-y-3 yoast-new-dashboard-notices"> {
 							notices.map( ( notice, index ) => (
 								<Notice
 									key={ index }
