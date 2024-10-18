1aae3899440d75078e2bf1457856b86d04f78993
Martijn van der Klis <martijnvanderklis@yoast.com>
2024-10-18 16:28:59 +0200

    Removes other uses of matchStringWithRegex

--- packages/yoastseo/src/languageProcessing/helpers/index.js
+++ packages/yoastseo/src/languageProcessing/helpers/index.js
@@ -1,11 +1,9 @@
-import matchStringWithRegex from "./regex/matchStringWithRegex";
 import { normalize } from "./sanitize/quotes";
 import { createShortcodeTagsRegex, filterShortcodesFromHTML } from "./sanitize/filterShortcodesFromTree";
 import processExactMatchRequest from "./match/processExactMatchRequest";
 import removeHtmlBlocks from "./html/htmlParser";
 
 export {
-	matchStringWithRegex,
 	normalize,
 	removeHtmlBlocks,
 	filterShortcodesFromHTML,
