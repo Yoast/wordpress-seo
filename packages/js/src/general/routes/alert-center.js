ff8b11967c3a423e6de7cf64dced6cbef3b278e3
Igor <35524806+igorschoester@users.noreply.github.com>
2024-10-21 14:45:44 +0200

    refactor: remove unneeded wrappers

--- packages/js/src/general/routes/alert-center.js
+++ packages/js/src/general/routes/alert-center.js
@@ -4,8 +4,8 @@ import { Paper, Title } from "@yoast/ui-library";
 import { PremiumUpsellList } from "../../shared-admin/components/premium-upsell-list";
 import SidebarRecommendations from "../../shared-admin/components/sidebar-recommendations";
 import { Notifications, Problems } from "../components";
+import { STORE_NAME } from "../constants";
 import { useSelectGeneralPage } from "../hooks";
-import { STORE_NAME } from ".././constants";
 
 /**
  * @returns {JSX.Element} The general page content placeholder.
@@ -19,29 +19,24 @@ export const AlertCenter = () => {
 	const { isPromotionActive } = useSelect( STORE_NAME );
 
 	return <div className="yst-flex yst-gap-8 xl:yst-flex-row yst-flex-col">
-		 { /* Alert center */ }
 		<div className="yst-flex yst-flex-wrap yst-flex-grow xl:yst-flex-row yst-flex-col">
 			<Paper className="yst-grow">
-				<header className="yst-p-8">
-					<div className="yst-max-w-screen-sm">
-						<Title>{ __( "Alert center", "wordpress-seo" ) }</Title>
-						<p className="yst-text-tiny yst-mt-3">
-							{ __( "Monitor and manage potential SEO problems affecting your site and stay informed with important notifications and updates.", "wordpress-seo" ) }
-						</p>
-					</div>
+				<header className="yst-p-8 yst-max-w-screen-sm">
+					<Title>{ __( "Alert center", "wordpress-seo" ) }</Title>
+					<p className="yst-text-tiny yst-mt-3">
+						{ __( "Monitor and manage potential SEO problems affecting your site and stay informed with important notifications and updates.", "wordpress-seo" ) }
+					</p>
 				</header>
 			</Paper>
-			<div className="yst-basis-full">
-				<div className="yst-grid lg:yst-grid-cols-2 yst-gap-8 yst-my-8 yst-grow yst-items-start">
-					<Problems />
-					<Notifications />
-				</div>
-				{ ! isPremium && <PremiumUpsellList
-					premiumLink={ premiumLinkList }
-					premiumUpsellConfig={ premiumUpsellConfig }
-					isPromotionActive={ isPromotionActive }
-				/> }
+			<div className="yst-grid lg:yst-grid-cols-2 yst-gap-8 yst-my-8 yst-grow yst-items-start">
+				<Problems />
+				<Notifications />
 			</div>
+			{ ! isPremium && <PremiumUpsellList
+				premiumLink={ premiumLinkList }
+				premiumUpsellConfig={ premiumUpsellConfig }
+				isPromotionActive={ isPromotionActive }
+			/> }
 		</div>
 		{ ! isPremium &&
 			<div className="yst-min-w-[16rem] xl:yst-max-w-[16rem]">
