4c70f8b70ba0faed80ee3a26b4c8e538e77183ae
Igor <35524806+igorschoester@users.noreply.github.com>
2024-10-21 16:55:39 +0200

    fix: adjust spacing to match design
    And while Kai was looking: spacing should be 24px instead of 32px

--- packages/js/src/general/routes/alert-center.js
+++ packages/js/src/general/routes/alert-center.js
@@ -18,7 +18,7 @@ export const AlertCenter = () => {
 	const academyLink = useSelectGeneralPage( "selectLink", [], "https://yoa.st/3t6" );
 	const { isPromotionActive } = useSelect( STORE_NAME );
 
-	return <div className="yst-flex yst-gap-8 xl:yst-flex-row yst-flex-col">
+	return <div className="yst-flex yst-gap-6 xl:yst-flex-row yst-flex-col">
 		<div className="yst-@container yst-flex yst-flex-wrap yst-flex-grow yst-flex-col">
 			<Paper className="yst-grow">
 				<header className="yst-p-8 yst-max-w-screen-sm">
@@ -28,7 +28,7 @@ export const AlertCenter = () => {
 					</p>
 				</header>
 			</Paper>
-			<div className="yst-grid yst-grid-cols-1 @3xl:yst-grid-cols-2 yst-gap-8 yst-my-8 yst-grow yst-items-start">
+			<div className="yst-grid yst-grid-cols-1 @3xl:yst-grid-cols-2 yst-gap-6 yst-my-6 yst-grow yst-items-start">
 				<Problems />
 				<Notifications />
 			</div>
