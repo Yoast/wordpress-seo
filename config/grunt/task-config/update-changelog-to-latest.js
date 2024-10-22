2337db6fed796c512b947a3f9badd82c137e6162
Enrico Battocchi <enrico.battocchi@gmail.com>
2024-10-22 10:50:21 +0200

    Revert "Move changelog to new file"

--- config/grunt/task-config/update-changelog-to-latest.js
+++ config/grunt/task-config/update-changelog-to-latest.js
@@ -4,17 +4,23 @@
 module.exports = {
 	"wordpress-seo": {
 		options: {
-			readmeFile: "./changelog.md",
-			releaseInChangelog: /[#] \d+\.\d+(\.\d+)?\n\n/g,
-			matchChangelogHeader: /Changelog\n=========\n/ig,
-			newHeadertemplate: "Changelog\n=========\n\n" + "## " + "VERSIONNUMBER" + "\n\nRelease date: " + "DATESTRING"  + "\n",
-			matchCorrectHeader: "## " + "VERSIONNUMBER" + "(.|\\n)*?\\n(?=(\\n#### \\w\+?\\n|## \\d+[\.\\d]+\\n|### Earlier versions|$))",
-			matchCorrectLines: "## " + "VERSIONNUMBER" + "(.|\\n)*?(?=(\\n## \\d+[\.\\d]+|### Earlier versions|$))",
-			matchCleanedChangelog: "## " + "VERSIONNUMBER" + "(.|\\n)*### Earlier versions|$",
-			replaceCleanedChangelog: "### Earlier versions",
+			// Header:
+			// ## 15.7
+			//
+			// Release Date: January 26th, 2021
+			//
+			// Enhancements:
+			readmeFile: "./readme.txt",
+			releaseInChangelog: /[=] \d+\.\d+(\.\d+)? =/g,
+			matchChangelogHeader: /[=]= Changelog ==\n\n/ig,
+			newHeadertemplate: "== Changelog ==\n\n" + "= " + "VERSIONNUMBER" + " =\n\nRelease date: " + "DATESTRING"  + "\n",
+			matchCorrectHeader: "= " + "VERSIONNUMBER" + " =(.|\\n)*?\\n(?=(\\n#### \\w\+?\\n|= \\d+[\.\\d]+|= Earlier versions =))",
+			matchCorrectLines: "= " + "VERSIONNUMBER" + " =(.|\\n)*?(?=(= \\d+[\.\\d]+ =|= Earlier versions =))",
+			matchCleanedChangelog: "= " + "VERSIONNUMBER" + "(.|\\n)*= Earlier versions =",
+			replaceCleanedChangelog: "= Earlier versions =",
 			pluginSlug: "wordpress-seo",
 			defaultChangelogEntries: "",
-			useANewLineAfterHeader: false,
+			useANewLineAfterHeader: true,
 			commitChangelog: true,
 			changelogToInject: ".tmp/n8nchangelog.txt",
 		},
