6f8a380daa149b7e50d2cde628318d41d1b93ba4
Leonidas Milosis <leonidas.milossis@gmail.com>
2024-10-23 15:09:29 +0300

    Stop removing the upgrade notice since it's never added

--- admin/class-admin.php
+++ admin/class-admin.php
@@ -73,8 +73,6 @@ class WPSEO_Admin {
 			add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_assets' ] );
 		}
 
-		$this->set_upsell_notice();
-
 		$this->initialize_cornerstone_content();
 
 		if ( WPSEO_Utils::is_plugin_network_active() ) {
@@ -363,17 +361,6 @@ class WPSEO_Admin {
 			],
 			YoastSEO()->helpers->wincher->get_admin_global_links()
 		);
-	}
-
-	/**
-	 * Sets the upsell notice.
-	 *
-	 * @return void
-	 */
-	protected function set_upsell_notice() {
-		$upsell = new WPSEO_Product_Upsell_Notice();
-		$upsell->dismiss_notice_listener();
-		$upsell->initialize();
 	}
 
 	/**
