54b7c9fbc7787b69171249d01e817d0fa957b791
Martijn van der Klis <martijnvanderklis@yoast.com>
2024-10-24 15:59:50 +0200

    Extracts constants and functions for readability

--- packages/yoastseo/src/languageProcessing/researches/getLongCenterAlignedTexts.js
+++ packages/yoastseo/src/languageProcessing/researches/getLongCenterAlignedTexts.js
@@ -1,14 +1,25 @@
 /**
- * Filters out all nodes that are center-aligned and longer than 50 characters.
+ * The maximum allowed length of a center-aligned paragraph or heading in characters.
+ * @type {number}
+ */
+const MAX_CENTER_ALIGNED_LENGTH = 50;
+
+/**
+ * A regular expression to match heading tags.
+ * @type {RegExp}
+ */
+const HEADING_TAGS_REGEX = /^h[1-6]$/;
+
+/**
+ * Filters out all nodes that are center-aligned and longer than 50 characters (as defined in `MAX_CENTER_ALIGNED_LENGTH`).
  * @param {Node[]} nodes An array of nodes.
  * @returns {Node[]} An array containing all center-aligned nodes that are longer than 50 characters.
  */
 function getLongCenterAlignedElements( nodes ) {
-	return nodes.filter( node =>
-		node.attributes.class instanceof Set &&
-		node.attributes.class.has( "has-text-align-center" ) &&
-		node.innerText().length > 50
-	);
+	return nodes.filter( node => {
+		const isCenterAligned = node.attributes.class instanceof Set && node.attributes.class.has( "has-text-align-center" );
+		return isCenterAligned && node.innerText().length > MAX_CENTER_ALIGNED_LENGTH;
+	} );
 }
 
 /**
@@ -20,7 +31,7 @@ export default function( paper ) {
 	const tree = paper.getTree();
 
 	const paragraphs = tree.findAll( node => node.name === "p" );
-	const headings = tree.findAll( node => /^h[1-6]$/.test( node.name ) );
+	const headings = tree.findAll( node => HEADING_TAGS_REGEX.test( node.name ) );
 
 	return getLongCenterAlignedElements( paragraphs.concat( headings ) );
 }
