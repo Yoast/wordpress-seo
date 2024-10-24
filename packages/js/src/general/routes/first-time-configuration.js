dc57cb3f7f5528f7e98954c0921f7a7b9138fd27
Igor <35524806+igorschoester@users.noreply.github.com>
2024-10-24 16:45:01 +0200

    FTC: remove horizontal rule
    We already have a border to separate the title/description from the content

--- packages/js/src/general/routes/first-time-configuration.js
+++ packages/js/src/general/routes/first-time-configuration.js
@@ -21,7 +21,6 @@ const FirstTimeConfiguration = () => {
 				title={ __( "First-time configuration", "wordpress-seo" ) }
 				description={ __( "Tell us about your site, so we can get it ranked! Let's get your site in tip-top shape for the search engines. Follow these 5 steps to make Google understand what your site is about.", "wordpress-seo" ) }
 			>
-				<hr id="configuration-hr-top" />
 				<div id="yoast-configuration" className="yst-p-8 yst-max-w-[715px]">
 					<FirstTimeConfigurationSteps />
 				</div>
