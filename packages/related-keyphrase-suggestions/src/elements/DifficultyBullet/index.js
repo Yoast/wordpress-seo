f7dbffdc63b54d85b995b326cdcb4adf03cbcfca
Vraja Das <vraja.pro@gmail.com>
2024-10-29 14:19:19 +0200

    refactor difficulty variant

--- packages/related-keyphrase-suggestions/src/elements/DifficultyBullet/index.js
+++ packages/related-keyphrase-suggestions/src/elements/DifficultyBullet/index.js
@@ -4,61 +4,62 @@ import classNames from "classnames";
 import { __ } from "@wordpress/i18n";
 import { Tooltip, useToggleState } from "@yoast/ui-library";
 
-const veryEasy = {
-	name: "very-easy",
-	tooltip: {
-		title: __( "Very easy", "wordpress-seo" ),
-		description: __( "Your chance to start ranking new pages.", "wordpress-seo" ),
-	},
-};
-
-const easy = {
-	name: "easy",
-	tooltip: {
-		title: __( "Easy", "wordpress-seo" ),
-		description: __( "You will need quality content focused on the keyword’s intent.", "wordpress-seo" ),
-	},
-};
-
-const possible = {
-	name: "possible",
-	tooltip: {
-		title: __( "Possible", "wordpress-seo" ),
-		description: __( "You will need well-structured and unique content.", "wordpress-seo" ),
-	},
-};
-
-const difficult = {
-	name: "difficult",
-	tooltip: {
-		title: __( "Difficult", "wordpress-seo" ),
-		description: __( "You will need lots of ref. domains and optimized content.", "wordpress-seo" ),
-	},
-};
-
-const hard = {
-	name: "hard",
-	tooltip: {
-		title: __( "Hard", "wordpress-seo" ),
-		description: __( "You will need lots of high-quality ref. domains and optimized content.", "wordpress-seo" ),
-	},
-};
-
-const veryHard = {
-	name: "very-hard",
-	tooltip: {
-		title: __( "Very hard", "wordpress-seo" ),
-		description: __( "It will take a lot of on-page SEO, link building, and content promotion efforts.", "wordpress-seo" ),
-	},
-};
 
 const variants = [
-	{ min: 0, max: 14, variant: veryEasy },
-	{ min: 15, max: 29, variant: easy },
-	{ min: 30, max: 49, variant: possible },
-	{ min: 50, max: 69, variant: difficult },
-	{ min: 70, max: 84, variant: hard },
-	{ min: 85, max: 100, variant: veryHard },
+	{ 	
+		min: 0,
+		max: 14,
+		name: "very-easy",
+		tooltip: {
+			title: __( "Very easy", "wordpress-seo" ),
+			description: __( "Your chance to start ranking new pages.", "wordpress-seo" ),
+		},
+	},
+	{ 
+		min: 15, 
+		max: 29,
+		name: "easy",
+		tooltip: {
+			title: __( "Easy", "wordpress-seo" ),
+			description: __( "You will need quality content focused on the keyword’s intent.", "wordpress-seo" ),
+		},
+	},
+	{ 
+		min: 30, 
+		max: 49, 
+		name: "possible",
+		tooltip: {
+			title: __( "Possible", "wordpress-seo" ),
+			description: __( "You will need well-structured and unique content.", "wordpress-seo" ),
+		},
+	},
+	{ 
+		min: 50, 
+		max: 69, 
+		name: "difficult",
+		tooltip: {
+			title: __( "Difficult", "wordpress-seo" ),
+			description: __( "You will need lots of ref. domains and optimized content.", "wordpress-seo" ),
+		},
+	},
+	{ 
+		min: 70, 
+		max: 84, 
+		name: "hard",
+		tooltip: {
+			title: __( "Hard", "wordpress-seo" ),
+			description: __( "You will need lots of high-quality ref. domains and optimized content.", "wordpress-seo" ),
+		},
+	},
+	{ 
+		min: 85, 
+		max: 100,
+		name: "very-hard",
+		tooltip: {
+			title: __( "Very hard", "wordpress-seo" ),
+			description: __( "It will take a lot of on-page SEO, link building, and content promotion efforts.", "wordpress-seo" ),
+		},
+	},
 ];
 
 /**
@@ -68,12 +69,11 @@ const variants = [
  * @returns {object} The variant of the difficulty.
  */
 const getVariant = ( value ) => {
-	for ( const { min, max, variant } of variants ) {
-		if ( min <= value && value <= max ) {
+	for ( const variant of variants ) {
+		if ( variant.min <= value && value <= variant.max ) {
 			return variant;
 		}
 	}
-	throw new Error( "Value out of range" );
 };
 
 
@@ -87,6 +87,10 @@ export const DifficultyBullet = ( { value } ) => {
 	const [ isVisible, , , handleMouseEnter, handleMouseLeave ] = useToggleState( false );
 
 	const variant = getVariant( value );
+
+	if ( ! variant ) {
+		return null;
+	}
 
 	const variantClass = `yst-difficulty--${ variant.name }`;
 
