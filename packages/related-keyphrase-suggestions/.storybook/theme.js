045150d87b2435eec57090f74679e5fde9cbf5a3
Vraja Das <vraja.pro@gmail.com>
2024-10-29 11:41:28 +0200

    remove theme comments

--- packages/related-keyphrase-suggestions/.storybook/theme.js
+++ packages/related-keyphrase-suggestions/.storybook/theme.js
@@ -15,25 +15,5 @@ export default create( {
 	// UI
 	appBg: "#f4f1f4",
 	appContentBg: "#ffffff",
-	// appBorderColor: "grey",
-	// appBorderRadius: 4,
-
-	// Typography
-	// fontBase: "-apple-system,BlinkMacSystemFont,\"Segoe UI\",Roboto,Oxygen-Sans,Ubuntu,Cantarell,\"Helvetica Neue\",sans-serif",
-	// fontCode: "monospace",
-
-	// Text colors
-	// textColor: "black",
-	// textInverseColor: "rgba(255,255,255,0.9)",
-
-	// Toolbar default and active colors
-	// barTextColor: "silver",
 	barSelectedColor: theme.extend.colors.primary[ 500 ],
-	// barBg: "hotpink",
-
-	// Form colors
-	// inputBg: "white",
-	// inputBorder: "silver",
-	// inputTextColor: "black",
-	// inputBorderRadius: 4,
 } );
