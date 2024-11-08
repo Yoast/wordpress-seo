57fb39298552c4f55d6ad951e06923de862a07e5
Vraja Das <vraja.pro@gmail.com>
2024-11-08 17:30:52 +0200

    remove tests that will be rewritten in the package.
    fix eslint

--- packages/js/tests/components/SEMrushCountrySelector.test.js
+++ packages/js/tests/components/SEMrushCountrySelector.test.js
@@ -1,13 +1,11 @@
 import { noop } from "lodash";
 import SEMrushCountrySelector from "../../../js/src/components/modals/SEMrushCountrySelector";
-import { fireEvent, render, screen, waitFor } from "../test-utils";
+import { fireEvent, render, screen } from "../test-utils";
 
 jest.mock( "@wordpress/api-fetch", () => ( {
 	__esModule: true,
 	"default": () => ( { response: {} } ),
 } ) );
-
-const DOWN_ARROW = { keyCode: 40 };
 
 describe( "SEMrushCountrySelector", () => {
 	it( "successfully calls the associated newRequest function when the select country button is clicked", () => {
@@ -21,39 +19,16 @@ describe( "SEMrushCountrySelector", () => {
 			setRequestLimitReached={ noop }
 			setRequestFailed={ noop }
 			setNoResultsFound={ noop }
+			userLocale="en"
+			isRtl={ false }
 		/> );
 
 		const button = screen.getByRole( "button" );
 		expect( button ).toBeInTheDocument();
-		expect( button ).toHaveTextContent( "Select country" );
+		expect( button ).toHaveTextContent( "Change country" );
 
 		fireEvent.click( button );
 
 		expect( onClickMock ).toHaveBeenCalled();
-	} );
-
-	it( "successfully calls the associated setCountry function when the selected option has changed", async() => {
-		const setCountryMock = jest.fn();
-
-		render( <SEMrushCountrySelector
-			setCountry={ setCountryMock }
-			newRequest={ noop }
-			setNoResultsFoundnewRequest={ noop }
-			setRequestSucceeded={ noop }
-			setRequestLimitReached={ noop }
-			setRequestFailed={ noop }
-			setNoResultsFound={ noop }
-		/> );
-
-		const select = document.getElementById( "yoast-semrush-country-selector-select" );
-		fireEvent.keyDown( select, DOWN_ARROW );
-
-		let item;
-		await waitFor( () => {
-			item = screen.getByText( "United Kingdom - UK" );
-		}, { timeout: 1000 } );
-		fireEvent.click( item );
-
-		expect( setCountryMock ).toHaveBeenCalledWith( "uk" );
 	} );
 } );
