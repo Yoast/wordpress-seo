8eb046dd46d929e36db36f82d99241d87278d236
Igor <35524806+igorschoester@users.noreply.github.com>
2024-11-25 11:58:47 +0100

    UX: fix score labels
    * using UI lib labels for styling

--- packages/js/src/dashboard/scores/components/score-list.js
+++ packages/js/src/dashboard/scores/components/score-list.js
@@ -1,4 +1,4 @@
-import { Badge, Button, SkeletonLoader } from "@yoast/ui-library";
+import { Badge, Button, Label, SkeletonLoader } from "@yoast/ui-library";
 import classNames from "classnames";
 import { SCORE_META } from "../score-meta";
 
@@ -12,6 +12,7 @@ import { SCORE_META } from "../score-meta";
 const CLASSNAMES = {
 	listItem: "yst-flex yst-items-center yst-py-3 first:yst-pt-0 last:yst-pb-0 yst-border-b last:yst-border-b-0",
 	score: "yst-shrink-0 yst-w-3 yst-aspect-square yst-rounded-full",
+	label: "yst-ml-3 yst-mr-2",
 };
 
 /**
@@ -26,7 +27,7 @@ export const ScoreListSkeletonLoader = ( { className } ) => (
 				className={ CLASSNAMES.listItem }
 			>
 				<SkeletonLoader className={ CLASSNAMES.score } />
-				<SkeletonLoader className="yst-ml-3 yst-mr-2">{ label }</SkeletonLoader>
+				<SkeletonLoader className={ CLASSNAMES.label }>{ label }</SkeletonLoader>
 				<SkeletonLoader className="yst-w-7 yst-mr-3">1</SkeletonLoader>
 				<SkeletonLoader className="yst-ml-auto yst-button yst-button--small">View</SkeletonLoader>
 			</li>
@@ -47,7 +48,7 @@ export const ScoreList = ( { className, scores } ) => (
 				className={ CLASSNAMES.listItem }
 			>
 				<span className={ classNames( CLASSNAMES.score, SCORE_META[ score.name ].color ) } />
-				<span className="yst-ml-3 yst-mr-2 yst-leading-4 yst-py-1.5">{ SCORE_META[ score.name ].label }</span>
+				<Label as="span" className={ classNames( CLASSNAMES.label, "yst-leading-4 yst-py-1.5" ) }>{ SCORE_META[ score.name ].label }</Label>
 				<Badge variant="plain" className={ classNames( score.links.view && "yst-mr-3" ) }>{ score.amount }</Badge>
 				{ score.links.view && (
 					<Button as="a" variant="secondary" size="small" href={ score.links.view } className="yst-ml-auto">View</Button>
