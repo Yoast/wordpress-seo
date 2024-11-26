6ad6444829ce3cc126ac58752f843c8b08b62d44
Leonidas Milosis <leonidas.milossis@gmail.com>
2024-11-26 12:45:19 +0200

    Refactor by creating a separate repository for current scores

--- src/dashboard/application/score-results/abstract-score-results-repository.php
+++ src/dashboard/application/score-results/abstract-score-results-repository.php
@@ -5,11 +5,8 @@ namespace Yoast\WP\SEO\Dashboard\Application\Score_Results;
 
 use Yoast\WP\SEO\Dashboard\Domain\Content_Types\Content_Type;
 use Yoast\WP\SEO\Dashboard\Domain\Score_Groups\Score_Groups_Interface;
-use Yoast\WP\SEO\Dashboard\Domain\Score_Results\Current_Score;
-use Yoast\WP\SEO\Dashboard\Domain\Score_Results\Current_Scores_List;
 use Yoast\WP\SEO\Dashboard\Domain\Score_Results\Score_Result;
 use Yoast\WP\SEO\Dashboard\Domain\Taxonomies\Taxonomy;
-use Yoast\WP\SEO\Dashboard\Infrastructure\Score_Groups\Score_Group_Link_Collector;
 use Yoast\WP\SEO\Dashboard\Infrastructure\Score_Results\Score_Results_Collector_Interface;
 
 /**
@@ -25,13 +22,6 @@ abstract class Abstract_Score_Results_Repository {
 	protected $score_results_collector;
 
 	/**
-	 * The score group link collector.
-	 *
-	 * @var Score_Group_Link_Collector
-	 */
-	protected $score_group_link_collector;
-
-	/**
 	 * All score groups.
 	 *
 	 * @var Score_Groups_Interface[]
@@ -39,18 +29,18 @@ abstract class Abstract_Score_Results_Repository {
 	protected $score_groups;
 
 	/**
-	 * Sets the score link collector.
+	 * Sets the repositories.
 	 *
 	 * @required
 	 *
-	 * @param Score_Group_Link_Collector $score_group_link_collector The score group link collector.
+	 * @param Current_Scores_Repository $current_scores_repository The current scores repository.
 	 *
 	 * @return void
 	 */
-	public function set_score_group_link_collector(
-		Score_Group_Link_Collector $score_group_link_collector
+	public function set_repositories(
+		Current_Scores_Repository $current_scores_repository
 	) {
-		$this->score_group_link_collector = $score_group_link_collector;
+		$this->current_scores_repository = $current_scores_repository;
 	}
 
 	/**
@@ -63,21 +53,11 @@ abstract class Abstract_Score_Results_Repository {
 	 * @return array<array<string, string|int|array<string, string>>> The scores.
 	 */
 	public function get_score_results( Content_Type $content_type, ?Taxonomy $taxonomy, ?int $term_id ): array {
-		$current_scores_list = new Current_Scores_List();
-		$score_results       = $this->score_results_collector->get_score_results( $this->score_groups, $content_type, $term_id );
+		$score_results = $this->score_results_collector->get_score_results( $this->score_groups, $content_type, $term_id );
 
-		foreach ( $this->score_groups as $score_group ) {
-			$score_name          = $score_group->get_name();
-			$current_score_links = [
-				'view' => $this->score_group_link_collector->get_view_link( $score_group, $content_type, $taxonomy, $term_id ),
-			];
+		$current_scores_list = $this->current_scores_repository->get_current_scores( $this->score_groups, $score_results, $content_type, $taxonomy, $term_id );
+		$score_result_object = new Score_Result( $current_scores_list, $score_results['query_time'], $score_results['cache_used'] );
 
-			$current_score = new Current_Score( $score_name, (int) $score_results['scores']->$score_name, $current_score_links );
-			$current_scores_list->add( $current_score, $score_group->get_position() );
-		}
-
-		$score_result = new Score_Result( $current_scores_list, $score_results['query_time'], $score_results['cache_used'] );
-
-		return $score_result->to_array();
+		return $score_result_object->to_array();
 	}
 }
