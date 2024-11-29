1682e76b048ceb698fb0dd074142721328e53d10
Diede Exterkate <5352634+diedexx@users.noreply.github.com>
2024-11-29 14:04:05 +0100

    Update helpers to a flat eslint config

---
+++ packages/helpers/eslint.config.mjs
@@ -0,0 +1,30 @@
+import globals from "globals";
+import yoastConfig, { reactConfig } from "eslint-config-yoast";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ ignores: [ "build" ] },
+	...yoastConfig,
+	...reactConfig,
+	{
+		languageOptions: {
+			ecmaVersion: "latest",
+			globals: {
+				...globals.browser,
+				...globals.jquery,
+			},
+		},
+		rules: {
+			"stylistic/comma-dangle": [
+				"error",
+				{
+					functions: "never",
+					arrays: "always-multiline",
+					objects: "always-multiline",
+					imports: "always-multiline",
+					exports: "always-multiline",
+				},
+			],
+		},
+	},
+];
