c2a397b24bc61264faa68bf412cb047b3fcc21d5
Leonidas Milosis <leonidas.milossis@gmail.com>
2024-11-29 14:00:48 +0200

    Try and fix the tests with taxonomy filtering

--- tests/WP/Dashboard/User_Interface/Scores/SEO_Scores_Route_Test.php
+++ tests/WP/Dashboard/User_Interface/Scores/SEO_Scores_Route_Test.php
@@ -206,21 +206,34 @@ final class SEO_Scores_Route_Test extends TestCase {
 	 *
 	 * @dataProvider data_provider_get_seo_scores
 	 *
-	 * @param array<array<string,string>> $inserted_posts   The posts to be insterted.
-	 * @param array<string,string|int>    $taxonomy_filter  An array of term IDs and slugs.
-	 * @param array<string,int>           $expected_amounts The amounts of the scores that are expected to be returned.
+	 * @param array<array<string,string|false>> $inserted_posts   The posts to be insterted.
+	 * @param bool                              $taxonomy_filter  Whether there's a taxonomy filter.
+	 * @param array<string,int>                 $expected_amounts The amounts of the scores that are expected to be returned.
 	 *
 	 * @return void
 	 */
 	public function test_get_seo_scores( $inserted_posts, $taxonomy_filter, $expected_amounts ) {
 		$this->register_blog_post_type();
 
+		$term = self::factory()->term->create_and_get(
+			[
+				'name'     => 'Test category',
+				'taxonomy' => 'category',
+				[
+					'slug' => 'test-category',
+				],
+			]
+		);
+
+		$term_id   = (int) $term->term_id;
+		$term_slug = 'test-category';
+
 		$request = new WP_REST_Request( 'GET', '/yoast/v1/seo_scores' );
 		$request->set_param( 'contentType', 'blog-post' );
 
 		if ( ! empty( $taxonomy_filter ) ) {
 			$request->set_param( 'taxonomy', 'category' );
-			$request->set_param( 'term', $taxonomy_filter['term_id'] );
+			$request->set_param( 'term', $term_id );
 		}
 
 		foreach ( $inserted_posts as $key => $post ) {
@@ -234,7 +247,7 @@ final class SEO_Scores_Route_Test extends TestCase {
 					'post_title'    => 'Test Post ' . $key,
 					'post_status'   => 'publish',
 					'post_type'     => 'blog-post',
-					'post_category' => [ $post['post_category'] ],
+					'post_category' => ( $post['custom_category'] ) ? [ $term_id ] : [ 1 ],
 					'meta_input'    => $meta_input,
 				]
 			);
@@ -263,7 +276,7 @@ final class SEO_Scores_Route_Test extends TestCase {
 		$this->assertEquals( $response_data['scores'][3]['name'], 'notAnalyzed' );
 		$this->assertEquals( $response_data['scores'][3]['amount'], $expected_amounts['notAnalyzed'] );
 
-		$link_suffix = ( ! empty( $taxonomy_filter ) ) ? '&category_name=' . $taxonomy_filter['term_slug'] : '';
+		$link_suffix = ( $taxonomy_filter ) ? '&category_name=' . $term_slug : '';
 		$this->assertEquals( $response_data['scores'][0]['links']['view'], 'http://example.org/wp-admin/edit.php?post_status=publish&post_type=blog-post&seo_filter=good' . $link_suffix );
 		$this->assertEquals( $response_data['scores'][1]['links']['view'], 'http://example.org/wp-admin/edit.php?post_status=publish&post_type=blog-post&seo_filter=ok' . $link_suffix );
 		$this->assertEquals( $response_data['scores'][2]['links']['view'], 'http://example.org/wp-admin/edit.php?post_status=publish&post_type=blog-post&seo_filter=bad' . $link_suffix );
@@ -276,89 +289,77 @@ final class SEO_Scores_Route_Test extends TestCase {
 	 * @return array<string,bool>
 	 */
 	public static function data_provider_get_seo_scores() {
-		$term = self::factory()->term->create_and_get(
-			[
-				'name'     => 'Test category',
-				'taxonomy' => 'category',
-				[
-					'slug' => 'test-category',
-				],
-			]
-		);
-
-		$term_id   = (int) $term->term_id;
-		$term_slug = 'test-category';
 
 		$inserted_posts_in_multiple_terms = [
 			[
-				'post_category' => 1,
-				'meta_input'    => [
+				'custom_category' => false,
+				'meta_input'      => [
 					'_yoast_wpseo_linkdex' => '30',
 					'_yoast_wpseo_focuskw' => 'test',
 				],
 			],
 			[
-				'post_category' => $term_id,
-				'meta_input'    => [
+				'custom_category' => true,
+				'meta_input'      => [
 					'_yoast_wpseo_linkdex' => '30',
 					'_yoast_wpseo_focuskw' => 'test',
 				],
 			],
 			[
-				'post_category' => 1,
-				'meta_input'    => [
+				'custom_category' => false,
+				'meta_input'      => [
 					'_yoast_wpseo_linkdex' => '10',
 					'_yoast_wpseo_focuskw' => 'test',
 				],
 			],
 			[
-				'post_category' => $term_id,
-				'meta_input'    => [
+				'custom_category' => true,
+				'meta_input'      => [
 					'_yoast_wpseo_linkdex' => '10',
 					'_yoast_wpseo_focuskw' => 'test',
 				],
 			],
 			[
-				'post_category' => 1,
-				'meta_input'    => [
+				'custom_category' => false,
+				'meta_input'      => [
 					'_yoast_wpseo_linkdex' => '50',
 					'_yoast_wpseo_focuskw' => 'test',
 				],
 			],
 			[
-				'post_category' => $term_id,
-				'meta_input'    => [
+				'custom_category' => true,
+				'meta_input'      => [
 					'_yoast_wpseo_linkdex' => '50',
 					'_yoast_wpseo_focuskw' => 'test',
 				],
 			],
 			[
-				'post_category' => 1,
-				'meta_input'    => [
+				'custom_category' => false,
+				'meta_input'      => [
 					'_yoast_wpseo_linkdex' => '80',
 					'_yoast_wpseo_focuskw' => 'test',
 				],
 			],
 			[
-				'post_category' => $term_id,
-				'meta_input'    => [
+				'custom_category' => true,
+				'meta_input'      => [
 					'_yoast_wpseo_linkdex' => '80',
 					'_yoast_wpseo_focuskw' => 'test',
 				],
 			],
 			[
-				'post_category' => 1,
-				'meta_input'    => [],
+				'custom_category' => false,
+				'meta_input'      => [],
 			],
 			[
-				'post_category' => $term_id,
-				'meta_input'    => [],
+				'custom_category' => true,
+				'meta_input'      => [],
 			],
 		];
 
 		yield 'No posts inserted' => [
 			'inserted_posts'   => [],
-			'taxonomy_filter'  => [],
+			'taxonomy_filter'  => false,
 			'expected_amounts' => [
 				'good'        => 0,
 				'ok'          => 0,
@@ -368,10 +369,7 @@ final class SEO_Scores_Route_Test extends TestCase {
 		];
 		yield 'Multiple posts of all sorts of SEO scores from term with ID 1' => [
 			'inserted_posts'   => $inserted_posts_in_multiple_terms,
-			'taxonomy_filter'  => [
-				'term_id'   => $term_id,
-				'term_slug' => $term_slug,
-			],
+			'taxonomy_filter'  => true,
 			'expected_amounts' => [
 				'good'        => 1,
 				'ok'          => 1,
@@ -381,7 +379,7 @@ final class SEO_Scores_Route_Test extends TestCase {
 		];
 		yield 'Multiple posts of all sorts of SEO scores from all terms' => [
 			'inserted_posts'   => $inserted_posts_in_multiple_terms,
-			'taxonomy_filter'  => [],
+			'taxonomy_filter'  => false,
 			'expected_amounts' => [
 				'good'        => 2,
 				'ok'          => 2,
