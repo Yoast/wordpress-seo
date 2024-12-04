a9f515f5f276e1e8fe9dd593b534690faa797627
Diede Exterkate <5352634+diedexx@users.noreply.github.com>
2024-12-04 11:42:25 +0100

    Update ui-library to a flat eslint config
    Also lint all js, instead of just src.
    This requires some very minor fixes.

--- packages/ui-library/tests/storyshots.js
+++ packages/ui-library/tests/storyshots.js
@@ -1,3 +1,7 @@
+/* eslint-disable import/no-unresolved --
+ * The tests are currently disabled and these packages are not installed.
+ * We'll resolve this when fixing and re-enabling the tests.
+ */
 import initStoryshots from "@storybook/addon-storyshots";
 import { imageSnapshot } from "@storybook/addon-storyshots-puppeteer";
 import path from "path";
