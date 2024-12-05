7f18d1b0bbcde1975321535d900d7a60bfa93779
Diede Exterkate <5352634+diedexx@users.noreply.github.com>
2024-12-05 10:56:23 +0100

    Update social-metadata-previews to a flat eslint config

---
+++ packages/social-metadata-previews/eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from "globals";
+import yoastConfig, { reactConfig } from "eslint-config-yoast";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	{ ignores: [ "build" ] },
+	...yoastConfig,
+	...reactConfig,
+	{
+		languageOptions: {
+			ecmaVersion: "latest",
+			globals: {
+				...globals.browser,
+			},
+		},
+		rules: {
+			// Deviate from the Yoast config to prohibit dangling commas in functions.
+			"stylistic/comma-dangle": [
+				"error",
+				{
+					functions: "never",
+					arrays: "always-multiline",
+					objects: "always-multiline",
+					imports: "always-multiline",
+					exports: "always-multiline",
+				},
+			],
+			// Deviate from the Yoast config to allow for not using the error that is caught.
+			"no-unused-vars": [ "error", { caughtErrors: "none" } ],
+		},
+	},
+	{
+		files: [ "*.config.*", "jest/**" ],
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+	},
+];
