fa4112eaea2ed0d2eb14aea6bc71a9d2896981b5
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2024-12-09 11:59:37 +0100

    Add more cases.

--- tests/WP/Dashboard/Application/Filter_Pairs/Filter_Pairs_Repository_Test.php
+++ tests/WP/Dashboard/Application/Filter_Pairs/Filter_Pairs_Repository_Test.php
@@ -34,35 +34,75 @@ final class Filter_Pairs_Repository_Test extends TestCase {
 		$taxonomy_validator = new Taxonomy_Validator();
 		$taxonomy_collector = new Taxonomies_Collector( $taxonomy_validator );
 		$this->instance     = new Filter_Pairs_Repository( $taxonomy_collector, new Product_Category_Filter_Pair() );
-
-		\register_taxonomy(
-			'product_cat',
-			'product',
-			[
-				'public'       => true,
-				'show_in_rest' => true,
-			]
-		);
-	}
-
-	/**
-	 * Remove the taxonomy after each test.
-	 *
-	 * @return void
-	 */
-	public function tear_down() {
-		\unregister_taxonomy( 'product_cat' );
-		parent::tear_down();
 	}
 
 	/**
 	 * Tests get_taxonomy to see if it returns the correct data when a pair is found.
 	 *
+	 * @dataProvider data_get_taxonomy
+	 *
+	 * @param string        $taxonomy_name    The taxonomy to register.
+	 * @param string        $object_type_name The object type that is looking.
+	 * @param bool          $is_public        If the taxonomy should be public.
+	 * @param bool          $show_in_rest     If the taxonomy should show in rest.
+	 * @param Taxonomy|null $expected         The expected result.
+	 *
 	 * @return void
 	 */
-	public function test_get_taxonomy() {
-		$product_cat_taxonomy = new Taxonomy( 'product_cat', 'Tags', 'http://example.org/index.php?rest_route=/wp/v2/product_cat' );
-		self::assertNull( $this->instance->get_taxonomy( 'post' ) );
-		self::assertEquals( $product_cat_taxonomy, $this->instance->get_taxonomy( 'product' ) );
+	public function test_get_taxonomy(
+		string $taxonomy_name,
+		string $object_type_name,
+		bool $is_public,
+		bool $show_in_rest,
+		?Taxonomy $expected
+	) {
+		\register_taxonomy(
+			$taxonomy_name,
+			$object_type_name,
+			[
+				'public'       => $is_public,
+				'show_in_rest' => $show_in_rest,
+			]
+		);
+		self::assertEquals( $expected, $this->instance->get_taxonomy( $object_type_name ) );
+		\unregister_taxonomy( $taxonomy_name );
+	}
+
+	/**
+	 * The data provider for the `get_taxonomy` test.
+	 *
+	 * @return array<array<string,bool,null,Taxonomy>> The data.
+	 */
+	public function data_get_taxonomy(): array {
+		return [
+			'Find product_cat taxonomy for product' => [
+				'product_cat',
+				'product',
+				true,
+				true,
+				new Taxonomy( 'product_cat', 'Tags', 'http://example.org/index.php?rest_route=/wp/v2/product_cat' ),
+			],
+			'Don\'t find product_cat taxonomy for product when not public' => [
+				'product_cat',
+				'product',
+				false,
+				true,
+				null,
+			],
+			'Don\'t find product_cat taxonomy for product when not in rest' => [
+				'product_cat',
+				'product',
+				true,
+				false,
+				null,
+			],
+			'Don\'t find taxonomy for a product that has no pair' => [
+				'not_found',
+				'fourofour',
+				true,
+				true,
+				null,
+			],
+		];
 	}
 }
