928996843200c185469fb09b2ee05bdb60268cf7
aidamarfuaty <aida@yoast.com>
2024-12-11 14:34:08 +0100

    Improve JSDoc

--- packages/yoastseo/src/languageProcessing/researches/countSentencesFromText.js
+++ packages/yoastseo/src/languageProcessing/researches/countSentencesFromText.js
@@ -2,9 +2,11 @@ import sentencesLength from "../helpers/sentence/sentencesLength.js";
 import getSentencesFromTree from "../helpers/sentence/getSentencesFromTree";
 
 /**
- * Count sentences in the text.
+ * Gets the sentences from the text and calculates the length of each sentence.
+ *
  * @param {Paper} paper The Paper object to get text from.
  * @param {Researcher} 	researcher 	The researcher to use for analysis.
+ *
  * @returns {SentenceLength[]} The sentences from the text.
  */
 export default function( paper, researcher ) {
