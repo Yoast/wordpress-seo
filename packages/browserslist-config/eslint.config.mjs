e8264b28dd8dc444bfd5a98c8d434f209441e056
Diede Exterkate <5352634+diedexx@users.noreply.github.com>
2024-12-13 10:37:22 +0100

    Update browserlist-config to a flag elsint config

---
+++ packages/browserslist-config/eslint.config.mjs
@@ -0,0 +1,36 @@
+import globals from "globals";
+import yoastConfig from "eslint-config-yoast";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+	...yoastConfig,
+	{
+		languageOptions: {
+			ecmaVersion: "latest",
+			globals: {
+				...globals.node,
+			},
+		},
+		rules: {
+			// Deviate from the Yoast config to prohibit dangling commas in functions.
+			"stylistic/comma-dangle": [
+				"error",
+				{
+					functions: "never",
+					arrays: "always-multiline",
+					objects: "always-multiline",
+					imports: "always-multiline",
+					exports: "always-multiline",
+				},
+			],
+		},
+	},
+	{
+		files: [ "*.config.*" ],
+		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+	},
+];
