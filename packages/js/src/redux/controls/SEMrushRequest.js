4a44b141108e1fe88e3d3fb1af78430ad7166555
Vraja Das <vraja.pro@gmail.com>
2024-12-16 15:00:50 +0200

    adds comments and refactor semrush request control
    fix typo

--- packages/js/src/redux/controls/SEMrushRequest.js
+++ packages/js/src/redux/controls/SEMrushRequest.js
@@ -2,23 +2,24 @@ import apiFetch from "@wordpress/api-fetch";
 import { addQueryArgs } from "@wordpress/url";
 
 /**
- * Control for handling SEMrush related keyphrases request.
+ * Control for handling SEMrush related keyphrases request and save of the country code to the database.
  *
- * @param {Object} action The action object.
+ * @param {string} countryCode The country code to use.
+ * @param {string} keyphrase The keyphrase to use.
  *
- * @returns {Promise} The API fetch promise.
+ * @returns {Promise} The API fetch promise, when resolved,
+ * contains a list of related keyphrses with their search volumne, trends in recent year, search intent, and difficulty index.
  */
-export const NEW_REQUEST = async( action ) => {
-	const { countryCode, keyphrase } = action;
-
-	await apiFetch( {
+export const NEW_REQUEST = async( { countryCode, keyphrase } ) => {
+	// Saves the country code in the database.
+	apiFetch( {
 		path: "yoast/v1/semrush/country_code",
 		method: "POST",
 		// eslint-disable-next-line camelcase
 		data: { country_code: countryCode },
 	} );
 
-	const response = await apiFetch( {
+	return apiFetch( {
 		path: addQueryArgs(
 			"/yoast/v1/semrush/related_keyphrases",
 			{
@@ -28,6 +29,4 @@ export const NEW_REQUEST = async( action ) => {
 			}
 		),
 	} );
-
-	return response;
 };
