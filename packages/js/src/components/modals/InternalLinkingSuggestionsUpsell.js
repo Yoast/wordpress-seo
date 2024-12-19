c2b8b13439d94f0eff7ed10d7e367e1d93b0872c
Vraja Das <vraja.pro@gmail.com>
2024-12-19 17:15:37 +0200

    fix translations for ads

--- packages/js/src/components/modals/InternalLinkingSuggestionsUpsell.js
+++ packages/js/src/components/modals/InternalLinkingSuggestionsUpsell.js
@@ -47,8 +47,10 @@ export const InternalLinkingSuggestionsUpsell = () => {
 								"Yoast SEO Premium"
 							) }
 							benefitsTitle={
-								/* translators: %s expands to 'Yoast SEO Premium'. */
-								sprintf( "%s also gives you:", "Yoast SEO Premium" )
+								sprintf(
+									/* translators: %s expands to 'Yoast SEO Premium'. */
+									__( "%s also gives you:", "wordpress-seo" ),
+									"Yoast SEO Premium" )
 							}
 							benefits={ getPremiumBenefits() }
 							upsellButtonText={
