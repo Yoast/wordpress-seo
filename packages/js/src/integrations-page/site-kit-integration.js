f320fb04938d881387d614809635aae59520e459
Vraja Das <vraja.pro@gmail.com>
2024-12-19 15:23:56 +0200

    reduce complexity and separate translations
    fix semicolon

--- packages/js/src/integrations-page/site-kit-integration.js
+++ packages/js/src/integrations-page/site-kit-integration.js
@@ -1,6 +1,6 @@
 import { __, sprintf } from "@wordpress/i18n";
 import { CheckIcon, XIcon } from "@heroicons/react/solid";
-import { Fragment, createInterpolateElement } from "@wordpress/element";
+import { Fragment, createInterpolateElement, useMemo, useCallback } from "@wordpress/element";
 import { SimpleIntegration } from "./simple-integration";
 import { ReactComponent as SiteKitLogo } from "../../images/site-kit-logo.svg";
 import { get } from "lodash";
@@ -28,70 +28,85 @@ const siteKitIntegration = {
 	logo: SiteKitLogo,
 };
 
+const buttonLabels = {
+	install: sprintf(
+		/* translators: 1: Site Kit by Google */
+		__( "Install %1$s", "wordpress-seo" ),
+		"Site Kit by Google"
+	),
+	activate: sprintf(
+		/* translators: 1: Site Kit by Google */
+		__( "Activate %1$s", "wordpress-seo" ),
+		"Site Kit by Google"
+	),
+	setup: sprintf(
+		/* translators: 1: Site Kit by Google */
+		__( "Set up %1$s", "wordpress-seo" ),
+		"Site Kit by Google"
+	),
+	connect: sprintf(
+		/* translators: 1: Site Kit by Google */
+		__( "Connect %1$s", "wordpress-seo" ),
+		"Site Kit by Google"
+	),
+	disconnect: __( "Disconnect", "wordpress-seo" ),
+};
+
 /**
- * Represents an integration.
+ * The Site Kit integration component.
  *
- * @param {object}  integration The integration.
- * @param {boolean} isActive    The integration state.
- *
- * @returns {WPElement} A card representing an integration.
+ * @returns {WPElement} The Site Kit integration component.
  */
 export const SiteKitIntegration = () => {
 	const isActive = get( window, "wpseoIntegrationsData.google_site_kit.active", false );
 	const afterSetup = get( window, "wpseoIntegrationsData.google_site_kit.setup", false );
 	const isInstalled = get( window, "wpseoIntegrationsData.google_site_kit.installed", false );
 	const isConnected = get( window, "wpseoIntegrationsData.google_site_kit.connected", false );
-	const button = {
-		className: "yst-mt-6 yst-w-full",
-		as: "a",
-	};
-	if ( ! isInstalled ) {
-		button.children = sprintf(
-			/* translators: 1: Site Kit by Google */
-			__( "Install %1$s", "wordpress-seo" ),
-			"Site Kit by Google"
-		);
-		button.href = "/wp-admin/plugins.php";
-	} else if ( ! isActive ) {
-		button.children = sprintf(
-			/* translators: 1: Site Kit by Google */
-			__( "Activate %1$s", "wordpress-seo" ),
-			"Site Kit by Google"
-		);
-		button.href = "/wp-admin/plugins.php";
-	} else if ( ! afterSetup ) {
-		button.children = sprintf(
-			/* translators: 1: Site Kit by Google */
-			__( "Set up %1$s", "wordpress-seo" ),
-			"Site Kit by Google"
-		);
-		button.href = "/wp-admin/admin.php?page=googlesitekit-splash";
-	} else if ( ! isConnected ) {
-		button.children = sprintf(
-			/* translators: 1: Site Kit by Google */
-			__( "Connect %1$s", "wordpress-seo" ),
-			"Site Kit by Google"
-		);
-	} else if ( isConnected ) {
-		button.children = __( "Disconnect", "wordpress-seo" );
-		button.variant = "secondary";
-	}
+
+	const getButtonConfig = useCallback( () => {
+		const button = {
+			className: "yst-mt-6 yst-w-full",
+			as: "a",
+		};
+
+		if ( ! isInstalled ) {
+			button.children = buttonLabels.install;
+			button.href = "/wp-admin/plugins.php";
+		} else if ( ! isActive ) {
+			button.children = buttonLabels.activate;
+			button.href = "/wp-admin/plugins.php";
+		} else if ( ! afterSetup ) {
+			button.children = buttonLabels.setup;
+			button.href = "/wp-admin/admin.php?page=googlesitekit-splash";
+		} else if ( ! isConnected ) {
+			button.children = buttonLabels.connect;
+		} else if ( isConnected ) {
+			button.children = buttonLabels.disconnect;
+			button.variant = "secondary";
+		}
+
+		return button;
+	}, [ isInstalled, isActive, afterSetup, isConnected ] );
+
+	const notConnected = useMemo( () => ! isConnected || ! afterSetup, [ isConnected, afterSetup ] );
+	const pluginNotDetected = useMemo( () => ! isActive || ! isInstalled, [ isActive, isInstalled ] );
+	const successfullyConnected = useMemo( () => isActive && afterSetup && isConnected, [ isActive, afterSetup, isConnected ] );
 
 	return (
 		<SimpleIntegration
 			integration={ siteKitIntegration }
 			isActive={ isInstalled && isActive  }
-			button={ button }
+			button={ getButtonConfig( isInstalled, isActive, afterSetup, isConnected ) }
 		>
 
-			{ isActive && afterSetup && isConnected && <Fragment>
+			{ successfullyConnected && <Fragment>
 				<span className="yst-text-slate-700 yst-font-medium">{ __( "Successfully connected", "wordpress-seo" ) }</span>
 				<CheckIcon
 					className="yst-h-5 yst-w-5 yst-text-green-400 yst-flex-shrink-0"
 				/>
 			</Fragment> }
 
-			{ ( ! isConnected || ! afterSetup ) && isActive && <Fragment>
+			{ notConnected && isActive && <Fragment>
 				<span className="yst-text-slate-700 yst-font-medium">
 					{
 						__( "Not connected", "wordpress-seo" )
@@ -102,7 +117,7 @@ export const SiteKitIntegration = () => {
 				/>
 			</Fragment> }
 
-			{ ( ! isActive || ! isInstalled ) && <Fragment>
+			{ pluginNotDetected && <Fragment>
 				<span className="yst-text-slate-700 yst-font-medium">
 					{
 						__( "Plugin not detected", "wordpress-seo" )
