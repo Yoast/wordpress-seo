a808333fd4daa0b2d9910e480dddc1fdfda06a01
Mykola Shlyakhtun <mykola.shlyakhtun@gmail.com>
2025-01-09 15:18:28 +0300

    Related keyphrase status neutral/orange instead of happy/green when all the results are good #4475 Unused class removed

--- packages/yoastseo/src/scoring/scoreAggregators/index.js
+++ packages/yoastseo/src/scoring/scoreAggregators/index.js
@@ -1,4 +1,3 @@
 export { default as ReadabilityScoreAggregator } from "./ReadabilityScoreAggregator";
-export { default as RelatedKeywordScoreAggregator } from "./RelatedKeywordScoreAggregator";
 export { default as ScoreAggregator } from "./ScoreAggregator";
 export { default as SEOScoreAggregator } from "./SEOScoreAggregator";
