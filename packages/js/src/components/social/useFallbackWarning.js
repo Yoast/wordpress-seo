857c579ac573acaead17b4aae63d08a94bdf1eca
Paolo L. Scala <paolo@yoast.com>
2025-01-13 10:43:42 +0100

    Restructure the conditional to avoid removing the original warning

--- packages/js/src/components/social/useFallbackWarning.js
+++ packages/js/src/components/social/useFallbackWarning.js
@@ -12,19 +12,19 @@ import { __, sprintf } from "@wordpress/i18n";
  */
 export const useFallbackWarning = ( imageFallbackUrl, imageUrl, imageWarnings ) => {
 	useEffect(() => {
-		if ( imageFallbackUrl.toLowerCase().endsWith( ".avif" ) && imageUrl === "" ) {
-			const warningMessage = sprintf(
-				/* Translators: %s expands to the jpg format, %s expands to the png format, %s expands to the gif format. */
-				__(
-					"The image automatically added from your content is in an unsupported format. Supported formats are %s, %s, %s and %s. Please select or upload an image in a supported format to ensure it displays correctly on social media.",
-					"wordpress-seo"
-				),
-				"JPG", "PNG", "WEBP", "GIF"
-			);
+		if ( imageUrl === "" ) {
 			imageWarnings.length = 0;
-			imageWarnings.push( warningMessage );
-		} else {
-			imageWarnings.length = 0;
+			if ( imageFallbackUrl.toLowerCase().endsWith( ".avif" ) ) {
+				const warningMessage = sprintf(
+					/* Translators: %s expands to the jpg format, %s expands to the png format, %s expands to the gif format. */
+					__(
+						"The image automatically added from your content is in an unsupported format. Supported formats are %s, %s, %s and %s. Please select or upload an image in a supported format to ensure it displays correctly on social media.",
+						"wordpress-seo"
+					),
+					"JPG", "PNG", "WEBP", "GIF"
+				);
+				imageWarnings.push( warningMessage );
+			}
 		}
 	}, [ imageFallbackUrl, imageUrl ] );
 };
