93d9adf26cffe12d2733bb766e4e383bd815938c
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-01-14 15:20:19 +0200

    Check if on tests and silence notice if so

--- inc/options/class-wpseo-option.php
+++ inc/options/class-wpseo-option.php
@@ -145,6 +145,14 @@ abstract class WPSEO_Option {
 	 */
 	protected function __construct() {
 
+		if ( defined( WERE_ARE_DOING_TESTS ) && WERE_ARE_DOING_TESTS ) {
+			add_action( 'doing_it_wrong_trigger_error', function ( $function_name, $message, $error_level  ) {
+				if ( '_load_textdomain_just_in_time ' == $function_name ) {
+					return false;
+				}
+			}, 10, 3 );
+		}
+
 		/* Add filters which get applied to the get_options() results. */
 		$this->add_default_filters(); // Return defaults if option not set.
 		$this->add_option_filters(); // Merge with defaults if option *is* set.
