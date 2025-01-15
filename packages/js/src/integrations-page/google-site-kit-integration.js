b63ee4ed9f3fefa49256c6d9dc4720206cfac4cb
Vraja Das <vraja.pro@gmail.com>
2025-01-15 10:16:14 +0200

    update design

--- packages/js/src/integrations-page/google-site-kit-integration.js
+++ packages/js/src/integrations-page/google-site-kit-integration.js
@@ -1,10 +1,10 @@
 import { __, sprintf } from "@wordpress/i18n";
-import { CheckIcon, XIcon } from "@heroicons/react/solid";
+import { CheckIcon } from "@heroicons/react/solid";
 import { createInterpolateElement, useCallback } from "@wordpress/element";
 import PropTypes from "prop-types";
 import { SimpleIntegration } from "./simple-integration";
 import { ReactComponent as SiteKitLogo } from "../../images/site-kit-logo.svg";
-import { useToggleState } from "@yoast/ui-library";
+import { Button, useToggleState } from "@yoast/ui-library";
 import { SiteKitConsentModal } from "../shared-admin/components";
 
 const integration = {
@@ -30,79 +30,19 @@ const integration = {
 };
 
 /**
- * The NotConnected component.
- *
- * @param {boolean} isConnected Whether the integration is connected.
- * @param {boolean} afterSetup Whether the integration has been set up.
- * @param {boolean} isActive Whether the integration is active.
- *
- * @returns {JSX.Element} The NotConnected component.
- */
-const NotConnected = ( { isConnected, afterSetup, isActive } ) => {
-	return ( ! isConnected || ! afterSetup ) && isActive && <>
-		<span className="yst-text-slate-700 yst-font-medium">
-			{ __( "Not connected", "wordpress-seo" ) }
-		</span>
-		<XIcon
-			className="yst-h-5 yst-w-5 yst-text-red-500 yst-flex-shrink-0"
-		/>
-	</>;
-};
-
-NotConnected.propTypes = {
-	isConnected: PropTypes.bool.isRequired,
-	afterSetup: PropTypes.bool.isRequired,
-	isActive: PropTypes.bool.isRequired,
-};
-
-/**
- * Plugin not detected.
- *
- * @param {boolean} isActive Whether the integration is active.
- * @param {boolean} isInstalled Whether the integration is installed.
- *
- * @returns {JSX.Element} The PluginNotDetected component.
- */
-const PluginNotDetected = ( { isActive, isInstalled } ) => {
-	return ( ! isActive || ! isInstalled ) && <>
-		<span className="yst-text-slate-700 yst-font-medium">
-			{ __( "Plugin not detected", "wordpress-seo" ) }
-		</span>
-		<XIcon
-			className="yst-h-5 yst-w-5 yst-text-red-500 yst-flex-shrink-0"
-		/>
-	</>;
-};
-
-PluginNotDetected.propTypes = {
-	isActive: PropTypes.bool.isRequired,
-	isInstalled: PropTypes.bool.isRequired,
-};
-
-/**
- * The successfully Connected component.
- *
- * @param {boolean} isActive Whether the integration is active.
- * @param {boolean} afterSetup Whether the integration has been set up.
- * @param {boolean} isConnected Whether the integration is connected.
+ * The successfully connected component.
  *
  * @returns {JSX.Element} The SuccessfullyConnected component.
  */
-const SuccessfullyConnected = ( { isActive, afterSetup, isConnected } ) => {
-	return isActive && afterSetup && isConnected && <>
+const SuccessfullyConnected = () => {
+	return <span className="yst-flex yst-justify-between yst-pb-4 yst-border-b yst-mb-6 yst-border-slate-200 yst--mt-2">
 		<span className="yst-text-slate-700 yst-font-medium">
 			{ __( "Successfully connected", "wordpress-seo" ) }
 		</span>
 		<CheckIcon
 			className="yst-h-5 yst-w-5 yst-text-green-400 yst-flex-shrink-0"
 		/>
-	</>;
-};
-
-SuccessfullyConnected.propTypes = {
-	isActive: PropTypes.bool.isRequired,
-	afterSetup: PropTypes.bool.isRequired,
-	isConnected: PropTypes.bool.isRequired,
+	</span>;
 };
 
 /**
@@ -163,20 +103,17 @@ export const GoogleSiteKitIntegration = ( { isActive, afterSetup, isInstalled, i
 		}
 	}, [ isInstalled, isActive, afterSetup, isConnected ] );
 
+	const successfullyConnected = isInstalled && isActive && afterSetup && isConnected;
 	return (
 		<>
 			<SimpleIntegration
 				integration={ integration }
-				isActive={ isInstalled && isActive }
-				button={ {
-					className: "yst-mt-6 yst-w-full",
-					id: "google-site-kit-button",
-					...getButtonProps( isInstalled, isActive, afterSetup, isConnected ),
-				} }
+				isActive={ successfullyConnected }
 			>
-				<SuccessfullyConnected isActive={ isActive } afterSetup={ afterSetup } isConnected={ isConnected } />
-				<NotConnected isConnected={ isConnected } afterSetup={ afterSetup } isActive={ isActive } />
-				<PluginNotDetected isActive={ isActive } isInstalled={ isInstalled } />
+				<span className="yst-flex yst-flex-col yst-flex-1">
+					{ successfullyConnected && <SuccessfullyConnected  /> }
+					<Button className="yst-w-full" id="google-site-kit-button" { ...getButtonProps( isInstalled, isActive, afterSetup, isConnected ) } />
+				</span>
 			</SimpleIntegration>
 			<SiteKitConsentModal isOpen={ isModalOpen } onClose={ toggleModal } />
 		</>
--- packages/js/src/integrations-page/simple-integration.js
+++ packages/js/src/integrations-page/simple-integration.js
@@ -13,11 +13,10 @@ import { getIsFreeIntegrationOrPremiumAvailable } from "./helper";
  * @param {object}     integration The integration.
  * @param {boolean}    isActive    The integration state.
  * @param {wp.Element} children    The child components.
- * @param {object} button      The button component props.
  *
  * @returns {WPElement} A card representing an integration.
  */
-export const SimpleIntegration = ( { integration, isActive, children, button } ) => {
+export const SimpleIntegration = ( { integration, isActive, children } ) => {
 	const IntegrationLogo = integration.logo;
 
 	return (
@@ -69,8 +68,6 @@ export const SimpleIntegration = ( { integration, isActive, children, button } )
 						</span>
 						<ArrowSmRightIcon className="yst-h-4 yst-w-4 yst-ms-1 yst-icon-rtl" />
 					</Link> }
-
-					{ button && <Button { ...button } /> }
 				</div>
 			</Card.Content>
 			<Card.Footer>
@@ -124,11 +121,9 @@ SimpleIntegration.propTypes = {
 		PropTypes.node,
 		PropTypes.arrayOf( PropTypes.node ),
 	] ),
-	button: PropTypes.object,
 };
 
 SimpleIntegration.defaultProps = {
 	isActive: true,
 	children: [],
-	button: null,
 };
