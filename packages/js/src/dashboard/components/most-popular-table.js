8156e79d279e04e4519bca2e72e050b17c2e7f2c
Vraja Das <vraja.pro@gmail.com>
2025-01-23 16:36:49 +0200

    add classes to align data

--- packages/js/src/dashboard/components/most-popular-table.js
+++ packages/js/src/dashboard/components/most-popular-table.js
@@ -16,22 +16,28 @@ export const MostPopularTable = ( { data } ) => {
 	return <TableWidget title={ __( "Top 5 most popular content", "wordpress-seo" ) }>
 		<TableWidget.Head>
 			<TableWidget.Header>{ __( "Landing page", "wordpress-seo" ) }</TableWidget.Header>
-			<TableWidget.Header>{ __( "Clicks", "wordpress-seo" ) }</TableWidget.Header>
-			<TableWidget.Header>{ __( "Impressions", "wordpress-seo" ) }</TableWidget.Header>
-			<TableWidget.Header>{ __( "CTR", "wordpress-seo" ) }</TableWidget.Header>
-			<TableWidget.Header>{ __( "Average position", "wordpress-seo" ) }</TableWidget.Header>
+			<TableWidget.Header className="yst-text-end">{ __( "Clicks", "wordpress-seo" ) }</TableWidget.Header>
+			<TableWidget.Header className="yst-text-end">{ __( "Impressions", "wordpress-seo" ) }</TableWidget.Header>
+			<TableWidget.Header className="yst-text-end">{ __( "CTR", "wordpress-seo" ) }</TableWidget.Header>
+			<TableWidget.Header  className="yst-text-end">
+				{ __( "Average position", "wordpress-seo" ) }
+			</TableWidget.Header>
 			<TableWidget.Header className="yst-text-center">
-				{ __( "SEO score", "wordpress-seo" ) }
+				<div className="yst-flex yst-justify-end yst-items-end">
+					<div className="yst-flex yst-justify-center yst-w-16">
+						{ __( "SEO score", "wordpress-seo" ) }
+					</div>
+				</div>
 			</TableWidget.Header>
 		</TableWidget.Head>
 		<TableWidget.Body>
 			{ data.map( ( { subject, clicks, impressions, ctr, averagePosition, seoScore }, index ) => (
 				<TableWidget.Row key={ `most-popular-content-${ index }` } index={ index }>
-					<TableWidget.Cell>{ subject }</TableWidget.Cell>
-					<TableWidget.Cell>{ clicks }</TableWidget.Cell>
-					<TableWidget.Cell>{ impressions }</TableWidget.Cell>
-					<TableWidget.Cell>{ ctr }</TableWidget.Cell>
-					<TableWidget.Cell>{ averagePosition }</TableWidget.Cell>
+					<TableWidget.Cell className="yst-text-slate-900 yst-font-medium">{ subject }</TableWidget.Cell>
+					<TableWidget.Cell className="yst-text-end">{ clicks }</TableWidget.Cell>
+					<TableWidget.Cell className="yst-text-end">{ impressions }</TableWidget.Cell>
+					<TableWidget.Cell className="yst-text-end">{ ctr }</TableWidget.Cell>
+					<TableWidget.Cell className="yst-text-end">{ averagePosition }</TableWidget.Cell>
 					<TableWidget.Cell><TableWidget.ScoreBullet score={ seoScore } /></TableWidget.Cell>
 				</TableWidget.Row>
 			) ) }
