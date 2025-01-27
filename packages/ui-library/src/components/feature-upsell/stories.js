1cb6cb4e72c2781137775ef95463fdfd47c066ca
dennis <dennis@yoast.com>
2025-01-27 17:02:31 +0100

    Allow toggling upsell state within the story

--- packages/ui-library/src/components/feature-upsell/stories.js
+++ packages/ui-library/src/components/feature-upsell/stories.js
@@ -1,15 +1,42 @@
-import React from "react";
-import FeatureUpsell from ".";
+import React, { useCallback } from "react";
+import { useArgs } from "@storybook/preview-api";
 import { InteractiveDocsPage } from "../../../.storybook/interactive-docs-page";
 import { card, component } from "./docs";
+import { Button, Title, FeatureUpsell } from "../..";
+
+const Template = ( { children, ...props } ) => {
+	const [ { shouldUpsell = true }, updateArgs ] = useArgs();
+	const disableUpsell = useCallback( ( e ) => {
+		e.preventDefault();
+		updateArgs( { shouldUpsell: false } );
+		return false;
+	}, [ updateArgs ] );
+
+	const enableUpsell = useCallback( () => updateArgs( { shouldUpsell: true } ), [ updateArgs ] );
+
+	return (
+		<FeatureUpsell { ...props } shouldUpsell={ shouldUpsell } onClick={ disableUpsell }>
+			<div>
+				{ children }
+				<div>
+					<Button onClick={ enableUpsell } className="yst-mt-4">
+						Enable feature upsell
+					</Button>
+				</div>
+			</div>
+		</FeatureUpsell>
+	);
+};
 
 export const Factory = {
+	render: Template.bind( {} ),
 	parameters: {
 		controls: { disable: false },
 	},
 };
 
 export const Card = {
+	render: Template.bind( {} ),
 	parameters: {
 		controls: { disable: false },
 		docs: { description: { story: card } },
