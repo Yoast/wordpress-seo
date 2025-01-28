ebba34f821c6ccfffdc74ff55083381d26d5de09
Vraja Das <vraja.pro@gmail.com>
2025-01-28 20:50:53 +0200

    fix stepper story
    Use update args and changed the argTypes

--- packages/ui-library/src/components/stepper/stories.js
+++ packages/ui-library/src/components/stepper/stories.js
@@ -1,4 +1,5 @@
 import React, { useState, useCallback } from "react";
+import { useArgs } from "@storybook/preview-api";
 import { Stepper } from ".";
 import { component } from "./docs";
 import { Button } from "../../index";
@@ -8,26 +9,24 @@ export const Factory = {
 		controls: { disable: false },
 	},
 	render: ( args ) =>{
-		const [ currentStep, setCurrentStep ] = useState( 1 );
 		const [ isComplete, setIsComplete ] = useState( false );
 		const steps = [ "INSTALL", "ACTIVATE", "SET UP", "CONNECT" ];
+		const [ { numberOfSteps, className, currentStep }, updateArgs ] = useArgs();
 
 		const handleNext = useCallback( () => {
-			setCurrentStep( ( prevStep ) => {
-				if ( isComplete ) {
-					setIsComplete( false );
-					return 1;
-				}
-				if ( prevStep === steps.length ) {
-					setIsComplete( true );
-					return prevStep;
-				}
-				return prevStep + 1;
-			} );
-		}, [ setIsComplete, setCurrentStep, isComplete ] );
+			if ( currentStep < steps.length ) {
+				setIsComplete( false );
+				updateArgs( { currentStep: currentStep + 1 } );
+			} else if ( currentStep === steps.length && ! isComplete ) {
+				setIsComplete( true );
+			} else if ( isComplete ) {
+				setIsComplete( false );
+				updateArgs( { currentStep: 1 } );
+			}
+		}, [ setIsComplete, updateArgs, isComplete, currentStep ] );
 
 		return <>
-			<Stepper { ...args } numberOfSteps={ steps.length } currentStep={ currentStep }>
+			<Stepper className={ className } numberOfSteps={ numberOfSteps } currentStep={ currentStep }>
 				{ steps.map( ( step, index ) => <Stepper.Step
 					key={ step }
 					label={ step }
@@ -51,7 +50,9 @@ export default {
 	title: "2) Components/Stepper",
 	component: Stepper,
 	argTypes: {
-		className: { control: false },
+		className: { control: "text" },
+		numberOfSteps: { control: "number" },
+		currentStep: { control: "number" },
 	},
 	parameters: {
 		docs: {
@@ -60,5 +61,7 @@ export default {
 	},
 	args: {
 		className: "yst-mb-5",
+		numberOfSteps: 4,
+		currentStep: 1,
 	},
 };
