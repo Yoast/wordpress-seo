e9e20f09a0e8aba7630b9ac5f50e5afc1bda9ef7
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-01-30 11:48:01 +0100

    Fix unit test

--- src/dashboard/infrastructure/integrations/site-kit.php
+++ src/dashboard/infrastructure/integrations/site-kit.php
@@ -61,10 +61,10 @@ class Site_Kit implements Integration_Data_Provider_Interface {
 		$site_kit_setup_url = \self_admin_url( 'admin.php?page=googlesitekit-splash' );
 
 		return [
-			'isInstalled'       => \file_exists( \WP_PLUGIN_DIR . '/' . self::SITE_KIT_FILE ),
-			'isActive'          => \is_plugin_active( self::SITE_KIT_FILE ),
+			'isInstalled'     => \file_exists( \WP_PLUGIN_DIR . '/' . self::SITE_KIT_FILE ),
+			'isActive'        => \is_plugin_active( self::SITE_KIT_FILE ),
 			'setup_completed' => \get_option( 'googlesitekit_has_connected_admins', false ) === '1',
-			'isConnected'       => $this->options_helper->get( 'google_site_kit_connected', false ),
+			'isConnected'     => $this->options_helper->get( 'google_site_kit_connected', false ),
 			'feature_enabled' => ( new Google_Site_Kit_Feature_Conditional() )->is_met(),
 			'install_url'     => $site_kit_install_url,
 			'activate_url'    => $site_kit_activate_url,
