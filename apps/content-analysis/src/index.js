50bd92128231cfb50253432db9c478d214ba6a08
Martijn van der Klis <martijnvanderklis@yoast.com>
2025-02-06 21:30:49 +0100

    Reverting something that got accidentally deleted

--- apps/content-analysis/src/index.js
+++ apps/content-analysis/src/index.js
@@ -53,6 +53,8 @@ const storageMiddleware = createStorageMiddleware( storageStates );
 
 const store = configureStore( initialState, [ storageMiddleware ] );
 const workerUnwrapped = new AnalysisWebWorker();
+// Pass language so that the right researcher is loaded.
+workerUnwrapped.postMessage( { language: window.localStorage.language } );
 const worker = new AnalysisWorkerWrapper( workerUnwrapped );
 
 const subscriber = new StoreSubscriber( { store, worker } );
