d7cc77e5f615c11f9c7f0490804ca682fb85c78d
Martijn van der Klis <martijnvanderklis@yoast.com>
2025-02-06 21:31:13 +0100

    Adds a language picker and some very minimal styling to the webworker example

--- apps/content-analysis-webworker/src/index.js
+++ apps/content-analysis-webworker/src/index.js
@@ -1,20 +1,26 @@
-import { AnalysisWorkerWrapper, Paper } from "yoastseo";
+import { AnalysisWorkerWrapper, Paper, interpreters } from "yoastseo";
 
-// `url` needs to be the full URL to the script for the browser to know where to load the worker script from.
-// This should be the script created by the previous code-snippet.
-const worker = new AnalysisWorkerWrapper( new Worker( new URL("./worker.js", import.meta.url) ) );
+const loadWebWorker = ( language ) => {
+	// `url` needs to be the full URL to the script for the browser to know where to load the worker script from.
+	// This should be the script created by the previous code-snippet.
+	const workerUnwrapped = new Worker( new URL("./worker.js", import.meta.url) );
+	// Pass language so that the right researcher is loaded.
+	workerUnwrapped.postMessage( { language } );
+	return new AnalysisWorkerWrapper( workerUnwrapped);
+}
 
 const createResultSection = (title, results) => {
 	const section = document.createElement('section');
-	const header = document.createElement('h2');
+	const header = document.createElement('h3');
 	header.textContent = title;
 	section.appendChild(header);
 	const resultList = document.createElement('ul');
 	section.appendChild(resultList);
 
 	results.forEach(result => {
-		if (result.text) {
+		if ( result.text ) {
 			const resultText = document.createElement('li');
+			resultText.className = interpreters.scoreToRating( result.score );
 			resultText.innerHTML = result.text;
 			resultList.appendChild(resultText);
 		}
@@ -28,7 +34,9 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	analyze.addEventListener("click", event => {
 		const paperText = document.getElementById( 'paper' ).value;
 		const keyphrase = document.getElementById( 'keyphrase' ).value;
-		const resultPre = document.getElementById( 'result-pre' );
+		const language = document.getElementById( 'language' ).value;
+
+		const worker = loadWebWorker( language );
 
 		worker.initialize( {
 			logLevel: "TRACE", // Optional, see https://github.com/pimterry/loglevel#documentation
@@ -42,7 +50,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
 		} ).then( ( results ) => {
 			console.log( 'Analysis results:' );
 			console.log( results );
-			resultPre.textContent = JSON.stringify( results, null, 2 );
 
 			const container = document.getElementById('result-div');
 			container.innerHTML = '';
