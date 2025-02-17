90cdc94ee48960b9f86786821b8b12532cad7967
marinakoleva <marina@yoast.com>
2025-02-17 16:11:06 +0100

    adapts unit tests after punctuation was removed from character count for Japanese texts

--- packages/yoastseo/spec/languageProcessing/languages/ja/helpers/countCharactersSpec.js
+++ packages/yoastseo/spec/languageProcessing/languages/ja/helpers/countCharactersSpec.js
@@ -4,12 +4,12 @@ describe( "counts characters in a string", function() {
 	it( "returns the number of characters", function() {
 		expect( countCharactersFunction( "これに対し日本国有鉄道（国鉄）は、十河信二国鉄総裁と技師長の島秀雄の下、" +
 			"高速運転が可能な標準軌新線を建設することを決定。1959年（昭和34年）4月20日、新丹那トンネル熱海口で起工式を行って着工し、" +
-			"東京オリンピック開会直前の1964年（昭和39年）10月1日に開業した。" ) ).toBe( 136 );
+			"東京オリンピック開会直前の1964年（昭和39年）10月1日に開業した。" ) ).toBe( 124 );
 	} );
 	it( "returns the number of characters not including URL characters in the count", function() {
 		expect( countCharactersFunction( "www.yoast.comこれに対し日本国有鉄道（国鉄）は、十河信二国鉄総裁と技師長の島秀雄の下、" +
 			"高速運転が可能な標準軌新線を建設することを決定。1959年（昭和34年）4月20日、新丹那トンネル熱海口で起工式を行って着工し、" +
-			"東京オリンピック開会直前の1964年（昭和39年）10月1日に開業した。" ) ).toBe( 136 );
+			"東京オリンピック開会直前の1964年（昭和39年）10月1日に開業した。" ) ).toBe( 124 );
 	} );
 	it( "makes sure the countCharacters function still works when the input is a non-Japanese string", function() {
 		expect( countCharactersFunction( "this is a string" ) ).toBe( 13 );
--- packages/yoastseo/spec/languageProcessing/researches/keyphraseLengthSpec.js
+++ packages/yoastseo/spec/languageProcessing/researches/keyphraseLengthSpec.js
@@ -1,4 +1,5 @@
 import EnglishResearcher from "../../../src/languageProcessing/languages/en/Researcher";
+import JapaneseResearcher from "../../../src/languageProcessing/languages/ja/Researcher";
 import getMorphologyData from "../../specHelpers/getMorphologyData";
 import keyphraseLength from "../../../src/languageProcessing/researches/keyphraseLength.js";
 import Paper from "../../../src/values/Paper.js";
@@ -18,7 +19,7 @@ describe( "the keyphrase length research", function() {
 } );
 
 describe( "the keyphrase length research", function() {
-	it( "should count the words in the input and filters function words", function() {
+	it( "should count the words in the input and filter function words", function() {
 		const paper = new Paper( "", { keyword: "word word the word" } );
 		const researcher = new EnglishResearcher( paper );
 		researcher.addResearchData( "morphology", morphologyData );
@@ -42,7 +43,7 @@ Describe( "the keyphrase length research", function() {
 } );
 */
 
-describe( "the keyphrase length research for empty keyword", function() {
+describe( "the keyphrase length research for an empty keyword", function() {
 	it( "should count the words in the input", function() {
 		const paper = new Paper( "", { keyword: "" } );
 		const researcher = new EnglishResearcher( paper );
@@ -53,3 +54,4 @@ describe( "the keyphrase length research for empty keyword", function() {
 		expect( result.keyphraseLength ).toBe( 0 );
 	} );
 } );
+
