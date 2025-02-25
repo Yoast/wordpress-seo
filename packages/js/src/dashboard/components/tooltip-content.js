80d162a9522e96242d79a3e4e17e4928c5d9ee3d
Paolo L. Scala <paolo@yoast.com>
2025-02-25 22:39:40 +0100

    Abstract tooltip content to be re-used elsewhere

---
+++ packages/js/src/dashboard/components/tooltip-content.js
@@ -0,0 +1,35 @@
+import { __ } from "@wordpress/i18n";
+import { Button } from "@yoast/ui-library";
+import { ArrowNarrowRightIcon  } from "@heroicons/react/solid";
+
+/**
+ * The content of the info tooltip.
+ *
+ * @param {string} url The learn more link.
+ * @param {string} localizedString The content of the tooltip.
+ *
+ * @returns {JSX.Element} The element.
+ */
+export const TooltipContent = ( { url, localizedString } ) => (
+	<>
+		<p>
+			{ localizedString }
+		</p>
+		<Button
+			variant="tertiary"
+			as="a"
+			target="_blank"
+			href={ url }
+			className="yst-px-0"
+		>
+			{ __( "Learn more", "wordpress-seo" ) }
+			<ArrowNarrowRightIcon className="yst-w-4 yst-h-4 yst-me-1 rtl:yst-rotate-180 yst-ms-1.5" />
+			<span className="yst-sr-only">
+				{
+					/* translators: Hidden accessibility text. */
+					__( "(Opens in a new browser tab)", "wordpress-seo" )
+				}
+			</span>
+		</Button>
+	</>
+);
