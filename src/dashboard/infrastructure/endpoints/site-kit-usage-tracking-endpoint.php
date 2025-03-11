80b53ba02c587ccd920846c4bb527578dc70f51b
Paolo L. Scala <paolo@yoast.com>
2025-03-11 12:06:24 +0100

    Add tracking route and endpoint

---
+++ src/dashboard/infrastructure/endpoints/site-kit-usage-tracking-endpoint.php
@@ -0,0 +1,50 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Dashboard\Infrastructure\Endpoints;
+
+use Exception;
+use Yoast\WP\SEO\Dashboard\Domain\Endpoint\Endpoint_Interface;
+use Yoast\WP\SEO\Dashboard\User_Interface\Configuration\Site_Kit_Usage_Tracking_Route;
+
+/**
+ * Represents the Site Kit usage tracking endpoint.
+ */
+class Site_Kit_Usage_Tracking_Endpoint implements Endpoint_Interface {
+
+	/**
+	 * Gets the name.
+	 *
+	 * @return string
+	 */
+	public function get_name(): string {
+		return 'siteKitUsageTracking';
+	}
+
+	/**
+	 * Gets the namespace.
+	 *
+	 * @return string
+	 */
+	public function get_namespace(): string {
+		return Site_Kit_Usage_Tracking_Route::ROUTE_NAMESPACE;
+	}
+
+	/**
+	 * Gets the route.
+	 *
+	 * @throws Exception If the route prefix is not overwritten this throws.
+	 * @return string
+	 */
+	public function get_route(): string {
+		return Site_Kit_Usage_Tracking_Route::ROUTE_PREFIX;
+	}
+
+	/**
+	 * Gets the URL.
+	 *
+	 * @return string
+	 */
+	public function get_url(): string {
+		return \rest_url( $this->get_namespace() . $this->get_route() );
+	}
+}
