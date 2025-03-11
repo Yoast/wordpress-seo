8a5ee767357e23af40d418b30924ef9ef116388c
Paolo L. Scala <paolo@yoast.com>
2025-03-11 15:21:45 +0100

    Moved the search ranking compare metric skeleton loader to its own component

--- packages/js/src/dashboard/widgets/search-ranking-compare-widget.js
+++ packages/js/src/dashboard/widgets/search-ranking-compare-widget.js
@@ -5,6 +5,7 @@ import { useRemoteData } from "../services/use-remote-data";
 import { Widget, WidgetTooltip } from "./widget";
 import { SearchRankingCompareMetric } from "../components/search-ranking-compare-metric";
 import { getDifference } from "../transformers/difference";
+import { SearchRankingCompareMetricSkeletonLoader } from "../components/search-ranking-compare-metric-skeleton-loader";
 /**
  * @type {import("../index").MetricData} MetricData
  * @type {import("../index").SearchRankingCompareData} SearchRankingCompareData
@@ -16,52 +17,24 @@ import { getDifference } from "../transformers/difference";
 const SearchRankingCompareSkeletonLoader = () => {
 	return (
 		<div className="yst-flex yst-flex-col yst-justify-center yst-items-center  @6xl:yst-flex-row @6xl:yst-justify-evenly rtl:yst-flex-row-reverse ">
-			<div className="yst-flex yst-flex-col yst-relative yst-items-center yst-w-72">
-				<div className="yst-absolute yst-end-6 yst-top-2">
-					<WidgetTooltip>
-						{ __( "The number of times your website appeared in Google search results over the last 28 days.", "wordpress-seo" ) }
-					</WidgetTooltip>
-				</div>
-				<SkeletonLoader className="yst-text-center yst-text-2xl yst-font-bold yst-text-slate-900">12345</SkeletonLoader>
-				<SkeletonLoader className="yst-text-center yst-text-sm yst-mt-2">Dummy</SkeletonLoader>
-				<SkeletonLoader className="yst-text-center yst-text-sm yst-mt-2 yst-font-semibold">- 13%</SkeletonLoader>
-			</div>
+			<SearchRankingCompareMetricSkeletonLoader
+				tooltipLocalizedString={ __( "The number of times your website appeared in Google search results over the last 28 days.", "wordpress-seo" ) }
+			/>
 			<div className="yst-h-px yst-w-full yst-bg-slate-200 yst-my-6 @6xl:yst-h-20 @6xl:yst-w-px @6xl:yst-my-1" />
 
-			<div className="yst-flex yst-flex-col yst-relative yst-items-center yst-w-72">
-				<div className="yst-absolute yst-end-6 yst-top-2">
-					<WidgetTooltip>
-						{ __( "The total number of times users clicked on your website's link in Google search results over the last 28 days.", "wordpress-seo" ) }
-					</WidgetTooltip>
-				</div>
-				<SkeletonLoader className="yst-text-center yst-text-2xl yst-font-bold yst-text-slate-900">12345</SkeletonLoader>
-				<SkeletonLoader className="yst-text-center yst-text-sm yst-mt-2">Dummy</SkeletonLoader>
-				<SkeletonLoader className="yst-text-center yst-text-sm yst-mt-2 yst-font-semibold">- 13%</SkeletonLoader>
-			</div>
+			<SearchRankingCompareMetricSkeletonLoader
+				tooltipLocalizedString={ __( "The total number of times users clicked on your website's link in Google search results over the last 28 days.", "wordpress-seo" ) }
+			/>
 			<div className="yst-h-px yst-w-full yst-bg-slate-200 yst-my-6 @6xl:yst-h-20 @6xl:yst-w-px @6xl:yst-my-1" />
 
-			<div className="yst-flex yst-flex-col yst-relative yst-items-center yst-w-72">
-				<div className="yst-absolute yst-end-6 yst-top-2">
-					<WidgetTooltip>
-						{ __( "The average click-through-rate for your website over the last 28 days.", "wordpress-seo" ) }
-					</WidgetTooltip>
-				</div>
-				<SkeletonLoader className="yst-text-center yst-text-2xl yst-font-bold yst-text-slate-900">12345</SkeletonLoader>
-				<SkeletonLoader className="yst-text-center yst-text-sm yst-mt-2">Dummy</SkeletonLoader>
-				<SkeletonLoader className="yst-text-center yst-text-sm yst-mt-2 yst-font-semibold">- 13%</SkeletonLoader>
-			</div>
+			<SearchRankingCompareMetricSkeletonLoader
+				tooltipLocalizedString={ __( "The average click-through-rate for your website over the last 28 days.", "wordpress-seo" ) }
+			/>
 			<div className="yst-h-px yst-w-full yst-bg-slate-200 yst-my-6 @6xl:yst-h-20 @6xl:yst-w-px @6xl:yst-my-1" />
 
-			<div className="yst-flex yst-flex-col yst-relative yst-items-center yst-w-72">
-				<div className="yst-absolute yst-end-6 yst-top-2">
-					<WidgetTooltip>
-						{ __( "Average position is the average position of your site in search results over the last 28 days.", "wordpress-seo" ) }
-					</WidgetTooltip>
-				</div>
-				<SkeletonLoader className="yst-text-center yst-text-2xl yst-font-bold yst-text-slate-900">12345</SkeletonLoader>
-				<SkeletonLoader className="yst-text-center yst-text-sm yst-mt-2">Dummy</SkeletonLoader>
-				<SkeletonLoader className="yst-text-center yst-text-sm yst-mt-2 yst-font-semibold">- 13%</SkeletonLoader>
-			</div>
+			<SearchRankingCompareMetricSkeletonLoader
+				tooltipLocalizedString={ __( "Average position is the average position of your site in search results over the last 28 days.", "wordpress-seo" ) }
+			/>
 		</div>
 	);
 };
