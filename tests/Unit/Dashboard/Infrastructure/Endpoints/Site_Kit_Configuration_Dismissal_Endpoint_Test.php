09743256e315bfa5dc53822418436be3edaf52c0
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-03-12 10:22:34 +0100

    Add endpoint verification unit tests

---
+++ tests/Unit/Dashboard/Infrastructure/Endpoints/Site_Kit_Configuration_Dismissal_Endpoint_Test.php
@@ -0,0 +1,80 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Tests\Unit\Dashboard\Infrastructure\Endpoints;
+
+use Brain\Monkey\Functions;
+use Yoast\WP\SEO\Dashboard\Infrastructure\Endpoints\Site_Kit_Configuration_Dismissal_Endpoint;
+use Yoast\WP\SEO\Tests\Unit\TestCase;
+
+/**
+ * Test class for the Site_Kit_Configuration_Dismissal_Endpoint class.
+ *
+ * @group Endpoints
+ *
+ * @covers Yoast\WP\SEO\Dashboard\Infrastructure\Endpoints\Site_Kit_Configuration_Dismissal_Endpoint
+ *
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded
+ */
+final class Site_Kit_Configuration_Dismissal_Endpoint_Test extends TestCase {
+
+	/**
+	 * Holds the instance.
+	 *
+	 * @var Site_Kit_Configuration_Dismissal_Endpoint
+	 */
+	private $instance;
+
+	/**
+	 * Sets up the test fixtures.
+	 *
+	 * @return void
+	 */
+	public function set_up() {
+		$this->instance = new Site_Kit_Configuration_Dismissal_Endpoint();
+	}
+
+	/**
+	 * Tests if the name is the expected value.
+	 *
+	 * @covers ::get_name
+	 * @return void
+	 */
+	public function test_get_name() {
+		$this->assertSame( 'siteKitConfigurationDismissal', $this->instance->get_name() );
+	}
+
+	/**
+	 * Tests if the namespace is the expected value.
+	 *
+	 * @covers ::get_namespace
+	 * @return void
+	 */
+	public function test_get_namespace() {
+		$this->assertSame( 'yoast/v1', $this->instance->get_namespace() );
+	}
+
+	/**
+	 * Tests if the route is the expected value.
+	 *
+	 * @covers ::get_route
+	 * @return void
+	 */
+	public function test_get_route() {
+		$this->assertSame( '/site_kit_configuration_permanent_dismissal', $this->instance->get_route() );
+	}
+
+	/**
+	 * Tests if the rest url is the expected value.
+	 *
+	 * @covers ::get_url
+	 * @return void
+	 */
+	public function test_get_url() {
+		Functions\expect( 'rest_url' )
+			->once()
+			->with(
+				'yoast/v1/site_kit_configuration_permanent_dismissal'
+			)->andReturnFirstArg();
+		$this->assertSame( 'yoast/v1/site_kit_configuration_permanent_dismissal', $this->instance->get_url() );
+	}
+}
