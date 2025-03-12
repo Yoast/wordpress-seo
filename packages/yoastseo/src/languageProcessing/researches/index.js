5da2d8fda46dfe31d2117196d6bc975e1bc73d07
Vrishabhsk <vrishabhsk@gmail.com>
2025-03-12 10:12:08 +0400

    Export getFleschReadingScore and readingTime

--- packages/yoastseo/src/languageProcessing/researches/index.js
+++ packages/yoastseo/src/languageProcessing/researches/index.js
@@ -1,9 +1,13 @@
 import getLongCenterAlignedTexts from "./getLongCenterAlignedTexts";
 import wordComplexity from "./wordComplexity";
 import keyphraseDistribution from "./keyphraseDistribution";
+import getFleschReadingScore from "./getFleschReadingScore";
+import readingTime from "./readingTime";
 
 export {
 	getLongCenterAlignedTexts,
 	keyphraseDistribution,
 	wordComplexity,
+	getFleschReadingScore,
+	readingTime,
 };
