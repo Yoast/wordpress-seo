01b38aeca3eedc4b50a9d8829714f963f50e7dff
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-03-12 15:00:28 +0200

    Start adding tests for the analytics adapter

---
+++ tests/WP/Doubles/Dashboard/Infrastructure/Analytics_4/Analytics_4_Module_Mock.php
@@ -0,0 +1,17 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong
+
+namespace Yoast\WP\SEO\Tests\WP\Doubles\Dashboard\Infrastructure\Analytics_4;
+
+/**
+ * Class Search_Console_Module_Mock
+ */
+class Analytics_4_Module_Mock {
+
+	/**
+	 * Helper method so provider can be replaced with a mocked version.
+	 *
+	 * @return void
+	 */
+	public function get_data() {}
+}
