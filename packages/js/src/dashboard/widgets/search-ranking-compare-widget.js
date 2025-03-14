545f96801447df4cf28fe8858aae995990b6d2b2
Paolo L. Scala <paolo@yoast.com>
2025-03-14 15:18:33 +0100

    Fix cs

--- packages/js/src/dashboard/widgets/search-ranking-compare-widget.js
+++ packages/js/src/dashboard/widgets/search-ranking-compare-widget.js
@@ -77,8 +77,8 @@ const transformData = ( rawData ) => {
 
 	if (  rawData[ 0 ].current.average_ctr ) {
 		data.ctr = {
-			value: rawData[0].current.average_ctr,
-			delta: getDifference( rawData[0].current.average_ctr, rawData[0].previous.average_ctr),
+			value: rawData[ 0 ].current.average_ctr,
+			delta: getDifference( rawData[ 0 ].current.average_ctr, rawData[ 0 ].previous.average_ctr),
 		};
 	}
 
