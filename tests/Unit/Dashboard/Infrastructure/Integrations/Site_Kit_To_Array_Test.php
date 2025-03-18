63b06d44ea7aede0d5afa31addca62ab90f8273f
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-03-18 12:29:19 +0100

    Update to array tests.

--- tests/Unit/Dashboard/Infrastructure/Integrations/Site_Kit_To_Array_Test.php
+++ tests/Unit/Dashboard/Infrastructure/Integrations/Site_Kit_To_Array_Test.php
@@ -4,6 +4,7 @@ namespace Yoast\WP\SEO\Tests\Unit\Dashboard\Infrastructure\Integrations;
 
 use Brain\Monkey\Functions;
 use Generator;
+use WP_User;
 
 /**
  * Test class for the is_onboarded method.
@@ -21,13 +22,16 @@ final class Site_Kit_To_Array_Test extends Abstract_Site_Kit_Test {
 	 *
 	 * @dataProvider generate_site_kit_to_array_provider
 	 *
-	 * @param bool               $is_site_kit_installed If the Site Kit plugin is installed.
-	 * @param bool               $is_site_kit_activated If the Site Kit plugin is activated.
-	 * @param int | bool         $is_setup_completed    The value that is returned from the option.
-	 * @param bool               $is_consent_granted    If consent is granted to our integration.
-	 * @param bool               $is_ga_connected       If the Google analytics setup is completed.
-	 * @param bool               $is_config_dismissed   If the configuration widget is dismissed.
-	 * @param array<bool|string> $expected              The expected value.
+	 * @param bool               $is_site_kit_installed   If the Site Kit plugin is installed.
+	 * @param bool               $is_site_kit_activated   If the Site Kit plugin is activated.
+	 * @param bool               $is_consent_granted      If consent is granted to our integration.
+	 * @param bool               $is_ga_connected         If the Google analytics setup is completed.
+	 * @param bool               $is_config_dismissed     If the configuration widget is dismissed.
+	 * @param string             $access_role_needed      The needed role for using the widgets.
+	 * @param string             $access_role_user        The role the user has.
+	 * @param int                $search_console_owner_id The id of the user that owns the SC connection.
+	 * @param int                $ga_owner_id             The id of the user that owns the GA connection.
+	 * @param array<bool|string> $expected                The expected value.
 	 *
 	 * @return void
 	 */
@@ -37,13 +41,16 @@ final class Site_Kit_To_Array_Test extends Abstract_Site_Kit_Test {
 		bool $is_consent_granted,
 		bool $is_ga_connected,
 		bool $is_config_dismissed,
+		string $access_role_needed,
+		string $access_role_user,
+		int $search_console_owner_id,
+		int $ga_owner_id,
 		array $expected
 	) {
 		Functions\expect( 'file_exists' )
 			->andReturn( $is_site_kit_installed );
 		Functions\expect( 'is_plugin_active' )
 			->andReturn( $is_site_kit_activated );
-
 
 		$this->site_kit_consent_repository->expects( 'is_consent_granted' )->once()->andReturn( $is_consent_granted );
 		$this->site_kit_analytics_4_adapter->expects( 'is_connected' )->once()->andReturn( $is_ga_connected );
@@ -73,6 +80,34 @@ final class Site_Kit_To_Array_Test extends Abstract_Site_Kit_Test {
 			->with( 'update.php?action=install-plugin&plugin=google-site-kit', 'install-plugin_google-site-kit' )
 			->once()
 			->andReturn( 'update.php?action=install-plugin&plugin=google-site-kit' );
+
+		Functions\expect( 'current_user_can' )
+			->with( 'install_plugins' )
+			->once()
+			->andReturnTrue();
+
+		Functions\expect( 'get_option' )
+			->with( 'googlesitekit_dashboard_sharing' )
+			->andReturn(
+				[ 'search-console' => [ 'sharedRoles' => [ $access_role_needed ] ] ],
+				[ 'analytics-4' => [ 'sharedRoles' => [ $access_role_needed ] ] ]
+			);
+
+		Functions\expect( 'get_option' )
+			->with( 'googlesitekit_search-console_settings' )
+			->andReturn( [ 'ownerID' => $search_console_owner_id ] );
+
+		Functions\expect( 'get_option' )
+			->with( 'googlesitekit_search-console_settings' )
+			->andReturn( [ 'ownerID' => $ga_owner_id ] );
+
+		$user1        = new WP_User();
+		$user1->ID    = 1;
+		$user1->roles = [ $access_role_user ];
+
+		Functions\expect( 'wp_get_current_user' )
+			->andReturn( $user1 );
+
 		$this->assertSame( $expected, $this->instance->to_array() );
 	}
 
@@ -83,60 +118,185 @@ final class Site_Kit_To_Array_Test extends Abstract_Site_Kit_Test {
 	 */
 	public static function generate_site_kit_to_array_provider() {
 		yield 'Everything setup' => [
-			'is_site_kit_installed' => true,
-			'is_site_kit_activated' => true,
-			'is_consent_granted'    => true,
-			'is_ga_connected'       => true,
-			'is_config_dismissed'   => true,
-			'expected'              => [
-				'isInstalled'              => true,
-				'isActive'                 => true,
-				'isSetupCompleted'         => false,
-				'isConnected'              => true,
-				'isAnalyticsConnected'            => true,
-				'isFeatureEnabled'         => false,
+			'is_site_kit_installed'   => true,
+			'is_site_kit_activated'   => true,
+			'is_consent_granted'      => true,
+			'is_ga_connected'         => true,
+			'is_config_dismissed'     => true,
+			'access_role_needed'      => 'admin',
+			'access_role_user'        => 'admin',
+			'search_console_owner_id' => 1,
+			'ga_owner_id'             => 1,
+			'expected'                => [
 				'installUrl'               => 'update.php?action=install-plugin&plugin=google-site-kit',
 				'activateUrl'              => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
 				'setupUrl'                 => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'isAnalyticsConnected'     => true,
+				'isFeatureEnabled'         => false,
 				'isConfigurationDismissed' => true,
+				'capabilities'             => [
+					'installPlugins'        => true,
+					'viewSearchConsoleData' => true,
+					'viewAnalyticsData'     => true,
+				],
+				'connectionStepsStatuses'  => [
+					'isInstalled'      => true,
+					'isActive'         => true,
+					'isSetupCompleted' => false,
+					'isConsentGranted' => true,
+				],
+
 			],
 		];
 		yield 'Installed not setup' => [
-			'is_site_kit_installed' => true,
-			'is_site_kit_activated' => false,
-			'is_consent_granted'    => false,
-			'is_ga_connected'       => false,
-			'is_config_dismissed'   => true,
-			'expected'              => [
-				'isInstalled'              => true,
-				'isActive'                 => false,
-				'isSetupCompleted'         => false,
-				'isConnected'              => false,
-				'isAnalyticsConnected'            => false,
-				'isFeatureEnabled'         => false,
+			'is_site_kit_installed'   => true,
+			'is_site_kit_activated'   => false,
+			'is_consent_granted'      => false,
+			'is_ga_connected'         => false,
+			'is_config_dismissed'     => true,
+			'access_role_needed'      => 'admin',
+			'access_role_user'        => 'admin',
+			'search_console_owner_id' => 1,
+			'ga_owner_id'             => 1,
+			'expected'                => [
 				'installUrl'               => 'update.php?action=install-plugin&plugin=google-site-kit',
 				'activateUrl'              => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
 				'setupUrl'                 => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'isAnalyticsConnected'     => false,
+				'isFeatureEnabled'         => false,
 				'isConfigurationDismissed' => true,
+				'capabilities'             => [
+					'installPlugins'        => true,
+					'viewSearchConsoleData' => true,
+					'viewAnalyticsData'     => true,
+				],
+				'connectionStepsStatuses'  => [
+					'isInstalled'      => true,
+					'isActive'         => false,
+					'isSetupCompleted' => false,
+					'isConsentGranted' => false,
+				],
 			],
 		];
 		yield 'Setup but no longer installed' => [
-			'is_site_kit_installed' => false,
-			'is_site_kit_activated' => false,
-			'is_consent_granted'    => true,
-			'is_ga_connected'       => true,
-			'is_config_dismissed'   => true,
-			'expected'              => [
-				'isInstalled'              => false,
-				'isActive'                 => false,
-				'isSetupCompleted'         => false,
-				'isConnected'              => true,
-				'isAnalyticsConnected'            => true,
-				'isFeatureEnabled'         => false,
+			'is_site_kit_installed'   => false,
+			'is_site_kit_activated'   => false,
+			'is_consent_granted'      => true,
+			'is_ga_connected'         => true,
+			'is_config_dismissed'     => true,
+			'access_role_needed'      => 'admin',
+			'access_role_user'        => 'admin',
+			'search_console_owner_id' => 1,
+			'ga_owner_id'             => 1,
+			'expected'                => [
 				'installUrl'               => 'update.php?action=install-plugin&plugin=google-site-kit',
 				'activateUrl'              => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
 				'setupUrl'                 => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'isAnalyticsConnected'     => true,
+				'isFeatureEnabled'         => false,
 				'isConfigurationDismissed' => true,
+				'capabilities'             => [
+					'installPlugins'        => true,
+					'viewSearchConsoleData' => true,
+					'viewAnalyticsData'     => true,
+				],
+				'connectionStepsStatuses'  => [
+					'isInstalled'      => false,
+					'isActive'         => false,
+					'isSetupCompleted' => false,
+					'isConsentGranted' => true,
+				],
+			],
+		];
+		yield 'Setup complete not the right role' => [
+			'is_site_kit_installed'   => false,
+			'is_site_kit_activated'   => false,
+			'is_consent_granted'      => true,
+			'is_ga_connected'         => true,
+			'is_config_dismissed'     => true,
+			'access_role_needed'      => 'admin',
+			'access_role_user'        => 'nothing',
+			'search_console_owner_id' => 1,
+			'ga_owner_id'             => 1,
+			'expected'                => [
+				'installUrl'               => 'update.php?action=install-plugin&plugin=google-site-kit',
+				'activateUrl'              => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'setupUrl'                 => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'isAnalyticsConnected'     => true,
+				'isFeatureEnabled'         => false,
+
+				'isConfigurationDismissed' => true,
+				'capabilities'             => [
+					'installPlugins'        => true,
+					'viewSearchConsoleData' => false,
+					'viewAnalyticsData'     => false,
+				],
+				'connectionStepsStatuses'  => [
+					'isInstalled'      => false,
+					'isActive'         => false,
+					'isSetupCompleted' => false,
+					'isConsentGranted' => true,
+				],
+			],
+		];
+		yield 'Setup complete not the right owner but correct role' => [
+			'is_site_kit_installed'   => false,
+			'is_site_kit_activated'   => false,
+			'is_consent_granted'      => true,
+			'is_ga_connected'         => true,
+			'is_config_dismissed'     => true,
+			'access_role_needed'      => 'admin',
+			'access_role_user'        => 'admin',
+			'search_console_owner_id' => 2,
+			'ga_owner_id'             => 2,
+			'expected'                => [
+				'installUrl'               => 'update.php?action=install-plugin&plugin=google-site-kit',
+				'activateUrl'              => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'setupUrl'                 => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'isAnalyticsConnected'     => true,
+				'isFeatureEnabled'         => false,
+				'isConfigurationDismissed' => true,
+				'capabilities'             => [
+					'installPlugins'        => true,
+					'viewSearchConsoleData' => true,
+					'viewAnalyticsData'     => true,
+				],
+				'connectionStepsStatuses'  => [
+					'isInstalled'      => false,
+					'isActive'         => false,
+					'isSetupCompleted' => false,
+					'isConsentGranted' => true,
+				],
+			],
+		];
+		yield 'Setup complete not the right owner or correct role' => [
+			'is_site_kit_installed'   => false,
+			'is_site_kit_activated'   => false,
+			'is_consent_granted'      => true,
+			'is_ga_connected'         => true,
+			'is_config_dismissed'     => true,
+			'access_role_needed'      => 'admin',
+			'access_role_user'        => 'not-admin',
+			'search_console_owner_id' => 2,
+			'ga_owner_id'             => 2,
+			'expected'                => [
+				'installUrl'               => 'update.php?action=install-plugin&plugin=google-site-kit',
+				'activateUrl'              => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'setupUrl'                 => 'plugins.php?action=activate&plugin=google-site-kit/google-site-kit.php',
+				'isAnalyticsConnected'     => true,
+				'isFeatureEnabled'         => false,
+				'isConfigurationDismissed' => true,
+				'capabilities'             => [
+					'installPlugins'        => true,
+					'viewSearchConsoleData' => false,
+					'viewAnalyticsData'     => false,
+				],
+				'connectionStepsStatuses'  => [
+					'isInstalled'      => false,
+					'isActive'         => false,
+					'isSetupCompleted' => false,
+					'isConsentGranted' => true,
+				],
 			],
 		];
 	}
