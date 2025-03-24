9d8be189c0a53131ab04b5d3203f780799ae2da1
Paolo L. Scala <paolo@yoast.com>
2025-03-24 16:14:59 +0100

    Fix typo

--- tests/Unit/Dashboard/User_Interface/Tracking/Site_Kit_Usage_Tracking_Route_Check_Capabilities_Test.php
+++ tests/Unit/Dashboard/User_Interface/Tracking/Site_Kit_Usage_Tracking_Route_Check_Capabilities_Test.php
@@ -21,7 +21,7 @@ final class Site_Kit_Usage_Tracking_Route_Check_Capabilities_Test extends Abstra
 	public function test_check_capabilities() {
 		$this->capability_helper->expects( 'current_user_can' )
 			->once()
-			->with( 'wpseo_manage_options' )\zx
+			->with( 'wpseo_manage_options' )
 			->andReturn( true );
 
 		$this->assertTrue( $this->instance->check_capabilities() );
