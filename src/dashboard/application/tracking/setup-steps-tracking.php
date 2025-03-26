e11729021be40d382a4897c051063520bbd26ab7
Paolo L. Scala <paolo@yoast.com>
2025-03-26 15:51:01 +0100

    Remove the tracking data from the Site Kit class and move it to its own class

---
+++ src/dashboard/application/tracking/setup-steps-tracking.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Yoast\WP\SEO\Dashboard\Application\tracking;
+
+use Yoast\WP\SEO\Dashboard\Infrastructure\Tracking\Setup_Steps_Tracking_Repository_Interface;
+
+/**
+ * Tracks the setup steps.
+ */
+class Setup_Steps_Tracking {
+
+	/**
+	 * The setup steps tracking repository.
+	 *
+	 * @var Setup_Steps_Tracking_Repository_Interface
+	 */
+	private $setup_steps_tracking_repository;
+
+	/**
+	 * Constructs the class.
+	 *
+	 * @param Setup_Steps_Tracking_Repository_Interface $setup_steps_tracking_repository The setup steps tracking repository.
+	 */
+	public function __construct( Setup_Steps_Tracking_Repository_Interface $setup_steps_tracking_repository ) {
+		$this->setup_steps_tracking_repository = $setup_steps_tracking_repository;
+	}
+
+	/**
+	 * If the Site Kit setup widget has been loaded.
+	 *
+	 * @return string "yes" on "no".
+	 */
+	public function get_setup_widget_loaded(): string {
+		return $this->setup_steps_tracking_repository->get_setup_steps_tracking_element( 'setup_widget_loaded' );
+	}
+
+	/**
+	 * Gets the stage of the first interaction.
+	 *
+	 * @return string The stage name.
+	 */
+	public function get_first_interaction_stage(): string {
+		return $this->setup_steps_tracking_repository->get_setup_steps_tracking_element( 'first_interaction_stage' );
+	}
+
+	/**
+	 * Gets the stage of the last interaction.
+	 *
+	 * @return string The stage name.
+	 */
+	public function get_last_interaction_stage(): string {
+		return $this->setup_steps_tracking_repository->get_setup_steps_tracking_element( 'last_interaction_stage' );
+	}
+
+	/**
+	 * If the setup widget has been dismissed.
+	 *
+	 * @return string "no", "pageload", or "permanently".
+	 */
+	public function get_setup_widget_dismissed(): string {
+		return $this->setup_steps_tracking_repository->get_setup_steps_tracking_element( 'setup_widget_dismissed' );
+	}
+
+	/**
+	 * Return this object represented by a key value array.
+	 *
+	 * @return array<string> The tracking data
+	 */
+	public function to_array(): array {
+		return [
+			'setupWidgetLoaded'     => $this->get_setup_widget_loaded(),
+			'firstInteractionStage' => $this->get_first_interaction_stage(),
+			'lastInteractionStage'  => $this->get_last_interaction_stage(),
+			'setupWidgetDismissed'  => $this->get_setup_widget_dismissed(),
+		];
+	}
+}
