0cb4e4b50d848ea069504ba61896ba19d4142ebc
Vraja Das <vraja.pro@gmail.com>
2025-04-03 14:04:03 +0300

    move custom step to story

--- packages/ui-library/src/components/stepper/stories.js
+++ packages/ui-library/src/components/stepper/stories.js
@@ -1,9 +1,48 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useCallback, useContext } from "react";
 import { Stepper } from ".";
 import { component, customStep } from "./docs";
 import { Button } from "../../index";
-import { CustomStep } from "./custom-step";
+import classNames from "classnames";
+import { CheckIcon } from "@heroicons/react/solid";
 
+/**
+ * Custom step component for stepper story.
+ *
+ * @param {JSX.Node} children The step label or children.
+ * @param {boolean} isComplete Is the step complete.
+ * @param {boolean} isActive Is the step
+ *
+ * @returns {JSX.Element} The step element.
+ */
+const CustomStep = ( { children, isComplete, isActive } ) => {
+	const { addStepRef } = useContext( Stepper.Context );
+	return (
+		<div
+			ref={ addStepRef }
+			className={ classNames(
+				"yst-step",
+				isComplete ? "yst-step--complete" : "",
+				isActive ? "yst-step--active yst-text-green-700" : "" ) }
+		>
+			<div
+				className={ classNames( "yst-step__circle yst-ring-green-500",
+					isComplete ? "yst-bg-green-500 yst-ring-green-500" : "yst-bg-white",
+				) }
+			>
+				{ isComplete && <CheckIcon
+					className="yst-step__icon yst-w-4 yst-z-50"
+				/> }
+
+				<div
+					className={
+						classNames( "yst-step__icon yst-bg-green-500 yst-w-2 yst-h-2 yst-rounded-full yst-delay-500",
+							! isComplete && isActive ? "yst-opacity-100" : "yst-opacity-0" ) }
+				/>
+			</div>
+			<div className="yst-font-semibold yst-text-xxs yst-mt-3">{ children }</div>
+		</div>
+	);
+};
 
 export default {
 	title: "2) Components/Stepper",
