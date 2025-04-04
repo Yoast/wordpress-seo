60c8a140a972a04b357a9c8df99c7958dc711ef2
Martijn van der Klis <martijnvanderklis@yoast.com>
2025-04-04 16:04:59 +0200

    Reverts code that did not solve a highlighting issue with NBSP characters

--- packages/yoastseo/src/helpers/htmlEntities.js
+++ packages/yoastseo/src/helpers/htmlEntities.js
@@ -23,8 +23,6 @@ const htmlEntities = new Map( [
 
 // Regex to find all HTML entities.
 const htmlEntitiesRegex = new RegExp( "&(" + [ ...htmlEntities.keys() ].join( "|" ) + ")", "ig" );
-// Regex to find all HTML entities except for the HTML entity for the greater-than sign `gt;`.
-const entitiesWithoutGTSRegex = new RegExp( "&(amp;|lt;|quot;|apos;|ndash;|mdash;|copy;|reg;|trade;|pound;|yen;|euro;|dollar;|deg;|asymp;|ne;|nbsp;)", "ig" );
 
 // Contains special characters along with their hashed HTML entities (e.g. '#amp;' instead of '&amp;' for the ampersand character '&').
 const hashedHtmlEntities = new Map();
@@ -36,7 +34,6 @@ const hashedHtmlEntitiesRegexEnd = new RegExp( "(" + [ ...hashedHtmlEntities.key
 
 export {
 	htmlEntities,
-	entitiesWithoutGTSRegex,
 	htmlEntitiesRegex,
 	hashedHtmlEntities,
 	hashedHtmlEntitiesRegexStart,
