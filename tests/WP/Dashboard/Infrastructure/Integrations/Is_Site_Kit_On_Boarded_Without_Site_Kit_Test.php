13f30b7f6231777ed1bae1fd4a3a31240dbf77cc
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-04-10 09:38:57 +0200

    Fix wp tests.

--- tests/WP/Dashboard/Infrastructure/Integrations/Is_Site_Kit_On_Boarded_Without_Site_Kit_Test.php
+++ tests/WP/Dashboard/Infrastructure/Integrations/Is_Site_Kit_On_Boarded_Without_Site_Kit_Test.php
@@ -3,6 +3,7 @@
 namespace Yoast\WP\SEO\Tests\WP\Dashboard\Infrastructure\Integrations;
 
 use Yoast\WP\SEO\Dashboard\Infrastructure\Analytics_4\Site_Kit_Analytics_4_Adapter;
+use Yoast\WP\SEO\Dashboard\Infrastructure\Analytics_4\Site_Kit_Analytics_4_Api_Call;
 use Yoast\WP\SEO\Dashboard\Infrastructure\Integrations\Site_Kit;
 use Yoast\WP\SEO\Tests\Unit\Dashboard\Infrastructure\Configuration\Permanently_Dismissed_Site_Kit_Configuration_Repository_Fake;
 use Yoast\WP\SEO\Tests\Unit\Dashboard\Infrastructure\Configuration\Site_Kit_Consent_Repository_Fake;
@@ -36,10 +37,11 @@ final class Is_Site_Kit_On_Boarded_Without_Site_Kit_Test extends TestCase {
 	public function set_up() {
 		parent::set_up();
 
-		$site_kit_consent_repository  = new Site_Kit_Consent_Repository_Fake();
-		$configuration_repository     = new Permanently_Dismissed_Site_Kit_Configuration_Repository_Fake();
-		$site_kit_analytics_4_adapter = new Site_Kit_Analytics_4_Adapter();
-		$this->instance               = new Site_Kit( $site_kit_consent_repository, $configuration_repository, $site_kit_analytics_4_adapter );
+		$site_kit_consent_repository   = new Site_Kit_Consent_Repository_Fake();
+		$configuration_repository      = new Permanently_Dismissed_Site_Kit_Configuration_Repository_Fake();
+		$site_kit_analytics_4_api_call = new Site_Kit_Analytics_4_Api_Call();
+		$site_kit_analytics_4_adapter  = new Site_Kit_Analytics_4_Adapter( $site_kit_analytics_4_api_call );
+		$this->instance                = new Site_Kit( $site_kit_consent_repository, $configuration_repository, $site_kit_analytics_4_adapter );
 	}
 
 	/**
