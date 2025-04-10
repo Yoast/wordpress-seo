621ec65e38181542c01fde5ee5d3c3b8418dfe7f
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-04-10 09:31:21 +0200

    not use Site kit constant outside of a method.

--- src/dashboard/infrastructure/analytics-4/site-kit-analytics-4-api-call.php
+++ src/dashboard/infrastructure/analytics-4/site-kit-analytics-4-api-call.php
@@ -17,7 +17,7 @@ class Site_Kit_Analytics_4_Api_Call {
 	/**
 	 * The Analytics 4 API route path.
 	 */
-	private const ANALYTICS_DATA_REPORT_ROUTE = '/' . REST_Routes::REST_ROOT . '/modules/analytics-4/data/report';
+	private const ANALYTICS_DATA_REPORT_ROUTE = '/modules/analytics-4/data/report';
 
 	/**
 	 * Runs the internal REST api call.
@@ -27,7 +27,7 @@ class Site_Kit_Analytics_4_Api_Call {
 	 * @return WP_REST_Response
 	 */
 	public function do_request( array $api_parameters ): WP_REST_Response {
-		$request = new WP_REST_Request( 'GET', self::ANALYTICS_DATA_REPORT_ROUTE );
+		$request = new WP_REST_Request( 'GET', '/' . REST_Routes::REST_ROOT . self::ANALYTICS_DATA_REPORT_ROUTE );
 		$request->set_query_params( $api_parameters );
 		return \rest_do_request( $request );
 	}
