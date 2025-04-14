32a765b975e24a7eb0f35169cc01a83d4a6c2397
Igor <35524806+igorschoester@users.noreply.github.com>
2025-04-14 14:35:11 +0200

    Remove mock and use actual implementation

--- packages/js/tests/helpers/i18n.test.js
+++ packages/js/tests/helpers/i18n.test.js
@@ -1,43 +1,29 @@
+import { describe, expect, it, jest } from "@jest/globals";
 import { safeCreateInterpolateElement } from "../../src/helpers/i18n";
-import { createInterpolateElement } from "@wordpress/element";
-
-jest.mock( "@wordpress/element", () => ( {
-	createInterpolateElement: jest.fn(),
-} ) );
 
 describe( "safeCreateInterpolateElement", () => {
 	it( "should return the result of createInterpolateElement when no error occurs", () => {
 		const interpolatedString = "Hello, <strong>world</strong>!";
 		const conversionMap = { strong: <strong /> };
-		const mockResult = <>Hello, <strong>world</strong>!</>;
-
-		createInterpolateElement.mockReturnValue( mockResult );
 
 		const result = safeCreateInterpolateElement( interpolatedString, conversionMap );
 
-		expect( createInterpolateElement ).toHaveBeenCalledWith( interpolatedString, conversionMap );
-		expect( result ).toBe( mockResult );
+		expect( result ).toEqual( <>Hello, <strong>world</strong>!</> );
 	} );
 
 	it( "should return the original string and log an error when createInterpolateElement throws an error", () => {
-		const interpolatedString = "Hello, <strong>world</strong>!";
+		const interpolatedString = "Hello, </strong>world<strong>!";
 		const conversionMap = { strong: <strong /> };
-		const mockError = new Error( "Test error" );
-
-		createInterpolateElement.mockImplementation( () => {
-			throw mockError;
-		} );
 
 		console.error = jest.fn();
 
 		const result = safeCreateInterpolateElement( interpolatedString, conversionMap );
 
-		expect( createInterpolateElement ).toHaveBeenCalledWith( interpolatedString, conversionMap );
+		expect( result ).toBe( interpolatedString );
 		expect( console.error ).toHaveBeenCalledWith(
 			"Error in translation for:",
 			interpolatedString,
-			mockError
+			expect.any( Error )
 		);
-		expect( result ).toBe( interpolatedString );
 	} );
 } );
