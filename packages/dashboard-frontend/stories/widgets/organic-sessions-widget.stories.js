1372bb0478fdcc499b1659f79e81ce3d2428e9da
Igor <35524806+igorschoester@users.noreply.github.com>
2025-04-28 15:04:52 +0200

    Add tooltip interactions

--- packages/dashboard-frontend/stories/widgets/organic-sessions-widget.stories.js
+++ packages/dashboard-frontend/stories/widgets/organic-sessions-widget.stories.js
@@ -1,3 +1,4 @@
+import { expect, userEvent } from "@storybook/test";
 import { ComparisonMetricsDataFormatter, OrganicSessionsWidget } from "../../src";
 import { createRemoteDataProvider } from "./organic-sessions-widget/create-remote-data-provider";
 import description from "./organic-sessions-widget/description.md";
@@ -74,5 +75,32 @@ export const NoDifference = {
 	parameters: { docs: { description: { story: noDifference } } },
 	args: {
 		remoteDataProvider: createRemoteDataProvider( getCompareData( getRandom( 1 ), 0 ), getDailyData() ),
+	},
+};
+
+export const Tooltip = {
+	tags: [ "!autodocs" ],
+	play: async( { canvas } ) => {
+		// Should have a button that opens a tooltip.
+		const tooltip = canvas.getByRole( "button" );
+		await expect( tooltip ).toBeInTheDocument();
+		await expect( tooltip.classList ).toContain( "yst-tooltip-trigger" );
+
+		// Check that the tooltip is not visible by default (but still in the DOM).
+		const paragraph = canvas.getByText( "The number of organic sessions that began on your website." );
+		await expect( paragraph ).toBeInTheDocument();
+		await expect( paragraph.parentElement ).toHaveClass( "yst-hidden" );
+
+		// Check that the tooltip is visible when hovered.
+		await userEvent.hover( tooltip );
+		// Give time for the tooltip to show, it is debounced for 100ms.
+		await new Promise( ( resolve ) => setTimeout( () => resolve(), 100 ) );
+		await expect( paragraph.parentElement ).not.toHaveClass( "yst-hidden" );
+
+		// Check that the tooltip remains visible when focused, yet something else is hovered.
+		await userEvent.click( tooltip );
+		await userEvent.hover( canvas.getByRole( "heading", { name: "Organic sessions" } ) );
+		await new Promise( ( resolve ) => setTimeout( () => resolve(), 100 ) );
+		await expect( paragraph.parentElement ).not.toHaveClass( "yst-hidden" );
 	},
 };
