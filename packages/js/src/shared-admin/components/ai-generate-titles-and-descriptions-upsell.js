4f69b08b120803f976c6dc36eada96b13c78e579
Vraja Das <vraja.pro@gmail.com>
2025-05-06 16:25:31 +0300

    fix import

--- packages/js/src/shared-admin/components/ai-generate-titles-and-descriptions-upsell.js
+++ packages/js/src/shared-admin/components/ai-generate-titles-and-descriptions-upsell.js
@@ -1,7 +1,7 @@
 import { LockOpenIcon } from "@heroicons/react/outline";
 import { ArrowNarrowRightIcon } from "@heroicons/react/solid";
 import { __, sprintf } from "@wordpress/i18n";
-import { safeCreateInterpolateElement } from "../../../helpers/i18n";
+import { safeCreateInterpolateElement } from "../../helpers/i18n";
 import { Badge, Button, useModalContext } from "@yoast/ui-library";
 import PropTypes from "prop-types";
 import { OutboundLink, VideoFlow } from ".";
