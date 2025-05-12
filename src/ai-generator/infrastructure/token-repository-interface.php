37e292b142a4f7f055a84d0511371f3d6fc09b5c
Paolo L. Scala <paolo@yoast.com>
2025-05-12 14:02:53 +0200

    Add token management classes

---
+++ src/ai-generator/infrastructure/token-repository-interface.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Yoast\WP\SEO\Ai_Generator\infrastructure;
+
+/**
+ * Interface Token_Repository_Interface
+ *
+ */
+interface Token_Repository_Interface {
+	/**
+	 * Get the token for a user.
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @return string The token data.
+	 */
+	public function get_token( int $user_id ): string;
+
+	/**
+	 * Store the token for a user.
+	 *
+	 * @param int    $user_id    The user ID.
+	 * @param string $value      The token value.
+	 * @param int    $expiration The expiration time.
+	 *
+	 * @return void
+	 */
+	public function store_token( int $user_id, string $value, int $expiration ): void;
+
+	/**
+	 * Delete the token for a user.
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @return void
+	 */
+	public function delete_token( int $user_id ): void;
+}
