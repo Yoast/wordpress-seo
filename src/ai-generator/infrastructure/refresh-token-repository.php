bb07f47e01ad1ac85fa208fd5d35b38e29de42ee
Paolo L. Scala <paolo@yoast.com>
2025-05-12 14:02:53 +0200

    Add token management classes

---
+++ src/ai-generator/infrastructure/refresh-token-repository.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Yoast\WP\SEO\Ai_Generator\infrastructure;
+
+use RuntimeException;
+use Yoast\WP\SEO\Helpers\User_Helper;
+
+/**
+ * Class Refresh_Token_Repository
+ */
+class Refresh_Token_Repository implements Token_Repository_Interface {
+
+	private const META_KEY = '_yoast_wpseo_ai_generator_refresh_jwt';
+
+	/**
+	 * The user helper.
+	 *
+	 * @var User_Helper
+	 */
+	private $user_helper;
+
+	/**
+	 * Refresh_Token_Repository constructor.
+	 *
+	 * @param User_Helper $user_helper The user helper.
+	 */
+	public function __construct( User_Helper $user_helper ) {
+		$this->user_helper = $user_helper;
+	}
+
+	/**
+	 * Get the token for a user.
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @return string The token data.
+	 *
+	 * @throws RuntimeException If the token is not found or invalid.
+	 */
+	public function get_token( int $user_id ): string {
+		$access_jwt = $this->user_helper->get_meta( $user_id, self::META_KEY, true );
+		if ( ! \is_string( $access_jwt ) || $access_jwt === '' ) {
+			throw new RuntimeException( 'Unable to retrieve the access token.' );
+		}
+
+		return $access_jwt;
+	}
+
+	/**
+	 * Store the token for a user.
+	 *
+	 * @param int    $user_id    The user ID.
+	 * @param string $value      The token value.
+	 * @param int    $expiration The expiration time.
+	 *
+	 * @return void
+	 */
+	public function store_token( int $user_id, string $value, int $expiration ): void {
+		\update_user_meta(
+			$user_id,
+			self::META_KEY,
+			[
+				'value'      => $value,
+				'expiration' => $expiration,
+			]
+		); }
+
+	/**
+	 * Delete the token for a user.
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @return void
+	 */
+	public function delete_token( int $user_id ): void {
+		\delete_user_meta( $user_id, self::META_KEY ); }
+}
