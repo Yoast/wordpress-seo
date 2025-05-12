2b8f2e35ded7b62b66d9b328884f6145e727651b
Paolo L. Scala <paolo@yoast.com>
2025-05-12 14:02:43 +0200

    Add code verifier classes

---
+++ src/ai-generator/domain/code-verifier.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Yoast\WP\SEO\Ai_Generator\Domain;
+
+/**
+ * Class Code_Verifier
+ *
+ * @package Yoast\WP\SEO\Ai_Generator\Domain
+ */
+class Code_Verifier {
+
+	/**
+	 * The code.
+	 *
+	 * @var string
+	 */
+	private $code;
+
+	/**
+	 * The time the code was created.
+	 *
+	 * @var int
+	 */
+	private $created_at;
+
+	/**
+	 * Code_Verifier constructor.
+	 *
+	 * @param string $code       The code.
+	 * @param int    $created_at The time the code was created.
+	 */
+	public function __construct( string $code, int $created_at ) {
+		$this->code       = $code;
+		$this->created_at = $created_at;
+	}
+
+	/**
+	 * Get the code.
+	 *
+	 * @return string The code.
+	 */
+	public function get_code(): string {
+		return $this->code;
+	}
+
+	/**
+	 * Check if the code is expired.
+	 *
+	 * @param int $validity_in_seconds The validity of the code in seconds.
+	 *
+	 * @return bool True if the code is expired, false otherwise.
+	 */
+	public function is_expired( int $validity_in_seconds ): bool {
+		return $this->created_at < ( \time() - $validity_in_seconds );
+	}
+}
---
+++ src/ai-generator/infrastructure/code-verifier-repository.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Yoast\WP\SEO\Ai_Generator\infrastructure;
+
+use Yoast\WP\SEO\Ai_Generator\Domain\Code_Verifier;
+
+/**
+ * Class Code_Verifier_Repository
+ */
+class Code_Verifier_Repository {
+
+	/**
+	 * Store the code verifier for a user.
+	 *
+	 * @param string $code       The code verifier.
+	 * @param int    $user_id    The user ID.
+	 * @param int    $created_at The time the code was created.
+	 *
+	 * @return void
+	 */
+	public function store_code_verifier( string $code, int $user_id, int $created_at ): void {
+		\update_user_meta(
+			$user_id,
+			'yoast_wpseo_ai_generator_code_verifier',
+			[
+				'code'       => $code,
+				'created_at' => $created_at,
+			]
+		);
+	}
+
+	/**
+	 * Get the code verifier for a user.
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @return Code_Verifier|null The code verifier or null if not found.
+	 */
+	public function get_code_verifier( int $user_id ): ?Code_Verifier {
+		$data = \get_user_meta( $user_id, 'yoast_wpseo_ai_generator_code_verifier', true );
+
+		if ( ! \is_array( $data ) || empty( $data['code'] ) || empty( $data['created_at'] ) ) {
+			return null;
+		}
+
+		return new Code_Verifier( $data['code'], $data['created_at'] );
+	}
+
+	/**
+	 * Delete the code verifier for a user.
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @return void
+	 */
+	public function delete_code_verifier( int $user_id ): void {
+		\delete_user_meta( $user_id, 'yoast_wpseo_ai_generator_code_verifier' );
+	}
+}
