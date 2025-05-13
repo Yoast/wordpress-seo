995e49239f33a5d5db5211120d34cf4664d04bf4
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-05-13 16:04:08 +0300

    Build the list of post links

--- src/llms-txt/application/markdown-builders/link-lists-builder.php
+++ src/llms-txt/application/markdown-builders/link-lists-builder.php
@@ -3,6 +3,7 @@
 namespace Yoast\WP\SEO\Llms_Txt\Application\Markdown_Builders;
 
 use Yoast\WP\SEO\Llms_Txt\Domain\Markdown\Sections\Link_List;
+use Yoast\WP\SEO\Llms_Txt\Infrastructure\Content_Types_Collector;
 
 
 /**
@@ -11,13 +12,27 @@ use Yoast\WP\SEO\Llms_Txt\Domain\Markdown\Sections\Link_List;
 class Link_Lists_Builder {
 
 	/**
+	 * The content types collector.
+	 *
+	 * @var Content_Types_Collector
+	 */
+	private $content_types_collector;
+
+	/**
+	 * Constructs the class.
+	 *
+	 * @param Content_Types_Collector $content_types_collector The content types collector.
+	 */
+	public function __construct( Content_Types_Collector $content_types_collector ) {
+		$this->content_types_collector = $content_types_collector;
+	}
+
+	/**
 	 * Builds the link list sections.
 	 *
 	 * @return Link_List[] The link list sections.
 	 */
 	public function build_link_lists(): array {
-		return [
-			new Link_List( 'Blog posts', [] ),
-		];
+		return $this->content_types_collector->get_content_types_lists();
 	}
 }
