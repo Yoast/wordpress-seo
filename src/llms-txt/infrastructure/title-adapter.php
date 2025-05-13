5dc663863f8353e7cdb4b2e615a9205e9f51027f
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-05-13 12:56:56 +0300

    Don't show site tagline if it's the default one

--- src/llms-txt/infrastructure/title-adapter.php
+++ src/llms-txt/infrastructure/title-adapter.php
@@ -4,6 +4,7 @@
 namespace Yoast\WP\SEO\Llms_Txt\Infrastructure;
 
 use Yoast\WP\SEO\Llms_Txt\Domain\Markdown\Sections\Title;
+use Yoast\WP\SEO\Services\Health_Check\Default_Tagline_Runner;
 
 /**
  * The adapter of the title.
@@ -11,11 +12,32 @@ use Yoast\WP\SEO\Llms_Txt\Domain\Markdown\Sections\Title;
 class Title_Adapter {
 
 	/**
+	 * The default tagline runner.
+	 *
+	 * @var Default_Tagline_Runner
+	 */
+	private $default_tagline_runner;
+
+	/**
+	 * Class constructor.
+	 *
+	 * @param Default_Tagline_Runner $default_tagline_runner The default tagline runner.
+	 */
+	public function __construct(
+		Default_Tagline_Runner $default_tagline_runner
+	) {
+		$this->default_tagline_runner = $default_tagline_runner;
+	}
+
+	/**
 	 * Gets the title.
 	 *
 	 * @return string The title.
 	 */
 	public function get_title(): Title {
-		return new Title( \get_bloginfo( 'name' ), \get_bloginfo( 'description' ) );
+		$this->default_tagline_runner->run();
+		$tagline = ( $this->default_tagline_runner->is_successful() ? \get_bloginfo( 'description' ) : '' );
+
+		return new Title( \get_bloginfo( 'name' ), $tagline );
 	}
 }
