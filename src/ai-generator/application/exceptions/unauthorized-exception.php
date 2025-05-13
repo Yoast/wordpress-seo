d99c658f4c31fa2233d9caa58b64f044f060a99a
Paolo L. Scala <paolo@yoast.com>
2025-05-13 15:56:59 +0200

    Add remote request exceptions

---
+++ src/ai-generator/application/exceptions/unauthorized-exception.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Yoast\WP\SEO\AI_Generator\Application\Exceptions;
+
+/**
+ * Class to manage a 401 - unauthorized response.
+ */
+class Unauthorized_Exception extends Remote_Request_Exception {
+
+}
