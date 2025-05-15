f6c0e17ba34ff2c22ccbc9230a1f174df4020968
aidamarfuaty <aida@yoast.com>
2025-05-15 17:14:55 +0200

    Delete obsolete matchStringWithRegex helper and adjust imageInText.js

--- packages/yoastseo/src/languageProcessing/helpers/image/imageInText.js
+++ packages/yoastseo/src/languageProcessing/helpers/image/imageInText.js
@@ -1,19 +1,19 @@
-/** @module stringProcessing/imageInText */
-
-import matchStringWithRegex from "../regex/matchStringWithRegex.js";
-
 /**
- * @returns {object} A precompiled regex for recognizing self closing image tags.
+ * @returns {object} A precompiled regex for recognizing self-closing image tags.
  */
 export const imageRegex = new RegExp( "<img(?:[^>]+)?>(</img>)*", "ig" );
 
 
 /**
- * Checks the text for images.
+ * Retrieves all image tags from a given text string.
  *
- * @param {string} text The text string to check for images
- * @returns {Array} Array containing all types of found images
+ * @param {string} text The text string to check for images.
+ * @returns {Array} An Array containing all types of found images.
  */
 export default function( text ) {
-	return matchStringWithRegex( text, imageRegex );
+	// Early return if the text is not a string.
+	if ( typeof text !== "string" ) {
+		return [];
+	}
+	return text.match( imageRegex ) || [];
 }
