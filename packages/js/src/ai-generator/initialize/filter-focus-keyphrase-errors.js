a584aa86d2a5a92f0eba6fa3c31b808713447570
Vraja Das <vraja.pro@gmail.com>
2025-05-16 11:08:17 +0300

    add initialize folder with methods from premium

---
+++ packages/js/src/ai-generator/initialize/filter-focus-keyphrase-errors.js
@@ -0,0 +1,36 @@
+import { isConsideredEmpty } from "../helpers";
+import { select as wpSelect } from "@wordpress/data";
+import { STORE_NAME_AI } from "../constants";
+
+let hasInteractedWithFeature = false;
+/**
+ * Used to keep track of whether the user interacted with this feature.
+ * @returns {void}
+ */
+export const updateInteractedWithFeature = () => {
+	if ( hasInteractedWithFeature ) {
+		return;
+	}
+	hasInteractedWithFeature = true;
+};
+
+/**
+ * Adds a focus keyphrase error, if applicable.
+ *
+ * @param {string[]} errors The current errors.
+ * @param {string} focusKeyphrase The current focus keyphrase.
+ *
+ * @returns {string[]} The errors.
+ */
+export const filterFocusKeyphraseErrors = ( errors, focusKeyphrase ) => {
+	if ( ! hasInteractedWithFeature || ! wpSelect( STORE_NAME_AI ).selectHasAiGeneratorConsent() ) {
+		// No interaction or consent: don't adjust the errors.
+		return errors;
+	}
+
+	if ( isConsideredEmpty( focusKeyphrase ) ) {
+		errors.push( __( "Please enter a valid focus keyphrase.", "wordpress-seo-premium" ) );
+	}
+
+	return errors;
+};
---
+++ packages/js/src/ai-generator/initialize/initialize-prompt-content.js
@@ -0,0 +1,44 @@
+import { preparePromptContent } from "../helpers";
+import { dispatch, select as wpSelect, subscribe } from "@wordpress/data";
+import { debounce, get, noop } from "lodash";
+import { STORE_NAME_AI } from "../constants";
+
+/**
+ * Creates an updater function: updates the prompt content, which we call on load and whenever the editor content changes.
+ * @returns {function} The updater function.
+ */
+const updatePromptContent = () => {
+	const { setPromptContent } = dispatch( STORE_NAME_AI );
+	return () => {
+		preparePromptContent( setPromptContent );
+	};
+};
+
+/**
+ * Creates a subscriber function: a watcher on getEditorDataContent from the yoast-seo/editor store.
+ * @returns {function} The watcher function.
+ */
+const createSubscriber = () => {
+    const { getEditorDataContent } = wpSelect( "yoast-seo/editor" );
+    const createWatcher = get( window, "yoast.editorModules.helpers.createWatcher", noop );
+    const updater = updatePromptContent();
+
+    // Force an initial update after 1.5 seconds.
+    setTimeout( updater, 1500 );
+
+    return createWatcher( getEditorDataContent, updater );
+};
+
+/**
+ * Initializes the prompt content: set the initial prompt content on load and subscribes to updates to the editor content.
+ * @returns {void}
+ */
+export const initializePromptContent = () => {
+	// Run the updater on load.
+	updatePromptContent();
+
+	// Subscribe to changes to the editor content afterward.
+	// Here, we delay execution by 1.5 seconds for any change, and force execution after 3 seconds.
+	// We do this similarly when fetching the results for the insights tab.
+	subscribe( debounce( createSubscriber(), 1500, { maxWait: 3000 } ) );
+};
