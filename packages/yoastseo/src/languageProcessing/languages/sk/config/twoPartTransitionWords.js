ed96ade5e69bbbb189bc0685730b47ae6311eca5
Martijn van der Klis <martijnvanderklis@yoast.com>
2025-05-19 11:00:42 +0200

    Fixes an error in the configuration of the Slovak two-part transition words

--- packages/yoastseo/src/languageProcessing/languages/sk/config/twoPartTransitionWords.js
+++ packages/yoastseo/src/languageProcessing/languages/sk/config/twoPartTransitionWords.js
@@ -4,7 +4,6 @@
  * Returns an array with two-part transition words to be used by the assessments.
  * @returns {Array} The array filled with two-part transition words.
  */
-export default [ [ "buď, buď" ], [ "buď, alebo" ], [ "ani, ani" ], [ "aj, aj" ], [ "tak, ako" ], [ "nielenže, lež aj" ],
-	[ "nielen, lež aj" ], [ "nielen, lež i" ], [ "nielenže, lež ai" ], [ "či, alebo" ], [ "i, i" ], [ "nielen, ale i" ],
-	[ "síce, ale" ] ];
-
+export default [ [ "buď", "buď" ], [ "buď", "alebo" ], [ "ani", "ani" ], [ "aj", "aj" ], [ "tak", "ako" ], [ "nielenže", "lež aj" ],
+	[ "nielen", "lež aj" ], [ "nielen", "lež i" ], [ "nielenže", "lež ai" ], [ "či", "alebo" ], [ "i", "i" ], [ "nielen", "ale i" ],
+	[ "síce", "ale" ] ];
