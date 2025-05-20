bb6901b6f62fa9549ea4fc5b903100ff4c6dc5d1
Paolo L. Scala <paolo@yoast.com>
2025-05-20 15:29:23 +0200

    Move the handling to the consent route to avoid circular dependency with the Token:Manager

--- src/ai-generator/application/consent-handler.php
+++ src/ai-generator/application/consent-handler.php
@@ -2,14 +2,6 @@
 
 namespace Yoast\WP\SEO\AI_Generator\Application;
 
-use RuntimeException;
-use Yoast\WP\SEO\AI_Generator\Domain\Exceptions\Bad_Request_Exception;
-use Yoast\WP\SEO\AI_Generator\Domain\Exceptions\Internal_Server_Error_Exception;
-use Yoast\WP\SEO\AI_Generator\Domain\Exceptions\Not_Found_Exception;
-use Yoast\WP\SEO\AI_Generator\Domain\Exceptions\Payment_Required_Exception;
-use Yoast\WP\SEO\AI_Generator\Domain\Exceptions\Request_Timeout_Exception;
-use Yoast\WP\SEO\AI_Generator\Domain\Exceptions\Service_Unavailable_Exception;
-use Yoast\WP\SEO\AI_Generator\Domain\Exceptions\Too_Many_Requests_Exception;
 use Yoast\WP\SEO\Helpers\User_Helper;
 
 /**
@@ -17,14 +9,6 @@ use Yoast\WP\SEO\Helpers\User_Helper;
  * Handles the consent given or revoked by the user.
  */
 class Consent_Handler {
-
-	/**
-	 * The token manager instance.
-	 *
-	 * @var Token_Manager
-	 */
-	private $token_manager;
-
 	/**
 	 * Holds the user helper instance.
 	 *
@@ -35,11 +19,9 @@ class Consent_Handler {
 	/**
 	 * Class constructor.
 	 *
-	 * @param Token_Manager $token_manager The token manager.
 	 * @param User_Helper $user_helper The user helper.
 	 */
-	public function __construct( Token_Manager $token_manager, User_Helper $user_helper ) {
-		$this->token_manager = $token_manager;
+	public function __construct( User_Helper $user_helper ) {
 		$this->user_helper = $user_helper;
 	}
 
@@ -63,36 +45,5 @@ class Consent_Handler {
 	 */
 	public function grant_consent( int $user_id ) {
 		$this->user_helper->update_meta( $user_id, '_yoast_wpseo_ai_consent', true );
-	}
-
-	/**
-	 * Stores the consent given or revoked by the user.
-	 *
-	 * @param int  $user_id The user ID.
-	 * @param bool $consent Whether the consent has been given.
-	 *
-	 * @return void
-	 *
-	 * @throws Bad_Request_Exception Bad_Request_Exception.
-	 * @throws Forbidden_Exception Forbidden_Exception.
-	 * @throws Internal_Server_Error_Exception Internal_Server_Error_Exception.
-	 * @throws Not_Found_Exception Not_Found_Exception.
-	 * @throws Payment_Required_Exception Payment_Required_Exception.
-	 * @throws Request_Timeout_Exception Request_Timeout_Exception.
-	 * @throws Service_Unavailable_Exception Service_Unavailable_Exception.
-	 * @throws Too_Many_Requests_Exception Too_Many_Requests_Exception.
-	 * @throws RuntimeException Unable to retrieve the access token.
-	 */
-	public function handle( int $user_id, bool $consent ): void {
-		if ( $consent ) {
-			// Store the consent at user level.
-			$this->grant_consent( $user_id );
-		}
-		else {
-			$this->token_manager->token_invalidate( $user_id );
-
-			// Delete the consent at user level.
-			$this->revoke_consent( $user_id );
-		}
 	}
 }
