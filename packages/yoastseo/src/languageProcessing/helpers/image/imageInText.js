3f09ec8c941b9a313329c20c231a8a0cc33a7247
Martijn van der Klis <martijnvanderklis@yoast.com>
2025-05-21 13:37:05 +0200

    Updates JSDoc

--- packages/yoastseo/src/languageProcessing/helpers/image/imageInText.js
+++ packages/yoastseo/src/languageProcessing/helpers/image/imageInText.js
@@ -1,14 +1,13 @@
 /**
- * @returns {object} A precompiled regex for recognizing self-closing image tags.
+ * @returns {RegExp} A precompiled regex for recognizing self-closing image tags.
  */
 export const imageRegex = new RegExp( "<img(?:[^>]+)?>(</img>)*", "ig" );
-
 
 /**
  * Retrieves all image tags from a given text string.
  *
  * @param {string} text The text string to check for images.
- * @returns {Array} An Array containing all types of found images.
+ * @returns {RegExpMatchArray|[]} An array containing all types of found images.
  */
 export default function( text ) {
 	// Early return if the text is not a string.
