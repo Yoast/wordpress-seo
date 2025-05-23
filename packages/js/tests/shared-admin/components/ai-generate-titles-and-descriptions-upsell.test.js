1a8bbaf4cc411297837bdb69808e26141922e9d3
Vraja Das <vraja.pro@gmail.com>
2025-05-23 13:25:31 +0300

    add test to the try for free button and the limit reached alert

--- packages/js/tests/shared-admin/components/ai-generate-titles-and-descriptions-upsell.test.js
+++ packages/js/tests/shared-admin/components/ai-generate-titles-and-descriptions-upsell.test.js
@@ -36,4 +36,17 @@ describe( "AiGenerateTitlesAndDescriptionsUpsell", () => {
 		expect( getByText( "Custom new to text" ) ).toBeInTheDocument();
 		expect( getByText( "Custom bundle note" ) ).toBeInTheDocument();
 	} );
+
+	it( "should call setTryAi when the 'Try for free' button is clicked", () => {
+		const setTryAi = jest.fn();
+		const { getByText } = render( <AiGenerateTitlesAndDescriptionsUpsell { ...props } setTryAi={ setTryAi } /> );
+		getByText( "Try for free" ).click();
+		expect( setTryAi ).toHaveBeenCalled();
+	} );
+
+	it( "should show the alert when isLimitReached is true and the 'try for free' button is not rendered", () => {
+		const { getByText, queryByText } = render( <AiGenerateTitlesAndDescriptionsUpsell { ...props } isLimitReached={ true } /> );
+		expect( getByText( "Oh no! Its seems like you're out of free Sparks. Keep the momentum going, unlock unlimited sparks with Yoast SEO Premium!" ) ).toBeInTheDocument();
+		expect( queryByText( "Try for free" ) ).not.toBeInTheDocument();
+	} );
 } );
