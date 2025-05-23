f51f8bfc2a137e758dca48091aabe2000196b9ab
Vraja Das <vraja.pro@gmail.com>
2025-05-23 12:47:13 +0300

    fix tests for missing TextDecoder in GradientButton

--- packages/js/tests/setupTests.js
+++ packages/js/tests/setupTests.js
@@ -71,3 +71,8 @@ global.HTMLCanvasElement.prototype.getContext = function( type ) {
 	}
 	return null;
 };
+
+// Add TextEncoder and TextDecoder from Node's util module for the GradientButton.
+const util = require('util');
+global.TextEncoder = util.TextEncoder;
+global.TextDecoder = util.TextDecoder;
