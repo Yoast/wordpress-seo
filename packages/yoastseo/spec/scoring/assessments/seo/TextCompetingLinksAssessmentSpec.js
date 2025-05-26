d0d76ae468a28ff79192067b7033c5570aa9826d
marinakoleva <marina@yoast.com>
2025-05-26 12:54:42 +0200

    makes competing links assessment available in the analysis from the get-go

--- packages/yoastseo/spec/scoring/assessments/seo/TextCompetingLinksAssessmentSpec.js
+++ packages/yoastseo/spec/scoring/assessments/seo/TextCompetingLinksAssessmentSpec.js
@@ -8,7 +8,7 @@ import buildTree from "../../../specHelpers/parse/buildTree";
 const morphologyData = getMorphologyData( "en" );
 
 describe( "An assessment for competing links in the text", function() {
-	it( "returns a 'bad' score if a paper is referring to another paper with the same keyword", function() {
+	it( "returns a 'bad' score if a paper includes a link which uses the keyphrase as its anchor text", function() {
 		const attributes = {
 			keyword: "keyword",
 			permalink: "http://example.org/keyword",
@@ -26,38 +26,101 @@ describe( "An assessment for competing links in the text", function() {
 		);
 
 		expect( result.getScore() ).toBe( 2 );
-		expect( result.getText() ).toBe( "<a href='https://yoa.st/34l' target='_blank'>Link keyphrase</a>: " +
-			"You're linking to another page with the words you want this page to rank for. " +
-			"<a href='https://yoa.st/34m' target='_blank'>Don't do that</a>!" );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/34l' target='_blank'>Competing links</a>: " +
+			"You have a link which uses your keyphrase or synonym as its anchor text. " +
+			"<a href='https://yoa.st/34m' target='_blank'>Fix that</a>!" );
 	} );
 
-	it( "returns the score when the paper is empty", function() {
-		const paper = new Paper( "" );
-		const result = new TextCompetingLinksAssessment( {} ).getResult( paper, new DefaultResearcher( paper ) );
-		expect( result.score ).toBe( 0 );
+	it( "returns a 'bad' score if a paper includes a link which uses the synonym as its anchor text", function() {
+		const attributes = {
+			keyword: "dingo",
+			synonyms: "keywords",
+			permalink: "http://example.org/keyword",
+		};
+
+		const paper = new Paper( "hello, here is a link with my <a href='http://example.com/keyword'>keyword</a>", attributes );
+		const researcher = new EnglishResearcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+
+		buildTree( paper, researcher );
+
+		const result = new TextCompetingLinksAssessment().getResult(
+			paper,
+			researcher
+		);
+
+		expect( result.getScore() ).toBe( 2 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/34l' target='_blank'>Competing links</a>: " +
+			"You have a link which uses your keyphrase or synonym as its anchor text. " +
+			"<a href='https://yoa.st/34m' target='_blank'>Fix that</a>!" );
 	} );
 
-	it( "is not applicable for papers without text", function() {
-		const paper = new Paper( "", { keyword: "some keyword" } );
-		const isApplicableResult = new TextCompetingLinksAssessment().isApplicable( paper );
-		expect( isApplicableResult ).toBe( false );
+	it( "returns a good score if a paper includes a link which uses the keyphrase plus other words as its anchor text", function() {
+		const attributes = {
+			keyword: "keywords",
+			permalink: "https://example.org/keyword",
+		};
+
+		const paper = new Paper( "hello, here is a link with my <a href='https://example.com/keyword'>keywords and baklava</a>", attributes );
+		const researcher = new EnglishResearcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+
+		buildTree( paper, researcher );
+
+		const result = new TextCompetingLinksAssessment().getResult(
+			paper,
+			researcher
+		);
+
+		expect( result.score ).toBe( 8 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/34l' target='_blank'>Competing links</a>: " +
+			"There are no links which use your keyphrase or synonym as their anchor text. Nice!" );
 	} );
 
-	it( "is not applicable for papers without keyword", function() {
-		const paper = new Paper( "some text", { keyword: "" } );
-		const isApplicableResult = new TextCompetingLinksAssessment().isApplicable( paper );
-		expect( isApplicableResult ).toBe( false );
+	it( "returns a good score if a paper includes a link whose anchor text doesn't include the keyphrase", function() {
+		const attributes = {
+			keyword: "keywords",
+			permalink: "https://example.org/keyword",
+		};
+
+		const paper = new Paper( "hello, here is a link with my <a href='https://example.com/keyword'>baklava</a>", attributes );
+		const researcher = new EnglishResearcher( paper );
+		researcher.addResearchData( "morphology", morphologyData );
+
+		buildTree( paper, researcher );
+
+		const result = new TextCompetingLinksAssessment().getResult(
+			paper,
+			researcher
+		);
+
+		expect( result.score ).toBe( 8 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/34l' target='_blank'>Competing links</a>: " +
+			"There are no links which use your keyphrase or synonym as their anchor text. Nice!" );
 	} );
 
-	it( "is not applicable for papers without keyword and text", function() {
+	it( "returns a good score when the paper is empty and no keyphrase has been set", function() {
 		const paper = new Paper( "", { keyword: "" } );
-		const isApplicableResult = new TextCompetingLinksAssessment().isApplicable( paper );
-		expect( isApplicableResult ).toBe( false );
+		const result = new TextCompetingLinksAssessment( {} ).getResult( paper, new DefaultResearcher( paper ) );
+		expect( result.score ).toBe( 8 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/34l' target='_blank'>Competing links</a>: " +
+			"There are no links which use your keyphrase or synonym as their anchor text. Nice!" );
 	} );
 
-	it( "is applicable for papers with keyword and text", function() {
-		const paper = new Paper( "some text", { keyword: "keyword" } );
-		const isApplicableResult = new TextCompetingLinksAssessment().isApplicable( paper );
-		expect( isApplicableResult ).toBe( true );
+	it( "returns a good score when the paper is empty and there is a keyphrase", function() {
+		const paper = new Paper( "", { keyword: "some keyword" } );
+		const result = new TextCompetingLinksAssessment( {} ).getResult( paper, new DefaultResearcher( paper ) );
+		expect( result.score ).toBe( 8 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/34l' target='_blank'>Competing links</a>: " +
+			"There are no links which use your keyphrase or synonym as their anchor text. Nice!" );
+	} );
+
+	it( "returns a good score when the paper is empty and there is a synonym but no keyphrase", function() {
+		// Note that if the keyphrase is not set, the paper is not assessed, as per getAnchorsWithKeyphrase, and a good result is expected.
+		const paper = new Paper( "", { keyword: "", synonyms: "dingo" } );
+		const result = new TextCompetingLinksAssessment( {} ).getResult( paper, new DefaultResearcher( paper ) );
+		expect( result.score ).toBe( 8 );
+		expect( result.getText() ).toBe( "<a href='https://yoa.st/34l' target='_blank'>Competing links</a>: " +
+			"There are no links which use your keyphrase or synonym as their anchor text. Nice!" );
 	} );
 } );
