20867940d9ee73bff130e57db34a6b60a5eaa489
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-05-27 11:58:16 +0200

    Cleanup tests.

---
+++ tests/Unit/Llms_Txt/User_Interface/Cleanup_Llms_Txt_On_Deactivation/Maybe_Remove_Llms_File_Test.php
@@ -0,0 +1,26 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+namespace Yoast\WP\SEO\Tests\Unit\Llms_Txt\User_Interface\Cleanup_Llms_Txt_On_Deactivation;
+
+/**
+ * Tests the maybe_remove_llms_file.
+ *
+ * @group llms.txt
+ *
+ * @covers Yoast\WP\SEO\Llms_Txt\User_Interface\Cleanup_Llms_Txt_On_Deactivation::maybe_remove_llms_file
+ */
+final class Maybe_Remove_Llms_File_Test extends Abstract_Cleanup_Llms_Txt_On_Deactivation_Test {
+
+	/**
+	 * Tests the registration of the hooks.
+	 *
+	 * @covers Cleanup_Llms_Txt_On_Deactivation::maybe_remove_llms_file
+	 *
+	 * @return void
+	 */
+	public function test_register_hooks() {
+		$this->command_handler->expects( 'handle' )->once();
+		$this->cron_scheduler->expects( 'unschedule_llms_txt_population' )->once();
+		$this->instance->maybe_remove_llms_file();
+	}
+}
