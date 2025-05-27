be216de2cd00dcea5c2eb2c231318fdb596bc7f6
Paolo L. Scala <paolo@yoast.com>
2025-05-27 12:07:04 +0200

    Introduce interface

---
+++ src/ai-authorization/infrastructure/code-verifier-user-meta-repository-interface.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Yoast\WP\SEO\AI_Authorization\Infrastructure;
+
+use RuntimeException;
+use Yoast\WP\SEO\AI_Authorization\Domain\Code_Verifier;
+
+interface Code_Verifier_User_Meta_Repository_Interface {
+
+	/**
+	 * Get the verification code for a user.
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @throws RuntimeException If the code verifier is not found or has expired.
+	 *@return Code_Verifier The verification code or null if not found.
+	 *
+	 */
+	public function get_code_verifier( int $user_id ): ?Code_Verifier;
+
+	/**
+	 * Store the verification code for a user.
+	 *
+	 * @param string $code       The verification code.
+	 * @param int    $user_id    The user ID.
+	 * @param int    $created_at The time the code was created.
+	 *
+	 * @return void
+	 */
+	public function store_code_verifier( string $code, int $user_id, int $created_at ): void;
+
+	/**
+	 * Delete the verification code for a user.
+	 *
+	 * @param int $user_id The user ID.
+	 *
+	 * @return void
+	 */
+	public function delete_code_verifier( int $user_id ): void;
+}
