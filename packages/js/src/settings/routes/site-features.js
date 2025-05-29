fe7ae1f6be52ce44413696f8ffed458adedeb9b3
Vraja Das <vraja.pro@gmail.com>
2025-05-29 11:30:35 +0300

    enable yoast ai feature card

--- packages/js/src/settings/routes/site-features.js
+++ packages/js/src/settings/routes/site-features.js
@@ -234,7 +234,7 @@ const SiteFeatures = () => {
 								cardId="card-wpseo-enable_ai_generator"
 								inputId="input-wpseo-enable_ai_generator"
 								imageSrc="/images/ai-generator.png"
-								isPremiumFeature={ true }
+								isPremiumFeature={ false }
 								hasPremiumBadge={ false }
 								isBetaFeature={ true }
 								isPremiumLink="https://yoa.st/get-ai-generator"
