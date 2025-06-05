a20d3b449c4fbed79fb3c109d750e6fa28fd2edb
Igor <35524806+igorschoester@users.noreply.github.com>
2025-06-05 16:24:35 +0200

    Add AI Free Sparks option/route
    Incomplete tests for the route itself

---
+++ tests/Unit/AI_Free_Sparks/Infrastructure/Endpoints/Free_Sparks_Endpoint/Get_Namespace_Test.php
@@ -0,0 +1,23 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+namespace Yoast\WP\SEO\Tests\Unit\AI_Free_Sparks\Infrastructure\Endpoints\Free_Sparks_Endpoint;
+
+/**
+ * Tests the Free_Sparks_Endpoint get_namespace method.
+ *
+ * @group  ai-free-sparks
+ *
+ * @covers \Yoast\WP\SEO\AI_Free_Sparks\Infrastructure\Endpoints\Free_Sparks_Endpoint::get_namespace
+ */
+final class Get_Namespace_Test extends Abstract_Free_Sparks_Endpoint_Test {
+
+	/**
+	 * Tests the get_namespace method.
+	 *
+	 * @return void
+	 */
+	public function test_get_namespace() {
+		$this->assertSame( 'yoast/v1', $this->instance->get_namespace() );
+	}
+}
