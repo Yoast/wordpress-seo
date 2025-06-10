9d4409ae969c16a9874ca7cd7e2e4c63226e8dc8
Vraja Das <vraja.pro@gmail.com>
2025-06-10 15:47:16 +0300

    restore generator and fix renaming

--- packages/js/src/ai-generator/store/free-sparks.js
+++ packages/js/src/ai-generator/store/free-sparks.js
@@ -12,24 +12,43 @@ const slice = createSlice( {
 		endpoint: "yoast/v1/ai/free_sparks",
 	},
 	reducers: {
+		setFreeSparks: ( state, { payload = true } ) => {
+			state.isFreeSparks = payload;
+		},
 		setFreeSparksEndpoint: ( state, { payload } ) => {
 			state.endpoint = payload;
 		},
-		startFreeSparks: ( state ) => {
+	},
+	extraReducers: ( builder ) => {
+		builder.addCase( `${ START_FREE_SPARKS_ACTION_NAME }`, ( state ) => {
 			state.isFreeSparks = true;
-		},
+		} );
 	},
 } );
 
-export const getInitialTrial = slice.getInitialState;
+export const getInitialFreeSparks = slice.getInitialState;
 
 export const freeSparksSelectors = {
 	selectFreeSparksEndpoint: state => get( state, [ FREE_SPARKS_NAME, "endpoint" ], slice.getInitialState().endpoint ),
 	selectIsFreeSparks: state => get( state, [ FREE_SPARKS_NAME, "isFreeSparks" ], slice.getInitialState().isFreeSparks ),
 };
 
+/**
+ * @param {string} endpoint The endpoint to start the freeSparks from.
+ * @returns {Object} Success or error action object.
+ */
+export function* startFreeSparks( { endpoint } ) {
+	try {
+		yield{ type: START_FREE_SPARKS_ACTION_NAME, payload: endpoint };
+	} catch ( error ) {
+		console.error( "Error starting free sparks:", error );
+	}
+	return { type: `${ START_FREE_SPARKS_ACTION_NAME }` };
+}
+
 export const freeSparksActions = {
 	...slice.actions,
+	startFreeSparks,
 };
 
 export const freeSparksControls = {
