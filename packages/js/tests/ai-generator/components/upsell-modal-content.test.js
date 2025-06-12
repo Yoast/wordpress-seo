7c13e5dadca4494761b7cab4e28cf1de21bebfa3
Vraja Das <vraja.pro@gmail.com>
2025-06-12 16:58:55 +0300

    fix tests

--- packages/js/tests/ai-generator/components/upsell-modal-content.test.js
+++ packages/js/tests/ai-generator/components/upsell-modal-content.test.js
@@ -13,10 +13,11 @@ jest.mock( "@wordpress/data", () => {
 } );
 
 describe( "UpsellModalContent", () => {
-	let startFreeSparksMock;
+	let activateFreeSparksMock;
 	let setWistiaEmbedPermissionMock;
+	const setDisplayMock = jest.fn();
 	beforeEach( () => {
-		startFreeSparksMock = jest.fn();
+		activateFreeSparksMock = jest.fn();
 		setWistiaEmbedPermissionMock = jest.fn();
 
 		useDispatch.mockImplementation( storeName => {
@@ -24,7 +25,7 @@ describe( "UpsellModalContent", () => {
 				return { setWistiaEmbedPermission: setWistiaEmbedPermissionMock };
 			}
 			if ( storeName === "yoast-seo/ai-generator" ) {
-				return { startFreeSparks: startFreeSparksMock };
+				return { activateFreeSparks: activateFreeSparksMock };
 			}
 			return {};
 		} );
@@ -50,13 +51,13 @@ describe( "UpsellModalContent", () => {
 				if ( storeName === "yoast-seo/ai-generator" ) {
 					return {
 						isUsageCountLimitReached: () => false,
-						selectFreeSparksStartEndpoint: () => "/free-sparks-endpoint",
+						selectFreeSparksActiveEndpoint: () => "/free-sparks-endpoint",
 					};
 				}
 				return {};
 			} );
 		} );
-		const { getByText } = render( <UpsellModalContent /> );
+		const { getByText } = render( <UpsellModalContent setDisplay={ setDisplayMock } /> );
 		expect( getByText( "Let AI do some of the thinking for you and help you save time. Get high-quality suggestions for titles and meta descriptions to make your content rank high and look good on social media." ) ).toBeInTheDocument();
 		expect( getByText( "Yoast SEO Premium" ) ).toBeInTheDocument();
 		expect( getByText( "Unlock with Yoast SEO Premium" ) ).toBeInTheDocument();
@@ -85,16 +86,17 @@ describe( "UpsellModalContent", () => {
 				if ( storeName === "yoast-seo/ai-generator" ) {
 					return {
 						isUsageCountLimitReached: () => false,
-						selectFreeSparksStartEndpoint: () => "/free-sparks-endpoint",
+						selectFreeSparksActiveEndpoint: () => "/free-sparks-endpoint",
 					};
 				}
 				return {};
 			} );
 		} );
-		const { getByText } = render( <UpsellModalContent /> );
+		const { getByText } = render( <UpsellModalContent setDisplay={ setDisplayMock } /> );
 		const tryButton = getByText( "Try for free" );
 		fireEvent.click( tryButton );
-		expect( startFreeSparksMock ).toHaveBeenCalledWith( { endpoint: "/free-sparks-endpoint" } );
+		expect( setDisplayMock ).toHaveBeenCalledWith( "askConsent" );
+		expect( activateFreeSparksMock ).toHaveBeenCalledWith( { endpoint: "/free-sparks-endpoint" } );
 	} );
 
 	it( "should show the alert when isUsageCountLimitReached is true and the 'try for free' button is not rendered", () => {
@@ -113,13 +115,13 @@ describe( "UpsellModalContent", () => {
 				if ( storeName === "yoast-seo/ai-generator" ) {
 					return {
 						isUsageCountLimitReached: () => true,
-						selectFreeSparksStartEndpoint: () => "/free-sparks-endpoint",
+						selectFreeSparksActiveEndpoint: () => "/free-sparks-endpoint",
 					};
 				}
 				return {};
 			} );
 		} );
-		const { getByText, queryByText } = render( <UpsellModalContent /> );
+		const { getByText, queryByText } = render( <UpsellModalContent setDisplay={ setDisplayMock } /> );
 		expect( getByText( "Oh no! Its seems like you're out of free Sparks. Keep the momentum going, unlock unlimited sparks with Yoast SEO Premium!" ) ).toBeInTheDocument();
 		expect( queryByText( "Try for free" ) ).not.toBeInTheDocument();
 	} );
