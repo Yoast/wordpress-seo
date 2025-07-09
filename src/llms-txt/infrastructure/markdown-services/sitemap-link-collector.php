a5cedd406fa9ab3ce50995d3c9beb7c9bf7c1929
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-07-09 15:36:03 +0200

    Change sitemap text.

--- src/llms-txt/infrastructure/markdown-services/sitemap-link-collector.php
+++ src/llms-txt/infrastructure/markdown-services/sitemap-link-collector.php
@@ -20,13 +20,13 @@ class Sitemap_Link_Collector {
 	public function get_link(): ?Link {
 		if ( WPSEO_Options::get( 'enable_xml_sitemap' ) ) {
 			$sitemap_url = WPSEO_Sitemaps_Router::get_base_url( 'sitemap_index.xml' );
-			return new Link( 'sitemap', $sitemap_url );
+			return new Link( 'this link', $sitemap_url );
 		}
 
 		$sitemap_url = \get_sitemap_url( 'index' );
 
 		if ( $sitemap_url !== false ) {
-			return new Link( 'sitemap', $sitemap_url );
+			return new Link( 'this link', $sitemap_url );
 		}
 
 		return null;
