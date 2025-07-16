ccc70e05a7e7482f61fd41219327bd947b5805d9
Paolo L. Scala <paolo@yoast.com>
2025-07-16 14:46:51 +0200

    Fix cs

--- src/ai/free-sparks/infrastructure/endpoints/free-sparks-endpoint.php
+++ src/ai/free-sparks/infrastructure/endpoints/free-sparks-endpoint.php
@@ -1,5 +1,6 @@
 <?php
 // phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
 namespace Yoast\WP\SEO\AI\Free_Sparks\Infrastructure\Endpoints;
 
 use Yoast\WP\SEO\AI\Free_Sparks\User_Interface\Free_Sparks_Route;
