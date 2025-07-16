9a070489a8a77030b7163e6aa658c1dde363edef
Paolo L. Scala <paolo@yoast.com>
2025-07-16 14:41:54 +0200

    Fix authorization namespace

--- src/ai/authorization/domain/code-verifier.php
+++ src/ai/authorization/domain/code-verifier.php
@@ -1,5 +1,6 @@
 <?php
-
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
 namespace Yoast\WP\SEO\AI\Authorization\Domain;
 
 /**
