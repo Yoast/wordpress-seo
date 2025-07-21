8a5a0813aaaf454847b72722bcb43dbbd03640cc
Paolo L. Scala <paolo@yoast.com>
2025-07-21 09:05:39 +0200

    Introduce specialized Endpoint interfaces
    This is done so that our DI container knows which endpoints to gather in each integration through the endpoints repo

---
+++ src/ai/consent/infrastructure/endpoints/consent-endpoint-interface.php
@@ -0,0 +1,8 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+namespace Yoast\WP\SEO\AI\Consent\Infrastructure\Endpoints;
+
+use Yoast\WP\SEO\Routes\Endpoint\Endpoint_Interface;
+
+interface Consent_Endpoint_Interface extends Endpoint_Interface {}
