28035f07f57b3f4349ed5b0be6e2c556e4228b55
Paolo L. Scala <paolo@yoast.com>
2025-07-21 09:07:05 +0200

    Implement endpoints repositories

---
+++ src/ai/generator/application/generator-endpoints-repository.php
@@ -0,0 +1,30 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+namespace Yoast\WP\SEO\AI\Generator\Application;
+
+use Yoast\WP\SEO\AI\Generator\Infrastructure\Endpoints\Generator_Endpoint_Interface;
+use Yoast\WP\SEO\Routes\Endpoint\Endpoints_Repository;
+
+/**
+ * Repository for endpoints.
+ */
+class Generator_Endpoints_Repository extends Endpoints_Repository {
+
+	/**
+	 * Holds the endpoints.
+	 *
+	 * @var array<Generator_Endpoint_Interface>
+	 */
+	private $endpoints;
+
+	/**
+	 * Constructs the repository.
+	 *
+	 * @param Generator_Endpoint_Interface ...$endpoints The endpoints to add to the repository.
+	 */
+	public function __construct( Generator_Endpoint_Interface ...$endpoints ) {
+		parent::__construct( ...$endpoints );
+		$this->endpoints = $endpoints;
+	}
+}
