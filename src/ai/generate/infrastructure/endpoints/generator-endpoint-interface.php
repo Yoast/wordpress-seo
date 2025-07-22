5cfb2ce229ef07f4c6faa914b81e6de1bae28266
Paolo L. Scala <paolo@yoast.com>
2025-07-22 09:51:40 +0200

    Rename Ai_Generator to Generate and fix namespace

--- src/ai/generate/infrastructure/endpoints/generator-endpoint-interface.php
+++ src/ai/generate/infrastructure/endpoints/generator-endpoint-interface.php
@@ -1,7 +1,7 @@
 <?php
 // phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
 // phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
-namespace Yoast\WP\SEO\AI\Generator\Infrastructure\Endpoints;
+namespace Yoast\WP\SEO\AI\Generate\Infrastructure\Endpoints;
 
 use Yoast\WP\SEO\Routes\Endpoint\Endpoint_Interface;
 
