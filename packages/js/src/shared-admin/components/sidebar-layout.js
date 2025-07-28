c28ebd3d51c9f3b51f9e6b822d70a290930f9c6f
Vraja Das <vraja.pro@gmail.com>
2025-07-28 11:37:51 +0300

    fix linting error and warnings
    Fix import and dropped prop types in favor of the jsdocs

--- packages/js/src/shared-admin/components/sidebar-layout.js
+++ packages/js/src/shared-admin/components/sidebar-layout.js
@@ -1,15 +1,14 @@
 import classNames from "classnames";
-import PropTypes from "prop-types";
 
 /**
  * Layout component for pages with a sidebar and main content.
  *
- * @param {string} [contentClassName] Extra class name for the children container.
+ * @param {string} [contentClassName=""] Extra class name for the children container.
  * @param {JSX.node} children The children.
  * @returns {JSX.Element} The element.
  */
 
-export const SidebarLayout = ( { contentClassName, children } ) => {
+export const SidebarLayout = ( { contentClassName = "", children } ) => {
 	return (
 		<div className="yst-flex yst-gap-6 xl:yst-flex-row yst-flex-col relative">
 			<div className={ classNames( "yst-@container yst-flex yst-flex-grow yst-flex-col", contentClassName ) }>
@@ -17,9 +16,4 @@ export const SidebarLayout = ( { contentClassName, children } ) => {
 			</div>
 		</div>
 	);
-};
-
-SidebarLayout.propTypes = {
-	contentClassName: PropTypes.string,
-	children: PropTypes.node,
 };
