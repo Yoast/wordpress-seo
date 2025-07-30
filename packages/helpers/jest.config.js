8e62611d9503cdaf194cc5082bdcf9fb3113db2b
Igor <35524806+igorschoester@users.noreply.github.com>
2025-07-30 11:39:09 +0200

    Fix tests
    Jest uses CommonJS and for some reason a sub-dependency is loading in ESM, pointing it to the CommonJS as a workaround.
    Note that this is presuming the package is hoisted to the root node_modules.
    
    Adding `@wordpress/redux-routine`, which is a missing sub-dependency of `@wordpress/data` of `@wordpress/components` in our replacement variable editor. Yes.

--- packages/helpers/jest.config.js
+++ packages/helpers/jest.config.js
@@ -15,6 +15,12 @@ const config = {
 		"text-summary",
 	],
 	testEnvironment: "jsdom",
+	moduleNameMapper: {
+		// The `@wordpress/i18n` package is using this module.
+		// It defaults to an ESM import in Jest for some reason.
+		// This is a workaround to point it to the CommonJS version.
+		memize: "<rootDir>/../../node_modules/memize/dist/index.cjs",
+	},
 };
 
 module.exports = config;
