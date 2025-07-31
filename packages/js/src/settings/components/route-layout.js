fbdf192be4e92629851fa0d24451226121b95055
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-07-31 15:57:02 +0300

    Move getting the document title in the shared admin store and use it in both settings and redirects

--- packages/js/src/settings/components/route-layout.js
+++ packages/js/src/settings/components/route-layout.js
@@ -1,9 +1,10 @@
+import { useSelect } from "@wordpress/data";
 import { __, sprintf } from "@wordpress/i18n";
 import { Title } from "@yoast/ui-library";
 import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 import { LiveAnnouncer, LiveMessage } from "react-aria-live";
-import { useDocumentTitle } from "../hooks";
+import { STORE_NAME } from "../constants";
 
 /**
  * @param {Object} props The properties.
@@ -17,7 +18,7 @@ const RouteLayout = ( {
 	title,
 	description,
 } ) => {
-	const documentTitle = useDocumentTitle( { prefix: `${ title } ‹ ` } );
+	const documentTitle = useSelect( select => select( STORE_NAME ).selectDocumentFullTitle({ prefix: title }), [] );
 	const ariaLiveTitle = sprintf(
 		/* translators: 1: Settings' section title, 2: Yoast SEO */
 		__( "%1$s Settings - %2$s", "wordpress-seo" ),
---
+++ packages/js/src/shared-admin/store/document-title.js
@@ -0,0 +1,33 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { defaultTo, get } from "lodash";
+
+export const DOCUMENT_TITLE_NAME = "documentTitle";
+
+const slice = createSlice( {
+	name: DOCUMENT_TITLE_NAME,
+	initialState: defaultTo( document?.title, "test" ),
+	reducers: {
+		setDocumentTitle: ( state, { payload } ) => payload,
+	},
+} );
+
+export const getInitialDocumentTitleState = slice.getInitialState;
+
+export const documentTitleSelectors = {
+	selectDocumentTitle: state => get( state, DOCUMENT_TITLE_NAME, "" ),
+	selectDocumentFullTitle: ( state, { prefix = "" } = {} ) => {
+		const title = get( state, DOCUMENT_TITLE_NAME, "" );
+
+		console. log( "prefix", prefix );
+		console. log( "title", title );
+		if ( title.startsWith( prefix ) ) {
+			return title;
+		}
+
+		return `${prefix} < ${title}`;
+	},
+};
+
+export const documentTitleActions = slice.actions;
+
+export const documentTitleReducer = slice.reducer;
