33c4b5a9a889f0799fd4a8198d28a892c6abca71
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-08-04 15:56:47 +0300

    Introduce new introduction about AI brand insights

---
+++ packages/js/src/introductions/components/ai-brand-insights-pre-launch.js
@@ -0,0 +1,114 @@
+import styled from "styled-components";
+import { __, sprintf } from "@wordpress/i18n";
+import { Button, useModalContext } from "@yoast/ui-library";
+import PropTypes from "prop-types";
+import { ReactComponent as AiSparkleIcon } from "../../../../../images/ai-sparkle.svg";
+
+const StyledButton = styled( Button )`
+	background-image: linear-gradient(to bottom right, #A61E69, #6366F1) !important;
+	font-weight: 400 !important;
+	color: white !important;
+	outline-color: transparent !important;
+	&:focus, &:hover {
+		background-image: linear-gradient(to bottom right, #8F0F57, #4338CA) !important;
+	}
+`;
+
+/**
+ * @param {Object} thumbnail The thumbnail: img props.
+ * @param {string} buttonLink The button link.
+ * @param {string} buttonLabel The button label.
+ * @param {string} productName The product name.
+ * @param {string} description The description for the introduction
+ * @param {string} ctbId The click to buy to register for this upsell instance.
+ * @returns {JSX.Element} The element.
+ */
+export const AiBrandInsightsPreLaunch = ( {
+	thumbnail,
+	buttonLink,
+	buttonLabel = __( "Join the waitlist", "wordpress-seo" ),
+	productName = sprintf(
+		/* translators: %1$s expands to Yoast AI brand insights. */
+		__( "Introducing %1$s", "wordpress-seo" ),
+		"Yoast AI brand insights"
+	),
+	description =  __( "Track visibility, control perception, and stay ahead" +
+		" - tools to manage your AI presence are coming soon!", "wordpress-seo" ),
+	ctbId = "f6a84663-465f-4cb5-8ba5-f7a6d72224b2",
+} ) => {
+	const { onClose, initialFocus } = useModalContext();
+
+	return (
+		<>
+			<div className="yst-px-10 yst-pt-10 yst-introduction-gradient yst-text-center">
+				<img
+					className="yst-w-full yst-h-auto yst-rounded-md yst-drop-shadow-md"
+					alt="Thumbnail for Yoast AI Brand Insights Pre-Launch"
+					loading="lazy"
+					decoding="async"
+					{ ...thumbnail }
+				/>
+				<div className="yst-mt-6 yst-text-xs yst-font-medium yst-flex yst-flex-col yst-items-center">
+					<span className="yst-introduction-modal-uppercase yst-flex yst-gap-2 yst-items-center">
+						<span className="yst-ai-insights-icon" />
+						{ productName }
+					</span>
+				</div>
+			</div>
+			<div className="yst-px-10 yst-pb-4 yst-flex yst-flex-col yst-items-center">
+				<div className="yst-mt-4 yst-mx-1.5 yst-text-center">
+					<h3 className="yst-text-slate-900 yst-text-lg yst-font-medium">
+						{
+							__( "How does your brand show up in AI responses?", "wordpress-seo" )
+						}
+					</h3>
+					<div className="yst-mt-2 yst-text-slate-600 yst-text-sm">
+						<p>{ description }</p>
+					</div>
+				</div>
+				<div className="yst-w-full yst-flex yst-mt-6">
+					<StyledButton
+						as="a"
+						className="yst-grow yst-button yst-button--upsell yst-button--extra-large"
+						size="extra-large"
+						variant="upsell"
+						href={ buttonLink }
+						target="_blank"
+						ref={ initialFocus }
+						data-action="load-nfd-ctb"
+						data-ctb-id={ ctbId }
+					>
+						<AiSparkleIcon className="yst--ms-1 yst-me-2 yst-h-4 yst-mt-[3px]" />
+						{ buttonLabel }
+						<span className="yst-sr-only">
+							{
+								/* translators: Hidden accessibility text. */
+								__( "(Opens in a new browser tab)", "wordpress-seo" )
+							}
+						</span>
+					</StyledButton>
+				</div>
+				<Button
+					as="a"
+					className="yst-mt-4"
+					variant="tertiary"
+					onClick={ onClose }
+				>
+					{ __( "Close", "wordpress-seo" ) }
+				</Button>
+			</div>
+		</>
+	);
+};
+AiBrandInsightsPreLaunch.propTypes = {
+	buttonLink: PropTypes.string.isRequired,
+	thumbnail: PropTypes.shape( {
+		src: PropTypes.string.isRequired,
+		width: PropTypes.string,
+		height: PropTypes.string,
+	} ).isRequired,
+	buttonLabel: PropTypes.string,
+	productName: PropTypes.string,
+	description: PropTypes.string,
+	ctbId: PropTypes.string,
+};
--- src/introductions/application/ai-brand-insights-pre-launch.php
+++ src/introductions/application/ai-brand-insights-pre-launch.php
@@ -4,32 +4,16 @@
 namespace Yoast\WP\SEO\Introductions\Application;
 
 use Yoast\WP\SEO\Helpers\Current_Page_Helper;
-use Yoast\WP\SEO\Helpers\Product_Helper;
-use Yoast\WP\SEO\Helpers\User_Helper;
 use Yoast\WP\SEO\Introductions\Domain\Introduction_Interface;
 
 /**
- * Represents the introduction for the Google Docs Addon feature.
+ * Represents the introduction for the AI Brand Insights pre-launch.
  */
-class Google_Docs_Addon_Upsell implements Introduction_Interface {
+class AI_Brand_Insights_Pre_Launch implements Introduction_Interface {
 
 	use User_Allowed_Trait;
 
-	public const ID = 'google-docs-addon-upsell';
-
-	/**
-	 * Holds the user helper.
-	 *
-	 * @var User_Helper
-	 */
-	private $user_helper;
-
-	/**
-	 * Holds the product helper.
-	 *
-	 * @var Product_Helper
-	 */
-	private $product_helper;
+	public const ID = 'ai-brand-insights-pre-launch';
 
 	/**
 	 * Holds the current page helper.
@@ -41,13 +25,9 @@ class Google_Docs_Addon_Upsell implements Introduction_Interface {
 	/**
 	 * Constructs the introduction.
 	 *
-	 * @param User_Helper         $user_helper         The user helper.
-	 * @param Product_Helper      $product_helper      The product helper.
 	 * @param Current_Page_Helper $current_page_helper The current page helper.
 	 */
-	public function __construct( User_Helper $user_helper, Product_Helper $product_helper, Current_Page_Helper $current_page_helper ) {
-		$this->user_helper         = $user_helper;
-		$this->product_helper      = $product_helper;
+	public function __construct( Current_Page_Helper $current_page_helper ) {
 		$this->current_page_helper = $current_page_helper;
 	}
 
@@ -82,11 +62,10 @@ class Google_Docs_Addon_Upsell implements Introduction_Interface {
 
 	/**
 	 * Returns whether this introduction should show.
-	 * We no longer show this introduction, so we always return false.
 	 *
 	 * @return bool Whether this introduction should show.
 	 */
 	public function should_show() {
-		return false;
+		return $this->current_page_helper->is_yoast_seo_page();
 	}
 }
