3bc787bf36563f1a8bdef88a0ebb2a75210f6964
Paolo L. Scala <paolo@yoast.com>
2025-08-11 15:49:47 +0200

    Widens the upsell button

--- admin/class-premium-upsell-admin-block.php
+++ admin/class-premium-upsell-admin-block.php
@@ -94,7 +94,7 @@ class WPSEO_Premium_Upsell_Admin_Block {
 		echo '<ul class="' . esc_attr( $class . '--motivation' ) . '">' . $arguments_html . '</ul>';
 
 		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- Correctly escaped in $upgrade_button and $button_text above.
-		echo '<p>' . $upgrade_button . '</p>';
+		echo '<p style="max-width: inherit;">' . $upgrade_button . '</p>';
 		echo '</div>';
 
 		echo '</div>';
