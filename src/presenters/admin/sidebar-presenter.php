1ec19169c5e6c3536e2e69067c1ca2cf6a59ef79
Paolo L. Scala <paolo@yoast.com>
2025-08-11 17:07:49 +0200

    Fix missing domain

--- src/presenters/admin/sidebar-presenter.php
+++ src/presenters/admin/sidebar-presenter.php
@@ -68,7 +68,7 @@ class Sidebar_Presenter extends Abstract_Presenter {
 							}
 							?>
 						</h2>
-						<span> <?php echo \esc_html__( 'Now with Local, News & Video SEO + 1 Google Docs seat!' ); ?></span>
+						<span> <?php echo \esc_html__( 'Now with Local, News & Video SEO + 1 Google Docs seat!', 'wordpress-seo' ); ?></span>
 							<?php
 							if ( \YoastSEO()->classes->get( Promotion_Manager::class )->is( 'black-friday-2024-promotion' ) ) {
 								echo '<p>';
