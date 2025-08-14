3a238176b5542c5b251c2fa4acc88800407f84fe
Paolo L. Scala <paolo@yoast.com>
2025-08-14 11:42:09 +0200

    Fix consent tests

--- tests/Unit/AI/Consent/User_Interface/Consent_Integration/Render_User_Profile_Test.php
+++ tests/Unit/AI/Consent/User_Interface/Consent_Integration/Render_User_Profile_Test.php
@@ -1,16 +1,16 @@
 <?php
 // phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
 // phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
-namespace Yoast\WP\SEO\Tests\Unit\AI_Consent\User_Interface\AI_Consent_Integration;
+namespace Yoast\WP\SEO\Tests\Unit\AI\Consent\User_Interface\Consent_Integration;
 
 /**
  * Tests the AI_Consent_Integration's render_user_profile method.
  *
  * @group ai-consent
  *
- * @covers \Yoast\WP\SEO\AI_Consent\User_Interface\Ai_Consent_Integration::render_user_profile
+ * @covers \Yoast\WP\SEO\AI\Consent\User_Interface\Consent_Integration::render_user_profile
  */
-final class Render_User_Profile_Test extends Abstract_AI_Consent_Integration_Test {
+final class Render_User_Profile_Test extends Abstract_Consent_Integration_Test {
 
 	/**
 	 * Tests rendering the user profile.
