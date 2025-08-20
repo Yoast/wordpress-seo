42755217627e4d56f8075932521717743161751b
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-08-20 09:50:38 +0300

    Decouple the intermediate content component from the initializer, to make alternating through introductions easier

--- packages/js/src/introductions/initialize.js
+++ packages/js/src/introductions/initialize.js
@@ -6,7 +6,8 @@ import { createRoot } from "@wordpress/element";
 import { Root } from "@yoast/ui-library";
 import { get, isEmpty, find } from "lodash";
 import { LINK_PARAMS_NAME, PLUGIN_URL_NAME, WISTIA_EMBED_PERMISSION_NAME } from "../shared-admin/store";
-import { Content, Introduction, IntroductionProvider } from "./components";
+import { Introduction, IntroductionProvider } from "./components";
+import { AiBrandInsightsPreLaunch } from "./components/modals/ai-brand-insights-pre-launch";
 import { STORE_NAME_INTRODUCTIONS } from "./constants";
 import { registerStore } from "./store";
 
@@ -19,7 +20,7 @@ domReady( () => {
 	}
 
 	const initialComponents = {
-		"ai-brand-insights-pre-launch": Content,
+		"ai-brand-insights-pre-launch": AiBrandInsightsPreLaunch,
 	};
 
 	if ( location.href.indexOf( "page=wpseo_dashboard#/first-time-configuration" ) !== -1 ) {
