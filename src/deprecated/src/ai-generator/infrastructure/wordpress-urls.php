f030418f69c4ed9f977e499f3699955973da9e07
Paolo L. Scala <paolo@yoast.com>
2025-08-24 23:35:38 +0200

    Deprecate ai-generator/infrastructure

--- src/deprecated/src/ai-generator/infrastructure/wordpress-urls.php
+++ src/deprecated/src/ai-generator/infrastructure/wordpress-urls.php
@@ -9,33 +9,51 @@ use Yoast\WP\SEO\Ai_Generator\Domain\URLs_Interface;
 /**
  * Class WordPress_URLs
  * Provides URLs for the AI Generator API in a WordPress context.
+ *
+ * @deprecated 26.0
+ * @codeCoverageIgnore
  */
 class WordPress_URLs implements URLs_Interface {
 
 	/**
 	 * Gets the license URL.
 	 *
+	 * @deprecated 26.0
+	 * @codeCoverageIgnore
+	 *
 	 * @return string The license URL.
 	 */
 	public function get_license_url(): string {
+		\_deprecated_function( __METHOD__, 'Yoast SEO 26.0', 'Yoast\WP\SEO\AI\Generator\Infrastructure\WordPress_URLs::get_license_url' );
+
 		return WPSEO_Utils::get_home_url();
 	}
 
 	/**
 	 * Gets the callback URL to be used by the API to send back the access token, refresh token and code challenge.
 	 *
+	 * @deprecated 26.0
+	 * @codeCoverageIgnore
+	 *
 	 * @return string The callbacks URL.
 	 */
 	public function get_callback_url(): string {
+		\_deprecated_function( __METHOD__, 'Yoast SEO 26.0', 'Yoast\WP\SEO\AI\Generator\Infrastructure\WordPress_URLs::get_callback_url' );
+
 		return \get_rest_url( null, 'yoast/v1/ai_generator/callback' );
 	}
 
 	/**
 	 * Gets the callback URL to be used by the API to send back the refreshed JWTs once they expire.
 	 *
+	 * @deprecated 26.0
+	 * @codeCoverageIgnore
+	 *
 	 * @return string The callbacks URL.
 	 */
 	public function get_refresh_callback_url(): string {
+		\_deprecated_function( __METHOD__, 'Yoast SEO 26.0', 'Yoast\WP\SEO\AI\Generator\Infrastructure\WordPress_URLs::get_refresh_callback_url' );
+
 		return \get_rest_url( null, 'yoast/v1/ai_generator/refresh_callback' );
 	}
 }
