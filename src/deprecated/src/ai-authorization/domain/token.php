05c4b135f9646e3c4dc62af777cc41adcc74a5fd
Paolo L. Scala <paolo@yoast.com>
2025-08-24 14:46:20 +0200

    Deprecate ai-authorization/domain classes

--- src/deprecated/src/ai-authorization/domain/token.php
+++ src/deprecated/src/ai-authorization/domain/token.php
@@ -6,6 +6,9 @@ namespace Yoast\WP\SEO\AI_Authorization\Domain;
 /**
  * Class Token
  * Represents a token used for authentication with the AI Generator API.
+ *
+ * @deprecated 26.0
+ * @codeCoverageIgnore
  */
 class Token {
 
@@ -26,10 +29,15 @@ class Token {
 	/**
 	 * Token constructor.
 	 *
+	 * @deprecated 26.0
+	 * @codeCoverageIgnore
+	 *
 	 * @param string $value      The token value.
 	 * @param int    $expiration The expiration time.
 	 */
 	public function __construct( string $value, int $expiration ) {
+		\_deprecated_function( __METHOD__, 'Yoast SEO 26.0', 'Yoast\WP\SEO\AI\Authorization\Domain\Token::__construct' );
+
 		$this->value      = $value;
 		$this->expiration = $expiration;
 	}
@@ -37,18 +45,28 @@ class Token {
 	/**
 	 * Get the token value.
 	 *
+	 * @deprecated 26.0
+	 * @codeCoverageIgnore
+	 *
 	 * @return string The token value.
 	 */
 	public function get_value(): string {
+		\_deprecated_function( __METHOD__, 'Yoast SEO 26.0', 'Yoast\WP\SEO\AI\Authorization\Domain\Token::get_value' );
+
 		return $this->value;
 	}
 
 	/**
 	 * Whether the token is expired.
 	 *
+	 * @deprecated 26.0
+	 * @codeCoverageIgnore
+	 *
 	 * @return bool True if the token is expired, false otherwise.
 	 */
 	public function is_expired(): bool {
+		\_deprecated_function( __METHOD__, 'Yoast SEO 26.0', 'Yoast\WP\SEO\AI\Authorization\Domain\Token::is_expired' );
+
 		return $this->expiration < \time();
 	}
 }
