a088f6b0292dc8e6149efc3ba12bcf6844d5da4b
Paolo L. Scala <paolo@yoast.com>
2025-08-24 21:32:01 +0200

    Deprecate ai-free-sparks/application classes

--- src/deprecated/src/ai-free-sparks/application/free-sparks-handler-interface.php
+++ src/deprecated/src/ai-free-sparks/application/free-sparks-handler-interface.php
@@ -4,14 +4,20 @@
 namespace Yoast\WP\SEO\AI_Free_Sparks\Application;
 
 /**
- * Interface Consent_Handler_Interface
+ * Interface Free_Sparks_Handler_Interface
  *
- * This interface defines the methods for handling user consent.
+ * This interface defines the methods for handling free sparks.
+ *
+ * @deprecated 26.0
+ * @codeCoverageIgnore
  */
 interface Free_Sparks_Handler_Interface {
 
 	/**
 	 * Retrieves the timestamp.
+	 *
+	 * @deprecated 26.0
+	 * @codeCoverageIgnore
 	 *
 	 * @param string $format The format in which to return the timestamp. Defaults to 'Y-m-d H:i:s'.
 	 *
@@ -21,6 +27,9 @@ interface Free_Sparks_Handler_Interface {
 
 	/**
 	 * Registers the starting of the free sparks.
+	 *
+	 * @deprecated 26.0
+	 * @codeCoverageIgnore
 	 *
 	 * @param ?int $timestamp The timestamp when the user started using free sparks. If null, the current time will be
 	 *                        used.
