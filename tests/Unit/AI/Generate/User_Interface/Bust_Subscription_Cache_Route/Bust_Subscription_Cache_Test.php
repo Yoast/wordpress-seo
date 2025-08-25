4d5794f3967daf91279912f3be85b6af0f750f52
Paolo L. Scala <paolo@yoast.com>
2025-08-25 16:44:37 +0200

    Fix tests

---
+++ tests/Unit/AI/Generate/User_Interface/Bust_Subscription_Cache_Route/Bust_Subscription_Cache_Test.php
@@ -0,0 +1,39 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+namespace Yoast\WP\SEO\Tests\Unit\AI\Generate\User_Interface\Bust_Subscription_Cache_Route;
+
+use Mockery;
+use WP_REST_Response;
+
+/**
+ * Tests the Bust_Subscription_Cache_Route's bust_subscription_cache method.
+ *
+ * @group ai-generator
+ *
+ * @covers \Yoast\WP\SEO\AI_Generator\User_Interface\Bust_Subscription_Cache_Route::bust_subscription_cache
+ */
+final class Bust_Subscription_Cache_Test extends Abstract_Bust_Subscription_Cache_Route_Test {
+
+	/**
+	 * Tests the bust_subscription_cache method.
+	 *
+	 * @return void
+	 */
+	public function test_bust_subscription_cache() {
+		$wp_rest_response = Mockery::mock( 'overload:' . WP_REST_Response::class );
+
+		$this->addon_manager
+			->expects( 'remove_site_information_transients' )
+			->once();
+
+		$wp_rest_response
+			->expects( '__construct' )
+			->with( 'Subscription cache successfully busted.' )
+			->once();
+
+		$result = $this->instance->bust_subscription_cache();
+
+		$this->assertInstanceOf( WP_REST_Response::class, $result );
+	}
+}
---
+++ tests/Unit/AI/Generate/User_Interface/Generator_Integration/Enqueue_Assets_Test.php
@@ -0,0 +1,72 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+namespace Yoast\WP\SEO\Tests\Unit\AI\Generate\User_Interface\Generator_Integration;
+
+use WPSEO_Addon_Manager;
+use Yoast\WP\SEO\Introductions\Application\Ai_Fix_Assessments_Upsell;
+
+/**
+ * Tests the AI_Generator_Integration's enqueue_assets method.
+ *
+ * @group ai-generator
+ *
+ * @covers \Yoast\WP\SEO\AI\Generator\User_Interface\Generator_Integration::enqueue_assets
+ */
+final class Enqueue_Assets_Test extends Base_Assets_Test {
+
+	/**
+	 * Tests enqueuing the assets.
+	 *
+	 * @return void
+	 */
+	public function test_enqueue_assets() {
+		// Current user ID is used for the consent permission.
+		$user_id = 1;
+		$this->user_helper->expects( 'get_current_user_id' )->once()->withNoArgs()->andReturn( $user_id );
+		// Has consent.
+		$this->user_helper->expects( 'get_meta' )
+			->once()
+			->with( $user_id, '_yoast_wpseo_ai_consent', true )
+			->andReturn( true );
+
+		$this->addon_manager
+			->expects( 'has_valid_subscription' )
+			->once()
+			->with( WPSEO_Addon_Manager::PREMIUM_SLUG )
+			->andReturn( true );
+
+		$this->addon_manager
+			->expects( 'has_valid_subscription' )
+			->once()
+			->with( WPSEO_Addon_Manager::WOOCOMMERCE_SLUG )
+			->andReturn( true );
+
+		$this->introductions_seen_repository->expects( 'is_introduction_seen' )
+			->once()
+			->with( $user_id, AI_Fix_Assessments_Upsell::ID )
+			->andReturn( true );
+
+		$this->api_client->expects( 'get_request_timeout' )
+			->once()
+			->andReturn( 0 );
+
+		$this->options_helper->expects( 'get' )
+			->once()
+			->with( 'ai_free_sparks_started_on', null )
+			->andReturn( \time() );
+
+		// Enqueueing.
+		$this->asset_manager->expects( 'enqueue_script' )->once()->with( 'ai-generator' );
+		$this->asset_manager->expects( 'enqueue_style' )->once()->with( 'ai-generator' );
+
+		$this->asset_manager->expects( 'localize_script' )->once()->with(
+			'ai-generator',
+			'wpseoAiGenerator',
+			$this->get_script_data()
+		);
+
+		$this->endpoints_expectations();
+		$this->instance->enqueue_assets();
+	}
+}
