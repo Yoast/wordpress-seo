5564bf16f33c0f89fd7d52d12fd0b416cd585ac7
Vraja Das <vraja.pro@gmail.com>
2025-08-25 13:46:29 +0300

    remove styled component button
    and classes that are not used

--- packages/js/src/components/contentAnalysis/SynonymsUpsell.js
+++ packages/js/src/components/contentAnalysis/SynonymsUpsell.js
@@ -1,20 +1,6 @@
-import styled from "styled-components";
 import { __ } from "@wordpress/i18n";
 import { useToggleState } from "@yoast/ui-library";
 import { PremiumSEOAnalysisModal } from "../modals/PremiumSEOAnalysisModal";
-
-const StyledButton = styled.button`
-	// Increase specificity to override WP rules.
-	&& {
-		display: flex;
-		align-items: center;
-	}
-
-	.yoast-svg-icon {
-		margin: 1px 7px 0 0;
-		fill: currentColor;
-	}
-`;
 
 export const SynonymsUpsell = ( { location } ) => {
 	const [ isOpen, , , openModal, closeModal ] = useToggleState( false );
@@ -22,13 +8,14 @@ export const SynonymsUpsell = ( { location } ) => {
 		? "shortlinks.upsell.sidebar.focus_keyword_synonyms_button"
 		: "shortlinks.upsell.metabox.focus_keyword_synonyms_button";
 	return <>
-		<StyledButton
+		<button
 			type="button"
 			onClick={ openModal }
-			className="wpseo-keyword-synonyms button-link yoast-modal__button-open"
+			className="button-link"
+			id="yoast-keyword-synonyms-button"
 		>
 			{ `+ ${ __( "Add synonyms", "wordpress-seo" ) }` }
-		</StyledButton>
+		</button>
 		<PremiumSEOAnalysisModal
 			isOpen={ isOpen }
 			closeModal={ closeModal }
