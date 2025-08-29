576808905b69cd96f0441cc27cb0b3423c08f100
Vraja Das <vraja.pro@gmail.com>
2025-08-29 17:48:05 +0300

    fix js doc for area chart

--- packages/js/src/components/AreaChart.js
+++ packages/js/src/components/AreaChart.js
@@ -16,7 +16,7 @@ import AreaChartTable from "./AreaChartTable";
  * @param {string} [strokeColor="#000000"] The SVG chart line color in HEX format.
  * @param {number} [strokeWidth=1] The SVG chart line width.
  * @param {string} [className=""] The CSS class name for the chart.
- * @param {Function} [mapChartDataToTableData=null] Function to adapt the chart points to meaningful data for the table.
+ * @param {Function|null} [mapChartDataToTableData=null] Function to adapt the chart points to meaningful data for the table.
  * @param {string} dataTableCaption The data table caption text.
  * @param {Array} dataTableHeaderLabels The text to be used for the data table headers.
  * @param {boolean} [isDataTableVisuallyHidden=true] Whether the data table is visually hidden.
