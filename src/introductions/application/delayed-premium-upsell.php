aad3f71aa3d9c49508fc7c98619a8bd8b341bf86
Paolo L. Scala <paolo@yoast.com>
2025-09-08 14:40:00 +0200

    Finalize the logic to take into account the 7 days delay after the last introduction has been seen

--- src/introductions/application/delayed-premium-upsell.php
+++ src/introductions/application/delayed-premium-upsell.php
@@ -3,18 +3,40 @@
 
 namespace Yoast\WP\SEO\Introductions\Application;
 
+use Yoast\WP\SEO\Helpers\Current_Page_Helper;
 use Yoast\WP\SEO\Helpers\Options_Helper;
+use Yoast\WP\SEO\Helpers\Product_Helper;
 use Yoast\WP\SEO\Introductions\Domain\Introduction_Interface;
+use Yoast\WP\SEO\Introductions\Infrastructure\Introductions_Seen_Repository;
 
 /**
  * Represents the Premium upsell that shows on Yoast SEO pages after a delay.
  */
 class Delayed_Premium_Upsell implements Introduction_Interface {
 
-	use Current_Page_Trait;
-
 	public const ID         = 'delayed-premium-upsell';
 	public const DELAY_DAYS = 14;
+
+	/**
+	 * Holds the repository.
+	 *
+	 * @var Introductions_Seen_Repository
+	 */
+	private $introductions_seen_repository;
+
+	/**
+	 * Holds the product helper.
+	 *
+	 * @var Product_Helper
+	 */
+	private $product_helper;
+
+	/**
+	 * Holds the current page helper.
+	 *
+	 * @var Current_Page_Helper
+	 */
+	private $current_page_helper;
 
 	/**
 	 * The options helper.
@@ -26,10 +48,16 @@ class Delayed_Premium_Upsell implements Introduction_Interface {
 	/**
 	 * Delayed_Premium_Upsell constructor.
 	 *
-	 * @param Options_Helper $options_helper The options helper.
+	 * @param Current_Page_Helper           $current_page_helper           The current page helper.
+	 * @param Introductions_Seen_Repository $introductions_seen_repository The introductions seen repository.
+	 * @param Options_Helper                $options_helper                The options helper.
+	 * @param Product_Helper                $product_helper                The product helper.
 	 */
-	public function __construct( Options_Helper $options_helper ) {
-		$this->options_helper = $options_helper;
+	public function __construct( Current_Page_Helper $current_page_helper, Introductions_Seen_Repository $introductions_seen_repository, Options_Helper $options_helper, Product_Helper $product_helper ) {
+		$this->current_page_helper           = $current_page_helper;
+		$this->introductions_seen_repository = $introductions_seen_repository;
+		$this->options_helper                = $options_helper;
+		$this->product_helper                = $product_helper;
 	}
 
 	/**
@@ -58,7 +86,7 @@ class Delayed_Premium_Upsell implements Introduction_Interface {
 	 * @return int The priority.
 	 */
 	public function get_priority(): int {
-		return 20;
+		return 30;
 	}
 
 	/**
@@ -67,7 +95,8 @@ class Delayed_Premium_Upsell implements Introduction_Interface {
 	 * @return bool Whether this introduction should show.
 	 */
 	public function should_show(): bool {
-		if ( ! $this->is_on_yoast_page( [ 'wpseo_page_settings' ] ) ) {
+		// Never show when not on a Yoast SEO page or when the user has Premium activated.
+		if ( ! $this->current_page_helper->is_yoast_seo_page() || $this->product_helper->is_premium() ) {
 			return false;
 		}
 
@@ -80,19 +109,50 @@ class Delayed_Premium_Upsell implements Introduction_Interface {
 	 * @return bool Whether the introduction should show after the delay.
 	 */
 	private function should_show_after_delay(): bool {
-		$delay        = ( self::DELAY_DAYS * \DAY_IN_SECONDS );
-		$current_time = \time();
-		$previous_version = $this->options_helper->get( 'previous_version' );
+		$delay              = ( self::DELAY_DAYS * \DAY_IN_SECONDS );
+		$current_time       = \time();
+		$previous_version   = $this->options_helper->get( 'previous_version' );
 		$first_activated_on = $this->options_helper->get( 'first_activated_on' );
 
 		// Case where the user has installed the plugin for the first time and the delay has passed.
-		if ( ( $current_time - $first_activated_on ) >= $delay  && $previous_version === '' ) {
-			return true;
+		if ( $previous_version === '' && ( $current_time - $first_activated_on ) >= $delay ) {
+			return $this->is_last_introduction_seen_older_than_a_week();
 		}
 
 		// Case where the user has updated the plugin and the delay has passed since the last update.
 		$last_updated_on = $this->options_helper->get( 'last_updated_on' );
 		if ( ( $current_time - $last_updated_on ) >= $delay ) {
+			return $this->is_last_introduction_seen_older_than_a_week();
+		}
+
+		return false;
+	}
+
+	/**
+	 * Checks if the last introduction seen is older than a week.
+	 *
+	 * @return bool True if the last introduction seen is older than a week, false otherwise.
+	 */
+	private function is_last_introduction_seen_older_than_a_week(): bool {
+		$seen_introductions = $this->introductions_seen_repository->get_all_introductions( \get_current_user_id() );
+		// No other introductions has been seen.
+		if ( empty( $seen_introductions ) ) {
+			return true;
+		}
+
+		// Find the most recent introduction seen.
+		$most_recent_introduction = \array_reduce(
+			$seen_introductions,
+			static function ( $carry, $item ) {
+				if ( $carry === null || $item['seen_on'] > $carry['seen_on'] ) {
+					return $item;
+				}
+				return $carry;
+			}
+		);
+
+		// If the most recent introduction seen is older than a week, return true.
+		if ( ( \time() - $most_recent_introduction['seen_on'] ) >= ( 7 * \DAY_IN_SECONDS ) ) {
 			return true;
 		}
 
