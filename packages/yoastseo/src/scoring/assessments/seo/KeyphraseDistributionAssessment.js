a0dfaf43c031334925e4a7a143e8c10832902858
aidamarfuaty <aida@yoast.com>
2025-09-08 16:41:46 +0200

    Refactor keyphrase assessment scoring and feedback strings for clarity

--- packages/yoastseo/src/scoring/assessments/seo/KeyphraseDistributionAssessment.js
+++ packages/yoastseo/src/scoring/assessments/seo/KeyphraseDistributionAssessment.js
@@ -3,9 +3,6 @@ import { mapValues, merge } from "lodash";
 import Assessment from "../assessment";
 import AssessmentResult from "../../../values/AssessmentResult";
 import { createAnchorOpeningTag } from "../../../helpers";
-import getSentences from "../../../languageProcessing/helpers/sentence/getSentences";
-import removeHtmlBlocks from "../../../languageProcessing/helpers/html/htmlParser";
-import { filterShortcodesFromHTML } from "../../../languageProcessing/helpers";
 
 /**
  * Represents an assessment that returns a score based on the largest percentage of text in which no keyword occurs.
@@ -23,8 +20,7 @@ class KeyphraseDistributionAssessment extends Assessment {
 	 * @param {number} [config.scores.good]             The score to return if keyword occurrences are evenly distributed.
 	 * @param {number} [config.scores.okay]             The score to return if keyword occurrences are somewhat unevenly distributed.
 	 * @param {number} [config.scores.bad]              The score to return if there is way too much text between keyword occurrences.
-	 * @param {number} [config.scores.badNoKeyphraseOrText]  The score to return if there is way no text and/or no keyphrase set.
-	 * @param {number} [config.scores.consideration]    The score to return if there are no keyword occurrences.
+	 * @param {number} [config.scores.noKeyphraseOrText]  The score to return if there is way no text and/or no keyphrase set.
 	 * @param {string} [config.urlTitle]                The URL to the article about this assessment.
 	 * @param {string} [config.urlCallToAction]         The URL to the help article for this assessment.
 	 * @param {object} [config.callbacks] 				The callbacks to use for the assessment.
@@ -44,7 +40,7 @@ class KeyphraseDistributionAssessment extends Assessment {
 				good: 9,
 				okay: 6,
 				bad: 1,
-				badNoKeyphraseOrText: 1,
+				noKeyphraseOrText: 1,
 			},
 			urlTitle: "https://yoa.st/33q",
 			urlCallToAction: "https://yoa.st/33u",
@@ -68,8 +64,7 @@ class KeyphraseDistributionAssessment extends Assessment {
 		this._canAssess = false;
 		this._keyphraseDistribution = researcher.getResearch( "keyphraseDistribution" );
 
-		if( paper.hasKeyword() && paper.hasText() ){
-			this._keyphraseDistribution = researcher.getResearch( "keyphraseDistribution" );
+		if ( paper.hasKeyword() && paper.hasText() ) {
 			this._canAssess = true;
 		}
 
@@ -80,8 +75,8 @@ class KeyphraseDistributionAssessment extends Assessment {
 		assessmentResult.setScore( calculatedResult.score );
 		assessmentResult.setText( calculatedResult.resultText );
 		assessmentResult.setHasMarks( calculatedResult.hasMarks );
-		if( calculatedResult.score < 9 && this._canAssess ) {
-			assessmentResult.setHasAIFixes(true);
+		if ( calculatedResult.score < 9 && this._canAssess ) {
+			assessmentResult.setHasAIFixes( true );
 		}
 		return assessmentResult;
 	}
@@ -96,20 +91,19 @@ class KeyphraseDistributionAssessment extends Assessment {
 			good: goodResultText,
 			okay: okayResultText,
 			bad: badResultText,
-			badNoKeyphraseOrText: noKeyphraseOrTextResultText,
+			noKeyphraseOrText: noKeyphraseOrTextResultText,
 		} = this.getFeedbackStrings();
 
 		const distributionScore = this._keyphraseDistribution.keyphraseDistributionScore;
+		const hasMarks = this._keyphraseDistribution.sentencesToHighlight?.length > 0;
 
 		if ( ! this._canAssess || distributionScore === 100 ) {
 			return {
-				score: this._config.scores.badNoKeyphraseOrText,
-				hasMarks: false,
+				score: this._config.scores.noKeyphraseOrText,
+				hasMarks: hasMarks,
 				resultText: noKeyphraseOrTextResultText,
 			};
 		}
-
-		const hasMarks = this._keyphraseDistribution.sentencesToHighlight?.length > 0;
 
 		if ( distributionScore > this._config.parameters.acceptableDistributionScore ) {
 			return {
@@ -143,9 +137,9 @@ class KeyphraseDistributionAssessment extends Assessment {
 	 * - good: string
 	 * - okay: string
 	 * - bad: string
-	 * - badNoKeyphraseOrText: string
+	 * - noKeyphraseOrText: string
 	 *
-	 * @returns {{good: string, okay: string, bad: string, badNoKeyphraseOrText: string}} The feedback strings.
+	 * @returns {{good: string, okay: string, bad: string, noKeyphraseOrText: string}} The feedback strings.
 	 */
 	getFeedbackStrings() {
 		// `urlTitleAnchorOpeningTag` represents the anchor opening tag with the URL to the article about this assessment.
@@ -158,7 +152,7 @@ class KeyphraseDistributionAssessment extends Assessment {
 				good: "%1$sKeyphrase distribution%3$s: Good job!",
 				okay: "%1$sKeyphrase distribution%3$s: Uneven. Some parts of your text do not contain the keyphrase or its synonyms. %2$sDistribute them more evenly%3$s.",
 				bad: "%1$sKeyphrase distribution%3$s: Very uneven. Large parts of your text do not contain the keyphrase or its synonyms. %2$sDistribute them more evenly%3$s.",
-				badNoKeyphraseOrText: "%1$sKeyphrase distribution%3$s: %2$sPlease add both a keyphrase and some text containing the keyphrase or its synonyms%3$s.",
+				noKeyphraseOrText: "%1$sKeyphrase distribution%3$s: %2$sPlease add both a keyphrase and some text containing the keyphrase or its synonyms%3$s.",
 			};
 			return mapValues(
 				defaultResultTexts,
