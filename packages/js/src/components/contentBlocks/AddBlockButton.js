a0a69587dbbbae6ec7be8325bba3521025c2e669
aidamarfuaty <aida@yoast.com>
2025-09-15 16:35:54 +0200

    Add different color for hovering state

--- packages/js/src/components/contentBlocks/AddBlockButton.js
+++ packages/js/src/components/contentBlocks/AddBlockButton.js
@@ -27,7 +27,7 @@ export const AddBlockButton = ( { showUpsellBadge, blockName, showTooltip } ) =>
 	}, [ showUpsellBadge, blockName ] );
 
 	const buttonClass = "yst-box-border yst-flex yst-flex-row yst-justify-center yst-items-center yst-p-1.5 yst-gap-1.5 yst-w-7 yst-h-7 " +
-		"yst-bg-white yst-border yst-border-solid yst-border-slate-300 yst-shadow-sm yst-rounded-md";
+		"yst-bg-white yst-border yst-border-solid yst-border-slate-300 yst-shadow-sm yst-rounded-md hover:yst-bg-slate-50";
 
 	const ariaLabel = showTooltip
 		? __( "Add block to content.", "wordpress-seo" )
