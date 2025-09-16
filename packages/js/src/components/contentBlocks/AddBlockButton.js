481a8cc1cf9f52874874333ef2d0e4bef55d806f
aidamarfuaty <aida@yoast.com>
2025-09-16 17:07:22 +0200

    Add comment

--- packages/js/src/components/contentBlocks/AddBlockButton.js
+++ packages/js/src/components/contentBlocks/AddBlockButton.js
@@ -45,6 +45,8 @@ export const AddBlockButton = ( { showUpsellBadge, blockName } ) => {
 	const baseButtonClass = "yst-box-border yst-flex yst-flex-row yst-justify-center yst-items-center yst-p-1.5 yst-gap-1.5 yst-w-7 yst-h-7 " +
 		"yst-border yst-border-solid yst-border-slate-300 yst-shadow-sm yst-rounded-md click:yst-bg-primary-500";
 	const backgroundClass = isClicked ? "yst-bg-primary-500" : "yst-bg-white";
+	// The check for `! isClicked` is to prevent `hover:yst-bg-slate-50` to override `yst-bg-primary-500` when the button is clicked.
+	// This is because the `hover` class has higher specificity than the normal class.
 	const tooltipClass = showTooltip && ! isClicked ? "hover:yst-bg-slate-50 yoast-tooltip yoast-tooltip-w" : "";
 	const buttonClass = `${baseButtonClass} ${backgroundClass} ${tooltipClass}`;
 	const plusIconClass = isClicked ? "yst-h-4 yst-w-4 yst-stroke-white" : "yst-h-4 yst-w-4";
