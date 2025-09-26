b2466c24ba1be5d7e7c13546ffeb32036b7302d2
Paolo L. Scala <68744851+pls78@users.noreply.github.com>
2025-09-26 14:40:45 +0200

    Revert "Re organize ai code"

---
+++ tests/Unit/AI_Authorization/Application/Code_Verifier_Handler/Abstract_Code_Verifier_Handler_Test.php
@@ -0,0 +1,53 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+namespace Yoast\WP\SEO\Tests\Unit\AI_Authorization\Application\Code_Verifier_Handler;
+
+use Mockery;
+use Yoast\WP\SEO\AI_Authorization\Application\Code_Verifier_Handler;
+use Yoast\WP\SEO\AI_Authorization\Infrastructure\Code_Verifier_User_Meta_Repository;
+use Yoast\WP\SEO\Helpers\Date_Helper;
+use Yoast\WP\SEO\Tests\Unit\TestCase;
+
+/**
+ * Abstract class for Code_Verifier_Handler tests.
+ *
+ * @group ai-authorization
+ */
+abstract class Abstract_Code_Verifier_Handler_Test extends TestCase {
+
+	/**
+	 * The date helper mock.
+	 *
+	 * @var Mockery\MockInterface|Date_Helper
+	 */
+	protected $date_helper;
+
+	/**
+	 * The code verifier repository mock.
+	 *
+	 * @var Mockery\MockInterface|Code_Verifier_User_Meta_Repository
+	 */
+	protected $code_verifier_repository;
+
+	/**
+	 * The instance to test.
+	 *
+	 * @var Code_Verifier_Handler
+	 */
+	protected $instance;
+
+	/**
+	 * Setup the test.
+	 *
+	 * @return void
+	 */
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->date_helper              = Mockery::mock( Date_Helper::class );
+		$this->code_verifier_repository = Mockery::mock( Code_Verifier_User_Meta_Repository::class );
+
+		$this->instance = new Code_Verifier_Handler( $this->date_helper, $this->code_verifier_repository );
+	}
+}
---
+++ tests/Unit/AI_Generator/Infrastructure/Endpoints/Get_Suggestions_Endpoint/Get_Name_Test.php
@@ -0,0 +1,23 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+namespace Yoast\WP\SEO\Tests\Unit\AI_Generator\Infrastructure\Endpoints\Get_Suggestions_Endpoint;
+
+/**
+ * Tests the Get_Suggestions_Endpoint's get_name method.
+ *
+ * @group ai-generator
+ *
+ * @covers \Yoast\WP\SEO\AI_Generator\Infrastructure\Endpoints\Get_Suggestions_Endpoint::get_name
+ */
+final class Get_Name_Test extends Abstract_Get_Suggestions_Endpoint_Test {
+
+	/**
+	 * Tests the get_name method.
+	 *
+	 * @return void
+	 */
+	public function test_get_name() {
+		$this->assertSame( 'getSuggestions', $this->instance->get_name() );
+	}
+}
