7ad780a60ea27e2ba640516c68be12d14dceab10
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-10-03 13:20:49 +0300

    Add most tests for the user interface layer

---
+++ tests/Unit/Alerts/User_Interface/Default_SEO_Data/Default_SEO_Data_Cron_Scheduler_Schedule_Default_SEO_Data_Detection_Test.php
@@ -0,0 +1,60 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Tests\Unit\Alerts\User_Interface\Default_SEO_Data;
+
+use Brain\Monkey\Functions;
+use Generator;
+
+/**
+ * Test class for the schedule_default_seo_data_detection method.
+ *
+ * @group Default_SEO_Data
+ *
+ * @covers Yoast\WP\SEO\Alerts\User_Interface\Default_Seo_Data\Default_SEO_Data_Cron_Scheduler::schedule_default_seo_data_detection
+ *
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded
+ */
+final class Default_SEO_Data_Cron_Scheduler_Schedule_Default_SEO_Data_Detection_Test extends Abstract_Default_SEO_Data_Cron_Scheduler_Test {
+
+	/**
+	 * Tests the schedule_default_seo_data_detection method.
+	 *
+	 * @dataProvider schedule_default_seo_data_detection_provider
+	 *
+	 * @param bool $wp_next_scheduled_result The result of wp_next_scheduled.
+	 * @param int  $wp_schedule_event_times  The number of times wp_schedule_event should be called.
+	 *
+	 * @return void
+	 */
+	public function test_schedule_default_seo_data_detection(
+		$wp_next_scheduled_result,
+		$wp_schedule_event_times
+	) {
+		Functions\expect( 'wp_next_scheduled' )
+			->once()
+			->with( 'wpseo_detect_default_seo_data' )
+			->andReturn( $wp_next_scheduled_result );
+
+		Functions\expect( 'wp_schedule_event' )
+			->times( $wp_schedule_event_times );
+
+		$this->instance->schedule_default_seo_data_detection();
+	}
+
+	/**
+	 * Data provider for the test_schedule_default_seo_data_detection test.
+	 *
+	 * @return Generator Test data to use.
+	 */
+	public static function schedule_default_seo_data_detection_provider() {
+		yield 'Cron already scheduled - do not schedule again' => [
+			'wp_next_scheduled_result' => 1234567890,
+			'wp_schedule_event_times'  => 0,
+		];
+
+		yield 'Cron not scheduled - schedule it' => [
+			'wp_next_scheduled_result' => false,
+			'wp_schedule_event_times'  => 1,
+		];
+	}
+}
---
+++ tests/Unit/Alerts/User_Interface/Default_SEO_Data/Default_SEO_Data_Watcher_Constructor_Test.php
@@ -0,0 +1,34 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Tests\Unit\Alerts\User_Interface\Default_SEO_Data;
+
+use Yoast\WP\SEO\Helpers\Options_Helper;
+use Yoast\WP\SEO\Repositories\Indexable_Repository;
+
+/**
+ * Test class for the constructor.
+ *
+ * @group Default_SEO_Data
+ *
+ * @covers Yoast\WP\SEO\Alerts\User_Interface\Default_SEO_Data\Default_SEO_Data_Watcher::__construct
+ *
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded
+ */
+final class Default_SEO_Data_Watcher_Constructor_Test extends Abstract_Default_SEO_Data_Watcher_Test {
+
+	/**
+	 * Tests if the needed attributes are set correctly.
+	 *
+	 * @return void
+	 */
+	public function test_constructor() {
+		$this->assertInstanceOf(
+			Indexable_Repository::class,
+			$this->getPropertyValue( $this->instance, 'indexable_repository' )
+		);
+		$this->assertInstanceOf(
+			Options_Helper::class,
+			$this->getPropertyValue( $this->instance, 'options_helper' )
+		);
+	}
+}
