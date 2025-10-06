bb3c021a3787e183c94e9f423d14e7266fce6fda
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-10-06 16:05:03 +0300

    Add submenu item for AI+

---
+++ src/integrations/admin/ai-plus-page.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Yoast\WP\SEO\Integrations\Admin;
+
+use Yoast\WP\SEO\Conditionals\Admin_Conditional;
+use Yoast\WP\SEO\Integrations\Integration_Interface;
+
+/**
+ * Ai_Plus_Page class
+ */
+class Ai_Plus_Page implements Integration_Interface {
+
+	/**
+	 * The page identifier.
+	 */
+	public const PAGE = 'wpseo_ai_plus';
+
+	/**
+	 * Returns the conditionals based in which this loadable should be active.
+	 *
+	 * @return array<string>
+	 */
+	public static function get_conditionals() {
+		return [
+			Admin_Conditional::class,
+		];
+	}
+
+	/**
+	 * Registers all hooks to WordPress.
+	 *
+	 * @return void
+	 */
+	public function register_hooks() {
+		\add_filter( 'wpseo_submenu_pages', [ $this, 'add_submenu_page' ], 12 );
+	}
+
+	/**
+	 * Adds the Yoast SEO AI+ submenu page.
+	 *
+	 * @param string[] $submenu_pages The Yoast SEO submenu pages.
+	 *
+	 * @return string[] The filtered submenu pages.
+	 */
+	public function add_submenu_page( $submenu_pages ) {
+		$submenu_pages[] = [
+			'wpseo_dashboard',
+			'',
+			'Yoast SEO AI+ <span class="yoast-badge yoast-premium-badge"></span>',
+			'edit_others_posts',
+			self::PAGE,
+			[ $this, 'show_ai_plus_page' ],
+		];
+
+		return $submenu_pages;
+	}
+
+	/**
+	 * The AI+ page render function, noop.
+	 *
+	 * @return void
+	 */
+	public function show_ai_plus_page() {
+		// Do nothing and let the redirect happen from the redirect integration.
+	}
+}
