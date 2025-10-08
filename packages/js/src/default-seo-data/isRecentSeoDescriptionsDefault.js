7e7c02b874678638fe5eb9d1a1b9b31954af72e6
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-10-08 12:40:45 +0300

    Move around the default SEO data info in script data

--- packages/js/src/default-seo-data/isRecentSeoDescriptionsDefault.js
+++ packages/js/src/default-seo-data/isRecentSeoDescriptionsDefault.js
@@ -1,14 +1,8 @@
-import getL10nObject from "./getL10nObject";
-
-import { get } from "lodash";
-
 /**
  * Returns whether or not the recent meta descriptions are default ones.
  *
  * @returns {boolean} Whether or not the recent meta descriptions are default ones.
  */
 export default function isRecentSeoDescriptionsDefault() {
-	const l10nObject = getL10nObject();
-
-	return get( l10nObject, "recentDefaultSeoDescriptions", false ) === true;
+	return !! window.wpseoScriptData.recentDefaultSeoDescriptions;
 }
--- packages/js/src/default-seo-data/isRecentSeoTitlesDefault.js
+++ packages/js/src/default-seo-data/isRecentSeoTitlesDefault.js
@@ -1,14 +1,8 @@
-import getL10nObject from "./getL10nObject";
-
-import { get } from "lodash";
-
 /**
  * Returns whether or not the recent SEO titles are default ones.
  *
  * @returns {boolean} Whether or not the recent SEO titles are default ones.
  */
 export default function isRecentSeoTitlesDefault() {
-	const l10nObject = getL10nObject();
-
-	return get( l10nObject, "recentDefaultSeoTitles", false ) === true;
+	return !! window.wpseoScriptData.recentDefaultSeoTitles;
 }
