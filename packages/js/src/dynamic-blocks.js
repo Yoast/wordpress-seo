8b2b9f7105cbbd5cecc544b6c8d2db3c881c884c
JordiPV <llimonero_4t@hotmail.com>
2025-10-10 11:29:19 +0200

    Avoids registering the blocks as placeholders

--- packages/js/src/dynamic-blocks.js
+++ packages/js/src/dynamic-blocks.js
@@ -2,7 +2,6 @@
 import { useBlockProps } from "@wordpress/block-editor";
 import { registerBlockType } from "@wordpress/blocks";
 import ServerSideRender from "@wordpress/server-side-render";
-import { UserGroupIcon, CollectionIcon } from "@heroicons/react/outline";
 
 /* Internal dependencies */
 import block from "./dynamic-blocks/breadcrumbs/block.json";
@@ -37,40 +36,6 @@ const registerDynamicBlocks = () => {
 		 *
 		 * @returns {null} Nothing.
 		 */
-		save: () => null,
-	} );
-
-	// Temporary placeholders for Siblings and Sub-pages blocks so they can be inserted from the Yoast menu.
-	// These will be replaced with proper implementations later.
-	registerBlockType( "yoast-seo/siblings", {
-		title: "Yoast Siblings",
-		category: "yoast-internal-linking-blocks",
-		icon: UserGroupIcon,
-		supports: { multiple: true },
-		edit: () => {
-			const blockProps = useBlockProps();
-			return (
-				<div { ...blockProps }>
-					<p>Yoast Siblings block</p>
-				</div>
-			);
-		},
-		save: () => null,
-	} );
-
-	registerBlockType( "yoast-seo/sub-pages", {
-		title: "Yoast Sub-pages",
-		category: "yoast-internal-linking-blocks",
-		icon: CollectionIcon,
-		supports: { multiple: true },
-		edit: () => {
-			const blockProps = useBlockProps();
-			return (
-				<div { ...blockProps }>
-					<p>Yoast Sub-pages block</p>
-				</div>
-			);
-		},
 		save: () => null,
 	} );
 };
