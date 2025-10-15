17d4d269c0af1b04d2098c1a3061191c2cc0ef40
Vraja Das <vraja.pro@gmail.com>
2025-10-15 15:07:56 +0300

    Checks if the title or description are the same as the default

--- packages/js/src/helpers/addAiGenerateToChecklist.js
+++ packages/js/src/helpers/addAiGenerateToChecklist.js
@@ -1,3 +1,5 @@
+import { get } from "lodash";
+
 /**
  * Determines if the AI Generate check should be shown.
  *
@@ -6,10 +8,19 @@
  * @returns {boolean} Whether to show the AI Generate check.
  */
 export function shouldShowAiGenerateCheck( store ) {
-    const { isRecentSeoTitlesDefault, isRecentSeoDescriptionsDefault } = store.getPreferences();
+	const { isRecentSeoTitlesDefault, isRecentSeoDescriptionsDefault } = store.getPreferences();
+	const snippetEditorData = store.getSnippetEditorData();
+	const titleDefaultTemplate = get( window, "wpseoScriptData.metabox.title_template", "" );
+	const descriptionDefaultTemplate = get( window, "wpseoScriptData.metabox.metadesc_template", "" );
 
-    console.log( 'isRecentSeoTitlesDefault:', isRecentSeoTitlesDefault );
-    console.log( 'isRecentSeoDescriptionsDefault:', isRecentSeoDescriptionsDefault );
-    
-    return true;
+	console.log( "isRecentSeoTitlesDefault:", isRecentSeoTitlesDefault );
+	console.log( "isRecentSeoDescriptionsDefault:", isRecentSeoDescriptionsDefault );
+
+	if ( snippetEditorData.title.trim() === titleDefaultTemplate.trim() ) {
+		return true;
+	}
+
+	if ( snippetEditorData.description.trim() === descriptionDefaultTemplate.trim() ) {
+		return true;
+	}
 }
