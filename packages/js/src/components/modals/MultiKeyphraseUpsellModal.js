f5d98a7e62d32b98efa17c51f8d694161ca8b8e3
Enrico Battocchi <enrico.battocchi@gmail.com>
2025-10-15 13:45:51 +0200

    Improve the title of the "Add related keyphrase" modal

--- packages/js/src/components/modals/MultiKeyphraseUpsellModal.js
+++ packages/js/src/components/modals/MultiKeyphraseUpsellModal.js
@@ -20,7 +20,7 @@ export const MultiKeyphraseUpsellModal = ( { isOpen, closeModal, id, upsellLink 
 		title={ __( "Cover more search intent with related keyphrases", "wordpress-seo" ) }
 		description={ __( "Optimize for up to 5 keyphrases to shape your content around different themes, audiences, and angles - helping it get discovered by a wider audience.", "wordpress-seo" ) }
 		note={ __( "Fine-tune your content for every audience", "wordpress-seo" ) }
-		modalTitle={ __( "Target multiple keyphrases", "wordpress-seo" ) }
+		modalTitle={ __( "Add more keyphrases with Premium", "wordpress-seo" ) }
 		ctbId="f6a84663-465f-4cb5-8ba5-f7a6d72224b2"
 	/>;
 };
