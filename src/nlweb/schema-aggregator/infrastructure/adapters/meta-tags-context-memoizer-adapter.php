c392c3f329eda9b243d96fb8a7b7877f0e126240
Paolo L. Scala <paolo@yoast.com>
2025-10-23 14:16:40 +0200

    Use a single adapter

--- src/nlweb/schema-aggregator/infrastructure/adapters/meta-tags-context-memoizer-adapter.php
+++ src/nlweb/schema-aggregator/infrastructure/adapters/meta-tags-context-memoizer-adapter.php
@@ -4,9 +4,9 @@ namespace Yoast\WP\SEO\NLWeb\Schema_Aggregator\Infrastructure\Adapters;
 use Yoast\WP\SEO\Context\Meta_Tags_Context;
 
 /**
- * Interface for converting Meta_Tags_Context objects to arrays.
+ * Class for converting Meta_Tags_Context objects to arrays.
  */
-interface Meta_Tags_Context_Memoizer_Adapter_Interface {
+class Meta_Tags_Context_Memoizer_Adapter {
 
 	/**
 	 * Converts a Meta_Tags_Context object to an array.
@@ -14,5 +14,7 @@ interface Meta_Tags_Context_Memoizer_Adapter_Interface {
 	 * @param Meta_Tags_Context $context The meta tags context.
 	 * @return array<string, string|int|bool> The meta tags context as an array.
 	 */
-	public function meta_tags_context_to_array( Meta_Tags_Context $context ): array;
+	public function meta_tags_context_to_array( Meta_Tags_Context $context ): array {
+		return $context->presentation->schema;
+	}
 }
