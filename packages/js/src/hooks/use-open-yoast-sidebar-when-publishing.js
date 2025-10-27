378873867cc45354eaa21c3e8c0b23415a366210
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-10-27 13:51:07 +0200

    Improve parameter name in custom hook

--- packages/js/src/hooks/use-open-yoast-sidebar-when-publishing.js
+++ packages/js/src/hooks/use-open-yoast-sidebar-when-publishing.js
@@ -4,11 +4,11 @@ import { useCallback } from "@wordpress/element";
 /**
  * Custom hook to open the Yoast SEO sidebar from the Publish sidebar.
  *
- * @param {boolean} alsoOpenSearchAppearance Whether to also open the Search Appearance modal.
+ * @param {boolean} shouldOpenSearchAppearance Whether to also open the Search Appearance modal.
  *
  * @returns {Function} Function to open the Search Appearance modal.
  */
-export const useOpenYoastSidebarWhenPublishing = ( alsoOpenSearchAppearance ) => {
+export const useOpenYoastSidebarWhenPublishing = ( shouldOpenSearchAppearance ) => {
 	const openGeneralSidebar = useDispatch( "core/edit-post" )?.openGeneralSidebar;
 	const closePublishSidebar = useDispatch( "core/editor" )?.closePublishSidebar;
 	const { openEditorModal } = useDispatch( "yoast-seo/editor" );
@@ -17,7 +17,7 @@ export const useOpenYoastSidebarWhenPublishing = ( alsoOpenSearchAppearance ) =>
 		() => {
 			closePublishSidebar();
 			openGeneralSidebar( "yoast-seo/seo-sidebar" );
-			if ( alsoOpenSearchAppearance ) {
+			if ( shouldOpenSearchAppearance ) {
 				openEditorModal( "yoast-search-appearance-modal" );
 			}
 		},
