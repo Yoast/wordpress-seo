2dcd4014019300e65dc7bda7c0d76f8d7e26fc01
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-11-04 15:10:08 +0100

    Start implementation.

---
+++ src/schema_aggregator/application/aggregate-site-schema-map-command-handler.php
@@ -0,0 +1,16 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Schema_Aggregator\Application;
+class Aggregate_Site_Schema_Map_Command_Handler {
+
+	/**
+	 * Handles the Aggregate_Site_Schema_Map_Command.
+	 *
+	 * @param Aggregate_Site_Schema_Map_Command $command The command.
+	 *
+	 * @return array<string> The schema map xml.
+	 */
+	public function handle( Aggregate_Site_Schema_Map_Command $command ): string {
+
+	}
+}
