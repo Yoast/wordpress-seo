b8f68e92ecc07bf68158c8c750fb424990f5a103
Vraja Das <vraja.pro@gmail.com>
2025-11-07 16:46:06 +0200

    adds task list route

---
+++ packages/js/src/general/routes/task-list.js
@@ -0,0 +1,23 @@
+import { Paper, Title } from "@yoast/ui-library";
+import { __ } from "@wordpress/i18n";
+
+/**
+ * @returns {JSX.Element} The task list page content placeholder.
+ */
+export const TaskList = () => {
+	return <Paper>
+		<>
+			<Paper.Header>
+				<Title>{ __( "Task list", "wordpress-seo" ) }</Title>
+				<p className="yst-max-w-screen-sm yst-mt-3 yst-text-tiny">
+					{ __( "Stay on top of your SEO progress with this task list. Complete each task to ensure your site is optimized and aligned with best SEO practices.", "wordpress-seo" ) }
+				</p>
+			</Paper.Header>
+			<Paper.Content>
+				<div>
+					{ __( "Task list content will be displayed here.", "wordpress-seo" ) }
+				</div>
+			</Paper.Content>
+		</>
+	</Paper>;
+};
