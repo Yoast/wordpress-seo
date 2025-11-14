30a31b107dc0efd008d70409709adf86258ca6de
Vraja Das <vraja.pro@gmail.com>
2025-11-14 13:55:31 +0200

    adds loading state to the task row

--- packages/dashboard-frontend/src/task-list/components/task-row.js
+++ packages/dashboard-frontend/src/task-list/components/task-row.js
@@ -1,6 +1,6 @@
 import { CheckCircleIcon } from "@heroicons/react/solid";
 import { ChevronRightIcon } from "@heroicons/react/outline";
-import { Table, useSvgAria } from "@yoast/ui-library";
+import { Table, useSvgAria, SkeletonLoader } from "@yoast/ui-library";
 import { Priority } from "./priority";
 import { Duration } from "./duration";
 import { TaskBadge } from "./task-badge";
@@ -9,6 +9,29 @@ import { __ } from "@wordpress/i18n";
 
 const badgeOptions = [ "premium", "woo", "ai" ];
 
+const LoadingTaskRow = ( { title } ) => {
+	const svgAriaProps = useSvgAria();
+	return <Table.Row>
+		<Table.Cell className="yst-font-medium yst-text-slate-800">
+			<div className="yst-flex yst-items-center yst-gap-2">
+				<Ellipse className="yst-w-4 yst-text-slate-200" { ...svgAriaProps } />
+				<SkeletonLoader className="yst-h-[18px]">{ title }</SkeletonLoader>
+			</div>
+		</Table.Cell>
+		<Table.Cell>
+			<Duration isLoading={ true } />
+		</Table.Cell>
+		<Table.Cell>
+			<Priority isLoading={ true } />
+		</Table.Cell>
+		<Table.Cell className="yst-align-middle">
+			<div className="yst-flex yst-items-center yst-justify-around yst-gap-2">
+				<ChevronRightIcon className="yst-w-4 yst-text-slate-600" { ...svgAriaProps } />
+			</div>
+		</Table.Cell>
+	</Table.Row>;
+};
+
 /**
  * The TaskRow component to display a task in a table row.
  *
@@ -16,13 +39,19 @@ const badgeOptions = [ "premium", "woo", "ai" ];
  * @param {number} duration Estimated duration to complete the task.
  * @param {string} priority Priority of the task: 'low', 'medium', 'high'.
  * @param {string} badge An optional badge to display next to the task title: `premium`, `woo`, `ai`.
- * @param {boolean} isCompleted Whether the task is completed. If true, the call to action button will be disabled.
+ * @param {boolean} [isCompleted] Whether the task is completed. If true, the call to action button will be disabled.
  * @param {Function} onClick Function to call when the row is clicked.
+ * @param {boolean} [isLoading=false] Whether the title is loading.
  *
  * @returns {JSX.Element} The TaskRow component.
  */
-export const TaskRow = ( { title, duration, priority, badge, isCompleted, onClick } ) => {
+export const TaskRow = ( { title, duration, priority, badge, isCompleted, onClick, isLoading } ) => {
 	const svgAriaProps = useSvgAria();
+
+	if ( isLoading ) {
+		return <LoadingTaskRow title={ title } />;
+	}
+
 	return <Table.Row>
 		<Table.Cell className="yst-font-medium yst-text-slate-800">
 			<div className="yst-flex yst-items-center yst-gap-2">
