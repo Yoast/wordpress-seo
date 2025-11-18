28f26c56cc3473cf25bc4f2195f50ab24a59f92c
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-11-18 15:46:24 +0200

    Add comment explaining the post type tasks

--- src/task-list/domain/tasks/post-type-task-interface.php
+++ src/task-list/domain/tasks/post-type-task-interface.php
@@ -4,6 +4,8 @@ namespace Yoast\WP\SEO\Task_List\Domain\Tasks;
 
 /**
  * Represents a post type task.
+ * Post type tasks are tasks that span throughout multiple post types.
+ * A post type task is relevant for multiple post types and it will be represented by separate tasks in the frontend, one per post type.
  */
 interface Post_Type_Task_Interface extends Task_Interface {
 
