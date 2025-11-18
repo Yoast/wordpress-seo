9f283a8743cf850de7f54cc6c551dcf2e093f79d
Vraja Das <vraja.pro@gmail.com>
2025-11-18 10:05:24 +0200

    fix icon color

--- packages/dashboard-frontend/src/icons/Ellipse.js
+++ packages/dashboard-frontend/src/icons/Ellipse.js
@@ -1,6 +1,6 @@
 const Ellipse = ( props ) => (
 	<svg { ...props } width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<circle cx="6.4" cy="6.4" r="6.4" fill="#E2E8F0" />
+		<circle cx="6.4" cy="6.4" r="6.4" fill="currentColor" />
 	</svg>
 );
 
