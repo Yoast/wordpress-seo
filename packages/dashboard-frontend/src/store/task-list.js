502b4115e918c398e42a9ac5fc09fc1d059f7804
Vraja Das <vraja.pro@gmail.com>
2025-11-18 13:08:28 +0200

    adds action and selector to the task list slice

--- packages/dashboard-frontend/src/store/task-list.js
+++ packages/dashboard-frontend/src/store/task-list.js
@@ -5,8 +5,23 @@ export const TASK_LIST_NAME = "taskList";
 
 const slice = createSlice( {
 	name: TASK_LIST_NAME,
-	initialState: { enabled: false },
-	reducers: {},
+	initialState: {
+		enabled: false,
+		endpoints: {
+			getTasks: "",
+			completeTask: "",
+		},
+		tasks: {},
+	},
+	reducers: {
+		setTaskCompleted( state, action ) {
+			const { taskId } = action.payload;
+			state.tasks[ taskId ].isCompleted = true;
+		},
+		setTasks( state, action ) {
+			state.tasks = action.payload;
+		},
+	},
 } );
 
 /**
@@ -14,9 +29,10 @@ const slice = createSlice( {
  */
 export const getInitialTaskListState = slice.getInitialState;
 
-
 export const taskListSelectors = {
 	selectIsTaskListEnabled: ( state ) => get( state, [ TASK_LIST_NAME, "enabled" ], false ),
+	selectTaskListEndpoints: ( state ) => get( state, [ TASK_LIST_NAME, "endpoints" ], {} ),
+	getTasks: ( state ) => get( state, [ TASK_LIST_NAME, "tasks" ], {} ),
 };
 
 export const taskListActions = {
