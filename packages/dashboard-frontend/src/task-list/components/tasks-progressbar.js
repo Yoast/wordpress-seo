1c3ffe591d2e97c2029b000d0c8340db98b64c33
Vraja Das <vraja.pro@gmail.com>
2025-11-19 10:04:24 +0200

    fix tasks progress bar translation, documentation and adds safety check

--- packages/dashboard-frontend/src/task-list/components/tasks-progressbar.js
+++ packages/dashboard-frontend/src/task-list/components/tasks-progressbar.js
@@ -4,12 +4,15 @@ import { __ } from "@wordpress/i18n";
 /**
  * Component displaying a progress bar for tasks.
  *
- * @param {number} completedTasks Number of completed tasks.
+ * @param {number} completedTasks Number of completed tasks, should be less than or equal to totalTasks.
  * @param {number} totalTasks Total number of tasks.
  * @param {boolean} isLoading Whether the tasks are loading.
  * @returns {JSX.Element} The TasksProgressBar component.
  */
 export const TasksProgressBar = ( { completedTasks, totalTasks, isLoading } ) => {
+	if ( completedTasks > totalTasks ) {
+		return null;
+	}
 	return (
 		<div>
 			<Title as="h2" className="yst-text-lg yst-font-medium yst-text-slate-900 yst-mb-2">{ __( "Tasks", "wordpress-seo" ) }</Title>
@@ -18,7 +21,7 @@ export const TasksProgressBar = ( { completedTasks, totalTasks, isLoading } ) =>
 					<SkeletonLoader className="yst-w-[184px] yst-h-1.5" /><SkeletonLoader className="yst-w-9 yst-h-5" />
 				</> : <>
 					<ProgressBar
-						label="Tasks Progress"
+						label={ __( "Tasks Progress", "wordpress-seo" ) }
 						progress={ completedTasks }
 						min={ 0 }
 						max={ totalTasks }
