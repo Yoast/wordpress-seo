c1d5a420d93af1abf6048b45396478dac7ec91f3
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2025-11-20 12:10:19 +0100

    Tiny indexable change.

--- src/schema-aggregator/infrastructure/indexable-repository/wordpress-query-repository.php
+++ src/schema-aggregator/infrastructure/indexable-repository/wordpress-query-repository.php
@@ -65,8 +65,7 @@ class WordPress_Query_Repository implements Indexable_Repository_Interface {
 		$post_ids          = isset( $query->posts ) && \is_array( $query->posts ) ? $query->posts : [];
 		$public_indexables = [];
 		foreach ( $post_ids as $post_id ) {
-			$indexable = $this->indexable_repository->find_by_id_and_type( $post_id, $post_type, false );
-			$indexable = $this->indexable_builder->build_for_id_and_type( $post_id, $post_type, $indexable );
+			$indexable = $this->indexable_repository->find_by_id_and_type( $post_id, $post_type );
 			if ( $indexable !== null && ( $indexable->is_public === true || $indexable->is_public === null ) ) {
 				$public_indexables[] = $indexable;
 			}
