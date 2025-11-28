77b8c3c2a183af7b227c8dcb3d5f7857e0d3b174
Paolo L. Scala <paolo@yoast.com>
2025-11-28 16:36:38 +0100

    Filtered map loader strategy

---
+++ src/schema-aggregator/infrastructure/elements-context-map/filtered-map-loader.php
@@ -0,0 +1,42 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Schema_Aggregator\Infrastructure\Elements_Context_Map;
+
+/**
+ * Map loader that applies filters to the elements-context map.
+ */
+class Filtered_Map_Loader implements Map_Loader_Interface {
+
+	/**
+	 * The base map loader strategy.
+	 *
+	 * @var Base_Map_Loader
+	 */
+	private $base_loader;
+
+	/**
+	 * Class constructor..
+	 *
+	 * @param Base_Map_Loader $base_loader The base map loader strategy.
+	 */
+	public function __construct( Base_Map_Loader $base_loader ) {
+		$this->base_loader = $base_loader;
+	}
+
+	/**
+	 * Loads a filtered elements-context map.
+	 *
+	 * @return array<string, array<string, string>> The filtered elements-context map.
+	 */
+	public function load(): array {
+		$map = $this->base_loader->load();
+
+		$map = \apply_filters( 'wpseo_schema_aggregator_elements_context_map', $map );
+
+		foreach ( $map as $context => $elements ) {
+			$map[ $context ] = \apply_filters( "wpseo_schema_aggregator_elements_context_map_{$context}", $elements );
+		}
+
+		return $map;
+	}
+}
