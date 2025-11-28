00e4cb860fdc6c2f87fc594b0edf8ba14e02ead6
Vraja Das <vraja.pro@gmail.com>
2025-11-28 14:33:17 +0200

    fix task completed selector and adds tests

--- packages/dashboard-frontend/src/store/task-list.js
+++ packages/dashboard-frontend/src/store/task-list.js
@@ -123,8 +123,7 @@ export const taskListSelectors = {
 	selectTaskError: ( state, id ) => get( state, [ TASK_LIST_NAME, "tasks", id, "error" ], null ),
 	selectTasksEndpoints: ( state ) => get( state, [ TASK_LIST_NAME, "endpoints" ], {} ),
 	selectNonce: ( state ) => get( state, [ TASK_LIST_NAME, "nonce" ], "" ),
-	// eslint-disable-next-line no-undefined
-	selectIsTaskCompleted: ( state, id ) => get( state, [ TASK_LIST_NAME, "tasks", id, "isCompleted" ], undefined ),
+	selectIsTaskCompleted: ( state, id ) => get( state, [ TASK_LIST_NAME, "tasks", id, "isCompleted" ], null ),
 };
 
 export const taskListActions = {
