cc155ae19fdbf3a2ddf86b734ccfb328f6be03cf
Paolo L. Scala <paolo@yoast.com>
2025-12-04 16:14:26 +0100

    Fix bug about missing constructor parameter for Default_Filter class

--- src/schema-aggregator/infrastructure/filtering-strategy-factory.php
+++ src/schema-aggregator/infrastructure/filtering-strategy-factory.php
@@ -4,6 +4,8 @@ namespace Yoast\WP\SEO\Schema_Aggregator\Infrastructure;
 
 use Yoast\WP\SEO\Schema_Aggregator\Application\Filtering\Default_Filter;
 use Yoast\WP\SEO\Schema_Aggregator\Application\Filtering\Filtering_Strategy_Interface;
+use Yoast\WP\SEO\Schema_Aggregator\Infrastructure\Elements_Context_Map\Base_Map_Loader;
+use Yoast\WP\SEO\Schema_Aggregator\Infrastructure\Elements_Context_Map\Elements_Context_Map_Repository;
 
 /**
  * Factory for creating filtering strategy instances.
@@ -18,10 +20,13 @@ class Filtering_Strategy_Factory {
 	public function create(): Filtering_Strategy_Interface {
 		$filtering_class = \apply_filters( 'wpseo_schema_aggregator_filtering_strategy', Default_Filter::class );
 
+		$map_loader                      = new Base_Map_Loader();
+		$elements_context_map_repository = new Elements_Context_Map_Repository( $map_loader );
+
 		if ( \is_a( $filtering_class, Filtering_Strategy_Interface::class, true ) ) {
-			return new $filtering_class();
+			return new $filtering_class( $elements_context_map_repository );
 		}
 
-		return new Default_Filter();
+		return new Default_Filter( $elements_context_map_repository );
 	}
 }
