2b67f094aa68f5b3e3aef024483c44cee57a4d94
Paolo L. Scala <paolo@yoast.com>
2025-12-04 16:13:37 +0100

    The default filtering logic

--- src/schema-aggregator/application/filtering/default-filter.php
+++ src/schema-aggregator/application/filtering/default-filter.php
@@ -1,5 +1,5 @@
 <?php
-
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
 namespace Yoast\WP\SEO\Schema_Aggregator\Application\Filtering;
 
 use Yoast\WP\SEO\Schema_Aggregator\Domain\Schema_Piece;
@@ -9,6 +9,18 @@ use Yoast\WP\SEO\Schema_Aggregator\Infrastructure\Elements_Context_Map\Elements_
  * Default filtering strategy implementation.
  */
 class Default_Filter implements Filtering_Strategy_Interface {
+
+	/**
+	 * The categories to filter.
+	 *
+	 * @var array<string>
+	 */
+	private $filter_categories = [
+		'action',
+		'enumeration',
+		'meta',
+		'website',
+	];
 
 	/**
 	 * The elements context map repository.
@@ -34,8 +46,30 @@ class Default_Filter implements Filtering_Strategy_Interface {
 	 * @return array<Schema_Piece> The filtered schema.
 	 */
 	public function filter( array $schema ): array {
+		$filtered_schema      = [];
 		$elements_context_map = $this->elements_context_map_repository->get_map();
-		// Use the map.
-		return $schema;
+		foreach ( $schema as $schema_piece ) {
+			$should_keep = true;
+			foreach ( $this->filter_categories as $category ) {
+				if ( \in_array( $schema_piece->get_type(), $elements_context_map[ $category ], true ) ) {
+					$filter_class_name = 'Yoast\WP\SEO\Schema_Aggregator\Application\Filtering\Schema_Node_Filter\\' . $schema_piece->get_type() . '_Schema_Node_Filter';
+					if ( \class_exists( $filter_class_name ) && \is_a( $filter_class_name, 'Yoast\WP\SEO\Schema_Aggregator\Application\Filtering\Schema_Node_Filter\Schema_Node_Filter_Interface', true ) ) {
+						$should_keep = ( new $filter_class_name() )->filter( $schema, $schema_piece );
+					}
+					else {
+						$should_keep = false;
+					}
+					break;
+				}
+			}
+			if ( $should_keep ) {
+				$properties_filter_class_name = 'Yoast\WP\SEO\Schema_Aggregator\Application\Filtering\Schema_Node_Property_Filter\\' . $schema_piece->get_type() . '_Schema_Node_Property_Filter';
+				if ( \class_exists( $properties_filter_class_name ) && \is_a( $properties_filter_class_name, 'Yoast\WP\SEO\Schema_Aggregator\Application\Filtering\Schema_Node_Property_Filter\Schema_Node_Property_Filter_Interface', true ) ) {
+					$schema_piece = ( new $properties_filter_class_name() )->filter_properties( $schema_piece );
+				}
+				$filtered_schema[] = $schema_piece;
+			}
+		}
+		return $filtered_schema;
 	}
 }
