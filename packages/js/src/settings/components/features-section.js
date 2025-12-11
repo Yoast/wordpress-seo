921253b3f1f744b11d095bd4952c5d34281903a0
Vraja Das <vraja.pro@gmail.com>
2025-12-11 13:45:29 +0200

    fix border and spacing

--- packages/js/src/settings/components/features-section.js
+++ packages/js/src/settings/components/features-section.js
@@ -1,9 +1,10 @@
 import { Title, useSvgAria } from "@yoast/ui-library";
-import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/outline";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/solid";
 import { FeatureItem } from "./feature-item";
 import { useCallback } from "@wordpress/element";
 import { useSelectSettings, useDispatchSettings } from "../hooks";
 import { __ } from "@wordpress/i18n";
+import classNames from "classnames";
 
 /**
  *
@@ -23,15 +24,20 @@ export const FeaturesSection = ( { id, title, features = [] } ) => {
 	const ChevronIcon = isOpen ? ChevronUpIcon : ChevronDownIcon;
 	const svgAriaProps = useSvgAria();
 	return (
-		<fieldset>
-			<div className="yst-flex yst-justify-between yst-items-center yst-py-3.5 yst-border-b-slate-200 yst-border-b">
-				<Title size="4" as="h2" className="">{ `${title} (${ featureCount })` }</Title>
-				<button onClick={ handleToggle } type="button">
-					<ChevronIcon className="yst-h-5 yst-w-5 yst-text-slate-400 yst-shrink-0" { ...svgAriaProps } />
+		<fieldset className="yst-group">
+			<button
+				type="button"
+				className={ classNames( "yst-flex yst-justify-between yst-items-center yst-py-3.5 yst-border-b-slate-200 yst-border-b yst-w-full",
+					isOpen ? "" : "group-last:yst-border-none"
+				) }
+				onClick={ handleToggle }
+			>
+				<Title size="4" as="h2">{ `${title} (${ featureCount })` }</Title>
+				<span>
+					<ChevronIcon className="yst-h-6 yst-w-6 yst-text-slate-400 yst-shrink-0 yst-me-3" { ...svgAriaProps } />
 					<span className="yst-sr-only">{ `${isOpen ? __( "Collapse", "wordpress-seo" ) : __( "Expand", "wordpress-seo" )} ${title}` }</span>
-				</button>
-
-			</div>
+				</span>
+			</button>
 			{ isOpen && <ul className="yst-mb-2">
 				{ features.map( ( feature ) => (
 					<li key={ feature.id } className="yst-border-b-slate-200 yst-border-b last:yst-border-0 yst-py-4">
