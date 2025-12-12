2939f86619ed631f19852574dad5c0f5fd31f24d
YoastBot <yoastbot@yoast.com>
2025-12-12 14:55:03 +0000

    Bump version to 26.6-RC5

--- wp-seo.php
+++ wp-seo.php
@@ -8,7 +8,7 @@
  *
  * @wordpress-plugin
  * Plugin Name: Yoast SEO
- * Version:     26.6-RC4
+ * Version:     26.6-RC5
  * Plugin URI:  https://yoa.st/1uj
  * Description: The first true all-in-one SEO solution for WordPress, including on-page content analysis, XML sitemaps and much more.
  * Author:      Team Yoast
@@ -20,7 +20,7 @@
  * Requires PHP: 7.4
  *
  * WC requires at least: 7.1
- * WC tested up to: 10.3
+ * WC tested up to: 10.4
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
