8e49b492923a4bf6f00055831e5b839cb80caecd
Leonidas Milosis <leonidas.milossis@gmail.com>
2025-12-16 10:07:41 +0200

    Add unit tests

---
+++ tests/Unit/Tracking/Infrastructure/Tracking_On_Page_Load/Abstract_Tracking_On_Page_Load_Integration_Test.php
@@ -0,0 +1,57 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Tests\Unit\Tracking\Infrastructure\Tracking_On_Page_Load;
+
+use Mockery;
+use Yoast\WP\SEO\Helpers\Capability_Helper;
+use Yoast\WP\SEO\Tests\Unit\TestCase;
+use Yoast\WP\SEO\Tracking\Application\Action_Tracker;
+use Yoast\WP\SEO\Tracking\Infrastructure\Tracking_On_Page_Load_Integration;
+
+/**
+ * Abstract class for the Tracking_On_Page_Load_Integration tests.
+ *
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded
+ *
+ * @group tracking
+ */
+abstract class Abstract_Tracking_On_Page_Load_Integration_Test extends TestCase {
+
+	/**
+	 * Holds the instance.
+	 *
+	 * @var Tracking_On_Page_Load_Integration
+	 */
+	protected $instance;
+
+	/**
+	 * Holds the action tracker.
+	 *
+	 * @var Mockery\MockInterface|Action_Tracker
+	 */
+	protected $action_tracker;
+
+	/**
+	 * Holds the capability helper.
+	 *
+	 * @var Mockery\MockInterface|Capability_Helper
+	 */
+	protected $capability_helper;
+
+	/**
+	 * Sets up the test fixtures.
+	 *
+	 * @return void
+	 */
+	protected function set_up() {
+		parent::set_up();
+
+		$this->action_tracker    = Mockery::mock( Action_Tracker::class );
+		$this->capability_helper = Mockery::mock( Capability_Helper::class );
+
+		$this->instance = new Tracking_On_Page_Load_Integration(
+			$this->action_tracker,
+			$this->capability_helper
+		);
+	}
+}
---
+++ tests/Unit/Tracking/Infrastructure/Tracking_On_Page_Load/Constructor_Test.php
@@ -0,0 +1,32 @@
+<?php
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Tests\Unit\Tracking\Infrastructure\Tracking_On_Page_Load;
+
+use Yoast\WP\SEO\Helpers\Capability_Helper;
+use Yoast\WP\SEO\Tracking\Application\Action_Tracker;
+
+/**
+ * Tests the Tracking_On_Page_Load_Integration constructor.
+ *
+ * @group tracking
+ *
+ * @covers Yoast\WP\SEO\Tracking\Infrastructure\Tracking_On_Page_Load_Integration::__construct
+ */
+final class Constructor_Test extends Abstract_Tracking_On_Page_Load_Integration_Test {
+
+	/**
+	 * Tests the constructor.
+	 *
+	 * @return void
+	 */
+	public function test_constructor() {
+		$this->assertInstanceOf(
+			Action_Tracker::class,
+			$this->getPropertyValue( $this->instance, 'action_tracker' )
+		);
+		$this->assertInstanceOf(
+			Capability_Helper::class,
+			$this->getPropertyValue( $this->instance, 'capability_helper' )
+		);
+	}
+}
