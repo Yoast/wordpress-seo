ad2e5effd70448d109e9b2b9690b65ef664b0d64
Enrico Battocchi <enrico@yoast.com>
2026-01-12 12:38:39 +0100

    Sets the minimum supported WordPress version to 6.8.

--- wp-seo.php
+++ wp-seo.php
@@ -16,7 +16,7 @@
  * Text Domain: wordpress-seo
  * Domain Path: /languages/
  * License:     GPL v3
- * Requires at least: 6.7
+ * Requires at least: 6.8
  * Requires PHP: 7.4
  *
  * WC requires at least: 7.1
