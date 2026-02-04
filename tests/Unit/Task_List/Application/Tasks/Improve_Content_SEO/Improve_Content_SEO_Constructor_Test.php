9c4533ef43051326d9143bf9b4850028185b29d5
Leonidas Milosis <leonidas.milossis@gmail.com>
2026-02-04 14:49:56 +0200

    Prevent showing tasks about content SEO if not applicable

--- tests/Unit/Task_List/Application/Tasks/Improve_Content_SEO/Improve_Content_SEO_Constructor_Test.php
+++ tests/Unit/Task_List/Application/Tasks/Improve_Content_SEO/Improve_Content_SEO_Constructor_Test.php
@@ -4,6 +4,8 @@
 // phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
 namespace Yoast\WP\SEO\Tests\Unit\Task_List\Application\Tasks\Improve_Content_SEO;
 
+use Yoast\WP\SEO\Editors\Application\Analysis_Features\Enabled_Analysis_Features_Repository;
+use Yoast\WP\SEO\Helpers\Indexable_Helper;
 use Yoast\WP\SEO\Task_List\Infrastructure\Indexables\Recent_Content_Indexable_Collector;
 
 /**
@@ -26,6 +28,14 @@ final class Improve_Content_SEO_Constructor_Test extends Abstract_Improve_Conten
 		$this->assertInstanceOf(
 			Recent_Content_Indexable_Collector::class,
 			$this->getPropertyValue( $this->instance, 'recent_content_indexable_collector' )
+		);
+		$this->assertInstanceOf(
+			Indexable_Helper::class,
+			$this->getPropertyValue( $this->instance, 'indexable_helper' )
+		);
+		$this->assertInstanceOf(
+			Enabled_Analysis_Features_Repository::class,
+			$this->getPropertyValue( $this->instance, 'enabled_analysis_features_repository' )
 		);
 	}
 }
