d5325e2b6090f5d035988100f5c477623a6edc13
JordiPV <llimonero_4t@hotmail.com>
2026-02-05 15:51:48 +0100

    Update TaskListOptInNotification styles and extend Tailwind CSS content paths

--- packages/ui-library/tailwind.config.js
+++ packages/ui-library/tailwind.config.js
@@ -1,4 +1,10 @@
 module.exports = {
 	presets: [ require( "@yoast/tailwindcss-preset" ) ],
-	content: [ "./src/**/*.js", "./src/**/*.mdx", "./src/**/*.md" ],
+	content: [
+		"./src/**/*.js",
+		"./src/**/*.mdx",
+		"./src/**/*.md",
+		// Scan packages/js for Tailwind classes used in WordPress plugin components
+		"../js/src/**/*.js",
+	],
 };
