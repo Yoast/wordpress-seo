9e1d39144c21f348dca03b371785f38749c4a1ff
Vraja Das <vraja.pro@gmail.com>
2026-02-10 16:37:45 +0200

    fix svg props, taken care by the hook

--- packages/ui-library/src/elements/ScoreIcon/icons/emoji-neutral-icon.js
+++ packages/ui-library/src/elements/ScoreIcon/icons/emoji-neutral-icon.js
@@ -7,9 +7,7 @@ import React from "react";
  */
 export const EmojiNeutralIcon = ( props ) => (
 	<svg
-		aria-hidden="true" role="img" focusable="false" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512"
-		fill="#ee7c1b"
-		{ ...props }
+		xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" fill="#ee7c1b" { ...props }
 	>
 		<path d="M248 8c137 0 248 111 248 248S385 504 248 504S0 393 0 256S111 8 248 8z M360 208c0-17.7-14.3-32-32-32 s-32 14.3-32 32s14.3 32 32 32S360 225.7 360 208z M344 368c21.2 0 21.2-32 0-32H152c-21.2 0-21.2 32 0 32H344z M200 208 c0-17.7-14.3-32-32-32s-32 14.3-32 32s14.3 32 32 32S200 225.7 200 208z" />
 	</svg>
