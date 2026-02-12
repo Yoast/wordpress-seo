7143dde1175bb7aa748f3ab9dfc59a0adcdea2ae
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2026-02-12 12:57:55 +0100

    Add global indexable alert.

---
+++ tests/Unit/Alerts/Application/Indexables_Disabled/Indexables_Disabled_Alert_Constructor_Test.php
@@ -0,0 +1,40 @@
+<?php
+
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\Tests\Unit\Alerts\Application\Indexables_Disabled;
+
+use Yoast\WP\SEO\Helpers\Indexable_Helper;
+use Yoast\WP\SEO\Helpers\Short_Link_Helper;
+use Yoast_Notification_Center;
+
+/**
+ * Test class for the constructor.
+ *
+ * @group Indexables_Disabled
+ *
+ * @covers Yoast\WP\SEO\Alerts\Application\Indexables_Disabled\Indexables_Disabled_Alert::__construct
+ *
+ * @phpcs:disable Yoast.NamingConventions.ObjectNameDepth.MaxExceeded
+ */
+final class Indexables_Disabled_Alert_Constructor_Test extends Abstract_Indexables_Disabled_Alert_Test {
+
+	/**
+	 * Tests if the needed attributes are set correctly.
+	 *
+	 * @return void
+	 */
+	public function test_constructor() {
+		$this->assertInstanceOf(
+			Yoast_Notification_Center::class,
+			$this->getPropertyValue( $this->instance, 'notification_center' )
+		);
+		$this->assertInstanceOf(
+			Indexable_Helper::class,
+			$this->getPropertyValue( $this->instance, 'indexable_helper' )
+		);
+		$this->assertInstanceOf(
+			Short_Link_Helper::class,
+			$this->getPropertyValue( $this->instance, 'short_link_helper' )
+		);
+	}
+}
