ed16098543333708465410226f90537f26c20d6b
Leonidas Milosis <leonidas.milossis@gmail.com>
2026-02-17 09:20:40 +0200

    Tweak copy

--- src/task-list/application/tasks/create-new-content.php
+++ src/task-list/application/tasks/create-new-content.php
@@ -108,7 +108,7 @@ class Create_New_Content extends Abstract_Task {
 			\__( 'Create new content', 'wordpress-seo' ),
 			\sprintf(
 				/* translators: %1$s and %3$s expands to an opening p tag, %2$s and %5$s expand to a closing p tag and %4$s expands to Yoast */
-				\__( '%1$sLong gaps without new content slow down your traffic growth. Publishing regularly gives search engines and visitors a reason to return.%2$s%3$sPlan a topic, write your post, and use the %4$s SEO and Readability Analysis to refine it before publishing.%5$s', 'wordpress-seo' ),
+				\__( '%1$sLong gaps without new content slow down your traffic growth. Publishing regularly gives search engines and visitors a reason to return.%2$s%3$sPlan a topic, write your post, and use the %4$s SEO and Readability Analyses to refine it before publishing.%5$s', 'wordpress-seo' ),
 				'<p>',
 				'</p>',
 				'<p>',
