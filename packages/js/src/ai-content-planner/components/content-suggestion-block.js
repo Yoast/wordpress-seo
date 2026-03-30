1d6d2b34fd36811c296e0384bbdf05ec59367a85
Vraja Das <vraja.pro@gmail.com>
2026-03-30 23:45:59 +0300

    add font family class for consistency across themes
    rename props and remove title

--- packages/js/src/ai-content-planner/components/content-suggestion-block.js
+++ packages/js/src/ai-content-planner/components/content-suggestion-block.js
@@ -5,15 +5,14 @@ import { useSvgAria } from "@yoast/ui-library";
 /**
  * ContentSuggestionBlock component.
  *
- * @param {string} title The title of the content suggestion block.
- * @param {string[]} suggestions The content suggestions to display.
+ * @param {string[]} contentNotes The content notes to display.
  *
  * @returns {JSX.Element} The content suggestion block component.
  */
-export const ContentSuggestionBlock = ( { title, suggestions } ) => {
+export const ContentSuggestionBlock = ( { contentNotes } ) => {
 	const svgAriaProps = useSvgAria();
 	return (
-		<div className="yst-bg-slate-50 yst-rounded-sm yst-shadow-sm yst-border yst-border-solid yst-border-slate-200 yst-p-4">
+		<div className="yst-bg-slate-50 yst-font-sans yst-rounded-sm yst-shadow-sm yst-border yst-border-solid yst-border-slate-200 yst-p-4">
 			<div className="yst-text-slate-500 yst-text-xs yst-flex yst-items-center yst-mb-4">
 				<EyeIcon className="yst-inline-block yst-w-3 yst-h-3 yst-mr-1 yst-shrink-0" { ...svgAriaProps } />
 				<span className="yst-text-xs yst-leading-3">{ __( "Only visible to you", "wordpress-seo" ) }</span>
@@ -23,14 +22,14 @@ export const ContentSuggestionBlock = ( { title, suggestions } ) => {
 					<div className="yst-flex yst-items-center yst-justify-center yst-w-4 yst-h-4 yst-bg-ai-500 yst-rounded-[1.5px]">
 						<DocumentIcon className="yst-w-3 yst-h-3 yst-shrink-0 yst-text-white yst-flex yst-items-center yst-justify-center" { ...svgAriaProps } />
 					</div>
-					<h3 className="yst-text-slate-800 yst-text-sm yst-leading-5 yst-font-medium yst-m-0">{ title }</h3>
+					<h3 className="yst-text-slate-800 yst-text-sm yst-leading-5 yst-font-medium yst-m-0">{ __( "Content notes", "wordpress-seo" ) }</h3>
 				</div>
-				{ suggestions.length > 0 &&
+				{ contentNotes.length > 0 &&
 				<ul className="yst-text-sm yst-text-slate-600 yst-font-normal yst-my-0 yst-ps-5 yst-leading-5 yst-list-none">
-					{ suggestions.map( ( suggestion, index ) => (
-						<li className="yst-flex yst-justify-start yst-gap-2" key={ `${index}-${title}` }>
+					{ contentNotes.map( ( note, index ) => (
+						<li className="yst-flex yst-justify-start yst-gap-2" key={ `${index}-${note}` }>
 							<span aria-hidden="true">•</span>
-							{ suggestion }</li>
+							{ note }</li>
 					) ) }
 				</ul>
 				}
