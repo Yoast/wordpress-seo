02caaf27818f080614eaa51b54def8d07dfd0e8b
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2026-04-03 14:02:52 +0200

    Add the endpoint to the front-end

---
+++ src/ai/content-planner/infrastructure/endpoints/content-planner-endpoint.php
@@ -0,0 +1,50 @@
+<?php
+
+// phpcs:disable Yoast.NamingConventions.NamespaceName.MaxExceeded
+
+// phpcs:disable Yoast.NamingConventions.NamespaceName.TooLong -- Needed in the folder structure.
+namespace Yoast\WP\SEO\AI\Content_Planner\Infrastructure\Endpoints;
+
+use Yoast\WP\SEO\AI\Content_Planner\User_Interface\Content_Planner_Route;
+
+/**
+ * Represents the content planner endpoint.
+ */
+class Content_Planner_Endpoint implements Content_Planner_Endpoint_Interface {
+
+	/**
+	 * Gets the name.
+	 *
+	 * @return string
+	 */
+	public function get_name(): string {
+		return 'contentPlanner';
+	}
+
+	/**
+	 * Gets the namespace.
+	 *
+	 * @return string
+	 */
+	public function get_namespace(): string {
+		return Content_Planner_Route::ROUTE_NAMESPACE;
+	}
+
+	/**
+	 * Gets the route.
+	 *
+	 * @return string
+	 */
+	public function get_route(): string {
+		return Content_Planner_Route::ROUTE_PREFIX;
+	}
+
+	/**
+	 * Gets the URL.
+	 *
+	 * @return string
+	 */
+	public function get_url(): string {
+		return \rest_url( $this->get_namespace() . $this->get_route() );
+	}
+}
