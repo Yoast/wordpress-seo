12434154a89a95ffbef70e057780cf735034e687
JordiPV <llimonero_4t@hotmail.com>
2026-04-13 14:16:04 +0200

    Refactor Content Planner editor item to use withDispatch for modal management

--- packages/js/src/ai-content-planner/components/content-planner-editor-item.js
+++ packages/js/src/ai-content-planner/components/content-planner-editor-item.js
@@ -1,19 +1,16 @@
 import { __ } from "@wordpress/i18n";
 import { useCallback } from "@wordpress/element";
-import { useDispatch } from "@wordpress/data";
 import { Button, Root } from "@yoast/ui-library";
-import { FEATURE_MODAL_STORE } from "../constants";
 
 /**
  * The section for the content planner feature in the Yoast sidebar.
  *
- * @param {Object}  props           The component props.
- * @param {string}  props.location  The location where the editor item is rendered. Can be "sidebar" or "metabox".
+ * @param {Object}   props           The component props.
+ * @param {string}   props.location  The location where the editor item is rendered. Can be "sidebar" or "metabox".
+ * @param {Function} props.openModal The function to open the content planner modal.
  * @returns {JSX.Element} The Content Planner section in the sidebar.
  */
-export const ContentPlannerEditorItem = ( { location } ) => {
-	const { openModal } = useDispatch( FEATURE_MODAL_STORE );
-
+export const ContentPlannerEditorItem = ( { location, openModal } ) => {
 	const handleClick = useCallback( () => {
 		openModal( false );
 	}, [ openModal ] );
--- packages/js/src/ai-content-planner/containers/content-planner-editor-item.js
+++ packages/js/src/ai-content-planner/containers/content-planner-editor-item.js
@@ -1,3 +1,13 @@
+import { compose } from "@wordpress/compose";
+import { withDispatch } from "@wordpress/data";
 import { ContentPlannerEditorItem } from "../components/content-planner-editor-item";
+import { CONTENT_PLANNER_STORE } from "../constants";
 
-export default ContentPlannerEditorItem;
+export default compose( [
+	withDispatch( ( dispatch ) => {
+		const { openModal } = dispatch( CONTENT_PLANNER_STORE );
+		return {
+			openModal,
+		};
+	} ),
+] )( ContentPlannerEditorItem );
