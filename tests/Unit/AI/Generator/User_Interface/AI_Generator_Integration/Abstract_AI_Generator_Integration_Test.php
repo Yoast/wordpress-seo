a82a6daf69a3d557bf7bd8b3164cb51a4f512ae5
Thijs van der heijden <thijsvanderheijden2@gmail.com>
2026-04-14 11:04:23 +0200

    cs

--- tests/Unit/AI/Generator/User_Interface/AI_Generator_Integration/Abstract_AI_Generator_Integration_Test.php
+++ tests/Unit/AI/Generator/User_Interface/AI_Generator_Integration/Abstract_AI_Generator_Integration_Test.php
@@ -110,15 +110,15 @@ abstract class Abstract_AI_Generator_Integration_Test extends TestCase {
 	public function setUp(): void {
 		parent::setUp();
 
-		$this->asset_manager                        = Mockery::mock( WPSEO_Admin_Asset_Manager::class );
-		$this->addon_manager                        = Mockery::mock( WPSEO_Addon_Manager::class );
-		$this->api_client                           = Mockery::mock( API_Client::class );
-		$this->current_page_helper                  = Mockery::mock( Current_Page_Helper::class );
-		$this->options_helper                       = Mockery::mock( Options_Helper::class );
-		$this->user_helper                          = Mockery::mock( User_Helper::class );
-		$this->introductions_seen_repository        = Mockery::mock( Introductions_Seen_Repository::class );
-		$this->generator_endpoints_repository       = Mockery::mock( Generator_Endpoints_Repository::class );
-		$this->consent_endpoints_repository         = Mockery::mock( Consent_Endpoints_Repository::class );
+		$this->asset_manager                    = Mockery::mock( WPSEO_Admin_Asset_Manager::class );
+		$this->addon_manager                    = Mockery::mock( WPSEO_Addon_Manager::class );
+		$this->api_client                       = Mockery::mock( API_Client::class );
+		$this->current_page_helper              = Mockery::mock( Current_Page_Helper::class );
+		$this->options_helper                   = Mockery::mock( Options_Helper::class );
+		$this->user_helper                      = Mockery::mock( User_Helper::class );
+		$this->introductions_seen_repository    = Mockery::mock( Introductions_Seen_Repository::class );
+		$this->generator_endpoints_repository   = Mockery::mock( Generator_Endpoints_Repository::class );
+		$this->consent_endpoints_repository     = Mockery::mock( Consent_Endpoints_Repository::class );
 		$this->free_sparks_endpoints_repository = Mockery::mock( Free_Sparks_Endpoints_Repository::class );
 
 		$this->instance = new Ai_Generator_Integration(
