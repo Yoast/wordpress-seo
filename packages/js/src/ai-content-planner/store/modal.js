50ae8e58c54484eeb4b2479b11d0e7d45534017b
Vraja Das <vraja.pro@gmail.com>
2026-04-15 19:39:43 +0300

    Todo: for now close modal on error

--- packages/js/src/ai-content-planner/store/modal.js
+++ packages/js/src/ai-content-planner/store/modal.js
@@ -39,9 +39,11 @@ const slice = createSlice( {
 		} );
 		builder.addCase( `${ FETCH_CONTENT_OUTLINE_ACTION_NAME }/${ ASYNC_ACTION_NAMES.error }`, ( state ) => {
 			state.featureModalStatus = FEATURE_MODAL_STATUS.error;
+			state.isOpen = false;
 		} );
 		builder.addCase( `${ FETCH_CONTENT_SUGGESTIONS_ACTION_NAME }/${ ASYNC_ACTION_NAMES.error }`, ( state ) => {
 			state.featureModalStatus = FEATURE_MODAL_STATUS.error;
+			state.isOpen = false;
 		} );
 	},
 } );
