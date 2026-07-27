d78be96a516fa4ec9133d5f3f249943d1d03ff7b
Vraja Das <vraja.pro@gmail.com>
2026-07-27 12:59:26 +0300

    fix: add learn more link to match ai generator

--- packages/js/src/bulk-editor/initialize.js
+++ packages/js/src/bulk-editor/initialize.js
@@ -40,6 +40,7 @@ domReady( () => {
 				isAvailable: Boolean( myyoastConnection ) && get( myyoastConnection, "isProvisioned", false ),
 				canConnect: get( myyoastConnection, "canConnect", false ),
 				connectUrl: get( myyoastConnection, "connectUrl", null ),
+				learnMoreUrl: get( myyoastConnection, "learnMoreUrl", "" ),
 			},
 		},
 	} );
