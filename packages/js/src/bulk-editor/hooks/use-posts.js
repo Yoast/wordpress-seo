cc06e862aca23043c2db67b8bb4753353a84b4d9
Vraja Das <vraja.pro@gmail.com>
2026-09-10 22:52:20 +0300

    fix: complexity

--- packages/js/src/bulk-editor/hooks/use-posts.js
+++ packages/js/src/bulk-editor/hooks/use-posts.js
@@ -1,6 +1,7 @@
 import { useDispatch, useSelect } from "@wordpress/data";
 import { useCallback, useEffect, useMemo, useRef, useState } from "@wordpress/element";
 import { NEEDS_IMPROVEMENT_FIELD_PARAMS, PAGE_SIZE, STORE_NAME } from "../constants";
+import { get } from "lodash";
 
 /**
  * Maps a single API row (snake_case) to a {@link BulkEditorItem} (camelCase).
@@ -19,13 +20,13 @@ const formatPost = ( post ) => ( {
 	metaDescription: post.meta_description,
 	socialTitle: post.social_title,
 	socialDescription: post.social_description,
-	seoTitleFallback: post.seo_title_fallback ?? "",
-	metaDescriptionFallback: post.meta_description_fallback ?? "",
-	socialTitleFallback: post.social_title_fallback ?? "",
-	socialDescriptionFallback: post.social_description_fallback ?? "",
+	seoTitleFallback: get( post, "seo_title_fallback", "" ),
+	metaDescriptionFallback: get( post, "meta_description_fallback", "" ),
+	socialTitleFallback: get( post, "social_title_fallback", "" ),
+	socialDescriptionFallback: get( post, "social_description_fallback", "" ),
 	editable: post.editable,
-	needsImprovement: post.needs_improvement ?? {},
-	images: post.images ?? [],
+	needsImprovement: get( post, "needs_improvement", {} ),
+	images: get( post, "images", [] ),
 } );
 
 /**
